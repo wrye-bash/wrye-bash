23034f83cf150a6a099cb82d518ffa9563cba1e9
pacmorrowind <pacmorrowind@refactor_bash.com>
2010-04-13 06:52:45 +0000

    slight improved message re: bashed patch rebuil and delinquent/missing masters.
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@271 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/basher.py
+++ Mopy/basher.py
@@ -8638,7 +8638,7 @@ class Mod_Patch_Update(Link):
                     text += '    %s: %s\n' % (label,master.s)
             previousMods.add(mod)
         if text:
-            balt.showWarning(self.window,(_('WARNING!\nThe following mods have master file errors:\n%sPlease adjust load order to rectify those probems before continuing.') % (text)),_("Missing or Delinquent master error"))
+            balt.showWarning(self.window,(_('WARNING!\nThe following mod(s) have master file error(s):\n%sPlease adjust your load order to rectify those probem(s) before continuing.') % (text)),_("Missing or Delinquent Master Errors"))
             return
         fileName = GPath(self.data[0])
         fileInfo = bosh.modInfos[fileName]
