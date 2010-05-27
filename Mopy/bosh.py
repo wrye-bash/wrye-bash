ec459fea501596eea577c0f6d2f64535caf573ef
pacmorrowind <pacmorrowind@refactor_bash.com>
2010-05-27 20:02:37 +0000

    extra check for non-existant editor id (!) or nonexistent model path for as intended creatures
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@400 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bosh.py
+++ Mopy/bosh.py
@@ -20013,7 +20013,7 @@ class AsIntendedBoarsPatcher(BasalCreatureTweaker):
         spell = (GPath('Oblivion.esm'), 0x02B54E)
         keep = patchFile.getKeeper()
         for record in patchFile.CREA.records:
-            if not record.full: continue #for unnamed creatures else next if crashes.
+            if not record.full or not record.eid or not record.model.modPath: continue #for unnamed creatures else next if crashes.
             if 'boar' in record.full.lower() or 'boar' in record.model.modPath.lower() or 'boar' in record.eid.lower():
                 if spell not in record.spells:
                     record.spells.append(spell)
