67f1c38b151174e7ce51d8357044eebae3867ddb
lojack5 <lojack5@refactor_bash.com>
2011-03-15 11:21:12 +0000

    - Updated packaging script:   - It will now also build the installer version (if NSIS is installed)   - It sets the correct version for the installer   - I had to add the DebugLog(Python2.6).bat, because the nsi script wouldn't compile without it. (PM take a look maybe?)
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@889 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Package For Release.py
+++ Package For Release.py
@@ -1,20 +1,31 @@
+# Simple Python scrip to package up the the various Wrye Bash
+# Files into archives for release.
+#
+# command line options:
+#  none             -> Build all versions
+#  -m OR -manual    -> Build the Manual (archive) version
+#  -w OR -wbsa      -> Build the Standalone version
+#  -i or -installer -> Build the Installer version
 import subprocess
 import os
 import shutil
 import re
+import sys
+import _winreg
+
+def GetWryeBashVersion():
+    readme = r'.\Mopy\Wrye Bash.txt'
+    if os.path.exists(readme):
+        file = open(readme, 'r')
+        reVersion = re.compile('^=== ([\.\d]+) \[')
+        for line in file:
+            maVersion = reVersion.match(line)
+            if maVersion:
+                return maVersion.group(1)
+    return 'SVN'
 
 # Archives to make
-WB = 'SVN'
-readme = r'.\Mopy\Wrye Bash.txt'
-if os.path.exists(readme):
-    file = open(readme)
-    reVersion = re.compile('^=== ([\.\d]+) \[')
-    for line in file:
-        maVersion = reVersion.match(line)
-        if maVersion:
-            WB = maVersion.group(1)
-            break
-    file.close()
+WB = GetWryeBashVersion()
 archive =   'Wrye Bash %s -- Archive Version.7z'    % WB
 installer = 'Wrye Bash %s -- Installer Version.exe' % WB
 WBSA =      'Wrye Bash %s -- Standalone Version.7z' % WB
@@ -34,9 +45,35 @@ def BuildStandaloneVersion():
     os.chdir(dir)
 
 def BuildInstallerVersion():
-    # Create the installer version
-    # TODO: DO THIS!
-    pass
+    # Edit the NSIS script to change the version
+    script = 'Wrye Bash.nsi'
+    if not os.path.exists(script): return
+    
+    file = open(script, 'rb')
+    lines = file.readlines()
+    file.close()
+
+    reVersion = re.compile('(\s+\!define\s+WB_NAME\s+)"(.+)"(.+)')
+    for i,line in enumerate(lines):
+        # Make sure we're actually overwriting what we want
+        maVersion = reVersion.match(line)
+        if maVersion:
+            newline = maVersion.group(1) + ('"Wrye Bash %s"' % WB) + maVersion.group(3) + '\n'
+            if line == newline:
+                break
+            lines[i] = newline
+            file = open(script, 'wb')
+            file.writelines(lines)
+            file.close()
+            break
+
+    # Now compile the NSIS script
+    try:
+        nsis = _winreg.QueryValue(_winreg.HKEY_LOCAL_MACHINE, r'Software\NSIS')
+        nsis = os.path.join(nsis, 'makensis.exe')
+        subprocess.call([nsis, 'Wrye Bash.nsi'], shell=True)
+    except:
+        pass
 
 def rmv(path):
     """Helper function to remove if a file/directory exists"""
@@ -46,6 +83,15 @@ def rmv(path):
         else:
             os.remove(path)
 
-BuildManualVersion()
-BuildStandaloneVersion()
-BuildInstallerVersion()
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        BuildManualVersion()
+        BuildStandaloneVersion()
+        BuildInstallerVersion()
+    else:
+        if '-m' in sys.argv or '-manual' in sys.argv:
+            BuildManualVersion()
+        if '-w' in sys.argv or '-wbsa' in sys.argv:
+            BuildStandaloneVersion()
+        if '-i' in sys.argv or '-installer' in sys.argv:
+            BuildInstallerVersion()
