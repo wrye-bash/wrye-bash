e4ea3874b4feb7afe9a5389f7cff048aafb2ef58
lojack5 <lojack5@refactor_bash.com>
2011-04-23 19:07:30 +0000

    -Undid my version of the AIPackages patcher, it relied on python 2.7+ only code. -Fixed a bug where sometimes some settings weren't saved
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@987 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bosh.py
+++ Mopy/bosh.py
@@ -20452,6 +20452,7 @@ class CBash_NPCAIPackagePatcher(CBash_ImportPatcher):
         """Returns the group types that this patcher checks"""
         return ['CREA','NPC_']
     #--Patch Phase ------------------------------------------------------------
+    '''
     def scan(self,modFile,record,bashTags):
         """Records information needed to apply the patch."""
         recordId = record.fid
@@ -20489,9 +20490,9 @@ class CBash_NPCAIPackagePatcher(CBash_ImportPatcher):
                 mergedPackages -= prevDeleted
                 self.mergedPackageList[recordId] = mergedPackages
                 break
-
+    '''
     # PM's version, not quite working right
-    def scan_old(self,modFile,record,bashTags):
+    def scan(self,modFile,record,bashTags):
         """Records information needed to apply the patch."""
         if modFile.GName in self.srcs:
             recordId = record.fid
