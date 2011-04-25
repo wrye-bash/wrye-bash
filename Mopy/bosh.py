238d15c40f554e32078630918325fa436d9be05b
lojack5 <lojo.jacob@gmail.com>
2011-04-25 04:55:15 +0000

    oops, used min where I should've used max
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@994 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bosh.py
+++ Mopy/bosh.py
@@ -31765,7 +31765,7 @@ class CBash_RacePatcher_Eyes(SpecialPatcher):
         mod_npcsFixed = self.mod_npcsFixed
         ObCollection = patchFile.ObCollection
         subProgress = SubProgress(progress)
-        subProgress.setFull(min(len(ObCollection.LoadOrderMods) * 2,1))
+        subProgress.setFull(min(max(ObCollection.LoadOrderMods) * 2,1))
         reX117 = self.reX117
         defaultEyes = {}
         defaultMaleHair = {}
