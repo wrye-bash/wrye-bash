2651c8a5d65d33956e6c47086379152a7b322821
lojack5 <lojack5@refactor_bash.com>
2011-06-02 04:53:48 +0000

    Should fix the 'Encode' not found in the translator function _().  Only shows up if the user is using a translation file, as far as I can tell.
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1136 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bolt.py
+++ Mopy/bolt.py
@@ -46,6 +46,50 @@ if os.name == 'nt':
         import _subprocess
         startupinfo.dwFlags |= _subprocess.STARTF_USESHOWWINDOW
 
+
+# Unicode Strings -------------------------------------------------------------
+# See Python's "aliases.py" for a list of possible encodings
+UnicodeEncodings = (
+    # Encodings we'll try for conversion to unicode
+    'UTF8',     # Standard encoding
+    'U16',      # Some files use UTF-16 though
+    'cp1252',   # Western Europe
+    'cp500',    # Western Europe
+    'cp932',    # Japanese SJIS-win 
+    'mbcs',     # Multi-byte character set (depends on the Windows locale)
+    )
+NumEncodings = len(UnicodeEncodings)
+
+def Unicode(name):
+    if not bUseUnicode: return name #don't change if not unicode mode.
+    if isinstance(name,unicode): return name
+    if isinstance(name,str):
+        for i in range(NumEncodings):
+            try:
+                return unicode(name,UnicodeEncodings[i])
+            except UnicodeDecodeError:
+                if i == NumEncodings - 1:
+                    raise
+    return name
+
+def Encode(name,tryFirstEncoding=False):
+    if not bUseUnicode: return name #don't change if not unicode mode.
+    if isinstance(name,str): return name
+    if isinstance(name,unicode):
+        if tryFirstEncoding:
+            try:
+                return name.encode(tryFirstEncoding)
+            except UnicodeEncodeError: 
+                deprint("Unable to encode '%s' in %s" % (name, tryFirstEncoding))
+                pass
+        for i in range(NumEncodings):
+            try:
+                return name.encode(UnicodeEncodings[i])
+            except UnicodeEncodeError:
+                if i == NumEncodings - 1:
+                    raise
+    return name
+
 # Localization ----------------------------------------------------------------
 #used instead of bosh.inisettings['EnableUnicode'] to avoid circular imports
 #has to be set by bolt before any Path's are instantiated
@@ -216,49 +260,6 @@ class LString(object):
     def __cmp__(self, other):
         if isinstance(other,LString): return cmp(self._cs, other._cs)
         else: return cmp(self._cs, other.lower())
-
-# Unicode Strings -------------------------------------------------------------
-# See Python's "aliases.py" for a list of possible encodings
-UnicodeEncodings = (
-    # Encodings we'll try for conversion to unicode
-    'UTF8',     # Standard encoding
-    'U16',      # Some files use UTF-16 though
-    'cp1252',   # Western Europe
-    'cp500',    # Western Europe
-    'cp932',    # Japanese SJIS-win 
-    'mbcs',     # Multi-byte character set (depends on the Windows locale)
-    )
-NumEncodings = len(UnicodeEncodings)
-
-def Unicode(name):
-    if not bUseUnicode: return name #don't change if not unicode mode.
-    if isinstance(name,unicode): return name
-    if isinstance(name,str):
-        for i in range(NumEncodings):
-            try:
-                return unicode(name,UnicodeEncodings[i])
-            except UnicodeDecodeError:
-                if i == NumEncodings - 1:
-                    raise
-    return name
-
-def Encode(name,tryFirstEncoding=False):
-    if not bUseUnicode: return name #don't change if not unicode mode.
-    if isinstance(name,str): return name
-    if isinstance(name,unicode):
-        if tryFirstEncoding:
-            try:
-                return name.encode(tryFirstEncoding)
-            except UnicodeEncodeError: 
-                deprint("Unable to encode '%s' in %s" % (name, tryFirstEncoding))
-                pass
-        for i in range(NumEncodings):
-            try:
-                return name.encode(UnicodeEncodings[i])
-            except UnicodeEncodeError:
-                if i == NumEncodings - 1:
-                    raise
-    return name
 
 # Paths -----------------------------------------------------------------------
 #------------------------------------------------------------------------------
