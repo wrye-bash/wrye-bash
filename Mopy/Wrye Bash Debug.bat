b238ccfa281a8f362a307ff7ea37ffeba9248bb6
myktaylor <myktaylor@refactor_bash.com>
2011-08-21 02:41:58 +0000

    apply some "for" trickery to capture paths with spaces in them
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1585 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/Wrye Bash Debug.bat
+++ Mopy/Wrye Bash Debug.bat
@@ -1,29 +1,30 @@
 @ECHO OFF && PUSHD "%~dp0" && SETLOCAL
 
 SET OUTFILE=BashBugDump.log
+SET PYPATH=
 
 :: get python path from registry
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=4*" %%i in (`%REGCMD%`) do SET PYPATH=%%i
-IF NOT x%PYPATH%==x GOTO FOUND
+%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=4*" %%i in (`%REGCMD%`) do SET PYPATH=%%i
-IF NOT x%PYPATH%==x GOTO FOUND
+%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=4*" %%i in (`%REGCMD%`) do SET PYPATH=%%i
-IF NOT x%PYPATH%==x GOTO FOUND
+%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=4*" %%i in (`%REGCMD%`) do SET PYPATH=%%i
-IF NOT x%PYPATH%==x GOTO FOUND
+%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 ECHO Python not found >%OUTFILE%
 GOTO END
 
 :FOUND
-%PYPATH%/Python.exe "Wrye Bash Launcher.pyw" -d >%OUTFILE% 2>&1
+"%PYPATH%"Python.exe "Wrye Bash Launcher.pyw" -d >%OUTFILE% 2>&1
 
 :END
 ENDLOCAL && EXIT /B
