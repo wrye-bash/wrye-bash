9c9dcbf409262b585a8c409e1faeff03e89b1830
lojack5 <lojack5@refactor_bash.com>
2011-08-21 06:51:29 +0000

    Debug batch file should work on my OS's now: tested on XP and Win7
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1590 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/Wrye Bash Debug.bat
+++ Mopy/Wrye Bash Debug.bat
@@ -5,27 +5,28 @@ SET PYPATH=
 
 
 :: determine whether reg has the newer or older response format
-SET REGCMD=reg query HKLM\SOFTWARE\Classes\.exe >NUL
-FOR /F "skip=4 usebackq tokens=1" %%i in (`%REGCMD%`) do SET FORMATCHECK=%%i
-SET SKIPVAL=3
-IF x"%FORMATCHECK%"==x"<NO" SET SKIPVAL=4
+SET REGCMD=reg query HKLM\SOFTWARE\Classes\.exe /ve >NUL
+FOR /F "skip=1 usebackq tokens=1" %%i in (`%REGCMD%`) do SET FORMATCHECK=%%i
+SET SKIPVAL=1
+SET TOKENS=2
+IF x"%FORMATCHECK%"==x"<NO" SET SKIPVAL=4 && SET TOKENS=3
 
 
 :: get python path from registry
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 ECHO Python not found >%OUTFILE%
