ac0e5e8b5794245177303aab873d3f74050b0f84
myktaylor <myktaylor@refactor_bash.com>
2011-08-21 05:49:31 +0000

    detect the output format of reg and adjust accordingly
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1589 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/Wrye Bash Debug.bat
+++ Mopy/Wrye Bash Debug.bat
@@ -3,21 +3,29 @@
 SET OUTFILE=BashBugDump.log
 SET PYPATH=
 
+
+:: determine whether reg has the newer or older response format
+SET REGCMD=reg query HKLM\SOFTWARE\Classes\.exe >NUL
+FOR /F "skip=4 usebackq tokens=1" %%i in (`%REGCMD%`) do SET FORMATCHECK=%%i
+SET SKIPVAL=3
+IF x"%FORMATCHECK%"==x"<NO" SET SKIPVAL=4
+
+
 :: get python path from registry
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.7\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKLM\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
-%REGCMD% >NUL 2>&1 && FOR /F "skip=4 usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
+%REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=3*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
 ECHO Python not found >%OUTFILE%
