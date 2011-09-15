544d64ec9b4f9559aa4c2df9bf75e8594414b041
myktaylor <myktaylor@refactor_bash.com>
2011-09-15 09:34:39 +0000

    - implement OS-version- and locale-independent detection routines for interpreting reg query output - if python install path not found in registry, query .py file association
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1728 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/Wrye Bash Debug.bat
+++ Mopy/Wrye Bash Debug.bat
@@ -1,15 +1,36 @@
-@ECHO OFF && PUSHD "%~dp0" && SETLOCAL
+@ECHO OFF && PUSHD "%~dp0" && SETLOCAL EnableDelayedExpansion
 
+:: Wrye Bash Debug.bat
+::
+:: Retrieves the Python executable path from the registry and runs Wrye Bash
+:: with debugging turned on, redirecting all output to a log file.
+
+
+:: initialize variables
 SET OUTFILE=BashBugDump.log
 SET PYPATH=
 
 
-:: determine whether reg has the newer or older response format
+:: adapt to the output format of reg on the current OS in the current locale
+:: use a known present key that has a known value for the format autodetection
 SET REGCMD=reg query HKLM\SOFTWARE\Classes\.exe /ve >NUL
-FOR /F "skip=1 usebackq tokens=1" %%i in (`%REGCMD%`) do SET FORMATCHECK=%%i
-SET SKIPVAL=1
+SET SKIPVAL=0
+FOR /F "usebackq tokens=*" %%i in (`%REGCMD%`) do (
+    SET LINE=%%i
+    SET TESTSTR=!LINE:exefile=!
+    IF NOT x"!TESTSTR!"==x"!LINE!" GOTO TOKENTEST
+    SET /A SKIPVAL=SKIPVAL+1)
+
+:TOKENTEST
 SET TOKENS=2
-IF x"%FORMATCHECK%"==x"<NO" SET SKIPVAL=4 && SET TOKENS=3
+FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do IF x"%%j"==x"exefile" GOTO TOKENTESTDONE
+SET TOKENS=3
+FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do IF x"%%j"==x"exefile" GOTO TOKENTESTDONE
+SET TOKENS=4
+FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do IF x"%%j"==x"exefile" GOTO TOKENTESTDONE
+SET TOKENS=5
+FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do IF x"%%j"==x"exefile" GOTO TOKENTESTDONE
+:TOKENTESTDONE
 
 
 :: get python path from registry
@@ -29,16 +50,32 @@ SET REGCMD=reg query HKCU\SOFTWARE\Python\PythonCore\2.6\InstallPath /ve
 %REGCMD% >NUL 2>&1 && FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYPATH=%%j
 IF NOT x"%PYPATH%"==x"" GOTO FOUND
 
+
+:: if all that failed, try querying the .py file association
+SET REGCMD=reg query HKCR\Python.File\shell\open\command /ve
+%REGCMD% >NUL 2>&1 || GOTO NOTFOUND
+FOR /F "skip=%SKIPVAL% usebackq tokens=%TOKENS%*" %%i in (`%REGCMD%`) do SET PYASSOC=%%j
+SET PYASSOC=!PYASSOC:"=?!
+FOR /F "tokens=1 delims=?" %%i in ("%PYASSOC%") do SET PYTHON=%%i
+IF NOT x"%PYTHON%"==x"" GOTO FOUNDPYTHON
+
+
+:NOTFOUND
 ECHO Python not found >%OUTFILE%
 ECHO Python not found
 GOTO END
 
+
 :FOUND
-ECHO Found Python in '%PYPATH%' >%OUTFILE%
-ECHO Found Python in '%PYPATH%'
+SET PYTHON=%PYPATH%python.exe
+
+:FOUNDPYTHON
+ECHO Found Python at '%PYTHON%' >%OUTFILE%
+ECHO Found Python at '%PYTHON%'
 ECHO Launching Wrye Bash in debug mode >>%OUTFILE%
 ECHO Launching Wrye Bash in debug mode
-"%PYPATH%"Python.exe "Wrye Bash Launcher.pyw" -d >>%OUTFILE% 2>&1
+"%PYTHON%" "Wrye Bash Launcher.pyw" -d >>%OUTFILE% 2>&1
+
 
 :END
 ENDLOCAL && EXIT /B
