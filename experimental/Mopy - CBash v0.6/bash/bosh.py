5009cde3e0e961049e54cf28d2a3b2af2c0da333
Waruddar <Waruddar@refactor_bash.com>
2011-09-19 22:54:12 +0000

    Tentative fix for ID: 3407058   Needs confirmation
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@1771 c80116c5-1073-4541-b2a9-63b9fd661f84

--- experimental/Mopy - CBash v0.6/bash/bosh.py
+++ experimental/Mopy - CBash v0.6/bash/bosh.py
@@ -32397,7 +32397,7 @@ class CBash_RacePatcher_Imports(SpecialPatcher):
         self.isActive = bool(srcs)
         if not self.isActive: return
         self.racesPatched = set()
-        self.id_tag_values = {}
+        self.fid_attr_value = {}
 
     def initData(self,type_patchers,progress):
         """Compiles material, i.e. reads source text, esp's, etc. as necessary."""
@@ -32412,38 +32412,31 @@ class CBash_RacePatcher_Imports(SpecialPatcher):
         """Records information needed to apply the patch."""
         recordId = record.fid
         for bashKey in bashTags & self.autoKey:
+            attrs = self.tag_attrs[bashKey]
             if bashKey == 'Hair':
-                #Using sets would make this clearer, and probably faster (though speed isn't a concern)
-                #So this is a bit convoluted, but makes the apply section work without special casing this tag
-                #Hairs should perhaps have it's own patcher, but...
-                allHairs = self.id_tag_values.setdefault(recordId,{}).setdefault(bashKey,[[]])
-                allHairs[0] += (hair for hair in record.hairs if hair.ValidateFormID(self.patchFile) and hair not in allHairs[0])
+                hairs = self.fid_attr_value.setdefault(recordId,{}).get('hairs', [])
+                hairs.extend([hair for hair in record.hairs if hair.ValidateFormID(self.patchFile) and hair not in hairs])
+                attr_value = {'hairs':hairs}
             else:
-                values = map(record.__getattribute__,self.tag_attrs[bashKey])
-                if ValidateList(values, self.patchFile):
-                    self.id_tag_values.setdefault(recordId,{})[bashKey] = values
-                else:
+                attr_value = record.ConflictDetails(attrs)
+                if not ValidateDict(attr_value, self.patchFile):
                     mod_skipcount = self.patchFile.patcher_mod_skipcount.setdefault(self.name,{})
                     mod_skipcount[modFile.GName] = mod_skipcount.setdefault(modFile.GName, 0) + 1
+                    continue
+            self.fid_attr_value.setdefault(recordId,{}).update(attr_value)
 
     def apply(self,modFile,record,bashTags):
         """Edits patch file as desired."""
         self.scan_more(modFile,record,bashTags)
         recordId = record.fid
-        if(recordId in self.id_tag_values):
-            allAttrs = []
-            prevValues = []
-            recValues = []
-            for bashKey in self.tag_attrs:
-                attrs = self.tag_attrs[bashKey]
-                allAttrs += attrs
-                tagValues = map(record.__getattribute__,attrs)
-                prevValues += self.id_tag_values[recordId].get(bashKey, tagValues)
-                recValues += tagValues
-            if recValues != prevValues:
+        prev_attr_value = self.fid_attr_value.get(recordId,None)
+        if prev_attr_value:
+            cur_attr_value = dict((attr,getattr(record,attr)) for attr in prev_attr_value)
+            if cur_attr_value != prev_attr_value:
                 override = record.CopyAsOverride(self.patchFile)
                 if override:
-                    map(override.__setattr__, allAttrs, prevValues)
+                    for attr, value in prev_attr_value.iteritems():
+                        setattr(override,attr,value)
                     self.racesPatched.add(record.eid)
                     record.UnloadRecord()
                     record._RecordID = override._RecordID
