698f445795f4e4180866c7da1480a32c9827820b
lojack5 <lojack5@refactor_bash.com>
2011-11-28 04:05:58 +0000

    Missed a few new files when merging trunk changes to the 295 branch
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Branches/Wrye%20Bash/295-fixes@1892 c80116c5-1073-4541-b2a9-63b9fd661f84

---
+++ Mopy/bash/game/oblivion.py
@@ -0,0 +1,207 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2011 Wrye Bash Team
+#
+# =============================================================================
+
+"""This modules defines static data for use by bush, when
+   TES IV: Oblivion is set at the active game."""
+
+#--Name of the game
+name = 'Oblivion'
+#--Alternate display name to use instead of "Wrye Bash for ***"
+altName = 'Wrye Bash'
+
+#--Exe to look for to see if this is the right game
+exe = 'Oblivion.exe'
+
+#--Registry keys to read to find the install location
+regInstallKeys = [
+    ('Bethesda Softworks\Oblivion','Installed Path'),
+    ]
+
+#--patch information
+patchURL = 'http://www.elderscrolls.com/downloads/updates_patches.htm'
+patchTip = 'http://www.elderscrolls.com/'
+
+#--Wrye Bash capabilities with this game
+canBash = True
+canEditSaves = True
+#--Script Extender information
+class se:
+    shortName = 'OBSE'                      # Abbreviated name
+    longName = 'Oblivion Script Extender'   # Full name
+    exe = 'obse_loader.exe'                 # Exe to run
+    steamExe = 'obse_1_2_416.dll'           # Exe to run if a steam install
+    url = 'http://obse.silverlock.org/'     # URL to download from
+    urlTip = 'http://obse.silverlock.org/'  # Tooltip for mouse over the URL
+
+#--Graphics Extender information
+class ge:
+    shortName = 'OBGE'
+    longName = 'Oblivion Graphics Extender'
+    exe = [('Data','obse','plugins','obge.dll'),
+           ('Data','obse','plugins','obgev2.dll'),
+           ]
+    url = 'http://www.tesnexus.com/downloads/file.php?id=30054'
+    urlTip = 'http://www.tesnexus.com/'
+
+
+#--The main plugin Wrye Bash should look for
+masterFiles = [
+    r'Oblivion.esm',
+    r'Nehrim.esm',
+    ]
+
+#--INI files that should show up in the INI Edits tab
+iniFiles = [
+    r'Oblivion.ini',
+    ]
+
+#--Game ESM/ESP/BSA files
+bethDataFiles = set((
+    #--Vanilla
+    r'oblivion.esm',
+    r'oblivion_1.1.esm',
+    r'oblivion_si.esm',
+    r'oblivion_1.1.esm.ghost',
+    r'oblivion_si.esm.ghost',
+    r'oblivion - meshes.bsa',
+    r'oblivion - misc.bsa',
+    r'oblivion - sounds.bsa',
+    r'oblivion - textures - compressed.bsa',
+    r'oblivion - textures - compressed.bsa.orig',
+    r'oblivion - voices1.bsa',
+    r'oblivion - voices2.bsa',
+    #--Shivering Isles
+    r'dlcshiveringisles.esp',
+    r'dlcshiveringisles - meshes.bsa',
+    r'dlcshiveringisles - sounds.bsa',
+    r'dlcshiveringisles - textures.bsa',
+    r'dlcshiveringisles - voices.bsa',
+    ))
+
+#--Every file in the Data directory from Bethsoft
+allBethFiles = set((
+    #vanilla
+    r'Credits.txt',
+    r'Oblivion - Meshes.bsa',
+    r'Oblivion - Misc.bsa',
+    r'Oblivion - Sounds.bsa',
+    r'Oblivion - Textures - Compressed.bsa',
+    r'Oblivion - Voices1.bsa',
+    r'Oblivion - Voices2.bsa',
+    r'Oblivion.esm',
+    r'Music\Battle\battle_01.mp3',
+    r'Music\Battle\battle_02.mp3',
+    r'Music\Battle\battle_03.mp3',
+    r'Music\Battle\battle_04.mp3',
+    r'Music\Battle\battle_05.mp3',
+    r'Music\Battle\battle_06.mp3',
+    r'Music\Battle\battle_07.mp3',
+    r'Music\Battle\battle_08.mp3',
+    r'Music\Dungeon\Dungeon_01_v2.mp3',
+    r'Music\Dungeon\dungeon_02.mp3',
+    r'Music\Dungeon\dungeon_03.mp3',
+    r'Music\Dungeon\dungeon_04.mp3',
+    r'Music\Dungeon\dungeon_05.mp3',
+    r'Music\Explore\atmosphere_01.mp3',
+    r'Music\Explore\atmosphere_03.mp3',
+    r'Music\Explore\atmosphere_04.mp3',
+    r'Music\Explore\atmosphere_06.mp3',
+    r'Music\Explore\atmosphere_07.mp3',
+    r'Music\Explore\atmosphere_08.mp3',
+    r'Music\Explore\atmosphere_09.mp3',
+    r'Music\Public\town_01.mp3',
+    r'Music\Public\town_02.mp3',
+    r'Music\Public\town_03.mp3',
+    r'Music\Public\town_04.mp3',
+    r'Music\Public\town_05.mp3',
+    r'Music\Special\death.mp3',
+    r'Music\Special\success.mp3',
+    r'Music\Special\tes4title.mp3',
+    r'Shaders\shaderpackage001.sdp',
+    r'Shaders\shaderpackage002.sdp',
+    r'Shaders\shaderpackage003.sdp',
+    r'Shaders\shaderpackage004.sdp',
+    r'Shaders\shaderpackage005.sdp',
+    r'Shaders\shaderpackage006.sdp',
+    r'Shaders\shaderpackage007.sdp',
+    r'Shaders\shaderpackage008.sdp',
+    r'Shaders\shaderpackage009.sdp',
+    r'Shaders\shaderpackage010.sdp',
+    r'Shaders\shaderpackage011.sdp',
+    r'Shaders\shaderpackage012.sdp',
+    r'Shaders\shaderpackage013.sdp',
+    r'Shaders\shaderpackage014.sdp',
+    r'Shaders\shaderpackage015.sdp',
+    r'Shaders\shaderpackage016.sdp',
+    r'Shaders\shaderpackage017.sdp',
+    r'Shaders\shaderpackage018.sdp',
+    r'Shaders\shaderpackage019.sdp',
+    r'Video\2k games.bik',
+    r'Video\bethesda softworks HD720p.bik',
+    r'Video\CreditsMenu.bik',
+    r'Video\game studios.bik',
+    r'Video\Map loop.bik',
+    r'Video\Oblivion iv logo.bik',
+    r'Video\Oblivion Legal.bik',
+    r'Video\OblivionIntro.bik',
+    r'Video\OblivionOutro.bik',
+    #SI
+    r'DLCShiveringIsles - Meshes.bsa',
+    r'DLCShiveringIsles - Textures.bsa',
+    r'DLCShiveringIsles - Sounds.bsa',
+    r'DLCShiveringIsles - Voices.bsa',
+    r'DLCShiveringIsles.esp',
+    r'Textures\Effects\TerrainNoise.dds',
+    #DLCs
+    r'DLCBattlehornCastle.bsa',
+    r'DLCBattlehornCastle.esp',
+    r'DLCFrostcrag.bsa',
+    r'DLCFrostcrag.esp',
+    r'DLCHorseArmor.bsa',
+    r'DLCHorseArmor.esp',
+    r'DLCMehrunesRazor.esp',
+    r'DLCOrrery.bsa',
+    r'DLCOrrery.esp',
+    r'DLCSpellTomes.esp',
+    r'DLCThievesDen.bsa',
+    r'DLCThievesDen.esp',
+    r'DLCVileLair.bsa',
+    r'DLCVileLair.esp',
+    r'Knights.bsa',
+    r'Knights.esp',
+    r'DLCList.txt',
+    ))
+
+#--BAIN: Directories that are OK to install to
+dataDirs = set(('bash patches','distantlod','docs','facegen','fonts',
+    'menus','meshes','music','shaders','sound', 'textures', 'trees','video'))
+dataDirsPlus = set(('streamline','_tejon','ini tweaks','scripts','pluggy','ini','obse'))
+
+#--Valid ESM/ESP header versions
+validHeaderVersions = (0.8,1.0)
+
+#--Class to use to read the TES4 record
+tes4ClassName = 'MreTes4'
+
+#--How to unpack the record header
+unpackRecordHeader = ('4s4I',20,'REC_HEAD')
---
+++ Mopy/bash/game/skyrim.py
@@ -0,0 +1,126 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2011 Wrye Bash Team
+#
+# =============================================================================
+
+"""This modules defines static data for use by bush, when
+   TES V: Skyrim is set at the active game."""
+
+#--Name of the game
+name = 'Skyrim'
+altName = 'Wrye Smash'
+
+#--exe to look for to see if this is the right game
+exe = 'TESV.exe'
+
+#--Registry keys to read to find the install location
+regInstallKeys = [
+    ('Bethesda Softworks\Skyrim','Installed Path'),
+    ]
+
+#--patch information
+patchURL = '' # Update via steam
+patchTip = 'Update via Steam'
+
+#--Script Extender information
+class se:
+    shortName = 'SKSE'                      # Abbreviated name
+    longName = 'Skyrim Script Extender'     # Full name
+    exe = 'skse_loader.exe'                 # Exe to run
+    steamExe = 'skse_loader.exe'            # Exe to run if a steam install
+    url = 'http://skse.silverlock.org/'     # URL to download from
+    urlTip = 'http://skse.silverlock.org/'  # Tooltip for mouse over the URL
+
+#--Graphics Extender information
+class ge:
+    shortName = ''
+    longName = ''
+    exe = ''
+    url = ''
+    urlTip = ''
+
+#--Wrye Bash capabilities with this game
+canBash = False      # No Bashed Patch creation or messing with mods
+canEditSaves = False # Only basic understanding of save games
+
+#--INI files that should show up in the INI Edits tab
+iniFiles = [
+    r'Skyrim.ini',
+    r'SkyrimPrefs.ini',
+    ]
+
+#--The main plugin file Wrye Bash should look for
+masterFiles = [
+    r'Skyrim.esm',
+    ]
+
+#--Game ESM/ESP/BSA files
+bethDataFiles = set((
+    #--Vanilla
+    r'skyrim.esm',
+    r'update.esm',
+    r'skyrim - animations.bsa',
+    r'skyrim - interface.bsa',
+    r'skyrim - meshes.bsa',
+    r'skyrim - misc.bsa',
+    r'skyrim - shaders.bsa',
+    r'skyrim - sounds.bsa',
+    r'skyrim - textures.bsa',
+    r'skyrim - voices.bsa',
+    r'skyrim - voicesextra.bsa',
+    ))
+
+#--Every file in the Data directory from Bethsoft
+allBethFiles = set((
+    #--Vanilla
+    r'skyrim.esm',
+    r'update.esm',
+    r'skyrim - animations.bsa',
+    r'skyrim - interface.bsa',
+    r'skyrim - meshes.bsa',
+    r'skyrim - misc.bsa',
+    r'skyrim - shaders.bsa',
+    r'skyrim - sounds.bsa',
+    r'skyrim - textures.bsa',
+    r'skyrim - voices.bsa',
+    r'skyrim - voicesextra.bsa',
+    r'interface\translate_english.txt', #--probably need one for each language
+    r'strings\skyrim_english.dlstrings', #--same here
+    r'strings\skyrim_english.ilstrings',
+    r'strings\skryim_english.strings',
+    r'strings\update_english.dlstrings',
+    r'strings\update_english.ilstrings',
+    r'strings\update_english.strings',
+    r'video\bgs_logo.bik',
+    ))
+
+#--BAIN: Directories that are OK to install to
+dataDirs = set(('bash patches','interface','meshes','strings','textures',
+    'video','lodsettings','grass','scripts','shadersfx','music','sound',))
+dataDirsPlus = set(('ini tweaks','skse','ini'))
+
+#--Valid ESM/ESP header versions
+validHeaderVersions = (0.94,)
+
+#--Class to use to read the TES4 record
+tes4ClassName = 'MreTes5'
+#--How to unpack the record header
+unpackRecordHeader = ('4s5I',24,'REC_HEAD')
