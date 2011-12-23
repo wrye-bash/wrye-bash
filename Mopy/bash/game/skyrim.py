3679569a60f4fb6f48ece1500eea492ce23253fd
lojack5 <lojack5@refactor_bash.com>
2011-12-23 10:30:18 +0000

    another dumb typo, this time for MreLv** records in Skyrim
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2044 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/game/skyrim.py
+++ Mopy/bash/game/skyrim.py
@@ -1387,7 +1387,7 @@ class MreLeveledList(MreLeveledListBase):
         # LVLN
         MelString('MODL','model'),
         MelBase('MODT','modt_p'),
-        MelStruct('COED','=I8s',(FID,'coed_fid',None),'code_unk')
+        MelStruct('COED','=I8s',(FID,'coed_fid',None),'coed_unk')
         )
     __slots__ = MreLeveledListBase.__slots__ + melSet.getSlotsUsed()
 
