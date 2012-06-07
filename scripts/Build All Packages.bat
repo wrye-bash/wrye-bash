1d573037cce7a10d1161e0ed199aa743505ae052
rlibiez <rlibiez@refactor_bash.com>
2012-06-07 03:25:58 +0000

    Batch file updates.
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2457 c80116c5-1073-4541-b2a9-63b9fd661f84

---
+++ scripts/Build All Packages.bat
@@ -0,0 +1,16 @@
+@echo off
+title Wrye Bash Release Build Script
+echo.
+echo.
+echo. Building Wrye Bash packages, patience Avatar....
+
+echo.
+echo.
+
+"C:\Python27\python.exe" "C:\Code Repositories\Wrye Bash\scripts\package_for_release.py"
+
+echo.
+echo.
+
+echo. All Done! Hopefully...
+pause
