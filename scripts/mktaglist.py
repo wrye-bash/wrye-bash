a824c667b387d0388fb8cf4660df4085399165ea
WrinklyNinja <wrinklyninja1@gmail.com>
2012-06-19 20:31:21 +0000

    Updated taglist generation script so that it will pull masterlists from a detected BOSS install. Also deleted the old taglist generation batch file.
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2534 c80116c5-1073-4541-b2a9-63b9fd661f84

--- scripts/mktaglist.py
+++ scripts/mktaglist.py
@@ -7,10 +7,9 @@
 # This script generates taglist.txt files in Mopy\Bashed Patches\Oblivion and
 # Mopy\Bashed Patches\Skyrim using the BOSS API and source masterlists. The 
 # masterlists must be named "masterlist.txt" and put in the folders mentioned
-# above. Upon generation of the taglists, the masterlists will be deleted (so
-# it is effectively a conversion). To generate the taglist for a game, you must
-# have the game installed. This script will generate taglists for all detected
-# games.
+# above, or be present in a BOSS install that was installed using its installer. 
+# To generate the taglist for a game, you must have the game installed. This 
+# script will generate taglists for all detected games.
 #
 # Usage:
 #   mktaglist.py
@@ -48,6 +47,17 @@ try:
         print u'Found Skyrim.'
 except:
     raise
+    
+# Detect a BOSS install if present. Doesn't detect manual installs because there is no way to obtain a path without guesswork.
+bossDir = None
+try:
+    key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, u'Software\\Boss', 0, _winreg.KEY_READ|_winreg.KEY_WOW64_32KEY)
+    value = _winreg.QueryValueEx(key,u'Installed Path')
+    if value[1] == _winreg.REG_SZ and os.path.exists(value[0]):
+        bossDir = value[0]
+        print u'Found BOSS.'
+except:
+    pass
 
 # Load BAPI.
 bapi.Init(bapiDir)
@@ -58,28 +68,32 @@ else:
 
 if oblivionDir:
     # Convert Oblivion masterlist.
-    masterlistDir = u'../Mopy/Bash Patches/Oblivion/masterlist.txt'
+    if bossDir and os.path.exists(bossDir + u'/Oblivion/masterlist.txt'):
+       masterlistDir = bossDir + u'/Oblivion/masterlist.txt'
+    else:
+       masterlistDir = u'../Mopy/Bash Patches/Oblivion/masterlist.txt'
+    print u'Getting masterlist from %s' % masterlistDir
     taglistDir = u'../Mopy/Bash Patches/Oblivion/taglist.txt'
     if os.path.exists(masterlistDir):
         boss = bapi.BossDb(oblivionDir,bapi.BOSS_API_GAME_OBLIVION)
         boss.Load(masterlistDir)
         boss.DumpMinimal(taglistDir,True)
-        # Delete converted masterlist.
-        os.remove(masterlistDir)
         print u'Oblivion masterlist converted.'
     else:
         print u'Error: Oblivion masterlist not found.'
     
 if skyrimDir:
     # Convert Skyrim masterlist.
-    masterlistDir = u'../Mopy/Bash Patches/Skyrim/masterlist.txt'
+    if bossDir and os.path.exists(bossDir + u'/Skyrim/masterlist.txt'):
+       masterlistDir = bossDir + u'/Skyrim/masterlist.txt'
+    else:
+       masterlistDir = u'../Mopy/Bash Patches/Skyrim/masterlist.txt'
+    print u'Getting masterlist from %s' % masterlistDir
     taglistDir = u'../Mopy/Bash Patches/Skyrim/taglist.txt'
     if os.path.exists(masterlistDir):
         boss = bapi.BossDb(skyrimDir,bapi.BOSS_API_GAME_SKYRIM)
         boss.Load(masterlistDir)
         boss.DumpMinimal(taglistDir,True)
-        # Delete converted masterlist.
-        os.remove(masterlistDir)
         print u'Skyrim masterlist converted.'
     else:
         print u'Error: Skyrim masterlist not found.'
