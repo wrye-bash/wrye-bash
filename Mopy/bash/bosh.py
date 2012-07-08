41f1b6aacf23ebd1db472655825ddc22f886d16e
Arthmoor <arthmoor@gmail.com>
2012-07-08 22:48:06 +0000

    Updated regex for Nexus files. [trira]
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2577 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -295,7 +295,7 @@ reCsvExt  = re.compile(ur'\.csv$',re.I|re.U)
 reINIExt  = re.compile(ur'\.ini$',re.I|re.U)
 reQuoted  = re.compile(ur'^"(.*)"$',re.U)
 reGroupHeader = re.compile(ur'^(\+\+|==)',re.U)
-reTesNexus = re.compile(ur'(.*?)(?:-(\d{1,6})(?:\.tessource)?(?:-bain)?(?:-\d{0,6})?(?:-\d{0,6})?(?:-\d{0,6})?(?:\w)?)?(\.7z|\.zip|\.rar|\.7z\.001|)$',re.I|re.U)
+reTesNexus = re.compile(ur'(.*?)(?:-(\d{1,6})(?:\.tessource)?(?:-bain)?(?:-\d{0,6})?(?:-\d{0,6})?(?:-\d{0,6})?(?:-\w{0,16})?(?:\w)?)?(\.7z|\.zip|\.rar|\.7z\.001|)$',re.I|re.U)
 reTESA = re.compile(ur'(.*?)(?:-(\d{1,6})(?:\.tessource)?(?:-bain)?)?(\.7z|\.zip|\.rar|)$',re.I|re.U)
 reSplitOnNonAlphaNumeric = re.compile(ur'\W+',re.U)
 
