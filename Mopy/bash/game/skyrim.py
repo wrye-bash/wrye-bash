f43fe400443ad87348c97b4c57accee0c9cfd4da
lojack5 <lojack5@refactor_bash.com>
2012-09-08 01:26:14 +0000

    stupid syntax fixup on  my part
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Branches/Wrye%20Bash/dev-sharlikran@2615 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/game/skyrim.py
+++ Mopy/bash/game/skyrim.py
@@ -13348,22 +13348,25 @@ class MreAstp(MelRecord):
 # Verified Correct for Skyrim
 #------------------------------------------------------------------------------
 class MreCobj(MelRecord):
+    """Constructible Object record (recipies)"""
+    classType = 'COBJ'
+    isKeyedByEid = True # NULL fids are acceptible
+
+
     class MelCobjCnto(MelGroups):
         def __init__(self):
             MelGroups.__init__(self,'components',
                 MelStruct('CNTO','=2I',(FID,'item',None),'count'),
                 MelOptStruct('COED','=IQ',(FID,'owner'),'coed_unk'),
                 )
-        def dumpData(self,record,out):
-        components = record.__getattribute__(self.attr)
-        if components:
-            # Only write the COCT/CNTO/COED subrecords if count > 0
-            out.packSub('COCT','I',len(components))
-            MelGroups.dumpData(self,record,out)
 
-    """Constructible Object record (recipies)"""
-    classType = 'COBJ'
-    isKeyedByEid = True # NULL fids are acceptible
+        def dumpData(self,record,out):
+            components = record.__getattribute__(self.attr)
+            if components:
+                # Only write the COCT/CNTO/COED subrecords if count > 0
+                out.packSub('COCT','I',len(components))
+                MelGroups.dumpData(self,record,out)
+
     melSet = MelSet(
         MelString('EDID','eid'),
         MelNull('COCT'), # Handled by MelCobjCnto
