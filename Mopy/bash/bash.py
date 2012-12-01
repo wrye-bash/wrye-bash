cb71ff1ba82a545af1a00a954f3965db056f0932
lojack5 <lojack5@refactor_bash.com>
2012-12-01 18:35:07 +0000

    new command line switch --uac, always starts in admin mode (if needed), no prompt required
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2811 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/bash.py
+++ Mopy/bash/bash.py
@@ -585,25 +585,29 @@ def main():
     basherImported = True
 
     basher.isUAC = isUAC
-    if isUAC and not opts.uac:
-        # Show a prompt asking if we should restart in Admin Mode
-        message = _(u"Wrye Bash needs Administrator Privileges to make changes to the %(gameName)s directory.  If you do not start Wrye Bash with elevated privileges, you will be prompted at each operation that requires elevated privileges.") % {'gameName':bush.game.name}
-        title=_(u'UAC Protection')
-        if balt.canVista:
-            admin = _(u'Run with Administrator Privileges')
-            result = balt.vistaDialog(None,
-                message=message,
-                buttons=[(True,'+'+admin),
-                         (False,_(u'Run normally')),
-                         ],
-                title=title,
-                footer=_(u'To skip this message in the future, launch Wrye Bash with the --no-uac command line switch.'),
-                )
-        else:
-            result = balt.askYes(None,
-                message+u'\n\n'+_(u'Start Wrye Bash with Administrator Privileges?'),
-                title)
-        if result:
+    if isUAC:
+        uacRestart = False
+        if not opts.noUac and not opts.uac:
+            # Show a prompt asking if we should restart in Admin Mode
+            message = _(u"Wrye Bash needs Administrator Privileges to make changes to the %(gameName)s directory.  If you do not start Wrye Bash with elevated privileges, you will be prompted at each operation that requires elevated privileges.") % {'gameName':bush.game.name}
+            title=_(u'UAC Protection')
+            if balt.canVista:
+                admin = _(u'Run with Administrator Privileges')
+                uacRestart = balt.vistaDialog(None,
+                    message=message,
+                    buttons=[(True,'+'+admin),
+                             (False,_(u'Run normally')),
+                             ],
+                        title=title,
+                    footer=_(u'To skip this message in the future, launch Wrye Bash with the --no-uac command line switch.'),
+                    )
+            else:
+                uacRestart = balt.askYes(None,
+                    message+u'\n\n'+_(u'Start Wrye Bash with Administrator Privileges?'),
+                    title)
+        elif opts.uac:
+            uacRestart = True
+        if uacRestart:
             basher.appRestart = True
             basher.uacRestart = True
             return
