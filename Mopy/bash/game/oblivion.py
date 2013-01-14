cb977663e57fc2c2d2a5768242b82fbf51e6b6e5
pacmorrowind <pacmorrowind@refactor_bash.com>
2013-01-14 07:55:44 +0000

    Skyrim:   mod->import/export prices (also refactored it a bit so it is WAY easier to make work for other games)   mod->import/export stats   (also adjusted the mod->import/export menu so only valid options are visible)   bashed patch-> import stats
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@2935 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/game/oblivion.py
+++ Mopy/bash/game/oblivion.py
@@ -1595,6 +1595,82 @@ patchers = (
 # For ListsMerger
 listTypes = ('LVLC','LVLI','LVSP',)
 
+namesTypes = set((
+        'ALCH', 'AMMO', 'APPA', 'ARMO', 'BOOK', 'BSGN', 'CLAS', 'CLOT', 'CONT', 'CREA', 'DOOR',
+        'EYES', 'FACT', 'FLOR', 'HAIR','INGR', 'KEYM', 'LIGH', 'MISC', 'NPC_', 'RACE', 'SGST',
+        'SLGM', 'SPEL','WEAP',))
+pricesTypes = {'ALCH':{},'AMMO':{},'APPA':{},'ARMO':{},'BOOK':{},'CLOT':{},'INGR':{},'KEYM':{},'LIGH':{},'MISC':{},'SGST':{},'SLGM':{},'WEAP':{}}
+statsTypes = {
+            'ALCH':('eid', 'weight', 'value'),
+            'AMMO':('eid', 'weight', 'value', 'damage', 'speed', 'enchantPoints'),
+            'APPA':('eid', 'weight', 'value', 'quality'),
+            'ARMO':('eid', 'weight', 'value', 'health', 'strength'),
+            'BOOK':('eid', 'weight', 'value', 'enchantPoints'),
+            'CLOT':('eid', 'weight', 'value', 'enchantPoints'),
+            'INGR':('eid', 'weight', 'value'),
+            'KEYM':('eid', 'weight', 'value'),
+            'LIGH':('eid', 'weight', 'value', 'duration'),
+            'MISC':('eid', 'weight', 'value'),
+            'SGST':('eid', 'weight', 'value', 'uses'),
+            'SLGM':('eid', 'weight', 'value'),
+            'WEAP':('eid', 'weight', 'value', 'health', 'damage', 'speed', 'reach', 'enchantPoints'),
+            }
+statsHeaders = (
+                #--Alch
+                (u'ALCH',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'))) + u'"\n')),
+                #Ammo
+                (u'AMMO',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Damage'),_(u'Speed'),_(u'EPoints'))) + u'"\n')),
+                #--Apparatus
+                (u'APPA',
+                    (u'"' + u'","'.join((_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Quality'))) + u'"\n')),
+                #--Armor
+                (u'ARMO',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Health'),_(u'AR'))) + u'"\n')),
+                #Books
+                (u'BOOK',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'EPoints'))) + u'"\n')),
+                #Clothing
+                (u'CLOT',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'EPoints'))) + u'"\n')),
+                #Ingredients
+                (u'INGR',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'))) + u'"\n')),
+                #--Keys
+                (u'KEYM',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'))) + u'"\n')),
+                #Lights
+                (u'LIGH',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Duration'))) + u'"\n')),
+                #--Misc
+                (u'MISC',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'))) + u'"\n')),
+                #Sigilstones
+                (u'SGST',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Uses'))) + u'"\n')),
+                #Soulgems
+                (u'SLGM',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'))) + u'"\n')),
+                #--Weapons
+                (u'WEAP',
+                    (u'"' + u'","'.join((_(u'Type'),_(u'Mod Name'),_(u'ObjectIndex'),
+                    _(u'Editor Id'),_(u'Weight'),_(u'Value'),_(u'Health'),_(u'Damage'),
+                    _(u'Speed'),_(u'Reach'),_(u'EPoints'))) + u'"\n')),
+                )
+
 #--CBash patchers available when building a Bashed Patch
 CBash_patchers = (
     'CBash_AliasesPatcher', 'CBash_AssortedTweaker', 'CBash_PatchMerger',
