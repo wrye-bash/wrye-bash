17d32b509f2ee82ad3c315d8468eb7eddd333267
d4id4los <d4id4los@refactor_bash.com>
2013-09-13 18:08:29 +0000

    Fix for Bug [#270] "False flagged ESP files cannot be sorted with the ESM group as expected."
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@3059 c80116c5-1073-4541-b2a9-63b9fd661f84

--- Mopy/bash/bosh.py
+++ Mopy/bash/bosh.py
@@ -4105,18 +4105,21 @@ class Plugins:
         # Remove non existent plugins from load order
         self.removeMods(removedFiles)
         # Add new plugins to load order
-        indexFirstEsp = None
+        indexFirstEsp = 0
+        while modInfos[self.LoadOrder[indexFirstEsp]].isEsm():
+            indexFirstEsp += 1
         for mod in addedFiles:
             if modInfos.data[mod].isEsm():
-                if indexFirstEsp is None:
-                    indexFirstEsp = 0
-                    while modInfos[self.LoadOrder[indexFirstEsp]].isEsm():
-                        indexFirstEsp += 1
-                    self.addMods([mod], indexFirstEsp)
-                else:
-                    self.addMods([mod], indexFirstEsp)
+                self.addMods([mod], indexFirstEsp)
+                indexFirstEsp += 1
             else:
                 self.addMods([mod])
+        # Check to see if any esm files are loaded below an esp and reorder as neccessar
+        for mod in self.LoadOrder[indexFirstEsp:]:
+            if modInfos.data[mod].isEsm():
+                self.LoadOrder.remove(mod)
+                self.LoadOrder.insert(indexFirstEsp, mod)
+                indexFirstEsp += 1
         # Save changes if necessary
         if removedFiles or addedFiles:
             self.saveLoadOrder()
