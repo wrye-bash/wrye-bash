7b25740656c3b49ad97b10202a8eecf67112c670
sharlikran <sharlikran@refactor_bash.com>
2014-01-11 19:28:55 +0000

    [WryeBash] Updated scripts/mktaglist.py to make a taglist.txt for Skyrim, Oblivion, Fallout 3, Fallout New Vegas
    git-svn-id: svn://svn.code.sf.net/p/oblivionworks/code/Programs/Wrye%20Bash@3103 c80116c5-1073-4541-b2a9-63b9fd661f84

--- scripts/mktaglist.py
+++ scripts/mktaglist.py
@@ -29,6 +29,9 @@ bapiDir = u'../Mopy/bash/compiled'
 # Detect games.
 oblivionDir = None
 skyrimDir = None
+fallout3Dir = None
+falloutNVDir = None
+
 # Detect Oblivion.
 try:
     key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, u'Software\\Bethesda Softworks\\Oblivion', 0, _winreg.KEY_READ|_winreg.KEY_WOW64_32KEY)
@@ -48,6 +51,26 @@ try:
 except:
     raise
     
+#Detect Fallout3.
+try:
+    key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, u'Software\\Bethesda Softworks\\Fallout3', 0, _winreg.KEY_READ|_winreg.KEY_WOW64_32KEY)
+    value = _winreg.QueryValueEx(key,u'Installed Path')
+    if value[1] == _winreg.REG_SZ and os.path.exists(value[0]):
+        fallout3Dir = value[0]
+        print u'Found Fallout3.'
+except:
+    raise
+
+#Detect FalloutNV.
+try:
+    key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, u'Software\\Bethesda Softworks\\FalloutNV', 0, _winreg.KEY_READ|_winreg.KEY_WOW64_32KEY)
+    value = _winreg.QueryValueEx(key,u'Installed Path')
+    if value[1] == _winreg.REG_SZ and os.path.exists(value[0]):
+        falloutNVDir = value[0]
+        print u'Found FalloutNV.'
+except:
+    raise
+
 # Detect a BOSS install if present. Doesn't detect manual installs because there is no way to obtain a path without guesswork.
 bossDir = None
 try:
@@ -97,5 +120,37 @@ if skyrimDir:
         print u'Skyrim masterlist converted.'
     else:
         print u'Error: Skyrim masterlist not found.'
+    
+if fallout3Dir:
+    # Convert Fallout 3 masterlist.
+    if bossDir and os.path.exists(bossDir + u'/Fallout 3/masterlist.txt'):
+       masterlistDir = bossDir + u'/Fallout 3/masterlist.txt'
+    else:
+       masterlistDir = u'../Mopy/Bash Patches/Fallout 3/masterlist.txt'
+    print u'Getting masterlist from %s' % masterlistDir
+    taglistDir = u'../Mopy/Bash Patches/Fallout 3/taglist.txt'
+    if os.path.exists(masterlistDir):
+        boss = bapi.BossDb(fallout3Dir,bapi.BOSS_API_GAME_FALLOUT3)
+        boss.Load(masterlistDir)
+        boss.DumpMinimal(taglistDir,True)
+        print u'Fallout 3 masterlist converted.'
+    else:
+        print u'Error: Fallout 3 masterlist not found.'
+    
+if falloutNVDir:
+    # Convert Fallout New Vegas masterlist.
+    if bossDir and os.path.exists(bossDir + u'/Fallout New Vegas/masterlist.txt'):
+       masterlistDir = bossDir + u'/Fallout New Vegas/masterlist.txt'
+    else:
+       masterlistDir = u'../Mopy/Bash Patches/Fallout New Vegas/masterlist.txt'
+    print u'Getting masterlist from %s' % masterlistDir
+    taglistDir = u'../Mopy/Bash Patches/Fallout New Vegas/taglist.txt'
+    if os.path.exists(masterlistDir):
+        boss = bapi.BossDb(falloutNVDir,bapi.BOSS_API_GAME_FALLOUTNV)
+        boss.Load(masterlistDir)
+        boss.DumpMinimal(taglistDir,True)
+        print u'Fallout New Vegas masterlist converted.'
+    else:
+        print u'Error: Fallout New Vegas masterlist not found.'
     
 print u'Taglist generator finished.'
