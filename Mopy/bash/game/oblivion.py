ba39ae3c37f97d17600ab8bf52530ff5eb28a281
Sharlikran <danielhmpdx16@gmail.com>
2014-09-10 19:18:21 -0700

    Added oblivion_const.py

--- Mopy/bash/game/oblivion.py
+++ Mopy/bash/game/oblivion.py
@@ -27,10 +27,11 @@
 
 import struct
 from .. import brec
-from .. import bolt
-from .. import bush
 from ..brec import *
+from .. import bolt
 from ..bolt import Flags, DataDict, StateError
+from .. import bush
+from oblivion_const import bethDataFiles, allBethFiles
 
 # Util Constants ---------------------------------------------------------------
 #--Null strings (for default empty byte arrays)
@@ -227,121 +228,13 @@ nonDeactivatableFiles = []
 pklfile = ur'bash\db\Oblivion_ids.pkl'
 
 #--Game ESM/ESP/BSA files
-bethDataFiles = set((
-    #--Vanilla
-    u'oblivion.esm',
-    u'oblivion_1.1.esm',
-    u'oblivion_si.esm',
-    u'oblivion_1.1.esm.ghost',
-    u'oblivion_si.esm.ghost',
-    u'oblivion - meshes.bsa',
-    u'oblivion - misc.bsa',
-    u'oblivion - sounds.bsa',
-    u'oblivion - textures - compressed.bsa',
-    u'oblivion - textures - compressed.bsa.orig',
-    u'oblivion - voices1.bsa',
-    u'oblivion - voices2.bsa',
-    #--Shivering Isles
-    u'dlcshiveringisles.esp',
-    u'dlcshiveringisles - meshes.bsa',
-    u'dlcshiveringisles - sounds.bsa',
-    u'dlcshiveringisles - textures.bsa',
-    u'dlcshiveringisles - voices.bsa',
-    ))
+#  These filenames need to be in lowercase,
+# bethDataFiles = set((
+# Moved to oblivion_const
 
 #--Every file in the Data directory from Bethsoft
-allBethFiles = set((
-    #vanilla
-    u'Credits.txt',
-    u'Oblivion - Meshes.bsa',
-    u'Oblivion - Misc.bsa',
-    u'Oblivion - Sounds.bsa',
-    u'Oblivion - Textures - Compressed.bsa',
-    u'Oblivion - Voices1.bsa',
-    u'Oblivion - Voices2.bsa',
-    u'Oblivion.esm',
-    u'Music\\Battle\\battle_01.mp3',
-    u'Music\\Battle\\battle_02.mp3',
-    u'Music\\Battle\\battle_03.mp3',
-    u'Music\\Battle\\battle_04.mp3',
-    u'Music\\Battle\\battle_05.mp3',
-    u'Music\\Battle\\battle_06.mp3',
-    u'Music\\Battle\\battle_07.mp3',
-    u'Music\\Battle\\battle_08.mp3',
-    u'Music\\Dungeon\\Dungeon_01_v2.mp3',
-    u'Music\\Dungeon\\dungeon_02.mp3',
-    u'Music\\Dungeon\\dungeon_03.mp3',
-    u'Music\\Dungeon\\dungeon_04.mp3',
-    u'Music\\Dungeon\\dungeon_05.mp3',
-    u'Music\\Explore\\atmosphere_01.mp3',
-    u'Music\\Explore\\atmosphere_03.mp3',
-    u'Music\\Explore\\atmosphere_04.mp3',
-    u'Music\\Explore\\atmosphere_06.mp3',
-    u'Music\\Explore\\atmosphere_07.mp3',
-    u'Music\\Explore\\atmosphere_08.mp3',
-    u'Music\\Explore\\atmosphere_09.mp3',
-    u'Music\\Public\\town_01.mp3',
-    u'Music\\Public\\town_02.mp3',
-    u'Music\\Public\\town_03.mp3',
-    u'Music\\Public\\town_04.mp3',
-    u'Music\\Public\\town_05.mp3',
-    u'Music\\Special\\death.mp3',
-    u'Music\\Special\\success.mp3',
-    u'Music\\Special\\tes4title.mp3',
-    u'Shaders\\shaderpackage001.sdp',
-    u'Shaders\\shaderpackage002.sdp',
-    u'Shaders\\shaderpackage003.sdp',
-    u'Shaders\\shaderpackage004.sdp',
-    u'Shaders\\shaderpackage005.sdp',
-    u'Shaders\\shaderpackage006.sdp',
-    u'Shaders\\shaderpackage007.sdp',
-    u'Shaders\\shaderpackage008.sdp',
-    u'Shaders\\shaderpackage009.sdp',
-    u'Shaders\\shaderpackage010.sdp',
-    u'Shaders\\shaderpackage011.sdp',
-    u'Shaders\\shaderpackage012.sdp',
-    u'Shaders\\shaderpackage013.sdp',
-    u'Shaders\\shaderpackage014.sdp',
-    u'Shaders\\shaderpackage015.sdp',
-    u'Shaders\\shaderpackage016.sdp',
-    u'Shaders\\shaderpackage017.sdp',
-    u'Shaders\\shaderpackage018.sdp',
-    u'Shaders\\shaderpackage019.sdp',
-    u'Video\\2k games.bik',
-    u'Video\\bethesda softworks HD720p.bik',
-    u'Video\\CreditsMenu.bik',
-    u'Video\\game studios.bik',
-    u'Video\\Map loop.bik',
-    u'Video\\Oblivion iv logo.bik',
-    u'Video\\Oblivion Legal.bik',
-    u'Video\\OblivionIntro.bik',
-    u'Video\\OblivionOutro.bik',
-    #SI
-    u'DLCShiveringIsles - Meshes.bsa',
-    u'DLCShiveringIsles - Textures.bsa',
-    u'DLCShiveringIsles - Sounds.bsa',
-    u'DLCShiveringIsles - Voices.bsa',
-    u'DLCShiveringIsles.esp',
-    u'Textures\\Effects\\TerrainNoise.dds',
-    #DLCs
-    u'DLCBattlehornCastle.bsa',
-    u'DLCBattlehornCastle.esp',
-    u'DLCFrostcrag.bsa',
-    u'DLCFrostcrag.esp',
-    u'DLCHorseArmor.bsa',
-    u'DLCHorseArmor.esp',
-    u'DLCMehrunesRazor.esp',
-    u'DLCOrrery.bsa',
-    u'DLCOrrery.esp',
-    u'DLCSpellTomes.esp',
-    u'DLCThievesDen.bsa',
-    u'DLCThievesDen.esp',
-    u'DLCVileLair.bsa',
-    u'DLCVileLair.esp',
-    u'Knights.bsa',
-    u'Knights.esp',
-    u'DLCList.txt',
-    ))
+# allBethFiles = set((
+# Moved to oblivion_const
 
 #--BAIN: Directories that are OK to install to
 dataDirs = set((
