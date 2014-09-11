2b012e70cce954e6515321422274686a87c27d6b
Sharlikran <danielhmpdx16@gmail.com>
2014-09-10 20:29:31 -0700

    Added MreSndr

--- Mopy/bash/game/skyrim.py
+++ Mopy/bash/game/skyrim.py
@@ -7348,6 +7348,37 @@ class MreSnct(MelRecord):
     __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
 
 # Verified for 305
+#------------------------------------------------------------------------------
+class MreSndr(MelRecord):
+    """Sound Descriptor"""
+    classType = 'SNDR'
+
+    # LNAM has wbEnum in TES5Edit
+    # Assigned to 'looping' for WB
+    # $00 , 'None',
+    # $08 , 'Loop',
+    # $10 , 'Envelope Fast',
+    # $20 , 'Envelope Slow'
+
+    melSet = MelSet(
+        MelString('EDID','eid'),
+        MelBase('CNAM','cnam_p'),
+        MelFid('GNAM','category',),
+        MelFid('SNAM','alternateSoundFor',),
+        MelGroups('sounds',
+            MelString('ANAM','fileName',),
+            ),
+        MelFid('ONAM','outputModel',),
+        MelLString('FNAM','string'),
+        MelConditions(),
+        MelStruct('LNAM','sBsB','unknown1','looping','unknown2',
+                  'rumbleSendValue',),
+        MelStruct('BNAM','2b2BH','pctFrequencyShift','pctFrequencyVariance','priority',
+                  'dbVariance','staticAttenuation',),
+        )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+# Verified for 305
 #--Mergeable record types
 mergeClasses = (
         MreAact, MreActi, MreAddn, MreAmmo, MreAnio, MreAppa, MreArma, MreArmo,
