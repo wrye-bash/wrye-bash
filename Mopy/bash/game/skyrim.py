7088cef69bf13f8d6c8e6b68894a9d1e4f208c37
Sharlikran <danielhmpdx16@gmail.com>
2014-09-10 20:14:26 -0700

    Added MreMato MreMatt MreMesg MreMgef

--- Mopy/bash/game/skyrim.py
+++ Mopy/bash/game/skyrim.py
@@ -5684,8 +5684,161 @@ class MreLvsp(MreLeveledList):
         )
     __slots__ = MreLeveledList.__slots__ + melSet.getSlotsUsed()
 
-# Verified Correct for Skyrim 1.8
+# Verified for 305
 #------------------------------------------------------------------------------
+class MreMato(MelRecord):
+    """Material Object Records"""
+    classType = 'MATO'
+
+    MatoTypeFlags = bolt.Flags(0L,bolt.Flags.getNames(
+            (0, 'singlePass'),
+        ))
+
+    melSet = MelSet(
+        MelString('EDID','eid'),
+        MelModel(),
+        MelGroups('wordsOfPower',
+            MelBase('DNAM','propertyData',),
+            ),
+        MelStruct('DATA','11fI','falloffScale','falloffBias','noiseUVScale',
+                  'materialUVScale','projectionVectorX','projectionVectorY',
+                  'projectionVectorZ','normalDampener',
+                  'singlePassColor','singlePassColor',
+                  'singlePassColor',(MatoTypeFlags,'flags',0L),),
+        )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+# Verified for 305
+#------------------------------------------------------------------------------
+class MreMatt(MelRecord):
+    """Material Type Record."""
+    classType = 'MATT'
+
+    MattTypeFlags = bolt.Flags(0L,bolt.Flags.getNames(
+            (0, 'stairMaterial'),
+            (1, 'arrowsStick'),
+        ))
+
+    melSet = MelSet(
+        MelString('EDID','eid'),
+        MelFid('PNAM', 'materialParent',),
+        MelString('MNAM','materialName'),
+        MelStruct('CNAM','3f','red','green','blue',),
+        MelStruct('BNAM','f','buoyancy',),
+        MelStruct('FNAM','I',(MattTypeFlags,'flags',0L),),
+        MelFid('HNAM', 'havokImpactDataSet',),
+    )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+# Verified for 305
+#------------------------------------------------------------------------------
+class MreMesg(MelRecord):
+    """Message Record."""
+    classType = 'MESG'
+
+    MesgTypeFlags = bolt.Flags(0L,bolt.Flags.getNames(
+            (0, 'messageBox'),
+            (1, 'autoDisplay'),
+        ))
+
+    melSet = MelSet(
+        MelString('EDID','eid'),
+        MelLString('DESC','description'),
+        MelLString('FULL','full'),
+        # 'INAM' leftover
+        MelFid('INAM','iconUnused'),
+        MelFid('QNAM','materialParent'),
+        MelStruct('DNAM','I',(MesgTypeFlags,'flags',0L),),
+        # Don't Show
+        MelStruct('TNAM','I','displayTime',),
+        MelGroups('menuButtons',
+            MelLString('ITXT','buttonText'),
+            MelConditions(),
+            ),
+    )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+# Verified for 305
+#------------------------------------------------------------------------------
+class MreMgef(MelRecord):
+    """Mgef Item"""
+    classType = 'MGEF'
+
+    # MGEF has many wbEnum in TES5Edit
+    # 'magicSkill', 'resistValue', 'mgefArchtype',
+    # 'actorValue', 'castingType', 'delivery', 'secondActorValue'
+    # 'castingSoundLevel', 'soundType'
+    # refer to TES5Edit for values
+
+    MgefGeneralFlags = bolt.Flags(0L,bolt.Flags.getNames(
+            (0, 'hostile'),
+            (1, 'recover'),
+            (2, 'detrimental'),
+            (3, 'snaptoNavmesh'),
+            (4, 'noHitEvent'),
+            (5, 'unknown6'),
+            (6, 'unknown7'),
+            (7, 'unknown8'),
+            (8, 'dispellwithKeywords'),
+            (9, 'noDuration'),
+            (10, 'noMagnitude'),
+            (11, 'noArea'),
+            (12, 'fXPersist'),
+            (13, 'unknown14'),
+            (14, 'goryVisuals'),
+            (15, 'hideinUI'),
+            (16, 'unknown17'),
+            (17, 'noRecast'),
+            (18, 'unknown19'),
+            (19, 'unknown20'),
+            (20, 'unknown21'),
+            (21, 'powerAffectsMagnitude'),
+            (22, 'powerAffectsDuration'),
+            (23, 'unknown24'),
+            (24, 'unknown25'),
+            (25, 'unknown26'),
+            (26, 'painless'),
+            (27, 'noHitEffect'),
+            (28, 'noDeathDispel'),
+            (29, 'unknown30'),
+            (30, 'unknown31'),
+            (31, 'unknown32'),
+    ))
+
+    melSet = MelSet(
+        MelString('EDID','eid'),
+        MelVmad(),
+        MelLString('FULL','full'),
+        MelFid('MDOB','harvestIngredient'),
+        MelCountedFidList('KWDA', 'keywords', 'KSIZ', '<I'),
+        MelStruct('DATA','IfIiiH2sIfIIIIffffIiIIIIiIIIfIfI4s4sIIIIff',
+            (MgefGeneralFlags,'flags',0L),'baseCost',(FID,'assocItem'),
+            'magicSkill','resistValue',
+            # 'counterEffectCount' is a count of ESCE records
+            'counterEffectCount',
+            ('unknown1',null2),(FID,'castingLight'),'taperWeight',(FID,'hitShader'),
+            (FID,'enchantShader'),'minimumSkillLevel','spellmakingArea',
+            'spellmakingCastingTime','taperCurve','taperDuration',
+            'secondAvWeight','mgefArchtype','actorValue',(FID,'projectile'),
+            (FID,'explosion'),'castingType','delivery','secondActorValue',
+            (FID,'castingArt'),(FID,'hitEffectArt'),(FID,'impactData'),
+            'skillUsageMultiplier',(FID,'dualCastingArt'),'dualCastingScale',
+            (FID,'enchantArt'),('unknown2',null4),('unknown3',null4),(FID,'equipAbility'),
+            (FID,'imageSpaceModifier'),(FID,'perkToApply'),'castingSoundLevel',
+            'scriptEffectAiScore','scriptEffectAiDelayTime',),
+        MelFids('ESCE','counterEffects'),
+        MelStructA('SNDD','2I','sounds','soundType',(FID,'sound')),
+        MelLString('DNAM','magicItemDescription'),
+        MelConditions(),
+        )
+    __slots__ = MelRecord.__slots__ + melSet.getSlotsUsed()
+
+    def dumpData(self,out):
+        counterEffects = self.counterEffects
+        self.counterEffectCount = len(counterEffects) if counterEffects else 0
+        MelRecord.dumpData(self,out)
+
+# Verified for 305
 class MreMisc(MelRecord):
     """Misc. Item"""
     classType = 'MISC'
