e1409e1b9ae0a2aeb4ca6b0aa49dd7afb26393c1
MrD <the.ubik@gmail.com>
2014-09-12 03:54:49 +0100

    Changed default in_plog() so P/C logs compare better:
    TODO:Revert parent as, in Import Cells:
    
    Cells/Worlds Patched
      Modified Oblivion.esm Records: 428
    
    Duh

--- Mopy/bash/patcher/oblivion/patchers/base.py
+++ Mopy/bash/patcher/oblivion/patchers/base.py
@@ -192,8 +192,9 @@ class ImportPatcher(AImportPatcher, ListPatcher):
     def _plog(self,log,logMsg,type_count):
         """Most common logging pattern - override as needed"""
         log(logMsg)
-        for type,count in sorted(type_count.iteritems()):
-            if count: log(u'* %s: %d' % (type,count))
+        for type_,count in sorted(type_count.iteritems()):
+            if count: log(u'* ' + _(u'Modified %(type)s Records: %(count)d')
+                          % {'type': type_, 'count': count})
 
 class CBash_ImportPatcher(AImportPatcher, CBash_ListPatcher):
     scanRequiresChecked = True
