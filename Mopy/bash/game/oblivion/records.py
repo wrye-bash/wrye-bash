61aafefa356e393471da192d4f978d761aa4d9ec
MrD <the.ubik@gmail.com>
2014-09-19 01:25:54 +0100

    records.py module:
    Changed the import position otherwise only skyrim loads (oblivion module not
    seen) - circular imports. It is the `from . import esp` in records.py
    that's to blame me thinks. See: http://stackoverflow.com/a/894885/281545
    
    --------------------- Mopy/bash/game/oblivion/__init__.py ---------------------
    index b11775d..d8f7cf0 100644
    @@ -25,7 +25,6 @@
     """This modules defines static data for use by bush, when
        TES IV: Oblivion is set at the active game."""
    
    -from game.oblivion.records import RecordHeader
     from oblivion_const import bethDataFiles, allBethFiles
     from ... import brec
     from ... import bush
    @@ -1678,6 +1677,7 @@ class esp:
    
         recordTypes = set(topTypes + 'GRUP,TES4,ROAD,REFR,ACHR,ACRE,PGRD,LAND,INFO'.split(','))
    
    +from records import * # MUST BE HERE otherwise all hell breaks loose
     #------------------------------------------------------------------------------
     # Record Elements    ----------------------------------------------------------
     #------------------------------------------------------------------------------

---
+++ Mopy/bash/game/oblivion/records.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash; if not, write to the Free Software Foundation,
+#  Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2014 Wrye Bash Team
+#  https://github.com/wrye-bash
+#
+# =============================================================================
+
+"""This module contains the oblivion record classes. Ripped from oblivion.py"""
+import struct
+import bash
+import bash.brec
+from . import esp
+
+#--Mod I/O
+class RecordHeader(bash.brec.BaseRecordHeader):
+    size = 20
+
+    def __init__(self,recType='TES4',size=0,arg1=0,arg2=0,arg3=0,*extra):
+        self.recType = recType
+        self.size = size
+        if recType == 'GRUP':
+            self.label = arg1
+            self.groupType = arg2
+            self.stamp = arg3
+        else:
+            self.flags1 = arg1
+            self.fid = arg2
+            self.flags2 = arg2
+        self.extra = extra
+
+    @staticmethod
+    def unpack(ins):
+        """Returns a RecordHeader object by reading the input stream."""
+        type,size,uint0,uint1,uint2 = ins.unpack('=4s4I',20,'REC_HEADER')
+        #--Bad?
+        if type not in esp.recordTypes:
+            raise bash.brec.ModError(ins.inName,u'Bad header type: '+repr(type))
+        #--Record
+        if type != 'GRUP':
+            pass
+        #--Top Group
+        elif uint1 == 0: # groupType == 0 (Top Group)
+            str0 = struct.pack('I',uint0)
+            if str0 in esp.topTypes:
+                uint0 = str0
+            elif str0 in esp.topIgTypes:
+                uint0 = esp.topIgTypes[str0]
+            else:
+                raise bash.brec.ModError(ins.inName,u'Bad Top GRUP type: '+repr(str0))
+        return RecordHeader(type,size,uint0,uint1,uint2)
+
+    def pack(self):
+        """Returns the record header packed into a string for writing to file."""
+        if self.recType == 'GRUP':
+            if isinstance(self.label,str):
+                return struct.pack('=4sI4sII',self.recType,self.size,self.label,self.groupType,self.stamp)
+            elif isinstance(self.label,tuple):
+                return struct.pack('=4sIhhII',self.recType,self.size,self.label[0],self.label[1],self.groupType,self.stamp)
+            else:
+                return struct.pack('=4s4I',self.recType,self.size,self.label,self.groupType,self.stamp)
+        else:
+            return struct.pack('=4s4I',self.recType,self.size,self.flags1,self.fid,self.flags2)
+
