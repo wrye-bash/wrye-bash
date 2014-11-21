wrye-bash/wrye-bash

commit 3fcbe3f87a99bca3f6aa85481697c0e3b073d390

Removed basher import from barb and belt (+ warns/import cleanup):
Notice new bash.bashFrame global introduced to eliminate the basher import
in barb - needs review, not sure if initialized when I set it in bash.
Belt only needed basher.settings -> bosh.settings (in barb too!).

Warnings corrected:
Comparison with None performed with equality operators
Unused local variables

Mopy/bash/bash.py
@@ -31,8 +31,6 @@ import atexit
 import os
 from time import time, sleep
 import sys
-import codecs
-import re
 import traceback
 import StringIO
 
@@ -41,8 +39,9 @@ import barg
 opts,extra = barg.parse()
 bass.language = opts.language
 import bolt
-from bolt import GPath, deprint
+from bolt import GPath
 basherImported = False
+bashFrame = None # introduced to avoid importing basher to barb.py
 #------------------------------------------------------------------------------
 def SetHomePath(homePath):
     drive,path = os.path.splitdrive(homePath)
@@ -74,11 +73,12 @@ def cmdBackup():
     # backup settings if app version has changed or on user request
     if not basherImported:
         import basher, barb
-    backup = None
+        global bashFrame
+        bashFrame = basher.bashFrame
     path = None
     quit = opts.backup and opts.quietquit
     if opts.backup: path = GPath(opts.filename)
-    backup = barb.BackupSettings(basher.bashFrame,path,quit,opts.backup_images)
+    backup = barb.BackupSettings(bashFrame,path,quit,opts.backup_images)
     if backup.PromptMismatch() or opts.backup:
         try:
             backup.Apply()
@@ -95,14 +95,17 @@ def cmdBackup():
 
 def cmdRestore():
     # restore settings on user request
-    if not basherImported: import basher, barb
+    if not basherImported:
+        import basher, barb
+        global bashFrame
+        bashFrame = basher.bashFrame
     backup = None
     path = None
     quit = opts.restore and opts.quietquit
     if opts.restore: path = GPath(opts.filename)
     if opts.restore:
         try:
-            backup = barb.RestoreSettings(basher.bashFrame,path,quit,
+            backup = barb.RestoreSettings(bashFrame,path,quit,
                                           opts.backup_images)
             backup.Apply()
         except barb.BackupCancelled:
@@ -630,8 +633,9 @@ def main():
     quit = cmdRestore() or quit
     if quit: return
 
-    global basherImported
+    global basherImported, bashFrame
     basherImported = True
+    bashFrame = basher.bashFrame
 
     basher.isUAC = isUAC
     if isUAC:

Mopy/bash/barb.py
@@ -22,7 +22,7 @@
 #
 # =============================================================================
 
-# Rollback library.
+"""Rollback library."""
 
 import os
 import re
@@ -33,7 +33,6 @@ from subprocess import Popen, PIPE
 import bash
 import bass
 import bosh
-import basher
 import bolt
 import bush
 from bosh import startupinfo, dirs
@@ -49,9 +48,9 @@ class BackupCancelled(BoltError):
 #------------------------------------------------------------------------------
 class BaseBackupSettings:
     def __init__(self, parent=None, path=None, quit=False):
-        if path != None and path.ext == u'' and not path.exists(): path = None
-        if path == None: path = basher.settings['bash.backupPath']
-        if path == None: path = dirs['modsBash']
+        if path is not None and path.ext == u'' and not path.exists(): path = None
+        if path is None: path = bosh.settings['bash.backupPath']
+        if path is None: path = dirs['modsBash']
         self.quit = quit
         self.dir = path
         self.archive = None
@@ -60,7 +59,7 @@ class BaseBackupSettings:
             self.archive = path.tail
         #end if
         self.parent = parent
-        self.verDat = basher.settings['bash.version']
+        self.verDat = bosh.settings['bash.version']
         self.verApp = bass.AppVersion
         self.files = {}
         self.tmp = None
@@ -87,12 +86,12 @@ class BaseBackupSettings:
         raise AbstractError
 
     def CmpDataVersion(self):
-        return cmp(self.verDat, basher.settings['bash.version'])
+        return cmp(self.verDat, bosh.settings['bash.version'])
 
     def CmpAppVersion(self):
         # Changed to prompt updating on any version change
         # Needs to check the cached value in settings for the initial upgrade check
-        return cmp(self.verApp, basher.settings['bash.version'])
+        return cmp(self.verApp, bosh.settings['bash.version'])
 
     def SameDataVersion(self):
         return not self.CmpDataVersion()
@@ -202,13 +201,13 @@ class BackupSettings(BaseBackupSettings):
         except StateError, e:
             raise
         #end try
-        basher.settings['bash.backupPath'] = self.dir
+        bosh.settings['bash.backupPath'] = self.dir
         self.InfoSuccess()
 
     def PromptFile(self):
         #prompt for backup filename
         #returns False if user cancels
-        if self.archive == None or self.dir.join(self.archive).exists():
+        if self.archive is None or self.dir.join(self.archive).exists():
             dt = datetime.datetime.now()
             file = u'Backup Bash Settings %s (%s) v%s-%s.7z' % (bush.game.fsName,dt.strftime(u'%Y-%m-%d %H.%M.%S'),self.verDat,self.verApp)
             if not self.quit:
@@ -228,10 +227,10 @@ class BackupSettings(BaseBackupSettings):
 
     def PromptMismatch(self):
         #returns False if same app version or old version == 0 (as in not previously installed) or user cancels
-        if basher.settings['bash.version'] == 0: return False
+        if bosh.settings['bash.version'] == 0: return False
         return not self.SameAppVersion() and self.PromptConfirm(
             _(u'A different version of Wrye Bash was previously installed.')+u'\n' +
-            _(u'Previous Version: ')+(u'%s\n' % basher.settings['bash.version']) +
+            _(u'Previous Version: ')+(u'%s\n' % bosh.settings['bash.version']) +
             _(u'Current Version: ')+(u'%s\n' % self.verApp) +
             _(u'Do you want to create a backup of your Bash settings before they are overwritten?'))
 
@@ -351,13 +350,13 @@ class RestoreSettings(BaseBackupSettings):
 
         # tell the user the restore is compete and warn about restart
         self.WarnRestart()
-        if basher.bashFrame: # should always exist
-            basher.bashFrame.Destroy()
+        if bash.bashFrame: # should always exist
+            bash.bashFrame.Destroy()
 
     def PromptFile(self):
         #prompt for backup filename
         #returns False if user cancels
-        if self.archive == None or not self.dir.join(self.archive).exists():
+        if self.archive is None or not self.dir.join(self.archive).exists():
             path = askOpen(self.parent,_(u'Restore Bash Settings'),self.dir,u'',u'*.7z')
             if not path: return False
             self.dir = path.head
@@ -376,7 +375,7 @@ class RestoreSettings(BaseBackupSettings):
         # return True if same app version or user confirms
         return self.SameAppVersion() or askWarning(self.parent,
               _(u'The version of Bash used to create the selected backup file does not match the current Bash version!')+u'\n' +
-              _(u'Backup v%s does not match v%s') % (self.verApp, basher.settings['bash.version']) + u'\n' +
+              _(u'Backup v%s does not match v%s') % (self.verApp, bosh.settings['bash.version']) + u'\n' +
               u'\n' +
               _(u'Do you want to restore this backup anyway?'),
               _(u'Warning: Version Mismatch!'))
@@ -386,7 +385,7 @@ class RestoreSettings(BaseBackupSettings):
         if self.CmpDataVersion() > 0:
             showError(self.parent,
                   _(u'The data format of the selected backup file is newer than the current Bash version!')+u'\n' +
-                  _(u'Backup v%s is not compatible with v%s') % (self.verApp, basher.settings['bash.version']) + u'\n' +
+                  _(u'Backup v%s is not compatible with v%s') % (self.verApp, bosh.settings['bash.version']) + u'\n' +
                   u'\n' +
                   _(u'You cannot use this backup with this version of Bash.'),
                   _(u'Error: Version Conflict!'))
@@ -409,7 +408,7 @@ class RestoreSettings(BaseBackupSettings):
             _(u'Before the settings can take effect, Wrye Bash must restart.')+u'\n' +
             _(u'Click OK to restart now.'),
             _(u'Bash Settings Restored'))
-        basher.bashFrame.Restart()
+        bash.bashFrame.Restart()
 
 #------------------------------------------------------------------------------
 def pack7z(dstFile, srcDir, progress=None):

Mopy/bash/basher.py
@@ -84,7 +84,6 @@ from patcher.patchers.special import \
 startupinfo = bolt.startupinfo
 
 #--Python
-import ConfigParser
 import StringIO
 import copy
 import datetime
@@ -94,26 +93,23 @@ import shutil
 import string
 import struct
 import sys
-import textwrap
 import time
 import subprocess
 import locale
 import win32gui
-import multiprocessing
 import webbrowser
 from types import *
-from operator import attrgetter,itemgetter
+from operator import attrgetter
 
 #--wxPython
 import wx
 import wx.gizmos
-from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin
 
 #--Balt
 import balt
 from balt import tooltip, fill, bell
 from balt import bitmapButton, button, toggleButton, checkBox, staticText, spinCtrl, textCtrl
-from balt import spacer, hSizer, vSizer, hsbSizer, vsbSizer
+from balt import spacer, hSizer, vSizer, hsbSizer
 from balt import colors, images, Image
 from balt import Links, Link, SeparatorLink, MenuLink
 from balt import ListCtrl

Mopy/bash/belt.py
@@ -29,7 +29,7 @@ import ScriptParser         # generic parser class
 from ScriptParser import error
 import wx
 import wx.wizard as wiz     # wxPython wizard class
-import bosh, balt, bolt, basher, bush
+import bosh, balt, bolt, bush
 import struct, string
 import win32api
 import StringIO
@@ -356,7 +356,7 @@ class PageSelect(PageInstaller):
 
 def generateTweakLines(wizardEdits, target):
     lines = []
-    lines.append(_(u'; Generated by Wrye Bash %s for \'%s\' via wizard') % (basher.settings['bash.version'], target.s))
+    lines.append(_(u'; Generated by Wrye Bash %s for \'%s\' via wizard') % (bosh.settings['bash.version'], target.s))
     for realSection in wizardEdits:
         if realSection == u']set[':
             modFormat = wizardEdits[realSection][0]+u' %(setting)s to %(value)s%(comment)s'
@@ -472,9 +472,9 @@ class PageFinish(PageInstaller):
         checkSubSizer.Add(self.checkApply,0,wx.BOTTOM,2)
         # Also install/anneal the package
         self.checkInstall = wx.CheckBox(self, wx.ID_ANY, _(u'Install this package'))
-        self.checkInstall.SetValue(basher.settings['bash.installers.autoWizard'])
+        self.checkInstall.SetValue(bosh.settings['bash.installers.autoWizard'])
         self.checkInstall.Bind(wx.EVT_CHECKBOX, self.OnCheckInstall)
-        self.parent.ret.Install = basher.settings['bash.installers.autoWizard']
+        self.parent.ret.Install = bosh.settings['bash.installers.autoWizard']
         checkSubSizer.Add(self.checkInstall)
         checkSizer.Add(checkSubSizer,0,wx.EXPAND)
         sizerMain.Add(checkSizer,0,wx.TOP|wx.RIGHT|wx.EXPAND,5)
@@ -1114,7 +1114,7 @@ class WryeParser(ScriptParser.Parser):
             # No graphics extender available for this game
             return 1
     def fnCompareWBVersion(self, wbWant):
-        wbHave = basher.settings['bash.version']
+        wbHave = bosh.settings['bash.version']
         return cmp(float(wbHave), float(wbWant))
     def fnDataFileExists(self, *filenames):
         for filename in filenames:
@@ -1586,7 +1586,7 @@ class WryeParser(ScriptParser.Parser):
         geWant = self._TestVersion_Want(ge)
         if geWant == u'None': ge = u'None'
         if not wbWant: wbWant = u'0.0'
-        wbHave = basher.settings['bash.version']
+        wbHave = bosh.settings['bash.version']
 
         ret = self._TestVersion(gameWant, bosh.dirs['app'].join(bush.game.exe))
         bGameOk = ret[0] >= 0

commit e7f9493637d1c0a64877093e8017b6f1cdbd1849

Regression - bash.bashFrame was None and restore settings failed:
See: #163
3fcbe3f87a99bca3f6aa85481697c0e3b073d390 (needed review!) introduced:

Traceback (most recent call last):
  File "<string>", line 11205, in Execute
  File "bash\barb.py", line 352, in Apply
    self.WarnRestart()
  File "bash\barb.py", line 411, in WarnRestart
    bash.bashFrame.Restart()
AttributeError: 'NoneType' object has no attribute 'Restart'

Turns out bashFrame is initialized in basher.BashApp.onInit(self):

def Init(self): # not OnInit(), we need to initialize _after_ the app
# has been instanced

There frame = BashFrame() is called and in there `BashFrame = self` is set.

Change: now basher.BashApp.onInit() returns the bashFrame !

Mopy/bash/bash.py
@@ -633,9 +633,8 @@ def main():
     quit = cmdRestore() or quit
     if quit: return
 
-    global basherImported, bashFrame
+    global basherImported
     basherImported = True
-    bashFrame = basher.bashFrame
 
     basher.isUAC = isUAC
     if isUAC:
@@ -691,7 +690,8 @@ def main():
             basher.uacRestart = True
             return
 
-    app.Init()
+    global bashFrame
+    bashFrame = app.Init() # basher.bashFrame is set here !
     app.MainLoop()
 
 if __name__ == '__main__':

Mopy/bash/basher/__init__.py
@@ -6147,7 +6147,8 @@ class WryeBashSplashScreen(wx.SplashScreen):
 #------------------------------------------------------------------------------
 class BashApp(wx.App):
     """Bash Application class."""
-    def Init(self): # not OnInit(), we need to initialize _after_ the app has been instanced
+    def Init(self): # not OnInit(), we need to initialize _after_ the app has been instanciated
+        """Initialize the application data, create and return the BashFrame."""
         global appRestart
         appRestart = False
         """wxWindows: Initialization handler."""
@@ -6156,7 +6157,7 @@ class BashApp(wx.App):
         splashScreen = None
         progress = wx.ProgressDialog(u'Wrye Bash',_(u'Initializing')+u' '*10,
              style=wx.PD_AUTO_HIDE|wx.PD_APP_MODAL|wx.PD_SMOOTH)
-        #   Any users who prefer the progress dialog can rename or delete wryesplash.png
+        # Is splash enabled in ini ?
         if bosh.inisettings['EnableSplashScreen']:
             if bosh.dirs['images'].join(u'wryesplash.png').exists():
                 try:
@@ -6173,7 +6174,7 @@ class BashApp(wx.App):
         self.InitVersion()
         #--MWFrame
         progress.Update(80,_(u'Initializing Windows'))
-        frame = BashFrame(
+        frame = BashFrame( # basher.bashFrame global set here
              pos=settings['bash.framePos'],
              size=settings['bash.frameSize'])
         progress.Destroy()
@@ -6182,6 +6183,7 @@ class BashApp(wx.App):
         self.SetTopWindow(frame)
         frame.Show()
         balt.ensureDisplayed(frame)
+        return frame
 
     def InitResources(self):
         """Init application resources."""