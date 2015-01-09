wrye-bash/wrye-bash

commit 4a24320b50f90888a0ced376500862b7d4a55200

Import fixes - skyrim module recognised
More import fixes (module was recognised even without those)

Import fixes 2 : from ...bolt import _encode, _unicode (as in skyrim.py)

Import fixes 3 : from constants import fid5Conditions

Another missed from skyrim.py: from_iterable

----------------------- Mopy/bash/game/skyrim/records.py -----------------------
index cd830fe..f1712c3 100644
@@ -27,4 +27,6 @@
 import struct
+import itertools
 from . import esp
-from ...bolt import StateError, Flags, BoltError, sio, DataDict, winNewLines
+from ...bolt import StateError, Flags, BoltError, sio, DataDict, winNewLines, \
+    _encode, _unicode
 from ...brec import MelRecord, BaseRecordHeader, ModError, MelStructs, null3, \
@@ -37,3 +39,6 @@
 from ...bush import genericAVEffects, mgef_school, mgef_basevalue, actorValues
-from constants import allConditions, fid1Conditions, fid2Conditions
+from constants import allConditions, fid1Conditions, fid2Conditions, \
+    fid5Conditions
+
+from_iterable = itertools.chain.from_iterable

 # This is the 2nd commit message:

Changed to import bush - notice nasty mgef_school or mgef_school

----------------------- Mopy/bash/game/skyrim/records.py -----------------------
index f1712c3..39677bb 100644
@@ -38,3 +38,3 @@
     MelOptStructA, MelCountedFids, MelSortedFidList
-from ...bush import genericAVEffects, mgef_school, mgef_basevalue, actorValues
+from ... import bush
 from constants import allConditions, fid1Conditions, fid2Conditions, \
@@ -446,3 +446,3 @@ def getEffects(self):
         effects = []
-        avEffects = genericAVEffects
+        avEffects = bush.genericAVEffects
         effectsAppend = effects.append
@@ -455,3 +455,3 @@ def getEffects(self):

-    def getSpellSchool(self,mgef_school=mgef_school):
+    def getSpellSchool(self,mgef_school=bush.mgef_school):
         """Returns the school based on the highest cost spell effect."""
@@ -460,3 +460,3 @@ def getSpellSchool(self,mgef_school=mgef_school):

Mopy/bash/game/skyrim/__init__.py
@@ -1447,7 +1447,9 @@ mergeClasses = (
     )
 
 #--Extra read classes: need info from magic effects
-readClasses = (MreMgef, MreScpt,)
+readClasses = (MreMgef,
+               # MreScpt, # Can't be resolved
+               )
 writeClasses = (MreMgef,)
 
 

commit 144b707a74c6bba504efff9b1a1164e3ac783a44

Regression: bsa redirection was enabled for Skyrim:
So switching to the installers tab would result in:

Traceback (most recent call last):
  File "bash\basher\__init__.py", line 4218, in OnShowPage
    self.GetPage(event.GetSelection()).OnShow()
  File "bash\basher\__init__.py", line 3065, in OnShow
    if data.refresh(progress,what,self.fullRefresh):
  File "bash\bosh.py", line 7785, in refresh
    oblivionIni.setBsaRedirection(True)
  File "bash\bosh.py", line 2684, in setBsaRedirection
    source.mtime = aiBsaMTime
  File "C:\Dropbox\eclipse_workspaces\python\wrye-bash\Mopy\bash\bolt.py", line 733, in setmtime
    os.utime(self._s,(self.atime,int(mtime)))
  File "C:\Dropbox\eclipse_workspaces\python\wrye-bash\Mopy\bash\bolt.py", line 689, in atime
    return os.path.getatime(self._s)
  File "C:\_\Python27\lib\genericpath.py", line 59, in getatime
    return os.stat(filename).st_atime
WindowsError: [Error 2] The system cannot find the file specified: u'C:\\Dropbox\\eclipse_workspaces\\python\\wrye-bash\\Mopy\\templates\\Skyrim\\ArchiveInvalidationInvalidated!.bsa'

See: second post on #177.

good: 3d692800f7cf323061cc5414abbbb4a50c473897
bad: 4a24320b50f90888a0ced376500862b7d4a55200

Introduced in 682bf130b36866ce26983c204eef1ba4288cac53 (broken for TESV!).
Apparently, on rebasing on dev this:

     #--INI Entry to enable BSA Redirection
-    bsaRedirection = (u'sResourceArchiveList',u'sResourceArchiveList2')
+    bsaRedirection = (u'',u'')

was left out.
Went through the bad -> good diff. Notes:
Some of the namespace changes, such as bolt.Flags -> Flags, were not needed
and blurred the diffs - lesson: no namespace changes if it can be avoided.
Diffs were blurred also by warning fixes and the like - oh well.
Added back a couple comments that were clearer in good (a lot were clearer
in bad I must say).

Newlines nitpick.

Mopy/bash/game/skyrim/__init__.py
@@ -148,7 +148,7 @@ class ini:
     allowNewLines = True
 
     #--INI Entry to enable BSA Redirection
-    bsaRedirection = (u'sResourceArchiveList',u'sResourceArchiveList2')
+    bsaRedirection = (u'',u'')
 
 #--Save Game format stuff
 class ess:
@@ -260,6 +260,7 @@ masterFiles = [
     u'Skyrim.esm',
     u'Update.esm',
     ]
+
 #--Plugin files that can't be deactivated
 nonDeactivatableFiles = [
     u'Skyrim.esm',
@@ -1446,12 +1447,12 @@ mergeClasses = (
         MreLvsp, MreMisc,
     )
 
-#--Extra read classes: need info from magic effects
+#--Extra read classes: these record types will always be loaded, even if
+# patchers don't need them directly (for example, MGEF for magic effects info)
 # MreScpt is Oblivion/FO3/FNV Only
 # MreMgef, has not been verified to be used here for Skyrim
 readClasses = ()
 writeClasses = ()
-
 
 def init():
     # Due to a bug with py2exe, 'reload' doesn't function properly.  Instead of