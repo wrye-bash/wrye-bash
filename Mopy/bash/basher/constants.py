6657f4450218520a9a242889697408bd0ddb3c34
MrD <the.ubik@gmail.com>
2015-01-09 23:28:58 +0100

    Regression: defaultGroups should be a list:
    Traceback (most recent call last):
      File "bash\balt.pyo", line 1798, in DoItemMenu
      File "bash\balt.pyo", line 2218, in PopupMenu
      File "bash\balt.pyo", line 2330, in AppendToMenu
      File "bash\balt.pyo", line 2362, in AppendToMenu
      File "bash\balt.pyo", line 2350, in _range
      File "bash\basher\mod_links.pyo", line 226, in items
    AttributeError: 'tuple' object has no attribute 'sort'
    
    cf 'bash.mods.ratings': ['+','1','2','3','4','5','=','~']
    
    Introduced in 2cb422fc35aa0454fc0bb0a037e9b8df8220d55b:
    -    'bash.mods.groups': [x[0] for x in bush.baloGroups],
    +    'bash.mods.groups': bush.defaultGroups, # a tuple

--- Mopy/bash/basher/constants.py
+++ Mopy/bash/basher/constants.py
@@ -242,7 +242,7 @@ settingDefaults = { # TODO(ut): belongs to bosh (or better to a settings package
         },
     #--Wrye Bash: Group and Rating
     'bash.mods.autoGhost':False,
-    'bash.mods.groups': bush.defaultGroups,
+    'bash.mods.groups': list(bush.defaultGroups),
     'bash.mods.ratings': ['+','1','2','3','4','5','=','~'],
     #--Wrye Bash: Col (Sort) Names
     'bash.colNames': {
