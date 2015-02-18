wrye-bash/wrye-bash

commit c62f4e8d8da3420ef3075553e3b8b6bca7de8b1e

Nitpick:
Coding style and such

balt, basher
- __enter__ and __exit__ are defined in wx.Dialog, no need to override
- last wx.TheClipboard pruned
- nitpick in mod links (report warned as possibly uninitialized, wrapping)

bosh: imports cleanup, coding style, tableUpdate is a subset of toDelete IIUC

Couple leftover TODOs in patchers

Mopy/bash/basher/__init__.py
@@ -66,7 +66,8 @@ startupinfo = bolt.startupinfo
 from .. import balt
 from ..balt import tooltip, fill, bell, CheckLink, EnabledLink, SeparatorLink, \
     Link, ChoiceLink, copyListToClipboard, roTextCtrl, staticBitmap
-from ..balt import bitmapButton, button, toggleButton, checkBox, staticText, spinCtrl, textCtrl
+from ..balt import bitmapButton, button, toggleButton, checkBox, staticText, \
+    spinCtrl, textCtrl
 from ..balt import spacer, hSizer, vSizer, hsbSizer
 from ..balt import colors, images, Image
 from ..balt import Links, ListCtrl, ItemLink
@@ -2996,12 +2997,7 @@ class InstallersList(balt.Tank):
                 if path.exists(): path.start()
         elif event.CmdDown() and code == ord('V'):
             ##Ctrl+V
-            if wx.TheClipboard.Open():
-                if wx.TheClipboard.IsSupported(wx.DataFormat(wx.DF_FILENAME)):
-                    obj = wx.FileDataObject()
-                    wx.TheClipboard.GetData(obj)
-                    wx.CallLater(10,self.OnDropFiles,0,0,obj.GetFilenames())
-                wx.TheClipboard.Close()
+            balt.clipboardDropFiles(10, self.OnDrKopFiles)
         else:
             event.Skip()
 
@@ -5170,7 +5166,8 @@ class BashFrame(wx.Frame):
             self.notebook.GetPage(index).OnCloseWindow()
         settings.save()
 
-    def CleanSettings(self):
+    @staticmethod
+    def CleanSettings():
         """Cleans junk from settings before closing."""
         #--Clean rename dictionary.
         modNames = set(bosh.modInfos.data.keys())

Mopy/bash/balt.py
@@ -1194,12 +1194,6 @@ class Dialog(wx.Dialog):
         self.Destroy()
         event.Skip()
 
-    #  __enter__ and __exit__ for use with the 'with' statement
-    def __enter__(self):
-        return self
-    def __exit__(self,type,value,traceback):
-        self.Destroy()
-
     @classmethod
     def Display(cls, *args, **kwargs):
         """Instantiate a dialog, display it and return the ShowModal result."""
@@ -2466,6 +2460,14 @@ def copyListToClipboard(selected):
         clipData = wx.FileDataObject()
         for mod in selected: clipData.AddFile(mod)
         wx.TheClipboard.SetData(clipData)
+        wx.TheClipboard.Close()
+
+def clipboardDropFiles(millis, callable_):
+    if wx.TheClipboard.Open():
+        if wx.TheClipboard.IsSupported(wx.DataFormat(wx.DF_FILENAME)):
+            obj = wx.FileDataObject()
+            wx.TheClipboard.GetData(obj)
+            wx.CallLater(millis, callable_, 0, 0, obj.GetFilenames())
         wx.TheClipboard.Close()
 
 def getKeyState(key): return wx.GetKeyState(key)

Mopy/bash/basher/mod_links.py
@@ -1974,8 +1974,8 @@ class Mod_Scripts_Import(_Mod_Import_Link):
             _(u'Choose directory to import scripts from'),defaultPath)
         if textDir is None:
             return
-        message = (_(u"Import scripts that don't exist in the esp as new scripts?")
-                   + u'\n' +
+        message = (_(u"Import scripts that don't exist in the esp as new"
+                     u" scripts?") + u'\n' +
                    _(u'(If not they will just be skipped).')
                    )
         makeNew = balt.askYes(self.window, message, _(u'Import Scripts'),
@@ -1983,28 +1983,32 @@ class Mod_Scripts_Import(_Mod_Import_Link):
         scriptText = self._parser()
         scriptText.readFromText(textDir.s,fileInfo)
         changed, added = scriptText.writeToMod(fileInfo,makeNew)
-    #--Log
+        #--Log
         if not (len(changed) or len(added)):
             balt.showOk(self.window,_(u"No changed or new scripts to import."),
                         _(u"Import Scripts"))
+            return
+        if changed:
+            changedScripts = (_(u'Imported %d changed scripts from %s:') +
+                              u'\n%s') % (
+                len(changed), textDir.s, u'*' + u'\n*'.join(sorted(changed)))
         else:
-            if changed:
-                changedScripts = (_(u'Imported %d changed scripts from %s:')
-                                  + u'\n%s') % (len(changed),textDir.s,u'*'+u'\n*'.join(sorted(changed)))
-            else:
-                changedScripts = u''
-            if added:
-                addedScripts = (_(u'Imported %d new scripts from %s:')
-                                + u'\n%s') % (len(added),textDir.s,u'*'+u'\n*'.join(sorted(added)))
-            else:
-                addedScripts = u''
-            if changed and added:
-                report = changedScripts + u'\n\n' + addedScripts
-            elif changed:
-                report = changedScripts
-            elif added:
-                report = addedScripts
-            balt.showLog(self.window,report,_(u'Import Scripts'),icons=Resources.bashBlue)
+            changedScripts = u''
+        if added:
+            addedScripts = (_(u'Imported %d new scripts from %s:')
+                            + u'\n%s') % (
+                len(added), textDir.s, u'*' + u'\n*'.join(sorted(added)))
+        else:
+            addedScripts = u''
+        report = None
+        if changed and added:
+            report = changedScripts + u'\n\n' + addedScripts
+        elif changed:
+            report = changedScripts
+        elif added:
+            report = addedScripts
+        balt.showLog(self.window, report, _(u'Import Scripts'),
+                     icons=Resources.bashBlue)
 
 #------------------------------------------------------------------------------
 from ..patcher.utilities import ItemStats, CBash_ItemStats

Mopy/bash/basher/saves_links.py
@@ -297,6 +297,7 @@ class Save_RenamePlayer(EnabledLink):
         bosh.saveInfos.refresh()
         self.window.RefreshUI()
 
+#------------------------------------------------------------------------------
 class Save_ExportScreenshot(OneItemLink):
     """Exports the saved screenshot from a save game."""
     text = _(u'Export Screenshot...')

Mopy/bash/bosh.py
@@ -63,8 +63,8 @@ from _ctypes import POINTER
 from ctypes import cast, c_ulong
 from cint import ObCollection, ObModFile, MGEFCode, FormID, dump_record, CBash, \
     ObBaseRecord
-from brec import _coerce, MreRecord, ModReader, ModError, ModWriter, \
-    getModIndex, genFid, getObjectIndex, getFormIndices
+from brec import MreRecord, ModReader, ModError, ModWriter, getModIndex, \
+    genFid, getObjectIndex, getFormIndices
 from patcher.record_groups import MobWorlds, MobDials, MobICells, \
     MobObjects, MobBase
 import loot
@@ -200,7 +200,6 @@ class PickleDict(bolt.PickleDict):
         """
         result = bolt.PickleDict.load(self)
         if not result and self.oldPath.exists():
-            ins = None
             try:
                 with self.oldPath.open('r') as ins:
                     self.data.update(cPickle.load(ins))
@@ -208,11 +207,11 @@ class PickleDict(bolt.PickleDict):
             except EOFError:
                 pass
         #--Update paths
-        def textDump(path):
-            deprint(u'Text dump:',path)
-            with path.open('w',encoding='utf-8-sig') as out:
-                for key,value in self.data.iteritems():
-                    out.write(u'= %s:\n  %s\n' % (key,value))
+        # def textDump(path):
+        #     deprint(u'Text dump:',path)
+        #     with path.open('w',encoding='utf-8-sig') as out:
+        #         for key,value in self.data.iteritems():
+        #             out.write(u'= %s:\n  %s\n' % (key,value))
         #textDump(self.path+'.old.txt')
         if not self.vdata.get('boltPaths',False):
             self.updatePaths()
@@ -3918,6 +3917,7 @@ class INIInfo(FileInfo):
                 log(line)
             return bolt.winNewLines(log.out.getvalue())
 
+#------------------------------------------------------------------------------
 class SaveInfo(FileInfo):
     def getFileInfos(self):
         """Returns modInfos or saveInfos depending on fileInfo type."""
@@ -4073,7 +4073,7 @@ class FileInfos(DataDict):
             # Normal folder items
             names |= {x for x in self.dir.list() if self.dir.join(x).isfile() and self.rightFileType(x)}
         names = list(names)
-        names.sort(key=lambda x: x.cext == u'.ghost')
+        names.sort(key=lambda x: x.cext == u'.ghost') # TODO(ut) Modinfos specific !!!@!
         for name in names:
             if self.dirdef and not self.dir.join(name).isfile():
                 fileInfo = self.factory(self.dirdef,name)
@@ -4170,10 +4170,9 @@ class FileInfos(DataDict):
             balt.shellDelete(toDelete, askOk_=askOk,recycle=not dontRecycle)
         finally:
             #--Table
-            for filePath in toDelete:
-                if filePath in tableUpdate:
-                    if not filePath.exists():
-                        self.table.delRow(tableUpdate[filePath])
+            for filePath in tableUpdate:
+                if not filePath.exists():
+                    self.table.delRow(tableUpdate[filePath])
             #--Refresh
             if doRefresh:
                 self.refresh()
@@ -4410,10 +4409,10 @@ class ModInfos(FileInfos):
     def autoGhost(self,force=False):
         """Automatically inactive files to ghost."""
         changed = []
-        allowGhosting = self.table.getColumn('allowGhosting')
         toGhost = settings.get('bash.mods.autoGhost',False)
         if force or toGhost:
             active = self.plugins.selected
+            allowGhosting = self.table.getColumn('allowGhosting')
             for mod in self.data:
                 modInfo = self.data[mod]
                 modGhost = toGhost and mod not in active and allowGhosting.get(mod,True)
@@ -5224,6 +5223,7 @@ class ModInfos(FileInfos):
 
 #------------------------------------------------------------------------------
 class SaveInfos(FileInfos):
+
     """SaveInfo collection. Represents save directory and related info."""
     #--Init
     def __init__(self):
@@ -5237,7 +5237,7 @@ class SaveInfos(FileInfos):
 
     #--Right File Type (Used by Refresh)
     def rightFileType(self,fileName):
-        """Bool: File is a mod."""
+        """Bool: File is a save."""
         return reSaveExt.search(fileName.s)
 
     def refresh(self):
@@ -5270,7 +5270,8 @@ class SaveInfos(FileInfos):
         CoSaves(self.dir,fileName).move(destDir,fileName)
 
     #--Local Saves ------------------------------------------------------------
-    def getLocalSaveDirs(self):
+    @staticmethod
+    def getLocalSaveDirs():
         """Returns a list of possible local save directories, NOT including the base directory."""
         baseSaves = dirs['saveBase'].join(u'Saves')
         if baseSaves.exists():
@@ -5527,7 +5528,6 @@ class ModRuleSet:
         self.modGroups = []
 
 #------------------------------------------------------------------------------
-
 class ConfigHelpers:
     """Encapsulates info from mod configuration helper files (LOOT masterlist, etc.)"""
 
@@ -10267,6 +10267,7 @@ class SaveEnchantments:
                 count += 1
         self.saveFile.safeSave()
 
+#------------------------------------------------------------------------------
 class Save_NPCEdits:
     """General editing of NPCs/player in savegame."""
 

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -2298,7 +2298,6 @@ class CBash_AssortedTweak_FactioncrimeGoldMultiplier(
 #------------------------------------------------------------------------------
 class AAssortedTweak_LightFadeValueFix(AMultiTweakItem):
     """Remove light flickering for low end machines."""
-    # TODO : was """Sets light fade value when not set to 1.0."""
 
     #--Config Phase -----------------------------------------------------------
     def __init__(self):

Mopy/bash/patcher/patchers/multitweak_names.py
@@ -299,8 +299,7 @@ class CBash_NamesTweak_Potions(ANamesTweak_Potions,CBash_MultiTweakItem):
                     record._RecordID = override._RecordID
 
 #------------------------------------------------------------------------------
-reSpell = re.compile(u'^(\([ACDIMR]\d\)|\w{3,6}:) ',re.U) # compile once (TODO:
-#  should be faster ? name ?)
+reSpell = re.compile(u'^(\([ACDIMR]\d\)|\w{3,6}:) ',re.U) # compile once
 
 class ANamesTweak_Scrolls(AMultiTweakItem):
     reOldLabel = reSpell
@@ -861,7 +860,7 @@ class CBash_TextReplacer(ATextReplacer,CBash_MultiTweakItem):
                     for entry in stage.entries:
                         changed = reMatch.search(entry.text or u'')
                         if changed: break
-##### TODO: is this code needed ?
+##### CRUFT: is this code needed ?
 ##                        compiled = entry.compiled_p
 ##                        if compiled:
 ##                            changed = reMatch.search(struct.pack('B' * len(compiled), *compiled) or '')
@@ -919,7 +918,7 @@ class CBash_TextReplacer(ATextReplacer,CBash_MultiTweakItem):
                             newString = entry.text
                             if newString:
                                 entry.text = reMatch.sub(self.reReplace, newString)
-##### TODO: is this code needed ?
+##### CRUFT: is this code needed ?
 ##                            newString = entry.compiled_p
 ##                            if newString:
 ##                                nSize = len(newString)

commit 986db60bd721deb388e3405fdcc6525cb20c0139

Typo
Introduced in c62f4e8d8da3420ef3075553e3b8b6bca7de8b1e

Mopy/bash/basher/__init__.py
@@ -2774,7 +2774,7 @@ class InstallersList(balt.Tank):
                 if path.exists(): path.start()
         elif event.CmdDown() and code == ord('V'):
             ##Ctrl+V
-            balt.clipboardDropFiles(10, self.OnDrKopFiles)
+            balt.clipboardDropFiles(10, self.OnDropFiles)
         else:
             event.Skip()
 