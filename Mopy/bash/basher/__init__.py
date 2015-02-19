wrye-bash/wrye-bash

commit ed39a8324bfd4ce6e54494104af8e71f27c2ff0e

Deglobalization: basher.bashFrame binned:
Using Link.Frame is not ideal (hence I wanted to reduce its uses) - avoid.
But `parent = parent or Link.Frame` - put there to eventually drop parent
argument - was not so innocuous: I had to fixup as Tab_Menu threw:

				 menu.append(Tab_Link(key,canDisable))
-            menu.PopupMenu(self, None)
+            menu.PopupMenu(self, Link.Frame, None)
		 else:

ini = None was unused

Mopy/bash/basher/__init__.py
@@ -113,7 +113,6 @@ isUAC = False      # True if the game is under UAC protection
 modDetails = None
 saveDetails = None
 gInstallers = None
-bashFrame = None
 gPeople = None # New global - yak
 
 # Settings --------------------------------------------------------------------
@@ -1043,7 +1042,6 @@ class INILineCtrl(wx.ListCtrl):
         num = self.GetItemCount()
         if resetScroll:
             self.EnsureVisible(0)
-        ini = None
         try:
             with bosh.iniInfos.ini.path.open('r') as ini:
                 lines = ini.readlines()
@@ -1056,8 +1054,8 @@ class INILineCtrl(wx.ListCtrl):
                     self.DeleteItem(len(lines))
         except IOError:
             warn = True
-            if hasattr(bashFrame,'notebook'):
-                page = bashFrame.notebook.GetPage(bashFrame.notebook.GetSelection())
+            if hasattr(Link.Frame,'notebook'): ##: why all this fuss ?
+                page = Link.Frame.notebook.currentPage
                 if page != self.GetParent().GetParent().GetParent():
                     warn = False
             if warn:
@@ -3331,7 +3329,7 @@ class InstallersPanel(SashTankPanel):
         selected = self.gEspmList.HitTest((x,y))
         self.gEspmList.SetSelection(selected)
         #--Show/Destroy Menu
-        InstallersPanel.espmMenu.PopupMenu(self,bashFrame,selected)
+        InstallersPanel.espmMenu.PopupMenu(self, selected)
 
     def SubsSelectionMenu(self,event):
         """Handle right click in espm list."""
@@ -3340,7 +3338,7 @@ class InstallersPanel(SashTankPanel):
         selected = self.gSubList.HitTest((x,y))
         self.gSubList.SetSelection(selected)
         #--Show/Destroy Menu
-        InstallersPanel.subsMenu.PopupMenu(self,bashFrame,selected)
+        InstallersPanel.subsMenu.PopupMenu(self, selected)
 
     def OnCheckEspmItem(self,event):
         """Handle check/uncheck of item."""
@@ -4168,7 +4166,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
             for key in settings['bash.tabs.order']:
                 canDisable = bool(key != 'Mods')
                 menu.append(Tab_Link(key,canDisable))
-            menu.PopupMenu(self,bashFrame,None)
+            menu.PopupMenu(self, Link.Frame, None)
         else:
             event.Skip()
 
@@ -4464,8 +4462,6 @@ class BashFrame(wx.Frame):
 
     def __init__(self, parent=None, pos=balt.defPos, size=(400, 500)):
         #--Singleton
-        global bashFrame
-        bashFrame = self
         balt.Link.Frame = self
         #--Window
         wx.Frame.__init__(self, parent, title=u'Wrye Bash', pos=pos, size=size)
@@ -4854,7 +4850,7 @@ class BashApp(wx.App):
         self.InitVersion()
         #--MWFrame
         progress.Update(80,_(u'Initializing Windows'))
-        frame = BashFrame( # basher.bashFrame global set here
+        frame = BashFrame( # Link.Frame global set here
              pos=settings['bash.framePos'],
              size=settings['bash.frameSize'])
         progress.Destroy()

Mopy/bash/balt.py
@@ -2192,7 +2192,8 @@ class Links(list):
     """List of menu or button links."""
 
     #--Popup a menu from the links
-    def PopupMenu(self,parent,eventWindow=None,*args):
+    def PopupMenu(self, parent=None, eventWindow=None, *args):
+        parent = parent or Link.Frame
         eventWindow = eventWindow or parent
         menu = wx.Menu()
         Link.Popup = menu

Mopy/bash/basher/dialogs.py
@@ -210,7 +210,7 @@ class ColorDialog(balt.Dialog):
         return self.text_key[self.comboBox.GetValue()]
 
     def UpdateUIColors(self):
-        """Update the bashFrame with the new colors"""
+        """Update the Bash Frame with the new colors"""
         nb = Link.Frame.notebook
         with balt.BusyCursor():
             for (className,title,panel) in tabInfo.itervalues():

Mopy/bash/basher/settings_links.py
@@ -62,7 +62,7 @@ class Settings_BackupSettings(ItemLink):
             msg = msg or _(u'Do you want to backup your Bash settings now?')
             return balt.askYes(Link.Frame, msg,_(u'Backup Bash Settings?'))
         BashFrame.SaveSettings(Link.Frame)
-        #backup = barb.BackupSettings(bashFrame)
+        #backup = barb.BackupSettings(Link.Frame)
         try:
             if PromptConfirm():
                 dialog = balt.Dialog(Link.Frame,_(u'Backup Images?'),size=(400,200))

commit 3a1259bc200feb996c846284b4397ad96244b9c5

Regression: espms menus
Introduced in ed39a8324bfd4ce6e54494104af8e71f27c2ff0e

Mopy/bash/basher/__init__.py
@@ -3333,7 +3333,7 @@ class InstallersPanel(SashTankPanel):
         selected = self.gEspmList.HitTest((x,y))
         self.gEspmList.SetSelection(selected)
         #--Show/Destroy Menu
-        InstallersPanel.espmMenu.PopupMenu(self, selected)
+        InstallersPanel.espmMenu.PopupMenu(self, Link.Frame, selected)
 
     def SubsSelectionMenu(self,event):
         """Handle right click in espm list."""
@@ -3342,7 +3342,7 @@ class InstallersPanel(SashTankPanel):
         selected = self.gSubList.HitTest((x,y))
         self.gSubList.SetSelection(selected)
         #--Show/Destroy Menu
-        InstallersPanel.subsMenu.PopupMenu(self, selected)
+        InstallersPanel.subsMenu.PopupMenu(self, Link.Frame, selected)
 
     def OnCheckEspmItem(self,event):
         """Handle check/uncheck of item."""