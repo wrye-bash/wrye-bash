wrye-bash/wrye-bash

commit 95533a7ed1c7ea41235c592d39fda2abc97d64c0

Last use of `details` parameter - Mod_FlipMasters rewrite:
- Simplified the logic
- Disabled on esm-mastered only mods
- Refresh mod data to account for LO changes - I had to edit
ModInfos.sameAs() too.
- Docs

Mopy/bash/basher/mod_links.py
@@ -1523,45 +1523,49 @@ class Mod_FlipSelf(EnabledLink):
 #------------------------------------------------------------------------------
 class Mod_FlipMasters(OneItemLink):
     """Swaps masters between esp and esm versions."""
+    help = _(
+        u"Flip esp/esm bit of esp masters to convert them to/from esm state")
 
     def _initData(self, window, selection):
         super(Mod_FlipMasters, self)._initData(window, selection)
         #--FileInfo
-        self.fileName = fileName = GPath(self.selected[0])
-        self.fileInfo = fileInfo = bosh.modInfos[fileName] # window.data == bosh.modInfos
+        self.fileName = GPath(self.selected[0])
+        fileInfo = bosh.modInfos[self.fileName]
         self.text = _(u'Esmify Masters')
-        if len(selection) == 1 and len(fileInfo.header.masters) > 1:
-            espMasters = [master for master in fileInfo.header.masters if bosh.reEspExt.search(master.s)]
-            if not espMasters: return
-            for masterName in espMasters:
-                masterInfo = bosh.modInfos.get(GPath(masterName),None)
-                if masterInfo and masterInfo.isInvertedMod():
-                    self.text = _(u'Espify Masters')
-                    self.toEsm = False
-                    break
-            else:
-                self.toEsm = True
+        enable = len(selection) == 1 and len(fileInfo.header.masters) > 1
+        self.espMasters = [GPath(master) for master in fileInfo.header.masters
+            if bosh.reEspExt.search(master.s)] if enable else []
+        self.enable = enable and bool(self.espMasters)
+        if not self.enable: return
+        for masterName in self.espMasters:
+            masterInfo = bosh.modInfos.get(GPath(masterName),None)
+            if masterInfo and masterInfo.isInvertedMod():
+                self.text = _(u'Espify Masters')
+                self.toEsm = False
+                break
+        else:
+            self.toEsm = True
 
-    def _enable(self): return super(Mod_FlipMasters, self)._enable() and \
-                              len(self.fileInfo.header.masters) > 1
+    def _enable(self): return self.enable
 
     def Execute(self,event):
         message = _(u"WARNING! For advanced modders only! Flips esp/esm bit of"
                     u" esp masters to convert them to/from esm state. Useful"
                     u" for building/analyzing esp mastered mods.")
         if not self._askContinue(message, 'bash.flipMasters.continue'): return
-        fileName= self.fileName
-        fileInfo = self.fileInfo
-        updated = [fileName]
-        espMasters = [GPath(master) for master in fileInfo.header.masters
-            if bosh.reEspExt.search(master.s)]
-        for masterPath in espMasters:
+        updated = [self.fileName]
+        for masterPath in self.espMasters:
             masterInfo = bosh.modInfos.get(masterPath,None)
             if masterInfo:
                 masterInfo.header.flags1.esm = self.toEsm
                 masterInfo.writeHeader()
                 updated.append(masterPath)
-        self.window.RefreshUI(files=updated, details=fileName)
+        bosh.modInfos.refresh(doInfos=False) # esms will be moved to the top -
+        # note that modification times won't change - so mods will revert to
+        # their original position once back to esp from esm (Oblivion etc)
+        self.window.RefreshUI(files=updated, refreshSaves=True) # True as LO
+        # will change as esms will be moved to the top
+        self.window.SelectItem(self.fileName) # refresh details for fileName
 
 #------------------------------------------------------------------------------
 class Mod_SetVersion(OneItemLink):

Mopy/bash/bosh.py
@@ -3559,7 +3559,8 @@ class ModInfo(FileInfo):
     def sameAs(self, fileInfo):
         try:
             return FileInfo.sameAs(self, fileInfo) and (
-                self.isGhost == fileInfo.isGhost)
+                self.isGhost == fileInfo.isGhost and
+                self.isEsp() == fileInfo.isEsp()) # update for reversed mod
         except AttributeError: #fileInfo has no isGhost attribute - not ModInfo
             return False
 

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -577,8 +577,17 @@
         <tr><td colspan="2">Add Master...<td>Add a master to the list of masters for the selected plugin. <b>Warning:</b> For advanced mod authors only. You should only use this if you know exactly what you're doing.
         <tr><td colspan="2" id="modsCopyToEsm">Copy To Esm/Esp<td>Creates a .esp copy of a .esm or vice versa.
         <tr><td colspan="2" id="modsDecompileAll">Decompile All<td>Undoes the effect of a <q>Compile All</q> performed in the Construction Set. It will not do anything to the game's master file.
-        <tr><td colspan="2" id="modsEsmifySelf">Esmify/Espify Self<td>The distinction between esp and esm plugins is not dependent on their file extensions, but by a bit in the file. This command toggles that bit so that .esp files behave as .esm files and vice versa. It is useful for creating and editing plugins with .esp files as masters, which cannot be done in the Construction Set otherwise. Esm files cannot be edited in the Construction Set as esp files, so this command is disabled for them. <b>Warning:</b> Make sure to toggle any plugins back before playing the game, otherwise savegames will get confused, possibly causing bugs and corruption.
-        <tr><td colspan="2" id="modsEsmifyMasters">Esmify/Espify Masters<td>This works as <a href="#modsEsmifySelf">Esmify/Espify Self</a> does, but toggles a plugin's masters rather than the plugin itself.
+        <tr><td colspan="2" id="modsEsmifySelf">Esmify/Espify Self<td>The distinction between esp and esm plugins is not dependent on their file
+            extensions, but on a bit in the file. This command toggles this bit so that .esp files behave as .esm files and vice versa. It is
+            useful for creating and editing plugins with .esp files as masters, which cannot be done in the Construction Set otherwise.
+            Esm files cannot be edited in the Construction Set as esp files, so this command is disabled for them. NB: the file extension won't change
+            and this is used by Bash so do not mess with it. <b>Warning:</b> Make sure to toggle any plugins back before playing the game, otherwise
+            savegames will get confused, possibly causing bugs and corruption. To toggle the masters back use the same command on the plugin which
+            should now be named Espify Self.
+        <tr><td colspan="2" id="modsEsmifyMasters">Esmify/Espify Masters<td>This works as <a href="#modsEsmifySelf">Esmify/Espify Self</a> does,
+            but toggles a plugin's masters rather than the plugin itself. The load order of plugins will change to reflect their new status but
+            since the modification time won't change they will revert back to their original positions for modification time based load orders.
+            Be sure to backup your load order for Skyrim.
         <tr><td colspan="2" id="modsVersion0.8">Version 0.8<td>Resets the file version number of the selected plugin so that it can be edited in an older version of the Construction Set, which is required when making certain changes due to bugs in the latest Construction Set. <b>Warning:</b> It is possible that this may cause data loss.
 </table>
 

commit aa29882dbcd90c2a6d7675077aa4a034edeae8bc

Fix for always isUpdated and correct (hell of a slow) update for flips:
Regression introduced in 95533a7ed1c7ea41235c592d39fda2abc97d64c0
Look at FileInfos.refresh():

            isUpdated = not isAdded and not fileInfo.sameAs(oldInfo)

Basically fileInfo.isEsp() would always be != oldInfo.isEsp(), the
former being a re.Match instance (cause there was no header) the
latter being computed from the header.
No (?) way to compare the esp status without loading the file - which
is what we try to avoid using the sameAs() call. So we
force refresh load order.

Should not be cheaper to load the header

Mopy/bash/basher/mod_links.py
@@ -1508,6 +1508,7 @@ class Mod_FlipSelf(EnabledLink):
                 return False
         return True
 
+    @balt.conversation
     def Execute(self,event):
         message = (_(u'WARNING! For advanced modders only!')
                    + u'\n\n' +
@@ -1520,10 +1521,10 @@ class Mod_FlipSelf(EnabledLink):
             header = fileInfo.header
             header.flags1.esm = not header.flags1.esm
             fileInfo.writeHeader()
-            #--Repopulate
-            bosh.modInfos.refresh(doInfos=False) ##: why in the loop ?
-        self.window.RefreshUI(files=self.selected, refreshSaves=True) # True,
-        # see Mod_FlipMasters
+        with balt.BusyCursor():
+            bosh.modInfos.plugins.refresh(forceRefresh=True)
+            # refreshSaves=True, see Mod_FlipMasters
+            self.window.RefreshUI(files=self.selected, refreshSaves=True)
 
 #------------------------------------------------------------------------------
 class Mod_FlipMasters(OneItemLink):
@@ -1553,6 +1554,7 @@ class Mod_FlipMasters(OneItemLink):
 
     def _enable(self): return self.enable
 
+    @balt.conversation
     def Execute(self,event):
         message = _(u"WARNING! For advanced modders only! Flips esp/esm bit of"
                     u" esp masters to convert them to/from esm state. Useful"
@@ -1565,11 +1567,12 @@ class Mod_FlipMasters(OneItemLink):
                 masterInfo.header.flags1.esm = self.toEsm
                 masterInfo.writeHeader()
                 updated.append(masterPath)
-        bosh.modInfos.refresh(doInfos=False) # esms will be moved to the top -
-        # note that modification times won't change - so mods will revert to
-        # their original position once back to esp from esm (Oblivion etc)
-        self.window.RefreshUI(files=updated, refreshSaves=True) # True as LO
-        # will change as esms will be moved to the top
+        with balt.BusyCursor():
+            bosh.modInfos.plugins.refresh(forceRefresh=True) # esms will be
+            # moved to the top - note that modification times won't change - so
+            # mods will revert to their original position once back to esp from
+            # esm (Oblivion etc) - refresh saves due to probable LO change
+            self.window.RefreshUI(files=updated, refreshSaves=True)
 
 #------------------------------------------------------------------------------
 class Mod_SetVersion(OneItemLink):

Mopy/bash/bosh.py
@@ -3127,18 +3127,12 @@ class FileInfo(_AFileInfo):
     #--Note that these tests only test extension, not the file data.
     def isMod(self):
         return reModExt.search(self.name.s)
-    def isEsp(self):
-        if not self.isMod(): return False
-        if self.header:
-            return int(self.header.flags1) & 1 == 0
-        else:
-            return reEspExt.search(self.name.s)
     def isEsm(self):
         if not self.isMod(): return False
         if self.header:
             return int(self.header.flags1) & 1 == 1
         else:
-            return reEsmExt.search(self.name.s) and False
+            return bool(reEsmExt.search(self.name.s)) and False
     def isInvertedMod(self):
         """Extension indicates esp/esm, but byte setting indicates opposite."""
         return (self.isMod() and self.header and
@@ -3442,8 +3436,7 @@ class ModInfo(FileInfo):
     def sameAs(self, fileInfo):
         try:
             return FileInfo.sameAs(self, fileInfo) and (
-                self.isGhost == fileInfo.isGhost and
-                self.isEsp() == fileInfo.isEsp()) # update for reversed mod
+                self.isGhost == fileInfo.isGhost)
         except AttributeError: #fileInfo has no isGhost attribute - not ModInfo
             return False
 