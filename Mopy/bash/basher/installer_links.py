wrye-bash/wrye-bash

commit 662d15b04c2354dc4bb1bba8f658c210caab083c

balt.OneItemLink - common behavior + reducing self.data occurrences:
Also added _Mod_Import_Link._parser(self) in mod_links.py
installer_links.py OneItemLink and deleted some now unused InstallerLink
methods

Mopy/bash/basher/installer_links.py
@@ -29,7 +29,7 @@ import wx
 from . import settingDefaults, InstallerProject_OmodConfigDialog, Resources, \
     Installers_Link
 from .. import bosh, bush, balt
-from ..balt import EnabledLink, CheckLink, AppendableLink, Link
+from ..balt import EnabledLink, CheckLink, AppendableLink, Link, OneItemLink
 from ..belt import InstallerWizard, generateTweakLines
 from ..bolt import CancelError, SkipError, GPath, StateError, deprint, \
     SubProgress, UncodedError, LogFile
@@ -66,16 +66,6 @@ class _InstallerLink(Installers_Link, EnabledLink):
                 if isinstance(self.idata[i],bosh.InstallerMarker):
                     return True
         return False
-
-    def isSingle(self):
-        """Indicates whether or not is single installer."""
-        return len(self.selected) == 1
-
-    def isSingleMarker(self):
-        """Indicates whether or not is single installer marker."""
-        if len(self.selected) != 1: return False
-        else: return isinstance(self.idata[self.selected[0]],
-                              bosh.InstallerMarker)
 
     def isSingleProject(self):
         """Indicates whether or not is single project."""
@@ -140,7 +130,7 @@ class Installer_EditWizard(_InstallerLink):
                     # quitting.
                     pass
 
-class Installer_Wizard(_InstallerLink):
+class Installer_Wizard(OneItemLink, _InstallerLink):
     """Runs the install wizard to select subpackages and esp/m filtering"""
     parentWindow = ''
     help = _(u"Run the install wizard.")
@@ -151,8 +141,8 @@ class Installer_Wizard(_InstallerLink):
         self.text = _(u'Auto Wizard') if self.bAuto else _(u'Wizard')
 
     def _enable(self):
-        return self.isSingle() and (self.idata[
-                                        self.selected[0]]).hasWizard != False
+        isSingle = super(Installer_Wizard, self)._enable()
+        return isSingle and (self.idata[self.selected[0]]).hasWizard != False
 
     def Execute(self, event):
         with balt.BusyCursor():
@@ -284,13 +274,14 @@ class Installer_Wizard(_InstallerLink):
             message += u'\n'.join([u' * ' + x[0].stail + u'\n   TO: ' + x[1].s for x in manuallyApply])
             balt.showInfo(self.gTank,message)
 
-class Installer_OpenReadme(_InstallerLink):
+class Installer_OpenReadme(OneItemLink, _InstallerLink):
     """Opens the installer's readme if BAIN can find one"""
     text = _(u'Open Readme')
     help = _(u"Opens the installer's readme.")
 
     def _enable(self):
-        return self.isSingle() and bool(self.idata[self.selected[0]].hasReadme)
+        isSingle = super(Installer_OpenReadme, self)._enable()
+        return isSingle and bool(self.idata[self.selected[0]].hasReadme)
 
     def Execute(self, event):
         installer = self.selected[0]
@@ -333,7 +324,7 @@ class Installer_Delete(_InstallerLink):
     def Execute(self, event): self.gTank.DeleteSelected(shellUI=False,
                                                         noRecycle=False)
 
-class Installer_Duplicate(_InstallerLink):
+class Installer_Duplicate(OneItemLink, _InstallerLink):
     """Duplicate selected Installer."""
     text = _(u'Duplicate...')
 
@@ -342,7 +333,10 @@ class Installer_Duplicate(_InstallerLink):
         self.help = _(u"Duplicate selected %(installername)s.") % (
             {'installername': self.selected[0]})
 
-    def _enable(self): return self.isSingle() and not self.isSingleMarker()
+    def _enable(self):
+        isSingle = super(Installer_Duplicate, self)._enable()
+        return isSingle and not isinstance(self.idata[self.selected[0]],
+                                           bosh.InstallerMarker)
 
     def Execute(self,event):
         """Handle selection."""
@@ -545,7 +539,8 @@ class Installer_ListStructure(_InstallerLink):   # Provided by Waruddar
     text = _(u"List Structure...")
 
     def _enable(self):
-        return self.isSingle() and not isinstance(self.idata[self.selected[0]],
+        isSingle = super(Installer_ListStructure, self)._enable()
+        return isSingle and not isinstance(self.idata[self.selected[0]],
                                                   bosh.InstallerMarker)
 
     def Execute(self,event):

Mopy/bash/balt.py
@@ -2318,6 +2318,13 @@ class EnabledLink(_Link):
         menuItem.Enable(self._enable())
         return menuItem
 
+class OneItemLink(EnabledLink):
+    """Link enabled only when there is one and only one selected item."""
+    # TODO(ut): edit help to add _(u'. Select one item only')
+    def _enable(self):
+        if hasattr(self, 'selected'): return len(self.selected) == 1
+        return len(self.data) == 1
+
 class CheckLink(_Link):
     kind = wx.ITEM_CHECK
 

Mopy/bash/basher/files_links.py
@@ -27,7 +27,7 @@ import time
 from .. import balt, bosh, bush, bolt
 from . import ListBoxes, Resources
 from ..balt import _Link, RadioLink, EnabledLink, AppendableLink, ChoiceLink, \
-    Link
+    Link, OneItemLink
 from ..bolt import CancelError, SkipError, GPath, BoltError
 from ..bosh import formatDate
 
@@ -272,7 +272,7 @@ class File_Hide(_Link):
         #--Refresh stuff
         Link.Frame.RefreshData()
 
-class File_ListMasters(EnabledLink):
+class File_ListMasters(OneItemLink):
     """Copies list of masters to clipboard."""
     text = _(u"List Masters...")
 
@@ -281,8 +281,6 @@ class File_ListMasters(EnabledLink):
         self.help = _(
             u"Copies list of %(filename)s's masters to the clipboard.") % (
                         {'filename': data[0]})
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -401,12 +399,10 @@ class File_Snapshot(_Link):
             #--Copy file
             self.window.data.copy(fileName,destDir,destName)
 
-class File_RevertToSnapshot(EnabledLink):
+class File_RevertToSnapshot(OneItemLink):
     """Revert to Snapshot."""
     text = _(u'Revert to Snapshot...')
     help = _(u"Revert to a previously created snapshot from the Bash/Snapshots dir.")
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         """Handle menu item selection."""

Mopy/bash/basher/ini_links.py
@@ -22,7 +22,7 @@
 #
 # =============================================================================
 from . import ListBoxes, Resources
-from ..balt import _Link, Link, BoolLink, EnabledLink
+from ..balt import _Link, Link, BoolLink, EnabledLink, OneItemLink
 from .. import bosh, balt, bush
 
 #------------------------------------------------------------------------------
@@ -82,7 +82,7 @@ class INI_ListErrors(EnabledLink):
         balt.showLog(self.window,text,_(u'INI Tweak Errors'),asDialog=False,fixedFont=False,icons=Resources.bashBlue)
 
 #------------------------------------------------------------------------------
-class INI_FileOpenOrCopy(EnabledLink):
+class INI_FileOpenOrCopy(OneItemLink):
     """Open specified file(s) only if they aren't Bash supplied defaults."""
     def _initData(self, window, data):
         super(INI_FileOpenOrCopy, self)._initData(window, data)
@@ -95,8 +95,6 @@ class INI_FileOpenOrCopy(EnabledLink):
         else:
             self.text = _(u'Copy...')
             self.help = _(u"Make an editable copy of the default tweak '%s'.") % data[0]
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         """Handle selection."""
@@ -194,7 +192,7 @@ class INI_Apply(EnabledLink):
             iniPanel.tweakContents.RefreshUI(self.data[0])
 
 #------------------------------------------------------------------------------
-class INI_CreateNew(EnabledLink):
+class INI_CreateNew(OneItemLink):
     """Create a new INI Tweak using the settings from the tweak file,
     but values from the target INI."""
     text = _(u'Create Tweak with current settings...')
@@ -210,8 +208,8 @@ class INI_CreateNew(EnabledLink):
                 u"Creates a new tweak based on '%(tweak)s' but with values "
                 u"from '%(ini)s'.") % {'tweak': (data[0]), 'ini': ini}
 
-    def _enable(self):
-        return len(self.data) == 1 and bosh.iniInfos[self.data[0]].status >= 0
+    def _enable(self): return super(INI_CreateNew, self)._enable() and \
+                              bosh.iniInfos[self.data[0]].status >= 0
 
     def Execute(self,event):
         """Handle creating a new INI tweak."""

Mopy/bash/basher/mod_links.py
@@ -28,7 +28,7 @@ import wx # FIXME(ut): wx
 from .. import bosh, bolt, balt, bush
 from ..balt import _Link, Link, textCtrl, toggleButton, vSizer, staticText, \
     spacer, hSizer, button, CheckLink, EnabledLink, AppendableLink, TransLink, \
-    RadioLink, MenuLink, SeparatorLink, ChoiceLink
+    RadioLink, MenuLink, SeparatorLink, ChoiceLink, OneItemLink
 from ..bolt import GPath, SubProgress, AbstractError, CancelError
 from . import  Mod_BaloGroups_Edit, DocBrowser, Resources
 from .constants import ID_GROUPS, JPEG
@@ -44,11 +44,9 @@ docBrowser = None
 #------------------------------------------------------------------------------
 # Mod Links -------------------------------------------------------------------
 #------------------------------------------------------------------------------
-class Mod_FullLoad(EnabledLink):
+class Mod_FullLoad(OneItemLink):
     """Tests all record definitions against a specific mod"""
     text = _(u'Test Full Record Definitions...')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -65,15 +63,15 @@ class Mod_FullLoad(EnabledLink):
 
 # File submenu ----------------------------------------------------------------
 # the rest of the File submenu links come from file_links.py
-class Mod_CreateDummyMasters(EnabledLink):
+class Mod_CreateDummyMasters(OneItemLink):
     """TES4Edit tool, makes dummy plugins for each missing master, for use if looking at a 'Filter' patch."""
     text = _(u'Create Dummy Masters...')
     help = _(u"TES4Edit tool, makes dummy plugins for each missing master of"
              u" the selected mod, for use if looking at a 'Filter' patch")
 
     def _enable(self):
-        return len(self.data) == 1 and bosh.modInfos[self.data[
-            0]].getStatus() == 30  # Missing masters
+        return super(Mod_CreateDummyMasters, self)._enable() and \
+               bosh.modInfos[self.data[0]].getStatus() == 30  # Missing masters
 
     def Execute(self,event):
         """Handle execution."""
@@ -385,12 +383,10 @@ class Mod_Ratings(_Mod_Labels):
 
 # Mod info menus --------------------------------------------------------------
 #------------------------------------------------------------------------------
-class Mod_Details(EnabledLink):
+class Mod_Details(OneItemLink):
     """Show Mod Details"""
     text = _(u'Details...')
     help = _(u'Show Mod Details')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         modName = GPath(self.data[0])
@@ -415,12 +411,10 @@ class Mod_Details(EnabledLink):
                 asDialog=False, fixedFont=True, icons=Resources.bashBlue)
             buff.close()
 
-class Mod_ShowReadme(EnabledLink):
+class Mod_ShowReadme(OneItemLink):
     """Open the readme."""
     text = _(u'Readme...')
     help = _(u'Open the readme')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -1342,11 +1336,9 @@ class Mod_UndeleteRefs(EnabledLink):
 
 # Rest of menu Links ----------------------------------------------------------
 #------------------------------------------------------------------------------
-class Mod_AddMaster(EnabledLink):
+class Mod_AddMaster(OneItemLink):
     """Adds master."""
     text = _(u'Add Master...')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         message = _(u"WARNING! For advanced modders only! Adds specified master to list of masters, thus ceding ownership of new content of this mod to the new master. Useful for splitting mods into esm/esp pairs.")
@@ -1517,7 +1509,7 @@ class Mod_FlipSelf(EnabledLink):
             self.window.RefreshUI(detail=fileInfo.name)
 
 #------------------------------------------------------------------------------
-class Mod_FlipMasters(EnabledLink):
+class Mod_FlipMasters(OneItemLink):
     """Swaps masters between esp and esm versions."""
 
     def _initData(self, window, data):
@@ -1538,8 +1530,8 @@ class Mod_FlipMasters(EnabledLink):
             else:
                 self.toEsm = True
 
-    def _enable(self):
-        return len(self.data) == 1 and len(self.fileInfo.header.masters) > 1
+    def _enable(self): return super(Mod_FlipMasters, self)._enable() and \
+                              len(self.fileInfo.header.masters) > 1
 
     def Execute(self,event):
         message = _(u"WARNING! For advanced modders only! Flips esp/esm bit of"
@@ -1586,15 +1578,14 @@ class Mod_SetVersion(EnabledLink):
 
 #------------------------------------------------------------------------------
 # Import/Export submenus ------------------------------------------------------
-#--Import only ----------------------------------------------------------------
+#------------------------------------------------------------------------------
+#--Import only
 from ..patcher.utilities import FidReplacer, CBash_FidReplacer
 
-class Mod_Fids_Replace(EnabledLink):
+class Mod_Fids_Replace(OneItemLink):
     """Replace fids according to text file."""
     text = _(u'Form IDs...')
     help = _(u'Replace fids according to text file')
-
-    def _enable(self): return len(self.data) == 1
 
     @staticmethod
     def _parser(): return CBash_FidReplacer() if CBash else FidReplacer()
@@ -1631,11 +1622,9 @@ class Mod_Fids_Replace(EnabledLink):
         else:
             balt.showLog(self.window,changed,_(u'Objects Changed'),icons=Resources.bashBlue)
 
-class Mod_Face_Import(EnabledLink):
+class Mod_Face_Import(OneItemLink):
     """Imports a face from a save to an esp."""
     text = _(u'Face...')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--Select source face file
@@ -1664,7 +1653,7 @@ class Mod_Face_Import(EnabledLink):
         self.window.RefreshUI()
         balt.showOk(self.window,_(u'Imported face to: %s') % npc.eid,fileName.s)
 
-#--Common ---------------------------------------------------------------------
+#--Common
 class _Mod_Export_Link(EnabledLink):
 
     def _enable(self): return bool(self.data)
@@ -1695,11 +1684,11 @@ class _Mod_Export_Link(EnabledLink):
 
     def _parser(self): raise AbstractError
 
-class _Mod_Import_Link(EnabledLink):
+class _Mod_Import_Link(OneItemLink):
 
-    def _enable(self): return len(self.data) == 1
+    def _parser(self): raise AbstractError # TODO(ut): class attribute ? initialised once...
 
-#------------------------------------------------------------------------------
+#--Links ----------------------------------------------------------------------
 from ..patcher.utilities import ActorLevels, CBash_ActorLevels
 
 class Mod_ActorLevels_Export(_Mod_Export_Link):
@@ -2543,7 +2532,8 @@ class _Mod_Export_Link_CBash(_Mod_Export_Link):
     def _enable(self): return bool(self.data) and bool(CBash)
 
 class _Mod_Import_Link_CBash(_Mod_Import_Link):
-    def _enable(self): return len(self.data) == 1 and bool(CBash)
+    def _enable(self):
+        return super(_Mod_Import_Link_CBash, self)._enable() and bool(CBash)
 
 #------------------------------------------------------------------------------
 from ..patcher.utilities import CBash_MapMarkers

Mopy/bash/basher/saves_links.py
@@ -29,7 +29,7 @@ import wx # FIXME(ut): wx
 from . import ImportFaceDialog, JPEG, Resources
 from .. import bosh, bolt, balt, bush
 from ..balt import EnabledLink, AppendableLink, Link, CheckLink, ChoiceLink, \
-    _Link, SeparatorLink
+    _Link, SeparatorLink, OneItemLink
 from ..bolt import GPath, ArgumentError, SubProgress, deprint, BoltError
 from ..bosh import formatInteger
 
@@ -219,12 +219,10 @@ class Saves_Profiles(ChoiceLink):
             bosh.modInfos.setOblivionVersion(voNew)
 
 #------------------------------------------------------------------------------
-class Save_LoadMasters(EnabledLink):
+class Save_LoadMasters(OneItemLink):
     """Sets the load list to the save game's masters.""" # TODO(ut): test
     text = _(u'Load Masters')
     help = _(u"Set the load list to the save game's masters")
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -237,12 +235,10 @@ class Save_LoadMasters(EnabledLink):
             balt.showError(self.window,errorMessage,fileName.s)
 
 #------------------------------------------------------------------------------
-class Save_ImportFace(EnabledLink):
+class Save_ImportFace(OneItemLink):
     """Imports a face from another save."""
     text = _(u'Import Face...')
     help = _(u'Import a face from another save')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--File Info
@@ -308,12 +304,10 @@ class Save_RenamePlayer(EnabledLink):
         bosh.saveInfos.refresh()
         self.window.RefreshUI()
 
-class Save_ExportScreenshot(EnabledLink):
+class Save_ExportScreenshot(OneItemLink):
     """Exports the saved screenshot from a save game."""
     text = _(u'Export Screenshot...')
     help = _(u'Export the saved screenshot from a save game')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         saveInfo = bosh.saveInfos[self.data[0]]
@@ -505,7 +499,7 @@ class Save_EditCreatedData(balt.ListEditorData):
             balt.showOk(self.parent, _(u'Names modified: %d.') % count,self.saveFile.fileInfo.name.s)
 
 #------------------------------------------------------------------------------
-class Save_EditCreated(EnabledLink):
+class Save_EditCreated(OneItemLink):
     """Allows user to rename custom items (spells, enchantments, etc)."""
     menuNames = {'ENCH':_(u'Rename Enchanted...'),
                  'SPEL':_(u'Rename Spells...'),
@@ -520,8 +514,6 @@ class Save_EditCreated(EnabledLink):
         super(Save_EditCreated, self).__init__()
         self.type = type
         self.menuName = self.text = Save_EditCreated.menuNames[self.type]
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         """Handle menu selection."""
@@ -581,12 +573,10 @@ class Save_EditPCSpellsData(balt.ListEditorData):
         self.saveSpells.removePlayerSpells(self.removed)
 
 #------------------------------------------------------------------------------
-class Save_EditPCSpells(EnabledLink):
+class Save_EditPCSpells(OneItemLink):
     """Save spell list editing dialog."""
     text = _(u'Delete Spells...')
     # TODO(ut): help + AppendtoMenu had : """Append ref replacer items to menu."""
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -595,12 +585,10 @@ class Save_EditPCSpells(EnabledLink):
         balt.ListEditor.Display(self.window, _(u'Player Spells'), data)
 
 #------------------------------------------------------------------------------
-class Save_EditCreatedEnchantmentCosts(EnabledLink):
+class Save_EditCreatedEnchantmentCosts(OneItemLink):
     """Dialogue and Menu for setting number of uses for Cast When Used Enchantments."""
     text = _(u'Set Number of Uses for Weapon Enchantments...')
     help = _(u'Set number of uses for Cast When Used Enchantments')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -686,13 +674,11 @@ class Save_Move(ChoiceLink):
             balt.showInfo(self.window,_(u'%d files copied to %s.') % (count,profile),_(u'Copy File'))
 
 #------------------------------------------------------------------------------
-class Save_RepairAbomb(EnabledLink):
+class Save_RepairAbomb(OneItemLink):
     """Repairs animation slowing by resetting counter(?) at end of TesClass
     data."""
     text = _(u'Repair Abomb')
     help = _(u'Repair animation slowing')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--File Info
@@ -854,12 +840,10 @@ class Save_RepairFactions(EnabledLink): # CRUFT
         log.out.close()
 
 #------------------------------------------------------------------------------
-class Save_RepairHair(EnabledLink):
+class Save_RepairHair(OneItemLink):
     """Repairs hair that has been zeroed due to removal of a hair mod."""
     text = _(u'Repair Hair')
     help = _(u'Repair hair that has been zeroed due to removal of a hair mod.')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--File Info
@@ -871,12 +855,10 @@ class Save_RepairHair(EnabledLink):
             balt.showOk(self.window,_(u'No repair necessary.'),fileName.s)
 
 #------------------------------------------------------------------------------
-class Save_ReweighPotions(EnabledLink):
+class Save_ReweighPotions(OneItemLink):
     """Changes weight of all player potions to specified value."""
     text = _(u'Reweigh Potions...')
     help = _(u'Change weight of all player potions to specified value')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--Query value
@@ -917,12 +899,10 @@ class Save_ReweighPotions(EnabledLink):
                 balt.showOk(self.window,_(u'No potions to reweigh!'),fileName.s)
 
 #------------------------------------------------------------------------------
-class Save_Stats(EnabledLink):
+class Save_Stats(OneItemLink):
     """Show savefile statistics."""
     text = _(u'Statistics')
     help = _(u'Show savefile statistics')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         fileName = GPath(self.data[0])
@@ -965,13 +945,11 @@ class Save_StatObse(AppendableLink, EnabledLink):
                      fixedFont=False, icons=Resources.bashBlue)
 
 #------------------------------------------------------------------------------
-class Save_Unbloat(EnabledLink):
+class Save_Unbloat(OneItemLink):
     """Unbloats savegame."""
     text = _(u'Remove Bloat...')
     help = _(u'Unbloat savegame. Experimental ! Back up your saves before'
              u' using it on them')
-
-    def _enable(self): return len(self.data) == 1
 
     def Execute(self,event):
         #--File Info

commit 26602397d9b3f45d5496115aba76d62c7629f3b9

Regressions in Installer_ListStructure:
Installer_ListStructure regression introduced in
662d15b04c2354dc4bb1bba8f658c210caab083c - it should inherit from
OneItemLink.
Another regression introduced in 708cd1d30276dcff4accbfd842a52b817a967f7f:
in InstallerProject.listSource() the body of the loop was indented out,
effectively breaking ListPackages for projects

Took the opportunity for some wrapping and adding Installer.reList
(apparently re.U does not make a difference, see
http://stackoverflow.com/q/21398251/281545) - also removed couple
unused variables

Mopy/bash/basher/installer_links.py
@@ -597,7 +597,7 @@ class Installer_Install(_InstallerLink):
             self.iPanel.RefreshUIMods()
             Link.Frame.RefreshData()
 
-class Installer_ListStructure(_InstallerLink):   # Provided by Waruddar
+class Installer_ListStructure(OneItemLink, _InstallerLink): # Provided by Waruddar
     """Copies folder structure of installer to clipboard."""
     text = _(u"List Structure...")
 

Mopy/bash/bosh.py
@@ -5817,8 +5817,13 @@ class Installer(object):
     docDirs = {u'screenshots'}
     dataDirsMinus = {u'bash', u'replacers',
                      u'--'}  #--Will be skipped even if hasExtraData == True.
-    reDataFile = re.compile(ur'(masterlist.txt|dlclist.txt|\.(esp|esm|bsa|ini))$',re.I|re.U)
-    reReadMe = re.compile(ur'^.*?([^\\]*)(read[ _]?me|lisez[ _]?moi)([^\\]*)\.(txt|rtf|htm|html|doc|odt)$',re.I|re.U)
+    reDataFile = re.compile(
+        ur'(masterlist.txt|dlclist.txt|\.(esp|esm|bsa|ini))$', re.I | re.U)
+    reReadMe = re.compile(
+        ur'^.*?([^\\]*)(read[ _]?me|lisez[ _]?moi)([^\\]*)'
+        ur'\.(txt|rtf|htm|html|doc|odt)$', re.I | re.U)
+    reList = re.compile(
+        u'(Solid|Path|Size|CRC|Attributes|Method) = (.*?)(?:\r\n|\n)')
     skipExts = {u'.exe', u'.py', u'.pyc', u'.7z', u'.zip', u'.rar', u'.db',
                 u'.ace', u'.tgz', u'.tar', u'.gz', u'.bz2', u'.omod',
                 u'.fomod', u'.tb2', u'.lzma', u'.bsl'}
@@ -7108,8 +7113,7 @@ class InstallerArchive(Installer):
         self.size = archive.size
         #--Get fileSizeCrcs
         fileSizeCrcs = self.fileSizeCrcs = []
-        oldstylefileSizeCrcs = []
-        reList = re.compile(u'(Solid|Path|Size|CRC|Attributes|Method) = (.*?)(?:\r\n|\n)',re.U)
+        reList = Installer.reList
         file = size = crc = isdir = 0
         self.isSolid = False
         with archive.unicodeSafe() as tempArch:
@@ -7296,9 +7300,8 @@ class InstallerArchive(Installer):
             log = bolt.LogFile(out)
             log.setHeader(_(u'Package Structure:'))
             log(u'[spoiler][xml]\n', False)
-            reList = re.compile(u'(Solid|Path|Size|CRC|Attributes|Method) = (.*?)(?:\r\n|\n)')
+            reList = Installer.reList
             file = u''
-            isdir = False
             apath = dirs['installers'].join(archive)
             with apath.unicodeSafe() as tempArch:
                 ins = listArchiveContents(tempArch.s)
@@ -7315,16 +7318,13 @@ class InstallerArchive(Installer):
                             text.append((u'%s' % file, isdir))
                         elif key == u'Method':
                             file = u''
-                            isdir = False
             text.sort()
             #--Output
             for line in text:
                 dir = line[0]
                 isdir = line[1]
-                if isdir:
-                    log(u'  ' * dir.count(os.sep) + os.path.split(dir)[1] + os.sep)
-                else:
-                    log(u'  ' * dir.count(os.sep) + os.path.split(dir)[1])
+                log(u'  ' * dir.count(os.sep) + os.path.split(dir)[1] + (
+                    os.sep if isdir else u''))
             log(u'[/xml][/spoiler]')
             return bolt.winNewLines(log.out.getvalue())
 
@@ -7557,16 +7557,16 @@ class InstallerProject(Installer):
 
     def listSource(self,archive):
         """Returns package structure as text."""
-        def walkPath(dir, depth):
-            for file in os.listdir(dir):
-                path = os.path.join(dir, file)
-            if os.path.isdir(path):
-                log(u' ' * depth + file + u'\\')
-                depth += 2
-                walkPath(path, depth)
-                depth -= 2
-            else:
-                log(u' ' * depth + file)
+        def walkPath(folder, depth):
+            for entry in os.listdir(folder):
+                path = os.path.join(folder, entry)
+                if os.path.isdir(path):
+                    log(u' ' * depth + entry + u'\\')
+                    depth += 2
+                    walkPath(path, depth)
+                    depth -= 2
+                else:
+                    log(u' ' * depth + entry)
         #--Setup
         with sio() as out:
             log = bolt.LogFile(out)