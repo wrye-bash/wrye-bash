wrye-bash/wrye-bash

commit 6e14861267a2fcff27f80aa41cb9332a2874d3cc

Nitpick in preparation of delete API:
bosh delete(dontRecycle -> recycle)
Added delete implementation to tracked file infos

Mopy/bash/bosh.py
@@ -1854,11 +1854,10 @@ class CoSaves:
         self.savePath = savePath
         self.paths = CoSaves.getPaths(savePath)
 
-    def delete(self,askOk=False,dontRecycle=False):
+    def delete(self,askOk=False,recycle=True):
         """Deletes cofiles."""
-        for path in self.paths:
-            if path.exists():
-                balt.shellDelete(path, askOk_=askOk,recycle=not dontRecycle)
+        balt.shellDelete(filter(lambda p: p.exists, self.paths), askOk_=askOk,
+                         recycle=recycle)
 
     def recopy(self,savePath,saveName,pathFunc):
         """Renames/copies cofiles depending on supplied pathFunc."""
@@ -3966,8 +3965,7 @@ class TrackedFileInfos(DataDict):
             fileInfo = self.factory(u'',name)
             filePath = fileInfo.getPath()
             if not filePath.exists(): # untrack
-                self.data.pop(name, None)
-                self.corrupted.pop(name, None)
+                self.delete(name)
                 changed.add(name)
             elif not fileInfo.sameAs(data[name]):
                 errorMsg = fileInfo.getHeaderError()
@@ -3982,6 +3980,10 @@ class TrackedFileInfos(DataDict):
 
     def track(self,fileName):
         self.refreshFile(GPath(fileName))
+
+    def delete(self, name, **kwargs):
+        self.data.pop(name, None)
+        self.corrupted.pop(name, None)
 
     def clear(self):
         self.data = {}
@@ -4103,7 +4105,7 @@ class FileInfos(DataDict):
         fileInfo.madeBackup = False
 
     #--Delete
-    def delete(self,fileName,doRefresh=True,askOk=False,dontRecycle=False):
+    def delete(self, fileName, doRefresh=True, askOk=False, recycle=True):
         """Deletes member file."""
         if not isinstance(fileName,(list,set)):
             fileNames = [fileName]
@@ -4140,7 +4142,7 @@ class FileInfos(DataDict):
         #--Now do actual deletions
         toDelete = [x for x in toDelete if x.exists()]
         try:
-            balt.shellDelete(toDelete, askOk_=askOk,recycle=not dontRecycle)
+            balt.shellDelete(toDelete, askOk_=askOk, recycle=recycle)
         finally:
             #--Table
             for filePath in tableUpdate:
@@ -4800,8 +4802,8 @@ class ModInfos(FileInfos):
         self.refreshInfoLists()
         if isSelected: self.select(newName)
 
-    def delete(self,fileName,doRefresh=True):
-        """Deletes member file."""
+    def delete(self, fileName, doRefresh=True, askOk_=True,recycle=True):
+        """Delete member file."""
         if fileName.s not in bush.game.masterFiles:
             self.unselect(fileName)
             FileInfos.delete(self,fileName,doRefresh)
@@ -5845,14 +5847,14 @@ class ScreensData(DataDict):
         self.data = newData
         return changed
 
-    def delete(self,fileName,askOk=True,dontRecycle=False):
+    def delete(self,fileName,askOk=True,recycle=True):
         """Deletes member file."""
         dirJoin = self.dir.join
         if isinstance(fileName,(list,set)):
             filePath = [dirJoin(file) for file in fileName]
         else:
             filePath = [dirJoin(fileName)]
-        deleted = balt.shellDelete(filePath, askOk_=askOk,recycle=not dontRecycle)
+        deleted = balt.shellDelete(filePath, askOk_=askOk,recycle=recycle)
         if deleted is not None:
             for file in filePath:
                 del self.data[file.tail]
@@ -7756,7 +7758,7 @@ class InstallersData(DataDict):
             self.hasChanged = False
 
     #--Dict Functions -----------------------------------------------------------
-    def delete(self,items,askOk=False,dontRecycle=False):
+    def delete(self,items,askOk=False,recycle=True):
         """Delete multiple installers. Delete entry AND archive file itself."""
         toDelete = []
         markers = []
@@ -7770,7 +7772,7 @@ class InstallersData(DataDict):
         #--Delete
         try:
             for m in markers: del self.data[m]
-            balt.shellDelete(toDelete, askOk_=askOk,recycle=not dontRecycle)
+            balt.shellDelete(toDelete, askOk_=askOk,recycle=recycle)
         finally:
             refresh = bool(markers)
             for item in toDelete:

Mopy/bash/balt.py
@@ -2262,7 +2262,7 @@ class UIList(wx.Panel):
 
     def DeleteItems(self, event=None, items=None,
                     dialogTitle=_(u'Delete Items'), order=True):
-        noRecycle = event.ShiftDown() if event is not None else False
+        recycle = True if event is None else not event.ShiftDown()
         items = self._toDelete(items)
         if not self.__class__._shellUI:
             items = self._promptDelete(items, dialogTitle, order)
@@ -2270,9 +2270,9 @@ class UIList(wx.Panel):
         for i in items:
             try:
                 if not self.__class__._shellUI:
-                    self.data.delete(i) ##: askOk=False,dontRecycle=noRecycle
+                    self.data.delete(i) ##: doRefresh=False, askOk=False, recycle=recycle
                 else:
-                    self.data.delete(items, askOk=True, dontRecycle=noRecycle)
+                    self.data.delete(items, askOk=True, recycle=recycle)
                     break
             except bolt.BoltError as e:
                 showError(self, u'%r' % e)

commit 36463942ea4e42f156b5070b1c9648623b518ab8

Regression - cosaves were not deleted - backups delete fix:
Regression introduced in 6e14861267a2fcff27f80aa41cb9332a2874d3cc
Took the opportunity to fix cosave backups not being deleted - hacky
cause delete (and backups) API is not yet finalized

Closes #224.

Mopy/bash/bosh.py
@@ -1834,8 +1834,8 @@ class CoSaves:
         maSave = CoSaves.reSave.search(savePath.s)
         if maSave: savePath = savePath.root
         first = maSave and maSave.group(1) or u''
-        return tuple(savePath+ext+first
-                     for ext in (u'.pluggy',u'.'+bush.game.se.shortName.lower()))
+        return tuple(savePath + ext + first for ext in
+                     (u'.pluggy', u'.' + bush.game.se.shortName.lower()))
 
     def __init__(self,savePath,saveName=None):
         """Initialize with savePath."""
@@ -1845,7 +1845,14 @@ class CoSaves:
 
     def delete(self, **kwargs): # not a DataDict subclass
         """Delete cofiles."""
-        _delete(filter(lambda p: p.exists, self.paths), **kwargs)
+        paths = filter(lambda pa: pa.exists(), self.paths)
+        #--Backups
+        backBase = kwargs['backupDir']
+        backpaths = filter(lambda b: b.exists(),
+                           (backBase.join(p.tail) for p in paths))
+        backpaths += filter(lambda bf: bf.exists(),
+                            (p + u'f' for p in backpaths))
+        _delete(paths + tuple(backpaths), **kwargs)
 
     def recopy(self,savePath,saveName,pathFunc):
         """Renames/copies cofiles depending on supplied pathFunc."""
@@ -4872,6 +4879,7 @@ class SaveInfos(FileInfos):
         """Deletes savefile and associated pluggy file."""
         FileInfos.delete(self, fileName, **kwargs)
         kwargs['confirm'] = False # ask only on save deletion
+        kwargs['backupDir'] = self.getBashDir().join('Backups')
         CoSaves(self.dir,fileName).delete(**kwargs)
 
     def rename(self,oldName,newName):