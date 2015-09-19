3c3658d9517741e79470bff4a9e2a9d9e9487221
Diana <crazykilla15@gmail.com>
2015-09-19 14:41:16 -0400

    Switch from optparse to argparse:
    Type arguments removed, they default to string and fail if provided as
    string.
    Added comments to mark groups of arguments, improves readability.
    
    Utumno: Use the ignore whitespace switch to see the differences (blurred
    a bit by wrapping)

--- Mopy/bash/barg.py
+++ Mopy/bash/barg.py
@@ -24,132 +24,177 @@
 
 """This module parses the command line that was used to start Wrye Bash."""
 
-import optparse
+import argparse
 
 def parse():
-    parser = optparse.OptionParser()
-    parser.add_option('-g', '--game',
+    """Helper function to define commandline arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--game',
                         action='store',
-                        type='string',
                         default='',
                         dest='gameName',
-                        help='Specifies the game Wrye Bash should try to manage.  Use this argument if more than one game Wrye Bash supports is installed.')
-    pathGroup = optparse.OptionGroup(parser, "Path Arguments",
-                         r"All path arguments must be absolute paths and use either forward slashes (/) or two backward slashes (\\). All of these can also be set in the ini (where  you can also use relative paths) and if set in both cmd line takes precedence.")
-    pathGroup.add_option('-o', '--oblivionPath',
-                        action='store',
-                        type='string',
-                        default='',
-                        dest='oblivionPath',
-                        help='Specifies the game directory (the one containing game\'s exe). Use this argument if Bash is located outside of the game directory, and the --game argument failed to find it.')
-    userPathGroup = optparse.OptionGroup(parser, "'User Directory Arguments",
-                        'These arguments allow you to specify your user directories in several ways.'
-                        ' These are only useful if the regular procedure for getting the user directory fails.'
-                        ' And even in that case, the user is probably better off installing win32com.')
-    userPathGroup.add_option('-p', '--personalPath',
-                        action='store',
-                        type='string',
-                        default='',
-                        dest='personalPath',
-                        help='Specify the user\'s personal directory. (Like "C:\\\\Documents and Settings\\\\Wrye\\\\My Documents\") '
-                             'If you need to set this then you probably need to set -l too')
-    userPathGroup.add_option('-u', '--userPath',
-                        action='store',
-                        type='string',
-                        default='',
-                        dest='userPath',
-                        help='Specify the user profile path. May help if HOMEDRIVE and/or HOMEPATH'
-                             ' are missing from the user\'s environment')
-    userPathGroup.add_option('-l', '--localAppDataPath',
-                        action='store',
-                        type='string',
-                        default='',
-                        dest='localAppDataPath',
-                        help='Specify the user\'s local application data directory.'
-                             'If you need to set this then you probably need to set -p too.')
-    backupGroup = optparse.OptionGroup(parser, "'Backup and Restore Arguments",
-                        'These arguments allow you to do backup and restore settings operations.')
-    backupGroup.add_option('-b', '--backup',
-                        action='store_true',
-                        default=False,
-                        dest='backup',
-                        help='Backup all Bash settings to an archive file before the app launches. Either specify the filepath with  the -f/--filename options or Wrye Bash will prompt the user for the backup file path.')
-    backupGroup.add_option('-r', '--restore',
-                        action='store_true',
-                        default=False,
-                        dest='restore',
-                        help='Backup all Bash settings to an archive file before the app launches. Either specify the filepath with  the -f/--filename options or Wrye Bash will prompt the user for the backup file path.')
-    backupGroup.add_option('-f', '--filename',
-                        action='store',
-                        default='',
-                        dest='filename',
-                        help='The file to use with the -r or -b options. Must end in \'.7z\' and be a valid path and for -r exist and for -b not already exist.')
-    backupGroup.add_option('-q', '--quiet-quit',
-                        action='store_true',
-                        default=False,
-                        dest='quietquit',
-                        help='Close Bash after creating or restoring backup and do not display any prompts or message dialogs.')
+                        help='Specifies the game Wrye Bash should try to '
+                             'manage.  Use this argument if more than one '
+                             'game Wrye Bash supports is installed.')
+    ### Path Group ###
+    pathGroup = parser.add_argument_group("Path Arguments",
+                                          r"All path arguments must be "
+                                          r"absolute paths and use either "
+                                          r"forward slashes (/) or two "
+                                          r"backward slashes (\\). All of "
+                                          r"these can also be set in the ini "
+                                          r"(where  you can also use relative "
+                                          r"paths) and if set in both cmd "
+                                          r"line takes precedence.")
+    pathGroup.add_argument('-o', '--oblivionPath',
+                           action='store',
+                           default='',
+                           dest='oblivionPath',
+                           help='Specifies the game directory (the one '
+                                'containing game\'s exe). Use this argument if '
+                                'Bash is located outside of the game '
+                                'directory, '
+                                'and the --game argument failed to find it.')
+    ### User Path Group ###
+    userPathGroup = parser.add_argument_group("'User Directory Arguments",
+                                              'These arguments allow you to '
+                                              'specify your user directories '
+                                              'in several ways.'
+                                              ' These are only useful if the '
+                                              'regular procedure for getting '
+                                              'the user directory fails.'
+                                              ' And even in that case, '
+                                              'the user is '
+                                              'probably better off installing '
+                                              'win32com.')
+    userPathGroup.add_argument('-p', '--personalPath',
+                               action='store',
+                               default='',
+                               dest='personalPath',
+                               help='Specify the user\'s personal directory. ('
+                                    'Like "C:\\\\Documents and '
+                                    'Settings\\\\Wrye\\\\My Documents\") '
+                                    'If you need to set this then you probably '
+                                    'need to set -l too')
+    userPathGroup.add_argument('-u', '--userPath',
+                               action='store',
+                               default='',
+                               dest='userPath',
+                               help='Specify the user profile path. May help '
+                                    'if HOMEDRIVE and/or HOMEPATH'
+                                    ' are missing from the user\'s environment')
+    userPathGroup.add_argument('-l', '--localAppDataPath',
+                               action='store',
+                               default='',
+                               dest='localAppDataPath',
+                               help='Specify the user\'s local application '
+                                    'data directory.'
+                                    'If you need to set this then you probably '
+                                    'need to set -p too.')
+    ### Backup Group ###
+    backupGroup = parser.add_argument_group("'Backup and Restore Arguments",
+                                            'These arguments allow you to do '
+                                            'backup and restore settings '
+                                            'operations.')
+    backupGroup.add_argument('-b', '--backup',
+                             action='store_true',
+                             default=False,
+                             dest='backup',
+                             help='Backup all Bash settings to an archive file '
+                                  'before the app launches. Either specify the '
+                                  'filepath with  the -f/--filename options or '
+                                  'Wrye Bash will prompt the user for the '
+                                  'backup file path.')
+    backupGroup.add_argument('-r', '--restore',
+                             action='store_true',
+                             default=False,
+                             dest='restore',
+                             help='Backup all Bash settings to an archive file '
+                                  'before the app launches. Either specify the '
+                                  'filepath with  the -f/--filename options or '
+                                  'Wrye Bash will prompt the user for the '
+                                  'backup file path.')
+    backupGroup.add_argument('-f', '--filename',
+                             action='store',
+                             default='',
+                             dest='filename',
+                             help='The file to use with the -r or -b options. '
+                                  'Must end in \'.7z\' and be a valid path and '
+                                  'for -r exist and for -b not already exist.')
+    backupGroup.add_argument('-q', '--quiet-quit',
+                             action='store_true',
+                             default=False,
+                             dest='quietquit',
+                             help='Close Bash after creating or restoring '
+                                  'backup and do not display any prompts or '
+                                  'message dialogs.')
     parser.set_defaults(backup_images=0)
-    backupGroup.add_option('-i', '--include-changed-images',
-                        action='store_const',
-                        const=1,
-                        dest='backup_images',
-                        help='Include changed images from mopy/bash/images in the backup. Include any image(s) from backup file in restore.')
-    backupGroup.add_option('-I', '--include-all-images',
-                        action='store_const',
-                        const=2,
-                        dest='backup_images',
-                        help='Include all images from mopy/bash/images in the backup/restore (if present in backup file).')
-    parser.add_option('-d', '--debug',
+    backupGroup.add_argument('-i', '--include-changed-images',
+                             action='store_const',
+                             const=1,
+                             dest='backup_images',
+                             help='Include changed images from '
+                                  'mopy/bash/images '
+                                  'in the backup. Include any image(s) from '
+                                  'backup file in restore.')
+    backupGroup.add_argument('-I', '--include-all-images',
+                             action='store_const',
+                             const=2,
+                             dest='backup_images',
+                             help='Include all images from mopy/bash/images in '
+                                  'the backup/restore (if present in backup '
+                                  'file).')
+    ### Individual Arguments ###
+    parser.add_argument('-d', '--debug',
                         action='store_true',
                         default=False,
                         dest='debug',
-                        help='Useful if bash is crashing on startup or if you want to print a lot of '
-                             'information (e.g. while developing or debugging).')
+                        help='Useful if bash is crashing on startup or if '
+                             'you want to print a lot of '
+                             'information (e.g. while developing or '
+                             'debugging).')
     parser.set_defaults(mode=0)
-    parser.add_option('-C', '--Cbash-mode',
+    parser.add_argument('-C', '--Cbash-mode',
                         action='store_const',
                         const=2,
                         dest='mode',
-                        help='enables CBash and uses CBash to build bashed patch.')
-    parser.add_option('-P', '--Python-mode',
+                        help='enables CBash and uses CBash to build bashed '
+                             'patch.')
+    parser.add_argument('-P', '--Python-mode',
                         action='store_const',
                         const=1,
                         dest='mode',
-                        help='disables CBash and uses python code to build bashed patch.')
-    parser.add_option('--restarting',
+                        help='disables CBash and uses python code to build '
+                             'bashed patch.')
+    parser.add_argument('--restarting',
                         action='store_true',
                         default=False,
                         dest='restarting',
-                        help=optparse.SUPPRESS_HELP)
-    parser.add_option('--no-uac',
+                        help=argparse.SUPPRESS)
+    parser.add_argument('--no-uac',
                         action='store_true',
                         default=False,
                         dest='noUac',
-                        help='suppress the prompt to restart in admin mode when UAC is detected.')
-    parser.add_option('--uac',
+                        help='suppress the prompt to restart in admin mode '
+                             'when UAC is detected.')
+    parser.add_argument('--uac',
                         action='store_true',
                         default=False,
                         dest='uac',
-                        help='always start in admin mode if UAC protection is detected.')
-    parser.add_option('--bashmon',
+                        help='always start in admin mode if UAC protection is '
+                             'detected.')
+    parser.add_argument('--bashmon',
                         action='store_true',
-                        help=optparse.SUPPRESS_HELP)
-    parser.add_option('--genHtml',
+                        help=argparse.SUPPRESS)
+    parser.add_argument('--genHtml',
                         default=None,
-                        help=optparse.SUPPRESS_HELP)
-    parser.add_option('-L', '--Language',
+                        help=argparse.SUPPRESS)
+    parser.add_argument('-L', '--Language',
                         action='store',
-                        type='string',
                         default='',
                         dest='language',
-                        help='Specify the user language overriding the system language settings.')
+                        help='Specify the user language overriding the system '
+                             'language settings.')
 
-    parser.add_option_group(pathGroup)
-    parser.add_option_group(userPathGroup)
-    parser.add_option_group(backupGroup)
-
-    opts,extra = parser.parse_args()
-    if len(extra) > 0:
-        parser.print_help()
-    return opts, extra
+    args = parser.parse_args()
+    return args
