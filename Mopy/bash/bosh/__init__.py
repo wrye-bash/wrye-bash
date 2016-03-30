wrye-bash/wrye-bash

commit d69cc8d440f66df9c396dac6a3a1a28219df375c

All doc extensions are checked for readme

Mopy/bash/bosh/__init__.py
@@ -4910,9 +4910,12 @@ class Installer(object):
                      u'--'}  #--Will be skipped even if hasExtraData == True.
     reDataFile = re.compile(
         ur'(masterlist.txt|dlclist.txt|\.(esp|esm|bsa|ini))$', re.I | re.U)
+    docExts = {u'.txt', u'.rtf', u'.htm', u'.html', u'.doc', u'.docx', u'.odt',
+               u'.mht', u'.pdf', u'.css', u'.xls', u'.xlsx', u'.ods', u'.odp',
+               u'.ppt', u'.pptx'}
     reReadMe = re.compile(
         ur'^.*?([^\\]*)(read[ _]?me|lisez[ _]?moi)([^\\]*)'
-        ur'\.(txt|rtf|htm|html|doc|odt)$', re.I | re.U)
+        ur'\.(' +ur'|'.join(docExts) + ur')$', re.I | re.U)
     reList = re.compile(
         u'(Solid|Path|Size|CRC|Attributes|Method) = (.*?)(?:\r\n|\n)')
     reValidNamePattern = re.compile(ur'^([^/\\:*?"<>|]+?)(\d*)$', re.I | re.U)
@@ -4920,9 +4923,6 @@ class Installer(object):
                 u'.ace', u'.tgz', u'.tar', u'.gz', u'.bz2', u'.omod',
                 u'.fomod', u'.tb2', u'.lzma', u'.manifest'}
     skipExts.update(set(readExts))
-    docExts = {u'.txt', u'.rtf', u'.htm', u'.html', u'.doc', u'.docx', u'.odt',
-               u'.mht', u'.pdf', u'.css', u'.xls', u'.xlsx', u'.ods', u'.odp',
-               u'.ppt', u'.pptx'}
     imageExts = {u'.gif', u'.jpg', u'.png', u'.jpeg', u'.bmp'}
     scriptExts = {u'.txt', u'.ini', u'.cfg'}
     commonlyEditedExts = scriptExts | {u'.xml'}

commit f4c563ee14d44e02c4e601d262a3d8f0146c3297

Regression - readme detection was borged:
Introduced in d69cc8d440f66df9c396dac6a3a1a28219df375c

Mopy/bash/bosh/__init__.py
@@ -4486,7 +4486,7 @@ class Installer(object):
                u'.ppt', u'.pptx'}
     reReadMe = re.compile(
         ur'^.*?([^\\]*)(read[ _]?me|lisez[ _]?moi)([^\\]*)'
-        ur'\.(' +ur'|'.join(docExts) + ur')$', re.I | re.U)
+        ur'(' +ur'|'.join(docExts) + ur')$', re.I | re.U)
     reList = re.compile(
         u'(Solid|Path|Size|CRC|Attributes|Method) = (.*?)(?:\r\n|\n)')
     reValidNamePattern = re.compile(ur'^([^/\\:*?"<>|]+?)(\d*)$', re.I | re.U)