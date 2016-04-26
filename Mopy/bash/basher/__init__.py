wrye-bash/wrye-bash

commit 9d286e1b47017fc4842fbdb96e2196f6df7c5e2b

WIP - idata.sorted_pairs():
Note how cleaner the code is including some ugly wrapping gone
Inlined some stuff in installers conflicts that should make no diff in
performance

TODO: remaining uses

Mopy/bash/basher/__init__.py
@@ -2458,9 +2458,10 @@ class InstallersList(balt.UIList):
                                abort=abort) as progress:
                 progress.setFull(len(toRefresh))
                 dest = set() # installer's destination paths rel to Data/
-                for index, (name, installer) in enumerate(sorted(toRefresh, key=lambda tup: tup[1].order)):
-                    progress(index,
-                             _(u'Refreshing Packages...') + u'\n' + name.s)
+                for index, (name, installer) in enumerate(
+                        self.data.sorted_pairs(toRefresh)):
+                    progress(index, _(u'Refreshing Packages...') + u'\n%s' +
+                                    name.s)
                     apath = bass.dirs['installers'].join(name)
                     dest.update(installer.refreshBasic(apath,
                         SubProgress(progress, index, index + 1),

Mopy/bash/bosh/__init__.py
@@ -6382,14 +6382,14 @@ class InstallersData(DataDict):
     #--Operations -------------------------------------------------------------
     def moveArchives(self,moveList,newPos):
         """Move specified archives to specified position."""
-        moveSet = set(moveList)
-        data = self.data
-        orderKey = lambda p: data[p].order
-        newList = [x for x in sorted(data,key=orderKey) if x not in moveSet]
-        moveList.sort(key=orderKey)
-        newList[newPos:newPos] = moveList
-        for index,archive in enumerate(newList):
-            data[archive].order = index
+        old_ordered = self.sorted_pairs(set(self.data) - set(moveList))
+        new_ordered = self.sorted_pairs(moveList)
+        for index,(archive,installer) in enumerate(old_ordered[:newPos]):
+            installer.order = index
+        for index,(archive,installer) in enumerate(new_ordered):
+            installer.order = newPos + index
+        for index,(archive,installer) in enumerate(old_ordered[newPos:]):
+            installer.order = newPos + len(new_ordered) + index
         self.setChanged()
 
     @staticmethod
@@ -6497,10 +6497,9 @@ class InstallersData(DataDict):
                     mask |= set(installer.data_sizeCrc)
         #--Install archives in turn
         progress.setFull(len(archives))
-        archives.sort(key=lambda x: self[x].order,reverse=True)
-        for index,archive in enumerate(archives):
+        for index, (archive, installer) in enumerate(
+                self.sorted_pairs(archives, reverse=True)):
             progress(index,archive.s)
-            installer = self[archive]
             destFiles = set(installer.data_sizeCrc) - mask
             if not override:
                 destFiles &= installer.missingFiles
@@ -6517,6 +6516,16 @@ class InstallersData(DataDict):
         if tweaksCreated:
             self._editTweaks(tweaksCreated)
         return tweaksCreated
+
+    def sorted_pairs(self, package_keys=None, reverse=False):
+        """Return pairs of key, installer for package_keys in self, sorted by
+        install order.
+        :type package_keys: None | collections.Iterable[Path]
+        :rtype: list[(Path, Installer)]
+        """
+        if package_keys is None: package_keys = self.keys()
+        pairs = [(installer, self[installer]) for installer in package_keys]
+        return sorted(pairs, key=lambda tup: tup[1].order, reverse=reverse)
 
     def bain_install(self, archives, refresh_ui, progress=None, last=False,
                      override=True):
@@ -6622,9 +6631,7 @@ class InstallersData(DataDict):
         removes = set()
         restores = {}
         #--March through archives in reverse order...
-        getArchiveOrder =  lambda tup: tup[1].order
-        for archive, installer in sorted(self.iteritems(), key=getArchiveOrder,
-                                         reverse=True):
+        for archive, installer in self.sorted_pairs(reverse=True):
             #--Uninstall archive?
             if archive in unArchives:
                 for data_sizeCrc in (installer.data_sizeCrc,installer.dirty_sizeCrc):
@@ -6649,14 +6656,11 @@ class InstallersData(DataDict):
             self.irefresh(what='NS')
 
     def _restoreFiles(self, restores, progress, refresh_ui):
-        getArchiveOrder = lambda x: self[x].order
-        restoreArchives = sorted(set(restores.itervalues()),
-                                 key=getArchiveOrder, reverse=True)
+        restoreArchives = self.sorted_pairs(restores.itervalues(),reverse=True)
         if not restoreArchives: return
         progress.setFull(len(restoreArchives))
-        for index, archive in enumerate(restoreArchives):
+        for index, (archive, installer) in enumerate(restoreArchives):
             progress(index, archive.s)
-            installer = self[archive]
             destFiles = set(x for x, y in restores.iteritems() if y == archive)
             if destFiles:
                 installer.install(archive, destFiles, self.data_sizeCrcDate,
@@ -6684,9 +6688,7 @@ class InstallersData(DataDict):
             installer.dirty_sizeCrc.clear()
         #--March through packages in reverse order...
         restores = {}
-        getArchiveOrder =  lambda tup: tup[1].order
-        for archive, installer in sorted(self.iteritems(), key=getArchiveOrder,
-                                         reverse=True):
+        for archive, installer in self.sorted_pairs(reverse=True):
             #--Other active package. May provide a restore file.
             #  And/or may block later uninstalls.
             if installer.isActive:
@@ -6808,8 +6810,7 @@ class InstallersData(DataDict):
                 if curConflicts: bsaConflicts.append((package, bsaPath, curConflicts))
 #        print("BSA Conflicts: {}".format(bsaConflicts))
         # Calculate esp/esm conflicts
-        getArchiveOrder = lambda tup: tup[1].order
-        for package, installer in sorted(self.iteritems(),key=getArchiveOrder):
+        for package, installer in self.sorted_pairs():
             if installer.order == srcOrder: continue
             if not showInactive and not installer.isActive: continue
             if not showLower and installer.order < srcOrder: continue
@@ -6865,18 +6866,18 @@ class InstallersData(DataDict):
         with sio() as out:
             log = bolt.LogFile(out)
             log.setHeader(_(u'Bain Packages:'))
-            orderKey = lambda x: self.data[x].order
-            allPackages = sorted(self.data,key=orderKey)
             #--List
             log(u'[spoiler][xml]\n',False)
-            for package in allPackages:
-                prefix = u'%03d' % self.data[package].order
-                if isinstance(self.data[package],InstallerMarker):
-                    log(u'%s - %s' % (prefix,package.s))
-                elif self.data[package].isActive:
-                    log(u'++ %s - %s (%08X) (Installed)' % (prefix,package.s,self.data[package].crc))
+            for package, installer in self.sorted_pairs():
+                prefix = u'%03d' % installer.order
+                if isinstance(installer, InstallerMarker):
+                    log(u'%s - %s' % (prefix, package.s))
+                elif installer.isActive:
+                    log(u'++ %s - %s (%08X) (Installed)' % (
+                        prefix, package.s, installer.crc))
                 elif showInactive:
-                    log(u'-- %s - %s (%08X) (Not Installed)' % (prefix,package.s,self.data[package].crc))
+                    log(u'-- %s - %s (%08X) (Not Installed)' % (
+                        prefix, package.s, installer.crc))
             log(u'[/xml][/spoiler]')
             return bolt.winNewLines(log.out.getvalue())
 

Mopy/bash/basher/installer_links.py
@@ -600,6 +600,7 @@ class Installer_ListStructure(OneItemLink, _InstallerLink): # Provided by Warudd
         return isSingle and not isinstance(self.idata[self.selected[0]],
                                                   bosh.InstallerMarker)
 
+    @balt.conversation
     def Execute(self):
         archive = self.selected[0]
         installer = self.idata[archive]
@@ -613,6 +614,7 @@ class Installer_Move(_InstallerLink):
     """Moves selected installers to desired spot."""
     text = _(u'Move To...')
 
+    @balt.conversation
     def Execute(self):
         curPos = min(self.idata[x].order for x in self.selected)
         message = (_(u'Move selected archives to what position?') + u'\n' +
@@ -726,8 +728,7 @@ class Installer_Refresh(_InstallerLink):
 
     @balt.conversation
     def Execute(self):
-        toRefresh = set((x, self.idata[x]) for x in self.selected)
-        self.window.rescanInstallers(toRefresh, abort=True,
+        self.window.rescanInstallers(self.selected, abort=True,
                             calculate_projects_crc=self.calculate_projects_crc)
 
 class Installer_SkipVoices(CheckLink, _RefreshingLink):
@@ -777,12 +778,10 @@ class Installer_CopyConflicts(_SingleInstallable):
     def Execute(self):
         """Copy files that conflict with this installer from all other
         installers to a project."""
-        idata = self.idata
-        installers_dir = idata.dir
         srcConflicts = set()
         packConflicts = []
         srcArchive = self.selected[0]
-        srcInstaller = idata[srcArchive]
+        srcInstaller = self.idata[srcArchive]
         src_sizeCrc = srcInstaller.data_sizeCrc # dictionary Path -> (int, int)
         all_files = set(src_sizeCrc) # bolt.PathS of ALL installer's files
         if not all_files:
@@ -791,9 +790,7 @@ class Installer_CopyConflicts(_SingleInstallable):
         with balt.BusyCursor():
             numFiles = 0
             destDir = GPath(u"%03d - Conflicts" % srcInstaller.order)
-            getArchiveOrder = lambda tup: tup[1].order
-            for package, installer in sorted(idata.items(),
-                                             key=getArchiveOrder):
+            for package, installer in self.idata.sorted_pairs():
                 curConflicts = set()
                 for z, y in installer.refreshDataSizeCrc().iteritems():
                     if z in all_files and installer.data_sizeCrc[z] != \
@@ -810,12 +807,13 @@ class Installer_CopyConflicts(_SingleInstallable):
         if not numFiles:
             self._showOk(_(u'No conflicts detected for %s') % srcArchive)
             return
+        ijoin = self.idata.dir.join
         def _copy_conflicts(curFile):
             inst = self.idata[package]
             if isinstance(inst, bosh.InstallerProject):
                 for src in curConflicts:
-                    srcFull = installers_dir.join(package, src)
-                    destFull = installers_dir.join(destDir, g_path, src)
+                    srcFull = ijoin(package, src)
+                    destFull = ijoin(destDir, g_path, src)
                     if srcFull.exists():
                         progress(curFile, srcArchive.s + u'\n' + _(
                             u'Copying files...') + u'\n' + src)
@@ -825,8 +823,7 @@ class Installer_CopyConflicts(_SingleInstallable):
                 inst.unpackToTemp(package, curConflicts,
                     SubProgress(progress, curFile, curFile + len(curConflicts),
                                 len(curConflicts)))
-                inst.getTempDir().moveTo(
-                    installers_dir.join(destDir, g_path))
+                inst.getTempDir().moveTo(ijoin(destDir, g_path))
                 curFile += len(curConflicts)
             return curFile
         with balt.Progress(_(u"Copying Conflicts..."),
@@ -1027,7 +1024,7 @@ class InstallerArchive_Unpack(AppendableLink, _InstallerLink):
         #--Copy to Build
         with balt.Progress(_(u"Unpacking to Project..."),u'\n'+u' '*60) as progress:
             projects = set()
-            for archive, installer in self._sorted_install_order():
+            for archive, installer in self.idata.sorted_pairs(self.selected):
                 project = archive.root
                 if self.isSingleArchive():
                     result = self._askText(_(u"Unpack %s to Project:") % archive.s,
@@ -1053,11 +1050,6 @@ class InstallerArchive_Unpack(AppendableLink, _InstallerLink):
             self.window.RefreshUI() # all files ? can status of others change ?
             self.window.SelectItemsNoCallback(projects)
             self.window.SelectAndShowItem(project)
-
-    def _sorted_install_order(self):
-        pairs = [(installer, self.idata[installer]) for installer in
-                 self.selected]
-        return sorted(pairs, key=lambda tup: tup[1].order)
 
 #------------------------------------------------------------------------------
 # InstallerProject Links ------------------------------------------------------

Mopy/bash/basher/installers_links.py
@@ -348,9 +348,8 @@ class Installers_AutoRefreshBethsoft(BoolLink, Installers_Link):
                 beth_files = set(GPath(x) for x in bush.game.bethDataFiles)
                 self.idata.update_data_SizeCrcDate(beth_files, progress)
         # Refresh Installers
-        toRefresh = set()
-        for name, installer in self.idata.iteritems():
-            if installer.hasBethFiles: toRefresh.add((name,installer))
+        toRefresh = set(name for name, installer in self.idata.iteritems() if
+                        installer.hasBethFiles)
         self.window.rescanInstallers(toRefresh, abort=False,
                                      update_from_data=False)
 

commit fdbc802aadc6ae354fb9b31b565c6c4a8e720772

Regression (hitting enter in installers/screens dialogs), renames fix:
9d286e1b47017fc4842fbdb96e2196f6df7c5e2b introduced a regression, namely
hitting enter on "Move To" dialog would open the moved installer.
Turned out that what triggered this was unbinding the OnShow event via
the @balt.conversation in Installer_Move.

--------------------- Mopy/bash/basher/installer_links.py ---------------------
index 917a121..2d2337b 100644
@@ -616,3 +616,3 @@ class Installer_Move(_InstallerLink):

-    @balt.conversation
+    @balt.conversation ##: enter key is propagated to installer
     def Execute(self):

Which send me to a goose chase trying to "fix" balt.conversation (notice
the (wrong) comment above):

    def BindRefresh(self, bind=True, __event=wx.EVT_ACTIVATE):
        if bind: self.Unbind(__event)
        self.Bind(__event, self.RefreshData if bind else self._noop)
        if not bind: self.Bind(__event, self._noop)

    def _noop(self, event): pass # event.Skip or Veto won't do

which did nothing to prevent the enter keystroke being propagated.
What solved it was moving the enter handling in OnChar - explanation:
unbinding RefreshData made the operation fast enough for Bash window
to gain focus, so the Key Up event (on hitting enter in the move dialog)
was caught by it and fired its event handler (open the installer) !
Applied the same cure to screnshots, got me rid of rename opening those
(under #259). Yey ! This has tripped me too long - note that it was not
always reproducible (depending on how fast I hit enter...)

Fixed another regression introduced in same commit - passing an order
greater that installers data length would move to last but keep the
order to this large value.

Finally b05821bbc511494ec99131b5e2605ee266482065 made apparent a flaw
in renaming - installer markers would all have their archive set to
u'====' leading to:

Traceback (most recent call last):
  File "bash\balt.py", line 2354, in __Execute
    self.Execute()

Mopy/bash/basher/__init__.py
@@ -2391,6 +2391,8 @@ class InstallersList(balt.UIList):
         elif event.CmdDown() and code == ord('V'):
             ##Ctrl+V
             balt.clipboardDropFiles(10, self.OnDropFiles)
+        # Enter: Open selected installers
+        elif code in balt.wxReturn: self.OpenSelected()
         else:
             event.Skip()
 
@@ -2426,8 +2428,6 @@ class InstallersList(balt.UIList):
             sel = map(lambda x: bass.dirs['installers'].join(x).s,
                       self.GetSelected())
             balt.copyListToClipboard(sel)
-        # Enter: Open selected installers
-        elif code in balt.wxReturn: self.OpenSelected()
         super(InstallersList, self).OnKeyUp(event)
 
     # Installer specific ------------------------------------------------------
@@ -3083,6 +3083,12 @@ class ScreensList(balt.UIList):
             self.SelectItemsNoCallback(newselected)
             event.Veto()
 
+    def OnChar(self,event):
+        # Enter: Open selected screens
+        code = event.GetKeyCode()
+        if code in balt.wxReturn: self.OpenSelected()
+        else: super(ScreensList, self).OnKeyUp(event)
+
     def OnKeyUp(self,event):
         """Char event: Activate selected items, select all items"""
         code = event.GetKeyCode()
@@ -3091,8 +3097,6 @@ class ScreensList(balt.UIList):
             sel = map(lambda x: bosh.screensData.dir.join(x).s,
                       self.GetSelected())
             balt.copyListToClipboard(sel)
-        # Enter: Open selected screens
-        elif code in balt.wxReturn: self.OpenSelected()
         super(ScreensList, self).OnKeyUp(event)
 
 #------------------------------------------------------------------------------

Mopy/bash/bosh/__init__.py
@@ -5393,6 +5393,7 @@ class InstallerMarker(Installer):
         if newName == archive:
             return False
         #--Add the marker to Bash and remove old one
+        installer.archive = newName.s
         data[newName] = installer
         del data[archive]
         return True, False, False
@@ -5923,6 +5924,10 @@ class InstallersData(DataDict):
         self.data = data.get('installers', {})
         self.data_sizeCrcDate = data.get('sizeCrcDate', {})
         self.crc_installer = data.get('crc_installer', {})
+        # fixup: all markers had their archive attribute set to u'===='
+        for key, value in self.iteritems():
+            if isinstance(value, InstallerMarker):
+                value.archive = key.s
         self.loaded = True
         return True
 
@@ -6381,6 +6386,7 @@ class InstallersData(DataDict):
         """Move specified archives to specified position."""
         old_ordered = self.sorted_pairs(set(self.data) - set(moveList))
         new_ordered = self.sorted_pairs(moveList)
+        if newPos >= len(self.keys()): newPos = len(self.keys()) - 1
         for index,(archive,installer) in enumerate(old_ordered[:newPos]):
             installer.order = index
         for index,(archive,installer) in enumerate(new_ordered):