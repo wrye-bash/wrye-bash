wrye-bash/wrye-bash

commit a79c07020d134ac10f86611a050d08f41db22926

Dropping global hacks in loot and libbsa:
Takes 15 warnings for the ride - the yellow kind. With this commit the
only globals hacks that remain are the game/patcher dynamic loading ones
plus the tabs creation one - they are more than enough.
See also: ccd9839b9b319c123c39899c8b58bee498428072

def _uint(const) is really nitpick - could split and squash with
wrapping - did not do finally.

Note:

- using %r in `__repr__` - better safe...
- Mopy/bash/liblo.py: errors had too much in it - no way to get this
kind of locals/globals hacks right really
- I am using del BSAHandle, LibbsaError, LootDb, LootError to avoid
"redeclared without usage" warns. Fragile.

Not final - fragile. All this should turn to normal modules, initialized
explicitly. Init should be some few lines. In the TODO list.

Under #200.

Mopy/bash/loot.py
@@ -32,6 +32,8 @@ from bolt import Path
 
 LootApi = None
 version = None
+loot_needs_cleaning_yes = None
+LootDb, LootError = None, None
 
 # Version of LOOT this Python script is written for.
 PythonAPIVersion = (0,6)
@@ -59,13 +61,14 @@ def Init(path):
         #else:
             path = os.path.join(path,u'loot32.dll')
 
-    global LootApi
+    global LootApi, loot_needs_cleaning_yes, LootDb, LootError
 
     # First unload any LOOT dll previously loaded
-    del LootApi
-    LootApi = None
+    del LootApi, LootDb, LootError
+    LootApi = loot_needs_cleaning_yes = None
 
     if not os.path.exists(path):
+        LootDb, LootError = None, None
         return
 
     try:
@@ -77,7 +80,7 @@ def Init(path):
             handle = LoadLibrary(path)
         LootApi = CDLL(path,handle=handle)
     except Exception as e:
-        LootApi = None
+        LootApi = LootDb = LootError = None
         raise
 
     # Some types
@@ -95,6 +98,9 @@ def Init(path):
     c_bool_p = POINTER(c_bool)
     loot_message_p = POINTER(loot_message)
     loot_message_p_p = POINTER(loot_message_p)
+
+    # helpers
+    def _uint(const): return c_uint.in_dll(LootApi, const).value
 
     # utility unicode functions
     def _uni(x): return u'' if x is None else unicode(x,'utf8')
@@ -126,35 +132,31 @@ def Init(path):
     # =========================================================================
     # API Constants - Return codes
     # =========================================================================
-    errors = {}
-    ErrorCallbacks = {}
-    for name in ['ok',
-                 'error_liblo_error',
-                 'error_file_write_fail',
-                 'error_parse_fail',
-                 'error_condition_eval_fail',
-                 'error_regex_eval_fail',
-                 'error_no_mem',
-                 'error_invalid_args',
-                 'error_no_tag_map',
-                 'error_path_not_found',
-                 'error_no_game_detected',
-                 'error_windows_error',
-                 'error_sorting_error',
-                 ]:
-        name = 'loot_'+name
-        errors[name] = c_uint.in_dll(LootApi,name).value
-        ErrorCallbacks[errors[name]] = None
-    loot_return_max = c_uint.in_dll(LootApi,'loot_return_max').value
-    globals().update(errors)
+    loot_ok = _uint('loot_ok')
+    loot_error_liblo_error = _uint('loot_error_liblo_error')
+    loot_error_file_write_fail = _uint('loot_error_file_write_fail')
+    loot_error_parse_fail = _uint('loot_error_parse_fail')
+    loot_error_condition_eval_fail = _uint('loot_error_condition_eval_fail')
+    loot_error_regex_eval_fail = _uint('loot_error_regex_eval_fail')
+    loot_error_no_mem = _uint('loot_error_no_mem')
+    loot_error_invalid_args = _uint('loot_error_invalid_args')
+    loot_error_no_tag_map = _uint('loot_error_no_tag_map')
+    loot_error_path_not_found = _uint('loot_error_path_not_found')
+    loot_error_no_game_detected = _uint('loot_error_no_game_detected')
+    loot_error_windows_error = _uint('loot_error_windows_error')
+    loot_error_sorting_error = _uint('loot_error_sorting_error')
+
+    errors = dict((name, value) for name, value in locals().iteritems() if
+                  name.startswith('loot_error_'))
+    loot_return_max = _uint('loot_return_max')
 
     # =========================================================================
     # API Constants - Games
     # =========================================================================
-    loot_game_tes4 = c_uint.in_dll(LootApi,'loot_game_tes4').value
-    loot_game_tes5 = c_uint.in_dll(LootApi,'loot_game_tes5').value
-    loot_game_fo3 = c_uint.in_dll(LootApi,'loot_game_fo3').value
-    loot_game_fonv = c_uint.in_dll(LootApi,'loot_game_fonv').value
+    loot_game_tes4 = _uint('loot_game_tes4')
+    loot_game_tes5 = _uint('loot_game_tes5')
+    loot_game_fo3 = _uint('loot_game_fo3')
+    loot_game_fonv = _uint('loot_game_fonv')
     games = {
         'Oblivion':loot_game_tes4,
         loot_game_tes4:loot_game_tes4,
@@ -169,22 +171,22 @@ def Init(path):
     # =========================================================================
     # API Constants - Message Types
     # =========================================================================
-    loot_message_say = c_uint.in_dll(LootApi,'loot_message_say').value
-    loot_message_warn = c_uint.in_dll(LootApi,'loot_message_warn').value
-    loot_message_error = c_uint.in_dll(LootApi,'loot_message_error').value
+    loot_message_say = _uint('loot_message_say')
+    loot_message_warn = _uint('loot_message_warn')
+    loot_message_error = _uint('loot_message_error')
 
     # =========================================================================
     # API Constants - Languages
     # =========================================================================
-    loot_lang_any = c_uint.in_dll(LootApi,'loot_lang_any').value
+    loot_lang_any = _uint('loot_lang_any')
     # Other language constants are unused by Bash, so omitted here.
 
     # =========================================================================
     # API Constants - Cleanliness
     # =========================================================================
-    loot_needs_cleaning_no = c_uint.in_dll(LootApi,'loot_needs_cleaning_no').value
-    loot_needs_cleaning_yes = c_uint.in_dll(LootApi,'loot_needs_cleaning_yes').value
-    loot_needs_cleaning_unknown = c_uint.in_dll(LootApi,'loot_needs_cleaning_unknown').value
+    loot_needs_cleaning_no = _uint('loot_needs_cleaning_no')
+    loot_needs_cleaning_yes = _uint('loot_needs_cleaning_yes')
+    loot_needs_cleaning_unknown = _uint('loot_needs_cleaning_unknown')
 
     # =========================================================================
     # API Functions - Error Handling
@@ -200,19 +202,14 @@ def Init(path):
             raise Exception(u'An error occurred while getting the details of a LOOT API error: %i' % ret)
         return unicode(details.value if details.value else 'None', 'utf8')
 
-    def RegisterCallback(errorCode,callback):
-        """Used to setup callback functions for whenever specific error codes
-           are encountered"""
-        ErrorCallbacks[errorCode] = callback
-
     class LootError(Exception):
         def __init__(self,value):
             self.code = value
-            msg = 'UNKNOWN(%i)' % value
-            for code in errors:
-                if errors[code] == value:
-                    msg = code
+            for errorName, errorCode in errors.iteritems():
+                if errorCode == value:
+                    msg = errorName
                     break
+            else: msg = 'UNKNOWN(%i)' % value
             msg += ':'
             try:
                 msg += GetLastErrorDetails()
@@ -221,12 +218,10 @@ def Init(path):
             self.msg = msg
             Exception.__init__(self,msg)
 
-        def __repr__(self): return '<LootError: %s>' % self.msg
+        def __repr__(self): return '<LootError: %r>' % self.msg
         def __str__(self): return 'LootError: %s' % self.msg
 
     def LootErrorCheck(result):
-        callback = ErrorCallbacks.get(result,None)
-        if callback: callback()
         if result == loot_ok: return result
         elif DebugLevel > 0:
             print GetLastErrorDetails()
@@ -393,13 +388,10 @@ def Init(path):
                code"""
             return [x for x in plugins if self.GetDirtyMessage(x)[1] == cleanCode]
 
-    # Put the locally defined functions, classes, etc into the module global namespace
-    globals().update(locals())
-
 # Initialize the LOOT API, assuming that loot32.dll and loot64.dll are in
 # the same directory. Call Init again with the path to these dll's if this
 # assumption is incorrect. LootApi will be None if this is the case.
 try:
     Init(os.getcwdu())
 except LootVersionError:
-    pass
+    LootDb, LootError = None, None

Mopy/bash/libbsa.py
@@ -34,6 +34,7 @@ from bolt import Path, GPath
 
 libbsa = None
 version = None
+BSAHandle, LibbsaError = None, None
 
 # Version of libbsa this Python script is written for.
 PythonLibbsaVersion = (2,0)
@@ -61,13 +62,14 @@ def Init(path):
         else:
             path = os.path.join(path,u'libbsa32.dll')
 
-    global libbsa
+    global libbsa, BSAHandle, LibbsaError
 
     # First unload any libbsa dll previously loaded
-    del libbsa
+    del libbsa, BSAHandle, LibbsaError
     libbsa = None
 
     if not os.path.exists(path):
+        BSAHandle, LibbsaError = None, None
         return
 
     try:
@@ -79,7 +81,7 @@ def Init(path):
             handle = LoadLibrary(path)
         libbsa = CDLL(path,handle=handle)
     except Exception as e:
-        libbsa = None
+        libbsa = BSAHandle = LibbsaError = None
         raise
 
     # Some types
@@ -97,6 +99,9 @@ def Init(path):
                     ]
     bsa_asset_p = POINTER(bsa_asset)
     bsa_asset_p_p = POINTER(bsa_asset_p)
+
+    # helpers
+    def _uint(const): return c_uint.in_dll(libbsa, const).value
 
     # utility unicode functions
     def _enc(x): return (x.encode('utf8') if isinstance(x,unicode)
@@ -129,9 +134,9 @@ def Init(path):
     # =========================================================================
     # API Constants - BSA Version Flags
     # =========================================================================
-    LIBBSA_VERSION_TES3 = c_uint.in_dll(libbsa,'LIBBSA_VERSION_TES3').value
-    LIBBSA_VERSION_TES4 = c_uint.in_dll(libbsa,'LIBBSA_VERSION_TES4').value
-    LIBBSA_VERSION_TES5 = c_uint.in_dll(libbsa,'LIBBSA_VERSION_TES5').value
+    LIBBSA_VERSION_TES3 = _uint('LIBBSA_VERSION_TES3')
+    LIBBSA_VERSION_TES4 = _uint('LIBBSA_VERSION_TES4')
+    LIBBSA_VERSION_TES5 = _uint('LIBBSA_VERSION_TES5')
     games = {
         'Morrowind':LIBBSA_VERSION_TES3,
         LIBBSA_VERSION_TES3:LIBBSA_VERSION_TES3,
@@ -147,35 +152,30 @@ def Init(path):
     # =========================================================================
     # API Constants - Compression Flags
     # =========================================================================
-    LIBBSA_COMPRESS_LEVEL_0 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_0').value
-    LIBBSA_COMPRESS_LEVEL_1 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_1').value
-    LIBBSA_COMPRESS_LEVEL_2 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_2').value
-    LIBBSA_COMPRESS_LEVEL_3 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_3').value
-    LIBBSA_COMPRESS_LEVEL_4 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_4').value
-    LIBBSA_COMPRESS_LEVEL_5 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_5').value
-    LIBBSA_COMPRESS_LEVEL_6 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_6').value
-    LIBBSA_COMPRESS_LEVEL_7 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_7').value
-    LIBBSA_COMPRESS_LEVEL_8 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_8').value
-    LIBBSA_COMPRESS_LEVEL_9 = c_uint.in_dll(libbsa,'LIBBSA_COMPRESS_LEVEL_9').value
+    LIBBSA_COMPRESS_LEVEL_0 = _uint('LIBBSA_COMPRESS_LEVEL_0')
+    LIBBSA_COMPRESS_LEVEL_1 = _uint('LIBBSA_COMPRESS_LEVEL_1')
+    LIBBSA_COMPRESS_LEVEL_2 = _uint('LIBBSA_COMPRESS_LEVEL_2')
+    LIBBSA_COMPRESS_LEVEL_3 = _uint('LIBBSA_COMPRESS_LEVEL_3')
+    LIBBSA_COMPRESS_LEVEL_4 = _uint('LIBBSA_COMPRESS_LEVEL_4')
+    LIBBSA_COMPRESS_LEVEL_5 = _uint('LIBBSA_COMPRESS_LEVEL_5')
+    LIBBSA_COMPRESS_LEVEL_6 = _uint('LIBBSA_COMPRESS_LEVEL_6')
+    LIBBSA_COMPRESS_LEVEL_7 = _uint('LIBBSA_COMPRESS_LEVEL_7')
+    LIBBSA_COMPRESS_LEVEL_8 = _uint('LIBBSA_COMPRESS_LEVEL_8')
+    LIBBSA_COMPRESS_LEVEL_9 = _uint('LIBBSA_COMPRESS_LEVEL_9')
 
     # =========================================================================
     # API Constants - Return codes
     # =========================================================================
-    errors = {}
-    ErrorCallbacks = {}
-    for name in ['OK',
-                 'ERROR_INVALID_ARGS',
-                 'ERROR_NO_MEM',
-                 'ERROR_FILESYSTEM_ERROR',
-                 'ERROR_BAD_STRING',
-                 'ERROR_ZLIB_ERROR',
-                 'ERROR_PARSE_FAIL',
-                 ]:
-        name = 'LIBBSA_'+name
-        errors[name] = c_uint.in_dll(libbsa,name).value
-        ErrorCallbacks[errors[name]] = None
-    LIBBSA_RETURN_MAX = c_uint.in_dll(libbsa,'LIBBSA_RETURN_MAX').value
-    globals().update(errors)
+    LIBBSA_OK = _uint('LIBBSA_OK')
+    LIBBSA_ERROR_INVALID_ARGS = _uint('LIBBSA_ERROR_INVALID_ARGS')
+    LIBBSA_ERROR_NO_MEM = _uint('LIBBSA_ERROR_NO_MEM')
+    LIBBSA_ERROR_FILESYSTEM_ERROR = _uint('LIBBSA_ERROR_FILESYSTEM_ERROR')
+    LIBBSA_ERROR_BAD_STRING = _uint('LIBBSA_ERROR_BAD_STRING')
+    LIBBSA_ERROR_ZLIB_ERROR = _uint('LIBBSA_ERROR_ZLIB_ERROR')
+    LIBBSA_ERROR_PARSE_FAIL = _uint('LIBBSA_ERROR_PARSE_FAIL')
+    errors = dict((name, value) for name, value in locals().iteritems() if
+                  name.startswith('LIBBSA_ERROR_'))
+    LIBBSA_RETURN_MAX = _uint('LIBBSA_RETURN_MAX')
 
     # =========================================================================
     # API Functions - Error Handling
@@ -191,19 +191,14 @@ def Init(path):
             raise Exception(u'An error occurred while getting the details of a libbsa error: %i' % ret)
         return unicode(details.value if details.value else 'None', 'utf8')
 
-    def RegisterCallback(errorCode,callback):
-        """Used to setup callback functions for whenever specific error codes
-           are encountered"""
-        ErrorCallbacks[errorCode] = callback
-
     class LibbsaError(Exception):
         def __init__(self,value):
             self.code = value
-            msg = 'UNKNOWN(%i)' % value
-            for code in errors:
-                if errors[code] == value:
-                    msg = code
+            for errorName, errorCode in errors.iteritems():
+                if errorCode == value:
+                    msg = errorName
                     break
+            else: msg = 'UNKNOWN(%i)' % value
             msg += ':'
             try:
                 msg += GetLastErrorDetails()
@@ -212,12 +207,10 @@ def Init(path):
             self.msg = msg
             Exception.__init__(self,msg)
 
-        def __repr__(self): return '<LibbsaError: %s>' % self.msg
+        def __repr__(self): return '<LibbsaError: %r>' % self.msg
         def __str__(self): return 'LibbsaError: %s' % self.msg
 
     def LibbsaErrorCheck(result):
-        callback = ErrorCallbacks.get(result,None)
-        if callback: callback()
         if result == LIBBSA_OK: return result
         elif DebugLevel > 0:
             print GetLastErrorDetails()
@@ -346,8 +339,6 @@ def Init(path):
             _Cbsa_extract_asset(self._handle, _enc(assetPath), _enc(destPath),
                                 True)
 
-    # Put the locally defined functions, classes, etc into the module global namespace
-    globals().update(locals())
 
 # Initialize libbsa, assuming that libbsa32.dll and libbsa64.dll are in the
 # same directory. Call Init again with the path to these dll's if this
@@ -355,4 +346,4 @@ def Init(path):
 try:
     Init(os.getcwdu())
 except LibbsaVersionError:
-    pass
+    BSAHandle, LibbsaError = None, None

Mopy/bash/liblo.py
@@ -172,7 +172,7 @@ def Init(path):
     LIBLO_WARN_INVALID_LIST = _uint('LIBLO_WARN_INVALID_LIST')
     LIBLO_ERROR_INVALID_ARGS = _uint('LIBLO_ERROR_INVALID_ARGS')
     errors = dict((name, value) for name, value in locals().iteritems() if
-                  name.startswith('LIBLO_'))
+                  name.startswith(('LIBLO_ERROR_', 'LIBLO_WARN_')))
     LIBLO_RETURN_MAX = _uint('LIBLO_RETURN_MAX')
 
     # =========================================================================

commit 42413eec5fc3058a0ac5686d3f513db4e1cbdcba

Regression - mktaglist.py broken:
Introduced in a79c07020d134ac10f86611a050d08f41db22926
and ae58db071f29d7923b898a1e14542e5aff2bd4cd

Monkey patch - binding to loot needs a complete rewrite

Under #299.

Mopy/bash/loot.py
@@ -43,6 +43,8 @@ DebugLevel = 0
 #  0 - (default) no additional feedback
 #  1 - print information about all return codes found
 
+LOOT_GAME_TES4 = LOOT_GAME_TES5 = LOOT_GAME_FO3 = LOOT_GAME_FONV = None
+
 class LootVersionError(Exception):
     """Exception thrown if the LOOT API loaded is not
        compatible with loot.py"""
@@ -156,6 +158,7 @@ def Init(path):
     # =========================================================================
     # API Constants - Games
     # =========================================================================
+    global LOOT_GAME_TES4, LOOT_GAME_TES5, LOOT_GAME_FO3, LOOT_GAME_FONV
     LOOT_GAME_TES4 = _uint('loot_game_tes4')
     LOOT_GAME_TES5 = _uint('loot_game_tes5')
     LOOT_GAME_FO3 = _uint('loot_game_fo3')

scripts/mktaglist.py
@@ -105,7 +105,7 @@ if oblivionMlist:
     print u'Getting masterlist from %s' % oblivionMlist
     taglistDir = u'../Mopy/Bash Patches/Oblivion/taglist.yaml'
     if os.path.exists(oblivionMlist):
-        lootDb = loot.LootDb(oblivionDir,loot.loot_game_tes4)
+        lootDb = loot.LootDb(oblivionDir,loot.LOOT_GAME_TES4)
         lootDb.PlainLoad(oblivionMlist)
         lootDb.DumpMinimal(taglistDir,True)
         print u'Oblivion masterlist converted.'
@@ -117,7 +117,7 @@ if skyrimMlist:
     print u'Getting masterlist from %s' % skyrimMlist
     taglistDir = u'../Mopy/Bash Patches/Skyrim/taglist.yaml'
     if os.path.exists(skyrimMlist):
-        lootDb = loot.LootDb(skyrimDir,loot.loot_game_tes5)
+        lootDb = loot.LootDb(skyrimDir,loot.LOOT_GAME_TES5)
         lootDb.PlainLoad(skyrimMlist)
         lootDb.DumpMinimal(taglistDir,True)
         print u'Skyrim masterlist converted.'
@@ -129,7 +129,7 @@ if fallout3Mlist:
     print u'Getting masterlist from %s' % fallout3Mlist
     taglistDir = u'../Mopy/Bash Patches/Fallout 3/taglist.yaml'
     if os.path.exists(fallout3Mlist):
-        lootDb = loot.LootDb(fallout3Dir,loot.loot_game_fo3)
+        lootDb = loot.LootDb(fallout3Dir,loot.LOOT_GAME_FO3)
         lootDb.PlainLoad(fallout3Mlist)
         lootDb.DumpMinimal(taglistDir,True)
         print u'Fallout 3 masterlist converted.'
@@ -141,7 +141,7 @@ if falloutNVMlist:
     print u'Getting masterlist from %s' % falloutNVMlist
     taglistDir = u'../Mopy/Bash Patches/Fallout New Vegas/taglist.yaml'
     if os.path.exists(falloutNVMlist):
-        lootDb = loot.LootDb(falloutNVDir,loot.loot_game_fonv)
+        lootDb = loot.LootDb(falloutNVDir,loot.LOOT_GAME_FONV)
         lootDb.PlainLoad(falloutNVMlist)
         lootDb.DumpMinimal(taglistDir,True)
         print u'Fallout New Vegas masterlist converted.'