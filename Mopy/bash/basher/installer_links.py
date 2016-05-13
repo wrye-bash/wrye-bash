wrye-bash/wrye-bash

commit a34b27aea5ab8a726bc5911ee1a26ff9db8a31df

Change! More focused silent skips - WIP:
Now skips top level dirs  fomod etc (as opposed to any path that started
with those) and only thumbs.db/desktop.ini (add more, like osX ?) files
as opposed to any path ending on those (like mydesktop.ini)

TODOS - #219:

- Still unsure about skipping config...
-  should we ignore _silentSkipsEnd to in _find_root_index,
_refreshBasic ?

Using os_sep is under linux #243 - strive for OS independent code.

Contains some last minute changes I hope won't introduce a regression
this time (as last minute changes on tested commits usually do) -
namely bringing os_sep to global scope, which I do not exactly like
but I prefer to other alternatives (namely bringing them to local scope
inside the methods - see: http://stackoverflow.com/q/36178330/281545 ).

Note that in python 2 list comprehension:

 +                     os_sep=os_sep, skips_start=tuple(
 +                [x.replace(os_sep, u'') for x in _silentSkipsStart])):

would work without moving os_sep to global scope but leaks x and it
is not python 3 friendly - I actually initially used a generator and
fell on my face:

http://stackoverflow.com/q/36184247/281545

I do not also like _refreshBasic but I do not want to expose caching
parameters. When I do I signal it by prepending them with _, see
size_or_mtime_changed commit (_lstat).

Mopy/bash/bosh/__init__.py
@@ -4457,6 +4457,7 @@ class ScreensData(DataDict):
     def delete_Refresh(self, deleted): self.refresh()
 
 #------------------------------------------------------------------------------
+os_sep = os.path.sep
 class Installer(object):
     """Object representing an installer archive, its user configuration, and
     its installation state."""
@@ -4474,8 +4475,9 @@ class Installer(object):
     __slots__ = persistent + volatile
     #--Package analysis/porting.
     docDirs = {u'screenshots'}
-    dataDirsMinus = {u'bash',
-                     u'--'}  #--Will be skipped even if hasExtraData == True.
+    #--Will be skipped even if hasExtraData == True (bonus: skipped also on
+    # scanning the game Data directory)
+    dataDirsMinus = {u'bash', u'--'}
     reDataFile = re.compile(
         ur'(masterlist.txt|dlclist.txt|\.(esp|esm|bsa|ini))$', re.I | re.U)
     docExts = {u'.txt', u'.rtf', u'.htm', u'.html', u'.doc', u'.docx', u'.odt',
@@ -4731,8 +4733,10 @@ class Installer(object):
     #--refreshDataSizeCrc, err, framework -------------------------------------
     # Those files/folders will be always skipped by refreshDataSizeCrc()
     _silentSkipsStart = (
-        u'--', u'omod conversion data', u'fomod', u'wizard images')
-    _silentSkipsEnd = (u'thumbs.db', u'desktop.ini', u'config')
+        u'--', u'omod conversion data%s' % os_sep, u'fomod%s' % os_sep,
+        u'wizard images%s' % os_sep)
+    _silentSkipsEnd = (
+        u'%sthumbs.db' % os_sep, u'%sdesktop.ini' % os_sep, u'config')
 
     # global skips that can be overridden en masse by the installer
     _global_skips = []
@@ -5133,7 +5137,8 @@ class Installer(object):
         return dest_src
 
     @staticmethod
-    def _find_root_index(fileSizeCrcs):
+    def _find_root_index(fileSizeCrcs, _os_sep=os_sep,
+                         skips_start=_silentSkipsStart):
         # basically just care for skips and complex/simple packages
         #--Sort file names
         def fscSortKey(fsc):
@@ -5147,9 +5152,9 @@ class Installer(object):
         layout = {}
         layoutSetdefault = layout.setdefault
         for file,size,crc in fileSizeCrcs:
-            if file.startswith(u'--'): continue ##: also ignore other silent skips !!
+            if file.startswith(skips_start): continue
             fileLower = file.lower()
-            frags = fileLower.split(u'\\')
+            frags = fileLower.split(_os_sep)
             if len(frags) == 1:
                 # Files in the root of the package, start there
                 rootIdex = 0
@@ -5173,7 +5178,7 @@ class Installer(object):
                     rootIdex = 0
                 else:
                     root = layout[rootStr]
-                    rootStr = u''.join((rootStr,u'\\'))
+                    rootStr = u''.join((rootStr, _os_sep))
                     while True:
                         if root['files']:
                             # There are files in this folder, call it the starting point
@@ -5189,7 +5194,7 @@ class Installer(object):
                                 break
                             # Keep looking deeper
                             root = rootDirs[rootDirKey]
-                            rootStr = u''.join((rootStr,rootDirKey,u'\\'))
+                            rootStr = u''.join((rootStr, rootDirKey, _os_sep))
                         else:
                             # Multiple folders, stop here even if it's no good
                             break
@@ -5197,6 +5202,11 @@ class Installer(object):
         return rootIdex
 
     def refreshBasic(self, archive, progress, recalculate_project_crc=True):
+        return self._refreshBasic(archive, progress, recalculate_project_crc)
+
+    def _refreshBasic(self, archive, progress, recalculate_project_crc=True,
+                     _os_sep=os_sep, skips_start=tuple(
+                x.replace(os_sep, u'') for x in _silentSkipsStart)):
         """Extract file/size/crc and BAIN structure info from installer."""
         self._refreshSource(archive, progress, recalculate_project_crc)
         self.fileRootIdex = rootIdex = self._find_root_index(self.fileSizeCrcs)
@@ -5211,7 +5221,7 @@ class Installer(object):
         for file, size, crc in self.fileSizeCrcs:
             file = file[rootIdex:]
             if type_ != 1:
-                frags = file.split(u'\\')
+                frags = file.split(_os_sep)
                 nfrags = len(frags)
                 #--Type 1?
                 if (nfrags == 1 and reDataFileSearch(frags[0]) or
@@ -5219,9 +5229,9 @@ class Installer(object):
                     type_ = 1
                     break
                 #--Type 2?
-                elif nfrags > 2 and not frags[0].startswith(u'--') and \
+                elif nfrags > 2 and not frags[0].startswith(skips_start) and \
                                 frags[1].lower() in dataDirsPlus \
-                 or nfrags == 2 and not frags[0].startswith(u'--') and \
+                 or nfrags == 2 and not frags[0].startswith(skips_start) and \
                                 reDataFileSearch(frags[1]):
                     subNameSetAdd(frags[0])
                     type_ = 2

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -231,16 +231,15 @@
 <ul>
     <li>Silent skips. These are <i>not</i> listed on the <q>Skipped</q> panel on the right and are:
         <ul>
-            <li><code>thumbs.db</code> and <code>desktop.ini</code> files (actually any file path that ends in those strings)
+            <li><code>thumbs.db</code> and <code>desktop.ini</code> files
             <li>any path that ends in <code>config</code>
-            <li>the omod conversion directory (<code>omod conversion data</code>, <code>fomod</code>) - actually any top level package or subpackage file/directory that starts with those
-                strings
-            <li>the wizard images directory (or any file/directory that starts with <code>wizard images</code>)
+            <li>the omod conversion directory (<code>omod conversion data</code>, <code>fomod</code>) if on the top level of package or subpackage
+            <li>the <code>wizard images</code> directory (again if on the top level of package or subpackage)
             <li>Developer files. Any file or directory that begins with <code>'--'</code> and is at the top level of a package or a subpackage will be silently skipped. This is so that mod
                 authors can include files related to a mod that are not required for it to work (eg. screenshots) in packages.
             <li>Bash will also skip voices for espms that are not installed from complex installers
             <li>Specified skips. There are a few options in BAIN to skip the installation of certain file types. If any such options are enabled, the file type(s) in question will be
-            <i>silently</i> skipped.
+                <i>silently</i> skipped.
             <li>Voices (on a per installer basis) if <a href="#bainSkipVoices">Skip Voices</a> is checked.
         </ul>
     <li>Non-standard directories. Standard directories include all the game's default directories (including those only found in BSAs by default) - see

commit 51cae863a268abe45bf1f40ea69f6b9ce6e635fc

Fixups for case insensitive paths compare in core BAIN (and more):
Wrong comparisons introduced in a34b27aea5ab8a726bc5911ee1a26ff9db8a31df

Refactored slightly and added comments for readability and possible
enhancements.

    # This is the 2nd commit message:

Renamed deleted -> deleted_or_pending for readability, added comments
on groupby.

    # This is the 3rd commit message:

Comments in _plugins_cache_wrapper from load order refactoring (in undo
I returned None)

  # This is the 2nd commit message:

refreshMissingStrings -> _refreshMissingStrings. Tried to move it
before load order check to get (in Skyrim)

Traceback (most recent call last):
  File "Wrye Bash Launcher.pyw", line 88, in <module>
    bash.main()
  File "bash\bash.py", line 431, in main
    frame = app.Init() # Link.Frame is set here !
  File "bash\basher\__init__.py", line 4257, in Init
    self.InitData(progress)
  File "bash\basher\__init__.py", line 4292, in InitData
    bosh.modInfos.refresh()
  File "bash\bosh\__init__.py", line 3421, in refresh
    hasMissingStrings = hasChanged and self._refreshMissingStrings()
  File "bash\bosh\__init__.py", line 3462, in _refreshMissingStrings
    bad = set(fileName for fileName, fileInfo in self.iteritems() if
  File "bash\bosh\__init__.py", line 3463, in <genexpr>
    fileInfo.isMissingStrings())
  File "bash\bosh\__init__.py", line 2578, in isMissingStrings
    return modInfos.isMissingStrings(self.name)
  File "bash\bosh\__init__.py", line 3911, in isMissingStrings
    bsaPaths = self.extra_bsas(modInfo)
  File "bash\bosh\__init__.py", line 3954, in extra_bsas
    iniFiles = self._ini_files(descending=descending)
  File "bash\bosh\__init__.py", line 3943, in _ini_files
    iniPaths = (self[name].getIniPath() for name in self.activeCached)
  File "bash\bosh\__init__.py", line 3350, in activeCached
    return self.plugins.lord.activeOrdered
AttributeError: 'NoneType' object has no attribute 'activeOrdered'

hence the comment

Mopy/bash/basher/installer_links.py
@@ -835,7 +835,9 @@ class Installer_CopyConflicts(_SingleInstallable):
             g_path = GPath(package.s)
             curFile = _copy_conflicts(curFile)
             for order,package,curConflicts in packConflicts:
-                g_path = GPath(u"%03d - %s" % (order, package.s))
+                g_path = GPath(u"%03d - %s" % (
+                    order if order < srcInstaller.order else order + 1,
+                    package.s))
                 curFile = _copy_conflicts(curFile)
             project = destDir.root
         self._get_refreshed(project, srcInstaller)

Mopy/bash/bosh/__init__.py
@@ -2185,7 +2185,8 @@ def _cache(lord_func):
                 args[0].LoadOrder, args[0].selected = list(
                     args[0].lord.loadOrder), list(args[0].lord.activeOrdered)
             except AttributeError: # lord is None, exception thrown in init
-                raise e[0], e[1], e[2]
+                if e: raise e[0], e[1], e[2]
+                raise # should never happen, lord must never be None after init
     return _plugins_cache_wrapper
 
 class Plugins:
@@ -3419,7 +3420,8 @@ class ModInfos(FileInfos):
         if hasChanged or _modTimesChange: self.refreshInfoLists()
         self.reloadBashTags()
         hasNewBad = self.refreshBadNames()
-        hasMissingStrings = self.refreshMissingStrings()
+        # we need a load order below: in skyrim it reads inis in active order
+        hasMissingStrings = self._refreshMissingStrings()
         self.setOblivionVersions()
         oldMergeable = set(self.mergeable)
         scanList = self.refreshMergeable()
@@ -3449,14 +3451,12 @@ class ModInfos(FileInfos):
                     bad.add(fileName)
         return bool(activeBad)
 
-    def refreshMissingStrings(self):
-        """Refreshes which mods are supposed to have strings files,
-           but are missing them (=CTD)."""
+    def _refreshMissingStrings(self):
+        """Refreshes which mods are supposed to have strings files, but are
+        missing them (=CTD). For Skyrim you need to have a valid load order."""
         oldBad = self.missing_strings
-        bad = set()
-        for fileName, fileInfo in self.iteritems():
-            if fileInfo.isMissingStrings():
-                bad.add(fileName)
+        bad = set(fileName for fileName, fileInfo in self.iteritems() if
+                  fileInfo.isMissingStrings())
         new = bad - oldBad
         self.missing_strings = bad
         self.new_missing_strings = new
@@ -5142,9 +5142,9 @@ class Installer(object):
         dataDirsPlus = Installer.dataDirsPlus
         layout = {}
         layoutSetdefault = layout.setdefault
-        for file,size,crc in fileSizeCrcs:
-            if file.startswith(skips_start): continue
-            fileLower = file.lower()
+        for full, size, crc in fileSizeCrcs:
+            fileLower = full.lower()
+            if fileLower.startswith(skips_start): continue
             frags = fileLower.split(_os_sep)
             if len(frags) == 1:
                 # Files in the root of the package, start there
@@ -5210,21 +5210,20 @@ class Installer(object):
         subNameSetAdd(u'')
         reDataFileSearch = self.reDataFile.search
         dataDirsPlus = self.dataDirsPlus
-        for file, size, crc in self.fileSizeCrcs:
-            file = file[rootIdex:]
-            if type_ != 1:
-                frags = file.split(_os_sep)
+        for full, size, crc in self.fileSizeCrcs:
+            full = full.lower()[rootIdex:]
+            if type_ != 1: # for type 2 will scan ALL filenames - can avoid ?
+                frags = full.split(_os_sep)
                 nfrags = len(frags)
                 #--Type 1?
                 if (nfrags == 1 and reDataFileSearch(frags[0]) or
-                    nfrags > 1 and frags[0].lower() in dataDirsPlus):
+                    nfrags > 1 and frags[0] in dataDirsPlus):
                     type_ = 1
                     break
                 #--Type 2?
-                elif nfrags > 2 and not frags[0].startswith(skips_start) and \
-                                frags[1].lower() in dataDirsPlus \
-                 or nfrags == 2 and not frags[0].startswith(skips_start) and \
-                                reDataFileSearch(frags[1]):
+                elif not frags[0].startswith(skips_start) and (
+                    (nfrags > 2 and frags[1] in dataDirsPlus) or
+                    (nfrags == 2 and reDataFileSearch(frags[1]))):
                     subNameSetAdd(frags[0])
                     type_ = 2
         self.type = type_
@@ -5233,7 +5232,7 @@ class Installer(object):
             self.subNames = sorted(subNameSet,key=unicode.lower)
             actives = set(x for x,y in zip(self.subNames,self.subActives) if (y or x == u''))
             if len(self.subNames) == 2: #--If only one subinstall, then make it active.
-                self.subActives = [True,True]
+                self.subActives = [True,True] # that's a complex/simple package
             else:
                 self.subActives = [(x in actives) for x in self.subNames]
         else:
@@ -6350,15 +6349,15 @@ class InstallersData(DataDict):
                 root_files.append((bass.dirs['mods'].s, name.s))
             else:
                 root_files.append((bass.dirs['mods'].join(sp[0]).s, sp[1]))
-        root_files.sort()
         root_dirs_files = []
+        root_files.sort(key=lambda t: t[0]) # must sort on same key as groupby
         for key, val in groupby(root_files, key=lambda t: t[0]):
             root_dirs_files.append((key, [], [j for i, j in val]))
         progress = progress or bolt.Progress()
         new_sizeCrcDate, pending, pending_size = self._process_data_dir(
             root_dirs_files, progress)
-        deleted = set(dest_paths) - set(new_sizeCrcDate)
-        for d in deleted: self.data_sizeCrcDate.pop(d, None)
+        deleted_or_pending = set(dest_paths) - set(new_sizeCrcDate)
+        for d in deleted_or_pending: self.data_sizeCrcDate.pop(d, None)
         Installer.calc_crcs(pending, pending_size, bass.dirs['mods'].stail,
                             new_sizeCrcDate, progress)
         for rpFile, (size, crc, date, _asFile) in new_sizeCrcDate.iteritems():