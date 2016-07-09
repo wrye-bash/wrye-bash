wrye-bash/wrye-bash

commit 2041a980f0522ed0a5d04ed6eed42fcc3ccf500e

Slight de-wx'ing:
Using belt methods and dropping wx.ID_ANY.
Was doing this since forever but it's also in the wx coding style wiki:
https://wiki.wxpython.org/wxPython%20Style%20Guide

2. Keyword arguments in constructors.
3. Don't use IDs.

Been there.
Took the opportunity to drop balt.Dialog unused init parameters.

Mopy/bash/belt.py
@@ -409,7 +409,7 @@ class PageFinish(PageInstaller):
                 espmShow.append(x)
         espmShow = [x.replace(u'&',u'&&') for x in espmShow]
 
-        sizerMain = wx.BoxSizer(wx.VERTICAL)
+        sizerMain = balt.vSizer()
 
         parent.parser.choiceIdex += 1
 
@@ -421,8 +421,8 @@ class PageFinish(PageInstaller):
         sizerMain.Add(sizerTitle,0,wx.EXPAND)
 
         #--Subpackages and Espms
-        sizerSubsEspms = wx.BoxSizer(wx.HORIZONTAL)
-        subPackageSizer = wx.BoxSizer(wx.VERTICAL)
+        sizerSubsEspms = balt.hSizer()
+        subPackageSizer = balt.vSizer()
         subPackageSizer.Add(balt.StaticText(self, _(u'Sub-Packages')),0,wx.BOTTOM,2)
         self.listSubs = balt.listBox(self, choices=subs, kind='checklist')
         self.listSubs.Bind(wx.EVT_CHECKLISTBOX, self.OnSelectSubs)
@@ -432,7 +432,7 @@ class PageFinish(PageInstaller):
                 self.listSubs.Check(index, True)
                 self.parent.ret.SelectSubPackages.append(key)
         subPackageSizer.Add(self.listSubs,1,wx.EXPAND)
-        espmSizer = wx.BoxSizer(wx.VERTICAL)
+        espmSizer = balt.vSizer()
         espmSizer.Add(balt.StaticText(self, _(u'Esp/ms')),0,wx.BOTTOM,2)
         self.listEspms = balt.listBox(self, choices=espmShow, kind='checklist')
         self.listEspms.Bind(wx.EVT_CHECKLISTBOX, self.OnSelectEspms)
@@ -447,13 +447,13 @@ class PageFinish(PageInstaller):
         sizerMain.Add(sizerSubsEspms,2,wx.EXPAND|wx.TOP|wx.BOTTOM,5)
 
         #--Ini tweaks
-        sizerIniTweaks = wx.BoxSizer(wx.HORIZONTAL)
-        sizerTweaks = wx.BoxSizer(wx.VERTICAL)
+        sizerIniTweaks = balt.hSizer()
+        sizerTweaks = balt.vSizer()
         sizerTweaks.Add(balt.StaticText(self, _(u'Ini Tweaks:')),0,wx.BOTTOM,2)
         self.listInis = balt.listBox(self, choices=[x.s for x in iniedits.keys()])
         self.listInis.Bind(wx.EVT_LISTBOX, self.OnSelectIni)
         sizerTweaks.Add(self.listInis,1,wx.EXPAND)
-        sizerContents = wx.BoxSizer(wx.VERTICAL)
+        sizerContents = balt.vSizer()
         sizerContents.Add(balt.StaticText(self, u''),0,wx.BOTTOM,2)
         self.listTweaks = balt.listBox(self)
         sizerContents.Add(self.listTweaks,1,wx.EXPAND)
@@ -468,8 +468,8 @@ class PageFinish(PageInstaller):
             balt.RoTextCtrl(self, u''.join(notes), autotooltip=False), 1,
             wx.EXPAND)
 
-        checkSizer = wx.BoxSizer(wx.HORIZONTAL)
-        checkSubSizer = wx.BoxSizer(wx.VERTICAL)
+        checkSizer = balt.hSizer()
+        checkSubSizer = balt.vSizer()
         checkSizer.AddStretchSpacer()
         # Apply the selections
         self.checkApply = balt.checkBox(self, _(u'Apply these selections'),
@@ -547,9 +547,11 @@ class PageVersions(PageInstaller):
         sizerVersions.Add(balt.StaticText(self, _(u'Have')))
         sizerVersions.AddStretchSpacer()
 
+        def _link(label, url): return wx.HyperlinkCtrl(self, label=label,
+                                                       url=url)
         # Game
         if bush.game.patchURL != u'':
-            linkGame = wx.HyperlinkCtrl(self, wx.ID_ANY, bush.game.displayName, bush.game.patchURL)
+            linkGame = _link(bush.game.displayName, bush.game.patchURL)
             linkGame.SetVisitedColour(linkGame.GetNormalColour())
         else:
             linkGame = balt.StaticText(self, bush.game.displayName)
@@ -561,7 +563,7 @@ class PageVersions(PageInstaller):
 
         # Script Extender
         if bush.game.se.shortName != u'':
-            linkSE = wx.HyperlinkCtrl(self, wx.ID_ANY, bush.game.se.longName, bush.game.se.url)
+            linkSE = _link(bush.game.se.longName, bush.game.se.url)
             linkSE.SetVisitedColour(linkSE.GetNormalColour())
             linkSE.SetToolTip(balt.tooltip(bush.game.se.urlTip))
             sizerVersions.Add(linkSE)
@@ -571,7 +573,7 @@ class PageVersions(PageInstaller):
 
         # Graphics extender
         if bush.game.ge.shortName != u'':
-            linkGE = wx.HyperlinkCtrl(self, wx.ID_ANY, bush.game.ge.longName, bush.game.ge.url)
+            linkGE = _link(bush.game.ge.longName, bush.game.ge.url)
             linkGE.SetVisitedColour(linkGE.GetNormalColour())
             linkGE.SetToolTip(balt.tooltip(bush.game.ge.urlTip))
             sizerVersions.Add(linkGE)
@@ -579,7 +581,8 @@ class PageVersions(PageInstaller):
             sizerVersions.Add(balt.StaticText(self, geHave))
             sizerVersions.Add(balt.staticBitmap(self, bmp[bGEOk]))
 
-        linkWB = wx.HyperlinkCtrl(self, wx.ID_ANY, u'Wrye Bash', u'http://oblivion.nexusmods.com/mods/22368')
+        linkWB = _link(u'Wrye Bash',
+                       u'http://oblivion.nexusmods.com/mods/22368')
         linkWB.SetVisitedColour(linkWB.GetNormalColour())
         linkWB.SetToolTip(balt.tooltip(u'http://oblivion.nexusmods.com/'))
         sizerVersions.Add(linkWB)

Mopy/bash/balt.py
@@ -960,7 +960,7 @@ class Dialog(wx.Dialog):
     title = u'OVERRIDE'
 
     def __init__(self, parent=None, title=None, size=defSize, pos=defPos,
-                 style=0, resize=True, caption=False, *args, **kwargs):
+                 style=0, resize=True, caption=False):
         ##: drop parent/resize parameters(parent=Link.Frame (test),resize=True)
         self.sizesKey = self.__class__.__name__
         self.title = title or self.__class__.title
@@ -968,8 +968,8 @@ class Dialog(wx.Dialog):
         self.resizable = resize
         if resize: style |= wx.RESIZE_BORDER
         if caption: style |= wx.CAPTION
-        super(Dialog, self).__init__(parent, wx.ID_ANY, self.title, size=size,
-                                     pos=pos, style=style, *args, **kwargs)
+        super(Dialog, self).__init__(parent, title=self.title, size=size,
+                                     pos=pos, style=style)
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow) # used in ImportFaceDialog and ListEditor
 
     def OnCloseWindow(self, event):

Mopy/bash/bash.py
@@ -478,10 +478,10 @@ def _showErrorInAnyGui(msg):
 
         class ErrorMessage(wx.Frame):
             def __init__(self):
-                wx.Frame.__init__(self, None, wx.ID_ANY, u'Wrye Bash')
-                self.panel = panel = wx.Panel(self, wx.ID_ANY)
+                wx.Frame.__init__(self, None, title=u'Wrye Bash')
+                self.panel = panel = wx.Panel(self)
                 sizer = wx.BoxSizer(wx.VERTICAL)
-                sizer.Add(wx.TextCtrl(panel, wx.ID_ANY, msg,
+                sizer.Add(wx.TextCtrl(panel, value=msg,
                                       style=wx.TE_MULTILINE | wx.TE_READONLY
                                             | wx.TE_BESTWRAP),
                           1, wx.GROW | wx.ALL, 5)
@@ -529,17 +529,17 @@ def _wxSelectGame(ret, msgtext):
 
     class GameSelect(wx.Frame):
         def __init__(self, gameNames, callback):
-            wx.Frame.__init__(self, None, wx.ID_ANY, u'Wrye Bash')
+            wx.Frame.__init__(self, None, title=u'Wrye Bash')
             self.callback = callback
-            self.panel = panel = wx.Panel(self, wx.ID_ANY)
+            self.panel = panel = wx.Panel(self)
             sizer = wx.BoxSizer(wx.VERTICAL)
-            sizer.Add(wx.TextCtrl(panel, wx.ID_ANY, msgtext,
+            sizer.Add(wx.TextCtrl(panel, value=msgtext,
                                   style=wx.TE_MULTILINE | wx.TE_READONLY |
                                         wx.TE_BESTWRAP),
                       1, wx.GROW | wx.ALL, 5)
             for gameName in gameNames:
                 gameName = gameName[0].upper() + gameName[1:]
-                sizer.Add(wx.Button(panel, wx.ID_ANY, gameName), 0,
+                sizer.Add(wx.Button(panel, label=gameName), 0,
                           wx.GROW | wx.ALL ^ wx.TOP, 5)
             button = wx.Button(panel, wx.ID_CANCEL, _(u'Quit'))
             button.SetDefault()

Mopy/bash/basher/dialogs.py
@@ -392,10 +392,10 @@ class CreateNewProject(balt.Dialog):
         okButton = OkButton(self, onButClickEventful=self.OnClose)
         cancelButton = CancelButton(self, onButClickEventful=self.OnCancel)
         # Panel Layout
-        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        hsizer = balt.hSizer()
         hsizer.Add(okButton,0,wx.ALL|wx.ALIGN_CENTER,10)
         hsizer.Add(cancelButton,0,wx.ALL|wx.ALIGN_CENTER,10)
-        vsizer = wx.BoxSizer(wx.VERTICAL)
+        vsizer = balt.vSizer()
         vsizer.Add(StaticText(self,_(u'What do you want to name the New Project?'),style=wx.TE_RICH2),0,wx.ALL|wx.ALIGN_CENTER,10)
         vsizer.Add(self.textName,0,wx.ALL|wx.ALIGN_CENTER|wx.EXPAND,2)
         vsizer.Add(StaticText(self,_(u'What do you want to add to the New Project?')),0,wx.ALL|wx.ALIGN_CENTER,10)
@@ -405,7 +405,7 @@ class CreateNewProject(balt.Dialog):
         vsizer.Add(self.checkWizardImages,0,wx.ALL|wx.ALIGN_TOP,5)
         vsizer.Add(self.checkDocs,0,wx.ALL|wx.ALIGN_TOP,5)
         # vsizer.Add(self.checkScreenshot,0,wx.ALL|wx.ALIGN_TOP,5)
-        vsizer.Add(wx.StaticLine(self,wx.ID_ANY))
+        vsizer.Add(wx.StaticLine(self))
         vsizer.AddStretchSpacer()
         vsizer.Add(hsizer,0,wx.ALIGN_CENTER)
         vsizer.AddStretchSpacer()

commit c5ef8ec3d5a58571373ff274912f48e6c6d87be9

Regression - id is required in wx.HyperlinkCtrl:
Introduced in 2041a980f0522ed0a5d04ed6eed42fcc3ccf500e

Signed-off-by: MrD <the.ubik@gmail.com>

Mopy/bash/belt.py
@@ -546,8 +546,8 @@ class PageVersions(PageInstaller):
         sizerVersions.Add(balt.StaticText(self, _(u'Have')))
         sizerVersions.AddStretchSpacer()
 
-        def _link(label, url): return wx.HyperlinkCtrl(self, label=label,
-                                                       url=url)
+        def _link(label, url): return wx.HyperlinkCtrl(self, balt.defId,
+                                                       label=label, url=url)
         # Game
         if bush.game.patchURL != u'':
             linkGame = _link(bush.game.displayName, bush.game.patchURL)