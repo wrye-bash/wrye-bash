wrye-bash/wrye-bash

commit b8b23d39d008f95e2dd903193551c5ce91da2afb

Poc ini tweak cache - 'deleted' broken:
This optimizes the getSetting and getSettings paths of calling
getTweakFileSettings but borgs "deleted" .I leave it as poc (as deleted
is not really used from what I can gather) but belongs with rest of
commits in this merge

Mopy/bash/bosh/__init__.py
@@ -1553,11 +1553,15 @@ class IniFile(object):
     reSetting = re.compile(ur'(.+?)\s*=(.*)',re.U)
     formatRes = (reSetting, reSection)
     encoding = 'utf-8'
+    __empty = {}
 
     def __init__(self,path,defaultSection=u'General'):
         self.path = path
         self.defaultSection = defaultSection
         self.isCorrupted = False
+        self._ini_size = self.path.size if self.path.exists() else 0
+        self._ini_mod_time = self.path.mtime if self.path.exists() else 0
+        self._settings_cache = self.__empty
 
     @classmethod
     def formatMatch(cls, path):
@@ -1582,7 +1586,15 @@ class IniFile(object):
 
     def getSettings(self):
         """Gets settings for self."""
-        return self.getTweakFileSettings(self.path,True)
+        if not self.path.exists() or self.path.isdir(): return {}, {}
+        psize, pmtime = self.path.size_mtime()
+        if self._settings_cache is not self.__empty and (
+            self._ini_size == psize) and (self._ini_mod_time == pmtime):
+            deleted = {}
+        else:
+            self._ini_size, self._ini_mod_time = psize, pmtime
+            self._settings_cache, deleted = self.getTweakFileSettings(self.path, True)
+        return copy.copy(self._settings_cache), deleted
 
     def getTweakFileSettings(self,tweakPath,setCorrupted=False,lineNumbers=False):
         """Gets settings in a tweak file."""
@@ -4328,7 +4340,8 @@ class ScreensData(_DataStore):
     def refresh(self):
         """Refresh list of screenshots."""
         self.dir = dirs['app']
-        ssBase = GPath(oblivionIni.getSetting(u'Display',u'SScreenShotBaseName',u'ScreenShot')) ##: cache ?
+        ssBase = GPath(oblivionIni.getSetting(
+            u'Display', u'SScreenShotBaseName', u'ScreenShot'))
         if ssBase.head:
             self.dir = self.dir.join(ssBase.head)
         newData = {}

commit fd3c0f560878611a2b9eea65fc3cabaff69beade

Deleted - don't really get what this is about but needs caching too:
I do not like self._deleted_cache but API calls simplified thanks to
with_deleted param.
Mopy/bash/basher/__init__.py:
just renames for readability
Mopy/bash/basher/ini_links.py:
deleted = {} was apparently a "free memory" trick - much cleaner now.

'settings' local variables renamed too - settings are for bass global.

Fixes deleted broken in b8b23d39d008f95e2dd903193551c5ce91da2afb

Mopy/bash/basher/__init__.py
@@ -683,11 +683,11 @@ class INITweakLineCtrl(INIListCtrl):
             else:
                 self.SetStringItem(i, 0, line[0])
             #--Line color
-            status = line[4]
+            status, deleted = line[4], line[6]
             if status == -10: color = colors['tweak.bkgd.invalid']
             elif status == 10: color = colors['tweak.bkgd.mismatched']
             elif status == 20: color = colors['tweak.bkgd.matched']
-            elif line[6]: color = colors['tweak.bkgd.mismatched']
+            elif deleted: color = colors['tweak.bkgd.mismatched']
             else: color = self.GetBackgroundColour()
             self.SetItemBackgroundColour(i, color)
             #--Set iniContents color

Mopy/bash/basher/ini_links.py
@@ -211,9 +211,8 @@ class INI_CreateNew(OneItemLink):
         bosh.iniInfos.refresh()
         oldTarget = self.window.data_store.ini
         target = bosh.BestIniFile(path)
-        settings,deleted = target.getSettings()
-        new_settings,deleted = oldTarget.getSettings()
-        deleted = {}
+        settings = target.getSettings()
+        new_settings = oldTarget.getSettings()
         for section in settings:
             if section in new_settings:
                 for setting in settings[section]:

Mopy/bash/bosh/__init__.py
@@ -1563,6 +1563,7 @@ class IniFile(object):
         self._ini_mod_time = self.path.mtime if self.path.exists() else 0
         self._settings_cache = self.__empty
         self._settings_cache_linenum = self.__empty
+        self._deleted_cache = self.__empty
 
     @classmethod
     def formatMatch(cls, path):
@@ -1579,26 +1580,29 @@ class IniFile(object):
     def getSetting(self,section,key,default=None):
         """Gets a single setting from the file."""
         section,key = map(bolt.LString,(section,key))
-        ini_settings,deleted_settings = self.getSettings()
+        ini_settings = self.getSettings()
         if section in ini_settings:
             return ini_settings[section].get(key,default)
         else:
             return default
 
-    def getSettings(self, with_line_numbers=False):
+    def getSettings(self, with_line_numbers=False, with_deleted=False):
         """Gets settings for self."""
-        if not self.path.exists() or self.path.isdir(): return {}, {}
+        if not self.path.exists() or self.path.isdir():
+            return ({}, {}) if with_deleted else {}
         psize, pmtime = self.path.size_mtime()
-        if self._settings_cache is not self.__empty and (
-            self._ini_size == psize) and (self._ini_mod_time == pmtime):
-            deleted = {}
-        else:
+        if self._settings_cache is self.__empty or self._ini_size != psize \
+                or self._ini_mod_time != pmtime:
             self._ini_size, self._ini_mod_time = psize, pmtime
-            self._settings_cache_linenum, deleted = self.getTweakFileSettings(self.path, lineNumbers=True)
+            self._settings_cache_linenum, self._deleted_cache = \
+                self.getTweakFileSettings(self.path, lineNumbers=True)
             self._settings_cache = dict(
                 (k, dict((x, y[0]) for x, y in v.iteritems())) for k, v in
                 self._settings_cache_linenum.iteritems())
-        return copy.copy(self._settings_cache if not with_line_numbers else self._settings_cache_linenum), deleted
+        cached = copy.copy(self._settings_cache if not with_line_numbers else
+                           self._settings_cache_linenum)
+        if with_deleted: return cached, copy.copy(self._deleted_cache)
+        return cached
 
     def getTweakFileSettings(self,tweakPath,lineNumbers=False):
         """Gets settings in a tweak file."""
@@ -1648,7 +1652,7 @@ class IniFile(object):
 
     def getTweakFileLines(self,tweakPath):
         """Get a line by line breakdown of the tweak file, in this format:
-        [(fulltext,section,setting,value,status,ini_line_number)]
+        [(fulltext,section,setting,value,status,ini_line_number, deleted)]
         where:
         fulltext = full line of text from the ini
         section = the section that is being edited
@@ -1659,7 +1663,8 @@ class IniFile(object):
               0: does exist, but it's a heading or something else without a value
              10: does exist, but value isn't the same
              20: does exist, and value is the same
-        ini_line_number = line number in the ini that this tweak applies to"""
+        ini_line_number = line number in the ini that this tweak applies to
+        deleted: deleted line (?)"""
         lines = []
         if not tweakPath.exists() or tweakPath.isdir():
             return lines
@@ -1667,7 +1672,7 @@ class IniFile(object):
             encoding = 'utf-8'
         else:
             encoding = self.encoding
-        iniSettings,deletedSettings = self.getSettings(with_line_numbers=True)
+        iniSettings,deletedSettings = self.getSettings(with_line_numbers=True, with_deleted=True)
         reComment = self.reComment
         reSection = self.reSection
         reDeleted = self.reDeletedSetting
@@ -2765,7 +2770,7 @@ class INIInfo(FileInfo):
 
     @property
     def tweak_status(self):
-        self.__target_ini, old = iniInfos.ini, self.__target_ini
+        self.__target_ini, old = self.getFileInfos().ini, self.__target_ini
         if self._status is None or self.__target_ini != old: self.getStatus()
         return self._status
 
@@ -2789,12 +2794,12 @@ class INIInfo(FileInfo):
             return -10
         match = False
         mismatch = 0
-        settings,deleted = ini.getSettings()
+        ini_settings = ini.getSettings()
         for key in tweak:
-            if key not in settings:
+            if key not in ini_settings:
                 self._status = -10
                 return -10
-            settingsKey = settings[key]
+            settingsKey = ini_settings[key]
             tweakKey = tweak[key]
             for item in tweakKey:
                 if item not in settingsKey:
@@ -2836,7 +2841,7 @@ class INIInfo(FileInfo):
         path = self.getPath()
         ini = iniInfos.ini
         tweak,deletes = ini.getTweakFileSettings(path)
-        settings,deleted_settings = ini.getSettings()
+        ini_settings = ini.getSettings()
         text = [u'%s:' % path.stail]
 
         if len(tweak) == 0:
@@ -2863,11 +2868,11 @@ class INIInfo(FileInfo):
                                  _(u'Tweak format: INI')))
         else:
             for key in tweak:
-                if key not in settings:
+                if key not in ini_settings:
                     text.append(u' [%s] - %s' % (key,_(u'Invalid Header')))
                 else:
                     for item in tweak[key]:
-                        if item not in settings[key]:
+                        if item not in ini_settings[key]:
                             text.append(u' [%s] %s' % (key, item))
         if len(text) == 1:
             text.append(u' None')