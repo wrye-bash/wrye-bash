wrye-bash/wrye-bash

commit 18befdc7bad24749e6267a6a26933f7ac75a27f2

Pruning more self.selected[0] - _SingleProject(OneItemLink):
Readability, plus I never really liked _InstallerLink

Added clarification on InstallerProject_Sync, see:
http://forums.bethsoft.com/topic/1606578-wrye-bash-thread-111/#entry25218878

Time to implement #41.

Mopy/bash/basher/installer_links.py
@@ -70,12 +70,6 @@ __all__ = ['Installer_Open', 'Installer_Duplicate', 'InstallerOpenAt_MainMenu',
 class _InstallerLink(Installers_Link, EnabledLink):
     """Common functions for installer links..."""
 
-    def isSingleProject(self):
-        """Indicates whether or not is single project."""
-        if len(self.selected) != 1: return False
-        else: return isinstance(self.idata[self.selected[0]],
-                                bosh.InstallerProject)
-
     def isSingleArchive(self):
         """Indicates whether or not is single archive."""
         if len(self.selected) != 1: return False
@@ -166,6 +160,12 @@ class _SingleInstallable(OneItemLink, _InstallerLink):
     def _enable(self):
         return super(_SingleInstallable, self)._enable() and bool(
             self.idata.filterInstallables(self.selected))
+
+class _SingleProject(OneItemLink, _InstallerLink):
+
+    def _enable(self):
+        return super(_SingleProject, self)._enable() and isinstance(
+            self._selected_info, bosh.InstallerProject)
 
 class _RefreshingLink(_SingleInstallable):
     _overrides_skips = False
@@ -384,7 +384,7 @@ class Installer_Duplicate(OneItemLink, _InstallerLink):
     def _initData(self, window, selection):
         super(Installer_Duplicate, self)._initData(window, selection)
         self.help = _(u"Duplicate selected %(installername)s.") % (
-            {'installername': self.selected[0]})
+            {'installername': self._selected_item})
 
     def _enable(self):
         isSingle = super(Installer_Duplicate, self)._enable()
@@ -394,7 +394,7 @@ class Installer_Duplicate(OneItemLink, _InstallerLink):
     @balt.conversation
     def Execute(self):
         """Duplicate selected Installer."""
-        curName = self.selected[0]
+        curName = self._selected_item
         isdir = self.idata.dir.join(curName).isdir()
         if isdir: root,ext = curName,u''
         else: root,ext = curName.rootExt
@@ -492,7 +492,7 @@ class Installer_OverrideSkips(CheckLink, _RefreshingLink):
         super(Installer_OverrideSkips, self)._initData(window, selection)
         self.help = _(
             u"Override global file type skipping for %(installername)s.") % (
-                    {'installername': self.selected[0]}) + u'  '+ _(u'BETA!')
+                {'installername': self._selected_item}) + u'  '+ _(u'BETA!')
 
     def _check(self):
         return self._enable() and self._selected_info.overrideSkips
@@ -502,12 +502,10 @@ class Installer_OverrideSkips(CheckLink, _RefreshingLink):
         self._overrides_skips = self._selected_info.overrideSkips
         super(Installer_OverrideSkips, self).Execute()
 
-class Installer_SkipRefresh(CheckLink, OneItemLink, _InstallerLink):
+class Installer_SkipRefresh(CheckLink, _SingleProject):
     """Toggle skipRefresh flag on project."""
     text = _(u"Don't Refresh")
     help = _(u"Don't automatically refresh project.")
-
-    def _enable(self): return self.isSingleProject()
 
     def _check(self): return self._enable() and self._selected_info.skipRefresh
 
@@ -743,7 +741,7 @@ class Installer_CopyConflicts(_SingleInstallable):
         installers to a project."""
         srcConflicts = set()
         packConflicts = []
-        srcArchive = self.selected[0]
+        srcArchive = self._selected_item
         srcInstaller = self.idata[srcArchive]
         src_sizeCrc = srcInstaller.data_sizeCrc # dictionary Path -> (int, int)
         all_files = set(src_sizeCrc) # bolt.PathS of ALL installer's files
@@ -1006,69 +1004,64 @@ class InstallerArchive_Unpack(AppendableLink, _InstallerLink):
 #------------------------------------------------------------------------------
 # InstallerProject Links ------------------------------------------------------
 #------------------------------------------------------------------------------
-class InstallerProject_OmodConfig(_InstallerLink):
+class InstallerProject_OmodConfig(_SingleProject):
     """Projects only. Allows you to read/write omod configuration info."""
     text = _(u'Omod Info...')
     help = _(u'Projects only. Allows you to read/write omod configuration info')
 
-    def _enable(self): return self.isSingleProject()
-
     def Execute(self):
-        project = self.selected[0]
         (InstallerProject_OmodConfigDialog(self.window, self.idata,
-                                           project)).Show()
+                                           self._selected_item)).Show()
 
 #------------------------------------------------------------------------------
-class InstallerProject_Sync(_InstallerLink):
+class InstallerProject_Sync(_SingleProject):
     """Synchronize the project with files from the Data directory."""
     text = _(u'Sync from Data')
-    help = _(u'Synchronize the project with files from the Data directory')
+    help = _(u'Synchronize the project with files from the Data directory') + \
+        u'.  ' + _(u'Currently only for projects (not archives)')
 
     def _enable(self):
-        if not self.isSingleProject(): return False
-        project = self.selected[0]
-        installer = self.idata[project]
-        return bool(installer.missingFiles or installer.mismatchedFiles)
+        if not super(InstallerProject_Sync, self)._enable(): return False
+        return bool(self._selected_info.missingFiles or
+                    self._selected_info.mismatchedFiles)
 
     def Execute(self):
-        project = self.selected[0]
-        installer = self.idata[project]
-        missing = installer.missingFiles
-        mismatched = installer.mismatchedFiles
-        message = (_(u'Update %s according to data directory?') + u'\n' +
-                   _(u'Files to delete:') + u'%d\n' +
-                   _(u'Files to update:') + u'%d') % (
-                        project.s, len(missing), len(mismatched))
+        missing = self._selected_info.missingFiles
+        mismatched = self._selected_info.mismatchedFiles
+        message = (_(u'Update %s according to data directory?') + u'\n' + _(
+            u'Files to delete:') + u'%d\n' + _(
+            u'Files to update:') + u'%d') % (
+                      self._selected_info.s, len(missing), len(mismatched))
         if not self._askWarning(message, title=self.text): return
         #--Sync it, baby!
         with balt.Progress(self.text, u'\n' + u' ' * 60) as progress:
             progress(0.1,_(u'Updating files.'))
-            installer.syncToData(project,missing|mismatched)
-            pProject = bass.dirs['installers'].join(project)
-            installer.refreshBasic(pProject, SubProgress(progress, 0.1, 0.99))
+            self._selected_info.syncToData(self._selected_info,
+                                           missing | mismatched)
+            pProject = bass.dirs['installers'].join(self._selected_info)
+            self._selected_info.refreshBasic(pProject,
+                                             SubProgress(progress, 0.1, 0.99))
             self.idata.irefresh(what='NS')
             self.window.RefreshUI()
 
 #------------------------------------------------------------------------------
-class InstallerProject_Pack(_InstallerLink):
+class InstallerProject_Pack(_SingleProject):
     """Pack project to an archive."""
     text = dialogTitle = _(u'Pack to Archive...')
     help = _(u'Pack project to an archive')
     release = False
 
-    def _enable(self): return self.isSingleProject()
-
     @balt.conversation
     def Execute(self):
         #--Generate default filename from the project name and the default extension
-        project = self.selected[0]
-        installer = self.idata[project]
-        archive = GPath(project.s + bolt.defaultExt)
+        archive = GPath(self._selected_item.s + bolt.defaultExt)
         #--Confirm operation
         archive = self._askFilename(
-            message=_(u'Pack %s to Archive:') % project.s, filename=archive.s)
+            message=_(u'Pack %s to Archive:') % self._selected_item.s,
+            filename=archive.s)
         if not archive: return
-        self._pack(archive, installer, project, release=self.__class__.release)
+        self._pack(archive, self._selected_info, self._selected_item,
+                   release=self.__class__.release)
         self.window.SelectAndShowItem(archive)
 
 #------------------------------------------------------------------------------

commit 84ab0a82897cd3e02c2607cdc9048139f7026cb7

Regression in Sync Data:
Introduced in 18befdc7bad24749e6267a6a26933f7ac75a27f2:

Traceback (most recent call last):
  File "bash\balt.py", line 2361, in __Execute
    self.Execute()
  File "bash\basher\installer_links.py", line 1057, in Execute
    self._selected_info.s, len(missing), len(mismatched))
AttributeError: 'InstallerProject' object has no attribute 's'

Reported by @saebel:
http://forums.bethsoft.com/topic/1606578-wrye-bash-thread-111/page-2#entry25224674

Took the opportunity to simplify the parameters of Installer methods -
we know our name, don't we now ? Had this be done earlier this
regression would have been avoided. Under #219 for this, more to come
(install, unpackToTemp,...)  - self.archive should maybe be a Path.

Mopy/bash/basher/__init__.py
@@ -2461,8 +2461,7 @@ class InstallersList(balt.UIList):
                         self.data_store.sorted_pairs(toRefresh)):
                     progress(index, _(u'Refreshing Packages...') + u'\n' +
                                     name.s)
-                    apath = bass.dirs['installers'].join(name)
-                    dest.update(installer.refreshBasic(apath,
+                    dest.update(installer.refreshBasic(
                         SubProgress(progress, index, index + 1),
                         recalculate_project_crc=calculate_projects_crc).keys())
                 self.data_store.hasChanged = True  # is it really needed ?

Mopy/bash/basher/installer_links.py
@@ -92,8 +92,7 @@ class _InstallerLink(Installers_Link, EnabledLink):
             self.idata.moveArchives([installer], src_installer.order + 1)
         installer_info = self.idata[installer]
         if is_project: # no need to call irefresh(what='I')
-            installer_path = self.idata.dir.join(installer) # bolt.Path
-            installer_info.refreshBasic(installer_path, progress=progress)
+            installer_info.refreshBasic(progress=progress)
         if do_refresh:
             self.idata.irefresh(what='NS')
         return installer_info
@@ -515,9 +514,8 @@ class Installer_SkipRefresh(CheckLink, _SingleProject):
         installer = self._selected_info
         installer.skipRefresh ^= True
         if not installer.skipRefresh:
-            installer.refreshBasic(
-                bass.dirs['installers'].join(installer.archive), progress=None,
-                recalculate_project_crc=False)
+            installer.refreshBasic(progress=None,
+                                   recalculate_project_crc=False)
             installer.refreshStatus(self.idata)
             self.idata.irefresh(what='N')
             self.window.RefreshUI()
@@ -992,7 +990,7 @@ class InstallerArchive_Unpack(AppendableLink, _InstallerLink):
                     if not self._askYes(
                         _(u"%s already exists. Overwrite it?") % project.s,
                         default=False): continue
-                installer.unpackToProject(archive,project,SubProgress(progress,0,0.8))
+                installer.unpackToProject(project,SubProgress(progress,0,0.8))
                 self._get_refreshed(project, installer, progress=SubProgress(progress, 0.8, 0.99), do_refresh=False)
                 projects.append(project)
             if not projects: return
@@ -1031,16 +1029,13 @@ class InstallerProject_Sync(_SingleProject):
         message = (_(u'Update %s according to data directory?') + u'\n' + _(
             u'Files to delete:') + u'%d\n' + _(
             u'Files to update:') + u'%d') % (
-                      self._selected_info.s, len(missing), len(mismatched))
+                      self._selected_item.s, len(missing), len(mismatched))
         if not self._askWarning(message, title=self.text): return
         #--Sync it, baby!
         with balt.Progress(self.text, u'\n' + u' ' * 60) as progress:
             progress(0.1,_(u'Updating files.'))
-            self._selected_info.syncToData(self._selected_info,
-                                           missing | mismatched)
-            pProject = bass.dirs['installers'].join(self._selected_info)
-            self._selected_info.refreshBasic(pProject,
-                                             SubProgress(progress, 0.1, 0.99))
+            self._selected_info.syncToData(missing | mismatched)
+            self._selected_info.refreshBasic(SubProgress(progress, 0.1, 0.99))
             self.idata.irefresh(what='NS')
             self.window.RefreshUI()
 

Mopy/bash/bosh/__init__.py
@@ -4605,8 +4605,8 @@ class Installer(object):
         if not exists: # the pickled package was deleted outside bash
             pass # don't do anything should be deleted from our data soon
         elif rescan:
-            dest_scr = self.refreshBasic(
-                package_path, bolt.Progress(), recalculate_project_crc=False)
+            dest_scr = self.refreshBasic(bolt.Progress(),
+                                         recalculate_project_crc=False)
         else: dest_scr = self.refreshDataSizeCrc()
         if exists and self.overrideSkips:
             InstallersData.overridden_skips.update(dest_scr.keys())
@@ -5099,15 +5099,15 @@ class Installer(object):
             self.extras_dict['root_path'] = rootStr # keeps case
             self.fileRootIdex = len(rootStr)
 
-    def refreshBasic(self, archive, progress, recalculate_project_crc=True):
-        return self._refreshBasic(archive, progress, recalculate_project_crc)
+    def refreshBasic(self, progress, recalculate_project_crc=True):
+        return self._refreshBasic(progress, recalculate_project_crc)
 
-    def _refreshBasic(self, archive, progress, recalculate_project_crc=True,
+    def _refreshBasic(self, progress, recalculate_project_crc=True,
                      _os_sep=os_sep, skips_start=tuple(
                 x.replace(os_sep, u'') for x in _silentSkipsStart)):
         """Extract file/size/crc and BAIN structure info from installer."""
         try:
-            self._refreshSource(archive, progress, recalculate_project_crc)
+            self._refreshSource(progress, recalculate_project_crc)
         except InstallerArchiveError:
             self.type = -1 # size, modified and some of fileSizeCrcs may be set
             return {}
@@ -5251,7 +5251,7 @@ class Installer(object):
         return self.__class__.__name__ + u"<" + repr(self.archive) + u">"
 
     #--ABSTRACT ---------------------------------------------------------------
-    def _refreshSource(self, archive, progress, recalculate_project_crc):
+    def _refreshSource(self, progress, recalculate_project_crc):
         """Refresh fileSizeCrcs, size, and modified from source
         archive/directory. fileSizeCrcs is a list of tuples, one for _each_
         file in the archive or project directory. _refreshSource is called
@@ -5323,7 +5323,7 @@ class InstallerMarker(Installer):
 
     def structure_string(self): return _(u'Structure: N/A')
 
-    def _refreshSource(self, archive, progress, recalculate_project_crc):
+    def _refreshSource(self, progress, recalculate_project_crc):
         """Marker: size is -1, fileSizeCrcs empty, modified = creation time."""
         pass
 
@@ -5342,7 +5342,7 @@ class InstallerMarker(Installer):
         del data[archive]
         return True, False, False
 
-    def refreshBasic(self, archive, progress, recalculate_project_crc=True):
+    def refreshBasic(self, progress, recalculate_project_crc=True):
         return {}
 
 #------------------------------------------------------------------------------
@@ -5357,10 +5357,11 @@ class InstallerArchive(Installer):
     type_string = _(u'Archive')
 
     #--File Operations --------------------------------------------------------
-    def _refreshSource(self, archive, progress, recalculate_project_crc):
+    def _refreshSource(self, progress, recalculate_project_crc):
         """Refresh fileSizeCrcs, size, modified, crc, isSolid from archive."""
         #--Basic file info
-        self.size, self.modified = archive.size_mtime()
+        archive_path = bass.dirs['installers'].join(self.archive)
+        self.size, self.modified = archive_path.size_mtime()
         #--Get fileSizeCrcs
         fileSizeCrcs = self.fileSizeCrcs = []
         self.isSolid = False
@@ -5379,12 +5380,12 @@ class InstallerArchive(Installer):
                     fileSizeCrcs.append((_li.filepath, _li.size, _li.crc))
                     _li.cumCRC += _li.crc
                 _li.filepath = _li.size = _li.crc = _li.isdir = 0
-        with archive.unicodeSafe() as tempArch:
+        with archive_path.unicodeSafe() as tempArch:
             try:
                 bolt.list_archive(tempArch, _parse_archive_line)
                 self.crc = _li.cumCRC & 0xFFFFFFFFL
             except:
-                archive_msg = u"Unable to read archive '%s'." % archive.s
+                archive_msg = u"Unable to read archive '%s'." % archive_path.s
                 deprint(archive_msg, traceback=True)
                 raise InstallerArchiveError(archive_msg)
 
@@ -5462,7 +5463,7 @@ class InstallerArchive(Installer):
         data_sizeCrcDate.update(data_sizeCrcDate_update)
         return count
 
-    def unpackToProject(self,archive,project,progress=None):
+    def unpackToProject(self, project, progress=None):
         """Unpacks archive to build directory."""
         progress = progress or bolt.Progress()
         files = self.sortFiles([x[0] for x in self.fileSizeCrcs])
@@ -5472,7 +5473,8 @@ class InstallerArchive(Installer):
         if destDir.exists(): destDir.rmtree(safety=u'Installers')
         #--Extract
         progress(0,project.s+u'\n'+_(u'Extracting files...'))
-        self.unpackToTemp(archive,files,SubProgress(progress,0,0.9))
+        self.unpackToTemp(GPath(self.archive), files,
+                          SubProgress(progress, 0, 0.9))
         #--Move
         progress(0.9,project.s+u'\n'+_(u'Moving files...'))
         count = 0
@@ -5620,7 +5622,7 @@ class InstallerProject(Installer):
         for empty in empties: empty.removedirs()
         projectDir.makedirs() #--In case it just got wiped out.
 
-    def _refreshSource(self, archive, progress, recalculate_project_crc):
+    def _refreshSource(self, progress, recalculate_project_crc):
         """Refresh src_sizeCrcDate, fileSizeCrcs, size, modified, crc from
         project directory, set project_refreshed to True."""
         self.modified = self._refresh_from_project_dir(progress,
@@ -5675,16 +5677,19 @@ class InstallerProject(Installer):
         data_sizeCrcDate.update(data_sizeCrcDate_update)
         return count
 
-    def syncToData(self,package,projFiles):
-        """Copies specified projFiles from Oblivion\Data to project directory."""
+    def syncToData(self, projFiles):
+        """Copies specified projFiles from Oblivion\Data to project
+        directory.
+        :type projFiles: set[bolt.Path]"""
         srcDir = dirs['mods']
-        projFiles = set(projFiles)
-        srcProj = tuple((x,y) for x,y in self.refreshDataSizeCrc().iteritems() if x in projFiles)
+        srcProj = tuple(
+            (x, y) for x, y in self.refreshDataSizeCrc().iteritems() if
+            x in projFiles)
         if not srcProj: return 0,0
         #--Sync Files
         updated = removed = 0
         norm_ghost = Installer.getGhosted()
-        projDir = dirs['installers'].join(package)
+        projDir = dirs['installers'].join(self.archive)
         for src,proj in srcProj:
             srcFull = srcDir.join(norm_ghost.get(src,src))
             projFull = projDir.join(proj)
@@ -5694,7 +5699,7 @@ class InstallerProject(Installer):
             else:
                 srcFull.copyTo(projFull)
                 updated += 1
-        self.removeEmpties(package)
+        self.removeEmpties(self.archive)
         return updated,removed
 
     def packToArchive(self,project,archive,isSolid,blockSize,progress=None,release=False):
@@ -5988,9 +5993,7 @@ class InstallersData(_DataStore):
                 installer = self.get(package, None)
                 if not installer:
                     installer = self.setdefault(package, iClass(package))
-                apath = dirs['installers'].join(package)
-                sub_progress = SubProgress(progress, index, index + 1)
-                installer.refreshBasic(apath, sub_progress,
+                installer.refreshBasic(SubProgress(progress, index, index + 1),
                                        recalculate_project_crc=fullRefresh)
         if changed: self.crc_installer = dict((x.crc, x) for x in
                         self.itervalues() if isinstance(x, InstallerArchive))