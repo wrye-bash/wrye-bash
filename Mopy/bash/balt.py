wrye-bash/wrye-bash

commit 325f3bc0b7f7caa64e4e6df04394a3534b7666d1

Disallowing renaming of quick/autosave bak:
Under #259, cosaves would not move along so broken - still WIP, I need
to drop:

        if not newName.lower().endswith(bush.game.ess.ext):
            newName += bush.game.ess.ext

Mopy/bash/basher/__init__.py
@@ -1772,6 +1772,7 @@ class SaveList(balt.UIList):
         selected = self.GetSelected()
         to_select = set(selected)
         for index, path in enumerate(selected):
+            if bosh.saveInfos.bak_file_pattern.match(path.s): continue
             if index:
                 newFileName = newName.replace(bush.game.ess.ext, (
                     u'%d' % index) + bush.game.ess.ext)
@@ -1796,8 +1797,7 @@ class SaveList(balt.UIList):
                                 new.moveTo(old)
                             if new.exists() and old.exists(): new.remove()
                         break
-        self.RefreshUI()
-        # self.RefreshUI(to_select) ##: not yet due to how PopulateItem works
+        self.RefreshUI(files=to_select)
         #--Reselect the items - renamed or not
         for save in to_select: self.SelectItem(save)
         event.Veto() # needed ! clears new name from label on exception

Mopy/bash/bosh/__init__.py
@@ -4200,6 +4200,7 @@ class SaveInfos(FileInfos):
         _ext + ur'|' + _ext[:-1] + ur'r' + ur'))$', # enabled or disabled save
         re.I | re.U)
     del _ext
+    bak_file_pattern = re.compile(ur'(quick|auto)save(\.bak)+', re.I | re.U)
 
     def _setLocalSaveFromIni(self):
         """Read the current save profile from the oblivion.ini file and set

commit 5990111ec8478cfbb02b3f6c85d4aecc1a852ae7

Factoring out new name validation:
This does quite a few things:

- image extensions centralized
- warns on renaming mixed markers and installers
- centralizes the invalid characters handling - in bolt at last!
- drops installers match_valid_name, reValidNamePattern as it
- _finally_ centralizes checking valid names. For the regexp used see:
http://stackoverflow.com/a/39884234/281545

- drops adding the extension for saves:

------------------------------ Mopy/bash/balt.py ------------------------------
index ed5d6b3..01b3c0d 100644
@@ -2113,7 +2113,5 @@ def Rename(self, selected=None):

-    def validate_filename(self, event, has_digits=False, ext=u'', add_ext=u''):
+    def validate_filename(self, event, has_digits=False, ext=u''):
         if event.IsEditCancelled(): return None, None
         newName = event.GetLabel()
-        if add_ext and not newName.lower().endswith(add_ext):
-            newName += add_ext
         if not newName:

------------------------- Mopy/bash/basher/__init__.py -------------------------
index 791c2cf..abdd6d5 100644
@@ -1796,4 +1796,5 @@ def OnLabelEdited(self, event):
         """Savegame renamed."""
-        maPattern, newName = self.validate_filename(event,
-                                                    add_ext=bush.game.ess.ext)
+        ext_group = u'(\.(' + bush.game.ess.ext[1:] + u'|' + \
+                    bush.game.ess.ext[1:-1] + u'r' + u'))'
+        maPattern, newName = self.validate_filename(event, ext=ext_group)
         if not maPattern: return

I now check for the correct extension.

- fixes a regression introduced in
325f3bc0b7f7caa64e4e6df04394a3534b7666d1:

@@ -1840,3 +1833,3 @@ def OnLabelEdited(self, event):
                         break
-        self.RefreshUI(files=to_select)
+        self.RefreshUI()
         #--Reselect the items - renamed or not

Dunno why I thought removed items was magically solved.

- note I need to double quote backslashes in regexes:

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -433,7 +433,8 @@ tab. The directories skipped are:
         <tr><td>TES Alliance...<td>Attempts to open the selected package's page on TES Alliance. This command assumes that the trailing digits in a package's name is the package ID at the site.
         <tr><td colspan="2" id="bainHide">Hide...<td>Hides the package in the list, and moves it to the
             <code>[Game] Mods/Bash Mod Data/Hidden</code> folder.
-        <tr><td colspan="2" id="bainRename">Rename...<td>Renames the selected package or marker.
+        <tr><td colspan="2" id="bainRename">Rename...<td>Renames the selected package(s) or marker(s). All selected items must be of the
+            same type. For archives the extension will remain the same.
         <tr><td colspan="2" id="bainRefresh">Refresh<td>As <a href="#bainRefresh">Quick Refresh</a> below. Refresh will however recalculate the
             <abbr title="Cyclic redundancy check, digital signature of the file">CRCs</abbr> for all selected items <i>from scratch</i>, which is not what you usually need (for projects
             especially it will take much longer than for packages, cause to calculate the CRC of a project <i>all the individual CRCs</i> of <i>all</i> the files in the project folder must be
@@ -1661,7 +1662,8 @@ and masterlist relative priority and role see the LOOT docs.
         <tr><td>Hide<td>Moves the selected save file to the <code>My Games\[Game]\Saves\[optional profile dir\]Bash\Hidden</code> directory.
         <tr><td>Revert to Backup<td>Reverts the selected save file to the last backup made of it.
         <tr><td>Revert to First Backup<td>Reverts the selected save file to the first backup made of it.
-        <tr><td>Rename...<td>Rename the selected save file.
+        <tr><td>Rename...<td>Rename the selected save file. bak files are not supported and you can't change the extension (specifying
+            another extension will make the rename fail)
         <tr><td>Re-number Save(s)...<td>Allows the re-numbering of the selected save file(s). If more than one file is selected, the first
             will be renumbered to the given number and the others will be numbered in increments of one from the given number. The save name
             must be of the form <code>&lt;Save (case insensitive)>&lt;space>&lt;some digits>&lt;optional text>.&lt;save extension></code>
@@ -1785,7 +1787,10 @@ and masterlist relative priority and role see the LOOT docs.
     <tbody>
         <tr><td>Delete<td>Deletes the selected file(s).
         <tr><td>Convert<td>Converts the selected files to one of the following formats: JPEG, PNG, BMP, TIFF. If a file of the conversion format already exists with the same name as a file to be converted, the conversion will not take place for that file.
-        <tr><td>Rename<td>Renames the selected file(s). If multiple files are selected, they will be numbered sequentially. If a file already exists with the same name as a file's resulting name, that file will not be renamed.
+        <tr><td>Rename<td>Renames the selected file(s). If multiple files are selected, they will be numbered sequentially. If a file
+            already exists with the same name as a file's resulting name, that file will not be renamed. The extension will remain the same
+            and specifying a non image extension will make the rename operation fail - in short, leave the extension alone. Empty filenames
+            will also abort the rename.
 </table>
 
 <h2 id="launchers">Advanced Launchers</h2>
@@ -2019,6 +2024,7 @@ and masterlist relative priority and role see the LOOT docs.
         <tr><td><code>ctrl-o</code>
             <td>Opens the related data directory - for instance in the mods tab will open "Data/", in Ini Edits will open "Ini Tweaks/" etc.
         <tr><td><code>ctrl-c</code><td>Copies files to the clipboard (Mods, Saves, Installers, Screens)
+        <tr><td><code>F2</code><td>Rename file(s) (Saves, Installers, Screens) - see Rename menu items for details
         <tr><td><code>ctrl-z</code><td>Undo load order state change (Mods)
         <tr><td><code>ctrl-y</code><td>Redo load order state change (Mods)
 </table>

Mopy/bash/balt.py
@@ -23,6 +23,8 @@
 # =============================================================================
 
 # Imports ---------------------------------------------------------------------
+import re
+
 import bass # for dirs - try to avoid
 #--Localization
 #..Handled by bolt, so import that.
@@ -2108,6 +2110,34 @@ class UIList(wx.Panel):
             index = self.GetIndex(selected[0])
             if index != -1:
                 self.__gList.EditLabel(index)
+
+    def validate_filename(self, event, has_digits=False, ext=u'',
+                          is_filename=True):
+        if event.IsEditCancelled(): return None, None
+        newName = event.GetLabel()
+        if not newName:
+            msg = _(u'Empty name !')
+            maPattern = None
+        else:
+            char = is_filename and bolt.Path.has_invalid_chars(newName)
+            if char:
+                msg = _(u'%(new_name)s contains invalid character (%(char)s)'
+                        ) % {'new_name': newName, 'char': char}
+                maPattern = None
+            else:
+                msg = _(u'Bad extension or file root: ') + newName
+                if ext: # require at least one char before extension
+                    regex = u'^(?=.+\.)(.*?)'
+                else:
+                    regex = u'^(.*?)'
+                if has_digits: regex += u'(\d*)'
+                regex += ext + u'$'
+                rePattern = re.compile(regex, re.I | re.U)
+                maPattern = rePattern.match(newName)
+        if not maPattern:
+            showError(self, msg)
+            event.Veto()
+        return maPattern, newName
 
     @conversation
     def DeleteItems(self, event=None, items=None,

Mopy/bash/basher/__init__.py
@@ -1802,19 +1802,12 @@ class SaveList(balt.UIList):
 
     def OnLabelEdited(self, event):
         """Savegame renamed."""
-        if event.IsEditCancelled(): return
-        #--File Info
-        newName = event.GetLabel()
+        ext_group = u'(\.(' + bush.game.ess.ext[1:] + u'|' + \
+                    bush.game.ess.ext[1:-1] + u'r' + u'))'
+        maPattern, newName = self.validate_filename(event, ext=ext_group)
+        if not maPattern: return
         detail_item = self.panel.GetDetailsItem()
         item_edited = detail_item.name if detail_item else None
-        if not newName.lower().endswith(bush.game.ess.ext):
-            newName += bush.game.ess.ext
-        rePattern = re.compile(ur'^[^/\\:*?"<>|]+?$', re.I | re.U)
-        maPattern = rePattern.match(newName)
-        if not maPattern:
-            balt.showError(self, _(u'Bad extension or file root: ') + newName)
-            event.Veto()
-            return
         newFileName = newName
         selected = self.GetSelected()
         to_select = set(selected)
@@ -1846,7 +1839,7 @@ class SaveList(balt.UIList):
                                 new.moveTo(old)
                             if new.exists() and old.exists(): new.remove()
                         break
-        self.RefreshUI(files=to_select)
+        self.RefreshUI()
         #--Reselect the items - renamed or not
         self.SelectItemsNoCallback(to_select)
         if item_edited: self.SelectItem(item_edited)
@@ -2173,6 +2166,8 @@ class InstallersList(balt.UIList):
         installer_type = type(self.data_store[to_rename[0]])
         for item in to_rename[1:]:
             if not isinstance(self.data_store[item], installer_type):
+                balt.showError(self, _(
+                    u"Bash can't rename mixed installers types"))
                 event.Veto()
                 return
             #--Also, don't allow renaming the 'Last' marker
@@ -2217,19 +2212,23 @@ class InstallersList(balt.UIList):
 
     def OnLabelEdited(self, event):
         """Renamed some installers"""
-        if event.IsEditCancelled(): return
-        newName = event.GetLabel()
         selected = self.GetSelected()
-        maPattern = self.data_store[selected[0]].match_valid_name(newName)
-        if not maPattern:
-            balt.showError(self, _(u'Bad extension or file root: ') + newName)
-            event.Veto()
-            return
+        installables = self.data_store.filterInstallables(selected)
+        validate = partial(self.validate_filename, event, has_digits=True,
+                           is_filename=bool(installables))
+        patterns = []
+        for inst in selected:
+            if isinstance(self.data_store[inst], bosh.InstallerArchive):
+                maPattern, newName = validate(ext=u'((\.(7z|rar|zip|001))+)')
+            else:
+                maPattern, newName = validate()
+            if not maPattern: return
+            patterns.append(maPattern)
         #--Rename each installer, keeping the old extension (for archives)
         with balt.BusyCursor():
             refreshes, ex = [(False, False, False)], None
             try:
-                self.data_store.batchRename(selected, maPattern, refreshes)
+                self.data_store.batchRename(selected, patterns, refreshes)
             except (OSError, IOError) as ex:
                 pass
             finally:
@@ -3048,18 +3047,11 @@ class ScreensList(balt.UIList):
         self.OpenSelected(selected=[hitItem])
 
     def OnLabelEdited(self, event):
-        """Renamed a screenshot"""
-        if event.IsEditCancelled(): return
-        newName = event.GetLabel()
+        """Rename selected screenshots."""
+        maPattern, _newName = self.validate_filename(event, has_digits=True,
+         ext=u'(\.(' + ur'|'.join(ext[1:] for ext in bosh.imageExts) + u')+)')
+        if not maPattern: return
         selected = self.GetSelected()
-        rePattern = re.compile(
-            ur'^([^/\\:*?"<>|]+?)(\d*)((\.(jpg|jpeg|png|tif|bmp))+)$',
-            re.I | re.U)
-        maPattern = rePattern.match(newName)
-        if not maPattern:
-            balt.showError(self,_(u'Bad extension or file root: ')+newName)
-            event.Veto()
-            return
         root,numStr = maPattern.groups()[:2]
         #--Rename each screenshot, keeping the old extension
         num = int(numStr or  0)

Mopy/bash/basher/installer_links.py
@@ -425,7 +425,8 @@ class Installer_Hide(_InstallerLink, UIList_Hide):
 
 class Installer_Rename(UIList_Rename, _InstallerLink):
     """Renames files by pattern."""
-    help = _(u"Rename selected installer(s).")
+    help = _(u"Rename selected installer(s).") + u'  ' + _(
+        u'All selected installers must be of the same type')
 
     def _enable(self):
         ##Only enable if all selected items are of the same type

Mopy/bash/bolt.py
@@ -516,6 +516,7 @@ class Path(object):
 
     #--Class Vars/Methods -------------------------------------------
     sys_fs_enc = sys.getfilesystemencoding() or 'mbcs'
+    invalid_chars_re = re.compile(ur'(.*)([/\\:*?"<>|]+)(.*)', re.I | re.U)
 
     @staticmethod
     def getNorm(name):
@@ -539,6 +540,12 @@ class Path(object):
     def setcwd(self):
         """Set cwd."""
         os.chdir(self._s)
+
+    @staticmethod
+    def has_invalid_chars(string):
+        match = Path.invalid_chars_re.match(string)
+        if not match: return None
+        return match.groups()[1]
 
     #--Instance stuff --------------------------------------------------
     #--Slots: _s is normalized path. All other slots are just pre-calced

Mopy/bash/bosh/__init__.py
@@ -105,6 +105,7 @@ _reEsmExt  = re.compile(ur'\.esm(.ghost)?$', re.I | re.U)
 reEspExt  = re.compile(ur'\.esp(.ghost)?$',re.I|re.U)
 reTesNexus = re.compile(ur'(.*?)(?:-(\d{1,6})(?:\.tessource)?(?:-bain)?(?:-\d{0,6})?(?:-\d{0,6})?(?:-\d{0,6})?(?:-\w{0,16})?(?:\w)?)?(\.7z|\.zip|\.rar|\.7z\.001|)$',re.I|re.U)
 reTESA = re.compile(ur'(.*?)(?:-(\d{1,6})(?:\.tessource)?(?:-bain)?)?(\.7z|\.zip|\.rar|)$',re.I|re.U)
+imageExts = {u'.gif', u'.jpg', u'.png', u'.jpeg', u'.bmp', u'.tif'}
 
 #------------------------------------------------------------------------------
 # Save I/O --------------------------------------------------------------------
@@ -4366,7 +4367,9 @@ class PeopleData(_DataStore):
 
 #------------------------------------------------------------------------------
 class ScreensData(_DataStore):
-    reImageExt = re.compile(ur'\.(bmp|jpg|jpeg|png|tif|gif)$', re.I | re.U)
+    reImageExt = re.compile(
+        ur'\.(' + ur'|'.join(ext[1:] for ext in imageExts) + ur')$',
+        re.I | re.U)
 
     def __init__(self):
         self.store_dir = dirs['app']
@@ -4433,12 +4436,10 @@ class Installer(object):
     reReadMe = re.compile(
         ur'^.*?([^\\]*)(read[ _]?me|lisez[ _]?moi)([^\\]*)'
         ur'(' +ur'|'.join(docExts) + ur')$', re.I | re.U)
-    reValidNamePattern = re.compile(ur'^([^/\\:*?"<>|]+?)(\d*)$', re.I | re.U)
     skipExts = {u'.exe', u'.py', u'.pyc', u'.7z', u'.zip', u'.rar', u'.db',
                 u'.ace', u'.tgz', u'.tar', u'.gz', u'.bz2', u'.omod',
                 u'.fomod', u'.tb2', u'.lzma', u'.manifest'}
     skipExts.update(set(readExts))
-    imageExts = {u'.gif', u'.jpg', u'.png', u'.jpeg', u'.bmp'}
     scriptExts = {u'.txt', u'.ini', u'.cfg'}
     commonlyEditedExts = scriptExts | {u'.xml'}
     #--Regular game directories - needs update after bush.game has been set
@@ -4762,7 +4763,7 @@ class Installer(object):
             Installer._global_start_skips.append(bush.game.se.shortName.lower() + os_sep)
             Installer._global_skip_extensions |= Installer._executables_ext
         if settings['bash.installers.skipImages']:
-            Installer._global_skip_extensions |= Installer.imageExts
+            Installer._global_skip_extensions |= imageExts
         Installer._init_executables_skips()
 
     @staticmethod
@@ -4941,7 +4942,6 @@ class Installer(object):
         archiveRoot = GPath(self.archive).sroot if isinstance(self,
                 InstallerArchive) else self.archive
         docExts = self.docExts
-        imageExts = self.imageExts
         docDirs = self.docDirs
         dataDirsPlus = self.dataDirsPlus
         dataDirsMinus = self.dataDirsMinus
@@ -5278,9 +5278,6 @@ class Installer(object):
         return self.status != oldStatus or self.underrides != oldUnderrides
 
     #--Utility methods --------------------------------------------------------
-    def match_valid_name(self, newName):
-        return self.reValidNamePattern.match(newName)
-
     def size_or_mtime_changed(self, apath):
         return (self.size, self.modified) != apath.size_mtime()
 
@@ -5380,7 +5377,6 @@ class InstallerMarker(Installer):
     """Represents a marker installer entry."""
     __slots__ = tuple() #--No new slots
     type_string = _(u'Marker')
-    reValidNamePattern = re.compile(ur'^(.+?)(\d*)$', re.I | re.U)
 
     def __init__(self,archive):
         Installer.__init__(self,archive)
@@ -5425,8 +5421,6 @@ class InstallerArchiveError(bolt.BoltError): pass
 class InstallerArchive(Installer):
     """Represents an archive installer entry."""
     __slots__ = tuple() #--No new slots
-    reValidNamePattern = re.compile(
-        ur'^([^/\\:*?"<>|]+?)(\d*)((\.(7z|rar|zip|001))+)$', re.I | re.U)
     type_string = _(u'Archive')
 
     #--File Operations --------------------------------------------------------
@@ -5996,14 +5990,16 @@ class InstallersData(_DataStore):
             self.converters_data.save()
             self.hasChanged = False
 
-    def batchRename(self, selected, maPattern, refreshNeeded):
-        root, numStr = maPattern.groups()[:2]
+    def batchRename(self, selected, patterns, refreshNeeded):
+        root_numStrings = [pattern.groups()[:2] for pattern in patterns]
+        numStr = root_numStrings[0][1]
         num = int(numStr or  0)
         digits = len(str(num + len(selected)))
         if numStr: numStr.zfill(digits)
-        for archive in selected:
+        for archive, root_numStr in zip(selected, root_numStrings):
             refreshNeeded.append(
-                self[archive].renameInstaller(archive, root, numStr, self))
+                self[archive].renameInstaller(archive, root_numStr[0], numStr,
+                                              self))
             num += 1
             numStr = unicode(num).zfill(digits)
 