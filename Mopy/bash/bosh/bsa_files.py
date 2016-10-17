4b8549973375e7f1f46c63a7820f30008a413957
MrD <the.ubik@gmail.com>
2016-10-17 06:05:47 +0200

    Read oblivion bsa:
    https://github.com/wrye-bash/test_bash/commit/fa13c4826bbf458c25f5894c65d6e6c4a27b0510

--- Mopy/bash/bosh/bsa_files.py
+++ Mopy/bash/bosh/bsa_files.py
@@ -22,10 +22,17 @@
 #
 # =============================================================================
 
-"""Bsa files."""
+"""Bsa files.
+
+For the file format see:
+http://www.uesp.net/wiki/Tes4Mod:BSA_File_Format
+http://www.uesp.net/wiki/Tes5Mod:Archive_File_Format
+"""
 
 __author__ = 'Utumno'
 
+import collections
+import os
 import struct
 
 class BSAHeader(object):
@@ -34,16 +41,121 @@ class BSAHeader(object):
         'folder_count', 'file_count', 'total_folder_name_length',
         'total_file_name_length', 'file_flags', )
 
-    def read_header(self, ins):
+    def load_header(self, ins):
         for attr in BSAHeader.__slots__:
             self.__setattr__(attr, struct.unpack('I', ins.read(4))[0])
 
-class BSAFolderRecord(object):
-    __slots__ = ('folder_name_hash', 'files_count', 'file_records_offset', )
+    def has_names_for_folders(self): return self.archive_flags & 1
+    def has_names_for_files(self): return self.archive_flags & 2
+    def is_compressed(self): return self.archive_flags & 4
+    def is_xbox(self): return self.archive_flags & 64
 
-class BSAFileRecord(object):
-    __slots__ = ('file_name_hash', 'file_data_size', 'raw_file_data_offset', )
+class _HashedRecord(object):
+    __slots__ = ('hash',)
+
+    def load_record(self, ins):
+        self.hash = struct.unpack('Q', ins.read(8))[0]
+        for attr in self.__class__.__slots__:
+            self.__setattr__(attr, struct.unpack('I', ins.read(4))[0])
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__): return self.hash == other.hash
+        return NotImplemented
+    def __ne__(self, other): return not (self == other)
+    def __hash__(self): return self.hash
+    def __lt__(self, other):
+        if isinstance(other, self.__class__): return self.hash < other.hash
+        return NotImplemented
+    def __ge__(self, other): return not (self < other)
+    def __gt__(self, other):
+        if isinstance(other, self.__class__): return self.hash > other.hash
+        return NotImplemented
+    def __le__(self, other): return not (self > other)
+
+class BSAFolderRecord(_HashedRecord):
+    __slots__ = ('files_count', 'file_records_offset',)
+
+class BSAFileRecord(_HashedRecord):
+    __slots__ = ('file_data_size', 'raw_file_data_offset',)
+
+    def load_record_from_buffer(self, memview, start):
+        self.hash = struct.unpack_from('Q', memview, start)[0]
+        start += 8
+        for attr in self.__class__.__slots__:
+            self.__setattr__(attr, struct.unpack_from('I', memview, start)[0])
+            start += 4
+        return start
+
+class BSAFolder(object):
+
+    def __init__(self, folder_record):
+        self.folder_record = folder_record
+        self.assets = collections.OrderedDict() # keep files order
+
+    def __repr__(self):
+        return repr(tuple(repr(a) for a in self.assets.itervalues()))
+
+class BSAAsset(object):
+
+    def __init__(self, filename, filerecord):
+        self.filerecord = filerecord
+        self.filename = filename
+
+    def __repr__(self): return repr(self.filename)
+    def __unicode__(self): return unicode(self.filename)
 
 class BSA(object):
-    __slots__ = ('bsa_header', 'folder_records', 'file_record_blocks',
-                 'file_name_block', 'data_block', )
+
+    def __init__(self, abs_path):
+        self.bsa_header = BSAHeader()
+        folder_records = []
+        self.bsa_folders = collections.OrderedDict() # keep folder order
+        with open(abs_path, 'rb') as bsa_file:
+            # load the header from input stream
+            self.bsa_header.load_header(bsa_file)
+            # load the folder records from input stream
+            for __ in xrange(self.bsa_header.folder_count):
+                rec = BSAFolderRecord()
+                rec.load_record(bsa_file)
+                folder_records.append(rec)
+            # load the file record block to parse later
+            has_folder_names = self.bsa_header.has_names_for_folders()
+            file_records_block_size = 16 * self.bsa_header.file_count
+            if has_folder_names:
+                file_records_block_size += ( # one byte for each folder name's size
+                    self.bsa_header.folder_count
+                    + self.bsa_header.total_folder_name_length)
+            file_records_block = memoryview(bsa_file.read(file_records_block_size))
+            # load the file names block
+            file_names_block = None
+            if self.bsa_header.has_names_for_files():
+                file_names_block = memoryview(bsa_file.read(
+                    self.bsa_header.total_file_name_length))
+            # close the file
+        file_records_index = names_record_index = 0
+        for folder_record in folder_records:
+            folder_path = u'?%d' % folder_record.hash # hack - untested
+            if has_folder_names:
+                name_size = struct.unpack_from('B', file_records_block, file_records_index)[0]
+                ## TODO: decode
+                # discard null terminator below
+                folder_path = unicode(struct.unpack_from('%ds' % (name_size - 1),
+                    file_records_block, file_records_index + 1)[0])
+                file_records_index += name_size + 1
+            current_folder = self.bsa_folders.setdefault(
+                folder_path, BSAFolder(folder_record)) # type: BSAFolder
+            for __ in xrange(folder_record.files_count):
+                rec = BSAFileRecord()
+                file_records_index = rec.load_record_from_buffer(
+                    file_records_block, file_records_index)
+                file_name = u'?%d' % rec.hash
+                if file_names_block is not None:
+                    file_name_len = 0
+                    for b in file_names_block[names_record_index:]:
+                        if b != '\x00': file_name_len += 1
+                        else: break
+                    file_name = unicode(struct.unpack_from('%ds' % file_name_len,
+                        file_names_block,names_record_index)[0])
+                    names_record_index += file_name_len + 1
+                current_folder.assets[file_name] = BSAAsset(
+                    os.path.sep.join((folder_path, file_name)), rec)
