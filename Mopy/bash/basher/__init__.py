wrye-bash/wrye-bash

commit d09b43476b345591e5aa2ef9247e888bb08ed395

[!]_DataStore.rename_info and using shellMove WIP:
This changes quite a few things:

- defining _rename_operation to use shellMove. FileInfos was using this
_but not for Cosaves_ while screens and installers used moveTo
- the error reverting code in rename_info applies to moveTo - kept for
the non shell path in env - but needs revisiting. For one our home
brewed exceptions only serve to confuse - I made them inherit from
OSError but that's a hack - they must be binned - under # 220
- installers are convoluted - I need to bin refreshInstaller somehow
- _rename_operation in ScreensData takes care of refresh
- new_name should be probably be moved to Infos
- probably the newPath.exists()) checks should be binned - note it's
inside Installer._installer_rename
- had to make SkipError(CancelError) - constructor arguments never used,
and except (OSError, IOError, CancelError, SkipError) is a bit too much.

Note very welcome binning of SaveInfos rename override. Most of the
CoSave stuff must follow this route. Initially I had moved only the
save rename code to save infos - from that commit:

- `if newFileName != save_key:` was not needed at all as
event.IsEventCancelled would return true if absolutely no changes were
made - plus new_name ensures the new name is unique.

The [!] is for exceptions.

Mopy/bash/basher/__init__.py
@@ -1417,8 +1417,11 @@ class ModDetails(_SashDetailsPanel):
                 ):
                 return
             settings.getChanged('bash.mods.renames')[oldName] = newName
-            bosh.modInfos.rename(oldName,newName)
-            fileName = newName
+            try:
+                bosh.modInfos.rename_info(oldName, newName)
+                fileName = newName
+            except (CancelError, OSError, IOError):
+                pass
         #--Change hedr/masters?
         if changeHedr or changeMasters:
             modInfo.header.author = self.authorStr.strip()
@@ -1819,38 +1822,19 @@ class SaveList(balt.UIList):
         root, newName, _numStr = self.validate_filename(event)
         if not root: return
         detail_item = self.panel.GetDetailsItem()
-        item_edited = detail_item.name if detail_item else None
+        item_edited = [detail_item.name if detail_item else None]
         selected = [s for s in self.GetSelected() if
                     not bosh.saveInfos.bak_file_pattern.match(s.s)] # YAK !
         to_select = set()
         for save_key in selected:
             newFileName = self.new_name(newName)
-            if newFileName != save_key:
-                oldPath = bosh.saveInfos.store_dir.join(save_key)
-                newPath = bosh.saveInfos.store_dir.join(newFileName)
-                renames = [(oldPath, newPath)]
-                renames.extend(CoSaves.get_new_paths(oldPath, newPath))
-                if not newPath.exists():
-                    try:
-                        bosh.saveInfos.rename(save_key, newFileName)
-                        to_select.add(newFileName)
-                        if save_key == item_edited:
-                            item_edited = newFileName
-                    except (OSError, IOError):
-                        deprint('Renaming %s to %s failed'
-                                % (oldPath, newPath), traceback=True)
-                        ##: WindowsError:[Error 32]The process cannot access...
-                        for old, new in renames:
-                            if new.exists() and not old.exists():
-                                # some cosave move failed, restore files
-                                new.moveTo(old)
-                            if new.exists() and old.exists(): new.remove()
-                        break
+            if not self._try_rename(save_key, newFileName, to_select,
+                                    item_edited): break
         if to_select:
             self.RefreshUI()
             #--Reselect the renamed items
             self.SelectItemsNoCallback(to_select)
-            if item_edited: self.SelectItem(item_edited)
+            if item_edited[0]: self.SelectItem(item_edited[0])
         event.Veto() # needed ! clears new name from label on exception
 
     @staticmethod
@@ -2045,7 +2029,10 @@ class SaveDetails(_SashDetailsPanel):
         #--Change Name?
         if changeName:
             (oldName,newName) = (saveInfo.name,GPath(self.fileStr.strip()))
-            bosh.saveInfos.rename(oldName,newName)
+            try:
+                bosh.saveInfos.rename_info(oldName, newName)
+            except (CancelError, OSError, IOError):
+                pass
         #--Change masters?
         if changeMasters:
             saveInfo.header.masters = self.uilist.GetNewMasters()
@@ -2241,10 +2228,9 @@ class InstallersList(balt.UIList):
                 for package in selected:
                     name_new = self.new_name(newName)
                     refreshes.append(
-                        self.data_store[package].renameInstaller(
-                            name_new, self.data_store))
+                        self.data_store.rename_info(package, name_new))
                     if refreshes[-1][0]: newselected.append(name_new)
-            except (OSError, IOError) as ex:
+            except (CancelError, OSError, IOError) as ex:
                 pass
             finally:
                 refreshNeeded, modsRefresh, iniRefresh = [
@@ -3075,30 +3061,17 @@ class ScreensList(balt.UIList):
         num = int(numStr or  0)
         digits = len(str(num + len(selected)))
         if numStr: numStr.zfill(digits)
-        screensDir = bosh.screensData.store_dir
         with balt.BusyCursor():
-            newselected = []
+            to_select = set()
             for screen in selected:
                 newName = GPath(root + numStr + screen.ext)
-                newPath = screensDir.join(newName)
-                oldPath = screensDir.join(screen)
-                if not newPath.exists():
-                    try:
-                        oldPath.moveTo(newPath)
-                        newselected.append(newName)
-                    except (OSError, IOError):
-                        deprint('Renaming %s to %s failed'
-                                % (oldPath, newPath), traceback=True)
-                        ##: WindowsError:[Error 32]The process cannot access...
-                        if newPath.exists() and oldPath.exists():
-                            newPath.remove()
-                        break
+                if not self._try_rename(screen, newName, to_select): break
                 num += 1
                 numStr = unicode(num).zfill(digits)
-            bosh.screensData.refresh()
-            self.RefreshUI()
-            #--Reselected the renamed items
-            self.SelectItemsNoCallback(newselected)
+            if to_select:
+                self.RefreshUI()
+                #--Reselected the renamed items
+                self.SelectItemsNoCallback(to_select)
             event.Veto()
 
     def OnChar(self,event):
@@ -3298,7 +3271,7 @@ class BSADetails(_EditableMixinOnFileInfos, SashPanel):
         #--Change Name?
         if changeName:
             (oldName,newName) = (BSAInfo.name,GPath(self.fileStr.strip()))
-            bosh.bsaInfos.rename(oldName,newName)
+            bosh.bsaInfos.rename_info(oldName, newName)
         #--Done
         try:
             bosh.bsaInfos.refreshFile(BSAInfo.name)

Mopy/bash/bosh/__init__.py
@@ -2948,6 +2948,35 @@ class _DataStore(DataDict):
         return deleted
     def refresh(self): raise AbstractError
     def save(self): pass # for Screenshots
+    # Renaming
+    def rename_info(self, oldName, newName):
+        try:
+            self._rename_operation(oldName, newName)
+        except (CancelError, OSError, IOError):
+            deprint(u'Renaming %s to %s failed' % (oldName, newName),
+                    traceback=True)
+            # When using moveTo I would get "WindowsError:[Error 32]The process
+            # cannot access ..." -  the code below was reverting the changes.
+            # With shellMove I mostly get CancelError so below not needed -
+            # except if a save is locked and user presses Skip - so cosaves are
+            # renamed! Error handling is still a WIP
+            for old, new in self._get_rename_paths(oldName, newName):
+                if new.exists() and not old.exists():
+                    # some cosave move failed, restore files
+                    new.moveTo(old)
+                if new.exists() and old.exists():
+                    # move copies then deletes, so the delete part failed
+                    new.remove()
+            raise
+
+    def _rename_operation(self, oldName, newName):
+        rename_paths = self._get_rename_paths(oldName, newName)
+        for tup in rename_paths[1:]: # if cosaves do not exist shellMove fails!
+            if not tup[0].exists(): rename_paths.remove(tup)
+        env.shellMove(*zip(*rename_paths))
+
+    def _get_rename_paths(self, oldName, newName):
+        return [tuple(map(self.store_dir.join, (oldName, newName)))]
 
     @property
     def bash_dir(self):
@@ -3069,18 +3098,19 @@ class FileInfos(_DataStore):
         """
         return cls.file_pattern.search(u'%s' % fileName)
 
+    def _get_rename_paths(self, oldName, newName):
+        return [(self[oldName].getPath(), self.store_dir.join(newName))]
+
     #--Rename
-    def rename(self,oldName,newName):
+    def _rename_operation(self, oldName, newName):
         """Renames member file from oldName to newName."""
         #--Update references
         fileInfo = self[oldName]
         #--File system
-        newPath = self.store_dir.join(newName)
         try:
-            if fileInfo.isGhost: newPath += u'.ghost'
+            if fileInfo.isGhost: newName += u'.ghost'
         except AttributeError: pass # not a mod info
-        oldPath = fileInfo.getPath()
-        env.shellMove(oldPath, newPath, parent=None)
+        _DataStore._rename_operation(self, oldName, newName)
         #--FileInfo
         fileInfo.name = newName
         #--FileInfos
@@ -3987,11 +4017,11 @@ class ModInfos(FileInfos):
         self._lo_wip = [x for x in self._lo_wip if x not in to_remove]
         self._active_wip  = [x for x in self._active_wip if x not in to_remove]
 
-    def rename(self,oldName,newName):
+    def _rename_operation(self, oldName, newName):
         """Renames member file from oldName to newName."""
         isSelected = load_order.isActiveCached(oldName)
         if isSelected: self.lo_deactivate(oldName, doSave=False) # will save later
-        FileInfos.rename(self,oldName,newName)
+        FileInfos._rename_operation(self, oldName, newName)
         # rename in load order caches
         oldIndex = self._lo_wip.index(oldName)
         self._lo_caches_remove_mods([oldName])
@@ -4195,10 +4225,10 @@ class SaveInfos(FileInfos):
         kwargs['backupDir'] = self.bash_dir.join('Backups')
         CoSaves(self.store_dir, fileName).delete(**kwargs)
 
-    def rename(self,oldName,newName):
-        """Renames member file from oldName to newName."""
-        FileInfos.rename(self,oldName,newName)
-        CoSaves(self.store_dir, oldName).move(self.store_dir, newName)
+    def _get_rename_paths(self, oldName, newName):
+        renames = [tuple(map(self.store_dir.join, (oldName, newName)))]
+        renames.extend(CoSaves.get_new_paths(*renames[0]))
+        return renames
 
     def copy_info(self, fileName, destDir, destName=empty_path, set_mtime=None):
         """Copies savefile and associated pluggy file."""
@@ -4279,8 +4309,11 @@ class SaveInfos(FileInfos):
             return fileName
         (root,ext) = fileName.rootExt
         newName = root + (bush.game.ess.ext if value else ext[:-1] + u'r')
-        self.rename(fileName,newName)
-        return newName
+        try:
+            self.rename_info(fileName, newName)
+            return newName
+        except (CancelError, OSError, IOError):
+            return fileName
 
 #------------------------------------------------------------------------------
 class BSAInfos(FileInfos):
@@ -4405,6 +4438,11 @@ class ScreensData(_DataStore):
             if not item.exists(): del self[item.tail]
 
     def delete_Refresh(self, deleted, check_existence=False): self.refresh()
+
+    def _rename_operation(self, oldName, newName):
+        _DataStore._rename_operation(self, oldName, newName)
+        self[newName] = self[oldName]
+        del self[oldName]
 
 #------------------------------------------------------------------------------
 os_sep = unicode(os.path.sep)
@@ -5281,35 +5319,26 @@ class Installer(object):
     def size_or_mtime_changed(self, apath):
         return (self.size, self.modified) != apath.size_mtime()
 
-    def _rename(self, data, newName):
+    def _installer_rename(self, data, newName):
         """Rename package or project."""
         g_path = GPath(self.archive)
         if newName != g_path:
             newPath = dirs['installers'].join(newName)
             if not newPath.exists():
-                oldPath = dirs['installers'].join(self.archive)
-                try:
-                    oldPath.moveTo(newPath)
-                except (OSError, IOError):
-                    deprint('Renaming %s to %s failed' % (oldPath, newPath),
-                            traceback=True)
-                    ##: WindowsError: [Error 32] The process cannot access...
-                    if newPath.exists() and oldPath.exists():
-                        newPath.remove()
-                    raise
-                self.archive = newName.s
+                _DataStore._rename_operation(data, g_path, newName)
                 #--Add the new archive to Bash and remove old one
                 data[newName] = self
                 del data[g_path]
                 #--Update the iniInfos & modInfos for 'installer'
                 mfiles = [x for x in modInfos.table.getColumn('installer') if
-                          modInfos.table[x]['installer'] == oldPath.stail]
+                          modInfos.table[x]['installer'] == self.archive]
                 ifiles = [x for x in iniInfos.table.getColumn('installer') if
-                          iniInfos.table[x]['installer'] == oldPath.stail]
+                          iniInfos.table[x]['installer'] == self.archive]
+                self.archive = newName.s # don't forget to rename !
                 for i in mfiles:
-                    modInfos.table[i]['installer'] = newPath.stail
+                    modInfos.table[i]['installer'] = self.archive
                 for i in ifiles:
-                    iniInfos.table[i]['installer'] = newPath.stail
+                    iniInfos.table[i]['installer'] = self.archive
                 return True, bool(mfiles), bool(ifiles)
         return False, False, False
 
@@ -5577,7 +5606,8 @@ class InstallerArchive(Installer):
                 os.sep if isdir else u''))
 
     def renameInstaller(self, name_new, data):
-        return self._rename(data, name_new.root + GPath(self.archive).ext)
+        return self._installer_rename(data,
+                                      name_new.root + GPath(self.archive).ext)
 
     def open_readme(self):
         with balt.BusyCursor():
@@ -5847,7 +5877,7 @@ class InstallerProject(Installer):
         walkPath(apath.s, 0)
 
     def renameInstaller(self, name_new, data):
-        return self._rename(data, name_new)
+        return self._installer_rename(data, name_new)
 
     def open_readme(self):
         bass.dirs['installers'].join(self.archive, self.hasReadme).start()
@@ -5986,6 +6016,9 @@ class InstallersData(_DataStore):
             self.dictFile.save()
             self.converters_data.save()
             self.hasChanged = False
+
+    def _rename_operation(self, oldName, newName):
+        return self[oldName].renameInstaller(newName, self)
 
     #--Dict Functions -----------------------------------------------------------
     def delete(self, items, **kwargs):

Mopy/bash/balt.py
@@ -1889,6 +1889,18 @@ class UIList(wx.Panel):
         (self.__gList.GetEditControl()).SetSelection(0, to)
     def OnLabelEdited(self,event): event.Skip()
 
+    def _try_rename(self, key, newFileName, to_select, item_edited=None):
+        newPath = self.data_store.store_dir.join(newFileName)
+        if not newPath.exists():
+            try:
+                self.data_store.rename_info(key, newFileName)
+                to_select.add(newFileName)
+                if item_edited and key == item_edited[0]:
+                    item_edited[0] = newFileName
+            except (CancelError, OSError, IOError):
+                return False # break
+        return True # continue
+
     def _getItemClicked(self, event, on_icon=False):
         (hitItem, hitFlag) = self.__gList.HitTest(event.GetPosition())
         if hitItem < 0 or (on_icon and hitFlag != wx.LIST_HITTEST_ONITEMICON):

Mopy/bash/basher/saves_links.py
@@ -36,7 +36,8 @@ from .. import bass, bosh, bolt, balt, bush, parsers, load_order
 from ..bass import Resources
 from ..balt import EnabledLink, AppendableLink, Link, CheckLink, ChoiceLink, \
     ItemLink, SeparatorLink, OneItemLink, Image, UIList_Rename
-from ..bolt import GPath, ArgumentError, SubProgress, BoltError, formatInteger
+from ..bolt import GPath, ArgumentError, SubProgress, BoltError, formatInteger, \
+    CancelError
 from ..bosh import faces
 
 __all__ = ['Saves_Profiles', 'Save_Rename', 'Save_Renumber', 'Save_Move',
@@ -369,7 +370,10 @@ class Save_Renumber(EnabledLink):
             newFileName = u"%s%d%s" % (maPattern[0],newNumber,maPattern[2])
             if newFileName != name.s:
                 new_file_path = GPath(newFileName)
-                bosh.saveInfos.rename(name, new_file_path)
+                try:
+                    bosh.saveInfos.rename_info(name, new_file_path)
+                except (CancelError, OSError, IOError):
+                    break
                 newNumber += 1
                 to_select.append(new_file_path)
         if to_select:

Mopy/bash/bolt.py
@@ -385,14 +385,10 @@ class CancelError(BoltError):
     def __init__(self,message=u'Action aborted by user.'):
         BoltError.__init__(self, message)
 
-class SkipError(BoltError):
+class SkipError(CancelError):
     """User pressed Skipped n operations."""
-    def __init__(self,count=None,message=u'%s actions skipped by user.'):
-        if count:
-            message = message % count
-        else:
-            message = u'Action skipped by user.'
-        BoltError.__init__(self,message)
+    def __init__(self):
+        CancelError.__init__(self, u'Action skipped by user.')
 
 #------------------------------------------------------------------------------
 class PermissionError(BoltError):

Mopy/bash/env.py
@@ -309,7 +309,7 @@ def test_permissions(path, permissions='rwcd'):
 
 # Shell (OS) File Operations --------------------------------------------------
 #------------------------------------------------------------------------------
-class FileOperationError(Exception):
+class FileOperationError(OSError):
     def __init__(self, errorCode):
         self.errno = errorCode
         Exception.__init__(self, u'FileOperationError: %i' % errorCode)

commit 1b81c4c6d8097f8468273bbc13e8ecf0a5b7d0d5

Regression in installers rename:
Introduced in d09b43476b345591e5aa2ef9247e888bb08ed395, reported:
http://forums.bethsoft.com/topic/1606578-wrye-bash-thread-111/page-4#entry25243216
Traceback (most recent call last):
  File "bash\basher\__init__.py", line 2191, in OnLabelEdited
    any(grouped) for grouped in zip(*refreshes)]
TypeError: zip argument #2 must support iteration
Installers rename code just _needs_ simplification.

Signed-off-by: MrD <the.ubik@gmail.com>

Mopy/bash/basher/__init__.py
@@ -2204,8 +2204,10 @@ class InstallersList(balt.UIList):
             except (CancelError, OSError, IOError) as ex:
                 pass
             finally:
-                refreshNeeded, modsRefresh, iniRefresh = [
-                    any(grouped) for grouped in zip(*refreshes)]
+                refreshNeeded = modsRefresh = iniRefresh = False
+                if len(refreshes) > 1:
+                    refreshNeeded, modsRefresh, iniRefresh = [
+                        any(grouped) for grouped in zip(*refreshes)]
             #--Refresh UI
             if refreshNeeded or ex: # refresh the UI in case of an exception
                 if modsRefresh: BashFrame.modList.RefreshUI(refreshSaves=False,

Mopy/bash/bosh/__init__.py
@@ -2950,7 +2950,7 @@ class _DataStore(DataDict):
     # Renaming
     def rename_info(self, oldName, newName):
         try:
-            self._rename_operation(oldName, newName)
+            return self._rename_operation(oldName, newName)
         except (CancelError, OSError, IOError):
             deprint(u'Renaming %s to %s failed' % (oldName, newName),
                     traceback=True)