wrye-bash/wrye-bash

commit 81450fc8654a13f865d12a5c1ce9bc621801f068

MobObjects.keepRecords optimization:
Mopy/bash/record_groups.py:
@@ -264,5 +264,6 @@ def keepRecords(self,keepIds):
         """Keeps records with fid in set keepIds. Discards the rest."""
-        self.records = [record for record in self.records if (record.fid == (
-            record.isKeyedByEid and GPath(bosh.modInfos.masterName),
-            0) and record.eid in keepIds) or record.fid in keepIds]
+        nullFid = (bosh.modInfos.masterName, 0)
+        self.records = [record for record in self.records if
+                        (record.fid in keepIds if not record.isKeyedByEid else
+                        (record.fid == nullFid and record.eid in keepIds))]

We called GPath in a loop for a potentially huge number of records,
plus readability suffered (still in pain).
Took the opportunity to rename local variables and such to single out
bosh.modInfos.masterName (Path actually) and dropped unneeded GPath
calls on it.

Mopy/bash/record_groups.py
@@ -250,7 +250,7 @@ class MobObjects(MobBase):
             self.indexRecords()
         record_id = record.fid
         if record.isKeyedByEid:
-            if record_id == (GPath(bosh.modInfos.masterName),0):
+            if record_id == (bosh.modInfos.masterName, 0):
                 record_id = record.eid
         if record_id in self.id_records:
             oldRecord = self.id_records[record_id]
@@ -262,9 +262,10 @@ class MobObjects(MobBase):
 
     def keepRecords(self,keepIds):
         """Keeps records with fid in set keepIds. Discards the rest."""
-        self.records = [record for record in self.records if (record.fid == (
-            record.isKeyedByEid and GPath(bosh.modInfos.masterName),
-            0) and record.eid in keepIds) or record.fid in keepIds]
+        nullFid = (bosh.modInfos.masterName, 0)
+        self.records = [record for record in self.records if
+                        (record.fid in keepIds if not record.isKeyedByEid else
+                        (record.fid == nullFid and record.eid in keepIds))]
         self.id_records.clear()
         self.setChanged()
 

Mopy/bash/basher/__init__.py
@@ -400,8 +400,8 @@ class MasterList(_ModsUIList):
         if not fileInfo:
             return
         #--Fill data and populate
-        for mi, masterName in enumerate(fileInfo.header.masters):
-            masterInfo = bosh.MasterInfo(masterName,0)
+        for mi, masters_name in enumerate(fileInfo.header.masters):
+            masterInfo = bosh.MasterInfo(masters_name, 0)
             self.data_store[mi] = masterInfo
         self._reList()
         self.PopulateItems()
@@ -409,39 +409,39 @@ class MasterList(_ModsUIList):
     #--Get Master Status
     def GetMasterStatus(self, mi):
         masterInfo = self.data_store[mi]
-        masterName = masterInfo.name
+        masters_name = masterInfo.name
         status = masterInfo.getStatus()
         if status == 30: return status # does not exist
         # current load order of master relative to other masters
-        loadOrderIndex = self.loadOrderNames.index(masterName)
+        loadOrderIndex = self.loadOrderNames.index(masters_name)
         ordered = load_order.activeCached()
         if mi != loadOrderIndex: # there are active masters out of order
             return 20  # orange
         elif status > 0:
             return status  # never happens
-        elif (mi < len(ordered)) and (ordered[mi] == masterName):
+        elif (mi < len(ordered)) and (ordered[mi] == masters_name):
             return -10  # Blue
         else:
             return status  # 0, Green
 
     def set_item_format(self, mi, item_format):
         masterInfo = self.data_store[mi]
-        masterName = masterInfo.name
+        masters_name = masterInfo.name
         #--Font color
         fileBashTags = masterInfo.getBashTags()
         mouseText = u''
         if masterInfo.isEsm():
             item_format.text_key = 'mods.text.esm'
             mouseText += _(u"Master file. ")
-        elif masterName in bosh.modInfos.mergeable:
+        elif masters_name in bosh.modInfos.mergeable:
             if u'NoMerge' in fileBashTags:
                 item_format.text_key = 'mods.text.noMerge'
                 mouseText += _(u"Technically mergeable but has NoMerge tag.  ")
             else:
                 item_format.text_key = 'mods.text.mergeable'
         #--Text BG
-        if bosh.modInfos.isBadFileName(masterName.s):
-            if load_order.isActiveCached(masterName):
+        if bosh.modInfos.isBadFileName(masters_name.s):
+            if load_order.isActiveCached(masters_name):
                 item_format.back_key = 'mods.bkgd.doubleTime.load'
             else:
                 item_format.back_key = 'mods.bkgd.doubleTime.exists'
@@ -458,8 +458,8 @@ class MasterList(_ModsUIList):
                 item_format.font = Resources.fonts.bold
         #--Image
         status = self.GetMasterStatus(mi)
-        oninc = load_order.isActiveCached(masterName) or (
-            masterName in bosh.modInfos.merged and 2)
+        oninc = load_order.isActiveCached(masters_name) or (
+            masters_name in bosh.modInfos.merged and 2)
         on_display = self.detailsPanel.displayed_item
         if status == 30: # master is missing
             mouseText += _(u"Missing master of %s.  ") % on_display
@@ -467,7 +467,7 @@ class MasterList(_ModsUIList):
         elif on_display in bosh.modInfos:
             if status == 20:
                 mouseText += _(u"Reordered relative to other masters.  ")
-            if load_order.loIndexCached(on_display) < load_order.loIndexCached(masterName):
+            if load_order.loIndexCached(on_display) < load_order.loIndexCached(masters_name):
                 mouseText += _(u"Loads after %s.  ") % on_display
                 status = 20 # paint orange
         item_format.icon_key = status, oninc
@@ -483,8 +483,7 @@ class MasterList(_ModsUIList):
         #--Pre-clean
         edited = False
         for mi, masterInfo in self.data_store.items():
-            masterName = masterInfo.name
-            newName = settings['bash.mods.renames'].get(masterName, None)
+            newName = settings['bash.mods.renames'].get(masterInfo.name, None)
             #--Rename?
             if newName and newName in bosh.modInfos:
                 masterInfo.setName(newName)

Mopy/bash/basher/misc_links.py
@@ -264,13 +264,13 @@ class Master_ChangeTo(_Master_EditList):
     def Execute(self):
         itemId = self.selected[0]
         masterInfo = self.window.data_store[itemId]
-        masterName = masterInfo.name
+        master_name = masterInfo.name
         #--File Dialog
         wildcard = _(u'%s Mod Files') % bush.game.displayName \
                    + u' (*.esp;*.esm)|*.esp;*.esm'
         newPath = self._askOpen(title=_(u'Change master name to:'),
                                 defaultDir=bosh.modInfos.dir,
-                                defaultFile=masterName, wildcard=wildcard,
+                                defaultFile=master_name, wildcard=wildcard,
                                 mustExist=True)
         if not newPath: return
         (newDir,newName) = newPath.headTail
@@ -279,11 +279,11 @@ class Master_ChangeTo(_Master_EditList):
             self._showError(
                _(u"File must be selected from Oblivion Data Files directory."))
             return
-        elif newName == masterName:
+        elif newName == master_name:
             return
         #--Save Name
         masterInfo.setName(newName)
-        bass.settings.getChanged('bash.mods.renames')[masterName] = newName
+        bass.settings.getChanged('bash.mods.renames')[master_name] = newName
         self.window.SetMasterlistEdited(repopulate=True)
 
 #------------------------------------------------------------------------------
@@ -297,8 +297,7 @@ class Master_Disable(AppendableLink, _Master_EditList):
     def Execute(self):
         itemId = self.selected[0]
         masterInfo = self.window.data_store[itemId]
-        masterName = masterInfo.name
-        newName = GPath(re.sub(u'[mM]$','p',u'XX'+masterName.s))
+        newName = GPath(re.sub(u'[mM]$', 'p', u'XX' + masterInfo.name.s))
         #--Save Name
         masterInfo.setName(newName)
         self.window.SetMasterlistEdited(repopulate=True)

Mopy/bash/bosh/__init__.py
@@ -3341,9 +3341,9 @@ class ModInfos(FileInfos):
 
     #--Load Order utility methods - be sure cache is valid when using them-----
     @staticmethod
-    def hexIndexString(masterName):
-        return u'%02X' % (load_order.activeIndexCached(masterName),) \
-            if load_order.isActiveCached(masterName) else u''
+    def hexIndexString(mod):
+        return u'%02X' % (load_order.activeIndexCached(mod),) \
+            if load_order.isActiveCached(mod) else u''
 
     def masterWithVersion(self, masterName):
         if masterName == u'Oblivion.esm' and self.voCurrent:

Mopy/bash/patcher/patch_files.py
@@ -270,7 +270,7 @@ class PatchFile(_PFile, ModFile):
         selfMergeFactoryType_class = self.mergeFactory.type_class
         selfReadFactoryAddClass = self.readFactory.addClass
         selfLoadFactoryAddClass = self.loadFactory.addClass
-        nullFid = (GPath(bosh.modInfos.masterName),0)
+        nullFid = (bosh.modInfos.masterName, 0)
         for blockType,block in modFile.tops.iteritems():
             iiSkipMerge = iiMode and blockType not in bush.game.listTypes
             #--Make sure block type is also in read and write factories

commit c03b6f2416ef3369fc84c9a33d13800050d091d2

Regression in GMST patcher:
Introduced in 81450fc8654a13f865d12a5c1ce9bc621801f068, spare me the
jokes on optimization.
Turns out that although the record has isKeyedByEid True it is in
keepIds with a full record.fid. Worth investigating (and commenting) but
that's good enough for now.

See:
https://cloud.githubusercontent.com/assets/1365079/19891585/74bd360c-a049-11e6-9ee1-34c200b640b5.jpg

Closes #335.

Signed-off-by: MrD <the.ubik@gmail.com>

Mopy/bash/record_groups.py
@@ -262,10 +262,9 @@ class MobObjects(MobBase):
 
     def keepRecords(self,keepIds):
         """Keeps records with fid in set keepIds. Discards the rest."""
-        nullFid = (bosh.modInfos.masterName, 0)
-        self.records = [record for record in self.records if
-                        (record.fid in keepIds if not record.isKeyedByEid else
-                        (record.fid == nullFid and record.eid in keepIds))]
+        self.records = [record for record in self.records if (record.fid == (
+            record.isKeyedByEid and bosh.modInfos.masterName,
+            0) and record.eid in keepIds) or record.fid in keepIds]
         self.id_records.clear()
         self.setChanged()
 