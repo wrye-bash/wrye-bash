wrye-bash/wrye-bash

commit da640842f06132e7388953a177107037d23b891c

Bye Tank - cures flickering in mods tab:
Using named tuple for fonts (will come handy)
New API for setting the UI applied in ex Tank subclasses and ModList.
Mouse texts and item formatting must be set together and the tuple return
value of getGuiKeys clearly suggested a class to group the format
elements. This finally opens the way to add more mouse texts etc in BAIN.
The flickering is cured with this commit, for reasons unknown, but time
will tell.

Under #163.
Closes #179.

Yey!

Mopy/bash/balt.py
@@ -38,7 +38,7 @@ import textwrap
 import time
 import threading
 from functools import partial, wraps
-from collections import OrderedDict
+from collections import OrderedDict, namedtuple
 #--wx
 import wx
 from wx.lib.mixins.listctrl import ListCtrlAutoWidthMixin
@@ -63,7 +63,8 @@ def fonts():
     font_italic = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
     font_bold.SetWeight(wx.FONTWEIGHT_BOLD)
     font_italic.SetStyle(wx.FONTSTYLE_SLANT)
-    return font_default, font_bold, font_italic
+    Fonts = namedtuple('Fonts', ['default', 'bold', 'italic'])
+    return Fonts(font_default, font_bold, font_italic)
 
 # Settings --------------------------------------------------------------------
 __unset = bolt.Settings(dictFile=None) # type information
@@ -1728,8 +1729,34 @@ class UIList(wx.Panel):
                 self._gList.SetStringItem(itemDex, colDex, labelTxt)
         self.setUI(item, itemDex)
 
+    class _ListItemFormat(object):
+        def __init__(self):
+            self.icon_key = self.text_key = self.back_key = None
+            self.font = Resources.fonts.default
+            self.underline = False
+
+    def set_item_format(self, item, item_format):
+        """Populate item_format attributes for text and background colors
+        and set icon, font and mouse text."""
+
     def setUI(self, fileName, itemDex):
         """Set font, status icon, background text etc."""
+        gItem = self._gList.GetItem(itemDex)
+        df = self._ListItemFormat()
+        self.set_item_format(fileName, df)
+        if df.icon_key and self.icons:
+            if isinstance(df.icon_key, tuple):
+                img = self.icons.Get(*df.icon_key)
+            else: img = self.icons[df.icon_key]
+            gItem.SetImage(img)
+        if df.text_key: gItem.SetTextColour(colors[df.text_key])
+        else: gItem.SetTextColour(self._gList.GetTextColour())
+        if df.back_key: gItem.SetBackgroundColour(colors[df.back_key])
+        else: gItem.SetBackgroundColour(self._defaultTextBackground)
+        font = gItem.GetFont()
+        font.SetUnderlined(df.underline)
+        gItem.SetFont(font)
+        self._gList.SetItem(gItem)
 
     def PopulateItems(self):
         """Sort items and populate entire list."""
@@ -2125,32 +2152,6 @@ class UIList(wx.Panel):
         with ListBoxes(self, dialogTitle, msg, [message]) as dialog:
             if not dialog.askOkModal(): return []
             return dialog.getChecked(message[0], items)
-
-#------------------------------------------------------------------------------
-class Tank(UIList):
-    """'Tank' format table. Takes the form of a wxListCtrl in Report mode, with
-    multiple columns and (optionally) column and item menus."""
-    _sunkenBorder = False
-
-    #--Updating/Sorting/Refresh -----------------------------------------------
-    def getGuiKeys(self, item):
-        """Returns keys for icon and text and background colors."""
-        iconKey = textKey = backKey = None
-        return iconKey, textKey, backKey
-
-    def getMouseText(self, *args, **kwdargs): return u''
-
-    def setUI(self, fileName, itemDex):
-        gItem = self._gList.GetItem(itemDex)
-        iconKey, textKey, backKey = self.getGuiKeys(fileName)
-        self.mouseTexts[fileName] = self.getMouseText(
-            iconKey, textKey, backKey)
-        if iconKey and self.icons: gItem.SetImage(self.icons[iconKey])
-        if textKey: gItem.SetTextColour(colors[textKey])
-        else: gItem.SetTextColour(self._gList.GetTextColour())
-        if backKey: gItem.SetBackgroundColour(colors[backKey])
-        else: gItem.SetBackgroundColour(self._defaultTextBackground)
-        self._gList.SetItem(gItem)
 
 # Links -----------------------------------------------------------------------
 #------------------------------------------------------------------------------

Mopy/bash/basher/__init__.py
@@ -460,7 +460,7 @@ class MasterList(_ModsSortMixin, balt.UIList):
             item.SetBackgroundColour(colors['default.bkgd'])
         if self.allowEdit:
             if masterInfo.oldName in settings['bash.mods.renames']:
-                item.SetFont(Resources.fonts[1]) # aka bold it - add namedtuple
+                item.SetFont(Resources.fonts.bold)
         listCtrl.SetItem(item)
         #--Image
         status = self.GetMasterStatus(mi)
@@ -825,9 +825,8 @@ class ModList(_ModsSortMixin, balt.UIList):
         self.RefreshUI(refreshSaves=True)
 
     #--Populate Item
-    def setUI(self, fileName, itemDex):
+    def set_item_format(self, fileName, item_format):
         fileInfo = self.data[fileName]
-        listCtrl = self._gList
         #--Image
         status = fileInfo.getStatus()
         checkMark = (
@@ -835,81 +834,72 @@ class ModList(_ModsSortMixin, balt.UIList):
             else 2 if fileName in bosh.modInfos.merged
             else 3 if fileName in bosh.modInfos.imported
             else 0)
-        listCtrl.SetItemImage(itemDex,self.icons.Get(status,checkMark))
-        #--Font color
-        item = listCtrl.GetItem(itemDex)
+        item_format.icon_key = status, checkMark
         #--Default message
         mouseText = u''
-        fileBashTags = bosh.modInfos[fileName].getBashTags()
+        fileBashTags = fileInfo.getBashTags()
         if fileName in bosh.modInfos.bad_names:
             mouseText += _(u'Plugin name incompatible, cannot be activated.  ')
         if fileName in bosh.modInfos.missing_strings:
             mouseText += _(u'Plugin is missing String Localization files.  ')
         if fileInfo.isEsm():
-            item.SetTextColour(colors['mods.text.esm'])
+            item_format.text_key = 'mods.text.esm'
             mouseText += _(u"Master file. ")
         elif fileName in bosh.modInfos.mergeable:
             if u'NoMerge' in fileBashTags:
-                item.SetTextColour(colors['mods.text.noMerge'])
+                item_format.text_key = 'mods.text.noMerge'
                 mouseText += _(u"Technically mergeable but has NoMerge tag.  ")
             else:
-                item.SetTextColour(colors['mods.text.mergeable'])
+                item_format.text_key = 'mods.text.mergeable'
                 if checkMark == 2:
                     mouseText += _(u"Merged into Bashed Patch.  ")
                 else:
                     mouseText += _(u"Can be merged into Bashed Patch.  ")
         else:
-            item.SetTextColour(colors['default.text'])
+            item_format.text_key = 'default.text'
         #--Image messages
         if status == 30:     mouseText += _(u"One or more masters are missing.  ")
         elif status == 20:   mouseText += _(u"Masters have been re-ordered.  ")
         if checkMark == 1:   mouseText += _(u"Active in load list.  ")
         elif checkMark == 3: mouseText += _(u"Imported into Bashed Patch.  ")
-
         #should mod be deactivated
         if u'Deactivate' in fileBashTags:
-            item.SetFont(Resources.fonts[2])
-        else:
-            item.SetFont(Resources.fonts[0])
+            item_format.font = Resources.fonts.italic
         #--Text BG
         if fileName in bosh.modInfos.bad_names:
-            item.SetBackgroundColour(colors['mods.bkgd.doubleTime.exists'])
+            item_format.back_key ='mods.bkgd.doubleTime.exists'
         elif fileName in bosh.modInfos.missing_strings:
             if bosh.modInfos.isActiveCached(fileName):
-                item.SetBackgroundColour(colors['mods.bkgd.doubleTime.load'])
+                item_format.back_key = 'mods.bkgd.doubleTime.load'
             else:
-                item.SetBackgroundColour(colors['mods.bkgd.doubleTime.exists'])
+                item_format.back_key = 'mods.bkgd.doubleTime.exists'
         elif fileInfo.hasBadMasterNames():
             if bosh.modInfos.isActiveCached(fileName):
-                item.SetBackgroundColour(colors['mods.bkgd.doubleTime.load'])
+                item_format.back_key = 'mods.bkgd.doubleTime.load'
             else:
-                item.SetBackgroundColour(colors['mods.bkgd.doubleTime.exists'])
+                item_format.back_key = 'mods.bkgd.doubleTime.exists'
             mouseText += _(u"WARNING: Has master names that will not load.  ")
         elif fileInfo.hasActiveTimeConflict():
-            item.SetBackgroundColour(colors['mods.bkgd.doubleTime.load'])
+            item_format.back_key = 'mods.bkgd.doubleTime.load'
             mouseText += _(u"WARNING: Has same load order as another mod.  ")
         elif u'Deactivate' in fileBashTags and checkMark == 1:
-            item.SetBackgroundColour(colors['mods.bkgd.deactivate'])
+            item_format.back_key = 'mods.bkgd.deactivate'
             mouseText += _(u"Mod should be imported and deactivated.  ")
         elif fileInfo.isExOverLoaded():
-            item.SetBackgroundColour(colors['mods.bkgd.exOverload'])
+            item_format.back_key = 'mods.bkgd.exOverload'
             mouseText += _(u"WARNING: Exclusion group is overloaded.  ")
         elif fileInfo.hasTimeConflict():
-            item.SetBackgroundColour(colors['mods.bkgd.doubleTime.exists'])
+            item_format.back_key = 'mods.bkgd.doubleTime.exists'
             mouseText += _(u"Has same time as another (unloaded) mod.  ")
         elif fileInfo.isGhost:
-            item.SetBackgroundColour(colors['mods.bkgd.ghosted'])
+            item_format.back_key = 'mods.bkgd.ghosted'
         else:
-            item.SetBackgroundColour(colors['default.bkgd'])
+            item_format.back_key = 'default.bkgd'
         if fileInfo.isGhost: mouseText += _(u"File is ghosted.  ")
         if settings['bash.mods.scanDirty']:
             message = fileInfo.getDirtyMessage()
             mouseText += message[1]
-            if message[0]:
-                font = item.GetFont()
-                font.SetUnderlined(True)
-                item.SetFont(font)
-        listCtrl.SetItem(item)
+            if message[0]: item_format.underline = True
         self.mouseTexts[fileName] = mouseText
 
     def RefreshUI(self, **kwargs):
@@ -2052,10 +2042,11 @@ class SavePanel(SashPanel):
         super(SavePanel, self).ClosePanel()
 
 #------------------------------------------------------------------------------
-class InstallersList(balt.Tank):
+class InstallersList(balt.UIList):
     mainMenu = Links()
     itemMenu = Links()
     icons = installercons
+    _sunkenBorder = False
     _shellUI = True
     _editLabels = True
     _default_sort_col = 'Package'
@@ -2095,8 +2086,7 @@ class InstallersList(balt.Tank):
     _type_textKey = {1: 'default.text', 2: 'installers.text.complex'}
 
     #--Item Info
-    def getGuiKeys(self, item):
-        """Returns keys for icon and text and background colors."""
+    def set_item_format(self, item, item_format):
         installer = self.data[item]
         #--Text
         if installer.type == 2 and len(installer.subNames) == 2:
@@ -2115,10 +2105,6 @@ class InstallersList(balt.Tank):
         elif isinstance(installer, bosh.InstallerProject): iconKey += '.dir'
         if settings['bash.installers.wizardOverlay'] and installer.hasWizard:
             iconKey += '.wiz'
-        return iconKey, textKey, backKey
-
-    def getMouseText(self,iconKey,textKey,backKey):
-        """Return mouse text to use, given the iconKey, textKey and backKey."""
         text = u''
         #if textKey == 'installers.text.invalid': # I need a 'text.markers'
         #    text += _(u'Marker Package. Use for grouping installers together')
@@ -2127,7 +2113,10 @@ class InstallersList(balt.Tank):
         elif backKey == 'installers.bkgd.dirty':
             text += _(u'Needs Annealing due to a change in configuration.')
         #--TODO: add mouse  mouse tips
-        return text
+        self.mouseTexts[item] = text
+        item_format.icon_key = iconKey
+        item_format.text_key = textKey
+        item_format.back_key = backKey
 
     def OnBeginEditLabel(self,event):
         """Start renaming installers"""
@@ -3259,10 +3248,11 @@ class BSAPanel(SashPanel):
         # bosh.bsaInfos.profiles.save()
 
 #------------------------------------------------------------------------------
-class PeopleList(balt.Tank):
+class PeopleList(balt.UIList):
     mainMenu = Links()
     itemMenu = Links()
     icons = karmacons
+    _sunkenBorder = False
     _recycle = False
     _default_sort_col = 'Name'
     _sort_keys = {'Name'  : lambda self, x: x.lower(),
@@ -3281,11 +3271,8 @@ class PeopleList(balt.Tank):
                                    self.data[name][2].split(u'\n', 1)[0][:75]),
     ])
 
-    def getGuiKeys(self, item):
-        """Return keys for icon and text and background colors."""
-        textKey = backKey = None
-        iconKey = u'karma%+d' % self.data[item][1]
-        return iconKey, textKey, backKey
+    def set_item_format(self, item, item_format):
+        item_format.icon_key = u'karma%+d' % self.data[item][1]
 
 #------------------------------------------------------------------------------
 class PeoplePanel(SashTankPanel):

commit d1a5350310e2e955b8063bf6cfe9d7cb9c605fe8

De-wx'ing fonts and display default tweaks in italics:
Incidentally I finally groked why we got the flickering in the mods tab.
If we passed a font in gItem and not _its_ font flickering occurs. So:
So just this:
@@ -1754,5 +1754,5 @@ def __setUI(self, fileName, itemDex):
         if df.back_key: gItem.SetBackgroundColour(colors[df.back_key])
         else: gItem.SetBackgroundColour(self._defaultTextBackground)
-        font = gItem.GetFont()
+        font = df.font
         font.SetUnderlined(df.underline)
         gItem.SetFont(font)
results in flickering! Mystery in #179 solved.

This commit fixes a regression introduced in
da640842f06132e7388953a177107037d23b891c - namely deactivate mods
would not display in italics - not using the Resources font in:
https://github.com/wrye-bash/wrye-bash/commit/da640842f06132e7388953a177107037d23b891c#diff-be6a3eed6c0edcc900fe0291f274970bR866
cured the flickering but the italics were left out.
Why passing the font in as opposed to getting the font from the item
and modify that is worth asking but hey, mystery basically solved after
5 years: https://sourceforge.net/p/oblivionworks/bugs/135/

Under #190.

Mopy/bash/balt.py
@@ -59,14 +59,14 @@ splitterStyle = wx.SP_LIVE_UPDATE # | wx.SP_3DSASH # ugly but
 wxPoint = wx.Point
 wxSize = wx.Size
 
-def fonts():
-    font_default = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
-    font_bold = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
-    font_italic = wx.SystemSettings_GetFont(wx.SYS_DEFAULT_GUI_FONT)
-    font_bold.SetWeight(wx.FONTWEIGHT_BOLD)
-    font_italic.SetStyle(wx.FONTSTYLE_SLANT)
-    Fonts = namedtuple('Fonts', ['default', 'bold', 'italic'])
-    return Fonts(font_default, font_bold, font_italic)
+class Font(wx.Font):
+
+    @staticmethod
+    def Style(font_, bold=False, slant=False, underline=False):
+        if bold: font_.SetWeight(wx.FONTWEIGHT_BOLD)
+        if slant: font_.SetStyle(wx.FONTSTYLE_SLANT)
+        if underline: font_.SetUnderlined()
+        return font_
 
 # Settings --------------------------------------------------------------------
 __unset = bolt.Settings(dictFile=None) # type information
@@ -1729,7 +1729,8 @@ class UIList(wx.Panel):
             self.icon_key = None
             self.back_key = 'default.bkgd'
             self.text_key = 'default.text'
-            self.font = Resources.fonts.default
+            self.strong = False
+            self.italics = False
             self.underline = False
 
     def set_item_format(self, item, item_format):
@@ -1753,9 +1754,8 @@ class UIList(wx.Panel):
         else: gItem.SetTextColour(self.__gList.GetTextColour())
         if df.back_key: gItem.SetBackgroundColour(colors[df.back_key])
         else: gItem.SetBackgroundColour(self._defaultTextBackground)
-        font = gItem.GetFont()
-        font.SetUnderlined(df.underline)
-        gItem.SetFont(font)
+        gItem.SetFont(Font.Style(gItem.GetFont(), bold=df.strong,
+                                 slant=df.italics, underline=df.underline))
         self.__gList.SetItem(gItem)
 
     def PopulateItems(self):

Mopy/bash/basher/__init__.py
@@ -454,7 +454,7 @@ class MasterList(_ModsUIList):
             item_format.back_key = 'mods.bkgd.ghosted'
         if self.allowEdit:
             if masterInfo.oldName in settings['bash.mods.renames']:
-                item_format.font = Resources.fonts.bold
+                item_format.strong = True
         #--Image
         status = self.GetMasterStatus(mi)
         oninc = load_order.isActiveCached(masters_name) or (
@@ -618,6 +618,10 @@ class INIList(balt.UIList):
             if not settings['bash.ini.allowNewLines']: icon = 20
             else: icon = 0
             mousetext = _(u'Tweak is invalid')
+        if iniInfo.is_default_tweak:
+            mousetext = _(u'Default Bash Tweak') + (
+                (u'.  ' + mousetext) if mousetext else u'')
+            item_format.italics = True
         self.mouseTexts[ini_name] = mousetext
         item_format.icon_key = icon, checkMark
         #--Font/BG Color
@@ -853,7 +857,7 @@ class ModList(_ModsUIList):
         elif checkMark == 3: mouseText += _(u"Imported into Bashed Patch.  ")
         #should mod be deactivated
         if u'Deactivate' in fileBashTags:
-            item_format.font = Resources.fonts.italic
+            item_format.italics = True
         #--Text BG
         if mod_name in bosh.modInfos.bad_names:
             item_format.back_key ='mods.bkgd.doubleTime.exists'
@@ -4008,7 +4012,6 @@ class BashApp(wx.App):
         Resources.bashRed = Resources.bashRed.GetIconBundle()
         Resources.bashDocBrowser = Resources.bashDocBrowser.GetIconBundle()
         Resources.bashMonkey = Resources.bashMonkey.GetIconBundle()
-        Resources.fonts = balt.fonts()
 
     @staticmethod
     def InitData(progress):

Mopy/bash/basher/gui_patchers.py
@@ -140,6 +140,10 @@ class _PatcherPanel(object):
 
     def mass_select(self, select=True): self.isEnabled = select
 
+    def _bold(self, index, cntrl):
+        get_font = self.gConfigPanel.GetFont()
+        cntrl.SetItemFont(index, balt.Font.Style(get_font, bold=True))
+
 #------------------------------------------------------------------------------
 class _AliasesPatcherPanel(_PatcherPanel):
     # CONFIG DEFAULTS
@@ -287,9 +291,7 @@ class _ListPatcherPanel(_PatcherPanel):
                         patcherOn = True
                     if not isFirstLoad:
                         # indicate that this is a new item by bolding it and its parent patcher
-                        font = self.gConfigPanel.GetFont()
-                        font.SetWeight(wx.FONTWEIGHT_BOLD)
-                        self.gList.SetItemFont(index, font)
+                        self._bold(index, self.gList)
                         patcherBold = True
                 self.gList.Check(index,self.configChecks.setdefault(item,effectiveDefaultItemCheck))
         self.configItems = items
@@ -540,9 +542,7 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
             self.gTweakList.Check(index,tweak.isEnabled)
             if not isFirstLoad and tweak.isNew():
                 # indicate that this is a new item by bolding it and its parent patcher
-                font = self.gConfigPanel.GetFont()
-                font.SetWeight(wx.FONTWEIGHT_BOLD)
-                self.gTweakList.SetItemFont(index, font)
+                self._bold(index, self.gTweakList)
                 patcherBold = True
         if patcherBold:
             self._BoldPatcherLabel()

Mopy/bash/basher/patcher_dialog.py
@@ -459,9 +459,8 @@ class PatchDialog(balt.Dialog):
         """Set the patcher label to bold font.  Called from a patcher when
         it realizes it has something new in its list"""
         index = self.patchers.index(patcher)
-        font = self.gPatchers.GetFont()
-        font.SetWeight(wx.FONTWEIGHT_BOLD)
-        self.gPatchers.SetItemFont(index, font)
+        get_font = self.gPatchers.GetFont()
+        self.gPatchers.SetItemFont(index, balt.Font.Style(get_font, bold=True))
 
     def OnCheck(self,event):
         """Toggle patcher activity state."""

Mopy/bash/bass.py
@@ -49,7 +49,6 @@ def GetBashIni(iniPath=None, reload_=False): ##: needs work
     return bashIni
 
 class Resources: # this belongs to basher but leads to cyclic imports, so...
-    fonts = None
     #--Icon Bundles
     bashRed = None
     bashBlue = None