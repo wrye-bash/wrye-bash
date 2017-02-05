wrye-bash/wrye-bash

commit fa427705db8685c65a4e7a85851ceb8163db44fa

WIP - Unifying deletion handling - menus/shortcuts common code - inis fix:
Under #172.

- Menu items:
File_Delete was used by: ModList, SaveList, BSAList, ScreensList
The rest used:
Message_Delete <-- I left this alone
Installer_Delete
INI_Delete <-- I filter default tweaks out then delegate to common
Tank_Delete (People)
File_Delete _did not use DeleteSelected()_ - so basically all the
trackedInfos updates etc were not applied when deleting via menu items...

- Shortcuts:
As seen in bf897ad6f4a42b5c05024a679a08c7a0:
ModList, SaveList, ScreensList, IniList, BSAList (untested) used
DeleteSelected (with updating trackedInfos and all). 97eb0477
added delete shortcut to MessageList (also untested) and PeopleList
(using the Tank variation of DeleteSelected, basically del data[key]).
See 97eb0477 for _shellUI and installers.

I am not entirely sure what to do with the tracked info thing - see
d72bd4fd75e8b25a63ee7effcb4fedff8a0f5ed0 where they are introduced
to deletion and efa89935fa2cd5d3f47345ef81067e57fa31fd16 for deletion
shellUI stuff.
Tracked file infos were introduced at
cd7fda982af2ad5385390e55029d416411eee6a9

I guess only Ini and Mod tabs should be "temporarily tracked" (?) What
about saves tab ?

In this commit:
1. Edited File_Delete to use DeleteSelected and made _all_ tabs use
this (apart from Messages) - renamed to UIList_Delete. File_Delete used
List.DeleteSelected() code apart from:
 - trivial differences (like `Uncheck files to skip` vs `items to skip`
etc and showError(self, u'%s' % e) vs self._showError(u'%r' % e))
 - not so trivial differences, namely it had no clue of the shellUI path.
So now Ini and Screens use this path as well when using the menu item.
2. Added DeleteItems() override to Ini tab - FIXes default tweaks
being deleted when using the shortcut (the menu item, INI_Delete,
had checks but common List.DeleteSelected() had not, present in 305).
3. Nitpick: wxDelete = {wx.WXK_DELETE, wx.WXK_NUMPAD_DELETE}

Mopy/bash/balt.py
@@ -2040,9 +2040,8 @@ class UIList(wx.Panel):
         code = event.GetKeyCode()
         if event.CmdDown() and code == ord('A'): self.SelectAll() # Ctrl+A
         elif self.__class__._editLabels and code == wx.WXK_F2: self.Rename()
-        elif code in (wx.WXK_DELETE, wx.WXK_NUMPAD_DELETE):
-            with BusyCursor(): self.DeleteSelected(
-                shellUI=self.__class__._shellUI, noRecycle=event.ShiftDown())
+        elif code in wxDelete:
+            with BusyCursor(): self.DeleteItems(event=event)
         event.Skip()
 
     ##: Columns callbacks - belong to a ListCtrl mixin
@@ -2273,13 +2272,19 @@ class UIList(wx.Panel):
             index = self._gList.FindItem(0, selected[0].s)
             if index != -1: self._gList.EditLabel(index)
 
-    def DeleteSelected(self,shellUI=False,noRecycle=False):
+    def DeleteItems(self, event=None, items=None):
+        noRecycle = event.ShiftDown() if event is not None else False
+        self.DeleteSelected(shellUI=self.__class__._shellUI,
+                            noRecycle=noRecycle, items=items)
+
+    def DeleteSelected(self, shellUI=False, noRecycle=False, items=None):
         """Deletes selected items."""
-        items = self.GetSelected()
+        items = items if items is not None else self.GetSelected()
         if not items: return
         if not shellUI:
             message = [u'',_(u'Uncheck items to skip deleting them if desired.')]
-            message.extend(sorted(items))
+            sortedItems = sorted(items)
+            message.extend(sortedItems)
             with ListBoxes(self, _(u'Delete Items'), _(
                     u'Delete these items?  This operation cannot be '
                     u'undone.'), [message]) as dialog:
@@ -2288,7 +2293,7 @@ class UIList(wx.Panel):
                 checks = dialog.FindWindowById(id_)
                 if checks:
                     dirJoin = self.data.dir.join
-                    for i,mod in enumerate(items):
+                    for i,mod in enumerate(sortedItems):
                         if checks.IsChecked(i):
                             try:
                                 self.data.delete(mod)
@@ -2296,7 +2301,7 @@ class UIList(wx.Panel):
                                 # update the status of its installers
                                 bosh.trackedInfos.track(dirJoin(mod))
                             except bolt.BoltError as e:
-                                showError(self, u'%s' % e)
+                                showError(self, u'%r' % e)
         else:
             try:
                 self.data.delete(items,askOk=True,dontRecycle=noRecycle)
@@ -2691,16 +2696,15 @@ class BoolLink(CheckLink):
 
     def Execute(self,event): bosh.settings[self.key] ^= True # toggle
 
-# Tank Links ------------------------------------------------------------------
-#------------------------------------------------------------------------------
-class Tank_Delete(ItemLink): # was used in BAIN would not refresh - used in People
-    """Deletes selected file from tank."""
+# UIList Links ----------------------------------------------------------------
+class UIList_Delete(ItemLink):
+    """Delete selected item(s) from UIList."""
     text = _(u'Delete')
     help = _(u'Delete selected item(s)')
 
     def Execute(self,event):
-        with BusyCursor():
-            self.window.DeleteSelected()
+        # event is a 'CommandEvent' and I can't check if shift is pressed - duh
+        with BusyCursor(): self.window.DeleteItems(items=self.selected)
 
 # wx Wrappers -----------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -2733,6 +2737,7 @@ wxArrowUp = {wx.WXK_UP, wx.WXK_NUMPAD_UP}
 wxArrowDown = {wx.WXK_DOWN, wx.WXK_NUMPAD_DOWN}
 wxArrows = wxArrowUp | wxArrowDown
 wxReturn = {wx.WXK_RETURN, wx.WXK_NUMPAD_ENTER}
+wxDelete = {wx.WXK_DELETE, wx.WXK_NUMPAD_DELETE}
 
 # ListBoxes -------------------------------------------------------------------
 class _CheckList_SelectAll(ItemLink):

Mopy/bash/basher/__init__.py
@@ -534,6 +534,17 @@ class INIList(balt.UIList):
         valid = filter(lambda k: self.data[k].status >= 0, self.data.keys())
         self.RefreshUI(files=valid)
 
+    @staticmethod
+    def filterOutDefaultTweaks(tweaks):
+        """Filter out default tweaks from tweaks iterable."""
+        return filter(lambda x: bosh.dirs['tweaks'].join(x).isfile(), tweaks)
+
+    def DeleteItems(self, event=None, items=None):
+        items = items if items is not None else self.GetSelected()
+        items = self.filterOutDefaultTweaks(items) # will refilter if coming
+        # from INI_Delete - expensive but I can't allow default tweaks deletion
+        super(INIList, self).DeleteItems(event, items)
+
     def getLabels(self, fileName):
         labels, table = {}, self.data.table
         labels['File'] = fileName.s
@@ -2673,9 +2684,7 @@ class InstallersPanel(SashTankPanel):
                     data[path] = (file.size,file.crc,file.mtime)
                     refresh = True
                 else:
-                    if data.get(path,None) is not None:
-                        data.pop(path,None)
-                        refresh = True
+                    refresh = bool(data.pop(path, None))
             if refresh:
                 self.data.refreshStatus()
                 self.RefreshUIMods()

Mopy/bash/basher/files_links.py
@@ -28,14 +28,14 @@ import time
 from .. import balt, bosh, bush, bolt
 from ..bass import Resources
 from ..balt import ItemLink, RadioLink, EnabledLink, AppendableLink, \
-    ChoiceLink, Link, OneItemLink, ListBoxes
-from ..bolt import CancelError, SkipError, GPath, BoltError
+    ChoiceLink, Link, OneItemLink
+from ..bolt import CancelError, SkipError, GPath
 from ..bosh import formatDate
 
 __all__ = ['Files_SortBy', 'Files_Unhide', 'Files_Open', 'File_Backup',
-           'File_Duplicate', 'File_Snapshot', 'File_Delete', 'File_Hide',
-           'File_Redate', 'File_Sort', 'File_RevertToBackup',
-           'File_RevertToSnapshot', 'File_ListMasters', 'File_Open']
+           'File_Duplicate', 'File_Snapshot', 'File_Hide', 'File_Redate',
+           'File_Sort', 'File_RevertToBackup', 'File_RevertToSnapshot',
+           'File_ListMasters', 'File_Open']
 
 #------------------------------------------------------------------------------
 # Files Links -----------------------------------------------------------------
@@ -145,32 +145,6 @@ class Files_Unhide(ItemLink):
 #------------------------------------------------------------------------------
 # File Links ------------------------------------------------------------------
 #------------------------------------------------------------------------------
-class File_Delete(ItemLink):
-    """Delete the file and all backups."""
-    text = _(u'Delete')
-
-    def _initData(self, window, data):
-        super(File_Delete, self)._initData(window, data)
-        self.help = _(u"Delete %(filename)s.") % ({'filename': data[0]})
-
-    def Execute(self,event):
-        message = [u'',_(u'Uncheck files to skip deleting them if desired.')]
-        message.extend(sorted(self.selected))
-        with ListBoxes(self.window,_(u'Delete Files'),
-                     _(u'Delete these files? This operation cannot be undone.'),
-                     [message]) as dialog:
-            if dialog.ShowModal() == ListBoxes.ID_CANCEL: return
-            id_ = dialog.ids[message[0]]
-            checks = dialog.FindWindowById(id_)
-            if checks:
-                for i,mod in enumerate(self.selected):
-                    if checks.IsChecked(i):
-                        try:
-                            self.window.data.delete(mod)
-                        except BoltError as e:
-                            self._showError(u'%r' % e)
-            self.window.RefreshUI()
-
 class File_Duplicate(ItemLink):
     """Create a duplicate of the file."""
 

Mopy/bash/basher/ini_links.py
@@ -28,8 +28,7 @@ attribute points to BashFrame.iniList singleton.
 
 from .. import bosh, balt, bush
 from ..bass import Resources
-from ..balt import ItemLink, Link, BoolLink, EnabledLink, OneItemLink, \
-    ListBoxes
+from ..balt import ItemLink, Link, BoolLink, EnabledLink, OneItemLink
 
 __all__ = ['INI_SortValid', 'INI_AllowNewLines', 'INI_ListINIs', 'INI_Apply',
            'INI_CreateNew', 'INI_ListErrors', 'INI_FileOpenOrCopy',
@@ -120,33 +119,20 @@ class INI_FileOpenOrCopy(OneItemLink):
                 self.window.RefreshUI()
 
 #------------------------------------------------------------------------------
-class INI_Delete(EnabledLink):
+class INI_Delete(balt.UIList_Delete, EnabledLink):
     """Delete the file and all backups."""
-    text = _(u"Delete")
 
     def _initData(self, window, data):
         super(INI_Delete, self)._initData(window, data)
-        self.isFile = bosh.dirs['tweaks'].join(data[0]).isfile()
-        self.help = _(u"Delete %(filename)s.") % (
-        {'filename': data[0]}) if self.isFile else _(
-            u"Bash default tweaks can't be deleted.")
+        self.selected = self.window.filterOutDefaultTweaks(self.selected)
+        if len(self.selected) and len(data) == 1:
+            self.help = _(u"Delete %(filename)s.") % ({'filename': data[0]})
+        elif len(self.selected):
+            self.help = _(
+                u"Delete selected tweaks (default tweaks won't be deleted)")
+        else: self.help = _(u"Bash default tweaks can't be deleted")
 
-    def _enable(self): return self.isFile
-
-    def Execute(self,event):
-        message = [u'',_(u'Uncheck files to skip deleting them if desired.')]
-        message.extend(sorted(self.selected))
-        with ListBoxes(self.window,_(u'Delete Files'),
-                     _(u'Delete these files? This operation cannot be undone.'),
-                     [message]) as dialog:
-            if dialog.ShowModal() == ListBoxes.ID_CANCEL: return
-            id_ = dialog.ids[message[0]]
-            checks = dialog.FindWindowById(id_)
-            if checks:
-                for i,mod in enumerate(self.selected):
-                    if checks.IsChecked(i) and bosh.dirs['tweaks'].join(mod).isfile():
-                        self.window.data.delete(mod)
-            self.window.RefreshUI()
+    def _enable(self): return len(self.selected) > 0
 
 #------------------------------------------------------------------------------
 class INI_Apply(EnabledLink):

Mopy/bash/basher/installer_links.py
@@ -45,25 +45,24 @@ from ..bolt import CancelError, SkipError, GPath, StateError, deprint, \
     SubProgress, LogFile
 from ..bosh import formatInteger
 
-__all__ = ['Installer_Open', 'Installer_Duplicate', 'Installer_Delete',
-           'InstallerOpenAt_MainMenu', 'Installer_OpenSearch',
-           'Installer_OpenTESA', 'Installer_OpenPES', 'Installer_Hide',
-           'Installer_Rename', 'Installer_Refresh', 'Installer_Move',
-           'Installer_HasExtraData', 'Installer_OverrideSkips',
-           'Installer_SkipVoices', 'Installer_SkipRefresh', 'Installer_Wizard',
-           'Installer_EditWizard', 'Installer_OpenReadme', 'Installer_Anneal',
-           'Installer_Install', 'Installer_Uninstall',
-           'InstallerConverter_MainMenu', 'InstallerConverter_Create',
-           'InstallerConverter_ConvertMenu', 'InstallerProject_Pack',
-           'InstallerArchive_Unpack', 'InstallerProject_ReleasePack',
-           'InstallerProject_Sync', 'Installer_CopyConflicts',
-           'InstallerProject_OmodConfig', 'Installer_ListStructure',
-           'Installer_Espm_SelectAll', 'Installer_Espm_DeselectAll',
-           'Installer_Espm_List', 'Installer_Espm_Rename',
-           'Installer_Espm_Reset', 'Installer_Espm_ResetAll',
-           'Installer_Subs_SelectAll', 'Installer_Subs_DeselectAll',
-           'Installer_Subs_ToggleSelection', 'Installer_Subs_ListSubPackages',
-           'Installer_OpenNexus']
+__all__ = ['Installer_Open', 'Installer_Duplicate', 'InstallerOpenAt_MainMenu',
+           'Installer_OpenSearch', 'Installer_OpenTESA', 'Installer_OpenPES',
+           'Installer_Hide', 'Installer_Rename', 'Installer_Refresh',
+           'Installer_Move', 'Installer_HasExtraData',
+           'Installer_OverrideSkips', 'Installer_SkipVoices',
+           'Installer_SkipRefresh', 'Installer_Wizard', 'Installer_EditWizard',
+           'Installer_OpenReadme', 'Installer_Anneal', 'Installer_Install',
+           'Installer_Uninstall', 'InstallerConverter_MainMenu',
+           'InstallerConverter_Create', 'InstallerConverter_ConvertMenu',
+           'InstallerProject_Pack', 'InstallerArchive_Unpack',
+           'InstallerProject_ReleasePack', 'InstallerProject_Sync',
+           'Installer_CopyConflicts', 'InstallerProject_OmodConfig',
+           'Installer_ListStructure', 'Installer_Espm_SelectAll',
+           'Installer_Espm_DeselectAll', 'Installer_Espm_List',
+           'Installer_Espm_Rename', 'Installer_Espm_Reset',
+           'Installer_Espm_ResetAll', 'Installer_Subs_SelectAll',
+           'Installer_Subs_DeselectAll', 'Installer_Subs_ToggleSelection',
+           'Installer_Subs_ListSubPackages', 'Installer_OpenNexus']
 
 #------------------------------------------------------------------------------
 # Installer Links -------------------------------------------------------------
@@ -379,13 +378,6 @@ class Installer_Anneal(_InstallerLink):
             self.idata.refresh(what='NS')
             self.iPanel.RefreshUIMods()
             Link.Frame.RefreshData()
-
-class Installer_Delete(_InstallerLink):
-    text = _(u'Delete')
-    help = _(u'Delete selected item(s)')
-
-    def Execute(self, event): self.window.DeleteSelected(shellUI=False,
-                                                         noRecycle=False)
 
 class Installer_Duplicate(OneItemLink, _InstallerLink):
     """Duplicate selected Installer."""

Mopy/bash/basher/links.py
@@ -357,7 +357,7 @@ def InitInstallerLinks():
     #--File
     InstallersList.itemMenu.append(Installer_Open())
     InstallersList.itemMenu.append(Installer_Duplicate())
-    InstallersList.itemMenu.append(Installer_Delete())
+    InstallersList.itemMenu.append(balt.UIList_Delete())
     if True: #--Open At...
         openAtMenu = InstallerOpenAt_MainMenu(oneDatumOnly=True)
         openAtMenu.links.append(Installer_OpenSearch())
@@ -512,7 +512,7 @@ def InitModLinks():
         fileMenu.links.append(File_Duplicate())
         fileMenu.links.append(File_Snapshot())
         fileMenu.links.append(SeparatorLink())
-        fileMenu.links.append(File_Delete())
+        fileMenu.links.append(balt.UIList_Delete())
         fileMenu.links.append(File_Hide())
         fileMenu.links.append(File_Redate())
         fileMenu.links.append(File_Sort())
@@ -659,7 +659,7 @@ def InitSaveLinks():
         fileMenu.links.append(File_Duplicate())
         #fileMenu.links.append(File_Snapshot())
         fileMenu.links.append(SeparatorLink())
-        fileMenu.links.append(File_Delete())
+        fileMenu.links.append(balt.UIList_Delete())
         fileMenu.links.append(File_Hide())
         fileMenu.links.append(SeparatorLink())
         fileMenu.links.append(File_RevertToBackup())
@@ -724,7 +724,7 @@ def InitBSALinks():
         fileMenu.links.append(File_Duplicate())
         #fileMenu.links.append(File_Snapshot())
         fileMenu.links.append(SeparatorLink())
-        fileMenu.links.append(File_Delete())
+        fileMenu.links.append(balt.UIList_Delete())
         fileMenu.links.append(File_Hide())
         fileMenu.links.append(SeparatorLink())
         fileMenu.links.append(File_RevertToBackup())
@@ -771,7 +771,7 @@ def InitScreenLinks():
     #--ScreensList: Item Links
     ScreensList.itemMenu.append(File_Open())
     ScreensList.itemMenu.append(Screen_Rename())
-    ScreensList.itemMenu.append(File_Delete())
+    ScreensList.itemMenu.append(balt.UIList_Delete())
     ScreensList.itemMenu.append(SeparatorLink())
     if True: #--Convert
         convertMenu = MenuLink(_(u'Convert'))
@@ -803,7 +803,7 @@ def InitPeopleLinks():
     PeopleList.itemMenu.append(People_Karma())
     PeopleList.itemMenu.append(SeparatorLink())
     PeopleList.itemMenu.append(People_AddNew())
-    PeopleList.itemMenu.append(balt.Tank_Delete())
+    PeopleList.itemMenu.append(balt.UIList_Delete())
     PeopleList.itemMenu.append(People_Export())
 
 #------------------------------------------------------------------------------

commit 1ca91a76696726f7a6d9fa40bfa8954b87b6fab5

Ghost of delete:
The important in this commit is fixing the break in the finally - the
rest (making check_existence obligatory and reworking BAIN/screens
delete_Refresh) will be superseded by later commits (need to add markers
parameter in delete_Refresh)

Note bolt error on deleting mod info was using %r - introduced in
fa427705db8685c65a4e7a85851ceb8163db44fa, where I picked it from
existing code. Have a look at that one, to get a glimpse at the state
of the code before my time...

Under the eternal #172.

Mopy/bash/balt.py
@@ -2181,19 +2181,21 @@ class UIList(wx.Panel):
         if not self.__class__._shellUI:
             items = self._promptDelete(items, dialogTitle, order, recycle)
         if not items: return
-        for i in items: ##: simplify and make sure delete_Refresh() runs !
-            try:
-                if not self.__class__._shellUI: # non shellUI path used to
-                    # delete as many as possible, I kept this behavior
+        if not self.__class__._shellUI: # non shellUI path used to delete as
+            # many as possible, mainly to show an error on trying to delete
+            # the master esm - I kept this behavior
+            for i in items:
+                try:
                     self.data_store.delete(i, doRefresh=False, recycle=recycle)
-                else: # shellUI path tries to delete all at once
-                    self.data_store.delete(items, confirm=True, recycle=recycle)
-            except bolt.BoltError as e: showError(self, u'%r' % e)
+                except bolt.BoltError as e: showError(self, u'%s' % e)
+                except (AccessDeniedError, CancelError, SkipError): pass
+            else:
+                self.data_store.delete_Refresh(items, check_existence=True)
+        else: # shellUI path tries to delete all at once
+            try:
+                self.data_store.delete(items, confirm=True, recycle=recycle,
+                    check_existence=True)
             except (AccessDeniedError, CancelError, SkipError): pass
-            finally: # FIXME: remove break from here masks tracebacks
-                if self.__class__._shellUI: break # could delete fail mid-way ?
-        else:
-            self.data_store.delete_Refresh(items, check_existence=True)
         self.RefreshUI(refreshSaves=True) # also cleans _gList internal dicts
 
     def _toDelete(self, items):

Mopy/bash/bosh/__init__.py
@@ -2330,7 +2330,7 @@ class _DataStore(DataDict):
     store_dir = empty_path # where the datas sit, static except for SaveInfos
 
     def delete(self, itemOrItems, **kwargs): raise AbstractError
-    def delete_Refresh(self, deleted, check_existence=False):
+    def delete_Refresh(self, deleted, check_existence):
         # Yak - absorb in refresh - add deleted parameter
         if check_existence:
             deleted = set(
@@ -2428,7 +2428,6 @@ class TableFileInfos(_DataStore):
             fileNames = [fileName]
         else:
             fileNames = fileName
-        doRefresh = kwargs.pop('doRefresh', True)
         #--Files to delete
         toDelete = []
         #--Cache table updates
@@ -2455,10 +2454,11 @@ class TableFileInfos(_DataStore):
                 if not filePath.exists(): self.table.delRow(modname)
                 else: del tableUpdate[filePath] # item was not deleted
             #--Refresh
-            if doRefresh:
-                self.delete_Refresh(tableUpdate.values())
+            if kwargs.pop('doRefresh', True):
+                self.delete_Refresh(tableUpdate.values(),
+                                    kwargs.pop('check_existence', False))
 
-    def delete_Refresh(self, deleted, check_existence=False):
+    def delete_Refresh(self, deleted, check_existence):
         deleted = super(TableFileInfos, self).delete_Refresh(deleted,
                                                              check_existence)
         if not deleted: return deleted
@@ -2729,7 +2729,7 @@ class INIInfos(TableFileInfos):
     @property
     def bash_dir(self): return dirs['modsBash'].join(u'INI Data')
 
-    def delete_Refresh(self, deleted, check_existence=False):
+    def delete_Refresh(self, deleted, check_existence):
         deleted = super(INIInfos, self).delete_Refresh(deleted,
                                                        check_existence)
         if not deleted: return deleted
@@ -3585,7 +3585,7 @@ class ModInfos(FileInfos):
         self.lo_deactivate(fileName, doSave=False)
         FileInfos.delete(self, fileName, **kwargs)
 
-    def delete_Refresh(self, deleted, check_existence=False):
+    def delete_Refresh(self, deleted, check_existence):
         # adapted from refresh() (avoid refreshing from the data directory)
         deleted = FileInfos.delete_Refresh(self, deleted, check_existence)
         if not deleted: return
@@ -3950,7 +3950,7 @@ class PeopleData(_DataStore):
         del self[key]
         self.hasChanged = True
 
-    def delete_Refresh(self, deleted, check_existence=False): pass
+    def delete_Refresh(self, deleted, check_existence): pass
 
     #--Operations
     def loadText(self,path):
@@ -4016,11 +4016,15 @@ class ScreensData(_DataStore):
             filePath = [dirJoin(file) for file in fileName]
         else:
             filePath = [dirJoin(fileName)]
-        _delete(filePath, **kwargs)
-        for item in filePath:
-            if not item.exists(): del self[item.tail]
+        try:
+            _delete(filePath, **kwargs)
+        finally:
+            if kwargs.pop('doRefresh', True):
+                self.delete_Refresh(filePath, check_existence=False)
 
-    def delete_Refresh(self, deleted, check_existence=False): self.refresh()
+    def delete_Refresh(self, deleted, check_existence):
+        for item in deleted:
+            if not item.exists(): del self[item.tail]
 
     def _rename_operation(self, oldName, newName):
         super(ScreensData, self)._rename_operation(oldName, newName)
@@ -5616,19 +5620,19 @@ class InstallersData(_DataStore):
             if isinstance(self[item], InstallerMarker): markers.append(item)
             else: toDelete.append(self.store_dir.join(item))
         #--Delete
-        doRefresh = kwargs.pop('doRefresh', True)
         try:
             for m in markers: del self[m]
             _delete(toDelete, **kwargs)
         finally:
-            if doRefresh:
-                deleted = set(markers)
-                deleted.update(
+            if kwargs.pop('doRefresh', True):
+                deleted = set( # markers are already popped
                     item.tail for item in toDelete if not item.exists())
                 if deleted:
-                    self.delete_Refresh(deleted) # markers are already popped
+                    self.delete_Refresh(deleted, check_existence=False)
+                elif markers:
+                    self.refreshOrder()
 
-    def delete_Refresh(self, deleted, check_existence=False):
+    def delete_Refresh(self, deleted, check_existence):
         deleted = super(InstallersData, self).delete_Refresh(deleted,
                                                              check_existence)
         if deleted: self.irefresh(what='I', deleted=deleted)