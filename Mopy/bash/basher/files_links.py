wrye-bash/wrye-bash

commit 81746fbfee4f8e6017f4435a556d1b072b7209e0

_BackupMixin WIP:
Finally dropped bashDir.
Also dropped `if not self in self.getFileInfos().values()` tests, they
are not needed - getFileInfos must go. EDIT:

@@ -2448,2 +2448,3 @@ def _doBackup(self,backupDir,forceBackup=False):
         #--Skip backup?
+        if not self in self.getFileInfos().values(): return
         if self.madeBackup and not forceBackup: return

Cause generateBashedPatch tries to backup non existent patch - DUH,
backup hacked in to eternal chaos, plus generateBashedPatch another
hack.

Implemented a revert_backup that allowed me dropping isEss (yey !).
However, we do need to revisit the file operations FileInfos API.

Under #292
Under #336

Mopy/bash/basher/files_links.py
@@ -267,8 +267,8 @@ class _RevertBackup(OneItemLink):
 
     def _initData(self, window, selection):
         super(_RevertBackup, self)._initData(window, selection)
-        self.backup_path = self._selected_info.bashDir.join(
-            u'Backups', self._selected_item)
+        self.backup_path = self._selected_info.backup_dir.join(
+            self._selected_item)
         self.help = _(u"Revert %(file)s to its last backup") % {
             'file': self._selected_item}
 
@@ -278,25 +278,18 @@ class _RevertBackup(OneItemLink):
 
     @balt.conversation
     def Execute(self):
-        fileName = self._selected_item
         #--Warning box
         message = _(u"Revert %s to backup dated %s?") % (
-            fileName.s, formatDate(self.backup_path.mtime))
-        if self._askYes(message):
-            with balt.BusyCursor():
-                dest = self._selected_info.getPath() # care for ghosts !
-                current_mtime = dest.mtime
-                self.backup_path.copyTo(dest)
-                # do not change load order for timestamp games - rest works ok
-                self._selected_info.setmtime(current_mtime)
-                if self._selected_info.isEss():
-                    #--Handle CoSave (.pluggy and .obse) files.
-                    bosh.CoSaves(self.backup_path).copy(dest)
-                try:
-                    self.window.data_store.refreshFile(fileName)
-                except bosh.FileError:
-                    self._showError(_(u'Old file is corrupt!'))
-                self.window.RefreshUI(files=[fileName], refreshSaves=False)
+            self._selected_item.s, formatDate(self.backup_path.mtime))
+        if not self._askYes(message): return
+        with balt.BusyCursor():
+            try:
+                self._selected_info.revert_backup()
+                self.window.RefreshUI(files=[self._selected_item],
+                                      refreshSaves=False)
+            except bosh.FileError:
+                self._showError(_(u'Old file is corrupt!'))
+                self.window.RefreshUI(refreshSaves=True)
 
 class _RevertFirstBackup(_RevertBackup):
     text = _(u'Revert to First Backup')

Mopy/bash/bosh/__init__.py
@@ -2359,7 +2359,6 @@ class FileInfo(_AFileInfo):
 
     def __init__(self, parent_dir, name):
         _AFileInfo.__init__(self, parent_dir, name)
-        self.bashDir = self.getFileInfos().bash_dir
         self.header = None
         self.masterNames = tuple()
         self.masterOrder = tuple()
@@ -2386,9 +2385,6 @@ class FileInfo(_AFileInfo):
         """Extension indicates esp/esm, but byte setting indicates opposite."""
         return (self.isMod() and self.header and
                 self.name.cext != (u'.esp',u'.esm')[int(self.header.flags1) & 1])
-
-    def isEss(self):
-        return self.name.cext == bush.game.ess.ext
 
     def info_refresh(self):
         self._file_size, self._file_mod_time, self.ctime = \
@@ -2437,6 +2433,13 @@ class FileInfo(_AFileInfo):
         """Writes header to file, overwriting old header."""
         raise AbstractError
 
+    def coCopy(self,oldPath,newPath):
+        """Copies co files corresponding to oldPath to newPath.
+        Provided so that SaveFileInfo can override for its cofiles."""
+        pass
+
+class _BackupMixin(FileInfo): # this should become a real mixin - under #336
+
     def _doBackup(self,backupDir,forceBackup=False):
         """Creates backup(s) of file, places in backupDir."""
         #--Skip backup?
@@ -2462,21 +2465,26 @@ class FileInfo(_AFileInfo):
 
     def makeBackup(self, forceBackup=False):
         """Creates backup(s) of file."""
-        backupDir = self.bashDir.join(u'Backups')
+        backupDir = self.backup_dir
         self._doBackup(backupDir,forceBackup)
         #--Done
         self.madeBackup = True
 
-    def coCopy(self,oldPath,newPath):
-        """Copies co files corresponding to oldPath to newPath.
-        Provided so that SaveFileInfo can override for its cofiles."""
-        pass
+    def _backup_paths(self):
+        return [(self.backup_dir.join(self.name), self.getPath())]
+
+    def revert_backup(self):
+        backup_paths = self._backup_paths()
+        for tup in backup_paths[1:]: # if cosaves do not exist shellMove fails!
+            if not tup[0].exists(): backup_paths.remove(tup)
+        env.shellCopy(*zip(*backup_paths))
+        # do not change load order for timestamp games - rest works ok
+        self.setmtime(self._file_mod_time)
+        self.getFileInfos().refreshFile(self.name)
 
     def getNextSnapshot(self):
         """Returns parameters for next snapshot."""
-        if not self in self.getFileInfos().values():
-            raise StateError(u"Can't get snapshot parameters for file outside main directory.")
-        destDir = self.bashDir.join(u'Snapshots')
+        destDir = self.snapshot_dir
         destDir.makedirs()
         (root,ext) = self.name.rootExt
         separator = u'-'
@@ -2503,11 +2511,19 @@ class FileInfo(_AFileInfo):
         destName = root+separator+(u'.'.join(snapLast))+ext
         return destDir,destName,(root+u'*'+ext).s
 
+    @property
+    def backup_dir(self):
+        return self.getFileInfos().bash_dir.join(u'Backups')
+
+    @property
+    def snapshot_dir(self):
+        return self.getFileInfos().bash_dir.join(u'Snapshots')
+
 #------------------------------------------------------------------------------
 reReturns = re.compile(u'\r{2,}',re.U)
 reBashTags = re.compile(ur'{{ *BASH *:[^}]*}}\s*\n?',re.U)
 
-class ModInfo(FileInfo):
+class ModInfo(_BackupMixin, FileInfo):
     """An esp/m file."""
 
     def __init__(self, parent_dir, name):
@@ -2840,7 +2856,7 @@ class INIInfo(FileInfo):
         0: not installed (green)
         -10: invalid tweak file (red).
         Also caches the value in self._status"""
-        infos = self.getFileInfos()
+        infos = iniInfos
         target_ini = target_ini or infos.ini
         tweak_settings = self.ini_info_file.getSettings()
         if self._incompatible(target_ini) or not tweak_settings:
@@ -2930,7 +2946,7 @@ class INIInfo(FileInfo):
             return bolt.winNewLines(log.out.getvalue())
 
 #------------------------------------------------------------------------------
-class SaveInfo(FileInfo):
+class SaveInfo(_BackupMixin, FileInfo):
     def getFileInfos(self): return saveInfos
 
     def getStatus(self):
@@ -2965,8 +2981,13 @@ class SaveInfo(FileInfo):
         """Returns CoSaves instance corresponding to self."""
         return CoSaves(self.getPath())
 
+    def _backup_paths(self):
+        save_paths = super(SaveInfo, self)._backup_paths()
+        save_paths.extend(CoSaves.get_new_paths(*save_paths[0]))
+        return save_paths
+
 #------------------------------------------------------------------------------
-class BSAInfo(FileInfo):
+class BSAInfo(_BackupMixin, FileInfo):
     def getFileInfos(self): return bsaInfos
 
     def resetMTime(self,mtime=u'01-01-2006 00:00:00'):

commit 832324cf15ad3c3a206efd93cb53f2053ba35ee7

Regression: first backups were never restored:
Introduced in 81746fbfee4f8e6017f4435a556d1b072b7209e0

Under #292 - WIP

Mopy/bash/basher/files_links.py
@@ -259,14 +259,20 @@ class File_Backup(ItemLink):
             fileInfo.makeBackup(True)
 
 class _RevertBackup(OneItemLink):
-    _text = _(u'Revert to Backup')
+
+    def __init__(self, first=False):
+        super(_RevertBackup, self).__init__()
+        self._text = _(u'Revert to First Backup') if first else _(
+            u'Revert to Backup')
+        self.first = first
 
     def _initData(self, window, selection):
         super(_RevertBackup, self)._initData(window, selection)
         self.backup_path = self._selected_info.backup_dir.join(
-            self._selected_item)
-        self.help = _(u"Revert %(file)s to its last backup") % {
-            'file': self._selected_item}
+            self._selected_item) + (u'f' if self.first else u'')
+        self.help = _(u"Revert %(file)s to its first backup") if self.first \
+            else _(u"Revert %(file)s to its last backup")
+        self.help %= {'file': self._selected_item}
 
     def _enable(self):
         return super(_RevertBackup,
@@ -280,22 +286,13 @@ class _RevertBackup(OneItemLink):
         if not self._askYes(message): return
         with balt.BusyCursor():
             try:
-                self._selected_info.revert_backup()
+                self._selected_info.revert_backup(self.first)
                 self.window.RefreshUI(redraw=[self._selected_item],
                                       refreshSaves=False)
             except bolt.FileError:
                 self._showError(_(u'Old file is corrupt!'))
                 self.window.RefreshUI(refreshSaves=True)
 
-class _RevertFirstBackup(_RevertBackup):
-    _text = _(u'Revert to First Backup')
-
-    def _initData(self, window, selection):
-        super(_RevertFirstBackup, self)._initData(window, selection)
-        self.backup_path += u'f'
-        self.help = _(u"Revert %(file)s to its first backup") % {
-            'file': self._selected_item}
-
 class File_RevertToBackup(ChoiceLink):
     """Revert to last or first backup."""
-    extraItems = [_RevertBackup(), _RevertFirstBackup()]
+    extraItems = [_RevertBackup(), _RevertBackup(first=True)]

Mopy/bash/bosh/__init__.py
@@ -1634,11 +1634,15 @@ class FileInfo(AFile):
         #--Done
         self.madeBackup = True
 
-    def backup_paths(self):
-        return [(self.backup_dir.join(self.name), self.getPath())]
+    def backup_paths(self, first=False):
+        """Return a list of tuples with backup paths and their restore
+        destinations
+        :rtype: list[tuple]"""
+        return [(self.backup_dir.join(self.name) + (u'f' if first else u''),
+                 self.getPath())]
 
-    def revert_backup(self):
-        backup_paths = self.backup_paths()
+    def revert_backup(self, first=False):
+        backup_paths = self.backup_paths(first)
         for tup in backup_paths[1:]: # if cosaves do not exist shellMove fails!
             if not tup[0].exists(): backup_paths.remove(tup)
         env.shellCopy(*zip(*backup_paths))
@@ -2230,8 +2234,8 @@ class SaveInfo(FileInfo):
         """Returns CoSaves instance corresponding to self."""
         return CoSaves(self.getPath())
 
-    def backup_paths(self):
-        save_paths = super(SaveInfo, self).backup_paths() # type: list[tuple]
+    def backup_paths(self, first=False):
+        save_paths = super(SaveInfo, self).backup_paths(first)
         save_paths.extend(CoSaves.get_new_paths(*save_paths[0]))
         return save_paths
 