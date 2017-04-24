wrye-bash/wrye-bash

commit 0bdf6449e9ec7f81d2bcbf4349c93aba8b6541e6

Reworking setOblivionVersion to drop modInfos refresh:
This was triggered by me realizing that I needed to refresh with
load_order.Unlock, but decided to solve it at the root. We should
eliminate _all_ occurrences of refresh outside the data model (under

- ghosting. Ghost state won't be preserved for renamed master if
autoGhost is False, but that's not important. What's important is
(further) reworking lo API to always unghost if needed (eliminate
setGhost calls).
- our pre alpha file system APIs. That's partly due to Path API (#368)
and partly to the hacked in SHFileOperation that should become the norm,
but error handling is a huge issue (see the ugly _retry in this context)
Should be moved inside env - #241

On the bright side: self.factory dropped, performance increased by an
order of magnitude and basher.saves_links.Saves_Profiles._ProfileLink
swapPluginsAndMasterVersion > setOblivionVersion call should now
properly refresh (oops).

Mopy/bash/bosh/__init__.py
@@ -3653,56 +3653,81 @@ class ModInfos(FileInfos):
             + u'\n\n' + _(u'Try again?') % (old.s, new.s, old.s),
             _(u'File in use'))
 
-    def setOblivionVersion(self,newVersion):
-        """Swaps Oblivion.esm to to specified version."""
-        #--Old info
-        baseName = self.masterName
+    def _get_version_paths(self, newVersion):
+        baseName = self.masterName # Oblivion.esm, say it's currently SI one
         newSize = self.version_voSize[newVersion]
         oldSize = self[baseName].size
-        if newSize == oldSize: return
+        if newSize == oldSize: return None, None
         if oldSize not in self.size_voVersion:
             raise StateError(u"Can't match current main ESM to known version.")
-        oldName = GPath(baseName.sbody+u'_'+self.size_voVersion[oldSize]+u'.esm')
+        oldName = GPath( # Oblivion_SI.esm: we will rename Oblivion.esm to this
+            baseName.sbody + u'_' + self.size_voVersion[oldSize] + u'.esm')
         if self.store_dir.join(oldName).exists():
             raise StateError(u"Can't swap: %s already exists." % oldName)
-        newName = GPath(baseName.sbody+u'_'+newVersion+u'.esm')
+        newName = GPath(baseName.sbody + u'_' + newVersion + u'.esm')
         if newName not in self.data:
             raise StateError(u"Can't swap: %s doesn't exist." % newName)
-        #--Rename
-        baseInfo = self[baseName]
+        return newName, oldName
+
+    def setOblivionVersion(self,newVersion):
+        """Swaps Oblivion.esm to to specified version."""
+        # if new version is u'1.1' then newName is Path(Oblivion_1.1.esm)
+        newName, oldName = self._get_version_paths(newVersion)
+        if newName is None: return
         newInfo = self[newName]
-        basePath = baseInfo.getPath()
-        newPath = newInfo.getPath()
-        oldPath = self.store_dir.join(oldName)
-        try:
-            basePath.moveTo(oldPath)
-        except OSError as werr:
-            while werr.errno == errno.EACCES and self._retry(basePath,oldPath):
-                try:
-                    basePath.moveTo(oldPath)
-                except OSError as werr:
-                    continue
-                break
-            else:
-                raise
-        try:
-            newPath.moveTo(basePath)
-        except OSError as werr:
-            while werr.errno == errno.EACCES and self._retry(newPath,basePath):
-                try:
-                    newPath.moveTo(basePath)
-                except OSError as werr:
-                    continue
-                break
-            else:
+        #--Rename
+        baseInfo = self[self.masterName]
+        master_time = baseInfo.mtime
+        new_info_time = newInfo.mtime
+        is_master_active = load_order.cached_is_active(self.masterName)
+        is_new_info_active = load_order.cached_is_active(newName)
+        # can't use ModInfos rename cause it will mess up the load order
+        rename_operation = super(ModInfos, self)._rename_operation
+        first_try = True
+        while first_try or (werr.errno == errno.EACCES and
+                self._retry(baseInfo.getPath(), self.store_dir.join(oldName))):
+            first_try = False
+            try:
+                rename_operation(self.masterName, oldName)
+            except OSError as werr: # can only occur if SHFileOperation
+                # isn't called, yak - file operation API badly needed
+                continue
+            except CancelError:
+                return
+            break
+        else:
+            raise
+        first_try = True
+        while first_try or (werr.errno == errno.EACCES and
+                self._retry(newInfo.getPath(), baseInfo.getPath())):
+            first_try = False
+            try:
+                rename_operation(newName, self.masterName)
+            except OSError as werr:
+                continue
+            except CancelError:
                 #Undo any changes
-                oldPath.moveTo(basePath)
-                raise
-        basePath.mtime = baseInfo.mtime
-        oldPath.mtime = newInfo.mtime
-        if newInfo.isGhost:
-            oldInfo = self.factory(self.store_dir, oldName) # type: ModInfo
-            oldInfo.setGhost(True)
+                rename_operation(oldName, self.masterName)
+                # return
+            break
+        else:
+            #Undo any changes
+            rename_operation(oldName, self.masterName)
+            raise
+        # set mtimes to previous respective values
+        self[self.masterName].setmtime(master_time)
+        self[oldName].setmtime(new_info_time)
+        oldIndex = self._lo_wip.index(newName)
+        self._lo_caches_remove_mods([newName])
+        self._lo_wip.insert(oldIndex, oldName)
+        def _activate(active, mod):
+            if active: self[mod].setGhost(False) # needed if autoGhost is False
+            (self.lo_activate if active else self.lo_deactivate)(mod,
+                                                                 doSave=False)
+        _activate(is_new_info_active, oldName)
+        _activate(is_master_active, self.masterName)
+        # Save to disc (load order and plugins.txt)
+        self.cached_lo_save_all() # sets ghost as needed iff autoGhost is True
         self.voCurrent = newVersion
 
     def swapPluginsAndMasterVersion(self, arcSaves, newSaves):

Mopy/bash/basher/mods_links.py
@@ -198,7 +198,6 @@ class Mods_OblivionVersion(CheckLink, EnabledLink):
         """Handle selection."""
         if bosh.modInfos.voCurrent == self.key: return
         bosh.modInfos.setOblivionVersion(self.key)
-        bosh.modInfos.refresh()
         self.window.RefreshUI(refreshSaves=True) # True: refresh save's masters
         if self.setProfile:
             bosh.saveInfos.profiles.setItem(bosh.saveInfos.localSave,'vOblivion',self.key)

commit ac5198dc846420bf8717b5a3c26a5bb6f85408d3

BAIN small fixups:
Performance - stat filesystem once. Plus ext needs only be calculated
for `if not rsDir` path (as needs sLower). Moreover, ext should be
calculated after getGhost - this however makes us recalculate the crc
for ghosts which we didn't do before - we should avoid recalculating
crcs for mods altogether, use modInfos cached one - problem is that this
is not updated correctly - see #336.
Note also the test `if sFileLower in bethFiles` would use the ghost.
Mopy/bash/bosh/__init__.py:
Small regression introduced in 0bdf6449e9ec7f81d2bcbf4349c93aba8b6541e6,
return must not be commented.

Under #219

Mopy/bash/bosh/__init__.py
@@ -3706,7 +3706,7 @@ class ModInfos(FileInfos):
             except CancelError:
                 #Undo any changes
                 rename_operation(oldName, self.masterName)
-                # return
+                return
             break
         else:
             #Undo any changes

Mopy/bash/bosh/bain.py
@@ -1287,10 +1287,9 @@ class InstallerProject(Installer):
                 rpFile = GPath(os.path.join(rsDir, sFile))
                 asFile = os.path.join(asDir, sFile)
                 # below calls may now raise even if "werr.winerror = 123"
-                size = os.path.getsize(asFile)
-                get_mtime = os.path.getmtime(asFile)
-                max_mtime = max_mtime if max_mtime >= get_mtime else get_mtime
-                date = int(get_mtime)
+                lstat = os.lstat(asFile)
+                size, date = lstat.st_size, int(lstat.st_mtime)
+                max_mtime = max_mtime if max_mtime >= date else date
                 oSize, oCrc, oDate = oldGet(rpFile, (0, 0, 0))
                 if size == oSize and date == oDate:
                     new_sizeCrcDate[rpFile] = (oSize, oCrc, oDate, asFile)
@@ -1939,22 +1938,20 @@ class InstallersData(DataStore):
             progress(index)
             rsDir = asDir[relPos:]
             for sFile in sFiles:
-                sFileLower = sFile.lower()
-                ext = sFileLower[sFileLower.rfind(u'.'):]
                 top_level_espm = False
                 if not rsDir:
+                    rpFile = GPath(sFile) # relative Path to file
+                    rpFile = ghostGet(rpFile, rpFile)
+                    ext = rpFile.cs[rpFile.s.rfind(u'.'):]
                     if ext in skipExts: continue
-                    if sFileLower in bethFiles: continue
+                    if rpFile.cs in bethFiles: continue
                     top_level_espm = ext in {u'.esp', u'.esm'}
-                    rpFile = GPath(os.path.join(rsDir, sFile))
-                    rpFile = ghostGet(rpFile,rpFile)
                 else: rpFile = GPath(os.path.join(rsDir, sFile))
                 asFile = os.path.join(asDir, sFile)
                 # below calls may now raise even if "werr.winerror = 123"
                 try:
-                    size = os.path.getsize(asFile)
-                    get_mtime = os.path.getmtime(asFile)
-                    date = int(get_mtime)
+                    lstat = os.lstat(asFile)
+                    size, date = lstat.st_size, int(lstat.st_mtime)
                     oSize, oCrc, oDate = oldGet(rpFile, (0, 0, 0))
                     if top_level_espm or size != oSize or date != oDate:
                         pending[rpFile] = (size, oCrc, date, asFile)