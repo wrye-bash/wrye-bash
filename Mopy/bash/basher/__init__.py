wrye-bash/wrye-bash

commit 3b8c9ac3de222da3be60c514be24da879f639d6d

Define bolt aliases for struct.pack/unpack:
Utumno - cleaned up a bit and fixed imports - thanks @BeermotorWB !
This chops off most uses of unpack/pack, opening the way to refactor
RecordHeader.
Mopy/bash/brec.py: netString() was unused
Some uses that remain in _saves.py will be pruned in cosaves commit.

Mopy/bash/bosh/__init__.py
@@ -51,7 +51,7 @@ from .. import patcher # for configIsCBash()
 from ..archives import readExts
 from ..bass import dirs, inisettings, tooldirs
 from ..bolt import GPath, DataDict, cstrip, deprint, sio, Path, decode, \
-    unpack_many, unpack_byte
+    unpack_many, unpack_byte, struct_pack, struct_unpack
 from ..brec import MreRecord, ModReader
 from ..cint import CBashApi
 from ..exception import AbstractError, ArgumentError, BoltError, BSAError, \
@@ -277,7 +277,7 @@ class BsaFile:
                     if hash != trueHash:
                         #print ' ',fileName,'\t',hex(hash-trueHash),hex(hash),hex(trueHash)
                         ios.seek(filePos)
-                        ios.write(struct.pack('Q',trueHash))
+                        ios.write(struct_pack('Q',trueHash))
                         resetCount += 1
         #--Done
         self.resetOblivionBSAMTimes()
@@ -598,10 +598,10 @@ class ModInfo(FileInfo):
             raise ArgumentError
         with self.getPath().open('r+b') as modFile:
             modFile.seek(8)
-            flags1 = MreRecord.flags1_(struct.unpack('I', modFile.read(4))[0])
+            flags1 = MreRecord.flags1_(struct_unpack('I', modFile.read(4))[0])
             flags1.esm = (esm_or_esp == u'esm')
             modFile.seek(8)
-            modFile.write(struct.pack('=I',int(flags1)))
+            modFile.write(struct_pack('=I', int(flags1)))
         self.header.flags1 = flags1
         self.setmtime(crc_changed=True)
 

Mopy/bash/bosh/bsa_files.py
@@ -40,7 +40,7 @@ from functools import partial
 from itertools import groupby, imap
 from operator import itemgetter
 from . import AFile
-from ..bolt import deprint
+from ..bolt import deprint, struct_unpack
 from ..exception import BSAError, BSADecodingError, BSAFlagError, \
     BSANotImplemented
 
@@ -65,7 +65,7 @@ class _Header(object):
 
     def load_header(self, ins):
         for fmt, attr in zip(_Header.formats, _Header.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
         # error checking
         if self.file_id != self.__class__.bsa_magic:
             raise BSAError(u'Magic wrong: %r' % self.file_id)
@@ -82,7 +82,7 @@ class BsaHeader(_Header):
     def load_header(self, ins):
         super(BsaHeader, self).load_header(ins)
         for fmt, attr in zip(BsaHeader.formats, BsaHeader.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
         # error checking
         if self.folder_records_offset != self.__class__.header_size:
             raise BSAError(u'Header size wrong: %r. Should be %r' % (
@@ -112,7 +112,7 @@ class Ba2Header(_Header):
     def load_header(self, ins):
         super(Ba2Header, self).load_header(ins)
         for fmt, attr in zip(Ba2Header.formats, Ba2Header.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
         # error checking
         if not self.b2a_files_type in self.file_types:
             raise BSAError(u'Unrecognised file types: %r. Should be %s' % (
@@ -138,11 +138,11 @@ class _HashedRecord(object):
 
     def load_record(self, ins):
         fmt, fmt_siz = _HashedRecord.formats[0]
-        self.hash, = struct.unpack(fmt, ins.read(fmt_siz))
+        self.hash, = struct_unpack(fmt, ins.read(fmt_siz))
 
     def load_record_from_buffer(self, memview, start):
         fmt, fmt_siz = _HashedRecord.formats[0]
-        self.hash, = struct.unpack_from(fmt, memview, start)
+        self.hash, = struct_unpack_from(fmt, memview, start)
         return start + fmt_siz
 
     @classmethod
@@ -172,7 +172,7 @@ class _BsaHashedRecord(_HashedRecord):
     def load_record(self, ins):
         super(_BsaHashedRecord, self).load_record(ins)
         for fmt, attr in zip(self.__class__.formats, self.__class__.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
 
     def load_record_from_buffer(self, memview, start):
         start = super(_BsaHashedRecord, self).load_record_from_buffer(memview,
@@ -217,10 +217,10 @@ class _B2aFileRecordCommon(_HashedRecord):
     formats = list((f, struct.calcsize(f)) for f in formats)
 
     def load_record(self, ins):
-        self.hash, = struct.unpack('I', ins.read(4)) # hash is I not Q !
+        self.hash, = struct_unpack('I', ins.read(4)) # hash is I not Q !
         for fmt, attr in zip(_B2aFileRecordCommon.formats,
                              _B2aFileRecordCommon.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
 
     def load_record_from_buffer(self, memview, start):
         start = super(_B2aFileRecordCommon, self).load_record_from_buffer(
@@ -247,7 +247,7 @@ class B2aFileRecordGeneral(_B2aFileRecordCommon):
         super(B2aFileRecordGeneral, self).load_record(ins)
         for fmt, attr in zip(B2aFileRecordGeneral.formats,
                              B2aFileRecordGeneral.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
 
 class B2aFileRecordTexture(_B2aFileRecordCommon):
     __slots__ = ('unk0C', 'num_of_chunks', 'chunk_header_size', 'height',
@@ -259,7 +259,7 @@ class B2aFileRecordTexture(_B2aFileRecordCommon):
         super(B2aFileRecordTexture, self).load_record(ins)
         for fmt, attr in zip(B2aFileRecordGeneral.formats,
                              B2aFileRecordGeneral.__slots__):
-            self.__setattr__(attr, struct.unpack(fmt[0], ins.read(fmt[1]))[0])
+            self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
 
 # Bsa content abstraction -----------------------------------------------------
 class BSAFolder(object):
@@ -339,7 +339,7 @@ class ABsa(AFile):
                     data_size = record.raw_data_size()
                     bsa_file.seek(record.raw_file_data_offset)
                     if self.bsa_header.embed_filenames(): # use len(filename) ?
-                        filename_len, = struct.unpack('B', bsa_file.read(1))
+                        filename_len, = struct_unpack('B', bsa_file.read(1))
                         bsa_file.read(filename_len) # discard filename
                         data_size -= filename_len + 1
                     # get the data!
@@ -442,9 +442,9 @@ class BSA(ABsa):
             for folder_record in folder_records:
                 folder_path = u'?%d' % folder_record.hash # hack - untested
                 if self.bsa_header.has_names_for_folders():
-                    name_size = struct.unpack('B', bsa_file.read(1))[0]
+                    name_size = struct_unpack('B', bsa_file.read(1))[0]
                     folder_path = _decode_path(
-                        struct.unpack('%ds' % (name_size - 1),
+                        struct_unpack('%ds' % (name_size - 1),
                                       bsa_file.read(name_size - 1))[0])
                     total_names_length += name_size
                     bsa_file.read(1) # discard null terminator

Mopy/bash/basher/saves_links.py
@@ -28,13 +28,12 @@ attribute points to BashFrame.saveList singleton."""
 import StringIO
 import re
 import shutil
-import struct
 from . import BashFrame
 from .dialogs import ImportFaceDialog
 from .. import bass, bosh, bolt, balt, bush, parsers, load_order
 from ..balt import EnabledLink, AppendableLink, Link, CheckLink, ChoiceLink, \
     ItemLink, SeparatorLink, OneItemLink, Image, UIList_Rename
-from ..bolt import GPath, SubProgress, formatInteger
+from ..bolt import GPath, SubProgress, formatInteger, struct_pack, struct_unpack
 from ..bosh import faces
 from ..exception import ArgumentError, BoltError, CancelError, ModError
 
@@ -660,7 +659,7 @@ class Save_RepairAbomb(OneItemLink):
         saveFile.load()
         (tcSize,abombCounter,abombFloat) = saveFile.getAbomb()
         #--Continue?
-        progress = 100*abombFloat/struct.unpack('f',struct.pack('I',0x49000000))[0]
+        progress = 100*abombFloat/struct_unpack('f', struct_pack('I',0x49000000))[0]
         newCounter = 0x41000000
         if abombCounter <= newCounter:
             self._showOk(_(u'Abomb counter is too low to reset.'))

Mopy/bash/bish.py
@@ -46,7 +46,6 @@ Rational Names:
 import os
 import re
 import string
-import struct
 import StringIO
 import sys
 from subprocess import Popen, PIPE
@@ -111,7 +110,7 @@ if ret != False: # False == success
     else:
         bush.setGame(ret[0])
 import bosh
-from bolt import GPath, Path, mainfunc
+from bolt import GPath, Path, mainfunc, struct_unpack
 
 indent = 0
 longest = 0
@@ -231,10 +230,10 @@ def readRecord(record, melSet=0, skipLabel=0):
         elif isinstance(item,float):
             print report, round(item,6)
         elif attr in ['unk1','unk2','unk3','unk4','unk5','unk6']:
-            if sum(struct.unpack(`len(item)`+'b',item)) == 0:
+            if sum(struct_unpack(`len(item)`+'b', item)) == 0:
                 print report, 'Null'
             else:
-                print report, struct.unpack(`len(item)`+'b',item)
+                print report, struct_unpack(`len(item)`+'b', item)
         elif isinstance(item, basestring):
             if len(item.splitlines()) > 1:
                 item = item.splitlines()
@@ -242,7 +241,7 @@ def readRecord(record, melSet=0, skipLabel=0):
                 for line in item:
                     readRecord(line,[attr],1)
             else:
-                if sum(struct.unpack(`len(item)`+'b',item)) == 0:
+                if sum(struct_unpack(`len(item)`+'b', item)) == 0:
                     print report, ''
                 else:
                     print report, item
@@ -716,7 +715,7 @@ def getIds(fileName=None):
             return (ins,ins.tell()+size)
         else:
             import zlib
-            sizeCheck, = struct.unpack('I',ins.read(4))
+            sizeCheck, = struct_unpack('I', ins.read(4))
             decomp = zlib.decompress(ins.read(size-4))
             if len(decomp) != sizeCheck:
                 raise exception.ModError(self.inName,

Mopy/bash/bolt.py
@@ -52,6 +52,10 @@ import bass
 import chardet
 import exception
 
+# structure aliases, mainly introduced to reduce uses of 'pack' and 'unpack'
+struct_pack = struct.pack
+struct_unpack = struct.unpack
+
 close_fds = True
 #-- To make commands executed with Popen hidden
 startupinfo = None
@@ -1615,22 +1619,22 @@ class Settings(DataDict):
         return self.data.pop(key,default)
 
 # Structure wrappers ----------------------------------------------------------
-def unpack_str8(ins): return ins.read(struct.unpack('B', ins.read(1))[0])
-def unpack_str16(ins): return ins.read(struct.unpack('H', ins.read(2))[0])
-def unpack_str32(ins): return ins.read(struct.unpack('I', ins.read(4))[0])
-def unpack_int(ins): return struct.unpack('I', ins.read(4))[0]
-def unpack_short(ins): return struct.unpack('H', ins.read(2))[0]
-def unpack_float(ins): return struct.unpack('f', ins.read(4))[0]
-def unpack_byte(ins): return struct.unpack('B', ins.read(1))[0]
-def unpack_int_signed(ins): return struct.unpack('i', ins.read(4))[0]
-def unpack_int64_signed(ins): return struct.unpack('q', ins.read(8))[0]
-def unpack_4s(ins): return struct.unpack('4s', ins.read(4))[0]
+def unpack_str8(ins): return ins.read(struct_unpack('B', ins.read(1))[0])
+def unpack_str16(ins): return ins.read(struct_unpack('H', ins.read(2))[0])
+def unpack_str32(ins): return ins.read(struct_unpack('I', ins.read(4))[0])
+def unpack_int(ins): return struct_unpack('I', ins.read(4))[0]
+def unpack_short(ins): return struct_unpack('H', ins.read(2))[0]
+def unpack_float(ins): return struct_unpack('f', ins.read(4))[0]
+def unpack_byte(ins): return struct_unpack('B', ins.read(1))[0]
+def unpack_int_signed(ins): return struct_unpack('i', ins.read(4))[0]
+def unpack_int64_signed(ins): return struct_unpack('q', ins.read(8))[0]
+def unpack_4s(ins): return struct_unpack('4s', ins.read(4))[0]
 
 def unpack_string(ins, string_len):
-    return struct.unpack('%ds' % string_len, ins.read(string_len))[0]
+    return struct_unpack('%ds' % string_len, ins.read(string_len))[0]
 
 def unpack_many(ins, fmt):
-    return struct.unpack(fmt, ins.read(struct.calcsize(fmt)))
+    return struct_unpack(fmt, ins.read(struct.calcsize(fmt)))
 
 #------------------------------------------------------------------------------
 class TableColumn:

Mopy/bash/bosh/_saves.py
@@ -32,7 +32,7 @@ from operator import attrgetter
 from .. import bolt, bush
 from ..bolt import Flags, sio, GPath, decode, deprint, encode, cstrip, \
     SubProgress, unpack_byte, unpack_str8, unpack_many, unpack_int, \
-    unpack_short
+    unpack_short, struct_pack, struct_unpack
 from ..brec import ModReader, MreRecord, ModWriter, getObjectIndex, \
     getFormIndices
 from ..exception import FileError, ModError, StateError
@@ -80,7 +80,7 @@ class SreNPC(object):
         """Loads variables from data."""
         with sio(data) as ins:
             def _unpack(fmt, fmt_siz):
-                return struct.unpack(fmt, ins.read(fmt_siz))
+                return struct_unpack(fmt, ins.read(fmt_siz))
             flags = SreNPC.flags(flags)
             if flags.form:
                 self.form, = _unpack('I',4)
@@ -123,7 +123,7 @@ class SreNPC(object):
         """Returns self.data."""
         with sio() as out:
             def _pack(fmt, *args):
-                out.write(struct.pack(fmt, *args))
+                out.write(struct_pack(fmt, *args))
             #--Form
             if self.form is not None:
                 _pack('I',self.form)
@@ -241,7 +241,7 @@ class PluggyFile:
         size = self.path.size
         with self.path.open('rb') as ins:
             buff = ins.read(size-4)
-            crc32, = struct.unpack('=i',ins.read(4))
+            crc32, = struct_unpack('=i', ins.read(4))
         crcNew = binascii.crc32(buff)
         if crc32 != crcNew:
             raise FileError(self.path.tail,
@@ -250,7 +250,7 @@ class PluggyFile:
         #--Header
         with sio(buff) as ins:
             def _unpack(fmt, fmt_siz):
-                return struct.unpack(fmt, ins.read(fmt_siz))
+                return struct_unpack(fmt, ins.read(fmt_siz))
             if ins.read(10) != 'PluggySave':
                 raise FileError(self.path.tail, u'File tag != "PluggySave"')
             self.version, = _unpack('I',4)
@@ -271,7 +271,7 @@ class PluggyFile:
                 self._plugins.append((espid, index, modName))
             #--Other
             self.other = ins.getvalue()[ins.tell():]
-        deprint(struct.unpack('I',self.other[-4:]),self.path.size-8)
+        deprint(struct_unpack('I', self.other[-4:]), self.path.size-8)
         #--Done
         self.valid = True
 
@@ -284,7 +284,7 @@ class PluggyFile:
         with sio() as buff:
             #--Save
             def _pack(fmt, *args):
-                buff.write(struct.pack(fmt, *args))
+                buff.write(struct_pack(fmt, *args))
             buff.write('PluggySave')
             _pack('=I',self.version)
             #--Plugins
@@ -305,7 +305,7 @@ class PluggyFile:
             text = buff.getvalue()
             with path.open('wb') as out:
                 out.write(text)
-                out.write(struct.pack('i',binascii.crc32(text)))
+                out.write(struct_pack('i', binascii.crc32(text)))
         path.mtime = mtime
 
     def safeSave(self):
@@ -556,7 +556,7 @@ class SaveFile:
         outPath = outPath or self.fileInfo.getPath()
         with outPath.open('wb') as out:
             def _pack(fmt, *data):
-                out.write(struct.pack(fmt, *data))
+                out.write(struct_pack(fmt, *data))
             #--Progress
             progress = progress or bolt.Progress()
             progress.setFull(self.fileInfo.size)
@@ -727,7 +727,7 @@ class SaveFile:
         log(_(u'  Float:\t%.2f') % abombFloat)
         #--FBomb
         log.setHeader(_(u'Fbomb Counter'))
-        log(_(u'  Next in-game object: %08X') % struct.unpack('I',self.preGlobals[:4]))
+        log(_(u'  Next in-game object: %08X') % struct_unpack('I', self.preGlobals[:4]))
         #--Array Sizes
         log.setHeader(u'Array Sizes')
         log(u'  %d\t%s' % (len(self.created),_(u'Created Items')))
@@ -782,7 +782,7 @@ class SaveFile:
                     knownTypes.add(type)
             #--Obj ref parents
             if type == 49 and mod == 255 and (flags & 2):
-                iref, = struct.unpack('I',data[4:8])
+                iref, = struct_unpack('I', data[4:8])
                 count,cumSize = objRefBases.get(iref,(0,0))
                 count += 1
                 cumSize += len(data) + 12
@@ -1085,7 +1085,7 @@ class SaveFile:
         for record in self.records:
             fid,recType,flags,version,data = record
             if recType == 49 and fid >> 24 == 0xFF and (flags & 2):
-                iref, = struct.unpack('I',data[4:8])
+                iref, = struct_unpack('I', data[4:8])
                 if iref >> 24 != 0xFF and fids[iref] == 0:
                     nullRefCount += 1
             progress.plus()
@@ -1122,7 +1122,7 @@ class SaveFile:
             if fid in uncreated:
                 numUnCreChanged += 1
             elif removeNullRefs and recType == 49 and fid >> 24 == 0xFF and (flags & 2):
-                iref, = struct.unpack('I',data[4:8])
+                iref, = struct_unpack('I', data[4:8])
                 if iref >> 24 != 0xFF and fids[iref] == 0:
                     numUnNulled += 1
                 else:
@@ -1144,21 +1144,21 @@ class SaveFile:
     def getAbomb(self):
         """Gets animation slowing counter(?) value."""
         data = self.preCreated
-        tesClassSize, = struct.unpack('H',data[:2])
+        tesClassSize, = struct_unpack('H', data[:2])
         abombBytes = data[2+tesClassSize-4:2+tesClassSize]
-        abombCounter, = struct.unpack('I',abombBytes)
-        abombFloat, = struct.unpack('f',abombBytes)
+        abombCounter, = struct_unpack('I', abombBytes)
+        abombFloat, = struct_unpack('f', abombBytes)
         return tesClassSize,abombCounter,abombFloat
 
     def setAbomb(self,value=0x41000000):
         """Resets abomb counter to specified value."""
         data = self.preCreated
-        tesClassSize, = struct.unpack('H',data[:2])
+        tesClassSize, = struct_unpack('H', data[:2])
         if tesClassSize < 4: return
         with sio() as buff:
             buff.write(data)
             buff.seek(2+tesClassSize-4)
-            buff.write(struct.pack('I',value))
+            buff.write(struct_pack('I', value))
             self.preCreated = buff.getvalue()
 
 #------------------------------------------------------------------------------

commit 8bdd05e60c2a296f0364df929bced7f82d9cacb3

Regressions and bsa fixups:
See:
https://afkmods.iguanadons.net/index.php?/topic/4966-wrye-bash-all-games/&do=findComment&comment=170339
That traceback was because of the typo in bsa_files.py (fixed another one
too introduced in 3b8c9ac3de222da3be60c514be24da879f639d6d). However the
logic was flawed as the texture ba2 was still passed for full loading,
although it had no assets in it. This resulted in BSANotImplemented
being thrown on extract_asssets - we could consider throwing this
in load (so the texture bsa was skipped altogether) but kept it -
till we hopefully code that - #339.

Mopy/bash/bosh/__init__.py: was IOError not OSError, duh, see:
https://www.python.org/dev/peps/pep-3151

Mopy/bash/basher/__init__.py: leftovers from #236 (see
0a300af01a85bb3535d3194203fd34cc0d3e9b29).

Mopy/bash/bosh/bsa_files.py: refactoring (using struct wrappers,
_load_bsa_light made private, common code)

Mopy/bash/basher/__init__.py
@@ -325,11 +325,6 @@ class MasterList(_ModsUIList):
     message = _(u"Edit/update the masters list? Note that the update process "
                 u"may automatically rename some files. Be sure to review the "
                 u"changes before saving.")
-    seName = bush.game.se.shortName
-    saves192warn = u'\n\n' + _(u"Note that %(scrExtender)s cosaves are NOT "
-            u"supported, meaning that if you have a save that has a cosave "
-            u"the masters in the cosave WON'T be updated leading to "
-            u"crashes/lost information and whatnot." % {'scrExtender': seName})
 
     def __init__(self, parent, listData=None, keyPrefix=keyPrefix, panel=None,
                  detailsPanel=None):
@@ -339,8 +334,6 @@ class MasterList(_ModsUIList):
         self.fileInfo = None
         self.loadOrderNames = [] # cache, orders missing last alphabetically
         self._allowEditKey = keyPrefix + '.allowEdit'
-        if isinstance(detailsPanel, SaveDetails): # yak ! fix #192
-            self.message += self.saves192warn
         #--Parent init
         super(MasterList, self).__init__(parent,
                       listData=listData if listData is not None else {},

Mopy/bash/bosh/__init__.py
@@ -857,6 +857,7 @@ class ModInfo(FileInfo):
                     deprint(u'Failed to parse %s' % bsa_info.name,
                             traceback=True)
                     continue
+                if not found_assets: continue
                 bsa_assets[bsa_info] = found_assets
                 #extract contains Paths that compare equal to lowercase strings
                 extract -= set(imap(unicode.lower, found_assets))
@@ -1149,9 +1150,9 @@ class SaveInfo(FileInfo):
         cosave_path = self.getPath().root + u'.' + bush.game.se.shortName.lower()
         try:
             return self._cosave_type(cosave_path) # type: cosaves.ACoSaveFile
-        except (OSError, FileError) as e:
+        except (OSError, IOError, FileError) as e:
             if isinstance(e, FileError) or (
-                        isinstance(e, OSError) and e.errno != errno.ENOENT):
+                isinstance(e, (OSError, IOError)) and e.errno != errno.ENOENT):
                 deprint(u'Failed to open %s' % cosave_path, traceback=True)
             return None
 

Mopy/bash/bosh/bsa_files.py
@@ -40,7 +40,8 @@ from functools import partial
 from itertools import groupby, imap
 from operator import itemgetter
 from . import AFile
-from ..bolt import deprint, struct_unpack
+from ..bolt import deprint, struct_unpack, unpack_byte, unpack_string, \
+    unpack_int
 from ..exception import BSAError, BSADecodingError, BSAFlagError, \
     BSANotImplemented
 
@@ -142,7 +143,7 @@ class _HashedRecord(object):
 
     def load_record_from_buffer(self, memview, start):
         fmt, fmt_siz = _HashedRecord.formats[0]
-        self.hash, = struct_unpack_from(fmt, memview, start)
+        self.hash, = struct.unpack_from(fmt, memview, start)
         return start + fmt_siz
 
     @classmethod
@@ -217,7 +218,7 @@ class _B2aFileRecordCommon(_HashedRecord):
     formats = list((f, struct.calcsize(f)) for f in formats)
 
     def load_record(self, ins):
-        self.hash, = struct_unpack('I', ins.read(4)) # hash is I not Q !
+        self.hash = unpack_int(ins) # hash is I not Q !
         for fmt, attr in zip(_B2aFileRecordCommon.formats,
                              _B2aFileRecordCommon.__slots__):
             self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
@@ -257,8 +258,8 @@ class B2aFileRecordTexture(_B2aFileRecordCommon):
 
     def load_record(self, ins):
         super(B2aFileRecordTexture, self).load_record(ins)
-        for fmt, attr in zip(B2aFileRecordGeneral.formats,
-                             B2aFileRecordGeneral.__slots__):
+        for fmt, attr in zip(B2aFileRecordTexture.formats,
+                             B2aFileRecordTexture.__slots__):
             self.__setattr__(attr, struct_unpack(fmt[0], ins.read(fmt[1]))[0])
 
 # Bsa content abstraction -----------------------------------------------------
@@ -275,6 +276,13 @@ class Ba2Folder(object):
         self.folder_assets = collections.OrderedDict() # keep files order
 
 # Files -----------------------------------------------------------------------
+def _makedirs_exists_ok(target_dir):
+    try:
+        os.makedirs(target_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+
 class ABsa(AFile):
     """:type bsa_folders: collections.OrderedDict[unicode, BSAFolder]"""
     header_type = BsaHeader
@@ -293,7 +301,7 @@ class ABsa(AFile):
             if not names_only:
                 self._load_bsa()
             else:
-                self.load_bsa_light()
+                self._load_bsa_light()
         except struct.error as e:
             raise BSAError, e.message, sys.exc_info()[2]
 
@@ -326,11 +334,7 @@ class ABsa(AFile):
         with open(u'%s' % self.abs_path, 'rb') as bsa_file:
             for folder, file_records in folder_to_assets.iteritems():
                 target_dir = os.path.join(dest_folder, folder)
-                try:
-                    os.makedirs(target_dir)
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise
+                _makedirs_exists_ok(target_dir)
                 for filename, record in file_records:
                     if global_compression ^ record.compression_toggle():
                         raise BSANotImplemented(
@@ -339,7 +343,7 @@ class ABsa(AFile):
                     data_size = record.raw_data_size()
                     bsa_file.seek(record.raw_file_data_offset)
                     if self.bsa_header.embed_filenames(): # use len(filename) ?
-                        filename_len, = struct_unpack('B', bsa_file.read(1))
+                        filename_len = unpack_byte(bsa_file)
                         bsa_file.read(filename_len) # discard filename
                         data_size -= filename_len + 1
                     # get the data!
@@ -362,9 +366,9 @@ class ABsa(AFile):
 
     # Abstract
     def _load_bsa(self): raise NotImplementedError
-    def load_bsa_light(self): raise NotImplementedError
+    def _load_bsa_light(self): raise NotImplementedError
 
-    # API
+    # API - delegates to abstract methods above
     def has_assets(self, asset_paths):
         return set((u'%s' % a).lower() for a in asset_paths) & self.assets
 
@@ -413,7 +417,7 @@ class BSA(ABsa):
             rec.load_record(bsa_file)
             file_records.append(rec)
 
-    def load_bsa_light(self):
+    def _load_bsa_light(self):
         folder_records = [] # we need those to parse the folder names
         _filenames = []
         path_folder_record = collections.OrderedDict()
@@ -442,10 +446,9 @@ class BSA(ABsa):
             for folder_record in folder_records:
                 folder_path = u'?%d' % folder_record.hash # hack - untested
                 if self.bsa_header.has_names_for_folders():
-                    name_size = struct_unpack('B', bsa_file.read(1))[0]
+                    name_size = unpack_byte(bsa_file)
                     folder_path = _decode_path(
-                        struct_unpack('%ds' % (name_size - 1),
-                                      bsa_file.read(name_size - 1))[0])
+                        unpack_string(bsa_file, name_size - 1))
                     total_names_length += name_size
                     bsa_file.read(1) # discard null terminator
                 read_file_records(bsa_file, folder_path, folder_record)
@@ -485,11 +488,7 @@ class BA2(ABsa):
         with open(u'%s' % self.abs_path, 'rb') as bsa_file:
             for folder, file_records in folder_to_assets.iteritems():
                 target_dir = os.path.join(dest_folder, folder)
-                try:
-                    os.makedirs(target_dir)
-                except OSError as e:
-                    if e.errno != errno.EEXIST:
-                        raise
+                _makedirs_exists_ok(target_dir)
                 for filename, record in file_records:
                     if record.packed_size: # seems to signify compression
                         raise BSANotImplemented(
@@ -538,7 +537,7 @@ class BA2(ABsa):
             current_folder.folder_assets[filename[folder_dex + 1:]] = \
                 file_records[index]
 
-    def load_bsa_light(self):
+    def _load_bsa_light(self):
         with open(u'%s' % self.abs_path, 'rb') as bsa_file:
             # load the header from input stream
             self.bsa_header.load_header(bsa_file)