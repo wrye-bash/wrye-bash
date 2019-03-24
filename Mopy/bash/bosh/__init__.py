wrye-bash/wrye-bash

commit 3214e6962093e770ec36dec43b315e05944f1bf6

Revise active mods limits for esls - hard limit of 4096 for now:
From @Infernio at acc80eb9214e2e18206282e323829f3e4fb9dd0a

So, the current knowledge on this is:

4096 is the theoretical limit on ESLs - that's how much space there
is in the FormID for them (8^4 = 4096).
However, testing shows that the vanilla game gives up at a much lower
limit. That limit can be raised to around 1800 ESLs with Engine
Fixes. At that point, the game runs out of stdio handles to use for
plugin files.
As for how Wrye Bash should handle this - not sure yet. I don't know
the exact number, but my suspicion is 2048 stdio handles - 254
regular plugins = 1794 ESLs, but I'm not sure if anyone has done
proper testing with that. For now, it's probably alright if WB just
sticks to the theoretical limit - but we should keep this in mind
once more testing is done.

For more information, see:

https://afkmods.iguanadons.net/index.php?/topic/5079-plugin-files-and-you-esmeslesp/
https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/setmaxstdio?view=vs-2017

Mopy/bash/bosh/__init__.py
@@ -441,8 +441,10 @@ class ModInfo(FileInfo):
         return int(self.header.flags1) & 1 == 1
 
     def is_esl(self):
-        # game seems not to care about the flag, at least for load order
-        return self.name.cext == u'.esl'
+        """Check if this is a light plugin - .esl files are automatically
+        set the light flag, for espms check the flag."""
+        return bush.game.has_esl and (
+                self.name.cext == u'.esl' or self.header.flags1.eslFile)
 
     def isInvertedMod(self):
         """Extension indicates esp/esm, but byte setting indicates opposite."""
@@ -2166,13 +2168,15 @@ class ModInfos(FileInfos):
         """Mutate _active_wip cache then save if needed."""
         if _activated is None: _activated = set()
         try:
-            minfo = self[fileName]
-            if not minfo.is_esl(): # don't check limit if activating an esl
-                acti_filtered_espm = [x for x in self._active_wip if
-                                      x.cext != u'.esl']
-                if len(acti_filtered_espm) == load_order.max_espms:
-                    raise PluginsFullError(u'%s: Trying to activate more than '
-                        u'%d espms' % (fileName,load_order.max_espms))
+            espms_extra, esls_extra = load_order.check_active_limit(
+                self._active_wip + [fileName])
+            if espms_extra or esls_extra:
+                msg = u'%s: Trying to activate more than ' % fileName
+                if espms_extra:
+                    msg += u'%d espms' % load_order.max_plugins()[0]
+                else:
+                    msg += u'%d light plugins' % load_order.max_plugins()[1]
+                raise PluginsFullError(msg)
             _children = (_children or tuple()) + (fileName,)
             if fileName in _children[:-1]:
                 raise BoltError(u'Circular Masters: ' +u' >> '.join(x.s for x in _children))
@@ -2182,7 +2186,7 @@ class ModInfos(FileInfos):
             #  Disabled for now
             ##if self[fileName].hasBadMasterNames():
             ##    return
-            for master in minfo.header.masters:
+            for master in self[fileName].header.masters:
                 if master in _modSet: self.lo_activate(master, False, _modSet,
                                                        _children, _activated)
             #--Select in plugins
@@ -2264,11 +2268,12 @@ class ModInfos(FileInfos):
         missingSet = modsSet - all_mods
         toSelect = modsSet - missingSet
         listToSelect = load_order.get_ordered(toSelect)
-        acti_filtered_espm = [x for x in listToSelect if x.cext != u'.esl']
-        skipped = acti_filtered_espm[load_order.max_espms:]
+        skipped_esms, skipped_esls = load_order.check_active_limit(
+            listToSelect)
+        skipped = skipped_esls | skipped_esms
         #--Save
         if skipped:
-            listToSelect = [x for x in listToSelect if x not in set(skipped)]
+            listToSelect = [x for x in listToSelect if x not in skipped]
         self.cached_lo_save_active(active=listToSelect)
         #--Done/Error Message
         message = u''

Mopy/bash/games.py
@@ -38,8 +38,6 @@ import bolt
 import env
 import exception
 
-max_espms = 255
-
 def _write_plugins_txt_(path, lord, active, _star):
     try:
         with path.open('wb') as out:
@@ -189,6 +187,8 @@ class Game(object):
 
     allow_deactivate_master = False
     must_be_active_if_present = ()
+    max_espms = 255
+    max_esls = 0
 
     def __init__(self, mod_infos, plugins_txt_path):
         super(Game, self).__init__()
@@ -458,7 +458,12 @@ class Game(object):
         # Check for duplicates
         fix_active.act_duplicates = self._check_for_duplicates(acti_filtered)
         # check if we have more than 256 active mods
-        self._check_active_limit(acti, acti_filtered, fix_active)
+        drop_espms, drop_esls = self.check_active_limit(acti_filtered)
+        disable = drop_espms | drop_esls
+        if disable: # chop off extra, update acti in place
+            acti[:] = [x for x in acti_filtered if x not in disable]
+            self.mod_infos.selectedExtra = fix_active.selectedExtra = [
+                x for x in acti_filtered if x in disable]
         before_reorder = acti # with overflowed plugins removed
         if self._order_fixed(acti):
             fix_active.act_reordered = (before_reorder, acti)
@@ -472,11 +477,8 @@ class Game(object):
             return True # changes, saved if loading plugins.txt
         return False # no changes, not saved
 
-    def _check_active_limit(self, acti, acti_filtered, fix_active):
-        if len(acti_filtered) > max_espms:
-            self.mod_infos.selectedExtra = fix_active.selectedExtra = \
-                acti_filtered[max_espms:]
-        acti[:] = acti_filtered[:max_espms] # chop off extra, update acti in place
+    def check_active_limit(self, acti_filtered):
+        return set(acti_filtered[self.max_espms:]), set()
 
     def _order_fixed(self, lord): return False
 
@@ -734,6 +736,8 @@ class TextfileGame(Game):
 
 class AsteriskGame(Game):
 
+    max_espms = 254
+    max_esls = 4096 # hard limit, game runs out of fds sooner, testing needed
     _ccc_filename = u''
 
     @property
@@ -815,15 +819,14 @@ class AsteriskGame(Game):
             return True
         return False
 
-    # esls
-    def _check_active_limit(self, acti, acti_filtered, fix_active):
-        acti_filtered_espm = [x for x in acti_filtered if x.cext != u'.esl']
-        if len(acti_filtered_espm) > max_espms:
-            self.mod_infos.selectedExtra = fix_active.selectedExtra = \
-                acti_filtered_espm[max_espms:]
-        disable = set(acti_filtered_espm[max_espms:])
-        # chop off extra, update acti in place
-        acti[:] = [x for x in acti_filtered if x not in disable]
+    def check_active_limit(self, acti_filtered):
+        acti_filtered_espm = []
+        acti_filtered_esl = []
+        for x in acti_filtered:
+            (acti_filtered_esl if self.mod_infos[
+                x].is_esl() else acti_filtered_espm).append(x)
+        return set(acti_filtered_espm[self.max_espms:]) , set(
+            acti_filtered_esl[self.max_esls:])
 
     # Asterisk game specific: plugins with fixed load order -------------------
     def _readd_in_lists(self, lo, active):

Mopy/bash/game/__init__.py
@@ -78,6 +78,7 @@ class GameInfo(object):
 
     def __init__(self, gamePath):
         self.gamePath = gamePath # absolute bolt Path to the game directory
+        self.has_esl = u'.esl' in self.espm_extensions
 
     # Construction Set information
     class cs(object):

Mopy/bash/load_order.py
@@ -59,10 +59,14 @@ locked = False
 warn_locked = False
 _lords_pickle = None # type: bolt.PickleDict
 
-max_espms = games.max_espms
-
 def in_master_block(minf):
     return _game_handle.in_master_block(minf) # minf is a master or mod info
+
+def check_active_limit(mods):
+    return _game_handle.check_active_limit(mods)
+
+def max_plugins():
+    return _game_handle.max_espms, _game_handle.max_esls
 
 def initialize_load_order_files():
     if bass.dirs['saveBase'] == bass.dirs['app']:

commit 8b5cee4ce406bcbd1acec2761fa947497db04a31

Several regression fixes, some miscellaneous fixes
This commit is a squashed version of several fixes that have been piling
up. A full list of the original commit messages follows:

-----------------------------------------------------------------------

Regression fix: Always fix active mods list

Fixes a regression introduced in 3214e6962093e770ec36dec43b315e05944f1bf6.

acti_filtered only contains those plugins from acti that are actually on
the disk. The previous code always ran

  acti[:] = acti_filtered[:max_espms]

while the new code only does the equivalent to that if more than 255
ESPs / 4096 ESLs are found. This causes an error and fails to sanitize
the plugins.txt file if an activated mod is manually deleted (i.e.
outside Wrye Bash) during runtime.

-----------------------------------------------------------------------

Set default Skyrim HEDR version to 1.7

The CK hasn't emitted entries with 0.94 since basically forever, so I
have no idea why Wrye Bash never got updated for this. This makes the
'New Mod' funcion create files with the correct 1.7 version (also
applies to empty Bashed Patch files).

-----------------------------------------------------------------------

Fix incorrect ConfigParser usage

This was causing a crash when the game's INI file is located inside the
game folder. The traceback looked like this:

Traceback (most recent call last):
  File "bash\bash.pyo", line 185, in main
  File "bash\bash.pyo", line 261, in _main
  File "bash\bash.pyo", line 383, in _detect_game
  File "bash\initialization.pyo", line 156, in init_dirs
  File "ConfigParser.pyo", line 242, in __init__
AttributeError: 'unicode' object has no attribute 'items'

The cause was that ConfigParser.__init__() doesn't actually take a
string - that string should be passed to ConfigParser.read() instead.

-----------------------------------------------------------------------

Also query string fields when retrieving exe versions

Mopy/bash/bosh/__init__.py
@@ -812,6 +812,10 @@ class ModInfo(FileInfo):
         :param resource_path: The path to the plugin-name-specific directory,
         as a list of path components.
         """
+        # If resource_path is empty, then we would effectively query
+        # self.dir.join(self.name), which always exists - that's the mod file!
+        if not resource_path:
+            return False
         return self.dir.join(*resource_path).join(self.name).exists()
 
 #------------------------------------------------------------------------------

Mopy/bash/bosh/_mergeability.py
@@ -49,9 +49,13 @@ def _is_mergeable_no_load(modInfo, reasons):
             facegen_dir_1 = _format_blocking_dir(bush.game.pnd.facegen_dir_1)
             facegen_dir_2 = _format_blocking_dir(bush.game.pnd.facegen_dir_2)
             voice_dir = _format_blocking_dir(bush.game.pnd.voice_dir)
-            bdirs = '\n'.join([facegen_dir_1, facegen_dir_2, voice_dir])
-            reasons.append(_(u'Has plugin-specific directory (one of\n' + bdirs
-                             + u').') % ({'plugin_name': modInfo.name.s}))
+            dir_list = u''
+            for blocking_dir in (facegen_dir_1, facegen_dir_2, voice_dir):
+                if blocking_dir:
+                    dir_list += u'\n  - ' + blocking_dir
+            reasons.append(_(u'Has plugin-specific directory - one of the '
+                             u'following:' + dir_list) %
+                           ({'plugin_name': modInfo.name.s}))
     # Client must make sure NoMerge tag not in tags - if in tags
     # don't show up as mergeable.
     return False if reasons else True

Mopy/bash/env.py
@@ -331,17 +331,83 @@ def test_permissions(path, permissions='rwcd'):
 
 # OS agnostic get file version function and helper functions
 def get_file_version(filename):
-    """Return the version of a dll/exe, using the native win32 functions
+    """
+    Return the version of a dll/exe, using the native win32 functions
     if available and otherwise a pure python implementation that works
-    on Linux. The return value is a 4-int tuple, for example (1.9.32.0)."""
+    on Linux.
+
+    :param filename: The file from which the version should be read.
+    :return A 4-int tuple, for example (1, 9, 32, 0).
+    """
     if win32api is None:
+        # TODO(inf) The linux method needs support for string fields
         return _linux_get_file_version_info(filename)
     else:
-        info = win32api.GetFileVersionInfo(filename, u'\\')
-        ms = info['FileVersionMS']
-        ls = info['FileVersionLS']
-        return win32api.HIWORD(ms), win32api.LOWORD(ms), \
-               win32api.HIWORD(ls), win32api.LOWORD(ls)
+        # These are ordered to maximize performance: fixed field with
+        # FileVersion is almost always enough, so that's first. SSE needs the
+        # string fields with ProductVersion, so that's the second one. After
+        # that, we prefer the fixed one since it's faster.
+        curr_ver = _query_fixed_field_version(filename, u'FileVersion')
+        if not _should_ignore_ver(curr_ver):
+            return curr_ver
+        curr_ver = _query_string_field_version(filename, u'ProductVersion')
+        if not _should_ignore_ver(curr_ver):
+            return curr_ver
+        curr_ver = _query_fixed_field_version(filename, u'ProductVersion')
+        if not _should_ignore_ver(curr_ver):
+            return curr_ver
+        return _query_string_field_version(filename, u'FileVersion')
+
+def _should_ignore_ver(test_ver):
+    """
+    Small helper method to determine whether or not a version should be
+    ignored. Versions are ignored if they are 1.0.0.0 or 0.0.0.0.
+
+    :param test_ver: The version to test. A tuple containing 4 integers.
+    :return: True if the specified versiom should be ignored.
+    """
+    return test_ver == (1, 0, 0, 0) or test_ver == (0, 0, 0, 0)
+
+def _query_string_field_version(file_name, version_prefix):
+    """
+    Retrieves the version with the specified prefix from the specified file via
+    its string fields.
+
+    :param file_name: The file from which the version should be read.
+    :param version_prefix: The prefix to use. Can be either FileVersion or
+    ProductVersion.
+    :return: A 4-tuple of integers containing the version of the file.
+    """
+    # We need to ask for language and copepage first, before we can
+    # query the actual version.
+    l_query = u'\\VarFileInfo\\Translation'
+    lang, codepage = win32api.GetFileVersionInfo(file_name, l_query)[0]
+    ver_query = u'\\StringFileInfo\\%04X%04X\\%s' % (lang, codepage,
+                                                     version_prefix)
+    full_ver = win32api.GetFileVersionInfo(file_name, ver_query)
+    # xSE uses commas in its version fields, so use this 'heuristic'
+    splitter = u',' if u',' in full_ver else u'.'
+    try:
+        return tuple([int(part) for part in full_ver.split(splitter)])
+    except ValueError:
+        return 0, 0, 0, 0
+
+def _query_fixed_field_version(file_name, version_prefix):
+    """
+    Retrieves the version with the specified prefix from the specified file via
+    its fixed fields. This is faster than using the string fields, but not all
+    EXEs set them (e.g. SkyrimSE.exe).
+
+    :param file_name: The file from which the version should be read.
+    :param version_prefix: The prefix to use. Can be either FileVersion or
+    ProductVersion.
+    :return: A 4-tuple of integers containing the version of the file.
+    """
+    info = win32api.GetFileVersionInfo(file_name, u'\\')
+    ms = info[u'%sMS' % version_prefix]
+    ls = info[u'%sLS' % version_prefix]
+    return win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), \
+           win32api.LOWORD(ls)
 
 def _linux_get_file_version_info(filename):
     """A python replacement for win32api.GetFileVersionInfo that can be used

Mopy/bash/game/skyrim/records.py
@@ -1086,7 +1086,7 @@ class MreHeader(MreHeaderBase):
 
     #--Data elements
     melSet = MelSet(
-        MelStruct('HEDR','f2I',('version',0.94),'numRecords',('nextObject',0xCE6)),
+        MelStruct('HEDR','f2I',('version',1.7),'numRecords',('nextObject',0xCE6)),
         MelUnicode('CNAM','author',u'',512),
         MelUnicode('SNAM','description',u'',512),
         MreHeaderBase.MelMasterName('MAST','masters'),

Mopy/bash/games.py
@@ -460,8 +460,10 @@ class Game(object):
         # check if we have more than 256 active mods
         drop_espms, drop_esls = self.check_active_limit(acti_filtered)
         disable = drop_espms | drop_esls
-        if disable: # chop off extra, update acti in place
-            acti[:] = [x for x in acti_filtered if x not in disable]
+        # update acti in place - this must always be done, since acti may
+        # contain files that are no longer on disk (i.e. not in acti_filtered)
+        acti[:] = [x for x in acti_filtered if x not in disable]
+        if disable: # chop off extra
             self.mod_infos.selectedExtra = fix_active.selectedExtra = [
                 x for x in acti_filtered if x in disable]
         before_reorder = acti # with overflowed plugins removed

Mopy/bash/initialization.py
@@ -153,7 +153,8 @@ def init_dirs(bashIni_, personal, localAppData, game_info):
     data_oblivion_ini = dirs['app'].join(game_info.iniFiles[0])
     use_data_dir = False
     if data_oblivion_ini.exists():
-        oblivionIni = ConfigParser(data_oblivion_ini.s)
+        oblivionIni = ConfigParser()
+        oblivionIni.read(data_oblivion_ini.s)
         # is bUseMyGamesDirectory set to 0?
         use_data_dir = get_ini_option(oblivionIni,
                                       u'bUseMyGamesDirectory') == u'0'