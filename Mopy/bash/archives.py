wrye-bash/wrye-bash

commit ccb22ee1cb2fcf81157063e3111a860c2c39b4aa

New top level exception module
There's a builtin module called exceptions (note the s at the end), so
lets avoid that name.

All exceptions except ScriptParser.ParserError, bolt.PickleDict.Mold
and cint.CBashError have been put in the exception module.

Also some clean up:

* super() instead of direct calls to the superclass
* underscore instead of camel case
* some oddities fixed or cleaned up
* removed ModUnknownSubrecord since it wasn't used anywhere
* added exception as import in bash.py
* replaced UncodedError with NotImplementedError

Closes #372

Mopy/bash/archives.py
@@ -26,7 +26,8 @@ import re
 import subprocess
 
 import bass
-from bolt import startupinfo, StateError, GPath, deprint
+from bolt import startupinfo, GPath, deprint
+from exception import StateError
 
 exe7z = u'7z.exe' if os.name == u'nt' else u'7z'
 defaultExt = u'.7z'

Mopy/bash/bolt.py
@@ -51,6 +51,7 @@ from functools import partial
 # Internal
 import bass
 import chardet
+import exception
 #-- To make commands executed with Popen hidden
 startupinfo = None
 if os.name == u'nt':
@@ -364,74 +365,6 @@ if locale.getlocale() == (None,None):
 initTranslator(bass.language)
 
 CBash = 0
-# Errors ----------------------------------------------------------------------
-class BoltError(Exception):
-    """Generic error with a string message."""
-    def __init__(self,message):
-        self.message = message
-    def __str__(self):
-        return self.message
-
-#------------------------------------------------------------------------------
-class AbstractError(BoltError):
-    """Coding Error: Abstract code section called."""
-    def __init__(self,message=u'Abstract section called.'):
-        BoltError.__init__(self,message)
-
-#------------------------------------------------------------------------------
-class ArgumentError(BoltError):
-    """Coding Error: Argument out of allowed range of values."""
-    def __init__(self,message=u'Argument is out of allowed ranged of values.'):
-        BoltError.__init__(self,message)
-
-#------------------------------------------------------------------------------
-class StateError(BoltError):
-    """Error: Object is corrupted."""
-    def __init__(self,message=u'Object is in a bad state.'):
-        BoltError.__init__(self,message)
-
-#------------------------------------------------------------------------------
-class UncodedError(BoltError):
-    """Coding Error: Call to section of code that hasn't been written."""
-    def __init__(self,message=u'Section is not coded yet.'):
-        BoltError.__init__(self,message)
-
-#------------------------------------------------------------------------------
-class CancelError(BoltError):
-    """User pressed 'Cancel' on the progress meter."""
-    def __init__(self,message=u'Action aborted by user.'):
-        BoltError.__init__(self, message)
-
-class SkipError(CancelError):
-    """User pressed Skipped n operations."""
-    def __init__(self):
-        CancelError.__init__(self, u'Action skipped by user.')
-
-#------------------------------------------------------------------------------
-class PermissionError(BoltError):
-    """Wrye Bash doesn't have permission to access the specified file/directory."""
-    def __init__(self,message=u'Access is denied.'):
-        BoltError.__init__(self,message)
-
-#------------------------------------------------------------------------------
-class FileError(BoltError):
-    """TES4/Tes4SaveFile Error: File is corrupted."""
-    def __init__(self,inName,message):
-        BoltError.__init__(self,message)
-        self.inName = inName
-
-    def __str__(self):
-        if self.inName: # Path or basestring
-            return (u'%s: ' % self.inName) + self.message
-        return u'Unknown File: ' + self.message
-
-#------------------------------------------------------------------------------
-class FileEditError(BoltError):
-    """Unable to edit a file"""
-    def __init__(self,filePath,message=None):
-        message = message or u"Unable to edit file %s." % filePath.s
-        BoltError.__init__(self,message)
-        self.filePath = filePath
 
 # LowStrings ------------------------------------------------------------------
 class LString(object):
@@ -934,7 +867,7 @@ class Path(object):
             destName.mtime = self.mtime
     def moveTo(self,destName):
         if not self.exists():
-            raise StateError(self._s + u' cannot be moved because it does not exist.')
+            raise exception.StateError(self._s + u' cannot be moved because it does not exist.')
         destPath = GPath(destName)
         if destPath._cs == self._cs: return
         if destPath.shead and not os.path.exists(destPath.shead):
@@ -1158,7 +1091,7 @@ class Flags(object):
             index = names[name]
             return (object.__getattribute__(self,'_field') >> index) & 1 == 1
         except KeyError:
-            raise AttributeError(name)
+            raise exception.AttributeError(name)
 
     def __setattr__(self,name,value):
         """Set value by flag name. E.g., flags.isQuestItem = False"""
@@ -1256,7 +1189,7 @@ class OrderedSet(list, MutableSet):
        to the end of the set.
     """
     def update(self, *args, **kwdargs):
-        if kwdargs: raise TypeError("update() takes no keyword arguments")
+        if kwdargs: raise exception.TypeError("update() takes no keyword arguments")
         for s in args:
             for e in s:
                 self.add(e)
@@ -1569,7 +1502,7 @@ class Settings(DataDict):
     def setChanged(self,key):
         """Marks given key as having been changed. Use if value is a dictionary, list or other object."""
         if key not in self.data:
-            raise ArgumentError(u'No settings data for '+key)
+            raise exception.ArgumentError(u'No settings data for ' + key)
         self.changed.add(key)
 
     def getChanged(self,key,default=None):
@@ -1626,7 +1559,7 @@ class StructFile(file):
             strLen, = self.unpack('H',2)
             strLen = strLen & 0x7f | (strLen >> 1) & 0xff80
             if strLen > 0x7FFF:
-                raise UncodedError(u'String too long to convert.')
+                raise NotImplementedError(u'String too long to convert.')
         return self.read(strLen)
 
     def writeNetString(self,str):
@@ -1635,7 +1568,7 @@ class StructFile(file):
         if strLen < 128:
             self.pack('b',strLen)
         elif strLen > 0x7FFF: #--Actually probably fails earlier.
-            raise UncodedError(u'String too long to convert.')
+            raise NotImplementedError(u'String too long to convert.')
         else:
             strLen =  0x80 | strLen & 0x7f | (strLen & 0xff80) << 1
             self.pack('H',strLen)
@@ -1995,7 +1928,7 @@ class LogFile(Log):
 class Progress:
     """Progress Callable: Shows progress when called."""
     def __init__(self,full=1.0):
-        if (1.0*full) == 0: raise ArgumentError(u'Full must be non-zero!')
+        if (1.0*full) == 0: raise exception.ArgumentError(u'Full must be non-zero!')
         self.message = u''
         self.full = 1.0 * full
         self.state = 0
@@ -2006,7 +1939,7 @@ class Progress:
 
     def setFull(self,full):
         """Set's full and for convenience, returns self."""
-        if (1.0*full) == 0: raise ArgumentError(u'Full must be non-zero!')
+        if (1.0*full) == 0: raise exception.ArgumentError(u'Full must be non-zero!')
         self.full = 1.0 * full
         return self
 
@@ -2016,7 +1949,7 @@ class Progress:
 
     def __call__(self,state,message=''):
         """Update progress with current state. Progress is state/full."""
-        if (1.0*self.full) == 0: raise ArgumentError(u'Full must be non-zero!')
+        if (1.0*self.full) == 0: raise exception.ArgumentError(u'Full must be non-zero!')
         if message: self.message = message
         if self.debug: deprint(u'%0.3f %s' % (1.0*state/self.full, self.message))
         self._do_progress(1.0 * state / self.full, self.message)
@@ -2042,7 +1975,7 @@ class SubProgress(Progress):
         Progress.__init__(self,full)
         if baseTo == '+1': baseTo = baseFrom + 1
         if baseFrom < 0 or baseFrom >= baseTo:
-            raise ArgumentError(u'BaseFrom must be >= 0 and BaseTo must be > BaseFrom')
+            raise exception.ArgumentError(u'BaseFrom must be >= 0 and BaseTo must be > BaseFrom')
         self.parent = parent
         self.baseFrom = baseFrom
         self.scale = 1.0*(baseTo-baseFrom)
@@ -2495,16 +2428,16 @@ class WryeText:
             css = WryeText.defaultCss
         else:
             if cssName.ext != u'.css':
-                raise BoltError(u'Invalid Css file: '+cssName.s)
+                raise exception.BoltError(u'Invalid Css file: ' + cssName.s)
             for css_dir in cssDirs:
                 cssPath = GPath(css_dir).join(cssName)
                 if cssPath.exists(): break
             else:
-                raise BoltError(u'Css file not found: '+cssName.s)
+                raise exception.BoltError(u'Css file not found: ' + cssName.s)
             with cssPath.open('r',encoding='utf-8-sig') as cssIns:
                 css = u''.join(cssIns.readlines())
             if u'<' in css:
-                raise BoltError(u'Non css tag in '+cssPath.s)
+                raise exception.BoltError(u'Non css tag in ' + cssPath.s)
         #--Write Output ------------------------------------------------------
         outWrite(WryeText.htmlHead % (title,css))
         didContents = False

Mopy/bash/bosh/__init__.py
@@ -51,11 +51,12 @@ from .. import bass, bolt, balt, bush, env, load_order, archives
 from .. import patcher # for configIsCBash()
 from ..archives import readExts
 from ..bass import dirs, inisettings, tooldirs, reModExt
-from ..bolt import BoltError, AbstractError, ArgumentError, StateError, \
-    PermissionError, FileError, CancelError, SkipError
 from ..bolt import GPath, DataDict, cstrip, deprint, sio, Path, decode
-from ..brec import MreRecord, ModReader, ModError
+from ..brec import MreRecord, ModReader
 from ..cint import CBashApi
+from ..exception import AbstractError, ArgumentError, BoltError, BSAError, \
+    CancelError, FileError, ModError, NonExistentDriveError, PermissionError, \
+    PluginsFullError, SaveFileError, SaveHeaderError, SkipError, StateError
 from ..parsers import ModFile
 
 #--Settings
@@ -329,12 +330,6 @@ class AFile(object):
 
     def __repr__(self): return self.__class__.__name__ + u"<" + repr(
         self.abs_path.stail) + u">"
-
-#------------------------------------------------------------------------------
-class PluginsFullError(BoltError):
-    """Usage Error: Attempt to add a mod to plugins when plugins is full."""
-    def __init__(self,message=_(u'Load list is full.')):
-        BoltError.__init__(self,message)
 
 #------------------------------------------------------------------------------
 class MasterInfo:
@@ -1086,8 +1081,8 @@ class INIInfo(IniFile):
             return bolt.winNewLines(log.out.getvalue())
 
 #------------------------------------------------------------------------------
-from .save_files import get_save_header_type, SaveHeaderError
-from ._saves import PluggyFile, ObseFile, SaveFileError
+from .save_files import get_save_header_type
+from ._saves import PluggyFile, ObseFile
 class SaveInfo(FileInfo):
     def getFileInfos(self): return saveInfos
 
@@ -1153,7 +1148,6 @@ class SaveInfo(FileInfo):
 
 #------------------------------------------------------------------------------
 from . import bsa_files
-from .bsa_files import BSAError
 
 try:
     _bsa_type = bsa_files.get_bsa_type(bush.game.fsName)
@@ -2470,7 +2464,7 @@ class ModInfos(FileInfos):
         for f in set(filenames):
             if f.s in bush.game.masterFiles:
                 if kwargs.pop('raise_on_master_deletion', True):
-                    raise bolt.BoltError(
+                    raise BoltError(
                         u"Cannot delete the game's master file(s).")
                 else:
                     filenames.remove(f)
@@ -3093,7 +3087,7 @@ def initDirs(bashIni, personal, localAppData):
             'bainData', 'bsaCache')
     try:
         env.shellMakeDirs([dirs[key] for key in keys])
-    except env.NonExistentDriveError as e:
+    except NonExistentDriveError as e:
         # NonExistentDriveError is thrown by shellMakeDirs if any of the
         # directories cannot be created due to residing on a non-existing
         # drive. Find which keys are causing the errors

Mopy/bash/games.py
@@ -34,6 +34,7 @@ import time
 from collections import defaultdict
 # Local
 import bolt
+import exception
 import env
 
 def _write_plugins_txt_(path, lord, active, _star):
@@ -298,7 +299,7 @@ class Game(object):
         raise NotImplementedError
 
     @staticmethod
-    def _must_update_active(deleted, reordered): raise bolt.AbstractError
+    def _must_update_active(deleted, reordered): raise exception.AbstractError
 
     def active_changed(self): return self._plugins_txt_modified()
 
@@ -321,25 +322,25 @@ class Game(object):
     def _fetch_load_order(self, cached_load_order, cached_active):
         """:type cached_load_order: tuple[bolt.Path]
         :type cached_active: tuple[bolt.Path]"""
-        raise bolt.AbstractError
+        raise exception.AbstractError
 
     def _fetch_active_plugins(self): # no override for AsteriskGame
         """:rtype: list[bolt.Path]"""
-        raise bolt.AbstractError
+        raise exception.AbstractError
 
     def _persist_load_order(self, lord, active):
         """Persist the fixed lord to disk - will break conflicts for
         timestamp games."""
-        raise bolt.AbstractError
+        raise exception.AbstractError
 
     def _persist_active_plugins(self, active, lord):
-        raise bolt.AbstractError
+        raise exception.AbstractError
 
     def _persist_if_changed(self, active, lord, previous_active,
                             previous_lord):
         # Override for fallout4 to write the file once and oblivion to save
         # active only if needed. Both active and lord must not be None.
-        raise bolt.AbstractError
+        raise exception.AbstractError
 
     # MODFILES PARSING --------------------------------------------------------
     def _parse_modfile(self, path):
@@ -391,7 +392,7 @@ class Game(object):
             master_dex = lord.index(master_name)
         except ValueError:
             if not master_name in self.mod_infos:
-                raise bolt.BoltError(
+                raise exception.BoltError(
                     u'%s is missing or corrupted' % master_name)
             fix_lo.lo_added = {master_name}
         if master_dex > 0:

Mopy/bash/basher/__init__.py
@@ -68,10 +68,12 @@ import wx
 #--Localization
 #..Handled by bosh, so import that.
 from .. import bush, bosh, bolt, bass, env, load_order, archives
-from ..bolt import BoltError, CancelError, SkipError, GPath, SubProgress, \
-    deprint, AbstractError, formatInteger, formatDate, round_size
+from ..bolt import GPath, SubProgress, deprint, formatInteger, formatDate, \
+    round_size
 from ..bosh import omods
 from ..cint import CBashApi
+from ..exception import AbstractError, BoltError, CancelError, FileError, \
+    SkipError
 
 startupinfo = bolt.startupinfo
 
@@ -806,7 +808,7 @@ class ModList(_ModsUIList):
         #--Save and Refresh
         try:
             bosh.modInfos.cached_lo_save_all()
-        except bolt.BoltError as e:
+        except BoltError as e:
             balt.showError(self, u'%s' % e)
         self.RefreshUI(refreshSaves=True)
 
@@ -1029,7 +1031,7 @@ class ModList(_ModsUIList):
                     touched |= set(activated)
                     activated = [x for x in activated if x != inact]
                     changes[self.__activated_key][inact] = activated
-            except bolt.BoltError as e:
+            except BoltError as e:
                 balt.showError(self, u'%s' % e)
                 break
         #--Refresh
@@ -1192,7 +1194,7 @@ class _EditableMixinOnFileInfos(_EditableMixin):
         try: # use self.file_info.name, as name may have been updated
             self.panel_uilist.data_store.refreshFile(self.file_info.name)
             return self.file_info.name
-        except bolt.FileError as e:
+        except FileError as e:
             deprint(u'Failed to edit details for %s' % self.displayed_item,
                     traceback=True)
             balt.showError(self,

Mopy/bash/game/fallout4/__init__.py
@@ -30,7 +30,8 @@ from .constants import *
 from .default_tweaks import default_tweaks
 from .records import MreHeader, MreLvli, MreLvln
 from ... import brec
-from ...brec import BaseRecordHeader, ModError
+from ...brec import BaseRecordHeader
+from ...exception import ModError
 
 #--Name of the game to use in UI.
 displayName = u'Fallout 4'

Mopy/bash/game/oblivion/__init__.py
@@ -37,7 +37,8 @@ from .records import MreActi, MreAlch, MreAmmo, MreAnio, MreAppa, MreArmo, \
     MreSpel, MreStat, MreTree, MreWatr, MreWeap, MreWthr, MreClmt, MreCsty, \
     MreIdle, MreLtex, MreRegn, MreSbsp, MreSkil, MreAchr, MreAcre, \
     MreCell, MreGmst, MreRefr, MreRoad, MreHeader, MreWrld, MreDial, MreInfo
-from ...brec import MreGlob, BaseRecordHeader, ModError
+from ...brec import MreGlob, BaseRecordHeader
+from ...exception import ModError
 
 #--Name of the game to use in UI.
 displayName = u'Oblivion'

Mopy/bash/game/skyrim/__init__.py
@@ -43,7 +43,8 @@ from .records import MreCell, MreWrld, MreFact, MreAchr, MreDial, MreInfo, \
     MreFlor, MreEyes, MreWeap, MreIngr, MreClfm, MreMesg, MreLigh, MreExpl, \
     MreLcrt, MreStat, MreAmmo, MreSmqn, MreImad, MreSoun, MreAvif, MreCont, \
     MreIpct, MreAspc, MreRela, MreEfsh, MreSnct, MreOtft
-from ...brec import MreGlob, BaseRecordHeader, ModError
+from ...brec import MreGlob, BaseRecordHeader
+from ...exception import ModError
 
 #--Name of the game to use in UI.
 displayName = u'Skyrim'

commit b3c685e7eeea0659593bc0e761db13786267f9ad

Fix FNV installer using incorrect variables - more small edits/fixes:
Looks like this got copied from FO4, but these two variables weren't
changed. No idea what impact this actually had, but my guess is that it
wouldn't install correctly.

Also added some remnants of the gimpshop/krita merge to the 'old files
to delete' list.

Utumno:

Mopy/bash/bolt.py: I threw in a fixup - exception.AttributeError
does not exist, introduced in ccb22ee1cb2fcf81157063e3111a860c2c39b4aa.
Mopy/bash/games.py: Some dup code editing and fix warn mutable default
Mopy/bash/archives.py: archive_path renames
Mopy/bash/bosh/__init__.py: docs and typo

Mopy/bash/archives.py
@@ -116,13 +116,13 @@ def wrapPopenOut(command, wrapper, errorMsg):
         raise StateError(errorMsg + u'\nPopen return value: %d' + returncode)
 
 #  WIP: http://sevenzip.osdn.jp/chm/cmdline/switches/method.htm
-def compressionSettings(archive, blockSize, isSolid):
-    archiveType = writeExts.get(archive.cext)
+def compressionSettings(archive_path, blockSize, isSolid):
+    archiveType = writeExts.get(archive_path.cext)
     if not archiveType:
         #--Always fall back to using the defaultExt
-        archive = GPath(archive.sbody + defaultExt).tail
-        archiveType = writeExts.get(archive.cext)
-    if archive.cext in noSolidExts: # zip
+        archive_path = GPath(archive_path.sbody + defaultExt).tail
+        archiveType = writeExts.get(archive_path.cext)
+    if archive_path.cext in noSolidExts: # zip
         solid = u''
     else:
         if isSolid:
@@ -138,13 +138,13 @@ def compressionSettings(archive, blockSize, isSolid):
             if not solid: # zip, will blow if ms=XXX is passed in
                 old = userArgs
                 userArgs = reSolid.sub(u'', userArgs).strip()
-                if old != userArgs: deprint(
-                    archive.s + u': 7zExtraCompressionArguments ini option '
-                                u'"' + old + u'" -> "' + userArgs + u'"')
+                if old != userArgs: deprint(archive_path.s +
+                    u': 7zExtraCompressionArguments ini option "' + old +
+                    u'" -> "' + userArgs + u'"')
             solid = userArgs
         else:
             solid += userArgs
-    return archive, archiveType, solid
+    return archive_path, archiveType, solid
 
 def compressCommand(destArchive, destDir, srcFolder, solid=u'-ms=on',
                     archiveType=u'7z'): # WIP - note solid on by default (7z)
@@ -162,10 +162,10 @@ def _extract_command(archivePath, outDirPath, recursive, filelist_to_extract):
     if filelist_to_extract: command += (u' @"%s"' % filelist_to_extract)
     return command
 
-def list_archive(archive, parse_archive_line, __reList=reListArchive):
+def list_archive(archive_path, parse_archive_line, __reList=reListArchive):
     """Client is responsible for closing the file ! See uses for
     _parse_archive_line examples."""
-    command = ur'"%s" l -slt -sccUTF-8 "%s"' % (exe7z, archive.s)
+    command = ur'"%s" l -slt -sccUTF-8 "%s"' % (exe7z, archive_path.s)
     ins, err = subprocess.Popen(command, stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT,
                                 stdin=subprocess.PIPE,

Mopy/bash/bolt.py
@@ -1188,7 +1188,7 @@ class Flags(object):
             index = names[name]
             return (object.__getattribute__(self,'_field') >> index) & 1 == 1
         except KeyError:
-            raise exception.AttributeError(name)
+            raise AttributeError(name)
 
     def __setattr__(self,name,value):
         """Set value by flag name. E.g., flags.isQuestItem = False"""

Mopy/bash/bosh/__init__.py
@@ -1062,7 +1062,8 @@ class SaveInfo(FileInfo):
 
 #------------------------------------------------------------------------------
 class DataStore(DataDict):
-    store_dir = empty_path # where the datas sit, static except for SaveInfos
+    """Base class for the singleton collections of infos."""
+    store_dir = empty_path # where the data sit, static except for SaveInfos
 
     def delete(self, delete_keys, **kwargs):
         """Deletes member file(s)."""

Mopy/bash/games.py
@@ -48,7 +48,7 @@ def _write_plugins_txt_(path, lord, active, _star):
             __write_plugins(out, lord, active, _star)
 
 def __write_plugins(out, lord, active, _star):
-    def asterisk(active_set=set(active)):
+    def asterisk(active_set=frozenset(active)):
         return '*' if _star and (mod in active_set) else ''
     for mod in (_star and lord) or active:
         # Ok, this seems to work for Oblivion, but not Skyrim
@@ -189,6 +189,7 @@ class Game(object):
     must_be_active_if_present = ()
     max_espms = 255
     max_esls = 0
+    _star = False # whether plugins.txt uses a star to denote an active plugin
 
     def __init__(self, mod_infos, plugins_txt_path):
         super(Game, self).__init__()
@@ -350,11 +351,11 @@ class Game(object):
         """:rtype: (list[bolt.Path], list[bolt.Path])"""
         if not path.exists(): return [], []
         #--Read file
-        acti, _lo = _parse_plugins_txt_(path, self.mod_infos, _star=False)
+        acti, _lo = _parse_plugins_txt_(path, self.mod_infos, _star=self._star)
         return acti, _lo
 
     def _write_modfile(self, path, lord, active):
-        _write_plugins_txt_(path, lord, active, _star=False)
+        _write_plugins_txt_(path, lord, active, _star=self._star)
 
     # PLUGINS TXT -------------------------------------------------------------
     def _parse_plugins_txt(self):
@@ -741,6 +742,7 @@ class AsteriskGame(Game):
     max_espms = 254
     max_esls = 4096 # hard limit, game runs out of fds sooner, testing needed
     _ccc_filename = u''
+    _star = True
 
     @property
     def remove_from_plugins_txt(self): return set()
@@ -802,15 +804,6 @@ class AsteriskGame(Game):
         if (previous_lord is None or previous_lord != lord) or (
                 previous_active is None or previous_active != active):
             self._persist_load_order(lord, active)
-
-    # Modfiles parsing overrides ----------------------------------------------
-    def _parse_modfile(self, path):
-        if not path.exists(): return [], []
-        acti, lo = _parse_plugins_txt_(path, self.mod_infos, _star=True)
-        return acti, lo
-
-    def _write_modfile(self, path, lord, active):
-        _write_plugins_txt_(path, lord, active, _star=True)
 
     # Validation overrides ----------------------------------------------------
     def _order_fixed(self, lord):

scripts/build/installer/install.nsi
@@ -379,11 +379,11 @@
                 ${If} $CheckState_FalloutNV_Py == ${BST_CHECKED}
                     CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash - FalloutNV.lnk" "$Path_FalloutNV\Mopy\Wrye Bash Launcher.pyw" "" "$Path_FalloutNV\Mopy\bash\images\bash_32.ico" 0
                     CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash - FalloutNV (Debug Log).lnk" "$Path_FalloutNV\Mopy\Wrye Bash Debug.bat" "" "$Path_FalloutNV\Mopy\bash\images\bash_32.ico" 0
-                    ${If} $CheckState_Fallout4_Exe == ${BST_CHECKED}
+                    ${If} $CheckState_FalloutNV_Exe == ${BST_CHECKED}
                         CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash (Standalone) - FalloutNV.lnk" "$Path_FalloutNV\Mopy\Wrye Bash.exe"
                         CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash (Standalone) - FalloutNV (Debug Log).lnk" "$Path_FalloutNV\Mopy\Wrye Bash.exe" "-d"
                     ${EndIf}
-                ${ElseIf} $CheckState_Fallout4_Exe == ${BST_CHECKED}
+                ${ElseIf} $CheckState_FalloutNV_Exe == ${BST_CHECKED}
                     CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash - FalloutNV.lnk" "$Path_FalloutNV\Mopy\Wrye Bash.exe"
                     CreateShortCut "$SMPROGRAMS\Wrye Bash\Wrye Bash - FalloutNV (Debug Log).lnk" "$Path_FalloutNV\Mopy\Wrye Bash.exe" "-d"
                 ${EndIf}

scripts/build/installer/macros.nsh
@@ -197,6 +197,9 @@
         Delete "${Path}\Mopy\bash\images\gimp16.png"
         Delete "${Path}\Mopy\bash\images\gimp24.png"
         Delete "${Path}\Mopy\bash\images\gimp32.png"
+        Delete "${Path}\Mopy\bash\images\gimpshop16.png"
+        Delete "${Path}\Mopy\bash\images\gimpshop24.png"
+        Delete "${Path}\Mopy\bash\images\gimpshop32.png"
         Delete "${Path}\Mopy\bash\images\gmax16.png"
         Delete "${Path}\Mopy\bash\images\gmax24.png"
         Delete "${Path}\Mopy\bash\images\gmax32.png"
@@ -542,6 +545,9 @@
         Delete "${Path}\Mopy\bash\patcher\utilities.p*"
         ; As of 307, the following are obsolete:
         Delete "${Path}\Mopy\bash\compiled\loot32.dll"
+        Delete "${Path}\Mopy\bash\images\tools\gimpshop16.png"
+        Delete "${Path}\Mopy\bash\images\tools\gimpshop24.png"
+        Delete "${Path}\Mopy\bash\images\tools\gimpshop32.png"
         Delete "${Path}\Mopy\bash\loot.py"
         ; And some leftovers from various releases, some of them only appeared on dev:
         Delete "${Path}\Mopy\INI Tweaks\Skyrim\Archery, NavMeshMove 4096 -Default [Skyrim].ini"