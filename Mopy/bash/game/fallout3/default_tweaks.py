e5a0d991e88c5d3d3938d1461d70c4344e31ae77
Infernio <infernio@icloud.com>
2019-10-25 16:22:09 +0200

    FO3/FNV: Add missing tweaks from valda's version
    Specifically, the tweaks to anisotropic filtering and anti-aliasing.
    Also reworked existing tweaks to be more consistent in their usage of
    '~Default'.

--- Mopy/bash/game/fallout3/default_tweaks.py
+++ Mopy/bash/game/fallout3/default_tweaks.py
@@ -24,10 +24,37 @@
 from collections import OrderedDict
 
 default_tweaks = {
+    u'Anisotropic Filtering, Disabled.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMaxAnisotropy', u'0')]))]),
+    u'Anisotropic Filtering, x2.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMaxAnisotropy', u'2')]))]),
+    u'Anisotropic Filtering, x4.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMaxAnisotropy', u'4')]))]),
+    u'Anisotropic Filtering, x8 ~Default.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMaxAnisotropy', u'8')]))]),
+    u'Anisotropic Filtering, x16.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMaxAnisotropy', u'16')]))]),
+    u'Anti-Aliasing, Disabled ~Default.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMultiSample', u'0')]))]),
+    u'Anti-Aliasing, x2.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMultiSample', u'2')]))]),
+    u'Anti-Aliasing, x4.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMultiSample', u'4')]))]),
+    u'Anti-Aliasing, x8.ini': OrderedDict(
+        [(u'Display', OrderedDict(
+            [(u'iMultiSample', u'8')]))]),
     u'Border Regions, Disabled.ini': OrderedDict(
         [(u'Main', OrderedDict(
             [(u'bEnableBorderRegion', u'0')]))]),
-    u'Border Regions, ~Enabled.ini': OrderedDict(
+    u'Border Regions, Enabled ~Default.ini': OrderedDict(
         [(u'Main', OrderedDict(
             [(u'bEnableBorderRegion', u'1')]))]),
     u'Fonts 1, ~Default.ini': OrderedDict(
@@ -38,13 +65,13 @@ default_tweaks = {
         [(u'Grass', OrderedDict(
             [(u'iMinGrassSize', u'140'),
              (u'fGrassMaxStartFadeDistance', u'5000.0000')]))]),
-    u'Mouse Acceleration, Default.ini': OrderedDict(
+    u'Mouse Acceleration, ~Default.ini': OrderedDict(
         [(u'Controls', OrderedDict(
             [(u'fForegroundMouseAccelBase', u''),
              (u'fForegroundMouseBase', u''),
              (u'fForegroundMouseAccelTop', u''),
              (u'fForegroundMouseMult', u'')]))]),
-    u'Mouse Acceleration, ~Fixed.ini': OrderedDict(
+    u'Mouse Acceleration, Fixed.ini': OrderedDict(
         [(u'Controls', OrderedDict(
             [(u'fForegroundMouseAccelBase', u'0'),
              (u'fForegroundMouseBase', u'0'),
@@ -53,10 +80,10 @@ default_tweaks = {
     u'Refraction Shader, Disabled.ini': OrderedDict(
         [(u'Display', OrderedDict(
             [(u'bUseRefractionShader', u'0')]))]),
-    u'Refraction Shader, ~Enabled.ini': OrderedDict(
+    u'Refraction Shader, Enabled ~Default.ini': OrderedDict(
         [(u'Display', OrderedDict(
             [(u'bUseRefractionShader', u'1')]))]),
-    u'Save Backups, 1.ini': OrderedDict(
+    u'Save Backups, 1 ~Default.ini': OrderedDict(
         [(u'General', OrderedDict(
             [(u'iSaveGameBackupCount', u'1')]))]),
     u'Save Backups, 2.ini': OrderedDict(
@@ -71,16 +98,16 @@ default_tweaks = {
     u'Save Backups, 5.ini': OrderedDict(
         [(u'General', OrderedDict(
             [(u'iSaveGameBackupCount', u'5')]))]),
-    u'bInvalidateOlderFiles, ~Default.ini': OrderedDict(
+    u'bInvalidateOlderFiles, Disabled ~Default.ini': OrderedDict(
         [(u'Archive', OrderedDict(
             [(u'bInvalidateOlderFiles', u'0')]))]),
-    u'bInvalidateOlderFiles, ~Enabled.ini': OrderedDict(
+    u'bInvalidateOlderFiles, Enabled.ini': OrderedDict(
         [(u'Archive', OrderedDict(
             [(u'bInvalidateOlderFiles', u'1')]))]),
-    u'iConsoleTextXPos, Default.ini': OrderedDict(
+    u'iConsoleTextXPos, ~Default.ini': OrderedDict(
         [(u'Menu', OrderedDict(
             [(u'iConsoleTextXPos', u'30')]))]),
-    u'iConsoleTextXPos, ~Fixed.ini': OrderedDict(
+    u'iConsoleTextXPos, Fixed.ini': OrderedDict(
         [(u'Menu', OrderedDict(
             [(u'iConsoleTextXPos', u'130')]))]),
 }
