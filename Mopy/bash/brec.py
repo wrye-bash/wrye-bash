wrye-bash/wrye-bash

commit 2c1f98a37f2a0009b804c76bcbf4f8a6a66c4e4a

Drop some pointless cruft
Drop MelBase.debug

Bad name, very pointless (since it dumps out so much output that the log
becomes useless), and worst of all, it slows down loading of everything,
even in release builds (since the if check and subsequent branch won't
be optimized out - this is CPython).

Drop MelRefrXmrk

REFR is in no shape to be loaded, and this isn't the way to handle that
anyways - a distributor is. Drops a few more print calls, which is nice.

Drop all prints in brec

Use deprints instead, should improve load/dump errors a bit.

Mopy/bash/brec.py
@@ -398,9 +398,9 @@ class ModWriter(object):
                 outWrite(struct_pack('=4sHI', 'XXXX', 4, lenData))
                 outWrite(struct_pack('=4sH', sub_rec_type, 0))
             outWrite(data)
-        except Exception as e:
-            print e
-            print self,sub_rec_type,data,values
+        except Exception:
+            bolt.deprint(u'%r: Failed packing: %s, %s, %s' % (
+                self, sub_rec_type, data, values), traceback=True)
 
     def packSub0(self, sub_rec_type, data):
         """Write subrecord header plus zero terminated string to output
@@ -500,12 +500,6 @@ class MelBase(object):
 
     def __init__(self, subType, attr, default=None):
         self.subType, self.attr, self.default = subType, attr, default
-        self._debug = False
-
-    def debug(self,on=True):
-        """Sets debug flag on self."""
-        self._debug = on
-        return self
 
     def getSlotsUsed(self):
         return self.attr,
@@ -567,7 +561,6 @@ class MelBase(object):
     def loadData(self, record, ins, sub_type, size_, readId):
         """Reads data from ins into record attribute."""
         record.__setattr__(self.attr, ins.read(size_, readId))
-        if self._debug: print u'%s' % record.__getattribute__(self.attr)
 
     def dumpData(self,record,out):
         """Dumps data from record to outstream."""
@@ -617,9 +610,6 @@ class MelCounter(MelBase):
         :type couns: str"""
         self.element = element
         self.counted_attr = counts
-
-    def debug(self, on=True):
-        self.element.debug(on)
 
     def getSlotsUsed(self):
         return self.element.getSlotsUsed()
@@ -683,7 +673,6 @@ class MelFid(MelBase):
 
     def loadData(self, record, ins, sub_type, size_, readId):
         record.__setattr__(self.attr,ins.unpackRef())
-        if self._debug: print u'  %08X' % (record.__getattribute__(self.attr),)
 
     def dumpData(self,record,out):
         try:
@@ -718,7 +707,6 @@ class MelFids(MelBase):
     def loadData(self, record, ins, sub_type, size_, readId):
         fid = ins.unpackRef()
         record.__getattribute__(self.attr).append(fid)
-        if self._debug: print u' ',hex(fid)
 
     def dumpData(self,record,out):
         type = self.subType
@@ -739,7 +727,6 @@ class MelNull(MelBase):
 
     def __init__(self, subType):
         self.subType = subType
-        self._debug = False
 
     def getSlotsUsed(self):
         return ()
@@ -748,8 +735,7 @@ class MelNull(MelBase):
         pass
 
     def loadData(self, record, ins, sub_type, size_, readId):
-        junk = ins.read(size_, readId)
-        if self._debug: print u' ',record.fid,unicode(junk)
+        ins.seek(size_, 1, readId)
 
     def dumpData(self,record,out):
         pass
@@ -764,9 +750,6 @@ class MelFidList(MelFids):
         if not size_: return
         fids = ins.unpack(`size_ / 4` + 'I', size_, readId)
         record.__setattr__(self.attr,list(fids))
-        if self._debug:
-            for fid in fids:
-                print u'  %08X' % fid
 
     def dumpData(self,record,out):
         fids = record.__getattribute__(self.attr)
@@ -803,11 +786,6 @@ class MelSequential(MelBase):
         self.elements, self.form_elements = elements, set()
         self._possible_sigs = {s for element in self.elements for s
                                in element.signatures}
-
-    def debug(self,on=True):
-        for element in self.elements:
-            element.debug(on)
-        return self
 
     def getDefaulters(self, defaulters, base):
         for element in self.elements:
@@ -1171,7 +1149,6 @@ class MelUnion(MelBase):
         :param fallback: The fallback element to use. Defaults to None, which
             will raise an error if the decider returns an unknown value.
         :type fallback: MelBase"""
-        self._debug = False
         self.element_mapping = element_mapping
         self.fid_elements = set()
         if not isinstance(decider, ADecider):
@@ -1301,7 +1278,6 @@ class MelString(MelBase):
     def loadData(self, record, ins, sub_type, size_, readId):
         value = ins.readString(size_, readId)
         record.__setattr__(self.attr,value)
-        if self._debug: print u' ',record.__getattribute__(self.attr)
 
     def dumpData(self,record,out):
         string_val = record.__getattribute__(self.attr)
@@ -1333,7 +1309,6 @@ class MelLString(MelString):
     def loadData(self, record, ins, sub_type, size_, readId):
         value = ins.readLString(size_, readId)
         record.__setattr__(self.attr,value)
-        if self._debug: print u' ',record.__getattribute__(self.attr)
 
 #------------------------------------------------------------------------------
 class MelStrings(MelString):
@@ -1348,7 +1323,6 @@ class MelStrings(MelString):
     def loadData(self, record, ins, sub_type, size_, readId):
         value = ins.readStrings(size_, readId)
         record.__setattr__(self.attr,value)
-        if self._debug: print u' ',value
 
     def dumpData(self,record,out):
         strings = record.__getattribute__(self.attr)
@@ -1363,7 +1337,6 @@ class MelStruct(MelBase):
         dumpExtra = kwdargs.get('dumpExtra', None)
         self.subType, self.format = subType, format
         self.attrs,self.defaults,self.actions,self.formAttrs = MelBase.parseElements(*elements)
-        self._debug = False
         if dumpExtra:
             self.attrs += (dumpExtra,)
             self.defaults += ('',)
@@ -1394,10 +1367,6 @@ class MelStruct(MelBase):
         if self.formatLen >= 0:
             # Dump remaining subrecord data into an attribute
             setter(self.attrs[-1], ins.read(size_ - self.formatLen))
-        if self._debug:
-            print u' ',zip(self.attrs,unpacked)
-            if len(unpacked) != len(self.attrs):
-                print u' ',unpacked
 
     def dumpData(self,record,out):
         values = []
@@ -1415,7 +1384,8 @@ class MelStruct(MelBase):
         try:
             out.packSub(self.subType,format,*values)
         except struct.error:
-            print self.subType,self.format,values
+            bolt.deprint(u'Failed to dump struct: %s (%r)' % (
+                self.subType, self))
             raise
 
     def mapFids(self,record,function,save=False):
@@ -1560,7 +1530,6 @@ class MelTruncatedStruct(MelStruct):
         for attr, value, action in zip(self.attrs, unpacked_val, self.actions):
             if callable(action): value = action(value)
             setter(attr, value)
-        if self._debug: print unpacked_val
 
     def _pre_process_unpacked(self, unpacked_val):
         """You may override this if you need to change the unpacked value in
@@ -1821,7 +1790,6 @@ class MelSet(object):
     """Set of mod record elments."""
 
     def __init__(self,*elements):
-        self._debug = False
         self.elements = elements
         self.defaulters = {}
         self.loaders = {}
@@ -1831,11 +1799,6 @@ class MelSet(object):
             element.getDefaulters(self.defaulters,'')
             element.getLoaders(self.loaders)
             element.hasFids(self.formElements)
-
-    def debug(self,on=True):
-        """Sets debug flag on self."""
-        self._debug = on
-        return self
 
     def getSlotsUsed(self):
         """This function returns all of the attributes used in record instances that use this instance."""
@@ -1856,15 +1819,12 @@ class MelSet(object):
         """Loads data from input stream. Called by load()."""
         rec_type = record.recType
         loaders = self.loaders
-        _debug = self._debug
-        if _debug: print u'\n>>>> %08X' % record.fid
         # Load each subrecord
         ins_at_end = ins.atEnd
         load_sub_header = ins.unpackSubHeader
         read_id_prefix = rec_type + '.'
         while not ins_at_end(endPos, rec_type):
             sub_type, sub_size = load_sub_header(rec_type)
-            if _debug: print sub_type, sub_size
             try:
                 loaders[sub_type].loadData(record, ins, sub_type, sub_size,
                                            read_id_prefix + sub_type)
@@ -1877,15 +1837,16 @@ class MelSet(object):
                     record, ins, sub_type, sub_size)
             except Exception as error:
                 self._handle_load_error(error, record, ins, sub_type, sub_size)
-        if _debug: print u'<<<<',getattr(record,'eid',u'[NO EID]')
 
     def _handle_load_error(self, error, record, ins, sub_type, sub_size):
-        print error
+        bolt.deprint(error)
         eid = getattr(record, 'eid', u'<<NO EID>>')
-        print(u'Error loading %r record and/or subrecord: %08X\n  '
-              u'eid = %r\n  subrecord = %r\n  subrecord size = %d\n  '
-              u'file pos = %d' % (
-            record.recType, record.fid, eid, sub_type, sub_size, ins.tell()))
+        bolt.deprint(u'Error loading %r record and/or subrecord: %08X' %
+                     record.recType, record.fid)
+        bolt.deprint(u'  eid = %r' % eid)
+        bolt.deprint(u'  subrecord = %r' % sub_type)
+        bolt.deprint(u'  subrecord size = %d' % sub_size)
+        bolt.deprint(u'  file pos = %d' % ins.tell())
         raise
 
     def dumpData(self,record, out):
@@ -1894,11 +1855,18 @@ class MelSet(object):
             try:
                 element.dumpData(record,out)
             except:
-                bolt.deprint('error dumping data:',traceback=True)
-                print u'Dumping:',getattr(record,'eid',u'<<NO EID>>'),record.fid,element
+                bolt.deprint(u'Error dumping data: ', traceback=True)
+                bolt.deprint(u'Occurred while dumping '
+                             u'<%(eid)s[%(signature)s:%(fid)s]>' % {
+                    u'signature': record.recType,
+                    u'fid': strFid(record.fid),
+                    u'eid': (record.eid + u' ' if hasattr(record, 'eid')
+                             and record.eid is not None else u''),
+                })
                 for attr in record.__slots__:
-                    if hasattr(record,attr):
-                        print u"> %s: %s" % (attr,repr(getattr(record,attr)))
+                    if hasattr(record, attr):
+                        bolt.deprint(u'> %s: %s' % (
+                            attr, repr(getattr(record, attr))))
                 raise
 
     def mapFids(self,record,mapper,save=False):
@@ -1953,8 +1921,6 @@ class _MelDistributor(MelNull):
     :type _sig_to_loader: dict[str, MelBase]
     :type _target_sigs: set[str]"""
     def __init__(self, distributor_config): # type: (dict) -> None
-        # For MelBase.debug
-        self._debug = False
         # Maps attribute name to loader
         self._attr_to_loader = {}
         # Maps subrecord signature to loader
@@ -2909,7 +2875,6 @@ class MelRaceParts(MelNull):
             loaders. These will be loaded and dumped directly after each
             INDX."""
         self._last_indx = None # used during loading
-        self._debug = False
         self._indx_to_attr = indx_to_attr
         # Create loaders for use at runtime
         self._indx_to_loader = {

Mopy/bash/game/fallout3/records.py
@@ -24,7 +24,6 @@
 """This module contains the fallout3 record classes. You must import from it
 __once__ only in game.fallout3.Fallout3GameInfo#init. No other game.records
 file must be imported till then."""
-import struct
 from operator import attrgetter
 from ... import bush, brec
 from ...bolt import Flags, struct_unpack, struct_pack
@@ -75,10 +74,6 @@ if brec.MelModel is None:
                                            'facegen_model_flags'))
                 ]
             MelGroup.__init__(self, attr, *model_elements)
-
-        def debug(self, on=True):
-            for element in self.elements[:2]: element.debug(on)
-            return self
 
     brec.MelModel = _MelModel
 from ...brec import MelModel
@@ -159,11 +154,6 @@ class MelConditions(MelGroups):
                 record.param1, record.param2 = unpacked2
                 record.runOn, record.reference = null4, null4
             record.ifunc, record.form1234 = ifunc, form1234
-            if self._debug:
-                unpacked = unpacked1 + unpacked2
-                print u' ', zip(self.attrs, unpacked)
-                if len(unpacked) != len(self.attrs):
-                    print u' ', unpacked
 
         def dumpData(self,record,out):
             out.packSub('CTDA', '=B3sfH2s' + record.form1234,
@@ -2176,7 +2166,6 @@ class MreNpc(MreActor):
             recordSetAttr = record.__setattr__
             recordSetAttr('health',unpacked[0])
             recordSetAttr('attributes',unpacked[1:])
-            if self._debug: print unpacked[0],unpacked[1:]
 
         def dumpData(self,record,out):
             recordGetAttr = record.__getattribute__
@@ -2193,7 +2182,6 @@ class MreNpc(MreActor):
             recordSetAttr = record.__setattr__
             recordSetAttr('skillValues',unpacked[:14])
             recordSetAttr('skillOffsets',unpacked[14:])
-            if self._debug: print unpacked[:14]+unpacked[14:]
 
         def dumpData(self,record,out):
             recordGetAttr = record.__getattribute__
@@ -2826,55 +2814,16 @@ class MreRefr(MelRecord):
     """Placed Object."""
     classType = 'REFR'
 
-    _flags = Flags(0L,Flags.getNames('visible', 'canTravelTo','showAllHidden'))
+    _marker_flags = Flags(0, Flags.getNames(
+        'visible',
+        'can_travel_to',
+        'show_all_hidden',
+    ))
     _parentFlags = Flags(0L,Flags.getNames('oppositeParent'))
     _actFlags = Flags(0L,Flags.getNames('useDefault', 'activate','open','openByDefault'))
     _lockFlags = Flags(0L,Flags.getNames(None, None, 'leveledLock'))
     _destinationFlags = Flags(0L,Flags.getNames('noAlarm'))
     reflectFlags = Flags(0L, Flags.getNames('reflection', 'refraction'))
-
-    class MelRefrXmrk(MelStruct):
-        """Handler for xmrk record. Conditionally loads next items."""
-        def loadData(self, record, ins, sub_type, size_, readId):
-            """Reads data from ins into record attribute."""
-            ins.seek(size_, 1, readId) # skip junk
-            record.hasXmrk = True
-            insTell = ins.tell
-            insUnpack = ins.unpack
-            pos = insTell()
-            (type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            while type_ in ['FNAM','FULL','TNAM','WMI1']:
-                if type_ == 'FNAM':
-                    value = insUnpack('B', size_, readId)
-                    record.flags = MreRefr._flags(*value)
-                elif type_ == 'FULL':
-                    record.full = ins.readString(size_, readId)
-                elif type_ == 'TNAM':
-                    record.markerType, record.unused5 = insUnpack('Bs', size_, readId)
-                # WMI1 not used in FO3, leaving so it doesn't break something
-                elif type_ == 'WMI1':
-                    record.reputation = insUnpack('I', size_, readId)
-                pos = insTell()
-                (type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            ins.seek(pos)
-            if self._debug: print ' ',record.flags,record.full,record.markerType
-
-        def dumpData(self,record,out):
-            if (record.flags,record.full,record.markerType,record.unused5,record.reputation) != self.defaults[1:]:
-                record.hasXmrk = True
-            if record.hasXmrk:
-                try:
-                    out.write(struct_pack('=4sH','XMRK',0))
-                    out.packSub('FNAM','B',record.flags.dump())
-                    value = record.full
-                    if value is not None:
-                        out.packSub0('FULL',value)
-                    out.packSub('TNAM','Bs',record.markerType, record.unused5)
-                    # WMI1 not used in FO3, leaving so it doesn't break something
-                    out.packRef('WMI1',record.reputation)
-                except struct.error:
-                    print self.subType,self.format,record.flags,record.full,record.markerType
-                    raise
 
     melSet = MelSet(
         MelEdid(),
@@ -2891,7 +2840,12 @@ class MreRefr(MelRecord):
         MelOptStruct('XMBO','3f','boundHalfExtentsX','boundHalfExtentsY','boundHalfExtentsZ'),
         MelOptStruct('XTEL','I6fI',(FID,'destinationFid'),'destinationPosX','destinationPosY',
             'destinationPosZ','destinationRotX','destinationRotY','destinationRotZ',(_destinationFlags,'destinationFlags')),
-        MelRefrXmrk('XMRK','',('hasXmrk',False),(_flags,'flags',0L),'full','markerType',('unused5',null1),(FID,'reputation')),
+        MelGroup('map_marker',
+            MelBase('XMRK', 'marker_data'),
+            MelOptUInt8('FNAM', (_marker_flags, 'marker_flags')),
+            MelFull(),
+            MelOptStruct('TNAM', 'Bs', 'marker_type', 'unused1'),
+        ),
         MelFid('XTRG','targetId'),
         MelOptSInt32('XLCM', ('levelMod', None)),
         MelGroup('patrolData',

Mopy/bash/game/fallout4/records.py
@@ -57,10 +57,6 @@ if brec.MelModel is None:
                 MelBase(types[3], 'modf_p')
             )
 
-        def debug(self, on=True):
-            for element in self.elements[:2]: element.debug(on)
-            return self
-
     brec.MelModel = _MelModel
 # Now we can import from parent game records file
 from ..skyrim.records import MreLeveledList

Mopy/bash/game/falloutnv/records.py
@@ -1252,53 +1252,16 @@ class MreRefr(MelRecord):
     """Placed Object"""
     classType = 'REFR'
 
-    _flags = Flags(0L,Flags.getNames('visible', 'canTravelTo'))
+    _marker_flags = Flags(0, Flags.getNames(
+        'visible',
+        'can_travel_to',
+        'show_all_hidden',
+    ))
     _parentFlags = Flags(0L,Flags.getNames('oppositeParent'))
     _actFlags = Flags(0L,Flags.getNames('useDefault', 'activate','open','openByDefault'))
     _lockFlags = Flags(0L,Flags.getNames(None, None, 'leveledLock'))
     _destinationFlags = Flags(0L,Flags.getNames('noAlarm'))
     reflectFlags = Flags(0L, Flags.getNames('reflection', 'refraction'))
-
-    class MelRefrXmrk(MelStruct):
-        """Handler for xmrk record. Conditionally loads next items."""
-        def loadData(self, record, ins, sub_type, size_, readId):
-            """Reads data from ins into record attribute."""
-            ins.seek(size_, 1, readId) # skip junk
-            record.hasXmrk = True
-            insTell = ins.tell
-            insUnpack = ins.unpack
-            pos = insTell()
-            (type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            while type_ in ['FNAM','FULL','TNAM','WMI1']:
-                if type_ == 'FNAM':
-                    value = insUnpack('B', size_, readId)
-                    record.flags = MreRefr._flags(*value)
-                elif type_ == 'FULL':
-                    record.full = ins.readString(size_, readId)
-                elif type_ == 'TNAM':
-                    record.markerType, record.unused5 = insUnpack('Bs', size_, readId)
-                elif type_ == 'WMI1':
-                    record.reputation = insUnpack('I', size_, readId)
-                pos = insTell()
-                (type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            ins.seek(pos)
-            if self._debug: print ' ',record.flags,record.full,record.markerType
-
-        def dumpData(self,record,out):
-            if (record.flags,record.full,record.markerType,record.unused5,record.reputation) != self.defaults[1:]:
-                record.hasXmrk = True
-            if record.hasXmrk:
-                try:
-                    out.write(struct.pack('=4sH','XMRK',0))
-                    out.packSub('FNAM','B',record.flags.dump())
-                    value = record.full
-                    if value is not None:
-                        out.packSub0('FULL',value)
-                    out.packSub('TNAM','Bs',record.markerType, record.unused5)
-                    out.packRef('WMI1',record.reputation)
-                except struct.error:
-                    print self.subType,self.format,record.flags,record.full,record.markerType
-                    raise
 
     melSet = MelSet(
         MelEdid(),
@@ -1315,7 +1278,13 @@ class MreRefr(MelRecord):
         MelOptStruct('XMBO','3f','boundHalfExtentsX','boundHalfExtentsY','boundHalfExtentsZ'),
         MelOptStruct('XTEL','I6fI',(FID,'destinationFid'),'destinationPosX','destinationPosY',
             'destinationPosZ','destinationRotX','destinationRotY','destinationRotZ',(_destinationFlags,'destinationFlags')),
-        MelRefrXmrk('XMRK','',('hasXmrk',False),(_flags,'flags',0L),'full','markerType',('unused5',null1),(FID,'reputation')),
+        MelGroup('map_marker',
+            MelBase('XMRK', 'marker_data'),
+            MelOptUInt8('FNAM', (_marker_flags, 'marker_flags')),
+            MelFull(),
+            MelOptStruct('TNAM', 'Bs', 'marker_type', 'unused1'),
+            MelFid('WMI1', 'marker_reputation'),
+        ),
         MelGroup('audioData',
             MelBase('MMRK','audioMarker'),
             MelBase('FULL','full_p'),

Mopy/bash/game/oblivion/records.py
@@ -22,10 +22,9 @@
 #
 # =============================================================================
 """This module contains the oblivion record classes."""
-import struct
 from .constants import condition_function_data
 from ... import brec
-from ...bolt import Flags, struct_pack
+from ...bolt import Flags
 from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelString, MreLeveledListBase, MelSet, MelFid, MelNull, MelOptStruct, \
     MelFids, MreHeaderBase, MelBase, MelUnicode, MelFidList, MelStrings, \
@@ -57,10 +56,6 @@ if brec.MelModel is None:
                 # Texture File Hashes
                 MelBase(types[2], 'modt_p')
             )
-
-        def debug(self,on=True):
-            for element in self.elements[:2]: element.debug(on)
-            return self
 
     brec.MelModel = _MelModel
 from ...brec import MelModel
@@ -130,11 +125,6 @@ class MelConditions(MelGroups):
             else: # size == 20, verified at the start
                 record.unused2 = null4
             record.ifunc, record.form12 = ifunc, form12
-            if self._debug:
-                unpacked = unpacked1 + unpacked2
-                print u' ',zip(self.attrs, unpacked)
-                if len(unpacked) != len(self.attrs):
-                    print u' ', unpacked
 
         def dumpData(self,record,out):
             out.packSub('CTDA','B3sfI'+ record.form12 + '4s',
@@ -1202,7 +1192,6 @@ class MreNpc(MreActor):
             recordSetAttr('health',unpacked[21])
             recordSetAttr('unused1',unpacked[22])
             recordSetAttr('attributes',unpacked[23:])
-            if self._debug: print unpacked[:21],unpacked[21],unpacked[23:]
 
         def dumpData(self,record,out):
             recordGetAttr = record.__getattribute__
@@ -1501,7 +1490,10 @@ class MreRefr(MelRecord):
     """Placed Object."""
     classType = 'REFR'
 
-    _flags = Flags(0L,Flags.getNames('visible', 'canTravelTo'))
+    _marker_flags = Flags(0, Flags.getNames(
+        'visible',
+        'can_travel_to',
+    ))
     _parentFlags = Flags(0L,Flags.getNames('oppositeParent'))
     _actFlags = Flags(0L, Flags.getNames('useDefault', 'activate', 'open',
                                          'openByDefault'))
@@ -1515,48 +1507,6 @@ class MreRefr(MelRecord):
                                 + self.defaults[len(unpacked_val) - 2:-2]
                                 + unpacked_val[-2:])
             return unpacked_val
-
-    class MelRefrXmrk(MelStruct):
-        """Handler for xmrk record. Conditionally loads next items."""
-        def loadData(self, record, ins, sub_type, size_, readId):
-            """Reads data from ins into record attribute."""
-            ins.seek(size_, 1, readId) # skip junk
-            record.hasXmrk = True
-            insTell = ins.tell
-            insUnpack = ins.unpack
-            pos = insTell()
-            (sub_type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            while sub_type_ in ['FNAM', 'FULL', 'TNAM']:
-                if sub_type_ == 'FNAM':
-                    value = insUnpack('B', size_, readId)
-                    record.flags = MreRefr._flags(*value)
-                elif sub_type_ == 'FULL':
-                    record.full = ins.readString(size_, readId)
-                elif sub_type_ == 'TNAM':
-                    record.markerType, record.unused5 = insUnpack('Bs', size_,
-                                                                  readId)
-                pos = insTell()
-                (sub_type_, size_) = insUnpack('4sH', 6, readId + '.FULL')
-            ins.seek(pos)
-            if self._debug: print ' ', record.flags, record.full, \
-                record.markerType
-
-        def dumpData(self,record,out):
-            if (record.flags, record.full, record.markerType,
-                record.unused5) != self.defaults[1:]:
-                record.hasXmrk = True
-            if record.hasXmrk:
-                try:
-                    out.write(struct_pack('=4sH','XMRK',0))
-                    out.packSub('FNAM','B',record.flags.dump())
-                    value = record.full
-                    if value is not None:
-                        out.packSub0('FULL',value)
-                    out.packSub('TNAM','Bs',record.markerType, record.unused5)
-                except struct.error:
-                    print self.subType, self.format, record.flags, \
-                        record.full, record.markerType
-                    raise
 
     melSet = MelSet(
         MelEdid(),
@@ -1587,8 +1537,12 @@ class MreRefr(MelRecord):
         MelFid('XRTM','xrtm'),
         MelOptUInt32('XACT', (_actFlags, 'actFlags', 0L)),
         MelOptSInt32('XCNT', 'count'),
-        MelRefrXmrk('XMRK', '', ('hasXmrk', False), (_flags, 'flags', 0L),
-                    'full', 'markerType', ('unused5', null1)),
+        MelGroup('map_marker',
+            MelBase('XMRK', 'marker_data'),
+            MelOptUInt8('FNAM', (_marker_flags, 'marker_flags')),
+            MelFull(),
+            MelOptStruct('TNAM', 'Bs', 'marker_type', 'unused1'),
+        ),
         MelBase('ONAM','onam_p'),
         MelBase('XRGD','xrgd_p'),
         MelOptFloat('XSCL', ('scale', 1.0)),

commit 915f7148e8cc0fbec4021bbbf686a2956831043d

Regression: fix _handle_load_error erroring
Introduced in 2c1f98a37f2a0009b804c76bcbf4f8a6a66c4e4a.

Yup, the error handler was erroring :)
Took the opportunity to drop a pointless deprint and raise the wrapped
ModError instead, which will cause the intended effect of making loading
only that mod fail instead of taking the entire program with it.

Mopy/bash/initialization.py:
Drop unused sys and os imports

Unused since d0eb20c1862260baa4a7d9d7d65c758a75b45786.

Mopy/bash/brec.py
@@ -1840,15 +1840,14 @@ class MelSet(object):
                 self._handle_load_error(error, record, ins, sub_type, sub_size)
 
     def _handle_load_error(self, error, record, ins, sub_type, sub_size):
-        bolt.deprint(error)
         eid = getattr(record, 'eid', u'<<NO EID>>')
         bolt.deprint(u'Error loading %r record and/or subrecord: %08X' %
-                     record.recType, record.fid)
+                     (record.recType, record.fid))
         bolt.deprint(u'  eid = %r' % eid)
         bolt.deprint(u'  subrecord = %r' % sub_type)
         bolt.deprint(u'  subrecord size = %d' % sub_size)
         bolt.deprint(u'  file pos = %d' % ins.tell())
-        raise
+        raise error
 
     def dumpData(self,record, out):
         """Dumps state into out. Called by getSize()."""

Mopy/bash/initialization.py
@@ -24,8 +24,6 @@
 """Functions for initializing Bash data structures on boot. For now export
 functions to init bass.dirs that need be initialized high up into the boot
 sequence to be able to backup/restore settings."""
-import os
-import sys
 from ConfigParser import ConfigParser
 # Local - don't import anything else
 import env