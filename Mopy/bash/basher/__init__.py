wrye-bash/wrye-bash

commit f1075eab9c78d387e604339c09481c82059e6dbb

Prepare builtin reduce changes for py3

Mopy/bash/basher/__init__.py
@@ -56,11 +56,10 @@ from __future__ import division
 import StringIO
 import collections
 import os
-import re
 import sys
 import time
 from collections import OrderedDict, namedtuple
-from functools import partial
+from functools import partial, reduce
 from operator import itemgetter
 #--wxPython
 import wx

Mopy/bash/cint.py
@@ -43,6 +43,7 @@ from ctypes import byref, cast, c_bool, c_byte, c_char, c_char_p, c_float, \
     POINTER, string_at
 import math
 import os
+from functools import reduce
 from os.path import exists, join
 try:
     #See if cint is being used by Wrye Bash

Mopy/bash/patcher/patchers/importers.py
@@ -25,6 +25,7 @@
 """This module contains the oblivion importer patcher classes."""
 import re
 from collections import defaultdict, Counter
+from functools import reduce
 from itertools import chain
 from operator import attrgetter
 # Internal

commit cccf29024ed20e8978205fa655401ffd034f07f1

Regression: Fix Screenshots tab failing to load
Introduced in f1075eab9c78d387e604339c09481c82059e6dbb, due to a bad
cherry-pick.

__init__.py 3484 __init__: Error constructing 'Screenshots' panel.
Traceback (most recent call last):
  File "bash\basher\__init__.py", line 3476, in __init__
    item = panel(self)
  File "bash\basher\__init__.py", line 3161, in __init__
    super(ScreensPanel, self).__init__(parent)
  File "bash\basher\__init__.py", line 251, in __init__
    super(BashTab, self).__init__(parent, isVertical)
  File "bash\basher\__init__.py", line 212, in __init__
    keyPrefix=self.keyPrefix, panel=self)
  File "bash\balt.py", line 1143, in __init__
    self.PopulateItems()
  File "bash\balt.py", line 1257, in PopulateItems
    self.SortItems()
  File "bash\balt.py", line 1492, in SortItems
    items = self._SortItems(column, reverse)
  File "bash\balt.py", line 1538, in _SortItems
    for lamda in self._extra_sortings: lamda(self, items)
  File "bash\basher\__init__.py", line 3051, in _order_by_number
    regex = re.compile(u'(.*?)(\d*)' + self.__ext_group + u'$')
NameError: global name 're' is not defined

Mopy/bash/basher/__init__.py
@@ -56,6 +56,7 @@ from __future__ import division
 import StringIO
 import collections
 import os
+import re
 import sys
 import time
 from collections import OrderedDict, namedtuple