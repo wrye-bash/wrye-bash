wrye-bash/wrye-bash

commit 4d64ee740cad85d04b0a5078c8c0fc0250361331

Display actually human-readable file sizes
124524 KB isn't much better than showing 127512576 B. 121.6 MB, on the
other hand, is much more understandable.
Note that I would like to (eventually) offer an option to show the
binary prefixes instead (i.e. KiB, MiB, etc.). Especially on Linux,
where they are pretty much standard.

This does not affect sorting in any way - we already correctly
differentiate between the visual display and the actual sorting logic,
which uses the raw byte number.

Mopy/bash/game/__init__.py:
Mopy/bash/game/fallout4/__init__.py:
Mopy/bash/parsers.py:
Properly support FO4's expanded ESL range

Follow-up to #92, the recent FO4 update allowed plugins with at least
one master to make use of the 0x000-0x800 range.

This works because the hardcoded records are actually only needed if the
plugin has no masters, the game master contains them no matter what.
It's especially useful for ESLs, which may now contain twice as many
records (4096 instead of 2048).

Mopy/bash/bash.py:
traceback.format_exc does not take an error

I have no idea why this code did that. As far as I can tell, format_exc
*never* took an error, and this will cause a crash in py3.

Mopy/bash/bash.py
@@ -85,11 +85,11 @@ def _import_wx():
         # moved/deleted wx modules
         from wx import _core
         sys.modules['wx._gdi'] = _core
-    except Exception as e:
+    except:
         but_kwargs = {'text': _(u"QUIT"),
                       'fg': 'red'}  # foreground button color
         msg = u'\n'.join([dump_environment(), u'', u'Unable to load wx:',
-                          traceback.format_exc(e), u'Exiting.'])
+                          traceback.format_exc(), u'Exiting.'])
         _tkinter_error_dial(msg, but_kwargs)
         sys.exit(1)
 
@@ -202,7 +202,7 @@ def main(opts):
     wx_locale = localize.setup_locale(opts.language, _wx)
     try:
         _main(opts, wx_locale)
-    except Exception as e:
+    except:
         msg = u'\n'.join([
             _(u'Wrye Bash encountered an error.'),
             _(u'Please post the information below to the official thread at'),
@@ -210,7 +210,7 @@ def main(opts):
             _(u'or to the Wrye Bash Discord at'),
             _(u'https://discord.gg/NwWvAFR'),
             u'',
-            traceback.format_exc(e)
+            traceback.format_exc()
         ])
         _close_dialog_windows()
         _show_wx_error(msg)
@@ -285,10 +285,10 @@ def _main(opts, wx_locale):
         env.isUAC = env.testUAC(bush_game.gamePath.join(u'Data'))
         global basher, balt
         from . import basher, balt
-    except (exception.BoltError, ImportError, OSError, IOError) as e:
+    except (exception.BoltError, ImportError, OSError, IOError):
         msg = u'\n'.join([_(u'Error! Unable to start Wrye Bash.'), u'\n', _(
             u'Please ensure Wrye Bash is correctly installed.'), u'\n',
-                          traceback.format_exc(e)])
+                          traceback.format_exc()])
         _close_dialog_windows()
         _show_wx_error(msg)
         return
@@ -464,13 +464,13 @@ def _show_wx_error(msg):
                               'fg': 'red'}  # foreground button color
                 _tkinter_error_dial(msg, but_kwargs)
 
-    except Exception as e:
+    except:
         print(u'Wrye Bash encountered an error but could not display it.')
         print(u'The following is the error that occurred when displaying the '\
               u'first error:')
         try:
-            print(traceback.format_exc(e))
-        except Exception:
+            print(traceback.format_exc())
+        except:
             print(u'   An error occurred while displaying the second error.')
 
 def _tkinter_error_dial(msg, but_kwargs):

Mopy/bash/bolt.py
@@ -181,9 +181,20 @@ def encode(text_str, encodings=encodingOrder, firstEncoding=None,
 
 def timestamp(): return datetime.datetime.now().strftime(u'%Y-%m-%d %H.%M.%S')
 
-def round_size(kbytes):
-    """Round non zero sizes to 1 KB."""
-    return u'%u KB' % (0 if kbytes == 0 else max(kbytes, 1024) // 1024)
+##: Keep an eye on https://bugs.python.org/issue31749
+def round_size(size_bytes):
+    """Returns the specified size in bytes as a human-readable size string."""
+    ##: Maybe offer an option to switch between KiB and KB?
+    prefix_pt2 = u'B' # if bass.settings[...] else u'iB'
+    size_bytes /= 1024 # Don't show bytes
+    for prefix_pt1 in (u'K', u'M', u'G', u'T', u'P', u'E', u'Z', u'Y'):
+        if size_bytes < 1024:
+            # Show a single decimal digit, but never show trailing zeroes
+            return u'%s %s' % (
+                    (u'%.1f' % size_bytes).rstrip(u'0').rstrip(u'.'),
+                    prefix_pt1 + prefix_pt2)
+        size_bytes /= 1024
+    return _(u'<very large>') # ;)
 
 # Helpers ---------------------------------------------------------------------
 def sortFiles(files, __split=os.path.split):

Mopy/bash/game/__init__.py
@@ -270,6 +270,10 @@ class GameInfo(object):
         ]
         # Signature of the main plugin header record type
         plugin_header_sig = b'TES4'
+        # If True, then plugins with at least one master can use the
+        # 0x000-0x800 range for their own records.
+        # If False, that range is reserved for hardcoded engine records.
+        expanded_plugin_range = False
 
     # Bash Tags supported by this game
     allTags = set()

Mopy/bash/game/fallout4/__init__.py
@@ -127,6 +127,7 @@ class Fallout4GameInfo(GameInfo):
         canBash = True
         canEditHeader = True
         validHeaderVersions = (0.95, 1.0)
+        expanded_plugin_range = True
 
     allTags = {
         u'Deactivate', u'Delev', u'Filter', u'ObjectBounds', u'Relev',

Mopy/bash/parsers.py
@@ -4086,16 +4086,25 @@ class ModFile(object):
     def getShortMapper(self):
         """Returns a mapping function to map long fids to short fids."""
         masters = self.tes4.masters + [self.fileInfo.name]
-        indices = dict((name, index) for index, name in enumerate(masters))
+        indices = {name: index for index, name in enumerate(masters)}
         gLong = self.getLongMapper()
+        has_expanded_range = bush.game.esp.expanded_plugin_range
+        if has_expanded_range and len(masters) > 1:
+            # Plugin has at least one master, it may freely use the
+            # expanded (0x000-0x800) range
+            def _master_index(m_name, obj_id):
+                return indices[m_name]
+        else:
+            # 0x000-0x800 are reserved for hardcoded (engine) records
+            def _master_index(m_name, obj_id):
+                return indices[m_name] if obj_id >= 0x800 else 0
         def mapper(fid):
             if fid is None: return None
-            if isinstance(fid, int):  # PY3 ensure this can never be long
+            ##: #312: drop this once convertToLongFids is auto-applied
+            if isinstance(fid, int):
                 fid = gLong(fid)
             modName, object_id = fid
-            long_id = int(object_id)
-            mod = indices[modName] if long_id >= 0x800 else 0
-            return (int(mod) << 24) | long_id
+            return (_master_index(modName, object_id) << 24) | object_id
         return mapper
 
     def convertToLongFids(self,types=None):

commit 9e90d6e5ed0ce6dcb14b6172d7ba43df3eb32660

Regression: fix bash.py catching too many exceptions
Broke in 4d64ee740cad85d04b0a5078c8c0fc0250361331, which broadened those
excepts from 'except Exception:' to just a plain 'except:', which made
them catch ones they definitely weren't supposed to catch - e.g. the
'Only one instance of Wrye Bash can run' popup called sys.exit, which
raises SystemExit, but that got caught by the 'except:'s and displayed
as an 'unhandled error'.

Mopy/bash/bash.py
@@ -86,7 +86,7 @@ def _import_wx():
         # moved/deleted wx modules
         from wx import _core
         sys.modules['wx._gdi'] = _core
-    except:
+    except Exception:
         but_kwargs = {'text': u"QUIT",
                       'fg': 'red'}  # foreground button color
         msg = u'\n'.join([dump_environment(), u'', u'Unable to load wx:',
@@ -204,7 +204,7 @@ def main(opts):
     wx_locale = localize.setup_locale(opts.language, _wx)
     try:
         _main(opts, wx_locale)
-    except:
+    except Exception:
         msg = u'\n'.join([
             _(u'Wrye Bash encountered an error.'),
             _(u'Please post the information below to the official thread at'),
@@ -474,13 +474,13 @@ def _show_wx_popup(msg, is_critical=True):
                 but_kwargs = {u'text': u'QUIT' if is_critical else u'OK',
                               u'fg': u'red'}  # foreground button color
                 _tkinter_error_dial(msg, but_kwargs)
-    except:
+    except Exception:
         print(u'Wrye Bash encountered an error but could not display it.')
         print(u'The following is the error that occurred when displaying the '\
               u'first error:')
         try:
             print(traceback.format_exc())
-        except:
+        except Exception:
             print(u'   An error occurred while displaying the second error.')
 
 def _tkinter_error_dial(msg, but_kwargs):