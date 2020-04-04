wrye-bash/wrye-bash

commit 36b0ef5076580e1ef7c96a6e60aa71d20564e885

[!]Move ini_files to top:
Original motivation was to use in parsing the game ini as soon as
possible, but this needs more work (rewrite the ConfigParser code).
Turns out this is a move in the right (init management) direction.
Will be useful in morrowind and in general - reworked to get
rid of a very nasty coupling between ini_files and bosh globals
(oblivionIni and iniInfos).

- Fixup in target_ini_exists logic (note return msg)
- Decouple ini_files from balt

Tagged an [!] as I now instantiate *but* the main game ini as GameIni:

@@ -3221,5 +3217,5 @@ def initBosh(bashIni, game_ini_path):
     gameInis = [oblivionIni]
-    gameInis.extend(OblivionIni(dirs['saveBase'].join(x), 'cp1252') for x in
+    gameInis.extend(IniFile(dirs['saveBase'].join(x), 'cp1252') for x in
                     bush.game.iniFiles[1:])

I followed uses and those should (must) not be GameIni instances
but I may have missed smth.

Signed-off-by: MrD <the.ubik@gmail.com>

Mopy/bash/basher/__init__.py
@@ -634,11 +634,11 @@ class INIList(balt.UIList):
         if self.apply_tweaks((bosh.iniInfos[hitItem], )):
             self.panel.ShowPanel(refresh_target=True)
 
-    @staticmethod
-    def apply_tweaks(tweak_infos, target_ini=None):
+    @classmethod
+    def apply_tweaks(cls, tweak_infos, target_ini=None):
         target_ini_file = target_ini or bosh.iniInfos.ini
-        if not target_ini_file.ask_create_target_ini() or not \
-                INIList._warn_tweak_game_ini(target_ini_file.abs_path.stail):
+        if not cls.ask_create_target_ini(target_ini) or not \
+                cls._warn_tweak_game_ini(target_ini_file.abs_path.stail):
             return False
         needsRefresh = False
         for ini_info in tweak_infos:
@@ -651,6 +651,39 @@ class INIList(balt.UIList):
             needsRefresh |= target_ini_file.applyTweakFile(
                 ini_info.read_ini_content())
         return needsRefresh
+
+    @staticmethod
+    @balt.conversation
+    def ask_create_target_ini(target_ini_file, msg):
+        """Check if target ini for operation exists - if not and the target is
+        the game ini ask if the user wants to create it by copying the default
+        ini"""
+        msg = target_ini_file.target_ini_exist(msg)
+        if msg in (True, False): return msg
+        # Game ini does not exist - try copying the default game ini
+        default_ini = bass.dirs[u'app'].join(bush.game.defaultIniFile)
+        if default_ini.exists():
+            msg += _(u'Do you want Bash to create it by copying '
+                     u'%(default_ini)s ?' % {u'default_ini': default_ini})
+            if not balt.askYes(None, msg, _(u'Missing game Ini')):
+                return False
+        else:
+            msg += _(u'Please create it manually to continue.')
+            balt.showError(None, msg, _(u'Missing game Ini'))
+            return False
+        try:
+            default_ini.copyTo(target_ini_file.abs_path)
+            if balt.Link.Frame.iniList:
+                balt.Link.Frame.iniList.panel.ShowPanel(refresh_target=True)
+            else:
+                bosh.iniInfos.refresh(refresh_infos=False)
+            return True
+        except (OSError, IOError):
+            error_msg = u'Failed to copy %s to %s' % (
+                default_ini, target_ini_file.abs_path)
+            deprint(error_msg, traceback=True)
+            balt.showError(None, error_msg, _(u'Missing game Ini'))
+        return False
 
     @staticmethod
     @balt.conversation
@@ -1930,6 +1963,14 @@ class SaveList(balt.UIList):
         newName = self.data_store.enable(hitItem, newEnabled)
         if newName != hitItem: self.RefreshUI(redraw=[newName],
                                               to_del=[hitItem])
+
+    # Save profiles
+    def set_local_save(self, new_saves, refreshSaveInfos):
+        if not INIList.ask_create_target_ini(bosh.oblivionIni, msg=_(
+            u'Setting the save profile is done by editing the game ini.')):
+            return
+        self.data_store.setLocalSave(new_saves, refreshSaveInfos)
+        balt.Link.Frame.set_bash_frame_title()
 
 #------------------------------------------------------------------------------
 class SaveDetails(_ModsSavesDetails):

Mopy/bash/bosh/__init__.py
@@ -56,6 +56,8 @@ from ..cint import CBashApi
 from ..exception import AbstractError, ArgumentError, BoltError, BSAError, \
     CancelError, DDSError, FileError, ModError, PluginsFullError, \
     SaveFileError, SaveHeaderError, SkipError, StateError
+from ..ini_files import IniFile, OBSEIniFile, DefaultIniFile, GameIni, \
+    get_ini_type_and_encoding
 from ..parsers import ModFile
 
 # Singletons, Constants -------------------------------------------------------
@@ -79,8 +81,8 @@ undefinedPaths = {GPath(u'C:\\Path\\exe.exe'), undefinedPath}
 oiMask = 0xFFFFFF
 
 #--Singletons
-gameInis = None    # type: tuple[OblivionIni]
-oblivionIni = None # type: OblivionIni
+gameInis = None    # type: tuple[GameIni | IniFile]
+oblivionIni = None # type: GameIni
 modInfos  = None   # type: ModInfos
 saveInfos = None   # type: SaveInfos
 iniInfos = None    # type: INIInfos
@@ -791,10 +793,8 @@ class ModInfo(FileInfo):
         return self.dir.join(*resource_path).join(self.name).exists()
 
 #------------------------------------------------------------------------------
-from .ini_files import IniFile, OBSEIniFile, DefaultIniFile, OblivionIni, \
-    get_ini_type_and_encoding
 def get_game_ini(ini_path, is_abs=True):
-    """:rtype: OblivionIni | None"""
+    """:rtype: GameIni | IniFile | None"""
     for game_ini in gameInis:
         game_ini_path = game_ini.abs_path
         if ini_path == ((is_abs and game_ini_path) or game_ini_path.stail):
@@ -810,7 +810,6 @@ def BestIniFile(abs_ini_path):
         abs_ini_path)
     return inferred_ini_type(abs_ini_path, detected_encoding)
 
-#------------------------------------------------------------------------------
 class INIInfo(IniFile):
     """Ini info, adding cached status and functionality to the ini files."""
     _status = None
@@ -2824,11 +2823,8 @@ class SaveInfos(FileInfos):
         self._initDB(dirs['saveBase'].join(self.localSave))
 
     def setLocalSave(self, localSave, refreshSaveInfos=True):
-        """Sets SLocalSavePath in Oblivion.ini."""
+        """Sets SLocalSavePath in Oblivion.ini. The latter must exist."""
         self.table.save()
-        if not oblivionIni.ask_create_target_ini(msg=_(
-                u'Setting the save profile is done by editing the game ini.')):
-            return
         self.localSave = localSave
         ##: not sure if appending the slash is needed for the game to parse
         # the setting correctly, kept previous behavior
@@ -3219,9 +3215,9 @@ def initBosh(bashIni, game_ini_path):
     # game ini files
     deprint(u'Found main game INI at %s' % game_ini_path)
     global oblivionIni, gameInis
-    oblivionIni = OblivionIni(game_ini_path, 'cp1252')
+    oblivionIni = GameIni(game_ini_path, 'cp1252')
     gameInis = [oblivionIni]
-    gameInis.extend(OblivionIni(dirs['saveBase'].join(x), 'cp1252') for x in
+    gameInis.extend(IniFile(dirs['saveBase'].join(x), 'cp1252') for x in
                     bush.game.iniFiles[1:])
     load_order.initialize_load_order_files()
     initOptions(bashIni)

Mopy/bash/ini_files.py
@@ -26,11 +26,11 @@ import re
 import time
 from collections import OrderedDict, Counter
 
-from .. import env, bush, balt
-from ..bass import dirs
-from ..bolt import LowerDict, CIstr, deprint, GPath, DefaultLowerDict, \
+from . import env, bush
+from .bass import dirs
+from .bolt import LowerDict, CIstr, deprint, GPath, DefaultLowerDict, \
     decode, getbestencoding, AFile
-from ..exception import AbstractError, CancelError, SkipError, BoltError
+from .exception import AbstractError, CancelError, SkipError, BoltError
 
 def _to_lower(ini_settings): # transform dict of dict to LowerDict of LowerDict
     return LowerDict((x, LowerDict(y)) for x, y in ini_settings.iteritems())
@@ -263,8 +263,8 @@ class IniFile(AFile):
         to out_encoding."""
         return codecs.getwriter(self.out_encoding)(open(filepath, 'w'))
 
-    def ask_create_target_ini(self, msg=_(
-            u'The target ini must exist to apply a tweak to it.')):
+    def target_ini_exist(self, msg=_(
+        u'The target ini must exist to apply a tweak to it.')):
         return self.abs_path.isfile()
 
     def saveSettings(self,ini_settings,deleted_settings={}):
@@ -554,8 +554,8 @@ class OBSEIniFile(IniFile):
         self.saveSettings(ini_settings,deleted_settings)
         return True
 
-class OblivionIni(IniFile):
-    """Oblivion.ini file."""
+class GameIni(IniFile):
+    """Main game ini file. Only use to instantiate bosh.oblivionIni"""
     bsaRedirectors = {u'archiveinvalidationinvalidated!.bsa',
                       u'..\\obmm\\bsaredirection.bsa'}
     _ini_language = None
@@ -571,37 +571,14 @@ class OblivionIni(IniFile):
                                                  u'English')
         return self._ini_language
 
-    @balt.conversation
-    def ask_create_target_ini(self, msg=_(
-            u'The game ini must exist to apply a tweak to it.')):
-        if super(OblivionIni, self).ask_create_target_ini(msg): return True
-        from . import oblivionIni, iniInfos # YAK
-        if self is not oblivionIni: return True
-        srcPath = dirs['app'].join(bush.game.defaultIniFile)
-        default_path_exists = srcPath.exists()
+    def target_ini_exist(self, msg=_(
+        u'The game ini must exist to apply a tweak to it.')):
+        """Attempt to create the game Ini in some scenarios"""
+        target_exists = super(GameIni, self).target_ini_exist()
+        if target_exists: return True
         msg = _(u'%(ini_path)s does not exist.' % {'ini_path': self.abs_path}) + \
               u'\n\n' + ((msg + u'\n\n') if msg else u'')
-        if default_path_exists:
-            msg += _(u'Do you want Bash to create it by copying '
-                     u'%(default_ini)s ?' % {'default_ini': srcPath})
-            if not balt.askYes(None, msg, _(u'Missing game Ini')):
-                return False
-        else:
-            msg += _(u'Please create it manually to continue.')
-            balt.showError(None, msg, _(u'Missing game Ini'))
-            return False
-        try:
-            srcPath.copyTo(self.abs_path)
-            if balt.Link.Frame.iniList:
-                balt.Link.Frame.iniList.panel.ShowPanel(refresh_target=True)
-            else:
-                iniInfos.refresh(refresh_infos=False)
-            return True
-        except (OSError, IOError):
-            error_msg = u'Failed to copy %s to %s' % (srcPath, self.abs_path)
-            deprint(error_msg, traceback=True)
-            balt.showError(None, error_msg, _(u'Missing game Ini'))
-        return False
+        return msg
 
     #--BSA Redirection --------------------------------------------------------
     def setBsaRedirection(self,doRedirect=True):

Mopy/bash/basher/saves_links.py
@@ -129,8 +129,7 @@ class Saves_ProfilesData(balt.ListEditorData):
         oldDir.moveTo(newDir)
         oldSaves, newSaves = (_win_join(name_) for name_ in (oldName, newName))
         if bosh.saveInfos.localSave == oldSaves:
-            bosh.saveInfos.setLocalSave(newSaves)
-            Link.Frame.set_bash_frame_title()
+            Link.Frame.iniList.set_local_save(newSaves)
         bosh.saveInfos.profiles.moveRow(oldSaves,newSaves)
         return newName
 
@@ -181,9 +180,8 @@ class Saves_Profiles(ChoiceLink):
             arcSaves = bosh.saveInfos.localSave
             newSaves = self.relativePath
             with balt.BusyCursor():
-                bosh.saveInfos.setLocalSave(newSaves, refreshSaveInfos=False)
+                Link.Frame.iniList.set_local_save(newSaves, refreshSaveInfos=False)
                 bosh.modInfos.swapPluginsAndMasterVersion(arcSaves, newSaves)
-                Link.Frame.set_bash_frame_title()
                 bosh.saveInfos.refresh()
                 self.window.DeleteAll() # let call below repopulate
                 self.window.RefreshUI(detail_item=None)

Mopy/bash/belt.py
@@ -33,8 +33,7 @@ from collections import OrderedDict
 import wx.adv as wiz  # wxPython wizard class
 from . import ScriptParser         # generic parser class
 from . import balt, bass, bolt, bosh, bush, load_order
-#Translateable strings
-from .bosh import OBSEIniFile
+from .ini_files import OBSEIniFile
 from .env import get_file_version
 from .gui import BOTTOM, CENTER, CheckBox, GridLayout, HBoxedLayout, HLayout, \
     Label, LayoutOptions, RIGHT, Stretch, TextArea, VLayout, HyperlinkLabel, \

Mopy/bash/bosh/bain.py
@@ -37,7 +37,6 @@ from itertools import groupby, imap
 from operator import itemgetter, attrgetter
 
 from . import imageExts, DataStore, BestIniFile, InstallerConverter, ModInfos
-from .ini_files import OBSEIniFile
 from .. import balt # YAK!
 from .. import bush, bass, bolt, env, archives
 from ..archives import readExts, defaultExt, list_archive, compress7z, \
@@ -46,6 +45,7 @@ from ..bolt import Path, deprint, round_size, GPath, sio, SubProgress, CIstr, \
     LowerDict, AFile
 from ..exception import AbstractError, ArgumentError, BSAError, CancelError, \
     InstallerArchiveError, SkipError, StateError, FileError
+from ..ini_files import OBSEIniFile
 
 os_sep = unicode(os.path.sep)
 

Mopy/bash/initialization.py
@@ -125,6 +125,11 @@ def getLegacyPathWithSource(newPath, oldPath, newSrc, oldSrc=None):
         return oldPath, oldSrc
 
 def init_dirs(bashIni_, personal, localAppData, game_info):
+    """Initialize bass.dirs dictionary. We need the bash.ini and the game
+    being set, so this is called upon setting the game. Global structures
+    that need info on Bash / Game dirs should be initialized here and set
+    as globals in module scope. It may be called two times if restoring
+    settings fails."""
     if not mopy_dirs_initialized:
         raise BoltError(u'init_dirs: Mopy dirs uninitialized')
     # Any warnings found during this stage can be added here as strings
@@ -152,7 +157,7 @@ def init_dirs(bashIni_, personal, localAppData, game_info):
     game_ini_path = dirs['saveBase'].join(game_info.iniFiles[0])
     dirs['mods'] = dirs['app'].join(u'Data')
     if data_oblivion_ini.isfile():
-        oblivionIni = ConfigParser(allow_no_value=True)
+        oblivionIni = ConfigParser(allow_no_value=True) ##: use GameIni here
         try:
             try:
                 # Try UTF-8 first, will also work for ASCII-encoded files

commit 7a458991de74a787dd672cb3868d95042a072ce7

Regression: fix applying tweaks
Ha, that took a long time :P
Introduced in 36b0ef5076580e1ef7c96a6e60aa71d20564e885.

Fixes two tracebacks:
Traceback (most recent call last):
  File "bash\balt.py", line 1792, in __Execute
    self.Execute()
  File "bash\basher\ini_links.py", line 142, in Execute
    if self.window.apply_tweaks(self.iselected_infos()):
  File "bash\basher\__init__.py", line 640, in apply_tweaks
    if not cls.ask_create_target_ini(target_ini) or not \
  File "bash\balt.py", line 911, in _conversation_wrapper
    return func(*args, **kwargs)
TypeError: ask_create_target_ini() takes exactly 2 arguments (1 given)

Traceback (most recent call last):
  File "bash\balt.py", line 1792, in __Execute
    self.Execute()
  File "bash\basher\ini_links.py", line 142, in Execute
    if self.window.apply_tweaks(self.iselected_infos()):
  File "bash\basher\__init__.py", line 640, in apply_tweaks
    if not cls.ask_create_target_ini(target_ini, u'') or not \
  File "bash\balt.py", line 911, in _conversation_wrapper
    return func(*args, **kwargs)
  File "bash\basher\__init__.py", line 661, in ask_create_target_ini
    msg = target_ini_file.target_ini_exist(msg)
AttributeError: 'NoneType' object has no attribute 'target_ini_exist'

Also fixes the popup for creating the game INI no longer showing the
'game INI must exist' part of the message and tweaks the startup INI
deprint to be more accurate. And I renamed target_ini_exist ->
target_ini_exists, cause it was bothering me :P

Mopy/bash/basher/__init__.py
@@ -637,7 +637,7 @@ class INIList(balt.UIList):
     @classmethod
     def apply_tweaks(cls, tweak_infos, target_ini=None):
         target_ini_file = target_ini or bosh.iniInfos.ini
-        if not cls.ask_create_target_ini(target_ini) or not \
+        if not cls.ask_create_target_ini(target_ini_file) or not \
                 cls._warn_tweak_game_ini(target_ini_file.abs_path.stail):
             return False
         needsRefresh = False
@@ -654,11 +654,11 @@ class INIList(balt.UIList):
 
     @staticmethod
     @balt.conversation
-    def ask_create_target_ini(target_ini_file, msg):
+    def ask_create_target_ini(target_ini_file, msg=None):
         """Check if target ini for operation exists - if not and the target is
         the game ini ask if the user wants to create it by copying the default
         ini"""
-        msg = target_ini_file.target_ini_exist(msg)
+        msg = target_ini_file.target_ini_exists(msg)
         if msg in (True, False): return msg
         # Game ini does not exist - try copying the default game ini
         default_ini = bass.dirs[u'app'].join(bush.game.defaultIniFile)

Mopy/bash/bosh/__init__.py
@@ -3213,7 +3213,7 @@ def initBosh(bashIni, game_ini_path):
     global configHelpers
     configHelpers = ConfigHelpers()
     # game ini files
-    deprint(u'Found main game INI at %s' % game_ini_path)
+    deprint(u'Looking for main game INI at %s' % game_ini_path)
     global oblivionIni, gameInis
     oblivionIni = GameIni(game_ini_path, 'cp1252')
     gameInis = [oblivionIni]

Mopy/bash/ini_files.py
@@ -263,7 +263,7 @@ class IniFile(AFile):
         to out_encoding."""
         return codecs.getwriter(self.out_encoding)(open(filepath, 'w'))
 
-    def target_ini_exist(self, msg=_(
+    def target_ini_exists(self, msg=_(
         u'The target ini must exist to apply a tweak to it.')):
         return self.abs_path.isfile()
 
@@ -571,13 +571,14 @@ class GameIni(IniFile):
                                                  u'English')
         return self._ini_language
 
-    def target_ini_exist(self, msg=_(
-        u'The game ini must exist to apply a tweak to it.')):
+    def target_ini_exists(self, msg=None):
         """Attempt to create the game Ini in some scenarios"""
-        target_exists = super(GameIni, self).target_ini_exist()
+        if msg is None:
+            msg = _(u'The game INI must exist to apply a tweak to it.')
+        target_exists = super(GameIni, self).target_ini_exists()
         if target_exists: return True
-        msg = _(u'%(ini_path)s does not exist.' % {'ini_path': self.abs_path}) + \
-              u'\n\n' + ((msg + u'\n\n') if msg else u'')
+        msg = _(u'%(ini_path)s does not exist.' % {
+            u'ini_path': self.abs_path}) + u'\n\n' + msg + u'\n\n'
         return msg
 
     #--BSA Redirection --------------------------------------------------------