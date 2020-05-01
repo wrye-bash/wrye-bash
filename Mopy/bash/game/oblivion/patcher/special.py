wrye-bash/wrye-bash

commit d8d03ca39e1e9f85250fd014cabcc2a65945e5e7

Fix long integers for py3
Long integers don't really have a point - ints are auto-converted into
longs as needed, even literals.

Under #460

Mopy/bash/game/oblivion/patcher/special.py
@@ -43,7 +43,7 @@ __all__ = ['AlchemicalCatalogs', 'CBash_AlchemicalCatalogs', 'CoblExhaustion',
 # Util Functions --------------------------------------------------------------
 def _PrintFormID(fid):
     # PBash short Fid
-    if isinstance(fid,(long,int)):
+    if isinstance(fid,int):  # PY3 ensure this can never be long
         fid = u'%08X' % fid
     # PBash long FId
     elif isinstance(fid, tuple):

Mopy/bash/ScriptParser.py
@@ -399,7 +399,6 @@ class Parser(object):
         def __pos__(self): return Parser.Token(+self.tkn)
         def __abs__(self): return abs(self.tkn)
         def __int__(self): return int(self.tkn)
-        def __long__(self): return long(self.tkn)
         def __float__(self): return float(self.tkn)
         def __str__(self): return str(self.tkn)
 

Mopy/bash/balt.py
@@ -1743,7 +1743,7 @@ class ListCtrl(wx.ListCtrl, ListCtrlAutoWidthMixin):
     # Ripped from Tank - _Monkey patch_ - we need a proper ListCtrl subclass
 
     def __id(self, item):
-        i = long(wx.NewId())
+        i = int(wx.NewId())
         self._item_itemId[item] = i
         self._itemId_item[i] = item
         return i

Mopy/bash/bolt.py
@@ -614,7 +614,7 @@ class Path(object):
     @property
     def crc(self):
         """Calculates and returns crc value for self."""
-        crc = 0L
+        crc = 0
         with self.open('rb') as ins:
             for block in iter(partial(ins.read, 2097152), ''):
                 crc = crc32(block, crc) # 2MB at a time, probably ok
@@ -935,13 +935,13 @@ class Flags(object):
     #--Generation
     def __init__(self,value=0,names=None):
         """Initialize. Attrs, if present, is mapping of attribute names to indices. See getAttrs()"""
-        object.__setattr__(self,'_field',int(value) | 0L)
+        object.__setattr__(self,'_field',int(value))
         object.__setattr__(self,'_names',names or {})
 
     def __call__(self,newValue=None):
         """Returns a clone of self, optionally with new value."""
         if newValue is not None:
-            return Flags(int(newValue) | 0L,self._names)
+            return Flags(int(newValue),self._names)
         else:
             return Flags(self._field,self._names)
 
@@ -977,8 +977,8 @@ class Flags(object):
 
     def __setitem__(self,index,value):
         """Set value by index. E.g., flags[3] = True"""
-        value = ((value or 0L) and 1L) << index
-        mask = 1L << index
+        value = ((value or 0) and 1) << index
+        mask = 1 << index
         self._field = ((self._field & ~mask) | value)
 
     #--As class

Mopy/bash/bosh/__init__.py
@@ -69,7 +69,7 @@ undefinedPath = GPath(u'C:\\not\\a\\valid\\path.exe')
 empty_path = GPath(u'') # evaluates to False in boolean expressions
 undefinedPaths = {GPath(u'C:\\Path\\exe.exe'), undefinedPath}
 #..Bit-and this with the fid to get the objectindex.
-oiMask = 0xFFFFFFL
+oiMask = 0xFFFFFF
 
 #--Singletons
 gameInis = None    # type: tuple[OblivionIni]

Mopy/bash/bosh/_saves.py
@@ -46,7 +46,7 @@ from ..parsers import LoadFactory, ModFile
 class SreNPC(object):
     """NPC change record."""
     __slots__ = ('form','health','unused2','attributes','acbs','spells','factions','full','ai','skills','modifiers')
-    sre_flags = Flags(0L, Flags.getNames(
+    sre_flags = Flags(0, Flags.getNames(
         (0,'form'),
         (2,'health'),
         (3,'attributes'),
@@ -220,7 +220,7 @@ class SreNPC(object):
 # Save File -------------------------------------------------------------------
 class SaveFile(object):
     """Represents a Tes4 Save file."""
-    recordFlags = Flags(0L,Flags.getNames(
+    recordFlags = Flags(0,Flags.getNames(
         'form','baseid','moved','havocMoved','scale','allExtra','lock','owner','unk8','unk9',
         'mapMarkerFlags','hadHavokMoveFlag','unk12','unk13','unk14','unk15',
         'emptyFlag','droppedItem','doorDefaultState','doorState','teleport',
@@ -474,7 +474,7 @@ class SaveFile(object):
             if fid is None: return None
             modName,object = fid
             mod = indices[modName]
-            return (long(mod) << 24 ) | long(object)
+            return (int(mod) << 24 ) | int(object)
         return mapper
 
     def getFid(self,iref,default=None):

Mopy/bash/bosh/bain.py
@@ -139,7 +139,7 @@ class Installer(object):
             progress(done, progress_msg + rpFile)
             sub = bolt.SubProgress(progress, done, done + size + 1)
             sub.setFull(size + 1)
-            crc = 0L
+            crc = 0
             try:
                 with open(asFile, 'rb') as ins:
                     insTell = ins.tell
@@ -1153,7 +1153,7 @@ class InstallerArchive(Installer):
         with archive_path.unicodeSafe() as tempArch:
             try:
                 list_archive(tempArch, _parse_archive_line)
-                self.crc = _li.cumCRC & 0xFFFFFFFFL
+                self.crc = _li.cumCRC & 0xFFFFFFFF
             except:
                 archive_msg = u"Unable to read archive '%s'." % archive_path.s
                 deprint(archive_msg, traceback=True)
@@ -1411,7 +1411,7 @@ class InstallerProject(Installer):
             cumCRC += crc
             cumSize += size
         self.size = cumSize
-        self.crc = cumCRC & 0xFFFFFFFFL
+        self.crc = cumCRC & 0xFFFFFFFF
         self.project_refreshed = True
 
     def _install(self, dest_src, progress):
@@ -1801,7 +1801,7 @@ class InstallersData(DataStore):
         try:
             converter.apply(destArchive, crc_installer,
                             bolt.SubProgress(progress, 0.0, 0.99),
-                            embedded=installer.crc if installer else 0L)
+                            embedded=installer.crc if installer else 0)
             #--Add the new archive to Bash
             if destArchive not in self:
                 self[destArchive] = InstallerArchive(destArchive)

Mopy/bash/bosh/converters.py
@@ -321,7 +321,7 @@ class InstallerConverter(object):
             raise StateError, (u'Error creating BCF.dat:\nError: %s' % e), \
                 sys.exc_info()[2]
 
-    def apply(self, destArchive, crc_installer, progress=None, embedded=0L):
+    def apply(self, destArchive, crc_installer, progress=None, embedded=0):
         """Applies the BCF and packages the converted archive"""
         #--Prepare by fully loading the BCF and clearing temp
         self.load(True)

commit 115462bf1d8fea2f8c7d451dc5ceacf8e389e2d0

Regression: Fix convertToShortFids blowing up
Introduced in d8d03ca39e1e9f85250fd014cabcc2a65945e5e7. Somewhere along
the way the PY3 comments disappeared as well? Also made the same change
to _PrintFormID, not sure if needed.

Mopy/bash/game/oblivion/patcher/special.py
@@ -42,7 +42,7 @@ _cobl_main = GPath(u'COBL Main.esm')
 # Util Functions --------------------------------------------------------------
 def _PrintFormID(fid):
     # PBash short Fid
-    if isinstance(fid,int):  # PY3 ensure this can never be long
+    if isinstance(fid, (int, long)): # PY3: just int here
         fid = u'%08X' % fid
     # PBash long FId
     elif isinstance(fid, tuple):

Mopy/bash/mod_files.py
@@ -312,7 +312,7 @@ class ModFile(object):
         def mapper(fid):
             if fid is None: return None
             ##: #312: drop this once convertToLongFids is auto-applied
-            if isinstance(fid, int):
+            if isinstance(fid, (int, long)): # PY3: just int here
                 fid = gLong(fid)
             modName, object_id = fid
             return (_master_index(modName, object_id) << 24) | object_id