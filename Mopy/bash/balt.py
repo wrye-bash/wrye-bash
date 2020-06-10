wrye-bash/wrye-bash

commit 0c676af4e06a0cf40d145357099fe1ea133535f0

Clean up WithCharEvents a bit
Dropped the bind_* class vars - never False, plus creating EventHandlers
is free (they lazily bind when a client subscribes).
Also dropped the g_list event parameter - seems entirely unused (unless
a future usage was intended?).

Mopy/bash/balt.py
@@ -1177,7 +1177,7 @@ class UIList(wx.Panel):
             self.mouse_index = None
             Link.Frame.set_status_info(u'')
 
-    def _handle_key_up(self, wrapped_evt, g_list):
+    def _handle_key_up(self, wrapped_evt):
         """Char event: select all items, delete selected items, rename."""
         code = wrapped_evt.key_code
         if wrapped_evt.is_cmd_down and code == ord(u'A'): # Ctrl+A

Mopy/bash/basher/__init__.py
@@ -360,7 +360,7 @@ class MasterList(_ModsUIList):
             self.detailsPanel.testChanges() # disable buttons if no other edits
 
     def _handle_select(self, item_key): pass
-    def _handle_key_up(self, wrapped_evt, g_list): pass
+    def _handle_key_up(self, wrapped_evt): pass
 
     def OnDClick(self, lb_dex_and_flags):
         if self.mouse_index < 0: return # nothing was clicked
@@ -1012,7 +1012,7 @@ class ModList(_ModsUIList):
             return EventResult.CONTINUE
         return EventResult.FINISH
 
-    def _handle_key_up(self, wrapped_evt, g_list):
+    def _handle_key_up(self, wrapped_evt):
         """Char event: Activate selected items, select all items"""
         ##Space
         if wrapped_evt.is_space:
@@ -1029,7 +1029,7 @@ class ModList(_ModsUIList):
         elif wrapped_evt.is_cmd_down and wrapped_evt.key_code == ord(u'C'):
             balt.copyListToClipboard([self.data_store[mod].getPath().s
                                       for mod in self.GetSelected()])
-        super(ModList, self)._handle_key_up(wrapped_evt, g_list)
+        super(ModList, self)._handle_key_up(wrapped_evt)
 
     def _handle_left_down(self, wrapped_evt, lb_dex_and_flags):
         """Left Down: Check/uncheck mods.
@@ -1960,14 +1960,14 @@ class SaveList(balt.UIList):
         item_format.icon_key = status, on
 
     #--Events ---------------------------------------------
-    def _handle_key_up(self, wrapped_evt, g_list):
+    def _handle_key_up(self, wrapped_evt):
         code = wrapped_evt.key_code
         # Ctrl+C: Copy file(s) to clipboard
         if wrapped_evt.is_cmd_down and code == ord(u'C'):
             sel = map(lambda save: self.data_store[save].getPath().s,
                       self.GetSelected())
             balt.copyListToClipboard(sel)
-        super(SaveList, self)._handle_key_up(wrapped_evt, g_list)
+        super(SaveList, self)._handle_key_up(wrapped_evt)
 
     def _handle_left_down(self, wrapped_evt, lb_dex_and_flags):
         #--Pass Event onward
@@ -2531,7 +2531,7 @@ class InstallersList(balt.UIList):
         else:
             self.OpenSelected(selected=[item])
 
-    def _handle_key_up(self, wrapped_evt, g_list):
+    def _handle_key_up(self, wrapped_evt):
         """Char events: Action depends on keys pressed"""
         code = wrapped_evt.key_code
         # Ctrl+Shift+N - Add a marker
@@ -2543,7 +2543,7 @@ class InstallersList(balt.UIList):
             sel = map(lambda x: bass.dirs['installers'].join(x).s,
                       self.GetSelected())
             balt.copyListToClipboard(sel)
-        super(InstallersList, self)._handle_key_up(wrapped_evt, g_list)
+        super(InstallersList, self)._handle_key_up(wrapped_evt)
 
     # Installer specific ------------------------------------------------------
     def addMarker(self):
@@ -3157,9 +3157,9 @@ class ScreensList(balt.UIList):
     def OnChar(self, wrapped_evt):
         # Enter: Open selected screens
         if wrapped_evt.key_code in balt.wxReturn: self.OpenSelected()
-        else: super(ScreensList, self)._handle_key_up(wrapped_evt, None)
+        else: super(ScreensList, self)._handle_key_up(wrapped_evt)
 
-    def _handle_key_up(self, wrapped_evt, g_list):
+    def _handle_key_up(self, wrapped_evt):
         """Char event: Activate selected items, select all items"""
         code = wrapped_evt.key_code
         # Ctrl+C: Copy file(s) to clipboard
@@ -3167,7 +3167,7 @@ class ScreensList(balt.UIList):
             sel = map(lambda x: bosh.screen_infos.store_dir.join(x).s,
                       self.GetSelected())
             balt.copyListToClipboard(sel)
-        super(ScreensList, self)._handle_key_up(wrapped_evt, g_list)
+        super(ScreensList, self)._handle_key_up(wrapped_evt)
 
 #------------------------------------------------------------------------------
 class ScreensDetails(_DetailsMixin, NotebookPanel):

Mopy/bash/gui/base_components.py
@@ -363,10 +363,7 @@ class WithCharEvents(_AComponent):
     Key events.
       - on_key_pressed(wrapped_evt: _WrapKeyEvt): key pressed
       - on_key_up(wrapped_evt: _WrapKeyEvt, self: WithCharEvents): key
-        released
-    """
-    bind_char_evt = bind_key_up_evt = True
-
+        released"""
     class _WrapKeyEvt(object):
         def __init__(self, mouse_evt):
             self.__key_evt = mouse_evt # type: _wx.KeyEvent
@@ -385,16 +382,13 @@ class WithCharEvents(_AComponent):
 
         @property
         def is_space(self):
-            return self.__key_evt.GetKeyCode() == _wx.WXK_SPACE
+            return self.key_code == _wx.WXK_SPACE
 
     def __init__(self, *args, **kwargs):
         super(WithCharEvents, self).__init__(*args, **kwargs)
-        if self.__class__.bind_char_evt:
-            self.on_key_pressed = self._evt_handler(_wx.EVT_CHAR,
-                lambda event: [self._WrapKeyEvt(event)])
-        if self.__class__.bind_key_up_evt:
-            self.on_key_up = self._evt_handler(_wx.EVT_KEY_UP, lambda event: [
-                self._WrapKeyEvt(event), self])
+        wrap_processor = lambda event: [self._WrapKeyEvt(event)]
+        self.on_key_pressed = self._evt_handler(_wx.EVT_CHAR, wrap_processor)
+        self.on_key_up = self._evt_handler(_wx.EVT_KEY_UP, wrap_processor)
 
 class WithFirstShow(_AComponent):
     """An _AComponent that does some initialization on first shown.

commit 3585fbbf1c9785fe4880e05ed2fc430fe01693f9

Regression: Drop Ctrl+A behavior of ListBoxes
Broke in 0c676af4e06a0cf40d145357099fe1ea133535f0, turns out g_list was
not entirely unused. Rather than reintroduce it for this behavior that
you can already get via right click > Select All, I just drop the Ctrl+A
behavior here. Again, you can already do this more intuitively via right
click > Select All, whereas Ctrl+A needed you to be correctly focused.

Mopy/bash/bosh/bain.py:
Fixup for non-ASCII PNGs in wizards

Also threw in a noop change to stop PyCharm complaining about
notify_external.

Mopy/bash/balt.py
@@ -2124,7 +2124,6 @@ class ListBoxes(DialogWindow):
             if liststyle == u'check':
                 checksCtrl = CheckListBox(self, choices=strings, isSingle=True,
                                           isHScroll=True)
-                checksCtrl.on_key_up.subscribe(self._on_key_up)
                 checksCtrl.on_context.subscribe(self._on_context)
                 checksCtrl.set_all_checkmarks(checked=True)
             elif liststyle == u'list':
@@ -2149,15 +2148,6 @@ class ListBoxes(DialogWindow):
         if self._width != self.component_size[0]:
             self._width = self.component_size[0]
             self.text.wrap(self.component_size[0] - 64)
-
-    @staticmethod
-    def _on_key_up(wrapped_evt, lb_instance):
-        """Char events"""
-        ##Ctrl-A - check all
-        if wrapped_evt.is_cmd_down and wrapped_evt.key_code == ord(u'A'):
-            lb_instance.set_all_checkmarks(
-                checked=not wrapped_evt.is_shift_down)
-            return EventResult.FINISH ##: needed?
 
     def _on_context(self, lb_instance):
         """Context Menu"""

Mopy/bash/bosh/bain.py
@@ -1331,7 +1331,9 @@ class InstallerArchive(Installer):
             if progress:
                 progress(pngs_processed,
                          u'\n%s\n%s' % (_(u'Fixing PNGs...'), target_png))
-            archives.fix_png(tmp_dir.join(target_png).s)
+            # Will pass through Popen, which tries to encode as ASCII
+            with tmp_dir.join(target_png).unicodeSafe() as ascii_png:
+                archives.fix_png(ascii_png.s)
 
     def _extract_wizard_files(self, wizard_file_name, wizard_prog_title):
         with balt.Progress(wizard_prog_title, u'\n' + u' ' * 60,
@@ -2164,7 +2166,8 @@ class InstallersData(DataStore):
         InstallersData._miscTrackedFiles[abspath] = AFile(abspath)
 
     @staticmethod
-    def notify_external(changed=frozenset(), deleted=frozenset(), renamed={}):
+    def notify_external(changed=frozenset(), deleted=frozenset(),
+            renamed=None):
         """Notifies BAIN of changes in the Data folder done by something other
         than BAIN.
 
@@ -2176,6 +2179,7 @@ class InstallersData(DataStore):
             paths to new ones. Currently only updates tracked changed/deleted
             paths.
         :type renamed: dict[Path, Path]"""
+        if renamed is None: renamed = {}
         ext_updated = InstallersData._externally_updated
         ext_deleted = InstallersData._externally_deleted
         ext_updated.update(changed)