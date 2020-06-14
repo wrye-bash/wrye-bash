wrye-bash/wrye-bash

commit b8a402a0aa5a7aa37bd1651bee412b69a0a690e8

Nirnroots are called Vynroots in Vyn
Mopy/bash/patcher/patchers/multitweak_actors.py:
Instantiate CBash actor tweaks per game

Mopy/bash/tests/__init__.py:
Temp change from 480-pt2

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -1068,7 +1068,7 @@ class AssortedTweak_ScriptEffectSilencer(AAssortedTweak_ScriptEffectSilencer,
         """Edits patch file as desired. Will write to log."""
         nullRef = (GPath(bush.game.master_file), 0)
         silentattrs = {
-            'model' : None,
+            'model': None,
             'projectileSpeed' : 9999,
             'light' : nullRef,
             'effectShader' : nullRef,
@@ -1645,13 +1645,17 @@ class CBash_AssortedTweak_DefaultIcons(AAssortedTweak_DefaultIcons,
             record._RecordID = override._RecordID
 
 #------------------------------------------------------------------------------
+# Will be refactored in inf-312-tweak-pooling
+_nirnroot_words = {u'nirnroot', u'vynroot', u'vynwurz'}
+def _is_nirnroot(record):
+    return any(x in record.eid.lower() for x in _nirnroot_words)
+
 class AAssortedTweak_SetSoundAttenuationLevels(AMultiTweakItem):
     """Sets Sound Attenuation Levels for all records except Nirnroots."""
     tweak_read_classes = 'SOUN',
     tweak_name = _(u'Set Sound Attenuation Levels')
-    tweak_tip = _(
-        u'The sound attenuation levels will be set to tweak%*current level, '
-        u'thereby increasing (or decreasing) the sound volume.')
+    tweak_tip = _(u'Sets sound attenuation levels to tweak%*current level. '
+                  u'Does not affect {}.').format(bush.game.nirnroots)
 
     def __init__(self):
         super(AAssortedTweak_SetSoundAttenuationLevels, self).__init__(
@@ -1668,7 +1672,7 @@ class AssortedTweak_SetSoundAttenuationLevels(
         id_records = patchBlock.id_records
         for record in modFile.SOUN.getActiveRecords():
             if mapper(record.fid) in id_records: continue
-            if record.staticAtten:
+            if record.staticAtten and not _is_nirnroot(record):
                 record = record.getTypeCopy(mapper)
                 patchBlock.setRecord(record)
 
@@ -1677,7 +1681,7 @@ class AssortedTweak_SetSoundAttenuationLevels(
         count = Counter()
         keep = patchFile.getKeeper()
         for record in patchFile.SOUN.records:
-            if record.staticAtten:
+            if record.staticAtten and not _is_nirnroot(record):
                 record.staticAtten = record.staticAtten * \
                                      self.choiceValues[self.chosen][0] / 100
                 keep(record.fid)
@@ -1693,7 +1697,7 @@ class CBash_AssortedTweak_SetSoundAttenuationLevels(
         if choice == 1:  # Prevent any pointless changes if a custom value
             # of 100 is used.
             return
-        if record.staticAtten:
+        if record.staticAtten and not _is_nirnroot(record):
             override = record.CopyAsOverride(self.patchFile)
             if override:
                 override.staticAtten *= choice
@@ -1705,14 +1709,14 @@ class CBash_AssortedTweak_SetSoundAttenuationLevels(
 class AAssortedTweak_SetSoundAttenuationLevels_NirnrootOnly(AMultiTweakItem):
     """Sets Sound Attenuation Levels for Nirnroots."""
     tweak_read_classes = 'SOUN',
-    tweak_name = _(u'Set Sound Attenuation Levels: Nirnroots Only')
-    tweak_tip = _(u'The sound attenuation levels will be set to '
-                  u'tweak%*current level, thereby increasing (or decreasing) '
-                  u'the sound volume. This one only affects Nirnroots.')
+    tweak_name = _(u'Set Sound Attenuation Levels: %s '
+                   u'Only') % bush.game.nirnroots
+    tweak_tip = _(u'Sets sound attenuation levels to tweak%*current level. '
+                  u'Only affects {}.').format(bush.game.nirnroots)
 
     def __init__(self):
         super(AAssortedTweak_SetSoundAttenuationLevels_NirnrootOnly,
-              self).__init__(u'Nirnroot Attenuation%:', (u'0%', 0), (u'5%', 5),
+              self).__init__(u'Attenuation%:', (u'0%', 0), (u'5%', 5),
             (u'10%', 10), (u'20%', 20), (u'50%', 50), (u'80%', 80),
             (_(u'Custom'), 0))
         self.logMsg = u'* ' + _(u'Sounds Modified') + u': %d'
@@ -1726,7 +1730,7 @@ class AssortedTweak_SetSoundAttenuationLevels_NirnrootOnly(
         id_records = patchBlock.id_records
         for record in modFile.SOUN.getActiveRecords():
             if mapper(record.fid) in id_records: continue
-            if record.staticAtten and u'nirnroot' in record.eid.lower():
+            if _is_nirnroot(record) and record.staticAtten:
                 record = record.getTypeCopy(mapper)
                 patchBlock.setRecord(record)
 
@@ -1735,7 +1739,7 @@ class AssortedTweak_SetSoundAttenuationLevels_NirnrootOnly(
         count = Counter()
         keep = patchFile.getKeeper()
         for record in patchFile.SOUN.records:
-            if record.staticAtten and u'nirnroot' in record.eid.lower():
+            if _is_nirnroot(record) and record.staticAtten:
                 record.staticAtten = record.staticAtten * \
                                      self.choiceValues[self.chosen][0] / 100
                 keep(record.fid)
@@ -1752,7 +1756,7 @@ class CBash_AssortedTweak_SetSoundAttenuationLevels_NirnrootOnly(
         if choice == 1:  # Prevent any pointless changes if a custom value
             # of 100 is used.
             return
-        if record.staticAtten and u'nirnroot' in record.eid.lower() :
+        if _is_nirnroot(record) and record.staticAtten:
             override = record.CopyAsOverride(self.patchFile)
             if override:
                 override.staticAtten *= choice

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -4907,7 +4907,7 @@ C.Location, -C.LockList
         <tr><td>Reweigh: Staffs/Staves<td>Reweighs staves to a maximum weight.
         <tr><td>Right Hand Rings<td>Makes rings behave more consistently by making all rings prefer to be on the right hand. With this tweak activated, rings will only equip to the left hand if the right hand has a ring, but the left hand does not. You can use this to make one ring permanently equipped - just equip it to the left hand and it will stay on until you specifically unequip it.
         <tr><td>Set Sound Attenuation Levels<td>The sound attenuation levels will be set to tweak%*current level, thereby increasing (or decreasing) the sound volume (lower attenuation = higher volume).
-        <tr><td>Set Sound Attenuation Levels: Nirnroots Only<td>The sound attenuation levels will be set to tweak%*current level, thereby increasing (or decreasing) the sound volume (lower attenuation = higher volume). This one only affects Nirnroots.
+        <tr><td>Set Sound Attenuation Levels: Nirnroots/Vynroots Only<td>The sound attenuation levels will be set to tweak%*current level, thereby increasing (or decreasing) the sound volume (lower attenuation = higher volume). This one only affects Nirnroots/Vynroots.
         <tr><td>Skyrim-style Weapons<td>Sets all one handed weapons as blades, two handed weapons as blunt.
         <tr><td>Uniform Groundcover<td>Eliminates the random variation in groundcover (grass, vines, shrubs, etc). Tall groundcover will still be taller than short groundcover, but there will be less variation in size. May help on lower end machines.
 </table>

Mopy/bash/game/__init__.py
@@ -507,6 +507,11 @@ class GameInfo(object):
     actor_tweaks = set()
 
     #--------------------------------------------------------------------------
+    # Assorted Tweaker
+    #--------------------------------------------------------------------------
+    nirnroots = _(u'Nirnroots')
+
+    #--------------------------------------------------------------------------
     # Names Tweaker
     #--------------------------------------------------------------------------
     body_tags = u''
@@ -569,7 +574,7 @@ class GameInfo(object):
         'graphicsLongsTypes', 'graphicsModelAttrs', 'graphicsTypes',
         'hostile_effects', 'inventoryTypes', 'keywords_types', 'listTypes',
         'mgef_basevalue', 'mgef_name', 'mgef_school', 'namesTypes',
-        'object_bounds_types', 'pricesTypes', 'record_type_name',
+        'nirnroots', 'object_bounds_types', 'pricesTypes', 'record_type_name',
         'save_rec_types', 'scripts_types', 'soundsLongsTypes', 'soundsTypes',
         'spell_stats_attrs', 'statsHeaders', 'statsTypes', 'text_long_types',
         'text_types',

Mopy/bash/game/enderal/constants.py
@@ -949,3 +949,8 @@ gmstEids = [
     'sNoChargeItems_', 'sRemoveItemFromInventory', 'sVerletCape',
     'uiMuteMusicPauseTime',
 ]
+
+#------------------------------------------------------------------------------
+# Assorted Tweaker
+#------------------------------------------------------------------------------
+nirnroots = _(u'Vynroots')

Mopy/bash/patcher/patchers/multitweak_actors.py
@@ -838,15 +838,12 @@ class TweakActors(MultiTweaker):
     """Sets Creature stuff or NPC Skeletons, Animations or other settings to
     better work with mods or avoid bugs."""
     _tweak_classes = sorted(
-        [globals()[tweak_name] for tweak_name in bush.game.actor_tweaks],
+        (globals()[t] for t in bush.game.actor_tweaks),
         key=lambda a: a.tweak_name.lower())
 
 class CBash_TweakActors(CBash_MultiTweaker):
     """Sets Creature stuff or NPC Skeletons, Animations or other settings to
     better work with mods or avoid bugs."""
-    _tweak_classes = [CBash_VORB_NPCSkeletonPatcher,
-        CBash_MAONPCSkeletonPatcher, CBash_VanillaNPCSkeletonPatcher,
-        CBash_RedguardNPCPatcher, CBash_NoBloodCreaturesPatcher,
-        CBash_AsIntendedImpsPatcher, CBash_AsIntendedBoarsPatcher,
-        CBash_QuietFeetPatcher, CBash_IrresponsibleCreaturesPatcher,
-        CBash_RWALKNPCAnimationPatcher, CBash_SWALKNPCAnimationPatcher]
+    _tweak_classes = sorted(
+        (globals()[u'CBash_' + t] for t in bush.game.actor_tweaks),
+        key=lambda a: a.tweak_name.lower()) if bush.game.Esp.canCBash else []

Mopy/bash/tests/__init__.py
@@ -29,7 +29,6 @@ import os
 import toml
 import traceback
 import wx
-from .. import bush, localize
 
 class FailedTest(Exception):
     """Misc exception for when a test should fail for meta reasons."""
@@ -118,7 +117,11 @@ def set_game(game_fsName):
 def _emulate_startup():
     """Emulates a normal Wrye Bash startup, but without launching basher
     etc."""
+    # bush needs _() to be available, so need to do it like this
+    global bush
+    from .. import localize
     localize.setup_locale(u'English', wx)
+    from .. import bush
     # noinspection PyProtectedMember
     bush._supportedGames()
     set_game(u'Oblivion') # just need to pick one to start

commit 55a1caa7c82d2d2a3f7c01586ec377de7e23a42d

Regression: Fix nirnroot tweak having duplicate key
Introduced in b8a402a0aa5a7aa37bd1651bee412b69a0a690e8.
inf-312-tweak-pooling can't come fast enough :P

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -1716,7 +1716,7 @@ class AAssortedTweak_SetSoundAttenuationLevels_NirnrootOnly(AMultiTweakItem):
 
     def __init__(self):
         super(AAssortedTweak_SetSoundAttenuationLevels_NirnrootOnly,
-              self).__init__(u'Attenuation%:', (u'0%', 0), (u'5%', 5),
+              self).__init__(u'Nirnroot Attenuation%:', (u'0%', 0), (u'5%', 5),
             (u'10%', 10), (u'20%', 20), (u'50%', 50), (u'80%', 80),
             (_(u'Custom'), 0))
         self.logMsg = u'* ' + _(u'Sounds Modified') + u': %d'