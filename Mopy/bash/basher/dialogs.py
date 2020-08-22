wrye-bash/wrye-bash

commit 664f1722a53c91794f192e343936dfd34b8e86a8

Backwards-compatible changes from lojack-py3
Infernio: consists of the following:

[py3] types.ClassType no longer exists

Traceback (most recent call last):
  File "..\Mopy\bash\bash.py", line 282, in _main
    from . import basher, balt
  File "..\Mopy\bash\basher\__init__.py", line 64, in <module>
    from types import ClassType
ImportError: cannot import name 'ClassType' from 'types' (C:\Code\Python\3.8\lib\types.py)

Fix:
https://stackoverflow.com/questions/529240/what-happened-to-types-classtype-in-python-3

[py3] Issue with slots and multiple inheritance

Traceback (most recent call last):
  File "..\Mopy\bash\bash.py", line 185, in main
    _main(opts)
  File "..\Mopy\bash\bash.py", line 282, in _main
    from . import basher, balt
  File "..\Mopy\bash\basher\__init__.py", line 3427, in <module>
    class BashNotebook(wx.Notebook, balt.TabDragMixin):
TypeError: multiple bases have instance lay-out conflict

TODO: See if there's a better fix other than just dropping slots?

Infernio: edited to only comment out the slots, no need to drop the
new-style class. Also added a PY3 note.

[py3] `types.IntType` and `types.LongType` no longer exist

Infernio: added a PY3: note for gettext install method, no longer takes
a unicode kwarg in py3

[wx] Only attempt to bind/unbind if the window still exists

Traceback (most recent call last):
  File "..\Mopy\bash\balt.py", line 1697, in _conversation_wrapper
    if not _depth: Link.Frame.BindRefresh(bind=True)
  File "..\Mopy\bash\basher\__init__.py", line 3738, in BindRefresh
    self.Bind(__event, self.RefreshData)
  File "..\.venv\lib\site-packages\wx\core.py", line 1406, in _EvtHandler_Bind
    event.Bind(self, id, id2, handler)
  File "..\.venv\lib\site-packages\wx\core.py", line 1477, in Bind
    target.Connect(id1, id2, et, function)
RuntimeError: wrapped C/C++ object of type BashFrame has been deleted

[wx] `wx.App.Exit` removed

Traceback (most recent call last):

Mopy/bash/basher/dialogs.py
@@ -23,7 +23,6 @@
 # =============================================================================
 
 import string
-from types import IntType, LongType
 import wx
 from . import bEnableWizard, tabInfo, BashFrame
 from .constants import colorInfo, settingDefaults, installercons
@@ -273,7 +272,7 @@ class ImportFaceDialog(balt.Dialog):
     def __init__(self, parent, title, fileInfo, faces):
         #--Data
         self.fileInfo = fileInfo
-        if faces and isinstance(faces.keys()[0],(IntType,LongType)):
+        if faces and isinstance(faces.keys()[0], int):
             self.data = dict((u'%08X %s' % (key,face.pcName),face) for key,face in faces.items())
         else:
             self.data = faces

Mopy/bash/balt.py
@@ -1349,7 +1349,8 @@ class TabDragMixin(object):
            event.oldIdex = old tab position (of tab that was moved
            event.newIdex = new tab position (of tab that was moved
     """
-    __slots__=('__dragX','__dragging','__justSwapped')
+    # PY3: These slots cause a crash on wx4
+    #__slots__ = ('__dragX','__dragging','__justSwapped')
 
     def __init__(self):
         self.__dragX = 0

Mopy/bash/bash.py
@@ -453,12 +453,12 @@ def _show_wx_error(msg):
         app = _wx.GetApp() # wx.App is a singleton, get it if it exists.
         if app:
             MessageBox(msg)
-            app.Exit()
+            _wx.Exit()
         else:
             app = _wx.App(False) # wx.App is not instantiated, do so now .
             if app:
                 MessageBox(msg)
-                app.Exit()
+                _wx.Exit()
             else:
                 # Instantiating wx.App failed, fallback to tkinter.
                 but_kwargs = {'text': _(u"QUIT"),

Mopy/bash/basher/__init__.py
@@ -61,7 +61,6 @@ import time
 from collections import OrderedDict
 from functools import partial
 from operator import itemgetter
-from types import ClassType
 #--wxPython
 import wx
 
@@ -3786,7 +3785,8 @@ class BashFrame(BaltFrame):
             balt.showWarning(self, message, _(u'Too many mod files.'))
 
     def BindRefresh(self, bind=True, __event=wx.EVT_ACTIVATE):
-        self.Bind(__event, self.RefreshData) if bind else self.Unbind(__event)
+        if self:
+            self.Bind(__event, self.RefreshData) if bind else self.Unbind(__event)
 
     def Restart(self, *args):
         """Restart Bash - edit bass.sys_argv with specified args then let
@@ -4166,7 +4166,7 @@ class BashApp(wx.App):
             #--Window sizes by class name rather than by class
             if bash_version < 43:
                 for key,value in balt.sizes.iteritems():
-                    if isinstance(key,ClassType):
+                    if isinstance(key, type):
                         balt.sizes[key.__name__] = value
                         del balt.sizes[key]
         #--Current Version

Mopy/bash/localize.py
@@ -130,6 +130,7 @@ def setup_locale(cli_lang, _wx):
             trans = gettext.NullTranslations()
     # Everything has gone smoothly, install the translation and remember what
     # we ended up with as the final locale
+    # PY3: drop the unicode=True, gone in py3 (this is always unicode now)
     trans.install(unicode=True)
     bass.active_locale = target_name
     del _temp_app

commit 7733a5c01d79cf74e1e8569d3724b9628af2efba

fomod: Fix radio buttons not freezing correctly
Was causing a loop in the AOS LE 'Smart' FOMOD installer.

Also featured:
 - Changed one of the warnings to assume less about what's going on -
   e.g. AOS for Skyrim LE uses NotUsable options in its 'Smart' FOMOD
   installer.
 - Fixed label flickering here and in the BP dialog. Caused by constant
   SetLabel calls, even when the text didn't change at all.

This fix for an extremely random issue:
-            for block_checkable, chk_state in frozen_state.iteritems():
-                block_checkable.is_checked = chk_state
+            for chk, chk_state in frozen_state.iteritems():
+                chk.is_checked = chk_state

took *way* too long to figure out. block_checkable was a method param
and got reassigned by that :/

Mopy/bash/bosh/bain.py:
Support .001 archives as well

Mopy/bash/basher/dialogs.py:
Regression: Fix Import Face dialog

Broke in 664f1722a53c91794f192e343936dfd34b8e86a8. Same as with the
other two places where we just used ints in the PBash code. Checked to
make sure there won't be any more whack-a-mole going on now.

Mopy/bash/basher/dialogs.py
@@ -37,7 +37,7 @@ class ImportFaceDialog(DialogWindow):
     def __init__(self, parent, title, fileInfo, faces):
         #--Data
         self.fileInfo = fileInfo
-        if faces and isinstance(faces.keys()[0], int):
+        if faces and isinstance(faces.keys()[0], (int, long)): # PY3: just int
             self.data = dict((u'%08X %s' % (key,face.pcName),face) for key,face in faces.items())
         else:
             self.data = faces

Mopy/bash/basher/gui_fomod.py
@@ -277,10 +277,10 @@ class PageSelect(PageInstaller):
                     any_selected = True
             if block_all_in_group:
                 checkables_to_block |= set(self.group_option_map[grp])
-                # Store a copy of the frozen state for each button
-                frozen_state = {c: c.is_checked for c in checkables_to_block}
-                for block_chk in checkables_to_block:
-                    self._frozen_states[block_chk] = frozen_state
+            # We'll need this when blocking user interaction with an unusable
+            # option
+            initial_state = {c: c.is_checked for c in
+                             self.group_option_map[grp]}
             if gtype == u'SelectAtMostOne':
                 none_button = RadioButton(panel_groups, label=_(u'None'))
                 if not any_selected:
@@ -288,9 +288,20 @@ class PageSelect(PageInstaller):
                 elif block_all_in_group:
                     checkables_to_block.add(none_button)
                 options_layout.add(none_button)
+                initial_state[none_button] = none_button.is_checked
             # At this point, all checkables that may have to be blocked have
             # been added, so perform the blocking and create the final layout
             for block_chk in checkables_to_block:
+                # For simplicity, we remember the initial state of all blocked
+                # checkables in this group even if we don't end up needing it
+                self._frozen_states[block_chk] = initial_state
+                ##: This will reset user choices if they've checked any usable
+                # options before checking an unusable one. We should find a way
+                # to catch 'valid' user interactions and update the frozen
+                # states at that point. Simply subscribing to all non-blocked
+                # on_clicked events does not work because they fire too early
+                # (e.g. radio buttons won't have run their logic for unchecking
+                # the other button yet).
                 block_chk.block_user(self._handle_block_user)
             groups_layout.add(HBoxedLayout(
                 panel_groups, title=grp.group_name, item_expand=True,
@@ -323,20 +334,16 @@ class PageSelect(PageInstaller):
         ##: Could maybe be de-wx'd further and moved to gui? RadioButtonGroup?
         frozen_state = self._frozen_states.get(block_checkable)
         if frozen_state:
-            for block_checkable, chk_state in frozen_state.iteritems():
-                block_checkable.is_checked = chk_state
-        if isinstance(block_checkable, CheckBox):
-            # Adjust the warning based on whether we can't check or can't
-            # uncheck this option
-            block_option = self.checkable_to_option[block_checkable]
-            if block_option.option_type == u'NotUsable':
-                balt.showWarning(self, _(
-                    u'This option cannot be enabled. It is probably '
-                    u'informational and only here to show a description on '
-                    u'the right.'))
-            else:
-                balt.showWarning(self, _(u'This option is required and cannot '
-                                         u'be disabled.'))
+            for chk, chk_state in frozen_state.iteritems():
+                chk.is_checked = chk_state
+        block_option = self.checkable_to_option[block_checkable]
+        # Adjust the warning based on whether the problem is due to this option
+        # or another one in the same group
+        if block_option.option_type == u'NotUsable':
+            balt.showWarning(self, _(u'This option cannot be enabled.'))
+        elif isinstance(block_checkable, CheckBox):
+            balt.showWarning(self, _(u'This option is required and cannot be '
+                                     u'disabled.'))
         else: # RadioButton
             balt.showWarning(self, _(u'One of the options in this group is '
                                      u'required and cannot be unselected by '

Mopy/bash/bosh/bain.py
@@ -1481,9 +1481,8 @@ class InstallerArchive(Installer):
             recurse=True)
         upt_numb, del_numb = self._do_sync_data(unpack_dir, delta_files)
         archive_name = GPath(self.archive)
-        new_archive = archive_name.root + (
-            archive_name.cext if archive_name.cext != u'.rar'
-            else archives.defaultExt)
+        new_archive = archive_name.root + archives.writeExts.get(
+            archive_name.cext, archives.defaultExt)
         self.packToArchive(unpack_dir, new_archive, isSolid=True,
                            blockSize=None)
         bass.rmTempDir()

Mopy/bash/gui/text_components.py
@@ -223,7 +223,9 @@ class _ALabel(_AComponent):
         """Changes the text of this label to the specified string.
 
         :param new_text: The new text to use."""
-        self._native_widget.SetLabel(new_text)
+        # Check first to avoid GUI flicker when setting to identical text
+        if self.label_text != new_text:
+            self._native_widget.SetLabel(new_text)
 
 class Label(_ALabel):
     """A static text element. Doesn't have a border and the text can't be