wrye-bash/wrye-bash

commit 0864d5795247c7f3f9d478da837710d939c68f6e

Create BOSS Report -> Create LOOT Entry
The BOSS masterlists aren't even maintained anymore, plus we don't even
get use out of BOSS contributions even if they *were* maintained.

Dropped the dirty/clean checking code from all but Oblivion where we can
use CBash to get accurate results.

Mopy/bash/basher/mod_links.py
@@ -51,7 +51,7 @@ from ..patcher import configIsCBash, exportConfig, patch_files
 
 __all__ = ['Mod_FullLoad', 'Mod_CreateDummyMasters', 'Mod_OrderByName',
            'Mod_Groups', 'Mod_Ratings', 'Mod_Details', 'Mod_ShowReadme',
-           'Mod_ListBashTags', 'Mod_CreateBOSSReport', 'Mod_CopyModInfo',
+           'Mod_ListBashTags', 'Mod_CreateLOOTReport', 'Mod_CopyModInfo',
            'Mod_AllowGhosting', 'Mod_Ghost', 'Mod_MarkMergeable',
            'Mod_Patch_Update', 'Mod_ListPatchConfig', 'Mod_ExportPatchConfig',
            'CBash_Mod_CellBlockInfo_Export', 'Mod_EditorIds_Export',
@@ -618,7 +618,7 @@ class Mod_ListBashTags(ItemLink):
         balt.copyToClipboard(tags_text)
         self._showLog(tags_text, title=_(u"Bash Tags"), fixedFont=False)
 
-def _getUrl(fileName, installer, log_txt):
+def _getUrl(fileName, installer):
     """"Try to get the url of the file (order of priority will be: TESNexus,
     TESAlliance)."""
     url = None
@@ -630,61 +630,92 @@ def _getUrl(fileName, installer, log_txt):
         if ma and ma.group(2):
             url = u'http://tesalliance.org/forums/index.php?app' \
                   u'=downloads&showfile=' + ma.group(2)
-    if url: log_txt += u'[url=' + url + u']' + fileName.s + u'[/url]'
-    else: log_txt += fileName.s
-    return log_txt
+    if url: return u"    url: [ '%s' ]\n" % url
+    return u''
 
-class Mod_CreateBOSSReport(EnabledLink):
-    """Copies appropriate information for making a report in the BOSS thread."""
-    _text = _(u"Create BOSS Report...")
-    _help = _(u'Gathers and displays information necessary for making a BOSS '
-              u'report.')
+class Mod_CreateLOOTReport(EnabledLink):
+    """Creates a basic LOOT masterlist entry with ."""
+    _text = _(u'Create LOOT Entry...')
 
     def _enable(self):
         return len(self.selected) != 1 or (
             not bosh.reOblivion.match(self.selected[0].s))
 
+    @property
+    def menu_help(self):
+        full_help = _(u'Creates LOOT masterlist entries based on the tags you '
+                      u'have applied to the selected plugin(s).')
+        if bass.settings[u'bash.CBashEnabled']:
+            full_help += u' ' + _(u'Also uses CBash to check for ITMs and '
+                                  u'deleted references.')
+        return full_help
+
     def Execute(self):
         log_txt = u''
-        if len(self.selected) > 5:
-            spoiler = True
-            log_txt += u'[spoiler]\n'
-        else:
-            spoiler = False
-        # Scan for ITM and UDR's
-        modInfos = [x for x in self.iselected_infos()]
-        try:
-            with balt.Progress(_(u"Dirty Edits"),u'\n'+u' '*60,abort=True) as progress:
-                udr_itm_fog = bosh.mods_metadata.ModCleaner.scan_Many(modInfos, progress=progress)
-        except CancelError:
-            return
-        # Create the report
+        # Scan for ITM and UDR's only in Oblivion with CBash, since all other
+        # results are not guaranteed to be accurate
+        udr_itm_fog = []
+        can_check_dirty = bass.settings[u'bash.CBashEnabled']
+        if can_check_dirty:
+            dirty_candidates = [x for x in self.iselected_infos()] ##: huh?
+            try:
+                with balt.Progress(_(u'Dirty Edits'), u'\n' + u' ' * 60,
+                        abort=True) as progress:
+                    udr_itm_fog = bosh.mods_metadata.ModCleaner.scan_Many(
+                        dirty_candidates, progress=progress)
+            except CancelError:
+                return
         for i, (fileName, fileInfo) in enumerate(self.iselected_pairs()):
-            if fileName == u'Oblivion.esm': continue
-            #-- Name of file, plus a link if we can figure it out
-            installer = bosh.modInfos.table.getItem(fileName,'installer',u'')
-            if not installer: log_txt += fileName.s
-            else: log_txt = _getUrl(fileName, installer, log_txt)
-            #-- Version, if it exists
-            version = bosh.modInfos.getVersion(fileName)
-            if version:
-                log_txt += u'\n'+_(u'Version')+u': %s' % version
-            #-- CRC
-            log_txt += u'\n'+_(u'CRC')+u': ' + fileInfo.crc_string()
-            #-- Dirty edits
-            if udr_itm_fog:
-                udrs,itms,fogs = udr_itm_fog[i]
-                if udrs or itms:
-                    if bass.settings['bash.CBashEnabled']:
-                        log_txt += (u'\nUDR: %i, ITM: %i '+_(u'(via Wrye Bash)')) % (len(udrs),len(itms))
-                    else:
-                        log_txt += (u'\nUDR: %i, ITM not scanned '+_(u'(via Wrye Bash)')) % len(udrs)
-            log_txt += u'\n\n'
-        if spoiler: log_txt += u'[/spoiler]'
-
-        # Show results + copy to clipboard
-        balt.copyToClipboard(log_txt)
-        self._showLog(log_txt, title=_(u'BOSS Report'), fixedFont=False)
+            if fileName in bush.game.masterFiles: continue
+            # Check if we found ITMs or deleted references
+            udrs, itms, fogs = udr_itm_fog[i] if can_check_dirty else (0, 0, 0)
+            # Gather all tags applied after the description (including existing
+            # LOOT tags - we'll want to replace the existing LOOT entry with
+            # this one after all)
+            desc_tags = fileInfo.getBashTagsDesc()
+            curr_tags = fileInfo.getBashTags()
+            added = curr_tags - desc_tags
+            removed = desc_tags - curr_tags
+            if not can_check_dirty and not added and not removed:
+                continue # Skip if it's going to be a useless entry
+            # Name of file, plus a link if we can figure it out
+            log_txt += u"  - name: '%s'\n" % fileName
+            installer = bosh.modInfos.table.getItem(
+                fileName, u'installer', u'')
+            if installer: log_txt += _getUrl(fileName, installer)
+            # Tags applied after the description
+            fmt_tags = sorted(added | {u'-%s' % t for t in removed})
+            if fmt_tags:
+                if len(fmt_tags) == 1:
+                    log_txt += u'    tag: [ %s ]\n' % fmt_tags[0]
+                else:
+                    log_txt += u'    tag:\n'
+                    for fmt_tag in fmt_tags:
+                        log_txt += u'      - %s\n' % fmt_tag
+            # Clean/dirty info (CBash only)
+            if can_check_dirty:
+                wb_ver = bass.AppVersion
+                if not udrs and not itms:
+                    log_txt += u'    clean:\n'
+                    log_txt += u'      - crc: 0x%s\n' % fileInfo.crc_string()
+                    log_txt += u"        util: 'Wrye Bash %s'\n" % wb_ver
+                else:
+                    log_txt += u'    dirty:\n'
+                    log_txt += u'      - <<: *quickClean\n'
+                    log_txt += u'        crc: 0x%s\n' % fileInfo.crc_string()
+                    log_txt += u"        util: 'Wrye Bash %s'\n" % wb_ver
+                    if itms:
+                        log_txt += u'        itm: %u\n' % len(itms)
+                    if udrs:
+                        log_txt += u'        udr: %u\n' % len(udrs)
+        if not log_txt:
+            self._showWarning(_(u'No tags applied that are not already in the '
+                                u'plugin description, entry would not be '
+                                u'useful.'))
+        else:
+            # Show results + copy to clipboard
+            balt.copyToClipboard(log_txt)
+            self._showLog(log_txt, title=_(u'LOOT Entry'), fixedFont=False)
 
 class Mod_CopyModInfo(ItemLink):
     """Copies the basic info about selected mod(s)."""

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -1070,7 +1070,15 @@ tab. The directories skipped are:
             <tr><td colspan="2">List Masters...<td>Outputs a list of the selected plugin's masters.
             <tr><td colspan="2">Readme...<td>Attempts to open the selected plugin's associated readme in the <a href="#tools-docs">Doc Browser</a>.
             <tr><td colspan="2">List Bash Tags...<td>Outputs a BBCode-formatted list of all the Bash Tags applied to the selected plugin, and where/how the Bash Tags was specified. It can be useful for debugging a broken load order.
-            <tr><td colspan="2" id="modsCreateBOSSReport">Create BOSS Report...<td>Outputs the plugin filename, <abbr title="Cyclic Redundancy Check">CRC</abbr>, and <abbr title="Identical To Master record">ITM</abbr>/<abbr title="Undeleted and Disabled Reference">UDR</abbr> count for easy reporting of the plugin to an official LOOT thread.
+            <tr>
+                <td colspan="2" id="modsCreateLOOTEntry">Create LOOT Entry...</td>
+                <td>Creates LOOT masterlist entries based on the tags you have
+                    applied to the selected plugin(s). Also tries to figure out
+                    the URL that the plugin came from based on the BAIN package
+                    it was installed from. In <strong>Oblivion only</strong>,
+                    CBash will also be used to check for ITMs and deleted
+                    references.</td>
+            </tr>
             <tr><td colspan="2" id="modsCopyModInfo">Copy Mod Info...<td>Outputs a report on the selected plugins(s) with the info from the currently displayed columns.
             <tr><td colspan="2">List Dependencies<td>Displays and copies to the clipboard a list of mods that have the currently selected
                 mod as master. Active mods are prefixed with their load order while merged, imported and inactive mods are prefixed with

Mopy/Docs/Wrye Bash General Readme.html
@@ -709,7 +709,14 @@ Just like the game's masters, this load order is hardcoded, and so it cannot be 
     <tbody>
         <tr><td>List Masters...<td>Outputs a list of the selected plugin's masters.
         <tr><td>List Bash Tags...<td>Outputs a BBCode-formatted list of all the Bash Tags applied to the selected plugin, and where/how the Bash Tags was specified. It can be useful for debugging a broken load order.
-        <tr><td>Create BOSS Report...<td>Outputs the plugin filename, <abbr title="Cyclic Redundancy Check">CRC</abbr>, and <abbr title="Identical To Master record">ITM</abbr>/<abbr title="Undeleted and Disabled Reference">UDR</abbr> count for easy reporting of the plugin to an official LOOT thread.
+        <tr>
+            <td>Create LOOT Entry...</td>
+            <td>Creates LOOT masterlist entries based on the tags you have
+                applied to the selected plugin(s). Also tries to figure out the
+                URL that the plugin came from based on the BAIN package it was
+                installed from. In <strong>Oblivion only</strong>, CBash will
+                also be used to check for ITMs and deleted references.</td>
+        </tr>
         <tr><td>Copy Mod Info...<td>Outputs a report on the selected plugins(s) with the info from the currently displayed columns.
         <tr><td>Don't Ghost<td>Don't ghost this plugin when it is inactive, even if <a href="#modsAutoGhost">Auto-Ghost</a> is enabled.
         <tr><td>Ghost<td>Ghost this plugin when it is inactive even if <a href="#modsAutoGhost">Auto-Ghost</a> is disabled.

Mopy/Docs/Wrye Bash Version History.html
@@ -663,7 +663,7 @@
 	<li>New mod commands:
 <ul>
 	  <li><a href="Wrye%20Bash%20Advanced%20Readme.html#modsLoadAll">Load All</a> will activate all mods (up to 255 of them, at least)
-	  <li><a href="Wrye%20Bash%20Advanced%20Readme.html#modsCreateBOSSReport">Create BOSS Report</a> will automatically gather the information you need to report a mod to the BOSS thread.
+	  <li><a href="Wrye%20Bash%20Advanced%20Readme.html#modsCreateLOOTEntry">Create BOSS Report</a> will automatically gather the information you need to report a mod to the BOSS thread.
 	  <li><a href="Wrye%20Bash%20Advanced%20Readme.html#modsCopyModInfo">Copy Mod Info</a> will automatically gather the displayed info on a mod.
 	  <li>Ghost will ghost just the selected mod(s)
 	  <li><a href="Wrye%20Bash%20Advanced%20Readme.html#modsExportPatchConfig">Export Patch Config...</a> exports the selected patch's configuration to a file.

Mopy/bash/basher/links_init.py
@@ -449,7 +449,7 @@ def InitModLinks():
     ModList.itemMenu.append(File_ListMasters())
     ModList.itemMenu.append(Mod_ShowReadme())
     ModList.itemMenu.append(Mod_ListBashTags())
-    ModList.itemMenu.append(Mod_CreateBOSSReport())
+    ModList.itemMenu.append(Mod_CreateLOOTReport())
     ModList.itemMenu.append(Mod_CopyModInfo())
     ModList.itemMenu.append(Mod_ListDependent())
     ModList.itemMenu.append(Mod_JumpToInstaller())

commit 804fe248c326927bba9dceb865a71202d31044b6

load_order: Bethesda ESMs -> Vanilla
Not only are we starting to support games not made by Bethesda (e.g.
Enderal and Nehrim), but 'Bethesda ESMs' was also stored as a string -
no good for py3. Needs some backwards compat code.

Mopy/bash/basher/mod_links.py:
Fix URL display in Copy Mod Info

Broke in 0864d5795247c7f3f9d478da837710d939c68f6e, when refactoring
this for use in Create LOOT Entry.

Mopy/bash/basher/mod_links.py
@@ -649,8 +649,7 @@ def _getUrl(installer):
         if ma and ma.group(2):
             url = u'http://tesalliance.org/forums/index.php?app' \
                   u'=downloads&showfile=' + ma.group(2)
-    if url: return u"    url: [ '%s' ]\n" % url
-    return u''
+    return url or u''
 
 class Mod_CreateLOOTReport(EnabledLink):
     """Creates a basic LOOT masterlist entry with ."""
@@ -701,7 +700,8 @@ class Mod_CreateLOOTReport(EnabledLink):
             log_txt += u"  - name: '%s'\n" % fileName
             installer = bosh.modInfos.table.getItem(
                 fileName, u'installer', u'')
-            if installer: log_txt += _getUrl(installer)
+            if installer:
+                log_txt += u"    url: [ '%s' ]\n" % _getUrl(installer)
             # Tags applied after the description
             fmt_tags = sorted(added | {u'-%s' % t for t in removed})
             if fmt_tags:
@@ -757,7 +757,7 @@ class Mod_CopyModInfo(ItemLink):
             #-- Name of file, plus a link if we can figure it out
             installer = bosh.modInfos.table.getItem(fileName,'installer',u'')
             if not installer: info_txt += fileName.s
-            else: info_txt = _getUrl(installer)
+            else: info_txt += _(u'URL: %s') % _getUrl(installer)
             labels = self.window.labels
             for col in self.window.cols:
                 if col == 'File': continue

Mopy/bash/basher/mods_links.py
@@ -154,7 +154,7 @@ class Mods_LoadList(ChoiceLink):
         wait for this being initialized in ModInfos.__init__"""
         if self.__class__.loadListsDict is self.__class__.__uninitialized:
             loadListData = load_order.get_active_mods_lists()
-            loadListData['Bethesda ESMs'] = [
+            loadListData[u'Vanilla'] = [
                 GPath(x) for x in bush.game.bethDataFiles if x.endswith(
                     u'.esm') # but avoid activating modding esms for oblivion
                 and (not re.match(bosh.reOblivion.pattern, x, re.I)

Mopy/bash/load_order.py
@@ -334,6 +334,9 @@ def __load_pickled_load_orders():
     _current_list_index = _lords_pickle.data.get('_current_list_index', -1)
     _active_mods_lists = _lords_pickle.data.get('_active_mods_lists',
                                                 active_mods_list)
+    if b'Bethesda ESMs' in _active_mods_lists: ##: backwards compat
+        _active_mods_lists[u'Vanilla'] = _active_mods_lists[b'Bethesda ESMs']
+        del _active_mods_lists[b'Bethesda ESMs']
     locked = bass.settings.get('bosh.modInfos.resetMTimes', False)
 
 def get_active_mods_lists():