wrye-bash/wrye-bash

commit 7733a5c01d79cf74e1e8569d3724b9628af2efba

fomod: Fix radio buttons not freezing correctly
Was causing a loop in the AOS LE 'Smart' FOMOD installer.

Also featured:
 - Changed one of the warnings to assume less about what's going on -
   e.g. AOS for Skyrim LE uses NotUsable options in its 'Smart' FOMOD
   installer.
 - Fixed label flickering here and in the BP dialog. Caused by constant
   SetLabel calls, even when the text didn't change at all.

This fix for an extremely random issue:
-            for block_checkable, chk_state in frozen_state.iteritems():
-                block_checkable.is_checked = chk_state
+            for chk, chk_state in frozen_state.iteritems():
+                chk.is_checked = chk_state

took *way* too long to figure out. block_checkable was a method param
and got reassigned by that :/

Mopy/bash/bosh/bain.py:
Support .001 archives as well

Mopy/bash/basher/dialogs.py:
Regression: Fix Import Face dialog

Broke in 664f1722a53c91794f192e343936dfd34b8e86a8. Same as with the
other two places where we just used ints in the PBash code. Checked to
make sure there won't be any more whack-a-mole going on now.

Mopy/bash/bosh/bain.py
@@ -1481,9 +1481,8 @@ class InstallerArchive(Installer):
             recurse=True)
         upt_numb, del_numb = self._do_sync_data(unpack_dir, delta_files)
         archive_name = GPath(self.archive)
-        new_archive = archive_name.root + (
-            archive_name.cext if archive_name.cext != u'.rar'
-            else archives.defaultExt)
+        new_archive = archive_name.root + archives.writeExts.get(
+            archive_name.cext, archives.defaultExt)
         self.packToArchive(unpack_dir, new_archive, isSolid=True,
                            blockSize=None)
         bass.rmTempDir()

Mopy/bash/basher/dialogs.py
@@ -37,7 +37,7 @@ class ImportFaceDialog(DialogWindow):
     def __init__(self, parent, title, fileInfo, faces):
         #--Data
         self.fileInfo = fileInfo
-        if faces and isinstance(faces.keys()[0], int):
+        if faces and isinstance(faces.keys()[0], (int, long)): # PY3: just int
             self.data = dict((u'%08X %s' % (key,face.pcName),face) for key,face in faces.items())
         else:
             self.data = faces

Mopy/bash/basher/gui_fomod.py
@@ -277,10 +277,10 @@ class PageSelect(PageInstaller):
                     any_selected = True
             if block_all_in_group:
                 checkables_to_block |= set(self.group_option_map[grp])
-                # Store a copy of the frozen state for each button
-                frozen_state = {c: c.is_checked for c in checkables_to_block}
-                for block_chk in checkables_to_block:
-                    self._frozen_states[block_chk] = frozen_state
+            # We'll need this when blocking user interaction with an unusable
+            # option
+            initial_state = {c: c.is_checked for c in
+                             self.group_option_map[grp]}
             if gtype == u'SelectAtMostOne':
                 none_button = RadioButton(panel_groups, label=_(u'None'))
                 if not any_selected:
@@ -288,9 +288,20 @@ class PageSelect(PageInstaller):
                 elif block_all_in_group:
                     checkables_to_block.add(none_button)
                 options_layout.add(none_button)
+                initial_state[none_button] = none_button.is_checked
             # At this point, all checkables that may have to be blocked have
             # been added, so perform the blocking and create the final layout
             for block_chk in checkables_to_block:
+                # For simplicity, we remember the initial state of all blocked
+                # checkables in this group even if we don't end up needing it
+                self._frozen_states[block_chk] = initial_state
+                ##: This will reset user choices if they've checked any usable
+                # options before checking an unusable one. We should find a way
+                # to catch 'valid' user interactions and update the frozen
+                # states at that point. Simply subscribing to all non-blocked
+                # on_clicked events does not work because they fire too early
+                # (e.g. radio buttons won't have run their logic for unchecking
+                # the other button yet).
                 block_chk.block_user(self._handle_block_user)
             groups_layout.add(HBoxedLayout(
                 panel_groups, title=grp.group_name, item_expand=True,
@@ -323,20 +334,16 @@ class PageSelect(PageInstaller):
         ##: Could maybe be de-wx'd further and moved to gui? RadioButtonGroup?
         frozen_state = self._frozen_states.get(block_checkable)
         if frozen_state:
-            for block_checkable, chk_state in frozen_state.iteritems():
-                block_checkable.is_checked = chk_state
-        if isinstance(block_checkable, CheckBox):
-            # Adjust the warning based on whether we can't check or can't
-            # uncheck this option
-            block_option = self.checkable_to_option[block_checkable]
-            if block_option.option_type == u'NotUsable':
-                balt.showWarning(self, _(
-                    u'This option cannot be enabled. It is probably '
-                    u'informational and only here to show a description on '
-                    u'the right.'))
-            else:
-                balt.showWarning(self, _(u'This option is required and cannot '
-                                         u'be disabled.'))
+            for chk, chk_state in frozen_state.iteritems():
+                chk.is_checked = chk_state
+        block_option = self.checkable_to_option[block_checkable]
+        # Adjust the warning based on whether the problem is due to this option
+        # or another one in the same group
+        if block_option.option_type == u'NotUsable':
+            balt.showWarning(self, _(u'This option cannot be enabled.'))
+        elif isinstance(block_checkable, CheckBox):
+            balt.showWarning(self, _(u'This option is required and cannot be '
+                                     u'disabled.'))
         else: # RadioButton
             balt.showWarning(self, _(u'One of the options in this group is '
                                      u'required and cannot be unselected by '

Mopy/bash/gui/text_components.py
@@ -223,7 +223,9 @@ class _ALabel(_AComponent):
         """Changes the text of this label to the specified string.
 
         :param new_text: The new text to use."""
-        self._native_widget.SetLabel(new_text)
+        # Check first to avoid GUI flicker when setting to identical text
+        if self.label_text != new_text:
+            self._native_widget.SetLabel(new_text)
 
 class Label(_ALabel):
     """A static text element. Doesn't have a border and the text can't be

commit 7a4b3ae5fc126392763270635be55d1e354e8352

Regression: Fix Sync From Data
Broke in 7733a5c01d79cf74e1e8569d3724b9628af2efba. Somehow didn't notice
that :/

Mopy/bash/basher/mod_links.py:
Fix ESM/ESL flip with ghosts

Really just demonstrates that this should be in a more central place.

Mopy/bash/basher/mod_links.py
@@ -1601,7 +1601,7 @@ class _Esm_Esl_Flip(EnabledLink):
             bosh.modInfos.rescanMergeable(updated, bolt.Progress())
             # This will have changed the plugin, so let BAIN know
             bosh.modInfos._notify_bain(
-                changed={bass.dirs[u'mods'].join(p) for p in updated})
+                changed={self.window.data_store[p].abs_path for p in updated})
         # will be moved to the top - note that modification times won't
         # change - so mods will revert to their original position once back
         # to esp from esm (Oblivion etc). Refresh saves due to esms move

Mopy/bash/bosh/bain.py
@@ -1481,10 +1481,10 @@ class InstallerArchive(Installer):
             recurse=True)
         upt_numb, del_numb = self._do_sync_data(unpack_dir, delta_files)
         archive_name = GPath(self.archive)
-        new_archive = archive_name.root + archives.writeExts.get(
-            archive_name.cext, archives.defaultExt)
-        self.packToArchive(unpack_dir, new_archive, isSolid=True,
-                           blockSize=None)
+        new_ext = (archive_name.cext if archive_name.cext in archives.writeExts
+                   else archives.defaultExt)
+        self.packToArchive(unpack_dir, archive_name.root + new_ext,
+            isSolid=True, blockSize=None)
         bass.rmTempDir()
         return upt_numb, del_numb
 