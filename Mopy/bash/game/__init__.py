wrye-bash/wrye-bash

commit 0beba3757a367abf5056f7663b9ba55f8e78a539

Reraise packSub errors and other changes:
Will be caught by MelSet.dumpData, where we can provide much more useful
error info - plus now such a dump failure will abort the BP procedure.

Utumno:

Mopy/bash/record_groups.py: Chopping off `format`
Mopy/bash/record_groups.py:
Rename rec_sig will be used for recType - 0 occurrences remain
Mopy/bash/record_groups.py:
Mopy/bash/brec/advanced_elements.py: random prefixing
Mopy/bash/patcher/patchers/importers.py: cell_fid renames

patcher:

getKeeper() -> don't return keepId

Mopy/bash/patcher/patchers/multitweak_settings.py:
Mopy/bash/patcher/patchers/special.py
only uses I found quickly with keep(

(?<!patchFile\.)\bgetKeeper\b just the definition

Mopy/bash/record_groups.py:
keepIds renames - should only be used for the patchFile attr
record_groups.MobBase.loadData -> record_groups.MobBase.load_rec_group

Co-authored-by: MrD <the.ubik@gmail.com>

Mopy/bash/patcher/patch_files.py
@@ -186,10 +186,7 @@ class PatchFile(_PFile, ModFile):
 
     def getKeeper(self):
         """Returns a function to add fids to self.keepIds."""
-        def keep(fid):
-            self.keepIds.add(fid)
-            return fid
-        return keep
+        return self.keepIds.add
 
     def initFactories(self,progress):
         """Gets load factories."""

Mopy/bash/patcher/patchers/special.py
@@ -201,7 +201,8 @@ class _PListsMerger(_AListsMerger, ListPatcher):
             for stored_list in sorted(stored_lists.values(),
                                       key=attrgetter('eid')):
                 if not stored_list.mergeOverLast: continue
-                list_fid = keep(stored_list.fid)
+                list_fid = stored_list.fid
+                keep(list_fid)
                 patch_block.setRecord(stored_lists[list_fid])
                 log(u'* ' + stored_list.eid)
                 for merge_source in stored_list.mergeSources:

Mopy/bash/brec/advanced_elements.py
@@ -336,8 +336,8 @@ class MelArray(MelBase):
         MelBase.__init__(self, next(iter(element.signatures)), array_attr)
         self._element = element
         # Underscore means internal usage only - e.g. distributor state
-        self._element_attrs = [s for s in element.getSlotsUsed()
-                              if not s.startswith('_')]
+        self._element_attrs = [s for s in element.getSlotsUsed() if
+                               not s.startswith(u'_')]
         if prelude and prelude.subType != element.subType:
             raise SyntaxError(u'MelArray preludes must have the same '
                               u'signature as the main element')

Mopy/bash/brec/mod_io.py
@@ -307,8 +307,8 @@ class ModWriter(object):
     def close(self): self.out.close()
 
     #--Additional functions -------------------------------
-    def pack(self,format,*data):
-        self.out.write(struct_pack(format, *data))
+    def pack(self, *args):
+        self.out.write(struct_pack(*args))
 
     def packSub(self, sub_rec_type, data, *values):
         """Write subrecord header and data to output stream.
@@ -331,7 +331,8 @@ class ModWriter(object):
             outWrite(data)
         except Exception:
             bolt.deprint(u'%r: Failed packing: %s, %s, %s' % (
-                self, sub_rec_type, data, values), traceback=True)
+                self, sub_rec_type, data, values))
+            raise
 
     def packSub0(self, sub_rec_type, data):
         """Write subrecord header plus zero terminated string to output

Mopy/bash/brec/record_structs.py
@@ -330,7 +330,7 @@ class MreRecord(object):
         return u'<%(eid)s[%(signature)s:%(fid)s]>' % {
             u'signature': self.recType,
             u'fid': strFid(self.fid),
-            u'eid': (self.eid + u' ' if hasattr(self, 'eid')
+            u'eid': (self.eid + u' ' if hasattr(self, u'eid')
                                      and self.eid is not None else u''),
         }
 
@@ -417,12 +417,12 @@ class MreRecord(object):
         self.subrecords = []
         if not self.data: return
         with self.getReader() as reader:
-            recType = self.recType
+            _rec_sig_ = self.recType
             readAtEnd = reader.atEnd
             readSubHeader = reader.unpackSubHeader
             subAppend = self.subrecords.append
-            while not readAtEnd(reader.size,recType):
-                (type,size) = readSubHeader(recType)
+            while not readAtEnd(reader.size,_rec_sig_):
+                (type,size) = readSubHeader(_rec_sig_)
                 subAppend(MreSubrecord(type,size,reader))
 
     def convertFids(self,mapper,toLong):
@@ -508,13 +508,13 @@ class MreRecord(object):
         # No subrecords, but we have data.
         elif self.data:
             with self.getReader() as reader:
-                recType = self.recType
+                _rec_sig_ = self.recType
                 readAtEnd = reader.atEnd
                 readSubHeader = reader.unpackSubHeader
                 readSeek = reader.seek
                 readRead = reader.read
-                while not readAtEnd(reader.size,recType):
-                    (type,size) = readSubHeader(recType)
+                while not readAtEnd(reader.size,_rec_sig_):
+                    (type,size) = readSubHeader(_rec_sig_)
                     if type != subType:
                         readSeek(size,1)
                     else:

Mopy/bash/patcher/patchers/importers.py
@@ -177,10 +177,10 @@ class _SimpleImporter(ImportPatcher):
         In:
             KFFZPatcher, DeathItemPatcher, ImportScripts, SoundPatcher
         """
-        id_data, set_id_data = self.id_data, set(self.id_data)
+        id_data = self.id_data
         for record in records:
             rec_fid = record.fid
-            if rec_fid not in set_id_data: continue
+            if rec_fid not in id_data: continue
             for attr, value in id_data[rec_fid].iteritems():
                 if record.__getattribute__(attr) != value: break
             else: continue
@@ -408,7 +408,8 @@ class CellImporter(ImportPatcher):
             Modified cell Blocks are kept, the other are discarded.
             """
             modified=False
-            for attr,value in cellData[patchCellBlock.cell.fid].iteritems():
+            patch_cell_fid = patchCellBlock.cell.fid
+            for attr,value in cellData[patch_cell_fid].iteritems():
                 if attr == 'regions':
                     if set(value).difference(set(patchCellBlock.cell.__getattribute__(attr))):
                         patchCellBlock.cell.__setattr__(attr, value)
@@ -418,26 +419,27 @@ class CellImporter(ImportPatcher):
                         patchCellBlock.cell.__setattr__(attr, value)
                         modified=True
             for flag, value in cellData[
-                        patchCellBlock.cell.fid + ('flags',)].iteritems():
+                patch_cell_fid + ('flags',)].iteritems():
                 if patchCellBlock.cell.flags.__getattr__(flag) != value:
                     patchCellBlock.cell.flags.__setattr__(flag, value)
                     modified=True
             if modified:
                 patchCellBlock.cell.setChanged()
-                keep(patchCellBlock.cell.fid)
+                keep(patch_cell_fid)
             return modified
         if not self.isActive: return
         keep = self.patchFile.getKeeper()
         cellData, count = self.cellData, Counter()
         for cellBlock in self.patchFile.CELL.cellBlocks:
-            if cellBlock.cell.fid in cellData and handlePatchCellBlock(cellBlock):
-                count[cellBlock.cell.fid[0]] += 1
+            cell_fid = cellBlock.cell.fid
+            if cell_fid in cellData and handlePatchCellBlock(cellBlock):
+                count[cell_fid[0]] += 1
         for worldBlock in self.patchFile.WRLD.worldBlocks:
             keepWorld = False
             for cellBlock in worldBlock.cellBlocks:
-                if cellBlock.cell.fid in cellData and handlePatchCellBlock(
-                        cellBlock):
-                    count[cellBlock.cell.fid[0]] += 1
+                cell_fid = cellBlock.cell.fid
+                if cell_fid in cellData and handlePatchCellBlock(cellBlock):
+                    count[cell_fid[0]] += 1
                     keepWorld = True
             if worldBlock.worldCellBlock:
                 if worldBlock.worldCellBlock.cell.fid in cellData:

Mopy/bash/patcher/patchers/multitweak_settings.py
@@ -102,7 +102,9 @@ class GmstTweak(DynamicNamedTweak, MultiTweakItem):
                 gmst = MreRecord.type_class['GMST'](
                     RecordHeader('GMST', 0, 0, 0, 0))
                 gmst.eid,gmst.value,gmst.longFids = eid,value,True
-                fid = gmst.fid = keep(gmst.getGMSTFid())
+                gmst_fid = gmst.getGMSTFid()
+                gmst.fid = gmst_fid
+                keep(gmst_fid)
                 patchFile.GMST.setRecord(gmst)
         if len(self.choiceLabels) > 1:
             if self.choiceLabels[self.chosen].startswith(_(u'Custom')):

Mopy/bash/record_groups.py
@@ -80,18 +80,18 @@ class MobBase(object):
                                  type(self))
         #--Analyze ins.
         elif ins is not None:
-            self.loadData(ins, ins.tell() +
-                          self.size - RecordHeader.rec_header_size)
+            self.load_rec_group(ins,
+                ins.tell() + self.size - RecordHeader.rec_header_size)
         #--Analyze internal buffer.
         else:
             with self.getReader() as reader:
-                self.loadData(reader,reader.size)
+                self.load_rec_group(reader, reader.size)
         #--Discard raw data?
         if do_unpack:
             self.data = None
             self.setChanged()
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         raise AbstractError
 
@@ -166,7 +166,7 @@ class MobObjects(MobBase):
         self.id_records = {}
         MobBase.__init__(self, header, loadFactory, ins, do_unpack)
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
         recClass = self.loadFactory.getRecClass(expType)
@@ -272,12 +272,12 @@ class MobObjects(MobBase):
             self.records.append(record)
         self.id_records[record_id] = record
 
-    def keepRecords(self,keepIds):
-        """Keeps records with fid in set keepIds. Discards the rest."""
+    def keepRecords(self, p_keep_ids):
+        """Keeps records with fid in set p_keep_ids. Discards the rest."""
         from . import bosh
         self.records = [record for record in self.records if (record.fid == (
             record.isKeyedByEid and bosh.modInfos.masterName,
-            0) and record.eid in keepIds) or record.fid in keepIds]
+            0) and record.eid in p_keep_ids) or record.fid in p_keep_ids]
         self.id_records.clear()
         self.setChanged()
 
@@ -298,7 +298,7 @@ class MobObjects(MobBase):
 class MobDials(MobObjects):
     """DIAL top block of mod file."""
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
         recClass = self.loadFactory.getRecClass(expType)
@@ -381,7 +381,7 @@ class MobCell(MobBase):
         self.pgrd = None
         MobBase.__init__(self, header, loadFactory, ins, do_unpack)
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         cellType_class = self.loadFactory.getCellTypeClass()
         persistent,temp,distant = self.persistent,self.temp,self.distant
@@ -571,18 +571,18 @@ class MobCell(MobBase):
                     recordList[fids[record.fid]] = record
                     mergeDiscard(record.fid)
 
-    def keepRecords(self,keepIds):
-        """Keeps records with fid in set keepIds. Discards the rest."""
-        if self.pgrd and self.pgrd.fid not in keepIds:
+    def keepRecords(self, p_keep_ids):
+        """Keeps records with fid in set p_keep_ids. Discards the rest."""
+        if self.pgrd and self.pgrd.fid not in p_keep_ids:
             self.pgrd = None
-        if self.land and self.land.fid not in keepIds:
+        if self.land and self.land.fid not in p_keep_ids:
             self.land = None
-        self.temp       = [x for x in self.temp if x.fid in keepIds]
-        self.persistent = [x for x in self.persistent if x.fid in keepIds]
-        self.distant    = [x for x in self.distant if x.fid in keepIds]
+        self.temp       = [x for x in self.temp if x.fid in p_keep_ids]
+        self.persistent = [x for x in self.persistent if x.fid in p_keep_ids]
+        self.distant    = [x for x in self.distant if x.fid in p_keep_ids]
         if self.pgrd or self.land or self.persistent or self.temp or \
                 self.distant:
-            keepIds.add(self.cell.fid)
+            p_keep_ids.add(self.cell.fid)
         self.setChanged()
 
 #------------------------------------------------------------------------------
@@ -679,12 +679,12 @@ class MobCells(MobBase):
         return count
 
     #--Fid manipulation, record filtering ----------------------------------
-    def keepRecords(self,keepIds):
-        """Keeps records with fid in set keepIds. Discards the rest."""
-        #--Note: this call will add the cell to keepIds if any of its
+    def keepRecords(self, p_keep_ids):
+        """Keeps records with fid in set p_keep_ids. Discards the rest."""
+        #--Note: this call will add the cell to p_keep_ids if any of its
         # related records are kept.
-        for cellBlock in self.cellBlocks: cellBlock.keepRecords(keepIds)
-        self.cellBlocks = [x for x in self.cellBlocks if x.cell.fid in keepIds]
+        for cellBlock in self.cellBlocks: cellBlock.keepRecords(p_keep_ids)
+        self.cellBlocks = [x for x in self.cellBlocks if x.cell.fid in p_keep_ids]
         self.id_cellBlock.clear()
         self.setChanged()
 
@@ -716,7 +716,7 @@ class MobCells(MobBase):
 class MobICells(MobCells):
     """Tes4 top block for interior cell records."""
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
         recCellClass = self.loadFactory.getRecClass(expType)
@@ -800,7 +800,7 @@ class MobWorld(MobCells):
         self.road = None
         MobCells.__init__(self, header, loadFactory, ins, do_unpack)
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         cellType_class = self.loadFactory.getCellTypeClass()
         errLabel = u'World Block'
@@ -998,17 +998,17 @@ class MobWorld(MobCells):
                                               mergeIds)
         MobCells.updateRecords(self,srcBlock,mapper,mergeIds)
 
-    def keepRecords(self,keepIds):
-        """Keeps records with fid in set keepIds. Discards the rest."""
-        if self.road and self.road.fid not in keepIds:
+    def keepRecords(self, p_keep_ids):
+        """Keeps records with fid in set p_keep_ids. Discards the rest."""
+        if self.road and self.road.fid not in p_keep_ids:
             self.road = None
         if self.worldCellBlock:
-            self.worldCellBlock.keepRecords(keepIds)
-            if self.worldCellBlock.cell.fid not in keepIds:
+            self.worldCellBlock.keepRecords(p_keep_ids)
+            if self.worldCellBlock.cell.fid not in p_keep_ids:
                 self.worldCellBlock = None
-        MobCells.keepRecords(self,keepIds)
+        MobCells.keepRecords(self, p_keep_ids)
         if self.road or self.worldCellBlock or self.cellBlocks:
-            keepIds.add(self.world.fid)
+            p_keep_ids.add(self.world.fid)
 
 #------------------------------------------------------------------------------
 class MobWorlds(MobBase):
@@ -1021,7 +1021,7 @@ class MobWorlds(MobBase):
         self.orphansSkipped = 0
         MobBase.__init__(self, header, loadFactory, ins, do_unpack)
 
-    def loadData(self,ins,endPos):
+    def load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
         recWrldClass = self.loadFactory.getRecClass(expType)
@@ -1087,7 +1087,7 @@ class MobWorlds(MobBase):
             totalSize = RecordHeader.rec_header_size + sum(
                 x.dump(out) for x in self.worldBlocks)
             out.seek(worldHeaderPos + 4)
-            out.pack('I', totalSize)
+            out.pack(u'I', totalSize)
             out.seek(worldHeaderPos + totalSize)
 
     def getNumRecords(self,includeGroups=True):
@@ -1137,10 +1137,10 @@ class MobWorlds(MobBase):
             self.worldBlocks.append(worldBlock)
             self.id_worldBlocks[fid] = worldBlock
 
-    def keepRecords(self,keepIds):
-        """Keeps records with fid in set keepIds. Discards the rest."""
-        for worldBlock in self.worldBlocks: worldBlock.keepRecords(keepIds)
+    def keepRecords(self, p_keep_ids):
+        """Keeps records with fid in set p_keep_ids. Discards the rest."""
+        for worldBlock in self.worldBlocks: worldBlock.keepRecords(p_keep_ids)
         self.worldBlocks = [x for x in self.worldBlocks if
-                            x.world.fid in keepIds]
+                            x.world.fid in p_keep_ids]
         self.id_worldBlocks.clear()
         self.setChanged()

commit a362533325c4ea298e3aaa15276fc22f329df99d

TES5: Properly fix water height importing
The refactoring using MelSkipInterior fixed attributes not getting set,
which made CellImporter start spitting out a bunch of ITMs due to water
height. Turns out that only worked accidentally. The right solution is
of course to skip the waterHeight attribute entirely in interiors, not
just when dumping but also when importing via patcher.

Also added a note about the scripts that we're currently just dropping
from the versions in Cobl Main.esm. Not sure what to do about that.

Mopy/bash/patcher/patch_files.py:
Make ESL flag part of BP description translatable

Mopy/bash/game/oblivion/patcher/special.py:
Regression fix for Cobl Catalogs

Broke in 0beba3757a367abf5056f7663b9ba55f8e78a539.

Mopy/bash/game/__init__.py
@@ -468,6 +468,7 @@ class GameInfo(object):
     cellRecAttrs = {}
     cellRecFlags = {}
     cell_float_attrs = set()
+    cell_skip_interior_attrs = set()
 
     #--------------------------------------------------------------------------
     # GraphicsPatcher
@@ -616,14 +617,14 @@ class GameInfo(object):
         u'actor_tweaks', u'actor_types', u'actor_values', u'bethDataFiles',
         u'body_tags', u'cc_valid_types', u'cc_passes', u'cellAutoKeys',
         u'cell_float_attrs', u'cellRecAttrs', u'cellRecFlags',
-        u'condition_function_data', u'default_eyes', u'destructible_types',
-        u'ench_stats_attrs', u'generic_av_effects', u'getvatsvalue_index',
-        u'gmstEids', u'graphicsFidTypes', u'graphicsModelAttrs',
-        u'graphicsTypes', u'hostile_effects', u'inventoryTypes',
-        u'keywords_types', u'listTypes', u'mgef_basevalue', u'mgef_name',
-        u'mgef_school', u'mgef_stats_attrs', u'namesTypes', u'nirnroots',
-        u'object_bounds_types', u'pricesTypes', u'record_type_name',
-        u'relations_attrs', u'relations_csv_header',
+        u'cell_skip_interior_attrs', u'condition_function_data',
+        u'default_eyes', u'destructible_types', u'ench_stats_attrs',
+        u'generic_av_effects', u'getvatsvalue_index', u'gmstEids',
+        u'graphicsFidTypes', u'graphicsModelAttrs', u'graphicsTypes',
+        u'hostile_effects', u'inventoryTypes', u'keywords_types', u'listTypes',
+        u'mgef_basevalue', u'mgef_name', u'mgef_school', u'mgef_stats_attrs',
+        u'namesTypes', u'nirnroots', u'object_bounds_types', u'pricesTypes',
+        u'record_type_name', u'relations_attrs', u'relations_csv_header',
         u'relations_csv_row_format', u'save_rec_types', u'scripts_types',
         u'soundsLongsTypes', u'soundsTypes', u'spell_stats_attrs',
         u'statsHeaders', u'statsTypes', u'text_long_types', u'text_types',

Mopy/bash/game/oblivion/patcher/special.py
@@ -123,14 +123,19 @@ class AlchemicalCatalogs(_AAlchemicalCatalogs,Patcher):
             book.full = full
             book.value = value
             book.weight = 0.2
-            book.fid = keep((GPath(u'Cobl Main.esm'),objectId))
             book.text = u'<div align="left"><font face=3 color=4444>'
-            book.text += _(u"Salan's Catalog of ")+u'%s\r\n\r\n' % full
+            book.text += (_(u"Salan's Catalog of %s") + u'\r\n\r\n') % full
             book.iconPath = iconPath
             book.model = book.getDefault('model')
             book.model.modPath = modelPath
             book.model.modb_p = modb_p
             book.modb = book
+            ##: In Cobl Main.esm, the books have a script attached
+            # (<cobGenDevalueOS [SCPT:01001DDD]>). This currently gets rid of
+            # that, should we keep it instead?
+            # book.script = (GPath(u'Cobl Main.esm'), 0x001DDD)
+            book.fid = (GPath(u'Cobl Main.esm'), objectId)
+            keep(book.fid)
             self.patchFile.BOOK.setRecord(book)
             return book
         #--Ingredients Catalog

Mopy/bash/game/skyrim/constants.py
@@ -2372,6 +2372,7 @@ cellRecFlags = {
 cell_float_attrs = {u'fogNear', u'fogFar', u'directionalFade', u'fogClip',
                     u'fogPower', u'fogMax', u'fresnelPower', u'lightFadeBegin',
                     u'lightFadeEnd', u'waterHeight'}
+cell_skip_interior_attrs = {u'waterHeight'}
 
 #------------------------------------------------------------------------------
 # GraphicsPatcher

Mopy/bash/patcher/patch_files.py
@@ -317,9 +317,9 @@ class PatchFile(_PFile, ModFile):
         # contains overrides, no new records
         if bush.game.has_esl and bass.settings['bash.mods.auto_flag_esl']:
             self.tes4.flags1.eslFile = True
-            self.tes4.description += (u'\nThis patch has been automatically '
-                                      u'ESL-flagged to save a load order '
-                                      u'slot.')
+            self.tes4.description += u'\n' + _(
+                u'This patch has been automatically ESL-flagged to save a '
+                u'load order slot.')
 
 class CBash_PatchFile(_PFile, ObModFile):
     """Defines and executes patcher configuration."""

Mopy/bash/patcher/patchers/preservers.py
@@ -526,7 +526,10 @@ class CellImporter(ImportPatcher):
             """
             if not cellBlock.cell.flags1.ignored:
                 fid = cellBlock.cell.fid
-                for attr in attrs:
+                # If we're in an interior, see if we have to ignore any attrs
+                actual_attrs = ((attrs - bush.game.cell_skip_interior_attrs)
+                                if cellBlock.cell.flags.isInterior else attrs)
+                for attr in actual_attrs:
                     tempCellData[fid][attr] = cellBlock.cell.__getattribute__(
                         attr)
                 for flg_ in flgs_:
@@ -543,7 +546,10 @@ class CellImporter(ImportPatcher):
             if not cellBlock.cell.flags1.ignored:
                 rec_fid = cellBlock.cell.fid
                 if rec_fid not in tempCellData: return
-                for attr in attrs:
+                # If we're in an interior, see if we have to ignore any attrs
+                actual_attrs = ((attrs - bush.game.cell_skip_interior_attrs)
+                                if cellBlock.cell.flags.isInterior else attrs)
+                for attr in actual_attrs:
                     master_attr = cellBlock.cell.__getattribute__(attr)
                     if tempCellData[rec_fid][attr] != master_attr:
                         cellData[rec_fid][attr] = tempCellData[rec_fid][attr]