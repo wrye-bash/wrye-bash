wrye-bash/wrye-bash

commit f8678228fe82c999e4b5d2645b1494109dbac459

[!] Encapsulate long/short FormID handling
Doesn't fully get rid of getLongMapper yet, since the tweaks will be
tackled in the rest of this branch (duh).

Required a bit of rewriting here and there, but the resulting code is
actually much nicer.

This is a serious behavior change. I was not able to get it to break,
despite testing it fairly extensively on all games, but that doesn't
mean anything - hence the [!].

Mopy/bash/basher/mod_links.py
@@ -1556,15 +1556,14 @@ class Mod_DecompileAll(EnabledLink):
                 for master in modFile.tes4.masters:
                     masterFile = mod_files.ModFile(bosh.modInfos[master], loadFactory)
                     masterFile.load(True)
-                    mapper = masterFile.getLongMapper()
                     for record in masterFile.SCPT.getActiveRecords():
-                        id_text[mapper(record.fid)] = record.script_source
-                mapper = modFile.getLongMapper()
+                        id_text[record.fid] = record.script_source
                 newRecords = []
+                generic_lore_fid = (bosh.modInfos.masterName, 0x025811)
                 for record in modFile.SCPT.records:
-                    fid = mapper(record.fid)
+                    fid = record.fid
                     #--Special handling for genericLoreScript
-                    if (fid in id_text and record.fid == 0x00025811 and
+                    if (fid in id_text and record.fid == generic_lore_fid and
                         record.compiled_size == 4 and record.last_index == 0):
                         removed.append(record.eid)
                         badGenericLore = True

Mopy/bash/basher/saves_links.py
@@ -878,6 +878,7 @@ class Save_UpdateNPCLevels(EnabledLink):
                 subProgress(index,_(u'Scanning ') + modName.s)
                 modInfo = bosh.modInfos[modName]
                 modFile = ModFile(modInfo, loadFactory)
+                short_mapper = modFile.getShortMapper()
                 try:
                     modFile.load(True)
                 except ModError as x:
@@ -888,7 +889,7 @@ class Save_UpdateNPCLevels(EnabledLink):
                 mapToOrdered = MasterMap(modFile.tes4.masters + [modName],
                                          ordered)
                 for npc in modFile.NPC_.getActiveRecords():
-                    fid = mapToOrdered(npc.fid,None)
+                    fid = mapToOrdered(short_mapper(npc.fid), None)
                     if not fid: continue
                     npc_info[fid] = (npc.eid, npc.level, npc.calcMin, npc.calcMax, npc.flags.pcLevelOffset)
             #--Loop over savefiles

Mopy/bash/bosh/_mergeability.py
@@ -100,12 +100,13 @@ def isPBashMergeable(modInfo, minfos, reasons):
         if not verbose: return False
         reasons.append(_(u'Empty mod.'))
     #--New record
-    lenMasters = len(modFile.tes4.masters)
     newblocks = []
+    self_name = modInfo.name
     for top_type,block in modFile.tops.iteritems():
         for record in block.iter_records():
-            if not record.flags1.ignored and record.fid >> 24 >= lenMasters:
-                if record.flags1.deleted: continue #if new records exist but are deleted just skip em.
+            if not record.flags1.ignored and record.fid[0] == self_name:
+                # if new records exist but are deleted just skip em.
+                if record.flags1.deleted: continue
                 if not verbose: return False
                 newblocks.append(top_type)
                 break

Mopy/bash/bosh/_saves.py
@@ -467,17 +467,6 @@ class SaveFile(object):
             del self.fid_recNum[fid]
             return True
 
-    def getShortMapper(self):
-        """Returns a mapping function to map long fids to short fids."""
-        indices = dict(
-            [(mas_name, idx) for idx, mas_name in enumerate(self._masters)])
-        def mapper(fid):
-            if fid is None: return None
-            modName,object = fid
-            mod = indices[modName]
-            return (int(mod) << 24 ) | int(object)
-        return mapper
-
     def getFid(self,iref,default=None):
         """Returns fid corresponding to iref."""
         if not iref: return default
@@ -746,9 +735,8 @@ class SaveSpells(object):
         except ModError as err:
             deprint(u'skipped mod due to read error (%s)' % err)
             return
-        modFile.convertToLongFids(('SPEL',))
-        spells = modInfo.extras['bash.spellList'] = dict(
-            [(record.fid,record) for record in modFile.SPEL.getActiveRecords()])
+        spells = modInfo.extras['bash.spellList'] = {
+            record.fid: record for record in modFile.SPEL.getActiveRecords()}
         self.allSpells.update(spells)
 
     def getPlayerSpells(self):

Mopy/bash/bosh/faces.py
@@ -397,6 +397,7 @@ class PCFaces(object):
         loadFactory = LoadFactory(False,MreRecord.type_class['NPC_'])
         modFile = ModFile(modInfo,loadFactory)
         modFile.load(True)
+        short_mapper = modFile.getShortMapper()
         faces = {}
         for npc in modFile.NPC_.getActiveRecords():
             face = PCFaces.PCFace()
@@ -406,7 +407,10 @@ class PCFaces(object):
                           'fggs_p','fgga_p','fgts_p','level','skills',
                           'health','unused2','baseSpell',
                           'fatigue','attributes','iclass'):
-                setattr(face,field,getattr(npc,field))
+                npc_val = getattr(npc, field)
+                if type(npc_val) == tuple: # Hacky check for FormIDs
+                    npc_val = short_mapper(npc_val)
+                setattr(face, field, npc_val)
             face.gender = npc.flags.female
             face.pcName = npc.full
             faces[face.eid] = face

Mopy/bash/brec/record_groups.py
@@ -187,9 +187,10 @@ class MobBase(object):
         """Updates set of master names according to masters actually used."""
         raise AbstractError(u'updateMasters not implemented')
 
-    def updateRecords(self,block,mapper,toLong):
+    def updateRecords(self, srcBlock, mergeIds):
         """Looks through all of the records in 'block', and updates any
-        records in self that exist with the data in 'block'."""
+        records in self that exist with the data in 'block'. 'block' must be in
+        long fids format."""
         raise AbstractError(u'updateRecords not implemented')
 
 #------------------------------------------------------------------------------
@@ -307,16 +308,16 @@ class MobObjects(MobBase):
         self.id_records.clear()
         self.setChanged()
 
-    def updateRecords(self,srcBlock,mapper,mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         if self.records and not self.id_records:
             self.indexRecords()
         merge_ids_discard = mergeIds.discard
         copy_to_self = self.setRecord
         dest_rec_fids = self.id_records
         for record in srcBlock.getActiveRecords():
-            src_rec_fid = mapper(record.fid)
+            src_rec_fid = record.fid
             if src_rec_fid in dest_rec_fids:
-                copy_to_self(record.getTypeCopy(mapper))
+                copy_to_self(record.getTypeCopy())
                 merge_ids_discard(src_rec_fid)
 
     def merge_records(self, block, loadSet, mergeIds, iiSkipMerge, doFilter):
@@ -464,9 +465,10 @@ class MobDial(MobObjects):
         loadSetIsSuperset = loadSet.issuperset
         # First, check the main DIAL record
         src_dial = block.dial
-        if self.dial.fid != src_dial.fid:
+        src_dial_fid = src_dial.fid
+        if self.dial.fid != src_dial_fid:
             raise ModFidMismatchError(self.inName, u'DIAL', self.dial.fid,
-                src_dial.fid)
+                src_dial_fid)
         if not src_dial.flags1.ignored:
             # If we're Filter-tagged, perform merge filtering first
             if doFilter:
@@ -483,7 +485,7 @@ class MobDial(MobObjects):
             if not iiSkipMerge:
                 # We're past all hurdles - mark the record as merged, and stick
                 # a copy into ourselves
-                mergeIdsAdd(src_dial.fid)
+                mergeIdsAdd(src_dial_fid)
                 self.dial = src_dial.getTypeCopy()
             # Now we're ready to filter and merge the INFO children
             super(MobDial, self).merge_records(block, loadSet, mergeIds,
@@ -494,9 +496,9 @@ class MobDial(MobObjects):
             self.dial.updateMasters(masterset_add)
         super(MobDial, self).updateMasters(masterset_add)
 
-    def updateRecords(self, srcBlock, mapper, mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         src_dial = srcBlock.dial
-        src_dial_fid = mapper(src_dial.fid)
+        src_dial_fid = src_dial.fid
         if self.dial.fid != src_dial_fid:
             raise ModFidMismatchError(self.inName, u'DIAL', self.dial.fid,
                 src_dial_fid)
@@ -504,9 +506,9 @@ class MobDial(MobObjects):
         # it as not merged because keepRecords above ensures that we never
         # discard a DIAL when it still has INFO children
         if not src_dial.flags1.ignored:
-            self.dial = src_dial.getTypeCopy(mapper)
+            self.dial = src_dial.getTypeCopy()
             mergeIds.discard(src_dial_fid)
-        super(MobDial, self).updateRecords(srcBlock, mapper, mergeIds)
+        super(MobDial, self).updateRecords(srcBlock, mergeIds)
 
     def _sort_by_pnam(self):
         """Returns a list of the INFOs of this DIAL record, sorted by their
@@ -740,15 +742,15 @@ class MobDials(MobBase):
         for dialogue in self.dialogues:
             dialogue.updateMasters(masterset_add)
 
-    def updateRecords(self, srcBlock, mapper, mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         if self.dialogues and not self.id_dialogues:
             self.indexRecords()
         lookup_dial = self.id_dialogues.get
         for src_dial in srcBlock.dialogues:
             # Check if we have a corresponding DIAL record in the destination
-            dest_dial = lookup_dial(mapper(src_dial.dial.fid))
+            dest_dial = lookup_dial(src_dial.dial.fid)
             if dest_dial:
-                dest_dial.updateRecords(src_dial, mapper, mergeIds)
+                dest_dial.updateRecords(src_dial, mergeIds)
 
     def __repr__(self):
         return u'<DIAL GRUP: %u record(s)>' % len(self.dialogues)
@@ -780,6 +782,7 @@ class MobCell(MobBase):
         distantAppend = self.distant_refs.append
         insSeek = ins.seek
         subgroupLoaded = [False, False, False]
+        groupType = None # guaranteed to compare False to any of them
         while not insAtEnd(endPos,'Cell Block'):
             header = insRecHeader()
             recType = header.recType
@@ -944,7 +947,7 @@ class MobCell(MobBase):
         if self.pgrd:
             self.pgrd.updateMasters(masterset_add)
 
-    def updateRecords(self, srcBlock, mapper, mergeIds, __attrget=attrgetter(
+    def updateRecords(self, srcBlock, mergeIds, __attrget=attrgetter(
         u'cell', u'pgrd', u'land', u'persistent_refs', u'temp_refs',
         u'distant_refs')):
         """Updates any records in 'self' that exist in 'srcBlock'."""
@@ -953,12 +956,12 @@ class MobCell(MobBase):
         for attr, (myRecord, record) in zip((u'cell', u'pgrd', u'land'),
                                           self_src_attrs):
             if myRecord and record:
-                src_rec_fid = mapper(record.fid)
+                src_rec_fid = record.fid
                 if myRecord.fid != src_rec_fid:
                     raise ModFidMismatchError(self.inName, myRecord.recType,
                         myRecord.fid, src_rec_fid)
                 if not record.flags1.ignored:
-                    record = record.getTypeCopy(mapper)
+                    record = record.getTypeCopy()
                     setattr(self, attr, record)
                     mergeDiscard(src_rec_fid)
         for attr, (self_rec_list, src_rec_list) in zip(
@@ -966,9 +969,9 @@ class MobCell(MobBase):
                 self_src_attrs[3:]):
             fids = {record.fid: i for i, record in enumerate(self_rec_list)}
             for record in src_rec_list:
-                src_fid = mapper(record.fid)
+                src_fid = record.fid
                 if not record.flags1.ignored and src_fid in fids:
-                    self_rec_list[fids[src_fid]] = record.getTypeCopy(mapper)
+                    self_rec_list[fids[src_fid]] = record.getTypeCopy()
                     mergeDiscard(src_fid)
 
     def iter_records(self):
@@ -1080,7 +1083,7 @@ class MobCells(MobBase):
 
     def indexRecords(self):
         """Indexes records by fid."""
-        self.id_cellBlock = dict((x.cell.fid,x) for x in self.cellBlocks)
+        self.id_cellBlock = {x.cell.fid: x for x in self.cellBlocks}
 
     def setCell(self,cell):
         """Adds record to record list and indexed."""
@@ -1106,11 +1109,11 @@ class MobCells(MobBase):
 
     def getUsedBlocks(self):
         """Returns a set of blocks that exist in this group."""
-        return set(x.getBsb()[0] for x in self.cellBlocks)
+        return {x.getBsb()[0] for x in self.cellBlocks}
 
     def getUsedSubblocks(self):
         """Returns a set of block/sub-blocks that exist in this group."""
-        return set(x.getBsb() for x in self.cellBlocks)
+        return {x.getBsb() for x in self.cellBlocks}
 
     def getBsbSizes(self):
         """Returns the total size of the block, but also returns a
@@ -1232,15 +1235,15 @@ class MobCells(MobBase):
         for cellBlock in self.cellBlocks:
             cellBlock.convertFids(mapper,toLong)
 
-    def updateRecords(self,srcBlock,mapper,mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         """Updates any records in 'self' that exist in 'srcBlock'."""
         if self.cellBlocks and not self.id_cellBlock:
             self.indexRecords()
         id_Get = self.id_cellBlock.get
         for srcCellBlock in srcBlock.cellBlocks:
-            cellBlock = id_Get(mapper(srcCellBlock.cell.fid))
+            cellBlock = id_Get(srcCellBlock.cell.fid)
             if cellBlock:
-                cellBlock.updateRecords(srcCellBlock,mapper,mergeIds)
+                cellBlock.updateRecords(srcCellBlock, mergeIds)
 
     def updateMasters(self, masterset_add):
         """Updates set of master names according to masters actually used."""
@@ -1518,24 +1521,24 @@ class MobWorld(MobCells):
             self.worldCellBlock.updateMasters(masterset_add)
         super(MobWorld, self).updateMasters(masterset_add)
 
-    def updateRecords(self,srcBlock,mapper,mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         """Updates any records in 'self' that exist in 'srcBlock'."""
         for attr in (u'world', u'road'):
             myRecord = getattr(self, attr)
             record = getattr(srcBlock, attr)
             if myRecord and record:
-                src_rec_fid = mapper(record.fid)
+                src_rec_fid = record.fid
                 if myRecord.fid != src_rec_fid:
                     raise ModFidMismatchError(self.inName, myRecord.recType,
                         myRecord.fid, src_rec_fid)
                 if not record.flags1.ignored:
-                    record = record.getTypeCopy(mapper)
+                    record = record.getTypeCopy()
                     setattr(self, attr, record)
                     mergeIds.discard(src_rec_fid)
         if self.worldCellBlock and srcBlock.worldCellBlock:
-            self.worldCellBlock.updateRecords(srcBlock.worldCellBlock,mapper,
-                                              mergeIds)
-        super(MobWorld, self).updateRecords(srcBlock, mapper, mergeIds)
+            self.worldCellBlock.updateRecords(srcBlock.worldCellBlock,
+                mergeIds)
+        super(MobWorld, self).updateRecords(srcBlock, mergeIds)
 
     def iter_records(self):
         single_recs = [x for x in (self.world, self.road) if x]
@@ -1729,23 +1732,23 @@ class MobWorlds(MobBase):
 
     def indexRecords(self):
         """Indexes records by fid."""
-        self.id_worldBlocks = dict((x.world.fid,x) for x in self.worldBlocks)
+        self.id_worldBlocks = {x.world.fid: x for x in self.worldBlocks}
 
     def updateMasters(self, masterset_add):
         """Updates set of master names according to masters actually used."""
         for worldBlock in self.worldBlocks:
             worldBlock.updateMasters(masterset_add)
 
-    def updateRecords(self,srcBlock,mapper,mergeIds):
+    def updateRecords(self, srcBlock, mergeIds):
         """Updates any records in 'self' that exist in 'srcBlock'."""
         if self.worldBlocks and not self.id_worldBlocks:
             self.indexRecords()
         id_worldBlocks = self.id_worldBlocks
         idGet = id_worldBlocks.get
         for srcWorldBlock in srcBlock.worldBlocks:
-            worldBlock = idGet(mapper(srcWorldBlock.world.fid))
+            worldBlock = idGet(srcWorldBlock.world.fid)
             if worldBlock:
-                worldBlock.updateRecords(srcWorldBlock,mapper,mergeIds)
+                worldBlock.updateRecords(srcWorldBlock, mergeIds)
 
     def setWorld(self, world):
         """Adds record to record list and indexed."""

Mopy/bash/brec/record_structs.py
@@ -439,8 +439,7 @@ class MreRecord(object):
     def convertFids(self,mapper,toLong):
         """Converts fids between formats according to mapper.
         toLong should be True if converting to long format or False if converting to short format."""
-        raise exception.AbstractError(u'convertFids called on skipped type '
-                                      u'%s' % self.recType)
+        self.fid = mapper(self.fid)
 
     def updateMasters(self, masterset_add):
         """Updates set of master names according to masters actually used."""

Mopy/bash/game/__init__.py
@@ -457,8 +457,6 @@ class GameInfo(object):
     #--------------------------------------------------------------------------
     # SoundPatcher
     #--------------------------------------------------------------------------
-    # Needs longs in SoundPatcher
-    soundsLongsTypes = set()  # initialize with literal
     soundsTypes = {}
 
     #--------------------------------------------------------------------------
@@ -495,7 +493,6 @@ class GameInfo(object):
     #--------------------------------------------------------------------------
     # Text Patcher
     #--------------------------------------------------------------------------
-    text_long_types = set()
     text_types = {}
 
     #--------------------------------------------------------------------------
@@ -627,7 +624,7 @@ class GameInfo(object):
         u'record_type_name', u'relations_attrs', u'relations_csv_header',
         u'relations_csv_row_format', u'save_rec_types', u'scripts_types',
         u'soundsLongsTypes', u'soundsTypes', u'spell_stats_attrs',
-        u'statsHeaders', u'statsTypes', u'text_long_types', u'text_types',
+        u'statsHeaders', u'statsTypes', u'text_types',
     }
 
     @classmethod

commit 0372e8c7d27fc351d05d4c3822581c8ae7fc1b5b

Regression: Fix Update NPC Levels
Broke in f8678228fe82c999e4b5d2645b1494109dbac459. Can't grab a short
mapper before the file has been loaded, otherwise the masters won't have
been loaded yet.

Maybe we should enforce this somehow - a boolean we set to true in
load()?

scripts/build.py:
build: Show better error on missing name/email

Rather than a plain KeyError, tell the user exactly what needs to be
done to fix it.

Mopy/bash/basher/mod_links.py:
Mopy/bash/basher/saves_links.py:
Two followups for .s pruning

Mopy/bash/basher/mod_links.py
@@ -1350,7 +1350,7 @@ class Mod_FogFixer(ItemLink):
             fixed = []
             for index,(fileName,fileInfo) in enumerate(self.iselected_pairs()):
                 if fileName == bush.game.master_file: continue
-                progress(index,_(u'Scanning')+fileName.s)
+                progress(index, _(u'Scanning %s') % fileName)
                 fog_fixer = bosh.mods_metadata.NvidiaFogFixer(fileInfo)
                 fog_fixer.fix_fog(SubProgress(progress, index, index + 1))
                 if fog_fixer.fixedCells:

Mopy/bash/basher/saves_links.py
@@ -874,16 +874,16 @@ class Save_UpdateNPCLevels(EnabledLink):
             subProgress = SubProgress(progress,0,0.4,len(ordered))
             modErrors = []
             for index,modName in enumerate(ordered):
-                subProgress(index,_(u'Scanning ') + modName.s)
+                subProgress(index, _(u'Scanning %s') % modName)
                 modInfo = bosh.modInfos[modName]
                 modFile = ModFile(modInfo, loadFactory)
-                short_mapper = modFile.getShortMapper()
                 try:
                     modFile.load(True)
                 except ModError as x:
                     modErrors.append(u'%s'%x)
                     continue
                 if 'NPC_' not in modFile.tops: continue
+                short_mapper = modFile.getShortMapper()
                 #--Loop over mod NPCs
                 mapToOrdered = MasterMap(modFile.tes4.masters + [modName],
                                          ordered)

scripts/build.py
@@ -378,6 +378,22 @@ def pack_installer(nsis_path, version, file_version):
     )
 
 
+def get_repo_sig(repo):
+    """Wrapper around pygit2 that shows a helpful error message to the user if
+    their credentials have not been configured yet."""
+    try:
+        return repo.default_signature
+    except KeyError:
+        print(u'\n'.join([u'', # empty line before the error
+            u'ERROR: You have not set up your git identity yet.',
+            u'This is necessary for the git operations that the build script '
+            u'uses.',
+            u'You can configure them as follows:',
+            u'   git config --global user.name "Your Name"',
+            u'   git config --global user.email "you@example.com"']))
+        sys.exit(1)
+
+
 @contextmanager
 def update_file_version(version, commit=False):
     fname = "bass.py"
@@ -397,7 +413,7 @@ def update_file_version(version, commit=False):
         os.fsync(fopen.fileno())
     if commit:
         repo = pygit2.Repository(ROOT_PATH)
-        user = repo.default_signature
+        user = get_repo_sig(repo)
         parent = [repo.head.target]
         rel_path = os.path.relpath(orig_path, repo.workdir).replace('\\', '/')
         if repo.status_file(rel_path) == pygit2.GIT_STATUS_WT_MODIFIED:
@@ -562,7 +578,7 @@ def clean_repo():
         # - then stash ignored and untracked
         # - unstash modified files
         # and we have a clean repo!
-        sig = repo.default_signature
+        sig = get_repo_sig(repo)
         # Not unused, PyCharm is not smart enough to understand utils.suppress
         mod_stashed = False
         unt_stashed = False