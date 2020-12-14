wrye-bash/wrye-bash

commit c966a4830270aef5280bf5f71613bfa992579f23

Address all remaining tweak pooling regressions
Unified the scanOrder/editOrder among tweakers as well.

Mopy/bash/patcher/patchers/multitweak_names.py:
PyCharm messed with the imports, kept it.

Closes #541

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -5419,7 +5419,7 @@ C.Location, -C.LockList
             <td>All but Fallout 4</td>
         </tr>
         <tr>
-            <td>Faction Crime Gold Multplier Fix</td>
+            <td>Faction Crime Gold Multiplier Fix</td>
             <td>Fix factions with unset Crime Gold Multiplier to have a Crime
                 Gold Multiplier of 1.0.</td>
             <td>Oblivion &amp; Nehrim&colon; At Fate's Edge</td>

Mopy/bash/game/oblivion/records.py
@@ -1853,13 +1853,13 @@ class MreSoun(MelRecord):
         # This is the old format of SNDX - read it, but dump SNDX only
         MelReadOnly(
             MelStruct(b'SNDD', u'2BbsH2s', u'minDistance', u'maxDistance',
-                      u'freqAdjustment', (u'unused1', null1),
-                      (_flags, u'flags'), (u'unused2', null2))
+                u'freqAdjustment', (u'unused1', null1), (_flags, u'flags'),
+                (u'unused2', null2))
         ),
-        MelOptStruct(b'SNDX', u'2BbsH2sH2B', u'minDistance', u'maxDistance',
-                     u'freqAdjustment', (u'unused1', null1),
-                     (_flags, u'flags'), (u'unused2', null2), u'staticAtten',
-                     u'stopTime', u'startTime'),
+        MelStruct(b'SNDX', u'2BbsH2sH2B', u'minDistance', u'maxDistance',
+            u'freqAdjustment', (u'unused1', null1), (_flags, u'flags'),
+            (u'unused2', null2), (u'staticAtten', None), u'stopTime',
+            u'startTime'),
     )
     __slots__ = melSet.getSlotsUsed()
 

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -143,11 +143,6 @@ _remaining_checks = re.compile(u'see.*me|mark(?!ynaz)')
 
 class _APlayableTweak(MultiTweakItem):
     """Shared code of 'armor/clothing playable' tweaks."""
-    ##: This was in the CBash version only - do we need to build something like
-    # this for PBash tweaks?
-    #scanOrder = 29 # Run before the show armor/clothing tweaks
-    #editOrder = 29
-
     @staticmethod
     def _any_body_flag_set(record):
         return any(getattr(record.biped_flags, bd_flag) for bd_flag
@@ -371,9 +366,9 @@ class _AWeightTweak_SEFF(_AWeightTweak):
         if not super(_AWeightTweak_SEFF, self).wants_record(record):
             return False
         return (self._ignore_effects or
-                (self._seff_code not in record.getEffects() and
-                 ##: Skip OBME records, at least for now
-                 record.obme_record_version is not None))
+                ##: Skip OBME records, at least for now
+                (record.obme_record_version is None and
+                 self._seff_code not in record.getEffects()))
 
 #------------------------------------------------------------------------------
 class AssortedTweak_PotionWeight(_AWeightTweak_SEFF):
@@ -421,11 +416,8 @@ class AssortedTweak_PotionWeightMinimum(_AWeightTweak):
     tweak_choices = [(u'0.1', 0.1), (u'0.5', 0.5), (u'1.0', 1.0),
                      (u'2.0', 2.0), (u'4.0', 4.0), (_(u'Custom'), 0.0)]
     tweak_log_msg = _(u'Ingestibles Reweighed: %(total_changed)d')
+    tweak_order = 11 # Run after Reweigh: Potions (Maximum) for consistency
     _log_weight_value = _(u'Ingestibles set to minimum weight of %f.')
-    ##: This was in the CBash version only - do we need to build something like
-    # this for PBash tweaks?
-    #scanOrder = 33 #Have it run after the max weight for consistent results
-    #editOrder = 33
 
     def wants_record(self, record): ##: no SEFF condition - intended?
         return (record.weight < self.chosen_weight and
@@ -859,6 +851,6 @@ class AssortedTweak_GunsUseISAnimation(MultiTweakItem):
 #------------------------------------------------------------------------------
 class TweakAssortedPatcher(MultiTweaker):
     """Tweaks assorted stuff. Sub-tweaks behave like patchers themselves."""
-    scanOrder = 32
-    editOrder = 32
+    scanOrder = 29 # Run before other tweakers (due to the 'playable' tweaks)
+    editOrder = 29
     _tweak_classes = [globals()[t] for t in bush.game.assorted_tweaks]

Mopy/bash/patcher/base.py
@@ -29,6 +29,8 @@ from this module outside of the patcher package."""
 # unhelpful) docs from overriding methods to save some (100s) lines. We must
 # also document which methods MUST be overridden by raising AbstractError. For
 # instance Patcher.buildPatch() apparently is NOT always overridden
+import copy
+
 from .. import load_order
 from ..exception import AbstractError
 
@@ -111,8 +113,8 @@ class AMultiTweaker(Abstract_Patcher):
     """Combines a number of sub-tweaks which can be individually enabled and
     configured through a choice menu."""
     patcher_group = u'Tweakers'
-    scanOrder = 20
-    editOrder = 20
+    scanOrder = 30
+    editOrder = 30
     _tweak_classes = [] # override in implementations
 
     def __init__(self, p_name, p_file, enabled_tweaks):
@@ -122,8 +124,12 @@ class AMultiTweaker(Abstract_Patcher):
 
     @classmethod
     def tweak_instances(cls):
-        return sorted((tc() for tc in cls._tweak_classes),
-                      key=lambda a: a.tweak_name.lower())
+        tweak_classes = copy.deepcopy(cls._tweak_classes)
+        # Sort alphabetically first for aesthetic reasons
+        tweak_classes.sort(key=lambda c: c.tweak_name)
+        # After that, sort to make tweaks instantiate & run in the right order
+        tweak_classes.sort(key=lambda c: c.tweak_order)
+        return [t() for t in tweak_classes]
 
 #------------------------------------------------------------------------------
 # AMultiTweakItem(object) -----------------------------------------------------
@@ -178,6 +184,10 @@ class AMultiTweakItem(object):
     # argument, total_changed, which contains the total number of changed
     # records. Automatically gets wtxt formatting prepended.
     tweak_log_msg = u'OVERRIDE'
+    # A sorting key which the tweaker that this tweak belongs to uses to
+    # determine the order in which to run tweaks. A lower number means that the
+    # tweak will run sooner, a higher number means later.
+    tweak_order = 10
     # If True, this tweak will be checked by default
     default_enabled = False
     # If True, tweak_key will be shown in the 'custom value' popup

Mopy/bash/patcher/patchers/base.py
@@ -167,7 +167,7 @@ class MultiTweaker(AMultiTweaker,Patcher):
                         keep(record.fid)
                         tweak_counter[p_tweak][record.fid[0]] += 1
         # We're done with all tweaks, give them a chance to clean up and do any
-        # finishing touches (e.g. injecting records for GMST tweaks), then log
+        # finishing touches (e.g. creating records for GMST tweaks), then log
         for tweak in self.enabled_tweaks:
             tweak.finish_tweaking(self.patchFile)
             tweak.tweak_log(log, tweak_counter[tweak])

Mopy/bash/patcher/patchers/multitweak_actors.py
@@ -32,7 +32,6 @@ from ...bolt import GPath
 from ...exception import AbstractError
 from .base import MultiTweakItem, MultiTweaker
 
-# Patchers: 30 ----------------------------------------------------------------
 class _AActorTweak(MultiTweakItem):
     """Base for all actor tweaks."""
     @staticmethod
@@ -100,6 +99,7 @@ class MAONPCSkeletonPatcher(_ASkeletonTweak):
                   u'you have MAO installed.')
     tweak_key = u'MAO Skeleton'
     tweak_log_header = _(u'MAO Skeleton Setter')
+    tweak_order = 11 # Run after the vanilla skeleton tweak for consistency
     _sheo_skeleton = u'characters\\_male\\skeletonsesheogorath.nif'
     _sheo_skeleton_mao = (u"Mayu's Projects[M]\\Animation Overhaul\\Vanilla\\"
                           u'SkeletonSESheogorath.nif')
@@ -173,10 +173,6 @@ class VanillaNPCSkeletonPatcher(_ASkeletonTweak):
     tweak_log_header = _(u'Vanilla Beast Skeleton')
     _new_skeleton = u'Characters\\_Male\\SkeletonBeast.nif'
     _old_skeleton = u'characters\\_male\\skeleton.nif'
-    ##: This was in the CBash version only - do we need to build something like
-    # this for PBash tweaks?
-    #scanOrder = 31 #Run before MAO
-    #editOrder = 31
 
     def wants_record(self, record):
         old_mod_path = self._get_skeleton_path(record)

Mopy/bash/patcher/patchers/multitweak_clothes.py
@@ -48,18 +48,13 @@ class ClothesTweak(DynamicTweak, MultiTweakItem):
         super(ClothesTweak, self).__init__(tweak_name, tweak_tip, tweak_key,
             *tweak_choices)
         type_key = tweak_key[:tweak_key.find(u'.')]
-        self.or_type_flags = type_key in (u'robes', u'rings')
+        self.or_type_flags = type_key == u'rings'
         self.type_flags = self.clothes_flags[type_key]
-
-    @staticmethod
-    def _get_biped_flags(record):
-        """Returns the biped flags of the specified record as an integer."""
-        return int(record.biped_flags) & 0xFFFF
 
     def wants_record(self, record):
         if self._is_nonplayable(record):
             return False
-        rec_type_flags = self._get_biped_flags(record)
+        rec_type_flags = int(record.biped_flags) & 0xFFFF
         my_type_flags = self.type_flags
         return ((rec_type_flags == my_type_flags) or (self.or_type_flags and (
                 rec_type_flags & my_type_flags == rec_type_flags)))
@@ -88,7 +83,7 @@ class ClothesTweak_MaxWeight(ClothesTweak):
         super(ClothesTweak_MaxWeight, self).tweak_log(log, count)
 
 #------------------------------------------------------------------------------
-class _AUnblockTweak(ClothesTweak):
+class ClothesTweak_Unblock(ClothesTweak):
     """Unlimited rings, amulets."""
     tweak_log_msg = _(u'Clothes Tweaked: %(total_changed)d')
 
@@ -102,10 +97,9 @@ class _AUnblockTweak(ClothesTweak):
         return self._unblock_flags
 
     def wants_record(self, record):
-        return super(_AUnblockTweak, self).wants_record(
-            record) and int(self._get_biped_flags(record) & self.unblock_flags)
+        return super(ClothesTweak_Unblock, self).wants_record(
+            record) and int(record.biped_flags & self.unblock_flags)
 
-class ClothesTweak_Unblock(_AUnblockTweak, ClothesTweak):
     def tweak_record(self, record):
         record.biped_flags &= ~self.unblock_flags
 
@@ -154,8 +148,6 @@ class _AClothesTweaker(AMultiTweaker):
                     (u'0.5', 0.5),
                     (u'1.0', 1.0),
                     (_(u'Custom'), 0.0),),)
-    scanOrder = 31
-    editOrder = 31
 
 class TweakClothesPatcher(_AClothesTweaker,MultiTweaker):
     @classmethod

commit b7790ab8069a60aafb5276c0361f8372a6052474

Fix several minor regression and nits
Mopy/bash/game/oblivion/records.py:
Regression fix for regression fix

Broke in c966a4830270aef5280bf5f71613bfa992579f23.
This would cause errors if we dumped a new SNDX subrecord.

Mopy/bash/basher/gui_patchers.py:
Mopy/bash/game/fallout3/__init__.py:
Mopy/bash/game/oblivion/__init__.py:
Mopy/bash/game/skyrim/__init__.py:
Fix name of ImportFactions

Forgot to change this one to match its end-user-visible name.

Mopy/bash/brec/advanced_elements.py:
_MelDistributor._load_state to list

No idea why I made this a tuple, completely wrong data structure.

Mopy/bash/basher/__init__.py:
Followup for old compat removal

bash_version variable is unused now.

Mopy/bash/bosh/__init__.py:
Fix regression on unghost

Broke in 6cee9084fe1f4b4aeadd599294ea9f447f764ba1:

Traceback (most recent call last):
  File "bash\bosh\__init__.py", line 506, in setGhost
    self.__mark_unchanged()
AttributeError: 'ModInfo' object has no attribute '_ModInfo__mark_unchanged'

Mopy/bash/patcher/patchers/multitweak_assorted.py:
Noop for correctness

This way the correct functioning of these tweaks no longer depends on
tweak name. Right now they sort correctly because of their tweak names
(e.g. All Armor Playable < Armor Shows Amulets), but this would break if
they're ever changed - or translated into a language where this is no
longer the case!

Mopy/Docs/Wrye Bash Advanced Readme.html:
Mopy/Docs/Wrye Bash General Readme.html:
Mopy/Docs/Wrye Bash Technical Readme.html:
Mopy/Docs/Wrye Bash Version History.html:
Bump docs versions

Forgot to bump those after the 307 release.

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -7521,7 +7521,7 @@ msgstr "Möchten Sie das Profil %s und die %d saves, die es enthält, löschen?"
 </div>
 
 <div id="footer">
-	<div id="version">Wrye Bash v307</div>
+	<div id="version">Wrye Bash v308</div>
     <a href="Wrye Bash General Readme.html">General</a>&#160;&#x2726;&#160;Advanced&#160;&#x2726;&#160;<a href="Wrye Bash Technical Readme.html">Technical</a>&#160;&#x2726;&#160;<a href="Wrye Bash Version History.html">Versions</a>
     <div id="oldie">Javascript disabled, menu and slideshows nonfunctional.</div>
 </div>

Mopy/Docs/Wrye Bash General Readme.html
@@ -1350,7 +1350,7 @@ Respect developer's time - keep discussions there short, sweet and technical. No
   <li><img class="smallcred" alt="Daidalos' Icon" src="../bash/images/people/dlosavi32.png"><span><a href="https://github.com/D4id4los">Daidalos:</a> LOOT API updates, boot process refactoring</span></li>
   <li><img class="smallcred" alt="DianaNites' Icon" src="../bash/images/people/dianavi32.png"><span><a href="https://github.com/DianaNites">DianaNites:</a> Refactoring Export/Import links and argument parsing</span></li>
   <li><img class="smallcred" alt="Ethatron's Icon" src="../bash/images/people/ethaavi32.jpg"><span><a href="https://github.com/Ethatron">Ethatron:</a> CBash updates</span></li>
-  <li><img class="smallcred" alt="FelesNoctis' Icon" src="../bash/images/people/flsnavi32.jpg"><span><a href="https://github.com/FelesNoctis">FelesNoctis:</a> Documentation editor for v307+, image updates</span></li>
+  <li><img class="smallcred" alt="FelesNoctis' Icon" src="../bash/images/people/flsnavi32.jpg"><span><a href="https://github.com/FelesNoctis">FelesNoctis:</a> Documentation editor for 307+, image updates</span></li>
   <li><img class="smallcred" alt="Fireundubh's Icon" src="../bash/images/people/fireavi32.png"><span><a href="https://github.com/fireundubh">fireundubh:</a> Installer improvements and fixes</span></li>
   <li><img class="smallcred" alt="GandaG's Icon" src="../bash/images/people/gndaavi32.jpg"><span><a href="https://github.com/GandaG">GandaG:</a> FOMOD support, Refactoring, build script reworks</span></li>
   <li><img class="smallcred" alt="Haama's Icon" src="../bash/images/people/genravi32.png"><span><a href="https://github.com/Haama">Haama:</a> OBSE/Pluggy cosave definitions, various fixes &amp; improvements</span></li>
@@ -1557,7 +1557,7 @@ Respect developer's time - keep discussions there short, sweet and technical. No
 </div>
 
 <div id="footer">
-	<div id="version">Wrye Bash v307</div>
+	<div id="version">Wrye Bash v308</div>
     General&#160;&#x2726;&#160;<a href="Wrye Bash Advanced Readme.html">Advanced</a>&#160;&#x2726;&#160;<a href="Wrye Bash Technical Readme.html">Technical</a>&#160;&#x2726;&#160;<a href="Wrye Bash Version History.html">Versions</a>
     <div id="oldie">Javascript disabled, menu and slideshows nonfunctional.</div>
 </div>

Mopy/Docs/Wrye Bash Technical Readme.html
@@ -1025,7 +1025,7 @@ PatchPlugin &lt;NewFile&gt; &lt;FileToPatch&gt; [Create]</code>
 </div>
 
 <div id="footer">
-	<div id="version">Wrye Bash v307</div>
+	<div id="version">Wrye Bash v308</div>
 	<a href="Wrye Bash General Readme.html">General</a>&#160;&#x2726;&#160;<a href="Wrye Bash Advanced Readme.html">Advanced</a>&#160;&#x2726;&#160;Technical&#160;&#x2726;&#160;<a href="Wrye Bash Version History.html">Versions</a>
 	<div id="oldie">Javascript disabled, menu and slideshows nonfunctional.</div>
 </div>

Mopy/Docs/Wrye Bash Version History.html
@@ -4268,7 +4268,7 @@
 </div>
 
 <div id="footer">
-	<div id="version">Wrye Bash v307</div>
+	<div id="version">Wrye Bash v308</div>
 	<a href="Wrye Bash General Readme.html">General</a>&#160;&#x2726;&#160;<a href="Wrye Bash Advanced Readme.html">Advanced</a>&#160;&#x2726;&#160;<a href="Wrye Bash Technical Readme.html">Technical</a>&#160;&#x2726;&#160;Versions
 	<div id="oldie">Javascript disabled, menu and slideshows nonfunctional.</div>
 </div>

Mopy/bash/basher/__init__.py
@@ -4187,8 +4187,6 @@ class BashApp(wx.App):
     @staticmethod
     def InitVersion():
         """Perform any version to version conversion. Called by Init()."""
-        #--Renames dictionary: Strings to Paths.
-        bash_version = settings['bash.version']
         #--Current Version
         if settings['bash.version'] != bass.AppVersion:
             settings['bash.version'] = bass.AppVersion

Mopy/bash/basher/gui_patchers.py
@@ -1037,7 +1037,7 @@ class ImportCells(_ImporterPatcherPanel):
     patcher_type = preservers.ImportCellsPatcher
 
 # -----------------------------------------------------------------------------
-class ImportFactions(_ImporterPatcherPanel, _AListPanelCsv):
+class ImportActorsFactions(_ImporterPatcherPanel, _AListPanelCsv):
     """Import factions to creatures and NPCs."""
     patcher_name = _(u'Import Actors: Factions')
     patcher_desc = _(u'Import actor factions from source mods/files.')

Mopy/bash/bosh/__init__.py
@@ -182,7 +182,7 @@ class FileInfo(AFile):
         self._file_size, self._file_mod_time, self.ctime = stat_tuple
         if load_cache: self.readHeader()
 
-    def __mark_unchanged(self):
+    def _mark_unchanged(self):
         self._reset_cache(self._stat_tuple(), load_cache=False)
 
     ##: DEPRECATED-------------------------------------------------------------
@@ -503,7 +503,7 @@ class ModInfo(FileInfo):
             self.isGhost = isGhost
             # reset cache info as un/ghosting should not make do_update return
             # True
-            self.__mark_unchanged()
+            self._mark_unchanged()
             # Notify BAIN, as this is basically a rename operation
             modInfos._notify_bain(renamed={ghost_source: ghost_target})
         except:
@@ -1510,7 +1510,7 @@ class FileInfos(TableFileInfos):
         self[newName] = self[oldName]
         del self[oldName]
         self.table.moveRow(oldName,newName)
-        # self[newName].__mark_unchanged() # not needed with shellMove !
+        # self[newName]._mark_unchanged() # not needed with shellMove !
 
     #--Move
     def move_info(self, fileName, destDir):

Mopy/bash/brec/advanced_elements.py
@@ -183,7 +183,7 @@ class _MelDistributor(MelNull):
         return u'_loader_state', u'_seq_index'
 
     def setDefault(self, record):
-        record._loader_state = ()
+        record._loader_state = []
         record._seq_index = None
 
     def set_mel_set(self, mel_set):
@@ -227,13 +227,13 @@ class _MelDistributor(MelNull):
             # A simple scope - add the signature to the load state and
             # distribute the load by signature. That way we will descend
             # into this scope on the next loadData call.
-            record._loader_state += (signature,)
+            record._loader_state.append(signature)
             self._distribute_load(signature, record, ins, size_, readId)
         elif el_type == tuple:
             # Mixed Scopes ------------------------------------------------
             # A mixed scope - implement it like a simple scope, but
             # distribute the load by attribute name.
-            record._loader_state += (signature,)
+            record._loader_state.append(signature)
             self._distribute_load((signature, mapped_el[0]), record, ins,
                                   size_, readId)
         elif el_type == list:
@@ -241,7 +241,7 @@ class _MelDistributor(MelNull):
             # A sequence - add the signature to the load state, set the
             # sequence index to 1, and distribute the load to the element
             # specified by the first sequence entry.
-            record._loader_state += (signature,)
+            record._loader_state.append(signature)
             record._seq_index = 1 # we'll load the first element right now
             self._distribute_load(mapped_el[0], record, ins, size_,
                                   readId)
@@ -292,14 +292,14 @@ class _MelDistributor(MelNull):
                 # Calculate the new loader state - contains signatures for all
                 # remaining scopes we haven't backtracked through yet plus the
                 # one we just backtrackd into
-                record._loader_state = tuple([x[0] for x in descent_tracker] +
-                                             [prev_sig])
+                record._loader_state = [x[0] for x
+                                        in descent_tracker] + [prev_sig]
                 self._apply_mapping(prev_mapping[sub_type], record, ins,
                                     sub_type, size_, readId)
                 return
         # We didn't find anything during backtracking, so it must be in the top
         # scope. Wipe the loader state first and then apply the mapping.
-        record._loader_state = ()
+        record._loader_state = []
         self._apply_mapping(self.distributor_config[sub_type], record, ins,
                             sub_type, size_, readId)
 

Mopy/bash/game/fallout3/__init__.py
@@ -160,13 +160,14 @@ class Fallout3GameInfo(GameInfo):
     patchers = {
         u'AliasModNames', u'ContentsChecker', u'FormIDLists', u'ImportActors',
         u'ImportActorsAIPackages', u'ImportActorsAnimations',
-        u'ImportActorsDeathItems', u'ImportActorsFaces', u'ImportActorsSpells',
-        u'ImportCells', u'ImportDestructible', u'ImportEffectsStats',
-        u'ImportEnchantmentStats', u'ImportFactions', u'ImportGraphics',
-        u'ImportInventory', u'ImportNames', u'ImportObjectBounds',
-        u'ImportRelations', u'ImportScripts', u'ImportSounds',
-        u'ImportSpellStats', u'ImportStats', u'ImportText', u'LeveledLists',
-        u'MergePatches', u'TweakActors', u'TweakAssorted', u'TweakSettings',
+        u'ImportActorsDeathItems', u'ImportActorsFaces',
+        u'ImportActorsFactions', u'ImportActorsSpells', u'ImportCells',
+        u'ImportDestructible', u'ImportEffectsStats',
+        u'ImportEnchantmentStats', u'ImportGraphics', u'ImportInventory',
+        u'ImportNames', u'ImportObjectBounds', u'ImportRelations',
+        u'ImportScripts', u'ImportSounds', u'ImportSpellStats', u'ImportStats',
+        u'ImportText', u'LeveledLists', u'MergePatches', u'TweakActors',
+        u'TweakAssorted', u'TweakSettings',
     }
 
     weaponTypes = (

Mopy/bash/game/oblivion/__init__.py
@@ -176,8 +176,8 @@ class OblivionGameInfo(GameInfo):
         u'AliasModNames', u'CoblCatalogs', u'CoblExhaustion',
         u'ContentsChecker', u'ImportActors', u'ImportActorsAIPackages',
         u'ImportActorsAnimations', u'ImportActorsDeathItems',
-        u'ImportActorsFaces', u'ImportActorsSpells', u'ImportCells',
-        u'ImportEffectsStats', u'ImportEnchantmentStats', u'ImportFactions',
+        u'ImportActorsFaces', u'ImportActorsFactions', u'ImportActorsSpells',
+        u'ImportCells', u'ImportEffectsStats', u'ImportEnchantmentStats',
         u'ImportGraphics', u'ImportInventory', u'ImportNames',
         u'ImportRelations', u'ImportRoads', u'ImportScripts', u'ImportSounds',
         u'ImportSpellStats', u'ImportStats', u'ImportText', u'LeveledLists',

Mopy/bash/game/oblivion/records.py
@@ -1858,8 +1858,7 @@ class MreSoun(MelRecord):
         ),
         MelStruct(b'SNDX', u'2BbsH2sH2B', u'minDistance', u'maxDistance',
             u'freqAdjustment', (u'unused1', null1), (_flags, u'flags'),
-            (u'unused2', null2), (u'staticAtten', None), u'stopTime',
-            u'startTime'),
+            (u'unused2', null2), u'staticAtten', u'stopTime', u'startTime'),
     )
     __slots__ = melSet.getSlotsUsed()
 

Mopy/bash/game/skyrim/__init__.py
@@ -185,13 +185,13 @@ class SkyrimGameInfo(GameInfo):
     patchers = {
         u'AliasModNames', u'ContentsChecker', u'ImportActors',
         u'ImportActorsAIPackages', u'ImportActorsDeathItems',
-        u'ImportActorsSpells', u'ImportCells', u'ImportDestructible',
-        u'ImportEffectsStats', u'ImportEnchantmentStats', u'ImportFactions',
-        u'ImportGraphics', u'ImportInventory', u'ImportKeywords',
-        u'ImportNames', u'ImportObjectBounds', u'ImportOutfits',
-        u'ImportRelations', u'ImportSounds', u'ImportSpellStats',
-        u'ImportStats', u'ImportText', u'LeveledLists', u'MergePatches',
-        u'TweakActors', u'TweakAssorted', u'TweakSettings',
+        u'ImportActorsFactions', u'ImportActorsSpells', u'ImportCells',
+        u'ImportDestructible', u'ImportEffectsStats',
+        u'ImportEnchantmentStats', u'ImportGraphics', u'ImportInventory',
+        u'ImportKeywords', u'ImportNames', u'ImportObjectBounds',
+        u'ImportOutfits', u'ImportRelations', u'ImportSounds',
+        u'ImportSpellStats', u'ImportStats', u'ImportText', u'LeveledLists',
+        u'MergePatches', u'TweakActors', u'TweakAssorted', u'TweakSettings',
     }
 
     weaponTypes = (

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -143,6 +143,8 @@ _remaining_checks = re.compile(u'see.*me|mark(?!ynaz)')
 
 class _APlayableTweak(MultiTweakItem):
     """Shared code of 'armor/clothing playable' tweaks."""
+    tweak_order = 9 # Run before 'armor/clothing shows' tweaks
+
     @staticmethod
     def _any_body_flag_set(record):
         return any(getattr(record.biped_flags, bd_flag) for bd_flag