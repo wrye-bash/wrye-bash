wrye-bash/wrye-bash

commit 8e0fa26dd6e755308dac1ec1b8256b5d652db3fe

[!]WIP - inisettings drop GPaths - iniedits strings
Were always used as strings - added prefixing
- cext_ is unused but will come handy soon
- ! cause Mopy/bash/belt.py is difficult to see what's going on with
those ini_edits - WIP cause the inisettings need better handling,
we should probably be using a LowerDict all along.

Infernio:

We have no more `str`s in those

Mopy/bash/basher/installer_links.py
@@ -43,7 +43,7 @@ from .. import bass, bolt, bosh, bush, balt, archives
 from ..balt import EnabledLink, CheckLink, AppendableLink, OneItemLink, \
     UIList_Rename, UIList_Hide
 from ..belt import InstallerWizard, generateTweakLines
-from ..bolt import GPath, SubProgress, LogFile, round_size, text_wrap
+from ..bolt import GPath, SubProgress, LogFile, round_size, text_wrap, body_
 from ..exception import CancelError, SkipError, StateError
 from ..gui import BusyCursor
 
@@ -92,7 +92,7 @@ class _InstallerLink(Installers_Link, EnabledLink):
         if archive_path.cext in archives.noSolidExts:
             isSolid = False
         else:
-            if not u'-ms=' in bass.inisettings['7zExtraCompressionArguments']:
+            if not u'-ms=' in bass.inisettings[u'7zExtraCompressionArguments']:
                 isSolid = self._askYes(_(u'Use solid compression for %s?')
                                        % archive_path, default=False)
                 if isSolid:
@@ -300,7 +300,7 @@ class Installer_Wizard(_Installer_AWizardLink):
         new_targets = {}
         for iniFile, wizardEdits in ret.ini_edits.iteritems():
             outFile = bass.dirs[u'ini_tweaks'].join(u'%s - Wizard Tweak [%s].ini' %
-                (installer.archive, iniFile.sbody))
+                (installer.archive, body_(iniFile)))
             with outFile.open('w') as out:
                 out.write(u'\n'.join(generateTweakLines(wizardEdits, iniFile)))
                 out.write(u'\n')
@@ -316,7 +316,7 @@ class Installer_Wizard(_Installer_AWizardLink):
             else: # suppose that the target ini file is in the Data/ dir
                 target_path = bass.dirs[u'mods'].join(iniFile)
                 new_targets[target_path.stail] = target_path
-                if not (iniFile.s in installer.ci_dest_sizeCrc and
+                if not (iniFile in installer.ci_dest_sizeCrc and
                         ret.should_install):
                     # Can only automatically apply ini tweaks if the ini was
                     # actually installed.  Since BAIN is setup to not auto

Mopy/bash/bolt.py
@@ -964,6 +964,12 @@ def clearReadOnly(dirPath):
     cmd = u'' r'attrib -R "%s\*" /S /D' % dirPath
     subprocess.call(cmd, startupinfo=startupinfo)
 
+# TMP functions to deprecate Paths functionality for simple filenames - SLOW!
+def cext_(string_val):
+    return os.path.splitext(string_val)[-1].lower()
+def body_(string_val):
+    return os.path.basename(string_val)
+
 # Util Constants --------------------------------------------------------------
 #--Unix new lines
 reUnixNewLine = re.compile(u'' r'(?<!\r)\n', re.U)

Mopy/bash/bosh/__init__.py
@@ -3062,7 +3062,7 @@ class BSAInfos(FileInfos):
         if bush.game.displayName == u'Oblivion':
             # Need to do this at runtime since it depends on inisettings (ugh)
             bush.game.Bsa.redate_dict[inisettings[
-                u'OblivionTexturesBSAName'].s] = u'2005-01-01'
+                u'OblivionTexturesBSAName']] = u'2005-01-01'
         self.__class__.file_pattern = re.compile(
             re.escape(bush.game.Bsa.bsa_extension) + u'$', re.I | re.U)
         _bsa_type = bsa_files.get_bsa_type(bush.game.fsName)
@@ -3226,13 +3226,11 @@ class InstallerMarker(InstallerMarker): pass
 class InstallerProject(InstallerProject): pass
 
 # Initialization --------------------------------------------------------------
-
 def initDefaultTools():
     #-- Other tool directories
     #   First to default path
     pf = [GPath(u'C:\\Program Files'),GPath(u'C:\\Program Files (x86)')]
     def pathlist(*args): return [x.join(*args) for x in pf]
-
     # BOSS can be in any number of places.
     # Detect locally installed (into game folder) BOSS
     if dirs[u'app'].join(u'BOSS', u'BOSS.exe').exists():
@@ -3246,7 +3244,6 @@ def initDefaultTools():
             if path_in_registry.isdir():
                 path_in_registry = path_in_registry.join(u'BOSS.exe')
             tooldirs[u'boss'] = path_in_registry
-
     tooldirs[u'Tes4FilesPath'] = dirs[u'app'].join(u'Tools', u'TES4Files.exe')
     tooldirs[u'Tes4EditPath'] = dirs[u'app'].join(u'TES4Edit.exe')
     tooldirs[u'Tes5EditPath'] = dirs[u'app'].join(u'TES5Edit.exe')
@@ -3332,40 +3329,40 @@ def initDefaultTools():
 
 def initDefaultSettings():
     #other settings from the INI:
-    inisettings['ScriptFileExt'] = u'.txt'
-    inisettings['ResetBSATimestamps'] = True
-    inisettings['EnsurePatchExists'] = True
-    inisettings['OblivionTexturesBSAName'] = GPath(u'Oblivion - Textures - Compressed.bsa')
-    inisettings['ShowDevTools'] = False
-    inisettings['Tes4GeckoJavaArg'] = u'-Xmx1024m'
-    inisettings['OblivionBookCreatorJavaArg'] = u'-Xmx1024m'
-    inisettings['ShowTextureToolLaunchers'] = True
-    inisettings['ShowModelingToolLaunchers'] = True
-    inisettings['ShowAudioToolLaunchers'] = True
-    inisettings['7zExtraCompressionArguments'] = u''
-    inisettings['xEditCommandLineArguments'] = u''
-    inisettings['AutoItemCheck'] = True
-    inisettings['SkipHideConfirmation'] = False
-    inisettings['SkipResetTimeNotifications'] = False
-    inisettings['SoundSuccess'] = GPath(u'')
-    inisettings['SoundError'] = GPath(u'')
-    inisettings['EnableSplashScreen'] = True
-    inisettings['PromptActivateBashedPatch'] = True
-    inisettings['WarnTooManyFiles'] = True
-    inisettings['SkippedBashInstallersDirs'] = u''
+    inisettings[u'ScriptFileExt'] = u'.txt'
+    inisettings[u'ResetBSATimestamps'] = True
+    inisettings[u'EnsurePatchExists'] = True
+    inisettings[u'OblivionTexturesBSAName'] = u'Oblivion - Textures - Compressed.bsa'
+    inisettings[u'ShowDevTools'] = False
+    inisettings[u'Tes4GeckoJavaArg'] = u'-Xmx1024m'
+    inisettings[u'OblivionBookCreatorJavaArg'] = u'-Xmx1024m'
+    inisettings[u'ShowTextureToolLaunchers'] = True
+    inisettings[u'ShowModelingToolLaunchers'] = True
+    inisettings[u'ShowAudioToolLaunchers'] = True
+    inisettings[u'7zExtraCompressionArguments'] = u''
+    inisettings[u'xEditCommandLineArguments'] = u''
+    inisettings[u'AutoItemCheck'] = True
+    inisettings[u'SkipHideConfirmation'] = False
+    inisettings[u'SkipResetTimeNotifications'] = False
+    inisettings[u'SoundSuccess'] = u''
+    inisettings[u'SoundError'] = u''
+    inisettings[u'EnableSplashScreen'] = True
+    inisettings[u'PromptActivateBashedPatch'] = True
+    inisettings[u'WarnTooManyFiles'] = True
+    inisettings[u'SkippedBashInstallersDirs'] = u''
 
 def initOptions(bashIni):
     initDefaultTools()
     initDefaultSettings()
 
     defaultOptions = {}
-    type_key = {str:u's',unicode:u's',list:u's',int:u'i',bool:u'b',bolt.Path:u's'}
+    type_key = {unicode: u's', list: u's', int: u'i', bool: u'b',
+                bolt.Path: u's'} # tooldirs only
     allOptions = [tooldirs, inisettings]
     unknownSettings = {}
     for settingsDict in allOptions:
         for defaultKey,defaultValue in settingsDict.iteritems():
-            settingType = type(defaultValue)
-            readKey = type_key[settingType] + defaultKey
+            readKey = type_key[type(defaultValue)] + defaultKey
             defaultOptions[readKey.lower()] = (defaultKey,settingsDict)
 
     # if bash.ini exists update the settings from there:

Mopy/bash/basher/__init__.py
@@ -3899,7 +3899,7 @@ class BashFrame(WindowFrame):
     @balt.conversation
     def warnTooManyModsBsas(self):
         if bush.game.fsName != u'Oblivion': return
-        if not bass.inisettings['WarnTooManyFiles']: return
+        if not bass.inisettings[u'WarnTooManyFiles']: return
         if not len(bosh.bsaInfos): bosh.bsaInfos.refresh()
         if len(bosh.bsaInfos) + len(bosh.modInfos) >= 325 and not \
                 settings['bash.mods.autoGhost']:
@@ -4021,7 +4021,7 @@ class BashFrame(WindowFrame):
 
     def _warn_reset_load_order(self):
         if load_order.warn_locked and not bass.inisettings[
-            'SkipResetTimeNotifications']:
+            u'SkipResetTimeNotifications']:
             balt.showWarning(self, _(u"Load order has changed outside of Bash "
                 u"and has been reverted to the one saved in Bash. You can hit "
                 u"Ctrl + Z while the mods list has focus to undo this."),
@@ -4289,7 +4289,7 @@ class BashApp(wx.App):
         # screens/people/installers data are refreshed upon showing the panel
         #--Patch check
         if bush.game.Esp.canBash:
-            if not bosh.modInfos.bashed_patches and bass.inisettings['EnsurePatchExists']:
+            if not bosh.modInfos.bashed_patches and bass.inisettings[u'EnsurePatchExists']:
                 progress(0.68, _(u'Generating Blank Bashed Patch'))
                 try:
                     bosh.modInfos.generateNextBashedPatch(selected_mods=())

Mopy/bash/archives.py
@@ -132,7 +132,7 @@ def compressionSettings(archive_path, blockSize, isSolid):
                 solid = u'-ms=on'
         else:
             solid = u'-ms=off'
-    userArgs = bass.inisettings['7zExtraCompressionArguments']
+    userArgs = bass.inisettings[u'7zExtraCompressionArguments']
     if userArgs:
         if reSolid.search(userArgs):
             if not solid: # zip, will blow if ms=XXX is passed in

Mopy/bash/balt.py
@@ -2048,7 +2048,7 @@ class UIList_Hide(ItemLink):
 
     @conversation
     def Execute(self):
-        if not bass.inisettings['SkipHideConfirmation']:
+        if not bass.inisettings[u'SkipHideConfirmation']:
             message = _(u'Hide these files? Note that hidden files are simply '
                         u'moved to the %(hdir)s directory.') % (
                           {'hdir': self.window.data_store.hidden_dir})

Mopy/bash/basher/app_buttons.py
@@ -402,7 +402,7 @@ class App_Tes4View(_ExeButton):
         skip_bsas = bush.game.Xe.xe_key_prefix and bass.settings[
             bush.game.Xe.xe_key_prefix + u'.skip_bsas']
         extraArgs = bass.inisettings[
-            'xEditCommandLineArguments'].split() if is_expert else []
+            u'xEditCommandLineArguments'].split() if is_expert else []
         if is_expert:
             extraArgs.append(u'-IKnowWhatImDoing')
         if skip_bsas:

Mopy/bash/basher/gui_patchers.py
@@ -316,7 +316,7 @@ class _ListPatcherPanel(_PatcherPanel):
         """Set item to specified set of items."""
         items = self.items = self.sortConfig(items)
         forceItemCheck = self.forceItemCheck
-        defaultItemCheck = self.__class__.canAutoItemCheck and bass.inisettings['AutoItemCheck']
+        defaultItemCheck = self.__class__.canAutoItemCheck and bass.inisettings[u'AutoItemCheck']
         self.gList.lb_clear()
         isFirstLoad = self._GetIsFirstLoad()
         patcherOn = False

commit fe84f860126d813e793509338d5d3f7c3dc6d8b1

[!]Fixups wip:
- body_ was broken since 8e0fa26dd6e755308dac1ec1b8256b5d652db3fe,
only one (far off) use happily in Mopy/bash/basher/installer_links.py,
inlined (this should called very seldom so let's be just explicit here).
- .s pruning

Fix restore settings on boot:

Mopy/bash/initialization.py:
Yep we need the path to 7z - might want to revisit import order for the
trillionth time

Mopy/bash/bolt.py: added UnpicklingError (IIUC is needed[!])

Fix for File_Duplicate - would override existing files

Yep has happened to me - that's a monkey patch and the issue shows that
#241 stayed in the backburner long enough - probably after #460 though.

We should also add .ghost in the wildcard for mod infos - monkey patch
also in the sense we might want to allow the user to overwrite a file,
we just need this in some central place

Mopy/bash/basher/files_links.py
@@ -133,10 +133,14 @@ class File_Duplicate(ItemLink):
                     defaultFile=destName.s, wildcard=wildcard)
                 if not destPath: return
                 destDir, destName = destPath.headTail
-            if (destDir == fileInfo.dir) and (destName == to_duplicate):
-                self._showError(
-                    _(u'Files cannot be duplicated to themselves!'))
-                continue
+                if destDir == fileInfo.dir:
+                    if destName == to_duplicate:
+                        self._showError(
+                            _(u'Files cannot be duplicated to themselves!'))
+                        continue
+                    elif destName in fileInfos:
+                        self._showError(_(u'%s exists!') % destPath)
+                        continue
             fileInfos.copy_info(to_duplicate, destDir, destName)
             if fileInfo.isMod(): ##: move this inside copy_info
                 fileInfos.cached_lo_insert_after(to_duplicate, destName)

Mopy/bash/basher/installer_links.py
@@ -43,7 +43,7 @@ from .. import bass, bolt, bosh, bush, balt, archives
 from ..balt import EnabledLink, CheckLink, AppendableLink, OneItemLink, \
     UIList_Rename, UIList_Hide
 from ..belt import InstallerWizard, generateTweakLines
-from ..bolt import GPath, SubProgress, LogFile, round_size, text_wrap, body_
+from ..bolt import GPath, SubProgress, LogFile, round_size, text_wrap
 from ..exception import CancelError, SkipError, StateError
 from ..gui import BusyCursor
 
@@ -310,8 +310,9 @@ class Installer_Wizard(_Installer_AWizardLink):
         lastApplied = None
         new_targets = {}
         for iniFile, wizardEdits in ret.ini_edits.iteritems():
-            outFile = bass.dirs[u'ini_tweaks'].join(u'%s - Wizard Tweak [%s].ini' %
-                (installer.archive, body_(iniFile)))
+            basen = os.path.basename(os.path.splitext(iniFile)[0])
+            outFile = bass.dirs[u'ini_tweaks'].join(
+                u'%s - Wizard Tweak [%s].ini' % (installer.archive, basen))
             with outFile.open(u'w') as out:
                 out.write(u'\n'.join(generateTweakLines(wizardEdits, iniFile)))
                 out.write(u'\n')
@@ -817,7 +818,7 @@ class Installer_CopyConflicts(_SingleInstallable):
                     srcFull = ijoin(package, src)
                     destFull = ijoin(destDir, g_path, src)
                     if srcFull.exists():
-                        progress(curFile, self._selected_item.s + u'\n' + _(
+                        progress(curFile, u'%s\n' % self._selected_item + _(
                             u'Copying files...') + u'\n' + src)
                         srcFull.copyTo(destFull)
                         curFile += 1

Mopy/bash/basher/saves_links.py
@@ -927,7 +927,7 @@ class Save_UpdateNPCLevels(EnabledLink):
                 subProgress(index+0.5,_(u'Updating ') + saveName.s)
                 if releveledCount:
                     saveFile.safeSave()
-                message += u'\n%d %s' % (releveledCount,saveName.s)
+                message += u'\n%d %s' % (releveledCount,saveName)
         if modErrors:
             message += u'\n\n'+_(u'Some mods had load errors and were skipped:')+u'\n* '
             message += u'\n* '.join(modErrors)

Mopy/bash/bolt.py
@@ -968,7 +968,7 @@ def clearReadOnly(dirPath):
 def cext_(string_val):
     return os.path.splitext(string_val)[-1].lower()
 def body_(string_val):
-    return os.path.basename(string_val)
+    return os.path.basename(os.path.splitext(string_val)[0])
 
 # Util Constants --------------------------------------------------------------
 #--Unix new lines
@@ -1373,7 +1373,8 @@ class PickleDict(object):
                     else:
                         raise PickleDict.Mold(path)
                 return 1 + (path == self.backup)
-            except (OSError, IOError, EOFError, ValueError): #PY3:FileNotFound
+            except (OSError, IOError, EOFError, ValueError,
+                    pickle.UnpicklingError): #PY3:FileNotFound
                 pass
         else:
             if cor is not None:

Mopy/bash/bosh/__init__.py
@@ -3377,9 +3377,6 @@ def initBosh(bashIni, game_ini_path):
     initOptions(bashIni)
     from .bain import Installer
     Installer.init_bain_dirs()
-    if os.name == u'nt': # don't add local directory to binaries on linux
-        archives.exe7z = dirs[u'compiled'].join(archives.exe7z).s
-        archives.pngcrush = dirs[u'compiled'].join(archives.pngcrush).s
 
 def initSettings(readOnly=False, _dat=u'BashSettings.dat',
                  _bak=u'BashSettings.dat.bak'):

Mopy/bash/initialization.py
@@ -23,6 +23,7 @@
 """Functions for initializing Bash data structures on boot. For now exports
 functions to initialize bass.dirs that need be initialized high up into the
 boot sequence to be able to backup/restore settings."""
+import os
 from ConfigParser import ConfigParser, MissingSectionHeaderError
 # Local - don't import anything else
 from . import env
@@ -272,6 +273,10 @@ def init_dirs_mopy():
     dirs[u'db'] = dirs[u'bash'].join(u'db')
     dirs[u'templates'] = dirs[u'mopy'].join(u'templates')
     dirs[u'images'] = dirs[u'bash'].join(u'images')
+    from . import archives
+    if os.name == u'nt': # don't add local directory to binaries on linux
+        archives.exe7z = dirs[u'compiled'].join(archives.exe7z).s
+        archives.pngcrush = dirs[u'compiled'].join(archives.pngcrush).s
     global mopy_dirs_initialized
     mopy_dirs_initialized = True
 