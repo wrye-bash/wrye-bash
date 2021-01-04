wrye-bash/wrye-bash

commit 8a1531a0249a57de8cc70e4be8280591ad5b78c2

[!]BoolLink key -> _bl_key:
- key must be banished as an attribute and preferably variable name - use
for k, v in ...
- Mopy/bash/basher/mods_links.py - self._version_key TTT

\bkey\b(?!=) renames

Using negative look ahead to avoid sorted(...key= - so I added couple
formatting changes as in Mopy/bash/record_groups.py

Mopy/bash/basher/installers_links.py:

Removed bash.installers.autoApplyEmbeddedBCFs was unused -> maybe a
bug hence the !

Mopy/bash/basher/app_buttons.py
@@ -338,7 +338,7 @@ class _Mods_xEditExpert(BoolLink):
     key = bush.game.Xe.xe_key_prefix + u'.iKnowWhatImDoing'
 
 class _Mods_xEditSkipBSAs(BoolLink):
-    """Toggle xEdit expert mode (when launched via Bash)."""
+    """Toggle xEdit skip bsa mode (when launched via Bash)."""
     _text = _(u'Skip BSAs')
     _help = _(u'Skip loading BSAs when opening %s. Will disable some of its '
               u'functions.') % bush.game.Xe.full_name
@@ -414,15 +414,15 @@ class App_Tes4View(_ExeButton):
 class _Mods_BOSSDisableLockTimes(BoolLink):
     """Toggle Lock Load Order disabling when launching BOSS through Bash."""
     _text = _(u'BOSS Disable Lock Load Order')
-    key = 'BOSS.ClearLockTimes'
+    _bl_key = u'BOSS.ClearLockTimes'
     _help = _(u"If selected, will temporarily disable Bash's Lock Load Order "
               u'when running BOSS through Bash.')
 
 #------------------------------------------------------------------------------
 class _Mods_BOSSLaunchGUI(BoolLink):
     """If BOSS.exe is available then boss_gui.exe should be too."""
-    _text, key, _help = _(u'Launch using GUI'), 'BOSS.UseGUI', \
-                        _(u"If selected, Bash will run BOSS's GUI.")
+    _text, _bl_key, _help = _(u'Launch using GUI'), u'BOSS.UseGUI', \
+                            _(u"If selected, Bash will run BOSS's GUI.")
 
 class App_BOSS(_ExeButton):
     """loads BOSS"""

Mopy/bash/basher/installers_links.py
@@ -284,12 +284,12 @@ class Installers_UninstallAllUnknownFiles(Installers_Link):
 # Installers BoolLinks --------------------------------------------------------
 #------------------------------------------------------------------------------
 class Installers_AutoAnneal(BoolLink):
-    _text, key, _help = _(u'Auto-Anneal'), 'bash.installers.autoAnneal', _(
-        u"Enable/Disable automatic annealing of packages.")
+    _text, _bl_key = _(u'Auto-Anneal'), u'bash.installers.autoAnneal'
+    _help = _(u"Enable/Disable automatic annealing of packages.")
 
 class Installers_AutoWizard(BoolLink):
     _text = _(u'Auto-Anneal/Install Wizards')
-    key = 'bash.installers.autoWizard'
+    _bl_key = u'bash.installers.autoWizard'
     _help = _(u"Enable/Disable automatic installing or anneal (as applicable)"
              u" of packages after running its wizard.")
 
@@ -301,23 +301,22 @@ class _Installers_BoolLink_Refresh(BoolLink):
 class Installers_WizardOverlay(_Installers_BoolLink_Refresh):
     """Toggle using the wizard overlay icon"""
     _text  = _(u'Wizard Icon Overlay')
-    key = 'bash.installers.wizardOverlay'
+    _bl_key = u'bash.installers.wizardOverlay'
     _help =_(u"Enable/Disable the magic wand icon overlay for packages with"
             u" Wizards.")
 
 class Installers_AutoRefreshProjects(BoolLink):
     """Toggle autoRefreshProjects setting and update."""
     _text = _(u'Auto-Refresh Projects')
-    key = 'bash.installers.autoRefreshProjects'
+    _bl_key = u'bash.installers.autoRefreshProjects'
     _help = _(u'Toggles whether or not Wrye Bash will automatically detect '
               u'changes to projects in the installers directory.')
 
 class Installers_AutoApplyEmbeddedBCFs(ItemLink):
     """Automatically apply Embedded BCFs to archives that have one."""
     _text = _(u'Auto-Apply Embedded BCFs')
-    key = 'bash.installers.autoApplyEmbeddedBCFs'
-    _help = _(
-        u'Automatically apply Embedded BCFs to their containing archives.')
+    _help = _(u'Automatically apply Embedded BCFs to their containing '
+              u'archives.')
 
     @balt.conversation
     def Execute(self):
@@ -333,7 +332,7 @@ class Installers_AutoApplyEmbeddedBCFs(ItemLink):
 class Installers_AutoRefreshBethsoft(BoolLink, Installers_Link):
     """Toggle refreshVanilla setting and update."""
     _text = _(u'Skip Bethsoft Content')
-    key = 'bash.installers.autoRefreshBethsoft'
+    _bl_key = u'bash.installers.autoRefreshBethsoft'
     _help = _(u'Skip installing Bethesda ESMs, ESPs, and BSAs')
     opposite = True
     message = _(u"Enable installation of Bethsoft Content?") + u'\n\n' + _(
@@ -344,10 +343,10 @@ class Installers_AutoRefreshBethsoft(BoolLink, Installers_Link):
 
     @balt.conversation
     def Execute(self):
-        if not bass.settings[self.key] and not self._askYes(self.message):
+        if not bass.settings[self._bl_key] and not self._askYes(self.message):
             return
         super(Installers_AutoRefreshBethsoft, self).Execute()
-        if bass.settings[self.key]:
+        if bass.settings[self._bl_key]:
             # Refresh Data - only if we are now including Bethsoft files
             with balt.Progress(title=_(u'Refreshing Bethsoft Content'),
                                message=u'\n' + u' ' * 60) as progress:
@@ -361,7 +360,7 @@ class Installers_AutoRefreshBethsoft(BoolLink, Installers_Link):
 
 class Installers_Enabled(BoolLink):
     """Flips installer state."""
-    _text, key, _help = _(u'Enabled'), 'bash.installers.enabled', _(
+    _text, _bl_key, _help = _(u'Enabled'), u'bash.installers.enabled', _(
         u'Enable/Disable the Installers tab.')
     dialogTitle = _(u'Enable Installers')
     message = _(u"Do you want to enable Installers?") + u'\n\n\t' + _(
@@ -371,10 +370,10 @@ class Installers_Enabled(BoolLink):
     @balt.conversation
     def Execute(self):
         """Enable/Disable the installers tab."""
-        enabled = bass.settings[self.key]
+        enabled = bass.settings[self._bl_key]
         if not enabled and not self._askYes(self.message,
                                             title=self.dialogTitle): return
-        enabled = bass.settings[self.key] = not enabled
+        enabled = bass.settings[self._bl_key] = not enabled
         if enabled:
             self.window.panel.ShowPanel(scan_data_dir=True)
         else:
@@ -383,7 +382,7 @@ class Installers_Enabled(BoolLink):
 
 class Installers_BsaRedirection(AppendableLink, BoolLink, EnabledLink):
     """Toggle BSA Redirection."""
-    _text, key = _(u'BSA Redirection'), 'bash.bsaRedirection'
+    _text, _bl_key = _(u'BSA Redirection'), u'bash.bsaRedirection'
     _help = _(u"Use Quarn's BSA redirection technique.")
 
     @property
@@ -401,7 +400,7 @@ class Installers_BsaRedirection(AppendableLink, BoolLink, EnabledLink):
 
     def Execute(self):
         super(Installers_BsaRedirection, self).Execute()
-        if bass.settings[self.key]:
+        if bass.settings[self._bl_key]:
             # Delete ArchiveInvalidation.txt, if it exists
             bosh.bsaInfos.remove_invalidation_file()
             if bush.game.displayName == u'Oblivion':
@@ -414,41 +413,41 @@ class Installers_BsaRedirection(AppendableLink, BoolLink, EnabledLink):
                 with balt.Progress(_(u'Enabling BSA Redirection...'),
                                    message=u'\n' + u' ' * 60) as progress:
                     bsaFile.undo_alterations(progress)
-        bosh.oblivionIni.setBsaRedirection(bass.settings[self.key])
+        bosh.oblivionIni.setBsaRedirection(bass.settings[self._bl_key])
 
 class Installers_ConflictsReportShowsInactive(_Installers_BoolLink_Refresh):
     """Toggles option to show inactive on conflicts report."""
     _text = _(u'Show Inactive Conflicts')
     _help = _(u'In the conflicts tab also display conflicts with inactive '
               u'(not installed) installers')
-    key = 'bash.installers.conflictsReport.showInactive'
+    _bl_key = u'bash.installers.conflictsReport.showInactive'
 
 class Installers_ConflictsReportShowsLower(_Installers_BoolLink_Refresh):
     """Toggles option to show lower on conflicts report."""
     _text = _(u'Show Lower Conflicts')
     _help = _(u'In the conflicts tab also display conflicts with lower order '
              u'installers (or lower loading active bsas)')
-    key = 'bash.installers.conflictsReport.showLower'
+    _bl_key = u'bash.installers.conflictsReport.showLower'
 
 class Installers_ConflictsReportShowBSAConflicts(_Installers_BoolLink_Refresh):
     """Toggles option to show files inside BSAs on conflicts report."""
     _text = _(u'Show Active BSA Conflicts')
     _help = _(u'In the conflicts tab also display same-name resources inside '
              u'installed *and* active bsas')
-    key = 'bash.installers.conflictsReport.showBSAConflicts'
+    _bl_key = u'bash.installers.conflictsReport.showBSAConflicts'
 
 class Installers_AvoidOnStart(BoolLink):
     """Ensures faster bash startup by preventing Installers from being startup tab."""
-    _text, key, _help = _(u'Avoid at Startup'), 'bash.installers.fastStart', _(
-        u"Toggles Wrye Bash to avoid the Installers tab on startup,"
-        u" avoiding unnecessary data scanning.")
+    _text, _bl_key, = _(u'Avoid at Startup'), u'bash.installers.fastStart'
+    _help = _(u'Toggles Wrye Bash to avoid the Installers tab on startup, '
+              u'avoiding unnecessary data scanning.')
 
 class Installers_RemoveEmptyDirs(BoolLink):
     """Toggles option to remove empty directories on file scan."""
     _text = _(u'Remove Empty Directories')
     _help = _(u'Toggles whether or not Wrye Bash will remove empty '
               u'directories when scanning the Data folder.')
-    key = 'bash.installers.removeEmptyDirs'
+    _bl_key = u'bash.installers.removeEmptyDirs'
 
 # Sorting Links
 class _Installer_Sort(ItemLink):
@@ -458,18 +457,20 @@ class _Installer_Sort(ItemLink):
 
 class Installers_SortActive(_Installer_Sort, BoolLink):
     """Sort by type."""
-    _text, key, _help = _(u'Sort by Active'), 'bash.installers.sortActive', _(
-        u'If selected, active installers will be sorted to the top of the '
-        u'list.')
+    _text = _(u'Sort by Active')
+    _bl_key = u'bash.installers.sortActive'
+    _help = _(u'If selected, active installers will be sorted to the top of '
+              u'the list.')
 
 class Installers_SortProjects(_Installer_Sort, BoolLink):
     """Sort dirs to the top."""
-    _text, key, _help = _(u'Projects First'), 'bash.installers.sortProjects', \
-        _(u'If selected, projects will be sorted to the top of the list.')
+    _text = _(u'Projects First')
+    _bl_key = u'bash.installers.sortProjects'
+    _help = _(u'If selected, projects will be sorted to the top of the list.')
 
 class Installers_SortStructure(_Installer_Sort, BoolLink):
     """Sort by type."""
-    _text, key = _(u'Sort by Structure'), 'bash.installers.sortStructure'
+    _text, _bl_key = _(u'Sort by Structure'), u'bash.installers.sortStructure'
 
 #------------------------------------------------------------------------------
 # Installers_Skip Links -------------------------------------------------------
@@ -507,49 +508,49 @@ class _Installers_SkipOBSEPlugins(AppendableLink, _Installers_Skip):
     _se_sd = bush.game.Se.se_abbrev + (
             u'/' + bush.game.Sd.long_name) if bush.game.Sd.sd_abbrev else u''
     _text = _(u'Skip %s Plugins') % _se_sd
-    key = u'bash.installers.allowOBSEPlugins'
+    _bl_key = u'bash.installers.allowOBSEPlugins'
     def _append(self, window): return bool(self._se_sd)
-    def _check(self): return not bass.settings[self.key]
+    def _check(self): return not bass.settings[self._bl_key]
 
 class _Installers_SkipScreenshots(_Installers_Skip):
     """Toggle skipScreenshots setting and update."""
-    _text, key = _(u'Skip Screenshots'), 'bash.installers.skipScreenshots'
+    _text, _bl_key = _(u'Skip Screenshots'), u'bash.installers.skipScreenshots'
 
 class _Installers_SkipScriptSources(AppendableLink, _Installers_Skip):
     """Toggle skipScriptSources setting and update."""
-    _text, key = _(u'Skip Script Sources'), 'bash.installers.skipScriptSources'
+    _text, _bl_key = _(u'Skip Script Sources'), u'bash.installers.skipScriptSources'
     def _append(self, window): return bool(bush.game.Psc.source_extensions)
 
 class _Installers_SkipImages(_Installers_Skip):
     """Toggle skipImages setting and update."""
-    _text, key = _(u'Skip Images'), 'bash.installers.skipImages'
+    _text, _bl_key = _(u'Skip Images'), u'bash.installers.skipImages'
 
 class _Installers_SkipDocs(_Installers_Skip):
     """Toggle skipDocs setting and update."""
-    _text, key = _(u'Skip Docs'), u'bash.installers.skipDocs'
+    _text, _bl_key = _(u'Skip Docs'), u'bash.installers.skipDocs'
 
 class _Installers_SkipDistantLOD(_Installers_Skip):
     """Toggle skipDistantLOD setting and update."""
-    _text, key = _(u'Skip DistantLOD'), 'bash.installers.skipDistantLOD'
+    _text, _bl_key = _(u'Skip DistantLOD'), u'bash.installers.skipDistantLOD'
 
 class _Installers_SkipLandscapeLODMeshes(_Installers_Skip):
     """Toggle skipLandscapeLODMeshes setting and update."""
     _text = _(u'Skip LOD Meshes')
-    key = 'bash.installers.skipLandscapeLODMeshes'
+    _bl_key = u'bash.installers.skipLandscapeLODMeshes'
 
 class _Installers_SkipLandscapeLODTextures(_Installers_Skip):
     """Toggle skipLandscapeLODTextures setting and update."""
     _text = _(u'Skip LOD Textures')
-    key = 'bash.installers.skipLandscapeLODTextures'
+    _bl_key = u'bash.installers.skipLandscapeLODTextures'
 
 class _Installers_SkipLandscapeLODNormals(_Installers_Skip):
     """Toggle skipLandscapeLODNormals setting and update."""
     _text = _(u'Skip LOD Normals')
-    key = 'bash.installers.skipLandscapeLODNormals'
+    _bl_key = u'bash.installers.skipLandscapeLODNormals'
 
 class _Installers_SkipBsl(AppendableLink, _Installers_Skip):
     """Toggle skipTESVBsl setting and update."""
-    _text, key = _(u'Skip BSL Files'), 'bash.installers.skipTESVBsl'
+    _text, _bl_key = _(u'Skip BSL Files'), u'bash.installers.skipTESVBsl'
     def _append(self, window): return bush.game.Bsa.has_bsl
 
 class Installers_GlobalSkips(balt.MenuLink):
@@ -577,7 +578,7 @@ class _Installers_RenameStrings(AppendableLink, _Installers_RescanningLink):
     _text = _(u'Rename String Translation Files')
     _help = _(u'If checked, Wrye Bash will rename all installed string files '
               u'so they match your current language.')
-    key = 'bash.installers.renameStrings'
+    _bl_key = u'bash.installers.renameStrings'
     def _append(self, window): return bool(bush.game.Esp.stringsFiles)
 
 class _Installers_RedirectScriptSources(AppendableLink, EnabledLink,
@@ -588,7 +589,7 @@ class _Installers_RedirectScriptSources(AppendableLink, EnabledLink,
               u'installed to incorrect directories (%s) to the correct ones. '
               u"'Skip Script Sources' must be "
               u'off.') % u', '.join(bush.game.Psc.source_redirects)
-    key = u'bash.installers.redirect_scripts'
+    _bl_key = u'bash.installers.redirect_scripts'
 
     def _append(self, window): return bool(bush.game.Psc.source_redirects)
     def _enable(self): return not bass.settings[

Mopy/bash/basher/mod_links.py
@@ -602,7 +602,7 @@ class Mod_Details(OneItemLink):
                     buff.write(u'  '+_(u'(Details not provided for this record type.)')+u'\n\n')
                     continue
                 records = mod_details.group_records[group]
-                records.sort(key = lambda a: a[1].lower())
+                records.sort(key=lambda a: a[1].lower())
                 #if group != 'GMST': records.sort(key = lambda a: a[0] >> 24)
                 for fid,eid in records:
                     buff.write(u'  %08X %s\n' % (fid,eid))
@@ -1086,11 +1086,11 @@ class Mod_Patch_Update(_Mod_BP_Link):
               u"the patch."), checklists, bCancel=_(u'Skip')) as dialog:
             if not dialog.show_modal(): return
             deselect = set()
-            for (lst, key) in [(unfiltered, unfilteredKey),
+            for (lst, lst_key) in [(unfiltered, unfilteredKey),
                                (merge, mergeKey),
                                (noMerge, noMergeKey),
                                (deactivate, deactivateKey), ]:
-                deselect |= set(dialog.getChecked(key, lst))
+                deselect |= set(dialog.getChecked(lst_key, lst))
             if not deselect:
                 return
             else:

Mopy/bash/ScriptParser.py
@@ -543,8 +543,8 @@ class Parser(object):
 
         # If we have a keyword, just run it
         if self.tokens[0].type == KEYWORD:
-            key = self.tokens.pop(0)
-            key(*self.tokens)
+            kwrd = self.tokens.pop(0)
+            kwrd(*self.tokens)
         # It's just an expression, didnt start with a keyword
         else:
             # Convert to reverse-polish notation and execute

Mopy/bash/balt.py
@@ -2001,14 +2001,14 @@ class RadioLink(CheckLink):
 
 class BoolLink(CheckLink):
     """Simple link that just toggles a setting."""
-    _text, key, _help = u'LINK TEXT', 'link.key', u'' # Override text and key !
+    _text, _bl_key, _help = u'LINK TEXT', u'link.key', u'' # Override!
     opposite = False
 
     def _check(self):
         # check if not the same as self.opposite (so usually check if True)
-        return _settings[self.key] ^ self.__class__.opposite
+        return _settings[self._bl_key] ^ self.__class__.opposite
 
-    def Execute(self): _settings[self.key] ^= True # toggle
+    def Execute(self): _settings[self._bl_key] ^= True # toggle
 
 # UIList Links ----------------------------------------------------------------
 class UIList_Delete(ItemLink):

Mopy/bash/basher/ini_links.py
@@ -35,8 +35,8 @@ __all__ = ['INI_SortValid', 'INI_AllowNewLines', 'INI_ListINIs', 'INI_Apply',
 
 class INI_SortValid(BoolLink):
     """Sort valid INI Tweaks to the top."""
-    _text, key, _help = _(u'Valid Tweaks First'), 'bash.ini.sortValid', \
-                        _(u'Valid tweak files will be shown first.')
+    _text, _bl_key, _help = _(u'Valid Tweaks First'), u'bash.ini.sortValid', \
+                            _(u'Valid tweak files will be shown first.')
 
     def Execute(self):
         super(INI_SortValid, self).Execute()
@@ -46,7 +46,7 @@ class INI_SortValid(BoolLink):
 class INI_AllowNewLines(BoolLink):
     """Consider INI Tweaks with new lines valid."""
     _text = _(u'Allow Tweaks with New Settings')
-    key = 'bash.ini.allowNewLines'
+    _bl_key = u'bash.ini.allowNewLines'
     _help = _(u'Tweak files adding new sections/settings are considered valid')
 
     def Execute(self):

commit b065a46dc74bd6574ef0651cc817991434db9b9e

Prune 'key =' occurrences:
Also fixes a regression from 8a1531a0249a57de8cc70e4be8280591ad5b78c2:
_Mods_xEditExpert and _Mods_xEditSkipBSAs did not have their key
attribute changed to _bl_key.

Ut: changed the formatting a bit in Mopy/bash/env.py (run into conflicts
with prefixing) - avoid 3 lines when you can have two with a bit of non
PEP8 - but PEP8 function defs always - and calls when there is no lines
added

Mopy/bash/basher/app_buttons.py
@@ -335,14 +335,14 @@ class _Mods_xEditExpert(BoolLink):
     """Toggle xEdit expert mode (when launched via Bash)."""
     _text = _(u'Expert Mode')
     _help = _(u'Launch %s in expert mode.') % bush.game.Xe.full_name
-    key = bush.game.Xe.xe_key_prefix + u'.iKnowWhatImDoing'
+    _bl_key = bush.game.Xe.xe_key_prefix + u'.iKnowWhatImDoing'
 
 class _Mods_xEditSkipBSAs(BoolLink):
     """Toggle xEdit skip bsa mode (when launched via Bash)."""
     _text = _(u'Skip BSAs')
     _help = _(u'Skip loading BSAs when opening %s. Will disable some of its '
               u'functions.') % bush.game.Xe.full_name
-    key = bush.game.Xe.xe_key_prefix + u'.skip_bsas'
+    _bl_key = bush.game.Xe.xe_key_prefix + u'.skip_bsas'
 
 class App_Tes4View(_ExeButton):
     """Allow some extra args for Tes4View."""

Mopy/bash/basher/installers_links.py
@@ -393,8 +393,8 @@ class Installers_BsaRedirection(AppendableLink, BoolLink, EnabledLink):
         else: return self._help
 
     def _append(self, window):
-        section, key = bush.game.Ini.bsa_redirection_key
-        return bool(section) and bool(key)
+        br_section, br_key = bush.game.Ini.bsa_redirection_key
+        return bool(br_section) and bool(br_key)
 
     def _enable(self): return bosh.oblivionIni.abs_path.exists()
 

Mopy/bash/basher/mod_links.py
@@ -603,7 +603,7 @@ class Mod_Details(OneItemLink):
                     continue
                 records = mod_details.group_records[group]
                 records.sort(key=lambda a: a[1].lower())
-                #if group != 'GMST': records.sort(key = lambda a: a[0] >> 24)
+                #if group != 'GMST': records.sort(key=lambda a: a[0] >> 24)
                 for fid,eid in records:
                     buff.write(u'  %08X %s\n' % (fid,eid))
                 buff.write(u'\n')

Mopy/bash/basher/settings_dialog.py
@@ -380,9 +380,9 @@ class ColorsPage(_AFixedPage): ##: _AScrollablePage breaks the color picker??
                     split = line.split(u':')
                     if len(split) != 2:
                         continue
-                    key = split[0]
+                    color_key = split[0]
                     # Verify color exists
-                    if key not in colors:
+                    if color_key not in colors:
                         continue
                     # Parse the color, verify that it's actually valid
                     color_tup = tuple([int(c.strip()) for c
@@ -395,9 +395,10 @@ class ColorsPage(_AFixedPage): ##: _AScrollablePage breaks the color picker??
                     else:
                         # All checks passed, save it
                         color = Color(*color_tup)
-                        if color == colors[key] and key not in self.changes:
+                        if (color == colors[color_key] and
+                                color_key not in self.changes):
                             continue # skip, identical to our current state
-                        self.changes[key] = color
+                        self.changes[color_key] = color
         except Exception as e:
             balt.showError(self,
                 _(u'An error occurred reading from ') + inPath.stail +

Mopy/bash/bolt.py
@@ -1144,7 +1144,7 @@ class Flags(object):
     def getTrueAttrs(self):
         """Returns attributes that are true."""
         trueNames = [name for name in self._names if getattr(self,name)]
-        trueNames.sort(key = lambda xxx: self._names[xxx])
+        trueNames.sort(key=lambda xxx: self._names[xxx])
         return tuple(trueNames)
 
     def __repr__(self):
@@ -1260,13 +1260,14 @@ class MainFunctions(object):
         """Initialization."""
         self.funcs = {}
 
-    def add(self,func,key=None):
+    def add(self, func, func_key=None):
         """Add a callable object.
         func - A function or class instance.
-        key - Command line invocation for object (defaults to name of func).
+        func_key - Command line invocation for object (defaults to name of
+        func).
         """
-        key = key or func.__name__
-        self.funcs[key] = func
+        func_key = func_key or func.__name__
+        self.funcs[func_key] = func
         return func
 
     def main(self):
@@ -1274,10 +1275,10 @@ class MainFunctions(object):
         #--Get func
         args = sys.argv[1:]
         attrs = args.pop(0).split(u'.')
-        key = attrs.pop(0)
-        func = self.funcs.get(key)
+        func_key = attrs.pop(0)
+        func = self.funcs.get(func_key)
         if not func:
-            msg = _(u"Unknown function/object: %s") % key
+            msg = _(u'Unknown function/object: %s') % func_key
             try: print(msg)
             except UnicodeError: print(msg.encode('mbcs'))
             return

Mopy/bash/bosh/bain.py
@@ -2237,11 +2237,13 @@ class InstallersData(DataStore):
             # Ini tweaks and mods - those are keyed in data by rel path...
             relpath = apath.relpath(bass.dirs[u'mods'])
             # ghosts...
-            key = relpath.root.s if relpath.cs[-6:] == u'.ghost' else relpath.s
+            path_key = (relpath.root.s if relpath.cs[-6:] == u'.ghost'
+                        else relpath.s)
             if apath in deleted:
-                do_refresh |= bool(self.data_sizeCrcDate.pop(key, None))
+                do_refresh |= bool(self.data_sizeCrcDate.pop(path_key, None))
             else:
-                self.data_sizeCrcDate[key] = (apath.size,apath.crc,apath.mtime)
+                self.data_sizeCrcDate[path_key] = (apath.size, apath.crc,
+                                                   apath.mtime)
                 do_refresh = True
         return do_refresh # Some tracked files changed, update installers status
 

Mopy/bash/env.py
@@ -56,14 +56,14 @@ def get_registry_path(subkey, entry, detection_file):
     for hkey in (winreg.HKEY_LOCAL_MACHINE, winreg.HKEY_CURRENT_USER):
         for wow6432 in (u'', u'Wow6432Node\\'):
             try:
-                key = winreg.OpenKey(
+                reg_key = winreg.OpenKey(
                     hkey, u'Software\\%s%s' % (wow6432, subkey), 0,
                     winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
-                value = winreg.QueryValueEx(key, entry)
+                reg_val = winreg.QueryValueEx(reg_key, entry)
             except OSError:
                 continue
-            if value[1] != winreg.REG_SZ: continue
-            installPath = GPath(value[0])
+            if reg_val[1] != winreg.REG_SZ: continue
+            installPath = GPath(reg_val[0])
             if not installPath.exists(): continue
             exePath = installPath.join(detection_file)
             if not exePath.exists(): continue
@@ -94,11 +94,11 @@ except ImportError:
     envDefs = _os.environ
 
     def subEnv(match):
-        key = match.group(1).upper()
-        if not envDefs.get(key):
-            raise BoltError(u'Can\'t find user directories in windows registry'
-                    u'.\n>> See "If Bash Won\'t Start" in bash docs for help.')
-        return envDefs[key]
+        env_var = match.group(1).upper()
+        if not envDefs.get(env_var):
+            raise BoltError(u"Can't find user directories in windows registry."
+                u'\n>> See "If Bash Won\'t Start" in bash docs for help.')
+        return envDefs[env_var]
 
     def _getShellPath(folderKey): ##: mkdirs
         if not winreg:  # Linux HACK

Mopy/bash/ini_files.py
@@ -673,14 +673,14 @@ class GameIni(IniFile):
     def setBsaRedirection(self,doRedirect=True):
         """Activate or deactivate BSA redirection - game ini must exist!"""
         if self.isCorrupted: return
-        section, key = bush.game.Ini.bsa_redirection_key
-        if not section or not key: return
+        br_section, br_key = bush.game.Ini.bsa_redirection_key
+        if not br_section or not br_key: return
         aiBsa = dirs[u'mods'].join(u'ArchiveInvalidationInvalidated!.bsa')
         aiBsaMTime = time.mktime((2006, 1, 2, 0, 0, 0, 0, 2, 0))
         if aiBsa.exists() and aiBsa.mtime > aiBsaMTime:
             aiBsa.mtime = aiBsaMTime
         # check if BSA redirection is active
-        sArchives = self.getSetting(section, key, u'')
+        sArchives = self.getSetting(br_section, br_key, u'')
         is_bsa_redirection_active = any(x for x in sArchives.split(u',')
             if x.strip().lower() in self.bsaRedirectors)
         if doRedirect == is_bsa_redirection_active:
@@ -692,7 +692,7 @@ class GameIni(IniFile):
                 env.shellCopy(source, aiBsa, allowUndo=True, autoRename=True)
             except (env.AccessDeniedError, CancelError, SkipError):
                 return
-        sArchives = self.getSetting(section,key,u'')
+        sArchives = self.getSetting(br_section, br_key, u'')
         #--Strip existing redirectors out
         archives_ = [x.strip() for x in sArchives.split(u',') if
                      x.strip().lower() not in self.bsaRedirectors]