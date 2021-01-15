wrye-bash/wrye-bash

commit 91cd88df95c250c6b0e36de66d8bc4df20e81410

Cache ESL status
Bit of a nuclear solution :P
Makes check_active_limit very cheap though - down to 0.001s.

Maybe the invalidation is going to be too hard? At a higher level,
caching this does make sense to me though, with how often we need the
ESL status. Actually, maybe we should cache the ESM status too?

Be less pessimistic about ESL flags

All ESLs have the ESL flag, but very few ESLs have the .esl extension.
Not a big win by any means, just annoyed me when I noticed it in the
profiler.

Mopy/bash/bosh/__init__.py
@@ -349,6 +349,7 @@ reBashTags = re.compile(u'{{ *BASH *:[^}]*}}\\s*\\n?',re.U)
 
 class ModInfo(FileInfo):
     """A plugin file. Currently, these are .esp, .esm, .esl and .esu files."""
+    _is_esl = False # Cached, since we need it so often
 
     def __init__(self, fullpath, load_cache=False):
         self.isGhost = endsInGhost = (fullpath.cs[-6:] == u'.ghost')
@@ -362,6 +363,7 @@ class ModInfo(FileInfo):
         super(ModInfo, self)._reset_cache(stat_tuple, load_cache)
         # check if we have a cached crc for this file, use fresh mtime and size
         if load_cache: self.calculate_crc() # for added and hopefully updated
+        if bush.game.has_esl: self._recalc_esl()
 
     def getFileInfos(self): return modInfos
 
@@ -389,13 +391,17 @@ class ModInfo(FileInfo):
     def set_esl_flag(self, new_esl_flag):
         """Changes this file's ESL flag to the specified value."""
         self.header.flags1.eslFile = new_esl_flag
+        self._recalc_esl()
         self.writeHeader()
 
     def is_esl(self):
         """Check if this is a light plugin - .esl files are automatically
         set the light flag, for espms check the flag."""
-        return bush.game.has_esl and (self.get_extension() == u'.esl' or
-                                      self.has_esl_flag())
+        return self._is_esl
+
+    def _recalc_esl(self):
+        """Forcibly recalculates the cached ESL status."""
+        self._is_esl = self.has_esl_flag() or self.get_extension() == u'.esl'
 
     def isInvertedMod(self):
         """Extension indicates esp/esm, but byte setting indicates opposite."""

commit b953adcee5a27862d873ddd0c7dd56e4e818fbd1

Regression: Fix New Plugin
Broke in 91cd88df95c250c6b0e36de66d8bc4df20e81410 and
b925fc720df315c80884859293b0d1cbebbaf9a2.

docs: Rewrite Advanced Installation section

This way we don't have to keep updating it when we upgrade to a newer
dependency version.
Also reorganized it to separate out optional dependencies, and updated
it to add all our current dependencies.

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -180,18 +180,35 @@
 
 <h3 id="install-components">Python Components <a class="back2top" href="#contents">Back to top</a></h3>
 <p>If you are installing the Python version of Wrye Bash and wish to install the Python dependencies separately, the following must be installed.</p>
-<p>Installation is easiest by just using pip, which comes with Python (see below). Simply run <code>path/to/python.exe -m pip install -r requirements.txt</code>
-    (obviously replacing <code>path/to/python.exe</code> with the actual path to your python.exe).</p>
 <p class="emphasis">Note that the 64-bit versions are <em>required</em> - we no longer support 32-bit operating systems.</p>
+<p><a href="https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi">Python 2.7.18</a>: Versions prior to this one may work with Wrye Bash,
+    but have not been tested. Updating is recommended for bug and security fixes.</p>
+<p>Installation is easiest by just using pip, which comes with Python (see above). Simply run <code>path/to/python.exe -m pip install -r requirements.txt</code>
+    (obviously replacing <code>path/to/python.exe</code> with the actual path to your python.exe).</p>
+<p>If you want to do it manually, the rest of this section will list out each
+    dependency and what it's used for. Using the latest version of these is
+    recommended. If that one doesn't work / isn't available for Python 2, use
+    the newest version that does work.</p>
+<p>Required libaries:</p>
 <ul>
-    <li><a href="https://www.python.org/ftp/python/2.7.18/python-2.7.18.msi">Python 2.7.18</a>: Versions prior to 2.7.18 may work with Wrye Bash,
-        but have not been tested. Updating is recommended for bug and security fixes.</li>
-    <li><a href="https://pypi.org/project/wxPython">wxPython 4.0.7.post2</a>: Newer versions may work, older ones will not.</li>
-    <li><a href="https://pypi.org/project/chardet/">Mozilla's chardet library</a>: Wrye Bash uses this library to help in detecting
-        character encodings used when reading and writing encoded Unicode strings.</li>
-    <li><a href="https://pypi.org/project/pywin32/">PyWin32</a>: Wrye Bash uses this library to interact with Windows in various ways.</li>
-    <li><a href="https://pypi.python.org/pypi/scandir">scandir</a>: Optional but highly recommended module (in the standard library since
-        python 3.5). Scandir speeds up disk I/O operations (namely os. walk) by 5-50x on the Windows operating system family.</li>
+    <li><a href="https://pypi.org/project/chardet">chardet</a>: Used to detect
+        character encodings for reading and writing encoded Unicode
+        strings.</li>
+    <li><a href="https://pypi.org/project/lz4">python-lz4</a>: Used to read and
+        write LZ4 data in saves and BSAs.</li>
+    <li><a href="https://pypi.org/project/pywin32">pywin32</a>: Used to
+        interact with Windows in various ways.</li>
+    <li><a href="https://pypi.org/project/PyYAML">PyYAML</a>: Used to parse
+        YAML data, e.g. LOOT masterlists.</li>
+    <li><a href="https://pypi.org/project/wxPython">wxPython</a>: Used for
+        building Wrye Bash's GUI.</li>
+</ul>
+<p>Optional libaries:</p>
+<ul>
+    <li><a href="https://pypi.org/project/scandir">scandir</a>: Many refresh
+        and BAIN operations will become faster if this is installed.</li>
+    <li><a href="https://pypi.org/project/PyMuPDF">PyMuPDF</a>: The doc browser
+        will be able to display PDFs if this is installed.</li>
 </ul>
 
 <h2 id="bain">Installers Tab <a class="back2top" href="#contents">Back to top</a></h2>

Mopy/Docs/Wrye Bash General Readme.html
@@ -1358,7 +1358,7 @@ Respect developer's time - keep discussions there short, sweet and technical. No
   <li><img class="smallcred" alt="Leandor's Icon" src="../bash/images/people/lndravi32.jpg"><span><a href="https://github.com/leandor">Leandor:</a> CBash and CBashAPI updates</span></li>
   <li><img class="smallcred" alt="Lord Nyriox's Icon" src="../bash/images/people/lonyavi32.png"><span><a href="https://github.com/LordNyriox">Lord Nyriox:</a> Expanded Oblivion.esm swapping support</span></li>
   <li><img class="smallcred" alt="Lojack's Icon" src="../bash/images/people/lojkavi32.png"><span><a href="https://github.com/lojack5">Lojack:</a> Maintainer from 278-305: BAIN Wizards, Ini Edits tab, etc.</span></li>
-  <li><img class="smallcred" alt="Luna Nova's Icon" src="../bash/images/people/nalravi32.png"><span><a href="https://github.com/nallar">Luna Nova:</a> Initial Skyrim VR support.</span></li>
+  <li><img class="smallcred" alt="Luna Nova's Icon" src="../bash/images/people/nalravi32.png"><span><a href="https://github.com/TransLunarInjection">Luna Nova:</a> Initial Skyrim VR support.</span></li>
   <li><img class="smallcred" alt="MacSplody's Icon" src="../bash/images/people/mcspavi32.png"><span><a href="https://github.com/MacSplody">MacSplody:</a> Patcher refinement, updating LOOT integration, cell handling</span></li>
   <li><img class="smallcred" alt="Myktaylor's Icon" src="../bash/images/people/myk2avi32.png"><span><a href="https://github.com/myk002">myktaylor:</a> Project management, Asynchronous installer tab, etc</span></li>
   <li><img class="smallcred" alt="Nephatrine's Icon" src="../bash/images/people/nephavi32.png"></img><a href="https://github.com/nephatrine">nephatrine:</a> Initial Fallout 4 VR support.</li>

Mopy/bash/bosh/__init__.py
@@ -362,9 +362,10 @@ class ModInfo(FileInfo):
     def _reset_cache(self, stat_tuple, load_cache):
         super(ModInfo, self)._reset_cache(stat_tuple, load_cache)
         # check if we have a cached crc for this file, use fresh mtime and size
-        if load_cache: self.calculate_crc() # for added and hopefully updated
-        if bush.game.has_esl: self._recalc_esl()
-        self._recalc_esm()
+        if load_cache:
+            self.calculate_crc() # for added and hopefully updated
+            if bush.game.has_esl: self._recalc_esl()
+            self._recalc_esm()
 
     def getFileInfos(self): return modInfos
 