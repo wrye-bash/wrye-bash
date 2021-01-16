wrye-bash/wrye-bash

commit 8b0e5f32e320eaba4a68c88a5c9b64b88c288bf0

Get rid of the hideous 'custom float' hack
We can just do our own verification and show an error message if it's
not a valid float. Main problem is probably going to be getting people
who are already used to having to multiply all these input values by 10
to realize that this is no longer necessary. I included a message to
tell them as much, but who's going to read things...

Also some minor GUI improvements to the other 'custom value' popups.

Mopy/bash/basher/gui_patchers.py
@@ -669,32 +669,44 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
         """Handle choice menu selection."""
         tweak = self._all_tweaks[tweakIndex] # type: base.AMultiTweakItem
         value = []
+        new = None
         for i, v in enumerate(tweak.choiceValues[index]):
             if isinstance(v,float):
-                label = self._msg + u'\n' + tweak.tweak_key[i]
-                new = balt.askNumber(
-                    self.gConfigPanel, label, prompt=_(u'Value'),
-                    title=tweak.tweak_name + _(u' ~ Custom Tweak Value'),
-                    value=tweak.choiceValues[index][i], min=-10000, max=10000)
-                if new is None: #user hit cancel
-                    return
-                value.append(float(new)/10)
+                while new is None: # keep going until user entered valid float
+                    label = (_(u'Enter the desired custom tweak value.')
+                             + u'\n\n' +
+                             _(u'Note: A floating point number is expected '
+                               u'here.') + tweak.tweak_key[i])
+                    new = balt.askText(
+                        self.gConfigPanel, label,
+                        title=tweak.tweak_name + _(u' - Custom Tweak Value'),
+                        default=unicode(tweak.choiceValues[index][i]))
+                    if new is None: #user hit cancel
+                        return
+                    try:
+                        value.append(float(new))
+                    except ValueError:
+                        balt.showError(self.gConfigPanel,
+                                       _(u"'%s' is not a valid floating point "
+                                         u"number.") % new,
+                                       title=tweak.tweak_name + _(u' - Error'))
+                        new = None # invalid float, try again
             elif isinstance(v, int) or isinstance(v, long): # PY3: int only
-                label = _(u'Enter the desired custom tweak value.') + u'\n' + \
-                        tweak.tweak_key[i]
+                label = (_(u'Enter the desired custom tweak value.')
+                         + u'\n\n' + tweak.tweak_key[i])
                 new = balt.askNumber(
                     self.gConfigPanel, label, prompt=_(u'Value'),
-                    title=tweak.tweak_name + _(u' ~ Custom Tweak Value'),
+                    title=tweak.tweak_name + _(u' - Custom Tweak Value'),
                     value=tweak.choiceValues[index][i], min=-10000, max=10000)
                 if new is None: #user hit cancel
                     return
                 value.append(new)
             elif isinstance(v,basestring):
-                label = _(u'Enter the desired custom tweak text.') + u'\n' + \
-                        tweak.tweak_key[i]
+                label = (_(u'Enter the desired custom tweak text.')
+                         + u'\n\n' + tweak.tweak_key[i])
                 new = balt.askText(
                     self.gConfigPanel, label,
-                    title=tweak.tweak_name + _(u' ~ Custom Tweak Text'),
+                    title=tweak.tweak_name + _(u' - Custom Tweak Text'),
                     default=tweak.choiceValues[index][i], strip=False) ##: strip ?
                 if new is None: #user hit cancel
                     return

commit d894dca346b970773f594225e39d8124c78b5f05

Upgrade to chardet 4.0.0
Just noticed that it finally had a release. Shouldn't require any
changes. Claimed to be faster, my test BP was exactly as fast as before
(though that might be because my mods are almost entirely
ASCII-decodable).

Under #460

Mopy/bash/basher/installer_links.py:
Fix File > Open on unreognized Installers

We should allow opening these.

Mopy/bash/basher/gui_patchers.py:
Regression: Fix custom choice for multi-key tweaks

Most likely broke in 8b0e5f32e320eaba4a68c88a5c9b64b88c288bf0.

Mopy/bash/basher/gui_patchers.py
@@ -677,6 +677,8 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
                         return
                     try:
                         value.append(float(new))
+                        new = None # Reset, we may have a multi-key tweak
+                        break
                     except ValueError:
                         balt.showError(self.gConfigPanel,
                                        _(u"'%s' is not a valid floating point "

Mopy/bash/basher/installer_links.py
@@ -648,11 +648,15 @@ class Installer_Move(_InstallerLink):
         self.window.RefreshUI(
             detail_item=self.iPanel.detailsPanel.displayed_item)
 
-class Installer_Open(balt.UIList_OpenItems, _NoMarkerLink):
+class Installer_Open(balt.UIList_OpenItems, EnabledLink):
     """Open selected installer(s). Selected markers are skipped."""
 
+    def _enable(self):
+        # Can't use _NoMarkerLink since it will skip unrecognized packages
+        return not any(p.is_marker() for p in self.iselected_infos())
+
     def Execute(self):
-        self.window.OpenSelected(selected=self._installables)
+        self.window.OpenSelected(selected=self.selected)
 
 #------------------------------------------------------------------------------
 class _Installer_OpenAt(_InstallerLink):