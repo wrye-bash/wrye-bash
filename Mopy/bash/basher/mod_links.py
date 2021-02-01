wrye-bash/wrye-bash

commit 1330850b72eb12ff88b2ba9fbe7738706bad1c0b

POC: Attempt at a parser ABC
Already more performant and much less noisy than the previous
copy-pasted spaghetti mess. Still needs to absorb the methods for
reading/writing text.

Mopy/bash/parsers.py
@@ -22,22 +22,293 @@
 #
 # =============================================================================
 
-"""This module contains the parser classes used by the importer patcher classes
-and the Mod_Import/Export Mods menu."""
+"""Parsers can read and write information from and to mods and from and to CSV
+files. They store the read information in an internal representation, which
+means that they can be used to export and import information from and to mods.
+They are also used by some of the patchers in order to not duplicate the work
+that has to be done when reading mods.
+However, not all parsers fit this pattern - some have to read mods twice,
+others barely even fit into the pattern at all (e.g. FidReplacer)."""
+
 from __future__ import division, print_function
 import ctypes
 import re
 from collections import defaultdict, Counter
 from operator import attrgetter, itemgetter
 # Internal
-from . import bush # for game
+from . import bush, load_order
 from .balt import Progress
 from .bass import dirs, inisettings
 from .bolt import GPath, decode, deprint, CsvReader, csvFormat, floats_equal
 from .brec import MreRecord, MelObject, _coerce, genFid, RecHeader
 from .cint import ObCollection, FormID, aggregateTypes, validTypes, MGEFCode, \
     ActorValue, ValidateList, IUNICODE, getattr_deep, setattr_deep
+from .exception import AbstractError
 from .mod_files import ModFile, LoadFactory
+
+
+# TODO(inf) Once refactoring is done, we could easily take in Progress objects
+#  for more accurate progress bars when importing/exporting
+class _AParser(object):
+    """The base class from which all parsers inherit. Attempts to accomodate
+    all the different parsers.
+
+    Reading from mods:
+     - This is the most complex part of this design - we offer up to two
+       passes, where the first pass reads a mod and all its masters, but does
+       not offer fine-grained filtering of the read information. It is mapped
+       by long FormID and stored in id_context. You will have to set
+       _fp_types appropriately and override _read_record_fp to use this pass.
+     - The second pass filters by record type and long FormID, and can choose
+       whether or not it wants to store certain information. The result is
+       stored in id_stored_info. You will have to set _sp_types appropriately
+       and override _is_record_useful and _read_record_sp to use this pass.
+     - If you want to skip either pass, just leave _fp_types / _sp_types
+       empty."""
+
+    def __init__(self):
+        # The types of records to read from in the first pass. These should be
+        # strings matching the record types, *not* classes.
+        self._fp_types = ()
+        # Internal variable, keeps track of mods we've already processed during
+        # the first pass to avoid repeating work
+        self._fp_mods = set()
+        # True if id_context needs another round of processing during the
+        # second pass
+        self._context_needs_followup = False
+        # Do we need to sort the masters during the first pass according to
+        # current LO?
+        self._needs_fp_master_sort = False
+        # Maps long fids to context info read during first pass
+        self.id_context = {}
+        # The types of records to read from in the second pass. These should be
+        # strings matching the record types, *not* classes.
+        self._sp_types = ()
+        # Maps record types to dicts that map long fids to stored information
+        # May have been retrieved from mod in second pass, or from a CSV file
+        self.id_stored_info = defaultdict(dict)
+        # Automatically set to True when called by a patcher - can be used to
+        # alter behavior correspondingly
+        self.called_from_patcher = False
+        # Automatically set in _parse_sources to the patch file's aliases -
+        # used if the Aliases Patcher has been enabled
+        self.aliases = {}
+
+    # Plugin-related utilities
+    def _load_plugin(self, mod_info, target_types):
+        """Loads the specified record types in the specified ModInfo and
+        returns the result. Abstract because it may be implemented by either
+        PBash or CBash.
+
+        :param mod_info: The ModInfo object to read.
+        :param target_types: A list, set or tuple containing strings that shows
+            which record types to load.
+        :return: An object representing the loaded plugin."""
+        raise AbstractError(u'_load_plugin not implemented')
+
+    # Reading from plugin - first pass
+    def _read_plugin_fp(self, loaded_mod):
+        """Performs a first pass of reading on the specified plugin and its
+        masters. Results are stored in id_context.
+
+        :param loaded_mod: The loaded mod to read from."""
+        raise AbstractError(u'_read_plugin_fp not implemented')
+
+    # TODO(inf) Might need second_pass parameter?
+    def _read_record_fp(self, record):
+        """Performs the actual parser-specific first pass code on the specified
+        record. Treat this as a kind of lambda for a map call over all records
+        matching the _fp_types. If _context_needs_followup is true, this will
+        be called on every record again during the second pass.
+
+        :param record: The record to read.
+        :return: Whatever representation you want to convert this record
+            into."""
+        raise AbstractError(u'_read_record_fp not implemented')
+
+    # Reading from plugin - second pass
+    def _read_plugin_sp(self, loaded_mod):
+        """Performs a second pass of reading on the specified plugin, but not
+        its masters. Results are stored in id_stored_info.
+
+        :param loaded_mod: The loaded mod to read from."""
+        raise AbstractError(u'_read_plugin_sp not implemented')
+
+    def _is_record_useful(self, record):
+        """The parser should check if the specified record would be useful to
+        it during the second pass, i.e. if we should store information for it.
+
+        :param record: The record in question.
+        :return: True if information for the record should be stored."""
+        raise AbstractError(u'_is_record_useful not implemented')
+
+    def _read_record_sp(self, record):
+        """Performs the actual parser-specific second pass code on the
+        specified record. Treat this as a kind of lambda for a map call over
+        all records matching the _sp_types. Unless _get_cur_record_info is
+        overriden, this will also be used during writing to compare new and old
+        record information.
+
+        :param record: The record to read.
+        :return: Whatever representation you want to convert this record
+            into."""
+        raise AbstractError(u'_read_record_sp not implemented')
+
+    # Note the non-PEP8 names - those point to refactored pseudo-API methods
+    def readFromMod(self, mod_info):
+        """Asks this parser to read information from the specified ModInfo
+        instance. Executes the needed passes and stores extracted information
+        in id_context and / or id_stored_info. Note that this does not
+        automatically clear id_stored_info to allow combining multiple sources.
+
+        :param mod_info: The ModInfo instance to read from."""
+        # Check if we need to read at all
+        all_types = set(self._fp_types) | set(self._sp_types)
+        if not all_types: return
+        # Load mod_info once and for all, then execute every needed pass
+        loaded_mod = self._load_plugin(mod_info, all_types)
+        if self._fp_types:
+            self._read_plugin_fp(loaded_mod)
+        if self._sp_types:
+            self._read_plugin_sp(loaded_mod)
+
+    # Writing to plugins
+    def _do_write_plugin(self, loaded_mod):
+        """Writes the information stored in id_stored_info into the specified
+        plugin.
+
+        :param loaded_mod: The loaded mod to write to.
+        :return: A dict mapping record types to the number of changed records
+            in them."""
+        raise AbstractError(u'_do_write_plugin not implemented')
+
+    def _get_cur_record_info(self, record):
+        """Reads current information for the specified record in order to
+        compare it with the stored information to determine if we need to write
+        out. Falls back to the regular _read_record_sp method if it's
+        implemented, since most parsers will want to do the same thing here,
+        but you may want to override this e.g. if your parser can write, but
+        not read plugins.
+
+        :param record: The record to read.
+        :return: Whatever representation you want to convert this record
+            into."""
+        return self._read_record_sp(record)
+
+    def _should_write_record(self, new_info, cur_info):
+        """Checks if we should write out information for the current record,
+        based on the 'new' information (i.e. the info stored in id_stored_info)
+        and the 'current' information (i.e. the info stored in the record
+        itself). By default, this returns True if they are different. However,
+        you may want to override this if you e.g. only care about the contents
+        of a list and not its order.
+
+        :param new_info: The new record info.
+        :param cur_info: The current record info.
+        :return: True if _write_record should be called."""
+        return new_info != cur_info
+
+    def _write_record(self, record, new_info, cur_info):
+        """This is where your parser should perform the actual work of writing
+        out the necessary changes to the record, using the given record
+        information to determine what to change.
+
+        :param record: The record to write to.
+        :param new_info: The new record info.
+        :param cur_info: The current record info."""
+        raise AbstractError(u'_write_record not implemented')
+
+    def writeToMod(self, mod_info):
+        """Asks this parser to write its stored information to the specified
+        ModInfo instance.
+
+        :param mod_info: The ModInfo instance to write to.
+        :return: A dict mapping record types to the number of changed records
+            in them."""
+        return self._do_write_plugin(self._load_plugin(
+            mod_info, self.id_stored_info.keys()))
+
+# TODO(inf) Should this really be based on _AParser? Would object be better? If
+#  we do that, we will lose typing though...
+class _PBashParser(_AParser):
+    """Mixin for parsers that implements reading and writing mods using PBash
+    calls. You will of course still have to implement _read_record_fp et al.,
+    depending on the passes and behavior you want."""
+
+    def _load_plugin(self, mod_info, target_types):
+        mod_file = ModFile(mod_info, LoadFactory(
+            False, *[MreRecord.type_class[t] for t in target_types]))
+        mod_file.load(do_unpack=True)
+        mod_file.convertToLongFids(target_types)
+        return mod_file
+
+    def _read_plugin_fp(self, loaded_mod):
+        from . import bosh
+        def _fp_loop(mod_to_read):
+            """Central loop of _read_plugin_fp, factored out into a method so
+            that it can easily be used twice."""
+            for block_type in self._fp_types:
+                rec_block = mod_to_read.tops.get(block_type, None)
+                if not rec_block: continue
+                for record in rec_block.getActiveRecords():
+                    self.id_context[record.fid] = \
+                        self._read_record_fp(record)
+            self._fp_mods.add(mod_to_read.fileInfo.name)
+        # Process the mod's masters first, but see if we need to sort them
+        master_names = loaded_mod.tes4.masters
+        if self._needs_fp_master_sort:
+            master_names = load_order.get_ordered(master_names)
+        for mod_name in master_names:
+            if mod_name in self._fp_mods: continue
+            _fp_loop(self._load_plugin(bosh.modInfos[mod_name],
+                                       self._fp_types))
+        # Finally, process the mod itself
+        if loaded_mod.fileInfo.name in self._fp_mods: return
+        _fp_loop(loaded_mod)
+
+    def _read_plugin_sp(self, loaded_mod):
+        for rec_type in self._sp_types:
+            rec_block = loaded_mod.tops.get(rec_type, None)
+            if not rec_block: continue
+            for record in rec_block.getActiveRecords():
+                # Check if we even want this record first
+                if self._is_record_useful(record):
+                    rec_fid = record.fid
+                    self.id_stored_info[rec_type][rec_fid] = \
+                        self._read_record_sp(record)
+                    # Check if we need to follow up on the first pass info
+                    if self._context_needs_followup:
+                        self.id_context[rec_fid] = \
+                            self._read_record_fp(record)
+
+    def _do_write_plugin(self, loaded_mod):
+        # Counts the number of records that were changed in each record type
+        num_changed_records = Counter()
+        # We know that the loaded mod only has the tops loaded that we need
+        for rec_type, stored_rec_info in self.id_stored_info.iteritems():
+            rec_block = loaded_mod.tops.get(rec_type, None)
+            # Check if this record type makes any sense to patch
+            if not stored_rec_info or not rec_block: continue
+            # TODO(inf) Copied from implementations below, may have to be
+            #  getActiveRecords()?
+            for record in rec_block.records:
+                rec_fid = record.fid
+                if rec_fid not in stored_rec_info: continue
+                # Compare the stored information to the information currently
+                # in the plugin
+                new_info = stored_rec_info[rec_fid]
+                cur_info = self._get_cur_record_info(record)
+                if self._should_write_record(new_info, cur_info):
+                    # It's different, ask the parser to write it out
+                    self._write_record(record, new_info, cur_info)
+                    record.setChanged()
+                    num_changed_records[rec_type] += 1
+        # Check if we've actually changed something, otherwise skip saving
+        if sum(num_changed_records):
+            # Don't forget to convert back to short fids when writing!
+            loaded_mod.convertToShortFids()
+            loaded_mod.safeSave()
+        return num_changed_records
 
 class ActorFactions(object):
     """Factions for npcs and creatures with functions for

Mopy/bash/mod_files.py
@@ -344,29 +344,33 @@ class ModFile(object):
             return (_master_index(modName, object_id) << 24) | object_id
         return mapper
 
-    def convertToLongFids(self,types=None):
-        """Convert fids to long format (modname,objectindex).
-        :type types: list[str] | tuple[str] | set[str]
-        """
-        mapper = self.getLongMapper()
-        if types is None: types = self.tops.keys()
-        else: assert isinstance(types, (list, tuple, set))
-        selfTops = self.tops
-        for type in types:
-            if type in selfTops:
-                selfTops[type].convertFids(mapper,True)
-        #--Done
-        self.longFids = True
-
     ##: Ideally we'd encapsulate all the long/short fid handling in load/save
+    def _convert_fids(self, target_types, mapper, to_long):
+        """Convert fids using the target mapper and the specified format - long
+        FormIDs if to_long is True, short FormIDs otherwise."""
+        if target_types is None:
+            target_types = self.tops.keys()
+        else:
+            assert isinstance(target_types, (list, tuple, set))
+        for target_type in target_types:
+            if target_type in self.tops:
+                self.tops[target_type].convertFids(mapper, to_long)
+        self.longFids = to_long
+
+    def convertToLongFids(self, target_types=None):
+        """Convert fids to long format (mod name, object index).
+
+        :type target_types: list[str] | tuple[str] | set[str]"""
+        self._convert_fids(target_types, self.getLongMapper(), to_long=True)
+
     def convertToShortFids(self):
-        """Convert fids to short (numeric) format."""
-        mapper = self.getShortMapper()
-        selfTops = self.tops
-        for type in selfTops:
-            selfTops[type].convertFids(mapper,False)
-        #--Done
-        self.longFids = False
+        """Convert fids to short (numeric) format. Has no types parameter,
+        since this function is only needed when writing out - so the IO
+        operations would dwarf any performance improvement gained from the
+        types parameter.
+
+        :type target_types: list[str] | tuple[str] | set[str]"""
+        self._convert_fids(None, self.getShortMapper(), to_long=False)
 
     def getMastersUsed(self):
         """Updates set of master names according to masters actually used."""

Mopy/bash/patcher/patchers/preservers.py
@@ -127,7 +127,9 @@ class _APreserver(ImportPatcher):
     def _parse_csv_sources(self, progress):
         """Parses CSV files. Only called if _csv_parser is set. Override as
         needed and call _process_csv_sources until parser ABC is done."""
-        parser_instance = self._csv_parser(aliases=self.patchFile.aliases)
+        parser_instance = self._csv_parser()
+        parser_instance.aliases = self.patchFile.aliases
+        parser_instance.called_from_patcher = True
         for src_path in self.csv_srcs:
             try:
                 parser_instance.readFromText(getPatchesPath(src_path))

commit c4c67ff26bfa827eb1c194974d81fbb5fdb2385d

writeToMod chain fixups:
Was broken since 1330850b72eb12ff88b2ba9fbe7738706bad1c0b ? Note there
_load_plugin is called with keepAll False in

+    def writeToMod(self, mod_info):
...
+        return self._do_write_plugin(self._load_plugin(
+            mod_info, self.id_stored_info.keys()))

Of course real broken stuff is parsers ravioli which is addressed anew
leveraging 426db77ee71c939bb785b94eab7d4281f0f1fa26

Happened while testing - I had picked import factions to mod (they're
_AParser - Oblivion ofc):

Traceback (most recent call last):
  File "bash\balt.py", line 1827, in __Execute
    self.Execute()
  File "bash\basher\mod_links.py", line 1812, in Execute
    changed = self._import(ext, textDir, textName, textPath)
  File "bash\basher\mod_links.py", line 1774, in _import
    changed = parser.writeToMod(self._selected_info)
  File "bash\parsers.py", line 326, in writeToMod
    mod_info, self.id_stored_info))
  File "bash\parsers.py", line 279, in _do_write_plugin
    loaded_mod.safeSave()
  File "bash\mod_files.py", line 274, in safeSave
    self.save(filePath.temp)
  File "bash\mod_files.py", line 285, in save
    if not self.loadFactory.keepAll: raise StateError(u"Insufficient data to write file.")
bash.exception.StateError: Insufficient data to write file.

the fix is:

index ff834ecf3..22696c75d 100644
@@ -379,2 +379,2 @@ def writeToMod(self, mod_info):
-        return self._do_write_plugin(self._load_plugin(mod_info, keepAll=False,
-            target_types=self.id_stored_info))
+        return self._do_write_plugin(
+            self._load_plugin(mod_info, target_types=self.id_stored_info))

but this now blows with:

Traceback (most recent call last):
  File "bash\balt.py", line 1827, in __Execute
    self.Execute()
  File "bash\basher\mod_links.py", line 1811, in Execute
    changed = self._import(ext, textDir, textName, textPath)
  File "bash\basher\mod_links.py", line 1773, in _import
    changed = parser.writeToMod(self._selected_info)

Mopy/bash/basher/mod_links.py
@@ -1634,7 +1634,7 @@ class Mod_SetVersion(OneItemLink):
 # Import/Export submenus ------------------------------------------------------
 #------------------------------------------------------------------------------
 #--Import only
-from ..parsers import FidReplacer
+from ..parsers import FidReplacer, _AParser
 
 class Mod_Fids_Replace(OneItemLink):
     """Replace fids according to text file."""
@@ -1750,8 +1750,9 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
     noChange = _(u'No changes required.')
     supportedExts = {u'.csv'}
     progressTitle = continueInfo = continueKey = u'OVERRIDE'
+    _parser_class = _AParser
 
-    def _parser(self): raise AbstractError
+    def _parser(self): return self.__class__._parser_class()
     @property
     def _wildcard(self):
         if len(self.supportedExts) == 1: return u'*' + self.__class__.csvFile
@@ -1849,9 +1850,7 @@ class Mod_ActorLevels_Import(_Mod_Import_Link):
         u'file.') + u'\n\n' + _(u'See the Bash help file for more info.')
     continueKey = u'bash.actorLevels.import.continue'
     noChange = _(u'No relevant NPC levels to import.')
-
-    def _parser(self):
-        return ActorLevels()
+    _parser_class = ActorLevels
 
 #------------------------------------------------------------------------------
 from ..parsers import FactionRelations
@@ -1880,9 +1879,7 @@ class Mod_FactionRelations_Import(_Mod_Import_Link):
         u'See the Bash help file for more info.')
     continueKey = u'bash.factionRelations.import.continue'
     noChange = _(u'No relevant faction relations to import.')
-
-    def _parser(self):
-        return FactionRelations()
+    _parser_class = FactionRelations
 
 #------------------------------------------------------------------------------
 from ..parsers import ActorFactions
@@ -1910,9 +1907,7 @@ class Mod_Factions_Import(_Mod_Import_Link):
         u'file.') + u'\n\n' + _(u'See the Bash help file for more info.')
     continueKey = u'bash.factionRanks.import.continue'
     noChange = _(u'No relevant faction ranks to import.')
-
-    def _parser(self):
-        return ActorFactions()
+    _parser_class = ActorFactions
 
     def _log(self, changed, fileName):
         log_out = u'\n'.join(
@@ -1996,9 +1991,7 @@ class Mod_Scripts_Import(_Mod_Import_Link):
         u'scripts and is not reversible (except by restoring from backup)!')
     continueKey = u'bash.scripts.import.continue'
     progressTitle = _(u'Import Scripts')
-
-    def _parser(self):
-        return ScriptText()
+    _parser_class = ScriptText
 
     def Execute(self):
         if not self._askContinueImport(): return
@@ -2072,9 +2065,7 @@ class Mod_Stats_Import(_Mod_Import_Link):
                      u'existing stats and is not reversible!')
     continueKey = u'bash.stats.import.continue'
     noChange = _(u'No relevant stats to import.')
-
-    def _parser(self):
-        return ItemStats()
+    _parser_class = ItemStats
 
     def _log(self, changed, fileName):
         buff = io.StringIO()
@@ -2108,9 +2099,7 @@ class Mod_Prices_Import(_Mod_Import_Link):
     continueKey = u'bash.prices.import.continue'
     noChange = _(u'No relevant prices to import.')
     supportedExts = {u'.csv', u'.ghost'} | bush.game.espm_extensions
-
-    def _parser(self):
-        return ItemPrices()
+    _parser_class = ItemPrices
 
     def _log(self, changed, fileName):
         buff = io.StringIO()
@@ -2146,9 +2135,7 @@ class Mod_SigilStoneDetails_Import(_Mod_Import_Link):
         u'not reversible!')
     continueKey = u'bash.SigilStone.import.continue'
     noChange = _(u'No relevant Sigil Stone details to import.')
-
-    def _parser(self):
-        return SigilStoneDetails()
+    _parser_class = SigilStoneDetails
 
     def _log(self, changed, fileName):
         buff = io.StringIO()
@@ -2236,9 +2223,7 @@ class Mod_IngredientDetails_Import(_Mod_Import_Link):
                      u'form ids and is not reversible!')
     continueKey = u'bash.Ingredient.import.continue'
     noChange = _(u'No relevant Ingredient details to import.')
-
-    def _parser(self):
-        return IngredientDetails()
+    _parser_class = IngredientDetails
 
     def _log(self, changed, fileName):
         buff = io.StringIO()

Mopy/bash/brec/record_groups.py
@@ -151,8 +151,8 @@ class MobBase(object):
     def convertFids(self,mapper,toLong):
         """Converts fids between formats according to mapper.
         toLong should be True if converting to long format or False if
-        converting to short format."""
-        raise AbstractError(u'convertFids not implemented')
+        converting to short format. Base implementation does nothing as its
+        records are not unpacked."""
 
     def indexRecords(self):
         """Indexes records by fid."""

Mopy/bash/parsers.py
@@ -355,8 +355,8 @@ class _AParser(_HandleAliases):
         :param mod_info: The ModInfo instance to write to.
         :return: A dict mapping record types to the number of changed records
             in them."""
-        return self._do_write_plugin(self._load_plugin(mod_info, keepAll=False,
-            target_types=self.id_stored_info))
+        return self._do_write_plugin(
+            self._load_plugin(mod_info, target_types=self.id_stored_info))
 
     # Other API
     @property
@@ -404,7 +404,7 @@ class ActorFactions(_AParser):
             target_entry.unused1 = b'ODB'
 
     def _parse_line(self, csv_fields):
-        type_, _aed, amod, aobj, _fed, fmod, fobj, rank = csv_fields[:9] # FIXME: 8?
+        type_, _aed, amod, aobj, _fed, fmod, fobj, rank = csv_fields[:8]
         aid = self._coerce_fid(amod, aobj)
         fid = self._coerce_fid(fmod, fobj)
         rank = int(rank)
@@ -466,9 +466,7 @@ class ActorLevels(_HandleAliases):
     def writeToMod(self,modInfo):
         """Exports actor levels to specified mod."""
         mod_id_levels = self.mod_id_levels
-        loadFactory = self._load_factory()
-        modFile = ModFile(modInfo,loadFactory)
-        modFile.load(True)
+        modFile = self._load_plugin(modInfo)
         changed = 0
         id_levels = mod_id_levels.get(modInfo.name,
                                       mod_id_levels.get(GPath(u'Unknown'),
@@ -578,9 +576,7 @@ class EditorIds(_HandleAliases):
 
     def writeToMod(self,modInfo):
         """Exports eids to specified mod."""
-        loadFactory = LoadFactory(True, generic=self.types)
-        modFile = ModFile(modInfo,loadFactory)
-        modFile.load(True)
+        modFile = self._load_plugin(modInfo)
         changed = []
         for type_ in self.types:
             id_eid = self.type_id_eid.get(type_, None)