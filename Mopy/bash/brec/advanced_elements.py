wrye-bash/wrye-bash

commit b85f11e11a134b5e9b92497375ce3f10bbf4b143

Replace rest of strings with list in MelStruct init:
Removed _format_list - threw in some random wrapping/prefixing (this
was manual yes)

Mopy/bash/brec/common_subrecords.py
@@ -85,7 +85,7 @@ class MelCtda(MelUnion):
         u'do_or', u'use_aliases', u'use_global', u'use_packa_data',
         u'swap_subject_and_target'))
 
-    def __init__(self, ctda_sub_sig=b'CTDA', suffix_fmt=u'',
+    def __init__(self, ctda_sub_sig=b'CTDA', suffix_fmt=[],
                  suffix_elements=None, old_suffix_fmts=None):
         """Creates a new MelCtda instance with the specified properties.
 
@@ -125,22 +125,23 @@ class MelCtda(MelUnion):
         # The '4s' here can actually be a float or a FormID. We do *not* want
         # to handle this via MelUnion, because the deep nesting is going to
         # cause exponential growth and bring PBash down to a crawl.
-        prefix_fmt = u'B3s4sH2s' + (u'%s' * len(func_data[1:]))
+        prefix_fmt = [u'B', u'3s', u'4s', u'H', u'2s']
         prefix_elements = [(self._ctda_type_flags, u'operFlag'),
                            u'unused1', u'compValue',
                            u'ifunc', u'unused2']
         # Builds an argument tuple to use for formatting the struct format
         # string from above plus the suffix we got passed in
-        fmt_list = tuple([self._param_types[func_param]
-                          for func_param in func_data[1:]])
-        shared_params = ([ctda_sub_sig, (prefix_fmt + suffix_fmt) % fmt_list] +
+        fmt_list = [self._param_types[func_param] for func_param in
+                    func_data[1:]]
+        shared_params = ([ctda_sub_sig, (prefix_fmt + fmt_list + suffix_fmt)] +
                          self._build_params(func_data, prefix_elements,
                                             suffix_elements))
         # Only use MelTruncatedStruct if we have old versions, save the
         # overhead otherwise
         if old_suffix_fmts:
-            full_old_versions = {(prefix_fmt + f) % fmt_list
-                                 for f in old_suffix_fmts}
+            full_old_versions = {
+                u''.join(prefix_fmt + fmt_list + [f] if f else []) for f in
+                old_suffix_fmts}
             return MelTruncatedStruct(*shared_params,
                                       old_versions=full_old_versions)
         return MelStruct(*shared_params)
@@ -645,7 +646,7 @@ class MelOwnership(MelGroup):
 class MelDebrData(MelStruct):
     def __init__(self):
         # Format doesn't matter, struct.Struct(u'') works! ##: MelStructured
-        super(MelDebrData, self).__init__(b'DATA', u'', u'percentage',
+        super(MelDebrData, self).__init__(b'DATA', [], u'percentage',
             (u'modPath', null1), u'flags')
 
     @staticmethod

Mopy/bash/brec/basic_elements.py
@@ -604,25 +604,11 @@ class MelStrings(MelString):
 class MelStruct(MelBase):
     """Represents a structure record."""
 
-    @staticmethod
-    def _format_list(li):
-        digit = u''
-        ret = []
-        for c in li:
-            if c == '=': continue
-            if (str if type(c) is bytes else unicode).isdigit(c):
-                digit += '%s' % c
-            elif digit:
-                ret.append(digit + c)
-                digit = u''
-            else:
-                ret.append(u'%s' % c)
-        return ret
     def __init__(self, mel_sig, struct_formats, *elements):
         """:type mel_sig: bytes
         :type struct_formats: list[unicode]"""
-        if isinstance(struct_formats, basestring):
-            struct_formats = self._format_list(struct_formats)
+        if not isinstance(struct_formats, list):
+            raise SyntaxError(u'Expected a list got "%s"' % struct_formats)
         # Sometimes subrecords have to preserve non-aligned sizes, check that
         # we don't accidentally pad those to alignment
         struct_format = u''.join(struct_formats)

Mopy/bash/game/fallout3/records.py
@@ -114,7 +114,7 @@ class MelBipedData(MelStruct):
     ), unknown_is_unused=True)
 
     def __init__(self):
-        super(MelBipedData, self).__init__(b'BMDT', u'IB3s',
+        super(MelBipedData, self).__init__(b'BMDT', [u'I', u'B', u'3s'],
             (self._biped_flags, u'biped_flags'),
             (self._general_flags, u'generalFlags'), u'biped_unused')
 
@@ -124,7 +124,7 @@ class MelConditions(MelGroups):
     def __init__(self):
         # Note that reference can be a fid - handled in MelCtdaFo3.mapFids
         super(MelConditions, self).__init__(u'conditions',
-            MelCtdaFo3(suffix_fmt=u'2I',
+            MelCtdaFo3(suffix_fmt=[u'2I'],
                        suffix_elements=[u'runOn', u'reference'],
                        old_suffix_fmts={u'I', u''}))
 
@@ -1108,7 +1108,9 @@ class MreEfsh(MelRecord):
         MelIco2(u'particleTexture'),
         MelString(b'NAM7', u'holesTexture'),
         MelTruncatedStruct(b'DATA',
-            u'B3s3I3Bs9f3Bs8f5I19f3Bs3Bs3Bs11fI5f3Bsf2I6f', (_flags, u'flags'),
+            [u'B', u'3s', u'3I', u'3B', u's', u'9f', u'3B', u's', u'8f', u'5I',
+             u'19f', u'3B', u's', u'3B', u's', u'3B', u's', u'11f', u'I',
+             u'5f', u'3B', u's', u'f', u'2I', u'6f'], (_flags, u'flags'),
             u'unused1', (u'memSBlend', 5), (u'memBlendOp', 1),
             (u'memZFunc', 3), u'fillRed', u'fillGreen', u'fillBlue',
             u'unused2', u'fillAIn', u'fillAFull', u'fillAOut',
@@ -1344,7 +1346,7 @@ class MreIdlm(MelRecord):
         MelBounds(),
         MelUInt8Flags(b'IDLF', u'flags', _flags),
         MelPartialCounter(MelTruncatedStruct(
-            b'IDLC', 'B3s', 'animation_count', 'unused',
+            b'IDLC', [u'B', u'3s'], 'animation_count', 'unused',
             old_versions={'B'}),
             counter='animation_count', counts='animations'),
         MelFloat(b'IDLT', 'idleTimerSetting'),
@@ -1479,17 +1481,17 @@ class MreImgs(MelRecord):
         u'cinematicBrightnessValue', u'cinematicTintRed',
         u'cinematicTintGreen', u'cinematicTintBlue', u'cinematicTintValue',
     ]
-
+    _dnam_fmts = [u'33f', u'4s', u'4s', u'4s', u'4s']
     melSet = MelSet(
         MelEdid(),
         MelUnion({
             152: MelStruct(
-                b'DNAM', u'33f4s4s4s4sB3s', *(_dnam_common + [
+                b'DNAM', _dnam_fmts + [u'B', u'3s'], *(_dnam_common + [
                     u'unused1', u'unused2', u'unused3', u'unused4',
                     (_dnam_flags, u'dnam_flags'), u'unused5',
                 ])),
             148: MelStruct(
-                b'DNAM', u'33f4s4s4s4s', *(_dnam_common + [
+                b'DNAM', _dnam_fmts, *(_dnam_common + [
                     u'unused1', u'unused2',
                     u'unused3', u'unused4',
                 ])),
@@ -1590,7 +1592,7 @@ class MreIpds(MelRecord):
     melSet = MelSet(
         MelEdid(),
         MelTruncatedStruct(
-            b'DATA', '12I', (FID, u'stone'), (FID, u'dirt'),
+            b'DATA', [u'12I'], (FID, u'stone'), (FID, u'dirt'),
             (FID, u'grass'), (FID, u'glass'), (FID, u'metal'),
             (FID, u'wood'), (FID, u'organic'), (FID, u'cloth'),
             (FID, u'water'), (FID, u'hollowMetal'), (FID, u'organicBug'),
@@ -1776,8 +1778,9 @@ class MreMgef(MelRecord):
         MelDescription(u'text'),
         MelIcon(),
         MelModel(),
-        MelPartialCounter(MelStruct(
-            b'DATA', u'IfI2iH2sIf6I2fIi', (_flags, u'flags'), u'base_cost',
+        MelPartialCounter(MelStruct(b'DATA',
+            [u'I', u'f', u'I', u'2i', u'H', u'2s', u'I', u'f', u'6I', u'2f',
+             u'I', u'i'], (_flags, u'flags'), u'base_cost',
             (FID, u'associated_item'), u'school', u'resist_value',
             u'counter_effect_count', u'unused1',
             (FID, u'light'), u'projectileSpeed', (FID, u'effectShader'),
@@ -2002,8 +2005,8 @@ class MreNpc(MreActor):
         MelStrings(b'KFFZ','animations'),
         MelFid(b'CNAM','iclass'),
         MelUnion({
-            11: _MelNpcData(u'=I7B'),
-            25: _MelNpcData(u'=I21B')
+            11: _MelNpcData([u'I', u'7B']),
+            25: _MelNpcData([u'I', u'21B'])
         }, decider=SizeDecider()),
         MelFids(b'PNAM','headParts'),
         MelNpcDnam(b'DNAM', [u'14B', u'14B'], (u'skillValues', [0] * 14),
@@ -2474,7 +2477,7 @@ class MreRace(MelRecord):
         MelFid(b'ONAM','Older'),
         MelFid(b'YNAM','Younger'),
         MelBase(b'NAM2','_nam2',b''),
-        MelRaceVoices(b'VTCK', '2I', (FID, 'maleVoice'), (FID, 'femaleVoice')),
+        MelRaceVoices(b'VTCK', [u'2I'], (FID, 'maleVoice'), (FID, 'femaleVoice')),
         MelOptStruct(b'DNAM', [u'2I'],(FID, u'defaultHairMale'),(FID, u'defaultHairFemale')),
         # Int corresponding to GMST sHairColorNN
         MelStruct(b'CNAM', [u'2B'],'defaultHairColorMale','defaultHairColorFemale'),
@@ -2610,7 +2613,7 @@ class MreRefr(MelRecord):
         ##: I dropped special handling here, looks like a regular truncated
         # record to me - but no way to test since we don't load this yet
         MelTruncatedStruct(
-            b'XLOC', 'B3sI4sB3s4s', 'lockLevel', 'unused1',
+            b'XLOC', [u'B', u'3s', u'I', u'4s', u'B', u'3s', u'4s'], 'lockLevel', 'unused1',
             (FID, 'lockKey'), 'unused2', (_lockFlags, 'lockFlags'),
             'unused3', 'unused4', is_optional=True,
             old_versions={'B3sI4s'}),
@@ -2698,8 +2701,9 @@ class MreRegn(MelRecord):
             MelStruct(b'RDAT', [u'I', u'2B', u'2s'], 'entryType', (rdatFlags, 'flags'),
                       'priority', 'unused1'),
             MelRegnEntrySubrecord(2, MelArray('objects',
-                MelStruct(
-                    b'RDOT', 'IH2sf4B2H5f3H2s4s', (FID, 'objectId'),
+                MelStruct(b'RDOT',
+                    [u'I', u'H', u'2s', u'f', u'4B', u'2H', u'5f', u'3H',
+                     u'2s', u'4s'], (FID, 'objectId'),
                     'parentIndex', 'unk1', 'density', 'clustering',
                     'minSlope', 'maxSlope', (obflags, 'flags'),
                     'radiusWRTParent', 'radius', 'minHeight', 'maxHeight',
@@ -3034,7 +3038,7 @@ class MreWatr(MelRecord):
         ('shininess', 100), ('reflectHdrMult', 1), ('lightRadius', 10000),
         ('lightBrightness', 1), ('noiseLayer1UvScale', 100),
         ('noiseLayer2UvScale', 100), ('noiseLayer3UvScale', 100)]
-
+    _fmts = [u'10f', u'3B', u's', u'3B', u's', u'3B', u's', u'I',]
     melSet = MelSet(
         MelEdid(),
         MelFull(),
@@ -3044,8 +3048,8 @@ class MreWatr(MelRecord):
         MelString(b'MNAM','material'),
         MelFid(b'SNAM','sound',),
         MelFid(b'XNAM','effect'),
-        MelWatrData(b'DATA','10f3Bs3Bs3BsI32fH', *(_els + ['damage'])),
-        MelWatrDnam(b'DNAM', '10f3Bs3Bs3BsI35f', *(
+        MelWatrData(b'DATA', _fmts + [u'32f', u'H'], *(_els + ['damage'])),
+        MelWatrDnam(b'DNAM', _fmts + [u'35f'], *(
                 _els + ['noiseLayer1Amp', 'noiseLayer2Amp', 'noiseLayer3Amp']),
                     old_versions={'10f3Bs3Bs3BsI32f'}),
         MelFidList(b'GNAM','relatedWaters'),

Mopy/bash/game/falloutnv/records.py
@@ -392,15 +392,13 @@ class MreCell(MelRecord):
         MelSkipInterior(MelTruncatedStruct(b'XCLC', [u'2i', u'I'], (u'posX', None),
             (u'posY', None), (_land_flags, u'land_flags'), is_optional=True,
             old_versions={u'2i'})),
-        MelTruncatedStruct(b'XCLL', [u'3B', u's', u'3B', u's', u'3B', u's', u'2f', u'2i', u'3f'], 'ambientRed',
-                           'ambientGreen', 'ambientBlue', 'unused1',
-                           'directionalRed', 'directionalGreen',
-                           'directionalBlue', 'unused2', 'fogRed',
-                           'fogGreen', 'fogBlue', 'unused3',
-                           'fogNear', 'fogFar', 'directionalXY',
-                           'directionalZ', 'directionalFade', 'fogClip',
-                           'fogPower', is_optional=True,
-                           old_versions={'3Bs3Bs3Bs2f2i2f'}),
+        MelTruncatedStruct(b'XCLL',
+            [u'3B', u's', u'3B', u's', u'3B', u's', u'2f', u'2i', u'3f'], 'ambientRed',
+            'ambientGreen', 'ambientBlue', 'unused1', 'directionalRed',
+            'directionalGreen', 'directionalBlue', 'unused2', 'fogRed',
+            'fogGreen', 'fogBlue', 'unused3', 'fogNear', 'fogFar',
+            'directionalXY', 'directionalZ', 'directionalFade', 'fogClip',
+            'fogPower', is_optional=True, old_versions={u'3Bs3Bs3Bs2f2i2f'}),
         MelBase(b'IMPF','footstepMaterials'), #--todo rewrite specific class.
         MelFid(b'LTMP','lightTemplate'),
         MelUInt32Flags(b'LNAM', u'lightInheritFlags', inheritFlags),
@@ -1154,8 +1152,9 @@ class MreProj(MelRecord):
         MelFull(),
         MelModel(),
         MelDestructible(),
-        MelTruncatedStruct(
-            b'DATA', '2H3f2I3f2I3f3I4f', (_flags,'flags'), 'type',
+        MelTruncatedStruct(b'DATA',
+            [u'2H', u'3f', u'2I', u'3f', u'2I', u'3f', u'3I', u'4f'],
+            (_flags, 'flags'), 'type',
             'gravity', ('speed', 10000.0), ('range', 10000.0),
             (FID, u'light'), (FID, u'muzzleFlash'), 'tracerChance',
             'explosionAltTrigerProximity',
@@ -1263,7 +1262,7 @@ class MreRefr(MelRecord):
         ##: I dropped special handling here, looks like a regular truncated
         # record to me - but no way to test since we don't load this yet
         MelTruncatedStruct(
-            b'XLOC', 'B3sI4sB3s4s', 'lockLevel', 'unused1',
+            b'XLOC', [u'B', u'3s', u'I', u'4s', u'B', u'3s', u'4s'], 'lockLevel', 'unused1',
             (FID, 'lockKey'), 'unused2', (_lockFlags, 'lockFlags'),
             'unused3', 'unused4', is_optional=True,
             old_versions={'B3sI4s'}),
@@ -1352,8 +1351,8 @@ class MreRegn(MelRecord):
             MelStruct(b'RDAT', [u'I', u'2B', u'2s'], 'entryType', (rdatFlags, 'flags'),
                       'priority', 'unused1'),
             MelRegnEntrySubrecord(2, MelArray('objects',
-                MelStruct(
-                    b'RDOT', 'IH2sf4B2H5f3H2s4s', (FID, 'objectId'),
+                MelStruct(b'RDOT',
+                    [u'I', u'H', u'2s', u'f', u'4B', u'2H', u'5f', u'3H', u'2s', u'4s'], (FID, 'objectId'),
                     'parentIndex', 'unk1', 'density', 'clustering',
                     'minSlope', 'maxSlope', (obflags, 'flags'),
                     'radiusWRTParent', 'radius', 'minHeight', 'maxHeight',
@@ -1578,7 +1577,10 @@ class MreWeap(MelRecord):
         MelFids(b'WMS1','soundMod1Shoot3Ds'),
         MelFid(b'WMS2','soundMod1Shoot2D'),
         MelStruct(b'DATA', [u'2I', u'f', u'H', u'B'],'value','health','weight','damage','clipsize'),
-        MelTruncatedStruct(b'DNAM', [u'I', u'2f', u'4B', u'5f', u'I', u'4B', u'2f', u'2I', u'11f', u'i', u'I', u'2f', u'i', u'4f', u'3I', u'3f', u'2I', u's', u'B', u'2s', u'6f', u'I'],
+        MelTruncatedStruct(b'DNAM',
+                    [u'I', u'2f', u'4B', u'5f', u'I', u'4B', u'2f', u'2I',
+                     u'11f', u'i', u'I', u'2f', u'i', u'4f', u'3I', u'3f',
+                     u'2I', u's', u'B', u'2s', u'6f', u'I'],
                     'animationType','animationMultiplier','reach',
                     (_dflags1, u'dnamFlags1'),('gripAnimation',255),'ammoUse',
                     'reloadAnimation','minSpread','spread','weapDnam1','sightFov',
@@ -1599,14 +1601,14 @@ class MreWeap(MelRecord):
                     'regenRate','killImpulse','valueBMod1','valueBMod2','valueBMod3',
                     'impulseDist','skillReq',
                     old_versions={
-                        'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2s6f',
-                        'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2s5f',
-                        'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2sf',
-                        'I2f4B5fI4B2f2I11fiI2fi4f3I3f2I',
-                        'I2f4B5fI4B2f2I11fiI2fi4f3I3f',
-                        'I2f4B5fI4B2f2I11fiI2fi3f',
-                        'I2f4B5fI4B2f2I11fiI2fi',
-                        'I2f4B5fI4B2f2I11fiI2f',
+                        u'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2s6f',
+                        u'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2s5f',
+                        u'I2f4B5fI4B2f2I11fiI2fi4f3I3f2IsB2sf',
+                        u'I2f4B5fI4B2f2I11fiI2fi4f3I3f2I',
+                        u'I2f4B5fI4B2f2I11fiI2fi4f3I3f',
+                        u'I2f4B5fI4B2f2I11fiI2fi3f',
+                        u'I2f4B5fI4B2f2I11fiI2fi',
+                        u'I2f4B5fI4B2f2I11fiI2f',
                     }),
         MelOptStruct(b'CRDT', [u'H', u'2s', u'f', u'B', u'3s', u'I'],'criticalDamage','weapCrdt1',
                      'criticalMultiplier',(_cflags, u'criticalFlags'),
@@ -1634,7 +1636,8 @@ class MelWthrColorsFnv(MelArray):
 
     def __init__(self, wthr_sub_sig, wthr_attr):
         struct_definition = [
-            u'3Bs3Bs3Bs3Bs3Bs3Bs', u'riseRed', u'riseGreen', u'riseBlue',
+            [u'3B', u's', u'3B', u's', u'3B', u's', u'3B', u's', u'3B', u's',
+             u'3B', u's'], u'riseRed', u'riseGreen', u'riseBlue',
             u'unused1', u'dayRed', u'dayGreen', u'dayBlue',
             u'unused2', u'setRed', u'setGreen', u'setBlue',
             u'unused3', u'nightRed', u'nightGreen', u'nightBlue',

Mopy/bash/game/oblivion/records.py
@@ -71,10 +71,10 @@ class MelConditions(MelGroups):
     will be upgraded on dump."""
     def __init__(self):
         super(MelConditions, self).__init__(u'conditions', MelUnion({
-            b'CTDA': MelCtda(suffix_fmt=u'4s',
+            b'CTDA': MelCtda(suffix_fmt=[u'4s'],
                 suffix_elements=[u'unused3']),
             # The old (CTDT) format is length 20 and has no suffix
-            b'CTDT': MelReadOnly(MelCtda(b'CTDT', suffix_fmt=u'4s',
+            b'CTDT': MelReadOnly(MelCtda(b'CTDT', suffix_fmt=[u'4s'],
                 suffix_elements=[u'unused3'], old_suffix_fmts={u''})),
             }),
         )
@@ -146,7 +146,7 @@ class MelEffects(MelSequential):
                           u'magnitude', u'area', u'duration', u'recipient',
                           u'actorValue'),
                 MelGroup(u'scriptEffect',
-                    MelEffectsScit(b'SCIT', u'2I4sB3s', (FID, u'script'),
+                    MelEffectsScit(b'SCIT', [u'2I', u'4s', u'B', u'3s'], (FID, u'script'),
                         u'school', u'visual', (MelEffects._se_flags, u'flags'),
                         u'unused1', old_versions={u'2I4s', u'I'}),
                     MelFull(),
@@ -156,7 +156,7 @@ class MelEffects(MelSequential):
         # OBME Elements -------------------------------------------------------
         self._obme_elements = [
             MelGroups(u'effects',
-                MelObme(b'EFME', extra_format=u'2B',
+                MelObme(b'EFME', extra_format=[u'2B'],
                         extra_contents=[u'efit_param_info',
                                         u'efix_param_info'],
                         reserved_byte_count=10),
@@ -347,8 +347,8 @@ class MelObme(MelOptStruct):
     """Oblivion Magic Extender subrecord. Prefixed every attribute with obme_
     both for easy grouping in debugger views and to differentiate them from
     vanilla attrs."""
-    def __init__(self, struct_sig=b'OBME', extra_format=u'',
-            extra_contents=None, reserved_byte_count=28):
+    def __init__(self, struct_sig=b'OBME', extra_format=[],
+                 extra_contents=None, reserved_byte_count=28):
         """Initializes a MelObme instance. Supports customization for the
         variations that exist for effects subrecords and MGEF records."""
         # Always begins with record version and OBME version - None here is on
@@ -363,9 +363,8 @@ class MelObme(MelOptStruct):
         struct_contents += extra_contents
         # Always ends with a statically sized reserved byte array
         struct_contents += [(u'obme_unused', null1 * reserved_byte_count)]
-        super(MelObme, self).__init__(
-            struct_sig, u'4B{}{}s'.format(extra_format, reserved_byte_count),
-            *struct_contents)
+        str_fmts =[ u'4B'] + extra_format +  [u'%ds' % reserved_byte_count]
+        super(MelObme, self).__init__(struct_sig, str_fmts, *struct_contents)
 
 #------------------------------------------------------------------------------
 class MelOwnershipTes4(brec.MelOwnership):
@@ -864,8 +863,10 @@ class MreCsty(MelRecord):
 
     melSet = MelSet(
         MelEdid(),
-        MelTruncatedStruct(
-            b'CSTD', '2B2s8f2B2s3fB3s2f5B3s2f2B2s7fB3sfI', 'dodgeChance',
+        MelTruncatedStruct(b'CSTD',
+            [u'2B', u'2s', u'8f', u'2B', u'2s', u'3f', u'B', u'3s', u'2f',
+             u'5B', u'3s', u'2f', u'2B', u'2s', u'7f', u'B', u'3s', u'f',
+             u'I'], 'dodgeChance',
             'lrChance', 'unused1', 'lrTimerMin', 'lrTimerMax',
             'forTimerMin', 'forTimerMax', 'backTimerMin', 'backTimerMax',
             'idleTimerMin', 'idleTimerMax', 'blkChance', 'atkChance',
@@ -1288,7 +1289,7 @@ class MreMgef(MelRecord):
 
     melSet = MelSet(
         MelEdid(),
-        MelObme(extra_format=u'2B2s4sI4s', extra_contents=[
+        MelObme(extra_format=[u'2B', u'2s', u'4s', u'I', u'4s'], extra_contents=[
             u'obme_param_a_info', u'obme_param_b_info', u'obme_unused_mgef',
             u'obme_handler', (_obme_flag_overrides, u'obme_flag_overrides'),
             u'obme_param_b']),
@@ -1300,8 +1301,9 @@ class MreMgef(MelRecord):
         MelString(b'DESC', u'text'),
         MelIcon(),
         MelModel(),
-        MelPartialCounter(MelTruncatedStruct(
-            b'DATA', u'IfIiiH2sIf6I2f', (_flags, u'flags'), u'base_cost',
+        MelPartialCounter(MelTruncatedStruct(b'DATA',
+            [u'I', u'f', u'I', u'i', u'i', u'H', u'2s', u'I', u'f', u'6I',
+             u'2f'], (_flags, u'flags'), u'base_cost',
             (FID, u'associated_item'), u'school', u'resist_value',
             u'counter_effect_count', u'unused1',
             (FID, u'light'), u'projectileSpeed', (FID, u'effectShader'),
@@ -1652,7 +1654,8 @@ class MreRefr(MelRecord):
         MelOptStruct(b'XTEL', [u'I', u'6f'], (FID, u'destinationFid'),
             u'destinationPosX', u'destinationPosY', u'destinationPosZ',
             u'destinationRotX', u'destinationRotY', u'destinationRotZ'),
-        MelRefrXloc(b'XLOC', u'B3sI4sB3s', u'lockLevel', u'unused1',
+        MelRefrXloc(b'XLOC',
+            [u'B', u'3s', u'I', u'4s', u'B', u'3s'], u'lockLevel', u'unused1',
             (FID, u'lockKey'), u'unused2', (_lockFlags, u'lockFlags'),
             u'unused3', is_optional=True, old_versions={u'B3sIB3s'}),
         MelOwnershipTes4(),
@@ -1928,8 +1931,9 @@ class MreWatr(MelRecord):
         MelUInt8Flags(b'FNAM', u'flags', _flags),
         MelString(b'MNAM','material'),
         MelFid(b'SNAM','sound'),
-        MelWatrData(
-            b'DATA', '11f3Bs3Bs3BsB3s10fH', ('windVelocity', 0.100),
+        MelWatrData(b'DATA',
+            [u'11f', u'3B', u's', u'3B', u's', u'3B', u's', u'B', u'3s',
+             u'10f', u'H'], ('windVelocity', 0.100),
             ('windDirection', 90.0), ('waveAmp', 0.5), ('waveFreq', 1.0),
             ('sunPower', 50.0), ('reflectAmt', 0.5), ('fresnelAmt', 0.0250),
             'xSpeed', 'ySpeed', ('fogNear', 27852.8),

commit 24391cb3f441bb7eddd3dbd4845e32e3e095c332

Regression: Fix loading of older CTDA formats
Broke in b85f11e11a134b5e9b92497375ce3f10bbf4b143.

Mopy/bash/brec/mod_io.py:
Make that message actually useful

Right now it shows the positions in the file as sizes, which is really
not helpful for diagnosing why something is broken.

Mopy/bash/brec/advanced_elements.py
@@ -549,7 +549,7 @@ class ADecider(object):
         :param record: The record instance we're reading attributes from.
         :return: Any value this decider deems fitting for the parameters it is
             given."""
-        if self.__class__.can_decide_at_dump:
+        if self.can_decide_at_dump:
             raise exception.AbstractError()
 
 class ACommonDecider(ADecider):

Mopy/bash/brec/common_subrecords.py
@@ -140,7 +140,7 @@ class MelCtda(MelUnion):
         # overhead otherwise
         if old_suffix_fmts:
             full_old_versions = {
-                u''.join(prefix_fmt + fmt_list + [f] if f else []) for f in
+                u''.join(prefix_fmt + fmt_list + ([f] if f else [])) for f in
                 old_suffix_fmts}
             return MelTruncatedStruct(*shared_params,
                                       old_versions=full_old_versions)

Mopy/bash/brec/mod_io.py
@@ -270,7 +270,8 @@ class ModReader(object):
         """Read from file."""
         endPos = self.ins.tell() + size
         if endPos > self.size:
-            raise ModSizeError(self.inName, debug_strs, (endPos,), self.size)
+            target_size = size - (endPos - self.size)
+            raise ModSizeError(self.inName, debug_strs, (target_size,), size)
         return self.ins.read(size)
 
     def readLString(self, size, *debug_strs):