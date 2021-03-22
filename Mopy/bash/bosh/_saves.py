wrye-bash/wrye-bash

commit bb82f9b11802e8f8eb07b643aaed2339c9aeeddd

[!] SaveFileHeader.dump_header WIP:
One thing led to another - we need to centralize string handling and
_saves is on the way. Completely untested but note imports in _saves.py
We need properties for pcName and pcLocation actually
Note wrapping with encode - it would probably just try 'ascii' before
implicitly. Dumping headers is a rare operation plus optimizing needs
new data structures (PluginStr)
- added some prefixing

Infernio:

- [!] because Oblivion-only save code gets very little testing by users
  and this reworks a good chunk of it.
- Properly decodes the Oblivion save header (no more _unused)
- moves some more code into bolt.
We do need to come up with a nicer API for querying size
before dumping out the components - maybe time to make the
unpack/pack/size combo into a class for each data type?

    components = OrderedDict([
        (u'major_version',  BByte),
        (u'minor_version',  BByte),
        (u'exe_time',       BSystemTime),
        (u'header_version', BInt),
	(u'header_size',    BInt),
        (u'saveNum',        BInt),
        (u'pcName',         BStr8),
	# ...

'B' prefix and 'components' are just random things I came up with on
the spot to make it different from just plain 'Byte'/'fields'.

Worth thinking about at least :P

Mopy/bash/bosh/_saves.py:
Some minor cleanup and prefixing - setattr instead of __settattr__

Co-authored-by: Infernio <infernio@icloud.com>

Mopy/bash/bosh/_saves.py
@@ -27,11 +27,11 @@ coded for rest of the games."""
 from __future__ import division, print_function
 from collections import Counter, defaultdict
 from itertools import starmap, repeat
-
+from .save_headers import OblivionSaveHeader
 from .. import bolt, bush
-from ..bolt import Flags, sio, GPath, decoder, deprint, encode, cstrip, \
-    SubProgress, unpack_byte, unpack_str8, unpack_many, unpack_int, \
-    unpack_short, struct_pack, struct_unpack, pack_int, pack_short, pack_byte
+from ..bolt import Flags, sio, deprint, encode, SubProgress, unpack_many, \
+    unpack_int, unpack_short, struct_pack, struct_unpack, pack_int, \
+    pack_short, pack_byte
 from ..brec import ModReader, MreRecord, ModWriter, getObjectIndex, \
     getFormIndices, unpack_header
 from ..exception import ModError, StateError
@@ -59,11 +59,12 @@ class SreNPC(object):
         ))
 
     class ACBS(object):
-        __slots__ = ['flags','baseSpell','fatigue','barterGold','level','calcMin','calcMax']
+        __slots__ = (u'flags', u'baseSpell', u'fatigue', u'barterGold',
+                     u'level', u'calcMin', u'calcMax')
 
     def __init__(self, sre_flags=0, data=None):
         for attr in self.__slots__:
-            self.__setattr__(attr,None)
+            setattr(self, attr, None)
         if data: self.load(sre_flags, data)
 
     def getDefault(self,attr):
@@ -114,8 +115,8 @@ class SreNPC(object):
         """Returns current flags set."""
         sr_flags = SreNPC.sre_flags()
         for attr in SreNPC.__slots__:
-            if attr != 'unused2':
-                sr_flags.__setattr__(attr,self.__getattribute__(attr) is not None)
+            if attr != u'unused2':
+                setattr(sr_flags, attr, getattr(self, attr) is not None)
         return int(sr_flags)
 
     def getData(self):
@@ -230,9 +231,8 @@ class SaveFile(object):
         self.fileInfo = saveInfo
         self.canSave = canSave
         #--File Header, Save Game Header
-        self.header = None
+        self.header = None # type: OblivionSaveHeader| None
         self.gameHeader = None
-        self.pcName = None
         #--Masters
         self._masters = []
         #--Global
@@ -256,25 +256,15 @@ class SaveFile(object):
         """Extract info from save file."""
         # TODO: This is Oblivion only code.  Needs to be refactored
         import array
-        with self.fileInfo.getPath().open(u'rb') as ins:
+        with self.fileInfo.abs_path.open(u'rb') as ins:
             #--Progress
             progress = progress or bolt.Progress()
             progress.setFull(self.fileInfo.size)
             #--Header
             progress(0,_(u'Reading Header.'))
-            self.header = ins.read(34)
-
-            #--Save Header, pcName
-            gameHeaderSize = unpack_int(ins)
-            self.saveNum,pcNameSize = unpack_many(ins, '=IB')
-            self.pcName = decoder(cstrip(ins.read(pcNameSize)))
-            self.postNameHeader = ins.read(gameHeaderSize-5-pcNameSize)
-
-            #--Masters
             del self._masters[:]
-            numMasters = unpack_byte(ins)
-            for count in range(numMasters):
-                self._masters.append(GPath(decoder(unpack_str8(ins))))
+            self.header = OblivionSaveHeader(self.fileInfo.abs_path,
+                                             load_image=True, ins=ins)
             #--Pre-Records copy buffer
             def insCopy(buff,size,backSize=0):
                 if backSize: ins.seek(-backSize,1)
@@ -345,20 +335,7 @@ class SaveFile(object):
             progress.setFull(self.fileInfo.size)
             #--Header
             progress(0,_(u'Writing Header.'))
-            out.write(self.header)
-            #--Save Header
-            pcName = encode(self.pcName)
-            _pack('=IIB',5+len(pcName)+1+len(self.postNameHeader),
-                self.saveNum, len(pcName)+1)
-            out.write(pcName)
-            out.write('\x00')
-            out.write(self.postNameHeader)
-            #--Masters
-            _pack('B', len(self._masters))
-            for master in self._masters:
-                enc_name = encode(master.s)
-                _pack('B', len(enc_name))
-                out.write(enc_name)
+            self.header.dump_header(out)
             #--Fids Pointer, num records
             fidsPointerPos = out.tell()
             _pack('I',0) #--Temp. Will write real value later.
@@ -604,12 +581,11 @@ class SaveFile(object):
         progress.setFull(len(self.created)+len(self.records))
         #--Created objects
         progress(0,_(u'Scanning created objects'))
-        fullAttr = 'full'
         for citem in self.created:
-            if fullAttr in citem.__class__.__slots__:
-                full = citem.__getattribute__(fullAttr)
+            if u'full' in citem.__class__.__slots__:
+                full = citem.full
             else:
-                full = citem.getSubString('FULL')
+                full = citem.getSubString(b'FULL')
             if full:
                 createdCounts[(citem.recType, full)] += 1
             progress.plus()
@@ -640,10 +616,10 @@ class SaveFile(object):
             progress(0,_(u'Scanning created objects'))
             kept = []
             for citem in self.created:
-                if 'full' in citem.__class__.__slots__:
-                    full = citem.__getattribute__('full')
+                if u'full' in citem.__class__.__slots__:
+                    full = citem.full
                 else:
-                    full = citem.getSubString('FULL')
+                    full = citem.getSubString(b'FULL')
                 if full and (citem.recType,full) in uncreateKeys:
                     uncreated.add(citem.fid)
                     numUncreated += 1
@@ -833,6 +809,6 @@ class Save_NPCEdits(object):
         (rec_id,rec_kind,recFlags,version,data) = saveFile.getRecord(7)
         npc = SreNPC(recFlags,data)
         npc.full = encode(newName)
-        saveFile.pcName = newName
+        saveFile.header.pcName = newName
         saveFile.setRecord(npc.getTuple(rec_id,version))
         saveFile.safeSave()

Mopy/bash/bolt.py
@@ -1486,8 +1486,6 @@ class Settings(DataDict):
         return self.data.pop(key,default)
 
 # Structure wrappers ----------------------------------------------------------
-def unpack_str8(ins, __unpack=struct.Struct(u'B').unpack):
-    return ins.read(__unpack(ins.read(1))[0])
 def unpack_str16(ins, __unpack=struct.Struct(u'H').unpack):
     return ins.read(__unpack(ins.read(2))[0])
 def unpack_str32(ins, __unpack=struct.Struct(u'I').unpack):
@@ -1506,11 +1504,11 @@ def pack_float(out, val, __pack=struct.Struct(u'=f').pack):
     out.write(__pack(val))
 def unpack_double(ins, __unpack=struct.Struct(u'd').unpack):
     return __unpack(ins.read(8))[0]
+def pack_double(out, val, __pack=struct.Struct(u'=d').pack):
+    out.write(__pack(val))
 def unpack_byte(ins, __unpack=struct.Struct(u'B').unpack):
     return __unpack(ins.read(1))[0]
 def pack_byte(out, val, __pack=struct.Struct(u'=B').pack):
-    out.write(__pack(val))
-def pack_double(out, val, __pack=struct.Struct(u'=d').pack):
     out.write(__pack(val))
 def unpack_int_signed(ins, __unpack=struct.Struct(u'i').unpack):
     return __unpack(ins.read(4))[0]
@@ -1528,9 +1526,19 @@ def unpack_str_int_delim(ins, __unpack=struct.Struct(u'Ic').unpack):
     return __unpack(ins.read(5))[0]
 def unpack_str_byte_delim(ins, __unpack=struct.Struct(u'Bc').unpack):
     return __unpack(ins.read(2))[0]
-
+def unpack_str8(ins, __unpack=struct.Struct(u'B').unpack):
+    return ins.read(__unpack(ins.read(1))[0])
+def pack_str8(out, val, __pack=struct.Struct(u'=B').pack):
+    pack_byte(out, len(val))
+    out.write(val)
+def pack_bzstr8(out, val, __pack=struct.Struct(u'=B').pack):
+    pack_byte(out, len(val) + 1)
+    out.write(val)
+    out.write(b'\x00')
 def unpack_string(ins, string_len):
     return struct_unpack(u'%ds' % string_len, ins.read(string_len))[0]
+def pack_string(out, val):
+    out.write(val)
 
 def pack_byte_signed(out, value, __pack=struct.Struct(u'b').pack):
     out.write(__pack(value))

Mopy/bash/bosh/save_headers.py
@@ -41,15 +41,17 @@ from .. import bolt
 from ..bolt import decoder, cstrip, unpack_string, unpack_int, unpack_str8, \
     unpack_short, unpack_float, unpack_str16, unpack_byte, struct_pack, \
     unpack_str_int_delim, unpack_str16_delim_null, unpack_str_byte_delim, \
-    unpack_many, encode, struct_unpack, pack_int, pack_byte, pack_short
-from ..exception import SaveHeaderError, raise_bolt_error
+    unpack_many, encode, struct_unpack, pack_int, pack_byte, pack_short, \
+    pack_float, pack_string, pack_str8, pack_bzstr8
+from ..exception import SaveHeaderError, raise_bolt_error, AbstractError
 
 # Utilities -------------------------------------------------------------------
 def _pack_c(out, value, __pack=struct.Struct(u'=c').pack):
     out.write(__pack(value))
+unpack_fstr16 = partial(unpack_string, string_len=16)
 
 class SaveFileHeader(object):
-    save_magic = 'OVERRIDE'
+    save_magic = b'OVERRIDE'
     # common slots Bash code expects from SaveHeader (added header_size and
     # turned image to a property)
     __slots__ = (u'header_size', u'pcName', u'pcLevel', u'pcLocation',
@@ -59,16 +61,19 @@ class SaveFileHeader(object):
     # seek relative to ins.tell(), otherwise to the beginning of the file
     unpackers = OrderedDict()
 
-    def __init__(self, save_path):
+    def __init__(self, save_path, load_image=False, ins=None):
         self._save_path = save_path
         self.ssData = None # lazily loaded at runtime
-        self.read_save_header()
+        self.read_save_header(load_image, ins)
 
-    def read_save_header(self, load_image=False):
+    def read_save_header(self, load_image=False, ins=None):
         """Fully reads this save header, optionally loading the image as
         well."""
         try:
-            with self._save_path.open(u'rb') as ins:
+            if ins is None:
+                with self._save_path.open(u'rb') as ins:
+                    self.load_header(ins, load_image)
+            else:
                 self.load_header(ins, load_image)
         #--Errors
         except (OSError, struct.error, OverflowError):
@@ -82,11 +87,8 @@ class SaveFileHeader(object):
         if save_magic != self.__class__.save_magic:
             raise SaveHeaderError(u'Magic wrong: %r (expected %r)' % (
                 save_magic, self.__class__.save_magic))
-        for attr, unp in self.__class__.unpackers.iteritems():
-            if unp[0]:
-                if unp[0] > 0: ins.seek(unp[0])
-                else: ins.seek(ins.tell() - unp[0])
-            self.__setattr__(attr, unp[1](ins))
+        for attr, (__pack, _unpack) in self.__class__.unpackers.iteritems():
+            setattr(self, attr, _unpack(ins))
         self.load_image_data(ins, load_image)
         self.load_masters(ins)
         # additional calculations - TODO(ut): rework decoding
@@ -97,6 +99,9 @@ class SaveFileHeader(object):
         self.masters = [bolt.GPath_no_norm(decoder(
             x, bolt.pluginEncoding, avoidEncodings=(u'utf8', u'utf-8')))
             for x in self.masters]
+
+    def dump_header(self, out):
+        raise AbstractError
 
     def load_image_data(self, ins, load_image=False):
         bpp = (4 if self.has_alpha else 3)
@@ -134,7 +139,7 @@ class SaveFileHeader(object):
         out.write(ins.read(self._mastersStart))
         oldMasters = self._write_masters(ins, out)
         #--Copy the rest
-        for block in iter(partial(ins.read, 0x5000000), ''):
+        for block in iter(partial(ins.read, 0x5000000), b''):
             out.write(block)
         return oldMasters
 
@@ -163,7 +168,7 @@ class SaveFileHeader(object):
         pack_byte(out, len(self.masters))
         for master in self.masters:
             pack_short(out, len(master))
-            out.write(master.s)
+            out.write(encode(master.s))
         return oldMasters
 
     def _master_block_size(self):
@@ -175,20 +180,33 @@ class SaveFileHeader(object):
         be read and displayed, but the Save/Cancel buttons will be disabled."""
         return True
 
+def _pack_str8_1(out, val): # TODO: val = val.reencode(...)
+    val = encode(val)
+    pack_bzstr8(out, val)
+    return len(val) + 2
 class OblivionSaveHeader(SaveFileHeader):
-    save_magic = 'TES4SAVEGAME'
-    __slots__ = ('gameTime', 'ssSize')
+    save_magic = b'TES4SAVEGAME'
+    __slots__ = (u'major_version', u'minor_version', u'exe_time',
+                 u'header_version', u'saveNum', u'gameTime', u'ssSize')
+
+    ##: exe_time and gameTime are SYSTEMTIME structs, as described here:
+    # https://docs.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-systemtime
     unpackers = OrderedDict([
-        ('header_size', (34, unpack_int)),
-        ('pcName',      (42, unpack_str8)),
-        ('pcLevel',     (00, unpack_short)),
-        ('pcLocation',  (00, unpack_str8)),
-        ('gameDays',    (00, unpack_float)),
-        ('gameTicks',   (00, unpack_int)),
-        ('gameTime',    (00, lambda ins: unpack_string(ins, 16))),
-        ('ssSize',      (00, unpack_int)),
-        ('ssWidth',     (00, unpack_int)),
-        ('ssHeight',    (00, unpack_int)),
+        (u'major_version',  (pack_byte, unpack_byte)),
+        (u'minor_version',  (pack_byte, unpack_byte)),
+        (u'exe_time',       (pack_string, unpack_fstr16)),
+        (u'header_version', (pack_int, unpack_int)),
+        (u'header_size',    (pack_int, unpack_int)),
+        (u'saveNum',        (pack_int, unpack_int)),
+        (u'pcName',         (_pack_str8_1, unpack_str8)),
+        (u'pcLevel',        (pack_short, unpack_short)),
+        (u'pcLocation',     (_pack_str8_1, unpack_str8)),
+        (u'gameDays',       (pack_float, unpack_float)),
+        (u'gameTicks',      (pack_int, unpack_int)),
+        (u'gameTime',       (pack_string, unpack_fstr16)),
+        (u'ssSize',         (pack_int, unpack_int)),
+        (u'ssWidth',        (pack_int, unpack_int)),
+        (u'ssHeight',       (pack_int, unpack_int)),
     ])
 
     def _write_masters(self, ins, out):
@@ -198,19 +216,41 @@ class OblivionSaveHeader(SaveFileHeader):
         for x in xrange(numMasters):
             oldMasters.append(unpack_str8(ins))
         #--Write new masters
-        pack_byte(out, len(self.masters))
-        for master in self.masters:
-            pack_byte(out, len(master))
-            out.write(master.s)
+        self.__write_masters_ob(out)
         #--Fids Address
         offset = out.tell() - ins.tell()
         fidsAddress = unpack_int(ins)
         pack_int(out, fidsAddress + offset)
         return oldMasters
 
+    def __write_masters_ob(self, out):
+        pack_byte(out, len(self.masters))
+        for master in self.masters:
+            pack_str8(out, encode(master.s))
+
+    def dump_header(self, out):
+        out.write(self.__class__.save_magic)
+        var_fields_size = 0
+        for attr, (_pack, __unpack) in self.unpackers.iteritems():
+            ret = _pack(out, getattr(self, attr))
+            if ret is not None:
+                var_fields_size += ret
+        # Update the header size before writing it out. Note that all fields
+        # before saveNum do not count towards this
+        # TODO(inf) We need a nicer way to do this (query size before dump) -
+        #  ut: we need the binary string size here, header size must be
+        #  updated when var fields change (like pcName)
+        self.header_size = var_fields_size + 42 + len(self.ssData)
+        self._mastersStart = out.tell()
+        out.seek(34)
+        self.unpackers[u'header_size'][0](out, self.header_size)
+        out.seek(self._mastersStart)
+        out.write(self.ssData)
+        self.__write_masters_ob(out)
+
 class SkyrimSaveHeader(SaveFileHeader):
     """Valid Save Game Versions 8, 9, 12 (?)"""
-    save_magic = 'TESV_SAVEGAME'
+    save_magic = b'TESV_SAVEGAME'
     # extra slots - only version is really used, gameDate used once (calc_time)
     # _formVersion distinguish between old and new save formats
     # _compressType of Skyrim SE saves - used to decide how to read/write them
@@ -219,20 +259,20 @@ class SkyrimSaveHeader(SaveFileHeader):
                  u'_compressType', u'_sse_start', u'has_esl_masters')
 
     unpackers = OrderedDict([
-        ('header_size', (00, unpack_int)),
-        ('version',     (00, unpack_int)),
-        ('saveNumber',  (00, unpack_int)),
-        ('pcName',      (00, unpack_str16)),
-        ('pcLevel',     (00, unpack_int)),
-        ('pcLocation',  (00, unpack_str16)),
-        ('gameDate',    (00, unpack_str16)),
-        ('raceEid',     (00, unpack_str16)), # pcRace
-        ('pcSex',       (00, unpack_short)),
-        ('pcExp',       (00, unpack_float)),
-        ('pcLvlExp',    (00, unpack_float)),
-        ('filetime',    (00, lambda ins: unpack_string(ins, 8))),
-        ('ssWidth',     (00, unpack_int)),
-        ('ssHeight',    (00, unpack_int)),
+        (u'header_size', (00, unpack_int)),
+        (u'version',     (00, unpack_int)),
+        (u'saveNumber',  (00, unpack_int)),
+        (u'pcName',      (00, unpack_str16)),
+        (u'pcLevel',     (00, unpack_int)),
+        (u'pcLocation',  (00, unpack_str16)),
+        (u'gameDate',    (00, unpack_str16)),
+        (u'raceEid',     (00, unpack_str16)), # pcRace
+        (u'pcSex',       (00, unpack_short)),
+        (u'pcExp',       (00, unpack_float)),
+        (u'pcLvlExp',    (00, unpack_float)),
+        (u'filetime',    (00, lambda ins: unpack_string(ins, 8))),
+        (u'ssWidth',     (00, unpack_int)),
+        (u'ssHeight',    (00, unpack_int)),
     ])
 
     def __is_sse(self): return self.version == 12
@@ -471,8 +511,7 @@ class SkyrimSaveHeader(SaveFileHeader):
 
 class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
     """Valid Save Game Versions 11, 12, 13, 15 (?)"""
-    save_magic = 'FO4_SAVEGAME'
-
+    save_magic = b'FO4_SAVEGAME'
     __slots__ = ()
 
     def _esl_block(self): return self.version == 15 and self._formVersion >= 68
@@ -519,21 +558,21 @@ class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
         super(SkyrimSaveHeader, self).writeMasters(ins, out)
 
 class FalloutNVSaveHeader(SaveFileHeader):
-    save_magic = 'FO3SAVEGAME'
-    __slots__ = ('language', 'ssDepth', 'pcNick', '_unknown', 'gameDate')
+    save_magic = b'FO3SAVEGAME'
+    __slots__ = (u'language', u'ssDepth', u'pcNick', u'_unknown', u'gameDate')
     _masters_unknown_byte = 0x1B
     unpackers = OrderedDict([
-        ('header_size', (00, unpack_int)),
-        ('_unknown',    (00, unpack_str_int_delim)),
-        ('language',    (00, lambda ins: unpack_many(ins, '64sc')[0])),
-        ('ssWidth',     (00, unpack_str_int_delim)),
-        ('ssHeight',    (00, unpack_str_int_delim)),
-        ('ssDepth',     (00, unpack_str_int_delim)),
-        ('pcName',      (00, unpack_str16_delim_null)),
-        ('pcNick',      (00, unpack_str16_delim_null)),
-        ('pcLevel',     (00, unpack_str_int_delim)),
-        ('pcLocation',  (00, unpack_str16_delim_null)),
-        ('gameDate',    (00, unpack_str16_delim_null)),
+        (u'header_size', (00, unpack_int)),
+        (u'_unknown',    (00, unpack_str_int_delim)),
+        (u'language',    (00, lambda ins: unpack_many(ins, '64sc')[0])),
+        (u'ssWidth',     (00, unpack_str_int_delim)),
+        (u'ssHeight',    (00, unpack_str_int_delim)),
+        (u'ssDepth',     (00, unpack_str_int_delim)),
+        (u'pcName',      (00, unpack_str16_delim_null)),
+        (u'pcNick',      (00, unpack_str16_delim_null)),
+        (u'pcLevel',     (00, unpack_str_int_delim)),
+        (u'pcLocation',  (00, unpack_str16_delim_null)),
+        (u'gameDate',    (00, unpack_str16_delim_null)),
     ])
 
     def load_masters(self, ins):
@@ -577,7 +616,7 @@ class FalloutNVSaveHeader(SaveFileHeader):
         for master in self.masters:
             pack_short(out, len(master))
             _pack_c(out, b'|')
-            out.write(master.s)
+            out.write(encode(master.s))
             _pack_c(out, b'|')
         return oldMasters
 
@@ -592,17 +631,17 @@ class FalloutNVSaveHeader(SaveFileHeader):
         self.gameTicks = playSeconds * 1000
 
 class Fallout3SaveHeader(FalloutNVSaveHeader):
-    save_magic = 'FO3SAVEGAME'
+    save_magic = b'FO3SAVEGAME'
     __slots__ = ()
     _masters_unknown_byte = 0x15
     unpackers = copy.copy(FalloutNVSaveHeader.unpackers)
-    del unpackers['language']
+    del unpackers[u'language']
 
 class MorrowindSaveHeader(SaveFileHeader):
     """Morrowind saves are identical in format to record definitions.
     Accordingly, we delegate loading the header to our existing mod API."""
-    save_magic = 'TES3'
-    __slots__ = ('pc_curr_health', 'pc_max_health')
+    save_magic = b'TES3'
+    __slots__ = (u'pc_curr_health', u'pc_max_health')
 
     def load_header(self, ins, load_image=False):
         # TODO(inf) A bit ugly, this is not a mod - maybe move readHeader out?

commit 035fd910f807d6d5e0b2daf2954440fe22efe662

Fix regressions due to save header refactoring
Broke in bb82f9b11802e8f8eb07b643aaed2339c9aeeddd, when we refactored
_saves.py to use save_headers.py for reading headers.

Mopy/bash/patcher/patchers/multitweak_actors.py:
Fix VORB tweak assigning a Path to a record attr

Also fixed it using the OS separator - this is for the records, so needs
a backslash no matter the OS.

Almost certainly broke during tweak pooling.

Closes #586

Mopy/bash/bosh/_saves.py
@@ -266,14 +266,13 @@ class SaveFile(object):
             progress.setFull(self.fileInfo.fsize)
             #--Header
             progress(0,_(u'Reading Header.'))
-            del self._masters[:]
             self.header = OblivionSaveHeader(self.fileInfo.abs_path,
                                              load_image=True, ins=ins)
+            self._masters = self.header.masters
             #--Pre-Records copy buffer
             def insCopy(buff, siz, backSize=0):
                 if backSize: ins.seek(-backSize,1)
                 buff.write(ins.read(siz + backSize))
-
             #--"Globals" block
             fidsPointer,recordsNum = unpack_many(ins, u'2I')
             #--Pre-globals
@@ -391,11 +390,6 @@ class SaveFile(object):
         self.save(filePath.temp,progress)
         filePath.untemp()
         self.fileInfo.setmtime()
-
-    def addMaster(self,master):
-        """Adds master to masters list."""
-        if master not in self._masters:
-            self._masters.append(master)
 
     def indexCreated(self):
         """Fills out self.fid_recNum."""

Mopy/bash/bosh/faces.py
@@ -178,16 +178,17 @@ class PCFaces(object):
             saveFile = SaveFile(saveInfo)
             saveFile.load()
         face = PCFaces.PCFace()
-        face.pcName = saveFile.pcName
+        face.pcName = saveFile.header.pcName
         face.face_masters = saveFile._masters
         #--Player ACHR
         record = saveFile.getRecord(0x14)
         data = record[-1]
-        namePos = PCFaces.save_getNamePos(saveFile.fileInfo.name,data,encode(saveFile.pcName))
+        namePos = PCFaces.save_getNamePos(saveFile.fileInfo.name, data,
+                                          encode(saveFile.header.pcName))
         (face.fggs_p, face.fgga_p, face.fgts_p, face.race, face.hair, face.eye,
             face.hairLength, face.hairRed, face.hairBlue, face.hairGreen, face.unused3, face.gender) = struct_unpack(
             '=200s120s200s3If3BsB',data[namePos-542:namePos-1])
-        classPos = namePos+len(saveFile.pcName)+1
+        classPos = namePos + len(saveFile.header.pcName) + 1
         face.iclass, = struct_unpack('I', data[classPos:classPos+4])
         #--Iref >> fid
         getFid = saveFile.getFid
@@ -306,7 +307,8 @@ class PCFaces(object):
                 buff.seek(4,1)
         oldRecord = saveFile.getRecord(0x14)
         oldData = oldRecord[-1]
-        namePos = PCFaces.save_getNamePos(saveFile.fileInfo.name,oldData,encode(saveFile.pcName))
+        namePos = PCFaces.save_getNamePos(saveFile.fileInfo.name, oldData,
+                                          encode(saveFile.header.pcName))
         buff.write(oldData)
         #--Modify buffer with face data.
         buff.seek(namePos-542)
@@ -327,15 +329,16 @@ class PCFaces(object):
             buff.seek(1,1)
         #--Name?
         if pcf_flags.pcf_name:
-            postName = buff.getvalue()[buff.tell()+len(saveFile.pcName)+2:]
+            postName = buff.getvalue()[buff.tell() +
+                                       len(saveFile.header.pcName) + 2:]
             pack_byte(buff,len(face.pcName)+1)
             buff.write(encode(face.pcName, firstEncoding=Path.sys_fs_enc))
             buff.write(b'\x00')
             buff.write(postName)
             buff.seek(-len(postName),1)
-            saveFile.pcName = face.pcName
+            saveFile.header.pcName = face.pcName
         else:
-            buff.seek(len(saveFile.pcName)+2,1)
+            buff.seek(len(saveFile.header.pcName) + 2, 1)
         #--Class?
         if pcf_flags.iclass and face.iclass:
             pos = buff.tell()
@@ -384,7 +387,8 @@ class PCFaces(object):
         saveFile.load()
         record = saveFile.getRecord(0x14)
         data = record[-1]
-        namePos = PCFaces.save_getNamePos(saveInfo.name,data,encode(saveFile.pcName))
+        namePos = PCFaces.save_getNamePos(saveInfo.name, data,
+                                          encode(saveFile.header.pcName))
         raceRef,hairRef = struct_unpack(u'2I', data[namePos-22:namePos-14])
         if hairRef != 0: return False
         raceForm = raceRef and saveFile.fids[raceRef]

Mopy/bash/patcher/patchers/multitweak_actors.py
@@ -115,7 +115,7 @@ class VORB_NPCSkeletonPatcher(_ASkeletonTweak):
                   u"Requires VadersApp's Oblivion Real Bodies.")
     tweak_key = u'VORB'
     tweak_log_header = _(u"VadersApp's Oblivion Real Bodies")
-    _skeleton_dir = GPath(u'Characters').join(u'_male')
+    _skeleton_dir = u'Characters\\_male'
 
     def _get_skeleton_collections(self):
         """construct skeleton mesh collections. skeleton_list gets files that
@@ -148,11 +148,11 @@ class VORB_NPCSkeletonPatcher(_ASkeletonTweak):
             return self._get_skeleton_path(record) # leave unchanged
         special_skel_mesh = u'skel_special_%X.nif' % record.fid[1]
         if special_skel_mesh in skeleton_specials:
-            return self._skeleton_dir.join(special_skel_mesh)
+            return u'%s\\%s' % (self._skeleton_dir, special_skel_mesh)
         else:
             random.seed(record.fid[1]) # make it deterministic
-            rand_index = random.randint(1, len(skeleton_list)) - 1
-            return self._skeleton_dir.join(skeleton_list[rand_index]).s
+            rand_index = random.randint(1, len(skeleton_list)) - 1 ##: choice?
+            return u'%s\\%s' % (self._skeleton_dir, skeleton_list[rand_index])
 
 #------------------------------------------------------------------------------
 class VanillaNPCSkeletonPatcher(_ASkeletonTweak):