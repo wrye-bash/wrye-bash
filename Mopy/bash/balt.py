wrye-bash/wrye-bash

commit 1ef881329aa008334bec49c939b191b1bfb1e683

Move env and windows to package
Proved to be tough (duh) so this is squashed from a bunch of commits so
as to not break dev.

And let the ripping begin :)

WIP continue splitting

Approximately split by internals/API

Move linux get_file_version to linux.py

Semi-solid TaskDialog API

Getting somewhere, maybe. I like the unguarded import int balt for sure.

Import fewer modules statically in bash

Ideally we'd be importing *no* modules here, because importing any adds
the chance that we blow up without showing a traceback to the user. The
code isn't ready for that yet, but these two can definitely get proper
handling inside _main/main.

Encapsulate high DPI marking inside env

Also moved it to main so that it sits inside a try - again, more
safety during startup. We really want to do as little as possible before
localize and wx are set up.

Stub out, implement or move remaining functions

Still far from ideal, too much stuff in __init__.py.

Mopy/bash/balt.py
@@ -315,27 +315,22 @@ def askNumber(parent,message,prompt=u'',title=u'',value=0,min=0,max=10000):
         return dialog.GetValue()
 
 # Message Dialogs -------------------------------------------------------------
-try:
-    from . import windows as _win # only import here !
-    canVista = _win.TASK_DIALOG_AVAILABLE
-except ImportError: # bare linux (in wine it's imported but malfunctions)
-    deprint(u'Importing windows.py failed', traceback=True)
-    _win = None
-    canVista = False
+from .env import TASK_DIALOG_AVAILABLE, TaskDialog, BTN_OK, BTN_CANCEL, \
+    BTN_YES, BTN_NO, GOOD_EXITS
+canVista = TASK_DIALOG_AVAILABLE
 
 def vistaDialog(parent, message, title, checkBoxTxt=None,
                 buttons=None, icon=u'warning', commandLinks=True, footer=u'',
                 expander=[], heading=u''):
     """Always guard with canVista == True"""
     buttons = buttons if buttons is not None else (
-        (_win.BTN_OK, u'ok'), (_win.BTN_CANCEL, u'cancel'))
+        (BTN_OK, u'ok'), (BTN_CANCEL, u'cancel'))
     heading = heading if heading is not None else title
     title = title if heading is not None else u'Wrye Bash'
-    dialog = _win.TaskDialog(title, heading, message,
-                             buttons=[x[1] for x in buttons],
-                             main_icon=icon,
-                             parenthwnd=parent.GetHandle() if parent else None,
-                             footer=footer)
+    dialog = TaskDialog(title, heading, message,
+                        buttons=[x[1] for x in buttons], main_icon=icon,
+                        parenthwnd=parent.GetHandle() if parent else None,
+                        footer=footer)
     if expander:
         dialog.set_expander(expander,False,not footer)
     if checkBoxTxt:
@@ -350,9 +345,9 @@ def vistaDialog(parent, message, title, checkBoxTxt=None,
         if title.startswith(u'+'): title = title[1:]
         if title == button:
             if checkBoxTxt:
-                return id_ in _win.GOOD_EXITS, checkbox
+                return id_ in GOOD_EXITS, checkbox
             else:
-                return id_ in _win.GOOD_EXITS, None
+                return id_ in GOOD_EXITS, None
     return False, checkbox
 
 def askStyled(parent, message, title, style, do_center=False):
@@ -370,12 +365,12 @@ def askStyled(parent, message, title, style, do_center=False):
                 yes = u'Yes'
             elif style & wx.NO_DEFAULT:
                 no = u'No'
-            vista_btn.append((_win.BTN_YES, yes))
-            vista_btn.append((_win.BTN_NO, no))
+            vista_btn.append((BTN_YES, yes))
+            vista_btn.append((BTN_NO, no))
         if style & wx.OK:
-            vista_btn.append((_win.BTN_OK, u'ok'))
+            vista_btn.append((BTN_OK, u'ok'))
         if style & wx.CANCEL:
-            vista_btn.append((_win.BTN_CANCEL, u'cancel'))
+            vista_btn.append((BTN_CANCEL, u'cancel'))
         if style & (wx.ICON_EXCLAMATION|wx.ICON_INFORMATION):
             icon = u'warning'
         if style & wx.ICON_HAND:

Mopy/bash/bash.py
@@ -29,7 +29,6 @@ loop."""
 from __future__ import print_function
 import atexit
 import codecs
-import ctypes
 import os
 import platform
 import shutil
@@ -37,7 +36,7 @@ import sys
 import traceback
 from ConfigParser import ConfigParser
 # Local
-from . import bass, bolt, env, exception, localize
+from . import bass, bolt, exception
 # NO OTHER LOCAL IMPORTS HERE (apart from the ones above) !
 basher = None # need to share it in _close_dialog_windows
 bass.is_standalone = hasattr(sys, u'frozen')
@@ -73,11 +72,6 @@ def _early_setup(debug):
             open(os.path.join(os.getcwdu(), u'BashBugDump.log'), u'w'))
         sys.stdout = _bugdump_handle
         sys.stderr = _bugdump_handle
-    # Mark us as High DPI-aware on Windows
-    try:
-        ctypes.windll.shcore.SetProcessDpiAwareness(True)
-    except (AttributeError, WindowsError):
-        pass # Not on Windows or on Windows < 8.1
 
 def _import_wx():
     """Import wxpython or show a tkinter error and exit if unsuccessful."""
@@ -255,10 +249,15 @@ def main(opts):
     _early_setup(opts.debug)
     # wx is needed to initialize locale, so that's first
     _import_wx()
-    # Next, proceed to initialize the locale using wx
-    wx_locale = localize.setup_locale(opts.language)
-    # At this point, we can show a wx error popup, so do it all in a try
     try:
+        # Next, initialize locale so that we can show a translated error
+        # message if WB crashes
+        from . import localize
+        wx_locale = localize.setup_locale(opts.language)
+        # Mark us as high DPI aware before gui/balt are imported
+        # FIXME(inf) test: do we need to do this before _import_wx?
+        from . import env
+        env.mark_high_dpi_aware()
         # Initialize gui, our wrapper above wx (also balt, temp module)
         from . import gui, balt
         # Check for some non-critical dependencies (e.g. lz4) and warn if
@@ -267,14 +266,20 @@ def main(opts):
         # Early setup is done, delegate to the main init method
         _main(opts, wx_locale)
     except Exception:
+        caught_exc = traceback.format_exc()
+        try:
+            # Check if localize succeeded in setting up translations, otherwise
+            # monkey patch in a noop underscore
+            _(u'')
+        except NameError:
+            def _(x): return x
         msg = u'\n'.join([
             _(u'Wrye Bash encountered an error.'),
             _(u'Please post the information below to the official thread at'),
             _(u'https://afkmods.com/index.php?/topic/4966-wrye-bash-all-games'),
             _(u'or to the Wrye Bash Discord at'),
             _(u'https://discord.gg/NwWvAFR'),
-            u'',
-            traceback.format_exc()
+            u'', caught_exc,
         ])
         _close_dialog_windows()
         _show_boot_popup(msg)
@@ -346,7 +351,8 @@ def _main(opts, wx_locale):
         # init problems (since we init those in main)
         from . import bosh
         bosh.initBosh(bashIni, game_ini_path)
-        env.isUAC = env.testUAC(bush_game.gamePath.join(bush_game.mods_dir))
+        from . import env
+        env.testUAC(bush_game.gamePath.join(bush_game.mods_dir))
         global basher # share this instance with _close_dialog_windows
         from . import basher
     except (exception.BoltError, ImportError, OSError, IOError):
@@ -355,6 +361,7 @@ def _main(opts, wx_locale):
                           traceback.format_exc()])
         _close_dialog_windows()
         _show_boot_popup(msg)
+        return # _show_boot_popup calls sys.exit, this gets pycharm to shut up
     atexit.register(exit_cleanup)
     basher.InitSettings()
     basher.InitLinks()

Mopy/bash/env/__init__.py
@@ -1,0 +1,268 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2020 Wrye Bash Team
+#  https://github.com/wrye-bash
+#
+# =============================================================================
+"""The env module encapsulates OS-specific classes and methods. This is the
+central import point, always import directly from here to get the right
+implementations for the current OS."""
+
+import errno
+import os
+import platform
+import shutil
+import stat
+from itertools import izip
+
+# Then check which OS we are running on and import *only* from there
+if os.name == u'nt':
+    from .windows import *
+elif platform.system() == u'Darwin':
+    raise ImportError(u'Wrye Bash does not support macOS yet.')
+else:
+    from .linux import *
+
+##: Internals that could not easily be split between windows.py and linux.py
+try:
+    from win32com.shell import shell, shellcon
+except ImportError:
+    shell = shellcon = None
+
+from ..bolt import deprint, Path
+from ..exception import CancelError, SkipError, AccessDeniedError, \
+    DirectoryFileCollisionError, FileOperationError
+
+# NB: AccessDeniedError is not 5 but 120 as seen in:
+# https://msdn.microsoft.com/en-us/library/windows/desktop/bb762164%28v=vs.85%29.aspx
+_file_op_error_map = {
+    120: AccessDeniedError,
+    # https://msdn.microsoft.com/en-us/library/windows/desktop/ms681383%28v=vs.85%29.aspx
+    1223: CancelError,
+}
+
+def __copyOrMove(operation, source, target, renameOnCollision, parent):
+    """WIP shutil move and copy adapted from #96"""
+    # renameOnCollision - if True auto-rename on moving collision, else ask
+    # TODO(241): renameOnCollision NOT IMPLEMENTED
+    doIt = shutil.copytree if operation == FO_COPY else shutil.move
+    for fileFrom, fileTo in izip(source, target):
+        if fileFrom.isdir():
+            dest_dir = fileTo.join(fileFrom.tail)
+            if dest_dir.exists():
+                if not dest_dir.isdir():
+                    raise DirectoryFileCollisionError(fileFrom, dest_dir)
+                # dir exists at target, copy contents individually/recursively
+                source_paths, dests = [], []
+                for content in os.listdir(fileFrom.s):
+                    source_paths.append(fileFrom.join(content))
+                    dests.append(dest_dir)
+                __copyOrMove(operation, source_paths, dests, renameOnCollision, parent)
+            else:  # dir doesn't exist at the target, copy it
+                doIt(fileFrom.s, fileTo.s)
+        # copy the file, overwrite as needed
+        elif fileFrom.isfile():  # or os.path.islink(file):
+            # move may not work if the target exists, copy instead and
+            # overwrite as needed
+            try:
+                shutil.copy2(fileFrom.s, fileTo.s)
+            except IOError as e:
+                if e.errno != errno.ENOENT: raise
+                # probably directory path does not exist, create it.
+                fileTo.head.makedirs()
+                shutil.copy2(fileFrom.s, fileTo.s)
+            if operation == FO_MOVE: fileFrom.remove() # then remove original
+    return {} ##: the renames map ?
+
+def _fileOperation(operation, source, target=None, allowUndo=True,
+                   confirm=True, renameOnCollision=False, silent=False,
+                   parent=None, __shell=True):
+    """Docs WIP
+
+    :param operation: one of FO_MOVE, FO_COPY, FO_DELETE, FO_RENAME
+    :param source: a Path, string or an iterable of those (yak, only accept
+       iterables)
+    :param target: as above, if iterable must have the same length as source
+    :param allowUndo: FOF_ALLOWUNDO: "Preserve undo information, if possible"
+    :param confirm: the opposite of FOF_NOCONFIRMATION ("Respond with Yes to
+        All for any dialog box that is displayed")
+    :param renameOnCollision: FOF_RENAMEONCOLLISION
+    :param silent: FOF_SILENT ("Do not display a progress dialog box")
+    :param parent: HWND to the dialog's parent window
+    .. seealso:
+        `SHFileOperation <http://msdn.microsoft.com/en-us/library/windows
+        /desktop/bb762164(v=vs.85).aspx>`
+    """
+    if not source:
+        return {}
+    abspath = os.path.abspath
+    # source may be anything - see SHFILEOPSTRUCT - accepts list or item
+    if isinstance(source, (Path, (unicode, bytes))):
+        source = [abspath(u'%s' % source)]
+    else:
+        source = [abspath(u'%s' % x) for x in source]
+    # target may be anything ...
+    target = target if target else u'' # abspath(u''): cwd (must be Mopy/)
+    if isinstance(target, (Path, (unicode, bytes))):
+        target = [abspath(u'%s' % target)]
+    else:
+        target = [abspath(u'%s' % x) for x in target]
+    _source = source; _target = target
+    if __shell and shell is not None:
+        # flags
+        flgs = shellcon.FOF_WANTMAPPINGHANDLE # enables mapping return value !
+        flgs |= FOF_NOCONFIRMMKDIR # never ask user for creating dirs
+        flgs |= (len(target) > 1) * shellcon.FOF_MULTIDESTFILES
+        if allowUndo: flgs |= shellcon.FOF_ALLOWUNDO
+        if not confirm: flgs |= shellcon.FOF_NOCONFIRMATION
+        if renameOnCollision: flgs |= shellcon.FOF_RENAMEONCOLLISION
+        if silent: flgs |= shellcon.FOF_SILENT
+        # null terminated strings
+        source = u'\x00'.join(source) # nope: + u'\x00'
+        target = u'\x00'.join(target)
+        # get the handle to parent window to feed to win api
+        parent = parent.GetHandle() if parent else None
+        # See SHFILEOPSTRUCT for deciphering return values
+        # result: a windows error code (or 0 for success)
+        # aborted: True if any operations aborted, False otherwise
+        # mapping: maps the old and new names of the renamed files
+        result, aborted, mapping = shell.SHFileOperation(
+                (parent, operation, source, target, flgs, None, None))
+        if result == 0:
+            if aborted: raise SkipError()
+            return dict(mapping)
+        elif result == 2 and operation == FO_DELETE:
+            # Delete failed because file didnt exist
+            return dict(mapping)
+        else:
+            if result == 124:
+                deprint(u'Invalid paths:\nsource: %s\ntarget: %s\nRetrying' % (
+                    source.replace(u'\x00', u'\n'),
+                    target.replace(u'\x00', u'\n')))
+                return _fileOperation(operation, _source, _target, allowUndo,
+                                      confirm, renameOnCollision, silent,
+                                      parent, __shell=False)
+            raise _file_op_error_map.get(result, FileOperationError(result))
+    else: # Use custom dialogs and such
+        from .. import balt # TODO(ut): local import, env should be above balt...
+        source = map(GPath, source)
+        target = map(GPath, target)
+        if operation == FO_DELETE:
+            # allowUndo - no effect, can't use recycle bin this way
+            # confirm - ask if confirm is True
+            # renameOnCollision - no effect, deleting files
+            # silent - no real effect (we don't show visuals deleting this way)
+            if confirm:
+                message = _(u'Are you sure you want to permanently delete '
+                            u'these %(count)d items?') % {u'count':len(source)}
+                message += u'\n\n' + u'\n'.join([u' * %s' % x for x in source])
+                if not balt.askYes(parent,message,_(u'Delete Multiple Items')):
+                    return {}
+            # Do deletion
+            for toDelete in source:
+                if not toDelete.exists(): continue
+                if toDelete.isdir():
+                    toDelete.rmtree(toDelete.stail)
+                else:
+                    toDelete.remove()
+            return {}
+        # allowUndo - no effect, we're not going to manually track file moves
+        # confirm - no real effect when moving
+        # silent - no real effect, since we're not showing visuals
+        return __copyOrMove(operation, source, target, renameOnCollision,
+                            parent)
+
+# Higher level APIs implemented by using the imported OS-specific ones above
+def clear_read_only(filepath): # copied from bolt
+    os.chmod(u'%s' % filepath, stat.S_IWUSR | stat.S_IWOTH)
+
+def shellDelete(files, parent=None, confirm=False, recycle=False):
+    try:
+        return _fileOperation(FO_DELETE, files, target=None, allowUndo=recycle,
+                              confirm=confirm, renameOnCollision=True,
+                              silent=False, parent=parent)
+    except CancelError:
+        if confirm:
+            return None
+        raise
+
+def shellDeletePass(node, parent=None):
+    """Delete tmp dirs/files - ignore errors (but log them)."""
+    if node.exists():
+        try: shellDelete(node, parent=parent, confirm=False, recycle=False)
+        except OSError: deprint(u'Error deleting %s:' % node, traceback=True)
+
+def shellMove(filesFrom, filesTo, parent=None, askOverwrite=False,
+              allowUndo=False, autoRename=False, silent=False):
+    return _fileOperation(FO_MOVE, filesFrom, filesTo, parent=parent,
+                          confirm=askOverwrite, allowUndo=allowUndo,
+                          renameOnCollision=autoRename, silent=silent)
+
+def shellCopy(filesFrom, filesTo, parent=None, askOverwrite=False,
+              allowUndo=False, autoRename=False):
+    return _fileOperation(FO_COPY, filesFrom, filesTo, allowUndo=allowUndo,
+                          confirm=askOverwrite, renameOnCollision=autoRename,
+                          silent=False, parent=parent)
+
+def shellMakeDirs(dirs, parent=None):
+    if not dirs: return
+    dirs = [dirs] if not isinstance(dirs, (list, tuple, set)) else dirs
+    #--Skip dirs that already exist
+    dirs = [x for x in dirs if not x.exists()]
+    #--Check for dirs that are impossible to create (the drive they are
+    #  supposed to be on doesn't exist
+    def _filterUnixPaths(path):
+        return (os.name != u'posix' and not path.s.startswith(u'\\')
+                and not path.drive().exists())
+    errorPaths = [d for d in dirs if _filterUnixPaths(d)]
+    if errorPaths:
+        raise NonExistentDriveError(errorPaths)
+    #--Checks complete, start working
+    tempDirs, fromDirs, toDirs = [], [], []
+    try:
+        for folder in dirs:
+            # Attempt creating the directory via normal methods, only fall back
+            # to shellMove if UAC or something else stopped it
+            try:
+                folder.makedirs()
+            except:
+                # Failed, try the UAC workaround
+                tmpDir = Path.tempDir()
+                tempDirs.append(tmpDir)
+                toMake = []
+                toMakeAppend = toMake.append
+                while not folder.exists() and folder != folder.head:
+                    # Need to test against dir == dir.head to prevent
+                    # infinite recursion if the final bit doesn't exist
+                    toMakeAppend(folder.tail)
+                    folder = folder.head
+                if not toMake:
+                    continue
+                toMake.reverse()
+                base = tmpDir.join(toMake[0])
+                toDir = folder.join(toMake[0])
+                tmpDir.join(*toMake).makedirs()
+                fromDirs.append(base)
+                toDirs.append(toDir)
+        if fromDirs:
+            # fromDirs will only get filled if folder.makedirs() failed
+            shellMove(fromDirs, toDirs, parent=parent)
+    finally:
+        for tmpDir in tempDirs:
+            tmpDir.rmtree(safety=tmpDir.stail)

commit 1b970ae16b37dbe62d215c43759c1e76ce35402e

Fix UAC detection, make it work for WS games
Broke in 1ef881329aa008334bec49c939b191b1bfb1e683.

Fix access violation on TaskDialog

(it was struct packing of course)

Fixup for ask_uac_restart

Was always returning False before.  Plus a Path object got put on
the command line.

Co-authored-by: Infernio <infernio@icloud.com>

Mopy/bash/balt.py
@@ -321,7 +321,7 @@ def vistaDialog(parent, message, title, checkBoxTxt=None,
     buttons = buttons if buttons is not None else (
         (BTN_OK, u'ok'), (BTN_CANCEL, u'cancel'))
     heading = heading if heading is not None else title
-    title = title if heading is not None else u'Wrye Bash'
+    title = title if title is not None else u'Wrye Bash'
     dialog = TaskDialog(title, heading, message,
                         buttons=[x[1] for x in buttons], main_icon=icon,
                         parenthwnd=parent.GetHandle() if parent else None,
@@ -2232,7 +2232,7 @@ def ask_uac_restart(message, title, mopy):
     admin = _(u'Run with Administrator Privileges')
     readme = readme_url(mopy) + u'#trouble-permissions'
     return vistaDialog(None, message=message,
-        buttons=[(True, u'+' + admin), (False, _(u'Run normally'))],
+        buttons=[(BTN_YES, u'+' + admin), (BTN_NO, _(u'Run normally'))],
         title=title, expander=[_(u'How to avoid this message in the future'),
             _(u'Less information'),
             _(u'Use one of the following command line switches:') +

Mopy/bash/bash.py
@@ -385,7 +385,7 @@ def _main(opts, wx_locale):
     app.locale = wx_locale
     if not bass.is_standalone and (
         not _rightWxVersion() or not _rightPythonVersion()): return
-    if env.isUAC:
+    if env.is_uac():
         uacRestart = opts.uac
         if not opts.noUac and not opts.uac:
             # Show a prompt asking if we should restart in Admin Mode
@@ -492,7 +492,7 @@ def _import_bush_and_set_game(opts, bashIni):
             return None
         # Add the game to the command line, so we use it if we restart
         gname, gm_path = retCode
-        bass.update_sys_argv([u'--oblivionPath', gm_path])
+        bass.update_sys_argv([u'--oblivionPath', u'%s' % gm_path])
         bush.detect_and_set_game(opts.oblivionPath, bashIni, gname, gm_path)
     return bush.game
 

Mopy/bash/env/__init__.py
@@ -55,6 +55,8 @@ from ..exception import CancelError, SkipError, AccessDeniedError, \
 # NB: AccessDeniedError is not 5 but 120 as seen in:
 # https://msdn.microsoft.com/en-us/library/windows/desktop/bb762164%28v=vs.85%29.aspx
 _file_op_error_map = {
+    # Returned for Windows Store games that need admin access
+    17: AccessDeniedError,
     120: AccessDeniedError,
     # https://msdn.microsoft.com/en-us/library/windows/desktop/ms681383%28v=vs.85%29.aspx
     1223: CancelError,

Mopy/bash/env/linux.py
@@ -31,8 +31,6 @@ from ..exception import EnvError
 from .common import get_env_var, iter_env_vars
 
 # API - Constants =============================================================
-isUAC = False # Not a thing on Linux
-
 try:
     MAX_PATH = int(subprocess.check_output([u'getconf', u'PATH_MAX', u'/']))
 except (ValueError, subprocess.CalledProcessError, OSError):
@@ -92,6 +90,9 @@ def testUAC(_gameDataPath):
 
 def setUAC(_handle, _uac=True):
     pass # Noop on Linux
+
+def is_uac():
+    return False # Not a thing on Linux
 
 def getJava(): # PY3: cache this
     try:

Mopy/bash/env/windows.py
@@ -43,7 +43,7 @@ from ..exception import AccessDeniedError, BoltError, NonExistentDriveError
 from .common import get_env_var
 
 # API - Constants =============================================================
-isUAC = False # True if the game is under UAC protection
+isUAC = False
 
 from ctypes.wintypes import MAX_PATH
 from win32com.shell.shellcon import FO_DELETE, FO_MOVE, FO_COPY, \
@@ -516,14 +516,17 @@ _PFTASKDIALOGCALLBACK = WINFUNCTYPE(c_void_p, c_void_p, c_uint, c_uint, c_long,
 #-------Win32 Stucts/Unions--------#
 # Callers do not have to worry about using these.
 class _TASKDIALOG_BUTTON(Structure):
+    _pack_ = 1
     _fields_ = [(u'nButtonID', c_int),
                 (u'pszButtonText', c_wchar_p)]
 
 class _FOOTERICON(Union):
+    _pack_ = 1
     _fields_ = [(u'hFooterIcon', c_void_p),
                 (u'pszFooterIcon', c_ushort)]
 
 class _MAINICON(Union):
+    _pack_ = 1
     _fields_ = [(u'hMainIcon', c_void_p),
                 (u'pszMainIcon', c_ushort)]
 
@@ -663,6 +666,7 @@ _SHGSI_LINKOVERLAY   = 0x00008000 # get icon with a link overlay on it
 _SHGSI_SELECTED      = 0x00010000 # get icon in selected state
 
 class _SHSTOCKICONINFO(Structure):
+    _pack_ = 1
     _fields_ = [(u'cbSize', c_ulong),
                 (u'hIcon', c_void_p),
                 (u'iSysImageIndex', c_int),
@@ -821,7 +825,7 @@ def get_file_version(filename):
     return _query_string_field_version(filename, u'FileVersion')
 
 def testUAC(gameDataPath):
-    print(u'testing UAC')
+    deprint(u'Testing if game folder is UAC-protected')
     tmpDir = Path.tempDir()
     tempFile = tmpDir.join(u'_tempfile.tmp')
     dest = gameDataPath.join(u'_tempfile.tmp')
@@ -867,6 +871,10 @@ def getJava(): # PY3: cache this
         # So javaw.exe would actually be in Windows\SysWOW64
         java_bin_path = sys_root.join(u'syswow64', u'javaw.exe')
     return java_bin_path
+
+def is_uac():
+    """Returns True if the game is under UAC protection."""
+    return isUAC
 
 def fixup_taskbar_icon():
     """On Windows 7+, if taskbar settings for taskbar buttons is set to 'Always