wrye-bash/wrye-bash

commit c39d5d3a67df881a01c1213ac9277c9a6ded970f

Support games with more than one path
Fairly general framework. Called them 'variants' so it makes sense why
some games may not have them - if we called them 'languages', people
would ask how to change it for non-WS games.

Mopy/bash/bash.py
@@ -472,23 +472,28 @@ def _detect_game(opts, backup_bash_ini):
     return bashIni, bush_game, game_ini_path
 
 def _import_bush_and_set_game(opts, bashIni):
-    from . import bush
+    from . import bush, balt
     bolt.deprint(u'Searching for game to manage:')
-    game_infos = bush.detect_and_set_game(opts.oblivionPath,
-                                                      bashIni)
+    game_infos = bush.detect_and_set_game(opts.oblivionPath, bashIni)
     if game_infos is not None:  # None == success
         if len(game_infos) == 0:
-            # FIXME(inf) Show as a separate popup
-            msgtext = _(u'Wrye Bash could not find a game to manage. Please '
-                        u'use the -o command line argument to specify the '
-                        u'game path.')
+            balt.showError(
+                None, _(u'Wrye Bash could not find a game to manage. Make '
+                        u'sure to launch games you installed through Steam '
+                        u'once and enable mods on games you installed through '
+                        u'the Windows Store.') + u'\n\n' +
+                      _(u'You can also use the -o command line argument or '
+                        u'bash.ini to specify the path manually.'),
+                title=_(u'No Game Found'))
+            return None
         retCode = _select_game_popup(game_infos)
-        if retCode is None:
+        if not retCode:
             bolt.deprint(u'No games were found or selected. Aborting.')
             return None
         # Add the game to the command line, so we use it if we restart
-        bass.update_sys_argv([u'--oblivionPath', bush.game_path(retCode).s])
-        bush.detect_and_set_game(opts.oblivionPath, bashIni, retCode)
+        gname, gm_path = retCode
+        bass.update_sys_argv([u'--oblivionPath', gm_path])
+        bush.detect_and_set_game(opts.oblivionPath, bashIni, gname, gm_path)
     return bush.game
 
 def _show_boot_popup(msg, is_critical=True):
@@ -573,17 +578,18 @@ def _select_game_popup(game_infos):
     from .balt import Resources
     from .gui import Label, TextAlignment, WindowFrame, VLayout, \
         ImageDropDown, LayoutOptions, SearchBar, VBoxedLayout, TextField, \
-        HLayout, QuitButton, ImageButton, HorizontalLine, Stretch
-    ##: Move to popups.py?
-    class GameSelect(WindowFrame):
+        HLayout, QuitButton, ImageButton, HorizontalLine, Stretch, DropDown
+    ##: Decouple game icon paths and move to popups.py once balt is refactored
+    # enough
+    class SelectGamePopup(WindowFrame):
         _def_size = (500, 400)
 
         def __init__(self, game_infos, callback):
-            super(GameSelect, self).__init__(None, _(u'Select Game'),
-                                             icon_bundle=Resources.bashRed)
+            super(SelectGamePopup, self).__init__(
+                None, title=_(u'Select Game'), icon_bundle=Resources.bashRed)
             self._callback = callback
             self._sorted_games = sorted(g.displayName for g in game_infos)
-            self._game_to_path = {g.displayName: p.s for g, p
+            self._game_to_paths = {g.displayName: ps for g, ps
                                   in game_infos.iteritems()}
             self._game_to_info = {g.displayName: g for g in game_infos}
             self._game_to_bitmap = {
@@ -594,16 +600,15 @@ def _select_game_popup(game_infos):
             game_search.on_text_changed.subscribe(self._perform_search)
             self._game_dropdown = ImageDropDown(self, value=u'', choices=[u''])
             self._game_dropdown.on_combo_select.subscribe(self._select_game)
+            self._lang_dropdown = DropDown(self, value=u'', choices=[u''])
+            self._lang_dropdown.on_combo_select.subscribe(self._select_lang)
             self._game_path = TextField(self, editable=False)
             quit_button = QuitButton(self)
-            quit_button.on_clicked.subscribe(lambda: self._close_game_select(
-                return_code=None))
+            quit_button.on_clicked.subscribe(self._handle_quit)
             launch_img = bass.dirs[u'images'].join(u'bash_32_2.png').s
             self._launch_button = ImageButton(self, _wx.Bitmap(launch_img),
                                               btn_label=_(u'Launch'))
-            self._launch_button.on_clicked.subscribe(
-                lambda: self._close_game_select(
-                    return_code=self._game_dropdown.get_value()))
+            self._launch_button.on_clicked.subscribe(self._handle_launch)
             # Start out with an empty search and the alphabetically first game
             # selected
             self._perform_search(search_str=u'')
@@ -614,6 +619,10 @@ def _select_game_popup(game_infos):
                 (VBoxedLayout(self, title=_(u'Game Details'), item_expand=True,
                               spacing=12, items=[
                     HLayout(spacing=6, item_expand=True, items=[
+                        Label(self, _(u'Variant:')),
+                        (self._lang_dropdown, LayoutOptions(weight=1)),
+                    ]),
+                    HLayout(spacing=6, item_expand=True, items=[
                         Label(self, _(u'Install Path:')),
                         (self._game_path, LayoutOptions(weight=1)),
                     ]),
@@ -623,6 +632,18 @@ def _select_game_popup(game_infos):
                     quit_button, Stretch(), self._launch_button,
                 ]), LayoutOptions(weight=1)),
             ]).apply_to(self)
+
+        @property
+        def _chosen_path(self):
+            avail_paths = self._game_to_paths[self._game_dropdown.get_value()]
+            if len(avail_paths) == 1:
+                return avail_paths[0]
+            else:
+                chosen_lang = self._lang_dropdown.get_value()
+                for p in avail_paths:
+                    if chosen_lang in p.s:
+                        return p
+                return None # Should never happen
 
         def _perform_search(self, search_str):
             prev_choice = self._game_dropdown.get_value()
@@ -649,17 +670,41 @@ def _select_game_popup(game_infos):
         def _select_game(self, selected_game):
             if not selected_game:
                 # No game selected, clear the details
+                self._lang_dropdown.set_choices([_(u'N/A')])
+                self._lang_dropdown.set_selection(0)
+                self._lang_dropdown.enabled = False
                 self._game_path.text_content = u''
             else:
-                self._game_path.text_content = self._game_to_path[selected_game]
+                # Enable the Language dropdown only if we >1 path for the newly
+                # active game
+                available_paths = self._game_to_paths[selected_game]
+                if len(available_paths) > 1:
+                    self._lang_dropdown.set_choices([p.stail for p
+                                                     in available_paths])
+                    self._lang_dropdown.set_selection(0)
+                    self._lang_dropdown.enabled = True
+                else:
+                    self._lang_dropdown.set_choices([_(u'N/A')])
+                    self._lang_dropdown.set_selection(0)
+                    self._lang_dropdown.enabled = False
+                # Set the path based on the default language
+                self._select_lang()
 
-        def _close_game_select(self, return_code):
+        def _select_lang(self, _selected_lang=None):
+            self._game_path.text_content = self._chosen_path.s
+
+        def _handle_quit(self):
+            self._callback(None)
             self.close_win(force_close=True)
-            self._callback(return_code)
+
+        def _handle_launch(self):
+            self._callback((self._game_dropdown.get_value(),
+                            self._chosen_path))
+            self.close_win(force_close=True)
     _app = _wx.App(False)
     _app.locale = _wx.Locale(_wx.LANGUAGE_DEFAULT)
     retCode = _AppReturnCode()
-    frame = GameSelect(game_infos, retCode.set)
+    frame = SelectGamePopup(game_infos, retCode.set)
     frame.show_frame()
     frame._native_widget.Center() # TODO(inf) de-wx!
     _app.MainLoop()

Mopy/bash/bush.py
@@ -32,7 +32,7 @@ import textwrap
 from . import bass
 from . import game as game_init
 from .bolt import GPath, Path, deprint, dict_sort
-from .env import get_registry_game_path, get_win_store_game_path
+from .env import get_registry_game_paths, get_win_store_game_paths
 from .exception import BoltError
 from .game import patch_game
 
@@ -42,14 +42,32 @@ foundGames = {}     # {'name': Path} dict used by the Settings switch game menu
 
 # Module Cache
 _allGames = {}        # 'name' -> GameInfo
-_registryGames = {}   # 'name' -> path
-_win_store_games = {} # 'name' -> path
+_registryGames = {}   # 'name' -> list[path]
+_win_store_games = {} # 'name' -> list[path]
 
 def reset_bush_globals():
     global game
     game = None
     for d in (_allGames, _registryGames):
         d.clear()
+
+def _print_found_games(game_dict):
+    """Formats and prints the specified dictionary of game detections in a
+    human-readable way."""
+    for found_name, found_paths in dict_sort(game_dict):
+        if len(found_paths) == 1:
+            # Single path, just print the name and path
+            deprint(u' - %s: %s' % (found_name, found_paths[0]))
+        else:
+            # Multiple paths, format as a multiline list
+            deprint(u' - %s: [%s,' % (found_name, found_paths[0]))
+            remaining_paths = found_paths[1:]
+            space_padding = u' ' * (6 + len(found_name))
+            for i, found_path in enumerate(remaining_paths):
+                if i + 1 < len(remaining_paths):
+                    deprint(u'%s%s,' % (space_padding, found_path))
+                else:
+                    deprint(u'%s%s]' % (space_padding, found_path))
 
 def _supportedGames():
     """Set games supported by Bash and return their paths from the registry."""
@@ -65,17 +83,15 @@ def _supportedGames():
             game_type = getattr(module, modname).GAME_TYPE
             _allGames[game_type.displayName] = game_type
             # Get this game's install path(s)
-            registry_path = get_registry_game_path(game_type)
-            win_store_path = get_win_store_game_path(game_type)
+            registry_paths = get_registry_game_paths(game_type)
+            win_store_paths = get_win_store_game_paths(game_type)
         except (ImportError, AttributeError):
             deprint(u'Error in game support module:', modname, traceback=True)
             continue
-        # If we have the same game in multiple places, let the registry win.
-        # This shouldn't happen since the MS versions are separate WB games
-        if registry_path:
-            _registryGames[game_type.displayName] = registry_path
-        elif win_store_path:
-            _win_store_games[game_type.displayName] = win_store_path
+        if registry_paths:
+            _registryGames[game_type.displayName] = registry_paths
+        if win_store_paths:
+            _win_store_games[game_type.displayName] = win_store_paths
         del module
     # unload some modules, _supportedGames is meant to run once
     del pkgutil
@@ -90,8 +106,7 @@ def _supportedGames():
     if _registryGames:
         deprint(u'  The following installed games were found via the '
                 u'registry:')
-        for found_name, found_path in dict_sort(_registryGames):
-            deprint(u'   - %s: %s' % (found_name, found_path))
+        _print_found_games(_registryGames)
     else:
         deprint(u'  No installed games were found via the registry')
     for wrapped_line in textwrap.wrap(
@@ -102,8 +117,7 @@ def _supportedGames():
     if _win_store_games:
         deprint(u'  The following installed games with modding enabled were '
                 u'found via the Windows Store:')
-        for found_name, found_path in dict_sort(_win_store_games):
-            deprint(u'   - %s: %s' % (found_name, found_path))
+        _print_found_games(_win_store_games)
     else:
         deprint(u'  No installed games with modding enabled were found via '
                 u'the Windows Store.')
@@ -111,9 +125,13 @@ def _supportedGames():
             u'Make sure to enable mods for each Windows Store game you have '
             u'installed, otherwise Wrye Bash will not be able to find it.'):
         deprint(u'  ' + wrapped_line)
-    # Join the dicts of games we found from all global sources, PY3: dict union
+    # Merge the dicts of games we found from all global sources
     all_found_games = _registryGames.copy()
-    all_found_games.update(_win_store_games)
+    for found_game, found_paths in _win_store_games.iteritems():
+        if found_game in all_found_games:
+            all_found_games[found_game].extend(found_paths)
+        else:
+            all_found_games[found_game] = found_paths
     return all_found_games
 
 def _detectGames(cli_path=u'', bash_ini_=None):
@@ -177,33 +195,36 @@ def _detectGames(cli_path=u'', bash_ini_=None):
             if all(test_path.join(g).exists() for g in info.game_detect_files):
                 # Must be this game
                 deprint(foundMsg % {u'gamename': gamename}, test_path)
-                foundGames_[gamename] = test_path
-                return foundGames_, gamename
+                foundGames_[gamename] = [test_path]
+                return foundGames_, gamename, test_path
         # no game exe in this install path - print error message
         deprint(errorMsg % {u'path': test_path})
     # no game found in installPaths - foundGames are the ones from the registry
-    return foundGames_, None
+    return foundGames_, None, None
 
-def __setGame(gamename, msg):
+def __setGame(gamename, gamePath, msg):
     """Set bush game globals - raise if they are already set."""
     global game
     if game is not None: raise BoltError(u'Trying to reset the game')
-    gamePath = foundGames[gamename]
     game = _allGames[gamename](gamePath)
     deprint(msg % {u'gamename': gamename}, gamePath)
     # Unload the other modules from the cache
     _allGames.clear()
     game.init()
 
-def detect_and_set_game(cli_game_dir=u'', bash_ini_=None, gname=None):
+def detect_and_set_game(cli_game_dir=u'', bash_ini_=None, gname=None,
+                        gm_path=None):
     if gname is None: # detect available games
-        foundGames_, gname = _detectGames(cli_game_dir, bash_ini_)
+        foundGames_, gname, gm_path = _detectGames(cli_game_dir, bash_ini_)
         foundGames.update(foundGames_) # set the global name -> game path dict
-    if gname is not None: # try the game returned by detectGames() or specified
-        __setGame(gname, u' Using %(gamename)s game:')
+    # Try the game returned by detectGames() or specified
+    if gname is not None and gm_path is not None:
+        __setGame(gname, gm_path, u'Using %(gamename)s game:')
         return None
-    elif len(foundGames) == 1:
-        __setGame(next(iter(foundGames)), u'Single game found [%(gamename)s]:')
+    elif len(foundGames) == 1 and len(next(iter(foundGames))) == 1:
+        single_game = next(iter(foundGames))
+        __setGame(single_game, next(iter(foundGames[single_game])),
+                  u'Single game found [%(gamename)s]:')
         return None
     # No match found, return the list of possible games (may be empty if
     # nothing is found in registry)

Mopy/bash/env/linux.py
@@ -67,11 +67,11 @@ def _get_error_info():
 def get_registry_path(_subkey, _entry, _detection_files):
     return None # no registry on Linux
 
-def get_registry_game_path(_submod):
-    return None # no registry on Linux
+def get_registry_game_paths(_submod):
+    return [] # no registry on Linux
 
-def get_win_store_game_path(_submod):
-    return None # no Windows Store on Linux
+def get_win_store_game_paths(_submod):
+    return [] # no Windows Store on Linux
 
 def get_personal_path():
     return _getShellPath(u'Personal'), _get_error_info()

Mopy/bash/env/windows.py
@@ -216,7 +216,6 @@ def _query_fixed_field_version(file_name, version_prefix):
     return win32api.HIWORD(ms), win32api.LOWORD(ms), win32api.HIWORD(ls), \
            win32api.LOWORD(ls)
 
-
 class _WindowsStoreFinder(object):
     developer_ids = {
         u'Bethesda': u'3275kfvn8vcwc'
@@ -229,8 +228,10 @@ class _WindowsStoreFinder(object):
         self.location_cache = {}
         self.full_names_cache = {}
 
-    def get_package_name(self, common_name, publisher_name=None, publisher_id=None):
-        """Gets the package name from the common_name an publisher information."""
+    def get_package_name(self, common_name, publisher_name=None,
+                         publisher_id=None):
+        """Gets the package name from the common_name an publisher
+        information."""
         if not publisher_id:
             publisher_id = self.developer_ids.get(publisher_name)
         if not publisher_id:
@@ -258,7 +259,8 @@ class _WindowsStoreFinder(object):
                 for i in xrange(num_families):
                     full_names.append(winreg.EnumKey(family_key, i))
         except WindowsError as e:
-            # We're on a version of Windows that does not have the package registry
+            # We're on a version of Windows that does not have the package
+            # registry
             pass
         return full_names
 
@@ -286,11 +288,13 @@ class _WindowsStoreFinder(object):
                 with repo_key:
                     data_key = winreg.OpenKey(repo_key, package_index)
                     with data_key:
-                        ml_entry = winreg.QueryValueEx(data_key, 'MutableLocation')
+                        ml_entry = winreg.QueryValueEx(data_key,
+                                                       u'MutableLocation')
                         if ml_entry[0] and ml_entry[1] == winreg.REG_SZ:
                             locations[package_full_name] = ml_entry[0]
         except WindowsError:
-            # We're on a version of Windows that does not have the package registry
+            # We're on a version of Windows that does not have the package
+            # registry
             pass
         return locations
 
@@ -703,40 +707,43 @@ def get_registry_path(subkey, entry, detection_files):
             return installPath
     return None
 
-def get_registry_game_path(submod):
+def get_registry_game_paths(submod):
     """Check registry-supplied game paths for the game detection file(s)."""
     reg_keys = submod.regInstallKeys
     if not reg_keys:
-        return None # Game is not detectable via registry
+        return [] # Game is not detectable via registry
     subkey, entry = reg_keys
-    return get_registry_path(subkey, entry, submod.game_detect_files)
+    reg_path = get_registry_path(subkey, entry, submod.game_detect_files)
+    return [] if not reg_path else [reg_path]
 
-def get_win_store_game_path(submod):
+def get_win_store_game_paths(submod):
     """Check Windows Store-supplied game paths for the game detection
     file(s)."""
-    ## TODO(lojack): There can potentially be multiple results for a given game
-    ## due to different game version/architecture.  Right now we just take
-    ## the first one, find a good way to pick the best one.  Probably by looking
-    ## at the install time value in the registry.
     publisher_name = submod.Ws.publisher_name
     publisher_id = submod.Ws.publisher_id
     common_name = submod.Ws.win_store_name
     package_name = _win_store_finder.get_package_name(
-        common_name,
-        publisher_name,
-        publisher_id
-    )
+        common_name, publisher_name, publisher_id)
     locations = _win_store_finder.get_mutable_locations(package_name)
     if locations:
-        # Use the first location for now, see the TODO above
-        ## TODO(lojack): This is kind of ulgy, probably a better way to store
-        ## this:
+        # TODO(lojack): This is kind of ulgy, probably a better way to store
+        #  this:
         submod.Ws._package_name = package_name
-        return GPath(locations.values()[0])
+        # TODO(lojack): There can potentially be multiple results for a given
+        #  game due to different game version/architecture. Right now we just
+        #  take the first one, find a good way to pick the best one. Probably
+        #  by looking at the install time value in the registry.
+        first_location = GPath(locations.values()[0])
+        if submod.Ws.game_language_dirs:
+            language_locations = [first_location.join(l)
+                                  for l in submod.Ws.game_language_dirs]
+            return [p for p in language_locations if p.isdir()]
+        else:
+            return [first_location]
     else:
         # Should already be u'', but set it in case it's not.
         submod.Ws._package_name = u''
-        return None
+        return []
 
 def get_personal_path():
     return (GPath(_get_known_path(_FOLDERID.Documents)),

Mopy/bash/game/__init__.py
@@ -140,6 +140,11 @@ class GameInfo(object):
 
     class Ws(object):
         """Information about this game on the Windows Store."""
+        # A list of directory names for different language versions that ship
+        # with this game. Each one acts as a separate game installation under
+        # the main Windows Store path. If empty, indicates that the Windows
+        # Store location is the game installtion
+        game_language_dirs = []
         # The publisher name for common games. Currently only 'Bethesda' is
         # allowed for Bethesda games. If specified, publisher_id is not
         # required

Mopy/bash/game/ws_morrowind/__init__.py
@@ -32,5 +32,8 @@ class WSMorrowindGameInfo(MorrowindGameInfo):
     class Ws(MorrowindGameInfo.Ws):
         publisher_name = u'Bethesda'
         win_store_name = u'BethesdaSoftworks.TESMorrowind-PC'
+        game_language_dirs = [u'Morrowind GOTY English',
+                              u'Morrowind GOTY French',
+                              u'Morrowind GOTY German']
 
 GAME_TYPE = WSMorrowindGameInfo

Mopy/bash/game/ws_oblivion/__init__.py
@@ -33,5 +33,10 @@ class WSOblivionGameInfo(OblivionGameInfo):
     class Ws(OblivionGameInfo.Ws):
         publisher_name = u'Bethesda'
         win_store_name = u'BethesdaSoftworks.TESOblivion-PC'
+        game_language_dirs = [u'Oblivion GOTY English',
+                              u'Oblivion GOTY French',
+                              u'Oblivion GOTY German',
+                              u'Oblivion GOTY Italian',
+                              u'Oblivion GOTY Spanish']
 
 GAME_TYPE = WSOblivionGameInfo

commit 0ca214842163c4e89e6ea3095b9dc545bd1c84cf

Fix regression when no games are installed
Yeah, wx.App isn't a thing yet. We're in early boot.

Broke in c39d5d3a67df881a01c1213ac9277c9a6ded970f.

Mopy/bash/bash.py
@@ -478,14 +478,13 @@ def _import_bush_and_set_game(opts, bashIni):
     game_infos = bush.detect_and_set_game(opts.oblivionPath, bashIni)
     if game_infos is not None:  # None == success
         if len(game_infos) == 0:
-            balt.showError(
-                None, _(u'Wrye Bash could not find a game to manage. Make '
-                        u'sure to launch games you installed through Steam '
-                        u'once and enable mods on games you installed through '
-                        u'the Windows Store.') + u'\n\n' +
-                      _(u'You can also use the -o command line argument or '
-                        u'bash.ini to specify the path manually.'),
-                title=_(u'No Game Found'))
+            _show_boot_popup(_(
+                u'Wrye Bash could not find a game to manage. Make sure to '
+                u'launch games you installed through Steam once and enable '
+                u'mods on games you installed through the Windows '
+                u'Store.') + u'\n\n' + _(
+                u'You can also use the -o command line argument or bash.ini '
+                u'to specify the path manually.'))
             return None
         retCode = _select_game_popup(game_infos)
         if not retCode: