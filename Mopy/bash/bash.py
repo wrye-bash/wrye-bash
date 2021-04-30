wrye-bash/wrye-bash

commit d22469bc235ab67b14e14dd45e4e3ce268bd936d

Change all items/values methods to iter*/view*
This allows 2to3 to correctly handle every single iter* and view* call
in the codebase. All we'll have to manually do afterwards is get rid of
the four view*/iter* methods in DataDict and DataTableColumn. The way
this works is that we now have zero calls of items() and values() left
over, they all call iteritems(), viewitems(), etc. 2to3 will change all
of those usages back to simple items()/values() calls. For the two
classes we have that define their own items/values/etc., I made all
three methods behave like viewitems/viewvalues to match py3 as closely
as possible.

Also rename some 'key's while I'm at it. Couple of lines are now >79
characters, but 2to3 will fix those, so let's not worry about it.

Mopy/bash/basher/settings_dialog.py:
Mopy/bash/game/oblivion/patcher/checkers.py:
Couple minor warning fixes for variable shadowing

Under #460

Mopy/bash/ini_files.py
@@ -333,7 +333,7 @@ class IniFile(AFile):
             # This will occur for the last INI section in the ini file
             _add_remaining_new_items()
             # Add remaining new entries
-            for section, sectionSettings in ini_settings.items():
+            for section, sectionSettings in ini_settings.iteritems():
                 # _add_remaining_new_items may modify ini_settings
                 if sectionSettings:
                     tmpFileWrite(u'[%s]\n' % section)

Mopy/bash/balt.py
@@ -583,7 +583,7 @@ class ListEditor(DialogWindow):
             (lid_data.showSave, _(u'Save'), self.DoSave),
             (lid_data.showCancel, _(u'Cancel'), self.DoCancel),
             ]
-        for k,v in (orderedDict or {}).items():
+        for k, v in (orderedDict or {}).iteritems():
             buttonSet.append((True, k, v))
         if sum(bool(x[0]) for x in buttonSet):
             def _btn(btn_label, btn_callback):

Mopy/bash/basher/__init__.py
@@ -205,7 +205,7 @@ class SashPanel(NotebookPanel):
         """Unfortunately can't use EVT_SHOW, as the panel needs to be
         populated for position to be set correctly."""
         if self._firstShow:
-            for key, ui_set in self._ui_settings.items():
+            for key, ui_set in self._ui_settings.iteritems():
                 sashPos = settings.get(self.__class__.keyPrefix + key,
                                        ui_set.default_(self))
                 ui_set.set_(self, sashPos)
@@ -213,7 +213,7 @@ class SashPanel(NotebookPanel):
 
     def ClosePanel(self, destroy=False):
         if not self._firstShow and destroy: # if the panel was shown
-            for key, ui_set in self._ui_settings.items():
+            for key, ui_set in self._ui_settings.iteritems():
                 settings[self.__class__.keyPrefix + key] = ui_set.get_(self)
 
 class SashUIListPanel(SashPanel):
@@ -517,7 +517,7 @@ class MasterList(_ModsUIList):
     def InitEdit(self):
         #--Pre-clean
         edited = False
-        for mi, masterInfo in self.data_store.items():
+        for mi, masterInfo in self.data_store.iteritems():
             newName = settings[u'bash.mods.renames'].get(
                 masterInfo.curr_name, None)
             #--Rename?
@@ -1893,7 +1893,8 @@ class INIDetailsPanel(_DetailsMixin, SashPanel):
     @property
     def current_ini_path(self):
         """Return path of currently chosen ini."""
-        return self.target_inis.values()[settings[u'bash.ini.choice']]
+        return list(self.target_inis.itervalues())[
+            settings[u'bash.ini.choice']]
 
     @property
     def target_inis(self):
@@ -2641,7 +2642,7 @@ class InstallersList(balt.UIList):
             sorted_ = sorted(selected, key=orderKey, reverse=(moveMod == 1))
             # get the index two positions after the last or before the first
             visibleIndex = self.GetIndex(sorted_[0]) + moveMod * 2
-            maxPos = max(x.order for x in self.data_store.values())
+            maxPos = max(x.order for x in self.data_store.itervalues())
             for thisFile in sorted_:
                 newPos = self.data_store[thisFile].order + moveMod
                 if newPos < 0 or maxPos < newPos: break
@@ -3594,9 +3595,9 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
         else:
             # It was disabled, enable it
             insertAt = 0
-            for key, is_enabled in bass.settings[u'bash.tabs.order'].items():
-                if key == self.tabKey: break
-                insertAt += is_enabled
+            for k, k_enabled in bass.settings[u'bash.tabs.order'].iteritems():
+                if k == self.tabKey: break
+                insertAt += k_enabled
             className,title,panel = tabInfo[self.tabKey]
             if not panel:
                 panel = globals()[className](Link.Frame.notebook)
@@ -3624,6 +3625,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         """Return dict containing saved tab order and enabled state of tabs."""
         newOrder = settings.getChanged(u'bash.tabs.order',
                                        BashNotebook._tabs_enabled_ordered)
+        # FIXME(inf) Backwards compatibility with <306, drop on VDATA3
         if not isinstance(newOrder, OrderedDict): # convert, on updating to 306
             enabled = settings.getChanged(u'bash.tabs', # deprecated -never use
                                           BashNotebook._tabs_enabled_ordered)
@@ -3647,7 +3649,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         balt.TabDragMixin.__init__(self)
         #--Pages
         iInstallers = iMods = -1
-        for page, enabled in self._tabOrder().items():
+        for page, enabled in self._tabOrder().iteritems():
             if not enabled: continue
             className, title, item = tabInfo[page]
             panel = globals().get(className,None)
@@ -4202,9 +4204,10 @@ class BashFrame(WindowFrame):
         modNames = set(bosh.modInfos)
         modNames.update(bosh.modInfos.table)
         renames = bass.settings.getChanged(u'bash.mods.renames')
-        for key,value in renames.items():
-            if value not in modNames:
-                del renames[key]
+        # Make a copy, we may alter it in the loop
+        for old_mname, new_mname in list(renames.iteritems()):
+            if new_mname not in modNames:
+                del renames[old_mname]
         #--Clean colors dictionary
         currentColors = set(settings[u'bash.colors'])
         defaultColors = set(settingDefaults[u'bash.colors'])

Mopy/bash/basher/constants.py
@@ -489,7 +489,7 @@ installercons.images.extend({
     u'on.yellow.dir.wiz': _png(u'diamond_yellow_inc_wiz.png'),
     #--Broken
     u'corrupt':   _png(u'red_x.png'),
-}.iteritems())
+}.viewitems())
 
 #--Buttons
 def imageList(template):

Mopy/bash/basher/frames.py
@@ -48,7 +48,7 @@ class DocBrowser(WindowFrame):
         self._db_is_editing = bosh.modInfos.table.getColumn(u'docEdit')
         self._doc_is_wtxt = False
         # Clean data
-        for mod_name, doc in self._db_doc_paths.items():
+        for mod_name, doc in list(self._db_doc_paths.iteritems()):
             if not isinstance(doc, bolt.Path):
                 self._db_doc_paths[mod_name] = GPath(doc)
         # Singleton

Mopy/bash/basher/mod_links.py
@@ -451,7 +451,7 @@ class _ModGroups(CsvParser):
     def assignedGroups():
         """Return all groups that are currently assigned to mods."""
         column = bosh.modInfos.table.getColumn(u'group')
-        return {x[1] for x in column.items() if x[1]} #x=(bolt.Path,'group')
+        return {x for x in column.itervalues() if x}
 
     def writeToModInfos(self,mods=None):
         """Exports mod groups to modInfos."""
@@ -543,8 +543,8 @@ class Mod_Groups(_Mod_Labels):
     def _initData(self, window, selection):
         super(Mod_Groups, self)._initData(window, selection)
         selection = set(selection)
-        mod_group = bosh.modInfos.table.getColumn(u'group').items()
-        modGroup = {x[1] for x in mod_group if x[0] in selection}
+        mod_group = bosh.modInfos.table.getColumn(u'group')
+        modGroup = {x[1] for x in mod_group.iteritems() if x[0] in selection}
         class _CheckGroup(CheckLink, self.__class__.choiceLinkType):
             def _check(self):
                 """Check the Link if any of the selected mods belongs to it."""

Mopy/bash/basher/mods_links.py
@@ -273,7 +273,7 @@ class Mods_CleanDummyMasters(EnabledLink):
     _help = _(u"Clean up after using a 'Create Dummy Masters...' command")
 
     def _enable(self):
-        for fileInfo in bosh.modInfos.values():
+        for fileInfo in bosh.modInfos.itervalues():
             if fileInfo.header.author == u'BASHED DUMMY':
                 return True
         return False

Mopy/bash/basher/saves_links.py
@@ -495,7 +495,7 @@ class Save_EditCreatedData(balt.ListEditorData):
         else:
             self.changed = False #--Allows graceful effort if close fails.
             count = 0
-            for newName,(oldName,records) in self.name_nameRecords.items():
+            for newName,(oldName,records) in self.name_nameRecords.iteritems():
                 if newName == oldName: continue
                 for record in records:
                     record.full = newName

commit ca3e460f06450380a6e6dd3bb38fd8a0185463c4

Fix regression when applying INI tweaks
And when doing any LO modification in Morrowind.

Broke in d22469bc235ab67b14e14dd45e4e3ce268bd936d.

Mopy/bash/bash.py:
Fix regression in BashBugDump

Was not writing any tracebacks to the log because io.open wants a
unicode object but Python's traceback handler prints str objects.
Grr, py3...

Broke in c120e18a43c2f5abb31d579c99f211e7aee6b322.

Mopy/bash/bash.py
@@ -28,6 +28,7 @@ loop."""
 # Imports ---------------------------------------------------------------------
 from __future__ import print_function
 import atexit
+import codecs
 import io
 import os
 import platform
@@ -64,9 +65,13 @@ def _early_setup(debug):
         # Also, setup stdout/stderr to the debug log if debug mode /
         # standalone before wxPython is up
         global _bugdump_handle
-        _bugdump_handle = io.open(
-            os.path.join(os.getcwdu(), u'BashBugDump.log'), u'w', buffering=1,
-            encoding=u'utf-8')
+        # PY3: Replace with this
+        # _bugdump_handle = io.open(
+        #     os.path.join(os.getcwdu(), u'BashBugDump.log'), u'w', buffering=1,
+        #     encoding=u'utf-8')
+        _bugdump_handle = codecs.getwriter(u'utf-8')(
+            open(os.path.join(os.getcwdu(), u'BashBugDump.log'), u'w',
+                 buffering=0))
         sys.stdout = _bugdump_handle
         sys.stderr = _bugdump_handle
 

Mopy/bash/ini_files.py
@@ -332,7 +332,7 @@ class IniFile(AFile):
             # This will occur for the last INI section in the ini file
             _add_remaining_new_items()
             # Add remaining new entries
-            for section, sectionSettings in ini_settings.iteritems():
+            for section, sectionSettings in list(ini_settings.iteritems()):
                 # _add_remaining_new_items may modify ini_settings
                 if sectionSettings:
                     tmpFileWrite(u'[%s]\n' % section)