wrye-bash/wrye-bash

commit b925fc720df315c80884859293b0d1cbebbaf9a2

Cache ESM flag status
Can't cache the entire ESM status since that's in load_order/_games_lo.
Still, this does chop ~500 function calls off.

Mopy/bash/bosh/__init__.py
@@ -349,7 +349,7 @@ reBashTags = re.compile(u'{{ *BASH *:[^}]*}}\\s*\\n?',re.U)
 
 class ModInfo(FileInfo):
     """A plugin file. Currently, these are .esp, .esm, .esl and .esu files."""
-    _is_esl = False # Cached, since we need it so often
+    _has_esm_flag = _is_esl = False # Cached, since we need it so often
 
     def __init__(self, fullpath, load_cache=False):
         self.isGhost = endsInGhost = (fullpath.cs[-6:] == u'.ghost')
@@ -364,6 +364,7 @@ class ModInfo(FileInfo):
         # check if we have a cached crc for this file, use fresh mtime and size
         if load_cache: self.calculate_crc() # for added and hopefully updated
         if bush.game.has_esl: self._recalc_esl()
+        self._recalc_esm()
 
     def getFileInfos(self): return modInfos
 
@@ -374,14 +375,19 @@ class ModInfo(FileInfo):
     def has_esm_flag(self):
         """Check if the mod info is a master file based on master flag -
         header must be set"""
-        return self.header.flags1.esm
+        return self._has_esm_flag
 
     def set_esm_flag(self, new_esm_flag):
         """Changes this file's ESM flag to the specified value. Recalculates
         ONAM info if necessary."""
         self.header.flags1.esm = new_esm_flag
         self.update_onam()
+        self._recalc_esm()
         self.writeHeader()
+
+    def _recalc_esm(self):
+        """Forcibly recalculates the cached ESM status."""
+        self._has_esm_flag = self.header.flags1.esm
 
     def has_esl_flag(self):
         """Check if the mod info is an ESL based on ESL flag alone - header

commit b2d7af06f1815ab2ffa923f9508c2750c0028e56

Fix ONAM not being updated when changing ESM flag
Broke in b925fc720df315c80884859293b0d1cbebbaf9a2.

Mopy/bash/bosh/__init__.py
@@ -517,8 +517,8 @@ class ModInfo(FileInfo):
         """Changes this file's ESM flag to the specified value. Recalculates
         ONAM info if necessary."""
         self.header.flags1.esm = new_esm_flag
-        self.update_onam()
         self._recalc_esm()
+        self.update_onam()
         self.writeHeader()
 
     def _recalc_esm(self):