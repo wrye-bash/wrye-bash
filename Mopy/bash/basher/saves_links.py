wrye-bash/wrye-bash

commit 4d031ef55bc08003ff1f33df50ca4c0d245db281

[!]WIP parsers fixups - move de/serializers to records:
This starts looking like something - in game handling I mean, bush uses
in parsers went down.

! cause last minute changes (like common _parse_line for
Sigil/Ingredients - already caught a bug that hit nightly in ItemPrices,
was still expecting a list - that's shelved functionality btw)

Note:

- spellType is never set to None ? so why have those values in the
dict ? Or should we use int_or_none?
-
+        return attr_dex if self._called_from_patcher else (
+            attr_dex[u'eid'], attr_dex[u'full'])
Use a dict here for write rows also! (done)
- int_or_none(self.fid_stats[mid][att]) - why? - was applied to uses,
value - but a noop - probably a regression introduced in
0179e94c8beaced23290dcd33933d626e028e738 - grr _coerce(uses,int)
- Mopy/bash/bolt.py: rename 'level' - yet another forbidden name
- more parse lines gone
- regression from Rounder()

@@ -922,10 +922,11 @@ def _serialize(c):
                     return u'"%s"' % val
                 elif tval is int:
                     return u'"%d"' % val
                 elif tval is float:
                     return u'"%f"' % val
+                return u'"%s"' % val
             return _serialize

Note type was Rounder here so this would return None - real fix was to
move deserializers to records too.

Lots of intermediate refactoring helpers removed here - especially
welcome the removal of _float_attrs/_int_attrs

WIP:
- wrap 'eid' to CIStr
- check the MreSpell dicts - is None needed?
- _write_record needs unification between AParser and HandleAliases

Mopy/bash/bosh/faces.py
@@ -494,7 +494,7 @@ class PCFaces(object):
         npc.fgga_p = face.fgga_p
         npc.fgts_p = face.fgts_p
         #--Stats
-        npc.level = face.level
+        npc.level_offset = face.level
         npc.baseSpell = face.baseSpell
         npc.fatigue = face.fatigue
         if face.skills: npc.skills = face.skills

Mopy/bash/bolt.py
@@ -264,12 +264,6 @@ def int_or_none(uni_str):
     except ValueError:
         return None
 
-def nonzero_or_none(uni_str):
-    try:
-        return int(uni_str) or None
-    except (ValueError, TypeError):
-        return None
-
 def int_or_zero(uni_str):
     try:
         return int(uni_str)
@@ -2530,7 +2524,6 @@ class WryeText(object):
         reCssTag = re.compile(u'' r'\s*{{CSS:(.+?)}}\s*$',re.U)
         #--Defaults ----------------------------------------------------------
         title = u''
-        level = 1
         spaces = u''
         cssName = None
         #--Init
@@ -2634,7 +2627,7 @@ class WryeText(object):
                 text = re.sub(u' *=*#?$', u'', text.strip())
                 anchor = unicode(quote(reWd.sub(u'', text).encode(u'utf8')),
                                  u'cp1252')
-                level = len(lead)
+                level_ = len(lead)
                 if anchorHeaders:
                     if re.match(u'' r'\d', anchor):
                         anchor = u'_' + anchor
@@ -2646,12 +2639,12 @@ class WryeText(object):
                         else:
                             anchor = anchor[:-1] + unicode(count)
                     anchorlist.append(anchor)
-                    line = (headFormatNA,headFormat)[anchorHeaders] % (level,anchor,text,level)
-                    if addContents: contents.append((level,anchor,text))
+                    line = (headFormatNA,headFormat)[anchorHeaders] % (level_,anchor,text,level_)
+                    if addContents: contents.append((level_,anchor,text))
                 else:
-                    line = headFormatNA % (level,text,level)
+                    line = headFormatNA % (level_,text,level_)
                 #--Title?
-                if not title and level <= 2: title = text
+                if not title and level_ <= 2: title = text
             #--Paragraph
             elif maPar and not states[u'code']:
                 line = u'<p>'+line+u'</p>\n'
@@ -2662,8 +2655,8 @@ class WryeText(object):
                 text = maList.group(3)
                 if bullet == u'.': bullet = u'&nbsp;'
                 elif bullet == u'*': bullet = u'&bull;'
-                level = len(spaces)//2 + 1
-                line = spaces+u'<p class="list-%i">'%level+bullet+u'&nbsp; '
+                level_ = len(spaces)//2 + 1
+                line = spaces+u'<p class="list-%i">'%level_+bullet+u'&nbsp; '
                 line = line + text + u'</p>\n'
             #--Empty line
             elif maEmpty:
@@ -2705,11 +2698,11 @@ class WryeText(object):
         for line in outLines:
             if reContentsTag.match(line):
                 if contents and not didContents:
-                    baseLevel = min([level for (level,name_,text) in contents])
-                    for (level,name_,text) in contents:
-                        level = level - baseLevel + 1
-                        if level <= addContents:
-                            outWrite(u'<p class="list-%d">&bull;&nbsp; <a href="#%s">%s</a></p>\n' % (level,name_,text))
+                    baseLevel = min([level_ for (level_,name_,text) in contents])
+                    for (level_,name_,text) in contents:
+                        level_ = level_ - baseLevel + 1
+                        if level_ <= addContents:
+                            outWrite(u'<p class="list-%d">&bull;&nbsp; <a href="#%s">%s</a></p>\n' % (level_,name_,text))
                     didContents = True
             else:
                 outWrite(line)

Mopy/bash/brec/record_structs.py
@@ -35,6 +35,109 @@ from .mod_io import ModReader
 from .utils_constants import strFid, _int_unpacker
 from .. import bolt, exception
 from ..bolt import decoder, struct_pack
+from ..bolt import float_or_none, int_or_zero, str_or_none
+
+def _str_to_bool(value, __falsy=frozenset(
+    [u'', u'none', u'false', u'no', u'0', u'0.0'])):
+    return value.strip().lower() not in __falsy
+
+# Cross game dict, mapping attribute -> csv (de)serializer/csv column header
+attr_csv_struct = {
+    u'weight': [float_or_none, _(u'Weight')],
+    u'rumbleRightMotorStrength': [
+        float_or_none, _(u'Rumble - Right Motor Strength')],
+    u'criticalDamage': [int_or_zero, _(u'Critical Damage')],
+    u'aimArc': [float_or_none, _(u'Aim Arc')],
+    u'dr': [int_or_zero, _(u'Damage Resistance')],
+    u'duration': [int_or_zero, _(u'Duration')],
+    u'attackShotsPerSec': [float_or_none, _(u'Attack Shots Per Second')],
+    u'speed': [float_or_none, _(u'Speed')],
+    u'semiAutomaticFireDelayMin': [
+        float_or_none, _(u'Minumum Semi-Automatic Fire Delay')],
+    u'minSpread': [float_or_none, _(u'Minimum Spread')],
+    u'minRange': [float_or_none, _(u'Minimum Range')],
+    u'baseVatsToHitChance': [int_or_zero, _(u'Base VATS To-Hit Chance')],
+    u'clipsize': [int_or_zero, _(u'Clip Size')],
+    u'reloadTime': [float_or_none, _(u'Reload Time')],
+    u'rumbleDuration': [float_or_none, _(u'Rumble - Duration')],
+    u'damage': [int_or_zero, _(u'Damage')],
+    u'sightUsage': [float_or_none, _(u'Sight Usage')],
+    u'sightFov': [float_or_none, _(u'Sight Fov')],
+    u'strengthReq': [int_or_zero, _(u'Strength Requirement')],
+    u'fireRate': [float_or_none, _(u'Fire Rate')],
+    u'skillReq': [int_or_zero, _(u'Skill Requirement')],
+    u'projPerShot': [int_or_zero, _(u'Projectiles Per Shot')],
+    u'regenRate': [float_or_none, _(u'Regeneration Rate')],
+    u'animationMultiplier': [float_or_none, _(u'Animation Multiplier')],
+    u'spread': [float_or_none, _(u'Spread')],
+    u'health': [int_or_zero, _(u'Health')],
+    u'semiAutomaticFireDelayMax': [
+        float_or_none, _(u'Maximum Semi-Automatic Fire Delay')],
+    u'rumbleWavelength': [float_or_none, _(u'Rumble - Wavelength')],
+    u'vatsSkill': [float_or_none, _(u'VATS Skill')],
+    u'vatsDamMult': [float_or_none, _(u'VATS Damage Multiplier')],
+    u'projectileCount': [int_or_zero, _(u'Projectile Count')],
+    u'limbDmgMult': [float_or_none, _(u'Limb Damage Multiplier')],
+    u'killImpulse': [float_or_none, _(u'Kill Impulse')],
+    u'reach': [float_or_none, _(u'Reach')],
+    u'vatsAp': [float_or_none, _(u'VATS AP')],
+    u'clipRounds': [int_or_zero, _(u'Clip Rounds')],
+    u'jamTime': [float_or_none, _(u'Jam Time')],
+    u'dt': [float_or_none, _(u'Damage Threshold')],
+    u'criticalMultiplier': [float_or_none, _(u'Critical Multiplier')],
+    u'maxRange': [float_or_none, _(u'Maximum Range')],
+    u'rumbleLeftMotorStrength': [
+        float_or_none, _(u'Rumble - Left Motor Strength')],
+    u'ammoUse': [int_or_zero, _(u'Ammunition Use')],
+    u'value': [int_or_zero, _(u'Value')],
+    u'eid': [str_or_none, u'EditorID'],
+    u'animationAttackMultiplier': [
+        float_or_none, _(u'Animation Attack Multiplier')],
+    u'overrideActionPoint': [float_or_none, _(u'Override - Action Point')],
+    u'impulseDist': [float_or_none, _(u'Impulse Distance')],
+    u'overrideDamageToWeaponMult': [
+        float_or_none, _(u'Override - Damage To Weapon Multiplier')],
+    u'strength': [int_or_zero, _(u'Strength')],
+    u'quality': [float_or_none, _(u'Quality')],
+    u'enchantPoints': [int_or_zero, _(u'Enchantment Points')],
+    u'uses': [int_or_zero, _(u'Uses')],
+    u'armorRating': [int_or_zero, _(u'Armor Rating')],
+    u'stagger': [float_or_none, _(u'Stagger')],
+    u'critDamage': [int_or_zero, _(u'Critical Damage')],
+    u'criticalEffect': [int_or_zero, _(u'Critical Effect')],
+    # some that are not used in ItemStats
+    u'offset': [int_or_zero, _(u'Offset')],
+    u'calcMin': [int_or_zero, _(u'CalcMin')],
+    u'calcMax': [int_or_zero, _(u'CalcMax')],
+    u'full': [str_or_none, _(u'Name')],
+    u'model.modPath': [str_or_none, _(u'Model Path')],
+    u'model.modb': [float_or_none, _(u'Bound Radius')],
+    u'iconPath': [str_or_none, _(u'Icon Path')],
+    u'cost': [int_or_zero, _(u'Manual Cost')],
+    u'level': [int_or_zero, _(u'Level Type')],
+    u'spellType': [int_or_zero, _(u'Spell Type')],
+    u'flags': [int_or_zero, _(u'Spell Flags')],
+    u'level_offset': [int_or_zero, _(u'Offset')],
+    u'flags.ignoreLOS': [_str_to_bool, _(u'Area Effect Ignores LOS')],
+    u'flags.startSpell': [_str_to_bool, _(u'Start Spell')],
+    u'flags.disallowAbsorbReflect': [
+        _str_to_bool, _(u'Disallow Absorb and Reflect')],
+    u'flags.noAutoCalc': [_str_to_bool, _(u'Manual Cost')],
+    u'flags.immuneToSilence': [_str_to_bool, _(u'Immune To Silence')],
+    u'flags.touchExplodesWOTarget': [
+        _str_to_bool, _(u'Touch Explodes Without Target')],
+    u'flags.scriptEffectAlwaysApplies': [
+        _str_to_bool, _(u'Script Always Applies')],
+}
+
+for _k, _v in attr_csv_struct.iteritems():
+    if _v[0] is int_or_zero: # should also cover Flags
+        _v.append(lambda x: u'"%d"' % x)
+    else: # also covers floats which should be wrapped in Rounder (see __str__)
+        _v.append(lambda x: u'"%s"' % x)
+del _k, _v
+attr_csv_struct[u'enchantPoints'][2] = lambda x: ( # can be None
+            u'"%d"' % x) if x is not None else u'"None"'
 
 #------------------------------------------------------------------------------
 # Mod Element Sets ------------------------------------------------------------
@@ -444,6 +547,18 @@ class MreRecord(object):
             value = bolt.cstrip(subrec.mel_data)
             break
         return decoder(value)
+
+    # Classmethods ------------------------------------------------------------
+    @classmethod
+    def parse_csv_line(cls, index_dict, csv_fields, reuse=False):
+        if not reuse:
+            attr_dict = {att: attr_csv_struct[att][0](csv_fields[dex]) for
+                         att, dex in index_dict.iteritems()}
+            return attr_dict
+        else:
+            for att, dex in index_dict.iteritems():
+                index_dict[att] = attr_csv_struct[att][0](csv_fields[dex])
+            return index_dict
 
 #------------------------------------------------------------------------------
 class MelRecord(MreRecord):

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -582,7 +582,7 @@ actor_importer_attrs = {
                          u'flags.pcLevelOffset', u'flags.respawn',
                          u'flags.swims', u'flags.tiltFrontBack',
                          u'flags.tiltLeftRight', u'flags.walks',
-                         u'flags.weaponAndShield', u'karma', u'level',
+                         u'flags.weaponAndShield', u'karma', u'level_offset',
                          u'speedMultiplier'),
         u'Actors.AIData': (u'aggression', u'aggroRadius',
                            u'aggroRadiusBehavior', u'assistance',
@@ -611,7 +611,7 @@ actor_importer_attrs = {
                          u'flags.noKnockDown', u'flags.noLowLevel',
                          u'flags.noRotatingHeadTrack', u'flags.notPushable',
                          u'flags.noVATSMelee', u'flags.pcLevelOffset',
-                         u'flags.respawn', u'karma', u'level',
+                         u'flags.respawn', u'karma', u'level_offset',
                          u'speedMultiplier'),
         u'Actors.AIData': (u'aggression', u'aggroRadius',
                            u'aggroRadiusBehavior', u'assistance',

Mopy/bash/game/fallout3/records.py
@@ -989,7 +989,7 @@ class MreCrea(MreActor):
         MelBodyParts(),
         MelBase(b'NIFT','nift_p'), # Texture File Hashes
         MelStruct(b'ACBS', [u'I', u'2H', u'h', u'3H', u'f', u'h', u'H'],(_flags, u'flags'),'fatigue',
-            'barterGold',('level',1),'calcMin','calcMax','speedMultiplier',
+            'barterGold',('level_offset',1),'calcMin','calcMax','speedMultiplier',
             'karma', 'dispositionBase',
             (MreActor.TemplateFlags, 'templateFlags')),
         MelFactions(),
@@ -2098,7 +2098,7 @@ class MreNpc(MreActor):
         MelModel(),
         MelStruct(b'ACBS', [u'I', u'2H', u'h', u'3H', u'f', u'2H'],
             (_flags, u'flags'),'fatigue','barterGold',
-            ('level',1),'calcMin','calcMax','speedMultiplier','karma',
+            ('level_offset',1),'calcMin','calcMax','speedMultiplier','karma',
             'dispositionBase', (MreActor.TemplateFlags, u'templateFlags')),
         MelFactions(),
         MelFid(b'INAM','deathItem'),

Mopy/bash/game/falloutnv/patcher/__init__.py
@@ -248,7 +248,7 @@ actor_importer_attrs[b'NPC_'][u'Actors.ACBS'] = ( # FO3 + flags.autocalcService
     u'flags.noBloodDecal', u'flags.noBloodSpray', u'flags.noKnockDown',
     u'flags.noLowLevel', u'flags.noRotatingHeadTrack', u'flags.notPushable',
     u'flags.noVATSMelee', u'flags.pcLevelOffset', u'flags.respawn', u'karma',
-    u'level', u'speedMultiplier')
+    u'level_offset', u'speedMultiplier')
 
 #------------------------------------------------------------------------------
 # Tweak Assorted

Mopy/bash/game/oblivion/patcher/__init__.py
@@ -512,7 +512,7 @@ actor_importer_attrs = {
                          u'flags.noRightArm', u'flags.noShadow',
                          u'flags.pcLevelOffset', u'flags.respawn',
                          u'flags.swims', u'flags.walks',
-                         u'flags.weaponAndShield', u'level'),
+                         u'flags.weaponAndShield', u'level_offset'),
         u'Actors.AIData': (u'aggression', u'confidence', u'energyLevel',
                            u'responsibility', u'services', u'trainLevel',
                            u'trainSkill'),
@@ -535,7 +535,7 @@ actor_importer_attrs = {
                          u'flags.female', u'flags.noLowLevel',
                          u'flags.noPersuasion', u'flags.noRumors',
                          u'flags.pcLevelOffset', u'flags.respawn',
-                         u'flags.summonable', u'level',),
+                         u'flags.summonable', u'level_offset',),
         u'Actors.AIData': (u'aggression', u'confidence', u'energyLevel',
                            u'responsibility', u'services', u'trainSkill',
                            u'trainLevel'),

Mopy/bash/game/oblivion/records.py
@@ -43,7 +43,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelRefScale, MelMapMarker, MelActionFlags, MelPartialCounter, MelScript, \
     MelDescription, BipedFlags, MelSpells, MelUInt8Flags, MelUInt32Flags, \
     SignatureDecider, MelRaceData, MelFactions, MelActorSounds, \
-    MelWeatherTypes, MelFactionRanks, MelLscrLocations
+    MelWeatherTypes, MelFactionRanks, MelLscrLocations, attr_csv_struct
 # Set brec MelModel to the one for Oblivion
 if brec.MelModel is None:
 
@@ -820,7 +820,7 @@ class MreCrea(MreActorBase):
         MelBase(b'NIFT','nift_p'), # Texture File Hashes
         MelStruct(b'ACBS', [u'I', u'3H', u'h', u'2H'],
             (_flags, u'flags'),'baseSpell','fatigue','barterGold',
-            ('level',1),'calcMin','calcMax'),
+            ('level_offset',1),'calcMin','calcMax'),
         MelFactions(),
         MelFid(b'INAM','deathItem'),
         MelScript(),
@@ -1372,7 +1372,7 @@ class MreNpc(MreActorBase):
         MelModel(),
         MelStruct(b'ACBS', [u'I', u'3H', u'h', u'2H'],
             (_flags, u'flags'),'baseSpell','fatigue','barterGold',
-            ('level',1),'calcMin','calcMax'),
+            ('level_offset',1),'calcMin','calcMax'),
         MelFactions(),
         MelFid(b'INAM','deathItem'),
         MelFid(b'RNAM','race'),
@@ -1853,6 +1853,27 @@ class MreSoun(MelRecord):
 class MreSpel(MelRecord,MreHasEffects):
     """Spell."""
     rec_sig = b'SPEL'
+    spellTypeNumber_Name = {None: u'NONE', ##: TODO do I ever get None ?
+                            0   : u'Spell',
+                            1   : u'Disease',
+                            2   : u'Power',
+                            3   : u'LesserPower',
+                            4   : u'Ability',
+                            5   : u'Poison'}
+    spellTypeName_Number = {y.lower(): x for x, y in
+                            spellTypeNumber_Name.iteritems() if x is not None}
+    levelTypeNumber_Name = {None : u'NONE',
+                            0    : u'Novice',
+                            1    : u'Apprentice',
+                            2    : u'Journeyman',
+                            3    : u'Expert',
+                            4    : u'Master'}
+    levelTypeName_Number = {y.lower(): x for x, y in
+                            levelTypeNumber_Name.iteritems() if x is not None}
+    attr_csv_struct[u'level'][2] = \
+        lambda val: u'"%s"' % MreSpel.levelTypeNumber_Name.get(val, val)
+    attr_csv_struct[u'spellType'][2] = \
+        lambda val: u'"%s"' % MreSpel.spellTypeNumber_Name.get(val, val)
 
     class SpellFlags(Flags):
         """For SpellFlags, immuneToSilence activates bits 1 AND 3."""
@@ -1876,6 +1897,17 @@ class MreSpel(MelRecord,MreHasEffects):
         MelEffectsObmeFull(),
     ).with_distributor(_effects_distributor)
     __slots__ = melSet.getSlotsUsed()
+
+    @classmethod
+    def parse_csv_line(cls, index_dict, csv_fields, reuse=False):
+        attr_dict = super(MreSpel, cls).parse_csv_line(index_dict, csv_fields,
+                                                       reuse)
+        attr_dict[u'level'] = cls.levelTypeName_Number.get(
+            attr_dict[u'level'].lower(), attr_dict[u'level'])
+        attr_dict[u'spellType'] = cls.spellTypeName_Number.get(
+            attr_dict[u'spellType'].lower(), attr_dict[u'spellType'])
+        attr_dict[u'flags'] = cls._SpellFlags(attr_dict.get(u'flags', 0))
+        return attr_dict
 
 class MreStat(MelRecord):
     """Static."""

commit 5d0482d7c8866f474db1b270d8bd3434d725c5b7

Fix regression in save face editing
Broke in 4d031ef55bc08003ff1f33df50ca4c0d245db281, which renamed 'level'
-> 'level_offset' in NPC records. I could have reverted that, but I
decided to simply take it further and rename the usages of 'level' in
saves/faces code as well.

Also contains a followup fix for
0164fff4bd35a799b33a5f6bfe347388ef638c85, which fixed a regression only
when reading saves but forgot to fix that same regression for writing
saves.

Mopy/bash/basher/saves_links.py
@@ -902,7 +902,8 @@ class Save_UpdateNPCLevels(EnabledLink):
                 for npc in modFile.tops[b'NPC_'].getActiveRecords():
                     fid = mapToOrdered(short_mapper(npc.fid), None)
                     if not fid: continue
-                    npc_info[fid] = (npc.eid, npc.level, npc.calcMin, npc.calcMax, npc.flags.pcLevelOffset)
+                    npc_info[fid] = (npc.eid, npc.level_offset, npc.calcMin,
+                                     npc.calcMax, npc.flags.pcLevelOffset)
             #--Loop over savefiles
             subProgress = SubProgress(progress,0.4,1.0,len(self.selected))
             message = _(u'NPCs Releveled:')
@@ -918,17 +919,18 @@ class Save_UpdateNPCLevels(EnabledLink):
                         enumerate(records):
                     orderedRecId = mapToOrdered(recId,None)
                     if recType_ != 35 or recId == 7 or orderedRecId not in npc_info: continue
-                    (eid,level,calcMin,calcMax,pcLevelOffset) = npc_info[orderedRecId]
+                    (eid, level_offset, calcMin, calcMax,
+                     pcLevelOffset) = npc_info[orderedRecId]
                     npc = bosh._saves.SreNPC(recFlags, data_)
                     acbs = npc.acbs
                     if acbs and (
-                        (acbs.level != level) or
+                        (acbs.level_offset != level_offset) or
                         (acbs.calcMin != calcMin) or
                         (acbs.calcMax != calcMax) or
                         (acbs.flags.pcLevelOffset != pcLevelOffset)
                         ):
                         acbs.flags.pcLevelOffset = pcLevelOffset
-                        acbs.level = level
+                        acbs.level_offset = level_offset
                         acbs.calcMin = calcMin
                         acbs.calcMax = calcMax
                         (recId,recType_,recFlags,version,data_) = saveFile.records[recNum]

Mopy/bash/bosh/_saves.py
@@ -65,7 +65,7 @@ class SreNPC(object):
 
     class ACBS(object):
         __slots__ = (u'flags', u'baseSpell', u'fatigue', u'barterGold',
-                     u'level', u'calcMin', u'calcMax')
+                     u'level_offset', u'calcMin', u'calcMax')
 
     def __init__(self, sre_flags=0, data_=None):
         for attr in self.__slots__:
@@ -76,8 +76,8 @@ class SreNPC(object):
         """Returns a default version. Only supports acbs."""
         assert attr == u'acbs'
         acbs = SreNPC.ACBS()
-        (acbs.flags, acbs.baseSpell, acbs.fatigue, acbs.barterGold, acbs.level,
-                acbs.calcMin, acbs.calcMax) = (0,0,0,0,1,0,0)
+        (acbs.flags, acbs.baseSpell, acbs.fatigue, acbs.barterGold,
+         acbs.level_offset, acbs.calcMin, acbs.calcMax) = (0,0,0,0,1,0,0)
         acbs.flags = MreRecord.type_class[b'NPC_']._flags(acbs.flags)
         return acbs
 
@@ -94,7 +94,8 @@ class SreNPC(object):
         if sr_flags.acbs:
             acbs = self.acbs = SreNPC.ACBS()
             (acbs.flags, acbs.baseSpell, acbs.fatigue, acbs.barterGold,
-             acbs.level, acbs.calcMin, acbs.calcMax) = _unpack(u'=I3Hh2H', 16)
+             acbs.level_offset, acbs.calcMin,
+             acbs.calcMax) = _unpack(u'=I3Hh2H', 16)
             acbs.flags = MreRecord.type_class[b'NPC_']._flags(acbs.flags)
         if sr_flags.factions:
             num = unpack_short(ins)
@@ -120,7 +121,7 @@ class SreNPC(object):
         for attr in SreNPC.__slots__:
             if attr != u'unused2':
                 setattr(sr_flags, attr, getattr(self, attr) is not None)
-        return int(sr_flags)
+        return sr_flags.dump()
 
     def getData(self):
         """Returns self.data."""
@@ -136,8 +137,9 @@ class SreNPC(object):
         #--Acbs
         if self.acbs is not None:
             acbs = self.acbs
-            _pack(u'=I3Hh2H',int(acbs.flags), acbs.baseSpell, acbs.fatigue,
-                  acbs.barterGold, acbs.level, acbs.calcMin, acbs.calcMax)
+            _pack(u'=I3Hh2H', acbs.flags.dump(), acbs.baseSpell, acbs.fatigue,
+                  acbs.barterGold, acbs.level_offset, acbs.calcMin,
+                  acbs.calcMax)
         #--Factions
         if self.factions is not None:
             pack_short(out, len(self.factions))
@@ -376,10 +378,12 @@ class SaveFile(object):
             _pack(u'I',fidsPos)
             out.seek(fidsPos)
             _pack(u'I',len(self.fids))
-            self.fids.tofile(out)
+            # PY3: self.fids.tofile(out)
+            out.write(self.fids.tostring())
             #--Worldspaces
             _pack(u'I',len(self.worldSpaces))
-            self.worldSpaces.tofile(out)
+            # PY3: self.worldSpaces.tofile(out)
+            out.write(self.worldSpaces.tostring())
             #--Done
             progress(1.0,_(u'Writing complete.'))
 

Mopy/bash/bosh/faces.py
@@ -45,7 +45,7 @@ class PCFaces(object):
         __slots__ = (
             u'face_masters', u'eid', u'pcName', u'race', u'gender', u'eye',
             u'hair', u'hairLength', u'hairRed', u'hairBlue', u'hairGreen',
-            u'unused3', u'fggs_p', u'fgga_p', u'fgts_p', u'level',
+            u'unused3', u'fggs_p', u'fgga_p', u'fgts_p', u'level_offset',
             u'attributes', u'skills', u'health', u'unused2', u'baseSpell',
             u'fatigue', u'iclass', u'factions', u'modifiers', u'spells')
 
@@ -55,7 +55,8 @@ class PCFaces(object):
             self.fggs_p = self.fgts_p = b'\x00'*4*50
             self.fgga_p = b'\x00'*4*30
             self.unused2 = null2
-            self.health = self.unused3 = self.baseSpell = self.fatigue = self.level = 0
+            self.health = self.unused3 = self.baseSpell = self.fatigue = 0
+            self.level_offset = 0
             self.skills = self.attributes = self.iclass = None
             self.factions = []
             self.modifiers = []
@@ -132,9 +133,9 @@ class PCFaces(object):
             face.face_masters = saveFile._masters
             for a in (u'eid', u'race', u'eye', u'hair', u'hairLength',
                       u'hairRed', u'hairBlue', u'hairGreen', u'unused3',
-                      u'fggs_p', u'fgga_p', u'fgts_p', u'level', u'skills',
-                      u'health', u'unused2', u'baseSpell', u'fatigue',
-                      u'attributes', u'iclass'):
+                      u'fggs_p', u'fgga_p', u'fgts_p', u'level_offset',
+                      u'skills', u'health', u'unused2', u'baseSpell',
+                      u'fatigue', u'attributes', u'iclass'):
                 setattr(face, a, getattr(npc, a))
             face.gender = (0,1)[npc.flags.female]
             face.pcName = npc.full
@@ -153,7 +154,7 @@ class PCFaces(object):
         npc = SreNPC(recFlags,data)
         if npc.acbs:
             face.gender = npc.acbs.flags.female
-            face.level = npc.acbs.level
+            face.level_offset = npc.acbs.level_offset
             face.baseSpell = npc.acbs.baseSpell
             face.fatigue = npc.acbs.fatigue
         for a in (u'attributes', u'skills', u'health', u'unused2'):
@@ -267,7 +268,7 @@ class PCFaces(object):
         npc = SreNPC(recFlags,data)
         if not npc.acbs: npc.acbs = npc.getDefault(u'acbs')
         npc.acbs.flags.female = face.gender
-        npc.acbs.level = face.level
+        npc.acbs.level_offset = face.level_offset
         npc.acbs.baseSpell = face.baseSpell
         npc.acbs.fatigue = face.fatigue
         npc.modifiers = face.modifiers[:]
@@ -360,7 +361,7 @@ class PCFaces(object):
             npc.acbs.flags.female = face.gender
         #--Stats
         if pcf_flags.stats and npc.acbs:
-            npc.acbs.level = face.level
+            npc.acbs.level_offset = face.level_offset
             npc.acbs.baseSpell = face.baseSpell
             npc.acbs.fatigue = face.fatigue
             npc.attributes = face.attributes
@@ -424,9 +425,9 @@ class PCFaces(object):
             face.face_masters = modFile.augmented_masters()
             for a in (u'eid', u'race', u'eye', u'hair', u'hairLength',
                       u'hairRed', u'hairBlue', u'hairGreen', u'unused3',
-                      u'fggs_p', u'fgga_p', u'fgts_p', u'level', u'skills',
-                      u'health', u'unused2', u'baseSpell', u'fatigue',
-                      u'attributes', u'iclass'):
+                      u'fggs_p', u'fgga_p', u'fgts_p', u'level_offset',
+                      u'skills', u'health', u'unused2', u'baseSpell',
+                      u'fatigue', u'attributes', u'iclass'):
                 npc_val = getattr(npc, a)
                 if isinstance(npc_val, tuple): # Hacky check for FormIDs
                     npc_val = short_mapper(npc_val)
@@ -494,7 +495,7 @@ class PCFaces(object):
         npc.fgga_p = face.fgga_p
         npc.fgts_p = face.fgts_p
         #--Stats
-        npc.level_offset = face.level
+        npc.level_offset = face.level_offset
         npc.baseSpell = face.baseSpell
         npc.fatigue = face.fatigue
         if face.skills: npc.skills = face.skills