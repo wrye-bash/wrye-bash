wrye-bash/wrye-bash

commit c5501635c218f88d2414d5b5eba77e6d76aaea7c

name -> ci_key (mostly):
Left some optimization notes - be aware that ci_key is currently slow
(property + GPath) but that is mostly not a problem (when called from
do once operations, menus, exceptions etc). Is is called in refresh
also however - will be an attribute again soon (we still need caching)

Note staticmethod askResourcesOk refactoring chopping off a use of
isMod (yak) - clearly belonged to Info classes.

These are all read only and we see some common patterns as:

- bosh.ModInfo.txt_status and co - actually all (apart ModChecker...)
uses of imported/merged and co point to a modInfo.active_status enum

Thanks Inf for fixup!

Under #543

Mopy/bash/basher/files_links.py
@@ -103,21 +103,17 @@ class File_Duplicate(ItemLink):
         u'Sound\\Voice\\%s), which will not be attached to the duplicate '
         u'mod.')
 
-    def _askResourcesOk(self, fileInfo):
-        msg = bosh.modInfos.askResourcesOk(fileInfo,
-                                           bsaAndBlocking=self._bsaAndBlocking,
-                                           bsa=self._bsa,
-                                           blocking=self._blocking)
-        if not msg: return True  # resources ok
-        return self._askWarning(msg, _(u'Duplicate %s') % fileInfo.name.s)
-
     @balt.conversation
     def Execute(self):
         dests = []
         fileInfos = self.window.data_store
         for to_duplicate, fileInfo in self.iselected_pairs():
             #--Mod with resources? Warn on rename if file has bsa and/or dialog
-            if not self._askResourcesOk(fileInfo): continue
+            msg = fileInfo.askResourcesOk(fileInfo,
+                bsaAndBlocking=self._bsaAndBlocking, bsa=self._bsa,
+                blocking=self._blocking)
+            if msg and not self._askWarning(msg, _(
+                u'Duplicate %s') % fileInfo): continue
             #--Continue copy
             r, e = to_duplicate.root, to_duplicate.ext
             destName = fileInfo.unique_key(r, e, add_copy=True)

Mopy/bash/basher/__init__.py
@@ -1042,7 +1042,7 @@ class ModList(_ModsUIList):
             from .frames import DocBrowser
             DocBrowser().show_frame()
             settings[u'bash.modDocs.show'] = True
-        Link.Frame.docBrowser.SetMod(modInfo.name)
+        Link.Frame.docBrowser.SetMod(modInfo.ci_key)
         Link.Frame.docBrowser.raise_frame()
 
     def _handle_key_down(self, wrapped_evt):
@@ -1348,7 +1348,7 @@ class _EditableMixinOnFileInfos(_EditableMixin):
     def file_info(self): raise AbstractError
     @property
     def displayed_item(self):
-        return self.file_info.name if self.file_info else None
+        return self.file_info.ci_key if self.file_info else None
 
     def __init__(self, masterPanel, ui_list_panel):
         # super(_EditableMixinOnFileInfos, self).__init__(masterPanel)
@@ -1386,12 +1386,12 @@ class _EditableMixinOnFileInfos(_EditableMixin):
 
     @balt.conversation
     def _refresh_detail_info(self):
-        try: # use self.file_info.name, as name may have been updated
+        try: # use self.file_info.ci_key, as name may have been updated
             # Although we could avoid rereading the header I leave it here as
             # an extra error check - error handling is WIP
-            self.panel_uilist.data_store.new_info(self.file_info.name,
+            self.panel_uilist.data_store.new_info(self.file_info.ci_key,
                                                   notify_bain=True)
-            return self.file_info.name
+            return self.file_info.ci_key
         except FileError as e:
             deprint(u'Failed to edit details for %s' % self.displayed_item,
                     traceback=True)
@@ -1541,7 +1541,7 @@ class ModDetails(_ModsSavesDetails):
         if fileName:
             modInfo = self.modInfo = bosh.modInfos[fileName]
             #--Remember values for edit checks
-            self.fileStr = modInfo.name.s
+            self.fileStr = modInfo.ci_key.s
             self.authorStr = modInfo.header.author
             self.modifiedStr = format_date(modInfo.mtime)
             self.descriptionStr = modInfo.header.description
@@ -1624,16 +1624,9 @@ class ModDetails(_ModsSavesDetails):
         u'Sound\\Voice\\%s) which will become detached when the mod is '
         u'renamed.')
 
-    def _askResourcesOk(self, fileInfo):
-        msg = bosh.modInfos.askResourcesOk(fileInfo,
-                                           bsaAndBlocking=self.bsaAndBlocking,
-                                           bsa=self.bsa,blocking=self.blocking)
-        if not msg: return True # resources ok
-        return balt.askWarning(self, msg, _(u'Rename ') + fileInfo.name.s)
-
     def testChanges(self): # used by the master list when editing is disabled
         modInfo = self.modInfo
-        if not modInfo or (self.fileStr == modInfo.name and
+        if not modInfo or (self.fileStr == modInfo.ci_key and
                            self.modifiedStr == format_date(modInfo.mtime) and
                            self.authorStr == modInfo.header.author and
                            self.descriptionStr == modInfo.header.description):
@@ -1643,13 +1636,17 @@ class ModDetails(_ModsSavesDetails):
     def DoSave(self):
         modInfo = self.modInfo
         #--Change Tests
-        changeName = (self.fileStr != modInfo.name)
+        changeName = (self.fileStr != modInfo.ci_key)
         changeDate = (self.modifiedStr != format_date(modInfo.mtime))
         changeHedr = (self.authorStr != modInfo.header.author or
                       self.descriptionStr != modInfo.header.description)
         changeMasters = self.uilist.edited
         #--Warn on rename if file has BSA and/or dialog
-        if changeName and not self._askResourcesOk(modInfo): return
+        if changeName:
+            msg = modInfo.askResourcesOk(bsaAndBlocking=self.bsaAndBlocking,
+                                         bsa=self.bsa, blocking=self.blocking)
+            if msg and not balt.askWarning(self, msg, _(
+                u'Rename ') + u'%s' % modInfo): return
         #--Only change date?
         if changeDate and not (changeName or changeHedr or changeMasters):
             self._set_date(modInfo)
@@ -1662,7 +1659,7 @@ class ModDetails(_ModsSavesDetails):
         modInfo.makeBackup()
         #--Change Name?
         if changeName:
-            oldName,newName = modInfo.name,GPath(self.fileStr.strip())
+            oldName,newName = modInfo.ci_key,GPath(self.fileStr.strip())
             #--Bad name?
             if (bosh.modInfos.isBadFileName(newName.s) and
                 not balt.askContinue(self,_(
@@ -1688,7 +1685,7 @@ class ModDetails(_ModsSavesDetails):
         if changeDate or changeHedr or changeMasters:
             # we reread header to make sure was written correctly
             detail_item = self._refresh_detail_info()
-        else: detail_item = self.file_info.name
+        else: detail_item = self.file_info.ci_key
         #--Done
         with load_order.Unlock():
             bosh.modInfos.refresh(refresh_infos=False, _modTimesChange=changeDate)
@@ -1785,7 +1782,7 @@ class ModDetails(_ModsSavesDetails):
                 _refresh_only_details()
         # Copy tags to various places
         bashTagsDesc = mod_info.getBashTagsDesc()
-        tag_plugin_name = mod_info.name
+        tag_plugin_name = mod_info.ci_key
         # We need to grab both the ones from the description and from LOOT,
         # since we need to save a diff in case of Copy to BashTags
         added_tags, deleted_tags = bosh.read_loot_tags(tag_plugin_name)
@@ -1798,7 +1795,7 @@ class ModDetails(_ModsSavesDetails):
             _text = _(u'Copy to BashTags')
             _help = _(u'Copies a diff between currently applied tags and '
                       u'description/LOOT tags to %s.') % (
-                bass.dirs[u'tag_files'].join(mod_info.name.body + u'.txt'))
+                bass.dirs[u'tag_files'].join(mod_info.ci_key.body + u'.txt'))
             def _enable(self):
                 return (not mod_info.is_auto_tagged() and
                         bosh.read_dir_tags(tag_plugin_name) != dir_diff)
@@ -2233,7 +2230,7 @@ class SaveDetails(_ModsSavesDetails):
         if fileName:
             saveInfo = self.saveInfo = bosh.saveInfos[fileName]
             #--Remember values for edit checks
-            self.fileStr = saveInfo.name.s
+            self.fileStr = saveInfo.ci_key.s
             self.playerNameStr = saveInfo.header.pcName
             self.curCellStr = saveInfo.header.pcLocation
             self.gameDays = saveInfo.header.gameDays
@@ -2292,7 +2289,7 @@ class SaveDetails(_ModsSavesDetails):
 
     def testChanges(self): # used by the master list when editing is disabled
         saveInfo = self.saveInfo
-        if not saveInfo or self.fileStr == saveInfo.name:
+        if not saveInfo or self.fileStr == saveInfo.ci_key:
             self.DoCancel()
 
     @balt.conversation
@@ -2300,7 +2297,7 @@ class SaveDetails(_ModsSavesDetails):
         """Event: Clicked Save button."""
         saveInfo = self.saveInfo
         #--Change Tests
-        changeName = (self.fileStr != saveInfo.name)
+        changeName = (self.fileStr != saveInfo.ci_key)
         changeMasters = self.uilist.edited
         #--Backup
         saveInfo.makeBackup() ##: why backup when just renaming - #292
@@ -2321,10 +2318,10 @@ class SaveDetails(_ModsSavesDetails):
             saveInfo.write_masters()
             saveInfo.setmtime(prevMTime)
             detail_item = self._refresh_detail_info()
-        else: detail_item = self.file_info.name
+        else: detail_item = self.file_info.ci_key
         kwargs = {u'to_del': to_del, u'detail_item': detail_item}
         if detail_item is None:
-            kwargs[u'to_del'] = to_del | {self.file_info.name}
+            kwargs[u'to_del'] = to_del | {self.file_info.ci_key}
         else:
             kwargs[u'redraw'] = [detail_item]
         self.panel_uilist.RefreshUI(**kwargs)
@@ -3508,7 +3505,7 @@ class BSADetails(_EditableMixinOnFileInfos, SashPanel):
         if fileName:
             self._bsa_info = bosh.bsaInfos[fileName]
             #--Remember values for edit checks
-            self.fileStr = self._bsa_info.name.s
+            self.fileStr = self._bsa_info.ci_key.s
             self.gInfo.text_content = self._bsa_info.get_table_prop(u'info',
                 _(u'Notes: '))
         else:
@@ -3527,12 +3524,12 @@ class BSADetails(_EditableMixinOnFileInfos, SashPanel):
     def DoSave(self):
         """Event: Clicked Save button."""
         #--Change Tests
-        changeName = (self.fileStr != self._bsa_info.name)
+        changeName = (self.fileStr != self._bsa_info.ci_key)
         #--Change Name?
         if changeName:
             newName = GPath(self.fileStr.strip())
             if self.panel_uilist.try_rename(self._bsa_info, newName):
-                self.panel_uilist.RefreshUI(detail_item=self.file_info.name)
+                self.panel_uilist.RefreshUI(detail_item=self.file_info.ci_key)
 
 #------------------------------------------------------------------------------
 class BSAPanel(BashTab):
@@ -4092,7 +4089,7 @@ class BashFrame(WindowFrame):
             message.append(m)
             self.knownCorrupted |= corruptSaves
         valid_vers = bush.game.Esp.validHeaderVersions
-        invalidVersions = {x.name for x in bosh.modInfos.itervalues() if
+        invalidVersions = {ck for ck, x in bosh.modInfos.iteritems() if
                            all(x.header.version != v for v in valid_vers)}
         if warn_mods and not invalidVersions <= self.knownInvalidVerions:
             m = [_(u'Unrecognized Versions'),

Mopy/bash/basher/dialogs.py
@@ -122,7 +122,7 @@ class ImportFaceDialog(DialogWindow):
         bass.settings[u'bash.faceImport.flags'] = int(pc_flags)
         bosh.faces.PCFaces.save_setFace(self.fileInfo, self.fdata[item],
                                         pc_flags)
-        balt.showOk(self, _(u'Face imported.'), self.fileInfo.name)
+        balt.showOk(self, _(u'Face imported.'), self.fileInfo.ci_key)
         self.accept_modal()
 
 #------------------------------------------------------------------------------

Mopy/bash/basher/mod_links.py
@@ -167,9 +167,10 @@ class Mod_CreateDummyMasters(OneItemLink, _LoadLink):
             # Add the appropriate flags based on extension. This is obviously
             # just a guess - you can have a .esm file without an ESM flag in
             # Skyrim LE - but these are also just dummy masters.
-            if newInfo.name.cext in (u'.esm', u'.esl'):
+            cext_ = newInfo.ci_key.cext
+            if cext_ in (u'.esm', u'.esl'):
                 newFile.tes4.flags1.esm = True
-            if newInfo.name.cext == u'.esl':
+            if cext_ == u'.esl':
                 newFile.tes4.flags1.eslFile = True
             newFile.safeSave()
         to_select = []

Mopy/bash/basher/patcher_dialog.py
@@ -159,7 +159,7 @@ class PatchDialog(DialogWindow):
         self.accept_modal()
         progress = None
         try:
-            patch_name = self.patchInfo.name
+            patch_name = self.patchInfo.ci_key
             patch_size = self.patchInfo.fsize
             progress = balt.Progress(patch_name.s,(u' '*60+u'\n'), abort=True)
             timer1 = time.clock()
@@ -200,7 +200,7 @@ class PatchDialog(DialogWindow):
             readme = bosh.modInfos.store_dir.join(u'Docs', patch_name.sroot + u'.txt')
             docsDir = bass.dirs[u'mopy'].join(u'Docs')
             tempReadmeDir = Path.tempDir().join(u'Docs')
-            tempReadme = tempReadmeDir.join(patch_name.sroot+u'.txt')
+            tempReadme = tempReadmeDir.join(patch_name.sroot + u'.txt')
             #--Write log/readme to temp dir first
             with tempReadme.open(u'w', encoding=u'utf-8-sig') as file:
                 file.write(logValue)
@@ -306,14 +306,14 @@ class PatchDialog(DialogWindow):
     def ExportConfig(self):
         """Export the configuration to a user selected dat file."""
         config = self.__config()
-        exportConfig(patch_name=self.patchInfo.name, config=config,
-            win=self.parent, outDir=bass.dirs[u'patches'])
+        exportConfig(patch_name=self.patchInfo.ci_key, config=config,
+                     win=self.parent, outDir=bass.dirs[u'patches'])
 
     __old_key = GPath(u'Saved Bashed Patch Configuration')
     __new_key = u'Saved Bashed Patch Configuration (%s)'
     def ImportConfig(self):
         """Import the configuration from a user selected dat file."""
-        config_dat = self.patchInfo.name + u'_Configuration.dat'
+        config_dat = self.patchInfo.ci_key + u'_Configuration.dat'
         textDir = bass.dirs[u'patches']
         textDir.makedirs()
         #--File dialog

Mopy/bash/basher/saves_links.py
@@ -504,7 +504,7 @@ class Save_EditCreatedData(balt.ListEditorData):
                 count += 1
             self.saveFile.safeSave()
             balt.showOk(self.parent, _(u'Names modified: %d.') % count,
-                        self.saveFile.fileInfo.name)
+                        self.saveFile.fileInfo.ci_key)
 
 #------------------------------------------------------------------------------
 class Save_EditCreated(OneItemLink):

commit 7ee474f5d6f61e93bad2de1586ab32f942a01b97

Fix Duplicate causing a traceback
Broke in c5501635c218f88d2414d5b5eba77e6d76aaea7c.

Ut: Mopy/bash/game/oblivion/records.py: fixed comment from
d2375b250c5ef82412ab2602a79f2db3f1fe0859

Update gitignore for l10n changes

Changed in 9a87ddadd1cd9435fb0bfee7300df83d64703ed0.

.gitignore
@@ -35,7 +35,7 @@ Mopy/redist/
 
 # translation files
 Mopy/bash/l10n/*.mo
-Mopy/bash/l10n/*NEW.txt
+Mopy/bash/l10n/*NEW.po
 
 # IDE files
 .idea/

Mopy/bash/basher/files_links.py
@@ -109,7 +109,7 @@ class File_Duplicate(ItemLink):
         fileInfos = self.window.data_store
         for to_duplicate, fileInfo in self.iselected_pairs():
             #--Mod with resources? Warn on rename if file has bsa and/or dialog
-            msg = fileInfo.askResourcesOk(fileInfo,
+            msg = fileInfo.askResourcesOk(
                 bsaAndBlocking=self._bsaAndBlocking, bsa=self._bsa,
                 blocking=self._blocking)
             if msg and not self._askWarning(msg, _(

Mopy/bash/game/oblivion/records.py
@@ -1864,7 +1864,7 @@ class MreSpel(MelRecord,MreHasEffects):
                             3   : u'LesserPower',
                             4   : u'Ability',
                             5   : u'Poison'}
-    spellTypeName_Number = {y.lower(): x for x, y in # if name is None it will
+    spellTypeName_Number = {y.lower(): x for x, y in
                             spellTypeNumber_Name.iteritems() if x is not None}
     levelTypeNumber_Name = {None : u'NONE',
                             0    : u'Novice',