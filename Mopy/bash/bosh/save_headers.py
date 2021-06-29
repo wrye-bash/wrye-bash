wrye-bash/wrye-bash

commit 015a699146f7306c3637954bbe99eea32bbb6c33

save_headers: Support writing SSE headers
Needs more testing - especially in LE - but SSE seems to happily load
our rewritten saves now. Compression settings for zlib are level 1, for
LZ4 they are the defaults. For some reason, the Linux version of
python-lz4 produces an identical file, while the Windows version makes a
larger one... SSE still loads both fine, so probably not a big deal.

Mopy/bash/bosh/save_headers.py
@@ -58,7 +58,6 @@ class SaveFileHeader(object):
     # map slots to (seek position, unpacker) - seek position negative means
     # seek relative to ins.tell(), otherwise to the beginning of the file
     unpackers = OrderedDict()
-    canEditMasters = True
 
     def __init__(self, save_path):
         try:
@@ -193,7 +192,7 @@ class SkyrimSaveHeader(SaveFileHeader):
     # _compressType of Skyrim SE saves - used to decide how to read/write them
     __slots__ = ('gameDate', 'saveNumber', 'version', 'raceEid', 'pcSex',
                  'pcExp', 'pcLvlExp', 'filetime', '_formVersion',
-                 '_compressType')
+                 '_compressType', '_sse_start')
 
     unpackers = OrderedDict([
         ('header_size', (00, unpack_int)),
@@ -211,9 +210,6 @@ class SkyrimSaveHeader(SaveFileHeader):
         ('ssWidth',     (00, unpack_int)),
         ('ssHeight',    (00, unpack_int)),
     ])
-
-    @property
-    def canEditMasters(self): return not self.__is_sse()
 
     def __is_sse(self): return self.version == 12
 
@@ -237,13 +233,11 @@ class SkyrimSaveHeader(SaveFileHeader):
         #  1 means zlib
         #  2 means lz4
         if self.__is_sse() and self._compressType in (1, 2):
+            self._sse_start = ins.tell()
             decompressed_size = unpack_int(ins)
             compressed_size = unpack_int(ins)
             sse_offset = ins.tell()
-            decompressor = (self._sse_decompress_lz4
-                           if self._compressType == 2
-                           else self._sse_decompress_zlib)
-            ins = decompressor(ins, compressed_size, decompressed_size)
+            ins = self._sse_decompress(ins, compressed_size, decompressed_size)
         else:
             sse_offset = 0
         self._formVersion = unpack_byte(ins)
@@ -269,6 +263,29 @@ class SkyrimSaveHeader(SaveFileHeader):
                     ins.tell() + sse_offset - self._mastersStart - 4,
                     mastersSize))
 
+    def _sse_compress(self, to_compress):
+        """Compresses the specified data using either LZ4 or zlib, depending on
+        self._compressType. Do not call for uncompressed files!"""
+        try:
+            if self._compressType == 2:
+                # SSE uses default lz4 settings; store_size is not in docs, so:
+                # noinspection PyArgumentList
+                return lz4.block.compress(to_compress.getvalue(),
+                                          store_size=False)
+            else:
+                # SSE uses zlib level 1
+                return zlib.compress(to_compress.getvalue(), 1)
+        except (zlib.error, lz4.block.LZ4BlockError) as e:
+            raise SaveHeaderError(u'Failed to compress header: %r' % e)
+
+    def _sse_decompress(self, ins, compressed_size, decompressed_size):
+        """Decompresses the specified data using either LZ4 or zlib, depending
+        on self._compressType. Do not call for uncompressed files!"""
+        decompressor = (self._sse_decompress_lz4
+                       if self._compressType == 2
+                       else self._sse_decompress_zlib)
+        return decompressor(ins, compressed_size, decompressed_size)
+
     @staticmethod
     def _sse_decompress_zlib(ins, compressed_size, decompressed_size):
         try:
@@ -277,19 +294,23 @@ class SkyrimSaveHeader(SaveFileHeader):
             raise SaveHeaderError(u'zlib error while decompressing '
                                   u'zlib-compressed header: %r' % e)
         if len(decompressed_data) != decompressed_size:
-            raise SaveHeaderError(_(u'zlib-decompressed header size incorrect '
-                                    u'- expected %u, but got %u.') %
-                                  (decompressed_size, len(decompressed_data)))
+            raise SaveHeaderError(u'zlib-decompressed header size incorrect - '
+                                  u'expected %u, but got %u.' % (
+                decompressed_size, len(decompressed_data)))
         return StringIO.StringIO(decompressed_data)
 
     @staticmethod
     def _sse_decompress_lz4(ins, compressed_size, decompressed_size):
-        decompressed_data = lz4.block.decompress(
-            ins.read(compressed_size), uncompressed_size=decompressed_size)
+        try:
+            decompressed_data = lz4.block.decompress(
+                ins.read(compressed_size), uncompressed_size=decompressed_size * 2)
+        except lz4.block.LZ4BlockError as e:
+            raise SaveHeaderError(u'LZ4 error while decompressing '
+                                  u'lz4-compressed header: %r' % e)
         if len(decompressed_data) != decompressed_size:
-            raise SaveHeaderError(_(u'lz4-decompressed header size incorrect '
-                                    u'- expected %u, but got %u.') %
-                                  (decompressed_size, len(decompressed_data)))
+            raise SaveHeaderError(u'lz4-decompressed header size incorrect - '
+                                  u'expected %u, but got %u.' % (
+                decompressed_size, len(decompressed_data)))
         return StringIO.StringIO(decompressed_data)
 
     def calc_time(self):
@@ -298,6 +319,31 @@ class SkyrimSaveHeader(SaveFileHeader):
         playSeconds = hours * 60 * 60 + minutes * 60 + seconds
         self.gameDays = float(playSeconds) / (24 * 60 * 60)
         self.gameTicks = playSeconds * 1000
+
+    def writeMasters(self, ins, out):
+        if not self.__is_sse() or self._compressType == 0:
+            # Skyrim LE or uncompressed - can use the default implementation
+            return super(SkyrimSaveHeader, self).writeMasters(ins, out)
+        # Write out everything up until the compressed portion
+        out.write(ins.read(self._sse_start))
+        # Now we need to decompress the portion again
+        decompressed_size = unpack_int(ins)
+        compressed_size = unpack_int(ins)
+        ins = self._sse_decompress(ins, compressed_size, decompressed_size)
+        # Gather the data that will be compressed
+        to_compress = StringIO.StringIO()
+        to_compress.write(struct_pack(u'=B', self._formVersion))
+        ins.seek(1, 1) # skip the form version
+        old_masters = self._write_masters(ins, to_compress)
+        for block in iter(partial(ins.read, 0x5000000), b''):
+            to_compress.write(block)
+        # Compress the gathered data, write out the sizes and finally write out
+        # the actual compressed data
+        compressed_data = self._sse_compress(to_compress)
+        _pack(out, u'=I', to_compress.tell())   # decompressed_size
+        _pack(out, u'=I', len(compressed_data)) # compressed_size
+        out.write(compressed_data)
+        return old_masters
 
     def _dump_masters(self, ins, numMasters, out):
         # Store these two blocks distinctly, *never* combine them - that
@@ -337,9 +383,6 @@ class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
 
     __slots__ = ()
 
-    @property
-    def canEditMasters(self): return True
-
     def _esl_block(self): return self.version == 15 and self._formVersion >= 68
 
     def load_image_data(self, ins):
@@ -370,6 +413,10 @@ class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
         # Assuming still 1000 ticks per second
         self.gameTicks = (days * 24 * 60 * 60 + hours * 60 * 60 + minutes
                              * 60) * 1000
+
+    def writeMasters(self, ins, out):
+        # Call the SaveFileHeader version - *not* the Skyrim one
+        super(SkyrimSaveHeader, self).writeMasters(ins, out)
 
 class FalloutNVSaveHeader(SaveFileHeader):
     save_magic = 'FO3SAVEGAME'

Mopy/bash/basher/__init__.py
@@ -1945,7 +1945,7 @@ class SaveDetails(_SashDetailsPanel):
     @property
     def file_infos(self): return bosh.saveInfos
     @property
-    def allowDetailsEdit(self): return self.file_info.header.canEditMasters
+    def allowDetailsEdit(self): return bush.game.ess.canReadBasic
 
     def __init__(self,parent):
         super(SaveDetails, self).__init__(parent)

commit 5a592dedec1fc9896bab55c2972420092a815536

FO4: Fix renaming save masters
Broke in 015a699146f7306c3637954bbe99eea32bbb6c33:

Traceback (most recent call last):
  File "bash\gui\events.pyo", line 177, in _post
  File "bash\balt.pyo", line 825, in _conversation_wrapper
  File "bash\basher\__init__.pyo", line 2318, in DoSave
  File "bash\bosh\__init__.pyo", line 1343, in write_masters
TypeError: 'NoneType' object is not iterable

Mopy/bash/bosh/save_headers.py
@@ -557,7 +557,7 @@ class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
 
     def writeMasters(self, ins, out):
         # Call the SaveFileHeader version - *not* the Skyrim one
-        super(SkyrimSaveHeader, self).writeMasters(ins, out)
+        return super(SkyrimSaveHeader, self).writeMasters(ins, out)
 
 class FalloutNVSaveHeader(SaveFileHeader):
     save_magic = b'FO3SAVEGAME'