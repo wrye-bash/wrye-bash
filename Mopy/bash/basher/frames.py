wrye-bash/wrye-bash

commit a7f2b9c00249f70c98faf43b6cc475411301aa32

More nit - recurse_masters:
recurse_masters: rule of thumb if it uses the key it's ModInfos - was
essentially static before
Mopy/bash/basher/gui_patchers.py: dropped some intermediate variables
and went a bit far with fstrings

Mopy/bash/basher/gui_patchers.py
@@ -425,12 +425,9 @@ class _ListPatcherPanel(_PatcherPanel):
         self.configChoices = copy.deepcopy(
             config.get(u'configChoices', self.__class__.default_configChoices))
         #--Verify file existence
-        newConfigItems = []
-        for srcPath in self.configItems:
-            if (srcPath in bosh.modInfos or (reCsvExt.search(
-                srcPath.s) and srcPath in patches_set())):
-                newConfigItems.append(srcPath)
-        self.configItems = newConfigItems
+        self.configItems = [srcPath.s for srcPath in self.configItems if (
+                    srcPath in bosh.modInfos or (reCsvExt.search(
+                srcPath.s) and srcPath in patches_set()))]
         if self.__class__.forceItemCheck:
             for item in self.configItems:
                 self.configChecks[item] = True
@@ -438,8 +435,8 @@ class _ListPatcherPanel(_PatcherPanel):
 
     def saveConfig(self, configs):
         """Save config to configs dictionary."""
-        #--Toss outdated configCheck data.
         config = super(_ListPatcherPanel, self).saveConfig(configs)
+        #--Toss outdated configCheck data.
         listSet = set(self.configItems)
         self.configChecks = config[u'configChecks'] = {
             k: v for k, v in self.configChecks.items() if k in listSet}
@@ -799,10 +796,9 @@ class _ImporterPatcherPanel(_ListPatcherPanel):
         """Save config to configs dictionary."""
         config = super(_ImporterPatcherPanel, self).saveConfig(configs)
         if self.isEnabled:
-            importedMods = [item for item,value in
-                            self.configChecks.items() if
-                            value and bosh.ModInfos.rightFileType(item)]
-            configs[u'ImportedMods'].update(importedMods)
+            configs[u'ImportedMods'].update(
+                [item for item, value in self.configChecks.items() if
+                 value and bosh.ModInfos.rightFileType(item)])
         return config
 
 class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
@@ -830,11 +826,9 @@ class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
     def getItemLabel(self,item):
         # Note that we do *not* want to escape the & here - that puts *two*
         # ampersands in the resulting ListBox for some reason
-        choice = [i[0] for i in self.configChoices.get(item, tuple())]
-        if choice:
-            return u'%s [%s]' % (item, u''.join(sorted(choice)))
-        else:
-            return u'%s' % item # Path or string - YAK
+        choice = ''.join(
+            sorted(i[0] for i in self.configChoices.get(item) if i))
+        return f'{item}{f" [{choice}]" if choice else ""}'
 
     def GetConfigPanel(self, parent, config_layout, gTipText):
         if self.gConfigPanel: return self.gConfigPanel

Mopy/bash/bosh/__init__.py
@@ -3149,6 +3149,15 @@ class ModInfos(FileInfos):
             for p_master in p_info.masterNames:
                 cached_dependents[p_master].add(p)
 
+    def recurse_masters(self, fn_mod):
+        """Recursively collect all masters of fn_mod."""
+        ret_masters = set()
+        src_masters = self[fn_mod].masterNames if fn_mod in self else []
+        for src_master in src_masters:
+            ret_masters.add(src_master)
+            ret_masters.update(self.recurse_masters(src_master))
+        return ret_masters
+
 #------------------------------------------------------------------------------
 class SaveInfos(FileInfos):
     """SaveInfo collection. Represents save directory and related info."""

Mopy/bash/bosh/mods_metadata.py
@@ -256,7 +256,7 @@ def checkMods(mc_parent, showModList=False, showCRC=False, showVersion=True,
                 add_deleted_rec = all_deleted_others[p_ci_key].append
                 add_old_weapon = old_weapon_records[p_ci_key].append
                 add_hitme = all_hitmes[p_ci_key].append
-                p_masters = modInfos[p_ci_key].masterNames + (p_ci_key,)
+                p_masters = (*modInfos[p_ci_key].masterNames, p_ci_key)
                 p_num_masters = len(p_masters)
                 for r, d in ext_data.items():
                     for r_fid, (r_header, r_eid) in d.items():

Mopy/bash/patcher/patchers/mergers.py
@@ -59,7 +59,7 @@ class _AMerger(ImportPatcher):
         super(_AMerger, self).__init__(p_name, p_file, p_sources)
         self.id_deltas = defaultdict(list)
         merger_masters = set(chain.from_iterable(
-            self._recurse_masters(srcMod, p_file.p_file_minfos)
+            p_file.p_file_minfos.recurse_masters(srcMod)
             for srcMod in self.srcs))
         self._masters_and_srcs = merger_masters | set(self.srcs)
         # Set of record signatures that are actually provided by sources
@@ -69,16 +69,6 @@ class _AMerger(ImportPatcher):
         self.inventOnlyMods = (
             {x for x in self.srcs if x in p_file.mergeSet and u'IIM' in
              p_file.p_file_minfos[x].getBashTags()} if self.iiMode else set())
-
-    ##: Move to ModInfo? get_recursive_masters()?
-    def _recurse_masters(self, srcMod, minfs):
-        """Recursively collects all masters of srcMod."""
-        ret_masters = set()
-        src_masters = minfs[srcMod].masterNames if srcMod in minfs else []
-        for src_master in src_masters:
-            ret_masters.add(src_master)
-            ret_masters.update(self._recurse_masters(src_master, minfs))
-        return ret_masters
 
     ##: post-tweak pooling, see if we can use RecPath for this
     def _entry_key(self, subrecord_entry):

commit 89375325c7f44d5052b1299c0b69a0ece3afc275

Fix regression - configItems were stored as strings:
Introduced in a7f2b9c00249f70c98faf43b6cc475411301aa32. Took the
opportunity to bin the pointless default_** class variables.
Some nit from the future

Mopy/bash/basher/frames.py
@@ -127,8 +127,8 @@ class DocBrowser(WindowFrame):
         if not doc_path:
             return bell()
         if not doc_path.is_file():
-            balt.showWarning(self, _(u'The assigned document is not present:')
-                             + u'\n  %s' % doc_path)
+            balt.showWarning(self, _('The assigned document is not '
+                                     'present:') + f'\n  {doc_path}')
         else:
             doc_path.start()
 
@@ -200,9 +200,9 @@ class DocBrowser(WindowFrame):
 
     def DoSave(self):
         """Saves doc, if necessary."""
-        if not self._doc_ctrl.is_text_modified(): return
         doc_path = self._db_doc_paths.get(self._mod_name)
         if not doc_path: return  # nothing to save if no file is loaded
+        if not self._doc_ctrl.is_text_modified(): return
         self._doc_ctrl.set_text_modified(False)
         with doc_path.open(u'w', encoding=u'utf-8-sig') as out:
             out.write(self._doc_ctrl.fallback_text)
@@ -259,8 +259,8 @@ class DocBrowser(WindowFrame):
                         template = bolt.decoder(ins.read())
                     break
             else:
-                template = u'= $modName {}#\n{}'.format(u'=' * (74-len(mod_name)),
-                                                        doc_path.s)
+                template = f'= $modName {u"=" * (74 - len(mod_name))}#\n' \
+                           f'{doc_path}'
             self._load_data(uni_str=string.Template(template).substitute(
                 modName=mod_name.s))
             # Start edit mode

Mopy/bash/basher/gui_patchers.py
@@ -28,7 +28,7 @@ from itertools import chain
 # Internal
 from .. import bass, bosh, bush, balt, load_order, bolt, exception
 from ..balt import Links, SeparatorLink, CheckLink
-from ..bolt import GPath, text_wrap, dict_sort
+from ..bolt import GPath, text_wrap, dict_sort, GPath_no_norm
 from ..gui import Button, CheckBox, HBoxedLayout, Label, LayoutOptions, \
     Spacer, TextArea, TOP, VLayout, EventResult, PanelWin, ListBox, \
     CheckListBox, DeselectAllButton, SelectAllButton, FileOpenMultiple
@@ -258,9 +258,6 @@ class _ListPatcherPanel(_PatcherPanel):
     # ADDITIONAL CONFIG DEFAULTS FOR LIST PATCHER
     default_autoIsChecked = True
     default_remove_empty_sublists = bush.game.displayName == u'Oblivion'
-    default_configItems   = []
-    default_configChecks  = {}
-    default_configChoices = {}
 
     def GetConfigPanel(self, parent, config_layout, gTipText):
         """Show config."""
@@ -416,16 +413,14 @@ class _ListPatcherPanel(_PatcherPanel):
         self.remove_empty_sublists = config.get(
             u'remove_empty_sublists',
             self.__class__.default_remove_empty_sublists)
-        self.configItems = copy.deepcopy(
-            config.get(u'configItems', self.__class__.default_configItems))
-        self.configChecks = copy.deepcopy(
-            config.get(u'configChecks', self.__class__.default_configChecks))
-        self.configChoices = copy.deepcopy(
-            config.get(u'configChoices', self.__class__.default_configChoices))
+        self.configItems = copy.deepcopy(config.get(u'configItems', []))
+        self.configChecks = copy.deepcopy(config.get(u'configChecks', {}))
+        self.configChoices = copy.deepcopy(config.get(u'configChoices', {}))
         #--Verify file existence
-        self.configItems = [srcPath.s for srcPath in self.configItems if (
-                    srcPath in bosh.modInfos or (reCsvExt.search(
-                srcPath.s) and srcPath in patches_set()))]
+        self.configItems = [ # fix regression where these became strings
+            srcPath for srcPath in map(GPath_no_norm,
+            self.configItems) if (srcPath in bosh.modInfos or (reCsvExt.search(
+            srcPath.s) and srcPath in patches_set()))]
         if self.__class__.forceItemCheck:
             for item in self.configItems:
                 self.configChecks[item] = True

Mopy/bash/bosh/__init__.py
@@ -3115,10 +3115,10 @@ class ModInfos(FileInfos):
         if oldSize not in self.size_voVersion:
             raise StateError(u"Can't match current main ESM to known version.")
         oldName = GPath( # Oblivion_SI.esm: we will rename Oblivion.esm to this
-            baseName.sbody + u'_' + self.size_voVersion[oldSize] + u'.esm')
+            f'{baseName.sbody}_{self.size_voVersion[oldSize]}.esm')
         if self.store_dir.join(oldName).exists():
-            raise StateError(u"Can't swap: %s already exists." % oldName)
-        newName = GPath(baseName.sbody + u'_' + newVersion + u'.esm')
+            raise StateError(f"Can't swap: {oldName} already exists.")
+        newName = GPath(f'{baseName.sbody}_{newVersion}.esm')
         if newName not in self:
             raise StateError(f"Can't swap: {newName} doesn't exist.")
         return newName, oldName