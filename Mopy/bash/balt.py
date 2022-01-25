wrye-bash/wrye-bash

commit 28c07f704e8585b863bc1071a440e3fc13c9bb7d

Nit images:
Reduce occurrences of methods/attributes - you get the drill

balt: icons -> lg_icons - icons is-a ImageList and this is central to
an images API

bmp_from_bitstream: we get a wx bitmap not a wrapper - belonged with
ImageWrapper rename WIP 46a272d8b15d8d3cce8969ed527564da45b69c63

Under #366

Mopy/bash/balt.py
@@ -118,7 +118,7 @@ class ImageList(object):
                 indices[key] = imageList.Add(image.get_bitmap())
         return self.imageList
 
-    def get_image(self, key): return self.images[self[key]][1] # YAK !
+    def get_icon(self, key): return self.images[self[key]][1].GetIcon() # YAK !
 
     def __getitem__(self,key):
         self.GetImageList()
@@ -132,8 +132,8 @@ class ColorChecks(ImageList):
         for state in (u'on', u'off', u'inc', u'imp'):
             for status in (u'purple', u'blue', u'green', u'orange', u'yellow',
                            u'red'):
-                shortKey = status + u'.' + state
-                image_key = u'checkbox.' + shortKey
+                shortKey = f'{status}.{state}'
+                image_key = f'checkbox.{shortKey}'
                 img = bass.dirs[u'images'].join(
                     f'checkbox_{status}_{state}.png')
                 image = images[image_key] = ImageWrapper(img, ImageWrapper.typesDict[u'png'])
@@ -1028,7 +1028,7 @@ class UIList(wx.Panel):
         self.autosizeColumns()
 
     __all = ()
-    def RefreshUI(self, redraw=__all, to_del=__all, detail_item=u'SAME',
+    def RefreshUI(self, redraw=__all, to_del=__all, detail_item='SAME',
                   **kwargs):
         """Populate specified files or ALL files, sort, set status bar count.
         """
@@ -1674,21 +1674,22 @@ class Link(object):
         return showError(self.window, message, title)
 
     _default_icons = object()
-    def _showLog(self, logText, title=u'', asDialog=False, fixedFont=False,
-                 icons=_default_icons):
-        if icons is self._default_icons: icons = Resources.bashBlue
-        Log(self.window, logText, title, asDialog, fixedFont, log_icons=icons)
+    def _showLog(self, logText, title='', asDialog=False, fixedFont=False,
+                 lg_icons=_default_icons):
+        if lg_icons is self._default_icons: lg_icons = Resources.bashBlue
+        Log(self.window, logText, title, asDialog, fixedFont,
+            log_icons=lg_icons)
 
     def _showInfo(self, message, title=_(u'Information')):
         return showInfo(self.window, message, title)
 
-    def _showWryeLog(self, logText, title=u'', asDialog=True,
-                     icons=_default_icons):
-        if icons is self._default_icons: icons = Resources.bashBlue
+    def _showWryeLog(self, logText, title='', asDialog=True,
+                     lg_icons=_default_icons):
+        if lg_icons is self._default_icons: lg_icons = Resources.bashBlue
         if not title: title = self._text
-        WryeLog(self.window, logText, title, asDialog, log_icons=icons)
+        WryeLog(self.window, logText, title, asDialog, log_icons=lg_icons)
 
-    def _askNumber(self, message, prompt=u'', title=u'', value=0, min=0,
+    def _askNumber(self, message, prompt='', title='', value=0, min=0,
                    max=10000):
         return askNumber(self.window, message, prompt, title, value, min, max)
 

Mopy/bash/basher/__init__.py
@@ -2222,7 +2222,7 @@ class SaveDetails(_ModsSavesDetails):
         if self.saveInfo:
             if not self.saveInfo.header.image_loaded:
                 self.saveInfo.header.read_save_header(load_image=True)
-            new_save_screen = ImageWrapper.from_bitstream(
+            new_save_screen = ImageWrapper.bmp_from_bitstream(
                 *self.saveInfo.header.image_parameters)
         else:
             new_save_screen = None # reset to default
@@ -4188,9 +4188,9 @@ class BashApp(object):
                            elapsed=False) as progress:
             # Is splash enabled in ini ?
             if bass.inisettings[u'EnableSplashScreen']:
-                if bass.dirs[u'images'].join(u'wryesplash.png').isfile():
-                    splash_screen = CenteredSplash(
-                        bass.dirs[u'images'].join(u'wryesplash.png').s)
+                if (splash := bass.dirs['images'].join(
+                    'wryesplash.png')).isfile():
+                    splash_screen = CenteredSplash(splash.s)
             #--Init Data
             progress(0.2, _(u'Initializing Data'))
             self.InitData(progress)

Mopy/bash/basher/mod_links.py
@@ -1314,8 +1314,8 @@ class Mod_ScanDirty(ItemLink):
             for p in skipped_plugins:
                 log(p)
             log(u'\n')
-        self._showWryeLog(log.out.getvalue(),
-                          title=_(u'Dirty Record Results'), asDialog=False)
+        self._showWryeLog(log.out.getvalue(), title=_(u'Dirty Record Results'),
+                          asDialog=False)
 
 #------------------------------------------------------------------------------
 class Mod_RemoveWorldOrphans(_NotObLink, _LoadLink):
@@ -1711,8 +1711,8 @@ class Mod_Fids_Replace(OneItemLink):
             progress(1.0,_(u'Done.'))
         #--Log
         if not changed: self._showOk(_(u'No changes required.'))
-        else: self._showLog(changed, title=_(u'Objects Changed'),
-                            asDialog=True)
+        else:
+            self._showLog(changed, title=_(u'Objects Changed'), asDialog=True)
 
 class Mod_Face_Import(OneItemLink):
     """Imports a face from a save to an esp."""
@@ -1743,7 +1743,7 @@ class Mod_Face_Import(OneItemLink):
         if not imagePath.exists():
             srcInfo.header.read_save_header(load_image=True)
             # TODO(inf) de-wx! Again, image/bitmap stuff
-            image = ImageWrapper.from_bitstream(
+            image = ImageWrapper.bmp_from_bitstream(
                 *srcInfo.header.image_parameters).ConvertToImage()
             imagePath.head.makedirs()
             image.SaveFile(imagePath.s, ImageWrapper.typesDict[u'jpg'])
@@ -1814,10 +1814,10 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
         return changed
 
     def _showLog(self, logText, title=u'', asDialog=False, fixedFont=False,
-                 icons=Link._default_icons):
+                 lg_icons=Link._default_icons):
         super(_Mod_Import_Link, self)._showLog(logText,
             title=title or self.__class__.progressTitle, asDialog=asDialog,
-            fixedFont=fixedFont, icons=icons)
+            fixedFont=fixedFont, icons=lg_icons)
 
     def _log(self, changed, fileName):
         self._showLog(u'* %03d  %s\n' % (changed, fileName))

Mopy/bash/basher/dialogs.py
@@ -160,7 +160,7 @@ class CreateNewProject(DialogWindow):
              LayoutOptions(h_align=CENTER))
         ]).apply_to(self, fit=True)
         # Dialog Icon Handlers
-        self.set_icon(installercons.get_image(u'off.white.dir').GetIcon())
+        self.set_icon(installercons.get_icon(u'off.white.dir'))
         self.OnCheckBoxChange()
         self.OnCheckProjectsColorTextCtrl(self.textName.text_content)
 
@@ -179,14 +179,11 @@ class CreateNewProject(DialogWindow):
         """Change the DialogWindow icon to represent what the project status
         will be when created. """
         if self.checkEsp.is_checked or self.checkEspMasterless.is_checked:
-            if self.checkWizard.is_checked:
-                self.set_icon(
-                    installercons.get_image(u'off.white.dir.wiz').GetIcon())
-            else:
-                self.set_icon(
-                    installercons.get_image(u'off.white.dir').GetIcon())
+            img_key = f'off.white.dir' \
+                      f'{self.checkWizard.is_checked and ".wiz" or ""}'
         else:
-            self.set_icon(installercons.get_image(u'off.grey.dir').GetIcon())
+            img_key = 'off.grey.dir'
+        self.set_icon(installercons.get_icon(img_key))
 
     def OnClose(self):
         """ Create the New Project and add user specified extras. """

Mopy/bash/basher/links_init.py
@@ -89,10 +89,10 @@ def InitStatusBar():
         app_button_factory(bass.dirs[u'app'].join(u'OblivionModManager.exe'),
                            imageList(u'obmm%s.png'), _(u"Launch OBMM"),
                            uid=u'OBMM'))
-    from .constants import toolbar_buttons
     # Just an _App_Button whose path is in bass.tooldirs
     Tooldir_Button = lambda *args: app_button_factory(bass.tooldirs[args[0]],
                                                       *args[1:])
+    from .constants import toolbar_buttons
     for tb in toolbar_buttons:
         BashStatusBar.buttons.append(Tooldir_Button(*tb))
     for tb2 in _init_tool_buttons():

Mopy/bash/basher/saves_links.py
@@ -318,7 +318,7 @@ class Save_ExportScreenshot(OneItemLink):
             _(u'Screenshot %s.jpg') % self._selected_item, u'*.jpg')
         if not imagePath: return
         # TODO(inf) de-wx! All the image stuff is still way too close to wx
-        image = ImageWrapper.from_bitstream(
+        image = ImageWrapper.bmp_from_bitstream(
             *self._selected_info.header.image_parameters).ConvertToImage()
         image.SaveFile(imagePath.s, ImageWrapper.typesDict[u'jpg'])
 

Mopy/bash/belt.py
@@ -1284,7 +1284,7 @@ class WryeParser(ScriptParser.Parser):
         main_desc = args.pop(0).replace(u'&', u'&&')
         if len(args) % 3:
             error(MISSING_ARGS % name_)
-        images = []
+        images_ = []
         titles = OrderedDict()
         descs = []
         image_paths = []
@@ -1295,7 +1295,7 @@ class WryeParser(ScriptParser.Parser):
                 title = title[1:]
             titles[title] = is_default
             descs.append(args.pop(0))
-            images.append(args.pop(0))
+            images_.append(args.pop(0))
         if self.bAuto:
             # auto wizard will resolve SelectOne/SelectMany only if default(s)
             # were specified.
@@ -1318,7 +1318,7 @@ class WryeParser(ScriptParser.Parser):
             imageJoin = bass.getTempDir().join
         else:
             imageJoin = bass.dirs[u'installers'].join(self._path).join
-        for i in images:
+        for i in images_:
             # Try looking inside the package first, then look if it's using one
             # of the images packaged with Wrye Bash (from Mopy/bash/images)
             wiz_img_path = imageJoin(i)

Mopy/bash/gui/base_components.py
@@ -460,7 +460,7 @@ class WithCharEvents(_AComponent):
 class ImageWrapper(object):
     """Wrapper for images, allowing access in various formats/classes.
 
-    Allows image to be specified before wx.App is initialized.""" # TODO: unneeded?
+    Allows image to be specified before wx.App is initialized."""
 
     typesDict = {u'png': _wx.BITMAP_TYPE_PNG, u'jpg': _wx.BITMAP_TYPE_JPEG,
                  u'jpeg': _wx.BITMAP_TYPE_JPEG, u'ico': _wx.BITMAP_TYPE_ICO,
@@ -510,7 +510,7 @@ class ImageWrapper(object):
         return self.icon
 
     @staticmethod
-    def from_bitstream(bm_width, bm_height, stream_data, with_alpha):
+    def bmp_from_bitstream(bm_width, bm_height, stream_data, with_alpha):
         """Creates a bitmap from the specified stream data."""
         wx_depth = (32 if with_alpha else 24)
         wx_fmt = (_wx.BitmapBufferFormat_RGBA if with_alpha

commit 26e8de84b6d27a3fa7269f2e3dd79695b1bf8306

Initialise ColorChecks once:
Since we have a wx.App and the only dir we need here is dirs['images']
which is set in init_mopy_dirs we can finally drop this hack - this was
triggered by the huge bugdumps where the same images were read from disc
once per tab. We still need to address #366 but getting there.
Mopy/bash/basher/mod_links.py: regression introduced in
28c07f704e8585b863bc1071a440e3fc13c9bb7d

Ok third's the charm - this emulate_startup needs rethinking, as well
as setting the working dir - probably the right way is to set the dirs
correctly in init_mopy_dirs and drop __get_translations_dir but this
will do for now.

Mopy/bash/balt.py
@@ -26,7 +26,7 @@ now. See #190, its code should be refactored and land in basher and/or gui."""
 # Imports ---------------------------------------------------------------------
 from . import bass # for dirs - try to avoid
 from . import bolt
-from .bolt import deprint, readme_url
+from .bolt import deprint, readme_url, Path
 from .exception import AbstractError, AccessDeniedError, BoltError, \
     CancelError, SkipError, StateError
 #--Python
@@ -129,13 +129,14 @@ class ColorChecks(ImageList):
     """ColorChecks ImageList. Used by several UIList classes."""
     def __init__(self):
         ImageList.__init__(self, 16, 16)
+        if not (im_dir := Path.getcwd().join('bash', 'images')).exists(): ##: CI Hack
+            im_dir = Path.getcwd().join('Mopy', 'bash', 'images')
         for state in (u'on', u'off', u'inc', u'imp'):
             for status in (u'purple', u'blue', u'green', u'orange', u'yellow',
                            u'red'):
                 shortKey = f'{status}.{state}'
                 image_key = f'checkbox.{shortKey}'
-                img = bass.dirs[u'images'].join(
-                    f'checkbox_{status}_{state}.png')
+                img = im_dir.join(f'checkbox_{status}_{state}.png')
                 image = images[image_key] = ImageWrapper(img, ImageWrapper.typesDict[u'png'])
                 self.images.append((shortKey, image))
 
@@ -824,9 +825,7 @@ class UIList(wx.Panel):
     # order in which categories are added will also be the display order.
     global_links = None
     #--gList image collection
-    __icons = ImageList(16, 16) # sentinel value due to bass.dirs not being
-    # yet initialized when balt is imported, so I can't use ColorChecks here
-    icons = __icons
+    _icons = ColorChecks()
     _shellUI = False # only True in Screens/INIList/Installers
     _recycle = True # False on tabs that recycle makes no sense (People)
     max_items_open = 7 # max number of items one can open without prompt
@@ -858,9 +857,6 @@ class UIList(wx.Panel):
         #--Columns
         self.__class__.persistent_columns = {self._default_sort_col}
         self._colDict = {} # used in setting column sort indicator
-        #--gList image collection
-        self.__class__.icons = ColorChecks() \
-            if self.__class__.icons is self.__icons else self.__class__.icons
         #--gList
         self.__gList = UIListCtrl(self, self.__class__._editLabels,
                                   self.__class__._sunkenBorder,
@@ -869,13 +865,13 @@ class UIList(wx.Panel):
                                   dndList=self.__class__._dndList,
                                   fnDropFiles=self.OnDropFiles,
                                   fnDropIndexes=self.OnDropIndexes)
-        if self.icons:
-            # Image List: Column sorting order indicators
-            # explorer style ^ == ascending
-            checkboxesIL = self.icons.GetImageList()
-            self.sm_up = checkboxesIL.Add(images[u'arrow.up'].get_bitmap())
-            self.sm_dn = checkboxesIL.Add(images[u'arrow.down'].get_bitmap())
-            self.__gList._native_widget.SetImageList(checkboxesIL, wx.IMAGE_LIST_SMALL)
+        # Image List: Column sorting order indicators
+        # explorer style ^ == ascending
+        checkboxesIL = self._icons.GetImageList()
+        self.sm_up = checkboxesIL.Add(images[u'arrow.up'].get_bitmap())
+        self.sm_dn = checkboxesIL.Add(images[u'arrow.down'].get_bitmap())
+        self.__gList._native_widget.SetImageList(checkboxesIL,
+                                                 wx.IMAGE_LIST_SMALL)
         if self.__class__._editLabels:
             self.__gList.on_edit_label_begin.subscribe(self.OnBeginEditLabel)
             self.__gList.on_edit_label_end.subscribe(self.OnLabelEdited)
@@ -986,10 +982,10 @@ class UIList(wx.Panel):
         gItem = self.__gList._native_widget.GetItem(itemDex)
         df = self._ListItemFormat()
         self.set_item_format(fileName, df, target_ini_setts=target_ini_setts)
-        if df.icon_key and self.icons:
+        if df.icon_key:
             if isinstance(df.icon_key, tuple):
-                img = self.icons.Get(*df.icon_key)
-            else: img = self.icons[df.icon_key]
+                img = self._icons.Get(*df.icon_key)
+            else: img = self._icons[df.icon_key]
             gItem.SetImage(img)
         if df.text_key:
             gItem.SetTextColour(colors[df.text_key].to_rgba_tuple())

Mopy/bash/basher/__init__.py
@@ -2328,7 +2328,7 @@ class InstallersList(balt.UIList):
     column_links = Links()
     context_links = Links()
     global_links = defaultdict(lambda: Links()) # Global menu
-    icons = installercons
+    _icons = installercons
     _sunkenBorder = False
     _shellUI = True
     _editLabels = _copy_paths = True

Mopy/bash/basher/mod_links.py
@@ -1810,7 +1810,7 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
                  lg_icons=Link._default_icons):
         super(_Mod_Import_Link, self)._showLog(logText,
             title=title or self.__class__.progressTitle, asDialog=asDialog,
-            fixedFont=fixedFont, icons=lg_icons)
+            fixedFont=fixedFont, lg_icons=lg_icons)
 
     def _log(self, changed, fileName):
         self._showLog(u'* %03d  %s\n' % (changed, fileName))

Mopy/bash/localize.py
@@ -191,7 +191,7 @@ def __get_translations_dir():
     if not os.path.exists(trans_path):
         # HACK: the CI has to run tests from the top dir, which causes us to
         # have a non-Mopy working dir here. Real fix is ditching the fake
-        # startup and adding a real headless mode to WB (see #568 and #554)
+        # startup and adding a real headless mode to WB (see #568, #554 and #600)
         trans_path = os.path.join(os.getcwd(), u'Mopy', u'bash', u'l10n')
     return trans_path
 