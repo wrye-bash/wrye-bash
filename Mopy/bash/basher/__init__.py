wrye-bash/wrye-bash

commit abad69badd397ebc0f287ab98089f9106b4f8ddf

is_archive/project erosion:
Note I replaced the io buffer with a plain list and join - I find this
more explicit and should be faster too (I mean io probably does this
internally so we are minus the function calls). Similar for concatenating
strings - generally the pattern is join - has the advantage of cleaning
the \n from the strings making them more readable.

More work on _dumpFiles:

- fstrings - note exception (would not wrap)
- initially I defined a

    markstr = lambda x: ' N/A' if is_mark else x

but hey don't calculate the string if not necessary

Looks like a helper class for info pages is needed with a
_dump_files and a dict for holding the info pages singletons, key'd
by pageName -  or not

squash! is_archive/project erosion:

Cheated a bit using _valid_exts_re to tell archives apart - actually
this is more correct, we do care here about the Installer having a
meaningful extension rather than it being an archive.

strings_exts was unused

Mopy/bash/basher/__init__.py
@@ -52,7 +52,6 @@ has its own data store)."""
 # Imports ---------------------------------------------------------------------
 #--Python
 import collections
-import io
 import os
 import sys
 import time
@@ -2894,72 +2893,53 @@ class InstallersDetails(_SashDetailsPanel):
         if initialized: return
         else: self.infoPages[index][1] = True
         pageName = gPage.get_component_name()
-        def dumpFiles(files, header=u''):
+        def _dumpFiles(files, header=u''):
             if files:
-                buff = io.StringIO()
+                buff = []
                 files = bolt.sortFiles(files)
-                if header: buff.write(header+u'\n')
+                if header: buff.append(header)
                 for file in files:
                     oldName = installer.getEspmName(file)
-                    buff.write(oldName)
                     if oldName != file:
-                        buff.write(u' -> ')
-                        buff.write(file)
-                    buff.write(u'\n')
-                return buff.getvalue()
+                        oldName = f'{oldName} -> {file}'
+                    buff.append(oldName)
+                return buff.append('') or '\n'.join(buff) # add a newline
             elif header:
                 return header+u'\n'
             else:
                 return u''
         if pageName == u'gGeneral':
-            info = u'== '+_(u'Overview')+u'\n'
-            info += _(u'Type: ') + installer.type_string + u'\n'
-            info += installer.structure_string() + u'\n'
+            inf_ = ['== ' + _('Overview'), _('Type: ') + installer.type_string,
+                    installer.structure_string(), installer.size_info_str()]
             nConfigured = len(installer.ci_dest_sizeCrc)
             nMissing = len(installer.missingFiles)
             nMismatched = len(installer.mismatchedFiles)
-            if installer.is_archive():
-                if installer.isSolid:
-                    if installer.blockSize:
-                        sSolid = _(u'Solid, Block Size: %d MB') % installer.blockSize
-                    elif installer.blockSize is None:
-                        sSolid = _(u'Solid, Block Size: Unknown')
-                    else:
-                        sSolid = _(u'Solid, Block Size: 7z Default')
-                else:
-                    sSolid = _(u'Non-solid')
-                info += _(u'Size: %s (%s)') % (installer.size_string(),
-                                               sSolid) + u'\n'
-            else:
-                info += _(u'Size:') + u' %s\n' % installer.size_string(
-                    marker_string=u'N/A')
-            info += (_(u'Modified:') +u' %s\n' % format_date(installer.modified),
-                     _(u'Modified:') +u' N/A\n',)[installer.is_marker()]
-            info += (_(u'Data CRC:')+u' %08X\n' % installer.crc,
-                     _(u'Data CRC:')+u' N/A\n',)[installer.is_marker()]
-            info += (_(u'Files:') + u' %s\n' % installer.number_string(
-                installer.num_of_files, marker_string=u'N/A'))
-            info += (_(u'Configured:')+u' %u (%s)\n' % (
-                nConfigured, round_size(installer.unSize)),
-                     _(u'Configured:')+u' N/A\n',)[installer.is_marker()]
-            info += (_(u'  Matched:') + u' %s\n' % installer.number_string(
-                nConfigured - nMissing - nMismatched, marker_string=u'N/A'))
-            info += (_(u'  Missing:')+u' %s\n' % installer.number_string(
-                nMissing, marker_string=u'N/A'))
-            info += (_(u'  Conflicts:')+u' %s\n' % installer.number_string(
-                nMismatched, marker_string=u'N/A'))
-            info += u'\n'
-            #--Infoboxes
-            gPage.text_content = info + dumpFiles(
-                installer.ci_dest_sizeCrc, u'== ' + _(u'Configured Files'))
+            is_mark = installer.is_marker()
+            numstr = partial(installer.number_string, marker_string='N/A')
+            inf_.extend([
+                _('Modified:') + (' N/A' if is_mark else
+                    f' {format_date(installer.modified)}'),
+                _('Data CRC:') + (' N/A' if is_mark else
+                    f' {installer.crc:08X}'),
+                _('Files:') + f' {numstr(installer.num_of_files)}',
+                _('Configured:') + (' N/A' if is_mark else
+                    f' {nConfigured:d} ({round_size(installer.unSize)})'),
+                _('  Matched:') + ' %s' % numstr(
+                    nConfigured - nMissing - nMismatched),
+                 _('  Missing:') + f' {numstr(nMissing)}',
+                 _('  Conflicts:') + f' {numstr(nMismatched)}',
+                #--Infoboxes
+                _dumpFiles(installer.ci_dest_sizeCrc,
+                           '== ' + _('Configured Files'))])
+            gPage.text_content = u'\n'.join(inf_)
         elif pageName == u'gMatched':
-            gPage.text_content = dumpFiles(set(
+            gPage.text_content = _dumpFiles(set(
                 installer.ci_dest_sizeCrc) - installer.missingFiles -
                                            installer.mismatchedFiles)
         elif pageName == u'gMissing':
-            gPage.text_content = dumpFiles(installer.missingFiles)
+            gPage.text_content = _dumpFiles(installer.missingFiles)
         elif pageName == u'gMismatched':
-            gPage.text_content = dumpFiles(installer.mismatchedFiles)
+            gPage.text_content = _dumpFiles(installer.mismatchedFiles)
         elif pageName == u'gConflicts':
             gPage.text_content = self._idata.getConflictReport(
                 installer, u'OVER', bosh.modInfos)
@@ -2967,11 +2947,11 @@ class InstallersDetails(_SashDetailsPanel):
             gPage.text_content = self._idata.getConflictReport(
                 installer, u'UNDER', bosh.modInfos)
         elif pageName == u'gDirty':
-            gPage.text_content = dumpFiles(installer.dirty_sizeCrc)
+            gPage.text_content = _dumpFiles(installer.dirty_sizeCrc)
         elif pageName == u'gSkipped':
-            gPage.text_content = u'\n'.join((dumpFiles(
+            gPage.text_content = u'\n'.join((_dumpFiles(
                 installer.skipExtFiles, u'== ' + _(u'Skipped (Extension)')),
-                                             dumpFiles(
+                                             _dumpFiles(
                 installer.skipDirFiles, u'== ' + _(u'Skipped (Dir)'))))
 
     #--Config

Mopy/bash/basher/installer_links.py
@@ -413,7 +413,7 @@ class Installer_Duplicate(OneItemLink, _InstallerLink):
         """Duplicate selected Installer."""
         newName = self._selected_info.unique_key(self._selected_item.root,
                                                  add_copy=True)
-        allowed_exts = {} if not self._selected_info.is_archive() else {
+        allowed_exts = set() if not self._selected_info.is_archive() else {
             self._selected_item.ext}
         result = self._askFilename(
             _(u'Duplicate %s to:') % self._selected_item, newName.s,

Mopy/bash/bosh/bain.py
@@ -252,8 +252,9 @@ class Installer(ListInfo):
     def number_string(number, marker_string=u''):
         return str(number)
 
-    def size_string(self, marker_string=u''):
-        return round_size(self.fsize)
+    def size_string(self): return round_size(self.fsize)
+
+    def size_info_str(self): return  _(u'Size:') + u' %s' % self.size_string()
 
     def structure_string(self):
         if self.type == 1:
@@ -466,7 +467,7 @@ class Installer(ListInfo):
             dest = file_relative
             if not parentDir:
                 archiveRoot = GPath(
-                    self.archive).sroot if self.is_archive() else self.archive
+                    self.archive).sroot if self._valid_exts_re else self.archive
                 if fileLower in {u'masterlist.txt', u'dlclist.txt'}:
                     self.skipDirFiles.add(full)
                     return None # we dont want to install those files
@@ -632,14 +633,13 @@ class Installer(ListInfo):
         #--Bad archive?
         if bain_type not in {1,2}: return dest_src
         archiveRoot = GPath(
-            self.archive).sroot if self.is_archive() else self.archive
+            self.archive).sroot if self._valid_exts_re else self.archive
         docExts = self.docExts
         dataDirsPlus = self.dataDirsPlus
         dataDirsMinus = self.dataDirsMinus
         skipExts = self.skipExts
         unSize = 0
         bethFiles = bush.game.bethDataFiles
-        strings_exts = self._strings_extensions
         skips, global_skip_ext = self._init_skips()
         if self.overrideSkips:
             ##: We should split this - Override Skips & Override Redirects
@@ -1274,7 +1274,9 @@ class InstallerMarker(Installer):
     @staticmethod
     def number_string(number, marker_string=u''): return marker_string
 
-    def size_string(self, marker_string=u''): return marker_string
+    def size_string(self): return u''
+
+    def size_info_str(self): return  _(u'Size:') + u' N/A\n'
 
     def structure_string(self): return _(u'Structure: N/A')
 
@@ -1302,6 +1304,18 @@ class InstallerArchive(Installer):
 
     @classmethod
     def is_archive(cls): return True
+
+    def size_info_str(self):
+        if self.isSolid:
+            if self.blockSize:
+                sSolid = _(u'Solid, Block Size: %d MB') % self.blockSize
+            elif self.blockSize is None:
+                sSolid = _(u'Solid, Block Size: Unknown')
+            else:
+                sSolid = _(u'Solid, Block Size: 7z Default')
+        else:
+            sSolid = _(u'Non-solid')
+        return _(u'Size: %s (%s)') % (self.size_string(), sSolid)
 
     @classmethod
     def validate_filename_str(cls, name_str, allowed_exts=archives.writeExts,

commit 19b63b4b4c975b8cc336686ceed7ca88ec4cc237

Update column arrow images to match Windows 10
We're still using ones matching Windows... I dunno, 7? Vista? XP?
Committing this directly on dev so the rebased image binaries don't get
stuck in our git objects.

Mopy/bash/basher/__init__.py:
Add newline to General tab

Minor regression introduced in abad69badd397ebc0f287ab98089f9106b4f8ddf.

Mopy/bash/basher/__init__.py
@@ -2981,7 +2981,7 @@ class InstallersDetails(_SashDetailsPanel):
                     nConfigured - nMissing - nMismatched),
                  _('  Missing:') + f' {numstr(nMissing)}',
                  _('  Conflicts:') + f' {numstr(nMismatched)}',
-                #--Infoboxes
+                '', # One newline in between the main info and the file list
                 _dumpFiles(installer.ci_dest_sizeCrc,
                            '== ' + _('Configured Files'))])
             gPage.text_content = u'\n'.join(inf_)