wrye-bash/wrye-bash

commit fd791769caadbe56a82e43bc895dd22215f90c33

[!]BAIN details WIP - encapsulate the '&' handling:
A glimpse on some data structures in need of change/ gui logic
encapsulation:

- .replace(u'&', u'&&') moved inside set_all_items_keep_pos, that one
sure belongs there -> '*' handling only in plugins though not on subs ofc
- espm_checklist_fns - last time (of many) I try to discern espms from
espms
- gEspmList further encapsulated and common ugly code like ...
- ...replace(u'&&', u'&') centralized - should we move this to lb_ ? WIP!
- we should also modify lb_get_str_items?

!:
- basher.installer_links.Installer_Wizard.Execute: we shouldn't chop off
the * here probably (see idetails.get_espm)
- Installer_Espm_List -> was not doing any get_espm (&& -> &, chop off
the star) I added it -> correct ?

basher/installer_links.py: the `or bool(sel_package.espmNots.add(...))`
is to get me rid of an occurrence of an extra lb_check_at_index

Mopy/bash/gui/multi_choices.py: get_checked_strings was unused

Mopy/bash/basher/installer_links.py: bugfix - newName (no ext) would
never be in espm_checklist_fns

Mopy/bash/belt.py: last minute addition (!) - move this ampersand logic
where it belongs

Under #219

Mopy/bash/basher/installer_links.py
@@ -285,15 +285,14 @@ class Installer_Wizard(_Installer_AWizardLink):
                     sel_package.subActives[index + 1] = select
                 idetails.refreshCurrent(sel_package)
                 # Check the plugins that were selected by the wizard
-                espms = idetails.gEspmList.lb_get_str_items()
-                espms = [x.replace(u'&&',u'&') for x in espms]
+                espm_strs = idetails.gEspmList.lb_get_str_items()
+                # don't chop off the leading star if there but replace && ##: do this in lb_get_str_items
+                espm_strs = [x.replace(u'&&',u'&') for x in espm_strs]
                 sel_package.espmNots = set()
-                for index, espm in enumerate(idetails.espms):
-                    if espms[index] in ret.select_plugins:
-                        idetails.gEspmList.lb_check_at_index(index, True)
-                    else:
-                        idetails.gEspmList.lb_check_at_index(index, False)
-                        sel_package.espmNots.add(espm)
+                for index, espm in enumerate(idetails.espm_checklist_fns):
+                    do_check = espm_strs[index] in ret.select_plugins or bool(
+                        sel_package.espmNots.add(espm)) # bool(None) == False
+                    idetails.gEspmList.lb_check_at_index(index, do_check)
                 idetails.refreshCurrent(sel_package)
                 #Rename the espms that need renaming
                 for oldName, renamed in ret.rename_plugins.items():
@@ -863,7 +862,7 @@ class Installer_CopyConflicts(_SingleInstallable):
 #------------------------------------------------------------------------------
 class _Installer_Details_Link(EnabledLink):
 
-    def _enable(self): return len(self.window.espms) != 0
+    def _enable(self): return len(self.window.espm_checklist_fns) != 0
 
     def _initData(self, window, selection):
         """:type window: bosh.InstallersDetails
@@ -887,7 +886,7 @@ class Installer_Espm_DeselectAll(_Installer_Details_Link):
     _help = _(u'Deselects all plugin files in the selected sub-packages.')
 
     def Execute(self):
-        self._installer.espmNots = set(self.window.espms)
+        self._installer.espmNots = set(self.window.espm_checklist_fns)
         self.window.gEspmList.set_all_checkmarks(checked=False)
         self.window.refreshCurrent(self._installer)
 
@@ -899,16 +898,13 @@ class Installer_Espm_Rename(_Installer_Details_Link):
     def _enable(self): return self.selected != -1
 
     def Execute(self):
-        curName = self.window.gEspmList.lb_get_str_item_at_index(self.selected).replace(u'&&',
-                                                                         u'&')
-        if curName[0] == u'*':
-            curName = curName[1:]
-        _file = GPath(curName)
+        _file = self.window.get_espm(self.selected)
         newName = self._askText(_(u'Enter new name (without the extension):'),
                                 title=_(u'Rename Plugin'), default=_file.sbody)
         if not newName: return
-        if newName in self.window.espms: return
-        self._installer.setEspmName(curName, newName + _file.cext)
+        if (newName := newName + _file.cext) in self.window.espm_checklist_fns:
+            return
+        self._installer.setEspmName(_file.s, newName)
         self.window.refreshCurrent(self._installer)
 
 class Installer_Espm_Reset(_Installer_Details_Link):
@@ -919,11 +915,8 @@ class Installer_Espm_Reset(_Installer_Details_Link):
 
     def _enable(self):
         if self.selected == -1: return False
-        curName = self.window.gEspmList.lb_get_str_item_at_index(self.selected).replace(u'&&',
-                                                                         u'&')
-        if curName[0] == u'*': curName = curName[1:]
-        self.curName = curName
-        return self._installer.isEspmRenamed(curName)
+        self.curName = self.window.get_espm(self.selected)
+        return self._installer.isEspmRenamed(self.curName)
 
     def Execute(self):
         self._installer.resetEspmName(self.curName)
@@ -949,8 +942,8 @@ class Installer_Espm_List(_Installer_Details_Link):
         subs = _(u'Plugin List for %s:') % self._installer + u'\n[spoiler]\n'
         espm_list = self.window.gEspmList
         for index in range(espm_list.lb_get_items_count()):
-            subs += [u'   ',u'** '][espm_list.lb_is_checked_at_index(index)] + \
-                    espm_list.lb_get_str_item_at_index(index) + u'\n'
+            subs += [u'   ', u'** '][espm_list.lb_is_checked_at_index(
+                index)] + self.window.get_espm(index) + u'\n'
         subs += u'[/spoiler]'
         copy_text_to_clipboard(subs)
         self._showLog(subs, title=_(u'Plugin List'), fixedFont=False)
@@ -965,10 +958,7 @@ class Installer_Espm_JumpToMod(_Installer_Details_Link):
         if self.selected == -1: return False
         ##: Maybe refactor all this plugin logic (especially the renamed plugin
         # (asterisk) handling) to a property inside a base class?
-        selected_plugin = self.window.gEspmList.lb_get_str_item_at_index(
-            self.selected).replace(u'&&', u'&')
-        if selected_plugin[0] == u'*': selected_plugin = selected_plugin[1:]
-        self.target_plugin = GPath(selected_plugin)
+        self.target_plugin = self.window.get_espm(self.selected)
         return self.target_plugin in bosh.modInfos
 
     def Execute(self):

Mopy/bash/basher/__init__.py
@@ -57,7 +57,7 @@ import sys
 import time
 from collections import OrderedDict, namedtuple, defaultdict
 from functools import partial, reduce
-from typing import Optional
+from typing import Optional, List
 
 #--wxPython
 import wx
@@ -2782,7 +2782,8 @@ class InstallersDetails(_SashDetailsPanel):
         self.sp_label = Label(self.sp_panel, _(u'Sub-Packages'))
         self._update_fomod_state()
         #--Espms
-        self.espms = []
+        # sorted list of the displayed installer espm names - esms sorted first
+        self.espm_checklist_fns = [] # type: List[bolt.Path]
         self.gEspmList = CheckListBox(espmsPanel, isExtended=True)
         self.gEspmList.on_box_checked.subscribe(self._on_check_plugin)
         self.gEspmList.on_mouse_left_dclick.subscribe(
@@ -2844,7 +2845,7 @@ class InstallersDetails(_SashDetailsPanel):
             self._save_comments()
         fileName = super(InstallersDetails, self).SetFile(fileName)
         self._displayed_installer = fileName
-        del self.espms[:]
+        del self.espm_checklist_fns[:]
         if fileName:
             installer = self._idata[fileName]
             #--Name
@@ -2860,21 +2861,21 @@ class InstallersDetails(_SashDetailsPanel):
             if len(installer.subNames) <= 2:
                 self.gSubList.lb_clear()
             else:
-                sub_names_ = [x.replace(u'&', u'&&') for x in
-                              installer.subNames[1:]]
-                vals = installer.subActives[1:]
-                self.gSubList.set_all_items_keep_pos(sub_names_, vals)
+                ##: TODO(ut) subNames/subActives should be a dict really no?
+                sub_isactive = zip(installer.subNames, installer.subActives)
+                next(sub_isactive) # pop empty subpackage, duh
+                sub_isactive = {k: v for k, v in sub_isactive}
+                self.gSubList.set_all_items_keep_pos(sub_isactive)
             self._update_fomod_state()
             #--Espms
             if not installer.espms:
                 self.gEspmList.lb_clear()
             else:
-                names = self.espms = sorted(installer.espms)
-                names.sort(key=lambda x: x.cext != u'.esm')
-                names_ = [[u'', u'*'][installer.isEspmRenamed(x.s)] +
-                          x.s.replace(u'&', u'&&') for x in names]
-                vals = [x not in installer.espmNots for x in names]
-                self.gEspmList.set_all_items_keep_pos(names_, vals)
+                fns = self.espm_checklist_fns = sorted(installer.espms, key=lambda x: (
+                    x.cext != u'.esm', x)) # esms first then alphabetically
+                espm_acti = {['', '*'][installer.isEspmRenamed(
+                    x)] + x.s: x not in installer.espmNots for x in fns}
+                self.gEspmList.set_all_items_keep_pos(espm_acti)
             #--Comments
             self.gComments.text_content = installer.comments
 
@@ -2996,28 +2997,28 @@ class InstallersDetails(_SashDetailsPanel):
     def _on_check_plugin(self, lb_selection_dex):
         """Handle check/uncheck of item."""
         espmNots = self.file_info.espmNots
-        plugin_name = self.gEspmList.lb_get_str_item_at_index(
-            lb_selection_dex).replace(u'&&', u'&')
-        if plugin_name[0] == u'*':
-            plugin_name = plugin_name[1:]
-        espm = GPath(plugin_name)
+        plugin_name = self.get_espm(lb_selection_dex)
         if self.gEspmList.lb_is_checked_at_index(lb_selection_dex):
-            espmNots.discard(espm)
+            espmNots.discard(plugin_name)
         else:
-            espmNots.add(espm)
+            espmNots.add(plugin_name)
         self.gEspmList.lb_select_index(lb_selection_dex)    # so that (un)checking also selects (moves the highlight)
         if not get_shift_down():
             self.refreshCurrent(self.file_info)
 
+    def get_espm(self, lb_selection_dex):
+        plugin_name = self.gEspmList.lb_get_str_item_at_index(
+            lb_selection_dex).replace(u'&&', u'&')
+        if plugin_name[0] == u'*':
+            plugin_name = plugin_name[1:]
+        return GPath_no_norm(plugin_name)
+
     def _on_plugin_filter_dclick(self, selected_index):
         """Handles double-clicking on a plugin in the plugin filter."""
         if selected_index < 0: return
-        selected_name = self.gEspmList.lb_get_str_item_at_index(
-            selected_index).replace(u'&&', u'&')
-        if selected_name[0] == u'*': selected_name = selected_name[1:]
-        selected_plugin = GPath(selected_name)
-        if selected_plugin not in bosh.modInfos: return
-        balt.Link.Frame.notebook.SelectPage(u'Mods', selected_plugin)
+        selected_name = self.get_espm(selected_index)
+        if selected_name not in bosh.modInfos: return
+        balt.Link.Frame.notebook.SelectPage(u'Mods', selected_name)
 
     def set_subpackage_checkmarks(self, checked):
         """Checks or unchecks all subpackage checkmarks and propagates that

Mopy/bash/belt.py
@@ -282,22 +282,21 @@ class PageFinish(PageInstaller):
         subs = sorted(subsList)
         plugins = sorted(plugin_list)
         #--make the list that will be displayed
-        displayed_plugins = [x.replace(u'&', u'&&') + (
-            u' -> ' + plugin_renames[x] if x in plugin_renames else u'')
-                             for x in plugins]
+        displayed_plugins = [f'{x} -> {plugin_renames[x]}'
+                             if x in plugin_renames else x for x in plugins]
         parent.parser.choiceIdex += 1
         textTitle = Label(self, _(u'The installer script has finished, and '
                                   u'will apply the following settings:'))
         textTitle.wrap(parent.get_page_size()[0] - 10)
         # Sub-packages
-        self.listSubs = CheckListBox(self,
-            choices=[x.replace(u'&', u'&&') for x in subs])
+        self.listSubs = CheckListBox(self, choices=subs, ampersand=True)
         self.listSubs.on_box_checked.subscribe(self._on_select_subs)
         for index,key in enumerate(subs):
             if subsList[key]:
                 self.listSubs.lb_check_at_index(index, True)
                 self._wiz_parent.ret.select_sub_packages.append(key)
-        self.plugin_selection = CheckListBox(self, choices=displayed_plugins)
+        self.plugin_selection = CheckListBox(self, choices=displayed_plugins,
+                                             ampersand=True)
         self.plugin_selection.on_box_checked.subscribe(self._on_select_plugin)
         for index,key in enumerate(plugins):
             if plugin_list[key]:

Mopy/bash/gui/multi_choices.py
@@ -183,7 +183,7 @@ class ListBox(WithMouseEvents):
                          state=_wx.LIST_STATE_SELECTED):
         return self._native_widget.GetNextItem(item, geometry, state)
 
-    def lb_get_str_item_at_index(self, lb_selection_dex):
+    def lb_get_str_item_at_index(self, lb_selection_dex): ##: && ->& ?
         return self._native_widget.GetString(lb_selection_dex)
 
     def lb_get_str_items(self):
@@ -232,7 +232,9 @@ class CheckListBox(ListBox, WithCharEvents):
 
     # note isSingle=False by default
     def __init__(self, parent, choices=None, isSingle=False, isSort=False,
-                 isHScroll=False, isExtended=False, onSelect=None):
+                 isHScroll=False, isExtended=False, onSelect=None,
+                 ampersand=False):
+        if ampersand: choices = [x.replace(u'&', u'&&') for x in choices]
         super(CheckListBox, self).__init__(parent, choices, isSingle, isSort,
                                            isHScroll, isExtended, onSelect)
         self.on_box_checked = self._evt_handler(_wx.EVT_CHECKLISTBOX,
@@ -257,11 +259,7 @@ class CheckListBox(ListBox, WithCharEvents):
             for i in range(self.lb_get_items_count()):
                 self.lb_check_at_index(i, checked)
 
-    def get_checked_strings(self):
-        """Returns a list of string representations of all checked items."""
-        return self._native_widget.GetCheckedStrings()
-
-    def set_all_items(self, keys_values):
+    def set_all_items(self, keys_values): ##: & ->  &&
         """Completely clears the list and repopulates it using the specified
         key and value lists. Much faster than set_all_items_keep_pos, but
         discards the current scroll position."""
@@ -272,21 +270,22 @@ class CheckListBox(ListBox, WithCharEvents):
                 self.lb_check_at_index(i, v)
 
     ##: Test that the claim below is actually accurate
-    def set_all_items_keep_pos(self, names, checkmarks):
+    def set_all_items_keep_pos(self, keys_values):
         """Convenience method for setting a bunch of wxCheckListBox items. The
         main advantage of this is that it doesn't clear the list unless it
         needs to, which is good if you want to preserve the scroll position
         of the list. If you do not need that behavior, however, use
         set_all_items instead as it is much faster."""
-        if not names:
+        if not keys_values:
             self.lb_clear()
             return
         with self.pause_drawing():
-            for index, (lab, ch) in enumerate(zip(names, checkmarks)):
+            for index, (lab, ch) in enumerate(keys_values.items()):
+                lab = lab.replace('&', '&&')
                 if index >= self.lb_get_items_count():
                     self.lb_append(lab)
                 else:
                     self.lb_set_label_at_index(index, lab)
                 self.lb_check_at_index(index, ch)
-            for index in range(self.lb_get_items_count(), len(names), -1):
+            for index in range(self.lb_get_items_count(), len(keys_values), -1):
                 self.lb_delete_at_index(index - 1)

commit 0277bf2f5293a48304384c2eaa5316849dd60c4f

Regression fixes - pytest 7.0.1:
Fix regression in List Plugins

Broke in fd791769caadbe56a82e43bc895dd22215f90c33:

Traceback (most recent call last):
  File "bash\balt.py", line 1786, in __Execute
  File "bash\basher\installer_links.py", line 950, in Execute
TypeError: can only concatenate str (not "Path") to str

TES4: Fix regression in Sort: Ingestibles

Broke in 802423a474da3e16631718de2ade72e3ed38383f.

ci: Bump pytest to 7.0.1

Just some minor bugfixes.

Utumno:

Mopy/bash/balt.py: Fixup for delinquent masters dialog:

Introduced in 94c8ba1d7e97c677109f00afaff70e257b259c3b - 'Continue
Despite Errors' would return an empty tuple instead of True - some
whitespace fixes also - I think I prefer the star here. Compliments
of 543 testing.

Co-authored-by: MrD <the.ubik@gmail.com>

Mopy/bash/balt.py
@@ -2120,8 +2120,8 @@ class ListBoxes(WrappingTextMixin, DialogWindow):
         with cls(*args, **kwargs) as dialog:
             res = dialog.show_modal()
             if not res: return tuple(() for __ in range(len(get_checked)))
-            return tuple(
-                dialog._get_checked(*args_tuple) for args_tuple in get_checked)
+            return tuple(dialog._get_checked(*args_tuple) for args_tuple in
+                         get_checked) or res # or for when get_checked is empty
 
     def _get_checked(self, cntrl_key, items):
         """Return a sublist of 'items' containing (un)checked items.

Mopy/bash/basher/installer_links.py
@@ -943,9 +943,9 @@ class Installer_Espm_List(_Installer_Details_Link):
     def Execute(self):
         subs = _(u'Plugin List for %s:') % self._installer + u'\n[spoiler]\n'
         espm_list = self.window.gEspmList
-        for index in range(espm_list.lb_get_items_count()):
-            subs += [u'   ', u'** '][espm_list.lb_is_checked_at_index(
-                index)] + self.window.get_espm(index) + u'\n'
+        for i in range(espm_list.lb_get_items_count()):
+            sub_prefix = ['   ', '** '][espm_list.lb_is_checked_at_index(i)]
+            subs += f'{sub_prefix}{self.window.get_espm(i)}\n'
         subs += u'[/spoiler]'
         copy_text_to_clipboard(subs)
         self._showLog(subs, title=_(u'Plugin List'), fixedFont=False)

Mopy/bash/basher/installers_links.py
@@ -140,13 +140,13 @@ class Installers_MonitorInstall(Installers_Link):
                 u'These files were deleted.  BAIN does not have the '
                 u'capability to remove files when installing.'), ]
             group.extend(sorted(delFiles))
-        lists= ListBoxes.display_dialog(self.window, self._dialog_title,
+        lists = ListBoxes.display_dialog(self.window, self._dialog_title,
             _(u'The following changes were detected in the %s directory.'
               ) % bush.game.mods_dir,
             checklists, bOk=_(u'Create Project'), get_checked=[(
                 newFilesKey, newFiles), (changedFilesKey, changedFiles),
                 (touchedFilesKey, touchedFiles)])
-        include = set(chain.from_iterable(lists))
+        include = set(chain(*lists))
         if not include: return
         # Create Project
         projectName = self._askFilename(_(u'Project Name'), u'', ##: some default here?
@@ -280,7 +280,7 @@ class Installers_UninstallAllUnknownFiles(Installers_Link):
                 _('Uncheck files to keep them in the %s folder.') % mdir]
             all_unknown_files.sort()
             message.extend(all_unknown_files)
-            selected_unknown_files, =  ListBoxes.display_dialog(self.window,
+            selected_unknown_files, = ListBoxes.display_dialog(self.window,
                 _(u'Move files out of the %s folder.') % mdir, _(
                 'Uncheck any files you want to keep in the %s folder.') % mdir,
                 [message], get_checked=[(message[0], all_unknown_files)])

Mopy/bash/basher/mod_links.py
@@ -1074,7 +1074,7 @@ class Mod_Patch_Update(_Mod_BP_Link):
         if not checklists: return
         getchecks = [(unfilteredKey, unfiltered), (mergeKey, merge),
                      (noMergeKey, noMerge), (deactivateKey, deactivate)]
-        deselect = set(chain.from_iterable(ListBoxes.display_dialog(Link.Frame,
+        deselect = set(chain(*ListBoxes.display_dialog(Link.Frame,
             _('Deactivate these mods prior to patching'),
             _('The following mods should be deactivated prior to building the '
               'patch.'), checklists, bCancel=_('Skip'),

Mopy/bash/patcher/patchers/multitweak_names.py
@@ -215,7 +215,7 @@ class _AEffectsTweak_Tes4(_AEffectsTweak, _AMgefNamesTweak):
     def _get_spell_level(self, record):
         return record.level
 
-    def _has_harmful_effect(self, record):
+    def _is_harmful(self, record):
         for rec_effect in record.effects:
             if not self._is_effect_hostile(rec_effect):
                 return False