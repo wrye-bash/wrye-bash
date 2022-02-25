wrye-bash/wrye-bash

commit 239cfeb32618915fb2f451963475d0d9eac4df3a

Fixups - bump lz4:
Fix LOOT condition evaluation

Needs an FName (or string, so just pass that).

Bump lz4 to 4.0.0

Pretty much just makes 3.10 wheels available, the breaking change
(removing lz4.stream by default) does not affect us.

Clean up some minor issues around _ask*

_askNumber's return value should always be compared against None,
otherwise it'll swallow up 0.

Mopy/bash/bosh/converters.py:
Just some minor typing improvements to get PyCharm to shut up

Mopy/bash/bosh/bain.py:
deleted -> del_item to not shadow the method parameter.

Utumno:
Mopy/bash/bosh/__init__.py: regression fixup (unknown parameter)

Mopy/bash/basher/gui_patchers.py:
Mopy/bash/basher/installer_links.py:
more 'value' renames

Mopy/bash/basher/ini_links.py: random nit

Co-authored-by: MrD <the.ubik@gmail.com>

Mopy/bash/bosh/loot_conditions.py
@@ -287,7 +287,7 @@ def _fn_version(file_path, expected_ver, comparison):
         # Read version from the description
         from . import modInfos
         actual_ver = LooseVersion(
-            modInfos.getVersion(file_path.tail) or '0')
+            modInfos.getVersion(file_path.stail) or '0')
     elif file_path.cext in ('.exe', '.dll'):
         # Read version from executable fields
         actual_ver = LooseVersion(_read_binary_ver(file_path.s))

Mopy/bash/basher/gui_patchers.py
@@ -649,7 +649,7 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
     def tweak_custom_choice(self, index, tweakIndex):
         """Handle choice menu selection."""
         tweak = self._all_tweaks[tweakIndex] # type: base.AMultiTweakItem
-        value = []
+        values = []
         new = None
         for i, v in enumerate(tweak.choiceValues[index]):
             if tweak.show_key_for_custom:
@@ -673,7 +673,7 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
                     if new is None: #user hit cancel
                         return
                     try:
-                        value.append(float(new))
+                        values.append(float(new))
                         new = None # Reset, we may have a multi-key tweak
                         break
                     except ValueError:
@@ -691,7 +691,7 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
                     value=tweak.choiceValues[index][i], min=-10000, max=10000)
                 if new is None: #user hit cancel
                     return
-                value.append(new)
+                values.append(new)
             elif isinstance(v, str):
                 label = (_(u'Enter the desired custom tweak text.')
                          + key_display)
@@ -701,26 +701,25 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
                     default=tweak.choiceValues[index][i], strip=False) ##: strip ?
                 if new is None: #user hit cancel
                     return
-                value.append(new)
-        if not value:
-            value = tweak.choiceValues[index]
-        value = tuple(value)
-        validation_error = tweak.validate_values(value)
+                values.append(new)
+        if not values:
+            values = tweak.choiceValues[index]
+        values = tuple(values)
+        validation_error = tweak.validate_values(values)
         if not validation_error: # no error, we're good to go
-            tweak.choiceValues[index] = value
+            tweak.choiceValues[index] = values
             tweak.chosen = index
-            label = _custom_label(tweak.getListLabel(), value[0])
+            label = _custom_label(tweak.getListLabel(), values[0])
             self.gTweakList.lb_set_label_at_index(tweakIndex, label)
             self.gTweakList.lb_check_at_index(tweakIndex, True)
             self.TweakOnListCheck() # fired so this line is needed (?)
         else:
             # The tweak doesn't like the values the user chose, let them know
             error_header = (_(u'The value you entered (%s) is not valid '
-                              u'for this tweak.') % value[0]
-                            if len(value) == 1 else
+                              u'for this tweak.') % values[0]
+                            if len(values) == 1 else
                             _(u'The values you entered (%s) are not valid '
-                              u'for this tweak.') % u', '.join(
-                                str(s) for s in value))
+                              u'for this tweak.') % ', '.join(map(str, values)))
             balt.showError(self.gConfigPanel,
                            error_header + u'\n\n' + validation_error,
                            title=_(u'%s - Error') % tweak.tweak_name)

Mopy/bash/basher/ini_links.py
@@ -105,10 +105,8 @@ class INI_FileOpenOrCopy(EnabledLink):
                    u"tweak(s).")][self._targets_default()]
 
     def Execute(self):
-        newly_copied = []
-        for i in self.selected:
-            if bosh.iniInfos.open_or_copy(i):
-                newly_copied.append(i)
+        newly_copied = [i for i in self.selected if
+                        bosh.iniInfos.open_or_copy(i)]
         self.window.RefreshUI(redraw=newly_copied)
 
 #------------------------------------------------------------------------------
@@ -160,10 +158,9 @@ class INI_CreateNew(OneItemLink):
     def link_help(self):
         if not len(self.selected) == 1:
             return _(u'Please choose one Ini Tweak')
-        else:
-            return _(u"Creates a new tweak based on '%(tweak)s' but with "
-                          u"values from '%(ini)s'.") % {
-                u'tweak': (self.selected[0]), u'ini': self.window.current_ini_name}
+        return _(u"Creates a new tweak based on '%(tweak)s' but with "
+                 u"values from '%(ini)s'.") % {u'tweak': (self.selected[0]),
+                   u'ini': self.window.current_ini_name}
 
     def _enable(self): return super(INI_CreateNew, self)._enable() and \
                               self._selected_info.tweak_status() >= 0

Mopy/bash/basher/installer_links.py
@@ -78,11 +78,11 @@ class _InstallerLink(Installers_Link, EnabledLink):
     """Common functions for installer links..."""
 
     ##: Methods below should be in archives.py
-    def _promptSolidBlockSize(self, title, value=0):
+    def _promptSolidBlockSize(self, title, default_size=0):
         return self._askNumber(
             _(u'Use what maximum size for each solid block?') + u'\n' + _(
                 u"Enter '0' to use 7z's default size."), prompt=u'MB',
-            title=title, value=value, min=0, max=102400)
+            title=title, value=default_size, min=0, max=102400)
 
     def _pack(self, archive_path, installer, project, release=False):
         #--Archive configuration options
@@ -1320,7 +1320,7 @@ class InstallerConverter_Create(_InstallerConverter_Link):
         blockSize = None
         if destInstaller.isSolid:
             blockSize = self._promptSolidBlockSize(
-                title=self._dialog_title, value=destInstaller.blockSize or 0)
+                title=self._dialog_title, default_size=destInstaller.blockSize or 0)
         with balt.Progress(_(u'Creating %s...') % BCFArchive,u'\n'+u' '*60) as progress:
             #--Create the converter
             converter = bosh.converters.InstallerConverter(self.selected,

Mopy/bash/basher/mod_links.py
@@ -2034,8 +2034,7 @@ class Mod_Scripts_Import(_Mod_Import_Link):
         textDir = self._askDirectory(
             message=_(u'Choose directory to import scripts from'),
             defaultPath=defaultPath)
-        if textDir is None:
-            return
+        if not textDir: return
         message = (_(u"Import scripts that don't exist in the esp as new"
                      u' scripts?') + u'\n' +
                    _(u'(If not they will just be skipped).')

Mopy/bash/basher/saves_links.py
@@ -389,7 +389,7 @@ class Save_Renumber(EnabledLink):
             _(u'Enter new number to start numbering the selected saves at.'),
             prompt=_(u'Save Number'), title=_(u'Re-number Saves'), value=1,
             min=1, max=10000)
-        if not newNumber: return
+        if newNumber is None: return
         old_names = set()
         new_names = set()
         for old_file_path, maPattern, sinf in self._matches:
@@ -599,7 +599,7 @@ class Save_EditCreatedEnchantmentCosts(OneItemLink):
               u'custom made enchantments.') + u'\n' + _(
                 u'(Enter 0 for unlimited uses)'), prompt=_(u'Uses'),
             title=_(u'Number of Uses'), value=50, min=0, max=10000)
-        if not dialog: return
+        if dialog is None: return
         Enchantments = bosh._saves.SaveEnchantments(self._selected_info)
         Enchantments.load()
         Enchantments.setCastWhenUsedEnchantmentNumberOfUses(dialog)

Mopy/bash/bosh/__init__.py
@@ -1803,7 +1803,7 @@ class DefaultIniInfo(DefaultIniFile, INIInfo):
     def is_default_tweak(self): return True
 
 # noinspection PyUnusedLocal
-def ini_info_factory(fullpath, load_cache=u'Ignored'):
+def ini_info_factory(fullpath, load_cache=u'Ignored', itsa_ghost=False):
     """INIInfos factory
     :param fullpath: fullpath to the ini file to wrap
     :param load_cache: dummy param used in INIInfos#new_info factory call

Mopy/bash/bosh/bain.py
@@ -1893,8 +1893,8 @@ class InstallersData(DataStore):
         elif refresh_info is None:
             refresh_info = self._RefreshInfo(deleted, pending, projects)
         changed = refresh_info.refresh_needed()
-        for deleted in refresh_info.deleted:
-            self.pop(deleted)
+        for del_item in refresh_info.deleted:
+            self.pop(del_item)
         pending, projects = refresh_info.pending, refresh_info.projects
         #--New/update crcs?
         for subPending, inst_type in zip(

commit 31af4f6094c5e767ad641cc0fc758f720c883b36

Fix regression in LOOT condition evaluation
Broke in 239cfeb32618915fb2f451963475d0d9eac4df3a. This only works on
543 due to strings getting implicitly converted into FNames by FNDict.
On dev, we *have* to pass paths in.

Mopy/bash/_games_lo.py:
TES5: Correct HearthFires.esm case in plugins.txt

Shouldn't cause any issues to have it in there with the wrong case, but
just to be safe...

requirements.txt:
Bump pygit2 to 1.9.0

Mopy/bash/_games_lo.py
@@ -1419,8 +1419,8 @@ class WindowsStoreGame(LoGame):
 # TextfileGame overrides
 class Skyrim(TextfileGame):
     must_be_active_if_present = tuple(GPath_no_norm(p) for p in (
-        u'Update.esm', u'Dawnguard.esm', u'Hearthfires.esm',
-        u'Dragonborn.esm'))
+        'Update.esm', 'Dawnguard.esm', 'HearthFires.esm',
+        'Dragonborn.esm'))
 
 class Enderal(TextfileGame):
     must_be_active_if_present = tuple(GPath_no_norm(p) for p in (
@@ -1602,7 +1602,7 @@ class Fallout4VR(Fallout4):
 
 class SkyrimSE(AsteriskGame):
     must_be_active_if_present = tuple(GPath_no_norm(p) for p in (
-        u'Update.esm', u'Dawnguard.esm', u'Hearthfires.esm', u'Dragonborn.esm'
+        'Update.esm', 'Dawnguard.esm', 'HearthFires.esm', 'Dragonborn.esm'
     ))
     _ccc_filename = u'Skyrim.ccc'
     _ccc_fallback = tuple(GPath_no_norm(p) for p in (
@@ -1691,8 +1691,8 @@ class SkyrimVR(SkyrimSE):
 class EnderalSE(SkyrimSE):
     # Update.esm is forcibly loaded after the (empty) DLC plugins by the game
     must_be_active_if_present = tuple(GPath_no_norm(p) for p in (
-        u'Dawnguard.esm', u'Hearthfires.esm', u'Dragonborn.esm', u'Update.esm',
-        u'Enderal - Forgotten Stories.esm',
+        'Dawnguard.esm', 'HearthFires.esm', 'Dragonborn.esm', 'Update.esm',
+        'Enderal - Forgotten Stories.esm',
     ))
 
     def _active_entries_to_remove(self):

Mopy/bash/bosh/loot_conditions.py
@@ -287,7 +287,7 @@ def _fn_version(file_path, expected_ver, comparison):
         # Read version from the description
         from . import modInfos
         actual_ver = LooseVersion(
-            modInfos.getVersion(file_path.stail) or '0')
+            modInfos.getVersion(file_path.tail) or '0')
     elif file_path.cext in ('.exe', '.dll'):
         # Read version from executable fields
         actual_ver = LooseVersion(_read_binary_ver(file_path.s))