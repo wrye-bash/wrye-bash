wrye-bash/wrye-bash

commit c55b60dfee2cb4337ddbcb056075d0cb76ee818b

More #543 nit:
Mopy/bash/basher/mod_links.py: could a masterName be a ghost? I guess no
(plus regex should be r'\.esp(\.ghost)?$')
len(modinfo_masters) > 1 ? (it's assuming the master esm is always a
master , not true(?))

StringTable.load unused?

Mopy/bash/balt.py: 'strings 'renames

Mopy/bash/belt.py:
- not sure why we were copying in _SelectAll
- belt data structures are very difficult to track, partly because they
end up in functions that are called by the parser (I guess those
receive str) - partly because they are passed to PageFinish
then that `ret` thing. Especially plugin_renames are a pain to follow.
Anyway did a few renames and refactoring to reduce uses:

plugin_list -> _?plugin_enabled
subsList -> sublist, yep it's the self.sublist

Mopy/bash/archives.py: another not needed .s

Mopy/bash/basher/mod_links.py: we mostly didn't need headTail

Mopy/bash/parsers.py: we don't need to GPath anymore

Mopy/bash/barb.py: avoid exists() when we just listed
Mopy/bash/basher/settings_dialog.py: only use of GPath in here - went
with os (normpath should not make a difference)

Infernio:

Mopy/bash/gui/misc_components.py: minor typing

Mopy/bash/ScriptParser.py
@@ -163,36 +163,6 @@ Types = {UNKNOWN:u'UNKNOWN',
          COLON:u'COLON',
          }
 
-# getType ---------------------------------------
-#  determines the type of a string.  If 'parser'
-#  is passed, then it will attempt it against
-#  various names as well.
-#------------------------------------------------
-def getType(item, parser=None):
-    if isinstance(item, str):
-        if not parser: return STRING
-        if item in parser.constants: return CONSTANT
-        if item in parser.variables: return VARIABLE
-        if item in parser.keywords : return KEYWORD
-        if item in parser.functions: return FUNCTION
-        if item in parser.operators: return OPERATOR
-        if item == u'(': return OPEN_PARENS
-        if item == u')': return CLOSE_PARENS
-        if item == u'[': return OPEN_BRACKET
-        if item == u']': return CLOSE_BRACKET
-        if item == u':': return COLON
-        if item == u',': return COMMA
-        if validName(item): return NAME
-        if validNumber(item):
-            if u'.' in item: return DECIMAL
-            return INTEGER
-        for i in item:
-            if i not in whitespace: return UNKNOWN
-        return WHITESPACE
-    if isinstance(item, int): return INTEGER
-    if isinstance(item, float): return DECIMAL
-    return UNKNOWN
-
 # FlowControl -------------------------------------
 #  Flow control object, to hold info about a flow
 #  control statement
@@ -230,6 +200,31 @@ class Parser(object):
     class ParserType(object):
         @property
         def Type(self): return self.__class__.__name__
+
+    def getType(self, token_or_num):
+        """Determine a token's type in self's type system."""
+        if isinstance(token_or_num, str): ##: use a dict here?
+            if token_or_num in self.constants: return CONSTANT
+            if token_or_num in self.variables: return VARIABLE
+            if token_or_num in self.keywords : return KEYWORD
+            if token_or_num in self.functions: return FUNCTION
+            if token_or_num in self.operators: return OPERATOR
+            if token_or_num == u'(': return OPEN_PARENS
+            if token_or_num == u')': return CLOSE_PARENS
+            if token_or_num == u'[': return OPEN_BRACKET
+            if token_or_num == u']': return CLOSE_BRACKET
+            if token_or_num == u':': return COLON
+            if token_or_num == u',': return COMMA
+            if validName(token_or_num): return NAME
+            if validNumber(token_or_num):
+                if u'.' in token_or_num: return DECIMAL
+                return INTEGER
+            for i in token_or_num:
+                if i not in whitespace: return UNKNOWN
+            return WHITESPACE
+        if isinstance(token_or_num, int): return INTEGER
+        if isinstance(token_or_num, float): return DECIMAL
+        return UNKNOWN
 
     class Callable(ParserType):
         def __init__(self, callable_name, function, min_args=0,
@@ -335,7 +330,7 @@ class Parser(object):
                 self.numArgs = token_or_text.numArgs
             else:
                 self.text = token_or_text
-                self.type = Type or getType(token_or_text, parser)
+                self.type = Type or parser.getType(token_or_text)
                 self.parser = parser
                 self.line = line
                 self.pos = pos
@@ -483,29 +478,29 @@ class Parser(object):
             error(_(u'Index out of bounds.'))
 
     def SetOperator(self, op_name, *args, **kwdargs):
-        type_ = getType(op_name, self)
+        type_ = self.getType(op_name)
         if type_ not in [NAME,OPERATOR,UNKNOWN]:
             err_cant_set(u'operator', op_name,  type_)
         self.operators[op_name] = Parser.Operator(op_name, *args, **kwdargs)
         for i in op_name:
             if i not in self.opChars: self.opChars += i
     def SetKeyword(self, keywrd_name, *args, **kwdargs):
-        type_ = getType(keywrd_name, self)
+        type_ = self.getType(keywrd_name)
         if type_ not in [NAME,KEYWORD]:
             err_cant_set(u'keyword', keywrd_name,  type_)
         self.keywords[keywrd_name] = Parser.Keyword(keywrd_name, *args, **kwdargs)
     def SetFunction(self, fun_name, *args, **kwdargs):
-        type_ = getType(fun_name, self)
+        type_ = self.getType(fun_name)
         if type_ not in [NAME,FUNCTION]:
             err_cant_set(u'function', fun_name,  type_)
         self.functions[fun_name] = Parser.Function(fun_name, *args, **kwdargs)
     def SetConstant(self, const_name, value):
-        type_ = getType(const_name, self)
+        type_ = self.getType(const_name)
         if type_ not in [NAME,CONSTANT]:
             err_cant_set(u'constant', const_name,  type_)
         self.constants[const_name] = value
     def SetVariable(self, var_name, value):
-        type_ = getType(var_name, self)
+        type_ = self.getType(var_name)
         if type_ not in [NAME, VARIABLE]:
             err_cant_set(u'variable', var_name,  type_)
         self.variables[var_name] = value
@@ -767,13 +762,13 @@ class Parser(object):
         word = word or self.word
         if word is None: return
         if self.wordStart is None: self.wordStart = self.cCol - 1
-        type_ = type_ or getType(word, self)
+        type_ = type_ or self.getType(word)
 
         # Try to figure out if it's multiple operators bunched together
         rightWord = None
         if type_ == UNKNOWN:
             for idex in range(len(word),0,-1):
-                newType = getType(word[0:idex], self)
+                newType = self.getType(word[0:idex])
                 if newType != UNKNOWN:
                     rightWord = word[idex:]
                     rightWordStart = self.wordStart + idex

Mopy/bash/archives.py
@@ -58,7 +58,7 @@ def compress7z(full_dest, rel_dest, srcDir, progress=None, *,
         *solid.split(), u'-y', u'-r', # quiet, recursive
         *out_args, u'-scsUTF-8', u'-sccUTF-8']  # encode output in UTF-8
     if progress is not None: #--Used solely for the progress bar
-        length = sum(map(len, (files for x, y, files in os.walk(srcDir.s))))
+        length = sum(map(len, (files for x, y, files in os.walk(srcDir))))
         progress(0, f'{rel_dest}\n' + _(u'Compressing files...'))
         progress.setFull(1 + length)
     #--Pack the files

Mopy/bash/balt.py
@@ -2087,15 +2087,15 @@ class ListBoxes(WrappingTextMixin, DialogWindow):
         for item_group in lists:
             title = item_group[0] # also serves as key in self._ctrls dict
             item_tip = item_group[1]
-            strings = [u'%s' % x for x in item_group[2:]] # works for Path & strings
-            if not strings: continue
+            if not (strs := [u'%s' % x for x in item_group[2:]]): # Path | str
+                continue
             if liststyle == u'check':
-                checksCtrl = CheckListBox(self, choices=strings, isSingle=True,
+                checksCtrl = CheckListBox(self, choices=strs, isSingle=True,
                                           isHScroll=True)
                 checksCtrl.on_context.subscribe(self._on_context)
                 checksCtrl.set_all_checkmarks(checked=True)
             elif liststyle == u'list':
-                checksCtrl = ListBox(self, choices=strings, isHScroll=True)
+                checksCtrl = ListBox(self, choices=strs, isHScroll=True)
             else: # u'tree'
                 checksCtrl = TreeCtrl(self, title, item_group[2])
             self._ctrls[title] = checksCtrl

Mopy/bash/barb.py
@@ -107,13 +107,15 @@ class BackupSettings(object):
         self.files = {}
         for (bash_dir, tmpdir), setting_files in _init_settings_files(
                 bak_name, mg_name, root_prefix, mods_folder).items():
+            tjoin = GPath(tmpdir).join
             if not setting_files: # we have to backup everything in there
-                setting_files = bash_dir.list()
-            tmp_dir = GPath(tmpdir)
-            for fname in setting_files:
-                fpath = bash_dir.join(fname)
-                if fpath.exists():
-                    self.files[tmp_dir.join(fname)] = fpath
+                self.files.update(
+                    (tjoin(fname), bash_dir.join(fname)) for fname in
+                    bash_dir.list())
+            else:
+                self.files.update(
+                    (tjoin(fname), fpath) for fname in setting_files if
+                    (fpath := bash_dir.join(fname)).exists())
         # backup save profile settings
         rel_save_dir = GPath(u'My Games').join(mg_name)
         save_dirs = ['', *initialization.getLocalSaveDirs()]
@@ -359,7 +361,7 @@ class RestoreSettings(object):
             GPath(self._timestamped_old).copyTo(u'bash.ini')
         elif self._bash_ini_path:
             # remove bash.ini as it is the one from the backup
-            bolt.GPath(u'bash.ini').remove()
+            GPath(u'bash.ini').remove()
 
     @staticmethod
     def warn_message(balt_, msg=u''):

Mopy/bash/basher/__init__.py
@@ -1308,10 +1308,10 @@ class ModList(_ModsUIList):
                                  canCancel=False)
 
     def jump_to_mods_installer(self, modName):
-        installer = self.get_installer(modName)
-        if installer is None:
+        fn_inst = self.get_installer(modName)
+        if fn_inst is None:
             return False
-        balt.Link.Frame.notebook.SelectPage(u'Installers', installer)
+        balt.Link.Frame.notebook.SelectPage(u'Installers', fn_inst)
         return True
 
     def get_installer(self, modName):
@@ -2584,7 +2584,7 @@ class InstallersList(balt.UIList):
                 if outDir.exists():
                     if balt.askYes(progress.dialog, _(
                         u"The project '%s' already exists.  Overwrite "
-                        u"with '%s'?") % (omod.body, om_name)):
+                        u"with '%s'?") % (omod.sbody, om_name)):
                         env.shellDelete(outDir, parent=self,
                                         recycle=True)  # recycle
                     else: continue

Mopy/bash/basher/mod_links.py
@@ -26,7 +26,6 @@ points to BashFrame.modList singleton."""
 
 import copy
 import io
-import re
 import traceback
 from collections import defaultdict, OrderedDict
 from itertools import chain
@@ -40,7 +39,6 @@ from .. import bass, bosh, bolt, balt, bush, load_order
 from ..balt import ItemLink, Link, CheckLink, EnabledLink, AppendableLink, \
     TransLink, SeparatorLink, ChoiceLink, OneItemLink, ListBoxes, MenuLink
 from ..bolt import GPath, SubProgress, dict_sort, sig_to_str
-from ..bosh import faces
 from ..brec import MreRecord
 from ..exception import AbstractError, BoltError, CancelError
 from ..gui import CancelButton, CheckBox, HLayout, Label, LayoutOptions, \
@@ -84,8 +82,8 @@ class _LoadLink(ItemLink):
     def _load_fact(self, keepAll=True):
         return LoadFactory(keepAll, by_sig=self._load_sigs)
 
-    def _load_mod(self, mod_info, keepAll=True, **kwargs):
-        loadFactory = self._load_fact(keepAll=keepAll)
+    def _load_mod(self, mod_info, keepAll=True, load_fact=None, **kwargs):
+        loadFactory = load_fact or self._load_fact(keepAll=keepAll)
         modFile = ModFile(mod_info, loadFactory)
         modFile.load(True, **kwargs)
         return modFile
@@ -97,8 +95,8 @@ class Mod_FullLoad(OneItemLink, _LoadLink):
     _load_sigs = tuple(MreRecord.type_class) # all available (decoded) records
 
     def Execute(self):
-        with balt.Progress(_(u'Loading:') + u'\n%s'
-                % self._selected_item.stail) as progress:
+        msg = _(u'Loading:') + f'\n{self._selected_item}'
+        with balt.Progress(msg) as progress:
             bolt.deprint(MreRecord.type_class)
             try:
                 self._load_mod(self._selected_info, keepAll=False,
@@ -529,9 +527,8 @@ class _Mod_Groups_Import(ItemLink):
         textPath = self._askOpen(_(u'Import names from:'), textDir, u'',
                                  u'*_Groups.csv')
         if not textPath: return
-        (textDir,textName) = textPath.headTail
         #--Extension error check
-        if textName.cext != u'.csv':
+        if textPath.cext != u'.csv':
             self._showError(_(u'Source file must be a csv file.'))
             return
         #--Import
@@ -1454,9 +1451,8 @@ class Mod_DecompileAll(_NotObLink, _LoadLink):
                 if scpt_grp.getNumRecords(includeGroups=False):
                     master_factory = self._load_fact(keepAll=False)
                     for master in modFile.tes4.masters:
-                        masterFile = ModFile(bosh.modInfos[master],
-                                             master_factory)
-                        masterFile.load(True)
+                        masterFile = self._load_mod(bosh.modInfos[master],
+                                                    load_fact=master_factory)
                         for rfid, r in masterFile.tops[b'SCPT'].iter_present_records():
                             id_text[rfid] = r.script_source
                     newRecords = []
@@ -1599,14 +1595,12 @@ class Mod_FlipMasters(OneItemLink, _Esm_Esl_Flip):
     @property
     def _already_flagged(self): return not self.toEsm
 
-    def _initData(self, window, selection,
-            __reEspExt=re.compile(r'\.esp(.ghost)?$', re.I | re.U)):
+    def _initData(self, window, selection):
         present_mods = window.data_store
         modinfo_masters = present_mods[selection[0]].masterNames
         if len(selection) == 1 and len(modinfo_masters) > 1:
-            self.espMasters = [master for master in modinfo_masters if
-                               master in present_mods and __reEspExt.search(
-                                   master.s)]
+            self.espMasters = [m for m in modinfo_masters if
+                               m in present_mods and m.cext == '.esp']
             self._do_enable = bool(self.espMasters)
         else:
             self.espMasters = []
@@ -1686,15 +1680,14 @@ class Mod_Fids_Replace(OneItemLink):
         textPath = self._askOpen(_(u'Form ID mapper file:'), textDir, u'',
                                  u'*_Formids.csv')
         if not textPath: return
-        (textDir,textName) = textPath.headTail
         #--Extension error check
-        if textName.cext != u'.csv':
+        if textPath.cext != u'.csv':
             self._showError(_(u'Source file must be a csv file.'))
             return
         #--Export
         with balt.Progress(_(u'Import Form IDs')) as progress:
             replacer = self._parser()
-            progress(0.1,_(u'Reading') + u' %s.' % textName)
+            progress(0.1, _(u'Reading') + f' {textPath.stail}.')
             replacer.read_csv(textPath)
             progress(0.2, _(u'Applying to') + u' %s.' % self._selected_item)
             changed = replacer.updateMod(self._selected_info)
@@ -1760,7 +1753,6 @@ class _Mod_Export_Link(_Import_Export_Link, _CsvExport_Link):
         textName = self.selected[0].root + self.__class__.csvFile
         textPath = self._csv_out(textName)
         if not textPath: return
-        (textDir, textName) = textPath.headTail
         #--Export
         with balt.Progress(self.__class__.progressTitle) as progress:
             parser = self._parser()
@@ -1769,7 +1761,7 @@ class _Mod_Export_Link(_Import_Export_Link, _CsvExport_Link):
             for index,(fileName,fileInfo) in enumerate(self.iselected_pairs()):
                 readProgress(index, _(u'Reading') + u' %s.' % fileName)
                 parser.readFromMod(fileInfo)
-            progress(0.8, _(u'Exporting to') + u' %s.' % textName)
+            progress(0.8, _(u'Exporting to') + f' {textPath.stail}.')
             parser.write_text_file(textPath)
             progress(1.0, _(u'Done.'))
 
@@ -1789,14 +1781,22 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
         return _(u'Mod/Text File') + u'|*' + self.__class__.csvFile + u';*' \
                + espml + u';*.ghost'
 
-    def _import(self, ext, textDir, textName, textPath):
+    def _import_from(self):
+        textName = self._selected_item.root + self.__class__.csvFile
+        textDir = bass.dirs[u'patches']
+        #--File dialog
+        textPath = self._askOpen(self.__class__.askTitle, textDir, textName,
+                                 self._wildcard)
+        return textPath
+
+    def _import(self, ext, textPath):
         with balt.Progress(self.__class__.progressTitle) as progress:
             parser = self._parser()
-            progress(0.1, _(u'Reading') + u' %s.' % textName)
+            progress(0.1, _(u'Reading') + f' {textPath.stail}.')
             if ext == u'.csv':
                 parser.read_csv(textPath)
             else:
-                srcInfo = bosh.ModInfo(GPath(textDir).join(textName))
+                srcInfo = bosh.ModInfo(textPath)
                 parser.readFromMod(srcInfo)
             progress(0.2, _(u'Applying to') + f' {self._selected_item}.')
             changed = parser.writeToMod(self._selected_info)
@@ -1822,15 +1822,10 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
         if not self._askContinueImport(): return
         supportedExts = self.__class__.supportedExts
         csv_filename = self.__class__.csvFile
-        textName = self._selected_item.root + csv_filename
-        textDir = bass.dirs[u'patches']
-        #--File dialog
-        textPath = self._askOpen(self.__class__.askTitle, textDir, textName,
-                                 self._wildcard)
+        textPath = self._import_from()
         if not textPath: return
-        (textDir, textName) = textPath.headTail
         #--Extension error check
-        ext = textName.cext
+        ext = textPath.cext
         if ext not in supportedExts:
             plugin_exts = u'or '.join(sorted(bush.game.espm_extensions
                                              | {u'.ghost'}))
@@ -1842,7 +1837,7 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
             self._showError(csv_err)
             return
         #--Import
-        changed = self._import(ext, textDir, textName, textPath)
+        changed = self._import(ext, textPath)
         #--Log
         self.show_change_log(changed, self._selected_item)
 
@@ -1865,8 +1860,10 @@ class Mod_ActorLevels_Export(_Mod_Export_Link):
         return ActorLevels()
 
     def Execute(self): # overrides _Mod_Export_Link
-        message = (_(u'This command will export the level info for NPCs whose level is offset with respect to the PC.  The exported file can be edited with most spreadsheet programs and then reimported.')
-                   + u'\n\n' +
+        message = (_(
+            'This command will export the level info for NPCs whose level is '
+            'offset with respect to the PC.  The exported file can be edited '
+            'with most spreadsheet programs and then reimported.') + u'\n\n' +
                    _(u'See the Bash help file for more info.'))
         if not self._askContinue(message, u'bash.actorLevels.export.continue',
                                  _(u'Export NPC Levels')): return
@@ -2288,15 +2285,10 @@ class Mod_EditorIds_Import(_Mod_Import_Link):
 
     def Execute(self):
         if not self._askContinueImport(): return
-        textName = self._selected_item.root + self.__class__.csvFile
-        textDir = bass.dirs[u'patches']
-        #--File dialog
-        textPath = self._askOpen(self.__class__.askTitle, textDir,
-                                 textName, self._wildcard)
+        textPath = self._import_from()
         if not textPath: return
-        (textDir,textName) = textPath.headTail
         #--Extension error check
-        if textName.cext != u'.csv':
+        if textPath.cext != u'.csv':
             self._showError(_(u'Source file must be a csv file.'))
             return
         #--Import
@@ -2305,7 +2297,7 @@ class Mod_EditorIds_Import(_Mod_Import_Link):
         try:
             with balt.Progress(self.__class__.progressTitle) as progress:
                 editorIds = self._parser(questionableEidsSet, badEidsList)
-                progress(0.1, _(u'Reading') + u' %s.' % textName)
+                progress(0.1, _(u'Reading') + f' {textPath.stail}.')
                 editorIds.read_csv(textPath)
                 progress(0.2, _(u'Applying to %s.') % self._selected_item)
                 changed = editorIds.writeToMod(self._selected_info)

Mopy/bash/basher/settings_dialog.py
@@ -29,7 +29,7 @@ from . import BashStatusBar, tabInfo
 from .constants import colorInfo, settingDefaults
 from .. import balt, barb, bass, bolt, bosh, bush, env, exception
 from ..balt import colors, Link, Resources, showOk
-from ..bolt import deprint, GPath, readme_url, os_name
+from ..bolt import deprint, readme_url, os_name
 from ..gui import ApplyButton, BusyCursor, Button, CancelButton, Color, \
     ColorPicker, DialogWindow, DropDown, HLayout, HorizontalLine, \
     LayoutOptions, OkButton, PanelWin, Stretch, TextArea, TreePanel, VLayout, \
@@ -574,15 +574,14 @@ class LanguagePage(_AScrollablePage):
 
     def _edit_l10n(self):
         """Opens the selected localization file in an editor."""
-        chosen_editor = GPath(bass.settings[u'bash.l10n.editor.path'])
-        editor_arg_fmt = bass.settings[u'bash.l10n.editor.param_fmt']
+        chosen_editor = bass.settings[u'bash.l10n.editor.path']
         # First, verify that the chosen editor is valid
         if not chosen_editor:
             balt.showError(self, _(u'No localization editor has been chosen. '
                                    u"Please click on 'Configure Editor' to "
                                    u'set one up.'), title=_(u'Invalid Editor'))
             return
-        elif not chosen_editor.is_file():
+        elif not os.path.isfile(chosen_editor):
             balt.showError(self, _(u'The chosen editor (%s) does not exist or '
                                    u'is not a file.') % chosen_editor,
                 title=_(u'Invalid Editor'))
@@ -590,6 +589,7 @@ class LanguagePage(_AScrollablePage):
         # Now we can move on to actually opening the editor
         selected_l10n = bass.dirs[u'l10n'].join(self._chosen_l10n)
         # Construct the final command and pass it to subprocess
+        editor_arg_fmt = bass.settings[u'bash.l10n.editor.param_fmt']
         subprocess.Popen(
             [chosen_editor, *((a % selected_l10n if '%s' in a else a)
             for a in editor_arg_fmt.split(u' '))], close_fds=True)

commit bf5f2ecfa2c1f8e75cbc3df45a567f9b1415cc69

Fix regression in BAIN wizards
A token may be created without a type or parser, at which point we'd
fail with an AttributeError:

Traceback (most recent call last):
  File "bash\belt.py", line 744, in Continue
  File "bash\ScriptParser.py", line 541, in RunLine
  File "bash\ScriptParser.py", line 405, in __call__
  File "bash\ScriptParser.py", line 297, in __call__
  File "bash\ScriptParser.py", line 607, in ExecuteTokens
  File "bash\ScriptParser.py", line 744, in ExecuteRPN
  File "bash\ScriptParser.py", line 333, in __init__
AttributeError: 'NoneType' object has no attribute 'getType'

Broke in c55b60dfee2cb4337ddbcb056075d0cb76ee818b.

Mopy/bash/ScriptParser.py
@@ -196,6 +196,13 @@ def error(msg):
 # Parser ------------------------------------------
 #  This is where the magic happens
 #--------------------------------------------------
+def _get_type_basic(token_or_num):
+    """Determines a token's type without considering a parser's type system."""
+    if isinstance(token_or_num, str): return STRING
+    if isinstance(token_or_num, int): return INTEGER
+    if isinstance(token_or_num, float): return DECIMAL
+    return UNKNOWN
+
 class Parser(object):
     class ParserType(object):
         @property
@@ -222,9 +229,7 @@ class Parser(object):
             for i in token_or_num:
                 if i not in whitespace: return UNKNOWN
             return WHITESPACE
-        if isinstance(token_or_num, int): return INTEGER
-        if isinstance(token_or_num, float): return DECIMAL
-        return UNKNOWN
+        return _get_type_basic(token_or_num)
 
     class Callable(ParserType):
         def __init__(self, callable_name, function, min_args=0,
@@ -330,7 +335,16 @@ class Parser(object):
                 self.numArgs = token_or_text.numArgs
             else:
                 self.text = token_or_text
-                self.type = Type or parser.getType(token_or_text)
+                if Type:
+                    # We were passed a type, so use that
+                    self.type = Type
+                elif parser:
+                    # We have a parser, so we can query for reliable type info
+                    self.type = parser.getType(token_or_text)
+                else:
+                    # We do not have a parser, so we can only query for basic
+                    # type info
+                    self.type = _get_type_basic(token_or_text)
                 self.parser = parser
                 self.line = line
                 self.pos = pos