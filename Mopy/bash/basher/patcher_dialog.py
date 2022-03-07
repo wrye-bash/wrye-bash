wrye-bash/wrye-bash

commit 3924576bef3aa9e875bfd5fda204fdaf42fce396

Unwrap pickle dicts:
DataTable must go - profiles are mostly encapsulated in saveInfos, still
it is not clear how they are related to initialization.getLocalSaveDirs.
Should we clean up and maybe avoid the setdefault by making sure we have
all the keys and only those? Anyway renames with the same number of chars
is what we do best. Note patcher.exportConfig used to try and load the
file.

BP config needs some dusting - no use nesting the keys anymore and also
configItems/Choices/Checks etc come from a time when dicts were not
ordered - needs backwards compatibility and some thought so it's (#)312+
barb: nit - profiles renames and chop off one exists() occurrence
Use map - cleaner - note I was getting desperate_ with iteration
variables.

Under #543

Mopy/bash/basher/patcher_dialog.py
@@ -29,7 +29,7 @@ from datetime import timedelta
 from . import BashFrame  ##: drop this - decouple !
 from .. import balt, bass, bolt, bosh, bush, env, load_order
 from ..balt import Link, Resources
-from ..bolt import SubProgress, GPath, Path
+from ..bolt import SubProgress, GPath, Path, GPath_no_norm
 from ..exception import BoltError, CancelError, FileEditError, \
     PluginsFullError, SkipError, BPConfigError
 from ..gui import CancelButton, DeselectAllButton, HLayout, Label, \
@@ -303,7 +303,7 @@ class PatchDialog(DialogWindow):
         exportConfig(patch_name=self.patchInfo.ci_key, config=config,
                      win=self.parent, outDir=bass.dirs[u'patches'])
 
-    __old_key = GPath(u'Saved Bashed Patch Configuration')
+    __old_key = u'Saved Bashed Patch Configuration'
     __new_key = u'Saved Bashed Patch Configuration (%s)'
     def ImportConfig(self):
         """Import the configuration from a user selected dat file."""
@@ -315,25 +315,20 @@ class PatchDialog(DialogWindow):
             u'Import Bashed Patch configuration from:'), textDir, config_dat,
             u'*.dat')
         if not textPath: return
-        table_get = bolt.DataTable(bolt.PickleDict(textPath)).getItem
+        pickle_dict = bolt.PickleDict(textPath, load_pickle=True).pickled_data
+        table_get = lambda x: (conf := pickle_dict.get(GPath_no_norm(x))) and (
+            conf.get('bash.patch.configs'), {})
         # try the current Bashed Patch mode.
-        config_key = u'bash.patch.configs'
-        patchConfigs = table_get(GPath(self.__new_key % u'Python'), config_key,
-                                 {})
-        convert = False
+        patchConfigs = table_get(self.__new_key % 'Python')
         if not patchConfigs: # try the non-current Bashed Patch mode
-            patchConfigs = table_get(GPath(self.__new_key % u'CBash'),
-                                     config_key, {})
-            convert = bool(patchConfigs)
-        if not patchConfigs: # try the old format
-            patchConfigs = table_get(self.__old_key, config_key, {})
-            convert = bool(patchConfigs)
-        if not patchConfigs:
-            balt.showWarning(self,
-                _(u'No patch config data found in %s') % textPath,
-                title=_(u'Import Config'))
-            return
-        if convert:
+            patchConfigs = table_get(self.__new_key % 'CBash')
+            if not patchConfigs: # try the old format
+                patchConfigs = table_get(self.__old_key)
+            if not patchConfigs:
+                balt.showWarning(self,
+                    _(u'No patch config data found in %s') % textPath,
+                    title=_(u'Import Config'))
+                return
             balt.showError(self,
                 _(u'The patch config data in %s is too old for this version '
                   u'of Wrye Bash to handle or was created with CBash. Please '

Mopy/bash/barb.py
@@ -115,19 +115,15 @@ class BackupSettings(object):
                 if fpath.exists():
                     self.files[tmp_dir.join(fname)] = fpath
         # backup save profile settings
-        savedir = GPath(u'My Games').join(mg_name)
-        profiles = ['', *initialization.getLocalSaveDirs()]
-        for profile in profiles:
-            pluginsTxt = (u'Saves', profile, u'plugins.txt')
-            loadorderTxt = (u'Saves', profile, u'loadorder.txt')
-            for txt in (pluginsTxt, loadorderTxt):
-                tpath = savedir.join(*txt)
-                fpath = dirs[u'saveBase'].join(*txt)
+        rel_save_dir = GPath(u'My Games').join(mg_name)
+        save_dirs = ['', *initialization.getLocalSaveDirs()]
+        for save_dir in save_dirs:
+            for txt in (['plugins.txt'], ['loadorder.txt'],
+                        ['Bash', 'Table.dat']):
+                tpath = rel_save_dir.join('Saves', save_dir, *txt)
+                fpath = dirs[u'saveBase'].join('Saves', save_dir, *txt)
                 if fpath.exists(): self.files[tpath] = fpath
-            prof_table = (u'Saves', profile, u'Bash', u'Table.dat')
-            tpath = savedir.join(*prof_table)
-            fpath = dirs[u'saveBase'].join(*prof_table)
-            if fpath.exists(): self.files[tpath] = fpath
+            # for 'Table.dat' check also the bak file
             if fpath.backup.exists(): self.files[tpath.backup] = fpath.backup
 
     @staticmethod

Mopy/bash/basher/__init__.py
@@ -3959,7 +3959,7 @@ class BashFrame(WindowFrame):
         else:
             title += _(u'Default')
         if bosh.modInfos.voCurrent:
-            title += u' ['+bosh.modInfos.voCurrent+u']'
+            title += f' [{bosh.modInfos.voCurrent}]'
         self._native_widget.SetTitle(title)
 
     def set_status_count(self, requestingPanel, countTxt):

Mopy/bash/basher/mods_links.py
@@ -214,7 +214,8 @@ class Mods_OblivionVersion(CheckLink, EnabledLink):
         bosh.modInfos.setOblivionVersion(self._version_key)
         self.window.RefreshUI(refreshSaves=True) # True: refresh save's masters
         if self.setProfile:
-            bosh.saveInfos.profiles.setItem(bosh.saveInfos.localSave,u'vOblivion', self._version_key)
+            bosh.saveInfos.set_profile_attr(bosh.saveInfos.localSave,
+                                            'vOblivion', self._version_key)
         Link.Frame.set_bash_frame_title()
 
 # "File" submenu --------------------------------------------------------------

Mopy/bash/basher/saves_links.py
@@ -81,12 +81,12 @@ class Saves_ProfilesData(balt.ListEditorData):
     def getInfo(self,item):
         """Returns string info on specified item."""
         profileSaves = _win_join(item)
-        return bosh.saveInfos.profiles.getItem(profileSaves, u'info',
+        return bosh.saveInfos.get_profile_attr(profileSaves, u'info',
                                                _(u'About %s:') % item)
     def setInfo(self, item, info_text):
         """Sets string info on specified item."""
         profileSaves = _win_join(item)
-        bosh.saveInfos.profiles.setItem(profileSaves, u'info', info_text)
+        bosh.saveInfos.set_profile_attr(profileSaves, u'info', info_text)
 
     def add(self):
         """Adds a new profile."""
@@ -109,7 +109,8 @@ class Saves_ProfilesData(balt.ListEditorData):
             return False
         self.baseSaves.join(newName).makedirs()
         newSaves = _win_join(newName)
-        bosh.saveInfos.profiles.setItem(newSaves,u'vOblivion',bosh.modInfos.voCurrent)
+        bosh.saveInfos.set_profile_attr(newSaves, 'vOblivion',
+                                        bosh.modInfos.voCurrent)
         return newName
 
     def rename(self,oldName,newName):
@@ -120,22 +121,21 @@ class Saves_ProfilesData(balt.ListEditorData):
         if newName.lower() in lowerNames:
             balt.showError(self,_(u'Name must be unique.'))
             return False
-        if len(newName) == 0 or len(newName) > 64:
+        if not (1 <= len(newName) <= 64):
             balt.showError(self.parent,
                 _(u'Name must be between 1 and 64 characters long.'))
             return False
         #--Rename
-        oldDir, newDir = (self.baseSaves.join(subdir) for subdir in
-                          (oldName, newName))
+        oldDir, newDir = map(self.baseSaves.join, (oldName, newName))
         oldDir.moveTo(newDir)
-        oldSaves, newSaves = (_win_join(name_) for name_ in (oldName, newName))
+        oldSaves, newSaves = map(_win_join, (oldName, newName))
         if bosh.saveInfos.localSave == oldSaves:
             Link.Frame.saveList.set_local_save(newSaves)
-        bosh.saveInfos.profiles.moveRow(oldSaves,newSaves)
+        bosh.saveInfos.rename_profile(oldSaves, newSaves)
         return newName
 
     def remove(self,profile):
-        """Removes load list."""
+        """Remove save profile."""
         profileSaves = _win_join(profile)
         #--Can't remove active or Default directory.
         if bosh.saveInfos.localSave == profileSaves:
@@ -154,7 +154,7 @@ class Saves_ProfilesData(balt.ListEditorData):
             raise BoltError(f'Sanity check failed: No '
                 f'"{bush.game.my_games_name}\\Saves" in {profileDir}.')
         shutil.rmtree(profileDir.s) #--DO NOT SCREW THIS UP!!!
-        bosh.saveInfos.profiles.delRow(profileSaves)
+        bosh.saveInfos.rename_profile(profileSaves, None)
         return True
 
 #------------------------------------------------------------------------------

Mopy/bash/bolt.py
@@ -1399,13 +1399,14 @@ def mainfunc(func):
 class PickleDict(object):
     """Dictionary saved in a pickle file.
     Note: self.vdata and self.data are not reassigned! (Useful for some clients.)"""
-    def __init__(self, pkl_path, readOnly=False):
+    def __init__(self, pkl_path, readOnly=False, load_pickle=False):
         """Initialize."""
         self._pkl_path = pkl_path
         self.backup = pkl_path.backup
         self.readOnly = readOnly
         self.vdata = {}
         self.pickled_data = {}
+        if load_pickle: self.load()
 
     def exists(self):
         return self._pkl_path.exists() or self.backup.exists()

Mopy/bash/bosh/__init__.py
@@ -3190,13 +3190,12 @@ class ModInfos(FileInfos):
     # does not really belong here, but then where ?
         """Save current plugins into arcSaves directory, load plugins from
         newSaves directory and set oblivion version."""
-        arcPath, newPath = (dirs[u'saveBase'].join(saves) for saves in
-                            (arcSaves, newSaves))
+        arcPath, newPath = map(dirs[u'saveBase'].join, (arcSaves, newSaves))
         load_order.swap(arcPath, newPath)
         # Swap Oblivion version to memorized version
-        voNew = saveInfos.profiles.getItem(newSaves, u'vOblivion', None)
+        voNew = saveInfos.get_profile_attr(newSaves, u'vOblivion', None)
         if voNew is None:
-            saveInfos.profiles.setItem(newSaves, u'vOblivion', self.voCurrent)
+            saveInfos.set_profile_attr(newSaves, u'vOblivion', self.voCurrent)
             voNew = self.voCurrent
         if voNew in self.voAvailable: self.setOblivionVersion(voNew)
 
@@ -3267,15 +3266,29 @@ class SaveInfos(FileInfos):
         super(SaveInfos, self).__init__(dirs[u'saveBase'].join(self.localSave),
                                         factory=SaveInfo)
         # Save Profiles database
-        self.profiles = bolt.DataTable(bolt.PickleDict(
-            dirs[u'saveBase'].join(u'BashProfiles.dat')))
+        self.profiles = bolt.PickleDict(
+            dirs[u'saveBase'].join(u'BashProfiles.dat'), load_pickle=True)
         # save profiles used to have a trailing slash, remove it if present
-        for row in list(self.profiles):
-            if row.endswith(u'\\'):
-                self.profiles.moveRow(row, row[:-1])
+        for row in [r for r in self.profiles.pickled_data if r.endswith('\\')]:
+            self.rename_profile(row, row[:-1])
         SaveInfo.cosave_types = cosaves.get_cosave_types(
             bush.game.fsName, self._parse_save_path,
             bush.game.Se.cosave_tag, bush.game.Se.cosave_ext)
+
+    def get_profile_attr(self, prof_key, attr_key, default_val):
+        return self.profiles.pickled_data.get(prof_key, {}).get(attr_key,
+                                                                default_val)
+
+    def set_profile_attr(self, prof_key, attr_key, val):
+        self.profiles.pickled_data.setdefault(prof_key, {})[attr_key] = val
+
+    def rename_profile(self, oldName, newName):
+        """Rename save profile - if newName is None just delete the row."""
+        pd = self.profiles.pickled_data
+        if oldName in pd:
+            if newName is not None:
+                pd[newName] = pd[oldName]
+            del pd[oldName]
 
     @classmethod
     def rightFileType(cls, fileName):

Mopy/bash/patcher/__init__.py
@@ -31,10 +31,10 @@ def exportConfig(patch_name, config, win, outDir):
         defaultDir=outDir, defaultFile=outFile,
         wildcard=u'*_Configuration.dat')
     if outPath:
-        table = bolt.DataTable(bolt.PickleDict(outPath))
-        table.setItem(bolt.GPath(u'Saved Bashed Patch Configuration (Python)'),
-            u'bash.patch.configs', config)
-        table.save()
+        pd = bolt.PickleDict(outPath)
+        gkey = bolt.GPath_no_norm('Saved Bashed Patch Configuration (Python)')
+        pd.pickled_data[gkey] = {'bash.patch.configs': config}
+        pd.save()
 
 def getPatchesPath(fileName):
     """Choose the correct Bash Patches path for the file."""

commit b3f99d8e3409d77b4d2ca93136cd2433fd78cf4f

Regression in Import BP Config:
Introduced in 3924576bef3aa9e875bfd5fda204fdaf42fce396

Took the opportunity to use int_unpacker more and further clarify those
cached method params by making them kwargs only.
Mopy/bash/game/skyrim/records.py: inline not needed
Some fdust from parsers.

Mopy/bash/basher/patcher_dialog.py
@@ -313,7 +313,7 @@ class PatchDialog(DialogWindow):
         if not textPath: return
         pickle_dict = bolt.PickleDict(textPath, load_pickle=True).pickled_data
         table_get = lambda x: (conf := pickle_dict.get(GPath_no_norm(x))) and (
-            conf.get('bash.patch.configs'), {})
+            conf.get('bash.patch.configs', {}))
         # try the current Bashed Patch mode.
         patchConfigs = table_get(self.__new_key % 'Python')
         if not patchConfigs: # try the non-current Bashed Patch mode

Mopy/bash/brec/basic_elements.py
@@ -25,7 +25,7 @@ higher-level building blocks can be found in common_subrecords.py."""
 from typing import BinaryIO
 
 from .utils_constants import FID, null1, _make_hashable, FixedString, \
-    _int_unpacker, get_structs
+    int_unpacker, get_structs
 from .. import bolt, exception
 from ..bolt import decoder, encode, structs_cache, struct_calcsize, Rounder
 
@@ -98,7 +98,7 @@ class Subrecord(object):
         outWrite(binary_data)
 
 def unpackSubHeader(ins, rsig=b'----', *,
-                    __unpacker=_int_unpacker, __sr=Subrecord):
+                    __unpacker=int_unpacker, __sr=Subrecord):
     """Unpack a subrecord header. Optionally checks for match with expected
     type and size."""
     mel_sig, mel_size = ins.unpack(__sr.sub_header_unpack,

Mopy/bash/brec/common_subrecords.py
@@ -31,7 +31,7 @@ from .basic_elements import MelBase, MelFid, MelGroup, MelGroups, MelLString, \
     MelNull, MelSequential, MelString, MelStruct, MelUInt32, MelOptStruct, \
     MelFloat, MelReadOnly, MelFids, MelUInt32Flags, MelUInt8Flags, MelSInt32, \
     MelStrings, MelUInt8, MelFidList
-from .utils_constants import _int_unpacker, FID, null1
+from .utils_constants import int_unpacker, FID, null1
 from ..bolt import Flags, encode, struct_pack, struct_unpack, unpack_byte, \
     dict_sort, TrimmedFlags
 from ..exception import ModError, ModSizeError
@@ -503,8 +503,8 @@ class MelRaceParts(MelNull):
         for element in self._indx_to_loader.values():
             element.setDefault(record)
 
-    def load_mel(self, record, ins, sub_type, size_, *debug_strs):
-        __unpacker=_int_unpacker # PY3: keyword only search for __unpacker
+    def load_mel(self, record, ins, sub_type, size_, *debug_strs,
+                 __unpacker=int_unpacker):
         if sub_type == b'INDX':
             self._last_indx = ins.unpack(__unpacker, size_, *debug_strs)[0]
         else:
@@ -591,7 +591,7 @@ class MelMODS(MelBase):
         setattr(record, self.attr, None)
 
     def load_mel(self, record, ins, sub_type, size_, *debug_strs):
-        __unpacker=_int_unpacker
+        __unpacker=int_unpacker
         insUnpack = ins.unpack
         insRead32 = ins.readString32
         count, = insUnpack(__unpacker, 4, *debug_strs)

Mopy/bash/brec/mod_io.py
@@ -26,7 +26,7 @@ import os
 from io import BytesIO
 
 # no local imports beyond this, imported everywhere in brec
-from .utils_constants import _int_unpacker, group_types, null1, strFid
+from .utils_constants import int_unpacker, group_types, null1, strFid
 from .. import bolt
 from ..bolt import decoder, struct_pack, struct_unpack, structs_cache, \
     sig_to_str
@@ -274,11 +274,10 @@ class ModReader(object):
             raise ModSizeError(self.inName, debug_strs, (target_size,), size)
         return self.ins.read(size)
 
-    def readLString(self, size, *debug_strs):
+    def readLString(self, size, *debug_strs, __unpacker=int_unpacker):
         """Read translatable string. If the mod has STRINGS files, this is a
         uint32 to lookup the string in the string table. Otherwise, this is a
         zero-terminated string."""
-        __unpacker = _int_unpacker
         if self.hasStrings:
             if size != 4:
                 endPos = self.ins.tell() + size
@@ -289,9 +288,8 @@ class ModReader(object):
         else:
             return self.readString(size, *debug_strs)
 
-    def readString32(self, *debug_str):
+    def readString32(self, *debug_str, __unpacker=int_unpacker):
         """Read wide pascal string: uint32 is used to indicate length."""
-        __unpacker = _int_unpacker
         strLen, = self.unpack(__unpacker, 4, debug_str)
         return self.readString(strLen, *debug_str)
 
@@ -313,7 +311,7 @@ class ModReader(object):
             raise ModReadError(self.inName, debug_strs, endPos, self.size)
         return struct_unpacker(self.ins.read(size))
 
-    def unpackRef(self, __unpacker=_int_unpacker):
+    def unpackRef(self, *, __unpacker=int_unpacker):
         """Read a ref (fid)."""
         return self.unpack(__unpacker, 4)[0]
 

Mopy/bash/brec/record_structs.py
@@ -30,13 +30,13 @@ import zlib
 
 from .basic_elements import SubrecordBlob, unpackSubHeader
 from .mod_io import ModReader
-from .utils_constants import strFid, _int_unpacker
+from .utils_constants import strFid, int_unpacker
 from .. import bolt, exception
 from ..bolt import decoder, struct_pack, sig_to_str
 from ..bolt import float_or_none, int_or_zero, str_or_none
 
 def _str_to_bool(value, __falsy=frozenset(
-    [u'', u'none', u'false', u'no', u'0', u'0.0'])):
+    ['', 'none', 'false', 'no', '0', '0.0'])):
     return value.strip().lower() not in __falsy
 
 # Cross game dict, mapping attribute -> csv (de)serializer/csv column header
@@ -130,12 +130,12 @@ attr_csv_struct = {
 
 for _k, _v in attr_csv_struct.items():
     if _v[0] is int_or_zero: # should also cover Flags
-        _v.append(lambda x: u'"%d"' % x)
+        _v.append(lambda x: f'"{x:d}"')
     else: # also covers floats which should be wrapped in Rounder (see __str__)
-        _v.append(lambda x: u'"%s"' % x)
+        _v.append(lambda x: f'"{x}"')
 del _k, _v
 attr_csv_struct[u'enchantPoints'][2] = lambda x: ( # can be None
-            u'"%d"' % x) if x is not None else u'"None"'
+    f'"{x:d}"' if x is not None else '"None"')
 
 #------------------------------------------------------------------------------
 # Mod Element Sets ------------------------------------------------------------
@@ -154,9 +154,8 @@ class MelSet(object):
             element.hasFids(self.formElements)
         for sig_candidate in self.loaders:
             if len(sig_candidate) != 4 or not isinstance(sig_candidate, bytes):
-                raise SyntaxError(u"Invalid signature '%s': Signatures must "
-                                  u'be bytestrings and 4 bytes in '
-                                  u'length.' % sig_candidate)
+                raise SyntaxError(f"Invalid signature '{sig_candidate}': "
+                    f"Signatures must be bytestrings and 4 bytes in length.")
 
     def getSlotsUsed(self):
         """This function returns all of the attributes used in record instances
@@ -406,7 +405,7 @@ class MreRecord(object):
         removed from the list."""
         pass
 
-    def getDecompressed(self, __unpacker=_int_unpacker):
+    def getDecompressed(self, *, __unpacker=int_unpacker):
         """Return self.data, first decompressing it if necessary."""
         if not self.flags1.compressed: return self.data
         decompressed_size, = __unpacker(self.data[:4])

Mopy/bash/brec/utils_constants.py
@@ -28,7 +28,7 @@ from ..bolt import cstrip, decoder, Flags, structs_cache, attrgetter_cache
 # no local imports, imported everywhere in brec
 
 # Random stuff ----------------------------------------------------------------
-_int_unpacker = structs_cache[u'I'].unpack
+int_unpacker = structs_cache['I'].unpack
 
 def _make_hashable(target_obj):
     """Bit of a HACK, but at least it fixes any code that just *assumed* set

Mopy/bash/game/oblivion/records.py
@@ -244,8 +244,7 @@ class MelEffects(MelSequential):
         self._obme_loaders = {}
         self._obme_form_elements = set()
         # Only for setting the possible signatures, redirected in load_mel etc.
-        super(MelEffects, self).__init__(*(self._vanilla_elements +
-                                           self._obme_elements))
+        super().__init__(*self._vanilla_elements, *self._obme_elements)
 
     # Note that we only support creating vanilla effects, as our records system
     # isn't expressive enough to pass more info along here

Mopy/bash/game/skyrim/records.py
@@ -44,7 +44,7 @@ from ...brec import MelRecord, MelObject, MelGroups, MelStruct, FID, \
     MelActorSounds, MelFactionRanks, MelSorted, vmad_properties_key, \
     vmad_qust_fragments_key, vmad_fragments_key, vmad_script_key, \
     vmad_qust_aliases_key, MelReflectedRefractedBy, perk_effect_key, \
-    MelValueWeight
+    MelValueWeight, int_unpacker
 from ...exception import ModError, ModSizeError, StateError
 
 # Set MelModel in brec but only if unset, otherwise we are being imported from
@@ -634,10 +634,9 @@ class ObjectRef(object):
     def array_from_file(cls, ins, obj_format, *debug_strs):
         """Reads an array of ObjectRefs directly from the specified input
         stream. Needs the current object format and a read ID as well."""
-        __unpacker=structs_cache[u'I'].unpack
         make_ref = cls.from_file
         return [make_ref(ins, obj_format, *debug_strs) for _x in
-                range(ins.unpack(__unpacker, 4, *debug_strs)[0])]
+                range(ins.unpack(int_unpacker, 4, *debug_strs)[0])]
 
     @staticmethod
     def dump_array(target_list, __packer=structs_cache[u'I'].pack):

Mopy/bash/mod_files.py
@@ -32,7 +32,7 @@ from .bolt import deprint, SubProgress, structs_cache, struct_error, decoder, \
     sig_to_str, str_to_sig
 from .brec import MreRecord, ModReader, RecordHeader, RecHeader, null1, \
     TopGrupHeader, MobBase, MobDials, MobICells, MobObjects, MobWorlds, \
-    unpack_header, FastModReader, Subrecord
+    unpack_header, FastModReader, Subrecord, int_unpacker
 from .exception import MasterMapError, ModError, StateError, ModReadError
 
 class MasterSet(set):
@@ -386,7 +386,7 @@ class ModFile(object):
         m_names = bush.game.mgef_name.copy()
         hostile_recs = set()
         nonhostile_recs = set()
-        unpack_eid = structs_cache[u'I'].unpack
+        unpack_eid = int_unpacker
         if b'MGEF' in self.tops:
             for record in self.tops[b'MGEF'].getActiveRecords():
                 ##: Skip OBME records, at least for now
@@ -470,8 +470,8 @@ class ModHeaderReader(object):
         return True
 
     @staticmethod
-    def extract_mod_data(mod_info, progress, skip_tes4=True,
-                         __unpacker=structs_cache[u'I'].unpack):
+    def extract_mod_data(mod_info, progress, skip_tes4=True, *,
+                         __unpacker=int_unpacker):
         """Reads the headers and EDIDs of every record in the specified mod,
         returning them as a dict, mapping record signature to a dict mapping
         FormIDs to a list of tuples containing the headers and EDIDs of every

Mopy/bash/parsers.py
@@ -173,7 +173,7 @@ class CsvParser(_TextParser):
         id_stored_data - both id and stored_data vary in type and meaning.
 
         :param csv_fields: A line in a CSV file, already split into fields."""
-        raise AbstractError(u'%s must implement _parse_line' % type(self))
+        raise AbstractError(f'{type(self)} must implement _parse_line')
 
     def _update_from_csv(self, top_grup_sig, csv_fields, index_dict=None):
         return MreRecord.type_class[top_grup_sig].parse_csv_line(csv_fields,