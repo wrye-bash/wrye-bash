wrye-bash/wrye-bash

commit a7f2b9c00249f70c98faf43b6cc475411301aa32

More nit - recurse_masters:
recurse_masters: rule of thumb if it uses the key it's ModInfos - was
essentially static before
Mopy/bash/basher/gui_patchers.py: dropped some intermediate variables
and went a bit far with fstrings

Mopy/bash/basher/gui_patchers.py
@@ -425,12 +425,9 @@ class _ListPatcherPanel(_PatcherPanel):
         self.configChoices = copy.deepcopy(
             config.get(u'configChoices', self.__class__.default_configChoices))
         #--Verify file existence
-        newConfigItems = []
-        for srcPath in self.configItems:
-            if (srcPath in bosh.modInfos or (reCsvExt.search(
-                srcPath.s) and srcPath in patches_set())):
-                newConfigItems.append(srcPath)
-        self.configItems = newConfigItems
+        self.configItems = [srcPath.s for srcPath in self.configItems if (
+                    srcPath in bosh.modInfos or (reCsvExt.search(
+                srcPath.s) and srcPath in patches_set()))]
         if self.__class__.forceItemCheck:
             for item in self.configItems:
                 self.configChecks[item] = True
@@ -438,8 +435,8 @@ class _ListPatcherPanel(_PatcherPanel):
 
     def saveConfig(self, configs):
         """Save config to configs dictionary."""
-        #--Toss outdated configCheck data.
         config = super(_ListPatcherPanel, self).saveConfig(configs)
+        #--Toss outdated configCheck data.
         listSet = set(self.configItems)
         self.configChecks = config[u'configChecks'] = {
             k: v for k, v in self.configChecks.items() if k in listSet}
@@ -799,10 +796,9 @@ class _ImporterPatcherPanel(_ListPatcherPanel):
         """Save config to configs dictionary."""
         config = super(_ImporterPatcherPanel, self).saveConfig(configs)
         if self.isEnabled:
-            importedMods = [item for item,value in
-                            self.configChecks.items() if
-                            value and bosh.ModInfos.rightFileType(item)]
-            configs[u'ImportedMods'].update(importedMods)
+            configs[u'ImportedMods'].update(
+                [item for item, value in self.configChecks.items() if
+                 value and bosh.ModInfos.rightFileType(item)])
         return config
 
 class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
@@ -830,11 +826,9 @@ class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
     def getItemLabel(self,item):
         # Note that we do *not* want to escape the & here - that puts *two*
         # ampersands in the resulting ListBox for some reason
-        choice = [i[0] for i in self.configChoices.get(item, tuple())]
-        if choice:
-            return u'%s [%s]' % (item, u''.join(sorted(choice)))
-        else:
-            return u'%s' % item # Path or string - YAK
+        choice = ''.join(
+            sorted(i[0] for i in self.configChoices.get(item) if i))
+        return f'{item}{f" [{choice}]" if choice else ""}'
 
     def GetConfigPanel(self, parent, config_layout, gTipText):
         if self.gConfigPanel: return self.gConfigPanel

Mopy/bash/bosh/__init__.py
@@ -3149,6 +3149,15 @@ class ModInfos(FileInfos):
             for p_master in p_info.masterNames:
                 cached_dependents[p_master].add(p)
 
+    def recurse_masters(self, fn_mod):
+        """Recursively collect all masters of fn_mod."""
+        ret_masters = set()
+        src_masters = self[fn_mod].masterNames if fn_mod in self else []
+        for src_master in src_masters:
+            ret_masters.add(src_master)
+            ret_masters.update(self.recurse_masters(src_master))
+        return ret_masters
+
 #------------------------------------------------------------------------------
 class SaveInfos(FileInfos):
     """SaveInfo collection. Represents save directory and related info."""

Mopy/bash/bosh/mods_metadata.py
@@ -256,7 +256,7 @@ def checkMods(mc_parent, showModList=False, showCRC=False, showVersion=True,
                 add_deleted_rec = all_deleted_others[p_ci_key].append
                 add_old_weapon = old_weapon_records[p_ci_key].append
                 add_hitme = all_hitmes[p_ci_key].append
-                p_masters = modInfos[p_ci_key].masterNames + (p_ci_key,)
+                p_masters = (*modInfos[p_ci_key].masterNames, p_ci_key)
                 p_num_masters = len(p_masters)
                 for r, d in ext_data.items():
                     for r_fid, (r_header, r_eid) in d.items():

Mopy/bash/patcher/patchers/mergers.py
@@ -59,7 +59,7 @@ class _AMerger(ImportPatcher):
         super(_AMerger, self).__init__(p_name, p_file, p_sources)
         self.id_deltas = defaultdict(list)
         merger_masters = set(chain.from_iterable(
-            self._recurse_masters(srcMod, p_file.p_file_minfos)
+            p_file.p_file_minfos.recurse_masters(srcMod)
             for srcMod in self.srcs))
         self._masters_and_srcs = merger_masters | set(self.srcs)
         # Set of record signatures that are actually provided by sources
@@ -69,16 +69,6 @@ class _AMerger(ImportPatcher):
         self.inventOnlyMods = (
             {x for x in self.srcs if x in p_file.mergeSet and u'IIM' in
              p_file.p_file_minfos[x].getBashTags()} if self.iiMode else set())
-
-    ##: Move to ModInfo? get_recursive_masters()?
-    def _recurse_masters(self, srcMod, minfs):
-        """Recursively collects all masters of srcMod."""
-        ret_masters = set()
-        src_masters = minfs[srcMod].masterNames if srcMod in minfs else []
-        for src_master in src_masters:
-            ret_masters.add(src_master)
-            ret_masters.update(self._recurse_masters(src_master, minfs))
-        return ret_masters
 
     ##: post-tweak pooling, see if we can use RecPath for this
     def _entry_key(self, subrecord_entry):

commit 4cb1cbf78f815913b70bb32c91a8c3917f2482a3

Fixups and nit:
Mopy/bash/fomod.py: fixup for ghosted files state:

if not self.dst_dir.join(test_file).exists() would fail if *only* the
ghost existed which is not what we wanted probably (in this case we want
FileState.INACTIVE). The elif would check if file and ghost both exist
but this should give precedence to the non ghosted file - so in this
case the state should be set to ACTIVE. Chops off one exists -
initially I had removed both, using the modInfos cache to check
existence but we don't know if test_file can be any file - see:
https://github.com/wrye-bash/wrye-bash/commit/555400a5fe8084f6d234bb7fbf238972e07cd6b1#commitcomment-75859387

The rest is fdust - some of it ghosting related

Mopy/bash/basher/gui_patchers.py: regression introduced in
a7f2b9c00249f70c98faf43b6cc475411301aa32

.github/workflows/wb_ci.yaml: run tests before build - makes more sense
plus build takes ages.

Mopy/bash/basher/gui_patchers.py: a regex is an overkill here

.github/workflows/wb_ci.yaml
@@ -26,15 +26,15 @@ jobs:
       run: |
         python -m pip install -U pip setuptools wheel
         python -m pip install -r requirements.txt
+    - name: Test Wrye Bash
+      run: |
+        python -m pytest
     - name: Build Wrye Bash
       # Note the configs - needed for build.py
       run: |
         git config --local user.name "GitHub Actions"
         git config --local user.email "github-action@users.noreply.github.com"
         python scripts/build.py --verbose
-    - name: Test Wrye Bash
-      run: |
-        python -m pytest
     - name: Copy executable/license files
       run: |
         cp "scripts/build/standalone/dist/Wrye Bash.exe" Mopy

Mopy/bash/basher/__init__.py
@@ -1263,7 +1263,7 @@ class ModList(_ModsUIList):
                     activated = [x for x in activated if x != inact]
                     changes[self.__activated_key][inact] = activated
             except BoltError as e:
-                balt.showError(self, u'%s' % e)
+                balt.showError(self, f'{e}')
                 break
         # Show warnings to the user if they attempted to deactivate mods that
         # can't be deactivated (e.g. vanilla masters on newer games) and/or
@@ -1271,14 +1271,14 @@ class ModList(_ModsUIList):
         # plugins).
         if illegal_deactivations:
             balt.askContinue(self,
-                _(u"You can't deactivate the following mods:")
-                + u'\n%s' % u', '.join(illegal_deactivations),
-                u'bash.mods.dnd.illegal_deactivation.continue')
+                             _("You can't deactivate the following mods:") +
+                             f"\n{u', '.join(illegal_deactivations)}",
+                             'bash.mods.dnd.illegal_deactivation.continue')
         if illegal_activations:
             balt.askContinue(self,
-                _(u"You can't activate the following mods:")
-                + u'\n%s' % u', '.join(illegal_activations),
-                u'bash.mods.dnd.illegal_activation.continue')
+                             _("You can't activate the following mods:") +
+                             f"\n{u', '.join(illegal_activations)}",
+                             'bash.mods.dnd.illegal_activation.continue')
         if touched:
             bosh.modInfos.cached_lo_save_active()
             self.__toggle_active_msg(changes)

Mopy/bash/basher/gui_fomod.py
@@ -218,7 +218,6 @@ class PageInstaller(WizardPage):
 
     def on_next(self):
         """Create flow control objects etc, implemented by sub-classes."""
-        pass
 
 class PageSelect(PageInstaller):
     """A Page that shows a message up top, with a selection box on the left
@@ -530,7 +529,7 @@ class PageFinish(PageInstaller):
     @staticmethod
     def display_files(file_dict):
         if not file_dict: return u''
-        lines = [u'{} -> {}'.format(v, k) for k, v in file_dict.items()]
+        lines = [f'{v} -> {k}' for k, v in file_dict.items()]
         lines.sort(key=str.lower)
         return u'\n'.join(lines)
 

Mopy/bash/basher/gui_patchers.py
@@ -37,8 +37,6 @@ from ..gui import Button, CheckBox, HBoxedLayout, Label, LayoutOptions, \
     SearchBar
 from ..patcher import patch_files, patches_set, base
 
-reCsvExt = re.compile(r'\.csv$', re.I | re.U)
-
 class _PatcherPanel(object):
     """Basic patcher panel with no options."""
     patcher_name = u'UNDEFINED'
@@ -205,7 +203,7 @@ class _AliasesPatcherPanel(_PatcherPanel):
     def SetAliasText(self):
         """Sets alias text according to current aliases."""
         self.gAliases.text_content = u'\n'.join([
-            u'%s >> %s' % (alias_target, alias_repl)
+            f'{alias_target} >> {alias_repl}'
             for alias_target, alias_repl in dict_sort(self._ci_aliases)])
 
     def OnEditAliases(self):
@@ -464,8 +462,8 @@ class _ListPatcherPanel(_PatcherPanel):
         #--Verify file existence
         self.configItems = [ # fix regression where these became strings
             srcPath for srcPath in map(GPath_no_norm,
-            self.configItems) if (srcPath in bosh.modInfos or (reCsvExt.search(
-            srcPath.s) and srcPath in patches_set()))]
+            self.configItems) if (srcPath in bosh.modInfos or (
+            srcPath.cext == 'csv' and srcPath in patches_set()))]
         if self.__class__.forceItemCheck:
             for item in self.configItems:
                 self.configChecks[item] = True
@@ -879,7 +877,7 @@ class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
         # Note that we do *not* want to escape the & here - that puts *two*
         # ampersands in the resulting ListBox for some reason
         choice = ''.join(
-            sorted(i[0] for i in self.configChoices.get(item) if i))
+            sorted(i[0] for i in self.configChoices.get(item, ()) if i))
         return f'{item}{f" [{choice}]" if choice else ""}'
 
     def GetConfigPanel(self, parent, config_layout, gTipText):

Mopy/bash/basher/mod_links.py
@@ -802,20 +802,17 @@ class _GhostLink(ItemLink):
     def getAllow(filename):
         return bosh.modInfos.table.getItem(filename, u'allowGhosting', True)
 
-    def _loop(self):
+    def Execute(self):
         """Loop selected files applying allow ghosting settings and
         (un)ghosting as needed."""
-        files = []
+        changed = []
+        set_allow = self.__class__.setAllow
+        to_ghost = self.__class__.toGhost
         for fileName, fileInfo in self.iselected_pairs():
-            fileInfo.set_table_prop(u'allowGhosting',
-                                    self.__class__.setAllow(fileName))
+            fileInfo.set_table_prop(u'allowGhosting', set_allow(fileName))
             oldGhost = fileInfo.isGhost
-            if fileInfo.setGhost(self.__class__.toGhost(fileName)) != oldGhost:
-                files.append(fileName)
-        return files
-
-    def Execute(self):
-        changed = self._loop()
+            if fileInfo.setGhost(to_ghost(fileName), itsa_ghost=oldGhost) != oldGhost:
+                changed.append(fileName)
         self.window.RefreshUI(redraw=changed, refreshSaves=False)
 
 class _Mod_AllowGhosting_All(_GhostLink, ItemLink):

Mopy/bash/bosh/__init__.py
@@ -2173,7 +2173,7 @@ class ModInfos(FileInfos):
         ourselves so ctime of the unghosted mods is not set."""
         load_order.save_lo(load_order.cached_lo_tuple(),
             load_order.cached_lord.lorder(
-                active if active is not None else self._active_wip))
+                self._active_wip if active is None else active))
 
     @_lo_cache
     def cached_lo_save_lo(self):
@@ -2716,7 +2716,7 @@ class ModInfos(FileInfos):
             espms_extra, esls_extra = load_order.check_active_limit(
                 self._active_wip + [fileName])
             if espms_extra or esls_extra:
-                msg = u'%s: Trying to activate more than ' % fileName
+                msg = f'{fileName}: Trying to activate more than '
                 if espms_extra:
                     msg += f'{load_order.max_espms():d} espms'
                 else:

Mopy/bash/fomod.py
@@ -45,7 +45,7 @@ __author__ = u'Ganda'
 import functools
 from enum import Enum
 
-from . import bass, bush, env
+from . import bass, bush, env, bosh # for modInfos
 from .bolt import GPath, Path, LooseVersion
 from .exception import XMLParsingError
 from .fomod_schema import schema_string
@@ -63,7 +63,6 @@ class FailedCondition(Exception):
     """Exception used to signal when a dependencies check is failed. Message
     passed to it should be human-readable and proper for a user to
     understand."""
-    pass
 
 class FileState(Enum):
     """The various states that a file can have. Implements the XML simpleType
@@ -562,11 +561,10 @@ class FomodInstaller(object):
         target_type = _str_to_fs[condition.get('state')]
         # Check if it's missing, ghosted or (in)active
         if not self.dst_dir.join(test_file).exists():
-            actual_type = FileState.MISSING
-        ##: Needed? Shouldn't this be handled by cached_is_active?
-        elif (test_file.cext in bush.game.espm_extensions and
-              self.dst_dir.join(test_file + u'.ghost').exists()):
-            actual_type = FileState.INACTIVE
+            if test_file not in bosh.modInfos:
+                actual_type = FileState.MISSING
+            else: # file in modInfos - its ghost must exist
+                actual_type = FileState.INACTIVE
         else:
             actual_type = (FileState.ACTIVE if cached_is_active(test_file)
                            else FileState.INACTIVE)

Mopy/bash/gui/doc_viewer.py
@@ -328,13 +328,12 @@ class DocumentViewer(_AComponent):
         """Load a PDF file if PDFViewer is available, or load the text.
 
         :param file_path: A bolt.Path instance or a unicode string."""
-        pdf_path = u'%s' % file_path
-        with open(pdf_path, u'rb') as ins:
+        with open(file_path, u'rb') as ins:
             pdf_valid = ins.read(4) == b'%PDF'
         if not pdf_valid:
-            deprint(u'%s is not a valid PDF' % pdf_path)
+            deprint(f'{file_path} is not a valid PDF')
         if pdf_valid and pdf_viewer_available():
-            self._pdf_ctrl.open_file(pdf_path)
+            self._pdf_ctrl.open_file('%s' % file_path)
             self.switch_to_pdf()
         else:
             self.try_load_text(file_path)