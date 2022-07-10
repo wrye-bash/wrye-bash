wrye-bash/wrye-bash

commit 4cb1cbf78f815913b70bb32c91a8c3917f2482a3

Fixups and nit:
Mopy/bash/fomod.py: fixup for ghosted files state:

if not self.dst_dir.join(test_file).exists() would fail if *only* the
ghost existed which is not what we wanted probably (in this case we want
FileState.INACTIVE). The elif would check if file and ghost both exist
but this should give precedence to the non ghosted file - so in this
case the state should be set to ACTIVE. Chops off one exists -
initially I had removed both, using the modInfos cache to check
existence but we don't know if test_file can be any file - see:
https://github.com/wrye-bash/wrye-bash/commit/555400a5fe8084f6d234bb7fbf238972e07cd6b1#commitcomment-75859387

The rest is fdust - some of it ghosting related

Mopy/bash/basher/gui_patchers.py: regression introduced in
a7f2b9c00249f70c98faf43b6cc475411301aa32

.github/workflows/wb_ci.yaml: run tests before build - makes more sense
plus build takes ages.

Mopy/bash/basher/gui_patchers.py: a regex is an overkill here

Mopy/bash/basher/__init__.py
@@ -1263,7 +1263,7 @@ class ModList(_ModsUIList):
                     activated = [x for x in activated if x != inact]
                     changes[self.__activated_key][inact] = activated
             except BoltError as e:
-                balt.showError(self, u'%s' % e)
+                balt.showError(self, f'{e}')
                 break
         # Show warnings to the user if they attempted to deactivate mods that
         # can't be deactivated (e.g. vanilla masters on newer games) and/or
@@ -1271,14 +1271,14 @@ class ModList(_ModsUIList):
         # plugins).
         if illegal_deactivations:
             balt.askContinue(self,
-                _(u"You can't deactivate the following mods:")
-                + u'\n%s' % u', '.join(illegal_deactivations),
-                u'bash.mods.dnd.illegal_deactivation.continue')
+                             _("You can't deactivate the following mods:") +
+                             f"\n{u', '.join(illegal_deactivations)}",
+                             'bash.mods.dnd.illegal_deactivation.continue')
         if illegal_activations:
             balt.askContinue(self,
-                _(u"You can't activate the following mods:")
-                + u'\n%s' % u', '.join(illegal_activations),
-                u'bash.mods.dnd.illegal_activation.continue')
+                             _("You can't activate the following mods:") +
+                             f"\n{u', '.join(illegal_activations)}",
+                             'bash.mods.dnd.illegal_activation.continue')
         if touched:
             bosh.modInfos.cached_lo_save_active()
             self.__toggle_active_msg(changes)

Mopy/bash/basher/mod_links.py
@@ -802,20 +802,17 @@ class _GhostLink(ItemLink):
     def getAllow(filename):
         return bosh.modInfos.table.getItem(filename, u'allowGhosting', True)
 
-    def _loop(self):
+    def Execute(self):
         """Loop selected files applying allow ghosting settings and
         (un)ghosting as needed."""
-        files = []
+        changed = []
+        set_allow = self.__class__.setAllow
+        to_ghost = self.__class__.toGhost
         for fileName, fileInfo in self.iselected_pairs():
-            fileInfo.set_table_prop(u'allowGhosting',
-                                    self.__class__.setAllow(fileName))
+            fileInfo.set_table_prop(u'allowGhosting', set_allow(fileName))
             oldGhost = fileInfo.isGhost
-            if fileInfo.setGhost(self.__class__.toGhost(fileName)) != oldGhost:
-                files.append(fileName)
-        return files
-
-    def Execute(self):
-        changed = self._loop()
+            if fileInfo.setGhost(to_ghost(fileName), itsa_ghost=oldGhost) != oldGhost:
+                changed.append(fileName)
         self.window.RefreshUI(redraw=changed, refreshSaves=False)
 
 class _Mod_AllowGhosting_All(_GhostLink, ItemLink):

Mopy/bash/bosh/__init__.py
@@ -2173,7 +2173,7 @@ class ModInfos(FileInfos):
         ourselves so ctime of the unghosted mods is not set."""
         load_order.save_lo(load_order.cached_lo_tuple(),
             load_order.cached_lord.lorder(
-                active if active is not None else self._active_wip))
+                self._active_wip if active is None else active))
 
     @_lo_cache
     def cached_lo_save_lo(self):
@@ -2716,7 +2716,7 @@ class ModInfos(FileInfos):
             espms_extra, esls_extra = load_order.check_active_limit(
                 self._active_wip + [fileName])
             if espms_extra or esls_extra:
-                msg = u'%s: Trying to activate more than ' % fileName
+                msg = f'{fileName}: Trying to activate more than '
                 if espms_extra:
                     msg += f'{load_order.max_espms():d} espms'
                 else:

.github/workflows/wb_ci.yaml
@@ -26,15 +26,15 @@ jobs:
       run: |
         python -m pip install -U pip setuptools wheel
         python -m pip install -r requirements.txt
+    - name: Test Wrye Bash
+      run: |
+        python -m pytest
     - name: Build Wrye Bash
       # Note the configs - needed for build.py
       run: |
         git config --local user.name "GitHub Actions"
         git config --local user.email "github-action@users.noreply.github.com"
         python scripts/build.py --verbose
-    - name: Test Wrye Bash
-      run: |
-        python -m pytest
     - name: Copy executable/license files
       run: |
         cp "scripts/build/standalone/dist/Wrye Bash.exe" Mopy

Mopy/bash/basher/gui_fomod.py
@@ -218,7 +218,6 @@ class PageInstaller(WizardPage):
 
     def on_next(self):
         """Create flow control objects etc, implemented by sub-classes."""
-        pass
 
 class PageSelect(PageInstaller):
     """A Page that shows a message up top, with a selection box on the left
@@ -530,7 +529,7 @@ class PageFinish(PageInstaller):
     @staticmethod
     def display_files(file_dict):
         if not file_dict: return u''
-        lines = [u'{} -> {}'.format(v, k) for k, v in file_dict.items()]
+        lines = [f'{v} -> {k}' for k, v in file_dict.items()]
         lines.sort(key=str.lower)
         return u'\n'.join(lines)
 

Mopy/bash/basher/gui_patchers.py
@@ -37,8 +37,6 @@ from ..gui import Button, CheckBox, HBoxedLayout, Label, LayoutOptions, \
     SearchBar
 from ..patcher import patch_files, patches_set, base
 
-reCsvExt = re.compile(r'\.csv$', re.I | re.U)
-
 class _PatcherPanel(object):
     """Basic patcher panel with no options."""
     patcher_name = u'UNDEFINED'
@@ -205,7 +203,7 @@ class _AliasesPatcherPanel(_PatcherPanel):
     def SetAliasText(self):
         """Sets alias text according to current aliases."""
         self.gAliases.text_content = u'\n'.join([
-            u'%s >> %s' % (alias_target, alias_repl)
+            f'{alias_target} >> {alias_repl}'
             for alias_target, alias_repl in dict_sort(self._ci_aliases)])
 
     def OnEditAliases(self):
@@ -464,8 +462,8 @@ class _ListPatcherPanel(_PatcherPanel):
         #--Verify file existence
         self.configItems = [ # fix regression where these became strings
             srcPath for srcPath in map(GPath_no_norm,
-            self.configItems) if (srcPath in bosh.modInfos or (reCsvExt.search(
-            srcPath.s) and srcPath in patches_set()))]
+            self.configItems) if (srcPath in bosh.modInfos or (
+            srcPath.cext == 'csv' and srcPath in patches_set()))]
         if self.__class__.forceItemCheck:
             for item in self.configItems:
                 self.configChecks[item] = True
@@ -879,7 +877,7 @@ class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
         # Note that we do *not* want to escape the & here - that puts *two*
         # ampersands in the resulting ListBox for some reason
         choice = ''.join(
-            sorted(i[0] for i in self.configChoices.get(item) if i))
+            sorted(i[0] for i in self.configChoices.get(item, ()) if i))
         return f'{item}{f" [{choice}]" if choice else ""}'
 
     def GetConfigPanel(self, parent, config_layout, gTipText):

Mopy/bash/fomod.py
@@ -45,7 +45,7 @@ __author__ = u'Ganda'
 import functools
 from enum import Enum
 
-from . import bass, bush, env
+from . import bass, bush, env, bosh # for modInfos
 from .bolt import GPath, Path, LooseVersion
 from .exception import XMLParsingError
 from .fomod_schema import schema_string
@@ -63,7 +63,6 @@ class FailedCondition(Exception):
     """Exception used to signal when a dependencies check is failed. Message
     passed to it should be human-readable and proper for a user to
     understand."""
-    pass
 
 class FileState(Enum):
     """The various states that a file can have. Implements the XML simpleType
@@ -562,11 +561,10 @@ class FomodInstaller(object):
         target_type = _str_to_fs[condition.get('state')]
         # Check if it's missing, ghosted or (in)active
         if not self.dst_dir.join(test_file).exists():
-            actual_type = FileState.MISSING
-        ##: Needed? Shouldn't this be handled by cached_is_active?
-        elif (test_file.cext in bush.game.espm_extensions and
-              self.dst_dir.join(test_file + u'.ghost').exists()):
-            actual_type = FileState.INACTIVE
+            if test_file not in bosh.modInfos:
+                actual_type = FileState.MISSING
+            else: # file in modInfos - its ghost must exist
+                actual_type = FileState.INACTIVE
         else:
             actual_type = (FileState.ACTIVE if cached_is_active(test_file)
                            else FileState.INACTIVE)

Mopy/bash/gui/doc_viewer.py
@@ -328,13 +328,12 @@ class DocumentViewer(_AComponent):
         """Load a PDF file if PDFViewer is available, or load the text.
 
         :param file_path: A bolt.Path instance or a unicode string."""
-        pdf_path = u'%s' % file_path
-        with open(pdf_path, u'rb') as ins:
+        with open(file_path, u'rb') as ins:
             pdf_valid = ins.read(4) == b'%PDF'
         if not pdf_valid:
-            deprint(u'%s is not a valid PDF' % pdf_path)
+            deprint(f'{file_path} is not a valid PDF')
         if pdf_valid and pdf_viewer_available():
-            self._pdf_ctrl.open_file(pdf_path)
+            self._pdf_ctrl.open_file('%s' % file_path)
             self.switch_to_pdf()
         else:
             self.try_load_text(file_path)

commit 6a0848fab12334e3ab5c3015591003ccf482f8c2

setGhost drop ghost state calculation:
The difficulty is tracing the _lo_cache decorator the rest of the uses
seem safe. Initially I had added the itsa_ghost parameter, but seeing
that we always passed the ghost state of the info we were called on,
and that _refresh_ghost_state would just set the isGhost to the value
we passed, therefore a no-op I went on and dropped the itsa_ghost:

@@ -665,3 +665,3 @@ def abs_path(self):

-    def setGhost(self, isGhost, *, itsa_ghost=None):
+    def setGhost(self, isGhost):
         """Sets file to/from ghost mode. Returns ghost status at end."""
@@ -672,4 +672,2 @@ def setGhost(self, isGhost, *, itsa_ghost=None):
         # Both should never exist simultaneously, Bash will warn in BashBugDump
-        # TODO(ut): we always pass itsa_ghost - drop the call below!
-        self._refresh_ghost_state(itsa_ghost=itsa_ghost)
         # Current status == what we want it?

Mopy/bash/basher/mod_links.py: the itsa_ghost param was added on dev in
4cb1cbf78f815913b70bb32c91a8c3917f2482a3 mistakenly - yey for
backporting.

Mopy/bash/basher/__init__.py: stray import from parent

Mopy/bash/basher/__init__.py
@@ -68,8 +68,7 @@ import wx
 from .. import bush, bosh, bolt, bass, env, load_order, archives, \
     initialization
 from ..bolt import GPath, SubProgress, deprint, round_size, dict_sort, \
-    top_level_items, GPath_no_norm, os_name, str_to_sig, FName, \
-    forward_compat_path_to_fn
+    top_level_items, os_name, str_to_sig, FName, forward_compat_path_to_fn
 from ..bosh import omods, ModInfo
 from ..exception import AbstractError, BoltError, CancelError, FileError, \
     SkipError, UnknownListener

Mopy/bash/basher/mod_links.py
@@ -810,7 +810,7 @@ class _GhostLink(ItemLink):
         for fileName, fileInfo in self.iselected_pairs():
             fileInfo.set_table_prop(u'allowGhosting', set_allow(fileName))
             oldGhost = fileInfo.isGhost
-            if fileInfo.setGhost(to_ghost(fileName), itsa_ghost=oldGhost) != oldGhost:
+            if fileInfo.setGhost(to_ghost(fileName)) != oldGhost:
                 changed.append(fileName)
         self.window.RefreshUI(redraw=changed, refreshSaves=False)
 

Mopy/bash/bosh/__init__.py
@@ -642,7 +642,7 @@ class ModInfo(FileInfo):
         return modInfos.dependents[self.fn_key]
 
     # Ghosting and ghosting related overrides ---------------------------------
-    def _refresh_ghost_state(self, regular_path=None, itsa_ghost=None):
+    def _refresh_ghost_state(self, regular_path=None, *, itsa_ghost=None):
         """Refreshes the isGhost state by checking existence on disk."""
         if itsa_ghost is not None:
             self.isGhost = itsa_ghost
@@ -663,15 +663,13 @@ class ModInfo(FileInfo):
         """Return joined dir and name, adding .ghost if the file is ghosted."""
         return (self._file_key + u'.ghost') if self.isGhost else self._file_key
 
-    def setGhost(self,isGhost): ## TODO pass itsa_ghost ?
+    def setGhost(self, isGhost):
         """Sets file to/from ghost mode. Returns ghost status at end."""
         # Refresh current status - it may have changed due to things like
         # libloadorder automatically unghosting plugins when activating them.
         # Libloadorder only un-ghosts automatically, so if both the normal
         # and ghosted version exist, treat the normal as the real one.
         # Both should never exist simultaneously, Bash will warn in BashBugDump
-        ##: Is this needed? Causes tons of stat calls on boot
-        self._refresh_ghost_state()
         # Current status == what we want it?
         if isGhost == self.isGhost: return isGhost
         normal = self._file_key
@@ -691,8 +689,8 @@ class ModInfo(FileInfo):
             # Notify BAIN, as this is basically a rename operation
             modInfos._notify_bain(renamed={ghost_source: ghost_target})
         except:
-            deprint(u'Failed to %sghost file %s' % ((u'un', u'')[isGhost],
-                (ghost, normal)[isGhost]), traceback=True)
+            deprint(f'Failed to {"" if isGhost else "un"}ghost file '
+                    f'{normal if isGhost else ghost}', traceback=True)
         return self.isGhost
 
     #--Bash Tags --------------------------------------------------------------