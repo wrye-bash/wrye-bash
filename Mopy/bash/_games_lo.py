wrye-bash/wrye-bash

commit c242561c94c8b9cc4c5bba5f89ca5b8a936fbc14

Some quick py3 cleanup
Had these sitting around on an older WIP branch. Pushing quickly so they
don't start conflicting again (that was not a fun rebase). Nothing major:

Mopy/bash/_games_lo.py:
Convert some type comments to annotations

Mopy/bash/balt.py:
Mopy/bash/gui/base_components.py:
Mopy/bash/gui/checkables.py:
Mopy/bash/gui/doc_viewer.py:
Drop _wx_type_override

Figured out why I needed it in the first place: delayed annotation
processing (aka from __future__ import annotations). You just have to
use typing.get_type_hints, but I completely forgot about that :P

Mopy/bash/bolt.py:
Mopy/bash/game/__init__.py:
Some (very) minor typing additions

Only the [un]pack methods. Helped a lot in debugging a cosaves issue a
while back, I just never pushed it.

Mopy/bash/bosh/cosaves.py:
Mopy/bash/tests/test_bosh/test_cosaves.py:
Modernize cosaves.py wrt. typing and py3

Mostly adding/converting type annotations and upgrading super() calls to
use the py3 format.

Under #619

Mopy/bash/_games_lo.py
@@ -35,7 +35,7 @@ from collections import defaultdict, OrderedDict
 
 # Local
 from . import bass, bolt, env, exception
-from .bolt import dict_sort, FName
+from .bolt import dict_sort, FName, Path
 from .ini_files import get_ini_type_and_encoding
 
 def _write_plugins_txt_(path, lord, active, _star):
@@ -63,7 +63,8 @@ def __write_plugins(out, lord, active, _star):
                          u'included in plugins.txt' % mod)
 
 _re_plugins_txt_comment = re.compile(b'^#.*')
-def _parse_plugins_txt_(path, mod_infos, _star):
+def _parse_plugins_txt_(path: Path, mod_infos, _star: bool) -> \
+        tuple[list[FName], list[FName]]:
     """Parse loadorder.txt and plugins.txt files with or without stars.
 
     Return two lists which are identical except when _star is True, whereupon
@@ -71,10 +72,7 @@ def _parse_plugins_txt_(path, mod_infos, _star):
     all other cases use the first list, which is either the list of active
     mods (when parsing plugins.txt) or the load order (when parsing
     loadorder.txt)
-    :type path: bolt.Path
     :type mod_infos: bosh.ModInfos
-    :type _star: bool
-    :rtype: (list[FName], list[FName])
     """
     with path.open(u'rb') as ins:
         #--Load Files
@@ -185,7 +183,7 @@ class LoGame(object):
     """API for setting, getting and validating the active plugins and the
     load order (of all plugins) according to the game engine (in principle)."""
     allow_deactivate_master = False
-    must_be_active_if_present = ()
+    must_be_active_if_present: tuple[FName] = ()
     max_espms = 255
     max_esls = 0
     # If set to False, indicates that this game has no plugins.txt. Currently
@@ -196,11 +194,12 @@ class LoGame(object):
     has_plugins_txt = True
     _star = False # whether plugins.txt uses a star to denote an active plugin
 
-    def __init__(self, mod_infos, plugins_txt_path):
+    def __init__(self, mod_infos, plugins_txt_path: bolt.Path):
+        """:type mod_infos: bosh.ModInfos"""
         super().__init__()
-        self.plugins_txt_path = plugins_txt_path # type: bolt.Path
+        self.plugins_txt_path = plugins_txt_path
         self.mod_infos = mod_infos # this is bosh.ModInfos, must be up to date
-        self.master_path = mod_infos._master_esm # type: bolt.FName
+        self.master_path = mod_infos._master_esm
         self.mtime_plugins_txt = 0.0
         self.size_plugins_txt = 0
 
@@ -711,13 +710,13 @@ class INIGame(LoGame):
 
     # INI directories, override if needed
     @property
-    def ini_dir_actives(self): # type: () -> bolt.Path
+    def ini_dir_actives(self) -> Path:
         """Returns the directory containing the actives INI. Defaults to the
         game path."""
         return bass.dirs[u'app']
 
     @property
-    def ini_dir_lo(self): # type: () -> bolt.Path
+    def ini_dir_lo(self) -> Path:
         """Returns the directory containing the load order INI. Defaults to the
         game path."""
         return bass.dirs[u'app']
@@ -1012,9 +1011,9 @@ class Morrowind(INIGame, TimestampGame):
 
 class TextfileGame(LoGame):
 
-    def __init__(self, mod_infos, plugins_txt_path, loadorder_txt_path):
+    def __init__(self, mod_infos, plugins_txt_path, loadorder_txt_path: Path):
         super().__init__(mod_infos, plugins_txt_path)
-        self.loadorder_txt_path = loadorder_txt_path # type: bolt.Path
+        self.loadorder_txt_path = loadorder_txt_path
         self.mtime_loadorder_txt = 0
         self.size_loadorder_txt = 0
 

Mopy/bash/gui/base_components.py
@@ -30,7 +30,7 @@ import os
 import platform
 import textwrap
 import wx as _wx
-from typing import Type
+from typing import Type, get_type_hints
 
 from .events import EventHandler, null_processor
 from ..bolt import deprint
@@ -122,15 +122,7 @@ class _AComponent(object):
     def __init__(self, parent, *args, **kwargs):
         """Creates a new _AComponent instance by initializing the wx widget
         with the specified parent, args and kwargs."""
-        try:
-            wx_widget_type = self.__class__._wx_type_override
-        except AttributeError:
-            wx_widget_type = self.__class__.__annotations__['_native_widget']
-            if not isinstance(wx_widget_type, type):
-                raise SyntaxError(f'Could not determine wx type from '
-                                  f'{self.__class__.__name__}._native_widget '
-                                  f'annotation, add a _wx_type_override to '
-                                  f'specify')
+        wx_widget_type = get_type_hints(self.__class__)['_native_widget']
         self._native_widget = wx_widget_type(self._resolve(parent), *args,
                                              **kwargs)
 

Mopy/bash/gui/doc_viewer.py
@@ -98,8 +98,6 @@ class WebViewer(_AComponent):
     navigating the history, clearing the history, navigating to a URL and
     reloading the current page."""
     _native_widget: _wx_html2.WebView
-    # Can't instantiate WebView directly, so we need an override
-    _wx_type_override = _wx_html2.WebView.New
 
     def __init__(self, parent, reload_ico, buttons_parent=None):
         """Creates a new WebViewer with the specified parent.
@@ -110,7 +108,10 @@ class WebViewer(_AComponent):
         :param buttons_parent: The object that the navigation buttons belong
                                to. If None, the same parent will be used."""
         if buttons_parent is None: buttons_parent = parent
-        super().__init__(parent, backend=_browser_backend)
+        # Can't use _AComponent.__init__ because we can't instantiate WebView
+        # directly
+        self._native_widget = _wx_html2.WebView.New(
+            parent, backend=_browser_backend)
         self._back_button = BackwardButton(buttons_parent)
         self._back_button.on_clicked.subscribe(self.go_back)
         self._forward_button = ForwardButton(buttons_parent)

Mopy/bash/balt.py
@@ -2086,7 +2086,6 @@ class _CheckList_SelectAll(ItemLink):
 # TODO(inf) Needs renaming, also need to make a virtual version eventually...
 class TreeCtrl(_AComponent):
     _native_widget: wx.TreeCtrl
-    _wx_type_override = wx.TreeCtrl ##: Why is this one necessary?
 
     def __init__(self, parent, title, items_dict):
         super(TreeCtrl, self).__init__(parent, size=(150, 200),

Mopy/bash/bolt.py
@@ -1753,72 +1753,72 @@ class _StructsCache(dict):
         return self.setdefault(key, struct.Struct(key))
 
 structs_cache = _StructsCache()
-def unpack_str16(ins, __unpack=structs_cache[u'H'].unpack):
+def unpack_str16(ins, __unpack=structs_cache[u'H'].unpack) -> bytes:
     return ins.read(__unpack(ins.read(2))[0])
-def unpack_str32(ins, __unpack=structs_cache[u'I'].unpack):
+def unpack_str32(ins, __unpack=structs_cache[u'I'].unpack) -> bytes:
     return ins.read(__unpack(ins.read(4))[0])
-def unpack_int(ins, __unpack=structs_cache[u'I'].unpack):
+def unpack_int(ins, __unpack=structs_cache[u'I'].unpack) -> int:
     return __unpack(ins.read(4))[0]
-def pack_int(out, value, __pack=structs_cache[u'=I'].pack):
+def pack_int(out, value: int, __pack=structs_cache[u'=I'].pack):
     out.write(__pack(value))
-def unpack_short(ins, __unpack=structs_cache[u'H'].unpack):
+def unpack_short(ins, __unpack=structs_cache[u'H'].unpack) -> int:
     return __unpack(ins.read(2))[0]
-def pack_short(out, val, __pack=structs_cache[u'=H'].pack):
+def pack_short(out, val: int, __pack=structs_cache[u'=H'].pack):
     out.write(__pack(val))
-def unpack_float(ins, __unpack=structs_cache[u'f'].unpack):
+def unpack_float(ins, __unpack=structs_cache[u'f'].unpack) -> float:
     return __unpack(ins.read(4))[0]
-def pack_float(out, val, __pack=structs_cache[u'=f'].pack):
+def pack_float(out, val: float, __pack=structs_cache[u'=f'].pack):
     out.write(__pack(val))
-def unpack_double(ins, __unpack=structs_cache[u'd'].unpack):
+def unpack_double(ins, __unpack=structs_cache[u'd'].unpack) -> float:
     return __unpack(ins.read(8))[0]
-def pack_double(out, val, __pack=structs_cache[u'=d'].pack):
+def pack_double(out, val: float, __pack=structs_cache[u'=d'].pack):
     out.write(__pack(val))
-def unpack_byte(ins, __unpack=structs_cache[u'B'].unpack):
+def unpack_byte(ins, __unpack=structs_cache[u'B'].unpack) -> int:
     return __unpack(ins.read(1))[0]
-def pack_byte(out, val, __pack=structs_cache[u'=B'].pack):
+def pack_byte(out, val: int, __pack=structs_cache[u'=B'].pack):
     out.write(__pack(val))
-def unpack_int_signed(ins, __unpack=structs_cache[u'i'].unpack):
+def unpack_int_signed(ins, __unpack=structs_cache[u'i'].unpack) -> int:
     return __unpack(ins.read(4))[0]
-def pack_int_signed(out, val, __pack=structs_cache[u'=i'].pack):
+def pack_int_signed(out, val: int, __pack=structs_cache[u'=i'].pack):
     out.write(__pack(val))
-def unpack_int64_signed(ins, __unpack=structs_cache[u'q'].unpack):
+def unpack_int64_signed(ins, __unpack=structs_cache[u'q'].unpack) -> int:
     return __unpack(ins.read(8))[0]
-def unpack_4s(ins, __unpack=structs_cache[u'4s'].unpack):
+def unpack_4s(ins, __unpack=structs_cache[u'4s'].unpack) -> bytes:
     return __unpack(ins.read(4))[0]
-def pack_4s(out, val, __pack=structs_cache[u'=4s'].pack):
+def pack_4s(out, val: bytes, __pack=structs_cache[u'=4s'].pack):
     out.write(__pack(val))
-def unpack_str16_delim(ins, __unpack=structs_cache[u'Hc'].unpack):
+def unpack_str16_delim(ins, __unpack=structs_cache[u'Hc'].unpack) -> bytes:
     str_len = __unpack(ins.read(3))[0]
     # The actual string (including terminator) isn't stored for empty strings
     if not str_len: return b''
     str_value = ins.read(str_len)
     ins.seek(1, 1) # discard string terminator
     return str_value
-def unpack_str_int_delim(ins, __unpack=structs_cache[u'Ic'].unpack):
+def unpack_str_int_delim(ins, __unpack=structs_cache[u'Ic'].unpack) -> int:
     return __unpack(ins.read(5))[0]
-def unpack_str_byte_delim(ins, __unpack=structs_cache[u'Bc'].unpack):
+def unpack_str_byte_delim(ins, __unpack=structs_cache[u'Bc'].unpack) -> int:
     return __unpack(ins.read(2))[0]
-def unpack_str8(ins, __unpack=structs_cache[u'B'].unpack):
+def unpack_str8(ins, __unpack=structs_cache[u'B'].unpack) -> bytes:
     return ins.read(__unpack(ins.read(1))[0])
-def pack_str8(out, val, __pack=structs_cache[u'=B'].pack):
+def pack_str8(out, val: bytes, __pack=structs_cache[u'=B'].pack):
     pack_byte(out, len(val))
     out.write(val)
-def pack_bzstr8(out, val, __pack=structs_cache[u'=B'].pack):
+def pack_bzstr8(out, val: bytes, __pack=structs_cache[u'=B'].pack):
     pack_byte(out, len(val) + 1)
     out.write(val)
     out.write(b'\x00')
-def unpack_string(ins, string_len):
+def unpack_string(ins, string_len) -> bytes:
     return struct_unpack(f'{string_len}s', ins.read(string_len))[0]
-def pack_string(out, val):
+def pack_string(out, val: bytes):
     out.write(val)
 
-def pack_byte_signed(out, value, __pack=structs_cache[u'b'].pack):
+def pack_byte_signed(out, value: int, __pack=structs_cache[u'b'].pack):
     out.write(__pack(value))
 
-def unpack_many(ins, fmt):
+def unpack_many(ins, fmt: str):
     return struct_unpack(fmt, ins.read(struct.calcsize(fmt)))
 
-def unpack_spaced_string(ins, replacement_char=b'\x07'):
+def unpack_spaced_string(ins, replacement_char=b'\x07') -> bytes:
     """Unpacks a space-terminated string. Occurs if someone used
     std::stringstream to convert struct data into strings. Obviously that means
     a replacement character is needed for spaces, which is \x07 by default."""

Mopy/bash/game/__init__.py
@@ -88,7 +88,7 @@ class GameInfo(object):
     # and therefore needs a different file here).
     version_detect_file = u''
     # The main plugin Wrye Bash should look for
-    master_file = bolt.FName('')
+    master_file: bolt.FName = bolt.FName('')
     # The directory in which mods and other data files reside. This is relative
     # to the game directory.
     mods_dir = u'Data'

commit d32dd76a7634d425ac6e0e7c7503302a0b2a7f8b

Finish converting _games_lo annotations
Fixes all but one PyCharm warning in _games_lo. Saves quite a few lines
as well, which is nice.

Mopy/bash/basher/gui_patchers.py:
Quick decoupling

Should have happened all the way back in
9fd10d9efe286120a9105baec9f0ba709df54f3e.

Mopy/bash/bosh/cosaves.py:
Minor nit in cosaves

Mopy/bash/gui/doc_viewer.py:
Mopy/bash/gui/top_level_windows.py:
Regression fix for doc browser

Broke in c242561c94c8b9cc4c5bba5f89ca5b8a936fbc14.

Mopy/bash/gui/base_components.py:
Drop _wx_type_override remnants

Obsoleted in c242561c94c8b9cc4c5bba5f89ca5b8a936fbc14.

Mopy/bash/_games_lo.py
@@ -26,6 +26,7 @@ load_order.py."""
 ##: multiple backups? fixes can happen in rapid succession, so preserving
 # several older files in a directory would be useful (maybe limit to some
 # number, e.g. 5 older versions)
+from __future__ import annotations
 
 __author__ = u'Utumno'
 
@@ -37,6 +38,10 @@ from collections import defaultdict, OrderedDict
 from . import bass, bolt, env, exception
 from .bolt import dict_sort, FName, Path
 from .ini_files import get_ini_type_and_encoding
+
+# Typing
+_LoTuple = tuple[FName, ...]
+_ParsedLo = tuple[list[FName], list[FName]]
 
 def _write_plugins_txt_(path, lord, active, _star):
     try:
@@ -63,8 +68,7 @@ def __write_plugins(out, lord, active, _star):
                          u'included in plugins.txt' % mod)
 
 _re_plugins_txt_comment = re.compile(b'^#.*')
-def _parse_plugins_txt_(path: Path, mod_infos, _star: bool) -> \
-        tuple[list[FName], list[FName]]:
+def _parse_plugins_txt_(path: Path, mod_infos, _star: bool) -> _ParsedLo:
     """Parse loadorder.txt and plugins.txt files with or without stars.
 
     Return two lists which are identical except when _star is True, whereupon
@@ -183,7 +187,7 @@ class LoGame(object):
     """API for setting, getting and validating the active plugins and the
     load order (of all plugins) according to the game engine (in principle)."""
     allow_deactivate_master = False
-    must_be_active_if_present: tuple[FName] = ()
+    must_be_active_if_present: _LoTuple = ()
     max_espms = 255
     max_esls = 0
     # If set to False, indicates that this game has no plugins.txt. Currently
@@ -194,7 +198,7 @@ class LoGame(object):
     has_plugins_txt = True
     _star = False # whether plugins.txt uses a star to denote an active plugin
 
-    def __init__(self, mod_infos, plugins_txt_path: bolt.Path):
+    def __init__(self, mod_infos, plugins_txt_path: Path):
         """:type mod_infos: bosh.ModInfos"""
         super().__init__()
         self.plugins_txt_path = plugins_txt_path
@@ -210,8 +214,9 @@ class LoGame(object):
                            self.plugins_txt_path.size_mtime())
 
     # API ---------------------------------------------------------------------
-    def get_load_order(self, cached_load_order, cached_active_ordered,
-                       fix_lo=None):
+    def get_load_order(self, cached_load_order: _LoTuple,
+            cached_active_ordered: _LoTuple,
+            fix_lo=None) -> tuple[_LoTuple, _LoTuple]:
         """Get and validate current load order and active plugins information.
 
         Meant to fetch at once both load order and active plugins
@@ -221,11 +226,7 @@ class LoGame(object):
         The caller is responsible for passing a valid cached value in. If you
         pass a cached value for either parameter this value will be returned
         unchanged, possibly validating the other one based on stale data.
-        NOTE: modInfos must exist and be up to date for validation.
-        :type cached_load_order: tuple[FName, ...]
-        :type cached_active_ordered: tuple[FName, ...]
-        :rtype: (tuple[FName, ...], tuple[FName, ...])
-        """
+        NOTE: modInfos must exist and be up to date for validation."""
         if cached_load_order is not None and cached_active_ordered is not None:
             return cached_load_order, cached_active_ordered # NOOP
         lo, active = self._cached_or_fetch(cached_load_order,
@@ -378,20 +379,18 @@ class LoGame(object):
         load order plugins list."""
         raise exception.AbstractError
 
-    def _fetch_load_order(self, cached_load_order, cached_active):
-        """:type cached_load_order: tuple[FName, ...] | None
-        :type cached_active: tuple[FName, ...]"""
+    def _fetch_load_order(self, cached_load_order: _LoTuple | None,
+            cached_active: _LoTuple):
         raise exception.AbstractError
 
-    def _fetch_active_plugins(self): # no override for AsteriskGame
-        """:rtype: list[FName]"""
-        raise exception.AbstractError
+    def _fetch_active_plugins(self) -> list[FName]:
+        raise exception.AbstractError # no override for AsteriskGame
 
     def _persist_load_order(self, lord, active, *, _cleaned=False):
         """Persist the fixed lord to disk - will break conflicts for
         timestamp games.
         :param _cleaned: internal, used in AsteriskGame - whether lists are
-        already cleaned."""
+            already cleaned."""
         raise exception.AbstractError(f'{type(self)} does not define '
                                       f'_persist_load_order')
 
@@ -405,8 +404,7 @@ class LoGame(object):
         raise exception.AbstractError
 
     # MODFILES PARSING --------------------------------------------------------
-    def _parse_modfile(self, path, do_raise=False):
-        """:rtype: (list[FName], list[FName])"""
+    def _parse_modfile(self, path, do_raise=False) -> _ParsedLo:
         #--Read file
         try:
             return _parse_plugins_txt_(path, self.mod_infos, _star=self._star)
@@ -418,9 +416,8 @@ class LoGame(object):
         _write_plugins_txt_(path, lord, active, _star=self._star)
 
     # PLUGINS TXT -------------------------------------------------------------
-    def _parse_plugins_txt(self):
-        """Read plugins.txt file and return a tuple of (active, loadorder).
-        :rtype: (list[FName], list[FName])"""
+    def _parse_plugins_txt(self) -> _ParsedLo:
+        """Read plugins.txt file and return a tuple of (active, loadorder)."""
         try:
             acti_lo = self._parse_modfile(self.plugins_txt_path, do_raise=True)
             self.__update_plugins_txt_cache_info()
@@ -432,7 +429,8 @@ class LoGame(object):
         self._write_modfile(self.plugins_txt_path, lord, active)
         self.__update_plugins_txt_cache_info()
 
-    def _filter_actives(self, active, rem_from_acti):
+    @staticmethod
+    def _filter_actives(active, rem_from_acti):
         """Removes entries that are not supposed to be in the actives file."""
         return [x for x in active if
                 x not in rem_from_acti] if rem_from_acti else active
@@ -459,32 +457,28 @@ class LoGame(object):
         self.size_plugins_txt, self.mtime_plugins_txt = \
             self.plugins_txt_path.size_mtime()
 
-    def get_acti_file(self):
+    def get_acti_file(self) -> Path | None:
         """Returns the path of the file used by this game for storing active
-        plugins.
-
-        :rtype: bolt.Path"""
+        plugins."""
         return None # base case
 
-    def get_lo_file(self):
+    def get_lo_file(self) -> Path | None:
         """Returns the path of the file used by this game for storing load
-        order.
-
-        :rtype: bolt.Path"""
+        order."""
         return None # base case
 
-    def _set_acti_file(self, new_acti_file):
+    def _set_acti_file(self, new_acti_file: Path):
         """Sets the path of the file used by this game for storing active
         plugins."""
         pass # base case
 
-    def _set_lo_file(self, new_lo_file):
+    def _set_lo_file(self, new_lo_file: Path):
         """Sets the path of the file used by this game for storing load
         order."""
         pass # base case
 
     # VALIDATION --------------------------------------------------------------
-    def _fix_load_order(self, lord, fix_lo):
+    def _fix_load_order(self, lord: list[FName], fix_lo):
         """Fix inconsistencies between given loadorder and actually installed
         mod files as well as impossible load orders. We need a refreshed
         bosh.modInfos reflecting the contents of Data/.
@@ -492,9 +486,7 @@ class LoGame(object):
         Called in get_load_order() to fix a newly fetched LO and in
         set_load_order() to check if a load order passed in is valid. Needs
         rethinking as save load and active should be an atomic operation -
-        leads to hacks (like the _selected parameter).
-        :type lord: list[FName]
-        """
+        leads to hacks (like the _selected parameter)."""
         if fix_lo is None: fix_lo = FixInfo() # discard fix info
         old_lord = lord[:]
         # game's master might be out of place (if using timestamps for load
@@ -640,8 +632,7 @@ class LoGame(object):
         return index_of_first_esp
 
     @staticmethod
-    def _check_for_duplicates(plugins_list):
-        """:type plugins_list: list[FName]"""
+    def _check_for_duplicates(plugins_list: list[FName]):
         mods, duplicates, j = set(), set(), 0
         mods_add = mods.add
         duplicates_add = duplicates.add
@@ -729,14 +720,10 @@ class INIGame(LoGame):
         return ini_type(ini_fpath, ini_encoding)
 
     @staticmethod
-    def _read_ini(cached_ini, ini_key):
-        """Reads a section specified INI using the specified key and returns
-        all its values, as bolt.FName objects. Handles missing INI file and an
-        absent section gracefully.
-
-        :type cached_ini: bosh.ini_files.IniFile
-        :type ini_key: tuple[str, str, str]
-        :rtype: list[bolt.FName]"""
+    def _read_ini(cached_ini, ini_key: tuple[str, str, str]) -> list[FName]:
+        """Reads a section specified IniFile using the specified key and
+        returns all its values as FName objects. Handles missing INI file and
+        an absent section gracefully."""
         # Returned format is dict[FName, tuple[str, int]], we want the
         # unicode (i.e. the mod names)
         section_mapping = cached_ini.get_setting_values(ini_key[1], {})
@@ -745,12 +732,10 @@ class INIGame(LoGame):
         return [FName(x[1][0]) for x in section_vals] ##: unpack - is len(x)==2?
 
     @staticmethod
-    def _write_ini(cached_ini, ini_key, mod_list):
-        """Writes out the specified INI using the specified key and mod list.
-
-        :type cached_ini: bosh.ini_files.IniFile
-        :type ini_key: tuple[str, str, str]
-        :type mod_list: list[FName]"""
+    def _write_ini(cached_ini, ini_key: tuple[str, str, str],
+            mod_list: list[FName]):
+        """Writes out the specified IniFile using the specified key and mod
+        list."""
         # Remove any existing section - also prevents duplicate sections with
         # different case
         cached_ini.remove_section(ini_key[1])
@@ -888,13 +873,10 @@ class INIGame(LoGame):
 
 class TimestampGame(LoGame):
     """Oblivion and other games where load order is set using modification
-    times.
-
-    :type _mtime_mods: dict[int, set[bolt.Path]]
-    """
-
+    times."""
     allow_deactivate_master = True
-    _mtime_mods = defaultdict(set) # intentionally imprecise mtime cache
+    # Intentionally imprecise mtime cache
+    _mtime_mods: defaultdict[int, set[Path]] = defaultdict(set)
     _get_free_time_step = 1.0 # step by one second intervals
 
     @classmethod
@@ -1079,15 +1061,15 @@ class TextfileGame(LoGame):
             bolt.deprint(f'Tried to back up {self.loadorder_txt_path},'
                          f' but it did not exist')
 
-    def _fetch_load_order(self, cached_load_order, cached_active):
+    def _fetch_load_order(self, cached_load_order,
+            cached_active: tuple[Path] | list[Path]):
         """Read data from loadorder.txt file. If loadorder.txt does not
         exist create it and try reading plugins.txt so the load order of the
         user is preserved (note it will create the plugins.txt if not
         existing). Additional mods should be added by caller who should
         anyway call _fix_load_order. If cached_active is passed, the relative
         order of mods will be corrected to match their relative order in
-        cached_active.
-        :type cached_active: tuple[bolt.Path] | list[bolt.Path]"""
+        cached_active."""
         if not self.loadorder_txt_path.exists():
             mods = cached_active or []
             if (cached_active is not None
@@ -1323,6 +1305,9 @@ class AsteriskGame(LoGame):
 class WindowsStoreGame(LoGame):
     """Mixin for Windows Store games, which have a second, fallback directory
     which we must keep in sync with the main one."""
+    _second_acti_file: Path | None
+    _second_lo_file: Path | None
+
     @property
     def _fallback_lo_files(self):
         """Returns a tuple containing the fallback actives/LO files"""

Mopy/bash/basher/gui_patchers.py
@@ -1169,9 +1169,8 @@ class ImportRaces(_ImporterPatcherPanel):
     patcher_name = _(u'Import Races')
     patcher_desc = _('Import race eyes, hair, body, voice, etc. from source '
                      'plugins.')
-    ##: Move to a game constant -> multi-game plus decouples this
     autoKey = set(chain.from_iterable(d for d in
-        preservers.ImportRacesPatcher.rec_attrs.values()))
+        bush.game.import_races_attrs.values()))
     _config_key = u'ImportRaces'
     patcher_type = preservers.ImportRacesPatcher
 

Mopy/bash/bosh/cosaves.py
@@ -1051,8 +1051,8 @@ class _PluggyNameBlock(_PluggyBlock):
 
         def dump_to_log(self, log, save_masters_):
             log(_(u'    - RefID : 0x%08X') % self.reference_id)
-            printable_name = ''.join([c if c in string.printable else ''
-                                      for c in self.name_data])
+            printable_name = ''.join([c for c in self.name_data
+                                      if c in string.printable])
             log(_(u'      Name  : %s') % printable_name)
 
     def __init__(self, ins, record_type):

Mopy/bash/gui/base_components.py
@@ -30,7 +30,7 @@ import os
 import platform
 import textwrap
 import wx as _wx
-from typing import Type, get_type_hints
+from typing import get_type_hints
 
 from .events import EventHandler, null_processor
 from ..bolt import deprint
@@ -115,9 +115,6 @@ class _AComponent(object):
     """Abstract base class for all GUI items. Holds a reference to the native
     wx widget that we abstract over."""
     _native_widget: _wx.Window
-    # Only override this if the type annotation is not enough to determine the
-    # right type
-    _wx_type_override: Type[_wx.Window]
 
     def __init__(self, parent, *args, **kwargs):
         """Creates a new _AComponent instance by initializing the wx widget

Mopy/bash/gui/doc_viewer.py
@@ -111,7 +111,7 @@ class WebViewer(_AComponent):
         # Can't use _AComponent.__init__ because we can't instantiate WebView
         # directly
         self._native_widget = _wx_html2.WebView.New(
-            parent, backend=_browser_backend)
+            self._resolve(parent), backend=_browser_backend)
         self._back_button = BackwardButton(buttons_parent)
         self._back_button.on_clicked.subscribe(self.go_back)
         self._forward_button = ForwardButton(buttons_parent)

Mopy/bash/gui/top_level_windows.py
@@ -334,7 +334,7 @@ class CenteredSplash(_AComponent):
         # user or is explicitly destroyed when the main window is ready
         splash_style = _adv.SPLASH_CENTER_ON_SCREEN | _adv.SPLASH_NO_TIMEOUT
         # Can't use _AComponent.__init__ here, because for some ungodly reason
-        # parent is the *third* parameter in SplashScreen
+        # parent is the *fourth* parameter in SplashScreen
         self._native_widget = _adv.SplashScreen(
             splash_bitmap, splash_style, 1, None) # Timeout - ignored
         self._on_close_evt = self._evt_handler(_wx.EVT_CLOSE)