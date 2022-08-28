wrye-bash/wrye-bash

commit df9f844734741d426f8a18fe32cb563d6794ac31

FO4: Implement ARTO, ASPC and ASTP
Also dropped some flags that aren't actually flags but enums instead.

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -4892,7 +4892,7 @@ C.Location, -C.LockList
                     <li>(ASPC) Acoustic Space:
                         <ul>
                             <li>(SNAM) Sound - Looping</li>
-                            <li>(RDAT) Use Sound from region (Interiors Only)</li>
+                            <li>(RDAT) Use Sound From Region (Interiors Only)</li>
                         </ul>
                     </li>
                     <li>(COBJ) Constructible Object:
@@ -5834,7 +5834,7 @@ C.Location, -C.LockList
                     <li>(ASPC) Acoustic Space:
                         <ul>
                             <li>(ZNAM) Ambient Sound</li>
-                            <li>(RDAT) Use Sound from Region (interiors Only)</li>
+                            <li>(RDAT) Use Sound From Region (Interiors Only)</li>
                             <li>(BNAM) Environment Type (reverb)</li>
                         </ul>
                     </li>

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -340,7 +340,7 @@ soundsTypes = {
     b'ADDN': ('sound',),
     b'ALCH': (u'dropSound', u'pickupSound', u'soundConsume'),
     b'ARMO': (u'pickupSound', u'dropSound'),
-    b'ASPC': ('sound', 'useSoundFromRegion'),
+    b'ASPC': ('sound', 'use_sound_from_region'),
     b'COBJ': (u'pickupSound', u'dropSound'),
     b'CONT': ('sound', 'soundClose'),
     b'CREA': (u'footWeight', u'inheritsSoundsFrom', u'sounds'),

Mopy/bash/game/skyrim/patcher/__init__.py
@@ -493,7 +493,7 @@ soundsTypes = {
     b'APPA': (u'pickupSound', u'dropSound'),
     b'ARMA': ('footstep_sound',),
     b'ARMO': (u'pickupSound', u'dropSound'),
-    b'ASPC': ('sound', 'regionData', 'reverb'),
+    b'ASPC': ('sound', 'use_sound_from_region', 'aspc_reverb'),
     b'BOOK': (u'pickupSound', u'dropSound'),
     b'CONT': ('sound', 'soundClose'),
     b'DOOR': ('sound', 'soundClose', 'soundLoop'),

Mopy/bash/game/fallout4/__init__.py
@@ -219,11 +219,11 @@ class Fallout4GameInfo(PatchGame):
     def init(cls):
         cls._dynamic_import_modules(__name__)
         from .records import MreAact, MreActi, MreAddn, MreAech, MreAmdl, \
-            MreAnio, MreAoru, MreArma, MreArmo, \
+            MreAnio, MreAoru, MreArma, MreArmo, MreArto, MreAstp, \
             MreGmst, MreLvli, MreLvln, MrePerk, MreTes4
         cls.mergeable_sigs = {clazz.rec_sig: clazz for clazz in (
             MreAact, MreActi, MreAddn, MreAech, MreAmdl, MreAnio, MreAoru,
-            MreArma, MreArmo,
+            MreArma, MreArmo, MreArto, MreAstp,
             MreGmst, MreLvli, MreLvln, MrePerk,
         )}
         # Setting RecordHeader class variables --------------------------------
@@ -256,7 +256,7 @@ class Fallout4GameInfo(PatchGame):
         header_type.plugin_form_version = 131
         brec.MreRecord.type_class = {x.rec_sig: x for x in (
             MreAact, MreActi, MreAddn, MreAech, MreAmdl, MreAnio, MreAoru,
-            MreArma, MreArmo,
+            MreArma, MreArmo, MreArto, MreAstp,
             MreGmst, MreLvli, MreLvln, MrePerk, MreTes4,
         )}
         brec.MreRecord.simpleTypes = (

Mopy/bash/brec/common_subrecords.py
@@ -184,6 +184,43 @@ class MelArtObject(MelFid):
         super().__init__(b'ONAM', 'art_object')
 
 #------------------------------------------------------------------------------
+class MelArtType(MelUInt32):
+    """Handles the ARTO subrecord DNAM (Art Type)."""
+    def __init__(self):
+        super().__init__(b'DNAM', 'art_type')
+
+#------------------------------------------------------------------------------
+class MelAspcBnam(MelFid):
+    """Handles the ASPC subrecord BNAM (Environment Type (reverb))."""
+    def __init__(self):
+        super().__init__(b'BNAM', 'environment_type')
+
+#------------------------------------------------------------------------------
+class MelAspcRdat(MelFid):
+    """Handles the ASPC subrecord RDAT (Use Sound From Region (Interiors
+    Only))."""
+    def __init__(self):
+        super().__init__(b'RDAT', 'use_sound_from_region')
+
+#------------------------------------------------------------------------------
+class MelAstpData(MelUInt32):
+    """Handles the ASTP subrecord DATA. Called 'Flags' in xEdit, but is really
+    a boolean enum."""
+    def __init__(self):
+        super().__init__(b'DATA', 'family_association')
+
+#------------------------------------------------------------------------------
+class MelAstpTitles(MelSequential):
+    """Handles the ASTP subrecords MPRT, FPRT, MCHT and FCHT."""
+    def __init__(self):
+        super().__init__(
+            MelString(b'MPRT', 'male_parent_title'),
+            MelString(b'FPRT', 'female_parent_title'),
+            MelString(b'MCHT', 'male_child_title'),
+            MelString(b'FCHT', 'female_child_title'),
+        )
+
+#------------------------------------------------------------------------------
 class MelAttx(MelLString):
     """Handles the common ATTX (Activate Text Override) subrecord. Skyrim uses
     an RNAM signature instead."""

Mopy/bash/game/fallout3/records.py
@@ -47,7 +47,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelReflectedRefractedBy, MelValueWeight, SpellFlags, MelBaseR, \
     MelSound, MelSoundActivation, MelWaterType, MelConditionsFo3, \
     MelNodeIndex, MelAddnDnam, MelEffectsFo3, MelShortName, PerkEpdfDecider, \
-    MelPerkParamsGroups
+    MelPerkParamsGroups, MelAspcRdat
 from ...exception import ModSizeError
 
 _is_fnv = bush.game.fsName == u'FalloutNV'
@@ -499,7 +499,6 @@ class MreAmmo(MelRecord):
 #------------------------------------------------------------------------------
 class MreAnio(MelRecord):
     """Animation Object."""
-
     rec_sig = b'ANIO'
 
     melSet = MelSet(
@@ -601,10 +600,10 @@ class MreAspc(MelRecord):
             # easier to load them like this than with a distributor
             fnv_version=MelFids('sound', MelFid(b'SNAM')),
         ),
-        fnv_only(MelUInt32(b'WNAM', 'wallaTrigerCount')),
-        MelFid(b'RDAT','useSoundFromRegion'),
-        MelUInt32(b'ANAM', 'environmentType'),
-        fnv_only(MelUInt32(b'INAM', 'isInterior')),
+        fnv_only(MelUInt32(b'WNAM', 'walla_trigger_count')),
+        MelAspcRdat(),
+        MelUInt32(b'ANAM', 'environment_type'),
+        fnv_only(MelUInt32(b'INAM', 'aspc_is_interior')),
     )
     __slots__ = melSet.getSlotsUsed()
 

Mopy/bash/game/fallout4/records.py
@@ -36,7 +36,8 @@ from ...brec import MelBase, MelGroup, MreHeaderBase, MelSet, MelString, \
     MelUInt16Flags, perk_effect_key, MelPerkParamsGroups, PerkEpdfDecider, \
     MelUInt32Flags, BipedFlags, MelArmaDnam, MelArmaModels, MelArmaSkins, \
     MelAdditionalRaces, MelFootstepSound, MelArtObject, MelEnchantment, \
-    MelIcons2, MelBids, MelBamt, MelTemplateArmor, MelObjectTemplate
+    MelIcons2, MelBids, MelBamt, MelTemplateArmor, MelObjectTemplate, \
+    MelArtType, MelAspcRdat, MelAspcBnam, MelAstpTitles, MelAstpData
 
 #------------------------------------------------------------------------------
 # Record Elements    ----------------------------------------------------------
@@ -486,6 +487,49 @@ class MreArmo(MelRecord):
             b'FULL': 'ot_combinations',
         },
     })
+    __slots__ = melSet.getSlotsUsed()
+
+#------------------------------------------------------------------------------
+class MreArto(MelRecord):
+    """Art Object."""
+    rec_sig = b'ARTO'
+
+    melSet = MelSet(
+        MelEdid(),
+        MelBounds(),
+        MelPreviewTransform(),
+        MelKeywords(),
+        MelModel(),
+        MelArtType(),
+    )
+    __slots__ = melSet.getSlotsUsed()
+
+#------------------------------------------------------------------------------
+class MreAspc(MelRecord):
+    """Acoustic Space."""
+    rec_sig = b'ASPC'
+
+    melSet = MelSet(
+        MelEdid(),
+        MelBounds(),
+        MelSound(),
+        MelAspcRdat(),
+        MelAspcBnam(),
+        MelUInt8(b'XTRI', 'aspc_is_interior'),
+        MelUInt16(b'WNAM', 'weather_attenuation'),
+    )
+    __slots__ = melSet.getSlotsUsed()
+
+#------------------------------------------------------------------------------
+class MreAstp(MelRecord):
+    """Association Type."""
+    rec_sig = b'ASTP'
+
+    melSet = MelSet(
+        MelEdid(),
+        MelAstpTitles(),
+        MelAstpData(),
+    )
     __slots__ = melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------

Mopy/bash/game/skyrim/records.py
@@ -46,7 +46,8 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelAttx, MelRace, \
     MelEffects, AMelLLItems, MelUnloadEvent, MelShortName, AVmadContext, \
     MelPerkData, MelNextPerk, PerkEpdfDecider, MelPerkParamsGroups, MelBids, \
     MelArmaDnam, MelArmaModels, MelArmaSkins, MelAdditionalRaces, MelBamt, \
-    MelFootstepSound, MelArtObject, MelTemplateArmor
+    MelFootstepSound, MelArtObject, MelTemplateArmor, MelArtType, \
+    MelAspcRdat, MelAspcBnam, MelAstpTitles, MelAstpData
 from ...exception import ModSizeError
 
 _is_sse = bush.game.fsName in (
@@ -594,17 +595,14 @@ class MreArmo(MelRecord):
 
 #------------------------------------------------------------------------------
 class MreArto(MelRecord):
-    """Art Effect Object."""
+    """Art Object."""
     rec_sig = b'ARTO'
-
-    ArtoTypeFlags = Flags.from_names('magic_casting', 'magic_hit_effect',
-                                     'enchantment_effect')
 
     melSet = MelSet(
         MelEdid(),
         MelBounds(),
         MelModel(),
-        MelUInt32Flags(b'DNAM', u'flags', ArtoTypeFlags),
+        MelArtType(),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -612,12 +610,13 @@ class MreArto(MelRecord):
 class MreAspc(MelRecord):
     """Acoustic Space."""
     rec_sig = b'ASPC'
+
     melSet = MelSet(
         MelEdid(),
         MelBounds(),
         MelSound(),
-        MelFid(b'RDAT', 'regionData'),
-        MelFid(b'BNAM', 'reverb'),
+        MelAspcRdat(),
+        MelAspcBnam(),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -626,15 +625,10 @@ class MreAstp(MelRecord):
     """Association Type."""
     rec_sig = b'ASTP'
 
-    _astp_flags = Flags.from_names('family_association')
-
     melSet = MelSet(
         MelEdid(),
-        MelString(b'MPRT', 'male_parent_title'),
-        MelString(b'FPRT', 'female_parent_title'),
-        MelString(b'MCHT', 'male_child_title'),
-        MelString(b'FCHT', 'female_child_title'),
-        MelUInt32Flags(b'DATA', 'association_type_flags', _astp_flags),
+        MelAstpTitles(),
+        MelAstpData(),
     )
     __slots__ = melSet.getSlotsUsed()
 

commit 5bf270705ddaaa2d592b2e681858c1f8e722b667

TES5: Fix regression in Import Sounds
And also patch ASPC\ANAM in FO3 (Environment Type, same as Skyrim).

Broke in df9f844734741d426f8a18fe32cb563d6794ac31.

Readme.md:
Link to all releases

Mopy/bash/bosh/save_headers.py:
Drop iterbytes comment

Seems like that PEP isn't likely to land, which sucks.

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -4901,6 +4901,7 @@ C.Location, -C.LockList
                         <ul>
                             <li>(SNAM) Sound - Looping</li>
                             <li>(RDAT) Use Sound From Region (Interiors Only)</li>
+                            <li>(ANAM) Environment Type (reverb)</li>
                         </ul>
                     </li>
                     <li>(COBJ) Constructible Object:

Mopy/bash/bosh/save_headers.py
@@ -568,7 +568,6 @@ class Fallout4SaveHeader(SkyrimSaveHeader): # pretty similar to skyrim
         # So handle it by concatenating digits until we hit a non-digit char
         def parse_int(gd_bytes: bytes):
             int_data = b''
-            ##: PY3.11: Use iterbytes (PEP 467)
             for i in gd_bytes:
                 c = i.to_bytes(1, sys.byteorder)
                 if c.isdigit():

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -340,7 +340,7 @@ soundsTypes = {
     b'ADDN': ('sound',),
     b'ALCH': (u'dropSound', u'pickupSound', u'soundConsume'),
     b'ARMO': (u'pickupSound', u'dropSound'),
-    b'ASPC': ('sound', 'use_sound_from_region'),
+    b'ASPC': ('sound', 'use_sound_from_region', 'environment_type'),
     b'COBJ': (u'pickupSound', u'dropSound'),
     b'CONT': ('sound', 'sound_close'),
     b'CREA': (u'footWeight', u'inheritsSoundsFrom', u'sounds'),

Mopy/bash/game/skyrim/patcher/__init__.py
@@ -493,7 +493,7 @@ soundsTypes = {
     b'APPA': (u'pickupSound', u'dropSound'),
     b'ARMA': ('footstep_sound',),
     b'ARMO': (u'pickupSound', u'dropSound'),
-    b'ASPC': ('sound', 'use_sound_from_region', 'aspc_reverb'),
+    b'ASPC': ('sound', 'use_sound_from_region', 'environment_type'),
     b'BOOK': (u'pickupSound', u'dropSound'),
     b'CONT': ('sound', 'sound_close'),
     b'DOOR': ('sound', 'sound_close', 'sound_looping'),