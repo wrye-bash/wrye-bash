wrye-bash/wrye-bash

commit 6f6ae195d3972f8a9c8e2e09849b17bba347ac2e

FO4: Implement CONT
This turned into a mini-refactoring round. We now use AMelLLItems for
all games and MelItems has been gained a base class in AMelItems.

Mopy/bash/brec/common_records.py
@@ -42,7 +42,8 @@ from ..bolt import decoder, FName, struct_pack, structs_cache, Flags, \
 # Base classes ----------------------------------------------------------------
 #------------------------------------------------------------------------------
 class AMreWithItems(MelRecord):
-    """Base class for record types that contain a list of items (MelItems)."""
+    """Base class for record types that contain a list of items (see
+    common_subrecords.AMelItems)."""
     __slots__ = []
 
     def mergeFilter(self, modSet):

Mopy/bash/game/fallout3/records.py
@@ -47,7 +47,8 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelReflectedRefractedBy, MelValueWeight, SpellFlags, MelBaseR, MelExtra, \
     MelSound, MelSoundActivation, MelWaterType, MelConditionsFo3, \
     MelNodeIndex, MelAddnDnam, MelEffectsFo3, MelShortName, PerkEpdfDecider, \
-    MelPerkParamsGroups, MelUnorderedGroups, MelImageSpaceMod, MelAspcRdat
+    MelPerkParamsGroups, MelUnorderedGroups, MelImageSpaceMod, MelAspcRdat, \
+    MelSoundClose, AMelItems, AMelLLItems, MelContData
 from ...exception import ModSizeError
 
 _is_fnv = bush.game.fsName == u'FalloutNV'
@@ -216,14 +217,10 @@ class MelEquipmentTypeFo3(MelSInt32):
         super().__init__(b'ETYP', 'equipment_type', -1)
 
 #------------------------------------------------------------------------------
-class MelItems(MelSorted):
-    """Wraps MelGroups for the common task of defining a list of items."""
+class MelItems(AMelItems):
+    """Handles the CNTO/COED subrecords defining items."""
     def __init__(self):
-        super(MelItems, self).__init__(MelGroups(u'items',
-            MelStruct(b'CNTO', [u'I', u'i'], (FID, u'item'), (u'count', 1)),
-            MelOptStruct(b'COED', [u'2I', u'f'], (FID, u'owner'), (FID, u'glob'),
-                         (u'condition', 1.0)),
-        ), sort_by_attrs=('item', 'count', 'condition', 'owner', 'glob'))
+        super().__init__(with_counter=False)
 
 #------------------------------------------------------------------------------
 class MelLevListLvld(MelUInt8):
@@ -250,6 +247,14 @@ class MelLinkedDecals(MelSorted):
         super().__init__(MelGroups('linkedDecals',
             MelStruct(b'XDCR', ['2I'], (FID, 'reference'), 'unknown'),
         ), sort_by_attrs='reference')
+
+#------------------------------------------------------------------------------
+class MelLLItems(AMelLLItems):
+    """Handles the LVLO/COED subrecords defining leveled list entries."""
+    def __init__(self):
+        super().__init__(MelLevListLvlo(b'LVLO', ['h', '2s', 'I', 'h', '2s'],
+            'level', 'unused1', (FID, 'listId'), ('count', 1), 'unused2',
+            old_versions={'iI'}), with_counter=False)
 
 #------------------------------------------------------------------------------
 class MelRaceHeadPart(MelGroup):
@@ -284,6 +289,13 @@ class MelRaceHeadPart(MelGroup):
         super(MelRaceHeadPart, self).dumpData(record, out)
 
 #------------------------------------------------------------------------------
+class MelSoundRandomLooping(MelFid):
+    """Handles the common RNAM (Sound - Random/Looping) subrecord introduced in
+    FNV."""
+    def __init__(self):
+        super().__init__(b'RNAM', 'sound_random_looping')
+
+#------------------------------------------------------------------------------
 class MreLeveledList(AMreLeveledList):
     """Leveled item/creature/spell list.."""
     top_copy_attrs = (u'chanceNone', u'glob')
@@ -295,14 +307,7 @@ class MreLeveledList(AMreLeveledList):
         MelLevListLvld(b'LVLD', u'chanceNone'),
         MelUInt8Flags(b'LVLF', u'flags', AMreLeveledList._flags),
         MelFid(b'LVLG', u'glob'),
-        MelSorted(MelGroups(u'entries',
-            MelLevListLvlo(b'LVLO', [u'h', u'2s', u'I', u'h', u'2s'], u'level',
-                           u'unused1', (FID, u'listId'), (u'count', 1),
-                           u'unused2', old_versions={u'iI'}),
-            MelOptStruct(b'COED', [u'2I', u'f'], (FID, u'owner'), (FID, u'glob'),
-                         (u'condition', 1.0)),
-        ), sort_by_attrs=('level', 'listId', 'count', 'condition', 'owner',
-                          'glob')),
+        MelLLItems(),
         MelModel(),
     )
     __slots__ = melSet.getSlotsUsed()
@@ -824,8 +829,6 @@ class MreCont(AMreWithItems):
     """Container."""
     rec_sig = b'CONT'
 
-    _flags = Flags.from_names(None, 'respawns')
-
     melSet = MelSet(
         MelEdid(),
         MelBounds(),
@@ -834,10 +837,10 @@ class MreCont(AMreWithItems):
         MelScript(),
         MelItems(),
         MelDestructible(),
-        MelStruct(b'DATA', [u'B', u'f'],(_flags, u'flags'),'weight'),
+        MelContData(),
         MelSound(),
-        MelFid(b'QNAM','soundClose'),
-        fnv_only(MelFid(b'RNAM', 'soundRandomLooping')),
+        MelSoundClose(),
+        fnv_only(MelSoundRandomLooping()),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -1065,7 +1068,7 @@ class MreDoor(MelRecord):
         MelScript(),
         MelDestructible(),
         MelSound(),
-        MelFid(b'ANAM','soundClose'),
+        MelSoundClose(b'ANAM'),
         MelFid(b'BNAM','soundLoop'),
         MelUInt8Flags(b'FNAM', u'flags', _flags),
     )
@@ -1627,7 +1630,7 @@ class MreKeym(MelRecord):
         MelDestructible(),
         MelSoundPickupDrop(),
         MelValueWeight(),
-        fnv_only(MelFid(b'RNAM', 'soundRandomLooping')),
+        fnv_only(MelSoundRandomLooping()),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -1830,7 +1833,7 @@ class MreMisc(MelRecord):
         MelDestructible(),
         MelSoundPickupDrop(),
         MelValueWeight(),
-        fnv_only(MelFid(b'RNAM', 'soundRandomLooping')),
+        fnv_only(MelSoundRandomLooping()),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -2878,7 +2881,7 @@ class MreStat(MelRecord):
         MelBounds(),
         MelModel(),
         fnv_only(MelSInt8(b'BRUS', 'passthroughSound', -1)),
-        fnv_only(MelFid(b'RNAM', 'soundRandomLooping')),
+        fnv_only(MelSoundRandomLooping()),
     )
     __slots__ = melSet.getSlotsUsed()
 

Mopy/bash/game/fallout4/records.py
@@ -40,7 +40,7 @@ from ...brec import MelBase, MelGroup, AMreHeader, MelSet, MelString, \
     MelArtType, MelAspcRdat, MelAspcBnam, MelAstpTitles, MelAstpData, \
     MelBookText, MelBookDescription, MelInventoryArt, MelUnorderedGroups, \
     MelImageSpaceMod, MelClmtWeatherTypes, MelClmtTiming, MelClmtTextures, \
-    MelCobjOutput
+    MelCobjOutput, AMreWithItems, AMelItems, MelContData, MelSoundClose
 
 ##: What about texture hashes? I carried discarding them forward from Skyrim,
 # but that was due to the 43-44 problems. See also #620.
@@ -158,12 +158,16 @@ class MelFtyp(MelFid):
         super().__init__(b'FTYP', 'forced_loc_ref_type')
 
 #------------------------------------------------------------------------------
+class MelItems(AMelItems):
+    """Handles the COCT/CNTO/COED subrecords defining items."""
+
+#------------------------------------------------------------------------------
 class MelLLItems(AMelLLItems):
-    """Handles the LVLO and LLCT subrecords defining leveled list items"""
+    """Handles the LLCT/LVLO/COED subrecords defining leveled list entries."""
     def __init__(self):
-        super().__init__([MelStruct(b'LVLO', ['H', '2s', 'I', 'H', 'B', 's'],
+        super().__init__(MelStruct(b'LVLO', ['H', '2s', 'I', 'H', 'B', 's'],
             'level', 'unused1', (FID, 'listId'), ('count', 1), 'chance_none',
-            'unused2')])
+            'unused2'))
 
 #------------------------------------------------------------------------------
 class MelNativeTerminal(MelFid):
@@ -791,6 +795,32 @@ class MreCobj(MelRecord):
     def mergeFilter(self, modSet):
         self.cobj_components = [c for c in self.cobj_components
                                 if c.component_fid.mod_id in modSet]
+
+#------------------------------------------------------------------------------
+class MreCont(AMreWithItems):
+    """Container."""
+    rec_sig = b'CONT'
+
+    melSet = MelSet(
+        MelEdid(),
+        MelVmad(),
+        MelBounds(),
+        MelPreviewTransform(),
+        MelFull(),
+        MelModel(),
+        MelItems(),
+        MelDestructible(),
+        MelContData(),
+        MelKeywords(),
+        MelFtyp(),
+        MelProperties(),
+        MelNativeTerminal(),
+        MelSound(),
+        MelSoundClose(),
+        MelFid(b'TNAM', 'sound_take_all'),
+        MelFid(b'ONAM', 'cont_filter_list'),
+    )
+    __slots__ = melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------
 class MreGmst(AMreGmst):

Mopy/bash/game/oblivion/records.py
@@ -43,7 +43,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, MelString, \
     MelClmtWeatherTypes, MelFactionRanks, MelLscrLocations, attr_csv_struct, \
     MelEnchantment, MelValueWeight, null4, SpellFlags, MelOwnership, \
     MelSound, MelWeight, MelEffectsTes4ObmeFull, MelBookText, MelClmtTiming, \
-    MelClmtTextures
+    MelClmtTextures, MelSoundClose, AMelItems, AMelLLItems, MelContData
 
 #------------------------------------------------------------------------------
 # Record Elements -------------------------------------------------------------
@@ -117,12 +117,10 @@ class MelEmbeddedScript(MelSequential):
         super(MelEmbeddedScript, self).__init__(*seq_elements)
 
 #------------------------------------------------------------------------------
-class MelItems(MelSorted):
-    """Wraps MelGroups for the common task of defining a list of items."""
+class MelItems(AMelItems):
+    """Handles the CNTO subrecords defining items."""
     def __init__(self):
-        super(MelItems, self).__init__(MelGroups(u'items',
-            MelStruct(b'CNTO', [u'I', u'i'], (FID, u'item'), u'count'),
-        ), sort_by_attrs='item')
+        super().__init__(with_coed=False, with_counter=False)
 
 #------------------------------------------------------------------------------
 class MelLevListLvld(MelUInt8):
@@ -145,6 +143,14 @@ class MelLevListLvlo(MelTruncatedStruct):
             # Pad it in the middle, then let our parent deal with the rest
             unpacked_val = (unpacked_val[0], null2, unpacked_val[1])
         return super(MelLevListLvlo, self)._pre_process_unpacked(unpacked_val)
+
+#------------------------------------------------------------------------------
+class MelLLItems(AMelLLItems):
+    """Handles the LVLO subrecords defining leveled list entries."""
+    def __init__(self):
+        super().__init__(MelLevListLvlo(b'LVLO', ['h', '2s', 'I', 'h', '2s'],
+            'level', 'unused1', (FID, 'listId'), ('count', 1), 'unused2',
+            old_versions={'iI'}), with_coed=False, with_counter=False)
 
 #------------------------------------------------------------------------------
 class MelOwnershipTes4(MelOwnership):
@@ -453,12 +459,7 @@ class MreLeveledList(AMreLeveledList):
         MelUInt8Flags(b'LVLF', u'flags', AMreLeveledList._flags),
         MelScript(), # LVLC only
         MelFid(b'TNAM','template'),
-        MelSorted(MelGroups('entries',
-            MelLevListLvlo(b'LVLO', [u'h', u'2s', u'I', u'h', u'2s'],
-                           u'level', u'unused1',
-                           (FID, u'listId'), (u'count', 1),
-                           u'unused2', old_versions={u'iI'}),
-        ), sort_by_attrs=('level', 'listId', 'count')),
+        MelLLItems(),
         MelNull(b'DATA'),
     )
     __slots__ = melSet.getSlotsUsed()
@@ -764,17 +765,15 @@ class MreCont(AMreWithItems):
     """Container."""
     rec_sig = b'CONT'
 
-    _flags = Flags.from_names(None,'respawns')
-
     melSet = MelSet(
         MelEdid(),
         MelFull(),
         MelModel(),
         MelScript(),
         MelItems(),
-        MelStruct(b'DATA', [u'B', u'f'],(_flags, u'flags'),'weight'),
+        MelContData(),
         MelSound(),
-        MelFid(b'QNAM','soundClose'),
+        MelSoundClose(),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -918,7 +917,7 @@ class MreDoor(MelRecord):
         MelModel(),
         MelScript(),
         MelSound(),
-        MelFid(b'ANAM','soundClose'),
+        MelSoundClose(b'ANAM'),
         MelFid(b'BNAM','soundLoop'),
         MelUInt8Flags(b'FNAM', u'flags', _flags),
         MelSorted(MelFids('destinations', MelFid(b'TNAM'))),

Mopy/bash/game/skyrim/records.py
@@ -40,7 +40,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelAttx, MelRace, \
     MelColorO, MelSpells, MelFixedString, MelUInt8Flags, MelUInt16Flags, \
     MelUInt32Flags, MelOwnership, MelDebrData, MelClmtWeatherTypes, AMelVmad, \
     MelActorSounds, MelFactionRanks, MelSorted, MelReflectedRefractedBy, \
-    perk_effect_key, MelValueWeight, MelCoed, MelSound, MelWaterType, \
+    perk_effect_key, MelValueWeight, MelSound, MelWaterType, \
     MelSoundActivation, MelInteractionKeyword, MelConditionList, MelAddnDnam, \
     MelConditions, ANvnmContext, MelNodeIndex, MelEquipmentType, MelAlchEnit, \
     MelEffects, AMelLLItems, MelUnloadEvent, MelShortName, AVmadContext, \
@@ -49,7 +49,8 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelAttx, MelRace, \
     MelFootstepSound, MelArtObject, MelTemplateArmor, MelArtType, \
     MelAspcRdat, MelAspcBnam, MelAstpTitles, MelAstpData, MelBookText, \
     MelBookDescription, MelInventoryArt, MelUnorderedGroups, MelExtra, \
-    MelImageSpaceMod, MelClmtTiming, MelClmtTextures, MelCobjOutput
+    MelImageSpaceMod, MelClmtTiming, MelClmtTextures, MelCobjOutput, \
+    MelSoundClose, AMelItems, MelContData
 from ...exception import ModSizeError
 
 _is_sse = bush.game.fsName in (
@@ -200,19 +201,8 @@ class MelIdleHandler(MelGroup):
         )
 
 #------------------------------------------------------------------------------
-class MelItems(MelSorted):
-    """Wraps MelGroups for the common task of defining a list of items."""
-    def __init__(self):
-        super(MelItems, self).__init__(MelGroups('items',
-            MelStruct(b'CNTO', [u'I', u'i'], (FID, u'item'), u'count'),
-            MelCoed(),
-        ), sort_by_attrs=('item', 'count', 'itemCondition', 'owner', 'glob'))
-
-class MelItemsCounter(MelCounter):
-    """Wraps MelCounter for the common task of defining an items counter."""
-    def __init__(self):
-        super(MelItemsCounter, self).__init__(MelUInt32(b'COCT', 'item_count'),
-                                              counts='items')
+class MelItems(AMelItems):
+    """Handles the COCT/CNTO/COED subrecords defining items."""
 
 #------------------------------------------------------------------------------
 class MelLinkedReferences(MelSorted):
@@ -226,12 +216,11 @@ class MelLinkedReferences(MelSorted):
 
 #------------------------------------------------------------------------------
 class MelLLItems(AMelLLItems):
-    """Handles the LVLO and LLCT subrecords defining leveled list items"""
+    """Handles the LLCT/LVLO/COED subrecords defining leveled list entries."""
     def __init__(self, with_coed=True):
-        super().__init__([
-            MelStruct(b'LVLO', ['H', '2s', 'I', 'H', '2s'], 'level',
-                'unknown1', (FID, 'listId'), ('count', 1), 'unknown2')],
-            with_coed)
+        super().__init__(MelStruct(b'LVLO', ['H', '2s', 'I', 'H', '2s'],
+            'level', 'unknown1', (FID, 'listId'), ('count', 1), 'unknown2'),
+            with_coed=with_coed)
 
 #------------------------------------------------------------------------------
 class MelLocation(MelUnion):
@@ -932,7 +921,6 @@ class MreCobj(AMreWithItems):
 
     melSet = MelSet(
         MelEdid(),
-        MelItemsCounter(),
         MelItems(),
         MelConditionList(),
         MelCobjOutput(),
@@ -945,21 +933,17 @@ class MreCont(AMreWithItems):
     """Container."""
     rec_sig = b'CONT'
 
-    ContTypeFlags = Flags.from_names('allowSoundsWhenAnimation', 'respawns',
-                                     'showOwner')
-
     melSet = MelSet(
         MelEdid(),
         MelVmad(),
         MelBounds(),
         MelFull(),
         MelModel(),
-        MelItemsCounter(),
         MelItems(),
         MelDestructible(),
-        MelStruct(b'DATA', [u'B', u'f'],(ContTypeFlags, u'flags'),'weight'),
+        MelContData(),
         MelSound(),
-        MelFid(b'QNAM','soundClose'),
+        MelSoundClose(),
     )
     __slots__ = melSet.getSlotsUsed()
 
@@ -1106,7 +1090,7 @@ class MreDoor(MelRecord):
         MelModel(),
         MelDestructible(),
         MelSound(),
-        MelFid(b'ANAM','soundClose'),
+        MelSoundClose(b'ANAM'),
         MelFid(b'BNAM','soundLoop'),
         MelUInt8Flags(b'FNAM', u'flags', DoorTypeFlags),
         MelSorted(MelFids('random_teleports', MelFid(b'TNAM'))),
@@ -2498,7 +2482,6 @@ class MreNpc(AMreActor):
         MelSorted(MelGroups('perks',
             MelOptStruct(b'PRKR', [u'I', u'B', u'3s'],(FID, 'perk'),'rank','prkrUnused'),
         ), sort_by_attrs='perk'),
-        MelItemsCounter(),
         MelItems(),
         MelStruct(b'AIDT', [u'B', u'B', u'B', u'B', u'B', u'B', u'B', u'B', u'I', u'I', u'I'], 'aggression', 'confidence',
                   'energyLevel', 'responsibility', 'mood', 'assistance',
@@ -2969,7 +2952,6 @@ class MreQust(MelRecord):
             ),
             MelConditionList(),
             MelKeywords(),
-            MelItemsCounter(),
             MelItems(),
             MelFid(b'SPOR','spectatorOverridePackageList'),
             MelFid(b'OCOR','observeDeadBodyOverridePackageList'),

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -4913,7 +4913,7 @@ C.Location, -C.LockList
                         <ul>
                             <li>(SNAM) Sound - Open</li>
                             <li>(QNAM) Sound - Close</li>
-                            <li>(RNAM) Sound - Looping/Random - <strong>Fallout&colon; New Vegas only</strong></li>
+                            <li>(RNAM) Sound - Random/Looping - <strong>Fallout&colon; New Vegas only</strong></li>
                         </ul>
                     </li>
                     <li>(CREA) Creature:
@@ -6375,7 +6375,7 @@ C.Location, -C.LockList
                     <li>(INAM) Items</li>
                 </ul>
             </td>
-            <td>All but Fallout 4</td>
+            <td>All games</td>
         </tr>
         <tr>
             <td>(CREA) Creature</td>

Mopy/bash/brec/common_subrecords.py
@@ -37,20 +37,51 @@ from ..bolt import Flags, encode, struct_pack, struct_unpack, unpack_byte, \
 from ..exception import ModError, ModSizeError
 
 #------------------------------------------------------------------------------
-class AMelLLItems(MelSequential):
-    """Base class for handling the LVLO and LLCT subrecords defining leveled
-    list items in Skyrim and newer games."""
-    def __init__(self, lvl_elements: list, with_coed=True):
-        final_elements = lvl_elements.copy()
-        final_sort_attrs = ('level', 'listId', 'count')
+class _MelCoed(MelOptStruct):
+    """Handles the COED (Owner Data) subrecord used for inventory items and
+    leveled lists since FO3."""
+    ##: Needs custom unpacker to look at FormID type of owner.  If owner is an
+    # NPC then it is followed by a FormID.  If owner is a faction then it is
+    # followed by an signed integer or '=Iif' instead of '=IIf' - see #282
+    def __init__(self):
+        super().__init__(b'COED', ['2I', 'f'], (FID, 'owner'),
+            (FID, 'glob'), 'itemCondition')
+
+#------------------------------------------------------------------------------
+class AMelItems(MelSequential):
+    """Base class for handling the CNTO (Items) subrecords defining items. Can
+    handle all games since Oblivion via the two kwargs."""
+    def __init__(self, *, with_coed=True, with_counter=True):
+        items_elements = [MelStruct(b'CNTO', ['I', 'i'], (FID, 'item'),
+            'count')]
+        items_sort_attrs = ('item', 'count')
         if with_coed:
-            final_elements.append(MelCoed())
-            final_sort_attrs += ('itemCondition', 'owner', 'glob')
-        super().__init__(
-            MelCounter(MelUInt8(b'LLCT', 'entry_count'), counts='entries'),
-            MelSorted(MelGroups('entries', *final_elements),
-                sort_by_attrs=final_sort_attrs),
-        )
+            items_elements.append(_MelCoed())
+            items_sort_attrs += ('itemCondition', 'owner', 'glob')
+        final_elements = [MelSorted(MelGroups('items', *items_elements),
+            sort_by_attrs=items_sort_attrs)]
+        if with_counter:
+            final_elements.insert(0, MelCounter(
+                MelUInt32(b'COCT', 'item_count'), counts='items'))
+        super().__init__(*final_elements)
+
+#------------------------------------------------------------------------------
+class AMelLLItems(MelSequential):
+    """Base class for handling the LVLO (and LLCT) subrecords defining leveled
+    list items. Can handle all games since Oblivion via the two kwargs."""
+    def __init__(self, lvl_element: MelBase, *, with_coed=True,
+            with_counter=True):
+        lvl_elements = [lvl_element]
+        lvl_sort_attrs = ('level', 'listId', 'count')
+        if with_coed:
+            lvl_elements.append(_MelCoed())
+            lvl_sort_attrs += ('itemCondition', 'owner', 'glob')
+        final_elements = [MelSorted(MelGroups('entries', *lvl_elements),
+            sort_by_attrs=lvl_sort_attrs)]
+        if with_counter:
+            final_elements.insert(0, MelCounter(
+                MelUInt8(b'LLCT', 'entry_count'), counts='entries'))
+        super().__init__(*final_elements)
 
 #------------------------------------------------------------------------------
 class MelActiFlags(MelUInt16Flags):
@@ -307,17 +338,6 @@ class MelCobjOutput(MelSequential):
         )
 
 #------------------------------------------------------------------------------
-class MelCoed(MelOptStruct):
-    """Handles the COED (Owner Data) subrecord used for inventory items and
-    leveled lists since Skyrim."""
-    ##: Needs custom unpacker to look at FormID type of owner.  If owner is an
-    # NPC then it is followed by a FormID.  If owner is a faction then it is
-    # followed by an signed integer or '=Iif' instead of '=IIf' - see #282
-    def __init__(self):
-        super().__init__(b'COED', ['I', 'I', 'f'], (FID, 'owner'),
-            (FID, 'glob'), 'itemCondition')
-
-#------------------------------------------------------------------------------
 class MelColor(MelStruct):
     """Required Color."""
     def __init__(self, color_sig=b'CNAM'):
@@ -340,6 +360,17 @@ class MelColorO(MelOptStruct):
     def __init__(self, color_sig=b'CNAM'):
         super().__init__(color_sig, ['4B'], 'red', 'green', 'blue',
             'unused_alpha')
+
+#------------------------------------------------------------------------------
+class MelContData(MelStruct):
+    """Handles the CONT subrecord DATA (Data)."""
+    # Flags 1 & 3 introduced in Skyrim, treat as unknown for earlier games
+    _cont_flags = Flags.from_names('allow_sounds_when_animation',
+        'cont_respawns', 'show_owner')
+
+    def __init__(self):
+        super().__init__(b'DATA', ['B', 'f'], (self._cont_flags, 'cont_flags'),
+            'cont_weight')
 
 #------------------------------------------------------------------------------
 class MelDebrData(MelStruct):
@@ -503,8 +534,7 @@ class MelInventoryArt(MelFid):
 
 #------------------------------------------------------------------------------
 class MelKeywords(MelSequential):
-    """Wraps MelSequential for the common task of defining a list of keywords
-    and a corresponding counter."""
+    """Handles the KSIZ/KWDA (Keywords) subrecords."""
     def __init__(self):
         super().__init__(
             MelCounter(MelUInt32(b'KSIZ', 'keyword_count'), counts='keywords'),
@@ -865,16 +895,22 @@ class MelSkipInterior(MelUnion):
         }, decider=FlagDecider('flags', ['isInterior']))
 
 #------------------------------------------------------------------------------
-class MelSoundActivation(MelFid):
-    """Handles the VNAM (Sound - Activation) subrecord in ACTI records."""
-    def __init__(self):
-        super().__init__(b'VNAM', 'soundActivation')
-
-#------------------------------------------------------------------------------
 class MelSound(MelFid):
     """Handles the common SNAM (Sound) subrecord."""
     def __init__(self):
         super().__init__(b'SNAM', 'sound')
+
+#------------------------------------------------------------------------------
+class MelSoundActivation(MelFid):
+    """Handles the ACTI subrecord VNAM (Sound - Activation)."""
+    def __init__(self):
+        super().__init__(b'VNAM', 'soundActivation')
+
+#------------------------------------------------------------------------------
+class MelSoundClose(MelFid):
+    """Handles the CONT/DOOR subrecord QNAM/ANAM (Sound - Close)."""
+    def __init__(self, sc_sig=b'QNAM'):
+        super().__init__(sc_sig, 'sound_close')
 
 #------------------------------------------------------------------------------
 class MelSoundPickupDrop(MelSequential):

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -342,9 +342,9 @@ soundsTypes = {
     b'ARMO': (u'pickupSound', u'dropSound'),
     b'ASPC': ('sound', 'use_sound_from_region'),
     b'COBJ': (u'pickupSound', u'dropSound'),
-    b'CONT': ('sound', 'soundClose'),
+    b'CONT': ('sound', 'sound_close'),
     b'CREA': (u'footWeight', u'inheritsSoundsFrom', u'sounds'),
-    b'DOOR': ('sound', 'soundClose', 'soundLoop'),
+    b'DOOR': ('sound', 'sound_close', 'soundLoop'),
     b'EXPL': (u'soundLevel', u'sound1', u'sound2'),
     b'IPCT': ('soundLevel', 'sound', 'sound2'),
     b'LIGH': (u'sound',),

commit b8cd304201ced1aa86113c44a790702046696581

FO3: Fix regression in Leveled Lists patcher
Broke in 6f6ae195d3972f8a9c8e2e09849b17bba347ac2e.

The rest is some minor py3 cleanup on the related attrs.

Mopy/bash/brec/common_records.py
@@ -266,7 +266,7 @@ class AMreLeveledList(MelRecord):
                                    u'useAllSpells', u'specialLoot')
     top_copy_attrs = ()
     # TODO(inf) Only overriden for FO3/FNV right now - Skyrim/FO4?
-    entry_copy_attrs = (u'listId', u'level', u'count')
+    entry_copy_attrs = ('listId', 'level', 'count')
     __slots__ = ('mergeOverLast', 'mergeSources', 'items', 'de_records',
                  're_records')
                 # + ['flags', 'entries'] # define those in the subclasses

Mopy/bash/game/fallout3/records.py
@@ -299,8 +299,8 @@ class MelSoundRandomLooping(MelFid):
 #------------------------------------------------------------------------------
 class MreLeveledList(AMreLeveledList):
     """Leveled item/creature/spell list.."""
-    top_copy_attrs = (u'chanceNone', u'glob')
-    entry_copy_attrs = (u'listId', u'level', u'count', u'owner', u'condition')
+    top_copy_attrs = ('chanceNone', 'glob')
+    entry_copy_attrs = ('listId', 'level', 'count', 'owner', 'itemCondition')
 
     melSet = MelSet(
         MelEdid(),

Mopy/bash/game/fallout4/records.py
@@ -1375,8 +1375,8 @@ class MreLvli(AMreLeveledList):
     """Leveled Item."""
     rec_sig = b'LVLI'
 
-    top_copy_attrs = ('chanceNone','maxCount','glob','filterKeywordChances',
-                      'epicLootChance','overrideName')
+    top_copy_attrs = ('chanceNone', 'maxCount', 'glob', 'filterKeywordChances',
+                      'epicLootChance', 'overrideName')
 
     melSet = MelSet(
         MelEdid(),
@@ -1399,8 +1399,8 @@ class MreLvln(AMreLeveledList):
     """Leveled NPC."""
     rec_sig = b'LVLN'
 
-    top_copy_attrs = ('chanceNone','maxCount','glob','filterKeywordChances',
-                      'model','modt_p')
+    top_copy_attrs = ('chanceNone', 'maxCount', 'glob', 'filterKeywordChances',
+                      'model', 'modt_p')
 
     melSet = MelSet(
         MelEdid(),

Mopy/bash/game/morrowind/records.py
@@ -216,8 +216,8 @@ class MreLeveledList(AMreLeveledList):
         u'calcFromAllLevels',
         u'calcForEachItem', # LEVI only, but will be ignored for LEVC so fine
     )
-    top_copy_attrs = (u'chanceNone',)
-    entry_copy_attrs = (u'listId', u'level') # no count
+    top_copy_attrs = ('chanceNone',)
+    entry_copy_attrs = ('listId', 'level') # no count
 
     # Bad names to mirror the other games (needed by AMreLeveledList)
     melSet = MelSet(

Mopy/bash/game/oblivion/records.py
@@ -453,7 +453,7 @@ class MreHasEffects(object):
 #------------------------------------------------------------------------------
 class MreLeveledList(AMreLeveledList):
     """Leveled item/creature/spell list."""
-    top_copy_attrs = ('script_fid','template','chanceNone',)
+    top_copy_attrs = ('script_fid', 'template', 'chanceNone',)
 
     melSet = MelSet(
         MelEdid(),

Mopy/bash/game/skyrim/records.py
@@ -1930,7 +1930,7 @@ class MreLtex(MelRecord):
 class MreLvli(AMreLeveledList):
     """Leveled Item."""
     rec_sig = b'LVLI'
-    top_copy_attrs = ('chanceNone','glob',)
+    top_copy_attrs = ('chanceNone', 'glob')
 
     melSet = MelSet(
         MelEdid(),
@@ -1946,7 +1946,7 @@ class MreLvli(AMreLeveledList):
 class MreLvln(AMreLeveledList):
     """Leveled NPC."""
     rec_sig = b'LVLN'
-    top_copy_attrs = ('chanceNone','model','modt_p',)
+    top_copy_attrs = ('chanceNone', 'model', 'modt_p')
 
     melSet = MelSet(
         MelEdid(),