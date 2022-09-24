wrye-bash/wrye-bash

commit 6c9bb11f5598d77f6fbc7293181aff584b206818

FO4: Implement ENCH
Plus some refactoring and renaming around ENCH.

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -773,7 +773,8 @@ relations_attrs = (u'faction', u'mod', u'group_combat_reaction')
 #------------------------------------------------------------------------------
 # Import Enchantment Stats
 #------------------------------------------------------------------------------
-ench_stats_attrs = (u'itemType', u'chargeAmount', u'enchantCost', u'flags')
+ench_stats_attrs = ('item_type', 'charge_amount', 'enchantment_cost',
+                    'enit_flags')
 
 #------------------------------------------------------------------------------
 # Import Effect Stats

Mopy/bash/game/fallout3/records.py
@@ -1150,17 +1150,17 @@ class MreEnch(MelRecord):
     """Object Effect."""
     rec_sig = b'ENCH'
 
-    _flags = Flags.from_names(
-        (0, 'noAutoCalc'),
-        fnv_only((1, 'autoCalculate')),
-        (2, 'hideEffect')
+    _enit_flags = Flags.from_names(
+        'ench_no_auto_calc',
+        fnv_only('auto_calculate'),
+        'hide_effect',
     )
 
     melSet = MelSet(
         MelEdid(),
         MelFull(),
-        MelStruct(b'ENIT', [u'3I', u'B', u'3s'],'itemType','chargeAmount','enchantCost',
-                  (_flags, u'flags'),'unused1'),
+        MelStruct(b'ENIT', ['3I', 'B', '3s'], 'item_type', 'charge_amount',
+            'enchantment_cost', (_enit_flags, u'flags'),'unused1'),
         MelEffectsFo3(),
     )
     __slots__ = melSet.getSlotsUsed()

Mopy/bash/game/fallout4/records.py
@@ -1111,6 +1111,29 @@ class MreEfsh(MelRecord):
     __slots__ = melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------
+class MreEnch(MelRecord):
+    """Object Effect."""
+    rec_sig = b'ENCH'
+
+    _enit_flags = Flags.from_names(
+        (0, 'ench_no_auto_calc'),
+        (2, 'extend_duration_on_recast'),
+    )
+
+    melSet = MelSet(
+        MelEdid(),
+        MelBounds(),
+        MelFull(),
+        MelStruct(b'ENIT', ['i', '2I', 'i', '2I', 'f', '2I'],
+            'enchantment_cost', (_enit_flags, 'enit_flags'), 'cast_type',
+            'enchantment_amount', 'enchantment_target_type',
+            'enchantment_type', 'charge_time', (FID, 'base_enchantment'),
+            (FID, 'worn_restrictions')),
+        MelEffects(),
+    )
+    __slots__ = melSet.getSlotsUsed()
+
+#------------------------------------------------------------------------------
 class MreGmst(AMreGmst):
     """Game Setting."""
     isKeyedByEid = True # NULL fids are acceptable.

Mopy/bash/game/skyrim/records.py
@@ -292,8 +292,8 @@ class MelSpit(MelStruct):
 
     def __init__(self):
         super().__init__(b'SPIT', ['3I', 'f', '2I', '2f', 'I'], 'cost',
-            (self.spit_flags, 'dataFlags'), 'spellType', 'chargeTime',
-            'castType', 'targetType', 'castDuration', 'range',
+            (self.spit_flags, 'dataFlags'), 'spellType', 'charge_time',
+            'cast_type', 'spell_target_type', 'castDuration', 'range',
             (FID, 'halfCostPerk'))
 
 #------------------------------------------------------------------------------
@@ -572,7 +572,7 @@ class MreArmo(MelRecord):
         MelBounds(),
         MelFull(),
         MelEnchantment(),
-        MelSInt16(b'EAMT', 'enchantmentAmount'),
+        MelSInt16(b'EAMT', 'enchantment_amount'),
         MelModel(b'MOD2', 'maleWorld'),
         MelIcons('maleIconPath', 'maleSmallIconPath'),
         MelModel(b'MOD4', 'femaleWorld'),
@@ -1179,19 +1179,20 @@ class MreEnch(MelRecord):
     """Object Effect."""
     rec_sig = b'ENCH'
 
-    EnchGeneralFlags = Flags.from_names('noAutoCalc', 'unknownTwo',
-                                        'extendDurationOnRecast')
+    _enit_flags = Flags.from_names(
+        (0, 'ench_no_auto_calc'),
+        (2, 'extend_duration_on_recast'),
+    )
 
     melSet = MelSet(
         MelEdid(),
         MelBounds(),
         MelFull(),
-        MelTruncatedStruct(b'ENIT', [u'i', u'2I', u'i', u'2I', u'f', u'2I'], 'enchantmentCost',
-                           (EnchGeneralFlags, u'generalFlags'), 'castType',
-                           'enchantmentAmount', 'targetType', 'enchantType',
-                           'chargeTime', (FID, 'baseEnchantment'),
-                           (FID, 'wornRestrictions'),
-                           old_versions={'i2Ii2IfI'}),
+        MelTruncatedStruct(b'ENIT', ['i', '2I', 'i', '2I', 'f', '2I'],
+            'enchantment_cost', (_enit_flags, 'enit_flags'), 'cast_type',
+            'enchantment_amount', 'enchantment_target_type',
+            'enchantment_type', 'charge_time', (FID, 'base_enchantment'),
+            (FID, 'worn_restrictions'), old_versions={'i2Ii2IfI'}),
         MelEffects(),
     )
     __slots__ = melSet.getSlotsUsed()
@@ -2559,13 +2560,13 @@ class MrePack(MelRecord):
     melSet = MelSet(
         MelEdid(),
         MelVmad(),
-        MelStruct(b'PKDT', [u'I', u'3B', u's', u'H', u'2s'], (_GeneralFlags, u'generalFlags'),
-                  'package_type', 'interruptOverride', 'preferredSpeed',
-                  'unknown1', (_InterruptFlags, u'interruptFlags'),
-                  'unknown2'),
-        MelStruct(b'PSDT', [u'2b', u'B', u'2b', u'3s', u'i'], 'schedule_month', 'schedule_day',
-                  'schedule_date', 'schedule_hour', 'schedule_minute',
-                  'unused1', 'schedule_duration'),
+        MelStruct(b'PKDT', ['I', '3B', 's', 'H', '2s'],
+            (_GeneralFlags, 'pack_flags'), 'package_type',
+            'interruptOverride', 'preferredSpeed', 'unknown1',
+            (_InterruptFlags, 'interruptFlags'), 'unknown2'),
+        MelStruct(b'PSDT', ['2b', 'B', '2b', '3s', 'i'], 'schedule_month',
+            'schedule_day', 'schedule_date', 'schedule_hour',
+            'schedule_minute', 'unused1', 'schedule_duration'),
         MelConditionList(),
         MelGroup('idleAnimations',
             MelUInt8(b'IDLF', u'animation_flags'),

Mopy/bash/game/fallout4/__init__.py
@@ -223,7 +223,7 @@ class Fallout4GameInfo(PatchGame):
             MreAnio, MreAoru, MreArma, MreArmo, MreArto, MreAstp, MreAvif, \
             MreBnds, MreBook, MreBptd, MreCams, MreClas, MreClfm, MreClmt, \
             MreCmpo, MreCobj, MreCont, MreCpth, MreCsty, MreDfob, MreDmgt, \
-            MreDobj, MreDoor, MreDual, MreEczn, MreEfsh, \
+            MreDobj, MreDoor, MreDual, MreEczn, MreEfsh, MreEnch, \
             MreGmst, MreLvli, MreLvln, MrePerk, MreTes4
         cls.mergeable_sigs = {clazz.rec_sig: clazz for clazz in (
             MreAact, MreActi, MreAddn, MreAech, MreAmdl, MreAnio, MreAoru,
@@ -231,6 +231,7 @@ class Fallout4GameInfo(PatchGame):
             MreBptd, MreCams, MreClas, MreClfm, MreClmt, MreCmpo, MreCobj,
             MreColl, MreCont, MreCpth, MreCsty, MreDebr, MreDfob, MreDlbr,
             MreDlvw, MreDmgt, MreDobj, MreDoor, MreDual, MreEczn, MreEfsh,
+            MreEnch,
             MreGmst, MreLvli, MreLvln, MrePerk,
         )}
         # Setting RecordHeader class variables --------------------------------
@@ -272,6 +273,7 @@ class Fallout4GameInfo(PatchGame):
             MreBptd, MreCams, MreClas, MreClfm, MreClmt, MreCmpo, MreCobj,
             MreColl, MreCont, MreCpth, MreCsty, MreDebr, MreDfob, MreDlbr,
             MreDlvw, MreDmgt, MreDobj, MreDoor, MreDual, MreEczn, MreEfsh,
+            MreEnch,
             MreGmst, MreLvli, MreLvln, MrePerk, MreTes4,
         )}
         brec.MreRecord.simpleTypes = (

Mopy/bash/game/oblivion/patcher/__init__.py
@@ -754,7 +754,8 @@ relations_attrs = (u'faction', u'mod') ##: mod?
 #------------------------------------------------------------------------------
 # Import Enchantment Stats
 #------------------------------------------------------------------------------
-ench_stats_attrs = (u'itemType', u'chargeAmount', u'enchantCost', u'flags')
+ench_stats_attrs = ('item_type', 'charge_amount', 'enchantment_cost',
+                    'enit_flags')
 
 #------------------------------------------------------------------------------
 # Import Effect Stats

Mopy/bash/game/oblivion/records.py
@@ -980,14 +980,14 @@ class MreEnch(MreHasEffects, MelRecord):
     """Enchantment."""
     rec_sig = b'ENCH'
 
-    _flags = Flags.from_names('noAutoCalc')
+    _enit_flags = Flags.from_names('ench_no_auto_calc')
 
     melSet = MelSet(
         MelEdid(),
         MelObme(),
         MelFull(), #--At least one mod has this. Odd.
-        MelStruct(b'ENIT', [u'3I', u'B', u'3s'], 'itemType', 'chargeAmount', 'enchantCost',
-                  (_flags, u'flags'), 'unused1'),
+        MelStruct(b'ENIT', ['3I', 'B', '3s'], 'item_type', 'charge_amount',
+            'enchantment_cost', (_enit_flags, 'enit_flags'), 'unused1'),
         MelEffectsTes4(),
         MelEffectsTes4ObmeFull(),
     ).with_distributor(_effects_distributor)

Mopy/bash/game/skyrim/patcher/__init__.py
@@ -818,9 +818,9 @@ spell_types = (b'LVSP', b'SPEL')
 #------------------------------------------------------------------------------
 # Import Spell Stats
 #------------------------------------------------------------------------------
-spell_stats_attrs = (u'eid', u'cost', u'spellType', u'chargeTime', u'castType',
-                     u'targetType', u'castDuration', u'range', u'halfCostPerk',
-                     u'dataFlags')
+spell_stats_attrs = ('eid', u'cost', 'spellType', 'charge_time', 'cast_type',
+                     'spell_target_type', 'castDuration', 'range',
+                     'halfCostPerk', 'dataFlags')
 spell_stats_types = {b'SCRL', b'SPEL'}
 
 #------------------------------------------------------------------------------
@@ -1035,9 +1035,10 @@ relations_attrs = (u'faction', u'mod', u'group_combat_reaction')
 #------------------------------------------------------------------------------
 # Import Enchantment Stats
 #------------------------------------------------------------------------------
-ench_stats_attrs = (u'enchantmentCost', u'generalFlags', u'castType',
-                    u'enchantmentAmount', u'targetType', u'enchantType',
-                    u'chargeTime', u'baseEnchantment', u'wornRestrictions')
+ench_stats_attrs = ('enchantment_cost', 'enit_flags', 'cast_type',
+                    'enchantment_amount', 'enchantment_target_type',
+                    'enchantment_type', 'charge_time', 'base_enchantment',
+                    'worn_restrictions')
 
 #------------------------------------------------------------------------------
 # Import Effect Stats

commit 4df45c4d174ed7fa865ff3520c7ee27c9f604b03

Fix a handful of 525 regressions
FO3: Fix regression in ENCH

Broke in 6c9bb11f5598d77f6fbc7293181aff584b206818.

FO3: Fix regression in PERK

Broke in 9baad453fc0e8cd13e52f7684ea11db862cafdbf.

Also refactor the shared distributor config.

FO3: Fix regression in EFSH

Broke in 8b87bc890be1ca27bb3fd28322e7f8ce265bb069.

Mopy/bash/brec/utils_constants.py
@@ -359,3 +359,14 @@ def get_structs(struct_format):
     tuple."""
     _struct = structs_cache[struct_format]
     return _struct.unpack, _struct.pack, _struct.size
+
+# Shared distributor for PERK records
+perk_distributor = {
+    b'DESC': {
+        b'CTDA|CIS1|CIS2': 'conditions',
+        b'DATA': 'perk_trait',
+    },
+    b'PRKE': {
+        b'CTDA|CIS1|CIS2|DATA': 'perk_effects',
+    },
+}

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -419,7 +419,7 @@ graphicsTypes = {
               'fill_color1_blue', 'fill_alpha_fade_in_time',
               'fill_full_alpha_time', 'fill_alpha_fade_out_time',
               'fill_persistent_alpha_ratio', 'fill_alpha_pulse_amplitude',
-              'fill_alpha_pulse_frequency', 'fill_texture_animation_speed_',
+              'fill_alpha_pulse_frequency', 'fill_texture_animation_speed_u',
               'fill_texture_animation_speed_v', 'ee_fall_off', 'ee_color_red',
               'ee_color_green', 'ee_color_blue', 'ee_alpha_fade_in_time',
               'ee_full_alpha_time', 'ee_alpha_fade_out_time',

Mopy/bash/game/fallout3/records.py
@@ -50,7 +50,8 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelPerkParamsGroups, MelUnorderedGroups, MelImageSpaceMod, MelAspcRdat, \
     MelSoundClose, AMelItems, AMelLLItems, MelContData, MelCpthShared, \
     MelSoundLooping, MelHairFlags, MelImpactDataset, MelFlstFids, MelObject, \
-    MelTxstFlags, MelGrasData, MelIdlmFlags, MelIdlmIdla, AMreImad
+    MelTxstFlags, MelGrasData, MelIdlmFlags, MelIdlmIdla, AMreImad,\
+    perk_distributor
 from ...exception import ModSizeError
 
 _is_fnv = bush.game.fsName == u'FalloutNV'
@@ -1161,7 +1162,7 @@ class MreEnch(MelRecord):
         MelEdid(),
         MelFull(),
         MelStruct(b'ENIT', ['3I', 'B', '3s'], 'item_type', 'charge_amount',
-            'enchantment_cost', (_enit_flags, u'flags'),'unused1'),
+            'enchantment_cost', (_enit_flags, 'enit_flags'), 'unused1'),
         MelEffectsFo3(),
     )
     __slots__ = melSet.getSlotsUsed()
@@ -2121,15 +2122,7 @@ class MrePerk(MelRecord):
             ),
             MelBaseR(b'PRKF', 'pe_end_marker'),
         ), sort_special=perk_effect_key),
-    ).with_distributor({
-        b'DESC': {
-            b'CTDA|CIS1|CIS2': 'conditions',
-            b'DATA': 'perk_trait',
-        },
-        b'PRKE': {
-            b'CTDA|CIS1|CIS2|DATA': 'effects',
-        },
-    })
+    ).with_distributor(perk_distributor)
     __slots__ = melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------

Mopy/bash/game/fallout4/records.py
@@ -48,7 +48,7 @@ from ...brec import MelBase, MelGroup, AMreHeader, MelSet, MelString, \
     MelFactFids, MelFactVendorInfo, MelReadOnly, MelFurnMarkerData, \
     MelGrasData, MelHdptShared, MelIdleEnam, MelIdleRelatedAnims, \
     MelIdleData, MelCounter, MelIdleTimerSetting, MelIdlmFlags, MelIdlmIdla, \
-    AMreImad, MelPartialCounter
+    AMreImad, MelPartialCounter, perk_distributor
 
 ##: What about texture hashes? I carried discarding them forward from Skyrim,
 # but that was due to the 43-44 problems. See also #620.
@@ -1606,13 +1606,5 @@ class MrePerk(MelRecord):
             ),
             MelBaseR(b'PRKF', 'pe_end_marker'),
         ), sort_special=perk_effect_key),
-    ).with_distributor({
-        b'DESC': {
-            b'CTDA|CIS1|CIS2': 'conditions',
-            b'DATA': 'perk_trait',
-        },
-        b'PRKE': {
-            b'CTDA|CIS1|CIS2|DATA': 'perk_effects',
-        },
-    })
+    ).with_distributor(perk_distributor)
     __slots__ = melSet.getSlotsUsed()

Mopy/bash/game/skyrim/records.py
@@ -53,7 +53,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelAttx, MelRace, \
     MelRandomTeleports, MelSoundLooping, MelEqupPnam, MelFactVendorInfo, \
     MelFactFlags, MelFactFids, MelSeasons, MelIngredient, MelFurnMarkerData, \
     MelHdptShared, MelIdleEnam, MelIdleRelatedAnims, MelIdleData, \
-    MelIdlmFlags
+    MelIdlmFlags, perk_distributor
 from ...exception import ModSizeError
 
 _is_sse = bush.game.fsName in (
@@ -2485,15 +2485,7 @@ class MrePerk(MelRecord):
             ),
             MelBaseR(b'PRKF', 'pe_end_marker'),
         ), sort_special=perk_effect_key),
-    ).with_distributor({
-        b'DESC': {
-            b'CTDA|CIS1|CIS2': 'conditions',
-            b'DATA': 'perk_trait',
-        },
-        b'PRKE': {
-            b'CTDA|CIS1|CIS2|DATA': 'perk_effects',
-        },
-    })
+    ).with_distributor(perk_distributor)
     __slots__ = melSet.getSlotsUsed()
 
 #------------------------------------------------------------------------------