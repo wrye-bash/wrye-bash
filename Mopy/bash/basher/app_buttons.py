wrye-bash/wrye-bash

commit 3a9aeca21f3ae56412b71e5e364442aeb1820e26

TES5: Add Races Have All Head Parts tweak
Requested on AFKMods. The custom choice is necessary, but somewhat
obscure - you just have to read the docs to figure it out.

Mopy/bash/patcher/patchers/multitweak_races.py
@@ -25,8 +25,9 @@
 from collections import defaultdict
 from typing import Union, Optional
 
-from .base import MultiTweakItem, MultiTweaker
-from ... import bush
+from .base import MultiTweakItem, MultiTweaker, IndexingTweak, \
+    CustomChoiceTweak
+from ... import bush, load_order
 from ...bolt import attrgetter_cache, Path
 
 _vanilla_races = [u'argonian', u'breton', u'dremora', u'dark elf',
@@ -216,14 +217,70 @@ class _ARUnblockTweak(_ARaceTweak):
         setattr(record, race_attr, self.tweak_races_data[race_sig])
 
 # -----------------------------------------------------------------------------
-class RaceTweak_AllHairs(_ARUnblockTweak):
+class RaceTweak_AllHairs(_ARaceTweak):
     """Gives all races ALL hairs."""
     tweak_read_classes = b'HAIR', b'RACE',
-    tweak_name = _(u'Races Have All Hairs')
-    tweak_tip = _(u'Gives all races every available hair.')
-    tweak_key = u'hairyraces'
-    tweak_choices = [(u'get down tonight', 1)]
-    _sig_and_attr = (b'HAIR', u'hairs')
+    tweak_name = _('Races Have All Hairs')
+    tweak_tip = _('Gives all races every available hair.')
+    tweak_key = 'hairyraces'
+    tweak_choices = [('get down tonight', 1)]
+    _sig_and_attr = (b'HAIR', 'hairs')
+
+class RaceTweak_AllHeadParts(IndexingTweak, CustomChoiceTweak):
+    ##: We *only* need FLST here for the create_record call down below
+    tweak_read_classes = b'FLST', b'HDPT',
+    tweak_name = _('Races Have All Head Parts')
+    tweak_tip = _('Gives all races every available head part.')
+    tweak_key = 'all_head_parts'
+    tweak_choices = [(_('Hair Only'), '3'),
+                     (_('Eyes Only'), '2'),
+                     (_('Eyes and Hair Only'), '23'),
+                     (_('All Parts'), '0123456')]
+    tweak_log_msg = _('Head Parts Tweaked: %(total_changed)d')
+    tweak_order = 11 # After playable tweaks
+    _index_sigs = [b'FLST', b'RACE']
+    _import_from_master = [bush.game.master_fid(f) for f in (
+        0x0A803F, # HeadPartsAllRacesMinusBeast
+        0x0A8039, # HeadPartsArgonianandVampire
+        0x0A8036, # HeadPartsKhajiitandVampire
+    )]
+
+    def validate_values(self, chosen_values: tuple) -> str | None:
+        for c in chosen_values[0]:
+            if c not in '0123456':
+                return _('Only numbers from 0-6 are allowed.')
+        if len(set(chosen_values[0])) != len(chosen_values[0]):
+            return _('Contains duplicate numbers.')
+
+    @property
+    def _chosen_parts(self):
+        return self.choiceValues[self.chosen][0]
+
+    def prepare_for_tweaking(self, patch_file):
+        super().prepare_for_tweaking(patch_file)
+        pr_flst = patch_file.create_record(b'FLST')
+        pr_flst.eid = 'BP_HeadPartsAllRaces'
+        self._playable_races_flst_fid = pr_flst.fid
+        all_race_fids = set()
+        # Start out with the ones from Skyrim.esm
+        for flst_fid in self._import_from_master:
+            flst_rec = self._indexed_records[b'FLST'][flst_fid]
+            all_race_fids |= set(flst_rec.formIDInList)
+        # Then merge in mod-added playable races
+        for race_fid, race_rec in self._indexed_records[b'RACE'].items():
+            if not race_rec.data_flags_1.playable: continue
+            all_race_fids.add(race_fid)
+        # Sort the result by final load order
+        pr_flst.formIDInList = sorted(all_race_fids,
+            key=lambda r: (load_order.cached_lo_index(r.mod_fn), r.object_dex))
+
+    def wants_record(self, record):
+        return (record._rec_sig == b'HDPT' and
+                record.flags.playable and
+                str(record.hdpt_type) in self._chosen_parts)
+
+    def tweak_record(self, record):
+        record.validRaces = self._playable_races_flst_fid
 
 # -----------------------------------------------------------------------------
 class RaceTweak_AllEyes(_ARUnblockTweak):

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -7620,6 +7620,58 @@ noted in the description of the tweak.</p>
                 Fallout&colon; New Vegas</td>
         </tr>
         <tr>
+            <td>Races Have All Head Parts</td>
+            <td>Gives all races all head parts. You have various options for
+                which types of head parts you want to target with this tweak.
+                Additionally, it is also possible to make a custom choice. For
+                the custom choice, enter a string of numbers (without spaces
+                separating them), where each number corresponds to one of the
+                following:
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Head Part Type</th>
+                            <th>Number</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Misc</td>
+                            <td>0</td>
+                        </tr>
+                        <tr>
+                            <td>Face</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>Eyes</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>Hair</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>Facial Hair</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>Scar</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>Eyebrows</td>
+                            <td>6</td>
+                        </tr>
+                    </tbody>
+                </table>
+                For example, if you wanted to make all facial hair and all
+                scars available to all races, you would enter <code>45</code>
+                as the custom tweak value.</td>
+            <td>Skyrim, Enderal&colon; Forgotten Stories &amp; Skyrim&colon;
+                Special Edition</td>
+        </tr>
+        <tr>
             <td>Genderless Hairs</td>
             <td>Sets all hairs to be useable regardless of gender.</td>
             <td>Oblivion, Nehrim&colon; At Fate's Edge, Fallout 3 &amp;

Mopy/bash/game/fallout3/patcher/__init__.py
@@ -819,14 +819,14 @@ enchantment_types = {b'ARMO', b'CREA', b'EXPL', b'NPC_', b'WEAP'}
 # Tweak Races
 #------------------------------------------------------------------------------
 race_tweaks = {
-    u'RaceTweak_PlayableHeadParts',
-    u'RaceTweak_MergeSimilarRaceHairs',
-    u'RaceTweak_MergeSimilarRaceEyes',
-    u'RaceTweak_PlayableEyes',
-    u'RaceTweak_PlayableHairs',
-    u'RaceTweak_GenderlessHairs',
-    u'RaceTweak_AllEyes',
-    u'RaceTweak_AllHairs',
+    'RaceTweak_PlayableHeadParts',
+    'RaceTweak_MergeSimilarRaceHairs',
+    'RaceTweak_MergeSimilarRaceEyes',
+    'RaceTweak_PlayableEyes',
+    'RaceTweak_PlayableHairs',
+    'RaceTweak_GenderlessHairs',
+    'RaceTweak_AllEyes',
+    'RaceTweak_AllHairs',
 }
 race_tweaks_need_collection = True
 

Mopy/bash/game/oblivion/patcher/__init__.py
@@ -848,14 +848,14 @@ enchantment_types = {b'AMMO', b'ARMO', b'BOOK', b'CLOT', b'WEAP'}
 # Tweak Races
 #------------------------------------------------------------------------------
 race_tweaks = {
-    u'RaceTweak_BiggerOrcsAndNords',
-    u'RaceTweak_MergeSimilarRaceHairs',
-    u'RaceTweak_MergeSimilarRaceEyes',
-    u'RaceTweak_PlayableEyes',
-    u'RaceTweak_PlayableHairs',
-    u'RaceTweak_GenderlessHairs',
-    u'RaceTweak_AllEyes',
-    u'RaceTweak_AllHairs',
+    'RaceTweak_BiggerOrcsAndNords',
+    'RaceTweak_MergeSimilarRaceHairs',
+    'RaceTweak_MergeSimilarRaceEyes',
+    'RaceTweak_PlayableEyes',
+    'RaceTweak_PlayableHairs',
+    'RaceTweak_GenderlessHairs',
+    'RaceTweak_AllEyes',
+    'RaceTweak_AllHairs',
 }
 race_tweaks_need_collection = True
 

Mopy/bash/game/skyrim/patcher/__init__.py
@@ -1081,10 +1081,11 @@ enchantment_types = {b'ARMO', b'EXPL', b'WEAP'}
 # Tweak Races
 #------------------------------------------------------------------------------
 race_tweaks = {
-    u'RaceTweak_PlayableHeadParts',
-    u'RaceTweak_GenderlessHeadParts',
-    u'RaceTweak_ForceBehaviorGraphGender_Female',
-    u'RaceTweak_ForceBehaviorGraphGender_Male',
+    'RaceTweak_PlayableHeadParts',
+    'RaceTweak_GenderlessHeadParts',
+    'RaceTweak_ForceBehaviorGraphGender_Female',
+    'RaceTweak_ForceBehaviorGraphGender_Male',
+    'RaceTweak_AllHeadParts',
 }
 
 #------------------------------------------------------------------------------

Mopy/bash/patcher/patchers/base.py
@@ -84,7 +84,7 @@ class MultiTweakItem(AMultiTweakItem):
 # a patcher/tweak wants it to index all records for certain record types in
 # some central place (and NOT by forwarding all records into the BP!)
 class IndexingTweak(MultiTweakItem):
-    _index_sigs = []
+    _index_sigs: list[bytes]
 
     def __init__(self):
         super(IndexingTweak, self).__init__()

commit c5b3c1869afdeae6ec71d21a8422fef42e1fccd3

Fix regression in Races Have All Hairs
Broke in 3a9aeca21f3ae56412b71e5e364442aeb1820e26.

Mopy/bash/basher/app_buttons.py:
Fix launching of elevated third-party apps

E.g. Construction Set Extender. Current handling was entirely broken.

Mopy/bash/basher/app_buttons.py
@@ -21,8 +21,10 @@
 #
 # =============================================================================
 import os
+import shlex
 import subprocess
 import webbrowser
+
 from . import BashStatusBar, BashFrame
 from .frames import PluginChecker, DocBrowser
 from .settings_dialog import SettingsDialog
@@ -265,7 +267,7 @@ class _ExeButton(_App_Button):
             try:
                 import win32api
                 win32api.ShellExecute(0, 'runas', exe_path.s,
-                    f'{self.exeArgs}', bass.dirs['app'].s, 1)
+                    shlex.join(exe_args[1:]), exe_path.head.s, 1)
             except:
                 self.ShowError(werr)
         except Exception as error:

Mopy/bash/patcher/patchers/multitweak_races.py
@@ -217,7 +217,7 @@ class _ARUnblockTweak(_ARaceTweak):
         setattr(record, race_attr, self.tweak_races_data[race_sig])
 
 # -----------------------------------------------------------------------------
-class RaceTweak_AllHairs(_ARaceTweak):
+class RaceTweak_AllHairs(_ARUnblockTweak):
     """Gives all races ALL hairs."""
     tweak_read_classes = b'HAIR', b'RACE',
     tweak_name = _('Races Have All Hairs')
@@ -226,6 +226,7 @@ class RaceTweak_AllHairs(_ARaceTweak):
     tweak_choices = [('get down tonight', 1)]
     _sig_and_attr = (b'HAIR', 'hairs')
 
+# -----------------------------------------------------------------------------
 class RaceTweak_AllHeadParts(IndexingTweak, CustomChoiceTweak):
     ##: We *only* need FLST here for the create_record call down below
     tweak_read_classes = b'FLST', b'HDPT',