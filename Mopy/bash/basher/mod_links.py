wrye-bash/wrye-bash

commit 7b385eefdb0f9e556adb607dba703b9433b9fddc

Fix several Export/Import Names bugs
- The deletion logic in _read_record was wrong and deleted every single
  exported name.
- _read_record sometimes stored booleans in the resulting stored_data,
  which blew in  _row_out (I think this was due to a wrongly
  parenthesized walrus operator?).
- Dropped the weird 'NO NAME' stuff. No other parser does this and it
  doesn't make sense to export/import records with missing FULLs
  anyways. Included backwards compatibility code to skip 'NO NAME's from
  older CSVs.

Mopy/bash/parsers.py
@@ -874,21 +874,24 @@ class FullNames(_HandleAliases):
         self._attr_dex = {u'full': 4} if self._called_from_patcher else {
             u'eid': 3, u'full': 4}
 
+    def _update_from_csv(self, top_grup_sig, csv_fields, index_dict=None):
+        if csv_fields[-1] == 'NO NAME':
+            raise ValueError # Leftover from pre-310 days, just skip it
+        super()._update_from_csv(top_grup_sig, csv_fields, index_dict)
+
     def _read_record(self, record, id_data, __attrgetters=attrgetter_cache):
         super()._read_record(record, id_data)
         rec_data = id_data[record.fid]
-        if not (full := rec_data['full']):
-            full = rec_data['full'] = record.rec_sig == b'LIGH' and 'NO NAME'
-        if not rec_data['eid'] or full: # never used from patcher
+        if not rec_data['full']: # No FULL -> skip this record
             del id_data[record.fid]
 
     def _write_record(self, record, di, changed):
-        full = record.full
-        newFull = di[u'full']
-        if newFull and newFull not in (full, u'NO NAME'):
-            record.full = newFull
+        old_full = record.full
+        new_full = di['full']
+        if new_full != old_full:
+            record.full = new_full
             record.setChanged()
-            changed[di[u'eid']] = (full, newFull)
+            changed[di['eid']] = (old_full, new_full)
 
     def _row_out(self, lfid, stored_data, top_grup):
         return ('"%s",%s,"%s","%s"\n' % (

commit c944d4fdeed4d4154da144dd5922db40ce5ea35e

Fix 637 regression in Nvidia Fog Fix
Also redraw the fixed plugins afterwards.

Mopy/bash/parsers.py:
Fix regression when importing Names CSV SSS

Broke in 7b385eefdb0f9e556adb607dba703b9433b9fddc.

Mopy/bash/basher/mod_links.py
@@ -1325,20 +1325,22 @@ class Mod_FogFixer(ItemLink):
                     'interior cells to avoid the Nvidia black screen bug.')
         if not self._askContinue(message, u'bash.cleanMod.continue',
                                  _(u'Nvidia Fog Fix')): return
+        fixed = {}
         with balt.Progress(_(u'Nvidia Fog Fix')) as progress:
             progress.setFull(len(self.selected))
-            fixed = []
             for index,(fileName,fileInfo) in enumerate(self.iselected_pairs()):
                 if fileName == bush.game.master_file: continue
                 progress(index, _(u'Scanning %s') % fileName)
                 fog_fixer = bosh.mods_metadata.NvidiaFogFixer(fileInfo)
                 fog_fixer.fix_fog(SubProgress(progress, index, index + 1))
                 if fog_fixer.fixedCells:
-                    fixed.append(
-                        f'* {len(fog_fixer.fixedCells):4d} {fileName}')
+                    fixed[fileName] = fog_fixer.fixedCells
         if fixed:
-            message = u'==='+_(u'Cells Fixed')+u':\n'+u'\n'.join(fixed)
+            message = '===' + _('Cells Fixed:') + '\n' + '\n'.join([
+                f'* {fixed_pname}: {len(cells_fixed)}'
+                for fixed_pname, cells_fixed in fixed.items()])
             self._showWryeLog(message)
+            self.window.RefreshUI(redraw=list(fixed))
         else:
             message = _(u'No changes required.')
             self._showOk(message)

Mopy/bash/bosh/mods_metadata.py
@@ -26,7 +26,8 @@ from collections import defaultdict, OrderedDict
 from ._mergeability import is_esl_capable
 from .. import balt, bolt, bush, bass, load_order
 from ..bolt import dict_sort, structs_cache, SubProgress, sig_to_str
-from ..brec import ModReader, SubrecordBlob, RecordHeader, unpack_header
+from ..brec import ModReader, SubrecordBlob, RecordHeader, unpack_header, \
+    ShortFidWriteContext
 from ..exception import CancelError
 from ..mod_files import ModHeaderReader
 
@@ -705,7 +706,7 @@ class NvidiaFogFixer(object):
         minfo_path = self.modInfo.getPath()
         #--Scan/Edit
         with ModReader.from_info(self.modInfo) as ins:
-            with minfo_path.temp.open(u'wb') as out:
+            with ShortFidWriteContext(minfo_path.temp) as out:
                 def copy(bsize):
                     buff = ins.read(bsize)
                     out.write(buff)

Mopy/bash/parsers.py
@@ -886,7 +886,7 @@ class FullNames(_HandleAliases):
     def _update_from_csv(self, top_grup_sig, csv_fields, index_dict=None):
         if csv_fields[-1] == 'NO NAME':
             raise ValueError # Leftover from pre-310 days, just skip it
-        super()._update_from_csv(top_grup_sig, csv_fields, index_dict)
+        return super()._update_from_csv(top_grup_sig, csv_fields, index_dict)
 
     def _read_record(self, record, id_data, __attrgetters=attrgetter_cache):
         super()._read_record(record, id_data)