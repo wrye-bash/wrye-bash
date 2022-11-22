wrye-bash/wrye-bash

commit f4885f41b3e6ce81605692642628504ada742bd7

[!]Extend cell handling for all games:
Stashing a ! cause I am really not sure about which are exactly the
nested types per game. Asked at:

https://discord.com/channels/471930020454072348/524788120810684431/1041341252307791902

but meanwhile - it all starts coming together. LoadFactory was
modernized and cleaned up (rest is #647). Note:

- LoadFactory.addClass: if class_sig were WRLD it would add the CELL
to types which makes no sense as WRLD is not nested in CELL. Also if
self.keepAll the type_class would return MreRecord anyway - so no need
for the `for cell_rec_sig in __cell_rec_sigs` loop. Anyway most the
cruft is gone now, including those `type` parameters :P
- as mentioned I am not sure about the games nested classes (FO4 is
currently being worked on and Morrowind remains a mystery - but as
discussed we still don't load those complex grups so these will be
addressed in time - note that most REFR types are yet not implemented
for most games see:
https://github.com/wrye-bash/wrye-bash/commit/6777345ca214340e74aeb998f1f9bbc66c1e3760
https://github.com/wrye-bash/wrye-bash/commit/fae844a6ead7c192cd81f417158f1d7eb97767a2#r89766907
)

Inline getRecTypeClass

Due to new design this will be a function call per record - renamed
type_class - naming is WIP but getting there - for one in python type
should only mean type (exceptions apply and sometimes class is more
clear - exceptions include the usual use of the word type in English).

Mopy/bash/mod_files.py
@@ -25,6 +25,7 @@ through PBash (LoadFactory + ModFile) as well as some related classes."""
 
 from collections import defaultdict
 from itertools import chain
+from typing import Iterable
 from zlib import decompress as zlib_decompress, error as zlib_error
 
 from . import bolt, bush, env, load_order
@@ -75,62 +76,42 @@ class MasterMap(object):
         else:
             raise MasterMapError(inIndex)
 
-class LoadFactory(object):
-    """Factory for mod representation objects."""
-    def __init__(self, keepAll, *, by_sig=(), generic=()):
+class LoadFactory:
+    """Encapsulate info on which record type we use to load which record
+    signature."""
+    def __init__(self, keepAll, *, by_sig: Iterable[bytes] = (),
+                 generic: Iterable[bytes] = ()):
         """Pass a collection of signatures to load - either by their
         respective type or using generic MreRecord.
-        :param by_sig: pass an iterable of top group signatures to load
-        :type by_sig: Iterable[bytes]
-        :param generic: top group signatures to load as generic MreRecord
-        :type generic: Iterable[bytes]
-        """
+        :param by_sig: pass an iterable of top group signatures to unpack
+        :param generic: top group signatures to load as generic MreRecord"""
         self.keepAll = keepAll
         self.recTypes = set()
         self.topTypes = set()
-        self.type_class = {}
-        recClasses = chain(generic, (RecordType.sig_to_class[x] for x in by_sig))
-        for recClass in recClasses:
-            self.addClass(recClass)
+        self.sig_to_type = defaultdict(lambda: MreRecord if keepAll else None)
+        # no generic classes if we keep all (we return MreRecord anyway)
+        self.add_class(*by_sig, generic=() if keepAll else generic)
 
-    def addClass(self, recClass, __cell_rec_sigs=frozenset([b'WRLD', b'ROAD',
-            b'CELL', b'REFR', b'ACHR', b'ACRE', b'PGRD', b'LAND'])):
-        """Adds specified class."""
-        if isinstance(recClass, bytes):
-            class_sig = recClass
-            recClass = MreRecord
-        else:
-            try:
-                class_sig = recClass.rec_sig
-            except AttributeError:
-                raise ValueError(f'addClass: bytes or MelRecord expected - '
-                                 f'got: {recClass!r}!')
+    def add_class(self, *by_sig, generic: Iterable[bytes] = ()):
+        """Adds specified record types - see __init__."""
         #--Don't replace complex class with default (MreRecord) class
-        if class_sig in self.type_class and recClass == MreRecord:
-            return
-        self.recTypes.add(class_sig)
-        self.type_class[class_sig] = recClass
+        if generic:
+            self.sig_to_type = {**dict.fromkeys(generic, MreRecord),
+                                **self.sig_to_type}
+        if by_sig:
+            self.sig_to_type.update(
+                (k, RecordType.sig_to_class[k]) for k in by_sig)
+        self.recTypes.update(all_sigs := (*by_sig, *generic))
         #--Top type
-        if class_sig in __cell_rec_sigs:
-            self.topTypes.add(b'CELL')
-            self.topTypes.add(b'WRLD')
-            if self.keepAll:
-                for cell_rec_sig in __cell_rec_sigs:
-                    if cell_rec_sig not in self.type_class:
-                        self.type_class[cell_rec_sig] = MreRecord
-        ##: apart from this and cell stuff above set(type_class) == topTypes
-        elif class_sig == b'INFO':
-            self.topTypes.add(b'DIAL')
-        else:
-            self.topTypes.add(class_sig)
-
-    def getRecClass(self,type):
-        """Returns class for record type or None."""
-        return self.type_class.get(type, (self.keepAll and MreRecord) or None)
+        for class_sig in all_sigs:
+            if class_sig in RecordType.nested_to_top:
+                self.topTypes.update(RecordType.nested_to_top[class_sig])
+            else:
+                self.topTypes.add(class_sig)
 
     def getCellTypeClass(self):
         """Returns type_class dictionary for cell objects."""
-        return {r: self.getRecClass(r) for r in (
+        return {r: self.sig_to_type[r] for r in (
             b'REFR', b'ACHR', b'ACRE', b'PGRD', b'LAND', b'CELL', b'ROAD')}
 
     def getUnpackCellBlocks(self,topType):

Mopy/bash/brec/common_records.py
@@ -474,6 +474,7 @@ class AMreCell(MelRecord):
     # All 'reference' types, i.e. record types that occur in CELL/WLRD groups
     # and place some sort of thing into the cell (e.g. ACHR, REFR, PMIS, etc.)
     ref_types = set()
+    interior_temp_extra = set()
 
     def getBsb(self):
         """Returns tesfile block and sub-block indices for cells in this group.
@@ -487,6 +488,22 @@ class AMreCell(MelRecord):
         else:
             x, y = self.posX or 0, self.posY or 0  # posXY can be None
             return (y // 32, x // 32), (y // 8, x // 8) # YX- ready for packing
+
+    @classmethod
+    def nested_records_sigs(cls):
+        return {*cls.ref_types, *cls.interior_temp_extra}
+
+class AMreWrld(MelRecord):
+    """Worldspace."""
+    rec_sig = b'WRLD'
+    ref_types = set() # same as AMreCell
+    exterior_temp_extra = [] # exterior cell temp cell references
+    wrld_children_extra = [] # record sigs that appear in wrld children grup
+
+    @classmethod
+    def nested_records_sigs(cls):
+        return {*cls.ref_types, *cls.exterior_temp_extra,
+                *cls.wrld_children_extra}
 
 #------------------------------------------------------------------------------
 # Full classes ----------------------------------------------------------------

Mopy/bash/brec/record_groups.py
@@ -203,7 +203,7 @@ class MobObjects(MobBase):
     def _load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
-        recClass = self.loadFactory.getRecClass(expType)
+        recClass = self.loadFactory.sig_to_type[expType]
         insAtEnd = ins.atEnd
         errLabel = f'{(exp_str := sig_to_str(expType))} Top Block'
         while not insAtEnd(endPos,errLabel):
@@ -349,7 +349,7 @@ class MobDial(MobObjects):
         super(MobDial, self).__init__(header, loadFactory, ins, do_unpack)
 
     def _load_rec_group(self, ins, endPos):
-        info_class = self.loadFactory.getRecClass(b'INFO')
+        info_class = self.loadFactory.sig_to_type[b'INFO']
         if not info_class:
             self.header.skip_blob(ins) # DIAL already read, skip all INFOs
         ins_at_end = ins.atEnd
@@ -525,7 +525,7 @@ class MobDials(MobBase):
 
     def _load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
-        dial_class = self.loadFactory.getRecClass(b'DIAL')
+        dial_class = self.loadFactory.sig_to_type[b'DIAL']
         ins_seek = ins.seek
         expType = self.label
         insAtEnd = ins.atEnd
@@ -1117,7 +1117,7 @@ class MobICells(MobCells):
     def _load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
-        recClass = self.loadFactory.getRecClass(expType)
+        recClass = self.loadFactory.sig_to_type[expType]
         insSeek = ins.seek
         if not recClass: insSeek(endPos) # skip the whole group
         cell = None
@@ -1443,7 +1443,7 @@ class MobWorlds(MobBase):
     def _load_rec_group(self, ins, endPos):
         """Loads data from input stream. Called by load()."""
         expType = self.label
-        recWrldClass = self.loadFactory.getRecClass(expType)
+        recWrldClass = self.loadFactory.sig_to_type[expType]
         insSeek = ins.seek
         if not recWrldClass: insSeek(endPos) # skip the whole group
         world = None

Mopy/bash/brec/record_structs.py
@@ -250,17 +250,21 @@ class RecordType(type):
     # Maps subrecord signatures to a set of record signatures that can contain
     # those subrecords
     subrec_sig_to_record_sig = defaultdict(set)
+    # nested record types mapped to top record type they belong to
+    nested_to_top = defaultdict(set)
 
     def __new__(cls, name, bases, classdict):
         slots = classdict.get('__slots__', ())
         classdict['__slots__'] = (*slots, *melSet.getSlotsUsed()) if (
             melSet := classdict.get('melSet', ())) else slots
         new = super(RecordType, cls).__new__(cls, name, bases, classdict)
-        if rs := getattr(new, 'rec_sig', None):
-            cls.sig_to_class[rs] = new
+        if rsig := getattr(new, 'rec_sig', None):
+            cls.sig_to_class[rsig] = new
             if new.melSet:
                 for sr_sig in new.melSet.loaders:
-                    RecordType.subrec_sig_to_record_sig[sr_sig].add(rs)
+                    RecordType.subrec_sig_to_record_sig[sr_sig].add(rsig)
+            for sig in new.nested_records_sigs():
+                RecordType.nested_to_top[sig].add(rsig)
         return new
 
 class MreRecord(metaclass=RecordType):
@@ -367,6 +371,10 @@ class MreRecord(metaclass=RecordType):
     __slots__ = ('header', '_rec_sig', 'fid', 'flags1', 'size', 'flags2',
                  'changed', 'data', 'inName')
     isKeyedByEid = False
+
+    @classmethod
+    def nested_records_sigs(cls):
+        return set()
 
     def __init__(self, header, ins=None, *, do_unpack=False):
         self.header = header

Mopy/bash/game/fallout3/__init__.py
@@ -1174,12 +1174,10 @@ class Fallout3GameInfo(PatchGame):
 
     @classmethod
     def _validate_records(cls, package_name, plugin_form_vers=15):
-        from .. import brec ## todo absorb this in super._validate_records
-        header_type = brec.RecordHeader
-        header_type.valid_header_sigs |= {b'INFO', b'NAVM', b'LAND'}
+        from .. import brec
         # We can't upgrade IMGS\DNAM (see definition), so skip upgrading form
         # version too
-        header_type.skip_form_version_upgrade = {b'IMGS'}
+        brec.RecordHeader.skip_form_version_upgrade = {b'IMGS'}
         super()._validate_records(package_name, plugin_form_vers)
         cls.mergeable_sigs = set(cls.top_groups) - {b'CELL', b'DIAL', b'WRLD',
                                                     b'NAVI'}

Mopy/bash/game/fallout3/records.py
@@ -34,11 +34,11 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelUnion, AttrValDecider, MelRegnEntrySubrecord, SizeDecider, MelFloat, \
     MelSInt8, MelSInt16, MelSInt32, MelUInt8, MelUInt16, MelUInt32, \
     MelPartialCounter, MelRaceParts, MelRelations, MelActorSounds, MelWeight, \
-    MelRaceVoices, MelBounds, null2, MelScriptVars, MelSorted, \
-    MelSequential, MelTruncatedStruct, PartialLoadDecider, MelReadOnly, \
-    MelSkipInterior, MelIcons, MelIcons2, MelIcon, MelIco2, MelEdid, MelFull, \
-    MelArray, MelWthrColors, AMreLeveledList, AMreActor, AMreWithItems, \
-    MelRef3D, MelXlod, MelNull, MelWorldBounds, MelEnableParent, MelPerkData, \
+    MelRaceVoices, MelBounds, null2, MelScriptVars, MelSorted, MelSequential, \
+    MelTruncatedStruct, PartialLoadDecider, MelReadOnly, MelSkipInterior, \
+    MelIcons, MelIcons2, MelIcon, MelIco2, MelEdid, MelFull, MelArray, \
+    MelWthrColors, AMreLeveledList, AMreActor, AMreWithItems, MelRef3D, \
+    MelXlod, MelNull, MelWorldBounds, MelEnableParent, MelPerkData, \
     MelRefScale, MelMapMarker, MelActionFlags, MelEnchantment, MelScript, \
     MelDecalData, MelDescription, MelLists, MelSoundPickupDrop, MelBookText, \
     MelActivateParents, BipedFlags, MelSpells, MelUInt8Flags, MelUInt16Flags, \
@@ -52,7 +52,7 @@ from ...brec import MelRecord, MelGroups, MelStruct, FID, MelGroup, \
     MelSoundLooping, MelHairFlags, MelImpactDataset, MelFlstFids, MelObject, \
     MelTxstFlags, MelGrasData, MelIdlmFlags, MelIdleAnimations, AMreImad, \
     perk_distributor, MelInfoResponsesFo3, MelIpctTextureSets, MelIpctSounds, \
-    MelLandShared, MelIdleAnimationCountOld, AMreCell
+    MelLandShared, MelIdleAnimationCountOld, AMreCell, AMreWrld
 from ...exception import ModSizeError
 
 _is_fnv = bush.game.fsName == u'FalloutNV'
@@ -697,7 +697,8 @@ class MreCams(MelRecord):
 #------------------------------------------------------------------------------
 class MreCell(AMreCell):
     """Cell."""
-    ref_types = {b'ACHR', b'ACRE', b'PBEA', b'PGRE', b'PMIS', b'REFR'} # TODO implement b'PBEA'
+    ref_types = {b'ACHR', b'ACRE', b'PBEA', b'PGRE', b'PMIS', b'REFR'}
+    interior_temp_extra = [b'NAVM']
 
     cellFlags = Flags.from_names(
         (0, 'isInterior'),
@@ -972,6 +973,10 @@ class MreDial(MelRecord):
     rec_sig = b'DIAL'
 
     _DialFlags = Flags.from_names('rumors', 'toplevel')
+
+    @classmethod
+    def nested_records_sigs(cls):
+        return {b'INFO'}
 
     melSet = MelSet(
         MelEdid(),
@@ -2996,9 +3001,11 @@ class MreWeap(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreWrld(MelRecord):
+class MreWrld(AMreWrld):
     """Worldspace."""
-    rec_sig = b'WRLD'
+    ref_types = MreCell.ref_types
+    exterior_temp_extra = [b'LAND', b'NAVM']
+    wrld_children_extra = [b'CELL']
 
     _flags = Flags.from_names('smallWorld', 'noFastTravel',
                               'oblivionWorldspace', None, 'noLODWater',

Mopy/bash/game/fallout4/__init__.py
@@ -748,10 +748,9 @@ class Fallout4GameInfo(PatchGame):
 
     @classmethod
     def _validate_records(cls, package_name, plugin_form_vers=131):
-        from .. import brec ## todo absorb this in super._validate_records
+        from .. import brec
         header_type = brec.RecordHeader
-        header_type.valid_header_sigs |= {b'INFO', b'NAVM', b'LAND', b'DIAL'## todo DIAL??
-                                          }
+        header_type.valid_header_sigs |= {b'DIAL'}
         # DMGT\DNAM changed completely in Form Version 78 and it's not possible
         # to upgrade it (unless someone reverse engineers what the game does to
         # it when loading)
@@ -769,6 +768,6 @@ class Fallout4GameInfo(PatchGame):
             b'SOPM', b'SOUN', b'SPEL', b'SPGD', b'STAG', b'STAT', b'TACT',
             b'TERM', b'TREE', b'TRNS', b'TXST', b'VTYP', b'WATR', b'WEAP',
             b'WRLD', b'WTHR', b'ZOOM'}
-        brec.RecordType.simpleTypes = cls.mergeable_sigs ## todo it used to include MreInfo, MreLand
+        brec.RecordType.simpleTypes = cls.mergeable_sigs
 
 GAME_TYPE = Fallout4GameInfo

Mopy/bash/game/fallout4/records.py
@@ -717,9 +717,10 @@ class MreCams(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreCell(AMreCell): # todo: implement I guess
+class MreCell(AMreCell):
     ref_types = {b'ACHR', b'PARW', b'PBAR', b'PBEA', b'PCON', b'PFLA', b'PGRE',
                  b'PHZD', b'PMIS', b'REFR'}
+    interior_temp_extra = [b'NAVM']
 
 #------------------------------------------------------------------------------
 class MreClas(MelRecord):

commit 06b724025e1eeef2a8e6ddbd76e0d5ce069264b5

Regression fixup for CELL patcher:
Introduced in f4885f41b3e6ce81605692642628504ada742bd7 - CELL was in
nested_to_top so was not added in topTypes.

Mopy/bash/mod_files.py
@@ -106,8 +106,8 @@ class LoadFactory:
         for class_sig in all_sigs:
             if class_sig in RecordType.nested_to_top:
                 self.topTypes.update(RecordType.nested_to_top[class_sig])
-            else:
-                self.topTypes.add(class_sig)
+            if class_sig in RecordHeader.top_grup_sigs:
+                self.topTypes.add(class_sig) # b'CELL' appears in both
 
     def getCellTypeClass(self):
         """Returns type_class dictionary for cell objects."""