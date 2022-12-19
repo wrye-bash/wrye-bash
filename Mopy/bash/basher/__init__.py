wrye-bash/wrye-bash

commit effcf831450fae37bd0b5b5d174c2398b0bc8552

Add links for LO undo/redo
To help users find the shortcuts in the first place. Moved the links for
opening LO files to the new Load Order submenu, which may also become
useful for housing other links in the future.

Mopy/bash/basher/__init__.py
@@ -1136,20 +1136,6 @@ class ModList(_ModsUIList):
 
     def _handle_key_down(self, wrapped_evt):
         """Char event: Reorder (Ctrl+Up and Ctrl+Down)."""
-        def undo_redo_op(lo_op):
-            # Grab copies of the old LO/actives for find_first_difference
-            prev_lo = load_order.cached_lo_tuple()
-            prev_acti = load_order.cached_active_tuple()
-            if not lo_op(): return # nothing to do
-            curr_lo = load_order.cached_lo_tuple()
-            curr_acti = load_order.cached_active_tuple()
-            low_diff = load_order.find_first_difference(
-                prev_lo, prev_acti, curr_lo, curr_acti)
-            if low_diff is None: return # load orders were identical
-            # Finally, we pass to _lo_redraw_targets to take all other relevant
-            # details into account
-            self.RefreshUI(redraw=self._lo_redraw_targets({curr_lo[low_diff]}),
-                           refreshSaves=True)
         kcode = wrapped_evt.key_code
         if wrapped_evt.is_cmd_down and kcode in balt.wxArrows:
             # Ctrl+Up/Ctrl+Down - move plugin up/down load order
@@ -1178,15 +1164,16 @@ class ModList(_ModsUIList):
                 lowest_index = min(lowest_index, newIndex)
                 moved |= self._dropIndexes(chunk, newIndex)
             if moved: self._refreshOnDrop(lowest_index)
-        elif wrapped_evt.is_cmd_down and kcode == ord(u'Z'):
-            # Ctrl+Z - undo last load order or active plugins change (unless
-            # shift is also pressed, in which case it acts like Ctrl+Y)
-            undo_redo_op(self.data_store.redo_load_order
-                         if wrapped_evt.is_shift_down
-                         else self.data_store.undo_load_order)
-        elif wrapped_evt.is_cmd_down and kcode == ord(u'Y'):
+        elif wrapped_evt.is_cmd_down and kcode == ord('Z'):
+            if wrapped_evt.is_shift_down:
+                # Ctrl+Shift+Z - redo last load order or active plugins change
+                self.lo_redo()
+            else:
+                # Ctrl+Z - undo last load order or active plugins change
+                self.lo_undo()
+        elif wrapped_evt.is_cmd_down and kcode == ord('Y'):
             # Ctrl+Y - redo last load order or active plugins change
-            undo_redo_op(self.data_store.redo_load_order)
+            self.lo_redo()
         else:
             # Correctly update the highlight around selected mod
             return EventResult.CONTINUE
@@ -1245,7 +1232,7 @@ class ModList(_ModsUIList):
     def _unhide_wildcard():
         return bosh.modInfos.plugin_wildcard()
 
-    #--Helpers ---------------------------------------------
+    # Helpers -----------------------------------------------------------------
     @staticmethod
     def _lo_redraw_targets(impacted_plugins):
         """Given a set of plugins (as paths) that were impacted by a load order
@@ -1367,6 +1354,34 @@ class ModList(_ModsUIList):
                                  [checklists], liststyle=u'tree',
                                  canCancel=False)
 
+    # Undo/Redo ---------------------------------------------------------------
+    def _undo_redo_op(self, undo_or_redo):
+        """Helper for load order undo/redo operations. Handles UI refreshes."""
+        # Grab copies of the old LO/actives for find_first_difference
+        prev_lo = load_order.cached_lo_tuple()
+        prev_acti = load_order.cached_active_tuple()
+        if not undo_or_redo():
+            return # Nothing to do
+        curr_lo = load_order.cached_lo_tuple()
+        curr_acti = load_order.cached_active_tuple()
+        low_diff = load_order.find_first_difference(
+            prev_lo, prev_acti, curr_lo, curr_acti)
+        if low_diff is None:
+            return # Load orders were identical
+        # Finally, we pass to _lo_redraw_targets to take all other relevant
+        # details into account
+        self.RefreshUI(redraw=self._lo_redraw_targets({curr_lo[low_diff]}),
+                       refreshSaves=True)
+
+    def lo_undo(self):
+        """Undoes a load order change."""
+        self._undo_redo_op(load_order.undo_load_order)
+
+    def lo_redo(self):
+        """Redoes a load order change."""
+        self._undo_redo_op(load_order.redo_load_order)
+
+    # Other -------------------------------------------------------------------
     def new_bashed_patch(self):
         """Create a new Bashed Patch and refresh the GUI for it."""
         new_patch_name = bosh.modInfos.generateNextBashedPatch(

Mopy/bash/basher/links_init.py
@@ -461,14 +461,19 @@ def InitModLinks():
             files_menu.links.append(SeparatorLink())
             files_menu.links.append(Mods_CreateBlank())
             files_menu.links.append(Mods_CreateBlankBashedPatch())
-        files_menu.links.append(SeparatorLink())
-        files_menu.links.append(Mods_OpenLOFileMenu())
         ModList.column_links.append(files_menu)
     ModList.column_links.append(SeparatorLink())
-    if True: #--Load
-        loadMenu = MenuLink(_('Active Plugins..'))
-        loadMenu.links.append(Mods_LoadList())
-        ModList.column_links.append(loadMenu)
+    if True: #--Active Plugins
+        ap_menu = MenuLink(_('Active Plugins..'))
+        ap_menu.links.append(Mods_LoadList())
+        ModList.column_links.append(ap_menu)
+    if True: #--Load Order
+        lo_menu = MenuLink(_('Load Order..'))
+        lo_menu.links.append(Mods_LOUndo())
+        lo_menu.links.append(Mods_LORedo())
+        lo_menu.links.append(SeparatorLink())
+        lo_menu.links.append(Mods_OpenLOFileMenu())
+        ModList.column_links.append(lo_menu)
     ModList.column_links.append(SeparatorLink())
     if bush.game.displayName == 'Oblivion': #--Versions
         versionsMenu = MenuLink('Oblivion.esm..')
@@ -651,14 +656,18 @@ def InitModLinks():
         file_menu.append(Mods_CreateBlank())
         file_menu.append(Mods_CreateBlankBashedPatch())
     file_menu.append(SeparatorLink())
-    file_menu.append(Mods_OpenLOFileMenu())
-    file_menu.append(SeparatorLink())
     file_menu.append(Misc_SaveData())
     # Edit Menu
     edit_menu = ModList.global_links[_('Edit')]
-    am_submenu = MenuLink(_('Active Plugins..'))
-    am_submenu.append(Mods_LoadList())
-    edit_menu.append(am_submenu)
+    ap_submenu = MenuLink(_('Active Plugins..'))
+    ap_submenu.append(Mods_LoadList())
+    edit_menu.append(ap_submenu)
+    lo_submenu = MenuLink(_('Load Order..'))
+    lo_submenu.links.append(Mods_LOUndo())
+    lo_submenu.links.append(Mods_LORedo())
+    lo_submenu.links.append(SeparatorLink())
+    lo_submenu.links.append(Mods_OpenLOFileMenu())
+    edit_menu.append(lo_submenu)
     if _is_oblivion:
         edit_menu.append(SeparatorLink())
         versions_menu = MenuLink('Oblivion.esm..')

Mopy/bash/basher/mods_links.py
@@ -43,7 +43,8 @@ __all__ = [u'Mods_EsmsFirst', u'Mods_LoadList', u'Mods_SelectedFirst',
            u'Mods_ScanDirty', u'Mods_CrcRefresh', u'Mods_AutoESLFlagBP',
            u'Mods_LockActivePlugins', u'Mods_PluginChecker',
            u'Mods_ExportBashTags', u'Mods_ImportBashTags',
-           u'Mods_ClearManualBashTags', u'Mods_OpenLOFileMenu']
+           'Mods_ClearManualBashTags', 'Mods_OpenLOFileMenu', 'Mods_LOUndo',
+           'Mods_LORedo']
 
 # "Load" submenu --------------------------------------------------------------
 class _Mods_LoadListData(balt.ListEditorData):
@@ -124,7 +125,7 @@ class Mods_LoadList(ChoiceLink):
                 balt.ListEditor.display_dialog(
                     self.window, _('Active Plugins Lists'), editorData)
         class _SaveLink(EnabledLink):
-            _text = _('Save Active Plugins List')
+            _text = _('Save Active Plugins List...')
             _help = _('Save the currently active plugins to a new active '
                       'plugins list.')
             def _enable(self): return bool(load_order.cached_active_tuple())
@@ -138,8 +139,8 @@ class Mods_LoadList(ChoiceLink):
                     return self._showError(message)
                 _self.load_lists[newItem] = list(
                     load_order.cached_active_tuple())
-        self.extraItems = [_All(), _None(), _Selected(), _SaveLink(), _Edit(),
-                           SeparatorLink()]
+        self.extraItems = [_All(), _None(), _Selected(), SeparatorLink(),
+                           _SaveLink(), _Edit(), SeparatorLink()]
         class _LoListLink(__Activate):
             def Execute(self):
                 """Activate mods in list."""
@@ -495,3 +496,23 @@ class Mods_OpenLOFileMenu(MultiLink):
     """Shows one or more links for opening LO management files."""
     def _links(self):
         return [_Mods_OpenLOFile(lo_f) for lo_f in load_order.get_lo_files()]
+
+#------------------------------------------------------------------------------
+class Mods_LOUndo(ItemLink):
+    """Undoes a load order or active plugins change."""
+    _text = _('Undo')
+    _help = _('Undoes a load order or active plugins change.')
+    _keyboard_hint = 'Ctrl+Z'
+
+    def Execute(self):
+        self.window.lo_undo()
+
+#------------------------------------------------------------------------------
+class Mods_LORedo(ItemLink):
+    """Redoes a load order or active plugins change."""
+    _text = _('Redo')
+    _help = _('Redoes a load order or active plugins change.')
+    _keyboard_hint = 'Ctrl+Y'
+
+    def Execute(self):
+        self.window.lo_redo()

commit 5b8d1c715352883c81a8993213f2a0b3b280b822

Fix undo/redo LO regression
Broke in effcf831450fae37bd0b5b5d174c2398b0bc8552 because I forgot about
_lo_cache.

Mopy/bash/basher/__init__.py
@@ -1375,11 +1375,11 @@ class ModList(_ModsUIList):
 
     def lo_undo(self):
         """Undoes a load order change."""
-        self._undo_redo_op(load_order.undo_load_order)
+        self._undo_redo_op(self.data_store.undo_load_order)
 
     def lo_redo(self):
         """Redoes a load order change."""
-        self._undo_redo_op(load_order.redo_load_order)
+        self._undo_redo_op(self.data_store.redo_load_order)
 
     # Other -------------------------------------------------------------------
     def new_bashed_patch(self):