wrye-bash/wrye-bash

commit 851a6a01db744a8fd26fbf90490c6720e44ce66d

Refactor se.steam_exe, kill SteamInstall setting
This was entirely unnecessary, only needed for xSE version detection. We
can instead just try several paths and use the first one that works
(which is going to be the very first one for everyone who installed xSE
by copying all its files, aka 99.9% of users).

Also corrected any wrong files (e.g. nvse_loader.dll and fose_loader.dll
don't actually exist), which means that xSE version detection now works
for FO3 and FNV as well.

Mopy/bash/belt.py
@@ -1061,12 +1061,11 @@ class WryeParser(ScriptParser.Parser):
 
     def fnCompareSEVersion(self, seWant):
         if bush.game.se.se_abbrev != u'':
-            if bass.inisettings['SteamInstall']:
-                se = bush.game.se.steam_exe   # User may not have obse_loader.exe, since it's only required for the CS
-            else:
-                se = bush.game.se.exe
-            ret = self._TestVersion(self._TestVersion_Want(seWant), bass.dirs['app'].join(se))
-            return ret[0]
+            ver_path = None
+            for ver_file in bush.game.se.ver_files:
+                ver_path = bass.dirs['app'].join(ver_file)
+                if ver_path.exists(): break
+            return self._TestVersion(self._TestVersion_Want(seWant), ver_path)
         else:
             # No script extender available for this game
             return 1
@@ -1585,11 +1584,11 @@ class WryeParser(ScriptParser.Parser):
         bGameOk = ret[0] >= 0
         gameHave = ret[1]
         if bush.game.se.se_abbrev != u'':
-            if bass.inisettings['SteamInstall']:
-                seName = bush.game.se.steam_exe
-            else:
-                seName = bush.game.se.exe
-            ret = self._TestVersion(seWant, bass.dirs['app'].join(seName))
+            ver_path = None
+            for ver_file in bush.game.se.ver_files:
+                ver_path = bass.dirs['app'].join(ver_file)
+                if ver_path.exists(): break
+            ret = self._TestVersion(seWant, ver_path)
             bSEOk = ret[0] >= 0
             seHave = ret[1]
         else:
@@ -1632,7 +1631,7 @@ class WryeParser(ScriptParser.Parser):
         return need
 
     def _TestVersion(self, need, file_):
-        if file_.exists():
+        if file_ and file_.exists():
             have = get_file_version(file_.s)
             ver = u'.'.join([unicode(i) for i in have])
             if need == u'None':

Mopy/bash/basher/app_buttons.py
@@ -103,12 +103,13 @@ class StatusBar_Button(ItemLink):
     @property
     def obseVersion(self):
         if not bass.settings['bash.statusbar.showversion']: return u''
-        if bass.inisettings['SteamInstall']:
-            se_exe = bass.dirs['app'].join(bush.game.se.steam_exe)
+        for ver_file in bush.game.se.ver_files:
+            ver_path = bass.dirs['app'].join(ver_file)
+            if ver_path.exists():
+                return u' ' + u'.'.join([u'%s' % x for x
+                                         in ver_path.strippedVersion])
         else:
-            se_exe = bass.dirs['app'].join(bush.game.se.exe)
-        if not se_exe.isfile(): return u''
-        return u' ' + u'.'.join([u'%s' % x for x in se_exe.strippedVersion])
+            return u''
 
 #------------------------------------------------------------------------------
 # App Links -------------------------------------------------------------------
@@ -254,10 +255,9 @@ class App_Button(StatusBar_Button):
                 args = [exePath.s]
             elif self.obseArg is not None and \
                     BashStatusBar.obseButton.button_state:
-                if bass.inisettings['SteamInstall'] and self.exePath.tail == u'Oblivion.exe':
-                    exePath = self.exePath
-                else:
-                    exePath = exeObse
+                exePath = (self.exePath if not exeObse.exists() and
+                           self.exePath.tail == u'Oblivion.exe'
+                           else exeObse)
                 args = [exePath.s]
                 if self.obseArg != u'':
                     args.append(u'%s' % self.obseArg)

Mopy/bash/bosh/__init__.py
@@ -3098,10 +3098,6 @@ def initDefaultTools():
 def initDefaultSettings():
     #other settings from the INI:
     inisettings['EnableUnicode'] = False
-    if 'steam' in dirs['app'].cs:
-        inisettings['SteamInstall'] = True
-    else:
-        inisettings['SteamInstall'] = False
     inisettings['ScriptFileExt'] = u'.txt'
     inisettings['KeepLog'] = 0
     inisettings['LogFile'] = dirs['mopy'].join(u'bash.log')

Mopy/bash/game/__init__.py
@@ -126,7 +126,9 @@ class GameInfo(object):
                           # cosave_ext instead.
         long_name = u''   # Full name
         exe = u''         # Exe to run
-        steam_exe = u''   # Exe to run if a steam install
+        ver_files = []    # List of file names to use for version detection.
+                          # Tried in order until one exists. Needed because
+                          # it's technically not required to install the EXE.
         plugin_dir = u''  # One level above the directory in which xSE plugins
                           # should be placed (e.g. when plugins should be in
                           # Data\OBSE\Plugins, this should be u'OBSE')

Mopy/bash/game/fallout3/__init__.py
@@ -59,7 +59,12 @@ class Fallout3GameInfo(GameInfo):
         se_abbrev = u'FOSE'
         long_name = u'Fallout 3 Script Extender'
         exe = u'fose_loader.exe'
-        steam_exe = u'fose_loader.dll'
+        # There is no fose_steam_loader.dll, so we have to list all included
+        # DLLs, since people could delete any DLL not matching the game version
+        # they're using
+        ver_files = [u'fose_loader.exe', u'fose_1_7ng.dll', u'fose_1_7.dll',
+                     u'fose_1_6.dll', u'fose_1_5.dll', u'fose_1_4b.dll',
+                     u'fose_1_4.dll', u'fose_1_1.dll', u'fose_1_0.dll']
         plugin_dir = u'FOSE'
         cosave_ext = u'.fose'
         url = u'http://fose.silverlock.org/'

Mopy/bash/game/fallout4/__init__.py
@@ -77,7 +77,7 @@ class Fallout4GameInfo(GameInfo):
         se_abbrev = u'F4SE'
         long_name = u'Fallout 4 Script Extender'
         exe = u'f4se_loader.exe'
-        steam_exe = u'f4se_steam_loader.dll'
+        ver_files = [u'f4se_loader.exe', u'f4se_steam_loader.dll']
         plugin_dir = u'F4SE'
         cosave_ext = u'.f4se'
         url = u'http://f4se.silverlock.org/'

Mopy/bash/game/falloutnv/__init__.py
@@ -47,7 +47,7 @@ class FalloutNVGameInfo(Fallout3GameInfo):
         se_abbrev = u'NVSE'
         long_name = u'Fallout Script Extender'
         exe = u'nvse_loader.exe'
-        steam_exe = u'nvse_loader.dll'
+        ver_files = [u'nvse_loader.exe', u'nvse_steam_loader.dll']
         plugin_dir = u'NVSE'
         cosave_ext = u'.nvse'
         url = u'http://nvse.silverlock.org/'

Mopy/bash/game/oblivion/__init__.py
@@ -64,7 +64,9 @@ class OblivionGameInfo(GameInfo):
         se_abbrev = u'OBSE'
         long_name = u'Oblivion Script Extender'
         exe = u'obse_loader.exe'
-        steam_exe = u'obse_1_2_416.dll'
+        # Not sure why we need obse_1_2_416.dll, was there before refactoring
+        ver_files = [u'obse_loader.exe', u'obse_steam_loader.dll',
+                     u'obse_1_2_416.dll']
         plugin_dir = u'OBSE'
         cosave_ext = u'.obse'
         url = u'http://obse.silverlock.org/'

commit 105551ef0571d179b245d7639052d4af30e6c9b8

Fix BAIN wizards failing on compareSEVersion
Broke way back in 851a6a01db744a8fd26fbf90490c6720e44ce66d, what a
throwback. Watch me fail to understand that this returns a list with two
values, and that only the first one matters!

Also took this opportunity to clean up version handling in belt, getting
rid of all the float and str comparisons of versions in favor of the proper
solution: bolt.LooseVersion.

Mopy/bash/_games_lo.py:
Fix WB not fixing LOs with too many actives

Not sure if this was always broken, but it was easy to reproduce: Create
>255 dummy plugins, open plugins.txt in a text editor and enable the
dummy plugins in there. Then tab into WB and watch it get stuck in an
infinite loop of showing warning popups about correcting the incorrect
LO.

Mopy/Docs/Wrye Bash Version History.html:
Add the 32-bit/Win7 notes to the version history

We already have these on the GitHub releases page and on the Nexus
pages, so we should list them in the version history for ease of access
too.

Mopy/Docs/Wrye Bash Version History.html
@@ -57,6 +57,7 @@
 </ul>
 
 <h3>309.1 Bug fixes for 309 [2021/06/09] [Infernio]</h3>
+<p><strong>Note: this is the last release to support Windows 7!</strong></p>
 <p>This release fixes a few critical bugs and regressions that made
 their way into 309.</p>
 
@@ -97,6 +98,7 @@ their way into 309.</p>
 </ul>
 
 <h3>307 Refactoring, new features &amp; games [2020/09/16] [Various community members]</h3>
+<p><strong>Note: this is the last release to support 32-bit operating systems!</strong></p>
 <ul>
 <li><a href="https://github.com/wrye-bash/wrye-bash/issues/528">#528</a>: Import Enchantment Stats &amp; Import Effect Stats [Infernio]</li>
 <li><a href="https://github.com/wrye-bash/wrye-bash/issues/527">#527</a>: Import Cells causes duplicate CELLs to be written into the BP [Infernio]</li>

Mopy/bash/_games_lo.py
@@ -138,7 +138,8 @@ class FixInfo(object):
         return bool(
             self.act_removed or self.act_added or self.act_duplicates or
             self.act_reordered or self.act_order_differs_from_load_order or
-            self.master_not_active or self.missing_must_be_active)
+            self.master_not_active or self.missing_must_be_active or
+            self.selectedExtra)
 
     def lo_deprint(self):
         self.warn_lo()

Mopy/bash/belt.py
@@ -29,7 +29,7 @@ from collections import OrderedDict, defaultdict
 
 from . import ScriptParser, bass, bolt, bosh, bush, load_order
 from .balt import ItemLink, Links, images, staticBitmap
-from .bolt import FName, FNDict
+from .bolt import FName, FNDict, LooseVersion
 from .env import get_file_version, get_game_version_fallback
 from .exception import AbstractError
 from .gui import CENTER, RIGHT, CheckBox, CheckListBox, GridLayout, \
@@ -975,7 +975,8 @@ class WryeParser(ScriptParser.Parser):
             for ver_file in bush.game.Se.ver_files:
                 ver_path = bass.dirs[u'app'].join(ver_file)
                 if ver_path.exists(): break
-            return self._TestVersion(self._TestVersion_Want(seWant), ver_path)
+            ret = self._TestVersion(self._TestVersion_Want(seWant), ver_path)
+            return ret[0]
         else:
             # No script extender available for this game
             return 1
@@ -990,7 +991,7 @@ class WryeParser(ScriptParser.Parser):
 
     def fnCompareWBVersion(self, wbWant):
         wbHave = bass.AppVersion
-        return bolt.cmp_(float(wbHave), float(wbWant))
+        return bolt.cmp_(LooseVersion(wbHave), LooseVersion(wbWant))
 
     def fnDataFileExists(self, *filenames):
         for filename in filenames:
@@ -1533,11 +1534,7 @@ class WryeParser(ScriptParser.Parser):
         else:
             bGEOk = True
             geHave = u'None'
-        try:
-            bWBOk = float(wbHave) >= float(wbWant)
-        except ValueError:
-            # Error converting to float, just assume it's OK
-            bWBOk = True
+        bWBOk = LooseVersion(wbHave) >= LooseVersion(wbWant)
         if not bGameOk or not bSEOk or not bGEOk or not bWBOk:
             self.page = PageVersions(self._wiz_parent, bGameOk, gameHave, game,
                                      bSEOk, seHave, se, bGEOk, geHave, ge,
@@ -1566,18 +1563,20 @@ class WryeParser(ScriptParser.Parser):
         if not have and file_ and file_.exists():
             have = get_file_version(file_.s)
         if have:
-            ver = u'.'.join([str(i) for i in have])
-            if need == u'None':
-                return [1, ver]
-            elif have > need:
-                return [1, ver]
-            elif have < need:
-                return [-1, ver]
+            have_fmt = '.'.join([str(i) for i in have])
+            if need == 'None':
+                return [1, have_fmt]
+            need_ver = LooseVersion('.'.join([str(i) for i in need]))
+            have_ver = LooseVersion(have_fmt)
+            if have_ver > need_ver:
+                return [1, have_fmt]
+            elif have_ver < need_ver:
+                return [-1, have_fmt]
             else:
-                return [0, ver]
-        elif need == u'None':
-            return [0, u'None']
-        return [-1, u'None']
+                return [0, have_fmt]
+        elif need == 'None':
+            return [0, 'None']
+        return [-1, 'None']
 
     def kwdReturn(self):
         self.page = PageFinish(self._wiz_parent, self.sublist,