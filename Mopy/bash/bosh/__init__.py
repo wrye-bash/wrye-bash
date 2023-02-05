wrye-bash/wrye-bash

commit e22cb3acd97a3015b9ae888ad67c6beea3bce11b

Rewrite lo_activate_exact to keep LO invariants
This method has always confused me because it's the only place in Wrye
Bash that can violate one of the key invariants of load order:

  Active(p) => ∀m ∈ masters(p): Active(m)

(i.e. if p is active, then all its masters must be active too)

The only other way to violate this invariant I can think of is to have
an active plugin with missing masters, but then you have an even bigger
problem (namely the game CTDing on the main menu) - and we rightfully
mark that situation with a giant red background! I can't fathom what
possible use case there is for activating a plugin without activating
its masters. It breaks the BP for one thing, since e.g. MobCell and
friends kind of rely on the masters having been loaded (see recent
tracebacks of the BP crashing with merged plugins that have inactive
masters - same thing happens here too, and fix is the same (i.e.
activating the darn masters)).

Also rename Activate Selected to Activate Only Selected to make it clear
that it doesn't just activate the selected plugins but *only* the
selected plugins.

Mopy/bash/bosh/__init__.py
@@ -2612,6 +2612,8 @@ class ModInfos(FileInfos):
         """Mutate _active_wip cache then save if needed."""
         if _activated is None: _activated = set()
         # Skip .esu files, those can't be activated
+        ##: This .esu handling needs to be centralized - sprinkled all over
+        # actives related lo_* methods
         if fileName.fn_ext == u'.esu': return []
         try:
             espms_extra, esls_extra = load_order.check_active_limit(
@@ -2711,36 +2713,47 @@ class ModInfos(FileInfos):
         finally:
             if wip_actives: self.cached_lo_save_active(active=wip_actives)
 
-    def lo_activate_exact(self, modNames):
-        """Activate exactly the specified set of mods."""
-        modsSet, all_mods = set(modNames), set(self)
-        #--Ensure plugins that cannot be deselected stay selected
-        modsSet.update(load_order.must_be_active_if_present() & all_mods)
-        #--Deselect/select plugins
-        missingSet = modsSet - all_mods
-        toSelect = modsSet - missingSet
-        listToSelect = load_order.get_ordered(toSelect)
-        skipped_esms, skipped_esls = load_order.check_active_limit(
-            listToSelect)
-        skipped = skipped_esls | skipped_esms
-        #--Save
-        if skipped:
-            listToSelect = [x for x in listToSelect if x not in skipped]
-        self.cached_lo_save_active(active=listToSelect)
-        #--Done/Error Message
-        message = u''
-        if missingSet:
-            message += _(u'Some plugins could not be found and were '
-                         u'skipped:') + u'\n* '
-            message += u'\n* '.join(missingSet)
-        if skipped:
-            if missingSet: message += u'\n'
-            message += _(u'Load order is full, so some plugins were '
-                         u'skipped:') + u'\n* '
-            message += u'\n* '.join(skipped)
+    def lo_activate_exact(self, partial_actives: Iterable[FName]):
+        """Activate exactly the specified iterable of plugin names (plus
+        required masters and plugins that can't be deactivated). May contain
+        missing plugins. Returns an error/warning message or an empty
+        string."""
+        partial_set = set(partial_actives)
+        missing_plugins = partial_set - set(self)
+        wip_actives = partial_set - missing_plugins
+        def _add_masters(target_plugin):
+            """Recursively adds the target and its masters (and their masters,
+            and so on)."""
+            wip_actives.add(target_plugin)
+            for tp_master in self[target_plugin].masterNames:
+                if tp_master in self:
+                    _add_masters(tp_master)
+        # Expand the WIP actives to include all masters and required plugins
+        for present_plugin in list(wip_actives):
+            if present_plugin.fn_ext != '.esu':
+                _add_masters(present_plugin)
+        wip_actives |= load_order.must_be_active_if_present()
+        # Sort the result and check if we would hit an actives limit
+        ordered_wip = load_order.get_ordered(wip_actives)
+        trim_regular, trim_esl = load_order.check_active_limit(ordered_wip)
+        trimmed_plugins = trim_regular | trim_esl
+        # Trim off any excess plugins and commit
+        self._active_wip = [p for p in ordered_wip if p not in trimmed_plugins]
+        self.cached_lo_save_active()
+        message = ''
+        if missing_plugins:
+            message += _('Some plugins could not be found and were '
+                         'skipped:') + '\n* '
+            message += '\n* '.join(load_order.get_ordered(missing_plugins))
+        if trimmed_plugins:
+            if missing_plugins:
+                message += '\n'
+            message += _('Plugin list is full, so some plugins were '
+                         'skipped:') + '\n* '
+            message += '\n* '.join(load_order.get_ordered(trimmed_plugins))
         return message
 
-    def lo_reorder(self, partial_order):
+    def lo_reorder(self, partial_order: list[FName]):
         """Changes the load order to match the specified potentially invalid
         'partial' load order as much as possible. To that end, it filters out
         plugins that don't exist in the Data folder and tries to insert plugins

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -1401,10 +1401,9 @@ tab. The directories skipped are:
                     activate any mergeable plugins.</td>
             </tr>
             <tr>
-                <td>Activate Selected</td>
-                <td>Activate only the currently selected plugins.
-                    <strong>Warning: their masters won't be
-                        activated!</strong></td>
+                <td>Activate Only Selected</td>
+                <td>Activate only the currently selected plugins (and their
+                    masters).</td>
             </tr>
             <tr>
                 <td>Deactivate All</td>

Mopy/bash/basher/mods_links.py
@@ -127,8 +127,8 @@ class _Mods_DeactivateAll(_AMods_ActivePlugins):
     def Execute(self):
         self._select_exact([])
 
-class _Mods_ActivateSelected(_AMods_ActivePlugins):
-    _text = _('Activate Selected')
+class _Mods_ActivateOnlySelected(_AMods_ActivePlugins):
+    _text = _('Activate Only Selected')
     _help = _('Activate only the currently selected plugins.')
 
     def Execute(self):
@@ -205,7 +205,7 @@ class Mods_ActivePlugins(MenuLink):
         super().__init__()
         self.append(_Mods_ActivateAll())
         self.append(_Mods_ActivateNonMergeable())
-        self.append(_Mods_ActivateSelected())
+        self.append(_Mods_ActivateOnlySelected())
         self.append(_Mods_DeactivateAll())
         self.append(SeparatorLink())
         self.append(_Mods_SaveActivePluginsList(self))

commit 99d688de6d27fda205ed5078abc8b6b97f319cd1

Fix lo_activate_exact in SSE without AE plugins
Problem is that I was unconditionally activating everything in
must_be_active_if_present(), but those must only be active *if present*,
as the name implies.

Broke in e22cb3acd97a3015b9ae888ad67c6beea3bce11b.

Mopy/bash/bosh/mods_metadata.py:
Improve display of records in Plugin Checker

Was showing

 [b'REFR':00071A4A] from Oblivion.esm (base record)

when it should be

[REFR:00071A4A] from Oblivion.esm (base record)

Note the extraneous leading space that got fixed as well.

Mopy/bash/bosh/__init__.py
@@ -2755,7 +2755,8 @@ class ModInfos(FileInfos):
         missing plugins. Returns an error/warning message or an empty
         string."""
         partial_set = set(partial_actives)
-        missing_plugins = partial_set - set(self)
+        present_plugins = set(self)
+        missing_plugins = partial_set - present_plugins
         wip_actives = partial_set - missing_plugins
         def _add_masters(target_plugin):
             """Recursively adds the target and its masters (and their masters,
@@ -2768,7 +2769,8 @@ class ModInfos(FileInfos):
         for present_plugin in list(wip_actives):
             if present_plugin.fn_ext != '.esu':
                 _add_masters(present_plugin)
-        wip_actives |= load_order.must_be_active_if_present()
+        wip_actives |= (load_order.must_be_active_if_present() &
+                        present_plugins)
         # Sort the result and check if we would hit an actives limit
         ordered_wip = load_order.get_ordered(wip_actives)
         trim_regular, trim_esl = load_order.check_active_limit(ordered_wip)

Mopy/bash/bosh/mods_metadata.py
@@ -499,7 +499,9 @@ def checkMods(mc_parent, modInfos, showModList=False, showCRC=False,
             log(u'* ' + _(u'%s from %s, colliding versions:')
                 % (proper_fid, coll_plugin))
         for ver_eid, ver_sig, ver_orig_plugin in coll_versions:
-            fmt_record = u'%s [%s:%s]' % (ver_eid, ver_sig, proper_fid)
+            fmt_record = f'[{sig_to_str(ver_sig)}:{proper_fid}]'
+            if ver_eid:
+                fmt_record = f'{ver_eid} {fmt_record}'
             # Mark the base record if the record wasn't injected
             if not coll_inj and ver_orig_plugin == coll_plugin:
                 log(u'  * ' + _(u'%s from %s (base record)') % (