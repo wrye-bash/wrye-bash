f817236786850eccdd60a3151335ade5a9c53dee
Infernio <infernio@icloud.com>
2023-02-13 14:52:19 +0100

    Amend copyright for 2023

--- scripts/utils.py
+++ scripts/utils.py
@@ -17,7 +17,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with Wrye Bash.  If not, see <https://www.gnu.org/licenses/>.
 #
-#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2022 Wrye Bash Team
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2023 Wrye Bash Team
 #  https://github.com/wrye-bash
 #
 # =============================================================================
