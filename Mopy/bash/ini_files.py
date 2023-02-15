wrye-bash/wrye-bash

commit cb9de8be5983da1ff15fbfa2eb01e6584ee1f5d1

Support TOML files on the INI Edits tab
This format is (mostly) a standardized version of INIs. It does contain
some complicated syntax that makes it more like a hybrid between YAML
and INI, but we can pretty much ignore that for now since all the mods
that have started using it use it like a bog standard INI file.

Also contains support for inline comments in INI files, since TOML needs
that.

Closes #661

Mopy/bash/ini_files.py
@@ -20,6 +20,7 @@
 #  https://github.com/wrye-bash
 #
 # =============================================================================
+import os
 import re
 import time
 from collections import OrderedDict, Counter
@@ -32,6 +33,9 @@ from .exception import AbstractError, CancelError, SkipError, \
     FailedIniInferError
 
 _comment_start_re = re.compile(r'^\s*[;#]\s*')
+
+# All extensions supported by this parser
+supported_ini_exts = {'.ini', '.cfg', '.toml'}
 
 def _to_lower(ini_settings):
     """Transforms dict of dict to LowerDict of LowerDict, respecting
@@ -53,6 +57,9 @@ def get_ini_type_and_encoding(abs_ini_path, fallback_type=None):
 
     :param fallback_type: If set, then if the INI type can't be detected,
         instead of raising an error, use this type."""
+    if os.path.splitext(abs_ini_path)[1] == '.toml':
+        # TOML must always use UTF-8, demanded by its specification
+        return TomlFile, 'utf-8'
     with open(abs_ini_path, u'rb') as ini_file:
         content = ini_file.read()
     detected_encoding, _confidence = getbestencoding(content)
@@ -93,12 +100,14 @@ class AIniFile(ListInfo):
     """ListInfo displayed on the ini tab - currently default tweaks or
     ini files, either standard or xSE ones."""
     reComment = re.compile('[;#].*')
-    reDeletedSetting = re.compile(r';-\s*(\w.*?)\s*(;.*$|=.*$|$)')
+    reDeletedSetting = re.compile(r'^\s*[;#]-\s*(\w.*?)\s*([;#].*$|=.*$|$)')
     reSection = re.compile(r'^\[\s*(.+?)\s*\]$')
-    reSetting = re.compile(r'(.+?)\s*=(.*)')
+    reSetting = re.compile(r'^\s*(.+?)\s*=\s*(.+?)(\s*[;#].*)?$')
     formatRes = (reSetting, reSection)
     out_encoding = 'cp1252' # when opening a file for writing force cp1252
     defaultSection = u'General'
+    # The comment character to use when writing new comments into this file
+    _comment_char = ';'
 
     def __init__(self, filekey):
         ListInfo.__init__(self, filekey)
@@ -176,9 +185,8 @@ class AIniFile(ListInfo):
         section = self.__class__.defaultSection
         for i, line in enumerate(tweak_file.read_ini_content()):
             maDeletedSetting = reDeleted.match(line)
-            stripped = reComment.sub(u'', line).strip()
-            maSection = reSection.match(stripped)
-            maSetting = reSetting.match(stripped)
+            maSection = reSection.match(line)
+            maSetting = reSetting.match(line)
             deleted = False
             setting = None
             value = u''
@@ -212,7 +220,7 @@ class AIniFile(ListInfo):
                     lineNo = ci_deletedSettings[section][setting]
                 deleted = True
             else:
-                if stripped:
+                if reComment.sub('', line).strip():
                     status = -10
             lines.append((line, section, setting, value, status, lineNo,
                           deleted))
@@ -305,34 +313,30 @@ class IniFile(AIniFile, AFile):
         ci_deleted_settings = DefaultLowerDict(LowerDict)
         default_section = self.__class__.defaultSection
         isCorrupted = u''
-        reComment = self.__class__.reComment
         reSection = self.__class__.reSection
         reDeleted = self.__class__.reDeletedSetting
         reSetting = self.__class__.reSetting
         #--Read ini file
-        with tweakPath.open(u'r', encoding=self.ini_encoding) as iniFile:
-            sectionSettings = None
-            section = None
-            for i,line in enumerate(iniFile.readlines()):
-                maDeleted = reDeleted.match(line)
-                stripped = reComment.sub(u'',line).strip()
-                maSection = reSection.match(stripped)
-                maSetting = reSetting.match(stripped)
-                if maSection:
-                    section = maSection.group(1)
-                    sectionSettings = ci_settings[section]
-                elif maSetting:
-                    if sectionSettings is None:
-                        sectionSettings = ci_settings[default_section]
-                        msg = _("Your %(tweak_ini)s should begin with a "
-                                "section header (e.g. '[General]'), but it "
-                                "does not.")
-                        isCorrupted = msg % {'tweak_ini': tweakPath}
-                    sectionSettings[maSetting.group(1)] = maSetting.group(
-                        2).strip(), i
-                elif maDeleted:
-                    if not section: continue
-                    ci_deleted_settings[section][maDeleted.group(1)] = i
+        sectionSettings = None
+        section = None
+        for i, line in enumerate(self.read_ini_content()):
+            maSection = reSection.match(line)
+            maDeleted = reDeleted.match(line)
+            maSetting = reSetting.match(line)
+            if maSection:
+                section = maSection.group(1)
+                sectionSettings = ci_settings[section]
+            elif maSetting:
+                if sectionSettings is None:
+                    sectionSettings = ci_settings[default_section]
+                    msg = _("Your %(tweak_ini)s should begin with a section "
+                            "header (e.g. '[General]'), but it does not.")
+                    isCorrupted = msg % {'tweak_ini': tweakPath}
+                sectionSettings[maSetting.group(1)] = (
+                    maSetting.group(2).strip(), i)
+            elif maDeleted:
+                if not section: continue
+                ci_deleted_settings[section][maDeleted.group(1)] = i
         return ci_settings, ci_deleted_settings, isCorrupted
 
     # Modify ini file ---------------------------------------------------------
@@ -346,63 +350,68 @@ class IniFile(AIniFile, AFile):
         u'The target ini must exist to apply a tweak to it.')):
         return self.abs_path.is_file()
 
-    def saveSettings(self,ini_settings,deleted_settings={}):
+    def saveSettings(self, ini_settings, deleted_settings=None):
         """Apply dictionary of settings to ini file, latter must exist!
         Values in settings dictionary must be actual (setting, value) pairs."""
         ini_settings = _to_lower(ini_settings)
         deleted_settings = LowerDict((x, {CIstr(u) for u in y}) for x, y in
-                                     deleted_settings.items())
+                                     (deleted_settings or {}).items())
         reDeleted = self.reDeletedSetting
-        reComment = self.reComment
         reSection = self.reSection
         reSetting = self.reSetting
         #--Read init, write temp
         section = None
         sectionSettings = {}
-        with self._open_for_writing() as tmpFile:
-            tmpFileWrite = tmpFile.write
+        with self._open_for_writing() as tmp_ini:
             def _add_remaining_new_items():
                 if section in ini_settings: del ini_settings[section]
                 if not sectionSettings: return
                 for sett, val in sectionSettings.items():
-                    tmpFileWrite(f'{sett}={val}\n')
-                tmpFileWrite(u'\n')
+                    tmp_ini.write(f'{self._fmt_setting(sett, val)}\n')
+                tmp_ini.write('\n')
             for line in self.read_ini_content(as_unicode=True):
-                stripped = reComment.sub(u'', line).strip()
-                maSection = reSection.match(stripped)
+                maSection = reSection.match(line)
                 if maSection:
                     # 'new' entries still to be added from previous section
                     _add_remaining_new_items()
                     section = maSection.group(1)  # entering new section
                     sectionSettings = ini_settings.get(section, {})
                 else:
-                    match = reSetting.match(stripped) or reDeleted.match(
-                        line)  # note we run maDeleted on LINE
-                    if match:
+                    match_set = reSetting.match(line)
+                    match_del = reDeleted.match(line)
+                    if match := (match_set or match_del):
+                        ##: What about inline comments in deleted lines?
+                        comment = match_set.group(3) if match_set else ''
                         setting = match.group(1)
                         if setting in sectionSettings:
                             value = sectionSettings[setting]
-                            line = f'{setting}={value}'
+                            line = self._fmt_setting(setting, value)
+                            if comment:
+                                line += comment # preserve inline comments
                             del sectionSettings[setting]
                         elif section in deleted_settings and setting in deleted_settings[section]:
-                            line = u';-' + line
-                tmpFileWrite(line + u'\n')
+                            line = f'{self._comment_char}-{line}'
+                tmp_ini.write(f'{line}\n')
             # This will occur for the last INI section in the ini file
             _add_remaining_new_items()
             # Add remaining new entries
             for section, sectionSettings in list(ini_settings.items()):
                 # _add_remaining_new_items may modify ini_settings
                 if sectionSettings:
-                    tmpFileWrite(f'[{section}]\n')
+                    tmp_ini.write(f'[{section}]\n')
                     _add_remaining_new_items()
         #--Done
         self.abs_path.untemp()
+
+    def _fmt_setting(self, setting, value):
+        """Format a key-value setting appropriately for the current INI
+        format."""
+        return f'{setting}={value}'
 
     def applyTweakFile(self, tweak_lines):
         """Read ini tweak file and apply its settings to self (the target ini).
         """
         reDeleted = self.reDeletedSetting
-        reComment = self.reComment
         reSection = self.reSection
         reSetting = self.reSetting
         #--Read Tweak file
@@ -410,10 +419,9 @@ class IniFile(AIniFile, AFile):
         deleted_settings = DefaultLowerDict(set)
         section = None
         for line in tweak_lines:
+            maSection = reSection.match(line)
             maDeleted = reDeleted.match(line)
-            stripped = reComment.sub(u'',line).strip()
-            maSection = reSection.match(stripped)
-            maSetting = reSetting.match(stripped)
+            maSetting = reSetting.match(line)
             if maSection:
                 section = maSection.group(1)
             elif maSetting:
@@ -429,7 +437,6 @@ class IniFile(AIniFile, AFile):
         this will only remove the first matching section. If you want to remove
         multiple, you will have to call this in a loop and check if the section
         still exists after each iteration."""
-        re_comment = self.reComment
         re_section = self.reSection
         # Tri-State: If None, we haven't hit the section yet. If True, then
         # we've hit it and are actively removing it. If False, then we've fully
@@ -437,8 +444,7 @@ class IniFile(AIniFile, AFile):
         remove_current = None
         with self._open_for_writing() as out:
             for line in self.read_ini_content(as_unicode=True):
-                stripped = re_comment.sub(u'', line).strip()
-                match_section = re_section.match(stripped)
+                match_section = re_section.match(line)
                 if match_section:
                     section = match_section.group(1)
                     # Check if we need to remove this section
@@ -472,8 +478,6 @@ class DefaultIniFile(AIniFile):
                 current_line += 1
 
     def get_ci_settings(self, with_deleted=False):
-        """Trivial override to avoid the if checks in parent (that would
-        return False anyway)."""
         if with_deleted:
             return self._ci_settings_cache_linenum, self._deleted_cache
         return self._ci_settings_cache_linenum
@@ -486,6 +490,20 @@ class DefaultIniFile(AIniFile):
             return iter(self.lines) # do not modify return value directly
         # Add a newline at the end of the INI
         return b'\r\n'.join(li.encode('ascii') for li in self.lines) + b'\r\n'
+
+class TomlFile(IniFile):
+    """A TOML file. Encoding is always UTF-8 (demanded by spec). Note that
+    ini_files only supports INI-like TOML files right now. That means TOML
+    files must be tables of key-value pairs and the values may not be arrays or
+    inline tables."""
+    out_encoding = 'utf-8' # see above
+    reComment = re.compile('#.*')
+    reDeletedSetting = re.compile(r'^\s*#-\s*(\w.*?)\s*(#.*$|=.*$|$)')
+    reSetting = re.compile(r'^\s*(.+?)\s*=\s*(.+?)(\s*#.*)?$')
+    _comment_char = '#'
+
+    def _fmt_setting(self, setting, value):
+        return f'{setting} = {value}'
 
 class OBSEIniFile(IniFile):
     """OBSE Configuration ini file.  Minimal support provided, only can
@@ -584,12 +602,12 @@ class OBSEIniFile(IniFile):
                           bool(maDeleted)))
         return lines
 
-    def saveSettings(self,ini_settings,deleted_settings={}):
+    def saveSettings(self, ini_settings, deleted_settings=None):
         """Apply dictionary of settings to self, latter must exist!
         Values in settings dictionary can be either actual values or
         full ini lines ending in newline char."""
         ini_settings = _to_lower(ini_settings)
-        deleted_settings = _to_lower(deleted_settings)
+        deleted_settings = _to_lower(deleted_settings or {})
         reDeleted = self.reDeleted
         reComment = self.reComment
         with self._open_for_writing() as tmpFile:

Mopy/Docs/Wrye Bash General Readme.html
@@ -1543,6 +1543,11 @@ Just like the game's masters, this load order is hardcoded, and so it cannot be 
 <p>The Ini Edits tab allows the user to quickly apply edits to the games's INI file (e.g. Oblivion.ini, Skyrim.ini, Fallout4Custom.ini, etc.) depending on your game, and mod-added ini files, including those containing scripts used to configure mods. Edits are applied from ini tweak files, which are just a text file that contains only the lines of the ini that you wish to change. Ini tweaks are stored in the <code>Data\INI Tweaks</code> folder, and may be installed through BAIN or placed there manually.
 <p>The drop-down box allows you to select the current ini file. The <q>Browse...</q> option in the drop-down box can be used to select an ini file not listed.
 <p>Selecting a tweak will display its contents in the middle column, and highlights the changes it applies to the current ini file in the right column. To apply a tweak, right click it and select <a href="#tweakApply">Apply</a>.
+<p>Supported file types are <code>.cfg</code>, <code>.ini</code> and
+    <code>.toml</code>. Note that only "INI-like" TOML files are currently
+    supported. That means some arrays and nested tables won't be correctly
+    parsed. See the <a href="https://toml.io/en/v1.0.0">TOML spec</a> for more
+    information on this terminology.</p>
 <p><strong>Warning:</strong> Incorrect INI settings in the game INI can cause bugs, CTDs or other damage to your game. Advanced settings in particular tend to be machine-specific and should not be casually copied from one computer to another.</p>
 
 <h3 id="ini-symbols">What Symbols &amp; Colours Mean <a class="back2top" href="#contents">Back to top</a></h3>

Mopy/bash/basher/__init__.py
@@ -2123,10 +2123,13 @@ class INIDetailsPanel(_DetailsMixin, SashPanel):
         full_path = self.target_inis[selection]
         if full_path is None:
             # 'Browse...'
-            wildcard =  u'|'.join(
-                [_(u'Supported files') + u' (*.ini,*.cfg)|*.ini;*.cfg',
-                 _(u'INI files') + u' (*.ini)|*.ini',
-                 _(u'Config files') + u' (*.cfg)|*.cfg', ])
+            wildcard =  '|'.join([
+                _('Supported files') + ' (*.ini,*.cfg,*.toml)'
+                                       '|*.ini;*.cfg;*.toml',
+                _('INI files') + ' (*.ini)|*.ini',
+                _('Config files') + ' (*.cfg)|*.cfg',
+                _('TOML files') + ' (*.toml)|*.toml',
+            ])
             full_path = FileOpen.display_dialog(self, defaultDir=self.lastDir,
                                                 wildcard=wildcard)
             if full_path: self.lastDir = full_path.shead

Mopy/bash/bosh/__init__.py
@@ -57,7 +57,7 @@ from ..exception import AbstractError, ArgumentError, BoltError, BSAError, \
     SaveHeaderError, SkipError, StateError, SkippedMergeablePluginsError, \
     FailedIniInferError
 from ..ini_files import IniFile, OBSEIniFile, DefaultIniFile, GameIni, \
-    get_ini_type_and_encoding, AIniFile
+    get_ini_type_and_encoding, AIniFile, supported_ini_exts
 from ..mod_files import ModFile, ModHeaderReader
 
 # Singletons, Constants -------------------------------------------------------
@@ -1688,7 +1688,8 @@ class FileInfos(TableFileInfos):
 
 #------------------------------------------------------------------------------
 class INIInfo(IniFile, AINIInfo):
-    _valid_exts_re = r'(\.(?:ini))'
+    _valid_exts_re = r'(\.(?:' + u'|'.join(
+        x[1:] for x in supported_ini_exts) + '))'
 
     def _reset_cache(self, stat_tuple, load_cache):
         super(INIInfo, self)._reset_cache(stat_tuple, load_cache)
@@ -1758,13 +1759,15 @@ def ini_info_factory(fullpath, load_cache=u'Ignored', itsa_ghost=False):
     :param load_cache: dummy param used in INIInfos#new_info factory call
     :rtype: INIInfo"""
     inferred_ini_type, detected_encoding = get_ini_type_and_encoding(fullpath)
-    ini_info_type = (inferred_ini_type is IniFile and INIInfo) or ObseIniInfo
+    ini_info_type = (ObseIniInfo if inferred_ini_type == OBSEIniFile
+                     else INIInfo)
     return ini_info_type(fullpath, detected_encoding)
 
 class INIInfos(TableFileInfos):
     """:type _ini: IniFile
     :type data: dict[bolt.Path, IniInfo]"""
-    file_pattern = re.compile(r'\.ini$', re.I)
+    file_pattern = re.compile('|'.join(
+        f'\\{x}' for x in supported_ini_exts) + '$' , re.I)
 
     def __init__(self):
         self._default_tweaks = FNDict((k, DefaultIniInfo(k, v)) for k, v in

Mopy/bash/bosh/bain.py
@@ -47,7 +47,7 @@ from ..bolt import Path, deprint, round_size, GPath, SubProgress, CIstr, \
     forward_compat_path_to_fn, ListInfo
 from ..exception import AbstractError, ArgumentError, BSAError, CancelError, \
     InstallerArchiveError, SkipError, StateError, FileError
-from ..ini_files import OBSEIniFile
+from ..ini_files import OBSEIniFile, supported_ini_exts
 
 os_sep = os.path.sep
 
@@ -92,7 +92,8 @@ class Installer(ListInfo):
                 '.tgz', '.tar', '.gz', '.bz2', '.omod', '.fomod', '.tb2',
                 '.lzma', '.manifest', '.ckm', '.vortex_backup'}
     skipExts.update(set(readExts))
-    commonlyEditedExts = {'.txt', '.ini', '.cfg', '.xml'}
+    commonlyEditedExts = {'.cfg', '.ini', '.modgroups', '.toml', '.txt',
+                          '.xml'}
     #--Regular game directories - needs update after bush.game has been set
     dataDirsPlus = screenshot_dirs | bush.game.Bain.wrye_bash_data_dirs | {
         'docs'}
@@ -2393,7 +2394,7 @@ class InstallersData(DataStore):
         installing the new inis then call _editTweaks() to populate the tweaks.
         """
         dest_files = (x for x in destFiles
-                if x[-4:].lower() in (u'.ini', u'.cfg')
+                if x[-4:].lower() in supported_ini_exts
                 # don't create ini tweaks for overridden ini tweaks...
                 and os.path.split(x)[0].lower() != u'ini tweaks')
         for relPath in dest_files:
@@ -2403,7 +2404,7 @@ class InstallersData(DataStore):
             iniAbsDataPath = bass.dirs[u'mods'].join(relPath)
             # Create a copy of the old one
             co_path = f'{iniAbsDataPath.sbody}, ~Old Settings ' \
-                      f'[{installer.fn_key}].ini'
+                      f'[{installer.fn_key}]{iniAbsDataPath.cext}'
             baseName = bass.dirs[u'ini_tweaks'].join(co_path)
             tweakPath = self.__tweakPath(baseName)
             iniAbsDataPath.copyTo(tweakPath)
@@ -2570,8 +2571,14 @@ class InstallersData(DataStore):
     @staticmethod
     def _is_ini_tweak(ci_relPath):
         parts = ci_relPath.lower().split(os_sep)
-        return len(parts) == 2 and parts[0] == u'ini tweaks' \
-           and parts[1][-4:] == u'.ini' and FName(parts[1])
+        # 1. Must have a single parent folder
+        # 2. That folder must be named 'ini tweaks'
+        # 3. The extension must be a valid INI-like extension
+        # If all that is true, return the filename as an FName
+        return (len(parts) == 2 and
+                parts[0] == 'ini tweaks' and
+                parts[1].rsplit('.', 1)[1] in supported_ini_exts and
+                FName(parts[1]))
 
     def _removeFiles(self, ci_removes, refresh_ui, progress=None):
         """Performs the actual deletion of files and updating of internal data,

commit d6ca9084feab37038379b2060ff6fdb161061b85

ini_files: Fix skipping empty values
Hey kids, wanna see the world's worst regexes? God I wish Python
supported Perl's \h syntax for horizontal whitespace.

Regression introduced in cb9de8be5983da1ff15fbfa2eb01e6584ee1f5d1. Was
causing values like

 SIntroSequence=

to be skipped.

Mopy/bash/mod_files.py:
Add note to extract_mod_data

Actually slower when benchmarking on py3.11 with the "inlines" removed.
Weird.

Mopy/bash/ini_files.py
@@ -32,7 +32,7 @@ from .bolt import AFile, CIstr, DefaultLowerDict, ListInfo, LowerDict, \
 from .exception import AbstractError, CancelError, FailedIniInferError, \
     SkipError
 
-_comment_start_re = re.compile(r'^\s*[;#]\s*')
+_comment_start_re = re.compile(r'^[^\S\r\n]*[;#][^\S\r\n]*')
 
 # All extensions supported by this parser
 supported_ini_exts = {'.ini', '.cfg', '.toml'}
@@ -100,9 +100,12 @@ class AIniFile(ListInfo):
     """ListInfo displayed on the ini tab - currently default tweaks or
     ini files, either standard or xSE ones."""
     reComment = re.compile('[;#].*')
-    reDeletedSetting = re.compile(r'^\s*[;#]-\s*(\w.*?)\s*([;#].*$|=.*$|$)')
-    reSection = re.compile(r'^\[\s*(.+?)\s*\]$')
-    reSetting = re.compile(r'^\s*(.+?)\s*=\s*(.+?)(\s*[;#].*)?$')
+    # These are horrible - Perl's \h (horizontal whitespace) sorely missed
+    reDeletedSetting = re.compile(r'^[^\S\r\n]*[;#]-[^\S\r\n]*(\w.*?)'
+                                  r'[^\S\r\n]*([;#].*$|=.*$|$)')
+    reSection = re.compile(r'^\[[^\S\r\n]*(.+?)[^\S\r\n]*\]$')
+    reSetting = re.compile(r'^[^\S\r\n]*(.+?)[^\S\r\n]*=[^\S\r\n]*(.*?)'
+                           r'([^\S\r\n]*[;#].*)?$')
     formatRes = (reSetting, reSection)
     out_encoding = 'cp1252' # when opening a file for writing force cp1252
     defaultSection = u'General'
@@ -498,8 +501,10 @@ class TomlFile(IniFile):
     inline tables."""
     out_encoding = 'utf-8' # see above
     reComment = re.compile('#.*')
-    reDeletedSetting = re.compile(r'^\s*#-\s*(\w.*?)\s*(#.*$|=.*$|$)')
-    reSetting = re.compile(r'^\s*(.+?)\s*=\s*(.+?)(\s*#.*)?$')
+    reDeletedSetting = re.compile(r'^[^\S\r\n]*#-[^\S\r\n]*(\w.*?)[^\S\r\n]*'
+                                  r'(#.*$|=.*$|$)')
+    reSetting = re.compile(r'^[^\S\r\n]*(.+?)[^\S\r\n]*=[^\S\r\n]*(.+?)'
+                           r'([^\S\r\n]*#.*)?$')
     _comment_char = '#'
 
     def _fmt_setting(self, setting, value):
@@ -508,10 +513,13 @@ class TomlFile(IniFile):
 class OBSEIniFile(IniFile):
     """OBSE Configuration ini file.  Minimal support provided, only can
     handle 'set', 'setGS', and 'SetNumericGameSetting' statements."""
-    reDeleted = re.compile(r';-(\w.*?)$', re.U)
-    reSet     = re.compile(r'\s*set\s+(.+?)\s+to\s+(.*)', re.I | re.U)
-    reSetGS   = re.compile(r'\s*setGS\s+(.+?)\s+(.*)', re.I | re.U)
-    reSetNGS  = re.compile(r'\s*SetNumericGameSetting\s+(.+?)\s+(.*)', re.I | re.U)
+    reDeleted = re.compile(r';-(\w.*?)$')
+    reSet     = re.compile(r'[^\S\r\n]*set[^\S\r\n]+(.+?)[^\S\r\n]+to'
+                           r'[^\S\r\n]+(.*)', re.I)
+    reSetGS   = re.compile(r'[^\S\r\n]*setGS[^\S\r\n]+(.+?)[^\S\r\n]+'
+                           r'(.*)', re.I)
+    reSetNGS  = re.compile(r'[^\S\r\n]*SetNumericGameSetting[^\S\r\n]+(.+?)'
+                           r'[^\S\r\n]+(.*)', re.I)
     out_encoding = 'utf-8' # FIXME: ask
     formatRes = (reSet, reSetGS, reSetNGS)
     defaultSection = u'' # Change the default section to something that

Mopy/bash/mod_files.py
@@ -414,6 +414,8 @@ class ModHeaderReader(object):
         with mod_info.abs_path.open(u'rb') as ins:
             initial_bytes = ins.read()
         with FastModReader(plugin_fn, initial_bytes) as ins:
+            # PY3.12: Check if removing these is faster now - on py3.11 it is
+            # *slower*, even though it really should be faster!
             # More local methods to avoid repeated dot access
             ins_tell = ins.tell
             ins_seek = ins.seek