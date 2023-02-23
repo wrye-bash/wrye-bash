wrye-bash/wrye-bash

commit a3b3d790f0797dbd273080709f5764d9b9e109ab

Properly fix and implement Filter
This properly sticks all patched FormID attributes into the
_rec_fid_attrs fields, which is where they have to go - otherwise Import
Patchers will happily stick FormIDs belonging to missing masters into
the BP. Additionally, we have to filter inactive Filter plugins as well
to properly implement the new behavior of the Filter tag, otherwise we
could (again) end up with FormIDs belonging to missing masters in the
BP. Some open questions remain (see ##: comments introduced here) and
the upcoming initData refactoring will obsolete the extremely ugly
_filtered_mod_read hack.

Note that this was just barely possible. Due to the CSV nonsense with
Import Stats and Import Spell Stats, we can't change the tuples (aside
from renaming attributes, i.e. no adding, removing or reordering
attributes without backwards compat code in parsers). So splitting them
into two dicts, one for regulars and one for FormIDs would break that.
Thankfully there were only two places where these patchers used FormIDs,
both in Skyrim where we don't support reading/writing such CSVs yet.

And finally, this extends FormID filtering a bit more to fix all
imported data that consists of lists of FormIDs. And *finally finally*,
it also expands the FO3/FNV Sound tag a bit because I randomly noticed
it was missing (and as part of that, it fixes the hideous mess that was
the FNV-exclusive sound handling in the FO3 definitions).

Fix the _fid_rec_attrs/rec_type_attrs merge logic

Turns out this has been broken since introduction, but we never noticed
because we didn't use _fid_rec_attrs that much. Fixed it properly by
introducing a helper in bolt for merging dictionaries by combining their
values.

Closes #654, for good this time

Mopy/bash/game/fallout3/records.py
@@ -53,7 +53,8 @@ from ...brec import MelGroups, MelStruct, FID, MelGroup, \
     perk_distributor, MelInfoResponsesFo3, MelIpctTextureSets, MelIpctSounds, \
     MelLandShared, MelIdleAnimationCountOld, AMreCell, AMreWrld, gen_color, \
     gen_color3, MelLighFade, MelLtexGrasses, MelLtexSnam, MelLLFlags, \
-    MelLLChanceNone, MelLLGlobal, NavMeshFlags, VWDFlag
+    MelLLChanceNone, MelLLGlobal, NavMeshFlags, VWDFlag, MelCombatStyle, \
+    MelDeathItem, AMreWthr, AMreRace
 from ...brec import MelRecord as _BaseMelRecord
 from ...exception import ModSizeError
 
@@ -96,6 +97,21 @@ class aiService(Flags):
 #------------------------------------------------------------------------------
 # Record Elements -------------------------------------------------------------
 #------------------------------------------------------------------------------
+class AMreActorFo3(AMreActor):
+    """Creatures and NPCs."""
+    class TemplateFlags(Flags):
+        useTraits: bool
+        useStats: bool
+        useFactions: bool
+        useActorEffectList: bool
+        useAIData: bool
+        useAIPackages: bool
+        useModelAnimation: bool
+        useBaseData: bool
+        useInventory: bool
+        useScript: bool
+
+#------------------------------------------------------------------------------
 class MelModel(MelGroup):
     """Represents a model subrecord."""
     typeSets = {
@@ -134,21 +150,6 @@ class MelActivationPrompt(MelString):
     """Handles the common XATO subrecord, introduced in FNV."""
     def __init__(self):
         super().__init__(b'XATO', 'activation_prompt')
-
-#------------------------------------------------------------------------------
-class MreActor(AMreActor):
-    """Creatures and NPCs."""
-    class TemplateFlags(Flags):
-        useTraits: bool
-        useStats: bool
-        useFactions: bool
-        useActorEffectList: bool
-        useAIData: bool
-        useAIPackages: bool
-        useModelAnimation: bool
-        useBaseData: bool
-        useInventory: bool
-        useScript: bool
 
 #------------------------------------------------------------------------------
 class MelBipedData(MelStruct):
@@ -493,7 +494,7 @@ class MreAlch(MelRecord):
         MelWeight(),
         MelStruct(b'ENIT', [u'i', u'B', u'3s', u'I', u'f', u'I'], u'value', (_flags, u'flags'),
                   u'unused1', (FID, u'withdrawalEffect'),
-                  u'addictionChance', (FID, u'soundConsume')),
+                  'addictionChance', (FID, 'sound_consume')),
         MelEffectsFo3(),
     )
 
@@ -626,15 +627,27 @@ class MreAspc(MelRecord):
         MelBounds(),
         if_fnv(
             fo3_version=MelSound(),
-            # Technically five subrecords with the same signature, but it's
-            # easier to load them like this than with a distributor
-            fnv_version=MelFids('sound', MelFid(b'SNAM')),
+            fnv_version=MelSequential(
+                MelFid(b'SNAM', 'sound_dawn_default_loop'),
+                MelFid(b'SNAM', 'sound_afternoon'),
+                MelFid(b'SNAM', 'sound_dusk'),
+                MelFid(b'SNAM', 'sound_night'),
+                MelFid(b'SNAM', 'sound_walla'),
+            ),
         ),
         fnv_only(MelUInt32(b'WNAM', 'walla_trigger_count')),
         MelAspcRdat(),
         MelUInt32(b'ANAM', 'environment_type'),
         fnv_only(MelUInt32(b'INAM', 'aspc_is_interior')),
-    )
+    ).with_distributor(fnv_only({
+        b'SNAM': [
+            (b'SNAM', 'sound_dawn_default_loop'),
+            (b'SNAM', 'sound_afternoon'),
+            (b'SNAM', 'sound_dusk'),
+            (b'SNAM', 'sound_night'),
+            (b'SNAM', 'sound_walla'),
+        ],
+    }))
 
 #------------------------------------------------------------------------------
 class MreAvif(MelRecord):
@@ -893,7 +906,7 @@ class MreCpth(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreCrea(MreActor):
+class MreCrea(AMreActorFo3):
     """Creature."""
     rec_sig = b'CREA'
 
@@ -943,9 +956,9 @@ class MreCrea(MreActor):
         MelStruct(b'ACBS', [u'I', u'2H', u'h', u'3H', u'f', u'h', u'H'],(_flags, u'flags'),'fatigue',
             'barterGold',('level_offset',1),'calcMin','calcMax','speedMultiplier',
             'karma', 'dispositionBase',
-            (MreActor.TemplateFlags, 'templateFlags')),
+            (AMreActorFo3.TemplateFlags, 'templateFlags')),
         MelFactions(),
-        MelFid(b'INAM','deathItem'),
+        MelDeathItem(),
         MelFid(b'VTCK','voice'),
         MelFid(b'TPLT','template'),
         MelDestructible(),
@@ -963,7 +976,7 @@ class MreCrea(MreActor):
             'perception','endurance','charisma','intelligence','agility',
             'luck'),
         MelUInt8(b'RNAM', 'attackReach'),
-        MelFid(b'ZNAM','combatStyle'),
+        MelCombatStyle(),
         MelFid(b'PNAM','bodyPartData'),
         MelFloat(b'TNAM', 'turningSpeed'),
         MelFloat(b'BNAM', 'baseScale'),
@@ -1882,7 +1895,7 @@ class _MelNpcDecider(SizeDecider):
     def decide_dump(self, record):
         return len(record.attributes) + 4
 
-class MreNpc_(MreActor):
+class MreNpc_(AMreActorFo3):
     """Non-Player Character."""
     rec_sig = b'NPC_'
 
@@ -1920,9 +1933,9 @@ class MreNpc_(MreActor):
         MelStruct(b'ACBS', [u'I', u'2H', u'h', u'3H', u'f', u'2H'],
             (_flags, u'flags'),'fatigue','barterGold',
             ('level_offset',1),'calcMin','calcMax','speedMultiplier','karma',
-            'dispositionBase', (MreActor.TemplateFlags, u'templateFlags')),
+            'dispositionBase', (AMreActorFo3.TemplateFlags, u'templateFlags')),
         MelFactions(),
-        MelFid(b'INAM','deathItem'),
+        MelDeathItem(),
         MelFid(b'VTCK','voice'),
         MelFid(b'TPLT','template'),
         MelRace(),
@@ -1951,7 +1964,7 @@ class MreNpc_(MreActor):
         MelFloat(b'LNAM', u'hairLength'),
         MelFid(b'ENAM', 'eye'),
         MelStruct(b'HCLR', [u'3B', u's'],'hairRed','hairBlue','hairGreen','unused3'),
-        MelFid(b'ZNAM','combatStyle'),
+        MelCombatStyle(),
         MelUInt32(b'NAM4', u'impactMaterialType'),
         MelBase(b'FGGS','fggs_p'), ####FaceGen Geometry-Symmetric
         MelBase(b'FGGA','fgga_p'), ####FaceGen Geometry-Asymmetric
@@ -2082,7 +2095,7 @@ class MrePack(MelRecord):
         ),
         MelBase(b'PKED','eatMarker'),
         MelUInt32(b'PKE2', 'escortDistance'),
-        MelFid(b'CNAM','combatStyle'),
+        MelCombatStyle(b'CNAM'),
         MelFloat(b'PKFD', 'followStartLocationTrigerRadius'),
         MelBase(b'PKPT','patrolFlags'), # byte or short
         MelOptStruct(b'PKW3', [u'I', u'B', u'B', u'3H', u'f', u'f', u'4s'],'weaponFlags','fireRate','fireCount','numBursts',
@@ -2283,8 +2296,8 @@ class MreProj(MelRecord):
                     'tracerChance', 'explosionAltTrigerProximity',
                     'explosionAltTrigerTimer', (FID, 'explosion'),
                     (FID, 'sound'), 'muzzleFlashDuration', 'fadeDuration',
-                    'impactForce', (FID, 'soundCountDown'),
-                    (FID, 'soundDisable'), (FID, 'defaultWeaponSource')]
+                    'impactForce', (FID, 'sound_countdown'),
+                    (FID, 'sound_disable'), (FID, 'defaultWeaponSource')]
 
     melSet = MelSet(
         MelEdid(),
@@ -2407,7 +2420,7 @@ class MelRaceFaceGen(MelGroup):
             MelBase(b'FGTS', u'fgts_p'), # FaceGen Texture  - Symmetric
             MelStruct(b'SNAM', [u'2s'], u'snam_p'))
 
-class MreRace(MelRecord):
+class MreRace(AMreRace):
     """Race."""
     rec_sig = b'RACE'
 
@@ -2901,7 +2914,7 @@ class MreTerm(MelRecord):
         MelScript(),
         MelDestructible(),
         MelDescription(),
-        MelSound(), ##: Why aren't we patching this in Import Sounds?
+        MelSound(),
         MelFid(b'PNAM','passwordNote'),
         MelTruncatedStruct(b'DNAM', [u'3B', u's'], 'baseHackingDifficulty',
                            (_flags,'flags'), 'serverType', 'unused1',
@@ -3027,6 +3040,7 @@ class MreWatr(MelRecord):
         ('lightBrightness', 1), ('noiseLayer1UvScale', 100),
         ('noiseLayer2UvScale', 100), ('noiseLayer3UvScale', 100)]
     _fmts = [u'10f', u'3B', u's', u'3B', u's', u'3B', u's', u'I',]
+
     melSet = MelSet(
         MelEdid(),
         MelFull(),
@@ -3143,10 +3157,8 @@ class MreWeap(MelRecord):
             MelFid(b'WMI2', 'mod2'),
             MelFid(b'WMI3', 'mod3'),
         )),
-        if_fnv(
-            fo3_version=MelSound(),
-            fnv_version=MelFids('sound', MelFid(b'SNAM')),
-        ),
+        MelSound(),
+        fnv_only(MelFid(b'SNAM', 'sound_gun_shoot_dist')),
         MelFid(b'XNAM','soundGunShot2D'),
         MelFid(b'NAM7','soundGunShot3DLooping'),
         MelFid(b'TNAM','soundMeleeSwingGunNoAmmo'),
@@ -3193,7 +3205,15 @@ class MreWeap(MelRecord):
             'vats_mod_required', 'weapVats1', old_versions={'I3f'},
             is_optional=True)),
         MelBase(b'VNAM','soundLevel'),
-    )
+    ).with_distributor(fnv_only({
+        b'SNAM': ('sound', {
+            b'SNAM': 'sound_gun_shoot_dist',
+        }),
+    }))
+
+    def keep_fids(self, keep_plugins):
+        if _is_fnv:
+            self.sounds = [s for s in self.sounds if s.mod_fn in keep_plugins]
 
 #------------------------------------------------------------------------------
 class MreWrld(AMreWrld):
@@ -3301,7 +3321,7 @@ class MelWthrColorsFnv(MelArray):
                                self._old_sizes[sub_type])
             raise ModSizeError(ins.inName, debug_strs, _expected_sizes, size_)
 
-class MreWthr(MelRecord):
+class MreWthr(AMreWthr):
     """Weather."""
     rec_sig = b'WTHR'
 

Mopy/bash/game/falloutnv/__init__.py
@@ -321,7 +321,7 @@ class FalloutNVGameInfo(Fallout3GameInfo):
     #--------------------------------------------------------------------------
     # Import Stats
     #--------------------------------------------------------------------------
-    statsTypes = Fallout3GameInfo.statsTypes | {
+    stats_csv_attrs = Fallout3GameInfo.stats_csv_attrs | {
         b'AMMO': ('eid', 'weight', 'value', 'speed', 'clipRounds',
                   'projPerShot'),
         b'ARMA': ('eid', 'weight', 'value', 'health', 'dr', 'dt'),
@@ -340,17 +340,29 @@ class FalloutNVGameInfo(Fallout3GameInfo):
             'killImpulse', 'impulseDist', 'skillReq', 'criticalDamage',
             'criticalMultiplier', 'vatsSkill', 'vatsDamMult', 'vatsAp'),
     }
+    stats_attrs = {r: tuple(x for x in a if x != 'eid')
+                   for r, a in stats_csv_attrs.items()}
 
     #--------------------------------------------------------------------------
     # Import Sounds
     #--------------------------------------------------------------------------
-    soundsTypes = Fallout3GameInfo.soundsTypes | {
+    sounds_attrs = Fallout3GameInfo.sounds_attrs | {
+        b'STAT': ('passthroughSound',),
+    }
+    sounds_fid_attrs = Fallout3GameInfo.sounds_fid_attrs | {
+        b'ASPC': ('sound_dawn_default_loop', 'sound_afternoon', 'sound_dusk',
+                  'sound_night', 'sound_walla', 'use_sound_from_region'),
         b'CONT': ('sound', 'sound_close', 'sound_random_looping'),
-        b'WEAP': ('pickupSound', 'dropSound', 'sound', 'soundGunShot2D',
+        b'KEYM': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
+        b'MISC': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
+        b'STAT': ('sound_random_looping',),
+        b'WEAP': ('sound_pickup', 'sound_drop', 'sound',
+                  'sound_gun_shoot_dist', 'soundGunShot2D',
                   'soundGunShot3DLooping', 'soundMeleeSwingGunNoAmmo',
                   'soundBlock', 'idleSound', 'equipSound', 'unequipSound',
-                  'soundMod1Shoot3Ds', 'soundMod1Shoot2D', 'soundLevel'),
+                  'soundMod1Shoot3Ds', 'soundMod1Shoot2D'),
     }
+    sounds_fid_attrs[b'WEAP'] = ()
 
     #--------------------------------------------------------------------------
     # Import Graphics

commit 0e85cd457f08e06a280807d249f9ac3b9b21c2f6

FNV, TES5: Fix regressions in Sounds patcher
Broke in a3b3d790f0797dbd273080709f5764d9b9e109ab.

Infernio: More regression fixups and dropped a useless comment.

Co-authored-by: Infernio <infernio@icloud.com>

Mopy/bash/game/fallout3/records.py
@@ -3159,8 +3159,13 @@ class MreWeap(MelRecord):
             MelFid(b'WMI2', 'mod2'),
             MelFid(b'WMI3', 'mod3'),
         )),
-        MelSound(),
-        fnv_only(MelFid(b'SNAM', 'sound_gun_shoot_dist')),
+        if_fnv(
+            fo3_version=MelSound(),
+            fnv_version=MelSequential(
+                MelFid(b'SNAM', 'sound_gun_shoot_3d'),
+                MelFid(b'SNAM', 'sound_gun_shoot_dist'),
+            ),
+        ),
         MelFid(b'XNAM','soundGunShot2D'),
         MelFid(b'NAM7','soundGunShot3DLooping'),
         MelFid(b'TNAM','soundMeleeSwingGunNoAmmo'),
@@ -3168,8 +3173,9 @@ class MreWeap(MelRecord):
         MelFid(b'UNAM','idleSound',),
         MelFid(b'NAM9','equipSound',),
         MelFid(b'NAM8','unequipSound',),
-        fnv_only(MelFids('soundMod1Shoot3Ds', MelFid(b'WMS1'))),
-        fnv_only(MelFid(b'WMS2', 'soundMod1Shoot2D')),
+        fnv_only(MelFid(b'WMS1', 'sound_mod1_shoot_3d')),
+        fnv_only(MelFid(b'WMS1', 'sound_mod1_shoot_dist')),
+        fnv_only(MelFid(b'WMS2', 'sound_mod1_shoot_2d')),
         MelStruct(b'DATA', [u'2I', u'f', u'H', u'B'],'value','health','weight','damage','clipsize'),
         if_fnv(
             fo3_version=MelTruncatedStruct(
@@ -3208,14 +3214,15 @@ class MreWeap(MelRecord):
             is_optional=True)),
         MelBase(b'VNAM','soundLevel'),
     ).with_distributor(fnv_only({
-        b'SNAM': ('sound', {
-            b'SNAM': 'sound_gun_shoot_dist',
-        }),
+        b'SNAM': [
+            (b'SNAM', 'sound_gun_shoot_3d'),
+            (b'SNAM', 'sound_gun_shoot_dist'),
+        ],
+        b'WMS1': [
+            (b'WMS1', 'sound_mod1_shoot_3d'),
+            (b'WMS1', 'sound_mod1_shoot_dist'),
+        ],
     }))
-
-    def keep_fids(self, keep_plugins):
-        if _is_fnv:
-            self.sounds = [s for s in self.sounds if s.mod_fn in keep_plugins]
 
 #------------------------------------------------------------------------------
 class MreWrld(AMreWrld):

Mopy/bash/game/falloutnv/__init__.py
@@ -356,13 +356,13 @@ class FalloutNVGameInfo(Fallout3GameInfo):
         b'KEYM': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
         b'MISC': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
         b'STAT': ('sound_random_looping',),
-        b'WEAP': ('sound_pickup', 'sound_drop', 'sound',
+        b'WEAP': ('sound_pickup', 'sound_drop', 'sound_gun_shoot_3d',
                   'sound_gun_shoot_dist', 'soundGunShot2D',
                   'soundGunShot3DLooping', 'soundMeleeSwingGunNoAmmo',
                   'soundBlock', 'idleSound', 'equipSound', 'unequipSound',
-                  'soundMod1Shoot3Ds', 'soundMod1Shoot2D'),
+                  'sound_mod1_shoot_3d', 'sound_mod1_shoot_dist',
+                  'sound_mod1_shoot_2d'),
     }
-    sounds_fid_attrs[b'WEAP'] = ()
 
     #--------------------------------------------------------------------------
     # Import Graphics

Mopy/bash/game/falloutnv/patcher/preservers.py
@@ -30,12 +30,12 @@ class ImportWeaponModificationsPatcher(APreserver):
     _config_key = u'WeaponModsPatcher'
     patcher_order = 27 ##: This seems unneeded + no reason given
     rec_attrs = {b'WEAP': (
-        u'modelWithMods', u'firstPersonModelWithMods', u'weaponMods',
-        u'soundMod1Shoot3Ds', u'soundMod1Shoot2D', u'effectMod1',
-        u'effectMod2', u'effectMod3', u'valueAMod1', u'valueAMod2',
-        u'valueAMod3', u'valueBMod1', u'valueBMod2', u'valueBMod3',
-        u'reloadAnimationMod', u'vats_mod_required',
-        u'dnamFlags2.scopeFromMod')}
+        'modelWithMods', 'firstPersonModelWithMods', 'weaponMods',
+        'effectMod1', 'effectMod2', 'effectMod3', 'valueAMod1', 'valueAMod2',
+        'valueAMod3', 'valueBMod1', 'valueBMod2', 'valueBMod3',
+        'reloadAnimationMod', 'vats_mod_required', 'dnamFlags2.scopeFromMod')}
+    _fid_rec_attrs = {b'WEAP': ('sound_mod1_shoot_3d', 'sound_mod1_shoot_dist',
+                                'sound_mod1_shoot_2d')}
 
     @classmethod
     def gui_cls_vars(cls):

Mopy/bash/game/skyrim/__init__.py
@@ -754,7 +754,28 @@ class SkyrimGameInfo(PatchGame):
     #--------------------------------------------------------------------------
     # Import Sounds
     #--------------------------------------------------------------------------
-    soundsTypes = {
+    sounds_attrs = {
+        b'EXPL': ('expl_sound_level',),
+        b'IPCT': ('ipct_sound_level',),
+        ##: 'sounds' needs to be filtered
+        b'MGEF': ('casting_sound_level', 'sounds'),
+        b'PROJ': ('soundLevel',),
+        b'SNCT': ('staticVolumeMultiplier',),
+        # sound_files does not need to loop here
+        b'SNDR': ('sound_files', 'looping_type', 'rumble_send_value',
+                  'pct_frequency_shift', 'pct_frequency_variance',
+                  'descriptor_priority', 'db_variance', 'staticAtten'),
+        b'SOPM': ('reverbSendpct', 'outputType', 'ch0_l', 'ch0_r', 'ch0_c',
+                  'ch0_lFE', 'ch0_rL', 'ch0_rR', 'ch0_bL', 'ch0_bR', 'ch1_l',
+                  'ch1_r', 'ch1_c', 'ch1_lFE', 'ch1_rL', 'ch1_rR', 'ch1_bL',
+                  'ch1_bR', 'ch2_l', 'ch2_r', 'ch2_c', 'ch2_lFE', 'ch2_rL',
+                  'ch2_rR', 'ch2_bL', 'ch2_bR', 'minDistance', 'maxDistance',
+                  'curve1', 'curve2', 'curve3', 'curve4', 'curve5'),
+        b'WEAP': ('detectionSoundLevel',),
+        # Has FormIDs, but will be filtered in AMreWthr.keep_fids
+        b'WTHR': ('sounds',),
+    }
+    sounds_fid_attrs = {
         b'ACTI': ('sound', 'sound_activation'),
         b'ADDN': ('sound',),
         b'ALCH': ('sound_pickup', 'sound_drop', 'sound_consume'),
@@ -767,42 +788,27 @@ class SkyrimGameInfo(PatchGame):
         b'CONT': ('sound', 'sound_close'),
         b'DOOR': ('sound', 'sound_close', 'sound_looping'),
         b'EFSH': ('sound_ambient',),
-        b'EXPL': ('expl_sound_level', 'expl_sound1', 'expl_sound2'),
+        b'EXPL': ('expl_sound1', 'expl_sound2'),
         b'FLOR': ('sound',),
         b'HAZD': ('hazd_sound',),
         b'INGR': ('sound_pickup', 'sound_drop'),
-        b'IPCT': ('ipct_sound_level', 'sound', 'ipct_sound2'),
+        b'IPCT': ('sound', 'ipct_sound2'),
         b'KEYM': ('sound_pickup', 'sound_drop'),
         b'LIGH': ('sound',),
-        #Needs to loop over all the sounds
-        b'MGEF': ('sounds', 'casting_sound_level'),
-        # b'REGN': ('entries',),
         b'MISC': ('sound_pickup', 'sound_drop'),
         b'MSTT': ('sound',),
-        b'PROJ': ('sound', 'sound_countdown', 'sound_disable', 'soundLevel'),
+        b'PROJ': ('sound', 'sound_countdown', 'sound_disable'),
         b'SCRL': ('sound_pickup', 'sound_drop'),
         b'SLGM': ('sound_pickup', 'sound_drop'),
-        b'SNCT': ('parent', 'staticVolumeMultiplier'),
-        # Sounds does not need to loop here
-        b'SNDR': ('descriptor_category', 'output_model', 'sound_files',
-                  'looping_type', 'rumble_send_value', 'pct_frequency_shift',
-                  'pct_frequency_variance', 'descriptor_priority',
-                  'db_variance', 'staticAtten'),
-        b'SOPM': ('reverbSendpct', 'outputType', 'ch0_l', 'ch0_r', 'ch0_c',
-                  'ch0_lFE', 'ch0_rL', 'ch0_rR', 'ch0_bL', 'ch0_bR', 'ch1_l',
-                  'ch1_r', 'ch1_c', 'ch1_lFE', 'ch1_rL', 'ch1_rR', 'ch1_bL',
-                  'ch1_bR', 'ch2_l', 'ch2_r', 'ch2_c', 'ch2_lFE', 'ch2_rL',
-                  'ch2_rR', 'ch2_bL', 'ch2_bR', 'minDistance', 'maxDistance',
-                  'curve1', 'curve2', 'curve3', 'curve4', 'curve5'),
+        b'SNCT': ('parent',),
+        b'SNDR': ('descriptor_category', 'output_model'),
         b'SOUN': ('soundDescriptor',),
         b'TACT': ('sound',),
         b'TREE': ('sound',),
         b'WATR': ('sound',),
         b'WEAP': ('sound_pickup', 'sound_drop', 'sound', 'attackSound2D',
                   'attackLoopSound', 'attackFailSound', 'idleSound',
-                  'equipSound', 'unequipSound', 'detectionSoundLevel'),
-        #Needs to loop over all the sounds
-        b'WTHR': ('sounds',),
+                  'equipSound', 'unequipSound'),
     }
 
     #--------------------------------------------------------------------------