wrye-bash/wrye-bash

commit 63699b8d4b8dd3847fb48974b37057552f596ede

Don't sort masters by ESM status by default
We usually want to show the raw data in the save when the 'MI' column is
selected (the force override for 'Current LO' is fine). This way a
renamed or disabled master won't 'jump around' due to it getting sorted
after the ESMs.

Mopy/bash/basher/__init__.py
@@ -339,6 +339,13 @@ class MasterList(_ModsUIList):
     _do_size_checks = False
 
     @property
+    def esmsFirst(self):
+        # Flip the default for masters, we want to show the order in the save
+        # so as to not make renamed/disabled masters 'jump around'
+        return (settings.get(self.keyPrefix + u'.esmsFirst', False) or
+                self.sort_column in self._esmsFirstCols)
+
+    @property
     def cols(self):
         # using self.__class__.keyPrefix for common saves/mods masters settings
         return settings.getChanged(self.__class__.keyPrefix + u'.cols')

commit de6080a1d24e28df1f353aac8ad1e64b58011732

Fix sorting by type in master lists
Broke in 63699b8d4b8dd3847fb48974b37057552f596ede.

Took the opportunity to rename it to 'Masters First' and rename and
refactor it a bit. Also added some docstrings.

Mopy/bash/game/fallout4/records.py:
FO4: Fix plugin loading failures due to 647

DIAL and WRLD needed their nested children defined, otherwise they'll be
treated as unrecognized signatures.

Mopy/bash/basher/__init__.py
@@ -273,23 +273,31 @@ class BashTab(_DetailsViewMixin, SashUIListPanel):
 
 #------------------------------------------------------------------------------
 class _ModsUIList(balt.UIList):
+    _masters_first_cols = balt.UIList.nonReversibleCols
 
-    _esmsFirstCols = balt.UIList.nonReversibleCols
     @property
-    def esmsFirst(self): return settings.get(self.keyPrefix + u'.esmsFirst',
-                            True) or self.sort_column in self._esmsFirstCols
-    @esmsFirst.setter
-    def esmsFirst(self, val): settings[self.keyPrefix + u'.esmsFirst'] = val
+    def masters_first(self):
+        """Whether or not masters should be sorted before non-masters for the
+        current sort column."""
+        return (settings.get(f'{self.keyPrefix}.esmsFirst', True) or
+                self.masters_first_required)
+
+    @masters_first.setter
+    def masters_first(self, val):
+        settings[f'{self.keyPrefix}.esmsFirst'] = val
 
     @property
     def selectedFirst(self):
-        return settings.get(self.keyPrefix + u'.selectedFirst', False)
+        return settings.get(f'{self.keyPrefix}.selectedFirst', False)
+
     @selectedFirst.setter
     def selectedFirst(self, val):
-        settings[self.keyPrefix + u'.selectedFirst'] = val
+        settings[f'{self.keyPrefix}.selectedFirst'] = val
 
-    def _sortEsmsFirst(self, items):
-        if self.esmsFirst:
+    def _sort_masters_first(self, items):
+        """Conditional sort, performs the actual 'masters-first' sorting if
+        needed."""
+        if self.masters_first:
             items.sort(key=lambda a: not self.data_store[a].in_master_block())
 
     def _activeModsFirst(self, items):
@@ -297,8 +305,11 @@ class _ModsUIList(balt.UIList):
             bosh.modInfos.imported | bosh.modInfos.merged | set(
                 load_order.cached_active_tuple()))
 
-    def forceEsmFirst(self):
-        return self.sort_column in _ModsUIList._esmsFirstCols
+    @property
+    def masters_first_required(self):
+        """Return True if sorting by master status is required for the current
+        sort column."""
+        return self.sort_column in self._masters_first_cols
 
 #------------------------------------------------------------------------------
 class MasterList(_ModsUIList):
@@ -325,7 +336,7 @@ class MasterList(_ModsUIList):
             items.sort(key=lambda x: self.data_store[x].curr_name not in set(
                 load_order.cached_active_tuple()) | bosh.modInfos.imported
                                            | bosh.modInfos.merged)
-    _extra_sortings = [_ModsUIList._sortEsmsFirst, _activeModsFirst]
+    _extra_sortings = [_ModsUIList._sort_masters_first, _activeModsFirst]
     _sunkenBorder, _singleCell = False, True
     #--Labels
     labels = {
@@ -344,11 +355,16 @@ class MasterList(_ModsUIList):
     _do_size_checks = False
 
     @property
-    def esmsFirst(self):
+    def masters_first(self):
         # Flip the default for masters, we want to show the order in the save
         # so as to not make renamed/disabled masters 'jump around'
-        return (settings.get(self.keyPrefix + u'.esmsFirst', False) or
-                self.sort_column in self._esmsFirstCols)
+        return (settings.get(f'{self.keyPrefix}.esmsFirst', False) or
+                self.masters_first_required)
+
+    # We have to override this, otherwise Mods_MastersFirst breaks
+    @masters_first.setter
+    def masters_first(self, val):
+        settings[f'{self.keyPrefix}.esmsFirst'] = val
 
     @property
     def cols(self):
@@ -920,7 +936,7 @@ class ModList(_ModsUIList):
         u'Mod Status': lambda self, a: self.data_store[a].txt_status(),
         u'CRC'       : lambda self, a: self.data_store[a].cached_mod_crc(),
     }
-    _extra_sortings = [_ModsUIList._sortEsmsFirst,
+    _extra_sortings = [_ModsUIList._sort_masters_first,
                        _ModsUIList._activeModsFirst]
     _dndList, _dndColumns = True, [u'Load Order']
     _sunkenBorder = False

Mopy/bash/basher/links_init.py
@@ -180,7 +180,7 @@ def InitMasterLinks():
     """Initialize master list menus."""
     #--MasterList: Column Links
     MasterList.column_links.append(SortByMenu(
-        sort_options=[Mods_EsmsFirst(), Mods_SelectedFirst()]))
+        sort_options=[Mods_MastersFirst(), Mods_SelectedFirst()]))
     MasterList.column_links.append(ColumnsMenu())
     MasterList.column_links.append(SeparatorLink())
     MasterList.column_links.append(Master_AllowEdit())
@@ -450,7 +450,7 @@ def InitModLinks():
     #--ModList: Column Links
     # Sorting and Columns
     ModList.column_links.append(SortByMenu(
-        sort_options=[Mods_EsmsFirst(), Mods_SelectedFirst()]))
+        sort_options=[Mods_MastersFirst(), Mods_SelectedFirst()]))
     ModList.column_links.append(ColumnsMenu())
     ModList.column_links.append(SeparatorLink())
     # Files Menu
@@ -669,7 +669,7 @@ def InitModLinks():
     # View Menu
     view_menu = ModList.global_links[_('View')]
     view_menu.append(SortByMenu(
-        sort_options=[Mods_EsmsFirst(), Mods_SelectedFirst()]))
+        sort_options=[Mods_MastersFirst(), Mods_SelectedFirst()]))
     view_menu.append(ColumnsMenu())
     view_menu.append(SeparatorLink())
     view_menu.append(Mods_ListMods())

Mopy/bash/basher/mods_links.py
@@ -34,7 +34,7 @@ from ..gui import BusyCursor, copy_text_to_clipboard, get_ctrl_down, \
     get_shift_down
 from ..parsers import CsvParser
 
-__all__ = [u'Mods_EsmsFirst', u'Mods_ActivePlugins', u'Mods_SelectedFirst',
+__all__ = ['Mods_MastersFirst', 'Mods_ActivePlugins', 'Mods_SelectedFirst',
            'Mods_OblivionEsmMenu', 'Mods_CreateBlankBashedPatch',
            u'Mods_CreateBlank', u'Mods_ListMods', u'Mods_ListBashTags',
            u'Mods_CleanDummyMasters', u'Mods_AutoGhost', u'Mods_LockLoadOrder',
@@ -226,16 +226,16 @@ class Mods_ActivePlugins(MenuLink):
         return active_lists
 
 # "Sort by" submenu -----------------------------------------------------------
-class Mods_EsmsFirst(CheckLink, EnabledLink):
-    """Sort esms to the top."""
-    _help = _(u'Sort masters by type. Always on if current sort is Load Order.')
-    _text = _(u'Type')
+class Mods_MastersFirst(CheckLink, EnabledLink):
+    """Sort masters to the top."""
+    _help = _('Sort masters by type. Always on if current sort is Load Order.')
+    _text = _('Masters First')
 
-    def _enable(self): return not self.window.forceEsmFirst()
-    def _check(self): return self.window.esmsFirst
+    def _enable(self): return not self.window.masters_first_required
+    def _check(self): return self.window.masters_first
 
     def Execute(self):
-        self.window.esmsFirst = not self.window.esmsFirst
+        self.window.masters_first = not self.window.masters_first
         self.window.SortItems()
 
 class Mods_SelectedFirst(CheckLink):

Mopy/bash/game/fallout4/records.py
@@ -57,7 +57,7 @@ from ...brec import FID, AMelItems, AMelLLItems, AMelNvnm, AMelVmad, \
     MelUnloadEvent, MelUnorderedGroups, MelValueWeight, MelWaterType, \
     MelWeight, NavMeshFlags, NotPlayableFlag, PartialLoadDecider, \
     PerkEpdfDecider, VWDFlag, gen_color, gen_color3, lens_distributor, \
-    perk_distributor, perk_effect_key
+    perk_distributor, perk_effect_key, AMreWrld
 
 ##: What about texture hashes? I carried discarding them forward from Skyrim,
 # but that was due to the 43-44 problems. See also #620.
@@ -787,7 +787,8 @@ class MreCams(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreCell(AMreCell):
+class MreCell(AMreCell): ##: Implement once regular records are done
+    """Cell."""
     ref_types = {b'ACHR', b'PARW', b'PBAR', b'PBEA', b'PCON', b'PFLA', b'PGRE',
                  b'PHZD', b'PMIS', b'REFR'}
     interior_temp_extra = [b'NAVM']
@@ -997,6 +998,15 @@ class MreDfob(MelRecord):
         MelEdid(),
         MelFid(b'DATA', 'default_object'),
     )
+
+#------------------------------------------------------------------------------
+class MreDial(MelRecord): ##: Implement once regular records are done
+    """Dialogue."""
+    rec_sig = b'DIAL'
+
+    @classmethod
+    def nested_records_sigs(cls):
+        return {b'INFO'}
 
 #------------------------------------------------------------------------------
 class MreDmgt(MelRecord):
@@ -2140,3 +2150,10 @@ class MrePerk(MelRecord):
             MelBaseR(b'PRKF', 'pe_end_marker'),
         ), sort_special=perk_effect_key),
     ).with_distributor(perk_distributor)
+
+#------------------------------------------------------------------------------
+class MreWrld(AMreWrld): ##: Implement once regular records are done
+    """Worldspace."""
+    ref_types = MreCell.ref_types
+    exterior_temp_extra = [b'LAND', b'NAVM']
+    wrld_children_extra = [b'CELL'] # CELL for the persistent block