wrye-bash/wrye-bash

commit c49a5401d505eefc802b063d96dde6d337d84ebd

_ObIcon(MelRecord):
Yep it needed some effort - the idea is as usual that the record should
know about this - _rec_sig is a hint as well as staticmethods.
The work was done manually but things like:

classes = [fr'Mre{s.title()}' for s in sorted(map (lambda s: s.decode('ascii'),_default_icons))]
print('|'.join(classes)) -> ...

(MreAlch|MreAmmo|MreAppa|MreArmo|MreBook|MreBsgn|MreClas|MreClot|MreIngr|MreKeym|MreLigh|MreMisc|MreQust|MreSgst|MreSlgm|MreWeap)\(MelRecord\) ->
$1(_ObIcon)

might be useful if one wants to adapt to other games.

Silence warnings introduced in parent:

Mopy/bash/game/oblivion/records.py is warn free in my inspection (apart
from PEP08 stuff)

Mopy/bash/game/oblivion/records.py
@@ -21,6 +21,7 @@
 #
 # =============================================================================
 """This module contains the oblivion record classes."""
+import random
 import re
 
 from ...bolt import Flags, LowerDict, flag, int_or_none, int_or_zero, \
@@ -179,6 +180,8 @@ class _AMreLeveledListTes4(AMreLeveledList):
     two subrecords can be out of order in some plugins.
 
     See also wbLVLAfterLoad in xEdit's wbDefinitionsTES4.pas."""
+    lvl_chance_none: int
+
     def loadData(self, ins, endPos, *, file_offset=0):
         super().loadData(ins, endPos, file_offset=file_offset)
         if self.lvl_chance_none >= 128:
@@ -265,9 +268,8 @@ actor_values = [ # Human-readable names for each actor value
     'ResistWaterDamage',
 ]
 
-class MreHasEffects(object):
+class MreHasEffects(MelRecord):
     """Mixin class for magic items."""
-    __slots__ = ()
     _recipient_number_name = {None: 'NONE', 0: 'Self', 1: 'Touch', 2: 'Target'}
     _recipient_name_number = {y.lower(): x for x, y in
                               _recipient_number_name.items() if x is not None}
@@ -292,12 +294,13 @@ class MreHasEffects(object):
     attr_csv_struct['script_fid'] = [None, (_('Script Mod Name'),
         _('Script ObjectIndex')), lambda val:(
     '"None","None"' if val is None else '"%s","0x%06X"' % val)]
+    # typing to silence IDE's warnings - move to pyi stubs!
+    effects: list
 
     @classmethod
     def parse_csv_line(cls, csv_fields, index_dict, reuse=False):
         effects_tuple = index_dict.pop('effects', None)
-        attr_dex = super(MreHasEffects, cls).parse_csv_line(csv_fields,
-                                                            index_dict, reuse)
+        attr_dex = super().parse_csv_line(csv_fields, index_dict, reuse)
         if effects_tuple is not None:
             effects_start, coerce_fid = effects_tuple
             attr_dex['effects'] = cls._read_effects(csv_fields[effects_start:],
@@ -424,9 +427,7 @@ class MreHasEffects(object):
         schoolTypeNumber_Name = cls._school_number_name
         recipientTypeNumber_Name = cls._recipient_number_name
         actorValueNumber_Name = cls._actor_val_number_name
-        effectFormat = ',,"%s","%d","%d","%d","%s","%s"'
         scriptEffectFormat = ',"%s","0x%06X","%s","%s","%s","%s"'
-        noscriptEffectFiller = ',"None","None","None","None","None","None"'
         output = []
         for effect in effects:
             efname, magnitude, area, duration, range_, actorvalue = \
@@ -434,8 +435,8 @@ class MreHasEffects(object):
                 effect.duration, effect.recipient, effect.actorValue
             range_ = recipientTypeNumber_Name.get(range_,range_)
             actorvalue = actorValueNumber_Name.get(actorvalue,actorvalue)
-            output.append(effectFormat % (
-                efname,magnitude,area,duration,range_,actorvalue))
+            output.append(f',,"{efname}","{magnitude:d}","{area:d}",'
+                          f'"{duration:d}","{range_}","{actorvalue}"')
             if effect.scriptEffect: ##: #480 - setDefault commit - return None
                 se = effect.scriptEffect
                 longid, seschool, sevisual, seflags, sename = \
@@ -446,7 +447,7 @@ class MreHasEffects(object):
                 output.append(scriptEffectFormat % (*longid,
                     seschool, sevisual, bool(int(seflags)), sename))
             else:
-                output.append(noscriptEffectFiller)
+                output.append(',"None","None","None","None","None","None"')
         return ''.join(output)
 
     # Tweaks APIs -------------------------------------------------------------
@@ -527,9 +528,20 @@ class MreActi(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreAlch(MreHasEffects, MelRecord):
+class _ObIcon(MelRecord):
+    _default_icons = None
+
+    def can_set_icon(self):
+        """Return True if we *can* tweak this record's icon."""
+        return not self.iconPath
+
+    def set_default_icon(self):
+        self.iconPath = self._default_icons
+
+class MreAlch(MreHasEffects, _ObIcon):
     """Potion."""
     rec_sig = b'ALCH'
+    _default_icons = 'Clutter\\Potions\\IconPotion01.dds'
 
     class _AlchFlags(Flags):
         autoCalc: bool
@@ -550,9 +562,10 @@ class MreAlch(MreHasEffects, MelRecord):
     ).with_distributor(_effects_distributor)
 
 #------------------------------------------------------------------------------
-class MreAmmo(MelRecord):
+class MreAmmo(_ObIcon):
     """Ammunition."""
     rec_sig = b'AMMO'
+    _default_icons = 'Weapons\\IronArrow.dds'
 
     class _AmmoFlags(Flags):
         notNormalWeapon: bool
@@ -581,9 +594,10 @@ class MreAnio(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreAppa(MelRecord):
+class MreAppa(_ObIcon):
     """Alchemical Apparatus."""
     rec_sig = b'APPA'
+    _default_icons = 'Clutter\\IconMortarPestle.dds'
 
     melSet = MelSet(
         MelEdid(),
@@ -616,12 +630,43 @@ class _CommonBipedFlags(BipedFlags):
 
     _not_playable_flags = {'backWeapon', 'quiver', 'weapon', 'torch',
                            'rightRing', 'sideWeapon'}
-class _ObPlayable(MelRecord):
+
+class _ObPlayable(_ObIcon):
     not_playable_flag = ('biped_flags', 'notPlayable')
+    maleIconPath: str
+    femaleIconPath: str
+    biped_flags: _CommonBipedFlags
+
+    def can_set_icon(self):
+        return not self.is_not_playable() and not self.maleIconPath and not \
+            self.femaleIconPath
+
+    def set_default_icon(self):
+        # Choose based on body flags:
+        body_flags = self.biped_flags
+        if body_flags.upperBody:
+            return self._default_icons[0]
+        elif body_flags.lowerBody:
+            return self._default_icons[1]
+        elif body_flags.head or body_flags.hair:
+            return self._default_icons[2]
+        elif body_flags.hand:
+            return self._default_icons[3]
+        elif body_flags.foot:
+            return self._default_icons[4]
+        return None
 
 class MreArmo(_ObPlayable):
     """Armor."""
     rec_sig = b'ARMO'
+    _default_icons = (
+        ['Armor\\Iron\\M\\Cuirass.dds', 'Armor\\Iron\\F\\Cuirass.dds'],
+        ['Armor\\Iron\\M\\Greaves.dds', 'Armor\\Iron\\F\\Greaves.dds'],
+        ['Armor\\Iron\\M\\Helmet.dds'],
+        ['Armor\\Iron\\M\\Gauntlets.dds', 'Armor\\Iron\\F\\Gauntlets.dds'],
+        ['Armor\\Iron\\M\\Boots.dds'],
+        ['Armor\\Iron\\M\\Shield.dds'],
+        ['Armor\\Iron\\M\\Shield.dds'])  # Default Armor icon
 
     class _ArmoBipedFlags(_CommonBipedFlags):
         hideRings: bool = flag(16)
@@ -646,8 +691,21 @@ class MreArmo(_ObPlayable):
                   'weight'),
     )
 
+    def set_default_icon(self):
+        if not(ic := super().set_default_icon()):
+            ic = self._default_icons[5 if self.biped_flags.shield else
+                6] # Default icon, probably a token or somesuch
+        for att, val in zip(('maleIconPath', 'femaleIconPath'), ic):
+            setattr(self, att, val)
+
 #------------------------------------------------------------------------------
-class MreBook(MelRecord):
+class _RandIco(_ObIcon):
+    def set_default_icon(self):
+        # Just a random book icon - for class/birthsign as well.
+        random.seed(self.fid.object_dex) # make it deterministic
+        self.iconPath = f'Clutter\\iconbook{random.randint(1, 13):d}.dds'
+
+class MreBook(_RandIco):
     """Book."""
     rec_sig = b'BOOK'
 
@@ -669,7 +727,7 @@ class MreBook(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreBsgn(MelRecord):
+class MreBsgn(_RandIco):
     """Birthsign."""
     rec_sig = b'BSGN'
 
@@ -718,7 +776,7 @@ class MreCell(AMreCell):
     )
 
 #------------------------------------------------------------------------------
-class MreClas(MelRecord):
+class MreClas(_RandIco):
     """Class."""
     rec_sig = b'CLAS'
 
@@ -755,6 +813,16 @@ class MreClmt(MelRecord):
 class MreClot(_ObPlayable):
     """Clothing."""
     rec_sig = b'CLOT'
+    _default_icons = 'Clothes\\MiddleClass\\01\\%s.dds' # to wrap better
+    _default_icons = (
+        (_default_icons % 'M\\Shirt', _default_icons % 'F\\Shirt'),
+        (_default_icons % 'M\\Pants', _default_icons % 'F\\Pants'),
+        ('Clothes\\MythicDawnrobe\\hood.dds',),
+        ('Clothes\\LowerClass\\Jail\\M\\JailShirtHandcuff.dds',),
+        (_default_icons % 'M\\Shoes', _default_icons % 'F\\Shoes'),
+        ('Clothes\\Ring\\RingNovice.dds',),
+        ('Clothes\\Amulet\\AmuletSilver.dds',)
+    )
 
     class _ClotBipedFlags(_CommonBipedFlags):
         hideRings: bool = flag(16)
@@ -776,6 +844,13 @@ class MreClot(_ObPlayable):
         MelIco2('femaleIconPath'),
         MelValueWeight(),
     )
+
+    def set_default_icon(self):
+        if not(ic := super().set_default_icon()):
+            ring = self.biped_flags.leftRing or self.biped_flags.rightRing
+            ic = self._default_icons[5 if ring else 6]
+        for att, val in zip(('maleIconPath', 'femaleIconPath'), ic):
+            setattr(self, att, val)
 
 #------------------------------------------------------------------------------
 class MreCont(AMreWithItems):
@@ -1115,9 +1190,10 @@ class MreInfo(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreIngr(MreHasEffects, MelRecord):
+class MreIngr(MreHasEffects, _ObIcon):
     """Ingredient."""
     rec_sig = b'INGR'
+    _default_icons = 'Clutter\\IconSeeds.dds'
 
     class _IngrFlags(Flags):
         ingr_no_auto_calc: bool
@@ -1138,9 +1214,10 @@ class MreIngr(MreHasEffects, MelRecord):
     ).with_distributor(_effects_distributor)
 
 #------------------------------------------------------------------------------
-class MreKeym(MelRecord):
+class MreKeym(_ObIcon):
     """Key."""
     rec_sig = b'KEYM'
+    _default_icons = ('Clutter\\Key\\Key.dds', 'Clutter\\Key\\Key02.dds')
 
     melSet = MelSet(
         MelEdid(),
@@ -1150,6 +1227,10 @@ class MreKeym(MelRecord):
         MelScript(),
         MelValueWeight(),
     )
+
+    def set_default_icon(self):
+        random.seed(self.fid.object_dex)  # make it deterministic
+        self.iconPath = self._default_icons[random.randint(0, 1)]
 
 #------------------------------------------------------------------------------
 class MreLand(MelRecord):
@@ -1161,9 +1242,10 @@ class MreLand(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreLigh(MelRecord):
+class MreLigh(_ObIcon):
     """Light."""
     rec_sig = b'LIGH'
+    _default_icons = 'Lights\\IconTorch02.dds'
 
     class _LighFlags(Flags):
         light_dynamic: bool = flag(0)
@@ -1176,6 +1258,8 @@ class MreLigh(MelRecord):
         light_pulses_slow: bool = flag(8)
         light_spot_light: bool = flag(9)
         light_shadow_spotlight: bool = flag(10)
+
+    light_flags: _LighFlags
 
     melSet = MelSet(
         MelEdid(),
@@ -1191,6 +1275,9 @@ class MreLigh(MelRecord):
         MelLighFade(),
         MelSound(),
     )
+
+    def can_set_icon(self):
+        return self.light_flags.light_can_take and super().can_set_icon()
 
 #------------------------------------------------------------------------------
 class MreLscr(MelRecord):
@@ -1555,9 +1642,10 @@ class MreMgef(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreMisc(MelRecord):
+class MreMisc(_ObIcon):
     """Misc. Item."""
     rec_sig = b'MISC'
+    _default_icons = 'Clutter\\Soulgems\\AzurasStar.dds'
 
     class HeaderFlags(MelRecord.HeaderFlags):
         @property
@@ -1588,6 +1676,7 @@ class MreMisc(MelRecord):
 class MreNpc_(AMreActor):
     """Non-Player Character."""
     rec_sig = b'NPC_'
+    model: object
 
     class NpcFlags(Flags):
         female: bool = flag(0)
@@ -1741,9 +1830,11 @@ class MrePgrd(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreQust(MelRecord):
+class MreQust(_ObIcon):
     """Quest."""
     rec_sig = b'QUST'
+    _default_icons = 'Quest\\icon_miscellaneous.dds'
+    stages: list
 
     class _QustFlags(Flags):
         startGameEnabled: bool
@@ -1788,6 +1879,9 @@ class MreQust(MelRecord):
             b'CTDA': 'targets',
         },
     })
+
+    def can_set_icon(self):
+        return not self.stages
 
 #------------------------------------------------------------------------------
 class MreRace(AMreRace):
@@ -2045,9 +2139,10 @@ class MreScpt(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreSgst(MreHasEffects, MelRecord):
+class MreSgst(MreHasEffects, _ObIcon):
     """Sigil Stone."""
     rec_sig = b'SGST'
+    _default_icons = 'IconSigilStone.dds'
 
     melSet = MelSet(
         MelEdid(),
@@ -2080,9 +2175,10 @@ class MreSkil(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreSlgm(MelRecord):
+class MreSlgm(_ObIcon):
     """Soul Gem."""
     rec_sig = b'SLGM'
+    _default_icons = 'Clutter\\Soulgems\\AzurasStar.dds'
 
     melSet = MelSet(
         MelEdid(),
@@ -2238,9 +2334,13 @@ class MreWatr(MelRecord):
     )
 
 #------------------------------------------------------------------------------
-class MreWeap(MelRecord):
+class MreWeap(_ObIcon):
     """Weapon."""
     rec_sig = b'WEAP'
+    _default_icons = ('Weapons\\IronDagger.dds', 'Weapons\\IronClaymore.dds',
+                      'Weapons\\IronMace.dds', 'Weapons\\IronBattleAxe.dds',
+                      'Weapons\\Staff.dds', 'Weapons\\IronBow.dds')
+    weaponType: int
 
     class _WeapFlags(Flags):
         notNormalWeapon: bool
@@ -2257,6 +2357,13 @@ class MreWeap(MelRecord):
                   'reach', (_WeapFlags, 'flags'), 'value', 'health', 'weight',
                   'damage'),
     )
+
+    def set_default_icon(self):
+        # Choose based on weapon type:
+        try:
+            self.iconPath = self._default_icons[self.weaponType]
+        except IndexError:  # just in case
+            self.iconPath = self._default_icons[0]
 
 #------------------------------------------------------------------------------
 class MreWrld(AMreWrld):

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -26,13 +26,11 @@ to the Assorted Multitweaker - as well as the tweaker itself."""
 
 from __future__ import annotations
 
-import random
 import re
 
 from .base import CustomChoiceTweak, IndexingTweak, MultiTweaker, \
     MultiTweakItem
 from ... import bolt, bush
-from ...bolt import deprint
 
 #------------------------------------------------------------------------------
 class _AShowsTweak(MultiTweakItem):
@@ -616,8 +614,7 @@ class AssortedTweak_SetCastWhenUsedEnchantmentCosts(CustomChoiceTweak):
         return final_cost, final_cost * chosen_uses
 
 #------------------------------------------------------------------------------
-##: It's possible to simplify this further, but will require some effort
-##: Also, will have to become more powerful in the process if we want it to
+##: This will have to become more powerful in the process if we want it to
 # support FO3/FNV eventually ##: does it even make sense there?
 class AssortedTweak_DefaultIcons(MultiTweakItem):
     """Sets a default icon for any records that don't have any icon
@@ -627,112 +624,17 @@ class AssortedTweak_DefaultIcons(MultiTweakItem):
                   u'icon assigned.')
     tweak_key = u'icons'
     tweak_choices = [(u'1', 1)]
-    _default_icons = {
-        b'ALCH': u'Clutter\\Potions\\IconPotion01.dds',
-        b'AMMO': u'Weapons\\IronArrow.dds',
-        b'APPA': u'Clutter\\IconMortarPestle.dds',
-        b'ARMO': ((u'Armor\\Iron\\M\\Cuirass.dds',
-                   u'Armor\\Iron\\F\\Cuirass.dds'),
-                 (u'Armor\\Iron\\M\\Greaves.dds',
-                  u'Armor\\Iron\\F\\Greaves.dds'),
-                 (u'Armor\\Iron\\M\\Helmet.dds',),
-                 (u'Armor\\Iron\\M\\Gauntlets.dds',
-                  u'Armor\\Iron\\F\\Gauntlets.dds'),
-                 (u'Armor\\Iron\\M\\Boots.dds',),
-                 (u'Armor\\Iron\\M\\Shield.dds',),
-                 (u'Armor\\Iron\\M\\Shield.dds',),), # Default Armor icon
-        b'BOOK': u'Clutter\\iconbook%d.dds',
-        b'BSGN': u'Clutter\\iconbook%d.dds',
-        b'CLAS': u'Clutter\\iconbook%d.dds',
-        b'CLOT': ((u'Clothes\\MiddleClass\\01\\M\\Shirt.dds',
-                   u'Clothes\\MiddleClass\\01\\F\\Shirt.dds'),
-                 (u'Clothes\\MiddleClass\\01\\M\\Pants.dds',
-                  u'Clothes\\MiddleClass\\01\\F\\Pants.dds'),
-                 (u'Clothes\\MythicDawnrobe\\hood.dds',),
-                 (u'Clothes\\LowerClass\\Jail\\M\\'
-                  u'JailShirtHandcuff.dds',),
-                 (u'Clothes\\MiddleClass\\01\\M\\Shoes.dds',
-                  u'Clothes\\MiddleClass\\01\\F\\Shoes.dds'),
-                 (u'Clothes\\Ring\\RingNovice.dds',),
-                 (u'Clothes\\Amulet\\AmuletSilver.dds',),),
-##                'FACT': u"", ToDo
-        b'INGR': u'Clutter\\IconSeeds.dds',
-        b'KEYM': (u'Clutter\\Key\\Key.dds', u'Clutter\\Key\\Key02.dds'),
-        b'LIGH': u'Lights\\IconTorch02.dds',
-        b'MISC': u'Clutter\\Soulgems\\AzurasStar.dds',
-        b'QUST': u'Quest\\icon_miscellaneous.dds',
-        b'SGST': u'IconSigilStone.dds',
-        b'SLGM': u'Clutter\\Soulgems\\AzurasStar.dds',
-        b'WEAP': (u'Weapons\\IronDagger.dds', u'Weapons\\IronClaymore.dds',
-                  u'Weapons\\IronMace.dds', u'Weapons\\IronBattleAxe.dds',
-                  u'Weapons\\Staff.dds', u'Weapons\\IronBow.dds',),
-    }
-    tweak_read_classes = tuple(_default_icons)
+    tweak_read_classes = (b'ALCH', b'AMMO', b'APPA', b'BOOK', b'BSGN', b'CLAS',  # ToDo 'FACT', / per game
+        b'INGR', b'KEYM', b'LIGH', b'MISC', b'QUST', b'SGST', b'SLGM', b'WEAP'
+    )
     tweak_log_msg = _(u'Default Icons Set: %(total_changed)d')
     default_enabled = True
 
     def wants_record(self, record):
-        rsig = record._rec_sig # we need a record mixin
-        if (rsig == b'LIGH' and not record.light_flags.light_can_take or
-            rsig == b'QUST' and not record.stages or rsig in (b'ARMO', b'CLOT')
-                and record.is_not_playable()): return False
-        return (not getattr(record, u'iconPath', None) and
-                not getattr(record, u'maleIconPath', None) and
-                not getattr(record, u'femaleIconPath', None))
-
-    @staticmethod
-    def _assign_icons(record, d_icons):
-        """Assigns the specified default icons to the specified record."""
-        try:
-            if isinstance(d_icons, tuple):
-                if len(d_icons) == 1:
-                    record.maleIconPath = d_icons[0]
-                else:
-                    record.maleIconPath, record.femaleIconPath = d_icons
-            else:
-                record.iconPath = d_icons
-        except ValueError:
-            deprint(u'Error while assigning default icons to %r' % record)
-            raise
+        return record.can_set_icon()
 
     def tweak_record(self, record):
-        curr_sig = record._rec_sig
-        d_icons = self._default_icons[curr_sig]
-        if isinstance(d_icons, tuple):
-            if curr_sig in (b'ARMO', b'CLOT'):
-                # Choose based on body flags:
-                body_flags = record.biped_flags
-                if body_flags.upperBody:
-                    d_icons = d_icons[0]
-                elif body_flags.lowerBody:
-                    d_icons = d_icons[1]
-                elif body_flags.head or body_flags.hair:
-                    d_icons = d_icons[2]
-                elif body_flags.hand:
-                    d_icons = d_icons[3]
-                elif body_flags.foot:
-                    d_icons = d_icons[4]
-                elif (curr_sig == b'ARMO' and body_flags.shield or
-                      curr_sig == b'CLOT' and (
-                              body_flags.leftRing or
-                              body_flags.rightRing)):
-                    d_icons = d_icons[5]
-                else: # Default icon, probably a token or somesuch
-                    d_icons = d_icons[6]
-            elif curr_sig == b'KEYM':
-                random.seed(record.fid.object_dex) # make it deterministic
-                d_icons = d_icons[random.randint(0, 1)]
-            elif curr_sig == b'WEAP':
-                # Choose based on weapon type:
-                try:
-                    d_icons = d_icons[record.weaponType]
-                except IndexError: # just in case
-                    d_icons = d_icons[0]
-        elif curr_sig in (b'BOOK', b'BSGN', b'CLAS'):
-            # Just a random book icon - for class/birthsign as well.
-            random.seed(record.fid.object_dex) # make it deterministic
-            d_icons %= random.randint(1, 13)
-        self._assign_icons(record, d_icons)
+        record.set_default_icon()
 
 #------------------------------------------------------------------------------
 class _AAttenuationTweak(CustomChoiceTweak):

commit cfba6c50bb3d2fdacbf3d33031155fe44442463c

Fix regression in Default Icons tweak
Introduced in c49a5401d505eefc802b063d96dde6d337d84ebd.
Stageless quests don't need icons & were previously skipped, &
can_set_icon override didn't include a check that a quest record doesn't
already have an icon.

SSE: Add flm, seasons to data_dirs

Seasons of Skyrim SKSE:
https://www.nexusmods.com/skyrimspecialedition/mods/62861
FormList Manipulator:
https://www.nexusmods.com/skyrimspecialedition/mods/74037

SSE: Add DAR's _conditions.txt to no_skip_regexes

Dynamic Animation Replacer:
https://www.nexusmods.com/skyrimspecialedition/mods/33746
Based on DAR's description (its docs, essentially):
<Project folder> can be any relative path containing a 'behaviors'
folder IIUC (as the author puts it, "behavior graph set for [a] race"),
e.g., 'character' or 'character\_1stperson'. Theoretically, this could
be an arbitrary number of nested folders (hence the '.+'), but checking
for one nested folder should be sufficient if this is too expensive.
Skyrim doesn't have more than one by default.
<Priority> is just a 32-bit signed integer excluding 0, so '-?\d+'
should be sufficient.

FNV: Add skip support for JIP LN's Script Runner

JIP LN:
https://www.nexusmods.com/newvegas/mods/58277
Script Runner intro:
https://geckwiki.com/index.php?title=JIP_LN_NVSE_Script_Runner_Introduction

It would technically be more accurate to implement this as a regex since
text files require certain prefixes to actually be run by JIP LN, but
it's unlikely any docs would end up in this folder.

Mopy/Docs/Wrye Bash General Readme.html
@@ -489,10 +489,10 @@ be restructured.</p>
         </tr>
         <tr>
             <td>Skyrim</td>
-            <td>asi, autobody, calientetools, dialogueviews, dyndolod, grass,
-                interface, lodsettings, mapmarkers, mapweathers, mcm, mlq,
-                nemesis_engine, osa, platform, scripts, seq, shadersfx, skse,
-                skyproc patchers, slanims, strings, tools</td>
+            <td>asi, autobody, calientetools, dialogueviews, dyndolod, flm,
+                grass, interface, lodsettings, mapmarkers, mapweathers, mcm,
+                mlq, nemesis_engine, osa, platform, scripts, seasons, seq,
+                shadersfx, skse, skyproc patchers, slanims, strings, tools</td>
         </tr>
         <tr>
             <td>Fallout 4</td>

Mopy/bash/game/falloutnv/__init__.py
@@ -22,6 +22,7 @@
 # =============================================================================
 """GameInfo override for Fallout NV."""
 from copy import deepcopy
+from os.path import join as _j
 
 from .. import WS_COMMON_FILES
 from ..fallout3 import Fallout3GameInfo
@@ -79,6 +80,10 @@ class FalloutNVGameInfo(Fallout3GameInfo):
 
     class Bain(Fallout3GameInfo.Bain):
         data_dirs = (Fallout3GameInfo.Bain.data_dirs - {'fose'}) | {'nvse'}
+        no_skip_dirs = Fallout3GameInfo.Bain.no_skip_dirs | {
+            # 3P: JIP LN's Script Runner
+            _j('nvse', 'plugins', 'scripts'): {'.txt'},
+        }
         skip_bain_refresh = {u'fnvedit backups', u'fnvedit cache'}
 
     class Esp(Fallout3GameInfo.Esp):

Mopy/bash/game/oblivion/records.py
@@ -1880,7 +1880,7 @@ class MreQust(_ObIcon):
     })
 
     def can_set_icon(self):
-        return not self.stages
+        return self.stages and super().can_set_icon()
 
 #------------------------------------------------------------------------------
 class MreRace(AMreRace):

Mopy/bash/game/skyrim/__init__.py
@@ -119,6 +119,7 @@ class SkyrimGameInfo(PatchGame):
             'calientetools', # 3P: BodySlide
             'dialogueviews',
             'dyndolod', # 3P: DynDOLOD
+            'flm', # 3P: FormList Manipulator
             'grass',
             'interface',
             'lodsettings',
@@ -130,6 +131,7 @@ class SkyrimGameInfo(PatchGame):
             'osa', # 3P: OSA Animation Framework
             'platform', # 3P: Skyrim Platform
             'scripts',
+            'seasons', # 3P: Seasons of Skyrim SKSE
             'seq',
             'shadersfx',
             'skse', # 3P: SKSE
@@ -173,6 +175,13 @@ class SkyrimGameInfo(PatchGame):
             # <SKSE plugin name>_preload.txt
             re.compile(bolt.os_sep_re.join([
                 'skse', 'plugins', r'.+_preload\.txt'])),
+            # 3P: Dynamic Animation Replacer - meshes\actors\<Project folder>\
+            # animations\DynamicAnimationReplacer\_CustomConditions\<Priority>\
+            # _conditions.txt
+            re.compile(bolt.os_sep_re.join([
+                'meshes', 'actors', '.+', 'animations',
+                'dynamicanimationreplacer', '_customconditions', r'-?\d+',
+                r'_conditions\.txt'])),
         )
         skip_bain_refresh = {u'tes5edit backups', u'tes5edit cache'}
 