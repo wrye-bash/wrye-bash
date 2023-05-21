wrye-bash/wrye-bash

commit d125492e5c4e3c18f7444f5cebb0fad33d41d07b

TES3: Fix a couple issues
Always a good idea to check in with Morrowind every once in a while to
make sure our code can handle a game that we don't support a BP for.

Turns out it was broken in a few places:
 - The Plugin Checker tried to load plugins even when it clearly can't
   (we probably need some more fine-grained game vars for this in the
   future, instead of tying all of it to canBash).
 - File > New Plugin and File > New Bashed Patch are correctly hidden,
   but Ctrl+N and Ctrl+Shift+N could still be used to access the
   functionality (which broke, of course).
 - Morrowind does not have tags yet. The code correctly handles a game
   with no Bash Tags, but Morrowind still had the three common tags from
   patch_game.

Mopy/bash/mod_files.py:
We were always using TES4 here when Morrowind obviously doesn't have a
TES4 signature. Doesn't actually make New Plugin work yet, but it's a
step in the right direction.

Mopy/bash/load_order.py:
Fix LO performance issue

Had this sitting around in my git stash. Slightly improves performance
by properly indexing into the actives list, which avoids causing
unnecessary refreshes on an actives change.

Mopy/bash/load_order.py
@@ -274,7 +274,11 @@ def find_first_difference(lo_a, acti_a, lo_b, acti_b):
     if low_diff != (None, None):
         low_acti = min(lindex_a[low_diff[0]], lindex_b[low_diff[1]])
     elif len(acti_a) != len(acti_b):
-        low_acti = min(len(acti_a), len(acti_b))
+        # This points into the actives list, need to convert to LO index
+        if len(acti_a) < len(acti_b):
+            low_acti = lindex_a[acti_a[-1]]
+        else:
+            low_acti = lindex_b[acti_b[-1]]
     else: low_acti = None
     # Finally, we need to deal with cases where one of the two is None and
     # return the smaller result

Mopy/bash/basher/__init__.py
@@ -1190,6 +1190,7 @@ class ModList(_ModsUIList):
                              else toActivate)
             self._toggle_active_state(*toggle_target)
         if wrapped_evt.is_cmd_down and wrapped_evt.key_code == ord('N'):
+            if not bush.game.Esp.canBash: return # We can't write plugins
             if wrapped_evt.is_shift_down:
                 # Ctrl+Shift+N - Create a new Bashed Patch
                 self.new_bashed_patch()

Mopy/bash/basher/frames.py
@@ -25,7 +25,7 @@ import re
 import string
 from collections import OrderedDict
 
-from .. import balt, bass, bolt, bosh, load_order
+from .. import balt, bass, bolt, bosh, bush, load_order
 from ..balt import Link, Resources
 from ..bolt import FName, GPath
 from ..bosh import empty_path, mods_metadata, omods
@@ -414,6 +414,9 @@ class PluginChecker(WindowFrame):
                          u'clipboard.'))
         _f(_UPDATE,       False, _(u'Update'),
            setting_tip=_(u'Regenerate the report from scratch.'))
+        # If we can't load plugins, don't even show the option
+        if not bush.game.Esp.canBash:
+            self._controls[_LOAD_PLUGINS].visible = False
         #--Events
         self.on_activate.subscribe(self.on_activation)
         VLayout(border=4, spacing=4, item_expand=True, items=[

Mopy/bash/bosh/mods_metadata.py
@@ -116,6 +116,9 @@ def checkMods(mc_parent, modInfos, showModList=False, showCRC=False,
               showVersion=True, scan_plugins=True):
     """Checks currently loaded mods for certain errors / warnings.
     mc_parent should be the instance of PluginChecker, to scan."""
+    if not bush.game.Esp.canBash:
+        # If we can't load plugins, then trying to do so will obviously fail
+        scan_plugins = False
     # Setup some commonly used collections of plugin info
     full_acti = load_order.cached_active_tuple()
     full_lo = load_order.cached_lo_tuple()
@@ -702,7 +705,8 @@ def checkMods(mc_parent, modInfos, showModList=False, showCRC=False,
     temp_log = log.out.getvalue()
     if not temp_log:
         log.setHeader(u'=== ' + _(u'No Problems Found'))
-        if not scan_plugins:
+        # Don't annoy the user with this message if we can't load plugins
+        if not scan_plugins and bush.game.Esp.canBash:
             log(_(u'Wrye Bash did not find any problems with your installed '
                   u'plugins without loading them. Turning on loading of '
                   u'plugins may find more problems.'))

Mopy/bash/game/morrowind/__init__.py
@@ -55,6 +55,8 @@ class MorrowindGameInfo(PatchGame):
     using_txt_file = False
     plugin_name_specific_dirs = [] # Morrowind seems to have no such dirs
 
+    allTags = set() # no BP functionality yet
+
     class Ck(GameInfo.Ck):
         ck_abbrev = u'TESCS'
         long_name = u'Construction Set'

Mopy/bash/mod_files.py
@@ -146,8 +146,9 @@ class ModFile(object):
         self.fileInfo = fileInfo
         self.loadFactory = loadFactory or LoadFactory(True) ##: trace
         #--Variables to load
-        self.tes4 = bush.game.plugin_header_class(RecHeader(arg2=ZERO_FID,
-                _entering_context=True))
+        self.tes4 = bush.game.plugin_header_class(RecHeader(
+            bush.game.Esp.plugin_header_sig, arg2=ZERO_FID,
+            _entering_context=True))
         self.tes4.setChanged()
         self.strings = bolt.StringTable()
         self.tops = _TopGroupDict(self) #--Top groups.

commit bcc2ee37655c8dd306d7d68264c3f319d3c5cd34

Fix regression when {un,re}doing certain LOs
Specifically, if the actives list got entirely emptied or was entirely
empty and got filled. Broke in d125492e5c4e3c18f7444f5cebb0fad33d41d07b.

requirements.txt:
Bump pywin32

Didn't catch this one because of the marker.

requirements.txt:
scripts/build/standalone/pyinstaller.spec:
Upgrade to pyinstaller 5.11

TOC is deprecated and should be replaced with a plain list.

Mopy/bash/load_order.py
@@ -274,11 +274,18 @@ def find_first_difference(lo_a, acti_a, lo_b, acti_b):
     if low_diff != (None, None):
         low_acti = min(lindex_a[low_diff[0]], lindex_b[low_diff[1]])
     elif len(acti_a) != len(acti_b):
-        # This points into the actives list, need to convert to LO index
-        if len(acti_a) < len(acti_b):
-            low_acti = lindex_a[acti_a[-1]]
+        if not acti_a and acti_b:
+            # Actives were empty and got filled, diff at first new active
+            low_acti = lindex_b[acti_b[0]]
+        elif not acti_b and acti_a:
+            # Actives were filled and are now empty, diff at first old active
+            low_acti = lindex_a[acti_a[0]]
         else:
-            low_acti = lindex_b[acti_b[-1]]
+            # This points into the actives list, need to convert to LO index
+            if len(acti_a) < len(acti_b):
+                low_acti = lindex_a[acti_a[-1]]
+            else:
+                low_acti = lindex_b[acti_b[-1]]
     else: low_acti = None
     # Finally, we need to deal with cases where one of the two is None and
     # return the smaller result

scripts/build/standalone/pyinstaller.spec
@@ -123,8 +123,8 @@ excluded_binaries = {
     'vcruntime140_1.dll',
     'vcruntime140.dll',
 }
-a.binaries = TOC([x for x in a.binaries
-                  if os.path.basename(x[0]).lower() not in excluded_binaries])
+a.binaries = [x for x in a.binaries
+              if os.path.basename(x[0]).lower() not in excluded_binaries]
 
 pyz = PYZ(a.pure, a.zipped_data,
           cipher=block_cipher)