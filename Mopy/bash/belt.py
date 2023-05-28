wrye-bash/wrye-bash

commit 1fe6d2dc38808d079836a2f575699f4ea7a4b2aa

Make wizard and FOMOD images work on Linux
Namely by introducing a function to convert a case insensitive path to
the right case for the current filesystem. This is basically what Wine
does as well, but my guess is Wine caches a whole bunch to make this
more efficient. Not a big deal right now since we only use it for fixing
the image paths, which were designed with Windows in mind, but if we
want to use this to fix BAIN, we'll have to think of a more performant
solution.

Under #243

Mopy/bash/belt.py
@@ -30,7 +30,7 @@ from collections import OrderedDict, defaultdict
 from . import ScriptParser, bass, bolt, bosh, bush, load_order
 from .balt import ItemLink, Links, images, staticBitmap
 from .bolt import FName, FNDict, LooseVersion
-from .env import get_file_version, get_game_version_fallback
+from .env import get_file_version, get_game_version_fallback, to_os_path
 from .gui import CENTER, RIGHT, CheckBox, CheckListBox, GridLayout, \
     HBoxedLayout, HLayout, HyperlinkLabel, Label, LayoutOptions, ListBox, \
     PictureWithCursor, Stretch, TextArea, VLayout, WizardDialog, WizardPage
@@ -214,9 +214,14 @@ class PageSelect(PageInstaller):
 
     def _click_on_image(self):
         img = self.images[self.index]
-        try: img.start()
-        except FileNotFoundError: pass
-        except OSError: bolt.deprint(f'Failed to open {img}.', traceback=True)
+        if not img:
+            return # None - no image path specified
+        try:
+            img.start()
+        except FileNotFoundError:
+            pass # Image path specified, but no image present at that path
+        except OSError:
+            bolt.deprint(f'Failed to open {img}.', traceback=True)
 
     def _on_right_click(self, lb_selection_dex):
         """Internal callback to show the context menu for appropriate pages."""
@@ -1360,13 +1365,17 @@ class WryeParser(ScriptParser.Parser):
             # Try looking inside the package first, then look if it's using one
             # of the images packaged with Wrye Bash (from
             # Mopy/bash/images/Wizard Images)
-            wiz_img_path = imageJoin(i)
-            if (i.lower().startswith('wizard images') and
-                    not wiz_img_path.is_file()):
-                std_img_path = bass.dirs['images'].join(i)
-                if std_img_path.is_file():
-                    wiz_img_path = std_img_path
-            image_paths.append(wiz_img_path)
+            # Note that these are almost always Windows paths, so we have to
+            # convert them if we're on Linux
+            wiz_img_path = to_os_path(imageJoin(i))
+            if wiz_img_path and wiz_img_path.is_file():
+                image_paths.append(wiz_img_path)
+            elif i.lower().startswith('wizard images'):
+                std_img_path = to_os_path(bass.dirs['images'].join(i).s)
+                if std_img_path and std_img_path.is_file():
+                    image_paths.append(std_img_path)
+            else:
+                image_paths.append(None)
         self.page = PageSelect(self._wiz_parent, bMany, main_desc, titles,
                                descs, image_paths)
 

Mopy/bash/basher/gui_fomod.py
@@ -25,8 +25,9 @@ __author__ = u'Ganda'
 
 from collections import defaultdict
 
-from .. import balt, bass, bolt, bush, env
+from .. import balt, bass, bolt, bush
 from ..balt import EnabledLink, Links, colors
+from ..env import get_file_version, get_game_version_fallback, to_os_path
 from ..fomod import FailedCondition, FomodInstaller, GroupType, \
     InstallerGroup, InstallerOption, InstallerPage, OptionType
 from ..gui import CENTER, TOP, BusyCursor, Button, CancelButton, CheckBox, \
@@ -116,11 +117,11 @@ class InstallerFomod(WizardDialog):
         # Get the game version, be careful about Windows Store games
         test_path = bass.dirs[u'app'].join(bush.game.version_detect_file)
         try:
-            gver = env.get_file_version(test_path.s)
+            gver = get_file_version(test_path.s)
             if gver == (0, 0, 0, 0) and bush.ws_info.installed:
-                gver = env.get_game_version_fallback(test_path, bush.ws_info)
+                gver = get_game_version_fallback(test_path, bush.ws_info)
         except OSError:
-            gver = env.get_game_version_fallback(test_path, bush.ws_info)
+            gver = get_game_version_fallback(test_path, bush.ws_info)
         version_string = u'.'.join([str(i) for i in gver])
         self.fomod_parser = FomodInstaller(
             fm_file, self.files_list, self.installer_root, bass.dirs[u'mods'],
@@ -376,7 +377,8 @@ class PageSelect(PageInstaller):
         self.update_layout()
 
     def _open_image(self, _dclick_ignored=0):
-        if self._current_image: # sanity check
+        # May be None if we're on Linux and a path didn't end up existing
+        if self._current_image:
             self._current_image.start()
 
     def _handle_block_user(self, block_checkable):
@@ -407,13 +409,16 @@ class PageSelect(PageInstaller):
         """Sets the image and description on the right side based on the
         specified checkable."""
         option = self.checkable_to_option[checkable]
-        opt_img = self._page_parent.archive_path.join(
-            self._page_parent.installer_root, option.option_image)
-        self._current_image = opt_img # To allow opening it via double click
+        opt_img = option.option_image
+        # Note that these are almost always Windows paths, so we have to
+        # convert them if we're on Linux
+        opt_img_path = to_os_path(self._page_parent.archive_path.join(
+            self._page_parent.installer_root, opt_img))
+        self._current_image = opt_img_path # To allow opening via double click
         try:
             final_image = self._img_cache[opt_img]
         except KeyError:
-            final_image = opt_img
+            final_image = opt_img_path
         self._img_cache[opt_img] = self._bmp_item.set_bitmap(final_image)
         # Check if we need to display a special string above the description
         type_desc, type_warn = self._option_type_info[option.option_type]

Mopy/bash/env/__init__.py
@@ -31,7 +31,7 @@ from collections.abc import Iterable
 # First import the shared API
 from .common import *
 from .common import file_operation as _default_file_operation
-from ..bolt import os_name
+from ..bolt import os_name, Path
 
 _TShellWindow = '_AComponent | _Window | None'
 
@@ -54,7 +54,13 @@ def _resolve(parent: _TShellWindow):
     except AttributeError:
         return parent   # type: ignore
 
-# Higher level APIs implemented by using the imported OS-specific ones above
+# Higher level APIs using imported OS-specific ones ---------------------------
+def to_os_path(questionable_path: os.PathLike | str) -> Path | None:
+    """Convenience method for converting a path of unknown origin to a path
+    compatible with this OS/FS. See normalize_ci_path and convert_separators
+    for more information."""
+    return normalize_ci_path(convert_separators(os.fspath(questionable_path)))
+
 def shellDelete(files: Iterable[Path], parent: _TShellWindow = None,
                 ask_confirm=False, recycle=False, __shell=True):
     operate = file_operation if __shell else _default_file_operation

Mopy/bash/env/linux.py
@@ -26,10 +26,12 @@ import functools
 import os
 import subprocess
 import sys
+from pathlib import Path as PPath ##: To be obsoleted when we refactor Path
 
 from .common import _find_legendary_games, _LegacyWinAppInfo
 # some hiding as pycharm is confused in __init__.py by the import *
 from ..bolt import GPath as _GPath
+from ..bolt import GPath_no_norm as _GPath_no_norm
 from ..bolt import Path as _Path
 from ..bolt import deprint as _deprint
 from ..bolt import dict_sort, structs_cache
@@ -236,6 +238,37 @@ def python_tools_dir():
 
 def convert_separators(p):
     return p.replace(u'\\', u'/')
+
+##: A more performant implementation would maybe cache folder contents or
+# something similar, as it stands this is not usable for fixing BAIN on Linux
+def normalize_ci_path(ci_path: os.PathLike | str) -> _Path | None:
+    if os.path.exists(ci_path):
+        # Fast path, but GPath it as we haven't normpathed it yet
+        return _GPath(ci_path)
+    # The first part is root, which we can obviously skip (has no other case)
+    ci_parts = PPath(os.path.normpath(os.fspath(ci_path))).parts[1:]
+    constructed_path = '/'
+    for ci_part in ci_parts:
+        new_ci_path = os.path.join(constructed_path, ci_part)
+        if os.path.exists(new_ci_path):
+            # If this part exists with the correct case, keep going
+            constructed_path = new_ci_path
+        else:
+            # Otherwise we have to list the entire folder and
+            # case-insensitively look for a match
+            ci_part_lower = ci_part.lower()
+            for candidate_file in os.listdir(constructed_path):
+                if candidate_file.lower() == ci_part_lower:
+                    # We found a matching file, construct the new path with the
+                    # right case and resume the outer loop
+                    constructed_path = os.path.join(constructed_path,
+                        candidate_file)
+                    break
+            else:
+                # We can't find this part at all, so the whole path can't be
+                # found -> None
+                return None
+    return _GPath_no_norm(constructed_path)
 
 # API - Classes ===============================================================
 class TaskDialog(object):

Mopy/bash/env/windows.py
@@ -1236,6 +1236,15 @@ def convert_separators(p):
     """Converts other OS's path separators to separators for this OS."""
     return p.replace(u'/', u'\\')
 
+def normalize_ci_path(ci_path: os.PathLike | str) -> _Path | None:
+    """Alter the case of a case-insensitive path to match directories and files
+    actually present in the filesystem, if any. This is basically identical to
+    what Wine does when emulating case insensitivity. If this returns None, the
+    path does not exist. However, if this does not return None then there is no
+    guarantee that the path exists, so check using exists()/is_file()/etc."""
+    # Windows is case-insensitive, nothing to do here
+    return _Path(os.fspath(ci_path))
+
 # API - Classes ===============================================================
 # The same note about the taskdialog license from above applies to the section
 # below.

commit 37e444b0fd83ae31365c7a098f863182766b8ab2

Fix regression when wizard image is missing
Forgot to add a fallback branch when an image is missing and is supposed
to be in the Wizard Images folder.
Broke in 1fe6d2dc38808d079836a2f575699f4ea7a4b2aa.

Mopy/bash/belt.py
@@ -1374,6 +1374,8 @@ class WryeParser(ScriptParser.Parser):
                 std_img_path = to_os_path(bass.dirs['images'].join(i).s)
                 if std_img_path and std_img_path.is_file():
                     image_paths.append(std_img_path)
+                else:
+                    image_paths.append(None)
             else:
                 image_paths.append(None)
         self.page = PageSelect(self._wiz_parent, bMany, main_desc, titles,