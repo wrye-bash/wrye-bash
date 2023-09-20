wrye-bash/wrye-bash

commit 888e2bd4bddb568a224e26da78254d9b17dbe846

Rework Import Order to add a proper dialog
This lets us offer some very neat functionality, like importing only
order, only installation status or both, as well as offering the option
to automatically create missing markers before reordering them.

Originally requested on Discord.

requirements.txt:
Bump requirements

Mostly bugfixes, nothing that should affect us. PyMuPDF apparently has a
new 'fitz_new' API, not sure if we should switch to that yet.

Mopy/bash/basher/dialogs.py
@@ -25,8 +25,9 @@ from dataclasses import dataclass
 
 from .. import balt, bass, bolt, bosh, bush, env, exception, load_order
 from ..balt import DecoratedTreeDict, ImageList, ImageWrapper, colors
-from ..bolt import CIstr, FName, GPath_no_norm, text_wrap, top_level_dirs
-from ..bosh import InstallerProject, ModInfo, faces
+from ..bolt import CIstr, FName, GPath_no_norm, text_wrap, top_level_dirs, \
+    reverse_dict
+from ..bosh import ModInfo, faces
 from ..fomod_schema import default_moduleconfig
 from ..gui import BOTTOM, CENTER, RIGHT, CancelButton, CheckBox, \
     CheckListBox, DeselectAllButton, DialogWindow, DropDown, EventResult, \
@@ -34,7 +35,9 @@ from ..gui import BOTTOM, CENTER, RIGHT, CancelButton, CheckBox, \
     OkButton, Picture, SearchBar, SelectAllButton, Spacer, Stretch, \
     TextAlignment, TextField, VBoxedLayout, VLayout, bell, AMultiListEditor, \
     MLEList, DocumentViewer, RadioButton, showOk, showError, WrappingLabel, \
-    MaybeModalDialogWindow, Tree, HorizontalLine, TreeNode
+    MaybeModalDialogWindow, Tree, HorizontalLine, TreeNode, ImageButton, \
+    FileOpen
+from ..parsers import CsvParser
 from ..update_checker import LatestVersion
 from ..wbtemp import TempDir, cleanup_temp_file, new_temp_file
 
@@ -919,3 +922,129 @@ class MasterErrorsDialog(_AModsChangeHighlightDialog):
     """Dialog shown when Wrye Bash detected master errors before building a
     BP."""
     title = _('Master Errors')
+
+#------------------------------------------------------------------------------
+class AImportOrderParser(CsvParser):
+    """Base class for export/import package order-related classes."""
+    _csv_header = _('Package'), (_('Installed? (%(inst_y)s/%(inst_n)s)')
+                                 % {'inst_y': 'Y', 'inst_n': 'N'})
+
+class ImportOrderDialog(DialogWindow, AImportOrderParser):
+    """Dialog shown when importing package order."""
+    title = _('Import Order')
+    _key_to_import = {
+        'imp_all':       _('Order and Installed Status'),
+        'imp_order':     _('Order Only'),
+        'imp_installed': _('Installed Status Only'),
+    }
+    _import_to_key = reverse_dict(_key_to_import)
+
+    def __init__(self, parent):
+        super().__init__(parent, icon_bundle=balt.Resources.bashBlue)
+        self._bain_parent = parent
+        self._import_target_field = TextField(self, hint=_('CSV to import'))
+        browse_file_btn = ImageButton(self,
+            balt.images['folder.16'].get_bitmap(),
+            btn_tooltip=_('Open a file dialog to interactively choose the '
+                          'saved package order to import.'))
+        browse_file_btn.on_clicked.subscribe(self._handle_browse)
+        self._import_what = DropDown(self,
+            value=self._key_to_import[
+                bass.settings['bash.installers.import_order.what']],
+            choices=sorted(self._key_to_import.values()),
+            dd_tooltip=_('Select what will be imported.'))
+        self._import_what.on_combo_select.subscribe(self._save_import_what)
+        self._create_markers = CheckBox(self, checked=bass.settings[
+            'bash.installers.import_order.create_markers'],
+            chkbx_tooltip=_("If checked, markers (i.e. entries beginning and "
+                            "ending with '==') that don't exist will be "
+                            "created upon import."))
+        ok_btn = OkButton(self)
+        ok_btn.on_clicked.subscribe(self._handle_ok)
+        VLayout(border=6, spacing=4, item_expand=True, items=[
+            HBoxedLayout(self, title=_('File'), spacing=4, items=[
+                (self._import_target_field,
+                 LayoutOptions(expand=True, weight=1)),
+                browse_file_btn,
+            ]),
+            VBoxedLayout(self, title=_('Options'), spacing=4, item_expand=True,
+                items=[
+                    HLayout(spacing=4, items=[
+                        Label(self, _('Import What?')),
+                        (self._import_what,
+                         LayoutOptions(expand=True, weight=1)),
+                    ]),
+                    HLayout(spacing=4, items=[
+                        Label(self, _('Create Missing Markers')),
+                        self._create_markers,
+                    ]),
+            ]),
+            Stretch(),
+            HorizontalLine(self),
+            HLayout(spacing=4, item_expand=True, items=[
+                Stretch(), ok_btn, CancelButton(self),
+            ]),
+        ]).apply_to(self, fit=True)
+
+    def _handle_browse(self):
+        """Internal callback, opens a file dialog to choose the order file."""
+        chosen_order_file = FileOpen.display_dialog(self,
+            title=_('Import Order - Choose File'),
+            defaultFile='PackageOrder.csv', defaultDir=bass.dirs['patches'],
+            wildcard='*.csv')
+        if chosen_order_file:
+            self._import_target_field.text_content = chosen_order_file.s
+
+    def _save_import_what(self, sel_import_what: str):
+        """Internal callback, saves the Import what? value when it's changed so
+        that future Import Order dialogs have a fitting default."""
+        bass.settings['bash.installers.import_order.what'] = \
+            self._import_to_key[sel_import_what]
+
+    def _handle_ok(self):
+        """Internal callback, performs the actual reordering etc."""
+        imp_path = self._import_target_field.text_content
+        if not imp_path: return
+        self.first_line = True
+        self._partial_package_order = []
+        try:
+            self.read_csv(imp_path)
+        except (exception.BoltError, NotImplementedError):
+            balt.showError(self, _('The selected file is not a valid package '
+                                   'order CSV export.'),
+                title=_('Import Order - Invalid CSV'))
+            return
+        bain_idata = self._bain_parent.data_store
+        reorder_err = bain_idata.reorder_packages(self._partial_package_order)
+        bain_idata.refresh_ns()
+        self._bain_parent.RefreshUI()
+        if reorder_err:
+            balt.showError(self, reorder_err, title=_('Import Order - Error'))
+        else:
+            balt.showInfo(self, _('Imported order and installation status for '
+                                  '%(total_imported)d package(s).') % {
+                'total_imported': len(self._partial_package_order)},
+                title=_('Import Order - Done'))
+
+    def _parse_line(self, csv_fields):
+        if self.first_line: # header validation
+            self.first_line = False
+            if len(csv_fields) != 2:
+                raise exception.BoltError(f'Header error: {csv_fields}')
+            return
+        pkg_fstr, pkg_installed_yn = csv_fields
+        bain_idata = self._bain_parent.data_store
+        pkg_fname = bolt.FName(pkg_fstr)
+        pkg_present = pkg_fname in bain_idata
+        pkg_is_marker = pkg_fstr.startswith('==') and pkg_fstr.endswith('==')
+        if (self._create_markers.is_checked and pkg_is_marker and
+                not pkg_present):
+            bain_idata.new_info(pkg_fname, is_mark=True)
+            pkg_present = True
+        if pkg_present:
+            # If we import something other than purely order (and this isn't a
+            # marker, which can't be enabled anyways)
+            if (not pkg_is_marker and
+                    self._import_what.get_value() != 'imp_order'):
+                bain_idata[pkg_fname].is_active = pkg_installed_yn == 'Y'
+            self._partial_package_order.append(pkg_fname)

Mopy/bash/basher/constants.py
@@ -343,6 +343,8 @@ settingDefaults = { # keep current naming format till refactored
     'bash.installers.badDlls': {},
     'bash.installers.onDropFiles.action': None,
     'bash.installers.commentsSplitterSashPos': 0,
+    'bash.installers.import_order.create_markers': True,
+    'bash.installers.import_order.what': 'imp_all',
     #--Wrye Bash: Wizards
     'bash.fomod.size': (600, 500),
     'bash.fomod.pos': DEFAULT_POSITION,

Mopy/bash/basher/gui_fomod.py
@@ -25,8 +25,9 @@ __author__ = u'Ganda'
 
 from collections import defaultdict
 
-from .. import balt, bass, bolt, bush
+from .. import balt, bass, bush
 from ..balt import EnabledLink, Links, colors
+from ..bolt import LowerDict, dict_sort, reverse_dict
 from ..env import get_file_version, get_game_version_fallback, to_os_path
 from ..fomod import FailedCondition, FomodInstaller, GroupType, \
     InstallerGroup, InstallerOption, InstallerPage, OptionType
@@ -44,7 +45,7 @@ class FomodInstallInfo(object):
         # canceled: true if the user canceled or if an error occurred
         self.canceled = False
         # install_files: file->dest mapping of files to install
-        self.install_files = bolt.LowerDict()
+        self.install_files = LowerDict()
         # should_install: boolean on whether to install the files
         self.should_install = True
 
@@ -185,9 +186,8 @@ class InstallerFomod(WizardDialog):
             self._run_wizard()
             # Invert keys and values ahead of time here, so that BAIN doesn't
             # have to do it just in time
-            self.fm_ret.install_files = bolt.LowerDict({
-                v: k for k, v
-                in self.fomod_parser.get_fomod_files().items()})
+            self.fm_ret.install_files = LowerDict(reverse_dict(
+                self.fomod_parser.get_fomod_files()))
         if self._is_arch:
             cleanup_temp_dir(self._fomod_dir)
         return self.fm_ret
@@ -492,7 +492,7 @@ class PageFinish(PageInstaller):
             self._output_text = TextArea(
                 self, editable=False, auto_tooltip=False,
                 init_text=self.display_files(installer_output))
-            sorted_output = bolt.dict_sort(installer_output,
+            sorted_output = dict_sort(installer_output,
                 key_f=lambda k: installer_output[k].lower())  # sort by source
             sorted_src = []
             sorted_dst = []

Mopy/bash/basher/installers_links.py
@@ -26,9 +26,9 @@ points to the InstallersList singleton."""
 from itertools import chain
 
 from . import Installers_Link
-from .dialogs import CreateNewProject, CleanDataEditor, \
-    MonitorExternalInstallationEditor
-from .. import balt, bass, bolt, bosh, bush, exception, load_order
+from .dialogs import CreateNewProject, CleanDataEditor, ImportOrderDialog, \
+    MonitorExternalInstallationEditor, AImportOrderParser
+from .. import balt, bass, bosh, bush, load_order
 from ..balt import AppendableLink, BoolLink, EnabledLink, ItemLink, \
     SeparatorLink
 from ..gui import copy_text_to_clipboard
@@ -284,12 +284,7 @@ class Installers_CreateNewProject(ItemLink):
         CreateNewProject.display_dialog(self.window)
 
 #------------------------------------------------------------------------------
-class _AInstallers_Order(Installers_Link, CsvParser):
-    """Base class for export/import package order links."""
-    _csv_header = _('Package'), (_('Installed? (%(inst_y)s/%(inst_n)s)')
-                                 % {'inst_y': 'Y', 'inst_n': 'N'})
-
-class Installers_ExportOrder(_AInstallers_Order):
+class Installers_ExportOrder(Installers_Link, AImportOrderParser):
     """Export order and installation status for all packages."""
     _text = _('Export Order...')
     _help = _('Export the order and installation status of all packages.')
@@ -326,7 +321,7 @@ class Installers_ExportOrder(_AInstallers_Order):
             self.packages_exported += 1
 
 #------------------------------------------------------------------------------
-class Installers_ImportOrder(_AInstallers_Order):
+class Installers_ImportOrder(Installers_Link):
     """Import order and installation status for a subset of all packages from a
     previous export."""
     _text = _('Import Order...')
@@ -341,47 +336,7 @@ class Installers_ImportOrder(_AInstallers_Order):
               'that. Packages that are not listed in the CSV file will not be '
               'touched.') + '\n\n' + _('Are you sure you want to proceed?'),
                 title=_('Import Order - Warning')): return
-        self._import_acti_status = self._askYes(
-            _("Do you want to import the enabled/disabled status from the CSV "
-              "file as well? Choosing 'No' will only reorder packages, "
-              "leaving enabled/disabled status unchanged."),
-            title=_('Import Order - Import Activation Status?'),
-            questionIcon=True)
-        imp_path = self._askOpen(title=_('Import Order - Choose Source'),
-            defaultDir=bass.dirs['patches'], defaultFile='PackageOrder.csv',
-            wildcard='*.csv')
-        if not imp_path: return
-        self.first_line = True
-        self._partial_package_order = []
-        try:
-            self.read_csv(imp_path)
-        except (exception.BoltError, NotImplementedError):
-            self._showError(_('The selected file is not a valid '
-                              'package order CSV export.'),
-                title=_('Import Order - Invalid CSV'))
-            return
-        reorder_err = self.idata.reorder_packages(self._partial_package_order)
-        self.idata.refresh_ns()
-        self.window.RefreshUI()
-        if reorder_err:
-            self._showError(reorder_err, title=_('Import Order - Error'))
-        else:
-            self._showInfo(_('Imported order and installation status for '
-                             '%(total_imported)d package(s).') % {
-                'total_imported': len(self._partial_package_order)},
-                title=_('Import Order - Done'))
-
-    def _parse_line(self, csv_fields):
-        if self.first_line: # header validation
-            self.first_line = False
-            if len(csv_fields) != 2:
-                raise exception.BoltError(f'Header error: {csv_fields}')
-            return
-        pkg_fstr, pkg_installed_yn = csv_fields
-        if (pkg_fname := bolt.FName(pkg_fstr)) in self.idata:
-            if self._import_acti_status:
-                self.idata[pkg_fname].is_active = pkg_installed_yn == 'Y'
-            self._partial_package_order.append(pkg_fname)
+        ImportOrderDialog.display_dialog(self.window)
 
 #------------------------------------------------------------------------------
 # Installers BoolLinks --------------------------------------------------------

Mopy/bash/basher/settings_dialog.py
@@ -31,7 +31,8 @@ from .constants import colorInfo, settingDefaults
 from .dialogs import UpdateNotification
 from .. import balt, barb, bass, bolt, bosh, bush, env, exception
 from ..balt import Link, Resources, colors
-from ..bolt import deprint, dict_sort, os_name, readme_url, LooseVersion
+from ..bolt import deprint, dict_sort, os_name, readme_url, LooseVersion, \
+    reverse_dict
 from ..gui import ApplyButton, ATreeMixin, BusyCursor, Button, CancelButton, \
     CheckBox, CheckListBox, ClickableImage, Color, ColorPicker, DialogWindow, \
     DirOpen, DoubleListBox, DropDown, FileOpen, FileSave, HBoxedLayout, \
@@ -481,8 +482,7 @@ class LanguagePage(_AScrollablePage):
         u'ru_RU': _(u'Russian') + u' (ру́сский язы́к)',
         u'en_US': _('American English') + ' (American English)',
     })
-    _localized_to_internal = _LangDict(
-        {v: k for k, v in _internal_to_localized.items()})
+    _localized_to_internal = _LangDict(reverse_dict(_internal_to_localized))
 
     def __init__(self, parent, page_desc):
         super(LanguagePage, self).__init__(parent, page_desc)
@@ -1044,7 +1044,7 @@ class ConfirmationsPage(_AFixedPage):
         u'COPY': _(u'Copy'),
         u'MOVE': _(u'Move'),
     }
-    _label_to_action = {v: k for k, v in _action_to_label.items()}
+    _label_to_action = reverse_dict(_action_to_label)
     ##: Maybe hide some of these per game? E.g. Nvidia Fog will never be
     # relevant outside of Oblivion/Nehrim, while Add/Remove ESL Flag makes no
     # sense for non-SSE/FO4 games
@@ -1273,13 +1273,13 @@ class GeneralPage(_AScrollablePage):
         _(u'UTF-8'): u'utf-8',
         _(u'Western European (English, French, German, etc)'): u'cp1252',
     }
-    _encodings_reverse = {v: k for k, v in _all_encodings.items()}
+    _encodings_reverse = reverse_dict(_all_encodings)
     _global_menu_options = {
         _('Both'): 0,
         _('Global Menu Only'): 1,
         _('Column Menu Only'): 2,
     }
-    _gm_reverse = {v: k for k, v in _global_menu_options.items()}
+    _gm_reverse = reverse_dict(_global_menu_options)
     _setting_ids = {'global_menu_state', 'res_scroll_on', 'managed_game',
                     'plugin_encoding', 'update_check_enabled',
                     'update_check_cooldown', 'uac_restart', 'wb_temp_dir'}

Mopy/bash/bolt.py
@@ -345,6 +345,12 @@ def combine_dicts(dict_a: dict[K, V], dict_b: dict[K, V],
     return {**dict_a, **dict_b,
             **{k: f(dict_a[k], dict_b[k]) for k in dict_a.keys() & dict_b}}
 
+def reverse_dict(target_dict: dict[K, V]) -> dict[V, K]:
+    """Create a dict that represents the reverse/inverse mapping of the
+    specified dict. If a -> b in target_dict, then b -> a in the returned
+    dict."""
+    return {v: k for k, v in target_dict.items()}
+
 _not_cached = object()
 
 # PY3.12: Benchmark to see if we can now replace this with cached_property

Mopy/bash/gui/text_components.py
@@ -64,14 +64,17 @@ class _ATextInput(_AComponent):
     _native_widget: _wx.TextCtrl
 
     # TODO: (fixed) font(s)
-    def __init__(self, parent, init_text=None, multiline=True, editable=True,
-            auto_tooltip=True, max_length=None, no_border=False,
+    def __init__(self, parent, init_text: str | None = None,
+            hint: str | None = None, multiline=True, editable=True,
+            auto_tooltip=True, max_length: int | None = None, no_border=False,
             alignment=TextAlignment.LEFT, style=0):
         """Creates a new _ATextInput instance with the specified properties.
 
         :param parent: The object that this text input belongs to. May be a wx
                        object or a component.
         :param init_text: The initial text in this text input.
+        :param hint: The hint for this text input (grey text shown when nothing
+                     has been entered into the text input yet).
         :param multiline: True if this text input allows multiple lines.
         :param editable: True if the user may edit text in this text input.
         :param auto_tooltip: Whether or not to automatically show a tooltip
@@ -96,6 +99,8 @@ class _ATextInput(_AComponent):
             ##: multiline + max length is not supported on GTK
             if not multiline or _wx.Platform != u'__WXGTK__':
                 self._native_widget.SetMaxLength(max_length)
+        if hint:
+            self._set_hint(hint)
         # Events
         # Internal use only - used to implement auto_tooltip below
         self._on_size_changed = self._evt_handler(_wx.EVT_SIZE)
@@ -107,6 +112,11 @@ class _ATextInput(_AComponent):
         if auto_tooltip:
             self._on_size_changed.subscribe(self._on_size_change)
             self.on_text_changed.subscribe(self._update_tooltip)
+
+    def _set_hint(self, hint: str):
+        """Internal method for setting a text input's hint. Needed since
+        SearchCtrl.SetHint does not work correctly on Windows."""
+        self._native_widget.SetHint(hint)
 
     def _update_tooltip(self, new_text: str):
         """Internal callback that shows or hides the tooltip depending on the
@@ -235,6 +245,10 @@ class SearchBar(TextField):
         def _clear_search_bar():
             self.text_content = ''
         on_cancel.subscribe(_clear_search_bar)
+
+    def _set_hint(self, hint: str):
+        # SearchCtrl.SetHint is broken (at least on Windows), avoid it
+        self._native_widget.SetDescriptiveText(hint)
 
 # Labels ----------------------------------------------------------------------
 class _ALabel(_AComponent):

commit 591b98825ca36b5c0b941afc1c880570e25639b0

Fix regression in Import Order
Broke in 888e2bd4bddb568a224e26da78254d9b17dbe846.

Mopy/bash/balt.py:
Fix KeyError when jumping to source

Can only happen if the owner info on an item goes out of sync with the
BAIN packages that are actually present. Clearly indicates that we need
to further fix ownership handling for UIList items (see my 456 branch
for a start).

Mopy/bash/patcher/patchers/preservers.py:
Fix regression from MreCell.flags being None

This probably won't be the only place we have to fix this, since the
patcher also accesses flags as part of its attrs-to-import.

requirements.txt:
Bump dependencies

Just bugfixes, except for pyfiglet which is pretty much just dropping
py2 support.

Mopy/bash/balt.py
@@ -1546,7 +1546,13 @@ class UIList(PanelWin):
         fn_package = self.get_source(uil_item)
         if fn_package is None:
             return False
-        Link.Frame.notebook.SelectPage('Installers', fn_package)
+        try:
+            Link.Frame.notebook.SelectPage('Installers', fn_package)
+        except KeyError:
+            # The package does not exist anymore
+            ##: This points to deeper bugs in our ownership handling/updating
+            # that should be fixed
+            return False
         return True
 
     def get_source(self, uil_item: FName) -> FName | None:

Mopy/bash/basher/dialogs.py
@@ -1044,7 +1044,8 @@ class ImportOrderDialog(DialogWindow, AImportOrderParser):
         if pkg_present:
             # If we import something other than purely order (and this isn't a
             # marker, which can't be enabled anyways)
+            order_only = self._key_to_import['imp_order']
             if (not pkg_is_marker and
-                    self._import_what.get_value() != 'imp_order'):
+                    self._import_what.get_value() != order_only):
                 bain_idata[pkg_fname].is_active = pkg_installed_yn == 'Y'
             self._partial_package_order.append(pkg_fname)

Mopy/bash/env/windows.py
@@ -539,6 +539,7 @@ def _find_ws_games() -> dict[str, _Path]:
     # First, look for the libraries
     found_libraries = []
     # Couldn't just return a list of strings of course - thanks pywin32
+    # PY3.12: Use os.listdrives()
     all_drives = win32api.GetLogicalDriveStrings().rstrip('\x00').split('\x00')
     for curr_drive in all_drives:
         library_path = _parse_gamingroot(curr_drive + '.GamingRoot')

Mopy/bash/patcher/patchers/preservers.py
@@ -443,8 +443,9 @@ class ImportCellsPatcher(ImportPatcher):
                 for cfid, cell_rec in block.iter_present_records(b'CELL'):
                     # If we're in an interior, see if we have to ignore
                     # any attrs
-                    actual_attrs = interior_attrs if \
-                        cell_rec.flags.isInterior else attrs
+                    actual_attrs = (interior_attrs
+                                    if cell_rec.flags and
+                                       cell_rec.flags.isInterior else attrs)
                     for att in actual_attrs:
                         tempCellData[cfid][att] = __attrgetters[att](cell_rec)
             # Add attribute values from record(s) in master file(s). Only adds
@@ -457,8 +458,9 @@ class ImportCellsPatcher(ImportPatcher):
                 for _sig, block in masterFile.iter_tops(self._read_sigs):
                     for cfid, cell_rec in block.iter_present_records(b'CELL'):
                         if cfid not in tempCellData: continue
-                        attrs1 = interior_attrs if cell_rec.flags.isInterior\
-                            else attrs
+                        attrs1 = (interior_attrs
+                                  if cell_rec.flags and
+                                     cell_rec.flags.isInterior else attrs)
                         for att in attrs1:
                             master_attr = __attrgetters[att](cell_rec)
                             if tempCellData[cfid][att] != master_attr: