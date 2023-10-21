wrye-bash/wrye-bash

commit d2b208c0fe3b6d36186239ffd5530b4894d422e8

Fixup for possible bug in EventHandler:
Mopy/bash/gui/events.py:
The docs claim that """The order in which the listeners are processed is
guaranteed to be the same as the order in which they subscribed""" but
pause_subscription would always append possibly violating this condition.

Rest is nit and some fdust - I also changed the docstring interpreter
part as it would not resolve the import - 'images' renames and a stray
bass import (only one local bass import in gui, images related)

Nit and renames from the future - especially from the Store(Enum) work:

Chop off one use of tabInfo and extract some local variables to more
easily track the usesites

bosh: typing nit in _parse_save_path

Note I dropped the bsa.refresh on creating the BSAPanel - we do that in
InitData and RefreshData (and anyway it's disabled). I kinda tried
nuking the BashTab inits (most exist just to set listData) but did
not come up with something satisfactory (remember this is from future,
the BashFrame.***List = self.uiList go bye-bye).

env: some nit split out from the ask_confirm commit for readability

Mopy/bash/basher/__init__.py
@@ -3798,7 +3798,6 @@ class BSAPanel(BashTab):
 
     def __init__(self,parent):
         self.listData = bosh.bsaInfos
-        bosh.bsaInfos.refresh()
         super(BSAPanel, self).__init__(parent)
         BashFrame.bsaList = self.uiList
 
@@ -3806,13 +3805,13 @@ class BSAPanel(BashTab):
 _widget_to_panel = {}
 class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
     """Handle hiding/unhiding tabs."""
-    def __init__(self,tabKey,canDisable=True):
-        super(_Tab_Link, self).__init__()
+
+    def __init__(self, _text, tabKey, *, canDisable=True):
+        super().__init__(_text)
         self.tabKey = tabKey
         self.enabled = canDisable
-        className, self._text, item = tabInfo.get(self.tabKey,[None,None,None])
-        self._help = _(u'Show/Hide the %(tabtitle)s Tab.') % (
-            {u'tabtitle': self._text})
+        self._help = _('Show/Hide the %(tabtitle)s Tab.') % {
+            'tabtitle': self._text}
 
     def _append(self, window): return self._text is not None
 
@@ -3821,6 +3820,7 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
     def _check(self): return bass.settings[u'bash.tabs.order'][self.tabKey]
 
     def Execute(self):
+        tab_info = tabInfo
         if bass.settings[u'bash.tabs.order'][self.tabKey]:
             # It was enabled, disable it.
             iMods = None
@@ -3828,11 +3828,11 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
             iDelete = None
             for i in range(Link.Frame.notebook.GetPageCount()):
                 pageTitle = Link.Frame.notebook.GetPageText(i)
-                if pageTitle == tabInfo[u'Mods'][1]:
+                if pageTitle == tab_info[u'Mods'][1]:
                     iMods = i
-                elif pageTitle == tabInfo[u'Installers'][1]:
+                elif pageTitle == tab_info[u'Installers'][1]:
                     iInstallers = i
-                if pageTitle == tabInfo[self.tabKey][1]:
+                if pageTitle == tab_info[self.tabKey][1]:
                     iDelete = i
             if iDelete == Link.Frame.notebook.GetSelection():
                 # We're deleting the current page...
@@ -3842,7 +3842,7 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
                     # the 'Installers' tab.  Change to the
                     # 'Mods' tab instead.
                     Link.Frame.notebook.SetSelection(iMods)
-            tabInfo[self.tabKey][2].ClosePanel() ##: note the panel remains in memory
+            tab_info[self.tabKey][2].ClosePanel() ##: note the panel remains in memory
             page = Link.Frame.notebook.GetPage(iDelete)
             Link.Frame.notebook.RemovePage(iDelete)
             page.Show(False)
@@ -3852,10 +3852,10 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
             for k, k_enabled in bass.settings[u'bash.tabs.order'].items():
                 if k == self.tabKey: break
                 insertAt += k_enabled
-            className,title,panel = tabInfo[self.tabKey]
+            className,title,panel = tab_info[self.tabKey]
             if not panel:
                 panel = globals()[className](Link.Frame.notebook)
-                tabInfo[self.tabKey][2] = panel
+                tab_info[self.tabKey][2] = panel
                 _widget_to_panel[panel.wx_id_()] = panel
             if insertAt > Link.Frame.notebook.GetPageCount():
                 Link.Frame.notebook.AddPage(panel._native_widget,title)
@@ -3892,9 +3892,10 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         balt.TabDragMixin.__init__(self)
         #--Pages
         iInstallers = iMods = -1
+        tab_info = tabInfo
         for page, enabled in self._tabOrder().items():
             if not enabled: continue
-            className, title, item = tabInfo[page]
+            className, title, _item = tab_info[page]
             panel = globals().get(className,None)
             if panel is None: continue
             deprint(f"Constructing panel '{title}'")
@@ -3905,7 +3906,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
             try:
                 item = panel(self)
                 self.AddPage(item._native_widget, title)
-                tabInfo[page][2] = item
+                tab_info[page][2] = item
                 _widget_to_panel[item.wx_id_()] = item
                 deprint(f"Panel '{title}' constructed successfully")
             except:
@@ -3930,9 +3931,9 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
     def tabLinks(menu):
         # use tabOrder here - it is used in InitLinks which runs *before*
         # settings['bash.tabs.order'] is set!
-        for key in BashNotebook._tabOrder():
-            canDisable = bool(key != u'Mods')
-            menu.append(_Tab_Link(key, canDisable))
+        for key, (__cls, tab_title, __panel) in BashNotebook._tabOrder():
+            menu.append(
+                _Tab_Link(tab_title, key, canDisable=bool(key != 'Mods')))
         return menu
 
     def SelectPage(self, page_title, item):
@@ -3942,12 +3943,12 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         EventResult.FINISH, otherwise a later OS handler may steal focus onto
         the now-invisible tab."""
         ind = 0
-        for title, enabled in settings[u'bash.tabs.order'].items():
-            if title == page_title:
-                if not enabled: return
+        for tab_key, is_enabled in settings['bash.tabs.order'].items():
+            if tab_key == page_title:
+                if not is_enabled: return
                 break
-            ind += enabled
-        else: raise BoltError(f'Invalid page: {page_title}')
+            ind += is_enabled
+        else: raise BoltError(f'Invalid tab key: {page_title}')
         self.SetSelection(ind)
         tabInfo[page_title][2].SelectUIListItem(item, deselectOthers=True)
 
@@ -3970,9 +3971,9 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
                 break
         oldOrder.remove(removeKey)
         if newPos == 0: # Moved to the front
-            newOrder = [removeKey] + oldOrder
+            newOrder = [removeKey, *oldOrder]
         elif newPos == self.GetPageCount() - 1: # Moved to the end
-            newOrder = oldOrder + [removeKey]
+            newOrder = [*oldOrder, removeKey]
         else: # Moved somewhere in the middle
             nextTabTitle = self.GetPageText(newPos+1)
             for nextTabKey in oldOrder:

Mopy/bash/bosh/__init__.py
@@ -3260,19 +3260,19 @@ class SaveInfos(FileInfos):
             return accepted_exts
 
     @classmethod
-    def _parse_save_path(cls, save_path: str) -> tuple[str | None,
-                                                       str | None]:
-        """Parses the specified save path into root and extension, returning
+    def _parse_save_path(cls, save_name: FName | str) -> tuple[
+            str | None, str | None]:
+        """Parses the specified save name into root and extension, returning
         them as a tuple. If the save path does not point to a valid save,
         returns two Nones instead."""
         accepted_exts = cls.valid_save_exts()
-        save_root, save_ext = os.path.splitext(save_path)
+        save_root, save_ext = os.path.splitext(save_name)
         save_ext_trunc = save_ext[1:]
         if save_ext_trunc.lower() not in accepted_exts:
             # Can't be a valid save, doesn't end in ess/esr/bak
             return None, None
         cs_ext = bush.game.Se.cosave_ext[1:]
-        if any(s.lower() == cs_ext for s in save_root.split(u'.')):
+        if any(s.lower() == cs_ext for s in save_root.split('.')):
             # Almost certainly not a valid save, had the cosave extension
             # in one of its root parts
             return None, None

Mopy/bash/balt.py
@@ -87,7 +87,7 @@ class ImageList(object):
     def __init__(self, il_width, il_height):
         self.width = scaled(il_width)
         self.height = scaled(il_height)
-        self.images = []
+        self._images = []
         self.indices = {}
         self.imageList = None
 
@@ -95,10 +95,10 @@ class ImageList(object):
         if not self.imageList:
             imageList = self.imageList = wx.ImageList(self.width, self.height)
             self.indices = {k: imageList.Add(im.get_bitmap()) for k, im in
-                            self.images}
+                            self._images}
         return self.imageList
 
-    def get_icon(self, key): return self.images[self[key]][1].GetIcon() # YAK !
+    def get_icon(self, key): return self._images[self[key]][1].GetIcon() # YAK !
 
     def __getitem__(self,key):
         self.GetImageList()
@@ -118,7 +118,7 @@ class ColorChecks(ImageList):
                 image_key = f'checkbox.{shortKey}'
                 img = im_dir.join(f'checkbox_{status}_{state}.png')
                 image = images[image_key] = ImageWrapper(img, iconSize=16)
-                self.images.append((shortKey, image))
+                self._images.append((shortKey, image))
 
     def Get(self,status,on):
         self.GetImageList()
@@ -157,7 +157,7 @@ class InstallerColorChecks(ImageList):
         super().__init__(16, 16)
         imDirJn = bass.dirs['images'].join
         def _icc(fname): return ImageWrapper(imDirJn(fname), iconSize=16)
-        self.images.extend({
+        self._images.extend({
             #--Off/Archive
             'off.green':  _icc('checkbox_green_off.png'),
             'off.grey':   _icc('checkbox_grey_off.png'),

Mopy/bash/basher/app_buttons.py
@@ -72,8 +72,7 @@ class StatusBar_Button(ItemLink):
         self.canHide = canHide
         self.gButton = None
         self._tip = button_tip or self.__class__._tip
-        if uid is None: uid = (self.__class__.__name__, self._tip)
-        self.uid = uid
+        self.uid = (self.__class__.__name__, self._tip) if uid is None else uid
 
     def IsPresent(self):
         """Due to the way status bar buttons are implemented debugging is a

Mopy/bash/belt.py
@@ -167,7 +167,7 @@ class PageSelect(PageInstaller):
                  image_paths):
         PageInstaller.__init__(self, parent)
         self.listItems = list(items_default)
-        self.images = image_paths
+        self._images = image_paths
         self.descs = listDescs
         self.bMany = bMany
         self.index = None
@@ -214,7 +214,7 @@ class PageSelect(PageInstaller):
         self.Selection(lb_selection_dex)
 
     def _click_on_image(self):
-        img = self.images[self.index]
+        img = self._images[self.index]
         if not img:
             return # None - no image path specified
         try:
@@ -233,7 +233,7 @@ class PageSelect(PageInstaller):
         self._page_parent.enable_forward(True)
         self.index = index
         self.textItem.text_content = self.descs[index]
-        self.bmp_item.set_bitmap(self.images[index])
+        self.bmp_item.set_bitmap(self._images[index])
         # self.Layout() # the bitmap would change size and show blurred
 
     def OnNext(self):

Mopy/bash/env/common.py
@@ -261,18 +261,19 @@ def __copy_or_move(sources_dests: dict[Path, Path], rename_on_collision: bool,
             raise FileNotFoundError(os.fspath(from_path))
     return operation_results
 
-def file_operation(operation: str | FileOperationType,
+
+def file_operation(operation: FileOperationType,
         sources_dests: dict[_StrPath, _StrPath], allow_undo=True,
         ask_confirm=False, rename_on_collision=False,
         silent = False, parent=None) -> dict[str, str]:
     """file_operation API. Performs a filesystem operation on the specified
     files.
 
-    NOTE: This generic version is still WIP: it doesn't support all the optional
-    features, and returns an empty mapping for the results.
+    NOTE: This generic version is still WIP: it doesn't support all the
+    optional features, and returns an empty mapping for the results.
 
-    :param operation: One of the FileOperationType enum values, corresponding to
-        a move, copy, rename, or delete operation.
+    :param operation: One of the FileOperationType enum values, corresponding
+        to a move, copy, rename, or delete operation.
     :param sources_dests: A mapping of source paths to destinations. The format
         of destinations depends on the operation:
         - For FileOperationTYpe.DELETE: destinations are ignored, they may be
@@ -289,7 +290,7 @@ def file_operation(operation: str | FileOperationType,
     :param ask_confirm: If False, responds to any OS dialog boxes with
         "Yes to All".
     :param rename_on_collision: If True, automatically renames files on a move
-        or copy when collisions occcur.
+        or copy when collisions occur.
     :param silent: If True, do not display progress dialogs.
     :param parent: The parent window for any dialogs.
 

Mopy/bash/env/windows.py
@@ -56,7 +56,6 @@ from ..bolt import Path as _Path
 from ..bolt import deprint as _deprint
 from ..bolt import unpack_int as _unpack_int
 from ..exception import BoltError, CancelError, SkipError
-from ..wbtemp import TempFile
 
 # File operations -------------------------------------------------------------
 try:

Mopy/bash/game/patch_game.py
@@ -294,7 +294,7 @@ class PatchGame(GameInfo):
                     continue
                 rec_class.validate_record_syntax()
         if miss := [s for s in valid_header_sigs if s not in _sig_class]:
-            bolt.deprint(f'Signatures {miss} lack an implementation - '
+            bolt.deprint(f'Signatures {sorted(miss)} lack an implementation - '
                          f'defaulting to MreRecord')
             _sig_class.update(dict.fromkeys(miss, _brec_.MreRecord))
         rtype.sig_to_class = {k: v for k, v in _sig_class.items() if

commit 1fc22e5df4fce9fa7fd8ba20f9fc4a367f5d4697

Fix regression in tab menu:
Introduced in d2b208c0fe3b6d36186239ffd5530b4894d422e8. Took the
opportunity to get us rid of the rather confusing menu creation every
time DoTabMenu was run.

_tabOrder was used once finally - adieu

Mopy/bash/basher/__init__.py
@@ -3860,9 +3860,12 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
 
 class BashNotebook(wx.Notebook, balt.TabDragMixin):
 
-    @staticmethod
-    def _tabOrder():
-        """Return dict containing saved tab order and enabled state of tabs."""
+    def __init__(self, parent):
+        wx.Notebook.__init__(self, parent)
+        balt.TabDragMixin.__init__(self)
+        #--Pages
+        iInstallers = iMods = -1
+        self._tab_menu = Links()
         # default tabs order and default enabled state, keys as in tabInfo
         tabs_enabled_ordered = dict(e.value for e in Store)
         newOrder = settings.get('bash.tabs.order', tabs_enabled_ordered)
@@ -3875,17 +3878,11 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         # Ensure the 'Mods' tab is always shown
         newOrder['Mods'] = True # would insert last
         settings[u'bash.tabs.order'] = newOrder
-        return newOrder
-
-    def __init__(self, parent):
-        wx.Notebook.__init__(self, parent)
-        balt.TabDragMixin.__init__(self)
-        #--Pages
-        iInstallers = iMods = -1
-        tab_info = tabInfo
-        for page, enabled in self._tabOrder().items():
+        tabs = {k: (v, *tabInfo[k][:2]) for k, v in newOrder.items()}
+        for page, (enabled, className, title) in tabs.items():
+            self._tab_menu.append(
+                _Tab_Link(title, page, canDisable=page != 'Mods'))
             if not enabled: continue
-            className, title, _item = tab_info[page]
             panel = globals().get(className,None)
             if panel is None: continue
             deprint(f"Constructing panel '{title}'")
@@ -3896,7 +3893,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
             try:
                 item = panel(self)
                 self.AddPage(item._native_widget, title)
-                tab_info[page][2] = item
+                tabInfo[page][2] = item
                 deprint(f"Panel '{title}' constructed successfully")
             except:
                 if page == 'Mods':
@@ -3919,15 +3916,6 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         return tabInfo[_title_to_tab[
             self.GetPageText(self.GetSelection())]][2]
 
-    @staticmethod
-    def tabLinks(menu):
-        # use tabOrder here - it is used in InitLinks which runs *before*
-        # settings['bash.tabs.order'] is set!
-        for key, (__cls, tab_title, __panel) in BashNotebook._tabOrder():
-            menu.append(
-                _Tab_Link(tab_title, key, canDisable=bool(key != 'Mods')))
-        return menu
-
     def SelectPage(self, page_title, item):
         """Jumps to the specified item on the specified tab.
 
@@ -3949,8 +3937,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         pos = self.ScreenToClient(pos)
         tabId = self.HitTest(pos)
         if tabId != wx.NOT_FOUND and tabId[0] != wx.NOT_FOUND:
-            menu = self.tabLinks(Links())
-            menu.popup_menu(self, None)
+            self._tab_menu.popup_menu(self, None)
         else:
             event.Skip()
 

Mopy/bash/basher/settings_dialog.py
@@ -282,7 +282,7 @@ class ColorsPage(_AFixedPage): ##: _AScrollablePage breaks the color picker??
     def UpdateUIColors():
         """Update the Bash Frame with the new colors"""
         with BusyCursor():
-            for (className,title,panel) in tabInfo.values():
+            for (_className, _title, panel) in tabInfo.values():
                 if panel is not None:
                     panel.RefreshUIColors()
 