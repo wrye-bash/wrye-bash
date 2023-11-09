wrye-bash/wrye-bash

commit d5673f44b0a9e35360746984efc2ed4daa8fcf6f

WIP inisettings:
This finally pins it and bins initOptions. I went ahead and collected
all the settings we query the ini for, added SkipWSDetection and stopped
passing the ConfigParser around - adieu bashIni, bashIni_, bash_ini_

Mopy/bash/initialization.py: we should make sure that we have an absolute
path and drop the rest of bass.dirs['app'].join

Nit in find_launcher sig

Under #178, #600

Mopy/bash/bash.py
@@ -329,14 +329,59 @@ def dump_environment(wxver=None):
     bolt.deprint(msg := '\n\t'.join([l for l in msg if l is not None]))
     return msg
 
-def _bash_ini_parser(bash_ini_path):
-    bash_ini_parser = None
-    if bash_ini_path is not None and os.path.exists(bash_ini_path):
-        bash_ini_parser = ConfigParser()
-        # bash.ini is always compatible with UTF-8 (Russian INI is UTF-8,
-        # English INI is ASCII)
-        bash_ini_parser.read(bash_ini_path, encoding='utf-8')
-    return bash_ini_parser
+def _parse_bash_ini(bash_ini_path):
+    """Set default values for all valid INI settings then update from ini."""
+    ini_set = { # sections are case-sensitive
+        'General': dict.fromkeys(
+            ['BashModData', 'InstallersData', 'LocalAppDataPath',
+             'OblivionMods', 'OblivionPath', 'PersonalPath', 'UserPath'], ''),
+        'Settings': {
+            'OblivionTexturesBSAName': 'Oblivion - Textures - Compressed.bsa',
+            'Command7z': '7z', 'ScriptFileExt': '.txt',
+            **dict.fromkeys(['AutoItemCheck', 'EnableSplashScreen',
+                'EnsurePatchExists', 'PromptActivateBashedPatch',
+                'ResetBSATimestamps', 'WarnTooManyFiles'], True),
+            **dict.fromkeys(['ShowDevTools', 'SkipHideConfirmation',
+                'SkipResetTimeNotifications', 'SkipWSDetection'], False),
+            **dict.fromkeys(['7zExtraCompressionArguments',
+                'SkippedBashInstallersDirs', 'SoundError', 'SoundSuccess',
+                'xEditCommandLineArguments'], '')
+        },
+        'Tool Options': {
+            'OblivionBookCreatorJavaArg': '-Xmx1024m',
+            'Tes4GeckoJavaArg': '-Xmx1024m', 'ShowTextureToolLaunchers': True,
+            'ShowModelingToolLaunchers': True, 'ShowAudioToolLaunchers': True
+        }
+    }
+    bass.inisettings.clear() #ini might be reinitialized due to restore failing
+    for v in ini_set.values():
+        bass.inisettings.update(v)
+    # if bash.ini exists update those settings from there
+    if bash_ini_path is None or not os.path.exists(bash_ini_path):
+        return
+    bash_ini_parser = ConfigParser()
+    # bash.ini is always compatible with UTF-8 (Russian INI is UTF-8,
+    # English INI is ASCII)
+    bash_ini_parser.read(bash_ini_path, encoding='utf-8')
+    for section in bash_ini_parser.sections():
+        section_defaults = ini_set.get(section, {})
+        section_defaults = {k.lower(): (k, v) for k, v in
+                            section_defaults.items()}
+        # retrieving ini settings is case-insensitive - key: lowercase
+        for ini_key_lower, value in bash_ini_parser.items(section):
+            if not value or value == '.': continue
+            if default := section_defaults.get(ini_key_lower[1:]):
+                ini_settings_key, default_value = default
+                if type(default_value) is bool:
+                    value = bash_ini_parser.getboolean(section, ini_key_lower)
+                else:
+                    value = value.strip()
+                bass.inisettings[ini_settings_key] = value
+            elif section == 'Tool Options':
+                ##:(570) provisional - we want to stop specifying tool paths
+                # in the ini but we need some UI for that
+                # stash all settings in here in case they match tool path keys
+                bass.inisettings[ini_key_lower[1:]] = value
 
 # Main ------------------------------------------------------------------------
 def main(opts):
@@ -444,7 +489,7 @@ def _main(opts, wx_locale, wxver):
             restore_ = None
     # The rest of backup/restore functionality depends on setting the game
     try:
-        bashIni, bush_game, game_ini_path = _detect_game(opts, bash_ini_path)
+        bush_game, game_ini_path = _detect_game(opts, bash_ini_path)
         if not bush_game: return
         if restore_:
             try:
@@ -461,11 +506,9 @@ def _main(opts, wx_locale, wxver):
                 # _detect_game -> _import_bush_and_set_game
                 from . import bush
                 bush.reset_bush_globals()
-                bashIni, bush_game, game_ini_path = _detect_game(opts, u'bash.ini')
-        ##: Break bosh-balt/gui coupling, though this doesn't actually cause
-        # init problems (since we init those in main)
+                bush_game, game_ini_path = _detect_game(opts, 'bash.ini')
         from . import bosh
-        bosh.initBosh(bashIni, game_ini_path)
+        bosh.initBosh(game_ini_path)
         from . import env
         env.testUAC(bush_game.gamePath.join(bush_game.mods_dir))
         global basher # share this instance with _close_dialog_windows
@@ -480,7 +523,7 @@ def _main(opts, wx_locale, wxver):
     basher.InitSettings()
     # Status bar buttons (initialized in InitStatusBar) use images
     basher.InitImages()
-    basher.links_init.InitStatusBar(bashIni)
+    basher.links_init.InitStatusBar()
     basher.InitLinks()
     #--Start application
     bapp = basher.BashApp(bash_app)
@@ -546,39 +589,35 @@ def _main(opts, wx_locale, wxver):
 
 def _detect_game(opts, backup_bash_ini):
     # Read the bash.ini file either from Mopy or from the backup location
-    bashIni = _bash_ini_parser(backup_bash_ini)
+    _parse_bash_ini(backup_bash_ini)
     # if uArg is None, then get the UserPath from the ini file
-    user_path = opts.userPath or None  ##: not sure why this must be set first
-    if user_path is None:
-        ini_user_path = bass.get_ini_option(bashIni, u'sUserPath')
-        if ini_user_path and not ini_user_path == u'.':
-            user_path = ini_user_path
+    ##: not sure why this must be set first
+    user_path = opts.userPath or bass.inisettings['UserPath']
     if user_path:
         homedrive, homepath = os.path.splitdrive(user_path)
         os.environ[u'HOMEDRIVE'] = homedrive
         os.environ[u'HOMEPATH'] = homepath
     # Detect the game we're running for ---------------------------------------
-    bush_game = _import_bush_and_set_game(opts, bashIni)
+    bush_game = _import_bush_and_set_game(opts)
     if not bush_game:
         return None, None, None
     #--Initialize Directories to perform backup/restore operations
     #--They depend on setting the bash.ini and the game
     from . import initialization
     game_ini_path, init_warnings = initialization.init_dirs(
-        bashIni, opts.personalPath, opts.localAppDataPath, bush_game)
+        opts.personalPath, opts.localAppDataPath, bush_game)
     if init_warnings:
         warning_msg = _('The following (non-critical) warnings were found '
                         'during initialization:')
         warning_msg += '\n\n'
         warning_msg += '\n'.join(f'- {w}' for w in init_warnings)
         _show_boot_popup(warning_msg, is_critical=False)
-    return bashIni, bush_game, game_ini_path
+    return bush_game, game_ini_path
 
-def _import_bush_and_set_game(opts, bashIni):
+def _import_bush_and_set_game(opts):
     from . import bush
     bolt.deprint(u'Searching for game to manage:')
-    game_infos = bush.detect_and_set_game(cli_game_dir=opts.oblivionPath,
-        bash_ini_=bashIni)
+    game_infos = bush.detect_and_set_game(opts.oblivionPath)
     if game_infos is not None:  # None == success
         if len(game_infos) == 0:
             _show_boot_popup(_(
@@ -596,7 +635,7 @@ def _import_bush_and_set_game(opts, bashIni):
         # Add the game to the command line, so we use it if we restart
         gname, gm_path = retCode
         bass.update_sys_argv([u'--oblivionPath', f'{gm_path}'])
-        bush.detect_and_set_game(opts.oblivionPath, bashIni, gname, gm_path)
+        bush.detect_and_set_game(opts.oblivionPath, gname, gm_path)
     return bush.game
 
 def _show_boot_popup(msg, is_critical=True):

Mopy/bash/bosh/__init__.py
@@ -32,7 +32,6 @@ import re
 import sys
 from collections import defaultdict, OrderedDict
 from collections.abc import Iterable
-from configparser import ConfigParser
 from functools import wraps
 from itertools import chain
 
@@ -3729,55 +3728,7 @@ class InstallerMarker(InstallerMarker): pass
 class InstallerProject(InstallerProject): pass
 
 # Initialization --------------------------------------------------------------
-def initOptions(bashIni: ConfigParser):
-    # *some* settings from the INI - note we get some ini settings (such as
-    # sOblivionMods) via get_ini_option/get_path_from_ini we never store those
-    # in bass.inisettings
-    inisettings['ScriptFileExt'] = '.txt'
-    inisettings['ResetBSATimestamps'] = True
-    inisettings['EnsurePatchExists'] = True
-    inisettings['OblivionTexturesBSAName'] = 'Oblivion - Textures - Compressed.bsa'
-    inisettings['ShowDevTools'] = False
-    inisettings['Tes4GeckoJavaArg'] = '-Xmx1024m'
-    inisettings['OblivionBookCreatorJavaArg'] = '-Xmx1024m'
-    inisettings['ShowTextureToolLaunchers'] = True
-    inisettings['ShowModelingToolLaunchers'] = True
-    inisettings['ShowAudioToolLaunchers'] = True
-    inisettings['7zExtraCompressionArguments'] = ''
-    inisettings['xEditCommandLineArguments'] = ''
-    inisettings['AutoItemCheck'] = True
-    inisettings['SkipHideConfirmation'] = False
-    inisettings['SkipResetTimeNotifications'] = False
-    inisettings['SoundSuccess'] = ''
-    inisettings['SoundError'] = ''
-    inisettings['EnableSplashScreen'] = True
-    inisettings['PromptActivateBashedPatch'] = True
-    inisettings['WarnTooManyFiles'] = True
-    inisettings['SkippedBashInstallersDirs'] = ''
-    inisettings['Command7z'] = '7z'
-    # if bash.ini exists update those settings from there
-    if bashIni:
-        defaultOptions = {}
-        for ini_settings_key, defaultValue in inisettings.items():
-            valueType = type(defaultValue)
-            ini_key = ('b' if valueType is bool else 's') + ini_settings_key
-            defaultOptions[ini_key.lower()] = (ini_settings_key, valueType)
-        for section in bashIni.sections():
-            # retrieving ini settings is case insensitive - key: lowecase
-            for ini_key_lower, value in bashIni.items(section):
-                if ini_settings_key := defaultOptions.get(ini_key_lower):
-                    if value == '.': continue
-                    ini_settings_key, value_type = ini_settings_key
-                    if value_type is bool:
-                        value = bashIni.getboolean(section, ini_key_lower)
-                    elif ini_settings_key[:5] == 'Sound':
-                        value = bass.dirs['app'].join(value).s if not (
-                            value := GPath(value)).is_absolute() else value.s
-                    inisettings[ini_settings_key] = value
-    if os_name != 'nt':
-        archives.exe7z = inisettings[u'Command7z']
-
-def initBosh(bashIni, game_ini_path):
+def initBosh(game_ini_path):
     # Setup loot_parser, needs to be done after the dirs are initialized
     if not initialization.bash_dirs_initialized:
         raise BoltError(u'initBosh: Bash dirs are not initialized')
@@ -3789,7 +3740,8 @@ def initBosh(bashIni, game_ini_path):
     gameInis.extend(IniFile(dirs[u'saveBase'].join(x), 'cp1252') for x in
                     bush.game.Ini.dropdown_inis[1:])
     load_order.initialize_load_order_files()
-    initOptions(bashIni)
+    if os_name != 'nt':
+        archives.exe7z = bass.inisettings['Command7z']
     Installer.init_bain_dirs()
 
 def initSettings(ask_yes, readOnly=False, _dat='BashSettings.dat',

Mopy/bash/basher/app_buttons.py
@@ -203,11 +203,11 @@ class AppButton(AppLauncher, StatusBarButton):
         showError(Link.Frame, msg, title=error_title)
 
     @classmethod
-    def app_button_factory(cls, app_key, app_launcher, path_kwargs, bash_ini,
-                           *args, **kwargs):
+    def app_button_factory(cls, app_key, app_launcher, path_kwargs, *args,
+                           **kwargs):
         if kwargs.setdefault('display_launcher', True):
             exe_path, is_present = cls.find_launcher(app_launcher, app_key,
-                                                     bash_ini, **path_kwargs)
+                                                     **path_kwargs)
             # App_Button is initialized once on boot, if the path doesn't exist
             # at this time then it will be detected on next launch of Bash
             kwargs['display_launcher'] &= is_present

Mopy/bash/basher/links_init.py
@@ -58,7 +58,7 @@ _is_skyrim = bush.game.fsName == 'Skyrim'
 _j = os.path.join
 
 #------------------------------------------------------------------------------
-def InitStatusBar(bashIni):
+def InitStatusBar():
     """Initialize status bar buttons."""
     __fp = GuiImage.from_path
     def _png_list(template):
@@ -88,7 +88,7 @@ def InitStatusBar(bashIni):
         if cli_args:
             kwargs['cli_args'] = (*kwargs.get('cli_args', ()), *cli_args)
         return clazz.app_button_factory(app_key, app_launcher, path_kwargs,
-            bashIni, list_img, tooltip_str, **kwargs)
+            list_img, tooltip_str, **kwargs)
     # Launchers of tools ------------------------------------------------------
     all_links.extend(_tool_args(*tool, display_launcher=_is_oblivion) for tool
                      in oblivion_tools.items())

Mopy/bash/bass.py
@@ -20,10 +20,10 @@
 #  https://github.com/wrye-bash
 #
 # =============================================================================
-
 """This module just stores some data that all modules have to be able to access
 without worrying about circular imports. Currently used to expose layout
 and environment issues - do not modify or imitate (ut)."""
+
 from collections import defaultdict
 from enum import Enum
 from typing import TYPE_CHECKING, NewType
@@ -65,13 +65,12 @@ def update_sys_argv(arg):
         if not arg[0] in sys_argv:
             sys_argv.append(arg[0])
 
-def get_ini_option(ini_parser, option_key, section_key=u'General'):
-    if not ini_parser:
-        return None
-    # logic for getting the path from the ini - get(section, key,
-    # fallback=default). section is case sensitive - key is not - return type
-    # is str in py3
-    return ini_parser.get(section_key, option_key, fallback=None)
+def get_path_from_ini(option_key, dir_key='app'):
+    if not (get_value := inisettings.get(option_key)):
+        return get_value
+    from .bolt import GPath
+    return value if (value := GPath(get_value)).is_absolute() else dirs[
+        dir_key].join(value)
 
 class Store(Enum):
     """Inter panel communication - member values are the tab keys in tabInfo

commit 79c0dd756f2996b7256102541d81f539953cbcd6

Nit and fixups in lo:
Partially revert c8a91decd768b3c3fc14e0bdb064dee0decdc389 as I now
realise that the problem lies deeper. `cached_lo_save_active` is meant
to be used when we activated/deactivated without changing the load order.
So we *should* pass None to set_load_order to bypass the _fix_load_order
call.

'espms' renames in comments

Infernio:

Regression fix for cached_lo_append_if_missing

Not sure when this broke, probably during the store enum refactoring.

Mopy/bash/bash.py: Regression fix for canceling boot popup

Broke in d5673f44b0a9e35360746984efc2ed4daa8fcf6f.

Add Starfield tag list to update script:

For future-proofing.

Under #667

Co-authored-by: Infernio <infernio@icloud.com>

Mopy/bash/_games_lo.py
@@ -284,9 +284,9 @@ class LoGame(object):
             self._persist_load_order(lo, None) # active is not used here
 
     def set_load_order(self, lord, active, previous_lord=None,
-                       previous_active=None, dry_run=False, fix_lo=None):
+                       previous_active=None, fix_lo=None):
         """Set the load order and/or active plugins (or just validate if
-        dry_run is True). The different way each game handles this and how
+        previous_* is None). The different way each game handles this and how
         it modifies common data structures necessitate that info on previous
         (cached) state is passed in, usually for both active plugins and
         load order. For instance, in the case of asterisk games, plugins.txt
@@ -312,18 +312,19 @@ class LoGame(object):
         :returns the (possibly fixed) lord and active lists
         """
         if lord is active is None:
-            raise ValueError(u'Load order or active must be not None')
+            raise ValueError('Load order or active must be not None')
+        dry_run = previous_lord is previous_active is None
         setting_lo = lord is not None
-        setting_active = active is not None
         if setting_lo:
             # fix the load order - lord is modified in place, hence test below
             self._fix_load_order(lord, fix_lo=fix_lo)
-        setting_lo = setting_lo and previous_lord != lord
+            setting_lo = previous_lord != lord
+        setting_active = active is not None
         if setting_lo and not setting_active:
             # changing load order - must test if active plugins must change too
             if previous_active is None: # active is None
                 raise ValueError(
-                    u'You must pass info on active when setting load order')
+                    'You must pass info on active when setting load order')
             setting_active = previous_lord is None # we must check active
             if not setting_active: # does active need change due to lo changes?
                 prev = set(previous_lord)
@@ -345,7 +346,7 @@ class LoGame(object):
                                      fix_active=fix_lo)
         lord = lord if setting_lo else previous_lord
         active = active if setting_active else previous_active
-        if lord is  None or active is  None: # sanity check
+        if lord is None or active is None: # sanity check
             raise Exception(u'Returned load order and active must be not None')
         if not dry_run: # else just return the (possibly fixed) lists
             self._persist_if_changed(active, lord, previous_active,

Mopy/bash/bash.py
@@ -590,7 +590,7 @@ def _detect_game(opts, backup_bash_ini):
     # Detect the game we're running for ---------------------------------------
     bush_game = _import_bush_and_set_game(opts)
     if not bush_game:
-        return None, None, None
+        return None, None
     #--Initialize Directories to perform backup/restore operations
     #--They depend on setting the bash.ini and the game
     from . import initialization

Mopy/bash/basher/installer_links.py
@@ -362,7 +362,7 @@ class Installer_Wizard(_Installer_AWizardLink):
                         sel_package.espmNots.add(espm)) # bool(None) == False
                     idetails.gEspmList.lb_check_at_index(index, do_check)
                 idetails.refreshCurrent(sel_package)
-                #Rename the espms that need renaming
+                #Rename the plugins that need renaming
                 for oldName, renamed in ret.rename_plugins.items():
                     sel_package.setEspmName(oldName, renamed)
                 idetails.refreshCurrent(sel_package)

Mopy/bash/bosh/__init__.py
@@ -2196,9 +2196,8 @@ class ModInfos(FileInfos):
 
         Always call AFTER setting the load order - make sure we unghost
         ourselves so ctime of the unghosted mods is not set."""
-        load_order.save_lo(load_order.cached_lo_tuple(),
-            load_order.cached_lord.lorder(
-                self._active_wip if active is None else active))
+        load_order.save_lo(None, load_order.cached_lord.lorder(
+            self._active_wip if active is None else active))
 
     @_lo_cache
     def cached_lo_save_lo(self):
@@ -2262,15 +2261,17 @@ class ModInfos(FileInfos):
         self._lo_wip[first_dex:first_dex] = modlist
 
     def cached_lo_append_if_missing(self, mods):
-        new = mods - set(self._lo_wip)
+        lo_wip_set = set(self._lo_wip)
+        new = [x for x in mods if x not in lo_wip_set]
         if not new: return
-        esms = {x for x in new if self[x].in_master_block()}
+        esms = [x for x in new if self[x].in_master_block()]
         if esms:
             last = self.cached_lo_last_esm()
             for esm in esms:
                 self.cached_lo_insert_after(last, esm)
                 last = esm
-            new -= esms
+            esms_set = set(esms)
+            new = [x for x in new if x not in esms_set]
         self._lo_wip.extend(new)
         self.cached_lo_save_lo()
 
@@ -2802,7 +2803,7 @@ class ModInfos(FileInfos):
         if fileName.fn_ext == u'.esu': return []
         try:
             espms_extra, esls_extra = load_order.check_active_limit(
-                self._active_wip + [fileName])
+                [*self._active_wip , fileName])
             if espms_extra or esls_extra:
                 msg = f'{fileName}: Trying to activate more than '
                 if espms_extra:
@@ -2812,9 +2813,13 @@ class ModInfos(FileInfos):
                         msg += ' and '
                     msg += f'{load_order.max_esls():d} light plugins'
                 raise PluginsFullError(msg)
-            _children = (_children or tuple()) + (fileName,)
-            if fileName in _children[:-1]:
-                raise BoltError(f'Circular Masters: {" >> ".join(_children)}')
+            if _children:
+                if fileName in _children:
+                    raise BoltError(f'Circular Masters: '
+                                    f'{" >> ".join((*_children, fileName))}')
+                _children.append(fileName)
+            else:
+                _children = [fileName]
             #--Select masters
             if _modSet is None: _modSet = set(self)
             #--Check for bad masternames:

Mopy/bash/bosh/bain.py
@@ -919,7 +919,7 @@ class Installer(ListInfo):
                                             file_relative)
                             continue
                         elif not rootLower and fileExt in plugin_extensions:
-                            #--Remap espms as defined by the user
+                            #--Remap plugins as defined by the user
                             if file_relative in self._remaps:
                                 file_relative = self._remaps[file_relative]
                                 # No need to update fileLower, will skip
@@ -2263,7 +2263,7 @@ class InstallersData(DataStore):
         """Update self.data_sizeCrcDate, using current data_sizeCrcDate as a
         cache.
 
-        Recalculates crcs for all espms in Data/ directory and all other
+        Recalculates crcs for all plugins in Data/ directory and all other
         files whose cached date or size has changed. Will skip directories
         (but not files) specified in Installer global skips and remove empty
         dirs if the setting is on."""
@@ -2279,7 +2279,7 @@ class InstallersData(DataStore):
         oldGet = self.data_sizeCrcDate.get
         siz_apath_mtime = bolt.LowerDict()
         new_sizeCrcDate = bolt.LowerDict()
-        from . import modInfos # to get the crcs for espms
+        from . import modInfos # to get the crcs for plugins
         # these should be already updated (fullRefresh explicitly calls
         # modInfos.refresh and so does RefreshData when tabbing in)
         plugins_scd = bolt.LowerDict()
@@ -2390,7 +2390,7 @@ class InstallersData(DataStore):
         root_dirs_files = [(key, [j for i, j in val]) for key, val in
                            groupby(root_files, key=itemgetter(0))]
         progress = progress or bolt.Progress()
-        from . import modInfos  # to get the crcs for espms
+        from . import modInfos  # to get the crcs for plugins
         progress.setFull(1 + len(root_dirs_files))
         siz_apath_mtime = bolt.LowerDict()
         new_sizeCrcDate = bolt.LowerDict()

Mopy/bash/load_order.py
@@ -288,11 +288,11 @@ def save_lo(lord, acti=None, __index_move=0, quiet=False):
     """Save the Load Order (rewrite loadorder.txt or set modification times).
 
     Will update plugins.txt too if using the textfile method to reorder it
-    as loadorder.txt, and of course rewrite it completely for fallout 4 (
-    asterisk method)."""
+    as loadorder.txt, and of course rewrite it completely for AsteriskGame."""
     acti_list = None if acti is None else list(acti)
+    load_list = None if lord is None else list(lord)
     fix_lo = None if quiet else _games_lo.FixInfo()
-    lord, acti = _game_handle.set_load_order(list(lord), acti_list,
+    lord, acti = _game_handle.set_load_order(load_list, acti_list,
                                              list(cached_lord.loadOrder),
                                              list(cached_lord.activeOrdered),
                                              fix_lo=fix_lo)
@@ -347,8 +347,8 @@ def refresh_lo(cached=False, cached_active=True):
         if cached_lord is not __lo_unset:
             if cached_lord != saved: # sanity check, should not happen
                 bolt.deprint(f'Bug: {cached_lord=} is different from {saved=}')
-        lord, acti = _game_handle.set_load_order( # make sure saved lo is valid
-            list(saved.loadOrder), list(saved.activeOrdered), dry_run=True)
+        lord, acti = _game_handle.set_load_order( # validate saved lo
+            list(saved.loadOrder), list(saved.activeOrdered))
         fixed = LoadOrder(lord, acti)
         if fixed != saved:
             bolt.deprint(f'Saved load order is no longer valid: {saved}\n'
@@ -417,8 +417,7 @@ def _restore_lo(index_move):
     previous = _saved_load_orders[index].lord
     # fix previous
     lord, acti = _game_handle.set_load_order(list(previous.loadOrder),
-                                             list(previous.activeOrdered),
-                                             dry_run=True)
+                                             list(previous.activeOrdered))
     previous = LoadOrder(lord, acti) # possibly fixed
     if previous == cached_lord:
         index_move += int(math.copysign(1, index_move)) # increase or decrease by 1

scripts/update_taglist.py
@@ -53,6 +53,7 @@ GAME_DATA = {
     'Skyrim': 'skyrim',
     'SkyrimSE': 'skyrimse',
     'SkyrimVR': 'skyrimvr',
+    'Starfield': 'starfield',
 }
 
 def setup_parser(parser):