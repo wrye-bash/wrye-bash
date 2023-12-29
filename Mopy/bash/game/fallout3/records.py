wrye-bash/wrye-bash

commit b866822b54497914190faf0c5aad9c9442053809

Convert BipedFlags
- Needed more work on this one, but nixes two attributes in GameInfo
- Also moved the other Flags helper classes to where BipedFlags is

Mopy/bash/game/fallout3/records.py
@@ -153,7 +153,31 @@ class MreActor(AMreActor):
 #------------------------------------------------------------------------------
 class MelBipedData(MelStruct):
     """Handles the common BMDT (Biped Data) subrecord."""
-    _bp_flags = BipedFlags.from_names()
+    class _bp_flags(BipedFlags):
+        head: bool
+        hair: bool
+        upperBody: bool
+        leftHand: bool
+        rightHand: bool
+        weapon: bool
+        pipboy: bool
+        backpack: bool
+        necklace: bool
+        headband: bool
+        hat: bool
+        eyeGlasses: bool
+        noseRing: bool
+        earrings: bool
+        mask: bool
+        choker: bool
+        mouthObject: bool
+        bodyAddon1: bool
+        bodyAddon2: bool
+        bodyAddon3: bool
+
+        ##: Taken from valda's version, investigate
+        _not_playable_flags = {'pipboy'}
+
     class _general_flags(TrimmedFlags):
         hasBackpack: bool = flag(fnv_only(2))
         medium_armor: bool = flag(fnv_only(3))

Mopy/bash/game/fallout4/records.py
@@ -125,7 +125,7 @@ class MelAppr(MelSimpleArray):
 #------------------------------------------------------------------------------
 class MelBod2(MelUInt32Flags):
     """Handles the BOD2 (Biped Body Template) subrecord."""
-    _bp_flags = BipedFlags.from_names()
+    _bp_flags = BipedFlags  # Needs filling in
 
     def __init__(self):
         super().__init__(b'BOD2', 'biped_flags', self._bp_flags)

Mopy/bash/game/oblivion/records.py
@@ -586,12 +586,36 @@ class MreAppa(MelRecord):
     )
 
 #------------------------------------------------------------------------------
+class CommonBipedFlags(BipedFlags):
+    head: bool
+    hair: bool
+    upperBody: bool
+    lowerBody: bool
+    hand: bool
+    foot: bool
+    rightRing: bool
+    leftRing: bool
+    amulet: bool
+    weapon: bool
+    backWeapon: bool
+    sideWeapon: bool
+    quiver: bool
+    shield: bool
+    torch: bool
+    tail: bool
+
+    _not_playable_flags = {'backWeapon', 'quiver', 'weapon', 'torch',
+                           'rightRing', 'sideWeapon'}
+
 class MreArmo(MelRecord):
     """Armor."""
     rec_sig = b'ARMO'
 
-    _flags = BipedFlags.from_names((16, u'hideRings'), (17, u'hideAmulet'),
-                                   (22, u'notPlayable'), (23, u'heavy_armor'))
+    class _flags(CommonBipedFlags):
+        hideRings: bool = flag(16)
+        hideAmulet: bool = flag(17)
+        notPlayable: bool = flag(22)
+        heavy_armor: bool = flag(23)
 
     melSet = MelSet(
         MelEdid(),
@@ -720,8 +744,10 @@ class MreClot(MelRecord):
     """Clothing."""
     rec_sig = b'CLOT'
 
-    _flags = BipedFlags.from_names((16, u'hideRings'), (17, u'hideAmulet'),
-                                   (22, u'notPlayable'))
+    class _flags(CommonBipedFlags):
+        hideRings: bool = flag(16)
+        hideAmulet: bool = flag(17)
+        notPlayable: bool = flag(22)
 
     melSet = MelSet(
         MelEdid(),

Mopy/bash/game/skyrim/records.py
@@ -115,7 +115,40 @@ class _MelBodt(MelTruncatedStruct):
 class MelBodtBod2(MelSequential):
     """Handler for BODT and BOD2 subrecords. Reads both types, but writes only
     BOD2."""
-    _bp_flags = BipedFlags.from_names()
+    class _bp_flags(BipedFlags):
+        head: bool
+        hair: bool
+        body: bool
+        hands: bool
+        forearms: bool
+        amulet: bool
+        ring: bool
+        feet: bool
+        calves: bool
+        shield: bool
+        addon_tail: bool
+        long_hair: bool
+        circlet: bool
+        addon_ears: bool
+        dragon_head: bool
+        dragon_lwing: bool
+        dragon_rwing: bool
+        dragon_body: bool
+        bodyaddon7: bool
+        bodyaddon8: bool
+        decapitate_head: bool
+        decapitate: bool
+        bodyaddon9: bool
+        bodyaddon10: bool
+        bodyaddon11: bool
+        bodyaddon12: bool
+        bodyaddon13: bool
+        bodyaddon14: bool
+        bodyaddon15: bool
+        bodyaddon16: bool
+        bodyaddon17: bool
+        fx01: bool
+
     # Used when loading BODT subrecords - #4 is the only one we care about
     class _legacy_flags(TrimmedFlags):
         modulates_voice: bool = flag(0) # From FO3's ARMA, unused in Skyrim

Mopy/bash/brec/common_records.py
@@ -38,28 +38,10 @@ from .common_subrecords import MelEdid, MelDescription, MelImpactDataset, \
     MelColor, MelDebrData, MelFull, MelIcon, MelBounds, MelColorInterpolator, \
     MelValueInterpolator
 from .record_structs import MelRecord, MelSet
-from .utils_constants import FID, FormId
+from .utils_constants import FID, FormId, NotPlayableFlag
 from .. import bolt, exception
 from ..bolt import decoder, FName, struct_pack, structs_cache, Flags, flag, \
     remove_newlines, to_unix_newlines, sig_to_str, to_win_newlines
-
-# Common record header flags.  Implemented as mixin classes, so the flag names
-# can be added to existing Flags superclasses
-class NotPlayableFlag:
-    not_playable: bool = flag(2)
-
-class VWDFlag:
-    """Mixin class to use with bolt.Flags for the common Visible When Distant
-    flag.
-    """
-    has_distant_lod: bool = flag(15)    # aka Visible when distant
-
-class NavMeshFlags:
-    """Common NavMesh related header flags, always show up together."""
-    # These show up in FO3+
-    navmesh_filter: bool = flag(26)
-    navmesh_bounding_box: bool = flag(27)
-    navmesh_ground: bool = flag(30)
 
 #------------------------------------------------------------------------------
 # Base classes ----------------------------------------------------------------

Mopy/bash/brec/utils_constants.py
@@ -29,7 +29,7 @@ from itertools import chain
 from typing import Callable, Type
 
 from .. import bolt
-from ..bolt import cstrip, decoder, Flags, structs_cache, attrgetter_cache
+from ..bolt import cstrip, decoder, Flags, flag, structs_cache, attrgetter_cache
 from ..exception import StateError, AbstractError
 
 # no local imports, imported everywhere in brec
@@ -301,17 +301,36 @@ class AutoFixedString(FixedString):
     _str_encoding = None
 
 # Common flags ----------------------------------------------------------------
+class NotPlayableFlag:
+    """Mixin to add the not_playable flag to a HeaderFlags class."""
+    not_playable: bool = flag(2)
+
+class VWDFlag:
+    """Mixin to add the Visible When Distant (has_distant_lod) flag to a
+    HeaderFlags class.
+    """
+    has_distant_lod: bool = flag(15)    # aka Visible when distant
+
+class NavMeshFlags:
+    """Mixin to add the NavMesh related flags to a HeaderFlags class."""
+    # These show up in FO3+
+    navmesh_filter: bool = flag(26)
+    navmesh_bounding_box: bool = flag(27)
+    navmesh_ground: bool = flag(30)
+
 ##: xEdit marks these as unknown_is_unused, at least in Skyrim, but it makes no
 # sense because it also marks all 32 of its possible flags as known
 class BipedFlags(Flags):
-    """Biped flags element. Includes biped flag set by default."""
-    __slots__ = ()
+    """Base Biped flags element. Includes logic for checking if armor/clothing
+    can be marked as playable.  Should be subclassed to add the appropriate
+    flags and, if needed, the non-playable flags.
+    """
+    _not_playable_flags: set[str] = set()
 
-    @classmethod
-    def from_names(cls, *names):
-        from .. import bush
-        flag_names = *bush.game.Esp.biped_flag_names, *names
-        return super(BipedFlags, cls).from_names(*flag_names)
+    @property
+    def any_body_flag_set(self) -> bool:
+        check_flags = set(type(self)._names) - type(self)._not_playable_flags
+        return any(getattr(self, flg_name) for flg_name in check_flags)
 
 # Sort Keys -------------------------------------------------------------------
 fid_key = attrgetter_cache[u'fid']

Mopy/bash/game/__init__.py
@@ -429,8 +429,6 @@ class GameInfo(object):
 
     # Plugin format stuff
     class Esp(object):
-        # A tuple containing all biped flag names (in order) for this game
-        biped_flag_names = ()
         # WB can create Bashed Patches
         canBash = False
         # WB can edit basic info in the main header record - generally has

Mopy/bash/game/fallout3/__init__.py
@@ -134,11 +134,6 @@ class Fallout3GameInfo(PatchGame):
         wrye_bash_data_files = {'archiveinvalidationinvalidated!.bsa'}
 
     class Esp(GameInfo.Esp):
-        biped_flag_names = (
-            'head', 'hair', 'upperBody', 'leftHand', 'rightHand', 'weapon',
-            'pipboy', 'backpack', 'necklace', 'headband', 'hat', 'eyeGlasses',
-            'noseRing', 'earrings', 'mask', 'choker', 'mouthObject',
-            'bodyAddOn1', 'bodyAddOn2', 'bodyAddOn3')
         canBash = True
         canEditHeader = True
         generate_temp_child_onam = True
@@ -1031,8 +1026,6 @@ class Fallout3GameInfo(PatchGame):
         'AssortedTweak_GunsUseISAnimation',
         'AssortedTweak_BookWeight',
     }
-    ##: Taken from valda's version, investigate
-    nonplayable_biped_flags = {'pipboy'}
     not_playable_flag = ('generalFlags', 'notPlayable')
     static_attenuation_rec_type = b'SOUN'
 

commit a3f63e28af54ae868fceb09cb8805a902271b981

FO3: Fix Sort: Armor/Clothes regression
Broke in b866822b54497914190faf0c5aad9c9442053809.

Mopy/bash/game/fallout3/records.py:
FO3: Add handling of 4-byte QUST\DATA subrecord

Apparently only the float can be cut off as well, as seen in the
Updated Unofficial Fallout 3 Patch, v3.3.3.

Mopy/bash/game/fallout3/records.py
@@ -2417,9 +2417,9 @@ class MreQust(MelRecord):
         MelScript(),
         MelFull(),
         MelIcon(),
-        MelTruncatedStruct(b'DATA', [u'2B', u'2s', u'f'], (_questFlags, u'questFlags'),
-                           'priority', 'unused2',
-                           'questDelay', old_versions={'2B'}),
+        MelTruncatedStruct(b'DATA', ['2B', '2s', 'f'],
+            (_questFlags, 'questFlags'), 'priority', 'unused2', 'questDelay',
+            old_versions={'2B', '2B2s'}),
         MelConditionsFo3(),
         MelSorted(MelGroups('stages',
             MelSInt16(b'INDX', 'stage'),

Mopy/bash/patcher/patchers/multitweak_names.py
@@ -303,8 +303,8 @@ class NamesTweak_Body_Armor_Fo3(_ANamesTweak_Body_Armor):
             equipment_tag = fancy_tag
         elif body_flags.backpack:
             equipment_tag = backpack_tag
-        elif (body_flags.bodyAddOn1 or body_flags.bodyAddOn2 or
-              body_flags.bodyAddOn3):
+        elif (body_flags.bodyAddon1 or body_flags.bodyAddon2 or
+              body_flags.bodyAddon3):
             equipment_tag = accessory_tag
         elif body_flags.pipboy:
             equipment_tag = pipboy_tag