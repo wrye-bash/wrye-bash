wrye-bash/wrye-bash

commit c4d9957a661b455744a8be95b7baada9bcb8f804

Properly handle circular masters
I.e. a plugin that depends on itself, either directly (foo.esp has
foo.esp as a master) or transitively (foo.esp has bar.esp as a master,
which has foo.esp as a master - or an even longer chain).

Add a warning to the Plugin Checker, don't allow Change To... to create
this situation and don't allow New Plugin... to create this situation
either.

Utumno:

Mopy/bash/patcher/patchers/mergers.py: the comment was from when I was
trying to load all mods *once* - we would need an override for mergers
but laterz.

Mopy/bash/bosh/__init__.py
@@ -30,7 +30,7 @@ import os
 import pickle
 import re
 import sys
-from collections import defaultdict, OrderedDict
+from collections import defaultdict, deque, OrderedDict
 from collections.abc import Iterable
 from functools import wraps
 from itertools import chain
@@ -102,13 +102,14 @@ _CosaveDict = dict[type[cosaves.ACosave], cosaves.ACosave]
 #------------------------------------------------------------------------------
 # File System -----------------------------------------------------------------
 #------------------------------------------------------------------------------
-class MasterInfo(object):
+class MasterInfo:
     """Slight abstraction over ModInfo that allows us to represent masters that
     are missing an active mod counterpart."""
-    __slots__ = (u'is_ghost', u'curr_name', u'mod_info', u'old_name',
-                 'stored_size', '_was_esl')
+    __slots__ = ('is_ghost', 'curr_name', 'mod_info', 'old_name',
+                 'stored_size', '_was_esl', 'parent_mod_info')
 
-    def __init__(self, master_name, master_size, was_esl):
+    def __init__(self, *, parent_minf, master_name, master_size, was_esl):
+        self.parent_mod_info = parent_minf
         self.stored_size = master_size
         self._was_esl = was_esl
         self.old_name = FName(master_name)
@@ -184,8 +185,8 @@ class FileInfo(AFile, ListInfo):
         g_path = GPath(fullpath)
         ListInfo.__init__(self, g_path.stail) # ghost must be lopped off
         self.header = None
-        self.masterNames = tuple()
-        self.masterOrder = tuple()
+        self.masterNames: tuple[FName, ...] = ()
+        self.masterOrder: tuple[FName, ...] = ()
         self.madeBackup = False
         # True if the masters for this file are not reliable
         self.has_inaccurate_masters = False
@@ -553,6 +554,40 @@ class ModInfo(FileInfo):
     def get_dependents(self):
         """Return a set of all plugins that have this plugin as a master."""
         return modInfos.dependents[self.fn_key]
+
+    def recurse_masters(self, *, fake_masters: list[FName] | None = None) \
+            -> set[FName]:
+        """Recursively collect all masters of this plugin, including transitive
+        ones.
+
+        :param fake_masters: If not None, use this instead of self.masterNames
+            for determining which masters to recurse into."""
+        plugins_to_check = deque([self])
+        checked_plugins = set()
+        ret_masters = set()
+        while plugins_to_check:
+            src_plugin = plugins_to_check.popleft()
+            checked_plugins.add(src_plugin.fn_key)
+            src_masters = (fake_masters
+                           if fake_masters is not None and src_plugin is self
+                           else src_plugin.masterNames)
+            for src_master in src_masters:
+                ret_masters.add(src_master)
+                # Check to make sure we're not going to enter an infinite loop
+                # if we hit a circular master situation
+                if (src_master not in checked_plugins and
+                        (src_master_info := modInfos.get(src_master))):
+                    plugins_to_check.append(src_master_info)
+        return ret_masters
+
+    def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
+        """Check if this plugin has circular masters, i.e. if it depends on
+        itself (either directly or transitively).
+
+        :param fake_masters: If not None, use this instead of self.masterNames
+            for determining which masters to recurse into. Useful for checking
+            if altering a master list would cause it to become circular."""
+        return self.fn_key in self.recurse_masters(fake_masters=fake_masters)
 
     # Ghosting and ghosting related overrides ---------------------------------
     def _refresh_ghost_state(self, regular_path=None, *, itsa_ghost=None):
@@ -3369,15 +3404,6 @@ class ModInfos(FileInfos):
         for p, p_info in self.items():
             for p_master in p_info.masterNames:
                 cached_dependents[p_master].add(p)
-
-    def recurse_masters(self, fn_mod):
-        """Recursively collect all masters of fn_mod."""
-        ret_masters = set()
-        src_masters = self[fn_mod].masterNames if fn_mod in self else []
-        for src_master in src_masters:
-            ret_masters.add(src_master)
-            ret_masters.update(self.recurse_masters(src_master))
-        return ret_masters
 
 #------------------------------------------------------------------------------
 class SaveInfos(FileInfos):

Mopy/bash/basher/__init__.py
@@ -468,7 +468,8 @@ class MasterList(_ModsUIList):
         for mi, ma_name in enumerate(fileInfo.masterNames):
             ma_size = all_master_sizes[mi] if can_have_sizes else 0
             ma_esl = can_have_esl and ma_name in all_esl_masters
-            self.data_store[mi] = bosh.MasterInfo(ma_name, ma_size, ma_esl)
+            self.data_store[mi] = bosh.MasterInfo(parent_minf=fileInfo,
+                master_name=ma_name, master_size=ma_size, was_esl=ma_esl)
         self._reList()
         self.populate_items()
 

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -12400,6 +12400,11 @@ iScreenShotIndex=0</code></td>
             <td>All games</td>
         </tr>
         <tr>
+            <td>Plugins with circular masters, i.e. plugins that depend on
+                themselves</td>
+            <td>All games</td>
+        </tr>
+        <tr>
             <td>Plugins with unknown header versions</td>
             <td>All games</td>
         </tr>

Mopy/bash/basher/dialogs.py
@@ -491,6 +491,16 @@ class CreateNewPlugin(DialogWindow):
             with_esl_flag=self._esl_flag.is_checked,
             with_overlay_flag=self._overlay_flag.is_checked,
             wanted_masters=[*map(FName, self._chosen_masters)])
+        # Check if we made a plugin with circular masters - we need the ModInfo
+        # object itself to check this. A bit ugly from a UX perspective, but OK
+        # because it's a  rare scenario anyways
+        created_plugin = pw.data_store[chosen_name]
+        if created_plugin.has_circular_masters():
+            showError(self, _('Creating a plugin named %(chosen_plugin_name)s '
+                              'with the chosen masters will cause it to '
+                              'have circular masters, i.e. depend on '
+                              'itself.') % {'chosen_plugin_name': chosen_name})
+            return EventResult.FINISH # leave the dialog open
         if windowSelected:  # assign it the group of the first selected mod
             mod_group = pw.data_store.table.getColumn(u'group')
             mod_group[chosen_name] = mod_group.get(windowSelected[0], u'')

Mopy/bash/basher/misc_links.py
@@ -201,10 +201,24 @@ class Master_ChangeTo(_Master_EditList):
             return
         curr_master_names = {m.curr_name for m in
                              self.window.data_store.values()}
-        if new_fname in curr_master_names:
-            self._showError(_('This plugin already has %(master_name)s as a '
-                              'master.') % {'master_name': new_fname})
-            return
+        parent_mi = masterInfo.parent_mod_info
+        # If the user is trying to fix a plugin with circular masters, allow
+        # any kind of reassignment - otherwise, run some sanity checks
+        if not parent_mi.has_circular_masters():
+            # Don't allow duplicate masters
+            if new_fname in curr_master_names:
+                self._showError(_('This plugin already has %(master_name)s as '
+                                  'a master.') % {'master_name': new_fname})
+                return
+            # Don't allow adding a master that makes the masters circular
+            altered_masters = [new_fname if m == master_name else m
+                               for m in parent_mi.masterNames]
+            if parent_mi.has_circular_masters(fake_masters=altered_masters):
+                self._showError(_('Having %(problem_master)s as a master '
+                                  'would cause this plugin to have circular '
+                                  'masters, i.e. depend on itself.') % {
+                    'problem_master': new_fname})
+                return
         #--Save Name
         if masterInfo.rename_if_present(new_fname):
             ##: should be True but needs extra validation -> cycles?

Mopy/bash/bosh/mods_metadata.py
@@ -183,7 +183,11 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     cannot_scan_overrides = set()
     p_missing_masters = set()
     p_delinquent_masters = set()
+    p_circular_masters = set()
     for p_fn_key, p in all_present_minfs.items():
+        if p.has_circular_masters():
+            # The plugin depends on itself (possibly transitively) -> report
+            p_circular_masters.add(p_fn_key)
         if p_fn_key in all_active_plugins:
             for p_master in p.masterNames:
                 if p_master not in all_present_plugins:
@@ -649,18 +653,36 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         log_plugins(should_activate)
     if p_missing_masters:
         log.setHeader('=== ' + _('Missing Masters'))
-        log(_(u'The following plugins have missing masters and are active. '
-              u'This will cause a CTD at the main menu and must be '
-              u'corrected.'))
+        log(_('The following plugins have missing masters and are active. '
+              'This will cause a CTD at the main menu and must be corrected '
+              'by installing the missing plugins or removing the plugins with '
+              'missing masters.'))
         log_plugins(p_missing_masters)
     if p_delinquent_masters:
         log.setHeader('=== ' + _('Delinquent Masters'))
-        log(_(u'The following plugins have delinquent masters, i.e. masters '
-              u'that are set to load after their dependent plugins. The game '
-              u'will try to force them to load before the dependent plugins, '
-              u'which can lead to unpredictable or undefined behavior and '
-              u'must be corrected.'))
+        log(_('The following plugins have delinquent masters, i.e. masters '
+              'that are set to load after their dependent plugins. The game '
+              'will try to force them to load before the dependent plugins, '
+              'which can lead to unpredictable or undefined behavior and '
+              'must be corrected. You should correct the load order.'))
         log_plugins(p_delinquent_masters)
+    if p_circular_masters:
+        log.setHeader('=== ' + _('Circular Masters'))
+        log(_("The following plugins have circular masters, i.e. they depend "
+              "on themselves. This can happen either directly (%(example_a)s "
+              "has %(example_a)s as a master) or transitively (%(example_a)s "
+              "has %(example_b)s as a master, which, in turn, has "
+              "%(example_a)s as a master - such a chain may be even longer). "
+              "Resolving this is impossible for the game, which will most "
+              "likely crash when trying to load these plugins. You can try to "
+              "investigate by using the 'Change To...' command to reassign "
+              "the circular master so that the plugin can be opened in "
+              "%(xedit_name)s.") % {
+            'example_a': 'foo.esp',
+            'example_b': 'bar.esp',
+            'xedit_name': bush.game.Xe.full_name,
+        })
+        log_plugins(p_circular_masters)
     if invalid_tes4_versions:
         ver_list = u', '.join(
             sorted(str(v) for v in bush.game.Esp.validHeaderVersions))

Mopy/bash/patcher/patchers/mergers.py
@@ -83,10 +83,9 @@ class _AMerger(ImportPatcher):
         """We need to scan the masters recursively - add to p_file read
         factories."""
         sup = super()._process_sources(p_sources, p_file)
-        self._masters_and_srcs = {*chain.from_iterable( # merger_masters
-            p_file.p_file_minfos.recurse_masters(srcMod) for srcMod in
+        self._masters_and_srcs = {*chain.from_iterable(
+            p_file.p_file_minfos[srcMod].recurse_masters() for srcMod in
             self.srcs), *self.srcs}
-        # self._update_patcher_factories(p_file, self._masters_and_srcs)
         return sup
 
     def initData(self,progress):

commit dd6e33c631e9b41eddc7973b7680780d0e00fa86

Fix Change To... on save masters
Saves can't have circular masters, but I don't see a reason to special
case this when we can just override the method for saves and return
False. Broke in c4d9957a661b455744a8be95b7baada9bcb8f804.

Mopy/bash/bosh/__init__.py
@@ -269,6 +269,16 @@ class FileInfo(AFile, ListInfo):
         :return: A list of the masters of this file, as paths."""
         raise NotImplementedError
 
+    def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
+        """Check if this file has circular masters, i.e. if it depends on
+        itself (either directly or transitively). If it doesn't have masters,
+        raise a NotImplementedError.
+
+        :param fake_masters: If not None, use this instead of self.masterNames
+            for determining which masters to recurse into. Useful for checking
+            if altering a master list would cause it to become circular."""
+        raise NotImplementedError
+
     # Backup stuff - beta, see #292 -------------------------------------------
     def get_hide_dir(self):
         return self.get_store().hidden_dir
@@ -576,6 +586,9 @@ class ModInfo(FileInfo):
         """Return the plugin masters, in the order listed in its header."""
         return self.header.masters
 
+    def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
+        return self.fn_key in self.recurse_masters(fake_masters=fake_masters)
+
     def get_dependents(self):
         """Return a set of all plugins that have this plugin as a master."""
         return modInfos.dependents[self.fn_key]
@@ -604,15 +617,6 @@ class ModInfo(FileInfo):
                         (src_master_info := modInfos.get(src_master))):
                     plugins_to_check.append(src_master_info)
         return ret_masters
-
-    def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
-        """Check if this plugin has circular masters, i.e. if it depends on
-        itself (either directly or transitively).
-
-        :param fake_masters: If not None, use this instead of self.masterNames
-            for determining which masters to recurse into. Useful for checking
-            if altering a master list would cause it to become circular."""
-        return self.fn_key in self.recurse_masters(fake_masters=fake_masters)
 
     # Ghosting and ghosting related overrides ---------------------------------
     def _refresh_ghost_state(self, regular_path=None, *, itsa_ghost=None):
@@ -1473,6 +1477,9 @@ class SaveInfo(FileInfo):
         # Fall back on the regular masters - either the cosave is unnecessary,
         # doesn't exist or isn't accurate
         return self.header.masters
+
+    def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
+        return False # Saves can't have circular masters
 
     def _reset_masters(self):
         super(SaveInfo, self)._reset_masters()