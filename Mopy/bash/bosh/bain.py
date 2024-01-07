wrye-bash/wrye-bash

commit 70fe06101ad20086c73dfcbdb7025ce5cb557951

Rebuke the lord of undead (functions) getGhosted (and fixups):
Using the power of Store artifact (and the observation that if the file
exists then the store knows all about it including the path - note that
limbo ghosts, that have both normal and ghost versions remain an issue.
I changed return type of data_path_to_key -> info to use that info).
A break was missing in:

-                for store, removed_files in removed_tracked:
-                    if fname_key := store.data_path_to_key(path):
-                        removed_files.add(fname_key)
                         break # <----- here
-                else:
                     removed_untracked.add(path)
                     empty_dir_candidates.add(path.head)

removed_untracked.add(path) would always run (and the data store dirs
would be added to empty_dir_candidates) which looks like a bug. Note
also:

@@ -2730,3 +2730,3 @@ def _removeFiles(self, ci_removes, refresh_ui, progress=None):
             for store, removed_files in removed_tracked:
-                refresh_ui[store.unique_store_key] = True
+                refresh_ui[store.unique_store_key] = bool(removed_files)
                 store.delete(removed_files, recycle=False)

which will hopefully soon change to use a RefrData instance :)

__installer_install refactor mtime checks

We can't avoid them really but we can use the created info that is in
scope - TTT very subtle interplay between order of operations and order
of stores.

Correct data_path_to_info to not return True for data paths like
'subdir/some.[esp|bsa]' (if we get those). Based on the observation
that self.get() won't return if data_path is not a basename - note for
screens override that:

*a, b = [1] -> a=[], b=1

Adds a couple lower() but keeps the basename call for 'would_be' path.

Typing here needs more work but this is natural as it's the boundary
between FName and CIstr clients - time to standardize (including
ci_* prefixing) but first refresh.

Mopy/bash/bosh/bain.py
@@ -38,7 +38,7 @@ from operator import attrgetter, itemgetter
 from zlib import crc32
 
 from . import DataStore, InstallerConverter, ModInfos, bain_image_exts, \
-    best_ini_files
+    best_ini_files, data_tracking_stores
 from .. import archives, bass, bolt, bush, env
 from ..archives import compress7z, defaultExt, extract7z, list_archive, \
     readExts
@@ -1281,26 +1281,31 @@ class _InstallerPackage(Installer, AFile):
                     unpackDir):
         """Filesystem install, if unpackDir is not None we are installing
          an archive."""
-        norm_ghostGet = Installer.getGhosted().get
         data_sizeCrcDate_update = bolt.LowerDict()
         data_sizeCrc = self.ci_dest_sizeCrc
-        from . import data_tracking_stores
-        affected_stores = [(s, set()) for s in data_tracking_stores()]
+        stores = data_tracking_stores()
+        store_to_paths = defaultdict(set)
         sources_dests = defaultdict(set)
         join_data_dir = bass.dirs[u'mods'].join
         for dest, src in dest_src.items():
             dest_size, crc = data_sizeCrc[dest]
             # Work with ghosts lopped off internally and check the destination,
             # since plugins may have been renamed
-            for store, file_tracker in affected_stores:
-                ##: Is the str() needed here? need to type some tough APIs like refreshDataSizeCrc
-                if fname_key := store.data_path_to_key(str(dest)):
-                    file_tracker.add(fname_key)
-            data_sizeCrcDate_update[dest] = (dest_size, crc, -1) ##: HACK we must try avoid stat'ing the mtime
+            for store in stores:
+                if fname_key := store.data_path_to_info(dest, would_be=True):
+                    try: # FName
+                        dest_path = join_data_dir(dest)
+                    except TypeError: # info is present, possibly ghosted
+                        dest_path = fname_key.abs_path
+                        fname_key = fname_key.fn_key
+                    store_to_paths[store].add((fname_key, dest))
+                    break
+            else:
+                dest_path = join_data_dir(dest)
+            data_sizeCrcDate_update[dest] = [dest_size, crc, -1]
             # Append the ghost extension JIT since the FS operation below will
             # need the exact path to copy to
-            sources_dests[srcDirJoin(src)].add(join_data_dir(
-                norm_ghostGet(dest, dest)))
+            sources_dests[srcDirJoin(src)].add(dest_path)
             subprogressPlus()
         #--Now Move
         try:
@@ -1311,8 +1316,26 @@ class _InstallerPackage(Installer, AFile):
             #--Clean up unpack dir if we're an archive
             if unpackDir:
                 cleanup_temp_dir(unpackDir)
+        # Update relevant data stores, adding new/modified files
+        refresh_ui = defaultdict(bool)
+        for store, owned_files in store_to_paths.items():
+            mtimes = []
+            for (owned_file, dest) in owned_files:
+                try:
+                    inf = store.new_info(owned_file, owner=self.fn_key,
+                        _in_refresh=True) # we don't want to refresh info sets
+                    data_sizeCrcDate_update[dest][2] = inf.mtime
+                    mtimes.append(owned_file)
+                except FileError as error: # repeated from refresh
+                    store.corrupted[owned_file] = error.message
+                    store.pop(owned_file, None)
+            if mtimes:
+                if store.unique_store_key is Store.MODS:
+                    store.cached_lo_append_if_missing(mtimes)
+                    store.refreshLoadOrder(unlock_lo=True)
+                refresh_ui[store.unique_store_key] = True
         #--Update Installers data
-        return data_sizeCrcDate_update, affected_stores
+        return data_sizeCrcDate_update, refresh_ui
 
     def listSource(self):
         """Return package structure as text."""
@@ -1950,7 +1973,6 @@ class InstallersData(DataStore):
         old_key = super().rename_operation(member_info, name_new)
         member_info.abs_path = self.store_dir.join(name_new)
         # Update the ownership information for relevant data stores
-        from . import data_tracking_stores
         owned_per_store = []
         for store in data_tracking_stores():
             storet = store.table
@@ -2597,33 +2619,16 @@ class InstallersData(DataStore):
             iniInfos.new_info(tweakPath.stail, notify_bain=True)
         tweaksCreated -= removed
 
-    def __installer_install(self, installer, destFiles, index, progress,
-                            refresh_ui):
+    def _installer_install(self, installer, destFiles, index, progress):
+        """Wrap installer.install to update data_sizeCrcDate."""
         sub_progress = SubProgress(progress, index, index + 1)
-        data_sizeCrcDate_update, affected_stores = installer.install(
+        data_sizeCrcDate_update, refresh_ui_ = installer.install(
             destFiles, sub_progress)
-        # Update relevant data stores, adding new/modified files
-        from . import modInfos
-        mods = []
-        for store, owned_files in affected_stores:
-            for owned_file in owned_files.copy(): # size may change
-                try:
-                    store.new_info(owned_file, owner=installer.fn_key)
-                except FileError:
-                    owned_files.discard(owned_file)
-            refresh_ui[store.unique_store_key] |= bool(owned_files)
-            if store is modInfos:
-                mods = owned_files
-        modInfos.cached_lo_append_if_missing(mods)
-        modInfos.refreshLoadOrder(unlock_lo=True)
-        # now that we saved load order update missing mtimes for mods:
-        for mod in mods:
-            s, c, _d = data_sizeCrcDate_update[mod]
-            data_sizeCrcDate_update[mod] = (s, c, modInfos[mod].mtime)
-        # and for rest of the files - we do mods separately for ghosts
-        self.data_sizeCrcDate.update((dest, (
-            s, c, (d != -1 and d) or bass.dirs[u'mods'].join(dest).mtime)) for
-            dest, (s, c, d) in data_sizeCrcDate_update.items())
+        # update mtime for the rest of the files
+        for dest, (s, c, d) in data_sizeCrcDate_update.items():
+            self.data_sizeCrcDate[dest] = (
+                s, c, bass.dirs['mods'].join(dest).mtime if d == -1 else d)
+        return refresh_ui_
 
     def bain_install(self, packages, refresh_ui, progress=None, last=False,
                      override=True):
@@ -2649,9 +2654,9 @@ class InstallersData(DataStore):
                         destFiles &= inst.missingFiles
                     if destFiles:
                         self._createTweaks(destFiles, inst, tweaksCreated)
-                        self.__installer_install(inst, destFiles, index,
-                                                 progress, refresh_ui)
-                    index += 1 # increment after it's used in __installer_install
+                        refresh_ui.update(self._installer_install(
+                            inst, destFiles, index, progress))
+                    index += 1 # increment after it's used in _installer_install
                     inst.is_active = True
                     if inst.order == min_order:
                         break  # we are done
@@ -2696,20 +2701,22 @@ class InstallersData(DataStore):
         if not ci_removes: return
         mods_dir_join = bass.dirs[u'mods'].join
         empty_dir_candidates = set()
-        from . import data_tracking_stores
         removed_tracked = [(s, set()) for s in data_tracking_stores()]
         removed_untracked = set()
+        remove_paths = {}
         #--Construct list of files to delete
-        norm_ghost_get = Installer.getGhosted().get
         for ci_rel_path in ci_removes:
-            path = mods_dir_join(norm_ghost_get(ci_rel_path, ci_rel_path))
-            if path.exists():
-                for store, removed_files in removed_tracked:
-                    if fname_key := store.data_path_to_key(path):
-                        removed_files.add(fname_key)
-                else:
+            for store, removed_files in removed_tracked:
+                if store_info := store.data_path_to_info(ci_rel_path):
+                    removed_files.add(store_info.fn_key)
+                    path = store_info.abs_path # it exists as the info exists
+                    break
+            else:
+                path = mods_dir_join(ci_rel_path)
+                if path.exists():
                     removed_untracked.add(path)
                     empty_dir_candidates.add(path.head)
+            remove_paths[ci_rel_path] = path
         #--Now determine which directories will be empty, replacing subsets of
         # removedFiles by their parent dir if the latter will be emptied
         removed_untracked = self._determineEmptyDirs(
@@ -2723,7 +2730,7 @@ class InstallersData(DataStore):
             # Delegate deletion of files that data stores care about to those
             # data stores
             for store, removed_files in removed_tracked:
-                refresh_ui[store.unique_store_key] = True
+                refresh_ui[store.unique_store_key] = bool(removed_files)
                 store.delete(removed_files, recycle=False)
         except (CancelError, SkipError): ex = sys.exc_info()
         except:
@@ -2731,10 +2738,8 @@ class InstallersData(DataStore):
             raise
         finally:
             if ex:
-                ci_removes = [
-                    f for f in ci_removes
-                    if not mods_dir_join(norm_ghost_get(f, f)).exists()
-                ]
+                ci_removes = [k for k, v in remove_paths.items() if
+                              not v.exists()]
             #--Update InstallersData
             data_sizeCrcDatePop = self.data_sizeCrcDate.pop
             for ci_relPath in ci_removes:
@@ -2813,16 +2818,14 @@ class InstallersData(DataStore):
             if anneal:
                 self._restoreFiles(restores, refresh_ui, progress)
             # Set the 'installer' column for files that track their owner
-            from . import data_tracking_stores
+            stores = data_tracking_stores()
             for ikey, owned_files in cede_ownership.items():
                 for owned_path in owned_files:
-                    for store in data_tracking_stores():
-                        if fname_key := store.data_path_to_key(owned_path):
-                            store[fname_key].set_table_prop(
-                                'installer', '%s' % ikey)
+                    for store in stores:
+                        if store_info := store.data_path_to_info(owned_path):
+                            store_info.set_table_prop('installer', f'{ikey}')
                             refresh_ui[store.unique_store_key] = True
-                            # Each file can only belong to one data store (at
-                            # least at the moment), so exit early
+                            # Each file may only belong to one data store
                             break
         finally:
             self.refresh_ns()
@@ -2840,8 +2843,8 @@ class InstallersData(DataStore):
             progress(index, fn_inst)
             if destFiles:
                 installer = self[fn_inst]
-                self.__installer_install(installer, destFiles, index, progress,
-                                         refresh_ui)
+                refresh_ui.update(self._installer_install(
+                    installer, destFiles, index, progress))
 
     def bain_anneal(self, anPackages, refresh_ui, progress=None):
         """Anneal selected packages. If no packages are selected, anneal all.
@@ -2849,7 +2852,8 @@ class InstallersData(DataStore):
         * Correct underrides in anPackages.
         * Install missing files from active anPackages."""
         progress = progress if progress else bolt.Progress()
-        anPackages = (self[package] for package in (anPackages or self))
+        anPackages = (self[package] for package in
+                      (anPackages or self.filterInstallables(self)))
         #--Get remove/refresh files from anPackages
         removes = set()
         for installer in anPackages:
@@ -3167,16 +3171,20 @@ class InstallersData(DataStore):
         """Remove markers from installerKeys."""
         return (x for x in installerKeys if not self[x].is_marker)
 
-    def createFromData(self, projectPath, ci_files, progress):
+    def createFromData(self, projectPath, ci_files: list[CIstr], progress,
+                       mod_infos):
         if not ci_files: return
-        norm_ghost_get = Installer.getGhosted().get
         subprogress = SubProgress(progress, 0, 0.8, full=len(ci_files))
         srcJoin = bass.dirs[u'mods'].join
         dstJoin = self.store_dir.join(projectPath).join
-        for i,filename in enumerate(ci_files):
-            subprogress(i, filename)
-            srcJoin(norm_ghost_get(filename, filename)).copyTo(
-                dstJoin(filename))
+        for i, ci_rel_path in enumerate(ci_files):
+            subprogress(i, ci_rel_path)
+            try:
+                srcJoin(ci_rel_path).copyTo(dstJoin(ci_rel_path))
+            except FileNotFoundError: # modInfos MUST BE UPDATED
+                if minf := mod_infos.get(str(ci_rel_path)): # try the ghost
+                    minf.abs_path.copyTo(dstJoin(ci_rel_path))
+                else: raise
         # Refresh, so we can manipulate the InstallerProject item
         self.new_info(projectPath, progress,
                       install_order=len(self)) # install last

Mopy/bash/basher/installers_links.py
@@ -140,8 +140,8 @@ class Installers_MonitorExternalInstallation(Installers_Link):
             return
         pr_path = bosh.InstallerProject.unique_name(projectName)
         # Copy Files
-        with balt.Progress(_('Creating Project...')) as prog:
-            self.idata.createFromData(pr_path, include, prog) # will order last
+        with balt.Progress(_('Creating Project...')) as prog: # will order last
+            self.idata.createFromData(pr_path, include, prog, bosh.modInfos)
         # createFromData placed the new project last in install order - install
         try:
             self.idata.bain_install([pr_path], ui_refresh, override=False)

Mopy/bash/bosh/__init__.py
@@ -61,6 +61,7 @@ from ..wbtemp import TempFile
 
 # Singletons, Constants -------------------------------------------------------
 empty_path = GPath(u'') # evaluates to False in boolean expressions
+_ListInf = AFile | ListInfo | None| FName
 
 #--Singletons
 gameInis: tuple[GameIni | IniFile] | None = None
@@ -74,8 +75,8 @@ screen_infos: ScreenInfos | None = None
 def data_tracking_stores() -> Iterable[FileInfos]:
     """Return an iterable containing all data stores that keep track of the
     Data folder and which files in it are owned by which BAIN package."""
-    return (s for s in (modInfos, iniInfos, bsaInfos, screen_infos) if
-            s is not None)
+    return tuple(s for s in (modInfos, iniInfos, bsaInfos, screen_infos) if
+                 s is not None)
 
 #--Header tags
 # re does not support \p{L} - [^\W\d_] is almost equivalent (N vs Nd)
@@ -573,7 +574,8 @@ class ModInfo(FileInfo):
     @FileInfo.abs_path.getter
     def abs_path(self):
         """Return joined dir and name, adding .ghost if the file is ghosted."""
-        return (self._file_key + u'.ghost') if self.isGhost else self._file_key
+        return (self._file_key + '.ghost' # Path.__add__
+                ) if self.isGhost else self._file_key
 
     def setGhost(self, isGhost):
         """Sets file to/from ghost mode. Returns ghost status at end."""
@@ -1591,13 +1593,15 @@ class TableFileInfos(DataStore):
         :rtype: _sre.SRE_Match | None"""
         return cls.file_pattern.search(fileName)
 
-    def data_path_to_key(self, data_path: os.PathLike | str) -> FName | None:
-        """Return an FName representing the key of the specified Data
-        folder-relative file path inside this data store iff it belongs to this
-        data store. If it does not, return None."""
-        # FName needed for ScreenInfos.rightFileType override
-        ret_candidate = FName(os.path.basename(data_path))
-        return ret_candidate if self.rightFileType(ret_candidate) else None
+    def data_path_to_info(self, data_path: str, would_be=False) -> _ListInf:
+        """Return the info corresponding to the specified (str, Fname or CIStr)
+        path relative to the  Data folder - iff it belongs to this data store.
+        If it does not, return None, except if would_be is True whereupon
+        return the fname, if it is a valid one for self."""
+        if (inf := self.get(fnkey := FName(str(data_path)))) or not would_be:
+            return inf
+        return fnkey if os.path.basename(data_path) == data_path and \
+            self.rightFileType(fnkey) else None
 
     #--Delete
     def files_to_delete(self, fileNames, **kwargs):
@@ -1871,15 +1875,13 @@ class INIInfos(TableFileInfos):
         self.ini = list(bass.settings[u'bash.ini.choices'].values())[
             bass.settings[u'bash.ini.choice']]
 
-    def data_path_to_key(self, data_path: os.PathLike | str) -> FName | None:
-        ci_parts = os.path.split(os.fspath(data_path).lower())
+    def data_path_to_info(self, data_path: str, would_be=False) -> _ListInf:
+        parts = os.path.split(os.fspath(data_path))
         # 1. Must have a single parent folder
         # 2. That folder must be named 'ini tweaks' (case-insensitively)
-        # 3. The extension must be a valid INI-like extension
-        if (len(ci_parts) == 2 and
-                ci_parts[0] == 'ini tweaks' and
-                ci_parts[1].rsplit('.', 1)[1] in supported_ini_exts):
-            return super().data_path_to_key(data_path)
+        # 3. The extension must be a valid INI-like extension - super checks it
+        if len(parts) == 2 and parts[0].lower() == 'ini tweaks':
+            return super().data_path_to_info(parts[1], would_be)
         return None
 
     @property
@@ -3444,7 +3446,7 @@ class SaveInfos(FileInfos):
     def rightFileType(cls, fileName: bolt.FName | str):
         return all(cls._parse_save_path(fileName))
 
-    def data_path_to_key(self, data_path: os.PathLike | str) -> FName | None:
+    def data_path_to_info(self, data_path: str, would_be=False) -> _ListInf:
         return None # Never relative to Data folder
 
     @classmethod
@@ -3690,19 +3692,17 @@ class ScreenInfos(FileInfos):
             return False
         return super().rightFileType(fileName)
 
-    def data_path_to_key(self, data_path: os.PathLike | str) -> FName | None:
+    def data_path_to_info(self, data_path: str, would_be=False) -> _ListInf:
         if not self._rel_to_data:
             # Current store_dir is not relative to Data folder, so we do not
             # need to pay attention to BAIN
             return None
-        ci_parts = os.path.split(os.fspath(data_path).lower())
-        prefix_len = len(self._ci_curr_data_prefix)
-        # The parent directories must match and the length must be +1 (for the
-        # filename itself)
-        if (len(ci_parts) != prefix_len + 1 or
-                ci_parts[:prefix_len] != self._ci_curr_data_prefix):
+        *parts, filename = os.path.split(os.fspath(data_path))
+        # The parent directories must match
+        if (len(parts) != len(self._ci_curr_data_prefix) or
+                [*map(str.lower, parts)] != self._ci_curr_data_prefix):
             return None
-        return super().data_path_to_key(data_path)
+        return super().data_path_to_info(filename, would_be)
 
     def refresh(self, refresh_infos=True, booting=False):
         self._set_store_dir()

Mopy/bash/wrye_text.py
@@ -161,6 +161,10 @@ def genHtml(ins, out=None, *css_dirs):
                 anchor += str(count)
             else:
                 anchor = anchor[:-1] + str(count)
+        # Clean out double underscores from the final anchor,
+        # otherwise we'd turn those into <b> tags down below
+        while '__' in anchor:
+            anchor = anchor.replace('__', '_')
         anchorlist.append(anchor)
         return f"<a id='{anchor}'>{text}</a>"
     #--Bold, Italic, BoldItalic

commit 8c2b95845b670fa954305e612cffe13136db8cb5

Fix INI Tweaks getting installed to Data folder
We have to join with the store's store_path here. This happens to work
for mods and BSAs because there, store_path == bass.dirs['mods'], but
for INI Tweaks, store_path == bass.dirs['mods'].join('INI Tweaks').
Broke in 70fe06101ad20086c73dfcbdb7025ce5cb557951.

Mopy/bash/bosh/bain.py
@@ -1294,7 +1294,7 @@ class _InstallerPackage(Installer, AFile):
             for store in stores:
                 if fname_key := store.data_path_to_info(dest, would_be=True):
                     try: # FName
-                        dest_path = join_data_dir(fname_key)
+                        dest_path = store.store_dir.join(fname_key)
                     except TypeError: # info is present, possibly ghosted
                         dest_path = fname_key.abs_path
                         fname_key = fname_key.fn_key