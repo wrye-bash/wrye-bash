wrye-bash/wrye-bash

commit b1d06b5bbc7538076014f45aa2780ba1706dcbd9

[!]Nit in belt - see todos:
_plugin_in_active_package would return True only if self.sublist[i] was
True - refactored and addressed the ancient fixme - use an FNDict
internally and translate at the boundary - we should make subNames to an
FNDict also (addressing the '' key in the process)

Inline _plugin_in_active_package:

I think I safely FName'd espmMap/_remaps, fixing related todos (belt use
of bain attributes is hard to follow, even type-wise, hopefully easier
now) - remains subNames/Active dict

Note I unconditionally access installer.espmMap[fn_sub] - should contain
all the subs in sublist (see refreshDataSizeCrc where those are set) but
still [!] - anyway belt does some checks sometimes for this and all
these checks must be centralized, WIP.

Note also in __setstate I dropped the checks as FName is (currently)
__reduce__'d to a path on pickling so no need to check. We should
reduce to a str come 313.

espmMap was created as an FNDict but in refreshDataSizeCrc we used
LowerDict since 5eecd360f407452cd0cba914766f87dc78933861 - a #543
regression leftover - yep this code did need some typing and that's
why it's hard to type it [!]

_extensions_to_process seems useless and Installer namespace could
use some cleanup. Note in particular the todos in refreshDataSizeCrc,
maybe not adding to espms is a bug [!]?

Mopy/bash/bosh/cosaves.py: I was hunting down plugin_renames

Under #219, yep

Mopy/bash/basher/belt.py
@@ -64,8 +64,8 @@ class WizInstallInfo(object):
     def __init__(self):
         self.canceled = False
         self.select_plugins = []
-        self.rename_plugins = {}
-        self.select_sub_packages = []
+        self.rename_plugins: FNDict[FName, FName] = FNDict()
+        self.select_sub_packages = {}
         self.ini_edits = bolt.LowerDict()
         self.should_install = False
 
@@ -332,13 +332,14 @@ class PageFinish(PageInstaller):
     """Page displayed at the end of a wizard, showing which sub-packages and
     which plugins will be selected. Also displays some notes for the user."""
 
-    def __init__(self, parent, sublist, plugin_enabled, plugin_renames, bAuto,
+    def __init__(self, parent, sublist, plugin_enabled, plugin_renames_, bAuto,
                  notes, iniedits):
         PageInstaller.__init__(self, parent)
         subs = sorted(sublist)
         #--make the list that will be displayed
-        displayed_plugins = [f'{x} -> {plugin_renames[x]}'
-            if x in plugin_renames else x for x in plugin_enabled]
+        displayed_plugins = [f'{x} -> {plugin_renames_[x]}'
+            if x in plugin_renames_ else x for x in plugin_enabled]
+        self._wiz_parent.ret.rename_plugins = plugin_renames_
         parent.parser.choiceIdex += 1
         textTitle = Label(self, _('The installer script has finished, and '
                                   'will apply the following settings:'))
@@ -346,17 +347,16 @@ class PageFinish(PageInstaller):
         # Sub-packages
         self.listSubs = CheckListBox(self, choices=subs)
         self.listSubs.on_box_checked.subscribe(self._on_select_subs)
-        for index,key in enumerate(subs):
-            if sublist[key]:
+        for index, fn_key in enumerate(subs):
+            if sublist[fn_key]:
                 self.listSubs.lb_check_at_index(index, True)
-                self._wiz_parent.ret.select_sub_packages.append(key)
+                self._wiz_parent.ret.select_sub_packages.add(str(fn_key))
         self.plugin_selection = CheckListBox(self, choices=displayed_plugins)
         self.plugin_selection.on_box_checked.subscribe(self._on_select_plugin)
         for index, (key, do_enable) in enumerate(plugin_enabled.items()):
             if do_enable:
                 self.plugin_selection.lb_check_at_index(index, True)
                 self._wiz_parent.ret.select_plugins.append(key)
-        self._wiz_parent.ret.rename_plugins = plugin_renames
         # Ini tweaks
         self.listInis = ListBox(self, onSelect=self._on_select_ini,
                                 choices=list(iniedits))
@@ -486,8 +486,7 @@ class WryeParser(PreParser):
         self.parser_finished = False
         ##: Figure out why BAIN insists on including an empty sub-package
         # everywhere. Broke this part of the code, hence the 'if s' below.
-        self.sublist = bolt.LowerDict.fromkeys(  # FIXME FNDict??
-            (s for s in installer.subNames if s), False)
+        self.sublist = bolt.FNDict.fromkeys([*installer.subNames][:1], False)
         # all plugins mapped to their must-install state - initially False
         self._plugin_enabled = FNDict.fromkeys(  # type:FNDict[(f:=FName),f]
             sorted(fn_ for sub_plugins in installer.espmMap.values() for fn_ in
@@ -540,22 +539,6 @@ class WryeParser(PreParser):
         self.reversing = self.choiceIdex-1
         self.choiceIdex = -1
         return self.Continue()
-
-    def _is_plugin_in_package(self, plugin_name, package):
-        if package not in self.installer.espmMap: return False
-        plugin_name = plugin_name.lower()
-        v = self.installer.espmMap[package]
-        for j in v:
-            if plugin_name == j.lower():
-                return True
-        return False
-
-    def _plugin_in_active_package(self, plugin_name):
-        for i in self.sublist:
-            if self._is_plugin_in_package(plugin_name, i):
-                if self.sublist[i]:
-                    return True
-        return False
 
     def _resolve_plugin_rename(self, plugin_name: str) -> FName | None:
         return fn if (fn := FName(plugin_name)) in self._plugin_enabled \
@@ -788,9 +771,11 @@ class WryeParser(PreParser):
         for fn_espm in self.installer.espmMap[subpackage]:
             if bSelect:
                 self._select_plugin(True, fn_espm)
-            else:
-                if not self._plugin_in_active_package(fn_espm):
-                    self._select_plugin(False, fn_espm)
+            else: # check if some other active subpackage includes fn_espm
+                for fn_sub, is_act in self.sublist.items():
+                    if is_act and fn_espm in self.installer.espmMap[fn_sub]:
+                        return
+                self._select_plugin(False, fn_espm)
 
     def _SelectAll(self, bSelect):
         self._set_all_values(self.sublist, bSelect)

Mopy/bash/basher/installer_links.py
@@ -1010,7 +1010,7 @@ class Installer_Espm_Rename(_Installer_Details_Link):
         newName = self._askText(_(u'Enter new name (without the extension):'),
                                 title=_(u'Rename Plugin'), default=curName.fn_body)
         if not newName: return
-        if (newName := newName + curName.fn_ext) in \
+        if (newName := FName(newName + curName.fn_ext)) in \
                 self.window.espm_checklist_fns:
             return
         self._installer.setEspmName(curName, newName)

Mopy/bash/ScriptParser.py
@@ -66,6 +66,7 @@ from collections import defaultdict
 from string import digits, whitespace
 
 from . import bolt # no other Bash imports!
+from .bolt import FName
 
 #--------------------------------------------------
 name_start = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'
@@ -1449,14 +1450,14 @@ class PreParser(Parser):
             if plugin_name.fn_ext != new_plugin_name[-4:]:
                 raise ParserError(_('Cannot rename %s to %s: the extensions '
                     'must match.') % (plugin_name, new_plugin_name))
-            self.plugin_renames[plugin_name] = new_plugin_name
+            self.plugin_renames[plugin_name] = FName(new_plugin_name)
 
     def kwd_reset_plugin_name(self, plugin_name):
         plugin_name = self._resolve_plugin_rename(plugin_name)
         if plugin_name and plugin_name in self.plugin_renames:
             del self.plugin_renames[plugin_name]
 
-    def _resolve_plugin_rename(self, plugin_name: str) -> bolt.FName | None:
+    def _resolve_plugin_rename(self, plugin_name: str) -> FName | None:
         raise NotImplementedError # needs self._plugin_enabled
 
     def kwd_reset_all_plugin_names(self):
@@ -1488,7 +1489,7 @@ class PreParser(Parser):
         self.variables.clear()
         self.Flow = []
         self.notes = []
-        self.plugin_renames = {}
+        self.plugin_renames: bolt.FNDict[FName, FName] = bolt.FNDict()
         self.iniedits = defaultdict(bolt.LowerDict)
 
     # codebox stuff

Mopy/bash/basher/__init__.py
@@ -3182,8 +3182,8 @@ class InstallersDetails(_SashDetailsPanel):
             else:
                 fns = self.espm_checklist_fns = sorted(installer.espms, key=lambda x: (
                     x.fn_ext != u'.esm', x)) # esms first then alphabetically
-                espm_acti = {['', '*'][installer.isEspmRenamed(
-                    x)] + x: x not in installer.espmNots for x in fns}
+                espm_acti = {[x, f'*{x}'][installer.isEspmRenamed(
+                    x)]: x not in installer.espmNots for x in fns}
                 self.gEspmList.set_all_items_keep_pos(espm_acti)
             #--Comments
             self.gComments.text_content = installer.comments

Mopy/bash/bosh/bain.py
@@ -44,7 +44,7 @@ from ..archives import compress7z, defaultExt, extract7z, list_archive, \
     readExts
 from ..bolt import AFile, CIstr, FName, GPath_no_norm, ListInfo, Path, \
     SubProgress, deprint, dict_sort, forward_compat_path_to_fn, \
-    forward_compat_path_to_fn_list, round_size, top_level_items
+    forward_compat_path_to_fn_list, round_size, top_level_items, DefaultFNDict
 from ..bass import Store
 from ..exception import ArgumentError, BSAError, CancelError, FileError, \
     InstallerArchiveError, SkipError, StateError
@@ -281,7 +281,7 @@ class Installer(ListInfo):
         self.group = u'' #--Default from abstract. Else set by user.
         self.order = -1 #--Set by user/interface.
         self.is_active = False
-        self.espmNots = set() #--Plugin FNames that user has decided not to install.
+        self.espmNots: set[FName] = set() #--Plugin FNames that user has decided not to install.
         self._remaps = bolt.FNDict() # Pickles to dict, equivalent to previous
         #--Volatiles (not pickled values)
         #--Volatiles: directory specific
@@ -293,7 +293,7 @@ class Installer(ListInfo):
         self.has_fomod_conf = False
         self.hasWizard = False
         self.hasBCF = False
-        self.espmMap = bolt.DefaultFNDict(list)
+        self.espmMap: DefaultFNDict[FName, list[FName]] = DefaultFNDict(list)
         self.hasReadme = False
         self.hasBethFiles = False
         self.skipExtFiles = set()
@@ -394,8 +394,7 @@ class Installer(ListInfo):
                 return old
         return currentName
 
-    def setEspmName(self,currentName,newName):
-        currentName, newName = map(FName, (currentName, newName)) ##: needed? setEspmName used in belt
+    def setEspmName(self, currentName: FName, newName: FName):
         oldName = self.getEspmName(currentName)
         self._remaps[oldName] = newName
         if currentName in self.espmNots:
@@ -446,12 +445,11 @@ class Installer(ListInfo):
             self.fn_key = self.fn_key.decode('utf-8')
         if not isinstance(self.fn_key, FName):
             self.fn_key = FName(u'%s' % self.fn_key)
-        if self.espmNots and not isinstance(next(iter(self.espmNots)), FName):
+        if self.espmNots:
             self.espmNots = forward_compat_path_to_fn_list(self.espmNots,
                                                            ret_type=set)
-        if self._remaps and not isinstance(next(iter(self._remaps)), FName):
-            self._remaps = forward_compat_path_to_fn(self._remaps,
-                value_type=lambda v: FName('%s' % v)) # Path -> FName
+        self._remaps = forward_compat_path_to_fn(self._remaps,
+            value_type=lambda v: FName('%s' % v))  # Path -> FName
         if isinstance(self, _InstallerPackage):
             self._file_key = bass.dirs['installers'].join(self.fn_key)
             if not isinstance(self.src_sizeCrcDate, bolt.LowerDict):
@@ -505,7 +503,6 @@ class Installer(ListInfo):
         return Installer._badDlls
     # while checking for skips process some installer attributes
     _attributes_process = {}
-    _extensions_to_process = set()
 
     @staticmethod
     def init_global_skips(ask_yes):
@@ -637,28 +634,29 @@ class Installer(ListInfo):
                         # Move top-level docs to the Docs folder
                         dest = docs_ + dest
             return dest
+        attr_process = Installer._attributes_process
         for ext in Installer.docExts:
-            Installer._attributes_process[ext] = _process_docs
+            attr_process [ext] = _process_docs
         def _process_BCF(self, fileLower, full, fileExt, file_relative, sub):
             if fileLower[-7:-3] == u'-bcf' or u'-bcf-' in fileLower: ##: DOCS!
                 self.hasBCF = full
                 return None # skip
             return file_relative
-        Installer._attributes_process[defaultExt] = _process_BCF # .7z
+        attr_process[defaultExt] = _process_BCF # .7z
         def _process_txt(self, fileLower, full, fileExt, file_relative, sub):
             if fileLower == u'wizard.txt': # first check if it's the wizard.txt
                 self.hasWizard = full
                 return None # skip
             return _process_docs(self, fileLower, full, fileExt, file_relative,
                                  sub)
-        Installer._attributes_process[u'.txt'] = _process_txt
+        attr_process['.txt'] = _process_txt
         def _process_csv(self, fileLower, full, fileExt, file_relative, sub):
             parent_dir, split_fn = _split_fr(file_relative)
             if (not parent_dir and
                     bass.settings['bash.installers.redirect_csvs']):
                 return f'Bash Patches{os_sep}{split_fn}'
             return file_relative
-        Installer._attributes_process['.csv'] = _process_csv
+        attr_process['.csv'] = _process_csv
         def _remap_espms(self, fileLower, full, fileExt, file_relative, sub):
             rootLower = file_relative.split(os_sep, 1)
             if len(rootLower) > 1:
@@ -668,19 +666,17 @@ class Installer(ListInfo):
                 self.hasBethFiles = True
                 if not self.overrideSkips and not bass.settings[
                     u'bash.installers.autoRefreshBethsoft']:
-                    self.skipDirFiles.add(_(u'[Bethesda Content]') + u' ' +
-                                          full)
+                    self.skipDirFiles.add(_('[Bethesda Content]') + f' {full}')
                     return None # FIXME - after renames ?
             fn_mod = FName(file_relative)
             fn_mod = self._remaps.get(fn_mod, fn_mod)
-            if fn_mod not in self.espmMap[sub]:
+            if fn_mod not in self.espmMap[sub]: ##: why this check/ - renames?
                 self.espmMap[sub].append(fn_mod)
             self.espms.add(fn_mod)
             if fn_mod in self.espmNots: return None # skip
             return str(fn_mod) # will end up in a LowerDict - so str
         for extension in bush.game.espm_extensions:
-            Installer._attributes_process[extension] = _remap_espms
-        Installer._extensions_to_process = set(Installer._attributes_process)
+            attr_process[extension] = _remap_espms
 
     def _init_skips(self):
         voice_dir = os_sep.join(('sound', 'voice'))
@@ -847,7 +843,7 @@ class Installer(ListInfo):
         skipDirFilesDiscard = skipDirFiles.discard
         skipExtFilesAdd = self.skipExtFiles.add
         commonlyEditedExts = Installer.commonlyEditedExts
-        espmMap = self.espmMap = bolt.DefaultLowerDict(list)
+        espmMap = self.espmMap = bolt.DefaultFNDict(list)
         plugin_extensions = bush.game.espm_extensions
         # FIXME DROP: reReadMeMatch = Installer.reReadMe.match
         #--Scan over fileSizeCrcs
@@ -856,6 +852,7 @@ class Installer(ListInfo):
         fm_active = self.extras_dict.get(u'fomod_active', False)
         fm_dict = self.extras_dict.get('fomod_dict_v2', {})
         module_config = os.path.join(u'fomod', u'moduleconfig.xml')
+        iprocess = Installer._attributes_process
         for full, cached_size, crc in self.fileSizeCrcs:
             if rootIdex: # exclude all files that are not under root_dir
                 if not full.startswith(root_path): continue
@@ -926,8 +923,12 @@ class Installer(ListInfo):
                             if file_relative in self._remaps:
                                 file_relative = self._remaps[file_relative]
                                 # No need to update fileLower, will skip
+                            else:
+                                file_relative = FName(file_relative)
+                            ##: do we need this test? see also _remap_espms
                             if file_relative not in sub_esps:
                                 sub_esps.append(file_relative)
+                            ##: _remap_espms adds also to self.espms - should we do this here too?
                         if skip:
                             continue
                 # Add sub key to the espmMap, needed in belt
@@ -950,8 +951,9 @@ class Installer(ListInfo):
                 dest = None # destination of the file relative to the Data/ dir
                 # process attributes and define destination for docs and images
                 # (if not skipped globally)
-                if fileExt in Installer._extensions_to_process:
-                    dest = Installer._attributes_process[fileExt](
+                if fileExt in iprocess:
+                    # we don't use EAFP in case a KeyError is raised in callee
+                    dest = iprocess[fileExt](
                         self, fileLower, full, fileExt, file_relative, sub)
                     if dest is None:
                         continue

Mopy/bash/bosh/cosaves.py
@@ -76,10 +76,10 @@ class _Remappable(object):
     of one or more plugin files referenced in the cosave has been changed."""
     __slots__ = ()
 
-    def remap_plugins(self, plugin_renames: dict[str, str]):
+    def remap_plugins(self, fnmod_rename: dict[str, str]):
         """Remaps the names of relevant plugin entries in this object.
 
-        :param plugin_renames: A dictionary containing the renames: key is the
+        :param fnmod_rename: A dictionary containing the renames: key is the
             name of the plugin before the renaming, value is the name
             afterwards."""
         raise NotImplementedError
@@ -298,8 +298,8 @@ class _xSEModListChunk(_xSEChunk, _Dumpable, _Remappable):
         for mod_name in self.mod_names:
             log(f'    - {mod_name}')
 
-    def remap_plugins(self, plugin_renames):
-        self.mod_names = [plugin_renames.get(x, x) for x in self.mod_names]
+    def remap_plugins(self, fnmod_rename):
+        self.mod_names = [fnmod_rename.get(x, x) for x in self.mod_names]
 
 class _xSEChunkARVR(_xSEChunk, _Dumpable):
     """An ARVR (Array Variable) chunk. Only available in OBSE and NVSE. See
@@ -658,8 +658,8 @@ class _xSEChunkPLGN(_xSEChunk, _Dumpable, _Remappable):
                 log(f'    {self.mod_index:02X}       {self.light_index:04X}  '
                     f'           {self.mod_name}')
 
-        def remap_plugins(self, plugin_renames):
-            self.mod_name = plugin_renames.get(self.mod_name, self.mod_name)
+        def remap_plugins(self, fnmod_rename):
+            self.mod_name = fnmod_rename.get(self.mod_name, self.mod_name)
 
     def __init__(self, ins, chunk_type):
         super().__init__(ins, chunk_type)
@@ -683,9 +683,9 @@ class _xSEChunkPLGN(_xSEChunk, _Dumpable, _Remappable):
         for mod_entry in self.mod_entries:
             mod_entry.dump_to_log(log, save_masters_)
 
-    def remap_plugins(self, plugin_renames):
+    def remap_plugins(self, fnmod_rename):
         for mod_entry in self.mod_entries:
-            mod_entry.remap_plugins(plugin_renames)
+            mod_entry.remap_plugins(fnmod_rename)
 
 class _xSEChunkSTVR(_xSEChunk, _Dumpable):
     """An STVR (String Variable) chunk. Only available in OBSE and NVSE. See
@@ -804,9 +804,9 @@ class _xSEPluginChunk(_AChunk, _Remappable):
             total_len += chunk.chunk_length()
         return total_len
 
-    def remap_plugins(self, plugin_renames):
+    def remap_plugins(self, fnmod_rename):
         for xse_chunk in self.remappable_chunks:
-            xse_chunk.remap_plugins(plugin_renames)
+            xse_chunk.remap_plugins(fnmod_rename)
 
     def __repr__(self):
         ##: Extremely hacky, the proper method is _get_plugin_signature - but
@@ -864,9 +864,9 @@ class _PluggyPluginBlock(_PluggyBlock, _Remappable):
             pack_byte(out, self.game_id)
             _pack_cosave_str32(out, self.plugin_name)
 
-        def remap_plugins(self, plugin_renames):
-            self.plugin_name = plugin_renames.get(self.plugin_name,
-                                                  self.plugin_name)
+        def remap_plugins(self, fnmod_rename):
+            self.plugin_name = fnmod_rename.get(self.plugin_name,
+                                                self.plugin_name)
 
         def dump_to_log(self, log, save_masters_):
             log(u'    %02X    %02X    %s' % (self.pluggy_id, self.game_id,
@@ -892,9 +892,9 @@ class _PluggyPluginBlock(_PluggyBlock, _Remappable):
         for plugin in self.plugins:
             plugin.dump_to_log(log, save_masters_)
 
-    def remap_plugins(self, plugin_renames):
+    def remap_plugins(self, fnmod_rename):
         for plugin in self.plugins:
-            plugin.remap_plugins(plugin_renames)
+            plugin.remap_plugins(fnmod_rename)
 
     def unique_identifier(self):
         return _(u'Plugin Block')
@@ -1420,11 +1420,11 @@ class ACosave(_Dumpable, _Remappable, AFile):
         self.read_cosave()
         self.cosave_header.dump_to_log(log, save_masters_)
 
-    def remap_plugins(self, plugin_renames):
+    def remap_plugins(self, fnmod_rename):
         # We need the entire cosave to remap
         self.read_cosave()
         for cosave_chunk in self.remappable_chunks:
-            cosave_chunk.remap_plugins(plugin_renames)
+            cosave_chunk.remap_plugins(fnmod_rename)
 
     @classmethod
     def get_cosave_path(cls, save_path: Path) -> Path:

commit 82a4365712cc48299a4be5f5c3ab3ea7f7b6d837

Fix another regression in wizards
Broke in b1d06b5bbc7538076014f45aa2780ba1706dcbd9, which changed a list
to a dict but updated its usage to a .add() call. Let's change it to a
set (and the other nearby one too), since duplicates don't make sense
here anyways.

Mopy/bash/basher/belt.py
@@ -46,10 +46,10 @@ class WizInstallInfo(object):
                  'select_sub_packages', 'ini_edits', 'should_install')
     # canceled: Set to true if the user canceled the wizard, or if an error
     # occurred
-    # select_plugins: List of plugins to 'select' for install
+    # select_plugins: Set of plugins to 'select' for install
     # rename_plugins: Dictionary of renames for plugins.  In the format of:
     #   'original name':'new name'
-    # select_sub_packages: List of sub-packages to 'select' for install
+    # select_sub_packages: Set of sub-packages to 'select' for install
     # ini_edits: Dictionary of INI edits to apply/create.  In the format of:
     #   'ini file': {
     #      'section': {
@@ -63,9 +63,9 @@ class WizInstallInfo(object):
 
     def __init__(self):
         self.canceled = False
-        self.select_plugins = []
+        self.select_plugins = set()
         self.rename_plugins: FNDict[FName, FName] = FNDict()
-        self.select_sub_packages = {}
+        self.select_sub_packages = set()
         self.ini_edits = bolt.LowerDict()
         self.should_install = False
 
@@ -356,7 +356,7 @@ class PageFinish(PageInstaller):
         for index, (key, do_enable) in enumerate(plugin_enabled.items()):
             if do_enable:
                 self.plugin_selection.lb_check_at_index(index, True)
-                self._wiz_parent.ret.select_plugins.append(key)
+                self._wiz_parent.ret.select_plugins.add(key)
         # Ini tweaks
         self.listInis = ListBox(self, onSelect=self._on_select_ini,
                                 choices=list(iniedits))

Mopy/bash/basher/installer_links.py
@@ -355,11 +355,12 @@ class Installer_Wizard(_Installer_AWizardLink):
                     sel_package.subActives[index + 1] = select
                 idetails.refreshCurrent(sel_package)
                 # Check the plugins that were selected by the wizard
-                espm_strs = idetails.gEspmList.lb_get_str_items()
+                espm_fns = list(map(FName,
+                    idetails.gEspmList.lb_get_str_items()))
                 sel_package.espmNots = set()
                 for index, espm in enumerate(idetails.espm_checklist_fns):
-                    do_check = espm_strs[index] in ret.select_plugins or bool(
-                        sel_package.espmNots.add(espm)) # bool(None) == False
+                    do_check = (espm_fns[index] in ret.select_plugins
+                                or bool(sel_package.espmNots.add(espm)))
                     idetails.gEspmList.lb_check_at_index(index, do_check)
                 idetails.refreshCurrent(sel_package)
                 #Rename the plugins that need renaming