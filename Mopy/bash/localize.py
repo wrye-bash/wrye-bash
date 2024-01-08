893d62642244772e7fc26b6056d4c3d2d6e44011
Infernio <infernio@icloud.com>
2024-01-08 21:12:11 +0100

    Add 'fuzzy' translation update support
    We should migrate to using msgmerge, but it doesn't give good results
    right now (because the translatable strings have veered so far from the
    translations themselves), so roll our own for the moment.

--- Mopy/bash/localize.py
+++ Mopy/bash/localize.py
@@ -266,6 +266,9 @@ def dump_translator(out_path, lang):
                         translated_line: str = _(stripped_line)
                         # We're going to need the msgid either way
                         out.write(new_line)
+                        if translated_line == stripped_line:
+                            translated_line = _fixup_old_translation(
+                                stripped_line, translated_line)
                         if translated_line != stripped_line:
                             # This has a translation, so write that one out
                             out.write(b'msgstr "')
@@ -301,6 +304,35 @@ def dump_translator(out_path, lang):
                 try: os.remove(tmp_po)
                 except OSError: pass
     return new_po
+
+_FMT_SPECIFIER_REGEX = re.compile(r'(%\([^)]+\)(\w))')
+_FMT_SPECIFIER_OLD_REGEX = re.compile(r'%(\w)')
+def _fixup_old_translation(stripped_line: str, translated_line: str):
+    # Try updating %s -> %(...)s
+    stripped_old = _FMT_SPECIFIER_REGEX.sub(
+        lambda m: f'%{m.group(2)}', stripped_line)
+    translated_old = _(stripped_old)
+    if translated_old != stripped_old:
+        # Add the %(...)s specifiers back in
+        new_specifiers = _FMT_SPECIFIER_REGEX.findall(
+            stripped_line)
+        spec_index = 0
+        i = 0
+        wip_translation = ''
+        while i < len(translated_old):
+            next_ma = _FMT_SPECIFIER_OLD_REGEX.search(
+                translated_old, i)
+            if next_ma:
+                wip_translation += translated_old[i:next_ma.start()]
+                wip_translation += new_specifiers[spec_index][0]
+                spec_index += 1
+                i = next_ma.end()
+            else:
+                # We've hit all specifiers, finish line
+                wip_translation += translated_old[i:]
+                break
+        return wip_translation
+    return translated_line
 
 #------------------------------------------------------------------------------
 # Formatting
