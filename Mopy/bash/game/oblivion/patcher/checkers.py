wrye-bash/wrye-bash

commit 7c3d5d94dc9685ddfd839f906cecc9d7038efb3f

game/oblivion/patcher: Rework translations
Make translations a bit more powerful by moving the "Salan's Catalog of"
part up and merging it with the substitutions.

Mopy/bash/bosh/_saves.py:
There is no point in translating just that 'AI' string in the method.
Plus the method seems unused.

Mopy/bash/game/oblivion/patcher/checkers.py
@@ -32,16 +32,16 @@ from ....brec import FormId, RecordType, null4, null3, null2
 
 # Cobl Catalogs ---------------------------------------------------------------
 _ingred_alchem = (
-    (1, 0xCED, _('Alchemical Ingredients I')),
-    (2, 0xCEC, _('Alchemical Ingredients II')),
-    (3, 0xCEB, _('Alchemical Ingredients III')),
-    (4, 0xCE7, _('Alchemical Ingredients IV')),
+    (1, 0xCED, _("Salan's Catalog of Alchemical Ingredients I")),
+    (2, 0xCEC, _("Salan's Catalog of Alchemical Ingredients II")),
+    (3, 0xCEB, _("Salan's Catalog of Alchemical Ingredients III")),
+    (4, 0xCE7, _("Salan's Catalog of Alchemical Ingredients IV")),
 )
 _effect_alchem = (
-    (1, 0xCEA, _('Alchemical Effects I')),
-    (2, 0xCE9, _('Alchemical Effects II')),
-    (3, 0xCE8, _('Alchemical Effects III')),
-    (4, 0xCE6, _('Alchemical Effects IV')),
+    (1, 0xCEA, _("Salan's Catalog of Alchemical Effects I")),
+    (2, 0xCE9, _("Salan's Catalog of Alchemical Effects II")),
+    (3, 0xCE8, _("Salan's Catalog of Alchemical Effects III")),
+    (4, 0xCE6, _("Salan's Catalog of Alchemical Effects IV")),
 )
 _book_fids = {FormId.from_tuple((cobl_main, book_data[1]))
               for book_data in chain(_ingred_alchem, _effect_alchem)}
@@ -106,7 +106,7 @@ class CoblCatalogsPatcher(ExSpecial):
                 return None # This shouldn't happen, but just in case...
             book = patch_books.id_records[book_fid]
             book.book_text = '<div align="left"><font face=3 color=4444>'
-            book.book_text += (_("Salan's Catalog of %s") + '\r\n\r\n') % full
+            book.book_text += (full + '\r\n\r\n') % full
             if keep(book_fid, book):
                 return book
         #--Ingredients Catalog
@@ -152,8 +152,10 @@ class CoblCatalogsPatcher(ExSpecial):
             book.book_text = re.sub('\r\n', '<br>\r\n', book.book_text)
         #--Log
         log.setHeader(u'= ' + self._patcher_name)
-        log('* ' + _('Ingredients Cataloged') + f': {len(id_ingred)}')
-        log('* ' + _('Effects Cataloged') + f': {len(effect_ingred)}')
+        log('* ' + _('Ingredients Cataloged: %(total_changed)d') % {
+            'total_changed': len(id_ingred)})
+        log('* ' + _('Effects Cataloged: %(total_changed)d') % {
+            'total_changed': len(effect_ingred)})
 
 #------------------------------------------------------------------------------
 _ob_path = bush.game.master_file
@@ -221,4 +223,5 @@ class SEWorldTestsPatcher(ExSpecial):
                 if keep(rid, record):
                     patched.append(record.eid)
         log.setHeader(f'= {self._patcher_name}')
-        log('===' + _('Quests Patched') + f': {len(patched)}')
+        log('===' + _('Quests Patched: %(total_changed)d') % {
+            'total_changed': len(patched)})

Mopy/bash/bosh/_saves.py
@@ -173,7 +173,7 @@ class SreNPC(object):
         """Returns record as a change record tuple."""
         return 35, self.getFlags(), version, self.getData()
 
-    def dumpText(self,saveFile):
+    def dumpText(self,saveFile): ## unused?
         """Returns informal string representation of data."""
         buff = []
         fids = saveFile.fids
@@ -195,7 +195,7 @@ class SreNPC(object):
             for spell in self.spells:
                 buff.append(f'  {fids[spell]:8X}')
         if self.ai is not None:
-            buff.append(_(u'AI') + f':\n  {self.ai}')
+            buff.append(f'AI:\n  {self.ai}')
         if self.health is not None:
             buff.append(f'Health\n  {self.health}')
             buff.append(f'Unused2\n  {self.unused2}')

Mopy/bash/game/oblivion/patcher/preservers.py
@@ -132,7 +132,8 @@ class CoblExhaustionPatcher(_ExSpecialList):
 
     def _pLog(self, log, count):
         log.setHeader(u'= ' + self._patcher_name)
-        log('* ' + _('Powers Tweaked') + f': {sum(count.values())}')
+        log('* ' + _('Powers Tweaked: %(total_changed)d') % {
+            'total_changed': sum(count.values())})
         for srcMod in load_order.get_ordered(count):
             log(f'  * {srcMod}: {count[srcMod]:d}')
 

commit 1fa4568cbb2e66dc797d0da0007fdc134388d7a8

SSE: Update bethDataFiles
A few new files were added in v1.6.1130.

Small fixup for Oblivion's checkers.py

Broke in 7c3d5d9.

Mopy/bash/game/oblivion/patcher/checkers.py
@@ -106,7 +106,7 @@ class CoblCatalogsPatcher(ExSpecial):
                 return None # This shouldn't happen, but just in case...
             book = patch_books.id_records[book_fid]
             book.book_text = '<div align="left"><font face=3 color=4444>'
-            book.book_text += (full + '\r\n\r\n') % full
+            book.book_text += full + '\r\n\r\n'
             if keep(book_fid, book):
                 return book
         #--Ingredients Catalog

Mopy/bash/game/skyrimse/__init__.py
@@ -132,6 +132,9 @@ class ASkyrimSEGameInfo(ASkyrimGameInfo):
         'skyrim - voices_ja0.bsa',
         'skyrim - voices_pl0.bsa',
         'skyrim - voices_ru0.bsa',
+        '_resourcepack.bsa',
+        '_resourcepack.esl',
+        'marketplacetextures.bsa',
         'ccafdsse001-dwesanctuary.bsa',
         'ccafdsse001-dwesanctuary.esm',
         'ccasvsse001-almsivi.bsa',