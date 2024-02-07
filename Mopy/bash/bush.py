wrye-bash/wrye-bash

commit 20dd955312a328000f217e9a84c9f20b8f8566c2

Rewrite Steam game detection
Now avoids the registry. This has several advantages:
 - Works on Linux
 - Works with Starfield, which does not have a custom registry key
   anymore
 - Works with Morrowind, which is *supposed* to have a custom registry
   key, but I've never managed to generate one on Windows
 - No more need to launch the game once for WB to detect it (and no more
   need to relaunch it after verifying)

Note that this isn't enough for proper automatic game detection on Linux
yet, because we don't set the local AppData folder correctly (should set
itself to automatically point to the compatdata protonprefix when the
Steam version is detected).

Note also the new dependency - didn't want to hack together my own
parser when there's a perfectly good, *tested* parser available. Could
have made it optional, but I chose not to for two reasons:
 1. It's pure Python, which means it's highly unlikely to ever block us
    from upgrading to a new Python version.
 2. Detecting Steam games is one of the most important things for WB to
    be able to do. If someone does not have PyMuPDF installed, that's
    not a big deal - they just won't be able to view PDFs. But if
    someone didn't have vdf installed, that would mean WB would just
    quietly ignore all their Steam games. Steam is the No. 1 platform
    for Bethesda games on PC, so I think it's very important that this
    work out of the box (including on all OSes).

Under #243

Mopy/bash/bush.py
@@ -35,7 +35,8 @@ from configparser import ConfigParser
 from . import game as game_init
 from .bolt import GPath, Path, deprint, dict_sort
 from .env import get_egs_game_paths, get_legacy_ws_game_info, \
-    get_legacy_ws_game_paths, get_registry_game_paths, get_ws_game_paths
+    get_legacy_ws_game_paths, get_gog_game_paths, get_ws_game_paths,\
+    get_steam_game_paths
 from .exception import BoltError
 from .game import GameInfo, patch_game
 from .initialization import get_path_from_ini
@@ -47,7 +48,8 @@ foundGames: dict[str, Path] = {}    # dict used by the Settings switch game menu
 
 # Module Cache
 _allGames: dict[str, type[GameInfo]] = {}
-_registry_games: dict[str, list[Path]] = {}
+_steam_games: dict[str, list[Path]] = {}
+_gog_games: dict[str, list[Path]] = {}
 _ws_legacy_games: dict[str, list[Path]] = {}
 _ws_games: dict[str, list[Path]] = {}
 _egs_games: dict[str, list[Path]] = {}
@@ -57,7 +59,8 @@ def reset_bush_globals():
     global ws_info
     game = None
     ws_info = None
-    for d in (_allGames, _registry_games):
+    for d in (_allGames, _steam_games, _gog_games, _ws_legacy_games, _ws_games,
+              _egs_games):
         d.clear()
 
 def _print_found_games(game_dict):
@@ -95,16 +98,19 @@ def _supportedGames(skip_ws_games=False):
                 continue
             gtype = module_container.GAME_TYPE
             game_types = gtype if isinstance(gtype, dict) else {
-                gtype.displayName: gtype}
+                gtype.unique_display_name: gtype}
             _allGames.update(game_types)
         except (ImportError, AttributeError):
             deprint(f'Error in game support module {modname}', traceback=True)
             continue
         # Get this game's install path(s)
         for gt_display_name, game_type in game_types.items():
-            registry_paths = get_registry_game_paths(game_type)
-            if registry_paths:
-                _registry_games[gt_display_name] = registry_paths
+            steam_paths = get_steam_game_paths(game_type)
+            if steam_paths:
+                _steam_games[gt_display_name] = steam_paths
+            gog_paths = get_gog_game_paths(game_type)
+            if gog_paths:
+                _gog_games[gt_display_name] = gog_paths
             ws_legacy_paths = get_legacy_ws_game_paths(game_type)
             if ws_legacy_paths:
                 _ws_legacy_games[gt_display_name] = ws_legacy_paths
@@ -122,30 +128,32 @@ def _supportedGames(skip_ws_games=False):
     # hack in settings_dialog)
     msg = ['The following games are supported by this version of Wrye Bash:']
     deduped_games = defaultdict(set)
-    for g in _allGames:
+    for g, _v in dict_sort(_allGames):
         g_split = g.split('(')
         base_game_name = g_split[0].strip()
         if len(g_split) > 1:
             deduped_games[base_game_name].add(g_split[1][:-1])
         else:
-            deduped_games[base_game_name].add('Steam')
+            deduped_games[base_game_name].add('Unknown')
     for base_game_name, game_variants in deduped_games.items():
         fmt_game_variants = ', '.join(sorted(game_variants))
         msg.append(f'  - {base_game_name} ({fmt_game_variants})')
     # Dump out info about all games that we *actually* found
     msg.append('Wrye Bash looked for installations of supported games in the '
                'following places:')
-    msg.append(' 1. Windows Registry:')
-    if _registry_games:
-        msg.append('  The following supported games were found via the '
-                   'registry:')
-        msg.extend(_print_found_games(_registry_games))
+    msg.append(' 1. Steam:')
+    if _steam_games:
+        msg.append('  The following supported games were found via Steam:')
+        msg.extend(_print_found_games(_steam_games))
     else:
-        msg.append('  No supported games were found via the registry.')
-    msg.extend(f'  {wrapped_line}' for wrapped_line in textwrap.wrap(
-        'Make sure to run the launcher of each game you installed through '
-        'Steam once, otherwise Wrye Bash will not be able to find it.'))
-    msg.append(' 2. Windows Store (Legacy):')
+        msg.append('  No supported games were found via Steam.')
+    msg.append(' 2. GOG (via Windows Registry):')
+    if _gog_games:
+        msg.append('  The following supported games were found via GOG:')
+        msg.extend(_print_found_games(_gog_games))
+    else:
+        msg.append('  No supported games were found via GOG.')
+    msg.append(' 3. Windows Store (Legacy):')
     if _ws_legacy_games:
         msg.append('  The following supported games with modding enabled were '
                    'found via the legacy Windows Store:')
@@ -153,7 +161,7 @@ def _supportedGames(skip_ws_games=False):
     else:
         msg.append('  No supported games with modding enabled were found via '
                    'the legacy Windows Store.')
-    msg.append(' 3. Windows Store:')
+    msg.append(' 4. Windows Store:')
     if skip_ws_games:
         msg.append('  Windows Store game detection was disabled via bash.ini.')
     elif _ws_games:
@@ -162,7 +170,7 @@ def _supportedGames(skip_ws_games=False):
         msg.extend(_print_found_games(_ws_games))
     else:
         msg.append('  No supported games were found via the Windows Store.')
-    msg.append(' 4. Epic Games Store:')
+    msg.append(' 5. Epic Games Store:')
     if _egs_games:
         msg.append('  The following supported games were found via the Epic '
                    'Games Store:')
@@ -172,7 +180,7 @@ def _supportedGames(skip_ws_games=False):
                    'Store.')
     deprint('\n'.join(msg))
     # Merge the dicts of games we found from all global sources
-    all_found_games = _registry_games.copy()
+    all_found_games = _steam_games.copy()
     def merge_games(to_merge_games):
         """Helper method for merging games and install paths from various
         sources into the final all_found_games dict."""
@@ -181,6 +189,7 @@ def _supportedGames(skip_ws_games=False):
                 all_found_games[found_game].extend(found_paths)
             else:
                 all_found_games[found_game] = found_paths
+    merge_games(_gog_games)
     merge_games(_ws_legacy_games)
     merge_games(_ws_games)
     merge_games(_egs_games)
@@ -287,4 +296,4 @@ def detect_and_set_game(cli_game_dir=u'', bash_ini_=None, gname=None,
     return {_allGames[found_game]: fg_path for found_game, fg_path
             in foundGames.items()}
 
-def game_path(display_name): return foundGames[display_name]
+def game_path(target_unique_dn): return foundGames[target_unique_dn]

Mopy/bash/env/linux.py
@@ -28,7 +28,8 @@ import subprocess
 import sys
 from pathlib import Path as PPath ##: To be obsoleted when we refactor Path
 
-from .common import _find_legendary_games, _LegacyWinAppInfo
+from .common import _find_legendary_games, _LegacyWinAppInfo, \
+    _parse_steam_manifests
 # some hiding as pycharm is confused in __init__.py by the import *
 from ..bolt import GPath as _GPath
 from ..bolt import GPath_no_norm as _GPath_no_norm
@@ -53,7 +54,7 @@ GOOD_EXITS = (BTN_OK, BTN_YES)
 
 # Internals ===================================================================
 def _getShellPath(folderKey): ##: mkdirs
-    home = os.path.expanduser(u'~')
+    home = os.path.expanduser('~')
     return _GPath({
         'Personal': f'{home}/Documents',
         'Local AppData': f'{home}/.local/share',
@@ -61,6 +62,18 @@ def _getShellPath(folderKey): ##: mkdirs
 
 def _get_error_info():
     return '\n'.join(f'  {k}: {v}' for k, v in dict_sort(os.environ))
+
+@functools.cache
+def _get_steam_path() -> _Path | None:
+    """Retrieve the path used by Steam."""
+    # Resolve the .steam/root symlink, the user may have moved their Steam
+    # install out of the default (.local/share/Steam) location
+    try:
+        steam_path = os.path.realpath(os.path.expanduser('~/.steam/root'),
+            strict=True)
+    except OSError:
+        return None # Steam path doesn't exist
+    return _GPath_no_norm(steam_path)
 
 # API - Functions =============================================================
 ##: Several of these should probably raise instead
@@ -80,14 +93,20 @@ def find_egs_games():
 def get_registry_path(_subkey, _entry, _test_path_callback):
     return None # no registry on Linux
 
-def get_registry_game_paths(_submod):
-    return [] # no registry on Linux
+def get_gog_game_paths(_submod):
+    ##: Implement reading from Heroic Games launcher (and maybe others like
+    # Lutris?)
+    return []
 
 def get_legacy_ws_game_info(_submod):
     return _LegacyWinAppInfo() # no Windows Store on Linux
 
 def get_ws_game_paths(_submod):
     return [] # no Windows Store on Linux
+
+def get_steam_game_paths(submod):
+    return [_GPath_no_norm(p) for p in
+            _parse_steam_manifests(submod, _get_steam_path())]
 
 def get_personal_path():
     return _getShellPath(u'Personal'), _get_error_info()

Mopy/bash/env/windows.py
@@ -48,10 +48,11 @@ import win32gui
 from win32con import FILE_ATTRIBUTE_HIDDEN
 
 from .common import _find_legendary_games, _get_language_paths, \
-    _LegacyWinAppInfo, _LegacyWinAppVersionInfo
+    _LegacyWinAppInfo, _LegacyWinAppVersionInfo, _parse_steam_manifests
 from .common import file_operation as _default_file_operation
 # some hiding as pycharm is confused in __init__.py by the import *
 from ..bolt import GPath as _GPath
+from ..bolt import GPath_no_norm as _GPath_no_norm
 from ..bolt import Path as _Path
 from ..bolt import deprint as _deprint
 from ..bolt import unpack_int as _unpack_int
@@ -564,6 +565,12 @@ def _find_ws_games() -> dict[str, _Path]:
                 found_ws_games[msgame_name] = ws_content_path
     return found_ws_games
 
+@functools.cache
+def _get_steam_path() -> _Path | None:
+    """Retrieve the path used by Steam."""
+    return get_registry_path(r'Valve\Steam', 'InstallPath',
+        lambda p: p.join('steam.exe').is_file())
+
 # All code starting from the 'BEGIN MIT-LICENSED PART' comment and until the
 # 'END MIT-LICENSED PART' comment is based on
 # https://gist.github.com/mkropat/7550097 by Michael Kropat
@@ -1043,9 +1050,9 @@ def get_registry_path(subkey, entry, test_path_callback):
             return installPath
     return None
 
-def get_registry_game_paths(submod):
-    """Check registry-supplied game paths for the game detection file(s)."""
-    reg_keys = submod.registry_keys
+def get_gog_game_paths(submod):
+    """Check registry for games with GOG keys."""
+    reg_keys = submod.gog_registry_keys
     if not reg_keys:
         return [] # Game is not detectable via registry
     for subkey, entry in reg_keys:
@@ -1069,6 +1076,12 @@ def get_ws_game_paths(submod):
             return _get_language_paths(submod.Ws.ws_language_dirs,
                 all_ws_games[ws_app_name])
     return []
+
+def get_steam_game_paths(submod):
+    """Read Steam config information to determine which Steam games are
+    installed and return their install paths."""
+    return [_GPath_no_norm(p) for p in
+            _parse_steam_manifests(submod, _get_steam_path())]
 
 def get_personal_path():
     return (_GPath(_get_known_path(_FOLDERID.Documents)),

Mopy/bash/game/__init__.py
@@ -51,11 +51,20 @@ class ObjectIndexRange(Enum):
 # Abstract class - to be overriden --------------------------------------------
 class GameInfo(object):
     # Main game info - should be overridden -----------------------------------
-    # Name of the game to use in UI.
-    displayName = u'' ## Example: u'Skyrim'
+    # The name of the game that will be shown to the user. That is its *only*
+    # use! There are a few places where this is (mis)used for other purposes
+    # left, those should be hunted down and replaced with dedicated game vars
+    display_name = '' ## Example: 'Skyrim'
+    # A name that must be 100% unique per game, but will also be shown in the
+    # GUI (namely when picking what game to launch). This is automatically set
+    # to display_name, with a (Store) suffix appended (e.g. (Steam)) by the
+    # appropriate mixin classes (e.g. SteamMixin), so there is generally no
+    # need to set it manually
+    unique_display_name = '' ## Example: 'Skyrim Special Edition (Steam)'
     # A name used throughout the codebase for identifying the current game in
     # various situations, e.g. to decide which BSAs to use, which save header
-    # types to use, etc.
+    # types to use, etc. *DEPRECATED* for new uses - introduce dedicated game
+    # vars instead
     fsName = u'' ## Example: u'Skyrim'
     # Alternate display name of Wrye Bash when managing this game
     altName = u'' ## Example: u'Wrye Smash'
@@ -123,17 +132,18 @@ class GameInfo(object):
     # The name that this game has on the BOSS command line. If empty, indicates
     # that BOSS does not support this game
     boss_game_name = u''
-    # Registry keys to read to find the install location. This is a list of
-    # tuples of two strings, where each tuple defines the subkey and entry to
-    # try. Multiple tuples in the list will be tried in order, with the first
-    # one that works being used.
+    # Registry keys to read to find the install location for GOG. This is a
+    # list of tuples of two strings, where each tuple defines the subkey and
+    # entry to try. Multiple tuples in the list will be tried in order, with
+    # the first one that works being used. Generally automatically generated
+    # via GOGMixin, so there is usually no need to fill this in manually.
     # These are relative to:
     #  HKLM\Software
     #  HKLM\Software\Wow6432Node
     #  HKCU\Software
     #  HKCU\Software\Wow6432Node
     # Example: [(r'Bethesda Softworks\Oblivion', 'Installed Path')]
-    registry_keys = []
+    gog_registry_keys = []
     # URL to the Nexus site for this game
     nexusUrl = u''   # URL
     nexusName = u''  # Long Name
@@ -176,6 +186,12 @@ class GameInfo(object):
             from .. import brec
             return cls.__master_fids.setdefault(object_id,
                 brec.FormId.from_tuple((cls.master_file, object_id)))
+
+    class St:
+        """Information about this game on Steam."""
+        # The app IDs on Steam. An empty list indicates the game is not
+        # available on Steam
+        steam_ids = []
 
     class Ws(object):
         """Information about this game on the Windows Store."""
@@ -521,7 +537,8 @@ class GameInfo(object):
 
     @fast_cached_property
     def modding_esm_size(self):
-        if self.displayName != 'Oblivion': # hack to avoid a bunch of overrides
+        # Hack to avoid a bunch of overrides
+        if self.display_name != 'Oblivion':
             return FNDict()
         b, e = self.master_file.rsplit('.', 1)
         return FNDict({

commit aeae289f7f1f3536d9da0df643efe50405153572

Restore support for disc/retail versions of games
Some of the old games (Morrowind, Oblivion and Fallout 3 to be exact)
had disc/retail versions that were independent of Steam.
20dd955312a328000f217e9a84c9f20b8f8566c2 broke detection of those, since
previously they were included in the registry check we used for the
Steam version. Restore support, but don't duplicate paths for the GOG
and Steam versions (which set the same registry key as the disc
versions).

Mopy/bash/bush.py
@@ -34,19 +34,21 @@ from . import game as game_init, bass
 from .bolt import GPath, Path, deprint, dict_sort
 from .env import get_egs_game_paths, get_legacy_ws_game_info, \
     get_legacy_ws_game_paths, get_gog_game_paths, get_ws_game_paths, \
-    get_steam_game_paths, get_file_version, get_game_version_fallback
+    get_steam_game_paths, get_file_version, get_game_version_fallback, \
+    get_disc_game_paths
 from .exception import BoltError
 from .game import GameInfo, patch_game
 
 # Game detection --------------------------------------------------------------
-game: 'patch_game.PatchGame' | None = None
-ws_info = None                      # type: env._LegacyWinAppInfo | None
+game: patch_game.PatchGame | None = None
+ws_info: 'env._LegacyWinAppInfo' | None = None
 foundGames: dict[str, Path] = {}    # dict used by the Settings switch game menu
 
 # Module Cache
 _allGames: dict[str, type[GameInfo]] = {}
 _steam_games: dict[str, list[Path]] = {}
 _gog_games: dict[str, list[Path]] = {}
+_disc_games: dict[str, list[Path]] = {}
 _ws_legacy_games: dict[str, list[Path]] = {}
 _ws_games: dict[str, list[Path]] = {}
 _egs_games: dict[str, list[Path]] = {}
@@ -108,6 +110,9 @@ def _supportedGames(skip_ws_games=False):
             gog_paths = get_gog_game_paths(game_type)
             if gog_paths:
                 _gog_games[gt_display_name] = gog_paths
+            disc_paths = get_disc_game_paths(game_type, steam_paths, gog_paths)
+            if disc_paths:
+                _disc_games[gt_display_name] = disc_paths
             ws_legacy_paths = get_legacy_ws_game_paths(game_type)
             if ws_legacy_paths:
                 _ws_legacy_games[gt_display_name] = ws_legacy_paths
@@ -150,7 +155,14 @@ def _supportedGames(skip_ws_games=False):
         msg.extend(_print_found_games(_gog_games))
     else:
         msg.append('  No supported games were found via GOG.')
-    msg.append(' 3. Windows Store (Legacy):')
+    msg.append(' 3. Disc Versions (via Windows Registry):')
+    if _disc_games:
+        msg.append('  The following disc versions of supported games were '
+                   'found:')
+        msg.extend(_print_found_games(_disc_games))
+    else:
+        msg.append('  No disc versions of supported games were found.')
+    msg.append(' 4. Windows Store (Legacy):')
     if _ws_legacy_games:
         msg.append('  The following supported games with modding enabled were '
                    'found via the legacy Windows Store:')
@@ -158,7 +170,7 @@ def _supportedGames(skip_ws_games=False):
     else:
         msg.append('  No supported games with modding enabled were found via '
                    'the legacy Windows Store.')
-    msg.append(' 4. Windows Store:')
+    msg.append(' 5. Windows Store:')
     if skip_ws_games:
         msg.append('  Windows Store game detection was disabled via bash.ini.')
     elif _ws_games:
@@ -167,7 +179,7 @@ def _supportedGames(skip_ws_games=False):
         msg.extend(_print_found_games(_ws_games))
     else:
         msg.append('  No supported games were found via the Windows Store.')
-    msg.append(' 5. Epic Games Store:')
+    msg.append(' 6. Epic Games Store:')
     if _egs_games:
         msg.append('  The following supported games were found via the Epic '
                    'Games Store:')
@@ -187,6 +199,7 @@ def _supportedGames(skip_ws_games=False):
             else:
                 all_found_games[found_game] = found_paths
     merge_games(_gog_games)
+    merge_games(_disc_games)
     merge_games(_ws_legacy_games)
     merge_games(_ws_games)
     merge_games(_egs_games)

Mopy/bash/env/linux.py
@@ -165,6 +165,11 @@ def get_gog_game_paths(_submod):
     # Lutris?)
     return []
 
+def get_disc_game_paths(_submod, _found_steam_paths, _found_gog_paths):
+    # We can't detect this on Linux because there's no registry to pull from,
+    # users will just have to tell us via -o/bash.ini
+    return []
+
 def get_legacy_ws_game_info(_submod):
     return _LegacyWinAppInfo() # no Windows Store on Linux
 

Mopy/bash/env/windows.py
@@ -541,6 +541,16 @@ def _get_steam_path() -> _Path | None:
     return get_registry_path(r'Valve\Steam', 'InstallPath',
         lambda p: p.join('steam.exe').is_file())
 
+def _find_registry_games(submod, reg_keys):
+    """Helper for finding games via registry keys."""
+    if not reg_keys:
+        return [] # Game is not detectable via registry
+    for subkey, entry in reg_keys:
+        reg_path = get_registry_path(subkey, entry, submod.test_game_path)
+        if reg_path:
+            return [reg_path]
+    return []
+
 # All code starting from the 'BEGIN MIT-LICENSED PART' comment and until the
 # 'END MIT-LICENSED PART' comment is based on
 # https://gist.github.com/mkropat/7550097 by Michael Kropat
@@ -1022,14 +1032,15 @@ def get_registry_path(subkey, entry, test_path_callback):
 
 def get_gog_game_paths(submod):
     """Check registry for games with GOG keys."""
-    reg_keys = submod.gog_registry_keys
-    if not reg_keys:
-        return [] # Game is not detectable via registry
-    for subkey, entry in reg_keys:
-        reg_path = get_registry_path(subkey, entry, submod.test_game_path)
-        if reg_path:
-            return [reg_path]
-    return []
+    return _find_registry_games(submod, submod.gog_registry_keys)
+
+def get_disc_game_paths(submod, found_steam_paths, found_gog_paths):
+    """Check registry for the disc versions of older games."""
+    found_games = _find_registry_games(submod, submod.disc_registry_keys)
+    # The Steam and GOG versions set the same registry key as the disc version,
+    # so avoid showing it twice
+    return [g for g in found_games
+            if g not in found_steam_paths and g not in found_gog_paths]
 
 def get_legacy_ws_game_info(submod):
     """Get all information about a legacy Windows Store application."""

Mopy/bash/game/__init__.py
@@ -160,6 +160,8 @@ class GameInfo(object):
     #  HKCU\Software\Wow6432Node
     # Example: [(r'Bethesda Softworks\Oblivion', 'Installed Path')]
     gog_registry_keys = []
+    # Same as above, but for the old disc versions of games
+    disc_registry_keys = []
     # URL to the Nexus site for this game
     nexusUrl = u''   # URL
     nexusName = u''  # Long Name

Mopy/bash/game/fallout3/__init__.py
@@ -24,7 +24,8 @@ from os.path import join as _j
 
 from .. import WS_COMMON_FILES, GameInfo
 from ..patch_game import PatchGame
-from ..store_mixins import EGSMixin, GOGMixin, SteamMixin, WindowsStoreMixin
+from ..store_mixins import DiscMixin, EGSMixin, GOGMixin, SteamMixin, \
+    WindowsStoreMixin
 from ...bolt import DefaultFNDict, FName, classproperty
 
 _GOG_IDS = [1454315831]
@@ -1256,6 +1257,10 @@ class EGSFallout3GameInfo(EGSMixin, AFallout3GameInfo):
         egs_app_names = ['adeae8bbfc94427db57c7dfecce3f1d4']
         egs_language_dirs = FO3_LANG_DIRS
 
+class DiscFallout3GameInfo(DiscMixin, AFallout3GameInfo):
+    """GameInfo override for the disc version of Fallout 3."""
+    _disc_subkey = 'Fallout3'
+
 class GOGFallout3GameInfo(GOGMixin, AFallout3GameInfo):
     """GameInfo override for the GOG version of Fallout 3."""
     _gog_game_ids = _GOG_IDS
@@ -1279,5 +1284,5 @@ class WSFallout3GameInfo(WindowsStoreMixin, AFallout3GameInfo):
         ws_language_dirs = FO3_LANG_DIRS
 
 GAME_TYPE = {g.unique_display_name: g for g in (
-    EGSFallout3GameInfo, GOGFallout3GameInfo, SteamFallout3GameInfo,
-    WSFallout3GameInfo)}
+    DiscFallout3GameInfo, EGSFallout3GameInfo, GOGFallout3GameInfo,
+    SteamFallout3GameInfo, WSFallout3GameInfo)}

Mopy/bash/game/morrowind/__init__.py
@@ -24,7 +24,7 @@ import struct as _struct
 
 from .. import WS_COMMON_FILES, GameInfo
 from ..patch_game import PatchGame
-from ..store_mixins import GOGMixin, SteamMixin, WindowsStoreMixin
+from ..store_mixins import DiscMixin, GOGMixin, SteamMixin, WindowsStoreMixin
 from ... import bolt
 
 _GOG_IDS = [
@@ -152,6 +152,12 @@ class _AMorrowindGameInfo(PatchGame):
         sub.sub_header_size = 8
         cls._import_records(__name__)
 
+class DiscMorrowindGameInfo(DiscMixin, _AMorrowindGameInfo):
+    """GameInfo override for the disc version of Morrowind."""
+    # This is according to xEdit's sources, but I've never seen it make that
+    # registry key
+    _disc_subkey = 'Morrowind'
+
 class GOGMorrowindGameInfo(GOGMixin, _AMorrowindGameInfo):
     """GameInfo override for the GOG version of Morrowind."""
     _gog_game_ids = _GOG_IDS
@@ -174,4 +180,5 @@ class WSMorrowindGameInfo(WindowsStoreMixin, _AMorrowindGameInfo):
                             'Morrowind GOTY German']
 
 GAME_TYPE = {g.unique_display_name: g for g in (
-    GOGMorrowindGameInfo, SteamMorrowindGameInfo, WSMorrowindGameInfo)}
+    DiscMorrowindGameInfo, GOGMorrowindGameInfo, SteamMorrowindGameInfo,
+    WSMorrowindGameInfo)}

Mopy/bash/game/oblivion/__init__.py
@@ -24,7 +24,7 @@ from os.path import join as _j
 
 from .. import WS_COMMON_FILES, GameInfo
 from ..patch_game import PatchGame
-from ..store_mixins import GOGMixin, SteamMixin, WindowsStoreMixin
+from ..store_mixins import DiscMixin, GOGMixin, SteamMixin, WindowsStoreMixin
 from ... import bolt
 
 _GOG_IDS = [
@@ -1235,6 +1235,10 @@ class AOblivionGameInfo(PatchGame):
         # in Oblivion we get them all except the TES4 record
         cls.mergeable_sigs = {*cls.top_groups, *_brec.RecordType.nested_to_top}
 
+class DiscOblivionGameInfo(DiscMixin, AOblivionGameInfo):
+    """GameInfo override for the disc version of Oblivion."""
+    _disc_subkey = 'Oblivion'
+
 class GOGOblivionGameInfo(GOGMixin, AOblivionGameInfo):
     """GameInfo override for the GOG version of Oblivion."""
     _gog_game_ids = _GOG_IDS
@@ -1264,4 +1268,5 @@ class WSOblivionGameInfo(WindowsStoreMixin, AOblivionGameInfo):
                             'Oblivion GOTY Spanish']
 
 GAME_TYPE = {g.unique_display_name: g for g in (
-    GOGOblivionGameInfo, SteamOblivionGameInfo, WSOblivionGameInfo)}
+    DiscOblivionGameInfo, GOGOblivionGameInfo, SteamOblivionGameInfo,
+    WSOblivionGameInfo)}

Mopy/bash/game/skyrimse/__init__.py
@@ -337,7 +337,6 @@ class ASkyrimSEGameInfo(ASkyrimGameInfo):
 
 class EGSSkyrimSEGameInfo(EGSMixin, ASkyrimSEGameInfo):
     """GameInfo override for the Epic Games Store version of Skyrim SE."""
-    unique_display_name = f'{ASkyrimSEGameInfo.display_name} (EGS)'
     my_games_name = 'Skyrim Special Edition EPIC'
     appdata_name = 'Skyrim Special Edition EPIC'
 

Mopy/bash/game/store_mixins.py
@@ -25,6 +25,18 @@ various stores."""
 from . import GameInfo, WS_COMMON_FILES
 from ..bolt import classproperty
 
+class DiscMixin(GameInfo):
+    """Mixin for variants of games that are installed via retail discs."""
+    _disc_subkey: str
+
+    @classproperty
+    def disc_registry_keys(cls):
+        return [(fr'Bethesda Softworks\{cls._disc_subkey}', 'Installed Path')]
+
+    @classproperty
+    def unique_display_name(cls):
+        return f'{cls.display_name} (Disc)'
+
 class EGSMixin(GameInfo):
     """Mixin for variants of games that are installed via the Epic Games
     Store."""