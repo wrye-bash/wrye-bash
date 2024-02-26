wrye-bash/wrye-bash

commit af11b461ee0b5d4ace54acef8822ca1841762490

Re-add compat code for bash.tabs.order:
Removed in 5e1acb6ac3417f2541049adb3ffc378155b2eb40 seems needed till
this day:

Traceback (most recent call last):
  File "bash\bash.py", line 425, in main
  File "bash\bash.py", line 573, in _main
  File "bash\basher\__init__.py", line 4349, in Init
  File "bash\basher\__init__.py", line 3928, in __init__
  File "bash\basher\__init__.py", line 3798, in __init__
TypeError: list indices must be integers or slices, not str

Rest is some nit from debugging refreshTracked:

Remove mismatchedEspms

No uses - this will add some tiny performance to refreshStatus - some
borderline EAFP

Renames

Iteration variables like this should have the same name - one less
variable in the debugger.

Some more EAFP

Nit is under #219.

Mopy/bash/bosh/bain.py
@@ -140,9 +140,9 @@ class Installer(ListInfo):
         'blockSize', 'overrideSkips', '_remaps', 'skipRefresh', 'fileRootIdex')
     volatile = ( # used when copying the installer do *not* add _file_key here
         'ci_dest_sizeCrc', 'skipExtFiles', 'skipDirFiles', 'status',
-        'missingFiles', 'mismatchedFiles', 'project_refreshed',
-        'mismatchedEspms', 'unSize', 'espms', 'underrides', 'hasWizard',
-        'espmMap', 'hasReadme', 'hasBCF', 'hasBethFiles', 'has_fomod_conf')
+        'missingFiles', 'mismatchedFiles', 'project_refreshed', 'unSize',
+        'espms', 'underrides', 'hasWizard', 'espmMap', 'hasReadme', 'hasBCF',
+        'hasBethFiles', 'has_fomod_conf')
 
     #--Package analysis/porting.
     type_string = _('Unrecognized')
@@ -305,7 +305,6 @@ class Installer(ListInfo):
         self.underrides = set()
         self.missingFiles = set()
         self.mismatchedFiles = set()
-        self.mismatchedEspms = set()
 
     @property
     def num_of_files(self): return len(self.fileSizeCrcs)
@@ -1107,8 +1106,9 @@ class Installer(ListInfo):
     def refreshStatus(self, installersData):
         """Updates missingFiles, mismatchedFiles and status.
         Status:
-        20: installed (green)
-        10: mismatches (yellow)
+        30: installed (green)
+        20: mismatches (yellow)
+        10: mismatches (orange)
         0: unconfigured (white)
         -10: missing files (red)
         -20: bad type (grey)
@@ -1118,12 +1118,10 @@ class Installer(ListInfo):
         ci_underrides_sizeCrc = installersData.ci_underrides_sizeCrc
         missing = self.missingFiles
         mismatched = self.mismatchedFiles
-        misEspmed = self.mismatchedEspms
         underrides = set()
         status = 0
         missing.clear()
         mismatched.clear()
-        misEspmed.clear()
         if not self.has_recognized_structure:
             status = -20
         elif data_sizeCrc:
@@ -1133,12 +1131,11 @@ class Installer(ListInfo):
                     missing.add(filename)
                 elif sizeCrc != sizeCrcDate[:2]:
                     mismatched.add(filename)
-                    if ModInfos.rightFileType(filename):
-                        misEspmed.add(filename)
                 if sizeCrc == ci_underrides_sizeCrc.get(filename):
                     underrides.add(filename)
             if missing: status = -10
-            elif misEspmed: status = 10
+            elif any(ModInfos.rightFileType(f) for f in mismatched):
+                status = 10
             elif mismatched: status = 20
             else: status = 30
         #--Clean Dirty
@@ -1150,9 +1147,9 @@ class Installer(ListInfo):
                 ):
                 del dirty_sizeCrc[filename]
         #--Done
-        (self.status,oldStatus) = (status,self.status)
-        (self.underrides,oldUnderrides) = (underrides,self.underrides)
-        return self.status != oldStatus or self.underrides != oldUnderrides
+        changed = self.status != status or self.underrides != underrides
+        self.status, self.underrides = status, underrides
+        return changed
 
     #--Utility methods --------------------------------------------------------
     def packToArchive(self, project, fn_archive, isSolid, blockSize,
@@ -2504,13 +2501,13 @@ class InstallersData(DataStore):
             InstallersData._externally_updated)
         InstallersData._externally_updated.clear()
         InstallersData._externally_deleted.clear()
-        for abspath, tracked in list(InstallersData._miscTrackedFiles.items()):
+        for apath, tracked in list(InstallersData._miscTrackedFiles.items()):
             try:
                 if tracked.do_update(raise_on_error=True):
-                    altered.add(abspath)
+                    altered.add(apath)
             except OSError: # untrack - runs on first run !!
-                InstallersData._miscTrackedFiles.pop(abspath, None)
-                del_paths.add(abspath)
+                InstallersData._miscTrackedFiles.pop(apath, None)
+                del_paths.add(apath)
         do_refresh = False
         for apath in altered | del_paths:
             # the Data dir - will give correct relative path for both
@@ -2556,9 +2553,9 @@ class InstallersData(DataStore):
                 and os.path.split(x)[0].lower() != u'ini tweaks')
         for relPath in dest_files:
             try:
-                oldCrc = self.data_sizeCrcDate.get(relPath)[1]
-                newCrc = installer.ci_dest_sizeCrc.get(relPath)[1]
-            except TypeError: # get returned None
+                oldCrc = self.data_sizeCrcDate[relPath][1]
+                newCrc = installer.ci_dest_sizeCrc[relPath][1]
+            except KeyError:
                 continue
             if newCrc == oldCrc: continue
             iniAbsDataPath = bass.dirs[u'mods'].join(relPath)
@@ -2757,25 +2754,25 @@ class InstallersData(DataStore):
 
     def __restore(self, installer, removes, restores, cede_ownership):
         """Populate restores dict with files to be restored by this
-        installer, removing those from removes. In case a mod or ini belongs
-        to another package, we must make sure we cede ownership, even if the
-        mod or ini is not restored (restore takes care of that).
-
-        Returns all of the files this installer would install. Used by
-        'bain_uninstall' and 'bain_anneal'."""
+        installer, removing those from removes. Used by 'bain_uninstall' and
+        'bain_anneal'. In case a mod or ini belongs to another package,
+        we must make sure we cede ownership, even if the mod or ini is not
+        restored (restore takes care of that). Return *all* the files this
+        installer would install."""
         # get all destination files for this installer
-        files = set(installer.ci_dest_sizeCrc)
+        dest_sc = installer.ci_dest_sizeCrc
         # keep those to be removed while not restored by a higher order package
-        to_keep = (removes & files) - set(restores)
+        to_keep = (removes & dest_sc.keys()) - restores.keys()
         for ci_dest in to_keep:
-            if installer.ci_dest_sizeCrc[ci_dest] != \
-                    self.data_sizeCrcDate.get(ci_dest,(0, 0, 0))[:2]:
-                # restore it from this installer
-                restores[ci_dest] = installer.fn_key
-            else: # don't mind the FName(str()) below - done seldom
-                cede_ownership[installer.fn_key].add(FName(str(ci_dest)))
             removes.discard(ci_dest) # don't remove it anyway
-        return files
+            try: # restore it from this installer?
+                if dest_sc[ci_dest] != self.data_sizeCrcDate[ci_dest][:2]:
+                    restores[ci_dest] = installer.fn_key
+                    continue
+            except KeyError: pass
+            # don't mind the FName(str()) below - done seldom
+            cede_ownership[installer.fn_key].add(FName(str(ci_dest)))
+        return set(dest_sc)
 
     def bain_uninstall_all(self, refresh_ui, progress=None):
         """Uninstall all present packages."""
@@ -2787,7 +2784,6 @@ class InstallersData(DataStore):
             progress)
 
     def _do_uninstall(self, unArchives, refresh_ui, progress):
-        data_sizeCrcDate = self.data_sizeCrcDate
         #--Determine files to remove and files to restore. Keep in mind that
         #  multiple input archives may be interspersed with other archives that
         #  may block (mask) them from deleting files and/or may provide files
@@ -2803,10 +2799,13 @@ class InstallersData(DataStore):
             #--Uninstall archive?
             if installer in unArchives:
                 for data_sizeCrc in (installer.ci_dest_sizeCrc,installer.dirty_sizeCrc):
-                    for cistr_file,sizeCrc in data_sizeCrc.items():
-                        sizeCrcDate = data_sizeCrcDate.get(cistr_file)
-                        if cistr_file not in masked and sizeCrcDate and sizeCrcDate[:2] == sizeCrc:
-                            removes.add(cistr_file)
+                    for ci_file, sizeCrc in data_sizeCrc.items():
+                        try:
+                            if ci_file not in masked and self.data_sizeCrcDate[
+                                    ci_file][:2] == sizeCrc:
+                                removes.add(ci_file)
+                        except KeyError:
+                            pass
             #--Other active archive. May undo previous removes, or provide a restore file.
             #  And/or may block later uninstalls.
             elif installer.is_active:
@@ -2852,9 +2851,8 @@ class InstallersData(DataStore):
         for index, (fn_inst, destFiles) in enumerate(installer_destinations):
             progress(index, fn_inst)
             if destFiles:
-                installer = self[fn_inst]
                 refresh_ui.update(self._installer_install(
-                    installer, destFiles, index, progress))
+                    self[fn_inst], destFiles, index, progress))
 
     def bain_anneal(self, anPackages, refresh_ui, progress=None):
         """Anneal selected packages. If no packages are selected, anneal all.

Mopy/bash/basher/__init__.py
@@ -2547,8 +2547,8 @@ class InstallersList(UIList):
     #--DnD
     _dndList, _dndFiles, _dndColumns = True, True, [u'Order']
     #--GUI
-    _status_color = {-20: u'grey', -10: u'red', 0: u'white', 10: u'orange',
-                     20: u'yellow', 30: u'green'}
+    _status_color = {-20: 'grey', -10: 'red', 0: 'white', 10: 'orange',
+                     20: 'yellow', 30: 'green'}
 
     @fast_cached_property
     def icons(self):
@@ -3174,9 +3174,8 @@ class InstallersDetails(_SashDetailsPanel):
                            '== ' + _('Configured Files'))])
             gPage.text_content = u'\n'.join(inf_)
         elif pageName == u'gMatched':
-            gPage.text_content = _dumpFiles(set(
-                installer.ci_dest_sizeCrc) - installer.missingFiles -
-                                           installer.mismatchedFiles)
+            gPage.text_content = _dumpFiles(installer.ci_dest_sizeCrc.keys() -
+                installer.missingFiles - installer.mismatchedFiles)
         elif pageName == u'gMissing':
             gPage.text_content = _dumpFiles(installer.missingFiles)
         elif pageName == u'gMismatched':
@@ -3789,6 +3788,13 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         # default tabs order and default enabled state, keys as in tabInfo
         tabs_enabled_ordered = dict(e.value for e in Store)
         newOrder = settings.get('bash.tabs.order', tabs_enabled_ordered)
+        if not isinstance(newOrder, dict): # convert, on updating to 306 ##: still needed
+            enabled = settings.get('bash.tabs',  # deprecated - never use
+                                   tabs_enabled_ordered)
+            newOrder = {x: enabled[x] for x in newOrder
+            # needed if user updates to 306+ that drops 'bash.tabs', the latter
+            # is unchanged from default and the new version also removes a tab
+            if x in enabled}
         # append any new tabs - appends last
         newTabs = set(tabInfo) - set(newOrder)
         for n in newTabs: newOrder[n] = tabs_enabled_ordered[n]

Mopy/bash/gui/_gui_globals.py
@@ -38,7 +38,6 @@ def init_image_resources(images_dir):
     _image_resource_dir = images_dir
     if not os.path.isdir(images_dir): # CI Hack we could move to caller or add a param
         _image_resource_dir = _Path.getcwd().join('Mopy', 'bash', 'images')
-    arrows = {}
     from .images import GuiImage
     def _icc(fname, bm_px_size=16):
         """Creates an Image wrapper.
@@ -48,6 +47,7 @@ def init_image_resources(images_dir):
             device-independent pixels (DIP)."""
         return GuiImage.from_path(fname, iconSize=bm_px_size)
     # Up/Down arrows for UIList columns
+    arrows = {}
     for arr in ['up', 'down']:
         arrows[f'arrow.{arr}.16'] = _icc(f'arrow_{arr}.svg')
     # collect the installer icons
@@ -69,7 +69,7 @@ def init_image_resources(images_dir):
                 f'diamond_{col}_{img_st}_wiz.png')
     _installer_icons['corrupt'] = _icc('red_x.svg')
     _gui_images.update(_installer_icons)
-     # collect color checks for the rest of the UILists
+    # collect color checks for the rest of the UILists
     _color_checks = dict(arrows)
     for st in ['imp', 'inc', 'off', 'on']:
         for col in ('purple', 'blue', 'green', 'orange', 'yellow', 'red'):

commit 99e58d858f47a97f8ecc263568273f78ad13e8b1

bain: Fix Anneal regression
Broke in af11b461ee0b5d4ace54acef8822ca1841762490, where a small mistake
was made while refactoring: the get() call was used to give special
behavior to files missing from the Data folder. The refactored version
accidentally got rid of that.

Added comments to make this more understandable cause it took me a
minute (or ten). Also updated the docs to be clearer on what Anneal
actually *does*.

Mopy/bash/brec/basic_elements.py:
Fix regression in simple groups

Introduced in 53095bc3b47eac20f8ed1b44019e74e11eaca60f (where else).
Pretty sure this worked when I wrote it, but maybe I'm misremembering.
Anyway, we were getting bypassed entirely during dump plus lacked the
mel_sig necessary for _dump_bytes, so fix that.

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -927,10 +927,10 @@ tab. The directories skipped are:
         <tr>
             <td rowspan="7">Packages..</td>
             <td id="bainAnnealAll">Anneal All</td>
-            <td>Install any missing files for installed packages and correct
-                all install order errors. Identical to running
-                <a href="#bainAnneal">Anneal</a> on all installed
-                packages.</td>
+            <td>Update all installed packages to match their configuration,
+                installing changed and missing files and uninstalling removed
+                ones. Identical to running <a href="#bainAnneal">Anneal</a>
+                on all packages.</td>
         </tr>
         <tr>
             <td id="bainUninstallAll">Uninstall All Packages</td>
@@ -1355,7 +1355,15 @@ tab. The directories skipped are:
         </tr>
         <tr>
             <td colspan="3" id="bainAnneal">Anneal</td>
-            <td>Install missing files and correct install order errors.<br>
+            <td>Update an installed ("active") package to match changes in its
+                installation configuration or in the order of packages. If you
+                check a subpackage, or change the order of two conflicting
+                packages, for example, you will have to run this command.<br>
+                <br>
+                Technically, what this does is install any missing and changed
+                files according to the package's configuration (for active
+                packages only, plus see the note below) and uninstall all files
+                that have been removed from the package's configuration.<br>
                 <br>
                 Note: If an already installed file is changed outside of Wrye
                 Bash's control, Wrye Bash will not anneal that file. Anneal is

Mopy/Docs/Wrye Bash General Readme.html
@@ -834,8 +834,9 @@ from other packages previously overridden by the uninstalled package's
         <tr>
             <td rowspan="3">Packages..</td>
             <td id="bainAnnealAll">Anneal All</td>
-            <td>Install any missing files for active packages and correct
-                all install order errors.</td>
+            <td>Same as <a href="#bainAnneal">Anneal</a>, but runs on all
+                packages. Useful if you moved a lot of packages around or
+                changed some global BAIN settings, e.g. skips.</td>
         </tr>
         <tr>
             <td>Uninstall All Packages</td>
@@ -896,8 +897,10 @@ from other packages previously overridden by the uninstalled package's
         </tr>
         <tr>
             <td colspan="2" id="bainAnneal">Anneal</td>
-            <td>Install missing files and correct install order errors
-                according to the package configuration.</td>
+            <td>Update an installed ("active") package to match changes in its
+                installation configuration or in the order of packages. If you
+                check a subpackage, or change the order of two conflicting
+                packages, for example, you will have to run this command.</td>
         </tr>
         <tr>
             <td colspan="2">Quick Refresh</td>

Mopy/bash/bosh/bain.py
@@ -2742,14 +2742,24 @@ class InstallersData(DataStore):
         # keep those to be removed while not restored by a higher order package
         to_keep = (removes & dest_sc.keys()) - restores.keys()
         for ci_dest in to_keep:
-            removes.discard(ci_dest) # don't remove it anyway
-            try: # restore it from this installer?
-                if dest_sc[ci_dest] != self.data_sizeCrcDate[ci_dest][:2]:
+            # We don't want to remove it right now, only if no higher package
+            # has it either -> that's checked later
+            removes.discard(ci_dest)
+            try:
+                version_in_data = self.data_sizeCrcDate[ci_dest]
+            except KeyError:
+                # The file isn't present in the Data folder at all -> missing
+                # file, restore it from this package
+                restores[ci_dest] = installer.fn_key
+            else:
+                if installer.ci_dest_sizeCrc[ci_dest] != version_in_data[:2]:
+                    # This package has a different version than the one in the
+                    # Data folder, restore that one
                     restores[ci_dest] = installer.fn_key
-                    continue
-            except KeyError: pass
-            # don't mind the FName(str()) below - done seldom
-            cede_ownership[installer.fn_key].add(FName(str(ci_dest)))
+                else: # don't mind the FName(str()) below - done seldom
+                    # This package has the same version as the one in the Data
+                    # folder, so simply take ownership of the existing file
+                    cede_ownership[installer.fn_key].add(FName(str(ci_dest)))
         return set(dest_sc)
 
     def bain_uninstall_all(self, refresh_ui, progress=None):

Mopy/bash/brec/basic_elements.py
@@ -536,6 +536,7 @@ class MelSimpleGroups(MelGroups):
         if not isinstance(element, MelNum):
             raise SyntaxError(f'MelSimpleGroups only accepts MelNum, passed: '
                               f'{element!r}')
+        self.mel_sig = element.mel_sig
         self._element = element
         super().__init__(groups_attr, element)
 
@@ -550,8 +551,10 @@ class MelSimpleGroups(MelGroups):
             if save_fids:
                 setattr(record, self.attr, mapped)
 
-    def pack_subrecord_data(self, record):
-        return b''.join(map(self._element.packer, getattr(record, self.attr)))
+    def dumpData(self, record, out):
+        pack_el = self._element.packer
+        for target in getattr(record, self.attr):
+            self.packSub(out, pack_el(target))
 
 #------------------------------------------------------------------------------
 class MelString(MelBase):