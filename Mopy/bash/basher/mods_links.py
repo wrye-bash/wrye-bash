wrye-bash/wrye-bash

commit 907bdccaea6e1474a433813a70cdbcddad28ae76

Renames API beta:
- _get_rename_paths found its rightful place in ListInfo. Note how
much more natural the methods now are - we stopped passing the oldName
(that's now self) and looking us up so finally lines wrap in here. Still
_rename_operation return value (and name) is wip

Mopy/bash/bosh/__init__.py
@@ -180,6 +180,11 @@ class ListInfo(object):
     def get_store(cls):
         raise AbstractError(u'%s does not provide a data store' % type(cls))
 
+    def get_rename_paths(self, newName):
+        """Return possible paths this file's renaming might affect (possibly
+        omitting some that do not exist)."""
+        return [(self.abs_path, self.get_store().store_dir.join(newName))]
+
 class MasterInfo(object):
     """Slight abstraction over ModInfo that allows us to represent masters that
     are missing an active mod counterpart."""
@@ -443,6 +448,17 @@ class FileInfo(AFile, ListInfo):
         if self.dir.join(name_str).exists(): ##: check using file_infos?
             return _(u'File %s already exists.') % name_str, None
         return self.__class__.validate_filename_str(name_str)
+
+    def get_rename_paths(self, newName):
+        old_new_paths = super(FileInfo, self).get_rename_paths(newName)
+        info_backup_paths = self.all_backup_paths
+        # all_backup_paths will return the backup paths for this file and its
+        # satellites (like cosaves). Passing newName in it returns the rename
+        # destinations of the backup paths. Backup paths may not exist.
+        for b_path, new_b_path in izip(info_backup_paths(),
+                                       info_backup_paths(newName)):
+            old_new_paths.append((b_path, new_b_path))
+        return old_new_paths
 
 #------------------------------------------------------------------------------
 reBashTags = re.compile(u'{{ *BASH *:[^}]*}}\\s*\\n?',re.U)
@@ -998,6 +1014,12 @@ class ModInfo(FileInfo):
     def match_oblivion_re(self):
         return reOblivion.match(self.name.s)
 
+    def get_rename_paths(self, newName):
+        old_new_paths = super(ModInfo, self).get_rename_paths(newName)
+        if self.isGhost:
+            old_new_paths[0] = (self.abs_path, old_new_paths[0][1] + u'.ghost')
+        return old_new_paths
+
 # Deprecated/Obsolete Bash Tags -----------------------------------------------
 # Tags that have been removed from Wrye Bash and should be dropped from pickle
 # files
@@ -1370,6 +1392,16 @@ class SaveInfo(FileInfo):
             self.has_inaccurate_masters = xse_cosave is None or \
                 not xse_cosave.has_accurate_master_list(True)
 
+    def get_rename_paths(self, newName):
+        old_new_paths = super(SaveInfo, self).get_rename_paths(newName)
+        # super call added the backup paths but not the actual rename cosave
+        # paths inside the store_dir - add those only if they exist
+        old, new = old_new_paths[0] # HACK: (oldName.ess, newName.ess) abspaths
+        for co_type, co_file in self._co_saves.items():
+            old_new_paths.append((co_file.abs_path,
+                                  co_type.get_cosave_path(new)))
+        return old_new_paths
+
 #------------------------------------------------------------------------------
 class ScreenInfo(FileInfo):
     """Cached screenshot, stores a bitmap and refreshes it when its cache is
@@ -1420,18 +1452,13 @@ class DataStore(DataDict):
     def refresh(self): raise AbstractError
     def save(self): pass # for Screenshots
 
-    def _rename_operation(self, oldName, newName):
-        rename_paths = self._get_rename_paths(oldName, newName)
+    def _rename_operation(self, member_info, newName):
+        rename_paths = member_info.get_rename_paths(newName)
         for tup in rename_paths[1:]: # first rename path must always exist
             # if cosaves or backups do not exist shellMove fails!
             if not tup[0].exists(): rename_paths.remove(tup)
         env.shellMove(*list(izip(*rename_paths)))
-        return True
-
-    def _get_rename_paths(self, oldName, newName):
-        """Return possible paths this file's renaming might affect (possibly
-        omitting some that do not exist)."""
-        return [(self.store_dir.join(oldName), self.store_dir.join(newName))]
+        return rename_paths[0][0].tail
 
     @property
     def bash_dir(self):
@@ -1556,11 +1583,10 @@ class TableFileInfos(DataStore):
             del self.table[deleted]
         self.table.save()
 
-    def _rename_operation(self, oldName, newName):
+    def _rename_operation(self, member_info, newName):
         # Override to allow us to notify BAIN if necessary
-        self._notify_bain(renamed=dict(
-            self._get_rename_paths(oldName, newName)))
-        return super(TableFileInfos, self)._rename_operation(oldName, newName)
+        self._notify_bain(renamed=dict(member_info.get_rename_paths(newName)))
+        return super(TableFileInfos, self)._rename_operation(member_info, newName)
 
 class FileInfos(TableFileInfos):
     """Common superclass for mod, saves and bsa infos."""
@@ -1630,38 +1656,26 @@ class FileInfos(TableFileInfos):
             self.table.pop(name, None)
         return deleted
 
-    def _get_rename_paths(self, oldName, newName):
-        old_new_paths = super(
-            FileInfos, self)._get_rename_paths(oldName, newName)
-        info_backup_paths = self[oldName].all_backup_paths
-        # all_backup_paths will return the backup paths for this file and its
-        # satellites (like cosaves). Passing newName in it returns the rename
-        # destinations of the backup paths. Backup paths may not exist.
-        for b_path, new_b_path in izip(info_backup_paths(),
-                                       info_backup_paths(newName)):
-            old_new_paths.append((b_path, new_b_path))
-        return old_new_paths
-
     def _additional_deletes(self, fileInfo, toDelete):
         #--Backups
         toDelete.extend(fileInfo.all_backup_paths()) # will include cosave ones
 
     #--Rename
-    def _rename_operation(self, oldName, newName):
+    def _rename_operation(self, member_info, newName):
         """Renames member file from oldName to newName."""
         #--Update references
-        fileInfo = self[oldName]
         #--File system
-        res = super(FileInfos, self)._rename_operation(oldName, newName)
+        super(FileInfos, self)._rename_operation(member_info, newName)
+        old_key = member_info.name
         #--FileInfo
-        fileInfo.name = newName
-        fileInfo.abs_path = self.store_dir.join(newName)
+        member_info.name = newName
+        member_info.abs_path = self.store_dir.join(newName)
         #--FileInfos
-        self[newName] = self[oldName]
-        del self[oldName]
-        self.table.moveRow(oldName,newName)
+        self[newName] = member_info
+        del self[old_key]
+        self.table.moveRow(old_key, newName)
         # self[newName]._mark_unchanged() # not needed with shellMove !
-        return res
+        return old_key
 
     #--Move
     def move_info(self, fileName, destDir):
@@ -2828,28 +2842,20 @@ class ModInfos(FileInfos):
         self._lo_wip = [x for x in self._lo_wip if x not in to_remove]
         self._active_wip  = [x for x in self._active_wip if x not in to_remove]
 
-    def _rename_operation(self, oldName, newName):
+    def _rename_operation(self, member_info, newName):
         """Renames member file from oldName to newName."""
-        isSelected = load_order.cached_is_active(oldName)
+        isSelected = load_order.cached_is_active(member_info.name)
         if isSelected:
-            self.lo_deactivate(oldName, doSave=False) # will save later
-        res = super(ModInfos, self)._rename_operation(oldName, newName)
+            self.lo_deactivate(member_info, doSave=False) # will save later
+        res = super(ModInfos, self)._rename_operation(member_info, newName)
         # rename in load order caches
-        oldIndex = self._lo_wip.index(oldName)
-        self._lo_caches_remove_mods([oldName])
+        oldIndex = self._lo_wip.index(res)
+        self._lo_caches_remove_mods([res])
         self._lo_wip.insert(oldIndex, newName)
         if isSelected: self.lo_activate(newName, doSave=False)
         # Save to disc (load order and plugins.txt)
         self.cached_lo_save_all()
         return res
-
-    def _get_rename_paths(self, oldName, newName):
-        old_new_paths = super(
-            ModInfos, self)._get_rename_paths(oldName, newName)
-        if self[oldName].isGhost:
-            old_new_paths[0] = (self[oldName].abs_path,
-                                old_new_paths[0][1] + u'.ghost')
-        return old_new_paths
 
     #--Delete
     def files_to_delete(self, filenames, **kwargs):
@@ -2975,7 +2981,7 @@ class ModInfos(FileInfos):
         rename_operation = super(ModInfos, self)._rename_operation
         while True:
             try:
-                rename_operation(self.masterName, oldName)
+                rename_operation(baseInfo, oldName)
                 break
             except OSError as werr: # can only occur if SHFileOperation
                 # isn't called, yak - file operation API badly needed
@@ -2987,7 +2993,7 @@ class ModInfos(FileInfos):
                 return
         while True:
             try:
-                rename_operation(newName, self.masterName)
+                rename_operation(newInfo, self.masterName)
                 break
             except OSError as werr:
                 if werr.errno == errno.EACCES and self._retry(
@@ -3113,10 +3119,10 @@ class SaveInfos(FileInfos):
         if not booting: self._refreshLocalSave() # otherwise we just did this
         return refresh_infos and FileInfos.refresh(self, booting=booting)
 
-    def _rename_operation(self, oldName, newName):
+    def _rename_operation(self, member_info, newName):
         """Renames member file from oldName to newName, update also cosave
         instance names."""
-        res = super(SaveInfos, self)._rename_operation(oldName, newName)
+        res = super(SaveInfos, self)._rename_operation(member_info, newName)
         for co_type, co_file in self[newName]._co_saves.items():
             co_file.abs_path = co_type.get_cosave_path(self[newName].abs_path)
         return res
@@ -3127,17 +3133,6 @@ class SaveInfos(FileInfos):
             x.abs_path for x in fileInfo._co_saves.values())
         # now add backups and cosaves backups
         super(SaveInfos, self)._additional_deletes(fileInfo, toDelete)
-
-    def _get_rename_paths(self, oldName, newName):
-        old_new_paths = super(
-            SaveInfos, self)._get_rename_paths(oldName, newName)
-        # super call added the backup paths but not the actual rename cosave
-        # paths inside the store_dir - add those only if they exist
-        old, new = old_new_paths[0] # HACK: (oldName.ess, newName.ess) abspaths
-        for co_type, co_file in self[oldName]._co_saves.items():
-            old_new_paths.append((co_file.abs_path,
-                                  co_type.get_cosave_path(new)))
-        return old_new_paths
 
     def copy_info(self, fileName, destDir, destName=empty_path, set_mtime=None):
         """Copies savefile and associated cosaves file(s)."""

Mopy/bash/balt.py
@@ -1250,7 +1250,7 @@ class UIList(wx.Panel):
         if not newPath.exists():
             key = GPath(u'%s' % info) # fixme hack cause Installers and File infos have different attrs
             try:
-                result = self.data_store._rename_operation(key, newFileName)
+                result = self.data_store._rename_operation(info, newFileName)
                 if to_select: to_select.add(newFileName)
                 if to_del: to_del.add(key)
                 if item_edited and key == item_edited[0]:
@@ -1264,8 +1264,7 @@ class UIList(wx.Panel):
                 # With shellMove I mostly get CancelError so below not needed -
                 # except if a save is locked and user presses Skip - so cosaves are
                 # renamed! Error handling is still a WIP
-                for old, new in self.data_store._get_rename_paths(key,
-                                                                  newFileName):
+                for old, new in info.get_rename_paths(newFileName):
                     if old == new: continue
                     if new.exists() and not old.exists():
                         # some cosave move failed, restore files

Mopy/bash/bosh/bain.py
@@ -1301,7 +1301,7 @@ class InstallerMarker(Installer):
 
     def renameInstaller(self, name_new, idata_):
         archive = GPath(self.archive)
-        if name_new == archive:
+        if name_new == archive: # TODO disallows case sensitive renames ??
             return False, False, False
         #--Add the marker to Bash and remove old one
         self.archive = name_new.s
@@ -1809,8 +1809,8 @@ class InstallersData(DataStore):
             self.converters_data.save()
             self.hasChanged = False
 
-    def _rename_operation(self, oldName, newName):
-        return self[oldName].renameInstaller(newName, self)
+    def _rename_operation(self, member_info, newName):
+        return member_info.renameInstaller(newName, self)
 
     #--Dict Functions ---------------------------------------------------------
     def files_to_delete(self, filenames, **kwargs):

commit 6e746f4b649944b834abb9f52f110166c5d9301c

Refactor setOblivionVersion:
Oblivion esm swapping is one of the most arcane pieces of the FileInfos
machinery. Specification lost in Oblivion, file operations (#241) -
reported heisenbug stuff. Note size_esm_version[oldSize] was outside of
the KeyError try/except - some of this could be converted to EAFP? Some
of it moved to bush.game? What exactly needs be refreshed? Should we
disallow activating the modding esms or does not make sense/it's too
complex (_game_lo is actively refactored currently)? Black magic, I
reduced the repetition in the logic - this made me notice a bug in
error handling probably introduced in
907bdccaea6e1474a433813a70cdbcddad28ae76 (I did not change the
parameters to infos in the error handler). The three next commits fix
more bugs and clean the logic up a bit but this needs handling of
store_dir to be finalized - see table merge. I absorbed the helper so
this is as few lines as possible.
Mopy/bash/basher/mods_links.py: disable the menu if it won't do anything

Mopy/bash/basher/mods_links.py
@@ -267,12 +267,11 @@ class _Mods_SetOblivionVersion(CheckLink, EnabledLink):
     def _check(self): return bosh.modInfos.voCurrent == self._version_key
 
     def _enable(self):
-        return bosh.modInfos.voCurrent is not None \
-               and self._version_key in bosh.modInfos.voAvailable
+        return bosh.modInfos.voCurrent is not None and not self._check() and \
+            self._version_key in bosh.modInfos.voAvailable
 
     def Execute(self):
         """Handle selection."""
-        if bosh.modInfos.voCurrent == self._version_key: return
         bosh.modInfos.setOblivionVersion(self._version_key, askYes)
         ##: Why refresh saves? Saves should only ever depend on Oblivion.esm,
         # not any of the modding ESMs. Maybe we should enforce that those

Mopy/bash/bosh/__init__.py
@@ -3187,20 +3187,11 @@ class ModInfos(TableFileInfos):
         return self[fileName].get_version() if fileName in self else ''
 
     #--Oblivion 1.1/SI Swapping -----------------------------------------------
-    def _retry(self, old, new, ask_yes):  ##: we should check *before* writing the patch
-        m = [_('Wrye Bash encountered an error when renaming %(old)s to '
-               '%(new)s.'),
-             '', '',
-             _('The file is in use by another process such as '
-               '%(xedit_name)s.'),
-             '',
-             _('Please close the other program that is accessing %(new)s.'),
-             '', '',
-             _('Try again?')]
-        msg = '\n'.join(m) % {'xedit_name': bush.game.Xe.full_name,
-                              'old': old, 'new': new}
-        return ask_yes(self, msg, title=_('File in Use'))
-
+    _retry_msg = [_('Wrye Bash encountered an error when renaming %(old)s to '
+                    '%(new)s.'), '', '',
+        _('The file is in use by another process such as %(xedit_name)s.'), '',
+        _('Please close the other program that is accessing %(new)s.'), '', '',
+        _('Try again?')]
     def setOblivionVersion(self, newVersion, ask_yes):
         """Swaps Oblivion.esm to specified version."""
         baseName = self._master_esm # Oblivion.esm, say it's currently SI one
@@ -3209,8 +3200,8 @@ class ModInfos(TableFileInfos):
         newSize = bush.game.modding_esm_size[newName]
         oldSize = self[baseName].fsize
         if newSize == oldSize: return
-        current_version = bush.game.size_esm_version[oldSize]
         try: # for instance: Oblivion_SI.esm, we rename Oblivion.esm to this
+            current_version = bush.game.size_esm_version[oldSize]
             oldName = FName(f'{fnb}_{current_version}.esm')
         except KeyError:
             raise StateError("Can't match current main ESM to known version.")
@@ -3227,32 +3218,26 @@ class ModInfos(TableFileInfos):
         is_new_info_active = load_order.cached_is_active(newName)
         # can't use ModInfos rename because it will mess up the load order
         file_info_rename_op = super(ModInfos, self).rename_operation
-        while True:
-            try:
-                file_info_rename_op(baseInfo, oldName)
-                break
-            except PermissionError: ##: can only occur if SHFileOperation
-                # isn't called, yak - file operation API badly needed
-                if self._retry(baseInfo.abs_path,
-                        self.store_dir.join(oldName), ask_yes):
-                    continue
-                raise
-            except CancelError:
-                return
-        while True:
-            try:
-                file_info_rename_op(newInfo, self._master_esm)
-                break
-            except PermissionError:
-                if self._retry(newInfo.abs_path, baseInfo.abs_path, ask_yes):
-                    continue
-                #Undo any changes
-                file_info_rename_op(oldName, self._master_esm)
-                raise
-            except CancelError:
-                #Undo any changes
-                file_info_rename_op(oldName, self._master_esm)
-                return
+        rename_args = {baseInfo: baseInfo.abs_path,
+                       oldName: self.store_dir.join(oldName)}, {
+            newInfo: newInfo.abs_path, self._master_esm: baseInfo.abs_path}
+        for do_undo, args_dict in enumerate(rename_args):
+            while True:
+                try:
+                    file_info_rename_op(*args_dict)
+                    break
+                except PermissionError: ##: can only occur if SHFileOperation
+                    # isn't called, yak - file operation API badly needed
+                    old, new = args_dict.values()
+                    msg = '\n'.join(self._retry_msg) % {'old': old, 'new': new,
+                        'xedit_name': bush.game.Xe.full_name, }
+                    if ask_yes(self, msg, title=_('File in Use')):
+                        continue
+                    if do_undo: file_info_rename_op(self[oldName], self._master_esm)
+                    raise
+                except CancelError:
+                    if do_undo: file_info_rename_op(self[oldName], self._master_esm)
+                    return
         # set mtimes to previous respective values
         self[self._master_esm].setmtime(master_time)
         self[oldName].setmtime(new_info_time)