wrye-bash/wrye-bash

commit 5eecd360f407452cd0cba914766f87dc78933861

[RH]Plug in FName:
A rabbit hole in its habitat. The notes of an unfortunate engineer who
fell into the collapsing RH were reconstructed from the hole's radiation
and are presented below.

------------------------------------------------------------------------
Pruning .s needs rest of the branch

Explicit is better than implicit yes, but those .s were just *too*
explicit - too much visual clutter and the real implicitness was there
anyway so we don't loose anything, apart the .s

I especially like join(x.s for x in li) -> join(li)

Note comparison with self_installer used to be case insensitive in bosh.

GPath hunting

Mopy/bash/basher/mods_links.py:
Note (pl_name := FName(pl_name)) in bosh.modInfos - FNDict always calls
FName.__new__ - is it cheaper to not-FName before?

_games_lo:
- What I like about map(FName, ...) is that we avoid the iteration
variable - cleaner. Happens to be faster too (by a 10% in my machine -
even faster for bigger lists)
- note also str(modname, encoding) - I finally decided not to allow
bytes in FName constructor

loot_parser use FNDict - bye plugin_s :)

'name' is gone for good:
(?<!(os|sys|fileInfo|modInfo|saveInfo|mod_info|effect|file_info|_bsa_info|patchInfo|newInfo|self)\.)
actually plain 'name' search in py files yields a single result

    os_name = os.name

More FName in records code

ci_body and ci_soul - more .s / moving from name -> ci_key

[!]Mopy/bash/bosh/save_headers.py:

Note we find the length of the str while we should find the len of the
encoded str - this will be addressed as part of the 480 refactoring
but _encode_masters does the right thing for now

Move __str__ to ListInfo / plugin to FileInfo

Finally the super slow ci_key property is gone along with self.name

Screen_ConvertTo: some tiny refactoring (lower not needed, just pass the
extension in lower case)

Drop GPath from patch config keys TTT ci_aliases

Mopy/bash/basher/patcher_dialog.py: strings should compare fine with
FName keys

Mopy/bash/basher/saves_links.py
@@ -33,7 +33,7 @@ from .dialogs import ImportFaceDialog
 from .. import bass, bosh, bolt, balt, bush, load_order, initialization
 from ..balt import EnabledLink, AppendableLink, Link, CheckLink, ChoiceLink, \
     ItemLink, SeparatorLink, OneItemLink, UIList_Rename
-from ..bolt import GPath, SubProgress, Path
+from ..bolt import GPath, SubProgress, Path, FName
 from ..bosh import faces, _saves
 from ..exception import ArgumentError, BoltError, ModError, AbstractError
 from ..gui import BusyCursor, ImageWrapper, FileSave
@@ -73,9 +73,7 @@ class Saves_ProfilesData(balt.ListEditorData):
     def getItemList(self):
         """Returns load list keys in alpha order."""
         #--Get list of directories in Hidden, but do not include default.
-        items = [x.s for x in initialization.getLocalSaveDirs()]
-        items.sort(key=lambda a: a.lower())
-        return items
+        return initialization.getLocalSaveDirs()
 
     #--Info box
     def getInfo(self,item):
@@ -146,7 +144,7 @@ class Saves_ProfilesData(balt.ListEditorData):
             return False
         #--Get file count. If > zero, verify with user.
         profileDir = bass.dirs[u'saveBase'].join(profileSaves)
-        files = [save_file for save_file in profileDir.list() if
+        files = [save_file for save_file in profileDir.ilist() if
                  bosh.SaveInfos.rightFileType(save_file)]
         if files:
             message = _(u'Delete profile %s and the %d save files it contains?') % (profile,len(files))
@@ -169,7 +167,7 @@ class Saves_Profiles(ChoiceLink):
     _my_games = GPath(_my_games)
 
     @property
-    def _choices(self): return [x.s for x in initialization.getLocalSaveDirs()]
+    def _choices(self): return initialization.getLocalSaveDirs()
 
     class _ProfileLink(CheckLink, EnabledLink):
         @property
@@ -278,7 +276,7 @@ class Save_ImportFace(OneItemLink):
                 saveFile = _saves.SaveFile(srcInfo)
                 saveFile.load(progress)
             srcFaces = faces.PCFaces.save_getFaces(saveFile)
-        elif bosh.ModInfos.rightFileType(srcPath): # Import from a mod
+        elif bosh.ModInfos.rightFileType(fname): # Import from a mod
             #--Get faces
             srcInfo = bosh.ModInfo(srcPath)
             srcFaces = faces.PCFaces.mod_getFaces(srcInfo)
@@ -360,11 +358,11 @@ class Save_DiffMasters(EnabledLink):
             message = u''
             if missing:
                 message += u'=== '+_(u'Removed Masters')+u' (%s):\n* ' % oldName
-                message += u'\n* '.join(x.s for x in load_order.get_ordered(missing))
+                message += u'\n* '.join(load_order.get_ordered(missing))
                 if added: message += u'\n\n'
             if added:
                 message += u'=== ' + _(u'Added Masters') + f' ({newName}):\n* '
-                message += u'\n* '.join(x.s for x in load_order.get_ordered(added))
+                message += u'\n* '.join(load_order.get_ordered(added))
             self._showWryeLog(message, title=_(u'Diff Masters'))
 
 #------------------------------------------------------------------------------
@@ -397,13 +395,12 @@ class Save_Renumber(EnabledLink):
         if newNumber is None: return
         old_names = set()
         new_names = set()
-        for old_file_path, maPattern, sinf in self._matches:
+        for fn_save, maPattern, sinf in self._matches:
             s_groups = maPattern.groups()
             if not s_groups[1]: continue
-            newFileName = f'{s_groups[0]}{newNumber:d}{s_groups[2]}'
-            if newFileName != old_file_path: # FIXME ci comp
-                new_file_path = GPath(newFileName)
-                if self.window.try_rename(sinf, new_file_path, new_names,
+            newFileName = FName(f'{s_groups[0]}{newNumber:d}{s_groups[2]}')
+            if newFileName != fn_save:
+                if self.window.try_rename(sinf, newFileName, new_names,
                                           old_names):
                     break
                 newNumber += 1
@@ -622,7 +619,7 @@ class Save_Move(ChoiceLink):
             u'Move save(s) to %s')
 
     @property
-    def _choices(self): return [x.s for x in initialization.getLocalSaveDirs()]
+    def _choices(self): return initialization.getLocalSaveDirs()
 
     def _initData(self, window, selection):
         super(Save_Move, self)._initData(window, selection)

Mopy/bash/bosh/save_headers.py
@@ -44,7 +44,7 @@ from ..bolt import decoder, cstrip, unpack_string, unpack_int, unpack_str8, \
     unpack_str_int_delim, unpack_str16_delim, unpack_str_byte_delim, \
     unpack_many, encode, struct_unpack, pack_int, pack_byte, pack_short, \
     pack_float, pack_string, pack_str8, pack_bzstr8, structs_cache, \
-    struct_error, remove_newlines, deprint
+    struct_error, remove_newlines, deprint, FName
 from ..exception import SaveHeaderError, AbstractError
 
 # Utilities -------------------------------------------------------------------
@@ -120,9 +120,8 @@ class SaveFileHeader(object):
             append_master(unpack_str8(ins))
 
     def _decode_masters(self):
-        self.masters = [bolt.GPath_no_norm(decoder(
-            x, bolt.pluginEncoding, avoidEncodings=(u'utf8', u'utf-8')))
-            for x in self.masters]
+        self.masters = [FName(decoder(x, bolt.pluginEncoding,
+            avoidEncodings=(u'utf8', u'utf-8'))) for x in self.masters]
 
     def calc_time(self): pass
 
@@ -172,10 +171,14 @@ class SaveFileHeader(object):
             oldMasters.append(unpack_str16(ins))
         #--Write new masters
         pack_byte(out, len(self.masters))
-        for master in self.masters:
-            pack_short(out, len(master))
-            out.write(encode(master.s))
+        self._encode_masters(out, self.masters)
         return oldMasters
+
+    def _encode_masters(self, out, masters_fns):
+        for fn_master in masters_fns:
+            encoded = encode(fn_master)
+            pack_short(out, len(encoded))
+            out.write(encoded)
 
     def _master_block_size(self):
         return 1 + sum(len(x) + 2 for x in self.masters)
@@ -232,7 +235,7 @@ class OblivionSaveHeader(SaveFileHeader):
     def __write_masters_ob(self, out):
         pack_byte(out, len(self.masters))
         for master in self.masters:
-            pack_str8(out, encode(master.s))
+            pack_str8(out, encode(master))
 
     def dump_header(self, out):
         out.write(self.__class__.save_magic)
@@ -354,12 +357,10 @@ class SkyrimSaveHeader(SaveFileHeader):
                                   f'not as expected ({mastersSize}).')
 
     def _decode_masters(self):
-        self.masters_regular = [bolt.GPath_no_norm(decoder(
-            x, bolt.pluginEncoding, avoidEncodings=(u'utf8', u'utf-8')))
-            for x in self.masters_regular]
-        self.masters_esl = [bolt.GPath_no_norm(decoder(
-            x, bolt.pluginEncoding, avoidEncodings=(u'utf8', u'utf-8')))
-            for x in self.masters_esl]
+        self.masters_regular = [FName(decoder(x, bolt.pluginEncoding,
+            avoidEncodings=(u'utf8', u'utf-8'))) for x in self.masters_regular]
+        self.masters_esl = [FName(decoder(x, bolt.pluginEncoding,
+            avoidEncodings=(u'utf8', u'utf-8'))) for x in self.masters_esl]
 
     def _sse_compress(self, to_compress):
         """Compresses the specified data using either LZ4 or zlib, depending on
@@ -523,14 +524,10 @@ class SkyrimSaveHeader(SaveFileHeader):
         # encode here, since we may be writing to BytesIO instead of a file
         num_regulars = len(regular_masters)
         pack_byte(out, num_regulars)
-        for master in self.masters[:num_regulars]:
-            pack_short(out, len(master))
-            out.write(encode(master.s))
+        self._encode_masters(out, self.masters[:num_regulars])
         if has_esl_block:
             pack_short(out, len(esl_masters))
-            for master in self.masters[num_regulars:]:
-                pack_short(out, len(master))
-                out.write(encode(master.s))
+            self._encode_masters(out, self.masters[num_regulars:])
         return regular_masters + esl_masters
 
     def _master_block_size(self):
@@ -647,7 +644,7 @@ class FalloutNVSaveHeader(SaveFileHeader):
         for master in self.masters:
             pack_short(out, len(master))
             _pack_c(out, b'|')
-            out.write(encode(master.s))
+            out.write(encode(master))
             _pack_c(out, b'|')
         return oldMasters
 

Mopy/bash/game/__init__.py
@@ -26,7 +26,6 @@ active game package as needed (currently the record and constants modules)
 and to set some brec.RecordHeader/MreRecord class variables."""
 
 import importlib
-from collections import defaultdict
 from itertools import chain
 from os.path import join as _j
 
@@ -89,7 +88,7 @@ class GameInfo(object):
     # and therefore needs a different file here).
     version_detect_file = u''
     # The main plugin Wrye Bash should look for
-    master_file = bolt.GPath_no_norm('')
+    master_file = bolt.FName('')
     # The directory in which mods and other data files reside. This is relative
     # to the game directory.
     mods_dir = u'Data'
@@ -333,7 +332,7 @@ class GameInfo(object):
         # time.mktime result due to locale issues. Generally used to redate
         # the vanilla BSAs before all mod BSAs, and all BSAs before loose
         # files by choosing dates older than the game's release date.
-        redate_dict = defaultdict(lambda: 1136066400) # '2006-01-01'
+        redate_dict = bolt.DefaultFNDict(lambda: 1136066400) # '2006-01-01'
         # All BSA versions accepted by this game. If empty, indicates that this
         # game does not use BSA versions and so BSA version checks will be
         # skipped entirely.

Mopy/bash/game/fallout3/__init__.py
@@ -22,7 +22,6 @@
 # =============================================================================
 """GameInfo override for Fallout 3."""
 
-from collections import defaultdict
 from os.path import join as _j
 
 from ..patch_game import GameInfo, PatchGame
@@ -42,7 +41,7 @@ class Fallout3GameInfo(PatchGame):
     launch_exe = u'Fallout3.exe'
     game_detect_includes = [u'Fallout3.exe']
     version_detect_file = u'Fallout3.exe'
-    master_file = bolt.GPath_no_norm(u'Fallout3.esm')
+    master_file = bolt.FName(u'Fallout3.esm')
     taglist_dir = u'Fallout3'
     loot_dir = u'Fallout3'
     boss_game_name = u'Fallout3'
@@ -93,7 +92,7 @@ class Fallout3GameInfo(PatchGame):
 
     class Bsa(GameInfo.Bsa):
         allow_reset_timestamps = True
-        redate_dict = defaultdict(lambda: 1136066400, { # '2006-01-01'
+        redate_dict = bolt.DefaultFNDict(lambda: 1136066400, { # '2006-01-01'
             'Fallout - MenuVoices.bsa': 1104530400,  # '2005-01-01',
             'Fallout - Meshes.bsa': 1104616800,      # '2005-01-02',
             'Fallout - Misc.bsa': 1104703200,        # '2005-01-03',

Mopy/bash/game/fallout4/__init__.py
@@ -41,7 +41,7 @@ class Fallout4GameInfo(PatchGame):
     game_detect_includes = [u'Fallout4.exe']
     game_detect_excludes = WS_COMMON
     version_detect_file = u'Fallout4.exe'
-    master_file = bolt.GPath_no_norm(u'Fallout4.esm')
+    master_file = bolt.FName(u'Fallout4.esm')
     taglist_dir = u'Fallout4'
     loot_dir = u'Fallout4'
     regInstallKeys = (u'Bethesda Softworks\\Fallout4', u'Installed Path')

Mopy/bash/game/fallout4vr/__init__.py
@@ -38,7 +38,7 @@ class Fallout4VRGameInfo(Fallout4GameInfo):
     launch_exe = u'Fallout4VR.exe'
     game_detect_includes = [u'Fallout4VR.exe']
     version_detect_file = u'Fallout4VR.exe'
-    master_file = bolt.GPath_no_norm(u'Fallout4.esm')
+    master_file = bolt.FName(u'Fallout4.esm')
     taglist_dir = 'Fallout4VR'
     loot_dir = u'Fallout4VR'
     regInstallKeys = (u'Bethesda Softworks\\Fallout 4 VR', u'Installed Path')

commit 4731feb4ce4abb7292d99e28588a86b073c3e273

More random fixups:
Fix for masterNames - was not always FNames it seems at least from what
I gather from Mopy/bash/bosh/save_headers.py - this might have led to
subtle bugs (difficult to track 'masters' (#480, #544) but seems an
FName was expected in masterNames). This brought to my attention that
finally masterOrder was only used locally which permitted me to drop
the attribute.

Mopy/bash/bosh/__init__.py: removed the break from move_infos -
introduced in 5eecd360f407452cd0cba914766f87dc78933861 looks like a bug.

Mopy/bash/basher/__init__.py: no ifs when you can repeat :P

Mopy/bash/basher/__init__.py
@@ -56,7 +56,7 @@ import time
 from collections import OrderedDict, defaultdict, namedtuple
 from collections.abc import Iterable
 from functools import partial
-from itertools import chain
+from itertools import chain, repeat
 
 import wx
 
@@ -529,9 +529,9 @@ class MasterList(_ModsUIList):
         all_esl_masters = (set(fileInfo.header.masters_esl) if can_have_esl
                            else None)
         all_master_sizes = (fileInfo.header.master_sizes if can_have_sizes
-                            else None)
-        for mi, ma_name in enumerate(fileInfo.masterNames):
-            ma_size = all_master_sizes[mi] if can_have_sizes else 0
+                            else repeat(0))
+        for mi, (ma_name, ma_size) in enumerate(
+                zip(fileInfo.masterNames, all_master_sizes)):
             ma_esl = can_have_esl and ma_name in all_esl_masters
             self.data_store[mi] = bosh.MasterInfo(parent_minf=fileInfo,
                 master_name=ma_name, master_size=ma_size, was_esl=ma_esl)

Mopy/bash/basher/saves_links.py
@@ -369,7 +369,7 @@ class Save_DiffMasters(EnabledLink):
         else:
             message = u''
             if missing:
-                message += u'=== '+_(u'Removed Masters')+u' (%s):\n* ' % oldName
+                message += '=== ' + _('Removed Masters') + f' ({oldName}):\n* '
                 message += u'\n* '.join(load_order.get_ordered(missing))
                 if added: message += u'\n\n'
             if added:

Mopy/bash/bosh/__init__.py
@@ -126,11 +126,12 @@ class MasterInfo:
     __slots__ = ('is_ghost', 'curr_name', 'mod_info', 'old_name',
                  'stored_size', '_was_esl', 'parent_mod_info')
 
-    def __init__(self, *, parent_minf, master_name, master_size, was_esl):
+    def __init__(self, *, parent_minf, master_name: FName, master_size,
+                 was_esl):
         self.parent_mod_info = parent_minf
         self.stored_size = master_size
         self._was_esl = was_esl
-        self.old_name = FName(master_name)
+        self.old_name = master_name
         self.mod_info = self.rename_if_present(master_name)
         if self.mod_info is None:
             self.curr_name = FName(master_name)
@@ -213,7 +214,6 @@ class FileInfo(AFileInfo):
     def __init__(self, fullpath, load_cache=False, **kwargs):
         self.header = None
         self.masterNames: tuple[FName, ...] = ()
-        self.masterOrder: tuple[FName, ...] = ()
         self.madeBackup = False
         # True if the masters for this file are not reliable
         self.has_inaccurate_masters = False
@@ -224,7 +224,6 @@ class FileInfo(AFileInfo):
     def _reset_masters(self):
         #--Master Names/Order
         self.masterNames = tuple(self._get_masters())
-        self.masterOrder = tuple() #--Reset to empty for now
 
     def _file_changed(self, stat_tuple):
         return (self.fsize, self.ftime, self.ctime) != stat_tuple
@@ -1041,15 +1040,14 @@ class ModInfo(FileInfo):
         return old_new_paths
 
     def _masters_order_status(self, status):
-        self.masterOrder = tuple(load_order.get_ordered(self.masterNames))
-        loads_before_its_masters = self.masterOrder and \
-                                   load_order.cached_lo_index(
-            self.masterOrder[-1]) > load_order.cached_lo_index(self.fn_key)
-        if self.masterOrder != self.masterNames and loads_before_its_masters:
+        mo = tuple(load_order.get_ordered(self.masterNames)) # masterOrder
+        loads_before_its_masters = mo and load_order.cached_lo_index(
+            mo[-1]) > load_order.cached_lo_index(self.fn_key)
+        if mo != self.masterNames and loads_before_its_masters:
             return 21
         elif loads_before_its_masters:
             return 20
-        elif self.masterOrder != self.masterNames:
+        elif mo != self.masterNames:
             return 10
         else:
             return status
@@ -1271,17 +1269,17 @@ class SaveInfo(FileInfo):
     def get_store(cls): return saveInfos
 
     def _masters_order_status(self, status):
-        self.masterOrder = tuple(load_order.get_ordered(self.masterNames))
-        if self.masterOrder != self.masterNames:
+        mo = tuple(load_order.get_ordered(self.masterNames))
+        if mo != self.masterNames:
             return 20 # Reordered masters are far more important in saves
         elif status > 0:
             # Missing or reordered masters -> orange or red
             return status
         active_tuple = load_order.cached_active_tuple()
-        if self.masterOrder == active_tuple:
+        if mo == active_tuple:
             # Exact match with LO -> purple
             return -20
-        if self.masterOrder == active_tuple[:len(self.masterOrder)]:
+        if mo == active_tuple[:len(mo)]:
             # Matches LO except for new plugins at the end -> blue
             return -10
         else:
@@ -1410,7 +1408,7 @@ class SaveInfo(FileInfo):
                     return [*map(FName, xse_cosave.get_master_list())]
         # Fall back on the regular masters - either the cosave is unnecessary,
         # doesn't exist or isn't accurate
-        return self.header.masters
+        return [*map(FName, self.header.masters)]
 
     def has_circular_masters(self, *, fake_masters: list[FName] | None = None):
         return False # Saves can't have circular masters
@@ -3436,7 +3434,6 @@ class SaveInfos(TableFileInfos):
             if FName(d.stail) in moved:
                 co_instances = SaveInfo.get_cosaves_for_path(s)
                 self.co_copy_or_move(co_instances, d, move_cosave=True)
-                break
         for m in moved:
             try:
                 self.new_info(m, notify_bain=True) ##: why True??

Mopy/bash/bosh/save_headers.py
@@ -36,6 +36,7 @@ import sys
 import zlib
 from enum import Enum
 from functools import partial
+from itertools import repeat
 
 import lz4.block
 
@@ -461,22 +462,19 @@ class _AEslSaveHeader(SaveFileHeader):
         masters_expected = unpack_int(ins)
         # Store separate lists of regular and ESLs masters for the Indices
         # column on the Saves tab
-        self.masters_regular = []
-        self.masters_esl = []
         num_regular_masters = unpack_byte(ins)
-        append_regular = self.masters_regular.append
-        for count in range(num_regular_masters):
-            append_regular(unpack_str16(ins))
+        self.masters_regular = [
+            *map(unpack_str16, repeat(ins, num_regular_masters))]
         # SSE / FO4 save format with esl block
         if self._esl_block():
             num_esl_masters = unpack_short(ins)
-            # Remember if we had ESL masters for the inacurracy warning
+            # Remember if we had ESL masters for the inaccuracy warning
             self.has_esl_masters = num_esl_masters > 0
-            append_esl = self.masters_esl.append
-            for count in range(num_esl_masters):
-                append_esl(unpack_str16(ins))
+            self.masters_esl = [
+                *map(unpack_str16, repeat(ins, num_esl_masters))]
         else:
             self.has_esl_masters = False
+            self.masters_esl = []
         # Check for master's table size (-4 for the stored size at the start of
         # the master table)
         masters_actual = ins.tell() + sse_offset - self._mastersStart - 4