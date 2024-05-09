wrye-bash/wrye-bash

commit 1bfea44fba1aa931f482d264f63d1db38577f1be

FO4: Implement NPC_
Includes a ton of renames related to NPC attributes. Also came with a
fun surprise in the form of MelActorSounds2. Turns out xEdit doesn't
properly decode them, which is not terribly surprising because Bethesda
chose a really weird way to implement them. See _MelCs2kCs2d for more
info.

With that, one of the nastiest records is done.

Mopy/bash/basher/dialogs.py
@@ -62,20 +62,22 @@ class ImportFaceDialog(DialogWindow):
         self.listBox.set_min_size(175, 150)
         #--Name,Race,Gender Checkboxes
         fi_flgs = bosh.faces.PCFaces.pcf_flags(
-            bass.settings.get(u'bash.faceImport.flags', 0x4))
-        self.nameCheck = CheckBox(self, _(u'Name'), checked=fi_flgs.pcf_name)
-        self.raceCheck = CheckBox(self, _(u'Race'), checked=fi_flgs.race)
-        self.genderCheck = CheckBox(self, _(u'Gender'), checked=fi_flgs.gender)
-        self.statsCheck = CheckBox(self, _(u'Stats'), checked=fi_flgs.stats)
-        self.classCheck = CheckBox(self, _(u'Class'), checked=fi_flgs.iclass)
-        #--Name,Race,Gender Text
+            bass.settings.get('bash.faceImport.flags', 0x4))
+        self.nameCheck = CheckBox(self, _('Name'), checked=fi_flgs.pcf_name)
+        self.raceCheck = CheckBox(self, _('Race'), checked=fi_flgs.pcf_race)
+        self.genderCheck = CheckBox(self, _('Gender'),
+            checked=fi_flgs.pcf_gender)
+        self.statsCheck = CheckBox(self, _('Stats'), checked=fi_flgs.pcf_stats)
+        self.classCheck = CheckBox(self, _('Class'),
+            checked=fi_flgs.pcf_class)
+        #--Name, Race, Gender Text
         self.nameText = Label(self, '-----------------------------')
         self.raceText = Label(self, '')
         self.genderText = Label(self, '')
         self.statsText = Label(self, '')
         self.classText = Label(self, '')
         #--Other
-        importButton = OkButton(self, btn_label=_(u'Import'))
+        importButton = OkButton(self, btn_label=_('Import'))
         importButton.on_clicked.subscribe(self.DoImport)
         self.picture = Picture(self, 350, 210, scaling=2) ##: unused
         GridLayout(border=4, stretch_cols=[0, 1], stretch_rows=[0], items=[
@@ -117,12 +119,12 @@ class ImportFaceDialog(DialogWindow):
         item = self.list_items[itemDex]
         #--Do import
         pc_flags = bosh.faces.PCFaces.pcf_flags() # make a copy of PCFaces flags
-        pc_flags.hair = pc_flags.eye = True
+        pc_flags.pcf_hair = pc_flags.pcf_eye = True
         pc_flags.pcf_name = self.nameCheck.is_checked
-        pc_flags.race = self.raceCheck.is_checked
-        pc_flags.gender = self.genderCheck.is_checked
-        pc_flags.stats = self.statsCheck.is_checked
-        pc_flags.iclass = self.classCheck.is_checked
+        pc_flags.pcf_race = self.raceCheck.is_checked
+        pc_flags.pcf_gender = self.genderCheck.is_checked
+        pc_flags.pcf_stats = self.statsCheck.is_checked
+        pc_flags.pcf_iclass = self.classCheck.is_checked
         #deprint(flags.getTrueAttrs())
         bass.settings[u'bash.faceImport.flags'] = int(pc_flags)
         bosh.faces.PCFaces.save_setFace(self.fileInfo, self.fdata[item],

Mopy/bash/bosh/faces.py
@@ -39,48 +39,49 @@ class PCFaces(object):
     """Package: Objects and functions for working with face data."""
     class pcf_flags(Flags):
         pcf_name: bool
-        race: bool
-        gender: bool
-        hair: bool
-        eye: bool
-        iclass: bool
-        stats: bool
-        factions: bool
-        modifiers: bool
-        spells: bool
+        pcf_race: bool
+        pcf_gender: bool
+        pcf_hair: bool
+        pcf_eye: bool
+        pcf_class: bool
+        pcf_stats: bool
+        pcf_factions: bool
+        pcf_modifiers: bool
+        pcf_spells: bool
 
     class PCFace(object):
         """Represents a face."""
         __slots__ = (
-            'face_masters', 'eid', 'pcName', 'race', 'gender', 'eye', 'hair',
-            'hairLength', 'hairRed', 'hairBlue', 'hairGreen', 'unused3',
-            'fggs_p', 'fgga_p', 'fgts_p', 'level_offset', 'attributes',
-            'skills', 'health', 'unused2', 'baseSpell', 'fatigue', 'iclass',
-            'factions', 'modifiers', 'spells')
+            'face_masters', 'eid', 'pcName', 'race', 'gender', 'eye',
+            'hair', 'hairLength', 'hairRed', 'hairBlue', 'hairGreen',
+            'unused3', 'fggs_p', 'fgga_p', 'fgts_p', 'level_offset',
+            'attributes', 'skills', 'health', 'unused2', 'base_spell',
+            'fatigue', 'npc_class', 'factions', 'modifiers', 'spells')
 
         def __init__(self):
             self.face_masters = []
-            self.eid = self.pcName = u'generic'
+            self.eid = self.pcName = 'generic'
             self.fggs_p = self.fgts_p = b'\x00'*4*50
             self.fgga_p = b'\x00'*4*30
             self.unused2 = null2
-            self.health = self.unused3 = self.baseSpell = self.fatigue = 0
+            self.health = self.unused3 = self.base_spell = self.fatigue = 0
             self.level_offset = 0
-            self.skills = self.attributes = self.iclass = None
+            self.skills = self.attributes = self.npc_class = None
             self.factions = []
             self.modifiers = []
             self.spells = []
 
         def getGenderName(self):
-            return self.gender and u'Female' or u'Male'
+            return _('Female') if self.gender else _('Male')
 
         def getRaceName(self):
-            return bush.game.raceNames.get(self.race,_(u'Unknown'))
+            return bush.game.raceNames.get(self.race, _('Unknown'))
 
         def convertRace(self,fromRace,toRace):
-            """Converts face from one race to another while preserving structure, etc."""
-            for a, f in ((u'fggs_p', u'50f'), (u'fgga_p', u'30f'),
-                         (u'fgts_p', u'50f')):
+            """Converts face from one race to another while preserving
+            structure, etc."""
+            for a, f in (('fggs_p', '50f'), ('fgga_p', '30f'),
+                         ('fgts_p', '50f')):
                 sValues = list(struct_unpack(f, getattr(self, a)))
                 fValues = list(struct_unpack(f, getattr(fromRace, a)))
                 tValues = list(struct_unpack(f, getattr(toRace, a)))
@@ -129,7 +130,7 @@ class PCFaces(object):
             for a in ('eid', 'race', 'eye', 'hair', 'hairLength', 'hairRed',
                       'hairBlue', 'hairGreen', 'unused3', 'fggs_p', 'fgga_p',
                       'fgts_p', 'level_offset', 'skills', 'health', 'unused2',
-                      'baseSpell', 'fatigue', 'attributes', 'iclass'):
+                      'base_spell', 'fatigue', 'attributes', 'npc_class'):
                 setattr(face, a, getattr(npc, a))
             face.gender = (0,1)[npc.npc_flags.npc_female]
             face.pcName = npc.full
@@ -148,7 +149,7 @@ class PCFaces(object):
         if npc.acbs:
             face.gender = npc.acbs.npc_flags.npc_female
             face.level_offset = npc.acbs.level_offset
-            face.baseSpell = npc.acbs.baseSpell
+            face.base_spell = npc.acbs.base_spell
             face.fatigue = npc.acbs.fatigue
         for att in ('attributes', 'skills', 'health', 'unused2'):
             npc_val = getattr(npc, att)
@@ -177,18 +178,18 @@ class PCFaces(object):
         data = record[-1]
         namePos = PCFaces.save_getNamePos(saveFile.fileInfo.fn_key, data,
                                           encode(saveFile.header.pcName))
-        (face.fggs_p, face.fgga_p, face.fgts_p, face.race, face.hair, face.eye,
-         face.hairLength, face.hairRed, face.hairBlue, face.hairGreen,
-         face.unused3, face.gender) = __faceunpack(
+        (face.fggs_p, face.fgga_p, face.fgts_p, face.race, face.hair,
+         face.eye, face.hairLength, face.hairRed, face.hairBlue,
+         face.hairGreen, face.unused3, face.gender) = __faceunpack(
             data[namePos - 542:namePos - 1])
         classPos = namePos + len(saveFile.header.pcName) + 1
-        face.iclass, = __unpacker(data[classPos:classPos+4])
+        face.npc_class, = __unpacker(data[classPos:classPos+4])
         #--Iref >> fid
         getFid = saveFile.getFid
         face.race = getFid(face.race)
         face.hair = getFid(face.hair)
         face.eye = getFid(face.eye)
-        face.iclass = getFid(face.iclass)
+        face.npc_class = getFid(face.npc_class)
         #--Changed NPC Record
         PCFaces.save_getChangedNpc(saveFile, _player_fid, face)
         #--Done
@@ -230,13 +231,13 @@ class PCFaces(object):
         #--Set face
         npc.npc_flags.npc_female = (face.gender & 0x1)
         PCFaces._set_npc_attrs(npc, face, masterMap)
-        #--Stats: Skip Level, baseSpell, fatigue and factions since they're discarded by game engine.
+        #--Stats: Skip Level, base_spell, fatigue and factions since they're discarded by game engine.
         if face.skills: npc.skills = face.skills
         if face.health:
             npc.health = face.health
             npc.unused2 = face.unused2
         if face.attributes: npc.attributes = face.attributes
-        if face.iclass: npc.iclass = face.iclass
+        if face.npc_class: npc.npc_class = face.npc_class
         npc.setChanged()
         npc.getSize()
         #--Change record?
@@ -246,7 +247,7 @@ class PCFaces(object):
         if not npc.acbs: npc.acbs = SreNPC.ACBS()
         npc.acbs.npc_flags.npc_female = face.gender
         npc.acbs.level_offset = face.level_offset
-        npc.acbs.baseSpell = face.baseSpell
+        npc.acbs.base_spell = face.base_spell
         npc.acbs.fatigue = face.fatigue
         npc.modifiers = face.modifiers[:]
         #--Fid conversion
@@ -262,7 +263,7 @@ class PCFaces(object):
         pcf_flags = PCFaces.pcf_flags(pcf_flags)
         #--Update masters
         maxMaster = len(face.face_masters) - 1
-        for fid in (face.race, face.eye, face.hair, face.iclass):
+        for fid in (face.race, face.eye, face.hair, face.npc_class):
             if not fid: continue
             master = face.face_masters[min(fid >> 24, maxMaster)]
             saveFile.addMaster(master) # won't add it if it's there
@@ -288,16 +289,16 @@ class PCFaces(object):
         buff.seek(namePos-542)
         buffPack(u'=200s120s200s',face.fggs_p, face.fgga_p, face.fgts_p)
         #--Race?
-        buffPackRef(face.race, pcf_flags.race)
+        buffPackRef(face.race, pcf_flags.pcf_race)
         #--Hair, Eyes?
-        buffPackRef(face.hair, pcf_flags.hair)
-        buffPackRef(face.eye, pcf_flags.eye)
-        if pcf_flags.hair:
+        buffPackRef(face.hair, pcf_flags.pcf_hair)
+        buffPackRef(face.eye, pcf_flags.pcf_eye)
+        if pcf_flags.pcf_hair:
             buffPack(u'=f3Bs',face.hairLength,face.hairRed,face.hairBlue,face.hairGreen,face.unused3)
         else:
             buff.seek(8,1)
         #--Gender?
-        if pcf_flags.gender:
+        if pcf_flags.pcf_gender:
             pack_byte(buff, face.gender)
         else:
             buff.seek(1,1)
@@ -314,9 +315,9 @@ class PCFaces(object):
         else:
             buff.seek(len(saveFile.header.pcName) + 2, 1)
         #--Class?
-        if pcf_flags.iclass and face.iclass:
+        if pcf_flags.pcf_class and face.npc_class:
             pos = buff.tell()
-            newClass = masterMap(face.iclass)
+            newClass = masterMap(face.npc_class)
             oldClass = saveFile.fids[struct_unpack(u'I', buff.read(4))[0]]
             customClass = saveFile.getIref(0x22843)
             if customClass not in (newClass.short_fid, oldClass):
@@ -327,12 +328,12 @@ class PCFaces(object):
         #--Player NPC
         npc, version = saveFile.get_npc()
         #--Gender
-        if pcf_flags.gender and npc.acbs:
+        if pcf_flags.pcf_gender and npc.acbs:
             npc.acbs.npc_flags.npc_female = face.gender
         #--Stats
-        if pcf_flags.stats and npc.acbs:
+        if pcf_flags.pcf_stats and npc.acbs:
             npc.acbs.level_offset = face.level_offset
-            npc.acbs.baseSpell = face.baseSpell
+            npc.acbs.base_spell = face.base_spell
             npc.acbs.fatigue = face.fatigue
             npc.attributes = face.attributes
             npc.skills = face.skills
@@ -340,8 +341,9 @@ class PCFaces(object):
             npc.unused2 = face.unused2
         #--Factions: Faction assignment doesn't work. (Probably stored in achr.)
         #--Modifiers, Spells, Name
-        if pcf_flags.modifiers: npc.modifiers = face.modifiers[:]
-        if pcf_flags.spells:
+        if pcf_flags.pcf_modifiers:
+            npc.modifiers = face.modifiers[:]
+        if pcf_flags.pcf_spells:
             npc.spells = [saveFile.getIref(x) for x in face.spells]
         npc.full = None
         #--Save
@@ -392,8 +394,9 @@ class PCFaces(object):
             face.face_masters = modFile.augmented_masters()
             for att in ('eid', 'race', 'eye', 'hair', 'hairLength', 'hairRed',
                         'hairBlue', 'hairGreen', 'unused3', 'fggs_p', 'fgga_p',
-                        'fgts_p', 'level_offset', 'skills', 'health', 'iclass',
-                        'unused2', 'baseSpell', 'fatigue', 'attributes'):
+                        'fgts_p', 'level_offset', 'skills', 'health',
+                        'npc_class', 'unused2', 'base_spell', 'fatigue',
+                        'attributes'):
                 npc_val = getattr(npc, att)
                 if isinstance(npc_val, FormId):
                     npc_val = npc_val.short_fid # saves code uses the ints...
@@ -444,11 +447,11 @@ class PCFaces(object):
         npc.eid = eid
         npc.npc_flags = RecordType.sig_to_class[b'NPC_'].NpcFlags() ##: setDefault - drop this!
         npc.npc_flags.npc_female = face.gender
-        npc.iclass = masterMap(face.iclass,0x237a8) #--Default to Acrobat
+        npc.npc_class = masterMap(face.npc_class,0x237a8) #--Default to Acrobat
         PCFaces._set_npc_attrs(npc, face, masterMap)
         #--Stats
         npc.level_offset = face.level_offset
-        npc.baseSpell = face.baseSpell
+        npc.base_spell = face.base_spell
         npc.fatigue = face.fatigue
         if face.skills: npc.skills = face.skills
         if face.health:

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -8182,7 +8182,7 @@ C.Location, -C.LockList
                     <li>(SPLO) Spells</li>
                 </ul>
             </td>
-            <td>All but Fallout 4</td>
+            <td>All games</td>
             <td></td>
         </tr>
         <tr>

Mopy/bash/basher/saves_links.py
@@ -903,8 +903,9 @@ class Save_UpdateNPCLevels(EnabledLink):
                 for rid, npc in npc_block.iter_present_records():
                     fid = mapToOrdered(rid, None)
                     if not fid: continue
-                    npc_info[fid] = (npc.eid, npc.level_offset, npc.calcMin,
-                                     npc.calcMax, npc.npc_flags.pc_level_offset)
+                    npc_info[fid] = (npc.eid, npc.level_offset,
+                                     npc.calc_min_level, npc.calc_max_level,
+                                     npc.npc_flags.pc_level_offset)
             #--Loop over savefiles
             subProgress = SubProgress(progress,0.4,1.0,len(self.selected))
             msg = [_(u'NPCs Releveled:')]
@@ -922,18 +923,18 @@ class Save_UpdateNPCLevels(EnabledLink):
                     if (rec_kind != 35 or recId == 7 or
                             orderedRecId not in npc_info):
                         continue
-                    eid, level_offset, calcMin, calcMax, pc_level_offset = \
+                    eid, info_lo, info_min_lv, info_max_lv, info_pc_lo = \
                         npc_info[orderedRecId]
                     npc = _saves.SreNPC(recFlags, rdata)
                     acbs = npc.acbs
-                    if acbs and (acbs.level_offset != level_offset or
-                                 acbs.calcMin != calcMin or
-                                 acbs.calcMax != calcMax or
-                                 acbs.npc_flags.pc_level_offset != pc_level_offset):
-                        acbs.npc_flags.pc_level_offset = pc_level_offset
-                        acbs.level_offset = level_offset
-                        acbs.calcMin = calcMin
-                        acbs.calcMax = calcMax
+                    if acbs and (acbs.level_offset != info_lo or
+                                 acbs.calc_min_level != info_min_lv or
+                                 acbs.calc_max_level != info_max_lv or
+                                 acbs.npc_flags.pc_level_offset != info_pc_lo):
+                        acbs.level_offset = info_lo
+                        acbs.calc_min_level = info_min_lv
+                        acbs.calc_max_level = info_max_lv
+                        acbs.npc_flags.pc_level_offset = info_pc_lo
                         releveledCount += 1
                         fid_rec[recId] = npc.getTuple(version)
                 #--Save changes?

Mopy/bash/bosh/_saves.py
@@ -63,8 +63,8 @@ class SreNPC(object):
         modifiers: bool = flag(28)
 
     class ACBS(object):
-        __slots__ = ('npc_flags', 'baseSpell', 'fatigue', 'barterGold',
-                     'level_offset', 'calcMin', 'calcMax')
+        __slots__ = ('npc_flags', 'base_spell', 'fatigue', 'barter_gold',
+                     'level_offset', 'calc_min_level', 'calc_max_level')
 
         def __init__(self, ins=None, *, __deflts=(0, 0, 0, 0, 1, 0, 0)):
             if ins is not None:
@@ -134,10 +134,10 @@ class SreNPC(object):
             _pack(u'8B', *self.attributes)
         #--Acbs
         if self.acbs is not None:
-            acbs = self.acbs
-            _pack(u'=I3Hh2H', acbs.npc_flags.dump(), acbs.baseSpell, acbs.fatigue,
-                  acbs.barterGold, acbs.level_offset, acbs.calcMin,
-                  acbs.calcMax)
+            _pack('=I3Hh2H', self.acbs.npc_flags.dump(), self.acbs.base_spell,
+                self.acbs.fatigue, self.acbs.barter_gold,
+                self.acbs.level_offset, self.acbs.calc_min_level,
+                self.acbs.calc_max_level)
         #--Factions
         if self.factions is not None:
             pack_short(out, len(self.factions))

Mopy/bash/brec/advanced_elements.py
@@ -1016,7 +1016,7 @@ class MelCounter(_MelWrapper):
     Additionally, dumping is skipped if the counter is falsy after updating.
 
     See also MelPartialCounter, which targets mixed structs."""
-    def __init__(self, counter_mel, *, counts):
+    def __init__(self, counter_mel, /, *, counts):
         """Creates a new MelCounter.
 
         :param counter_mel: The element that stores the counter's value.

Mopy/bash/brec/basic_elements.py
@@ -475,7 +475,7 @@ class MelGroups(MelGroup):
 
 #------------------------------------------------------------------------------
 class MelUnorderedGroups(MelGroups):
-    """A verion of MelGroups that does not use the usual 'initial sigs'
+    """A version of MelGroups that does not use the usual 'initial sigs'
     mechanism. Instead any element in the group can start a new object if it's
     already been encountered while loading the current object. As an example,
     consider these two subrecord definitions:

commit faf1376ec80cdb01a579d9bd3072a5bda7ab6a84

Fix Import Face in saves
Broke in 1bfea44, fixing revealed another bug from 77ef1bf (or maybe
4964710a8ee797d77805ce5b58916d657fb3eb43).

Traceback (most recent call last):
  File "bash\gui\events.py", line 181, in _post
  File "bash\basher\dialogs.py", line 136, in DoImport
  File "bash\bosh\faces.py", line 205, in save_setFace
  File "bash\bosh\faces.py", line 269, in save_setPlayerFace
TypeError: unsupported operand type(s) for >>: 'FormId' and 'int'

For the actual player reference (00000007), fid appears to always be an
int. However, for the dynamic(?) player references (FFxxxxxx), fid
appears to always be a FormId.

Utumno:

Nit in faces:

I split the slots to ones that are set from the NPC record and ones that
are set manually - inlined one use _save_getCreatedFaces - the whole
thing needs a rewrite. It's ImportFaceDialog that passes the faces in,
start from there.

More EAFP in MasterMap

Mopy/bash/basher/installer_links.py: make "To" translatable

Co-authored-by: MrD <the.ubik@gmail.com>

Mopy/bash/basher/dialogs.py
@@ -60,8 +60,7 @@ class ImportFaceDialog(DialogWindow):
             self.fdata = faces
         self.list_items = sorted(self.fdata, key=str.lower)
         #--GUI
-        super(ImportFaceDialog, self).__init__(parent, title=title,
-                                               sizes_dict=bass.settings)
+        super().__init__(parent, title=title, sizes_dict=bass.settings)
         #--List Box
         self.listBox = ListBox(self, choices=self.list_items,
                                onSelect=self.EvtListBox)
@@ -130,7 +129,7 @@ class ImportFaceDialog(DialogWindow):
         pc_flags.pcf_race = self.raceCheck.is_checked
         pc_flags.pcf_gender = self.genderCheck.is_checked
         pc_flags.pcf_stats = self.statsCheck.is_checked
-        pc_flags.pcf_iclass = self.classCheck.is_checked
+        pc_flags.pcf_class = self.classCheck.is_checked
         #deprint(flags.getTrueAttrs())
         bass.settings[u'bash.faceImport.flags'] = int(pc_flags)
         bosh.faces.PCFaces.save_setFace(self.fileInfo, self.fdata[item],

Mopy/bash/basher/installer_links.py
@@ -428,7 +428,9 @@ class Installer_Wizard(_Installer_AWizardLink):
             message = [_('The following INI Tweaks were not automatically '
                          'applied. Be sure to apply them after installing the '
                          'package.'), '', '']
-            message.extend(f' * {x}\n   TO: {y}' for x, y in manuallyApply)
+            message.extend(
+                f' * {x}\n {_("To: %(tweak_target)s") % {"tweak_target": y}}'
+                for x, y in manuallyApply)
             self._showInfo('\n'.join(message))
 
 class Installer_OpenReadme(_SingleInstallable):

Mopy/bash/bosh/faces.py
@@ -52,12 +52,13 @@ class PCFaces(object):
 
     class PCFace(object):
         """Represents a face."""
-        __slots__ = (
-            'face_masters', 'eid', 'pcName', 'race', 'gender', 'eye',
-            'hair', 'hairLength', 'hairRed', 'hairBlue', 'hairGreen',
-            'unused3', 'fggs_p', 'fgga_p', 'fgts_p', 'level_offset',
-            'attributes', 'skills', 'health', 'unused2', 'base_spell',
-            'fatigue', 'npc_class', 'factions', 'modifiers', 'spells')
+        set_from_npc = ('attributes', 'base_spell', 'eid', 'eye', 'fatigue',
+            'fgga_p', 'fggs_p', 'fgts_p', 'hair', 'hairBlue', 'hairGreen',
+            'hairLength', 'hairRed', 'health', 'level_offset', 'npc_class',
+            'race', 'skills', 'unused2', 'unused3')
+        set_manually = ('face_masters', 'factions', 'gender', 'modifiers',
+                        'pcName', 'spells')
+        __slots__ = set_from_npc + set_manually
 
         def __init__(self):
             self.face_masters = []
@@ -110,16 +111,7 @@ class PCFaces(object):
     # Save Get ----------------------------------------------------------------
     @staticmethod
     def save_getFaces(saveFile):
-        """Returns player and created faces from a save file or saveInfo."""
-        faces = PCFaces._save_getCreatedFaces(saveFile)
-        playerFace = PCFaces.save_getPlayerFace(saveFile)
-        faces[_player_fid] = playerFace
-        return faces
-
-    @staticmethod
-    def _save_getCreatedFaces(saveFile):
-        """Returns created faces from savefile. If fid is supplied, will only
-        return created face with that fid.
+        """Returns player and created faces from a save file.
         Note: Created NPCs do NOT use irefs!"""
         faces = {}
         for rfid, record in saveFile.created.items():
@@ -128,21 +120,19 @@ class PCFaces(object):
             npc = record.getTypeCopy()
             face = faces[npc.fid] = PCFaces.PCFace()
             face.face_masters = saveFile._masters
-            for a in ('eid', 'race', 'eye', 'hair', 'hairLength', 'hairRed',
-                      'hairBlue', 'hairGreen', 'unused3', 'fggs_p', 'fgga_p',
-                      'fgts_p', 'level_offset', 'skills', 'health', 'unused2',
-                      'base_spell', 'fatigue', 'attributes', 'npc_class'):
+            for a in PCFaces.PCFace.set_from_npc:
                 setattr(face, a, getattr(npc, a))
             face.gender = (0,1)[npc.npc_flags.npc_female]
             face.pcName = npc.full
             #--Changed NPC Record
             PCFaces.save_getChangedNpc(saveFile, rfid, face)
+        playerFace = PCFaces.save_getPlayerFace(saveFile)
+        faces[_player_fid] = playerFace
         return faces
 
     @staticmethod
-    def save_getChangedNpc(saveFile, npc_fid, face=None):
+    def save_getChangedNpc(saveFile, npc_fid, face):
         """Update face with data from npc change record."""
-        face = face or PCFaces.PCFace()
         changeRecord = saveFile.get_npc(npc_fid)
         if changeRecord is None:
             return face
@@ -266,7 +256,8 @@ class PCFaces(object):
         maxMaster = len(face.face_masters) - 1
         for fid in (face.race, face.eye, face.hair, face.npc_class):
             if not fid: continue
-            master = face.face_masters[min(fid >> 24, maxMaster)]
+            master_mod_dex = fid >> 24 if isinstance(fid, int) else fid.mod_dex
+            master = face.face_masters[min(master_mod_dex, maxMaster)]
             saveFile.addMaster(master) # won't add it if it's there
         masterMap = MasterMap(face.face_masters, saveFile._masters)
         #--Player ACHR
@@ -393,11 +384,7 @@ class PCFaces(object):
         for _rid, npc in modFile.tops[b'NPC_'].iter_present_records():
             face = PCFaces.PCFace()
             face.face_masters = modFile.augmented_masters()
-            for att in ('eid', 'race', 'eye', 'hair', 'hairLength', 'hairRed',
-                        'hairBlue', 'hairGreen', 'unused3', 'fggs_p', 'fgga_p',
-                        'fgts_p', 'level_offset', 'skills', 'health',
-                        'npc_class', 'unused2', 'base_spell', 'fatigue',
-                        'attributes'):
+            for att in PCFaces.PCFace.set_from_npc:
                 npc_val = getattr(npc, att)
                 if isinstance(npc_val, FormId):
                     npc_val = npc_val.short_fid # saves code uses the ints...

Mopy/bash/mod_files.py
@@ -49,7 +49,7 @@ class MasterMap(object):
             try:
                 mast_map[i] = outMasters.index(master)
             except ValueError:
-                mast_map[i] = -1
+                pass # this master is not present in outMasters
         self._mast_map = mast_map
         self._out_masters = outMasters
 
@@ -58,19 +58,18 @@ class MasterMap(object):
         possible, then either returns default (if given) or raises
         MasterMapError."""
         if not fid_to_map: return fid_to_map
-        mod_dex_in = (int(fid_to_map >> 24) if isinstance(fid_to_map, int) else
-                      fid_to_map.mod_dex)
-        mod_dex_out = self._mast_map.get(mod_dex_in, -2)
-        if mod_dex_out >= 0:
+        is_int = isinstance(fid_to_map, int)
+        mod_dex_in = int(fid_to_map >> 24) if is_int else fid_to_map.mod_dex
+        try:
+            mod_dex_out = self._mast_map[mod_dex_in]
             mapped_object_dex = (
-                fid_to_map & 0xFFFFFF if isinstance(fid_to_map, int) else
-                fid_to_map.object_dex)
+                fid_to_map & 0xFFFFFF if is_int else fid_to_map.object_dex)
             return FormId.from_tuple((self._out_masters[mod_dex_out],
                                       mapped_object_dex))
-        elif dflt_fid != ZERO_FID:
-            return dflt_fid
-        else:
-            raise MasterMapError(mod_dex_in)
+        except KeyError:
+            if dflt_fid != ZERO_FID:
+                return dflt_fid
+        raise MasterMapError(mod_dex_in)
 
 class LoadFactory:
     """Encapsulate info on which record type we use to load which record