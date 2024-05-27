wrye-bash/wrye-bash

commit 0e85cd457f08e06a280807d249f9ac3b9b21c2f6

FNV, TES5: Fix regressions in Sounds patcher
Broke in a3b3d790f0797dbd273080709f5764d9b9e109ab.

Infernio: More regression fixups and dropped a useless comment.

Co-authored-by: Infernio <infernio@icloud.com>

Mopy/bash/game/fallout3/records.py
@@ -3159,8 +3159,13 @@ class MreWeap(MelRecord):
             MelFid(b'WMI2', 'mod2'),
             MelFid(b'WMI3', 'mod3'),
         )),
-        MelSound(),
-        fnv_only(MelFid(b'SNAM', 'sound_gun_shoot_dist')),
+        if_fnv(
+            fo3_version=MelSound(),
+            fnv_version=MelSequential(
+                MelFid(b'SNAM', 'sound_gun_shoot_3d'),
+                MelFid(b'SNAM', 'sound_gun_shoot_dist'),
+            ),
+        ),
         MelFid(b'XNAM','soundGunShot2D'),
         MelFid(b'NAM7','soundGunShot3DLooping'),
         MelFid(b'TNAM','soundMeleeSwingGunNoAmmo'),
@@ -3168,8 +3173,9 @@ class MreWeap(MelRecord):
         MelFid(b'UNAM','idleSound',),
         MelFid(b'NAM9','equipSound',),
         MelFid(b'NAM8','unequipSound',),
-        fnv_only(MelFids('soundMod1Shoot3Ds', MelFid(b'WMS1'))),
-        fnv_only(MelFid(b'WMS2', 'soundMod1Shoot2D')),
+        fnv_only(MelFid(b'WMS1', 'sound_mod1_shoot_3d')),
+        fnv_only(MelFid(b'WMS1', 'sound_mod1_shoot_dist')),
+        fnv_only(MelFid(b'WMS2', 'sound_mod1_shoot_2d')),
         MelStruct(b'DATA', [u'2I', u'f', u'H', u'B'],'value','health','weight','damage','clipsize'),
         if_fnv(
             fo3_version=MelTruncatedStruct(
@@ -3208,14 +3214,15 @@ class MreWeap(MelRecord):
             is_optional=True)),
         MelBase(b'VNAM','soundLevel'),
     ).with_distributor(fnv_only({
-        b'SNAM': ('sound', {
-            b'SNAM': 'sound_gun_shoot_dist',
-        }),
+        b'SNAM': [
+            (b'SNAM', 'sound_gun_shoot_3d'),
+            (b'SNAM', 'sound_gun_shoot_dist'),
+        ],
+        b'WMS1': [
+            (b'WMS1', 'sound_mod1_shoot_3d'),
+            (b'WMS1', 'sound_mod1_shoot_dist'),
+        ],
     }))
-
-    def keep_fids(self, keep_plugins):
-        if _is_fnv:
-            self.sounds = [s for s in self.sounds if s.mod_fn in keep_plugins]
 
 #------------------------------------------------------------------------------
 class MreWrld(AMreWrld):

Mopy/bash/game/falloutnv/__init__.py
@@ -356,13 +356,13 @@ class FalloutNVGameInfo(Fallout3GameInfo):
         b'KEYM': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
         b'MISC': ('sound_pickup', 'sound_drop', 'sound_random_looping'),
         b'STAT': ('sound_random_looping',),
-        b'WEAP': ('sound_pickup', 'sound_drop', 'sound',
+        b'WEAP': ('sound_pickup', 'sound_drop', 'sound_gun_shoot_3d',
                   'sound_gun_shoot_dist', 'soundGunShot2D',
                   'soundGunShot3DLooping', 'soundMeleeSwingGunNoAmmo',
                   'soundBlock', 'idleSound', 'equipSound', 'unequipSound',
-                  'soundMod1Shoot3Ds', 'soundMod1Shoot2D'),
+                  'sound_mod1_shoot_3d', 'sound_mod1_shoot_dist',
+                  'sound_mod1_shoot_2d'),
     }
-    sounds_fid_attrs[b'WEAP'] = ()
 
     #--------------------------------------------------------------------------
     # Import Graphics

Mopy/bash/game/falloutnv/patcher/preservers.py
@@ -30,12 +30,12 @@ class ImportWeaponModificationsPatcher(APreserver):
     _config_key = u'WeaponModsPatcher'
     patcher_order = 27 ##: This seems unneeded + no reason given
     rec_attrs = {b'WEAP': (
-        u'modelWithMods', u'firstPersonModelWithMods', u'weaponMods',
-        u'soundMod1Shoot3Ds', u'soundMod1Shoot2D', u'effectMod1',
-        u'effectMod2', u'effectMod3', u'valueAMod1', u'valueAMod2',
-        u'valueAMod3', u'valueBMod1', u'valueBMod2', u'valueBMod3',
-        u'reloadAnimationMod', u'vats_mod_required',
-        u'dnamFlags2.scopeFromMod')}
+        'modelWithMods', 'firstPersonModelWithMods', 'weaponMods',
+        'effectMod1', 'effectMod2', 'effectMod3', 'valueAMod1', 'valueAMod2',
+        'valueAMod3', 'valueBMod1', 'valueBMod2', 'valueBMod3',
+        'reloadAnimationMod', 'vats_mod_required', 'dnamFlags2.scopeFromMod')}
+    _fid_rec_attrs = {b'WEAP': ('sound_mod1_shoot_3d', 'sound_mod1_shoot_dist',
+                                'sound_mod1_shoot_2d')}
 
     @classmethod
     def gui_cls_vars(cls):

Mopy/bash/game/skyrim/__init__.py
@@ -754,7 +754,28 @@ class SkyrimGameInfo(PatchGame):
     #--------------------------------------------------------------------------
     # Import Sounds
     #--------------------------------------------------------------------------
-    soundsTypes = {
+    sounds_attrs = {
+        b'EXPL': ('expl_sound_level',),
+        b'IPCT': ('ipct_sound_level',),
+        ##: 'sounds' needs to be filtered
+        b'MGEF': ('casting_sound_level', 'sounds'),
+        b'PROJ': ('soundLevel',),
+        b'SNCT': ('staticVolumeMultiplier',),
+        # sound_files does not need to loop here
+        b'SNDR': ('sound_files', 'looping_type', 'rumble_send_value',
+                  'pct_frequency_shift', 'pct_frequency_variance',
+                  'descriptor_priority', 'db_variance', 'staticAtten'),
+        b'SOPM': ('reverbSendpct', 'outputType', 'ch0_l', 'ch0_r', 'ch0_c',
+                  'ch0_lFE', 'ch0_rL', 'ch0_rR', 'ch0_bL', 'ch0_bR', 'ch1_l',
+                  'ch1_r', 'ch1_c', 'ch1_lFE', 'ch1_rL', 'ch1_rR', 'ch1_bL',
+                  'ch1_bR', 'ch2_l', 'ch2_r', 'ch2_c', 'ch2_lFE', 'ch2_rL',
+                  'ch2_rR', 'ch2_bL', 'ch2_bR', 'minDistance', 'maxDistance',
+                  'curve1', 'curve2', 'curve3', 'curve4', 'curve5'),
+        b'WEAP': ('detectionSoundLevel',),
+        # Has FormIDs, but will be filtered in AMreWthr.keep_fids
+        b'WTHR': ('sounds',),
+    }
+    sounds_fid_attrs = {
         b'ACTI': ('sound', 'sound_activation'),
         b'ADDN': ('sound',),
         b'ALCH': ('sound_pickup', 'sound_drop', 'sound_consume'),
@@ -767,42 +788,27 @@ class SkyrimGameInfo(PatchGame):
         b'CONT': ('sound', 'sound_close'),
         b'DOOR': ('sound', 'sound_close', 'sound_looping'),
         b'EFSH': ('sound_ambient',),
-        b'EXPL': ('expl_sound_level', 'expl_sound1', 'expl_sound2'),
+        b'EXPL': ('expl_sound1', 'expl_sound2'),
         b'FLOR': ('sound',),
         b'HAZD': ('hazd_sound',),
         b'INGR': ('sound_pickup', 'sound_drop'),
-        b'IPCT': ('ipct_sound_level', 'sound', 'ipct_sound2'),
+        b'IPCT': ('sound', 'ipct_sound2'),
         b'KEYM': ('sound_pickup', 'sound_drop'),
         b'LIGH': ('sound',),
-        #Needs to loop over all the sounds
-        b'MGEF': ('sounds', 'casting_sound_level'),
-        # b'REGN': ('entries',),
         b'MISC': ('sound_pickup', 'sound_drop'),
         b'MSTT': ('sound',),
-        b'PROJ': ('sound', 'sound_countdown', 'sound_disable', 'soundLevel'),
+        b'PROJ': ('sound', 'sound_countdown', 'sound_disable'),
         b'SCRL': ('sound_pickup', 'sound_drop'),
         b'SLGM': ('sound_pickup', 'sound_drop'),
-        b'SNCT': ('parent', 'staticVolumeMultiplier'),
-        # Sounds does not need to loop here
-        b'SNDR': ('descriptor_category', 'output_model', 'sound_files',
-                  'looping_type', 'rumble_send_value', 'pct_frequency_shift',
-                  'pct_frequency_variance', 'descriptor_priority',
-                  'db_variance', 'staticAtten'),
-        b'SOPM': ('reverbSendpct', 'outputType', 'ch0_l', 'ch0_r', 'ch0_c',
-                  'ch0_lFE', 'ch0_rL', 'ch0_rR', 'ch0_bL', 'ch0_bR', 'ch1_l',
-                  'ch1_r', 'ch1_c', 'ch1_lFE', 'ch1_rL', 'ch1_rR', 'ch1_bL',
-                  'ch1_bR', 'ch2_l', 'ch2_r', 'ch2_c', 'ch2_lFE', 'ch2_rL',
-                  'ch2_rR', 'ch2_bL', 'ch2_bR', 'minDistance', 'maxDistance',
-                  'curve1', 'curve2', 'curve3', 'curve4', 'curve5'),
+        b'SNCT': ('parent',),
+        b'SNDR': ('descriptor_category', 'output_model'),
         b'SOUN': ('soundDescriptor',),
         b'TACT': ('sound',),
         b'TREE': ('sound',),
         b'WATR': ('sound',),
         b'WEAP': ('sound_pickup', 'sound_drop', 'sound', 'attackSound2D',
                   'attackLoopSound', 'attackFailSound', 'idleSound',
-                  'equipSound', 'unequipSound', 'detectionSoundLevel'),
-        #Needs to loop over all the sounds
-        b'WTHR': ('sounds',),
+                  'equipSound', 'unequipSound'),
     }
 
     #--------------------------------------------------------------------------

commit fb6386f78c8883e81145a88b2c82e74790ad69e4

FNV: fix handling of duplicate WEAP/SNAMs
Broke in 0e85cd4.

Using MelSequential here seems to cause the BP to only keep one SNAM.

Mopy/bash/game/fallout3/records.py
@@ -3168,11 +3168,9 @@ class MreWeap(MelRecord):
         )),
         if_fnv(
             fo3_version=MelSound(),
-            fnv_version=MelSequential(
-                MelFid(b'SNAM', 'sound_gun_shoot_3d'),
-                MelFid(b'SNAM', 'sound_gun_shoot_dist'),
-            ),
+            fnv_version=MelFid(b'SNAM', 'sound_gun_shoot_3d'),
         ),
+        fnv_only(MelFid(b'SNAM', 'sound_gun_shoot_dist')),
         MelFid(b'XNAM','soundGunShot2D'),
         MelFid(b'NAM7','soundGunShot3DLooping'),
         MelFid(b'TNAM','soundMeleeSwingGunNoAmmo'),