wrye-bash/wrye-bash

commit 243897772ec2314000dfee473abf6a8b6af3bc1c

Make DataFileExists work on Linux
Those generally use backslashes and assume case-insensitive filesystems,
so just like with FOMODs, we need to run a to_os_path here. Also update
the docs to mention this.

.github/workflows/update_pot.yaml:
.github/workflows/wb_ci.yaml:
ci: Bump actions/cache version

To fix the annoying node 16 deprecation warning Github spits out about
it.

Mopy/Docs/Wrye Bash Technical Readme.html
@@ -209,17 +209,32 @@
     testing for is a plugin, this will also detect ghosted versions of the file.</p>
 <code class="box">DataFileExists(file_name [, ..., file_name_n])</code>
 <ul>
-    <li><var>file_name</var> - A string or variable holding a string, specifying the path relative to the Data directory to test. For example
-        using <code>"Oblivion.esm"</code> would test for <code>"[path to game]\Data\Oblivion.esm"</code>. Notes:
-    <ul>
-        <li>When specifying a path below the Data directory, use double slashes to indicate folder separation. For example assuming
-            MenuQue.dll is installed correctly, <code>"obse\\plugins\\MenuQue.dll"</code> will work but
-            <code>"obse\plugins\MenuQue.dll"</code> will fail.
-        <li>You can also look for files above the Data directory using the following syntax: <code>"..\\SoundCommands.log"</code>
-        <li>When testing for multiple files, separate each file with commas. They do not have to all be in the same directory. Example:
-        <code>DataFileExists("Oblivion.esm","..\\SoundCommands.log","obse\\plugins\\MenuQue.dll")</code> will accurately test to see if each
-        of the files exist.
-    </ul>
+    <li><var>file_name</var> - A string or variable holding a string,
+        specifying the path relative to the Data directory to test. For
+        example, using <code>"Oblivion.esm"</code> would test for
+        <code>"[path to game]\Data\Oblivion.esm"</code>. Notes:
+        <ul>
+            <li>When specifying a path below the Data directory, use double
+                backslashes to indicate folder separation. For example assuming
+                MenuQue.dll is installed correctly,
+                <code>"obse\\plugins\\MenuQue.dll"</code> will work, but
+                <code>"obse\plugins\MenuQue.dll"</code> will fail.</li>
+            <li>You can also look for files above the Data directory using the
+                following syntax: <code>"..\\SoundCommands.log"</code></li>
+            <li>When testing for multiple files, separate each file with
+                commas. They do not have to all be in the same directory.
+                Example:
+                <code>DataFileExists("Oblivion.esm","..\\SoundCommands.log",
+                    "obse\\plugins\\MenuQue.dll")</code> will accurately test
+                to see if each of the files exist.</li>
+            <li>Path separators will automatically be converted to the right
+                ones for the current operating system, so you can use either
+                backslashes or forward slashes.</li>
+            <li>This will check case-insensitively, <strong>even on
+                case-sensitive filesystems</strong> (e.g. most Linux
+                filesystems).</li>
+        </ul>
+    </li>
 </ul>
 <p class="indent">Return values:
 <ul class="indent">

Mopy/bash/basher/belt.py
@@ -597,12 +597,12 @@ class WryeParser(PreParser):
         wbHave = bass.AppVersion
         return bolt.cmp_(LooseVersion(wbHave), LooseVersion(wbWant))
 
-    def fnDataFileExists(self, *filenames):
-        for filename in filenames:
-            if not bass.dirs['mods'].join(filename).exists():
-                # Check for ghosted mods
-                if filename in bosh.modInfos:
-                    return True # It's a ghosted mod
+    def fnDataFileExists(self, *rel_paths):
+        for rel_path in rel_paths:
+            if rel_path in bosh.modInfos:
+                return True # It's a (potentially ghosted) plugin
+            rel_path_os = to_os_path(bass.dirs['mods'].join(rel_path))
+            if not rel_path_os.exists():
                 return False
         return True
 

.github/workflows/update_pot.yaml
@@ -22,7 +22,7 @@ jobs:
         python-version: '3.12'
         architecture: 'x64'
     - name: Restore dependencies cache
-      uses: actions/cache@v3
+      uses: actions/cache@v4
       with:
         path: ~/.cache/pip
         key: ${{ runner.os }}-pip-${{ hashFiles('requirements-scripts.txt') }}

.github/workflows/wb_ci.yaml
@@ -16,7 +16,7 @@ jobs:
         python-version: '3.12'
         architecture: 'x64'
     - name: Restore dependencies cache
-      uses: actions/cache@v3
+      uses: actions/cache@v4
       with:
         path: ~\AppData\Local\pip\Cache
         key: ${{ runner.os }}-pip-${{ hashFiles('requirements.txt') }}

commit df1fe085797831e2d16d661c708bc59af499d75f

Fix broken DataFileExists logic
I broke this back in 243897772ec2314000dfee473abf6a8b6af3bc1c. It's
supposed to return True iff all files exist, but I made it short-circuit
as soon as a single plugin specified in the list exists.

sibir:

docs: Correct Camera: Chase Distance docs

Only exists in TES4, FO3, & FNV.

Co-authored-by: sibir <ine.sibir@gmail.com>

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -10099,7 +10099,8 @@ noted in the description of the tweak.</p>
             <td>Changes the maximum distance the chase camera can be moved away
                 from the player. Very useful for creating a bird's eye view –
                 but also a little bit unbalancing for that reason.</td>
-            <td>All games but Fallout 4</td>
+            <td>Oblivion, Nehrim&colon; At Fate's Edge, Fallout 3 &amp;
+                Fallout&colon; New Vegas</td>
         </tr>
         <tr>
             <td id="patch-tweak-settings-camera-chase-tightness">Camera: Chase

Mopy/Docs/Wrye Bash Technical Readme.html
@@ -205,8 +205,11 @@
     <li><code>0</code> - Installed version is equal to the version specified in <var>version_string</var>.
     <li><code>1</code> - Installed version is greater than the version specified in <var>version_string</var>.
 </ul>
-<p><dfn id="wizardsDataFileExists">DataFileExists</dfn> - Tests for the existence of a file in the Data directory. If the file you are
-    testing for is a plugin, this will also detect ghosted versions of the file.</p>
+<p><dfn id="wizardsDataFileExists">DataFileExists</dfn> - Tests for the
+    existence of one or more files, relative to the Data directory. If the
+    files you are testing for are plugins, this will also detect ghosted
+    versions of them. All specified files must exist for this to return
+    <code>True</code>.</p>
 <code class="box">DataFileExists(file_name [, ..., file_name_n])</code>
 <ul>
     <li><var>file_name</var> - A string or variable holding a string,

Mopy/bash/basher/belt.py
@@ -596,7 +596,7 @@ class WryeParser(PreParser):
     def fnDataFileExists(self, *rel_paths):
         for rel_path in rel_paths:
             if rel_path in bosh.modInfos:
-                return True # It's a (potentially ghosted) plugin
+                continue # It's a (potentially ghosted) plugin, check next
             rel_path_os = to_os_path(bass.dirs['mods'].join(rel_path))
             if not rel_path_os or not rel_path_os.exists():
                 return False