wrye-bash/wrye-bash

commit 1c5a9506b2e585957530679b60338b12c9f04f01

DataTable(PickleDict):
A DataDict override down - especially welcome nuking hasChanged. I could
also nuke the class completely but keeping it around for now.

Mopy/bash/basher/saves_links.py
@@ -678,8 +678,8 @@ class Save_Move(ChoiceLink):
     def _move_saves(self, destDir, profile: str | None):
         #--bashDir
         destTable = bolt.DataTable(bolt.PickleDict(destDir.join(
-            u'Bash', u'Table.dat')))
-        count = 0
+            'Bash', 'Table.dat')), load_pickle=True)
+        count = do_save = 0
         ask = True
         for fileName, save_inf in self.iselected_pairs():
             if ask and destDir.join(fileName).exists():
@@ -696,12 +696,13 @@ class Save_Move(ChoiceLink):
                 ask = ask and result != 2 # so don't warn for rest of operation
             if self.copyMode:
                 bosh.saveInfos.copy_info(fileName, destDir, fileName)
-                if att_dict := save_inf.get_persistent_attrs():
-                    destTable[fileName] = att_dict
             else:
                 bosh.saveInfos[fileName].move_info(destDir)
+            if att_dict := save_inf.get_persistent_attrs():
+                destTable.pickled_data[fileName] = att_dict
+                do_save = 1
             count += 1
-        destTable.save()
+        if do_save: destTable.save()
         return count
 
 #------------------------------------------------------------------------------

Mopy/bash/bolt.py
@@ -2105,44 +2105,20 @@ def unpack_spaced_string(ins, replacement_char=b'\x07') -> bytes:
     return b''.join(wip_string)
 
 #------------------------------------------------------------------------------
-class DataTable(DataDict):
+class DataTable(PickleDict):
     """Simple data table of rows and columns, saved in a pickle file. It is
-    currently used by TableFileInfos to represent properties associated with
+    currently used by TableFileInfos to pickle properties associated with
     mod/save/bsa/ini files, where each file is a row, and each property (e.g.
     modified date or 'mtime') is a column.
 
-    The "table" is actually a dictionary of dictionaries. E.g.
-        propValue = table['fileName']['propName']
+    self.pickled_data is actually a dictionary of dictionaries. E.g.
+        propValue = self.pickled_data['fileName']['propName']
     Rows are the first index ('fileName') and columns are the second index
     ('propName')."""
 
-    def __init__(self, dictFile: PickleDict):
-        """Initialize and read data from dictFile, if available."""
-        self.dictFile = dictFile
-        dictFile.load()
-        self.vdata = dictFile.vdata
-        self.dictFile.pickled_data = _data = forward_compat_path_to_fn(
-            self.dictFile.pickled_data)
-        super().__init__(_data)
-        self.hasChanged = False ##: move to PickleDict
-
-    def save(self):
-        """Saves to pickle file."""
-        dictFile = self.dictFile
-        if self.hasChanged and not dictFile.readOnly:
-            dictFile.pickled_data = self._data # note we reassign pickled_data
-            self.hasChanged = not dictFile.save()
-
-    #--Dictionary emulation
-    def __setitem__(self,key,value):
-        self._data[key] = value
-        self.hasChanged = True
-    def __delitem__(self,key):
-        del self._data[key]
-        self.hasChanged = True
-    def pop(self,key,default=None):
-        self.hasChanged = True
-        return self._data.pop(key, default)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pickled_data = forward_compat_path_to_fn(self.pickled_data)
 
 # Util Functions --------------------------------------------------------------
 #------------------------------------------------------------------------------

Mopy/bash/bosh/__init__.py
@@ -1812,9 +1812,8 @@ class TableFileInfos(_AFileInfos):
         """Load pickled data for mods, saves, inis and bsas."""
         deprint(f' bash_dir: {self.bash_dir}') # self.store_dir may need be set
         self.bash_dir.makedirs()
-        pickled = bolt.PickleDict(self.bash_dir.join('Table.dat'),
-                                  load_pickle=True)
-        return forward_compat_path_to_fn(pickled.pickled_data)
+        return bolt.DataTable(self.bash_dir.join('Table.dat'),
+                              load_pickle=True).pickled_data
 
     def refresh(self, *args, **kwargs):
         if not self._table_loaded:
@@ -1823,11 +1822,10 @@ class TableFileInfos(_AFileInfos):
         return super().refresh(*args, **kwargs)
 
     def save_pickle(self):
-        pd = bolt.PickleDict(self.bash_dir.join('Table.dat')) # don't load!
+        pd = bolt.DataTable(self.bash_dir.join('Table.dat')) # don't load!
         for k, v in self.items():
             if pickle_dict := v.get_persistent_attrs():
                 pd.pickled_data[k] = pickle_dict
-        # fixme do we need to update self.vdata too??
         pd.save()
 
 class Corrupted(AFile):

commit d17a287f16164c33614ca614b322fb1af6289439

Regression fixups in saveSettings:
- in Morrowind activating more than one plugin at a time, it wipes the
[Game Files] section & just keeps the last active plugin - from
fa4dfd2a261fa655d334a028babf843e89107144
- extra newlines get inserted before each section header every time
changes are made - 5f3b6b1709152f9fff33deb074c5f9c72e845f36

Well if we wanted to be really correct we should keep the previous line
and check if it's an empty line (whitespace only) - then only add a
newline if not. Or we could only add newlines after the loop that
reads the ini lines, so for new sections. Or, as I do here, stop writing
the newlines altogether.

Mopy/bash/basher/saves_links.py: regression from
1c5a9506b2e585957530679b60338b12c9f04f01 - this commit saw lots of
iterations so at some point I was passing a pickle dict instead of
a Path, duh. Rest is backup renames - a Path attribute.

Mopy/bash/bash.py
@@ -668,13 +668,13 @@ def _main(opts, wx_locale, wxver):
                 defaultDir=base_dir, wildcard='*.7z', defaultFile=bkf)
         if settings_file:
             with gui.BusyCursor():
-                backup = barb.BackupSettings(
+                bkp_setts = barb.BackupSettings(
                     settings_file, bush_game.bak_game_name,
                     bush_game.my_games_name, bush_game.bash_root_prefix,
                     bush_game.mods_dir)
             try:
                 with gui.BusyCursor():
-                    backup.backup_settings(balt)
+                    bkp_setts.backup_settings(balt)
             except exception.StateError:
                 msg = [_('There was an error while trying to backup the Wrye '
                          'Bash settings!'),

Mopy/bash/basher/saves_links.py
@@ -675,8 +675,8 @@ class Save_Move(ChoiceLink):
 
     def _move_saves(self, destDir, profile: str | None):
         #--bashDir
-        destTable = bolt.DataTable(bolt.PickleDict(destDir.join(
-            'Bash', 'Table.dat')), load_pickle=True)
+        destTable = bolt.DataTable(destDir.join('Bash', 'Table.dat'),
+                                   load_pickle=True)
         count = do_save = 0
         ask = True
         for fileName, save_inf in self.iselected_pairs():

Mopy/bash/basher/settings_dialog.py
@@ -757,15 +757,15 @@ class BackupsPage(_AFixedPage):
                 bush.game.bak_game_name))
         if not settings_file: return
         with BusyCursor():
-            backup = barb.BackupSettings(
+            bkp_setts = barb.BackupSettings(
                 settings_file, bush.game.bak_game_name,
                 bush.game.my_games_name, bush.game.bash_root_prefix,
                 bush.game.mods_dir)
         try:
-            with BusyCursor(): backup.backup_settings(balt)
+            with BusyCursor(): bkp_setts.backup_settings(balt)
         except exception.StateError:
             deprint(u'Backup settings failed', traceback=True)
-            backup.warn_message(balt)
+            bkp_setts.warn_message(balt)
         finally:
             self._populate_backup_list()
 

Mopy/bash/bolt.py
@@ -1888,14 +1888,14 @@ class PickleDict(object):
     def __init__(self, pkl_path, readOnly=False, load_pickle=False):
         """Initialize."""
         self._pkl_path = pkl_path
-        self.backup = pkl_path.backup
+        self._backup = pkl_path.backup
         self.readOnly = readOnly
         self.vdata = {}
         self.pickled_data = {}
         if load_pickle: self.load()
 
     def exists(self):
-        return self._pkl_path.exists() or self.backup.exists()
+        return self._pkl_path.exists() or self._backup.exists()
 
     class Mold(Exception):
         def __init__(self, moldedFile):
@@ -1925,7 +1925,7 @@ class PickleDict(object):
         def _perform_load():
             self.vdata.update(pickle.load(ins, encoding='bytes'))
             self.pickled_data.update(pickle.load(ins, encoding='bytes'))
-        for path in (self._pkl_path, self.backup):
+        for path in (self._pkl_path, self._backup):
             if cor is not None:
                 cor.moveTo(cor_name)
                 cor = None
@@ -1957,7 +1957,7 @@ class PickleDict(object):
                     # saving over it
                     path.copyTo(path.root + '-vdata2.dat.bak') # Path.__add__!
                     self.save()
-                return 1 + (path == self.backup)
+                return 1 + (path == self._backup)
             except (OSError, EOFError, ValueError,
                     pickle.UnpicklingError): #PY3:FileNotFound
                 pass
@@ -1980,7 +1980,7 @@ class PickleDict(object):
                 for pkl in ('VDATA3', self.vdata, self.pickled_data):
                     pickle.dump(pkl, out, -1)
             try:
-                self._pkl_path.copyTo(self._pkl_path.backup)
+                self._pkl_path.copyTo(self._backup)
             except FileNotFoundError:
                 pass # No settings file to back up yet, this is fine
             self._pkl_path.replace_with_temp(temp_pkl)

Mopy/bash/ini_files.py
@@ -360,7 +360,7 @@ class IniFileInfo(AIniInfo, AFileInfo):
                 for line in self.read_ini_content(missing_ok=True):
                     stripped, setting, val, new_section, is_del = \
                         self.parse_ini_line(line, inline_comments=True)
-                    if setting: # modify? we need be in a section
+                    if setting and not skip: # modify? we need be in a section
                         sect = new_section or section
                         try: # Check if we have a value for this setting
                             value = ini_settings[sect].pop(setting) # KeyError
@@ -378,8 +378,6 @@ class IniFileInfo(AIniInfo, AFileInfo):
                     elif new_section: # we never enter this block for OBSEIni
                         # 'new' entries still to be added from previous section
                         _add_remaining_new_items()
-                        if section is not None:
-                            tmp_ini.write('\n') # newline between sections
                         section = new_section # _add_remaining uses the section
                         if skip := section.lower() in skip_sections:
                             if section in ini_settings:
@@ -391,9 +389,6 @@ class IniFileInfo(AIniInfo, AFileInfo):
                 # Add remaining new entries that were not in the ini file
                 for sect, sectionSettings in ini_settings.items():
                     if sectionSettings and not isinstance(self, OBSEIniFile):
-                        if section is not None:
-                            tmp_ini.write('\n') #separate section from previous
-                        section = sect
                         tmp_ini.write(f'[{sect}]\n')
                     _add_remaining_new_items(sectionSettings)
             self.abs_path.replace_with_temp(tmp_ini_path)