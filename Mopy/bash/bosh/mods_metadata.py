wrye-bash/wrye-bash

commit e9fca800568fbec0ca9b90b64bcb5c3a5a725198

WIP absorb more specialized handling in PluginsFlag
Still thinking about mergeable_plugins - could absorb into MasterFlag or
not - WIP (some of it will be moved to MergeabilityCheck).

Mopy/bash/bosh/mods_metadata.py
@@ -202,8 +202,6 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     all_corrupted = modInfos.corrupted
     # -------------------------------------------------------------------------
     can_merge = modInfos.mergeable_plugins
-    can_esl_flag = modInfos.esl_capable_plugins
-    can_overlay_flag = modInfos.overlay_capable_plugins
     # Don't show NoMerge-tagged plugins as mergeable and remove ones that have
     # already been merged into a BP
     for m in list(can_merge):
@@ -667,19 +665,15 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         log(_(u'Wrye Bash could not read the follow plugins. They most likely '
               u'have corrupt or otherwise malformed headers.'))
         log_plugin_messages(all_corrupted) ##: Just log_plugins?
-    if can_esl_flag:
-        _log_plugins('=== ' + _('ESL-Capable'),
-        _('The following plugins could be assigned an ESL flag, but do '
-          'not have one right now.'), can_esl_flag)
+    for pflag in bush.game.scale_flags:
+        minfos_cache, head, msg = pflag.cached_types(modInfos)
+        if minfos_cache:
+            _log_plugins(head, msg, minfos_cache)
     if remove_esl_flag:
         _log_plugins(u'=== ' + _('Incorrect ESL Flag'), _(
             'The following plugins have an ESL flag, but do not qualify. '
             "Either remove the flag with 'Remove ESL Flag', or "
             "change the extension to '.esp' if it is '.esl'."),remove_esl_flag)
-    if can_overlay_flag:
-        _log_plugins('=== ' + _('Overlay-Capable'),
-        _('The following plugins could be assigned an Overlay flag, but '
-          'do not have one right now.'), can_overlay_flag)
     if overlays_with_new_recs:
         _log_plugins('=== ' + _('Incorrect Overlay Flag: New Records'),
         _("The following plugins have an Overlay flag, but do not "

Mopy/bash/basher/__init__.py
@@ -378,12 +378,11 @@ class _ModsUIList(UIList):
                     mouseText += _('Merged into Bashed Patch.') + ' '
                 else:
                     mouseText += _('Can be merged into Bashed Patch.') + ' '
-        if item_name in bosh.modInfos.esl_capable_plugins:
-            item_format.text_key = 'mods.text.mergeable'
-            mouseText += _('Can be ESL-flagged.') + ' '
-        if item_name in bosh.modInfos.overlay_capable_plugins:
-            item_format.text_key = 'mods.text.mergeable'
-            mouseText += _('Can be Overlay-flagged.') + ' '
+        for pflag in bush.game.scale_flags:
+            if pflag.can_flag(item_name, bosh.modInfos):
+                item_format.text_key = 'mods.text.mergeable'
+                mouseText += (_('Can be %(plugin_type)s-flagged.') + ' '
+                              ) % {'plugin_type': pflag.name.title()}
         final_text_key = 'mods.text.es'
         if minf.is_esl():
             final_text_key += 'l'

Mopy/bash/basher/mod_links.py
@@ -1532,8 +1532,8 @@ class _AFlipFlagLink(EnabledLink):
         first_flagged = self._already_flagged
         self._flag_value = not first_flagged # flip the (common) flag value
         return all(m.fn_ext in self._allowed_ext and
-                   self._plugin_flag.has_flagged(minfo) == first_flagged and
-                   (first_flagged or self._can_flag(m))
+            self._plugin_flag.has_flagged(minfo) == first_flagged and
+            (first_flagged or self._plugin_flag.can_flag(m, bosh.modInfos))
                    for m, minfo in self.iselected_pairs())
     @property
     def link_text(self):
@@ -1556,10 +1556,6 @@ class _AFlipFlagLink(EnabledLink):
             # plugin that was affected to update the Indices column
             self.window.RefreshUI(refresh_others=Store.SAVES.DO(), redraw={
                 *self.selected, *ldiff.reordered, *ldiff.act_index_change})
-
-    def _can_flag(self, fn_mod):
-        """Check cached ModInfos info on whether we can flag this mod."""
-        return True
 
     @property
     def _already_flagged(self):
@@ -1585,17 +1581,11 @@ class Mod_FlipEsl(_AFlipFlagLink):
     _help = _('Flip the ESL flag on the selected plugins, turning light '
               'plugins into regular ones and vice versa.')
 
-    def _can_flag(self, modinf):
-        return modinf in bosh.modInfos.esl_capable_plugins
-
 #------------------------------------------------------------------------------
 class Mod_FlipOverlay(_AFlipFlagLink):
     """Add or remove the Overlay flag. Extension must be .esm, .esp or .esu."""
     _help = _('Flip the Overlay flag on the selected plugins, turning overlay '
               'plugins into regular ones and vice versa.')
-
-    def _can_flag(self, fn_mod):
-        return fn_mod in bosh.modInfos.overlay_capable_plugins
 
     def _exec_flip(self):
         message = (_('WARNING! For advanced modders only!') + '\n\n' +

Mopy/bash/bosh/__init__.py
@@ -2116,18 +2116,6 @@ class ModInfos(TableFileInfos):
         already *be* merged - see ModInfos.merged)."""
         return self._mergeable_by_type[MergeabilityCheck.MERGE]
 
-    @property
-    def esl_capable_plugins(self) -> set[FName]:
-        """All plugins that could receive an ESL flag, but don't have one
-        yet."""
-        return self._mergeable_by_type[MergeabilityCheck.ESL_CHECK]
-
-    @property
-    def overlay_capable_plugins(self) -> set[FName]:
-        """All plugins that could receive al Overlay flag, but don't have one
-        yet."""
-        return self._mergeable_by_type[MergeabilityCheck.OVERLAY_CHECK]
-
     # Load order API for the rest of Bash to use - if the load order or
     # active plugins changed, those methods run a refresh on modInfos data
     @_lo_cache

Mopy/bash/game/__init__.py
@@ -115,6 +115,15 @@ class PluginFlag(Enum):
         flag state and possibly on the file extension."""
         return getattr(mod_info, self._mod_info_attr)
 
+    def can_flag(self, fn_mod, mod_infos):
+        """Check if the self._flag_attr can be set on the mod info flags -
+        for now delegate to the modInfos cache."""
+        try:
+            return fn_mod in getattr(mod_infos, '_mergeable_by_type')[
+                self.merge_check]
+        except (AttributeError, KeyError):
+            return True
+
     @classmethod
     def plugin_counts(cls, mod_infos, active_mods):
         counts = Counter(dict.fromkeys((mem.name for mem in cls), 0))
@@ -154,6 +163,20 @@ class EslMixin(PluginFlag):
         super().__init__(flag_attr, mod_info_attr)
         self.max_plugins = max_plugins
         self.merge_check = merge_check
+
+    def cached_types(self, mod_infos):
+        match self.merge_check: # I use MergeabilityCheck to avoid overriding
+            case MergeabilityCheck.ESL_CHECK:
+                h, m = '=== ' + _('ESL-Capable'), _(
+                    'The following plugins could be assigned an ESL flag, but '
+                    'do not have one right now.')
+            case MergeabilityCheck.OVERLAY_CHECK:
+                h, m = '=== ' + _('Overlay-Capable'), _(
+                    'The following plugins could be assigned an Overlay flag, '
+                    'but do not have one right now.')
+            case _:
+                return None, '', ''
+        return getattr(mod_infos, '_mergeable_by_type')[self.merge_check], h, m
 
     def set_mod_flag(self, mod_info, set_flag):
         if super().set_mod_flag(mod_info, set_flag):

commit 90f88eacee892e13873c6dcf0d4b0a4beca3e5c5

Fix regression breaking Plugin Checker
Broke in e9fca800568fbec0ca9b90b64bcb5c3a5a725198.

Mopy/bash/bosh/mods_metadata.py
@@ -635,7 +635,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         if pflag.merge_check is not None:
             minfos_cache, head, msg = pflag.merge_check.cached_types(modInfos)
             if minfos_cache:
-                _log_plugins(head, msg, minfos_cache)
+                _log_plugins(head, msg, [minf.fn_key for minf in minfos_cache])
         for (head, msg), pl_set in flag_errors.get(pflag, {}).items():
             if pl_set:
                 _log_plugins(head, msg, pl_set)