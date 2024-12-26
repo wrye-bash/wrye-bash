wrye-bash/wrye-bash

commit d12c9ede2125986481de6ed387a3fcd86d82d3c4

Nit in copyTo:
The only use of a dir is in projects and it is still WIP:

- use AFile for the lo files (done in a couple merges)
- copyTo needs to be partly absorbed in AFile (see copy_installer) -
also done later.

Mopy/bash/bolt.py
@@ -1205,23 +1205,19 @@ class Path(os.PathLike):
                 os.startfile(self._s)
             else: ##: TTT linux - WIP move this switch to env launch_file
                 subprocess.call(['xdg-open', f'{self._s}'])
-    def copyTo(self,destName):
-        """Copy self to destName, make dirs if necessary and preserve ftime."""
-        destName = GPath(destName)
+    def copyTo(self, dest_path, set_time=None):
+        """Copy self to dest_path make dirs if necessary and preserve ftime."""
+        dest_path = GPath(dest_path)
         if self.is_dir():
-            ##: Does not preserve mtimes - is that a problem?
-            shutil.copytree(self._s, destName._s,
-                copy_function=copy_or_reflink2)
-            return
+            raise exception.StateError(f'{self._s} is a directory.')
         try:
-            copy_or_reflink(self._s, destName._s)
-            destName.mtime = self.mtime
+            copy_or_reflink(self._s, dest_path._s)
+            dest_path.mtime = set_time or self.mtime
         except FileNotFoundError:
-            if not (dest_par := destName.shead) or os.path.exists(dest_par):
+            if not (dest_par := dest_path.shead) or os.path.exists(dest_par):
                 raise
             os.makedirs(dest_par)
-            copy_or_reflink(self._s, destName._s)
-            destName.mtime = self.mtime
+            self.copyTo(dest_path, set_time)
     def moveTo(self, destName, *, check_exist=True):
         if check_exist and not self.exists():
             raise exception.StateError(f'{self._s} cannot be moved because it '

Mopy/bash/bosh/bain.py
@@ -28,6 +28,7 @@ import copy
 import io
 import os
 import re
+import shutil
 import sys
 import time
 from collections import defaultdict
@@ -45,7 +46,8 @@ from ..archives import compress7z, defaultExt, extract7z, list_archive, \
 from ..bass import Store
 from ..bolt import AFile, CIstr, FName, GPath_no_norm, ListInfo, Path, \
     SubProgress, deprint, dict_sort, forward_compat_path_to_fn, \
-    forward_compat_path_to_fn_list, round_size, top_level_items, DefaultFNDict
+    forward_compat_path_to_fn_list, round_size, top_level_items, DefaultFNDict, \
+    copy_or_reflink2
 from ..exception import ArgumentError, BSAError, CancelError, FileError, \
     InstallerArchiveError, SkipError, StateError
 from ..ini_files import OBSEIniFile, supported_ini_exts
@@ -2029,9 +2031,15 @@ class InstallersData(DataStore):
 
     def copy_installer(self, src_inst, destName):
         """Copies archive to new location."""
-        src_inst.abs_path.copyTo(self.store_dir.join(destName))
+        dest_path = self.store_dir.join(destName)
+        if proj := src_inst.is_project:
+            # does not preserve mtimes so next do_update will return True?
+            shutil.copytree(src_inst.abs_path.s, dest_path.s,
+                            copy_function=copy_or_reflink2)
+        else:
+            src_inst.abs_path.copyTo(dest_path)
         clone = self.new_info(destName,
-            is_proj=src_inst.is_project, install_order=src_inst.order + 1,
+            is_proj=proj, install_order=src_inst.order + 1,
             do_refresh=False, # we only need to call refresh_n()
             load_cache=False) # don't load from disc - copy all attributes over
         atts = (*Installer.persistent, *Installer.volatile) # drop fn_key

Mopy/bash/_games_lo.py
@@ -239,10 +239,10 @@ class LoGame(object):
                               "must_be_active_if_present!")
 
     def _plugins_txt_modified(self):
-        exists = self.plugins_txt_path.exists()
+        exists = (pl_path := self.plugins_txt_path).exists()
         if not exists and self.mtime_plugins_txt: return True # deleted !
         return exists and ((self.size_plugins_txt, self.mtime_plugins_txt) !=
-                           self.plugins_txt_path.size_mtime())
+                           pl_path.size_mtime())
 
     # API ---------------------------------------------------------------------
     def get_load_order(self, cached_load_order: LoList,
@@ -384,15 +384,13 @@ class LoGame(object):
         # If this game has no plugins.txt, don't try to swap it
         if not self.__class__.has_plugins_txt: return False
         # Save plugins.txt inside the old (saves) directory
-        if self.plugins_txt_path.exists():
-            self.plugins_txt_path.copyTo(_resolve_case_ambiguity(old_dir.join(
-                self.plugins_txt_path.stail)))
+        if (pl_path := self.plugins_txt_path).exists():
+            pl_path.copyTo(_resolve_case_ambiguity(
+                old_dir.join(pl_path.stail)))
         # Move the new plugins.txt here for use
-        move = _resolve_case_ambiguity(new_dir.join(
-            self.plugins_txt_path.stail))
-        if move.exists():
-            move.copyTo(self.plugins_txt_path)
-            self.plugins_txt_path.mtime = time.time() # copy will not change mtime, bad
+        move = _resolve_case_ambiguity(new_dir.join(pl_path.stail))
+        if move.exists(): # copy will not change mtime, bad
+            move.copyTo(pl_path, set_time=time.time())
             return True
         return False
 
@@ -998,9 +996,8 @@ class TextfileGame(_CleanPlugins):
         # Move the new loadorder.txt here for use
         move = _resolve_case_ambiguity(new_dir.join(
             self.loadorder_txt_path.stail))
-        if move.exists():
-            move.copyTo(self.loadorder_txt_path)
-            self.loadorder_txt_path.mtime = time.time() # update mtime to trigger refresh
+        if move.exists(): # update mtime to trigger refresh
+            move.copyTo(self.loadorder_txt_path, set_time=time.time())
             return True
         return False
 
@@ -1022,8 +1019,8 @@ class TextfileGame(_CleanPlugins):
         try:
             self.loadorder_txt_path.copyTo(self.loadorder_txt_path.backup)
         except OSError:
-            bolt.deprint(f'Tried to back up {self.loadorder_txt_path},'
-                         f' but it did not exist')
+            bolt.deprint(f'Tried to back up {self.loadorder_txt_path}, '
+                         f'but it did not exist')
 
     def _fetch_load_order(self, cached_load_order,
             cached_active: tuple[FName] | list[FName]):

commit 85b337f112eb2989d5edace3eaa647c451203434

Fixup for backup failing to backup dirs:
Probably broke in d12c9ede2125986481de6ed387a3fcd86d82d3c4 - reverted.
Incidentally we get a PermissionError - should be an IsADirectoryError.

Traceback (most recent call last):
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\bash.py", line 524, in main
    _main(opts, wx_locale, wxver)
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\bash.py", line 681, in _main
    bkp_setts.backup_settings(balt)
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\barb.py", line 155, in backup_settings
    self._backup_settings(GPath_no_norm(temp_settings_backup_dir))
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\barb.py", line 162, in _backup_settings
    fpath.copyTo(temp_dir.join(tpath))
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\bolt.py", line 1244, in copyTo
    copy_or_reflink(self._s, dest_path._s)
  File "D:\Dropbox (Personal)\eclipse_workspaces\python\wrye-bash\Mopy\bash\bolt.py", line 2752, in copy_or_reflink
    shutil.copyfile(a, b)
  File "C:\Users\MrD\AppData\Local\Programs\Python\Python312\Lib\shutil.py", line 260, in copyfile
    with open(src, 'rb') as fsrc:
         ^^^^^^^^^^^^^^^
PermissionError: [Errno 13] Permission denied: 'D:\\Dropbox (Personal)\\GAMES\\TESIV\\Oblivion\\Data\\Bash Patches\\ConjurationTomeOOO-OBSE.esp Exported Scripts'

Nit around Installer __init__:

Keyword arguments are absorbed in Installer.__init__ - I am after
instantiating ListInfos directly - should not happen (use factory/
new_info, go through the store) but it's difficult to track. In
particular that's why I want to get rid of the bosh.Installer***
classes - here I make _inst_types use once - I followed instantiations
manually and I believe InstallerPackage params are always passed as
kwargs.

Mopy/bash/basher/__init__.py
@@ -3010,18 +3010,18 @@ class InstallersDetails(_SashDetailsPanel):
             nConfigured = len(installer.ci_dest_sizeCrc)
             nMissing = len(installer.missingFiles)
             nMismatched = len(installer.mismatchedFiles)
-            is_mark = installer.is_marker
+            ismark = installer.is_marker
             numstr = partial(installer.number_string, marker_string='N/A')
             inf_.extend([
                 _('Modified: %(modified_date)s') % {
-                    'modified_date': 'N/A' if is_mark else
+                    'modified_date': 'N/A' if ismark else
                     format_date(installer.ftime)},
                 _('Data CRC: %(data_crc)s') % {
-                    'data_crc': 'N/A' if is_mark else f'{installer.crc:08X}'},
+                    'data_crc': 'N/A' if ismark else f'{installer.crc:08X}'},
                 _('Files: %(num_files)s') % {
                     'num_files': f'{numstr(installer.num_of_files)}'},
                 _('Configured: %(files_and_size)s') % {
-                    'files_and_size': 'N/A' if is_mark else
+                    'files_and_size': 'N/A' if ismark else
                     f'{nConfigured:d} ({round_size(installer.unSize)})'},
                 '  ' + _('Matched: %(num_files)s') % {
                     'num_files': numstr(nConfigured - nMissing - nMismatched)},

Mopy/bash/bolt.py
@@ -1240,6 +1240,11 @@ class Path(os.PathLike):
     def copyTo(self, dest_path, set_time=None):
         """Copy self to dest_path make dirs if necessary and preserve ftime."""
         dest_path = GPath(dest_path)
+        if self.is_dir():
+            ##: Does not preserve mtimes - is that a problem?
+            shutil.copytree(self._s, dest_path._s,
+                copy_function=copy_or_reflink2)
+            return
         try:
             copy_or_reflink(self._s, dest_path._s)
             dest_path.mtime = set_time or self.mtime

Mopy/bash/bosh/bain.py
@@ -1178,7 +1178,7 @@ class Installer(ListInfo):
 class _InstallerPackage(Installer, AFileInfo):
     """Installer that corresponds to a file system node (archive or folder)."""
 
-    def __init__(self, fn_key, progress=None, load_cache=False):
+    def __init__(self, fn_key, *, progress=None, load_cache=False):
         super().__init__(fn_key) # will call Installer -> ListInfo __init__
         self._file_key = bass.dirs['installers'].join(self.fn_key)
         if load_cache: # load from disc, useful when adding a new installer
@@ -1862,12 +1862,12 @@ class InstallersData(DataStore):
         if not is_mark:
             progress = progress if _index is None else SubProgress(
                 progress, _index, _index + 1)
-            info = self[fileName] = self._inst_types[is_proj](
-                fileName, progress=progress, load_cache=load_cache)
         else:
-            info = self[fileName] = self._inst_types[2](fileName)
+            is_proj = 2
             if install_order is None:
                 install_order = self[self.lastKey].order
+        info = self[fileName] = self._inst_types[is_proj](
+            fileName, progress=progress, load_cache=load_cache)
         if install_order is not None:
             self.moveArchives([fileName], install_order)
         if progress and not is_mark: progress(1.0, _('Done'))

Mopy/bash/tests/__init__.py
@@ -44,8 +44,8 @@ def get_meta_value(base_file_path, meta_key):
     """Returns the value corresponding to the given meta key from the meta file
     for the specified file. Gives helpful error messages if the file is
     missing, malformed, is missing the specified key, etc."""
-    base_file_path = u'%s' % base_file_path # To support bolt.Path as well
-    meta_file = base_file_path + u'.meta'
+    base_file_path = f'{base_file_path}' # To support bolt.Path as well
+    meta_file = f'{base_file_path}.meta'
     try:
         parsed_meta = _meta_cache[base_file_path]
     except KeyError:
@@ -53,16 +53,15 @@ def get_meta_value(base_file_path, meta_key):
             with open(meta_file, 'rb') as ins:
                 parsed_meta = _meta_cache[base_file_path] = tomllib.load(ins)
         except FileNotFoundError:
-            raise FailedTest(u'%s is missing a .meta file.' % base_file_path)
+            raise FailedTest(f'{base_file_path} is missing a .meta file.')
         except tomllib.TOMLDecodeError:
             traceback.print_exc()
-            raise FailedTest(u'%s has malformed TOML syntax. Check the log '
-                             u'for a traceback pointing to the '
-                             u'problem.' % meta_file)
+            raise FailedTest(f'{meta_file} has malformed TOML syntax. Check '
+                f'the log for a traceback pointing to the problem.')
     try:
         return parsed_meta[meta_key]
     except KeyError:
-        raise FailedTest(u"%s is missing the key '%s'" % (meta_file, meta_key))
+        raise FailedTest(f"{meta_file} is missing the key '{meta_key}'")
 
 def iter_games(resource_subfolder):
     """Yields all games for which resources from the specified subfolder are