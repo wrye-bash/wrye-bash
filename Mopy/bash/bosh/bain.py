wrye-bash/wrye-bash

commit fcdabd874959522ff9edc4cecbe3f410aff77635

bosh._mergeability -> plugin_types.py:
Move plugin types handling at top level - we have enough variety now to
need a centralized model. Note I had to move the mod_files import in
local scope - this is #600 territory, but this commit already simplifies
stuff as GameInfo can freely import isPBashMergeable and MasterFlag. The
former will allow making mergeability checks a dict reducing flags
handling in client code. I added comments and rearranged method
definitions - with the help of the IDE's marking of overrides this
makes it easier to see what are the responsibilities of each (sub)class.

Note _pbash_mergeable_no_load is a quick check for MERGE

Mopy/bash/bosh/mods_metadata.py
@@ -30,7 +30,7 @@ from ..bolt import SubProgress, dict_sort, sig_to_str, structs_cache
 from ..brec import ModReader, RecordHeader, RecordType, ShortFidWriteContext, \
     SubrecordBlob, unpack_header
 from ..exception import CancelError
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..mod_files import ModHeaderReader
 from ..wbtemp import TempFile
 
@@ -210,11 +210,12 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     # Check for ESL-flagged plugins that aren't ESL-capable and Overlay-flagged
     # plugins that shouldn't be Overlay-flagged. Also check for conflicts
     # between ESL and Overlay flags.
+    pflags = bush.game.scale_flags
     flag_errors = {k: {h_msg: set() for h_msg in v} for k, v in
-                   bush.game.scale_flags.error_msgs.items()}
+                   pflags.error_msgs.items()}
     for m, modinf in modInfos.items():
-        for pflag in bush.game.scale_flags:
-            if pflag in type(pflag).error_msgs and pflag.cached_type(modinf):
+        for pflag in pflags:
+            if pflag in pflags.error_msgs and pflag.cached_type(modinf):
                 pflag.validate_type(modinf, flag_errors[pflag].values())
     # -------------------------------------------------------------------------
     # Check for Deactivate-tagged plugins that are active and
@@ -598,7 +599,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         if raw_eid:
             ret_fmt = f'{raw_eid} {ret_fmt}'
         return ret_fmt
-    format_fid = bush.game.scale_flags.format_fid
+    format_fid = pflags.format_fid
     def log_collision(coll_fid, coll_inj, coll_plugin, coll_versions):
         """Logs a single collision with the specified FormID, injected status,
         origin plugin and collision info."""
@@ -636,7 +637,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         log(_(u'Wrye Bash could not read the follow plugins. They most likely '
               u'have corrupt or otherwise malformed headers.'))
         log_plugin_messages(all_corrupted) ##: Just _log_plugins?
-    for pflag in bush.game.scale_flags:
+    for pflag in pflags:
         if pflag.merge_check is not None:
             minfos_cache, head, msg = pflag.merge_check.cached_types(modInfos)
             if minfos_cache:

Mopy/bash/plugin_types.py
@@ -1,0 +1,323 @@
+# -*- coding: utf-8 -*-
+#
+# GPL License and Copyright Notice ============================================
+#  This file is part of Wrye Bash.
+#
+#  Wrye Bash is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation, either version 3
+#  of the License, or (at your option) any later version.
+#
+#  Wrye Bash is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Wrye Bash.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  Wrye Bash copyright (C) 2005-2009 Wrye, 2010-2024 Wrye Bash Team
+#  https://github.com/wrye-bash
+#
+# =============================================================================
+"""Houses data structures to represent plugin types - usually these are set
+as flags but file extension may play a role. The PluginFlag enum is used to
+define the various flags a plugin can have, while the MergeabilityCheck enum
+is used to define the various mergeability checks that a game can have. We
+might need game specific information (check the game_handle argument) but these
+classes are above GameInfo - keep this module top level. PF was created after
+MC as plugin types started proliferating - their contract is still WIP."""
+import os
+from collections import Counter
+from enum import Enum
+
+# we are imported early (in game/__init__), so only import from library modules
+from .bolt import sig_to_str
+from .exception import ModError
+
+__exit = lambda x: True # trick to exit early on non-verbose mode
+
+def _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
+    _exit = __exit if reasons is None else reasons.append # append returns None
+    if MasterFlag.ESM.has_flagged(modInfo) and _exit(_('This plugin has the ESM flag.')):
+        return False
+    #--Bashed Patch
+    if modInfo.isBP() and _exit(_('This plugin is a Bashed Patch.')):
+        return False
+    # Plugin INIs would get deactivated if the plugin got merged
+    if ((plugin_ini_name := modInfo.get_ini_name()) in minfos.plugin_inis and
+            _exit(_('This plugin has an associated INI file '
+                    '(%(plugin_ini_name)s).') % {
+                'plugin_ini_name': plugin_ini_name})):
+        return False
+    #--Bsa / blocking resources?
+    hasBsa, has_blocking_resources = modInfo.hasResources()
+    if hasBsa and _exit(_('This plugin has an associated %(bsa_ext)s file.'
+                          ) % {'bsa_ext': game_handle.Bsa.bsa_extension}):
+        return False
+    if has_blocking_resources:
+        dir_list = '\n  - '.join(f'{pnd}{os.sep}%(blocking_plugin_name)s'
+            for pnd in game_handle.plugin_name_specific_dirs if pnd)
+        if _exit((_('Has plugin-specific directory - one of the following:') +
+              f'\n  - {dir_list}') % {'blocking_plugin_name': modInfo.fn_key}):
+            return False
+    #--Missing Strings Files?
+    if modInfo.fn_key in minfos.missing_strings:
+        if reasons is None: return False
+        from . import oblivionIni
+        i_lang = oblivionIni.get_ini_language(game_handle.Ini.default_game_lang)
+        strings_example = (f'{os.path.join("Strings", modInfo.fn_key.fn_body)}'
+                           f'_{i_lang}.STRINGS')
+        reasons.append(_('Missing string translation files '
+                         '(%(strings_example)s, etc).') % {
+            'strings_example': strings_example})
+    # if not verbose we already returned False, else we continue anyway
+    return True
+
+def isPBashMergeable(modInfo, minfos, reasons, game_handle):
+    """Return True or error message indicating whether specified mod is
+    mergeable."""
+    if not _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
+        return False  # non verbose mode
+    _exit = __exit if reasons is None else reasons.append # append returns None
+    if not game_handle.Esp.canBash and _exit(
+        _('Wrye Bash does not currently support loading plugins for '
+          '%(game_name)s.') % {'game_name': game_handle.display_name}):
+        return False
+    #--Load test: use generic MreRecord (without unpacking). ModFile.load will
+    # unpack the header which is enough for record.flags1|fid checks
+    from .mod_files import LoadFactory, ModFile # don't import brec in boot
+    merge_types_fact = LoadFactory(False, generic=game_handle.mergeable_sigs)
+    modFile = ModFile(modInfo, merge_types_fact)
+    try:
+        modFile.load_plugin(loadStrings=False, catch_errors=False)
+    except ModError as error:
+        if _exit(f'{error}.'): return False
+    #--Skipped over types?
+    if modFile.topsSkipped and _exit(
+            _('Wrye Bash does not support the following record types: '
+              '%(unsupported_rec_types)s.') % {
+                'unsupported_rec_types': _join_sigs(modFile.topsSkipped)}):
+        return False
+    #--Empty mod
+    elif not modFile.tops and _exit(_('This plugin is empty.')):
+        return False
+    #--New record
+    newblocks = []
+    self_name = modInfo.fn_key
+    for top_sig, block in modFile.tops.items():
+        for candidate_rec in block.iter_records(): # skip deleted/ignored
+            if candidate_rec.group_key().mod_fn == self_name:
+                if reasons is None: return False
+                newblocks.append(top_sig)
+                break
+    if newblocks: reasons.append(
+        _('This plugin has new records in the following groups: '
+          '%(new_rec_groups)s.') % {'new_rec_groups': _join_sigs(newblocks)})
+    dependent = _dependent(self_name, minfos)
+    if dependent and _exit(_('This plugin is a master of the following non-mergeable '
+              'plugins: %(non_mergeable_plugins)s.') % {
+                'non_mergeable_plugins': ', '.join(sorted(dependent))}):
+        return False
+    # Client must make sure NoMerge tag not in tags - if in tags
+    # don't show up as mergeable.
+    return not reasons
+
+def _join_sigs(rec_sigs):
+    return ', '.join(map(sig_to_str, sorted(rec_sigs)))
+
+def _dependent(minfo_key, minfos):
+    """Get mods for which modInfo is a master mod (excluding BPs and
+    mergeable)."""
+    dependent = [mname for mname, info in minfos.items() if
+                 not info.isBP() and minfo_key in info.masterNames and
+                 MergeabilityCheck.MERGE not in info.merge_types]
+    return dependent
+
+#------------------------------------------------------------------------------
+# The int values are stored in the settings files (mergeability cache), so they
+# should always remain the same just to be safe
+class MergeabilityCheck(Enum):
+    """The various mergeability checks that a game can have. See the comment
+    above each of them for more information."""
+    # If set for the game, the Merge Patches patcher will be enabled, the
+    # NoMerge tag will be available and WB will check plugins for their BP
+    # mergeability.
+    MERGE = 0
+    # If set for the game, the Add ESL Flag command will be available and WB
+    # will check plugins for their ESL capability.
+    ESL_CHECK = 1
+    # If set for the game, the Add Overlay Flag command will be available and
+    # WB will check plugins for their Overlay capability.
+    OVERLAY_CHECK = 2
+
+    def cached_types(self, mod_infos):
+        """Return *all* mod infos that passed our mergeability check, with a
+        header and message for the mod checker UI."""
+        match self:
+            case MergeabilityCheck.ESL_CHECK:
+                h, m = '=== ' + _('ESL-Capable'), _(
+                    'The following plugins could be assigned an ESL flag, but '
+                    'do not have one right now.')
+            case MergeabilityCheck.OVERLAY_CHECK:
+                h, m = '=== ' + _('Overlay-Capable'), _(
+                    'The following plugins could be assigned an Overlay flag, '
+                    'but do not have one right now.')
+            case _:
+                h, m = '', ''
+        return [p for p in mod_infos.values() if self in p.merge_types], h, m
+
+    def display_info(self, minf, checkMark):
+        """Return a UI settings key and a mouse text for the mod list UI."""
+        if self not in minf.merge_types: return '', ''
+        match self:
+            case MergeabilityCheck.MERGE:
+                if 'NoMerge' in minf.getBashTags():
+                    return 'mods.text.noMerge', _('Technically mergeable, '
+                                                  'but has NoMerge tag.')
+                if checkMark == 2: # Merged plugins won't be in master lists
+                    mtext = _('Merged into Bashed Patch.')
+                else:
+                    mtext = _('Can be merged into Bashed Patch.')
+            case MergeabilityCheck.ESL_CHECK:
+                mtext = _('Can be ESL-flagged.')
+            case MergeabilityCheck.OVERLAY_CHECK:
+                mtext = _('Can be Overlay-flagged.')
+        return 'mods.text.mergeable', mtext
+
+#------------------------------------------------------------------------------
+class PluginFlag(Enum):
+    """Enum for plugin flags and plugin types - they're friends with ModInfo
+    to the point the latter lets it modify its private attributes. This
+    intimate class relationship drastically simplifies client code."""
+    _ignore_ = ('count_str', )
+    count_str = ''
+
+    def __init__(self, flag_attr, mod_info_attr):
+        self._flag_attr = flag_attr # the ModInfo.header.flags1 attribute
+        self._mod_info_attr = mod_info_attr # (private) ModInfo cache attribute
+        self._offset = None
+
+    def has_flagged(self, mod_info):
+        """Check if the self._flag_attr is set on the mod info flags."""
+        return getattr(mod_info.header.flags1, self._flag_attr)
+
+    def cached_type(self, minf):
+        """Return the cached type of mod/master info - depends on the
+        corresponding flag state and possibly on the file extension."""
+        try:
+            return getattr(minf, self._mod_info_attr)
+        except AttributeError: # MasterInfo
+            if minf.mod_info:
+                return getattr(minf.mod_info, self._mod_info_attr)
+            return minf.flag_fallback(self)
+
+    def set_mod_flag(self, mod_info, set_flag, game_handle):
+        """Set the flag on the mod info and update the internal mod info state.
+        """
+        try:
+            if set_flag is not None:
+                setattr(mod_info.header.flags1, self._flag_attr, set_flag)
+                setattr(mod_info, self._mod_info_attr, set_flag)
+            else: # just init
+                set_flag = self.has_flagged(mod_info)
+                setattr(mod_info, self._mod_info_attr, set_flag)
+            return set_flag # if we are not flagged check the file extension
+        except AttributeError: # mod_info is a ModInfo.header.flags1 instance
+            setattr(mod_info, self._flag_attr, set_flag)
+            return True # don't do any extension checks
+
+    @classmethod
+    def check_flag_assignments(cls, flag_dict, raise_on_invalid=True):
+        return flag_dict
+
+    @classmethod
+    def guess_flags(cls, mod_fn_ext, masters_supplied=()):
+        """Guess the flags of a mod/master info from its filename extension.
+        Also used to force the plugin type (for .esm/esl) in set_mod_flag."""
+        return {MasterFlag.ESM: True} if mod_fn_ext == '.esm' else {}
+
+    # FIDs and mod index handling
+    @classmethod
+    def format_fid(cls, whole_lo_fid: int, _fid_orig_plugin, mod_infos):
+        """For non-ESL games simple hexadecimal formatting will do."""
+        return f'{whole_lo_fid:08X}'
+
+    @classmethod
+    def get_indexes(cls, iter_infos, real_indexes):
+        limit_flags = [m for m in cls if m._offset]
+        indexes = Counter()
+        indexes.update(dict(((m, m._offset) for m in limit_flags)))
+        for p, inf in iter_infos:
+            for pflag in limit_flags: # currently only ESL - revisit
+                if pflag.cached_type(inf):
+                    msg, sub = 'FE%03X', pflag._offset
+                    break
+            else:
+                msg, pflag, sub = '%02X', 'regular', 0
+            real_indexes[p] = (indexes[pflag], msg % (indexes[pflag] - sub))
+            indexes[pflag] += 1
+
+    # UI and menu info
+    @classmethod
+    def checkboxes(cls):
+        """Return a dict of init params for the plugin flags checkboxes in
+        the CreateNewPlugin dialog."""
+        return {}
+
+    @classmethod
+    def plugin_counts(cls, mod_infos, active_mods):
+        counts = Counter(dict.fromkeys((mem.name for mem in cls), 0))
+        regular_count = 0
+        for m in active_mods:
+            for member in cls:
+                if member.cached_type(m):
+                    counts[member.name] += 1
+                    break
+            else:
+                regular_count += 1
+        return cls.count_str % {**counts, 'status_num': len(active_mods),
+          'total_status_num': len(mod_infos), 'status_num_espm': regular_count}
+
+    @classmethod
+    def deactivate_msg(cls, max_regular_plugins):
+        return _('The following plugins have been deactivated '
+                 'because only %(max_regular_plugins)d plugins '
+                 'may be active at the same time.') % {
+            'max_regular_plugins': max_regular_plugins}
+
+    def link_args(self):
+        match self:
+            case MasterFlag.ESM:
+               return [('.esp', '.esu'), (), _('Flip the ESM flag on '
+                    'the selected plugins, turning masters into regular '
+                    'plugins and vice versa.')]
+        return []
+
+# easiest way to define enum class variables
+PluginFlag.count_str = _('Mods: %(status_num)d/%(total_status_num)d')
+
+class MasterFlag(PluginFlag):
+    ESM = ('esm_flag', '_is_master')
+
+    def set_mod_flag(self, mod_info, set_flag, game_handle):
+        if super().set_mod_flag(mod_info, set_flag, game_handle) or \
+                self is not self.ESM: # only check extension for esms
+            return
+        mext = mod_info.get_extension()
+        if game_handle.fsName == 'Morrowind':
+            ##: This is wrong, but works for now. We need game-specific
+            # record headers to parse the ESM flag for MW correctly - #480!
+            setattr(mod_info, self._mod_info_attr, mext == '.esm')
+        elif game_handle.Esp.extension_forces_flags:
+            # For games since FO4/SSE, .esm and .esl files set the master flag
+            # in memory even if not set on the file on disk. For .esp files we
+            # must check for the flag explicitly.
+            setattr(mod_info, self._mod_info_attr,
+                    self in self.guess_flags(mext))
+
+    @classmethod
+    def checkboxes(cls):
+        return {cls.ESM: {'cb_label': _('ESM Flag'), 'chkbx_tooltip': _(
+            'Whether or not the the resulting plugin will be a master, i.e. '
+            'have the ESM flag.')}}

Mopy/bash/_games_lo.py
@@ -40,6 +40,7 @@ from functools import partial
 from itertools import chain
 
 from . import bass, bolt, env, exception
+from .plugin_types import MasterFlag
 from .bolt import AFile, FName, Path, deprint, dict_sort
 from .ini_files import get_ini_type_and_encoding
 
@@ -441,7 +442,6 @@ class LoGame:
         Called in get_load_order() to fix a newly fetched LO and in
         set_load_order() to check if a load order passed in is valid. Needs
         rethinking as saving load and active should be an atomic operation."""
-        from .game import MasterFlag
         old_lord = lord[:]
         # game's master might be out of place (if using timestamps for load
         # ordering or a manually edited loadorder.txt) so move it up
@@ -738,7 +738,6 @@ class TimestampGame(LoGame):
     def __calculate_mtime_order(self, mods=None): # excludes mods in corrupted
         mods = ((k, self.mod_infos[k]) for k in
                 (self.mod_infos if mods is None else mods))
-        from .game import MasterFlag
         is_m = MasterFlag.ESM.cached_type
         return [m for m, _inf in sorted(mods, key=lambda x: (
             # split into master block and not master block then sort by ftime

Mopy/bash/basher/__init__.py
@@ -81,7 +81,7 @@ from ..bosh import ModInfo, omods, RefrData
 from ..bosh.mods_metadata import read_dir_tags, read_loot_tags
 from ..exception import BoltError, CancelError, FileError, SkipError, \
     UnknownListener
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..gui import CENTER, BusyCursor, Button, CancelButton, CenteredSplash, \
     CheckListBox, Color, CopyOrMovePopup, DateAndTimeDialog, DropDown, \
     EventResult, FileOpen, GlobalMenu, HLayout, Label, LayoutOptions, \
@@ -375,7 +375,7 @@ class _ModsUIList(UIList):
             item_format.text_key = 'mods.text.bashedPatch'
             mouse_text.append(_('Bashed Patch.'))
         for mchk in bush.game.mergeability_checks:
-            txtkey, mtext = mchk.can_convert_cached(minf, checkMark)
+            txtkey, mtext = mchk.display_info(minf, checkMark)
             if txtkey:
                 item_format.text_key = txtkey
                 mouse_text.append(mtext)

Mopy/bash/basher/constants.py
@@ -24,7 +24,7 @@
 """This module contains some constants ripped out of basher.py"""
 from .. import bush
 from ..bolt import GPath
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..gui import DEFAULT_POSITION
 
 # some repetitive strings

Mopy/bash/basher/dialogs.py
@@ -32,7 +32,7 @@ from ..bolt import CIstr, FName, GPath_no_norm, text_wrap, top_level_dirs, \
     reverse_dict
 from ..bosh import ModInfo, faces
 from ..fomod_schema import default_moduleconfig
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..gui import BOTTOM, CENTER, RIGHT, AMultiListEditor, CancelButton, \
     CheckBox, CheckListBox, DeselectAllButton, DialogWindow, DocumentViewer, \
     DropDown, EventResult, FileOpen, GridLayout, HBoxedLayout, HLayout, \

Mopy/bash/basher/gui_patchers.py
@@ -31,7 +31,7 @@ from .. import bass, bolt, bosh, bush, load_order
 from ..balt import CheckLink, SeparatorLink
 from ..bolt import FName, dict_sort, forward_compat_path_to_fn, \
     forward_compat_path_to_fn_list, text_wrap
-from ..game import MergeabilityCheck
+from ..plugin_types import MergeabilityCheck
 from ..gui import TOP, Button, CheckBox, CheckListBox, DeselectAllButton, \
     EventResult, FileOpenMultiple, HBoxedLayout, Label, LayoutOptions, \
     ListBox, Links, PanelWin, SearchBar, SelectAllButton, Spacer, TextArea, \

Mopy/bash/basher/links_init.py
@@ -48,7 +48,7 @@ from ..balt import BashStatusBar, MenuLink, SeparatorLink, UIList_Delete, \
     UIList_Hide, UIList_OpenItems, UIList_OpenStore, UIList_Rename
 from ..bolt import os_name
 from ..env import init_app_links
-from ..game import MasterFlag
+from ..plugin_types import MasterFlag
 from ..game.patch_game import PatchGame
 from ..gui import GuiImage, get_image
 

commit f050b25d1a963c3374cf9cc6193f2b5a1fd33130

Fix regression in Plugin Checker
I think this broke in fcdabd874959522ff9edc4cecbe3f410aff77635, but not
100% sure. The new format_fid method needs ModInfos passed in.

Mopy/bash/bosh/bain.py:
bain: Fix regression in Anneal All

And refactor a bit, add some typing, etc. Not sure when this broke.

Utumno:
Fixup in plugin checker for Oblivion:

https://discord.com/channels/537706885965676554/537710082755133460/1325415569612210176

Co-authored-by: MrD <the.ubik@gmail.com>

Mopy/bash/bosh/bain.py
@@ -2881,17 +2881,19 @@ class InstallersData(DataStore):
         finally:
             self.refresh_ns()
 
-    def bain_anneal(self, anPackages, refresh_ui_, progress=None):
+    def bain_anneal(self, annealed_package_fnames: Iterable[FName] | None,
+            refresh_ui_, progress=None):
         """Anneal selected packages. If no packages are selected, anneal all.
         Anneal will:
         * Correct underrides in anPackages.
         * Install missing files from active anPackages."""
         progress = progress if progress else bolt.Progress()
-        anPackages = self.filterInstallables(self) if anPackages is None else {
-            self[package] for package in anPackages}
-        #--Get remove/refresh files from anPackages
+        if annealed_package_fnames is None:
+            annealed_package_fnames = self.filterInstallables(self)
+        annealed_packages = [self[p] for p in annealed_package_fnames]
+        #--Get remove/refresh files from annealed packages
         removes = set()
-        for installer in anPackages:
+        for installer in annealed_packages:
             removes |= installer.underrides
             if installer.is_active:
                 removes |= installer.missingFiles # re-added in __restore

Mopy/bash/bosh/mods_metadata.py
@@ -205,12 +205,12 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     # plugins that shouldn't be Overlay-flagged. Also check for conflicts
     # between ESL and Overlay flags.
     pflags = bush.game.plugin_flags
-    flag_errors = {k: {h_msg: set() for h_msg in v} for k, v in
-                   pflags.error_msgs.items()}
-    for m, modinf in modInfos.items():
-        for pflag in flag_errors:
-            if pflag.cached_type(modinf):
-                pflag.validate_type(modinf, flag_errors[pflag].values())
+    if flag_errors := {k: {h_msg: set() for h_msg in v} for k, v in
+                       pflags.error_msgs.items()}:
+        for m, modinf in modInfos.items():
+            for pflag in flag_errors:
+                if pflag.cached_type(modinf):
+                    pflag.validate_type(modinf, flag_errors[pflag].values())
     # -------------------------------------------------------------------------
     # Check for Deactivate-tagged plugins that are active and
     # MustBeActiveIfImported-tagged plugins that are imported, but inactive.
@@ -598,7 +598,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         """Logs a single collision with the specified FormID, injected status,
         origin plugin and collision info."""
         # FormIDs must be in long format at this point
-        proper_fid = format_fid(coll_fid, coll_plugin)
+        proper_fid = format_fid(coll_fid, coll_plugin, modInfos)
         if coll_inj:
             log('* ' + _('%(injected_formid)s injected into '
                          '%(injection_target)s, colliding versions:') % {
@@ -820,7 +820,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
             for orig_plugin, dupe_count in duplicates_counter.items():
                 log('* ' + _('%(full_fid)s in %(orig_plugin)s: '
                              'occurs %(num_duplicates)d times') % {
-                    'full_fid': format_fid(orig_fid, orig_plugin),
+                    'full_fid': format_fid(orig_fid, orig_plugin, modInfos),
                     'orig_plugin': orig_plugin,
                     'num_duplicates': dupe_count,
                 })

Mopy/bash/plugin_types.py
@@ -209,7 +209,7 @@ class PluginFlag(Enum):
     """Enum for plugin flags and plugin types - they're friends with ModInfo
     to the point the latter lets it modify its private attributes. This
     intimate class relationship drastically simplifies client code."""
-    _ignore_ = ('count_str', )
+    _ignore_ = ('count_str', 'error_msgs', 'max_plugins')
     count_str = ''
 
     def __init__(self, flag_attr, mod_info_attr, ui_letter_key,
@@ -326,6 +326,7 @@ class PluginFlag(Enum):
 # easiest way to define enum class variables
 PluginFlag.count_str = _('Mods: %(status_num)d/%(total_status_num)d')
 PluginFlag.max_plugins = 255
+PluginFlag.error_msgs = {}
 
 class AMasterFlag(PluginFlag):
     """Master flags - affect load order - mutually compatible and compatible