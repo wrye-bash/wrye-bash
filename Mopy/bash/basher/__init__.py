wrye-bash/wrye-bash

commit 350b4d9db589fc1fd97f5eaca00b74709fed9a08

balt.Installer_Op:
Centralize BAIN refresh logic - 7 RefreshUI calls (and issue_warnings)
dropped - the goal here is to pass more detailed info to the panels'
RefreshUI. ui_refresh_ in the UI and refresh_ui_ in BAIN are for me to
track where we just pass along function parameters as opposed to
refresh_ui and ui_refresh which are the actual usesites - provisional,
the right thing is to pass kwargs along, done later).
Note Installer_Anneal Execute was not decorated with conversation - if
wasn't needed won't harm to add it, if it was then this is a bugfix.
Same goes double with issue_warnings, more of whose were missing. Then
issue_warnings had one use and turns out the final use of ui_refresh
variable so no harm adding the data_store_key to it at this point. WIP!

Under #353 core - see next commits

Mopy/bash/basher/installers_links.py
@@ -31,7 +31,7 @@ from .dialogs import CreateNewProject, CleanDataEditor, ImportOrderDialog, \
     MonitorExternalInstallationEditor, AImportOrderParser
 from .. import balt, bass, bosh, bush, load_order
 from ..balt import AppendableLink, BoolLink, EnabledLink, ItemLink, \
-    SeparatorLink
+    SeparatorLink, Installer_Op
 from ..gui import copy_text_to_clipboard, askYes
 
 __all__ = ['Installers_InstalledFirst', 'Installers_ProjectsFirst',
@@ -161,41 +161,35 @@ class Installers_ListPackages(Installers_Link):
         self._showLog(package_list, title=_('BAIN Packages'))
 
 #------------------------------------------------------------------------------
-class Installers_AnnealAll(Installers_Link):
+class Installers_AnnealAll(Installer_Op, Installers_Link):
     """Anneal all packages."""
     _text = _('Anneal All')
     _help = _('Install any missing files (for active packages) and update '
               'the contents of the %(data_folder)s folder to account for all '
               'install order and configuration changes.') % {
         'data_folder': bush.game.mods_dir}
+    _prog_args = _('Annealing…'),
 
-    @balt.conversation
-    def Execute(self):
+    def _perform_action(self, ui_refresh_, progress):
         """Anneal all packages."""
-        ui_refresh = defaultdict(bool)
-        try:
-            with balt.Progress(_('Annealing…')) as progress:
-                self.idata.bain_anneal(None, ui_refresh, progress=progress)
-        finally:
-            self.window.RefreshUI(refresh_others=ui_refresh)
-            self.window.issue_warnings(warn_others=ui_refresh)
+        self.idata.bain_anneal(None, ui_refresh_, progress=progress)
 
 #------------------------------------------------------------------------------
-class Installers_UninstallAllPackages(Installers_Link):
+class Installers_UninstallAllPackages(Installer_Op, Installers_Link):
     """Uninstall all packages."""
     _text = _('Uninstall All Packages')
     _help = _('Uninstall all files from all installed packages.')
+    _prog_args = _('Uninstalling…'),
 
     @balt.conversation
     def Execute(self):
         """Uninstall all packages."""
         if not self._askYes(_('Really uninstall all packages?')): return
-        ui_refresh = defaultdict(bool)
-        try:
-            with balt.Progress(_('Uninstalling…')) as progress:
-                self.idata.bain_uninstall_all(ui_refresh, progress=progress)
-        finally:
-            self.window.RefreshUI(refresh_others=ui_refresh)
+        super().Execute()
+
+    def _perform_action(self, ui_refresh_, progress):
+        """Uninstall all present packages."""
+        self.idata.bain_uninstall(None, ui_refresh_, progress)
 
 #------------------------------------------------------------------------------
 class _AInstallers_Refresh(AppendableLink, Installers_Link):
@@ -231,7 +225,7 @@ class Installers_RefreshData(_AInstallers_Refresh):
               'directories.') % {'data_folder': bush.game.mods_dir}
 
 #------------------------------------------------------------------------------
-class Installers_CleanData(Installers_Link):
+class Installers_CleanData(Installer_Op, Installers_Link):
     """Uninstall all files that do not come from a current package/bethesda
     files. For safety just moved to Game Mods/Bash Installers/Bash/Data
     Folder Contents (date/time)."""
@@ -260,17 +254,20 @@ class Installers_CleanData(Installers_Link):
                            '%(data_folder)s folder.') % mdir_fmt,
                 title=_('Clean Data - %(data_folder)s is Clean') % mdir_fmt)
             return
-        ed_ok, ed_unknown = CleanDataEditor.display_dialog(self.window,
+        ed_ok, self.__ed_unknown = CleanDataEditor.display_dialog(self.window,
             unknown_files=all_unknown_files)
-        if not ed_ok or not ed_unknown:
+        if not ed_ok or not self.__ed_unknown:
             return # Aborted by user or nothing left to clean, cancel
-        ui_refresh = defaultdict(bool)
-        try:
-            with balt.Progress(_('Cleaning %(data_folder)s '
-                                 'contents…') % mdir_fmt, f'\n{" " * 65}'):
-                self.idata.clean_data_dir(ed_unknown, ui_refresh)
-        finally:
-            self.window.RefreshUI(refresh_others=ui_refresh)
+        super().Execute()
+
+    @property
+    def _prog_args(self):
+        return _('Cleaning %(data_folder)s contents…') % {
+            'data_folder': bush.game.mods_dir}, f'\n{" " * 65}'
+
+    def _perform_action(self, ui_refresh_, progress):
+        """Clean the data directory."""
+        self.idata.clean_data_dir(self.__ed_unknown, ui_refresh_, progress)
 
 #------------------------------------------------------------------------------
 class Installers_CreateNewProject(ItemLink):

Mopy/bash/balt.py
@@ -820,14 +820,6 @@ class UIList(PanelWin):
                 del refresh_others[self.data_store_key]
             Link.Frame.distribute_ui_refresh(refresh_others)
 
-    def issue_warnings(self,
-            warn_others: defaultdict[str, bool] | None = None):
-        """Show warnings for this tab and any others that are specified."""
-        final_warn_dict = defaultdict(bool, {self.data_store_key: True})
-        if warn_others:
-            final_warn_dict |= warn_others
-        Link.Frame.distribute_warnings(final_warn_dict)
-
     def _refresh_details(self, to_redraw, detail_item):
         if detail_item is None:
             self.panel.ClearDetails()
@@ -2031,6 +2023,27 @@ class UIList_Hide(EnabledLink):
             if not self._askYes(message, _(u'Hide Files')): return
         self.window.hide(self._filter_unhideable(self.selected))
         self.window.RefreshUI(refresh_others=Store.SAVES.DO())
+
+class Installer_Op(ItemLink):
+    """Common refresh logic for BAIN operations."""
+    _prog_args = ()
+
+    @conversation
+    def Execute(self):
+        ui_refresh = defaultdict(bool)
+        try:
+            with (Progress(*self._prog_args) if self._prog_args else
+                  bolt.Progress() as progress):
+                return self._perform_action(ui_refresh, progress)
+        except (CancelError, SkipError):
+            return None
+        finally:
+            self.window.RefreshUI(refresh_others=ui_refresh)
+            ui_refresh[self.window.data_store_key] = True
+            Link.Frame.distribute_warnings(ui_refresh)
+
+    def _perform_action(self, ui_refresh_, progress):
+        raise NotImplementedError
 
 # wx Wrappers -----------------------------------------------------------------
 #------------------------------------------------------------------------------

commit 34a89f5a09089efc5ffe57574e2db3067c70e02d

Fixup for clean data dir:
Broke in 350b4d9db589fc1fd97f5eaca00b74709fed9a08

Closes #697

Renames of modInfo locals - triggered by me working on the details panel,
we would like to get rid of _EditableMixinOnFileInfos

Mopy/bash/basher/__init__.py
@@ -1182,9 +1182,8 @@ class _DetailsMixin(object):
     def _resetDetails(self): raise NotImplementedError
 
     # Details panel API
-    def SetFile(self, fileName=_same_file):
-        """Set file to be viewed. Leave fileName empty to reset.
-        :type fileName: str | FName | None"""
+    def SetFile(self, fileName: str | FName | None = _same_file):
+        """Set file to be viewed. Leave fileName empty to reset."""
         #--Reset?
         fileName = self.displayed_item if fileName is _same_file else fileName
         if not fileName or (fileName not in self.file_infos):
@@ -1445,14 +1444,14 @@ class ModDetails(_ModsSavesDetails):
     def SetFile(self, fileName=_same_file):
         fileName = super(ModDetails, self).SetFile(fileName)
         if fileName:
-            modInfo = self.modInfo = bosh.modInfos[fileName]
+            mod_inf = self.modInfo = bosh.modInfos[fileName]
             #--Remember values for edit checks
-            self.fileStr = str(modInfo.fn_key)
-            self.authorStr = modInfo.header.author
-            self.modifiedStr = format_date(modInfo.ftime)
-            self.descriptionStr = modInfo.header.description
-            self.versionStr = f'v{modInfo.header.version:0.2f}'
-            minf_tags = list(modInfo.getBashTags())
+            self.fileStr = str(mod_inf.fn_key)
+            self.authorStr = mod_inf.header.author
+            self.modifiedStr = format_date(mod_inf.ftime)
+            self.descriptionStr = mod_inf.header.description
+            self.versionStr = f'v{mod_inf.header.version:0.2f}'
+            minf_tags = list(mod_inf.getBashTags())
         else:
             minf_tags = []
         #--Set fields
@@ -1571,11 +1570,11 @@ class ModDetails(_ModsSavesDetails):
         'renamed.')
 
     def testChanges(self): # used by the master list when editing is disabled
-        modInfo = self.modInfo
-        if not modInfo or (modInfo.named_as(self.fileStr) and
-                           self.modifiedStr == format_date(modInfo.ftime) and
-                           self.authorStr == modInfo.header.author and
-                           self.descriptionStr == modInfo.header.description):
+        mod_inf = self.modInfo
+        if not mod_inf or (mod_inf.named_as(self.fileStr) and
+                           self.modifiedStr == format_date(mod_inf.ftime) and
+                           self.authorStr == mod_inf.header.author and
+                           self.descriptionStr == mod_inf.header.description):
             self.DoCancel()
 
     __bad_name_msg = _('File name %(bad_file_name)s cannot be encoded to '
@@ -1583,18 +1582,18 @@ class ModDetails(_ModsSavesDetails):
         'plugin because of this. Do you want to rename the plugin anyway?')
     @balt.conversation
     def DoSave(self):
-        modInfo = self.modInfo
+        mod_inf = self.modInfo
         #--Change Tests
         if (changeName := self._rename_detail_item()) is None:
             return
-        changeDate = (self.modifiedStr != format_date(modInfo.ftime))
-        changeHedr = (self.authorStr != modInfo.header.author or
-                      self.descriptionStr != modInfo.header.description)
+        changeDate = (self.modifiedStr != format_date(mod_inf.ftime))
+        changeHedr = (self.authorStr != mod_inf.header.author or
+                      self.descriptionStr != mod_inf.header.description)
         changeMasters = self.uilist.edited
         unlock_lo = changeDate and not bush.game.using_txt_file
         #--Only change date?
         if changeDate and not (changeName or changeHedr or changeMasters):
-            self._set_date(modInfo)
+            self._set_date(mod_inf)
             bosh.modInfos.refresh(refresh_infos=False, unlock_lo=unlock_lo)
             self.panel_uilist.propagate_refresh( # refresh saves if lo changed
                 True, refr_saves=not bush.game.using_txt_file)
@@ -1602,16 +1601,16 @@ class ModDetails(_ModsSavesDetails):
         #--Change hedr/masters?
         if refr_inf := (changeHedr or changeMasters):
             #--Backup
-            modInfo.makeBackup()
-            modInfo.header.author = self.authorStr.strip()
-            modInfo.header.description = self.descriptionStr.strip()
-            old_mi_masters = modInfo.header.masters
-            modInfo.header.masters = self.uilist.GetNewMasters()
-            modInfo.header.setChanged()
-            modInfo.writeHeader(old_mi_masters)
+            mod_inf.makeBackup()
+            mod_inf.header.author = self.authorStr.strip()
+            mod_inf.header.description = self.descriptionStr.strip()
+            old_mi_masters = mod_inf.header.masters
+            mod_inf.header.masters = self.uilist.GetNewMasters()
+            mod_inf.header.setChanged()
+            mod_inf.writeHeader(old_mi_masters)
         #--Change date?
         if changeDate:
-            self._set_date(modInfo) # crc recalculated in writeHeader if needed
+            self._set_date(mod_inf) # crc recalculated in writeHeader if needed
         detail_item = self._refresh_detail_info(refr_inf, unlock_lo=unlock_lo)
         self.panel_uilist.propagate_refresh(True, refr_saves=(
                 detail_item is None or changeName or unlock_lo),
@@ -1640,8 +1639,8 @@ class ModDetails(_ModsSavesDetails):
             settings['bash.mods.renames'].update(ren_data.renames)
         return ren_data
 
-    def _set_date(self, modInfo):
-        modInfo.setmtime(time.mktime(time.strptime(self.modifiedStr)))
+    def _set_date(self, mod_inf):
+        mod_inf.setmtime(time.mktime(time.strptime(self.modifiedStr)))
 
     #--Bash Tags
     ##: Once we're on wx4.1.1, we can use OnDimiss to fully refreshUI the

Mopy/bash/basher/installers_links.py
@@ -267,7 +267,7 @@ class Installers_CleanData(Installer_Op, Installers_Link):
 
     def _perform_action(self, ui_refresh_, progress):
         """Clean the data directory."""
-        self.idata.clean_data_dir(self.__ed_unknown, ui_refresh_, progress)
+        self.idata.clean_data_dir(self.__ed_unknown, ui_refresh_)
 
 #------------------------------------------------------------------------------
 class Installers_CreateNewProject(ItemLink):

Mopy/bash/basher/mod_links.py
@@ -1253,7 +1253,7 @@ class Mod_ScanDirty(ItemLink):
         # Change a FID to something more useful for displaying
         def _log_fids(del_title, del_fids):
             nonlocal full_dirty_msg
-            mod_masters = modInfo.masterNames
+            mod_masters = mod_inf.masterNames
             len_mas = len(mod_masters)
             full_dirty_msg += '\n'.join([f'  * {del_title}: {len(del_fids)}',
                 *(f"    * "
@@ -1264,14 +1264,14 @@ class Mod_ScanDirty(ItemLink):
         dirty_plugins = []
         clean_plugins = []
         skipped_plugins = []
-        for i, (plugin_fn, modInfo) in enumerate(all_present_minfs.items()):
+        for i, (plugin_fn, mod_inf) in enumerate(all_present_minfs.items()):
             del_navms = all_deleted_navms[plugin_fn]
             del_refs = all_deleted_refs[plugin_fn]
             del_others = all_deleted_others[plugin_fn]
             if plugin_fn == game_master_name or plugin_fn.fn_ext == '.esu':
-                skipped_plugins.append(f'* __{modInfo}__')
+                skipped_plugins.append(f'* __{mod_inf}__')
             elif del_navms or del_refs or del_others:
-                full_dirty_msg = f'* __{modInfo}__:\n'
+                full_dirty_msg = f'* __{mod_inf}__:\n'
                 if del_navms:
                     _log_fids(_('Deleted Navmeshes'), del_navms)
                 if del_refs:
@@ -1280,7 +1280,7 @@ class Mod_ScanDirty(ItemLink):
                     _log_fids(_('Deleted Base Records'), del_others)
                 dirty_plugins.append(full_dirty_msg)
             else:
-                clean_plugins.append(f'* __{modInfo}__')
+                clean_plugins.append(f'* __{mod_inf}__')
         if dirty_plugins:
             log(_('Detected %(num_dirty_plugins)d plugins with deleted '
                   'records:') % {'num_dirty_plugins': len(dirty_plugins)})

Mopy/bash/basher/mods_links.py
@@ -378,10 +378,10 @@ class Mods_AutoGhost(BoolLink):
         super(Mods_AutoGhost, self).Execute()
         flipped = []
         toGhost = bass.settings['bash.mods.autoGhost']
-        for mod, modInfo in bosh.modInfos.items():
+        for mod, mod_inf in bosh.modInfos.items():
             modGhost = toGhost and not load_order.cached_is_active(
-                mod) and modInfo.get_table_prop('allowGhosting', True)
-            if modInfo.setGhost(modGhost):
+                mod) and mod_inf.get_table_prop('allowGhosting', True)
+            if mod_inf.setGhost(modGhost):
                 flipped.append(mod)
         self.refresh_sel(flipped)
 

Mopy/bash/basher/saves_links.py
@@ -922,8 +922,8 @@ class Save_UpdateNPCLevels(EnabledLink):
             for index,modName in enumerate(ordered):
                 subProgress(index, _('Scanning %(scanning_plugin)s') % {
                     'scanning_plugin': modName})
-                modInfo = bosh.modInfos[modName]
-                modFile = ModFile(modInfo, lf)
+                mod_inf = bosh.modInfos[modName]
+                modFile = ModFile(mod_inf, lf)
                 try:
                     modFile.load_plugin()
                 except ModError as x:

Mopy/bash/parsers.py
@@ -114,13 +114,13 @@ class _TextParser(object):
 
     # Write plugin ------------------------------------------------------------
     _changed_type = dict # used in writeToMod to report changed records
-    def writeToMod(self,modInfo):
+    def writeToMod(self, mod_inf):
         """Hasty writeToMod implementation - export id_stored_data to specified
         mod.
 
-        :param modInfo: The ModInfo instance to write to.
+        :param mod_inf: The ModInfo instance to write to.
         :return: info on number of changed records, usually per record type."""
-        modFile = self._load_plugin(modInfo, keepAll=True,
+        modFile = self._load_plugin(mod_inf, keepAll=True,
                                     target_types=self.id_stored_data)
         changed_stats = self._changed_type()
         # Check which record types makes any sense to patch
@@ -251,9 +251,9 @@ class _HandleAliases(CsvParser):
     def _key2(self, csv_fields):
         return self._coerce_fid(*self._key2_getter(csv_fields))
 
-    def readFromMod(self, modInfo):
+    def readFromMod(self, mod_inf):
         """Hasty readFromMod implementation."""
-        modFile = self._load_plugin(modInfo)
+        modFile = self._load_plugin(mod_inf)
         for top_grup_sig, typeBlock in modFile.iter_tops(self._parser_sigs):
             id_data = self.id_stored_data[top_grup_sig]
             for rfid, record in typeBlock.iter_present_records():
@@ -545,12 +545,12 @@ class ActorLevels(_HandleAliases):
         self.gotLevels = set()
         self._skip_mods = {'none', bush.game.master_file.lower()}
 
-    def readFromMod(self,modInfo):
+    def readFromMod(self, mod_inf):
         """Imports actor level data from the specified mod and its masters."""
         from . import bosh
         mod_id_levels, gotLevels = self.id_stored_data, self.gotLevels
         load_f = self._load_factory(keepAll=False)
-        for modName in (*modInfo.masterNames, modInfo.fn_key):
+        for modName in (*mod_inf.masterNames, mod_inf.fn_key):
             if modName in gotLevels: continue
             modFile = self._load_plugin(bosh.modInfos[modName],
                                         load_fact=load_f)
@@ -564,15 +564,15 @@ class ActorLevels(_HandleAliases):
                 mod_id_levels[modName][rfid] = dict(items)
             gotLevels.add(modName)
 
-    def writeToMod(self, modInfo):
+    def writeToMod(self, mod_inf):
         """Exports actor levels to specified mod."""
-        id_levels = self.id_stored_data.get(modInfo.fn_key,
-            self.id_stored_data.get('Unknown', None))
+        id_levels = self.id_stored_data.get(mod_inf.fn_key,
+                                            self.id_stored_data.get('Unknown', None))
         if id_levels:
             # pretend we are a normal parser
             real = self.id_stored_data
             self.id_stored_data = {b'NPC_': id_levels}
-            changed_stats = super().writeToMod(modInfo)
+            changed_stats = super().writeToMod(mod_inf)
             self.id_stored_data = real
             return changed_stats
         return 0
@@ -808,9 +808,9 @@ class FidReplacer(_HandleAliases):
         self.old_eid[oldId] = oldEid
         self.new_eid[newId] = newEid
 
-    def updateMod(self,modInfo,changeBase=False):
+    def updateMod(self, mod_inf, changeBase=False):
         """Updates specified mod file."""
-        modFile = self._load_plugin(modInfo, keepAll=True)
+        modFile = self._load_plugin(mod_inf, keepAll=True)
         # Create filtered versions of our mappings
         masters_list = set(modFile.augmented_masters())
         filt_fids = {oldId for oldId in self.old_eid if
@@ -1024,24 +1024,24 @@ class ScriptText(_TextParser):
         scpt_lines, _longid, _eid = self._writing_state
         out.write('\n'.join(scpt_lines) + '\n')
 
-    def readFromMod(self, modInfo):
+    def readFromMod(self, mod_inf):
         """Reads scripts from specified mod."""
         eid_data = self.eid_data
-        modFile = self._load_plugin(modInfo)
+        modFile = self._load_plugin(mod_inf)
         with Progress(_('Export Scripts')) as progress:
             present_recs = list(modFile.tops[b'SCPT'].iter_present_records())
             y = len(present_recs)
             for z, (rfid, record) in enumerate(present_recs):
                 progress((0.5 / y) * z,
-                    _('Reading scripts in %(source_plugin)s.') % {
-                        'source_plugin': modInfo})
+                         _('Reading scripts in %(source_plugin)s.') % {
+                        'source_plugin': mod_inf})
                 eid_data[record.eid] = record.script_source.splitlines(), rfid
 
     _changed_type = list
-    def writeToMod(self, modInfo, makeNew=False):
+    def writeToMod(self, mod_inf, makeNew=False):
         """Writes scripts to specified mod."""
         self.makeNew = makeNew
-        changed_stats = super(ScriptText, self).writeToMod(modInfo)
+        changed_stats = super(ScriptText, self).writeToMod(mod_inf)
         return ([], []) if changed_stats is None else changed_stats
 
     def _check_write_record(self, rfid, record, eid_data, changed_stats):

Mopy/bash/plugin_types.py
@@ -50,24 +50,24 @@ def is_vanilla(mod_info, reasons, game_handle):
         return True if reasons is None else not reasons.append(_(
             'Is Vanilla Plugin')) # return True - block rest of checks
 
-def _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
-    if is_vanilla(modInfo, reasons, game_handle):
+def _pbash_mergeable_no_load(mod_inf, minfos, reasons, game_handle):
+    if is_vanilla(mod_inf, reasons, game_handle):
         return False # don't do further checks even in verbose mode
     _exit = __exit if reasons is None else reasons.append # append returns None
-    if game_handle.master_flag.has_flagged(modInfo) and _exit(_(
+    if game_handle.master_flag.has_flagged(mod_inf) and _exit(_(
             'This plugin has the ESM flag.')):
         return False
     #--Bashed Patch
-    if modInfo.isBP() and _exit(_('This plugin is a Bashed Patch.')):
+    if mod_inf.isBP() and _exit(_('This plugin is a Bashed Patch.')):
         return False
     # Plugin INIs would get deactivated if the plugin got merged
-    if ((plugin_ini_name := modInfo.get_ini_name()) in minfos.plugin_inis and
+    if ((plugin_ini_name := mod_inf.get_ini_name()) in minfos.plugin_inis and
             _exit(_('This plugin has an associated INI file '
                     '(%(plugin_ini_name)s).') % {
                 'plugin_ini_name': plugin_ini_name})):
         return False
     #--Bsa / blocking resources?
-    hasBsa, has_blocking_resources = modInfo.hasResources()
+    hasBsa, has_blocking_resources = mod_inf.hasResources()
     if hasBsa and _exit(_('This plugin has an associated %(bsa_ext)s file.'
                           ) % {'bsa_ext': game_handle.Bsa.bsa_extension}):
         return False
@@ -75,14 +75,14 @@ def _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
         dir_list = '\n  - '.join(f'{pnd}{os.sep}%(blocking_plugin_name)s'
             for pnd in game_handle.plugin_name_specific_dirs if pnd)
         if _exit((_('Has plugin-specific directory - one of the following:') +
-              f'\n  - {dir_list}') % {'blocking_plugin_name': modInfo.fn_key}):
+              f'\n  - {dir_list}') % {'blocking_plugin_name': mod_inf.fn_key}):
             return False
     #--Missing Strings Files?
-    if modInfo.fn_key in minfos.missing_strings:
+    if mod_inf.fn_key in minfos.missing_strings:
         if reasons is None: return False
         from . import oblivionIni
         i_lang = oblivionIni.get_ini_language(game_handle.Ini.default_game_lang)
-        strings_example = (f'{os.path.join("Strings", modInfo.fn_key.fn_body)}'
+        strings_example = (f'{os.path.join("Strings", mod_inf.fn_key.fn_body)}'
                            f'_{i_lang}.STRINGS')
         reasons.append(_('Missing string translation files '
                          '(%(strings_example)s, etc).') % {
@@ -90,10 +90,10 @@ def _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
     # if not verbose we already returned False, else we continue anyway
     return True
 
-def isPBashMergeable(modInfo, minfos, reasons, game_handle):
+def isPBashMergeable(mod_inf, minfos, reasons, game_handle):
     """Return True or error message indicating whether specified mod is
     mergeable."""
-    if not _pbash_mergeable_no_load(modInfo, minfos, reasons, game_handle):
+    if not _pbash_mergeable_no_load(mod_inf, minfos, reasons, game_handle):
         return False  # non verbose mode or vanilla plugin
     _exit = __exit if reasons is None else reasons.append # append returns None
     if not game_handle.Esp.canBash and _exit(
@@ -104,7 +104,7 @@ def isPBashMergeable(modInfo, minfos, reasons, game_handle):
     # unpack the header which is enough for record.flags1|fid checks
     from .mod_files import LoadFactory, ModFile # don't import brec in boot
     merge_types_fact = LoadFactory(False, generic=game_handle.mergeable_sigs)
-    modFile = ModFile(modInfo, merge_types_fact)
+    modFile = ModFile(mod_inf, merge_types_fact)
     try:
         modFile.load_plugin(loadStrings=False, catch_errors=False)
     except ModError as error:
@@ -120,7 +120,7 @@ def isPBashMergeable(modInfo, minfos, reasons, game_handle):
         return False
     #--New record
     newblocks = []
-    self_name = modInfo.fn_key
+    self_name = mod_inf.fn_key
     for top_sig, block in modFile.tops.items():
         for candidate_rec in block.iter_records(): # skip deleted/ignored
             if candidate_rec.group_key().mod_fn == self_name: