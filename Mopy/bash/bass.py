4c6dd54277d925521cd36d37ea844eb45c9740bb
Infernio <infernio@icloud.com>
2025-04-05 14:35:02 +0200

    Bump Wrye Bash version to 315

--- Mopy/bash/bass.py
+++ Mopy/bash/bass.py
@@ -37,7 +37,7 @@ else:
 
 # The name of the locale we ended up with after localize.setup_locale()
 active_locale = None
-AppVersion = '314'  # must represent a valid float
+AppVersion = '315'  # must represent a valid float
 is_standalone = False # whether or not we're on standalone
 
 #--Global dictionaries - do _not_ reassign !
