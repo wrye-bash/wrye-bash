wrye-bash/wrye-bash

commit bfc0b5b275965ff13e4bd0d432a70dab859369ee

Move to_rdata() calls in _lo_cache:
There was a single case we still needed ldiff after we saved - you
guessed it, it's modInfos.refresh. Hence, an output parameter for
_lo_cache. LordDiff and RefrData are finally encapsulated, eliminating
repetitive logic - speaking of the latter:

RefrData.ior from future:

Syntactic sugar and cleanup - in particular, flattened calls to
undo_load_order.

Mopy/bash/basher/mods_links.py
@@ -689,8 +689,8 @@ class _AImportLOBaseLink(ItemLink):
         # Don't show the exact same message twice
         for msg in dict.fromkeys([msg_lo, msg_acti]):
             if msg: self._showWarning(msg, title=self._warning_title)
-        ldiff = bosh.modInfos.cached_lo_save_all()
-        self.window.propagate_refresh(ldiff.to_rdata())
+        lordata = bosh.modInfos.cached_lo_save_all()
+        self.window.propagate_refresh(lordata)
         self._showInfo(_('Successfully imported a load order from '
                          '%(source_path)s.') % {'source_path': import_path},
                        title=self._success_title)

Mopy/bash/basher/__init__.py
@@ -971,8 +971,8 @@ class ModList(_ModsUIList):
         if moved:
             #--Save and Refresh
             try:
-                ldiff = bosh.modInfos.cached_lo_save_all()
-                self.propagate_refresh(ldiff.to_rdata())
+                lordata = bosh.modInfos.cached_lo_save_all()
+                self.propagate_refresh(lordata)
             except (BoltError, NotImplementedError) as e:  ##: why NotImplementedError?
                 showError(self, f'{e}')
 
@@ -1072,16 +1072,14 @@ class ModList(_ModsUIList):
                 newIndex := (first := c[0]) + moveMod) >= 0 and (
                     (last := c[-1]) + moveMod) < self.item_count]
             self._drop_dexes(chunks)
-        elif wrapped_evt.is_cmd_down and kcode == ord('Z'):
-            if wrapped_evt.is_shift_down:
-                # Ctrl+Shift+Z - redo last load order or active plugins change
-                self.lo_redo()
-            else:
-                # Ctrl+Z - undo last load order or active plugins change
-                self.lo_undo()
-        elif wrapped_evt.is_cmd_down and kcode == ord('Y'):
-            # Ctrl+Y - redo last load order or active plugins change
-            self.lo_redo()
+        elif wrapped_evt.is_cmd_down:
+            # Ctrl+Y/Ctrl+Shift+Z redo last load order or active plugins change
+            # Ctrl+Z - undo last load order or active plugins change
+            redo = wrapped_evt.is_shift_down if kcode == ord('Z') else \
+                (kcode == ord('Y') or None)
+            if redo is not None:
+                lordata = self.data_store.undo_redo_load_order(redo)
+                self.propagate_refresh(lordata) # no additions or removals
         else:
             return super()._handle_key_down(wrapped_evt)
         # Otherwise we'd jump to a random plugin that starts with the key code
@@ -1196,29 +1194,14 @@ class ModList(_ModsUIList):
         if warn_msg:
             balt.askContinue(self, warn_msg, warn_cont_key, show_cancel=False)
         if touched:
-            ldiff = bosh.modInfos.cached_lo_save_active()
+            lordata = bosh.modInfos.cached_lo_save_active()
             # If we have no changes, pass - if we do have changes, only one of
             # these can be truthy at a time
             if ch := changes[self._activated_key]:
                 MastersAffectedDialog(self, ch).show_modeless()
             elif ch := changes[self._deactivated_key]:
                 DependentsAffectedDialog(self, ch).show_modeless()
-            self.propagate_refresh(ldiff.to_rdata())
-
-    # Undo/Redo ---------------------------------------------------------------
-    def _undo_redo_op(self, undo_or_redo):
-        """Helper for load order undo/redo operations. Handles UI refreshes."""
-        ldiff = undo_or_redo() # no additions or removals
-        if changed := ldiff.to_rdata():
-            self.propagate_refresh(changed)
-
-    def lo_undo(self):
-        """Undoes a load order change."""
-        self._undo_redo_op(self.data_store.undo_load_order)
-
-    def lo_redo(self):
-        """Redoes a load order change."""
-        self._undo_redo_op(self.data_store.redo_load_order)
+            self.propagate_refresh(lordata)
 
     # Other -------------------------------------------------------------------
     def new_bashed_patch(self):

Mopy/bash/basher/mod_links.py
@@ -259,8 +259,8 @@ class Mod_OrderByName(EnabledLink):
         self.selected.sort(key=lambda m: ( # sort masters first
             *bush.game.master_flags.sort_masters_key(bosh.modInfos[m]), m))
         lowest = load_order.get_ordered(self.selected)[0]
-        ldiff = bosh.modInfos.cached_lo_insert_at(lowest, self.selected)
-        self.window.propagate_refresh(ldiff.to_rdata())
+        lordata = bosh.modInfos.cached_lo_insert_at(lowest, self.selected)
+        self.window.propagate_refresh(lordata)
 
 #------------------------------------------------------------------------------
 class Mod_Move(EnabledLink):
@@ -299,10 +299,9 @@ class Mod_Move(EnabledLink):
         active_plugins = load_order.cached_active_tuple()
         # Clamp between 0 and max plugin index
         target_index = max(0, min(target_index, len(active_plugins) - 1))
-        ldiff = bosh.modInfos.cached_lo_insert_at(active_plugins[target_index],
-                                                  self.selected)
-        self.window.propagate_refresh(ldiff.to_rdata(),
-                                      detail_item=self.selected[0])
+        lordata = bosh.modInfos.cached_lo_insert_at(
+            active_plugins[target_index], self.selected)
+        self.window.propagate_refresh(lordata, detail_item=self.selected[0])
 
 #------------------------------------------------------------------------------
 class Mod_Redate(File_Redate):
@@ -1001,8 +1000,7 @@ class Mod_RebuildPatch(_Mod_BP_Link):
                         self._showError(msg)
                         break # don't keep trying
             if act_save:
-                ldiff = bosh.modInfos.cached_lo_save_active()
-                bp_rdata.redraw |= ldiff.to_rdata().redraw
+                bp_rdata |= bosh.modInfos.cached_lo_save_active()
             if bp_masters:
                 msg = _('Masters Activated: %(num_activated)d') % {
                     'num_activated': len(bp_masters)}
@@ -1551,15 +1549,14 @@ class AFlipFlagLink(EnabledLink):
                 minfo.set_plugin_flags(set_flags)
             ##: HACK: forcing active refresh cause mods may be reordered and
             # we then need to sync order in skyrim's plugins.txt
-            ldiff = bosh.modInfos.refreshLoadOrder()
+            lordata = bosh.modInfos.refreshLoadOrder()
             # This will have changed the plugin, so let BAIN know
             bosh.modInfos._notify_bain(
                 altered={p.abs_path for p in self.iselected_infos()})
             # We need to RefreshUI all higher-loading plugins than the lowest
             # plugin that was affected to update the Indices column
-            rdata = ldiff.to_rdata()
-            rdata.redraw.update(self.selected)
-            self.window.propagate_refresh(rdata)
+            lordata.redraw.update(self.selected)
+            self.window.propagate_refresh(lordata)
 
 #------------------------------------------------------------------------------
 class Mod_FlipMasters(OneItemLink, AFlipFlagLink):

Mopy/bash/basher/patcher_dialog.py
@@ -293,7 +293,7 @@ class PatchDialog(DialogWindow):
             self._bps.extend(attrs)
             # We have to parse the new infos first since the masters may differ
             # note this won't activate the new masters, the caller has to do it
-            self._bp_rdata.redraw |= patchFile.p_file_minfos.refresh(rinf).redraw
+            self._bp_rdata |= patchFile.p_file_minfos.refresh(rinf)
         except CancelError:
             pass
         except BPConfigError as e: # User configured BP incorrectly

Mopy/bash/bolt.py
@@ -1922,6 +1922,25 @@ class RefrIn:
         return cls({k: (None, {}) for k in added_fns})
 
 #------------------------------------------------------------------------------
+@dataclass(slots=True)
+class RefrData:
+    """Encapsulate info the backend needs to pass on to the UI for refresh."""
+    redraw: set[FName] = field(default_factory=set)
+    to_del: set[FName] = field(default_factory=set, kw_only=True)
+    to_add: set[FName] = field(default_factory=set, kw_only=True)
+    # renames are a dict of old fn keys to new fn keys
+    renames: dict[FName, FName] = field(default_factory=dict)
+    ren_paths: dict[Path, Path] = field(default_factory=dict)
+
+    def __bool__(self):
+        return bool(self.to_add or self.to_del or self.redraw)
+
+    def __ior__(self, other):
+        for att in self.__slots__:
+            getattr(self, att).update(getattr(other, att)) # sets and dicts
+        return self
+
+#------------------------------------------------------------------------------
 class PickleDict(object):
     """Dictionary saved in a pickle file.
     Note: self.vdata and self.data are not reassigned! (Useful for some clients.)"""
@@ -2783,16 +2802,3 @@ else:
         https://en.wikipedia.org/wiki/Data_deduplication#reflink for more
         information."""
         shutil.copy2(a, b)
-
-@dataclass(slots=True)
-class RefrData:
-    """Encapsulate info the backend needs to pass on to the UI for refresh."""
-    redraw: set[FName] = field(default_factory=set)
-    to_del: set[FName] = field(default_factory=set, kw_only=True)
-    to_add: set[FName] = field(default_factory=set, kw_only=True)
-    # renames are a dict of old fn keys to new fn keys
-    renames: dict[FName, FName] = field(default_factory=dict)
-    ren_paths: dict[Path, Path] = field(default_factory=dict)
-
-    def __bool__(self):
-        return bool(self.to_add or self.to_del or self.redraw)

Mopy/bash/bosh/__init__.py
@@ -2027,13 +2027,15 @@ def _lo_cache(lord_func):
     whenever I change (or attempt to change) the latter, and that I do
     refresh modInfos."""
     @wraps(lord_func)
-    def _modinfos_cache_wrapper(self: ModInfos, *args, **kwargs) -> LordDiff:
+    def _modinfos_cache_wrapper(self: ModInfos, *args, ldiff=None,
+                                **kwargs) -> RefrData:
         """Sync the ModInfos load order and active caches and refresh for
         load order or active changes."""
         try:
-            ldiff: LordDiff = lord_func(self, *args, **kwargs)
+            ldiff = LordDiff() if ldiff is None else ldiff
+            ldiff |= lord_func(self, *args, **kwargs)
             if ldiff.inact_changes_only():
-                return ldiff
+                return ldiff.to_rdata()
             # Update all data structures that may be affected by LO change
             ldiff.affected |= self._refresh_mod_inis_and_strings()
             ldiff.affected |= self._file_or_active_updates()
@@ -2045,7 +2047,7 @@ def _lo_cache(lord_func):
                     self[k].get_table_prop('allowGhosting', True)})
             ldiff.affected.update(mod for mod, modGhost in ghostify.items()
                                   if self[mod].setGhost(modGhost))
-            return ldiff
+            return ldiff.to_rdata()
         finally:
             self._lo_wip = list(load_order.cached_lo_tuple())
             self._active_wip = list(load_order.cached_active_tuple())
@@ -2054,10 +2056,11 @@ def _lo_cache(lord_func):
 def _lo_op(lop_func):
     """Decorator centralizing saving active state/load order changes."""
     @wraps(lop_func)
-    def _lo_activate_wrapper(self: ModInfos, *args, doSave=False,
+    def _lo_activate_wrapper(self: ModInfos, *args, doSave=False, ldiff=None,
                              save_wip_lo=False, **kwargs):
         """Update _active_wip/_lo_wip cache and possibly save changes."""
         out_diff = kwargs.setdefault('out_diff', LordDiff())
+        ldiff = LordDiff() if ldiff is None else ldiff
         save = sum((doSave, save_wip_lo))
         if save > 1:
             raise ValueError('Only one of doSave and save_wip_lo can be True')
@@ -2066,11 +2069,11 @@ def _lo_op(lop_func):
             lo_msg = lop_func(self, *args, **kwargs)
         finally:
             if doSave and out_diff:
-                out_diff = self.cached_lo_save_active().to_rdata()
+                out_diff = self.cached_lo_save_active(ldiff=ldiff)
             elif save_wip_lo and out_diff:
-                out_diff = self._cached_lo_save_lo().to_rdata()
+                out_diff = self._cached_lo_save_lo(ldiff=ldiff)
             elif save:
-                out_diff = out_diff.to_rdata()
+                out_diff = out_diff.to_rdata() # should be empty
             return out_diff if lo_msg is None else (lo_msg, out_diff)
     return _lo_activate_wrapper
 
@@ -2144,14 +2147,16 @@ class ModInfos(TableFileInfos):
         rdata = super().refresh(refresh_infos, booting=booting)
         mods_changes = bool(rdata)
         self._refresh_bash_tags()
+        ldiff = LordDiff()
         if insert_after:
             for k, v in insert_after.items():
                 self._cached_lo_insert_after(v, k)
-            ldiff = self._cached_lo_save_lo()
+            lordata = self._cached_lo_save_lo(ldiff=ldiff)
         else: # if refresh_infos is False but mods are added force refresh
-            ldiff = self.refreshLoadOrder(forceRefresh=mods_changes or
-                unlock_lo, forceActive=bool(rdata.to_del), unlock_lo=unlock_lo)
-        rdata.redraw |= ldiff.reordered # any reordered mods must be redrawn
+            lordata = self.refreshLoadOrder(ldiff=ldiff,
+                forceRefresh=mods_changes or unlock_lo,
+                forceActive=bool(rdata.to_del), unlock_lo=unlock_lo)
+        rdata |= lordata
         # if active did not change, we must perform the refreshes below
         if ldiff.inact_changes_only():
             # in case ini files were deleted or modified or maybe string files
@@ -2160,8 +2165,6 @@ class ModInfos(TableFileInfos):
             rdata.redraw |= self._refresh_mod_inis_and_strings()
             if mods_changes:
                 rdata.redraw |= self._file_or_active_updates()
-        else: # we did all the refreshes above in _modinfos_cache_wrapper
-            rdata.redraw |= ldiff.act_changed() | ldiff.affected
         self._voAvailable, self.voCurrent = bush.game.modding_esms(self)
         return rdata
 
@@ -2483,10 +2486,8 @@ class ModInfos(TableFileInfos):
         return load_order.save_lo(self._lo_wip, acti=self._active_wip)
 
     @_lo_cache
-    def undo_load_order(self): return load_order.undo_load_order()
-
-    @_lo_cache
-    def redo_load_order(self): return load_order.redo_load_order()
+    def undo_redo_load_order(self, redo):
+        return load_order.undo_redo_load_order(redo)
 
     #--Load Order utility methods - be sure cache is valid when using them
     def _cached_lo_insert_after(self, previous, new_mod):

Mopy/bash/load_order.py
@@ -406,9 +406,8 @@ def get_active_mods_lists():
         _active_mods_lists = settings_mods_list
     return _active_mods_lists
 
-def undo_load_order(): return _restore_lo(-1)
-
-def redo_load_order(): return _restore_lo(1)
+def undo_redo_load_order(redo):
+    return _restore_lo(1 if redo else -1)
 
 def _restore_lo(index_move):
     index = _current_list_index + index_move

commit 8cc3f3ab37f1a6c127cdf7b89cf25b8e210d974e

Fix regression in lo undo/redo menus:
Broke in bfc0b5b275965ff13e4bd0d432a70dab859369ee

Mopy/bash/basher/mods_links.py
@@ -612,7 +612,7 @@ class Mods_LOUndo(ItemLink):
     _keyboard_hint = 'Ctrl+Z'
 
     def Execute(self):
-        self.window.lo_undo()
+        self.window.data_store.wip_lo_undo_redo_load_order(False)
 
 #------------------------------------------------------------------------------
 class Mods_LORedo(ItemLink):
@@ -622,7 +622,7 @@ class Mods_LORedo(ItemLink):
     _keyboard_hint = 'Ctrl+Y'
 
     def Execute(self):
-        self.window.lo_redo()
+        self.window.data_store.wip_lo_undo_redo_load_order(True)
 
 #------------------------------------------------------------------------------
 class Mods_LOExport(ItemLink):