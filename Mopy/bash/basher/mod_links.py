wrye-bash/wrye-bash

commit 426937d8514ea44ff607fcd8329e8f750cb28d3e

[!]GPath in canonize_ci_path:
canonize_ci_path is (usually?) passed a path in so it is already GPathed

to_os_path was more difficult to track - there is a single case where
we pass a str (in _KeywordSelect) - we cache those paths now but these
should be few and we do never want to use a bare Path constructor (to
track Path creation for #368). Note that if we don't pass a str here we
could drop the `return _GPath(ci_path)` in the linux implementation.

I went ahead and required to_os_path receiving a Path always -
_image_resource_dir is promoted to a Path. I used to count those `.s`
(#543...) now I don't mind a couple here and there. It's the long
bumpy way towards #368 (actually this has low priority now #543 is
settled).

Chopping off a couple direct Path.__init__ uses - the if check in bash
did not make much sense Path.sys_fs_enc is never Falsy (thanks Infernio
for fixup!) - see:

https://github.com/wrye-bash/wrye-bash/commit/558e4185e022118418ae94db3ab4b7ee5d82c306#r156028348

Finally I dropped os.fspath from the windows implementation - [!] for a
second pair of eyes:

@@ -1249,3 +1249,3 @@ def canonize_ci_path(ci_path: _Path | str) -> _Path | None:
     # Windows is case-insensitive, nothing to do here
-    return _GPath_no_norm(os.fspath(ci_path))
+    return _GPath_no_norm(ci_path)

Makes no sense for path and in all strings come from to_os_path which
got a Path instance for questionable_path argument.

Lastly seems canonize_ci_path never is passed (so returns) None - we
should drop it from the return type if so.

Mopy/bash/bolt.py
@@ -846,18 +846,16 @@ def GPath(str_or_uni: str | os.PathLike[str]) -> Path: ...
 def GPath(str_or_uni: str | os.PathLike[str] | None) -> Path | None:
     """Path factory and cache."""
     if isinstance(str_or_uni, Path) or str_or_uni is None: return str_or_uni
-    if not str_or_uni: return Path('') # needed, os.path.normpath('') = '.'!
+    if not str_or_uni: return empty_path # needed, os.path.normpath('') = '.'!
     if str_or_uni in _gpaths: return _gpaths[str_or_uni]
     return _gpaths.setdefault(str_or_uni, Path(os.path.normpath(str_or_uni)))
 
 ##: generally points at file names, masters etc. using Paths, which they should
 # not - hunt down and just use strings
-def GPath_no_norm(str_or_uni):
+def GPath_no_norm(str_or_uni: str) -> Path:
     """Alternative to GPath that does not call normpath. It is up to the caller
     to ensure that the precondition name == os.path.normpath(name) holds for
-    all values passed into this method. Only str instances accepted!
-
-    :rtype: Path"""
+    all values passed into this method. Only str instances accepted!"""
     if str_or_uni in _gpaths: return _gpaths[str_or_uni]
     return _gpaths.setdefault(str_or_uni, Path(str_or_uni))
 
@@ -903,7 +901,7 @@ class Path(os.PathLike):
 
     @staticmethod
     def getcwd():
-        return Path(os.getcwd())
+        return GPath_no_norm(os.getcwd())
 
     @staticmethod
     def has_invalid_chars(path_str):
@@ -1347,6 +1345,7 @@ class Path(os.PathLike):
         return self # immutable
 
 undefinedPath = GPath(r'C:\not\a\valid\path.exe')
+empty_path = GPath_no_norm('') # evaluates to False in boolean expressions
 
 # We need to split every time we hit a new 'type' of component. So greedily
 # match as many of one type as possible (except dots and dashes, since those

Mopy/bash/env/linux.py
@@ -274,14 +274,14 @@ def mark_high_dpi_aware():
     pass # Windows only
 
 def convert_separators(p):
-    return p.replace(u'\\', u'/')
+    return p.replace('\\', '/')
 
 ##: A more performant implementation would maybe cache folder contents or
 # something similar, as it stands this is not usable for fixing BAIN on Linux
-def canonize_ci_path(ci_path: os.PathLike | str) -> _Path | None:
+def canonize_ci_path(ci_path: _Path | str) -> _Path | None:
     if os.path.exists(ci_path):
-        # Fast path, but GPath it as we haven't normpathed it yet
-        return _GPath(ci_path)
+        # to_os_path receives a Path and returns a str - we have normpath'ed
+        return _GPath_no_norm(ci_path)
     # Find the longest prefix that exists in the filesystem - *some* prefix
     # must exist, even if it's only root
     path_prefix, ci_rem_part = os.path.split(os.path.normpath(ci_path))

Mopy/bash/env/windows.py
@@ -1238,16 +1238,16 @@ def _real_sys_prefix():
 
 def convert_separators(p):
     """Converts other OS's path separators to separators for this OS."""
-    return p.replace(u'/', u'\\')
+    return p.replace('/', '\\')
 
-def canonize_ci_path(ci_path: os.PathLike | str) -> _Path | None:
+def canonize_ci_path(ci_path: _Path | str) -> _Path | None:
     """Alter the case of a case-insensitive path to match directories and files
     actually present in the filesystem, if any. This is basically identical to
     what Wine does when emulating case insensitivity. If this returns None, the
     path does not exist. However, if this does not return None then there is no
     guarantee that the path exists, so check using exists()/is_file()/etc."""
     # Windows is case-insensitive, nothing to do here
-    return _Path(os.fspath(ci_path))
+    return _GPath_no_norm(ci_path)
 
 def set_file_hidden(file_to_hide: str | os.PathLike, is_hidden=True):
     """Mark the file with the specified path as hidden or unhidden, based on

Mopy/bash/barb.py
@@ -44,7 +44,7 @@ from os.path import join as jo
 from . import archives, bolt, initialization
 from . import bass  # for settings (duh!)
 from .bass import AppVersion, dirs
-from .bolt import GPath, GPath_no_norm, deprint, top_level_files
+from .bolt import GPath, GPath_no_norm, deprint, top_level_files, empty_path
 from .exception import BoltError, StateError
 from .wbtemp import TempDir
 
@@ -202,7 +202,7 @@ class RestoreSettings(object):
     to restore the backed up ini, if it exists. Restoring the settings must
     be done on boot as soon as we are able to initialize bass#dirs."""
     __tmpdir_prefix = u'RestoreSettingsWryeBash_'
-    __unset = bolt.Path('')
+    __unset = empty_path
 
     def __init__(self, settings_file):
         self._settings_file = GPath(settings_file)

Mopy/bash/bash.py
@@ -388,7 +388,6 @@ def dump_environment(wxver=None):
         ifileoperation_ver = 'not found'
     # Now that we have checked all dependencies (including potentially missing
     # ones), we can build the environment dump
-    fse = bolt.Path.sys_fs_enc
     msg = [
         f'Using Wrye Bash Version {bass.AppVersion}'
         f'{u" (Standalone)" if bass.is_standalone else u""}',
@@ -412,8 +411,7 @@ def dump_environment(wxver=None):
         # 'encoding' attribute and stdin will be None
         f'Input encoding: {sys.stdin.encoding if sys.stdin else None}; '
         f'output encoding: {getattr(sys.stdout, u"encoding", None)}',
-        f'Filesystem encoding: {fse}'
-        f'{f" - using {bolt.Path.sys_fs_enc}" if not fse else ""}',
+        f'Filesystem encoding: {bolt.Path.sys_fs_enc}',
         f'Command line: {sys.argv}',
     ]
     bolt.deprint(msg := '\n\t'.join([l for l in msg if l is not None]))

Mopy/bash/basher/__init__.py
@@ -1817,9 +1817,8 @@ class INIDetailsPanel(_DetailsMixin, SashPanel):
         return list(self.target_inis.values())[settings['bash.ini.choice']]
 
     @property
-    def target_inis(self):
-        """Return settings[u'bash.ini.choices'], set in IniInfos#__init__.
-        :rtype: OrderedDict[str, bolt.Path]"""
+    def target_inis(self) -> OrderedDict[str, bolt.Path]:
+        """Return settings['bash.ini.choices'], set in IniInfos.__init__."""
         return settings[u'bash.ini.choices']
 
     @property
@@ -3794,7 +3793,7 @@ def Init(bash_app):
     """Initialize the application data and create the BashFrame."""
     #--OnStartup SplashScreen and/or Progress
     # Progress gets hidden behind splash by default
-    splash = GPath(os.path.join(get_image_dir(), 'wryesplash.png'))
+    splash = get_image_dir().join('wryesplash.png')
     show_splash = bass.inisettings['EnableSplashScreen'] and splash.is_file()
     with CenteredSplash(splash.s, show_splash), balt.Progress(
             'Wrye Bash', _('Initializing') + ' ' * 10, elapsed=False) as prog:
@@ -3879,7 +3878,7 @@ def InitImages():
             settings[u'bash.colors'][color_key] = color_val
         colors[color_key] = Color(*color_val)
     #--Images
-    init_image_resources(bass.dirs['images'].s)
+    init_image_resources(bass.dirs['images'])
 
 ##: This hides a circular dependency (__init__ -> links_init -> __init__)
 from .links_init import InitLinks

Mopy/bash/basher/belt.py
@@ -714,22 +714,20 @@ class WryeParser(PreParser):
             self.reversing -= 1
             self.PushFlow('Select', False, ['SelectOne', 'SelectMany', 'Case', 'Default', 'EndSelect'], values = self.choices[self.choiceIdex], hitCase=False)
             return
-        imageJoin = self._wizard_dir.join
-        for i in images_:
+        im_dir_join = get_image_dir().join
+        for im in images_:
             # Try looking inside the package first, then look if it's using one
             # of the images packaged with Wrye Bash (from
             # Mopy/bash/images/Wizard Images)
             # Note that these are almost always Windows paths, so we have to
             # convert them if we're on Linux
-            wiz_img_path = to_os_path(imageJoin(i))
+            wiz_img_path = to_os_path(self._wizard_dir.join(im))
             if wiz_img_path and wiz_img_path.is_file():
                 image_paths.append(wiz_img_path)
-            elif i.lower().startswith('wizard images'):
-                std_img_path = to_os_path(os.path.join(get_image_dir(), i))
-                if std_img_path and std_img_path.is_file():
-                    image_paths.append(std_img_path)
-                else:
-                    image_paths.append(None)
+            elif im.lower().startswith('wizard images'):
+                std_img_path = to_os_path(im_dir_join(im))
+                image_paths.append(std_img_path if std_img_path and
+                    std_img_path.is_file() else None)
             else:
                 image_paths.append(None)
         self.page = PageSelect(self._wiz_parent, bMany, main_desc, titles,

Mopy/bash/basher/frames.py
@@ -28,8 +28,8 @@ from collections import OrderedDict
 
 from .. import balt, bass, bolt, bosh, bush, load_order, wrye_text
 from ..balt import Link, Resources
-from ..bolt import FName, FNDict
-from ..bosh import empty_path, mods_metadata, omods
+from ..bolt import FName, FNDict, empty_path
+from ..bosh import mods_metadata, omods
 from ..env import canonize_ci_path
 from ..exception import StateError, CancelError
 from ..gui import Button, CancelButton, CheckBox, DocumentViewer, DropDown, \

commit 2686905162822bc5a057990e5360a8f3ff68b7f9

Fix canonize_ci_path crashing when passed Paths
Broke in 426937d8514ea44ff607fcd8329e8f750cb28d3e. Only noticed it
breaking on Linux, but the same bug seemingly exists in the Windows
implementation, so fixed it there as well. The real long-term fix is
probably hunting down the last couple places that pass Paths directly
and make them strings too, then change the function signature to make it
require str rather than _Path | str.

Clean up Path a bit

Fix docstrings rendering nonsense characters in PyCharm (the \ was
treated as a bogus escape sequence) and return a tuple instead of a list
for headTail, as indicated by the docs (and as used by all its usages).

Mopy/bash/basher/mod_links.py: Fix regression in Ratings/Groups

Broke in 445f2309a2624116ccf5ab34fa001d651b656644. Doesn't work because
Link expects the parent to be self.window. It also feels really hacky
calling into instance methods from a class that isn't actually part of
our inheritance tree :P

Mopy/bash/basher/mod_links.py
@@ -366,7 +366,7 @@ class _Mod_LabelsData(balt.ListEditorData):
             if mod_inf.get_table_prop(self.column) == oldName:
                 mod_inf.set_table_prop(self.column, newName)
                 renamed.append(fn)
-        Link.refresh_sel(self.parent, renamed)
+        self.parent.RefreshUI(RefrData(set(renamed)))
         #--Done
         return newName
 
@@ -379,7 +379,7 @@ class _Mod_LabelsData(balt.ListEditorData):
             if mod_inf.get_table_prop(self.column) == item:
                 mod_inf.set_table_prop(self.column, None)
                 deletd.append(fn)
-        Link.refresh_sel(self.parent, deletd)
+        self.parent.RefreshUI(RefrData(to_del=set(deletd)))
         #--Done
         return True
 

Mopy/bash/bolt.py
@@ -965,15 +965,15 @@ class Path(os.PathLike):
     #--String/unicode versions.
     @property
     def s(self):
-        """Path as string."""
+        """Returns this path as a string."""
         return self._s
     @property
     def cs(self):
-        """Path as string in normalized case."""
+        """Returns this path as a string in lowercase."""
         return self._cs
     @property
     def sroot(self):
-        """Root as string."""
+        """For alpha/beta.gamma returns alpha/beta as a string."""
         try:
             return self._sroot
         except AttributeError:
@@ -981,7 +981,7 @@ class Path(os.PathLike):
             return self._sroot
     @property
     def shead(self):
-        """Head as string."""
+        """For alpha/beta.gamma, returns alpha as a string."""
         try:
             return self._shead
         except AttributeError:
@@ -989,7 +989,7 @@ class Path(os.PathLike):
             return self._shead
     @property
     def stail(self):
-        """Tail as string."""
+        """For alpha/beta.gamma, returns beta.gamma as a string."""
         try:
             return self._stail
         except AttributeError:
@@ -997,7 +997,7 @@ class Path(os.PathLike):
             return self._stail
     @property
     def sbody(self):
-        """For alpha\beta.gamma returns beta as string."""
+        """For alpha/beta.gamma returns beta as a string."""
         try:
             return self._sbody
         except AttributeError:
@@ -1007,29 +1007,30 @@ class Path(os.PathLike):
     #--Head, tail
     @property
     def headTail(self):
-        """For alpha\beta.gamma returns (alpha,beta.gamma)"""
-        return [GPath(self.shead), GPath(self.stail)]
+        """For alpha/beta.gamma, returns (alpha, beta.gamma)"""
+        return GPath(self.shead), GPath(self.stail)
     @property
     def head(self):
-        """For alpha\beta.gamma, returns alpha."""
+        """For alpha/beta.gamma, returns alpha."""
         return GPath(self.shead)
     @property
     def tail(self):
-        """For alpha\beta.gamma, returns beta.gamma."""
+        """For alpha/beta.gamma, returns beta.gamma."""
         return GPath_no_norm(self.stail)
     @property
     def body(self):
-        """For alpha\beta.gamma, returns beta."""
+        """For alpha/beta.gamma, returns beta."""
         return GPath_no_norm(self.sbody)
 
     #--Root, ext
     @property
     def root(self):
-        """For alpha\beta.gamma returns alpha\beta"""
+        """For alpha/beta.gamma, returns alpha/beta."""
         return GPath(self.sroot)
     @property
     def ext(self):
-        """Extension (including leading period, e.g. '.txt')."""
+        """For alpha/beta.gamma, returns .gamma as a string (including leading
+        period, e.g. '.txt')."""
         try:
             return self._ext
         except AttributeError:
@@ -1037,7 +1038,8 @@ class Path(os.PathLike):
             return self._ext
     @property
     def cext(self):
-        """Extension in normalized case."""
+        """For alpha/beta.gamma, returns .gamma as a lowercase string (e.g.
+        foo.TxT becomes '.txt')."""
         try:
             return self._cext
         except AttributeError:
@@ -1061,8 +1063,8 @@ class Path(os.PathLike):
 
     @property
     def backup(self):
-        """Backup file path."""
-        return self+u'.bak'
+        """Returns a copy of this path with .bak appended to the end."""
+        return self + '.bak' # Path.__add__ !
 
     #--size, atime
     @property

Mopy/bash/env/linux.py
@@ -280,8 +280,10 @@ def convert_separators(p):
 # something similar, as it stands this is not usable for fixing BAIN on Linux
 def canonize_ci_path(ci_path: _Path | str) -> _Path | None:
     if os.path.exists(ci_path):
-        # to_os_path receives a Path and returns a str - we have normpath'ed
-        return _GPath_no_norm(ci_path)
+        # ci_path could either be a str or a Path, but _GPath_no_norm wants a
+        # string, so check first
+        return (ci_path if isinstance(ci_path, _Path) else
+                _GPath_no_norm(ci_path))
     # Find the longest prefix that exists in the filesystem - *some* prefix
     # must exist, even if it's only root
     path_prefix, ci_rem_part = os.path.split(os.path.normpath(ci_path))

Mopy/bash/env/windows.py
@@ -1246,8 +1246,10 @@ def canonize_ci_path(ci_path: _Path | str) -> _Path | None:
     what Wine does when emulating case insensitivity. If this returns None, the
     path does not exist. However, if this does not return None then there is no
     guarantee that the path exists, so check using exists()/is_file()/etc."""
-    # Windows is case-insensitive, nothing to do here
-    return _GPath_no_norm(ci_path)
+    # Windows is case-insensitive, nothing to do here - ci_path could either be
+    # a str or a Path, but _GPath_no_norm wants a string, so check first
+    return (ci_path if isinstance(ci_path, _Path) else
+            _GPath_no_norm(ci_path))
 
 def set_file_hidden(file_to_hide: str | os.PathLike, is_hidden=True):
     """Mark the file with the specified path as hidden or unhidden, based on