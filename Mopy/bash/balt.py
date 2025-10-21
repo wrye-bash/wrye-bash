wrye-bash/wrye-bash

commit 332e213498c377a61477adb04301ed81ed0603c5

Some more details cleanup:
While reviewing 916af6f7e0aad545bd2049b5aea957c1e91fae14, I noticed that
`displayed_item` had very few remaining occurrences. I resisted the
temptation to include its cleanup in that merge without a nightly round -
experience shows that most regressions on `dev` stem from such last-minute
additions to otherwise tested merges - edit: turns out this also fixes a
bug - renaming on mods details would try to use the old name still.

Mopy/bash/balt.py
@@ -1089,7 +1089,7 @@ class UIList(PanelWin):
         if root is None:
             showError(self, newName)
             return EventResult.CANCEL # validate_filename would Veto
-        item_edited = self.panel.detailsPanel.displayed_item
+        item_edited = self.panel.detailsPanel.detail_fn
         with BusyCursor():
             rdata = RefrData()
             for sel_inf in selected:

Mopy/bash/basher/__init__.py
@@ -539,7 +539,7 @@ class MasterList(_ModsUIList):
         status, checkMark = super()._set_icon_text(masterInfo, item_format,
             item_name, loadOrderIndex=self._curr_lo_index[item_name],
             mi=mi, _mouse_text=mouseText)
-        on_display = self.detailsPanel.displayed_item
+        on_display = self.detailsPanel.detail_fn
         if status == 30: # master is missing
             mouseText.append(_('Missing master of %(child_plugin_name)s.') % {
                 'child_plugin_name': on_display})
@@ -1171,25 +1171,23 @@ class _DetailsMixin(object):
         super().__init__(*args, **kwargs)
 
     @property
-    def file_info(self): return self.file_infos.get(self.displayed_item, None)
-    @property
-    def displayed_item(self): return self._detail_fn
+    def file_info(self): return self.file_infos.get(self.detail_fn, None)
+
     @property
     def file_infos(self): raise NotImplementedError
 
     def _resetDetails(self):
-        self._detail_fn = None
+        self.detail_fn = None
 
     # Details panel API
     def SetFile(self, fileName: FName | None = _same_file):
         """Set file to be viewed. Leave fileName empty to reset."""
         #--Reset?
-        fileName = self.displayed_item if fileName is _same_file else fileName
-        if not fileName or (fileName not in self.file_infos):
+        self.detail_fn = fn = self.detail_fn if fileName is _same_file \
+            else fileName
+        if not fn or (fn not in self.file_infos):
             self._resetDetails()
-        else:
-            self._detail_fn = fileName
-        return self._detail_fn
+        return self.detail_fn
 
 def _check_displayed(func):
     """Check there is an item displayed before proceeding."""
@@ -1251,6 +1249,7 @@ class _EditableMixin(_DetailsMixin):
         #--Change Tests
         if (ren_data := self._rename_detail_item()) is None:
             return
+        self.detail_fn = det_it = next(iter(ren_data.renames.values()))
         change_hdr, ref_saves, kwargs = self._extra_changes(ren_data)
         # Although we could avoid rereading the header by passing the info in
         # I leave it here as an extra error check - error handling is WIP
@@ -2586,7 +2585,7 @@ class InstallersDetails(_SashDetailsPanel):
     }
 
     @property
-    def file_infos(self): return self._idata
+    def file_infos(self): return self.installersPanel.listData
 
     def __init__(self, parent, ui_list_panel):
         """Initialize."""
@@ -2594,7 +2593,6 @@ class InstallersDetails(_SashDetailsPanel):
         self.infoPages = []
         super().__init__(parent)
         self.installersPanel = ui_list_panel
-        self._idata = self.installersPanel.listData
         top, bottom = self.left, self.right
         commentsSplitter = self.splitter
         self.subSplitter, commentsPanel = commentsSplitter.make_panes(
@@ -2684,7 +2682,7 @@ class InstallersDetails(_SashDetailsPanel):
         if wx_id == self.gNotebook.wx_id_(): # todo because of BashNotebook event??
             # todo use the pages directly not the index
             gPage,initialized = self.infoPages[selected_index]
-            if self._detail_fn and not initialized:
+            if self.detail_fn and not initialized:
                 self.RefreshInfoPage(selected_index, self.file_info)
 
     def ClosePanel(self, destroy=False):
@@ -2699,16 +2697,16 @@ class InstallersDetails(_SashDetailsPanel):
         inst = self.file_info
         if inst and self.gComments.modified:
             inst.comments = self.gComments.text_content
-            self._idata.hasChanged = True
+            self.file_infos.hasChanged = True
 
     def SetFile(self, fileName=_same_file):
         """Refreshes detail view associated with data from item."""
-        if self._detail_fn is not None:
+        if self.detail_fn is not None:
             self._save_comments()
         fileName = super(InstallersDetails, self).SetFile(fileName)
         del self.espm_checklist_fns[:]
         if fileName:
-            installer = self._idata[fileName]
+            installer = self.file_info
             #--Name
             self.gPackage.text_content = fileName
             #--Info Pages
@@ -2816,10 +2814,10 @@ class InstallersDetails(_SashDetailsPanel):
         elif pageName == u'gMismatched':
             gPage.text_content = _dumpFiles(installer.mismatchedFiles)
         elif pageName == u'gConflicts':
-            gPage.text_content = self._idata.getConflictReport(
+            gPage.text_content = self.file_infos.getConflictReport(
                 installer, u'OVER', bosh.modInfos)
         elif pageName == u'gUnderrides':
-            gPage.text_content = self._idata.getConflictReport(
+            gPage.text_content = self.file_infos.getConflictReport(
                 installer, u'UNDER', bosh.modInfos)
         elif pageName == u'gDirty':
             gPage.text_content = _dumpFiles(installer.dirty_sizeCrc)
@@ -2833,13 +2831,13 @@ class InstallersDetails(_SashDetailsPanel):
     def refreshCurrent(self,installer):
         """Refreshes current item while retaining scroll positions."""
         installer.refreshDataSizeCrc()
-        installer.refreshStatus(self._idata)
+        installer.refreshStatus(self.file_infos)
         # Save scroll bar positions, because gList.RefreshUI will
         subScrollPos  = self.gSubList.lb_get_vertical_scroll_pos()
         espmScrollPos = self.gEspmList.lb_get_vertical_scroll_pos()
         subIndices = self.gSubList.lb_get_selections()
-        self.installersPanel.uiList.RefreshUI(RefrData({self.displayed_item}),
-                                              detail_item=self.displayed_item)
+        self.installersPanel.uiList.RefreshUI(RefrData({self.detail_fn}),
+                                              detail_item=self.detail_fn)
         for subIndex in subIndices:
             self.gSubList.lb_select_index(subIndex)
         # Reset the scroll bars back to their original position
@@ -3162,7 +3160,7 @@ class ScreensList(UIList):
         if numStr is None: # note we allow for number only names
             showError(self, root)
             return EventResult.CANCEL
-        item_edited = self.panel.detailsPanel.displayed_item
+        item_edited = self.panel.detailsPanel.detail_fn
         with BusyCursor():
             rdata = RefrData()
             for sel_inf in selected:

Mopy/bash/basher/installer_links.py
@@ -716,7 +716,7 @@ class Installer_Move(_InstallerLink):
         self.idata.moveArchives(self.selected, newPos)
         self.idata.refresh_n()
         self.window.RefreshUI(
-            detail_item=self.iPanel.detailsPanel.displayed_item)
+            detail_item=self.iPanel.detailsPanel.detail_fn)
 
 #------------------------------------------------------------------------------
 class _Installer_OpenAt(_InstallerLink):

commit f41d009124d26cbfa62fcc4a2a15730f17c3f214

Use to_add for renames - fixup in ghost's BAIN refresh:
As far as the rest of the code is concerned those are to be added not
redrawn, although as seen redraw and to_add mostly come in pairs - very
few occurrences of either which is a good sign for RefrData.

`new_changed` to track redraw | to_add:

Once the dust settles a final test for RefrData would be to use to_add
and redraw separately in RUI (instead of dumping them together). We
would need to add code in __ior__ for that - and centralize RefrData
attributes access which are now few (41/50 occurrences in code/in total
of to_add/to_del/redraw).

Mopy/bash/bolt.py: I should be using abs_path here for ghosts - this
can explain some ghostly glitches in BAIN refresh - more on the latter
in next commit (ghosts is the single worst source of complications in
the codebase). From cd2460c199d20a0ff615d904c31ffcd2c9427117

Mopy/bash/basher/__init__.py: regression from
332e213498c377a61477adb04301ed81ed0603c5 - renames could be empty.
More work on this in next commit.

`dest_dir` renames - will come handy soon

Under #353

Mopy/bash/balt.py
@@ -786,7 +786,7 @@ class UIList(PanelWin):
             with self.pause_drawing():
                 for d in rdata.to_del:
                     self.__gList.RemoveItemAt(self._get_uil_index(d))
-                for upd in rdata.redraw | rdata.to_add:
+                for upd in rdata.new_changed():
                     self.PopulateItem(item=upd, **ui_kwargs)
                 #--Sort
                 self.SortItems()
@@ -1120,7 +1120,7 @@ class UIList(PanelWin):
             else:
                 self.RefreshUI(rdata, **args_dict)
             #--Reselect the renamed items
-            self.SelectItemsNoCallback(rdata.redraw)
+            self.SelectItemsNoCallback(rdata.to_add)
 
     def _on_f2_handler(self, is_f2_down, ec_value, uilist_ctrl):
         """For pressing F2 on the edit box for renaming"""

Mopy/bash/barb.py
@@ -275,10 +275,10 @@ class RestoreSettings(object):
             full_back_path.join(*end_path).copyTo(dest)
         restore_paths = list(_init_settings_files(
             bak_name, mg_name, root_prefix, mods_folder_name))
-        for dest_dir, back_path in restore_paths:
+        for destdir, back_path in restore_paths:
             full_back_path = self._extract_dir.join(back_path)
             for fname in top_level_files(full_back_path):
-                _restore_file(dest_dir, GPath(back_path), fname)
+                _restore_file(destdir, GPath(back_path), fname)
         # restore savegame profile settings
         back_path = GPath('My Games').join(mg_name, saves_folder)
         sav_path = dirs['saveBase'].join(saves_folder)

Mopy/bash/basher/__init__.py
@@ -1249,7 +1249,8 @@ class _EditableMixin(_DetailsMixin):
         #--Change Tests
         if (ren_data := self._rename_detail_item()) is None:
             return
-        self.detail_fn = det_it = next(iter(ren_data.renames.values()))
+        self.detail_fn = det_it = next(iter(ren.values())) if (
+            ren := ren_data.renames) else self.detail_fn
         change_hdr, ref_saves, kwargs = self._extra_changes(ren_data)
         # Although we could avoid rereading the header by passing the info in
         # I leave it here as an extra error check - error handling is WIP

Mopy/bash/basher/files_links.py
@@ -138,7 +138,7 @@ class File_Duplicate(ItemLink):
                 {k: pairs[v] for k, v in mod_previous.items()})
             ##:(701) we should reset-status here for lower loading mods
             rdata = fileInfos.refresh(rinf, insert_after=mod_previous)
-            self.refresh_sel(rdata.redraw | rdata.to_add,
+            self.refresh_sel(rdata.new_changed(),
                              detail_item=next(reversed(mod_previous)))
             self.window.SelectItemsNoCallback(mod_previous)
 

Mopy/bash/bolt.py
@@ -1837,7 +1837,8 @@ class ListInfo:
 
     def set_path_keys(self, new_fn: FName, *, infodir=None):
         old_key, self.fn_key = self.fn_key, new_fn
-        return RefrData({new_fn}, to_del={old_key}, renames={old_key: new_fn})
+        return RefrData(to_add={new_fn}, to_del={old_key},
+                        renames={old_key: new_fn})
 
     def get_rename_paths(self, newName):
         return [] # no rename paths for markers
@@ -1901,7 +1902,7 @@ class AFileInfo(AFile, ListInfo):
         ren_d = super().set_path_keys(new_fn)
         new_path = (infodir or self._store().store_dir).join(new_fn)
         old_path, self.abs_path = self.abs_path, new_path
-        ren_d.ren_paths[old_path] = new_path
+        ren_d.ren_paths[old_path] = self.abs_path
         return ren_d
 
     def __repr__(self): # bypass AFInfo - abs path is not always set
@@ -1973,7 +1974,7 @@ class RefrData:
 
     def __ior__(self, other):
         # we suppose `other` is more up to date so deleted infos might reappear
-        self.to_del -= other.redraw | other.to_add
+        self.to_del -= other.new_changed()
         it = ((getattr(self, att), oth_att) for att in self.__slots__ if
               (oth_att := getattr(other, att)))
         for self_att, other_att in it:
@@ -1982,6 +1983,8 @@ class RefrData:
             setattr(self, att, getattr(self, att) - self.to_del)
         # we suppose renames/ren_paths key/values are unique while we ior them
         return self
+
+    def new_changed(self): return self.to_add | self.redraw
 
 #------------------------------------------------------------------------------
 class PickleDict(object):

Mopy/bash/bosh/__init__.py
@@ -413,20 +413,20 @@ class ModInfo(_WithMastersInfo):
         super().__init__(fullpath, **kwargs)
 
     def get_hide_dir(self):
-        dest_dir = self._store().hide_dir
+        hide_d = self._store().hide_dir
         #--Use author subdirectory instead?
         mod_author = self.header.author
         if mod_author:
-            authorDir = dest_dir.join(mod_author)
+            authorDir = hide_d.join(mod_author)
             if authorDir.is_dir():
                 return authorDir
         #--Use group subdirectory instead?
         file_group = self.get_table_prop(u'group')
         if file_group:
-            groupDir = dest_dir.join(file_group)
+            groupDir = hide_d.join(file_group)
             if groupDir.is_dir():
                 return groupDir
-        return dest_dir
+        return hide_d
 
     def get_persistent_attrs(self, *, exclude=frozenset()):
         if exclude is True:
@@ -1628,7 +1628,7 @@ class _AFileInfos(DataStore):
                         traceback=True)
         if delinfos:
             rdata.to_del |= self._delete_refresh(delinfos)
-        if not booting and ((alt := rdata.redraw | rdata.to_add) or delinfos):
+        if not booting and ((alt := rdata.new_changed()) or delinfos):
             alt = {self[n].abs_path for n in alt}
             self._notify_bain({inf.abs_path for inf in delinfos}, alt)
         return rdata
@@ -2016,8 +2016,8 @@ def _lo_cache(lord_func):
                 new_inactive = ldiff.new_inact | (ldiff.added - ldiff.new_act)
                 ghostify.update({k: True for k in new_inactive if
                     self[k].get_table_prop('allowGhosting', True)})
-            ldiff.affected.update(mod for mod, modGhost in ghostify.items()
-                                  if self[mod].setGhost(modGhost))
+            ldiff.affected.update(mod for mod, ghost_it in ghostify.items()
+                                  if self[mod].setGhost(ghost_it))
             return ldiff.to_rdata()
         finally:
             self._lo_wip = list(load_order.cached_lo_tuple())

Mopy/bash/bosh/bain.py
@@ -2328,7 +2328,7 @@ class InstallersData(DataStore):
                     refresh_info.redraw.add(item)
                 else: installers.add(item)
         if scanning:
-            exist = installers | refresh_info.to_add | refresh_info.redraw
+            exist = installers | refresh_info.new_changed()
             refresh_info.to_del = set(self.ipackages(self)) - exist
         return refresh_info
 