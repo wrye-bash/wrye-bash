wrye-bash/wrye-bash

commit 3ecdcfa586a4aade3c91a1869eed3ff28830b89f

WIP nit around core BAIN:
Follow up from parent 960c0d5018120cb5aded9e861a02cd64ead04b86.

- _install was really only providing the source paths - I extracted this
to _install_source_args permitting me to inline _fs_install. Flat is
better than nested - in particular it is now obvious how/when filesystem
install is performed, as opposed to deletions that happen in
_modify_data_dir at once. Should we move that there too? So make all the
operations just on RAM here (collect list of files to copy) as we do for
deletions.
- I inlined _removeFiles whose docs were badly out of date anyway.
_remove_restore is not the beast it used to be and we can flatten.
- cleaning this up made me realize that the unArchives argument is
redundant - setting the archive to inactive should be done right in
bain_uninstall
- I inlined listSource which really belongs to the UI - some of it needs
be moved to InstallerPackage

Under #219

Mopy/bash/basher/installer_links.py
@@ -665,7 +665,13 @@ class Installer_ListStructure(_SingleInstallable):
 
     @balt.conversation ##: no use ! _showLog returns immediately
     def Execute(self):
-        source_list_txt = self._selected_info.listSource()
+        """Return package structure as text."""
+        log = bolt.LogFile(io.StringIO())
+        log.setHeader(f'{self} ' + _('Package Structure:'))
+        log('[spoiler]\n', False) ##: do we need these spoiler tags?
+        self._selected_info.list_package(log)
+        log('[/spoiler]')
+        source_list_txt = log.out.getvalue()
         #--Get masters list
         copy_text_to_clipboard(source_list_txt)
         self._showLog(source_list_txt, title=_('Package Structure'))

Mopy/bash/bosh/bain.py
@@ -1312,21 +1312,16 @@ class _InstallerPackage(Installer, AFileInfo):
         raise NotImplementedError
 
     #--ABSTRACT ---------------------------------------------------------------
-    def install(self, destFiles: set[CIstr], progress, **kwargs):
+    def install(self, destFiles: set[CIstr], progress, *, rui_data, **kwargs):
         """Install specified files to Data directory."""
         dest_src = self.refreshDataSizeCrc(True)
         dest_src = {k: v for k, v in dest_src.items() if k in destFiles}
-        if not dest_src: return bolt.LowerDict(), defaultdict(bool)
+        if not dest_src:
+            return
         progress = progress if progress else bolt.Progress()
-        self._install(dest_src, progress, **kwargs)
-
-    def _install(self, dest_src, progress, **kwargs):
-        raise NotImplementedError
-
-    def _fs_install(self, dest_src, srcDirJoin, progress, subprogressPlus,
-                    unpackDir, *, rui_data, **kwargs):
-        """Filesystem install, if unpackDir is not None we are installing
-         an archive."""
+        srcDirJoin, subprogressPlus, unpackDir = self._install_source_args(
+            dest_src, progress)
+        # Filesystem install, unpackDir is not None only for archives
         data_sizeCrcDate_update = bolt.LowerDict()
         data_sizeCrc = self.ci_dest_sizeCrc
         stores = data_tracking_stores()
@@ -1365,7 +1360,7 @@ class _InstallerPackage(Installer, AFileInfo):
         idata_data_scd = self.instData.data_sizeCrcDate
         from ..bosh import modInfos
         for dest, (s, c, dest_path) in data_sizeCrcDate_update.items():
-            d = dest_path.mtime # update mtime after copy/move
+            d = dest_path.mtime # update mtime after copy/move ##:(241) needed or use cached value?
             if st_fn := dest_to_store.get(dest):
                 at = {'installer': str(self.fn_key)}
                 if (st := st_fn[0]) is modInfos:
@@ -1374,17 +1369,10 @@ class _InstallerPackage(Installer, AFileInfo):
                     extra_attrs={st_fn[1]: at}, store=st)
             idata_data_scd[dest] = (s, c, d)
 
-    def listSource(self):
-        """Return package structure as text."""
-        log = bolt.LogFile(io.StringIO())
-        log.setHeader(f'{self} ' + _('Package Structure:'))
-        log('[spoiler]\n', False) ##: do we need these spoiler tags?
-        self._list_package(self.abs_path, log)
-        log('[/spoiler]')
-        return log.out.getvalue()
+    def _install_source_args(self, dest_src, progress):
+        raise NotImplementedError
 
-    @staticmethod
-    def _list_package(apath, log):
+    def list_package(self, log):
         raise NotImplementedError
 
     def sync_from_data(self, delta_files: set[CIstr], progress, archive_name):
@@ -1602,7 +1590,7 @@ class InstallerArchive(_InstallerPackage):
                 bolt.clearReadOnly(unpack_dir)
         return GPath_no_norm(unpack_dir)
 
-    def _install(self, dest_src, progress, **kwargs):
+    def _install_source_args(self, dest_src, progress):
         #--Extract
         progress(0, ('%s\n' % self) + _('Extracting files…'))
         unpackDir = self.unpackToTemp(list(dest_src.values()),
@@ -1610,11 +1598,10 @@ class InstallerArchive(_InstallerPackage):
         #--Rearrange files
         progress(0.9, ('%s\n' % self) + _('Organizing files…'))
         srcDirJoin = unpackDir.join
-        subprogress = SubProgress(progress,0.9,1.0)
+        subprogress = SubProgress(progress, 0.9, 1.0)
         subprogress.setFull(len(dest_src))
         subprogressPlus = subprogress.plus
-        self._fs_install(dest_src, srcDirJoin, progress, subprogressPlus,
-                         unpackDir, **kwargs)
+        return srcDirJoin, subprogressPlus, unpackDir
 
     def unpackToProject(self, project, progress):
         """Unpacks archive to build directory."""
@@ -1642,8 +1629,7 @@ class InstallerArchive(_InstallerPackage):
         cleanup_temp_dir(unpack_dir)
         return count
 
-    @staticmethod
-    def _list_package(apath, log):
+    def list_package(self, log):
         list_text = []
         filepath = u''
         def _parse_archive_line(key, value):
@@ -1655,7 +1641,7 @@ class InstallerArchive(_InstallerPackage):
                     (f'{filepath}', value and (u'D' in value)))
             elif key == u'Method':
                 filepath = u''
-        list_archive(apath, _parse_archive_line)
+        list_archive(self.abs_path, _parse_archive_line)
         list_text.sort()
         #--Output
         for node, isdir_ in list_text:
@@ -1805,20 +1791,18 @@ class InstallerProject(_InstallerPackage):
         self.project_refreshed = True
 
     # Installer API -----------------------------------------------------------
-    def _install(self, dest_src, progress, **kwargs):
+    def _install_source_args(self, dest_src, progress):
         progress.setFull(len(dest_src))
         progress(0, f'{self}\n' + _('Moving files…'))
         progressPlus = progress.plus
         #--Copy Files
         srcDirJoin = self.abs_path.join
-        self._fs_install(dest_src, srcDirJoin, progress, progressPlus, None,
-                         **kwargs)
+        return srcDirJoin, progressPlus, None
 
     def sync_from_data(self, delta_files: set[CIstr], progress, archive_name):
         return self._do_sync_data(self.abs_path, delta_files, progress)
 
-    @staticmethod
-    def _list_package(apath, log):
+    def list_package(self, log):
         def walkPath(folder, depth):
             r, folders, files = next(os.walk(folder))
             indent = u' ' * depth
@@ -1829,7 +1813,7 @@ class InstallerProject(_InstallerPackage):
                 depth += 2
                 walkPath(os.path.join(r, d), depth)
                 depth -= 2
-        walkPath(apath, 0)
+        walkPath(self.abs_path, 0)
 
     def _open_txt_file(self, rel_path): self.abs_path.join(rel_path).start()
 
@@ -2713,23 +2697,6 @@ class InstallersData(DataStore):
             emptyDirs -= excludir
         return allRemoves
 
-    def _removeFiles(self, ci_removes, *, removed_tracked, removed_untracked,
-                     **kwargs):
-        """Performs the actual deletion of files and updating of internal data,
-           used by 'bain_uninstall' and 'bain_anneal'."""
-        if not ci_removes: return
-        mods_dir_join = bass.dirs[u'mods'].join
-        #--Construct list of files to delete
-        for ci_rel_path in ci_removes:
-            for store, removed_files in removed_tracked.items():
-                if store_info := store.data_path_to_info(ci_rel_path):
-                    removed_files.add(store_info.fn_key)
-                    break
-            else:
-                path = mods_dir_join(ci_rel_path)
-                if path.exists():
-                    removed_untracked.add(path)
-
     def __restore(self, installer, removes, restores, *, cede_ownership,
                   **kwargs):
         """Populate restores dict with files to be restored by this
@@ -2781,6 +2748,7 @@ class InstallersData(DataStore):
         for installer in self.sorted_values(reverse=True):
             #--Uninstall archive?
             if installer in unArchives:
+                installer.is_active = False
                 for data_sizeCrc in (installer.ci_dest_sizeCrc,installer.dirty_sizeCrc):
                     for ci_file, sizeCrc in data_sizeCrc.items():
                         try:
@@ -2794,15 +2762,22 @@ class InstallersData(DataStore):
             elif installer.is_active:
                 masked |= self.__restore(installer, removes, restores, **kwargs)
         anneal = bass.settings[u'bash.installers.autoAnneal']
-        self._remove_restore(removes, restores, unArchives, anneal, **kwargs)
+        self._remove_restore(removes, restores, anneal, **kwargs)
 
-    def _remove_restore(self, removes, restores, unArchives, anneal=True, *,
-                        progress, **kwargs):
-        #--Remove files, update InstallersData, update load order
-        self._removeFiles(removes, **kwargs)
-        #--De-activate
-        for inst in unArchives:
-            inst.is_active = False
+    def _remove_restore(self, removes, restores, anneal=True, *, progress,
+                        removed_tracked, removed_untracked, **kwargs):
+        #--Construct list of files to delete
+        if removes:
+            mods_dir_join = bass.dirs['mods'].join
+            for ci_rel_path in removes:
+                for store, removed_files in removed_tracked.items():
+                    if store_info := store.data_path_to_info(ci_rel_path):
+                        removed_files.add(store_info.fn_key)
+                        break
+                else:
+                    path = mods_dir_join(ci_rel_path)
+                    if path.exists():
+                        removed_untracked.add(path)
         #--Restore files
         if anneal:
             restores = dict_sort(restores, by_value=True)
@@ -2846,7 +2821,7 @@ class InstallersData(DataStore):
             #  And/or may block later uninstalls.
             if installer.is_active:
                 self.__restore(installer, removes, restores, **kwargs)
-        self._remove_restore(removes, restores, frozenset(), **kwargs)
+        self._remove_restore(removes, restores, **kwargs)
 
     @_bain_op
     def bain_wiz_install(self, packages, *, progress, **kwargs):

commit ee366bae5e6f566e71d0aeee02710c115927fd2a

Remove BBCode spoiler tags
They're only useful when posting to a forum that uses BBCode, but we
mostly receive issue reports via Discord & GitHub which use different
spoiler tags (& Discord's don't even collapse tagged text, the entire
reason we use them). AFK Mods uses them, but you can easily add spoiler
tags via its UI.

installer_links.py:
Print package name correctly when listing package structure. Broke in
3ecdcfa586a4aade3c91a1869eed3ff28830b89f.

TES4/FO3/FNV: Prevent ITMs in Nvidia Fog Fix tweak

WB doesn't write XCLL when it's missing, so avoid records missing XCLL
to prevent ITMs.

Clean up the code a little & correct the docs on the command as well.

Utumno: we decided to drop tweak & command but let's have the fixed
version dropped :)

Mopy/Docs/Wrye Bash Advanced Readme.html
@@ -2905,7 +2905,7 @@ tab. The directories skipped are:
                 Systems with very old Nvidia graphics cards can suffer a
                 black screen bug when interior cell fog distances are set to 0.
                 This command scans the selected plugins and sets any
-                zero-valued fog distances to 0.001.<br>
+                zero-valued fog distances to 0.0001.<br>
                 <br>
                 <strong>Note:</strong> This command is probably not needed
                 anymore. 'Very old' does mean <em>very</em> old, i.e. referring

Mopy/bash/basher/bsa_links.py
@@ -96,11 +96,10 @@ class BSA_ListContents(ItemLink):
               u'clipboard.')
 
     def Execute(self):
-        full_text = ['=== Selected BSA Contents:', '[spoiler]']
+        full_text = ['=== Selected BSA Contents:']
         for bsa_inf in self.iselected_infos():
             full_text.append(f'\n* {bsa_inf.fn_key}:')
             full_text.extend(sorted(bsa_inf.assets))
-        full_text.append('[/spoiler]')
         full_text = '\n'.join(full_text)
         copy_text_to_clipboard(full_text)
         self._showLog(full_text, _('BSA Contents'))

Mopy/bash/basher/frames.py
@@ -498,7 +498,7 @@ class PluginChecker(WindowFrame):
 
     def OnCopyText(self):
         """Copies text of report to clipboard."""
-        mods_txt = f'[spoiler]\n{self.check_mods_text}[/spoiler]'
+        mods_txt = self.check_mods_text
         mods_txt = re.sub(r'\[\[.+?\|\s*(.+?)\]\]', r'\1', mods_txt)
         mods_txt = re.sub(r'(__|\*\*|~~)', '', mods_txt)
         mods_txt = re.sub('&bull; &bull;', '**', mods_txt)

Mopy/bash/basher/ini_links.py
@@ -60,9 +60,8 @@ class INI_ListINIs(ItemLink):
 
     def Execute(self):
         """Handle printing out the errors."""
-        tweaklist = [_('Active INI Tweaks:'), '[spoiler]', *map(str, (
-            tweak for tweak, info in dict_sort(self._data_store)
-            if not info.ini_st == 20)), '[/spoiler]', '']
+        tweaklist = [_('Active INI Tweaks:'), *map(str, (tweak for tweak, info
+            in dict_sort(self._data_store) if not info.ini_st == 20)), '']
         tweak_list = '\n'.join(tweaklist)
         copy_text_to_clipboard(tweak_list)
         self._showLog(tweak_list, title=_('Active INIs'))

Mopy/bash/basher/installer_links.py
@@ -667,10 +667,8 @@ class Installer_ListStructure(_SingleInstallable):
     def Execute(self):
         """Return package structure as text."""
         log = bolt.LogFile(io.StringIO())
-        log.setHeader(f'{self} ' + _('Package Structure:'))
-        log('[spoiler]\n', False) ##: do we need these spoiler tags?
+        log.setHeader(f"{self._selected_item} {_('Package Structure:')}")
         self._selected_info.list_package(log)
-        log('[/spoiler]')
         source_list_txt = log.out.getvalue()
         #--Get masters list
         copy_text_to_clipboard(source_list_txt)
@@ -1024,13 +1022,12 @@ class Installer_Espm_List(_Installer_Details_Link):
               'sub-packages (and copies it to the system clipboard).')
 
     def Execute(self):
-        subs = _('Plugin List for %(sel_pkg)s:') % {'sel_pkg': self._installer}
-        subs += '\n[spoiler]\n'
+        subs = _('Plugin List for %(sel_pkg)s:') % {
+            'sel_pkg': self._installer} + '\n'
         el = self.window.gEspmList
         for i in range(el.lb_get_items_count()):
             subs += (f"{'**' if el.lb_is_checked_at_index(i) else '  '} "
                      f"{self.window.get_espm(i)}\n")
-        subs += '[/spoiler]'
         copy_text_to_clipboard(subs)
         self._showLog(subs, title=_('Plugin List'))
 
@@ -1095,12 +1092,11 @@ class Installer_Subs_ListSubPackages(_Installer_Subs):
 
     def Execute(self):
         subs = _('Sub-Packages List for %(sel_pkg)s:') % {
-            'sel_pkg': self._installer} + '\n[spoiler]\n'
+            'sel_pkg': self._installer} + '\n'
         sl = self.window.gSubList
         for i in range(sl.lb_get_items_count()):
             subs += (f"{'**' if sl.lb_is_checked_at_index(i) else '  '} "
                      f"{sl.lb_get_str_item_at_index(i)}\n")
-        subs += '[/spoiler]'
         copy_text_to_clipboard(subs)
         self._showLog(subs, title=_('Sub-Package List'))
 

Mopy/bash/basher/mod_links.py
@@ -751,8 +751,6 @@ class Mod_CopyModInfo(ItemLink):
                     'plugin_ver': vers})
             info_txt.append('\n'.join(mod_text))
         info_txt = '\n\n'.join(info_txt) # add a blank line in between mods
-        if len(self.selected) > 5:
-            info_txt = f'[spoiler]\n{info_txt}\n[/spoiler]'
         # Show results + copy to clipboard
         copy_text_to_clipboard(info_txt)
         self._showLog(info_txt, title=_('Plugin Info Report'))
@@ -1124,7 +1122,6 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
               'report has also been copied into your clipboard.') + '\n')
         clip = io.StringIO()
         clip.write('%s %s:\n' % (bp_parent_info.fn_key, _('Config')))
-        clip.write(u'[spoiler]\n')
         log.setHeader(u'== '+_(u'Patch Mode'))
         clip.write(u'== '+_(u'Patch Mode')+u'\n')
         log(u'Python')
@@ -1134,7 +1131,6 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
             patcher._bp = temp_bp
             patcher.log_config(config, clip, log)
         #-- Show log
-        clip.write(u'[/spoiler]')
         copy_text_to_clipboard(clip.getvalue())
         log_text = log.out.getvalue()
         self._showWryeLog(log_text, title=_(u'Bashed Patch Configuration'))

Mopy/bash/bosh/__init__.py
@@ -3021,7 +3021,6 @@ class ModInfos(TableFileInfos):
         all_mods = load_order.get_ordered(all_mods)
         #--List
         modIndex = 0
-        if not wtxt: log(u'[spoiler]\n', appendNewline=False)
         for mname in all_mods:
             if mname in masters_set:
                 prefix = f'{bul}{modIndex:02X}'
@@ -3045,7 +3044,6 @@ class ModInfos(TableFileInfos):
                     elif load_order.get_ordered(
                             (mname, master2))[1] == master2:
                         log(sDelinquent % {'d_master': master2})
-        if not wtxt: log(u'[/spoiler]')
         return log.out.getvalue()
 
     def getTagList(self, mod_list=None):
@@ -3053,7 +3051,6 @@ class ModInfos(TableFileInfos):
         ones are applied via a patch) - either for all mods in the data folder
         or if specified for one specific mod."""
         tagList = f"=== {_('Current Bash Tags:')}\n"
-        tagList += u'[spoiler]\n'
         tagList += _(u'Note: Sources are processed from top to bottom, '
                      u'meaning that lower-ranking sources override '
                      u'higher-ranking ones.') + u'\n'
@@ -3067,7 +3064,6 @@ class ModInfos(TableFileInfos):
         for modInfo in mod_list:
             tagList += f'\n* {modInfo}\n'
             tagList = modInfo.tagsies(tagList)
-        tagList += u'[/spoiler]'
         return tagList
 
     def masterWithVersion(self, master_name):

Mopy/bash/bosh/bain.py
@@ -3112,10 +3112,8 @@ class InstallersData(DataStore):
         log = bolt.LogFile(io.StringIO())
         log.setHeader(_('BAIN Packages:'))
         #--List
-        log(u'[spoiler]\n', False)
         for inst in self.sorted_values():
             inst.log_package(log, showInactive)
-        log(u'[/spoiler]')
         return log.out.getvalue()
 
     def filterInstallables(self, installerKeys: Iterable[FName]):

Mopy/bash/patcher/patchers/multitweak_assorted.py
@@ -266,20 +266,20 @@ class AssortedTweak_DarnBooks(MultiTweakItem):
 #------------------------------------------------------------------------------
 class AssortedTweak_FogFix(MultiTweakItem):
     """Fix fog in cell to be non-zero."""
-    tweak_name = _(u'Nvidia Fog Fix')
-    tweak_tip = _(u'Fix fog related Nvidia black screen problems.')
-    tweak_key = u'FogFix'
-    tweak_choices = [(u'0.0001', u'0.0001')]
-    tweak_log_msg = _(u'Cells With Fog Tweaked To 0.0001: %(total_changed)d')
-    # Probably not needed on newer games, so default-enable only on TES4
-    default_enabled = bush.game.fsName == u'Oblivion'
     tweak_read_classes = b'CELL',
+    tweak_name = _('Nvidia Fog Fix')
+    tweak_tip = _('Fix fog related Nvidia black screen problems.')
+    tweak_key = 'FogFix'
+    tweak_log_msg = _('Cells With Fog Tweaked To 0.0001: %(total_changed)d')
+    # Probably not needed on newer games, so default-enable only on TES4
+    default_enabled = bush.game.fsName == 'Oblivion'
 
     def wants_record(self, record):
         # All of these floats must be approximately equal to 0
-        for fog_attr in (u'fogNear', u'fogFar', u'fogClip'):
+        for fog_attr in ('fogNear', 'fogFar', 'fogClip'):
             fog_val = getattr(record, fog_attr)
-            if fog_val is not None and fog_val != 0.0: # type: bolt.Rounder
+            # Skip records missing XCLL, WB won't write it if it is
+            if fog_val is None or fog_val != 0.0: # type: bolt.Rounder
                 return False
         return not record.should_skip()
 