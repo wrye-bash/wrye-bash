wrye-bash/wrye-bash

commit f946908162f153a295854fb3078f446684f2f295

Some more caching in RUI:
Broadcast kwargs - in particular I wanted to encapsulate merged/imported
caches. This is also #190 related - see the comments in
InsertListCtrlItem.

Nit

Mopy/bash/basher:
__init__.py: those are already sets, inlining should not matter
*_links: the active tuple is never empty

Mopy/bash/basher/__init__.py
@@ -293,6 +293,9 @@ class _ModsUIList(UIList):
         if bush.game.master_flag:
             self._extra_sortings.insert(0, _ModsUIList._sort_masters_first)
 
+    def _cache_rui_structs(self):
+        return {'act_dicts': bosh.modInfos.active_statuses()}
+
     def _sort_masters_first(self, items):
         """Conditional sort, performs the actual 'masters-first' sorting if
         needed."""
@@ -301,18 +304,18 @@ class _ModsUIList(UIList):
 
     def _activeModsFirst(self, items):
         if self.selectedFirst:
-            set_active = set(load_order.cached_active_tuple())
-            set_merged = set(bosh.modInfos.merged)
-            set_imported = set(bosh.modInfos.imported)
+            act_dicts = bosh.modInfos.active_statuses()
             def _sel_sort_key(x):
                 # First active, then merged, then imported, then inactive
-                x = self._item_name(x)
-                if x in set_active: return 0
-                elif x in set_merged: return 1
-                elif x in set_imported: return 2
-                else: return 3
+                return self._active_keys(self._item_name(x), act_dicts, 3)
             items.sort(key=_sel_sort_key)
     _extra_sortings = [_activeModsFirst]
+
+    def _active_keys(self, item_key, act_dicts, unactive_val):
+        for k, v in act_dicts.items():
+            if item_key in v:
+                return k
+        return unactive_val
 
     @property
     def masters_first(self):
@@ -342,9 +345,9 @@ class _ModsUIList(UIList):
     def _item_name(self, x): # hack to centralize some nasty modInfos accesses
         return x
 
-    def set_item_format(self, item_key, item_format, target_ini_setts):
+    def set_item_format(self, item_key, item_format, **ui_kwargs):
         self.mouseTexts[item_key] = mouseText = []
-        minf = super().set_item_format(item_key, item_format, target_ini_setts)
+        minf = super().set_item_format(item_key, item_format, **ui_kwargs)
         if minf.hasActiveTimeConflict():
             item_format.back_key = 'mods.bkgd.doubleTime.load'
             mouseText.append(_('Another plugin has the same timestamp.'))
@@ -364,11 +367,10 @@ class _ModsUIList(UIList):
         self.mouseTexts[item_key] = ' '.join(mouseText)
         return minf
 
-    def _set_icon_text(self, minf, item_format, item_name, *, _mouse_text,
-                       **kwargs): # we get item_name not item_key so we need _mouse_text
-        checkMark = (load_order.cached_is_active(item_name) # 1
-                     or (item_name in bosh.modInfos.merged and 2) or (
-                             item_name in bosh.modInfos.imported and 3)) # or 0
+    def _set_icon_text(self, minf, item_format, item_name, *, act_dicts,
+                       # we get item_name not item_key so we need _mouse_text
+                       _mouse_text, **kwargs):
+        checkMark = self._active_keys(item_name, act_dicts, -1) + 1
         status = super()._set_icon_text(minf, item_format, item_name, **kwargs)
         #--Font color
         # Text foreground - prioritize BP color, then mergeable/NoMerge color
@@ -453,9 +455,8 @@ class MasterList(_ModsUIList):
         self._allowEditKey = keyPrefix + u'.allowEdit'
         self.is_inaccurate = False # Mirrors SaveInfo.has_inaccurate_masters
         #--Parent init
-        super(MasterList, self).__init__(parent,
-                      listData=listData if listData is not None else {},
-                      keyPrefix=keyPrefix, panel=panel)
+        super().__init__(parent, listData={} if listData is None else listData,
+                         keyPrefix=keyPrefix, panel=panel)
 
     @property
     def allowEdit(self): return bass.settings.get(self._allowEditKey, False)
@@ -516,8 +517,8 @@ class MasterList(_ModsUIList):
                     ma_name in sc_masters})
         self._reList()
 
-    def set_item_format(self, item_key, item_format, target_ini_setts):
-        minf = super().set_item_format(item_key, item_format, target_ini_setts)
+    def set_item_format(self, item_key, item_format, **ui_kwargs):
+        minf = super().set_item_format(item_key, item_format, **ui_kwargs)
         if self.allowEdit:
             if minf.old_name in settings['bash.mods.renames']:
                 item_format.bold = True
@@ -534,7 +535,7 @@ class MasterList(_ModsUIList):
                                'activated.'))
         status, checkMark = super()._set_icon_text(masterInfo, item_format,
             item_name, loadOrderIndex=self._curr_lo_index[item_name],
-            mi=mi, _mouse_text=mouseText)
+            mi=mi, _mouse_text=mouseText, **kwargs)
         on_display = self.detailsPanel.detail_fn
         if status == 30: # master is missing
             mouseText.append(_('Missing master of %(child_plugin_name)s.') % {
@@ -654,7 +655,9 @@ class INIList(UIList):
     labels = {'File': lambda self, p: p,
         'Installer': _ask_info('get_table_prop', ('installer', '')),
     }
-    _target_ini = True # pass the target_ini settings on PopulateItem
+
+    def _cache_rui_structs(self):
+        return {'target_ini_settings': self.data_store.ini.get_ci_settings()}
 
     @property
     def current_ini_name(self): return self.panel.detailsPanel.ini_name

Mopy/bash/bosh/__init__.py
@@ -779,7 +779,7 @@ class ModInfo(_WithMastersInfo):
     def isBP(self):
         return self.header.author == u'BASHED PATCH'
 
-    def txt_status(self):
+    def txt_status(self): ##:(701) we should cache the modInfo status (checkMark)
         fnkey = self.fn_key
         if load_order.cached_is_active(fnkey): return _(u'Active')
         elif fnkey in modInfos.merged: return _(u'Merged')
@@ -2331,12 +2331,11 @@ class ModInfos(TableFileInfos):
             return self.plugin_inis # empty FNDict
         # First, check the Data folder for INIs present in it. Order does not
         # matter, we will only use this to look up existence
-        lower_data_cont = (f.lower() for f in os.listdir(data_folder_path))
-        present_inis = {i for i in lower_data_cont if i.endswith('.ini')}
+        present_inis = {i for i in os.listdir(data_folder_path) if
+                        i.lower().endswith('.ini')}
         # Determine which INIs are active based on LO. Order now matters
-        possible_inis = [self[m].get_ini_name() for m in
-                         load_order.cached_active_tuple()]
-        active_inis = [i for i in possible_inis if i.lower() in present_inis]
+        active_inis = [i for m in load_order.cached_active_tuple() if
+                       (i := self[m].get_ini_name()).lower() in present_inis]
         # Add new or modified INIs to the cache and copy the final order
         inis_active = []
         # check present inis for updates
@@ -2381,7 +2380,7 @@ class ModInfos(TableFileInfos):
         it merges or imports.
 
         :param patches: A set of mods to look for bashed patches in."""
-        merged_,imported_ = set(),set()
+        merged_, imported_ = set(), set()
         for patch in patches & self.bashed_patches: # this must be up to date!
             patchConfigs = self[patch].get_table_prop('bash.patch.configs')
             if not patchConfigs: continue
@@ -2394,7 +2393,12 @@ class ModInfos(TableFileInfos):
             for mod_set, bp_mods in mod_sets:
                 mod_set.update(fn for fn in forward_compat_path_to_fn_list(
                     bp_mods) if fn in self)
-        return merged_,imported_
+        return merged_, imported_
+
+    def active_statuses(self):
+        """Return a dict with keys 0, 1, 2 for active, merged and imported."""
+        return {0: set(load_order.cached_active_tuple()), 1: self.merged,
+                2: self.imported}
 
     # Rest of DataStore overrides ---------------------------------------------
     def rename_operation(self, member_info, newName, store_refr=None):
@@ -2898,18 +2902,17 @@ class ModInfos(TableFileInfos):
             log.setHeader(head + _('Missing Masters for %(mm_plugin)s:') % {
                 'mm_plugin': fileInfo})
             for mod in missing:
-                log(bul + f'xx {mod}')
+                log(f'{bul}xx {mod}')
             log.setHeader(head + _('Masters for %(m_plugin)s:') % {
                 'm_plugin': fileInfo})
             present = {x for x in masters_set if x in self}
             if fileInfo.fn_key in self: #--In case is bashed patch (cf getSemiActive)
                 present.add(fileInfo.fn_key)
             merged, imported = self.getSemiActive(present)
+            all_mods = (masters_set | merged | imported) & set(self)
         else:
             log.setHeader(head + _(u'Active Plugins:'))
-            masters_set = set(load_order.cached_active_tuple())
-            merged, imported = self.merged, self.imported
-        all_mods = (masters_set | merged | imported) & set(self)
+            all_mods = {*chain.from_iterable(self.active_statuses().values())}
         all_mods = load_order.get_ordered(all_mods)
         #--List
         modIndex = 0

Mopy/bash/balt.py
@@ -592,7 +592,6 @@ class UIList(PanelWin):
     #--DnD
     _dndFiles = _dndList = False
     _dndColumns = ()
-    _target_ini = False # pass the target_ini settings on PopulateItem
     _copy_paths = False # enable the Ctrl+C shortcut
 
     def __init__(self, parent, keyPrefix, listData=None, panel=None):
@@ -699,7 +698,7 @@ class UIList(PanelWin):
     def item_count(self): return self.__gList.lc_item_count()
 
     #--Items ----------------------------------------------
-    def PopulateItem(self, itemDex=-1, item=None, target_ini_setts=None):
+    def PopulateItem(self, itemDex=-1, item=None, **ui_kwargs):
         """Populate ListCtrl for specified item. Either item or itemDex must be
         specified.
 
@@ -726,12 +725,12 @@ class UIList(PanelWin):
             # first SetItem call - see InsertListCtrlItem
             self.__gList.InsertListCtrlItem(
                 itemDex, str_label, item,
-                decorate_cb=partial(self.__setUI, item, target_ini_setts))
+                decorate_cb=partial(self.__setUI, fileName=item, **ui_kwargs))
         else:
             # The item is already in the UIList, so we only need to redecorate
             # and set text for all labels
             gItem = self.__gList.get_item_data(itemDex)
-            self.__setUI(item, target_ini_setts, gItem)
+            self.__setUI(item, gItem, **ui_kwargs)
             # Piggyback off the SetItem call we need for __setUI to also set
             # the first column's text
             gItem.SetText(str_label)
@@ -747,26 +746,25 @@ class UIList(PanelWin):
         with self.pause_drawing():
             self.mouseTexts.clear()
             items = set(self.data_store)
-            if self.__class__._target_ini:
-                # hack for avoiding the syscall in get_ci_settings
-                t_setts = self.data_store.ini.get_ci_settings()
-            else:
-                t_setts = None
+            ui_kwargs = self._cache_rui_structs()
             #--Update existing items.
             index = 0
             while index < self.item_count:
                 item = self.GetItem(index)
                 if item not in items: self.__gList.RemoveItemAt(index)
                 else:
-                    self.PopulateItem(itemDex=index, target_ini_setts=t_setts)
+                    self.PopulateItem(itemDex=index, **ui_kwargs)
                     items.remove(item)
                     index += 1
             #--Add remaining new items
             for item in items:
-                self.PopulateItem(item=item, target_ini_setts=t_setts)
+                self.PopulateItem(item=item, **ui_kwargs)
             #--Sort
             self.SortItems()
             self.autosizeColumns()
+
+    def _cache_rui_structs(self):
+        return {}
 
     _same_item = object()
     @final
@@ -782,13 +780,14 @@ class UIList(PanelWin):
         if rdata is None:
             self.populate_items()
         else: # a RefrData instance
+            ui_kwargs = self._cache_rui_structs()
             # Make sure to freeze/thaw, all the InsertListCtrlItem calls make
             # the GUI lag
             with self.pause_drawing():
                 for d in rdata.to_del:
                     self.__gList.RemoveItemAt(self._get_uil_index(d))
                 for upd in rdata.redraw | rdata.to_add:
-                    self.PopulateItem(item=upd)
+                    self.PopulateItem(item=upd, **ui_kwargs)
                 #--Sort
                 self.SortItems()
                 self.autosizeColumns()
@@ -852,15 +851,14 @@ class UIList(PanelWin):
             'installers.text.complex',
         ])}
 
-    def set_item_format(self, item, item_format, target_ini_setts):
+    def set_item_format(self, item, item_format, **ui_kwargs):
         """Populate item_format attributes for text and background colors
         and set icon, font and mouse text. Responsible (applicable if the
         data_store is a FileInfo subclass) for calling info_status to update
         respective info's status."""
+        inf = self.data_store[item]
         try:
-            inf = self.data_store[item]
-            icon_key = self._set_icon_text(inf, item_format, item,
-                target_ini_settings=target_ini_setts)
+            icon_key = self._set_icon_text(inf, item_format, item, **ui_kwargs)
             item_format.icon_dex = self.icons.img_dex(*icon_key)
         except NotImplementedError:
             return # screens, bsas
@@ -871,10 +869,10 @@ class UIList(PanelWin):
         icon key tuple - populate mouse text, item_format attrs, etc."""
         return inf.info_status(**kwargs)
 
-    def __setUI(self, fileName, target_ini_setts, gItem):
+    def __setUI(self, fileName, gItem, **ui_kwargs):
         """Set font, status icon, background text etc."""
         df = _ListItemFormat(self)
-        self.set_item_format(fileName, df, target_ini_setts)
+        self.set_item_format(fileName, df, **ui_kwargs)
         if (icon_index := df.icon_dex) is not None:
             gItem.SetImage(icon_index)
         gItem.SetTextColour(self.lookup_text_key(df.text_key).to_rgba_tuple())
@@ -898,10 +896,11 @@ class UIList(PanelWin):
         else:
             return self._defaultTextBackground
 
-    def decorate_tree_dict(self, tree_dict: dict[FName, list[FName]],
-                           target_ini_setts=None) -> DecoratedTreeDict:
+    def decorate_tree_dict(self, tree_dict: dict[FName, list[FName]]
+                           ) -> DecoratedTreeDict:
         """Add appropriate TreeNodeFormat instances to the specified dict
         mapping items in this UIList to lists of items in this UIList."""
+        ui_kwargs = self._cache_rui_structs()
         def _decorate(i):
             lif = _ListItemFormat(self)
             # Only run set_item_format when the item is actually present,
@@ -909,7 +908,7 @@ class UIList(PanelWin):
             # those since the default text/background colors may have been
             # changed from the OS default)
             if i in self.data_store:
-                self.set_item_format(i, lif, target_ini_setts)
+                self.set_item_format(i, lif, **ui_kwargs)
             return lif.to_tree_node_format()
         return {i: (_decorate(i), [(c, _decorate(c)) for c in i_children])
                 for i, i_children in tree_dict.items()}

Mopy/bash/basher/mod_links.py
@@ -1019,7 +1019,7 @@ class Mod_RebuildPatch(_Mod_BP_Link):
         # Clean up some memory
         bolt.GPathPurge()
         # We need active mods
-        if not load_order.cached_active_tuple():
+        if len(load_order.cached_active_tuple()) == 1: # game esm always active
             self._showWarning(
                 _('That which does not exist cannot be patched.') + '\n' +
                 _('Load some plugins and try again.'),

Mopy/bash/basher/mods_links.py
@@ -153,13 +153,10 @@ class _Mods_EditActivePluginsLists(_AMods_ActivePluginsContext):
         balt.ListEditor.display_dialog(self.window, _('Active Plugins Lists'),
             ap_editor_data)
 
-class _Mods_SaveActivePluginsList(EnabledLink, _AMods_ActivePluginsContext):
+class _Mods_SaveActivePluginsList(_AMods_ActivePluginsContext):
     _text = _('Save Active Plugins List…')
     _help = _('Save the currently active plugins to a new active '
               'plugins list.')
-
-    def _enable(self):
-        return bool(load_order.cached_active_tuple())
 
     def Execute(self):
         new_actives_name = self._askText(_('Save currently active plugins '

Mopy/bash/basher/saves_links.py
@@ -906,12 +906,10 @@ class Save_Unbloat(OneItemLink):
         self.refresh_sel()
 
 #------------------------------------------------------------------------------
-class Save_UpdateNPCLevels(EnabledLink):
+class Save_UpdateNPCLevels(ItemLink):
     """Update NPC levels from active mods."""
     _text = _('Update NPC Levels…')
     _help = _(u'Update NPC levels from active mods')
-
-    def _enable(self): return bool(load_order.cached_active_tuple())
 
     def Execute(self):
         msg = _('This will relevel the NPCs in the selected saves according '

commit 56a6376e9d01439b8aeeb5719987071cd4146b79

Very WIP caching mod active status - fix for MasterList edits:
`active_statuses` should remain fixed for a refresh cycle - note that we
should issue a refresh if we mess with load_order/BP that could change
these statuses - finalized on next commit.

basher/__init__.py: regression from f946908162f153a295854fb3078f446684f2f295
(probably) - act_dicts is a required argument

SaveInfo.info_status returns a tuple:

This drops the List override transferring the responsibility to the info

Mopy/bash/basher/__init__.py
@@ -78,8 +78,8 @@ from ..bolt import FName, GPath, LooseVersion, RefrIn, RefrData, SubProgress, \
     attrgetter_cache, deprint, dict_sort, fast_cached_property, \
     forward_compat_path_to_fn, round_size, str_to_sig, to_unix_newlines, \
     to_win_newlines, top_level_files
-from ..bosh import DataStore, ModInfo, active_keys, omods, read_dir_tags, \
-    read_loot_tags, save_tags_to_dir
+from ..bosh import DataStore, ModInfo, omods, read_dir_tags, read_loot_tags, \
+    save_tags_to_dir
 from ..exception import BoltError, CancelError, SkipError, UnknownListener
 from ..gui import CENTER, BusyCursor, Button, CancelButton, CenteredSplash, \
     CheckListBox, Color, CopyOrMovePopup, DateAndTimeDialog, DropDown, \
@@ -91,6 +91,7 @@ from ..gui import CENTER, BusyCursor, Button, CancelButton, CenteredSplash, \
     showOk, BmpFromStream, init_image_resources, get_image, \
     get_installer_color_checks, get_image_dir, copy_text_to_clipboard
 from ..localize import format_date
+from ..plugin_types import active_keys, ST_MERGED
 from ..update_checker import LatestVersion, UCThread
 
 #  - Make sure that python root directory is in PATH, so can access dll's.
@@ -290,7 +291,7 @@ class _ModsUIList(UIList):
             self._extra_sortings.insert(0, _ModsUIList._sort_masters_first)
 
     def _cache_rui_structs(self):
-        return {'act_dicts': bosh.modInfos.active_statuses()}
+        return {'act_dicts': bosh.modInfos.active_statuses}
 
     def _sort_masters_first(self, items):
         """Conditional sort, performs the actual 'masters-first' sorting if
@@ -301,7 +302,7 @@ class _ModsUIList(UIList):
 
     def _activeModsFirst(self, items):
         if self.selectedFirst:
-            act_dicts = bosh.modInfos.active_statuses()
+            act_dicts = bosh.modInfos.active_statuses
             def _sel_sort_key(x):
                 # First active, then merged, then imported, then inactive
                 return active_keys(self._item_name(x), act_dicts, 3)
@@ -362,7 +363,7 @@ class _ModsUIList(UIList):
     def _set_icon_text(self, minf, item_format, item_name, *, act_dicts,
                        # we get item_name not item_key so we need _mouse_text
                        _mouse_text, **kwargs):
-        checkMark = active_keys(item_name, act_dicts) + 1
+        checkMark = active_keys(item_name, act_dicts)
         status = super()._set_icon_text(minf, item_format, item_name, **kwargs)
         #--Font color
         # Text foreground - prioritize BP color, then mergeable/NoMerge color
@@ -370,7 +371,7 @@ class _ModsUIList(UIList):
             item_format.text_key = 'mods.text.bashedPatch'
             _mouse_text.append(_('Bashed Patch.'))
         for mchk in bush.game.mergeability_checks:
-            txtkey, mtext = mchk.display_info(minf, checkMark)
+            txtkey, mtext = mchk.display_info(minf, checkMark == ST_MERGED)
             if txtkey:
                 item_format.text_key = txtkey
                 _mouse_text.append(mtext)
@@ -384,7 +385,7 @@ class _ModsUIList(UIList):
             pass
         if 'Deactivate' in minf.getBashTags(): # was for mods only
             item_format.italics = True
-        return status, checkMark
+        return status, checkMark + 1 # duh - the chekboxes key
 
 #------------------------------------------------------------------------------
 class MasterList(_ModsUIList):
@@ -600,7 +601,7 @@ class MasterList(_ModsUIList):
             bass.settings[u'bash.mods.renames'][
                 masterInfo.old_name] = masterInfo.curr_name
             # populate, refresh must be called last
-            self.PopulateItem(evt_index)
+            self.PopulateItem(evt_index, **self._cache_rui_structs())
             return EventResult.FINISH ##: needed?
         elif evt_label == u'':
             return EventResult.CANCEL
@@ -1945,11 +1946,6 @@ class SaveList(UIList):
         'Cell': _ask_info('header.pcLocation'),
     }
 
-    #--Populate Item
-    def _set_icon_text(self, inf, *args, **kwargs):
-        status = super()._set_icon_text(inf, *args, **kwargs)
-        return status, inf.is_save_enabled()
-
     # Events ------------------------------------------------------------------
     @balt.conversation
     def _handle_left_down(self, wrapped_evt, lb_dex_and_flags):
@@ -1970,8 +1966,7 @@ class SaveList(UIList):
             u'save_ext_on': enabled_ext, u'save_ext_off': disabled_ext}
         if not balt.askContinue(self, msg, u'bash.saves.askDisable.continue'):
             return
-        do_enable = not sinf.is_save_enabled()
-        extension = enabled_ext if do_enable else disabled_ext
+        extension = disabled_ext if sinf.is_save_enabled() else enabled_ext
         self.try_rename([[sinf, fn_item.fn_body]], item_edited=fn_item,
                         forced_ext=extension)
 
@@ -3625,7 +3620,7 @@ class BashFrame(WindowFrame):
         if refr_saves and ui_refreshes.get(bosh.modInfos):
             to_redraw = set()
             for fn, save in bosh.saveInfos.items():
-                old, new = save.master_st, save.info_status(recalc_st=True)
+                old, new = save.master_st, save.info_status(recalc_st=True)[0]
                 if old != new: # save master status changed, redraw
                     to_redraw.add(fn)
             if rdict := ui_refreshes.get(bosh.saveInfos):

Mopy/bash/basher/belt.py
@@ -30,7 +30,8 @@ from .. import ScriptParser, bass, bolt, bush, load_order
 from ..ScriptParser import error, PreParser
 from ..balt import ItemLink
 from ..bolt import FName, FNDict, LooseVersion
-from ..bosh import ST_ACTIVE, ST_MERGED, ST_IMPORTED, ST_INACTIVE, active_keys
+from ..plugin_types import ST_ACTIVE, ST_MERGED, ST_IMPORTED, ST_INACTIVE, \
+    active_keys
 from ..env import get_file_version, to_os_path
 from ..gui import CENTER, RIGHT, CheckBox, CheckListBox, GridLayout, \
     HBoxedLayout, HLayout, HyperlinkLabel, Label, LayoutOptions, Links, \
@@ -499,7 +500,7 @@ class WryeParser(PreParser):
         # used in self.fn_get_plugin_status which uses different constants!
         int_map = {ST_ACTIVE: 2, ST_MERGED: 3, ST_IMPORTED: 1, ST_INACTIVE: 0}
         self._act_dicts = {int_map[k]: v for k, v in
-                           mod_infos.active_statuses().items()}
+                           mod_infos.active_statuses.items()}
         # file exists but not active/merged/imported - dicts are ordered
         self._act_dicts[int_map[ST_INACTIVE]] = mod_infos
 

Mopy/bash/bosh/__init__.py
@@ -57,7 +57,8 @@ from ..ini_files import AIniInfo, GameIni, IniFileInfo, OBSEIniFile, \
     get_ini_type_and_encoding, supported_ini_exts
 from ..load_order import LordDiff, LoadOrder
 from ..mod_files import ModFile, ModHeaderReader
-from ..plugin_types import MergeabilityCheck, PluginFlag
+from ..plugin_types import MergeabilityCheck, PluginFlag, ST_ACTIVE, \
+    ST_MERGED, ST_IMPORTED
 from ..wbtemp import TempFile
 
 # Singletons, Constants -------------------------------------------------------
@@ -1392,6 +1393,9 @@ class SaveInfo(_WithMastersInfo):
         # That means the LO has new plugins, but not at the end -> green
         return 0
 
+    def info_status(self, *args, **kwargs):
+        return super().info_status(*args, **kwargs), self.is_save_enabled()
+
     def is_save_enabled(self):
         """True if I am enabled."""
         return self.fn_key.fn_ext == bush.game.Ess.ext
@@ -2266,16 +2270,6 @@ def _lo_op(lop_func):
     return _lo_wip_wrapper
 
 #------------------------------------------------------------------------------
-# active status magic numbers
-ST_ACTIVE, ST_MERGED, ST_IMPORTED, ST_INACTIVE = *range(3), -1
-
-def active_keys(item_key, act_dicts, unactive_val=ST_INACTIVE):
-    """Return the key in act_dicts whose value contains item_key."""
-    for k, v in act_dicts.items():
-        if item_key in v:
-            return k
-    return unactive_val
-
 class ModInfos(TableFileInfos):
     """Collection of modinfos. Represents mods in the Data directory."""
     _dir_key = 'mods'
@@ -2477,8 +2471,11 @@ class ModInfos(TableFileInfos):
             self.activeBad ^ old_ab, self.bad_names ^ old_bad)}
         to_redraw = set()
         # reset and cache master status for (all) mod infos (more granular?)
+        self.active_statuses = {ST_ACTIVE: {*load_order.cached_active_tuple()},
+            ST_MERGED: self.merged, ST_IMPORTED: self.imported}
         for fn, plug in self.items(): # we could use dependents here?
-            old, new = plug.master_st, plug.info_status(recalc_st=True)
+            old, new = plug.master_st, plug.info_status(recalc_st=True,
+                act_dicts=self.active_statuses)
             if old != new or fn in chain_ch: # we need to redraw
                 to_redraw.add(fn)
         return to_redraw
@@ -2607,11 +2604,6 @@ class ModInfos(TableFileInfos):
                 mod_set.update(fn for fn in forward_compat_path_to_fn_list(
                     bp_mods) if fn in self)
         return merged_, imported_
-
-    def active_statuses(self):
-        """Return a dict with keys 0, 1, 2 for active, merged and imported."""
-        return {ST_ACTIVE: set(load_order.cached_active_tuple()),
-                ST_MERGED: self.merged, ST_IMPORTED: self.imported}
 
     # Rest of DataStore overrides ---------------------------------------------
     def rename_operation(self, info_new_name, dest_dir=None, **kwargs):
@@ -3122,7 +3114,7 @@ class ModInfos(TableFileInfos):
             all_mods = (masters_set | merged | imported) & set(self)
         else:
             log.setHeader(head + _(u'Active Plugins:'))
-            statuses = self.active_statuses()
+            statuses = self.active_statuses
             all_mods = {*chain.from_iterable(statuses.values())}
             masters_set, merged = statuses[ST_ACTIVE], statuses[ST_MERGED]
         all_mods = load_order.get_ordered(all_mods)

Mopy/bash/plugin_types.py
@@ -37,6 +37,16 @@ from typing import final
 from .bolt import sig_to_str
 from .exception import ModError
 
+# active status magic numbers
+ST_ACTIVE, ST_MERGED, ST_IMPORTED, ST_INACTIVE = *range(3), -1
+
+def active_keys(item_key, act_dicts, unactive_val=ST_INACTIVE):
+    """Return the key in act_dicts whose value contains item_key."""
+    for k, v in act_dicts.items():
+        if item_key in v:
+            return k
+    return unactive_val
+
 # global holding the scale flags mapped to their offsets see _init_plugin_types
 # ESL, MID in this order - avoid using it, it's for caching function parameters
 scale_flags = {}
@@ -180,7 +190,7 @@ class MergeabilityCheck(Enum):
                     _('The following plugins could be %(FLAG)s-flagged.') % n)
         return [p for p in mod_infos.values() if self in p.merge_types()], h, m
 
-    def display_info(self, minf, checkMark):
+    def display_info(self, minf, is_merged):
         """Return a UI settings key and a mouse text for the mod list UI."""
         if self not in minf.merge_types(): return '', ''
         match self:
@@ -188,7 +198,7 @@ class MergeabilityCheck(Enum):
                 if 'NoMerge' in minf.getBashTags():
                     return 'mods.text.noMerge', _('Technically mergeable, '
                                                   'but has NoMerge tag.')
-                if checkMark == 2: # Merged plugins won't be in master lists
+                if is_merged: # Merged plugins won't be in master lists
                     mtext = _('Merged into Bashed Patch.')
                 else:
                     mtext = _('Can be merged into Bashed Patch.')