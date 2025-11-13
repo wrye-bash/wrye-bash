wrye-bash/wrye-bash

commit 4ce6d792048a6988ff4c1932508343476fa2d3f8

Move Store to basher:
After removing the local bosh import from balt I realized that
`.unique_store_key` was really accessed in two places - and that every
time the data store itself was in scope. This meant I don't need Store
anymore. Historically Store was introduced to communicate information
on refresh #353, and to store info on the new tabs - see
46aa64fa7847b1884d372c172d659cbf91b33571. The use in refresh was
a refactoring artifact needed to trace and eventually prune the places
where a refresh_info=True was passed. Since then all these calls were
centralized in propagate refresh and we are able to drop the ubiquitous
Store. A bosh.Store enum whose members are the data store singletons
(possible with Enums?) is what we would really like to have for #600
but that's just cosmetics for now (at least before #190 is done). The
weird `store_key = self` assignments are for minimizing the conflicts
with a future commit as this came up while rebasing.

Mopy/bash/basher/__init__.py
@@ -56,10 +56,11 @@ import sys
 import time
 from collections import OrderedDict, defaultdict, namedtuple
 from collections.abc import Iterable
+from enum import Enum
 from functools import partial
 from itertools import chain, repeat, starmap, count
 
-import wx
+import wx ##:(190) de-wx!
 
 # basher-local imports - maybe work towards dropping (some of) these?
 from .constants import colorInfo, settingDefaults
@@ -73,12 +74,11 @@ from .. import archives, balt, bass, bolt, bosh, bush, env, initialization, \
 from ..balt import AppendableLink, BashStatusBar, CheckLink, ColorChecks, \
     EnabledLink, INIListCtrl, ItemLink, Link, NotebookPanel, Resources, \
     SeparatorLink, UIList, colors
-from ..bass import Store
 from ..bolt import FName, GPath, LooseVersion, RefrIn, RefrData, SubProgress, \
     attrgetter_cache, deprint, dict_sort, fast_cached_property, \
     forward_compat_path_to_fn, round_size, str_to_sig, to_unix_newlines, \
     to_win_newlines, top_level_files
-from ..bosh import ModInfo, omods, active_keys
+from ..bosh import ModInfo, omods, active_keys, DataStore
 from ..bosh.mods_metadata import read_dir_tags, read_loot_tags
 from ..exception import BoltError, CancelError, SkipError, UnknownListener
 from ..gui import CENTER, BusyCursor, Button, CancelButton, CenteredSplash, \
@@ -803,11 +803,12 @@ class INIList(UIList):
             showError(None, msg, title=_('Missing Game INI'))
             return False
         try:
+            ini_infos = bosh.iniInfos
             default_ini.copyTo(target_ini_file.abs_path)
-            if ini_uilist := balt.Link.Frame.all_uilists[Store.INIS]:
+            if ini_uilist := balt.Link.Frame.all_uilists[ini_infos]:
                 ini_uilist.panel.ShowPanel()
             else:
-                bosh.iniInfos.refresh(False)
+                ini_infos.refresh(False)
             return True
         except OSError:
             target_ini_pth = target_ini_file.abs_path
@@ -3337,6 +3338,21 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
                 Link.Frame.notebook.InsertPage(insertAt,panel._native_widget,title)
         bass.settings[u'bash.tabs.order'][self.tabKey] ^= True
 
+# Bash Notebook ---------------------------------------------------------------
+class _BashTab(Enum):
+    """Define Bash tabs order and default enabled state - member values are the
+    tab key in tabInfo and default enabled state, members order is the default
+    tabs order."""
+    INSTALLERS = ('Installers', True)
+    MODS = ('Mods', True)
+    SAVES = ('Saves', True)
+    BSAS = ('BSAs', False)
+    INIS = ('INI Edits', True)
+    SCREENSHOTS = ('Screenshots', True)
+
+    def __repr__(self):
+        return self.name
+
 class BashNotebook(wx.Notebook, balt.TabDragMixin):
 
     def __init__(self, parent):
@@ -3346,7 +3362,7 @@ class BashNotebook(wx.Notebook, balt.TabDragMixin):
         iInstallers = iMods = -1
         self._tab_menu = Links()
         # default tabs order and default enabled state, keys as in tabInfo
-        tabs_enabled_ordered = dict(e.value for e in Store)
+        tabs_enabled_ordered = dict(e.value for e in _BashTab)
         newOrder = settings.get('bash.tabs.order', tabs_enabled_ordered)
         if not isinstance(newOrder, dict): # convert, on updating to 306 ##: still needed
             enabled = settings.get('bash.tabs',  # deprecated - never use
@@ -3464,7 +3480,7 @@ class BashFrame(WindowFrame):
     plugin_checker = None
     # UILists - use sparingly for inter Panel communication - may be None if
     # the tab is not enabled
-    all_uilists: dict[Store, UIList | None] = defaultdict(lambda: None)
+    all_uilists: dict[DataStore, UIList | None] = defaultdict(lambda: None)
     # Panels - use sparingly
     iPanel = None # BAIN panel
     # initial size/position
@@ -3609,13 +3625,13 @@ class BashFrame(WindowFrame):
         # refresh the backend - order matters, bsas must come first for strings
         # inis and screens call refresh in ShowPanel
         ##: maybe we need to refresh inis and *not* refresh saves but on ShowPanel?
+        modinfs = bosh.modInfos
         ui_refresh = {store: rdata for store in (
-            bosh.bsaInfos, bosh.modInfos, bosh.saveInfos) if (
+            bosh.bsaInfos, modinfs, bosh.saveInfos) if (
                 rdata := not booting and store.refresh(True))}
         #--Repopulate, focus will be set in ShowPanel
-        self.all_uilists[Store.MODS].propagate_refresh(ui_refresh.get(
-            bosh.modInfos), ui_refreshes=ui_refresh, focus_list=False,
-            booting=booting)
+        self.all_uilists[modinfs].propagate_refresh(ui_refresh.get(modinfs),
+            ui_refreshes=ui_refresh, focus_list=False, booting=booting)
         #--Show current notebook panel
         if self.iPanel: self.iPanel.frameActivated = True
         self.notebook.currentPage.ShowPanel(refresh_infos=not booting,
@@ -3641,30 +3657,29 @@ class BashFrame(WindowFrame):
     def refresh_and_warn(self, ui_refreshes, booting, refr_saves):
         # ONLY use in propagate_refresh - RUI will be triggered for each key
         # if a RefreshUI is requested for ModList we should also refresh Saves
-        if refr_saves and ui_refreshes.get(Store.MODS):
+        if refr_saves and ui_refreshes.get(bosh.modInfos):
             to_redraw = set()
             for fn, save in bosh.saveInfos.items():
                 old, new = save.master_st, save.info_status(recalc_st=True)
                 if old != new: # save master status changed, redraw
                     to_redraw.add(fn)
-            if rdict := ui_refreshes.get(Store.SAVES):
+            if rdict := ui_refreshes.get(bosh.saveInfos):
                 if rd_saves := rdict.get('rdata'):
                     rd_saves |= RefrData(to_redraw) # else leave it to None
             else:
-                ui_refreshes[Store.SAVES] = {'rdata': RefrData(to_redraw),
-                                             'focus_list': False}
-        for list_key, ref_args in ui_refreshes.items():
-            if (uil := self.all_uilists[list_key]) is not None:
+                ui_refreshes[bosh.saveInfos] = {'rdata': RefrData(to_redraw),
+                                                'focus_list': False}
+        for ds, ref_args in ui_refreshes.items():
+            if (uil := self.all_uilists[ds]) is not None:
                 uil.RefreshUI(**ref_args)
-        stores = {Store.BSAS: bosh.bsaInfos, Store.MODS: bosh.modInfos,
-                  Store.SAVES: bosh.saveInfos} # this belongs to Store somehow
+        stores = [bosh.bsaInfos, bosh.modInfos, bosh.saveInfos]
         if booting: # trigger warnings on boot, ui_refresh is empty then
             ui_refreshes = stores
         else:
-            ui_refreshes = {k: stores[k] for k in ui_refreshes if k in stores}
+            ui_refreshes = [k for k in ui_refreshes if k in stores]
         multi_warns, lo_warns = [], []
-        for list_key, ds in ui_refreshes.items():
-            ds.warning_args(multi_warns, lo_warns, self, list_key)
+        for ds in ui_refreshes:
+            ds.warning_args(multi_warns, lo_warns, self)
         if multi_warns:
             mk = (mwd := MultiWarningDialog).make_highlight_entry
             mwd(self, highlight_items=starmap(mk, multi_warns)).show_modeless()

Mopy/bash/basher/dialogs.py
@@ -27,7 +27,6 @@ from typing import Iterable
 from .. import balt, bass, bolt, bosh, bush, env, exception, load_order, \
     wrye_text
 from ..balt import DecoratedTreeDict, colors, Link
-from ..bass import Store
 from ..bolt import CIstr, FName, GPath_no_norm, text_wrap, top_level_dirs, \
     reverse_dict, RefrData
 from ..bosh import ModInfo, faces
@@ -899,9 +898,9 @@ class _AItemHighlightDialog(MaybeModalDialogWindow):
     @classmethod
     def make_highlight_entry(cls, highlight_desc: str,
             highlighted_items: Iterable[FName] | dict[FName, list[FName]],
-            data_key=Store.MODS):
+            data_key=None):
         """Create a _HighlightData object for highlighting dialogs."""
-        target_uil = Link.Frame.all_uilists[data_key]
+        target_uil = Link.Frame.all_uilists[data_key or bosh.modInfos]
         if target_uil is not None:
             tree_dict = highlighted_items if isinstance(highlighted_items,
                 dict) else {i: [] for i in sorted(highlighted_items)}

commit df2638918bc7dcb94f27b9ebc0b2cb5e09ffbded

Fixup for de/activation:
From @sibir-ine:

I found another regression in 4ce6d79. Deactivating the dependency of an
active plugin or activating the dependent of a deactivated plugin throws
this traceback:

 File "*****\bash\basher\dialogs.py", line 916, in make_highlight_entry
    parent_tab_key=data_key.value[0])
                   ^^^^^^^^^^^^^^
AttributeError: 'NoneType' object has no attribute 'value'

Nit - hunt down modInfos:

parsers: removed _mod_has_tag unused since
a5ca81cbd2c52f689d9aafbda5b7640c8a68741f. More local imports pruned -
modInfos is now used directly in few files - namely bosh and basher but
also save_headers, bain.py and fomod.py - those should be revisited.

Mopy/bash/basher/__init__.py
@@ -3233,7 +3233,7 @@ class TabInfo(Enum):
     INSTALLERS = 'Installers', True, _('Installers')
     MODS = 'Mods', True, _('Mods')
     SAVES = 'Saves', True, _('Saves')
-    # BSAS = 'BSAs', False, _('BSAs')
+    BSAS = 'BSAs', False, None
     INIS = 'INI Edits', True, _('INI Edits')
     SCREENSHOTS = 'Screenshots', True, _('Screenshots')
 
@@ -3244,7 +3244,7 @@ class TabInfo(Enum):
             case 'Installers': cls = InstallersPanel
             case 'Mods': cls = ModPanel
             case 'Saves': cls = SavePanel
-            # case 'BSAs': cls = BSAPanel
+            case 'BSAs': cls = None # BSAPanel to enable the tab
             case 'INI Edits': cls = INIPanel
             case 'Screenshots': cls = ScreensPanel
             case _: cls = None
@@ -3276,7 +3276,7 @@ class _Tab_Link(AppendableLink, CheckLink, EnabledLink):
         self._help = _('Show/Hide the %(tabtitle)s Tab.') % {
             'tabtitle': self._text}
 
-    def _append(self, window): return self._text is not None
+    def _append(self, window): return bool(self._text)
 
     def _enable(self): return self.enabled
 

Mopy/bash/basher/dialogs.py
@@ -900,7 +900,8 @@ class _AItemHighlightDialog(MaybeModalDialogWindow):
             highlighted_items: Iterable[FName] | dict[FName, list[FName]],
             data_key=None):
         """Create a _HighlightData object for highlighting dialogs."""
-        target_uil = Link.Frame.all_uilists[data_key or bosh.modInfos]
+        data_key = data_key or bosh.modInfos
+        target_uil = Link.Frame.all_uilists[data_key]
         if target_uil is not None:
             tree_dict = highlighted_items if isinstance(highlighted_items,
                 dict) else {i: [] for i in sorted(highlighted_items)}
@@ -911,9 +912,11 @@ class _AItemHighlightDialog(MaybeModalDialogWindow):
             warning_dec_items = {i: (None, []) for i in
                 sorted(highlighted_items)}
             uil_images = None
+        ##:(600) HACK - this mapping belongs to a proper Store class
+        hack = {bosh.modInfos: 'Mods', bosh.saveInfos: 'Saves', bosh.bsaInfos: 'BSAs'}
         return _HighlightData(uil_image_list=uil_images,
             highlight_desc=highlight_desc, highlighted_items=warning_dec_items,
-            parent_tab_key=data_key.value[0])
+            parent_tab_key=hack[data_key])
 
 # Note: we sometimes use 'unnecessary' subclasses here for the separate
 # bass.settings['bash.window.sizes'] key provided by the unique class name

Mopy/bash/basher/mod_links.py
@@ -1695,7 +1695,7 @@ class _Mod_Export_Link(_Import_Export_Link, _CsvExport_Link):
                     return
                 readProgress(index, _('Reading %(reading_plugin)s.') % {
                     'reading_plugin': fileName})
-                parser.readFromMod(fileInfo)
+                parser.readFromMod(fileInfo, self._data_store)
             progress(0.8, _('Exporting to %(exporting_file)s.') % {
                 'exporting_file': textPath.stail})
             parser.write_text_file(textPath)
@@ -1734,7 +1734,7 @@ class _Mod_Import_Link(_Import_Export_Link, OneItemLink):
                 parser.read_csv(textPath)
             else:
                 srcInfo = bosh.modInfos.factory(textPath)
-                parser.readFromMod(srcInfo)
+                parser.readFromMod(srcInfo, self._data_store)
             progress(0.2, _('Applying to %(applying_plugin)s.') % {
                 'applying_plugin': self._selected_item})
             changes = parser.writeToMod(self._selected_info)

Mopy/bash/bosh/_saves.py
@@ -665,7 +665,7 @@ class SaveSpells(_SaveData):
         #--spells[(modName,objectIndex)] = (name,type)
         self.allSpells: dict[FormId, (str, int)] = {}
 
-    def load_data(self, progress, modInfos):
+    def load_data(self, progress, modinfos):
         """Load savegame and extract created spells from it and its masters."""
         progress = progress or bolt.Progress()
         self._load_save(SubProgress(progress, 0, 0.4))
@@ -673,8 +673,8 @@ class SaveSpells(_SaveData):
         #--Extract spells from masters
         for index,master in enumerate(self.saveFile._masters):
             progress(index, master)
-            if master in modInfos:
-                self.importMod(modInfos[master])
+            if master in modinfos:
+                self.importMod(modinfos[master])
         #--Extract created spells
         allSpells = self.allSpells
         saveName = self.saveInfo.fn_key

Mopy/bash/bosh/mods_metadata.py
@@ -156,7 +156,7 @@ def diff_tags(plugin_new_tags, plugin_old_tags):
 _cleaning_wiki_url = (u'[[!https://tes5edit.github.io/docs/7-mod-cleaning-and'
                       u'-error-checking.html|Tome of xEdit]]')
 
-def checkMods(progress, modInfos, showModList=False, showCRC=False,
+def checkMods(progress, modinfos, showModList=False, showCRC=False,
               showVersion=True, scan_plugins=True):
     """Checks currently loaded mods for certain errors / warnings."""
     if not bush.game.Esp.canBash:
@@ -167,7 +167,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     full_lo = load_order.cached_lo_tuple()
     plugin_to_acti_index = {p: i for i, p in enumerate(full_acti)}
     all_present_plugins = set(full_lo)
-    all_present_minfs = {x: modInfos[x] for x in full_lo} #ascending load order
+    all_present_minfs = {x: modinfos[x] for x in full_lo} #ascending load order
     all_active_plugins = set(full_acti)
     game_master_name = bush.game.master_file
     vanilla_masters = bush.game.bethDataFiles
@@ -180,7 +180,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
                     u'identify in your currently installed plugins.')
     # -------------------------------------------------------------------------
     # Check for corrupt plugins
-    all_corrupted = modInfos.corrupted
+    all_corrupted = modinfos.corrupted
     # -------------------------------------------------------------------------
     # Check for ESL-flagged plugins that aren't ESL-capable and Overlay-flagged
     # plugins that shouldn't be Overlay-flagged. Also check for conflicts
@@ -188,7 +188,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     pflags = bush.game.plugin_flags
     if flag_errors := {k: {h_msg: set() for h_msg in v} for k, v in
                        pflags.error_msgs.items()}:
-        for m, modinf in modInfos.items():
+        for m, modinf in modinfos.items():
             for pflag in flag_errors:
                 if pflag.cached_type(modinf):
                     pflag.validate_type(modinf, flag_errors[pflag].values())
@@ -199,7 +199,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     should_activate = []
     for plugin_fn, p_minf in all_present_minfs.items():
         p_active = plugin_fn in all_active_plugins
-        p_imported = plugin_fn in modInfos.imported
+        p_imported = plugin_fn in modinfos.imported
         p_tags = p_minf.getBashTags()
         if u'Deactivate' in p_tags and p_active:
             should_deactivate.append(plugin_fn)
@@ -241,11 +241,11 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     valid_vers = bush.game.Esp.validHeaderVersions
     invalid_tes4_versions = {
         p: f'{p_ver}' for p in all_active_plugins
-        if (p_ver := modInfos[p].header.version) not in valid_vers}
+        if (p_ver := modinfos[p].header.version) not in valid_vers}
     # -------------------------------------------------------------------------
     # Check for older form versions, which may point to improperly converted
     # plugins
-    old_fvers = modInfos.older_form_versions
+    old_fvers = modinfos.older_form_versions
     # -------------------------------------------------------------------------
     # Check for cleaning information from LOOT.
     cleaning_messages = {}
@@ -332,7 +332,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
                 add_old_weapon = old_weapon_records[plugin_fn].append
                 add_hitme = all_hitmes[plugin_fn].append
                 add_null_fid = null_formid_records[plugin_fn].append
-                p_masters = (*modInfos[plugin_fn].masterNames, plugin_fn)
+                p_masters = (*modinfos[plugin_fn].masterNames, plugin_fn)
                 p_num_masters = len(p_masters)
                 for r, d in ext_data.items():
                     for r_header, r_eid in d:
@@ -579,7 +579,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         """Logs a single collision with the specified FormID, injected status,
         origin plugin and collision info."""
         # FormIDs must be in long format at this point
-        proper_fid = format_fid(coll_fid, coll_plugin, modInfos)
+        proper_fid = format_fid(coll_fid, coll_plugin, modinfos)
         if coll_inj:
             log('* ' + _('%(injected_formid)s injected into '
                          '%(injection_target)s, colliding versions:') % {
@@ -614,7 +614,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
         log_plugin_messages(all_corrupted) ##: Just _log_plugins?
     for pflag in pflags:
         if pflag.merge_check is not None:
-            minfos_cache, head, msg = pflag.merge_check.cached_types(modInfos)
+            minfos_cache, head, msg = pflag.merge_check.cached_types(modinfos)
             if minfos_cache:
                 _log_plugins(head, msg, [minf.fn_key for minf in minfos_cache])
         for (head, msg), pl_set in flag_errors.get(pflag, {}).items():
@@ -624,9 +624,9 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     # Don't show NoMerge-tagged plugins as mergeable and remove ones that have
     # already been merged into a BP
     if (merge := MergeabilityCheck.MERGE) in bush.game.mergeability_checks:
-        minfos_cache, head, msg = merge.cached_types(modInfos)
+        minfos_cache, head, msg = merge.cached_types(modinfos)
         can_merge = {m for inf in minfos_cache if (m := inf.fn_key) not in
-                     modInfos.merged and 'NoMerge' not in inf.getBashTags()}
+                     modinfos.merged and 'NoMerge' not in inf.getBashTags()}
         if can_merge:
             _log_plugins(head, msg, can_merge)
     if should_deactivate:
@@ -801,7 +801,7 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
             for orig_plugin, dupe_count in duplicates_counter.items():
                 log('* ' + _('%(full_fid)s in %(orig_plugin)s: '
                              'occurs %(num_duplicates)d times') % {
-                    'full_fid': format_fid(orig_fid, orig_plugin, modInfos),
+                    'full_fid': format_fid(orig_fid, orig_plugin, modinfos),
                     'orig_plugin': orig_plugin,
                     'num_duplicates': dupe_count,
                 })
@@ -848,6 +848,6 @@ def checkMods(progress, modInfos, showModList=False, showCRC=False,
     # We already logged missing or delinquent masters up above, so don't
     # duplicate that info in the mod list
     if showModList:
-        log(u'\n' + modInfos.getModList(showCRC, showVersion, wtxt=True,
+        log(u'\n' + modinfos.getModList(showCRC, showVersion, wtxt=True,
                                         log_problems=False).strip())
     return log_header + u'\n\n' + log.out.getvalue()

Mopy/bash/parsers.py
@@ -251,7 +251,7 @@ class _HandleAliases(CsvParser):
     def _key2(self, csv_fields):
         return self._coerce_fid(*self._key2_getter(csv_fields))
 
-    def readFromMod(self, mod_inf):
+    def readFromMod(self, mod_inf, modinfos):
         """Hasty readFromMod implementation."""
         modFile = self._load_plugin(mod_inf)
         for top_grup_sig, typeBlock in modFile.iter_tops(self._parser_sigs):
@@ -316,21 +316,12 @@ class _AParser(_HandleAliases):
         for k in sorted(rows, key=lambda k_: (id_eid_.get(k_) or '').lower()):
             yield k, (rows[k], id_eid_[k])
 
-    # Plugin-related utilities
-    def _mod_has_tag(self, tag_name):
-        """Returns True if the current mod has a Bash Tag with the specified
-        name."""
-        from . import bosh
-        return self._current_mod and tag_name in bosh.modInfos[
-            self._current_mod].getBashTags()
-
     # Reading from plugin - first pass
-    def _read_plugin_fp(self, loaded_mod):
+    def _read_plugin_fp(self, loaded_mod, modinfos):
         """Performs a first pass of reading on the specified plugin and its
         masters. Results are stored in id_context.
 
         :param loaded_mod: The loaded mod to read from."""
-        from . import bosh
         def _fp_loop(mod_to_read):
             """Central loop of _read_plugin_fp, factored out into a method so
             that it can easily be used twice."""
@@ -344,7 +335,7 @@ class _AParser(_HandleAliases):
             master_names = load_order.get_ordered(master_names)
         for mod_name in master_names:
             if mod_name in self._fp_mods: continue
-            _fp_loop(self._load_plugin(bosh.modInfos[mod_name],
+            _fp_loop(self._load_plugin(modinfos[mod_name],
                                        target_types=self._fp_types))
         # Finally, process the mod itself
         if loaded_mod.fileInfo.fn_key in self._fp_mods: return
@@ -399,7 +390,7 @@ class _AParser(_HandleAliases):
         raise NotImplementedError
 
     # Note the non-PEP8 names - those point to refactored pseudo-API methods
-    def readFromMod(self, mod_info):
+    def readFromMod(self, mod_info, modinfos):
         """Asks this parser to read information from the specified ModInfo
         instance. Executes the needed passes and stores extracted information
         in id_context and / or id_stored_data. Note that this does not
@@ -416,7 +407,7 @@ class _AParser(_HandleAliases):
         # Load mod_info once and for all, then execute every needed pass
         loaded_mod = self._load_plugin(mod_info, target_types=a_types)
         if self._fp_types:
-            self._read_plugin_fp(loaded_mod)
+            self._read_plugin_fp(loaded_mod, modinfos)
         if self._sp_types:
             self._read_plugin_sp(loaded_mod)
         # We need to unset _current_mod since we're no longer loading a mod
@@ -545,15 +536,13 @@ class ActorLevels(_HandleAliases):
         self.gotLevels = set()
         self._skip_mods = {'none', bush.game.master_file.lower()}
 
-    def readFromMod(self, mod_inf):
+    def readFromMod(self, mod_inf, modinfos):
         """Imports actor level data from the specified mod and its masters."""
-        from . import bosh
         mod_id_levels, gotLevels = self.id_stored_data, self.gotLevels
         load_f = self._load_factory(keepAll=False)
         for modName in (*mod_inf.masterNames, mod_inf.fn_key):
             if modName in gotLevels: continue
-            modFile = self._load_plugin(bosh.modInfos[modName],
-                                        load_fact=load_f)
+            modFile = self._load_plugin(modinfos[modName], load_fact=load_f)
             for rfid, record in modFile.tops[b'NPC_'].iter_present_records():
                 items = zip(
                     ('eid', 'npc_flags.pc_level_offset', 'level_offset',