wrye-bash/wrye-bash

commit 8163746dd4eda18bda81541da2abef91a78b2caf

Refresh fixups:
Mopy/bash/basher/files_links.py: seems we need to use rdata here as
load order of files above the ones added might change for instance.
That would also take care of the scenario where duplicated file is named
after a missing master to a higher loading plugin missing that master,
for a lower loading (going from missing -> delinquent) we would need
a reset status mechanism *for ModInfo also*.

Mopy/bash/basher/saves_links.py:

Traceback (most recent call last):
  File "C:\...\wrye-bash\Mopy\bash\balt.py", line 1720, in __Execute
    self.Execute()
  File "C:\...\wrye-bash\Mopy\bash\basher\saves_links.py", line 652, in Execute
    def Execute(self): _self.MoveFiles(profile=self._text)
                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "C:\...\wrye-bash\Mopy\bash\basher\saves_links.py", line 670, in MoveFiles
    self.window.RefreshUI(RefrData(to_del=moved))
  File "C:\...\wrye-bash\Mopy\bash\balt.py", line 789, in RefreshUI
    self.__gList.RemoveItemAt(self._get_uil_index(d))
                              ^^^^^^^^^^^^^^^^^^^^^^
  File "C:\...\wrye-bash\Mopy\bash\balt.py", line 1325, in _get_uil_index
    return self.__gList.FindIndexOf(item)
           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "C:\...\wrye-bash\Mopy\bash\gui\list_ctrl.py", line 279, in FindIndexOf
    return self._native_widget.FindItem(-1, self._item_itemId[item])
                                            ~~~~~~~~~~~~~~~~~^^^^^^
KeyError: SaveInfo<POST_MASTER Copy699966 Copy Rename2.ess>

I also renamed check_existence to check_removed (duh)

Mopy/bash/basher/__init__.py: Chop off a propagate_refresh(True). Note
unlock_lo == bush.game.using_txt_file here. More on DoSave in next
commits.

balt: MODS key could appear with an empty RefrData - oops.

Under #701

Mopy/bash/bosh/__init__.py
@@ -1511,7 +1511,7 @@ class DataStore(DataDict):
         try:
             self._delete_operation(finfos, recycle)
         finally: # markers are popped from finfos - we refreshed in _delete_op
-            if finfos := self.check_existence(finfos):
+            if finfos := self.check_removed(finfos):
                 # ok to suppose the only lo modification is due to deleted
                 # files at this point
                 self.refresh(RefrIn(del_infos=finfos), what='I',
@@ -1522,7 +1522,7 @@ class DataStore(DataDict):
                 *chain.from_iterable(inf.delete_paths() for inf in finfos)]:
             env.shellDelete(abs_del_paths, recycle=recycle)
 
-    def check_existence(self, infos):
+    def check_removed(self, infos):
         """Lift your skirts, we are entering the realm of #241."""
         return {inf for inf in infos if not inf.abs_path.exists()}
 
@@ -1910,11 +1910,11 @@ class INIInfos(TableFileInfos):
             for ini_info in self.values(): ini_info.reset_status()
         return rdata
 
-    def check_existence(self, infos):
+    def check_removed(self, infos):
         regular_tweaks = []
         def_tweaks = {inf for inf in infos if inf.fn_key in
                       self._default_tweaks or regular_tweaks.append(inf)}
-        return {*def_tweaks, *super().check_existence(regular_tweaks)}
+        return {*def_tweaks, *super().check_removed(regular_tweaks)}
 
     def filter_essential(self, fn_items: Iterable[FName]):
         # Can't remove default tweaks

Mopy/bash/basher/__init__.py
@@ -1594,9 +1594,11 @@ class ModDetails(_ModsSavesDetails):
         #--Only change date?
         if changeDate and not (changeName or changeHedr or changeMasters):
             self._set_date(mod_inf)
-            bosh.modInfos.refresh(refresh_infos=False, unlock_lo=unlock_lo)
+            rdata = bosh.modInfos.refresh(refresh_infos=False,
+                                          unlock_lo=unlock_lo)
+            rdata.redraw.add(mod_inf.fn_key) # needed!
             self.panel_uilist.propagate_refresh( # refresh saves if lo changed
-                True, refr_saves=not bush.game.using_txt_file)
+                rdata, refr_saves=unlock_lo)
             return
         #--Change hedr/masters?
         if refr_inf := (changeHedr or changeMasters):

Mopy/bash/balt.py
@@ -821,7 +821,7 @@ class UIList(PanelWin):
         # TODO(353): we need to be more granular here which needs caching
         #  info_status - we need similar logic in _refresh_mod_inis_and_strings
         #  (bsas vs mods) - return dicts[Store, RefrIn] from refresh?
-        if refr_saves and Store.MODS in ui_refreshes:
+        if refr_saves and ui_refreshes.get(Store.MODS):
             ui_refreshes[Store.SAVES] = True
         Link.Frame.refresh_and_warn(ui_refreshes, booting)
 

Mopy/bash/basher/files_links.py
@@ -135,8 +135,9 @@ class File_Duplicate(ItemLink):
         if mod_previous:
             rinf = RefrIn.from_tabled_infos(
                 {k: pairs[v] for k, v in mod_previous.items()})
-            fileInfos.refresh(rinf, insert_after=mod_previous)
-            self.refresh_sel(mod_previous,
+            ##:(701) we should reset-status here for lower loading mods
+            rdata = fileInfos.refresh(rinf, insert_after=mod_previous)
+            self.refresh_sel(rdata.redraw | rdata.to_add,
                              detail_item=next(reversed(mod_previous)))
             self.window.SelectItemsNoCallback(mod_previous)
 

Mopy/bash/basher/saves_links.py
@@ -663,11 +663,10 @@ class Save_Move(ChoiceLink):
         try:
             count = self._move_saves(destDir, profile)
         finally:
-            if not self.copyMode: # files moved to other profile, refresh
-                if moved := bosh.saveInfos.check_existence(
-                        self.iselected_infos()):
-                    bosh.saveInfos.refresh(RefrIn(del_infos=moved))
-                self.window.RefreshUI(RefrData(to_del=moved))
+            if not self.copyMode and (moved := bosh.saveInfos.check_removed(
+                        self.iselected_infos())):
+                rdata = bosh.saveInfos.refresh(RefrIn(del_infos=moved))
+                self.window.RefreshUI(rdata)
         profile_rel = os.path.relpath(destDir, bass.dirs['saveBase'])
         msg = (_('%(num_save_files)d files copied to %(save_profile)s.')
                if self.copyMode else

commit 9a4ef20cf03861a77015b7e32960907ed295258b

Move timestamp conflicts caching to ModInfos:
This is to fix a bug reported by @sibir-ine:

> When two plugins in a timestamp-based LO game have the same timestamp
> & you attempt to resolve it by redating one of them via its Modified
> field, it only updates the duplicate timestamp status of that plugin,
> not both. This regressed in 8163746dd4eda18bda81541da2abef91a78b2caf

The regression was due to passing the rdata instance to
propagate_refresh (rather than True). Note that for a pure redate (that
does not affect load_order) we need to manually add the info to
redraw in ModDetails._extra_changes. That change in info status is only
needed for the UI to detect the conflict, but moving it there raised
several issues (like needing to pass caches into _set_icon_text). The
solution adopted here was to build the cache alongside the other
load order caches, ensuring correctness and simplifying the model.
games_lo no longer builds its own _mtime_mods cache, which it never
used and which added unnecessary complexity (cf the ##: todo comment).
Finally the conflict detection logic is now centralized in lo_cache
rather than split across load_order/games_lo. Rest is some nit.

Under #353, #578

Mopy/bash/bosh/__init__.py
@@ -904,11 +904,11 @@ class ModInfo(_WithMastersInfo):
 
     def hasTimeConflict(self):
         """True if there is another mod with the same ftime."""
-        return load_order.has_load_order_conflict(self.fn_key)
+        return self.fn_key in self._store().lo_conflicts
 
     def hasActiveTimeConflict(self):
         """True if it has an active mtime conflict with another mod."""
-        return load_order.has_load_order_conflict_active(self.fn_key)
+        return self.fn_key in self._store().act_lo_conflicts
 
     def hasBadMasterNames(self): # used in status calculation
         """True if has a master with un unencodable name in cp1252."""
@@ -2240,10 +2240,33 @@ def _lo_cache(lord_func):
                         self[k].get_table_prop('allowGhosting', True)})
                 ldiff.affected.update(mod for mod, ghost_it in ghostify.items()
                                       if self[mod].setGhost(ghost_it))
+            # check for load order conflicts - if ldiff is empty we should keep
+            # it empty (for refresh to check if it needs the refreshes above),
+            # but we should notify the UI to redraw items that changed status
+            mt_conflicts_changes = set()
+            if bush.game.mtime_lo:
+                mtime_mods = defaultdict(set)
+                for mod, info in self.items():
+                    mtime_mods[int(info.ftime)].add(mod)
+                mtime_mods = {frozenset(v) for v in mtime_mods.values() if
+                              len(v) > 1} # keep conflicting sets of mods
+                lo_conflicts, act_lo_conflicts = set(), set()
+                if mtime_mods:
+                    activ = {*load_order.cached_active_tuple()}
+                    for confls in mtime_mods:
+                        lo_conflicts |= confls
+                        if len(confls_act := confls & activ) > 1:
+                            # active mods conflicting with other active mods
+                            act_lo_conflicts |= confls_act
+                # mods that started/stopped conflicting
+                mt_conflicts_changes |= (self.lo_conflicts ^ lo_conflicts |
+                    act_lo_conflicts ^ self.act_lo_conflicts)
+                self.lo_conflicts = lo_conflicts
+                self.act_lo_conflicts = act_lo_conflicts
             # note we ignore missing/added here - this is the responsibility of
             # refresh - if we are not called from refresh those should be empty
             return RefrData(ldiff.reordered | ldiff.affected |
-                            ldiff.act_ord_status())
+                            ldiff.act_ord_status() | mt_conflicts_changes)
         finally:
             self._lo_wip = list(load_order.cached_lo_tuple())
             self._active_wip = list(load_order.cached_active_tuple())
@@ -2327,6 +2350,8 @@ class ModInfos(TableFileInfos):
         self.__bsa_lo = self.__bsa_cause = self.__available_bsas = None
         global modInfos
         modInfos = self ##: hack needed in ModInfo.readHeader
+        # lo conflicts cache only used in _ModsUIList.set_item_format
+        self.lo_conflicts, self.act_lo_conflicts = set(), set()
         super().__init__(ModInfo)
 
     # Refresh - not quite surprisingly this is super complex - therefore define
@@ -2357,7 +2382,6 @@ class ModInfos(TableFileInfos):
                 forceActive=bool(rdata.to_del), unlock_lo=unlock_lo)
             if not unlock_lo and ldiff.missing: # unlock_lo=True in delete/BAIN
                 self.warn_missing_lo_act.update(ldiff.missing)
-        rdata |= lordata
         # if load order did not change, we must perform the refreshes below
         if not ldiff:
             # in case ini files were deleted or modified or maybe string files
@@ -2366,6 +2390,7 @@ class ModInfos(TableFileInfos):
             rdata.redraw |= self._refresh_mod_inis_and_strings()
             if mods_changes:
                 rdata.redraw |= self._file_or_active_updates()
+        rdata |= lordata
         self._voAvailable, self.voCurrent = bush.game.modding_esms(self)
         return rdata
 

Mopy/bash/games_lo.py
@@ -369,10 +369,6 @@ class LoGame:
                                      previous_lord)
         return lord, active # return what we set or was previously set
 
-    # Conflicts - only for timestamp games
-    def has_load_order_conflict(self, mod_name): return False
-    def has_load_order_conflict_active(self, mod_name, active): return False
-
     @classmethod
     def _must_update_active(cls, deleted_plugins, reord_plugins):
         raise NotImplementedError
@@ -714,8 +710,6 @@ class INIGame(LoGame):
 class TimestampGame(LoGame):
     """Oblivion and other games where load order is set using modification
     times."""
-    # Intentionally imprecise mtime cache
-    _mtime_mods: defaultdict[int, set[Path]] = defaultdict(set)
 
     @staticmethod
     def _check_active_order(acti, lord):
@@ -723,16 +717,8 @@ class TimestampGame(LoGame):
         return [] # no need to reorder plugins.txt - fix_lo.act_reordered False
 
     @classmethod
-    def _must_update_active(cls, deleted_plugins, reord_plugins): return deleted_plugins
-
-    def has_load_order_conflict(self, mod_name):
-        ti = int(self._mod_infos[mod_name].ftime)
-        return ti in self._mtime_mods and len(self._mtime_mods[ti]) > 1
-
-    def has_load_order_conflict_active(self, mod_name, active):
-        ti = int(self._mod_infos[mod_name].ftime)
-        return self.has_load_order_conflict(mod_name) and bool(
-            (self._mtime_mods[ti] - {mod_name}) & active)
+    def _must_update_active(cls, deleted_plugins, reord_plugins):
+        return deleted_plugins
 
     # Abstract overrides ------------------------------------------------------
     def __calculate_mtime_order(self, mods=None): # excludes mods in corrupted
@@ -742,7 +728,6 @@ class TimestampGame(LoGame):
         return sorted(self._mod_infos if mods is None else mods, key=is_m)
 
     def _fetch_load_order(self, cached_load_order, cached_active):
-        self._rebuild_mtimes_cache() ##: will need that tweaked for lock load order
         return self.__calculate_mtime_order()
 
     def _persist_load_order(self, lord, active):
@@ -767,13 +752,6 @@ class TimestampGame(LoGame):
             restamp.append((ordered, self._mod_infos[mod].ftime))
         for ordered, modification_time in restamp:
             self._mod_infos[ordered].setmtime(modification_time)
-        # rebuild our cache
-        self._rebuild_mtimes_cache()
-
-    def _rebuild_mtimes_cache(self):
-        self._mtime_mods.clear()
-        for mod, info in self._mod_infos.items():
-            self._mtime_mods[int(info.ftime)].add(mod)
 
     def _persist_if_changed(self, active, lord, previous_active,
                             previous_lord):

Mopy/bash/load_order.py
@@ -427,15 +427,6 @@ def get_lo_files() -> set[bolt.Path]:
     # implementations
     return set(_lo_handler.get_lo_files())
 
-# Timestamp games helpers
-def has_load_order_conflict(mod_name):
-    return _lo_handler.has_load_order_conflict(mod_name)
-
-def has_load_order_conflict_active(mod_name):
-    if not cached_is_active(mod_name): return False
-    return _lo_handler.has_load_order_conflict_active(mod_name,
-                                                      _cached_lord.active)
-
 # Lock load order -------------------------------------------------------------
 def toggle_lock_load_order(user_warning_callback):
     global locked