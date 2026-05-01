wrye-bash/wrye-bash

commit 237c151145dc166065982cc0ebf9f5fcdd44daee

Fix for markers renames:
Introduced in 1ddf7a1a93b9c47884f42f9fd6680fb7f12082af - what happened
was that False would raise a TypeError on being __added__ to Path in
unique_key which would be absorbed by the TypeError except block in the
client. The exception control flow here is flawed but, as noted in
098836bbddf73df18935700989970227d2a0324d more tools are needed to
completely settle #580.

Renames - keep ui_refreshes for propagate_refresh.

mod_links - fix regression in lo undo/redo menus:

Broke in bfc0b5b275965ff13e4bd0d432a70dab859369ee

Rename renames (a RefrData attribute).

Mopy/bash/basher/mods_links.py
@@ -612,7 +612,7 @@ class Mods_LOUndo(ItemLink):
     _keyboard_hint = 'Ctrl+Z'
 
     def Execute(self):
-        self.window.lo_undo()
+        self.window.data_store.wip_lo_undo_redo_load_order(False)
 
 #------------------------------------------------------------------------------
 class Mods_LORedo(ItemLink):
@@ -622,7 +622,7 @@ class Mods_LORedo(ItemLink):
     _keyboard_hint = 'Ctrl+Y'
 
     def Execute(self):
-        self.window.lo_redo()
+        self.window.data_store.wip_lo_undo_redo_load_order(True)
 
 #------------------------------------------------------------------------------
 class Mods_LOExport(ItemLink):

Mopy/bash/basher/__init__.py
@@ -2053,7 +2053,7 @@ class SaveList(UIList):
         do_enable = not sinf.is_save_enabled()
         extension = enabled_ext if do_enable else disabled_ext
         if rdata := self.try_rename(sinf, fn_item.fn_body,
-                                    force_ext=extension):
+                                    forced_ext=extension):
             self.RefreshUI(rdata)
 
     # Save profiles
@@ -2333,8 +2333,8 @@ class InstallersList(UIList):
         #--Rename each installer, keeping the old extension (for archives)
         if isinstance(root, tuple):
             root = root[0]
-        ui_refreshes = defaultdict(bool) # Store refreshes
-        return newName, root, ui_refreshes
+        store_refr = defaultdict(bool) # Store refreshes
+        return newName, root, store_refr
 
     @staticmethod
     def _unhide_wildcard():
@@ -3743,22 +3743,22 @@ class BashFrame(WindowFrame):
                 title=_('Lock Load Order'))
             load_order.warn_locked = False
 
-    def refresh_and_warn(self, ui_refresh, booting):
+    def refresh_and_warn(self, ui_refreshes, booting):
         # ONLY use in propagate_refresh
-        for list_key, do_refr in ui_refresh.items():
-            if do_refr and (uil := self.all_uilists[list_key]) is not None:
-                if not isinstance(do_refr, dict): # True or RefrData
-                    do_refr = {'rdata': do_refr} if isinstance(do_refr,
+        for list_key, ref_args in ui_refreshes.items():
+            if ref_args and (uil := self.all_uilists[list_key]) is not None:
+                if not isinstance(ref_args, dict): # True or RefrData
+                    ref_args = {'rdata': ref_args} if isinstance(ref_args,
                         RefrData) else {}
-                do_refr.setdefault('focus_list', False)
-                uil.RefreshUI(**do_refr)
+                ref_args.setdefault('focus_list', False)
+                uil.RefreshUI(**ref_args)
         stores = {Store.BSAS: bosh.bsaInfos, Store.MODS: bosh.modInfos,
                   Store.SAVES: bosh.saveInfos} # this belongs to stores
         if booting: # trigger warnings on boot, ui_refresh is empty then
-            ui_refresh = dict.fromkeys(stores, True)
+            ui_refreshes = dict.fromkeys(stores, True)
         multi_warns, lo_warns = [], []
-        for list_key, do_refr in ui_refresh.items():
-            if do_refr and (ds := stores.get(list_key)):
+        for list_key, ref_args in ui_refreshes.items():
+            if ref_args and (ds := stores.get(list_key)):
                 ds.warning_args(multi_warns, lo_warns, self, list_key)
         if multi_warns:
             mk = (mwd := MultiWarningDialog).make_highlight_entry
@@ -3826,11 +3826,11 @@ class BashFrame(WindowFrame):
         #--Clean rename dictionary.
         modNames = {*bosh.modInfos.corrupted}
         modNames.update(bosh.modInfos)
-        renames = bass.settings[u'bash.mods.renames']
+        bash_mod_renames = bass.settings['bash.mods.renames']
         # Make a copy, we may alter it in the loop
-        for old_mname, new_mname in list(renames.items()):
+        for old_mname, new_mname in list(bash_mod_renames.items()):
             if new_mname not in modNames:
-                del renames[old_mname]
+                del bash_mod_renames[old_mname]
         # Clean backup directories of old .es*/.es*f files
         for tc_store in (bosh.modInfos, bosh.saveInfos):
             existing_roots = {p.fn_body for p in tc_store}

Mopy/bash/balt.py
@@ -1134,11 +1134,11 @@ class UIList(PanelWin):
 
     @final
     @conversation
-    def try_rename(self, info, new_root, store_refr=None, *, force_ext=False):
+    def try_rename(self, info, new_root, store_refr=None, *, forced_ext=''):
         """Rename Mods/BSAs/Screens/Installers/Saves - note the @conversation,
-         this needs to be atomic with respect to refreshes and ideally
-         atomic short - store_refr is Installers only. Inis won't be added."""
-        newName = info.unique_key(new_root, force_ext)
+        this needs to be atomic with respect to refreshes and ideally atomic
+        short - store_refr is Installers only. Inis won't be added."""
+        newName = info.unique_key(new_root, forced_ext)
         if newName is None: # new and old names are ci-same
             return RefrData()
         try:

Mopy/bash/basher/belt.py
@@ -327,10 +327,10 @@ class PageFinish(PageInstaller):
         PageInstaller.__init__(self, parent)
         subs = sorted(wrye_parser.sublist)
         #--make the list that will be displayed
-        renames = wrye_parser.plugin_renames
-        displayed_plugins = [f'{x} -> {renames[x]}' if x in renames else x for
-                             x in wrye_parser.plugin_enabled]
-        self._wiz_parent.ret.rename_plugins = renames
+        pl_renames = wrye_parser.plugin_renames
+        displayed_plugins = [f'{x} -> {pl_renames[x]}' if x in pl_renames
+                             else x for x in wrye_parser.plugin_enabled]
+        self._wiz_parent.ret.rename_plugins = pl_renames
         parent.parser.choiceIdex += 1
         textTitle = Label(self, _('The installer script has finished, and '
                                   'will apply the following settings:'))

commit 5fab295f97a26b385bdff7103c47564f1228a77b

[!]Fixups for activating all mods/toggling active:
_lo_op will swallow the exceptions due to the `finally: return` - so
the control flow in lo_activate_all and lo_toggle_active was flawed.
Fixed by retiring _lo_activate(_lo_op) and using directly _do_activate
(at the expense of indeed creating the LordDiff, named differently here)

Seems also like detection of circular masters was broken - this would
raise a BoltError whose handling is now moved to lo_toggle_active - [!],
but minor - couple uses that should not have that problem. Still the
exception control flow needs work (led to this bug for one).

Note we are two _lo_op uses down - we want to centralize more here (cf
comments on esu files)

Mopy/bash/basher/mods_links.py: the UI was not notified - broke in
237c151145dc166065982cc0ebf9f5fcdd44daee

Mopy/bash/basher/mods_links.py
@@ -94,8 +94,7 @@ class _Mods_ActivateAll(_AMods_ActivePlugins):
         """Activate all mods."""
         lordata = True # on exception refresh all mods
         try:
-            lordata = bosh.modInfos.lo_activate_all(save_act=True,
-                activate_mergeable=self._activate_mergeable)
+            lordata = bosh.modInfos.do_activate_all(self._activate_mergeable)
         except exception.PluginsFullError:
             self._showError(_('Plugin list is full, so some plugins '
                               'were skipped.'),
@@ -108,7 +107,7 @@ class _Mods_ActivateAll(_AMods_ActivePlugins):
                 title=_('Too Many Plugins'))
         except (exception.BoltError, NotImplementedError) as e:
             deprint('Error while activating plugins', traceback=True)
-            self._showError(f'{e}')
+            self._showError(f'{e!r}')
         self.window.propagate_refresh(lordata)
 
 class _Mods_ActivateNonMergeable(AppendableLink, _Mods_ActivateAll):
@@ -612,7 +611,8 @@ class Mods_LOUndo(ItemLink):
     _keyboard_hint = 'Ctrl+Z'
 
     def Execute(self):
-        self.window.data_store.wip_lo_undo_redo_load_order(False)
+        self.window.propagate_refresh(
+            self.window.data_store.wip_lo_undo_redo_load_order(False))
 
 #------------------------------------------------------------------------------
 class Mods_LORedo(ItemLink):
@@ -622,7 +622,8 @@ class Mods_LORedo(ItemLink):
     _keyboard_hint = 'Ctrl+Y'
 
     def Execute(self):
-        self.window.data_store.wip_lo_undo_redo_load_order(True)
+        self.window.propagate_refresh(
+            self.window.data_store.wip_lo_undo_redo_load_order(True))
 
 #------------------------------------------------------------------------------
 class Mods_LOExport(ItemLink):

Mopy/bash/bosh/__init__.py
@@ -2229,7 +2229,8 @@ def _lo_cache(lord_func):
     return _modinfos_cache_wrapper
 
 def _lo_op(lop_func):
-    """Decorator centralizing saving active state/load order changes."""
+    """Decorator centralizing saving active state/load order changes. Don't
+    raise exceptions in lop_func, will be swallowed in the finally block."""
     @wraps(lop_func)
     def _lo_wip_wrapper(self: ModInfos, *args, ldiff=None, save_all=False,
                         save_wip_lo=False, save_act=False, **kwargs):
@@ -2701,17 +2702,12 @@ class ModInfos(_AFileInfos):
         return load_order.undo_redo_load_order(redo)
 
     #--Lo/active wip caches management ----------------------------------------
-    @_lo_op
-    def _lo_activate(self, fileName, *, out_diff):
-        """Never passed save_***=True - kept it a _lo_op for creating the
-        LordDiff() in one place."""
-        self._do_activate(fileName, set(self), [], out_diff)
-
-    def _do_activate(self, fileName, _modSet, _children, out_diff):
+    def _do_activate(self, fileName, _children=()):
+        outdiff = LordDiff()
         # Skip .esu files, those can't be activated
         ##: This .esu handling needs to be centralized - sprinkled all over
         # actives related lo_* methods
-        if fileName.fn_ext == '.esu': return
+        if fileName.fn_ext == '.esu': return outdiff
         # Speed up lookups, since they occur for the plugin and all masters
         acti_set = set(self._active_wip)
         if fileName not in acti_set: # else we are called to activate masters
@@ -2720,23 +2716,25 @@ class ModInfos(_AFileInfos):
             if msg:
                 msg = f'{fileName}: Trying to activate more than {msg}'
                 raise PluginsFullError(msg)
-        if _children:
+        if _children := _children or []:
             if fileName in _children:
                 raise BoltError(f'Circular Masters: '
                                 f'{" >> ".join((*_children, fileName))}')
-        _children = [fileName]
+        _children.append(fileName)
         #--Check for bad masternames:
         #  Disabled for now
         ##if self[fileName].hasBadMasterNames(): return
         #--Select masters
         for master in self[fileName].masterNames:
             # Check that the master is on disk and not already activated
-            if master in _modSet and master not in acti_set:
-                self._do_activate(master, _modSet, _children, out_diff)
+            if master in self and master not in acti_set:
+                outdiff |= self._do_activate(master, _children)
+                _children.pop() # pop the master from the end of the list
         #--Select in plugins
         if fileName not in acti_set:
             self._active_wip.append(fileName)
-            out_diff.new_act.add(fileName) # manipulate out_diff attrs directly
+            outdiff.new_act.add(fileName) # manipulate out_diff attrs directly
+        return outdiff
 
     @_lo_op
     def lo_deactivate(self, *to_deac, out_diff, _deleted=False):
@@ -2762,7 +2760,7 @@ class ModInfos(_AFileInfos):
     @_lo_op
     def lo_toggle_active(self, mods, *, do_activate=True, out_diff):
         impacted_mods = {}
-        _lo_meth, attr = (self._lo_activate, 'new_act') if do_activate \
+        _lo_meth, attr = (self._do_activate, 'new_act') if do_activate \
             else (self.lo_deactivate, 'new_inact')
         modified_attr = attrgetter_cache[attr]
         # Track illegal activations/deactivations for the return value
@@ -2776,8 +2774,10 @@ class ModInfos(_AFileInfos):
             #if fileName in self.bad_names: return
             try:
                 changes_diff = _lo_meth(fn_mod)
-            except (BoltError, PluginsFullError) as e: # only for _lo_activate
+            except (BoltError, PluginsFullError) as e: # only for _do_activate
                 act_error = e
+                if isinstance(e, BoltError):
+                    out_diff.new_act.clear() # Don't save, something went wrong
                 break
             if not changes_diff: # Can't de/activate that mod, track this
                 illegal.append(fn_mod)
@@ -2788,8 +2788,7 @@ class ModInfos(_AFileInfos):
                 impacted_mods[fn_mod] = load_order.get_ordered(impacted)
         return impacted_mods, illegal, act_error
 
-    @_lo_op
-    def lo_activate_all(self, *, activate_mergeable=True, out_diff):
+    def do_activate_all(self, activate_mergeable=True):
         """Activates all non-mergeable plugins (except ones tagged Deactivate),
         then all mergeable plugins (again, except ones tagged Deactivate).
         Raises a PluginsFullError if too many non-mergeable plugins are present
@@ -2809,19 +2808,14 @@ class ModInfos(_AFileInfos):
         # Then activate as many of the mergeable plugins as we can
         if mergeable and activate_mergeable:
             to_act.extend(p for p, v in s_plugins.items() if v in mergeable)
-        if not to_act: return
-        try:
-            try:
-                for j, p in enumerate(to_act):
-                    if p not in out_diff.new_act: # else a delinquent master(?)
-                        self._lo_activate(p, out_diff=out_diff)
-            except PluginsFullError as e:
-                if j >= first_mergeable:
-                    raise SkippedMergeablePluginsError from e
-                raise
-        except BoltError:
-            out_diff.new_act.clear() # Don't save, something went wrong
-            raise
+        (_impctd, _illgl, act_error), lordata = self.lo_toggle_active(to_act,
+            save_act=True, out_diff=(outdiff := LordDiff()))
+        if act_error:
+            if isinstance(act_error, PluginsFullError):
+                if not ({*to_act[:first_mergeable]} - outdiff.new_act):
+                    raise SkippedMergeablePluginsError from act_error
+            raise act_error
+        return lordata
 
     @_lo_op
     def lo_activate_exact(self, partial_actives: Iterable[FName], *, out_diff):