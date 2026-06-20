wrye-bash/wrye-bash

commit 7651e354ef9a09e3344a1ce0035ed980891166d6

Add tags info on the patch log:
A useful piece of info for bug reports - I don't think this breaks
anything but maybe a better presentation/other places we could log

Finalize some of the new APIs introduced for blueprint handling:

See comments in b1d15f128eda4144f050b6ff267a5c3883b97466 on naming and
responsibilities - turns out _set need only return pin_active_state
confining mutating fixed_order_plugins in _set_pinned_mods, which made
much easier following it, so I could fix the typing warnings also.

Mopy/bash/game/starfield/__init__.py
@@ -346,15 +346,15 @@ class _AStarfieldGameInfo(PatchGame):
         _ccc_filename = 'Starfield.ccc'
         _ccc_dirs = 'saveBase', 'app'
 
-        def _set(self, *, active=None, **kwargs):
+        def _get_force_act(self, *, active=frozenset(), **kwargs):
             # use an override instead of passing kwargs to _set_pinned_mods to
             # avoid calling super._set_pinned_mods
-            mbaip, fo_mods = super()._set(**kwargs)
+            mbaip = super()._get_force_act(**kwargs)
             stems = {p.fn_body for p in
                      {*active, *(k for k, v in mbaip.items() if v)}}
             for stem, bp_ship in self._blue_ships.items():
                 mbaip[bp_ship] = stem.lower() in stems
-            return mbaip, fo_mods
+            return mbaip
 
         def _readd_mods(self, lo, active, sorted_rem):
             super()._readd_mods(lo, active, sorted_rem)

Mopy/bash/games_lo.py
@@ -233,7 +233,7 @@ class FixInfo(object):
 class LoGame:
     """API for setting, getting and validating the active plugins and the
     load order (of all plugins) according to the game engine (in principle)."""
-    force_load_first: LoTuple = []
+    force_load_first: tuple[FName, ...] = () # master_file is added in __init__
     _star = False # whether plugins.txt uses a star to denote an active plugin
 
     def __init__(self, mod_infos, game_handle, plugins_txt_path: Path, *,
@@ -249,17 +249,21 @@ class LoGame:
         self.pin_active_state = self.fixed_order_plugins = None
         self._print_lo_paths()
 
-    def _set(self, *, _reset=False, **kwargs):
+    def _get_force_act(self, *, _reset=False, **kwargs) -> dict[FName, bool]:
+        """Get (possibly re/setting) the pinned load order/active state caches
+        and return the latter. Called in: LoGame._fix_load_order for setting
+        the caches initially and in _fix_active_plugins to force de/activate.
+        See also AsteriskGame._cached_or_fetch/_persist_load_order."""
         pinn = self.pin_active_state, self.fixed_order_plugins
         if any(p is None for p in pinn) or _reset:
-            pinn = self._set_pinned_mods()
-            self.pin_active_state, self.fixed_order_plugins = pinn
-        return pinn
+            self.pin_active_state, self.fixed_order_plugins = \
+                self._set_pinned_mods()
+        return self.pin_active_state
 
     # INITIALIZATION ----------------------------------------------------------
     def _set_pinned_mods(self):
         """Set the master file(s) that must always be active if present."""
-        fo = self.force_load_first = [*self._existing(self.force_load_first)]
+        fo = self.force_load_first = (*self._existing(self.force_load_first),)
         return dict.fromkeys(fo, True), fo
 
     def _print_lo_paths(self):
@@ -490,13 +494,13 @@ class LoGame:
                 lord.append(mod)
         # we need to set the _shipwrecks for sort() - we will force activate in
         # _fix_active_plugins - and repeat the ccc files do_update - ##: avoid?
-        self._set(active=set(), _reset=(fix_lo.lo_added | fix_lo.lo_removed))
+        self._get_force_act(_reset=bool(fix_lo.lo_added or fix_lo.lo_removed))
         # See if any esm files are loaded below an esp and reorder as necessary
         lord.sort(key=self.lo_sort_key())
         # check if any of the existing mods were moved in/out the master block
         lo_order_changed |= ol != [x for x in lord if x not in fix_lo.lo_added]
         fix_lo.lo_duplicates = self._check_for_duplicates(lord)
-        # loaded in _set - those ones come first
+        # loaded in _get_force_act - those ones come first
         if lord[:len(fo_mods := self.force_load_first)] != fo_mods:
             fo_set = set(fo_mods)
             lord[:] = [*fo_mods, *(x for x in lord if x not in fo_set)]
@@ -520,7 +524,7 @@ class LoGame:
         fix_active.act_removed = set(acti) - acti_filtered_set
         # present mods that are always active - noop for AsteriskGame as always
         # active plugins are manually added on getting the load order
-        for fn_plugin, isact in self._set(active=acti_filtered_set)[0].items():
+        for fn_plugin, isact in self._get_force_act(active=acti_filtered_set).items():
             if isact and fn_plugin not in acti_filtered_set:
                 if fn_plugin == self._game_handle.master_file:
                     acti_filtered.insert(0, fn_plugin)
@@ -951,7 +955,7 @@ class AsteriskGame(_TextFileLo):
         to our caller being get_load_order *at least one* is None."""
         try:
             active, lo = self._plugins_txt.parse_modfile()
-            rem_from_acti = self._set(active=active)[0]
+            rem_from_acti = self._get_force_act(active=active)
             if any_dropped := [x for x in lo if x in rem_from_acti]:
                 bolt.deprint(f'Removing {_pl(any_dropped)} from '
                              f'{self._plugins_txt.abs_path}')
@@ -969,13 +973,13 @@ class AsteriskGame(_TextFileLo):
             bolt.deprint(f'Created {self._plugins_txt.abs_path}')
         return lo, active
 
-    def _set(self, **kwargs):
+    def _get_force_act(self, **kwargs):
         fload_set = {*(fload := self.__class__.force_load_first)}
         for ccc_file in self.__cc_files.values():
             try:
                 ccc_file.do_update(raise_os_error=True)
-                fload = [*fload, *(m for m in ccc_file.ccc_contents if
-                                   m not in fload_set)]
+                fload = (*fload, *(m for m in ccc_file.ccc_contents if
+                                   m not in fload_set))
                 break # first ccc file found
             except OSError as e:
                 if ccc_file.has_changed: # freshly deleted or not found
@@ -985,10 +989,10 @@ class AsteriskGame(_TextFileLo):
                         deprint(f'Failed to open {ccc_file.abs_path}',
                                 traceback=True)
                     ccc_file.has_changed = False # deprint the first time only
-        if (fload := [*self._existing(fload)]) != self.force_load_first:
+        if (fload := (*self._existing(fload),)) != self.force_load_first:
             self.force_load_first = fload
             kwargs['_reset'] = True
-        return super()._set(**kwargs)
+        return super()._get_force_act(**kwargs)
 
     def _readd_mods(self, lo, active, sorted_rem):
         # Prepend all present fixed-order plugins that can't be in the
@@ -1005,7 +1009,7 @@ class AsteriskGame(_TextFileLo):
 
     def _persist_load_order(self, lord, active, *, backup_act=False,
                             rem_from_acti=None):
-        rem_from_acti = self._set(active=active)[0] \
+        rem_from_acti = self._get_force_act(active=active) \
             if rem_from_acti is None else rem_from_acti
         lord = [x for x in lord if x not in rem_from_acti]
         active = [x for x in active if x not in rem_from_acti]

Mopy/bash/patcher/patch_files.py
@@ -541,11 +541,10 @@ class PatchFile(ModFile):
                 message = f'* {self.load_dict[mname]:02X} '
             except KeyError:
                 message = '* ++ '
-            if version:
-                message += _('%(msg_plugin)s [Version %(plugin_ver)s]') % {
-                    'msg_plugin': mname, 'plugin_ver': version}
-            else:
-                message += mname
+            message += _('%(msg_plugin)s [Version %(plugin_ver)s]') % {
+              'msg_plugin': mname, 'plugin_ver': version} if version else mname
+            if tgs := self.all_tags.get(mname):
+                message += f' [{", ".join(tgs)}]'
             log(message)
         #--Load Mods and error mods
         if self.pfile_aliases:

commit 7263cc5713458af78b1b1f71b7068952fcbd20f5

[Starfield]Exclude .psc files from generated achlists:
This touches a bit on path separator handling which I don't want to
open up now. Just searched for '\\' uses and pruned a couple of
unneeded ones — path separators are crucial to cross-platform code —
WIP. Mopy/bash/tests/test_bolt.py: it's the same on Windows due to
os.path.normpath:

>>> GPath('c:/users/mrd') == GPath('c:\\users\\mrd')
True # on windows

Could prune some more occurrences of '\\' like that.

Mopy/bash/games_lo.py: regression from 7651e354ef9a09e3344a1ce0035ed980891166d6
I also shuffled the methods a bit
Mopy/bash/brec/complex_subrecords.py: some more nit from previous merge

Closes #747

Mopy/bash/basher/installer_links.py
@@ -688,17 +688,19 @@ class Installer_ExportAchlist(_SingleInstallable):
     def Execute(self):
         out_dir = bass.dirs['app'].join(self.__class__._mode_info_dir)
         achlist = out_dir.join(f'{self._selected_info.fn_key}.achlist')
+        pdata = '\\\\'.join((bgame := bush.game).mods_dir_path)
+        it = bolt.sortFiles(self._selected_info.ci_dest_sizeCrc)
+        it = (d.replace('\\', '\\\\') for d in it
+              # exclude top level files and docs - last one monkey patched
+              if os.path.split(d)[0] and not d.lower().startswith('docs'))
+        if exc := bgame.Bain.achlist_excludes:
+            it = (d for d in it if os.path.splitext(d)[1] not in exc)
         ##: Windows-1252 is a guess. The CK is able to decode non-ASCII
         # characters encoded with it correctly, at the very least (UTF-8/UTF-16
         # both fail), but the encoding might depend on the game language?
         with BusyCursor(), achlist.open('w', encoding='cp1252') as out:
             out.write(u'[\n\t"')
-            lines = u'",\n\t"'.join(
-                u'\\'.join((*bush.game.mods_dir_path, d)).replace(u'\\', u'\\\\')
-                for d in bolt.sortFiles(self._selected_info.ci_dest_sizeCrc)
-                # exclude top level files and docs - last one monkey patched
-                if os.path.split(d)[0] and not d.lower().startswith(u'docs'))
-            out.write(lines)
+            out.write('",\n\t"'.join(fr'{pdata}\\{d}' for d in it))
             out.write(u'"\n]')
 
 class Installer_Move(_InstallerLink):

Mopy/bash/bosh/bain.py
@@ -159,8 +159,7 @@ class Installer(ListInfo):
                '.ppt', '.pptx', '.md', '.rst', '.url'}
     reReadMe = re.compile(
         f'^.*?([^{bolt.os_sep_re}]*)(read[ _]?me|lisez[ _]?moi)'
-        f'([^{bolt.os_sep_re}]*)(' +
-        '|'.join((f'\\{e}' for e in docExts)) + ')$', re.I)
+        f'([^{bolt.os_sep_re}]*)({"|".join(map(re.escape, docExts))})$', re.I)
     # Filename roots (i.e. filenames without extensions) that are common and
     # should be renamed by BAIN to avoid conflicts if they are used as doc
     # files (e.g. 'credits' means 'Credits.txt' etc. would be caught). May be

Mopy/bash/bosh/bsa_files.py
@@ -52,12 +52,12 @@ from ..exception import BSACompressionError, BSADecodingError, \
     BSADecompressionError, BSADecompressionSizeError, BSAError, BSAFlagError
 
 _bsa_encoding = 'cp1252' # rumor has it that's the files/folders names encoding
-path_sep = u'\\'
+_bsa_path_sep = '\\'
 
 # Utilities -------------------------------------------------------------------
 def _decode_path(byte_path: bytes, bsa_name: str):
     try:
-        return byte_path.decode(_bsa_encoding).replace('/', path_sep)
+        return byte_path.decode(_bsa_encoding).replace('/', _bsa_path_sep)
     except UnicodeDecodeError:
         raise BSADecodingError(bsa_name, byte_path)
 
@@ -570,7 +570,7 @@ class ABsa(AFile):
                                 f"\n{folder}" % {'target_bsa': self._bsa_name})
                 # BSA paths always have backslashes, so we need to convert them
                 # to the platform's path separators before we extract
-                target_dir = os.path.join(dest_folder, *folder.split(path_sep))
+                target_dir = os.path.join(dest_folder, *folder.split(_bsa_path_sep))
                 os.makedirs(target_dir, exist_ok=True)
                 for filename, record in file_records:
                     data_size = record.raw_data_size()
@@ -606,7 +606,7 @@ class ABsa(AFile):
         for folder_path, bsa_folder in self.bsa_folders.items():
             # The incoming folder_files_dict uses native path separators, so
             # convert the BSA separators over
-            ffd_key = folder_path.lower().replace(path_sep, os.sep)
+            ffd_key = folder_path.lower().replace(_bsa_path_sep, os.sep)
             if ffd_key not in folder_files_dict: continue
             # Has assets we need to extract. Keep order to avoid seeking
             # back and forth in the file
@@ -699,7 +699,7 @@ class BSA(ABsa):
             self._filenames = [*chain.from_iterable(
                 # Inlined from _decode_path for startup performance - no
                 # need to replace slashes though since these are file names
-                (f'{fp}{path_sep}{fname.decode(_bsa_encoding)}' for
+                (f'{fp}{_bsa_path_sep}{fname.decode(_bsa_encoding)}' for
                  fname in fnames) for fp, fnames in names_pairs)]
         except UnicodeDecodeError:
             raise BSADecodingError(self._bsa_name, file_names)
@@ -807,7 +807,7 @@ class BA2(ABsa):
                                 f"\n{folder}" % {'target_bsa': self._bsa_name})
                 # BSA paths always have backslashes, so we need to convert them
                 # to the platform's path separators before we extract
-                target_dir = os.path.join(dest_folder, *folder.split(path_sep))
+                target_dir = os.path.join(dest_folder, *folder.split(_bsa_path_sep))
                 os.makedirs(target_dir, exist_ok=True)
                 for filename, f_record in file_records:
                     if is_dx10:
@@ -852,7 +852,7 @@ class BA2(ABsa):
             filename = _decode_path(
                 file_names_block[2:name_size + 2].tobytes(), self._bsa_name)
             file_names_block = file_names_block[name_size + 2:]
-            folder_dex = filename.rfind(path_sep)
+            folder_dex = filename.rfind(_bsa_path_sep)
             folder_name = '' if folder_dex == -1 else filename[:folder_dex]
             self.bsa_folders[folder_name].folder_assets[
                 filename[folder_dex + 1:]] = file_records[index]

Mopy/bash/brec/complex_subrecords.py
@@ -841,22 +841,19 @@ class MelEffectsTes4(MelSequential):
             formElements.add(self)
 
     def load_mel(self, record, ins, sub_type, size_, *debug_strs):
-        target_loaders = (self._obme_loaders
-                          if record.obme_record_version is not None
-                          else self._vanilla_loaders)
+        target_loaders = (self._vanilla_loaders
+            if record.obme_record_version is None else self._obme_loaders)
         target_loaders[sub_type].load_mel(record, ins, sub_type, size_, *debug_strs)
 
     def dumpData(self, record, out):
-        target_elements = (self._obme_elements
-                           if record.obme_record_version is not None
-                           else self._vanilla_elements)
+        target_elements = (self._vanilla_elements
+            if record.obme_record_version is None else self._obme_elements)
         for element in target_elements:
             element.dumpData(record, out)
 
     def mapFids(self, record, function, save_fids=False):
-        target_form_elements = (self._obme_form_elements
-                                if record.obme_record_version is not None
-                                else self._vanilla_form_elements)
+        target_form_elements = (self._vanilla_form_elements
+          if record.obme_record_version is None else self._obme_form_elements)
         for form_element in target_form_elements:
             form_element.mapFids(record, function, save_fids)
 

Mopy/bash/game/__init__.py
@@ -865,6 +865,7 @@ class GameInfo(object):
         wrye_bash_data_files = set()
         # Wrye Bash directories to install and exclude from Clean Data
         wrye_bash_data_dirs = {'bash patches', 'bashtags', 'ini tweaks'}
+        achlist_excludes = set() # files to exclude from achlist
 
         @classmethod
         def mk_lod_tex_func(cls, normals, ossep):

Mopy/bash/game/starfield/__init__.py
@@ -193,6 +193,7 @@ class _AStarfieldGameInfo(PatchGame):
             _j('interface', 'translations'): {'.txt'},
         }
         skip_bain_refresh = {'sf1edit backups', 'sf1edit cache'}
+        achlist_excludes = {'.psc'}
 
     class Esp(GameInfo.Esp):
         extension_forces_flags = True

Mopy/bash/games_lo.py
@@ -249,35 +249,6 @@ class LoGame:
         self.pin_active_state = self.fixed_order_plugins = None
         self._print_lo_paths()
 
-    def _get_force_act(self, *, _reset=False, **kwargs) -> dict[FName, bool]:
-        """Get (possibly re/setting) the pinned load order/active state caches
-        and return the latter. Called in: LoGame._fix_load_order for setting
-        the caches initially and in _fix_active_plugins to force de/activate.
-        See also AsteriskGame._cached_or_fetch/_persist_load_order."""
-        pinn = self.pin_active_state, self.fixed_order_plugins
-        if any(p is None for p in pinn) or _reset:
-            self.pin_active_state, self.fixed_order_plugins = \
-                self._set_pinned_mods()
-        return self.pin_active_state
-
-    # INITIALIZATION ----------------------------------------------------------
-    def _set_pinned_mods(self):
-        """Set the master file(s) that must always be active if present."""
-        fo = self.force_load_first = (*self._existing(self.force_load_first),)
-        return dict.fromkeys(fo, True), fo
-
-    def _print_lo_paths(self):
-        """Prints the paths that will be used and what they'll be used for.
-        Useful for debugging."""
-        acti_lo = self.get_lo_files()
-        bolt.deprint('Using the following load order files:')
-        if len(acti_lo) == 2 and acti_lo[0] == acti_lo[1]:
-            bolt.deprint(f' - Load order and active plugins: {acti_lo[0]}')
-        else:
-            bolt.deprint(f' - Active plugins: {acti_lo.pop(0)}')
-            if acti_lo:
-                bolt.deprint(f' - Load order: {acti_lo.pop(0)}')
-
     # API: Get and helpers ----------------------------------------------------
     def get_load_order(self, cached_load_order: LoList,
             cached_active_ordered: LoList, fix_lo) -> tuple[LoTuple, LoTuple]:
@@ -319,6 +290,23 @@ class LoGame:
         if fix_lo.lo_changed():
             self._backup_load_order()
             self._persist_load_order(lo, None) # active is not used here
+
+    # Asterisk game overrides -------------------------------------------------
+    def _get_force_act(self, *, _reset=False, **kwargs) -> dict[FName, bool]:
+        """Get (possibly re/setting) the pinned load order/active state caches
+        and return the latter. Called in: LoGame._fix_load_order for setting
+        the caches initially and in _fix_active_plugins to force de/activate.
+        See also AsteriskGame._cached_or_fetch/_persist_load_order."""
+        pinn = self.pin_active_state, self.fixed_order_plugins
+        if any(p is None for p in pinn) or _reset:
+            self.pin_active_state, self.fixed_order_plugins = \
+                self._set_pinned_mods()
+        return self.pin_active_state
+
+    def _set_pinned_mods(self):
+        """Set the master file(s) that must always be active if present."""
+        fo = self.force_load_first = (*self._existing(self.force_load_first),)
+        return dict.fromkeys(fo, True), fo
 
     # API: Set and helpers ----------------------------------------------------
     def set_load_order(self, lord, active, previous_lord=None,
@@ -501,7 +489,7 @@ class LoGame:
         lo_order_changed |= ol != [x for x in lord if x not in fix_lo.lo_added]
         fix_lo.lo_duplicates = self._check_for_duplicates(lord)
         # loaded in _get_force_act - those ones come first
-        if lord[:len(fo_mods := self.force_load_first)] != fo_mods:
+        if (*lord[:len(fo_mods := self.force_load_first)],) != fo_mods:
             fo_set = set(fo_mods)
             lord[:] = [*fo_mods, *(x for x in lord if x not in fo_set)]
             lo_order_changed = True
@@ -623,6 +611,18 @@ class LoGame:
         # https://github.com/Ortham/libloadorder/issues/86#issuecomment-4254218481
         return sorted(sorted(mods, key=str.upper, reverse=True),
                       key=self.lo_sort_key(by_time=True))
+
+    def _print_lo_paths(self):
+        """Prints the paths that will be used and what they'll be used for.
+        Useful for debugging."""
+        acti_lo = self.get_lo_files()
+        bolt.deprint('Using the following load order files:')
+        if len(acti_lo) == 2 and acti_lo[0] == acti_lo[1]:
+            bolt.deprint(f' - Load order and active plugins: {acti_lo[0]}')
+        else:
+            bolt.deprint(f' - Active plugins: {acti_lo.pop(0)}')
+            if acti_lo:
+                bolt.deprint(f' - Load order: {acti_lo.pop(0)}')
 
 def _mk_ini(ini_key, star, ini_fpath):
     """Creates a new IniFile from the specified bolt.Path object."""

Mopy/bash/tests/test_bolt.py
@@ -657,7 +657,7 @@ class TestFname(object):
         assert type(GPath_no_norm(FName('Passes through!')).s) is FName ##: oopsie
 
     def test_path_join_fname(self):
-        assert type(GPath('c:\\users\\mrd').join(FName('wrye')).s) is str
+        assert type(GPath('c:/users/mrd').join(FName('wrye')).s) is str
 
     def test_containers(self):
         """Test membership of FN's vs paths and strings in various container