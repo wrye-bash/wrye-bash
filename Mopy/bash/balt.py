wrye-bash/wrye-bash

commit a81fe6be06e42b47573b3704693e2fe8e19e4003

Fix for timestamp games added plugins:
New plugins wouldn't be added at the end of the load order. I needed to
patch both set and get paths, the latter conveniently using rdata_mods.
Don't mind mod_links, that's the last hacky piece in there, fixed in
next commit.

Mopy/bash/bosh/__init__.py
@@ -2344,8 +2344,8 @@ class ModInfos(_AFileInfos):
                                             ldiff=ldiff)
         else: # if refresh_infos is False but mods are added force refresh
             lordata = self.refreshLoadOrder(ldiff=ldiff,
-                forceRefresh=mods_changes or unlock_lo,
-                forceActive=False, unlock_lo=unlock_lo)
+                forceRefresh=mods_changes or unlock_lo, forceActive=False,
+                unlock_lo=unlock_lo, rdata_mods=rdata)
             if not unlock_lo and ldiff.missing: # unlock_lo=True in delete/BAIN
                 self.warn_missing_lo_act.update(ldiff.missing)
         # if load order did not change, we must perform the refreshes below
@@ -2666,11 +2666,11 @@ class ModInfos(_AFileInfos):
     # active plugins changed, those methods run a refresh on modInfos data
     @_lo_cache
     def refreshLoadOrder(self, forceRefresh=True, forceActive=True,
-                         unlock_lo=False):
+                         unlock_lo=False, rdata_mods=None):
         # Needed for BAIN, which may have to reorder installed plugins
         with load_order.Unlock(unlock_lo):
             return load_order.refresh_lo(cached=not forceRefresh,
-                                         cached_active=not forceActive)
+                cached_active=not forceActive, rdata_mods=rdata_mods)
 
     @_lo_cache
     def _wip_lo_save(self, update_lo, update_act):

Mopy/bash/games_lo.py
@@ -257,7 +257,8 @@ class LoGame:
 
     # API: Get and helpers ----------------------------------------------------
     def get_load_order(self, cached_load_order: LoList,
-            cached_active_ordered: LoList, fix_lo) -> tuple[LoTuple, LoTuple]:
+                       cached_active_ordered: LoList, fix_lo, rdata_mods) -> \
+            tuple[LoTuple, LoTuple]:
         """Get and validate current load order and active plugins information.
 
         Meant to fetch at once both load order and active plugins
@@ -269,7 +270,7 @@ class LoGame:
         unchanged, possibly validating the other one based on stale data.
         NOTE: modInfos must exist and be up to date for validation."""
         lo, active = self._cached_or_fetch(cached_load_order,
-                                           cached_active_ordered, fix_lo)
+            cached_active_ordered, fix_lo, rdata_mods)
         # for timestamps we use modInfos so we should not get an invalid
         # load order (except redated master). For text based games however
         # the fetched order could be in whatever state, so get this fixed
@@ -281,7 +282,8 @@ class LoGame:
         self._save_fixed_load_order(fix_lo, fixed_active, lo, active)
         return tuple(lo), tuple(active)
 
-    def _cached_or_fetch(self, cached_load_order, cached_active, fix_lo):
+    def _cached_or_fetch(self, cached_load_order, cached_active, fix_lo,
+                         rdata_mods):
         """Responsible for deciding if cached values are still valid."""
         if cached_active is None or self._plugins_txt.do_update():
             cached_active = self._fetch_active_plugins(fix_lo.act_duplicates)
@@ -341,10 +343,10 @@ class LoGame:
         if lord is active is None:
             raise ValueError('Load order or active must be not None')
         dry_run = previous_lord is previous_active is None
-        if quiet := fix_lo is None: fix_lo = FixInfo() # will be discarded
+        if fix_lo is None: fix_lo = FixInfo() # will be discarded
         if setting_lo := lord is not None:
             # fix the load order - lord is modified in place, hence test below
-            self._fix_load_order(lord, fix_lo, not quiet, _saving=True)
+            self._fix_load_order(lord, fix_lo, _saving=True)
             setting_lo = previous_lord != lord
         setting_active = active is not None
         if setting_lo and not setting_active:
@@ -426,8 +428,7 @@ class LoGame:
         raise NotImplementedError
 
     # VALIDATION --------------------------------------------------------------
-    def _fix_load_order(self, lord: list[FName], fix_lo, _mtime_order=True,
-                        _saving=False):
+    def _fix_load_order(self, lord: list[FName], fix_lo, _saving=False):
         """Fix inconsistencies between given loadorder and actually installed
         mod files as well as impossible load orders. We need a refreshed
         bosh.modInfos reflecting the contents of Data/.
@@ -753,10 +754,13 @@ class TimestampGame(LoGame):
         return deleted_plugins
 
     # Abstract overrides ------------------------------------------------------
-    def _cached_or_fetch(self, *args):
-        _lo, act = super()._cached_or_fetch(*args)
-        # ignore cached - we could patch fix_load_order to do minimal work here
-        return self.calculate_mtime_order(self._mod_infos), act
+    def _cached_or_fetch(self, cached_load_order, cached_active, fix_lo,
+                         rdata_mods):
+        _lo, act = super()._cached_or_fetch(cached_load_order, cached_active,
+                                            fix_lo, rdata_mods)
+        lord = self.calculate_mtime_order(self._mod_infos)
+        self._add_last(lord, rdata_mods.to_add)
+        return lord, act
 
     def _persist_load_order(self, lord, active):
         current = self.calculate_mtime_order(modinfos := self._mod_infos)
@@ -770,13 +774,19 @@ class TimestampGame(LoGame):
             self._persist_active_plugins(active, lord)
 
     # Other overrides ---------------------------------------------------------
-    def _fix_load_order(self, lord, fix_lo, _mtime_order=True, _saving=False):
-        """If _mtime_order is True, the load order will be sorted by mtime -
-        previous behavior (see clients) but may not be what we want in all
-        cases."""
-        super()._fix_load_order(lord, fix_lo, _saving=_saving)
-        if _mtime_order and fix_lo.lo_added:
-            lord[:] = self.calculate_mtime_order(mods=lord)
+    def _fix_load_order(self, lord, fix_lo, _saving=False):
+        """If new mods are added we need to order them last - set_load_order
+        path."""
+        super()._fix_load_order(lord, fix_lo, _saving)
+        if _saving: self._add_last(lord, fix_lo.lo_added)
+
+    def _add_last(self, lord, added):
+        if added:
+            lo_added = []
+            lo_new = [m for m in lord if m not in added or lo_added.append(m)]
+            lo_new = sorted([*lo_new, *lo_added], key=self.lo_sort_key())
+            set_mtimes(lo_new, lord, self._mod_infos)
+            lord[:] = lo_new
 
 class _TextFileLo(LoGame):
     """Common code for games that use a text file to store the load order."""
@@ -814,7 +824,8 @@ class TextfileGame(_TextFileLo):
         return self._loadorder_txt
 
     # Abstract overrides ------------------------------------------------------
-    def _cached_or_fetch(self, cached_load_order, cached_active, fix_lo):
+    def _cached_or_fetch(self, cached_load_order, cached_active, fix_lo,
+                         rdata_mods):
         """Read data from loadorder.txt file. If loadorder.txt does not
         exist create it and use cached/plugins.txt info so the load order of
         the user is preserved (note it will create the plugins.txt if not
@@ -822,7 +833,7 @@ class TextfileGame(_TextFileLo):
         anyway call _fix_load_order. The relative order of mods will be
         corrected to match their relative order in active returned by super."""
         _lo, act = super()._cached_or_fetch(cached_load_order, cached_active,
-                                            fix_lo)
+                                            fix_lo, rdata_mods)
         pl_changed = cached_active is not act # we fetched or requested update
         lo_changed = (pl_changed or cached_load_order is None or
                       self._loadorder_txt.do_update())
@@ -927,7 +938,7 @@ class AsteriskGame(_TextFileLo):
                  self._ccc_dirs) if self._ccc_filename else ()
         self.__cc_files = {p: _CCCFile(False, p) for p in paths}
 
-    def _cached_or_fetch(self, ca_load_order, ca_active, fix_lo):
+    def _cached_or_fetch(self, ca_load_order, ca_active, fix_lo, rdata_mods):
         """Read data from plugins.txt file once. If plugins.txt does not exist
         create it. Will *always* fetch both load order and active."""
         chs = ca_load_order, ca_active

Mopy/bash/basher/mod_links.py
@@ -1510,7 +1510,7 @@ class AFlipFlagLink(EnabledLink):
                 minfo.set_plugin_flags(set_flags)
             ##: HACK: forcing active refresh cause mods may be reordered and
             # we then need to sync order in skyrim's plugins.txt
-            lordata = bosh.modInfos.refreshLoadOrder()
+            lordata = bosh.modInfos.refreshLoadOrder(rdata_mods=RefrData())
             # This will have changed the plugin, so let BAIN know
             bosh.modInfos._notify_bain(
                 altered={p.abs_path for p in self.iselected_infos()})

Mopy/bash/load_order.py
@@ -320,7 +320,7 @@ def _update_cache(lord: LoList, acti_sorted: LoList, __index_move=0)->LordDiff:
             _new_entry()
     return lorddiff
 
-def refresh_lo(cached: bool, cached_active: bool): # one use - keep it so!
+def refresh_lo(cached: bool, cached_active: bool, rdata_mods): # one use - keep it so!
     """Refresh _cached_lord, reverting if locked to the saved one. If any of
     cached or cached_active are True, we will keep the cached values for
     those except if _game_handle.***_changed() respective methods return
@@ -350,7 +350,7 @@ def refresh_lo(cached: bool, cached_active: bool): # one use - keep it so!
             _cached_lord.activeOrdered if cached_active else None)
     try:
         fix_lo = FixInfo()
-        lo, active = _lo_handler.get_load_order(lo, active, fix_lo)
+        lo, active = _lo_handler.get_load_order(lo, active, fix_lo, rdata_mods)
         fix_lo.lo_deprint()
         ldiff = _update_cache(lo, active)
     except Exception:

commit e192d181aaf2f889fb2e4ae55cacb62d3ce0ba7a

Fixup for _enable returning None in Oblivion.esm menu:
Traceback (most recent call last):
  File "bash\bash.py", line 471, in main
  File "bash\bash.py", line 653, in _main
  File "bash\bash.py", line 647, in _main
  File "bash\basher\__init__.py", line 3777, in Init
  File "bash\balt.py", line 422, in _conversation_wrapper
  File "bash\basher\__init__.py", line 3582, in RefreshData
  File "bash\basher\__init__.py", line 189, in ShowPanel
  File "bash\basher\__init__.py", line 257, in ShowPanel
  File "bash\balt.py", line 1294, in setup_global_menu
  File "bash\gui\misc_components.py", line 489, in populate_immediately
  File "bash\gui\misc_components.py", line 518, in _populate_menu
  File "bash\balt.py", line 1269, in _populate_category
  File "bash\balt.py", line 1635, in AppendToMenu
  File "bash\balt.py", line 1646, in AppendToMenu
  File "bash\balt.py", line 1527, in AppendToMenu
  File "bash\balt.py", line 1687, in AppendToMenu
  File "bash\balt.py", line 1666, in AppendToMenu
TypeError: MenuItem.Enable(): argument 1 has unexpected type 'NoneType'

It happened for unknown Oblivion.esm size (...)

And some comments for #690

Mopy/bash/games_lo.py: regression fixup (the fix_lo case broke in
a81fe6be06e42b47573b3704693e2fe8e19e4003)

Mopy/bash/balt.py
@@ -1651,7 +1651,7 @@ class EnabledLink(ItemLink):
     a class attribute.
     """
 
-    def _enable(self):
+    def _enable(self) -> bool:
         """Override as needed to enable or disable the menu item (enabled
         by default)."""
         return True

Mopy/bash/bosh/__init__.py
@@ -3110,7 +3110,7 @@ class ModInfos(_AFileInfos):
         can_set = (set_version and curr_ver and set_version != curr_ver and
                    set_version in self._voAvailable and not (
                         move_to in self or move_to in self.corrupted))
-        if not do_swap: return can_set # we can/can't swap
+        if not do_swap: return bool(can_set) # we can/can't swap
         ren_data = RefrData()
         if not can_set:
             return ren_data

Mopy/bash/games_lo.py
@@ -503,12 +503,9 @@ class LoGame:
     # API: Exposed validation helpers - see load_order.py
     def lo_sort_key(self, *, ds=None, by_time=False):
         ds = self._mod_infos if ds is None else ds
-        def _key(fn):
-            is_m = self._game_handle.master_flags.sort_masters_key(ds[fn],self)
-            if by_time:
-                is_m = *is_m, ds[fn].ftime
-            return is_m
-        return _key
+        smk = self._game_handle.master_flags.sort_masters_key
+        return (lambda fn: (*smk(ds[fn], self), ds[fn].ftime)) if by_time \
+            else lambda fn: smk(ds[fn], self)
 
     def check_active_limit(self, acti_filtered, *, filter_list=None,
                            fix_active=None):
@@ -740,28 +737,6 @@ class INIGame(LoGame):
             return True # Assume order is important for the INI
         return super()._must_update_active(*args)
 
-def set_mtimes(wanted_lord, current_lord, fnkey_info: dict):
-    """Set the mtimes of the mods in current lord to match wanted_lord order.
-    set(wanted_lord) == set(current_lord) == mods is assumed."""
-    # mods's mtimes match the current lord's order - break conflicts
-    mods_it = map(fnkey_info.__getitem__, current_lord)
-    older = next(mods_it).ftime # initialize to older mod's ftime
-    for info in mods_it:
-        # mods_it is ordered in ftime so conflicts come in chunks
-        if older == (older := info.ftime):
-            # respace this and next mods in 60 sec intervals
-            for inf in (info, *mods_it):
-                older += 60.0
-                inf.setmtime(older, mark_redated=True)
-            break
-    restamp = []
-    # set(wanted_lord) == set(current_lord) - collect modification times
-    for ordered, mod in zip(wanted_lord, current_lord, strict=True):
-        if ordered != mod:
-            restamp.append((ordered, fnkey_info[mod].ftime))
-    for ordered, modification_time in restamp:
-        fnkey_info[ordered].setmtime(modification_time)
-
 class TimestampGame(LoGame):
     """Oblivion and other games where load order is set using modification
     times."""
@@ -774,17 +749,42 @@ class TimestampGame(LoGame):
 
     def _persist_if_changed(self, lord, previous_lord, *args, **kwargs):
         if previous_lord is None or previous_lord != lord:
-            current = self._calculate_mtime_order(modinfos := self._mod_infos)
-            set_mtimes(lord, current, modinfos)
+            self._set_mtimes(lord)
         super()._persist_if_changed(lord, previous_lord, *args, **kwargs)
 
     def _add_last(self, lord, added):
         if added:
-            lo_added = []
-            lo_new = [m for m in lord if m not in added or lo_added.append(m)]
-            lo_new = sorted([*lo_new, *lo_added], key=self.lo_sort_key())
-            set_mtimes(lo_new, lord, self._mod_infos)
+            lo_new = [] # if added mods are present in lord keep relative order
+            # rdata.to_add are already present in lord, fix_lo.lo_added are not
+            old = (m for m in lord if m not in added or ( # on boot to_add=lord
+                    lo_new.append(m) or added.remove(m)))
+            lo_new = [*old, *lo_new, *self._calculate_mtime_order(added)]
+            lo_new.sort(key=self.lo_sort_key()) # sort added master files first
+            self._set_mtimes(lo_new)
             lord[:] = lo_new
+
+    def _set_mtimes(self, wanted_lord):
+        """Set the mtimes of the mods in self._mod_infos to match wanted_lord
+        order. If set(wanted_lord) != set(modInfos) a ValueError is raised."""
+        current_lord = self._calculate_mtime_order(modinfos := self._mod_infos)
+        # mods's mtimes match the current lord's order - break conflicts
+        mods_it = map(modinfos.__getitem__, current_lord)
+        older = next(mods_it).ftime # initialize to older mod's ftime
+        for info in mods_it:
+            # mods_it is ordered in ftime so conflicts come in chunks
+            if older == (older := info.ftime):
+                # respace this and next mods in 60 sec intervals
+                for inf in (info, *mods_it):
+                    older += 60.0
+                    inf.setmtime(older, mark_redated=True)
+                break
+        restamp = []
+        # set(wanted_lord) == set(current_lord) - collect modification times
+        for ordered, mod in zip(wanted_lord, current_lord, strict=True):
+            if ordered != mod:
+                restamp.append((ordered, modinfos[mod].ftime))
+        for ordered, modification_time in restamp:
+            modinfos[ordered].setmtime(modification_time)
 
 class TextfileGame(LoGame):
     # If True, the game master (e.g. Skyrim.esm) must never be written to

scripts/merge_weblate_changes.py
@@ -66,6 +66,8 @@ Please clean up the rewritten {_WEBLATE_OUT_BRANCH} branch now. Tasks to do:
  - check each commit's message and author/co-authors
  - verify the rebased branch matches the remote.
  - add any manual commits needed (e.g. README updates, etc.)
+ - run the template updating script if you made any changes to any Python
+   file *with translations* on the rewritten branch
 Then"""
 def main(args):
     setup_log(_LOGGER, args)