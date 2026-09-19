wrye-bash/wrye-bash

commit b7469a482e330a54abc497bd3f184b9d332e0ed7

WIP aliases renames
- Mopy/bash/game/skyrim/records.py: qust_aliases - not sure what this is
about but certainly not an alias like the others.

Rest are renames of aliases public attributes - WIP, I left aliases
locals alone as well as the parser instance ones - those are the real
places they are used, need to stand out - maybe eliminate intermediate
fields?

Mopy/bash/basher/gui_patchers.py: _ci_aliases to be

Squash: gui_patchers

Mopy/bash/basher/gui_patchers.py:
- back from the future backwards compat
- default_aliases was unused really (?)

Mopy/bash/basher/gui_patchers.py
@@ -172,8 +172,6 @@ class _PatcherPanel(object):
 
 #------------------------------------------------------------------------------
 class _AliasesPatcherPanel(_PatcherPanel):
-    # CONFIG DEFAULTS
-    default_aliases = {}
     patcher_name = _(u'Alias Mod Names')
     patcher_desc = _(u'Specify mod aliases for reading CSV source files.')
 
@@ -197,15 +195,15 @@ class _AliasesPatcherPanel(_PatcherPanel):
         """Sets alias text according to current aliases."""
         self.gAliases.text_content = u'\n'.join([
             u'%s >> %s' % (alias_target, alias_repl)
-            for alias_target, alias_repl in sorted(self.aliases.items())])
+            for alias_target, alias_repl in sorted(self._ci_aliases.items())])
 
     def OnEditAliases(self):
         aliases_text = self.gAliases.text_content
-        self.aliases.clear()
+        self._ci_aliases.clear()
         for line in aliases_text.split(u'\n'):
             fields = map(unicode.strip,line.split(u'>>'))
             if len(fields) != 2 or not fields[0] or not fields[1]: continue
-            self.aliases[GPath(fields[0])] = GPath(fields[1])
+            self._ci_aliases[GPath(fields[0])] = GPath(fields[1])
         self.SetAliasText()
 
     #--Config Phase -----------------------------------------------------------
@@ -213,20 +211,20 @@ class _AliasesPatcherPanel(_PatcherPanel):
         """Get config from configs dictionary and/or set to default."""
         config = super(_AliasesPatcherPanel, self).getConfig(configs)
         #--Update old configs to use Paths instead of strings.
-        self.aliases = dict(# map(GPath, item) gives a list (item is a tuple)
+        self._ci_aliases = dict(# map(GPath, item) gives a list (item is a tuple)
             map(GPath, item) for item in
-            config.get('aliases', self.__class__.default_aliases).iteritems())
+            (config.get(u'aliases', {}) or config.get(b'aliases', {})).iteritems())
         return config
 
     def saveConfig(self, configs):
         """Save config to configs dictionary."""
         #--Toss outdated configCheck data.
         config = super(_AliasesPatcherPanel, self).saveConfig(configs)
-        config['aliases'] = self.aliases
+        config[u'aliases'] = self._ci_aliases
         return config
 
     def _log_config(self, conf, config, clip, log):
-        aliases = conf.get('aliases', {})
+        aliases = config.get(u'aliases', {}) or config.get(b'aliases', {})
         for mod, alias in aliases.iteritems():
             log(u'* __%s__ >> %s' % (mod, alias))
             clip.write(u'  %s >> %s\n' % (mod, alias))
@@ -234,7 +232,7 @@ class _AliasesPatcherPanel(_PatcherPanel):
     def get_patcher_instance(self, patch_file):
         """Set patch_file aliases dict"""
         if self.isEnabled:
-            patch_file.aliases = self.aliases
+            patch_file.pfile_aliases = self._ci_aliases
         return self.patcher_type(self.patcher_name, patch_file)
 
 #------------------------------------------------------------------------------

Mopy/bash/patcher/patchers/base.py
@@ -211,7 +211,7 @@ class ReplaceFormIDsPatcher(ListPatcher):
     def readFromText(self,textPath):
         """Reads replacement data from specified text file."""
         old_new,old_eid,new_eid = self.old_new,self.old_eid,self.new_eid
-        aliases = self.patchFile.aliases
+        aliases = self.patchFile.pfile_aliases
         with CsvReader(textPath) as ins:
             for fields in ins:
                 if len(fields) < 7 or fields[2][:2] != u'0x' or fields[6][:2] != u'0x': continue

Mopy/bash/game/oblivion/patcher/special.py
@@ -207,7 +207,7 @@ class CoblExhaustionPatcher(ListPatcher, _ExSpecialList):
 
     def readFromText(self, textPath):
         """Imports type_id_name from specified text file."""
-        aliases = self.patchFile.aliases
+        aliases = self.patchFile.pfile_aliases
         id_exhaustion = self.id_exhaustion
         with CsvReader(textPath) as ins:
             for fields in ins:
@@ -298,7 +298,7 @@ class MorphFactionsPatcher(ListPatcher, _ExSpecialList):
 
     def readFromText(self, textPath):
         """Imports id_info from specified text file."""
-        aliases = self.patchFile.aliases
+        aliases = self.patchFile.pfile_aliases
         id_info = self.id_info
         with CsvReader(textPath) as ins:
             for fields in ins:

Mopy/bash/game/skyrim/records.py
@@ -797,10 +797,10 @@ class MelVmad(MelBase):
             super(MelVmad.VmadHandlerQUST, self).load_frag(
                 record, ins, vmad_version, obj_format, read_id)
             # Then, load each alias
-            record.aliases = []
+            record.qust_aliases = []
             new_alias = self._alias_loader.make_new
             load_alias = self._alias_loader.load_frag
-            append_alias = record.aliases.append
+            append_alias = record.qust_aliases.append
             for x in xrange(ins.unpack(__unpacker, 2, read_id)[0]):
                 alias = new_alias()
                 load_alias(alias, ins, vmad_version, obj_format, read_id)
@@ -810,21 +810,21 @@ class MelVmad(MelBase):
             # Dump the regular fragments first
             out_data = super(MelVmad.VmadHandlerQUST, self).dump_frag(record)
             # Then, dump each alias
-            out_data += __packer(len(record.aliases))
+            out_data += __packer(len(record.qust_aliases))
             dump_alias = self._alias_loader.dump_frag
-            for alias in record.aliases:
+            for alias in record.qust_aliases:
                 out_data += dump_alias(alias)
             return out_data
 
         def map_fids(self, record, map_function, save=False):
             # No need to call parent, QUST fragments can't contain fids
             map_alias = self._alias_loader.map_fids
-            for alias in record.aliases:
+            for alias in record.qust_aliases:
                 map_alias(alias, map_function, save)
 
         @property
         def used_slots(self):
-            return [u'aliases'] + super(
+            return [u'qust_aliases'] + super(
                 MelVmad.VmadHandlerQUST, self).used_slots
 
     ##: Identical to VmadHandlerINFO + some overrides
@@ -4072,7 +4072,7 @@ class MreQust(MelRecord):
             ),
         ),
         MelBase('ANAM','aliasMarker'),
-        MelGroups('aliases',
+        MelGroups(u'qust_aliases',
             MelUnion({
                 b'ALST': MelUInt32(b'ALST', u'aliasId'),
                 b'ALLS': MelUInt32(b'ALLS', u'aliasId'),
@@ -4143,7 +4143,7 @@ class MreQust(MelRecord):
             b'CTDA|CIS1|CIS2|FNAM|NNAM|QSTA': u'objectives',
         },
         b'ANAM': {
-            b'CTDA|CIS1|CIS2|FNAM': u'aliases',
+            b'CTDA|CIS1|CIS2|FNAM': u'qust_aliases',
             # ANAM is required, so piggyback off of it here to resolve QSTA
             b'QSTA': (u'targets', {
                 b'CTDA|CIS1|CIS2': u'targets',

Mopy/bash/patcher/patch_files.py
@@ -124,9 +124,9 @@ class PatchFile(ModFile):
                 message += mname.s
             log(message)
         #--Load Mods and error mods
-        if self.aliases:
+        if self.pfile_aliases:
             log.setHeader(u'= ' + _(u'Mod Aliases'))
-            for alias_target, alias_repl in sorted(self.aliases.iteritems()):
+            for alias_target, alias_repl in sorted(self.pfile_aliases.iteritems()):
                 log(u'* %s >> %s' % (alias_target, alias_repl))
 
     def init_patchers_data(self, patchers, progress):
@@ -150,7 +150,7 @@ class PatchFile(ModFile):
         self.longFids = True
         self.keepIds = set()
         # Aliases from one mod name to another. Used by text file patchers.
-        self.aliases = {}
+        self.pfile_aliases = {}
         self.mergeIds = set()
         self.loadErrorMods = []
         self.worldOrphanMods = []

Mopy/bash/patcher/patchers/preservers.py
@@ -96,7 +96,7 @@ class _APreserver(ImportPatcher):
         """Parses CSV files. Only called if _csv_parser is set. Override as
         needed and call _process_csv_sources until parser ABC is done."""
         parser_instance = self._csv_parser()
-        parser_instance.aliases = self.patchFile.aliases
+        parser_instance.aliases = self.patchFile.pfile_aliases
         parser_instance.called_from_patcher = True
         for src_path in self.csv_srcs:
             try:

commit 33ef0cadf1c7f89edbdfe540787be0c8b9a8f4b2

Make log_config static:
log_config and _log_config become classmethods, so Mod_ListPatchConfig
no longer creates a temporary PatchFile and gui patcher instances just
to log the config. The Tweaker loads the tweaks config via the new
_tweaks_config classmethod - IndexingTweak accepts a None bashed patch
for that case.

Fix for logging the aliases of Alias Plugin Names:

_log_config read the aliases from the whole BP config instead of from
the patcher's own conf, so the config log never listed any. Broke in
b7469a482e330a54abc497bd3f184b9d332e0ed7, which changed
conf.get('aliases') to config.get(u'aliases').

Core #312

Mopy/bash/basher/gui_patchers.py
@@ -135,18 +135,20 @@ class _PatcherPanel(Lazy, PanelWin):
         config[u'isEnabled'] = self.isEnabled
         return config # return the config dict for this patcher to further edit
 
-    def log_config(self, config, clip, log):
-        ckey = self.__class__._config_key
+    @classmethod
+    def log_config(cls, config, clip, log):
+        ckey = cls._config_key
         # Check if the patcher is in the config and was enabled
         if ckey not in config or not (conf := config[ckey]).get('isEnabled'):
             return
-        humanName = self.__class__.patcher_name
+        humanName = cls.patcher_name
         log.setHeader(f'== {humanName}')
         clip.write('\n')
         clip.write(f'== {humanName}\n')
-        self._log_config(conf, config, clip, log)
+        cls._log_config(conf, config, clip, log)
 
-    def _log_config(self, conf, config, clip, log):
+    @classmethod
+    def _log_config(cls, conf, config, clip, log):
         items = conf.get(u'configItems', [])
         if not items:
             log(u' ')
@@ -225,8 +227,9 @@ class _AliasesPatcherPanel(_PatcherPanel):
         config[u'aliases'] = self._fn_aliases
         return config
 
-    def _log_config(self, conf, config, clip, log):
-        aliases = config.get(u'aliases', {})
+    @classmethod
+    def _log_config(cls, conf, config, clip, log):
+        aliases = conf.get('aliases', {})
         for mod, alias in aliases.items():
             log(f'* __{mod}__ >> {alias}')
             clip.write(f'  {mod} >> {alias}\n')
@@ -798,11 +801,16 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
     def _getConfig(self, configs):
         """Get config from configs dictionary and/or set to default."""
         config = super()._getConfig(configs)
-        all_tweaks = self.__class__.patcher_type.tweak_instances(self._bp)
+        self._all_tweaks = self._curr_tweaks = self._tweaks_config(config,
+                                                                   self._bp)
+        return config
+
+    @classmethod
+    def _tweaks_config(cls, config, bashed_patch=None):
+        all_tweaks = cls.patcher_type.tweak_instances(bashed_patch)
         for tweak in all_tweaks:
             tweak.init_tweak_config(config)
-        self._all_tweaks = self._curr_tweaks = all_tweaks
-        return config
+        return all_tweaks
 
     def saveConfig(self, configs):
         """Save config to configs dictionary."""
@@ -811,9 +819,10 @@ class _TweakPatcherPanel(_ChoiceMenuMixin, _PatcherPanel):
             tweak.save_tweak_config(config)
         return config
 
-    def _log_config(self, conf, config, clip, log):
-        self._getConfig(config) # set self._all_tweaks and load their config
-        for tweak in self._all_tweaks:
+    @classmethod
+    def _log_config(cls, conf, config, clip, log):
+        all_tweaks = cls._tweaks_config(conf) # load tweaks config
+        for tweak in all_tweaks:
             if tweak.tweak_key in conf:
                 enabled, value = conf.get(tweak.tweak_key, (False, u''))
                 list_label = tweak.getListLabel().replace('[[', '[').replace(
@@ -942,9 +951,10 @@ class _ListsMergerPanel(_ChoiceMenuMixin, _ListPatcherPanel):
         #--Show/Destroy Menu
         links.popup_menu(gui_li, None)
 
-    def _log_config(self, conf, config, clip, log):
+    @classmethod
+    def _log_config(cls, conf, config, clip, log):
         conf_choices = conf.get('configChoices', {})
-        for item in (self.getItemLabel(i, conf_choices) for i in conf.get(
+        for item in (cls.getItemLabel(i, conf_choices) for i in conf.get(
                 'configItems', [])):
             log(f'. __{item}__')
             clip.write(f'    {item}\n')

Mopy/bash/basher/mod_links.py
@@ -1120,10 +1120,8 @@ class Mod_ListPatchConfig(_Mod_BP_Link):
         clip.write(u'== '+_(u'Patch Mode')+u'\n')
         log(u'Python')
         clip.write(u' ** Python\n')
-        temp_bp = PatchFile(bp_parent_info, bosh.modInfos)
         for ptype in gpatcher_types:
-            patcher = ptype(temp_bp)
-            patcher.log_config(config, clip, log)
+            ptype.log_config(config, clip, log)
         #-- Show log
         copy_text_to_clipboard(clip.getvalue())
         log_text = log.out.getvalue()

Mopy/bash/patcher/base.py
@@ -303,7 +303,7 @@ class MultiTweakItem:
     # If True, tweak_key will be shown in the 'custom value' popup
     show_key_for_custom = False
 
-    def __init__(self, bashed_patch):
+    def __init__(self, bashed_patch): ##: only used in IndexingTweaker - drop!
         # Don't check tweak_log_msg, settings tweaks don't use it
         for tweak_attr in (u'tweak_name', u'tweak_tip', u'tweak_key',
                            u'tweak_log_msg'):

Mopy/bash/patcher/patchers/base.py
@@ -41,10 +41,11 @@ class IndexingTweak(MultiTweakItem):
 
     def __init__(self, bashed_patch):
         super().__init__(bashed_patch)
-        bashed_patch.update_read_factories(self._index_sigs,
-            bashed_patch.merged_or_loaded_ord) ##: all_plugins?
-        self._indexed_records = defaultdict(dict)
-        self._tweak_bp = bashed_patch
+        if bashed_patch is not None: # else we want to log the config
+            bashed_patch.update_read_factories(self._index_sigs,
+                bashed_patch.merged_or_loaded_ord) ##: all_plugins?
+            self._indexed_records = defaultdict(dict)
+            self._tweak_bp = bashed_patch
 
     def prepare_for_tweaking(self, patch_file):
         for fn_plugin in patch_file.merged_or_loaded_ord: ##: all_plugins?